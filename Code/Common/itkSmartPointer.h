741800279e426fe6aa8803ba1a7a369398156361
Will Schroeder <will.schroeder@kitware.com>
2000-03-15 12:37:19 -0500

    ENH:refined the class

--- Code/Common/itkSmartPointer.h
+++ Code/Common/itkSmartPointer.h
@@ -1,102 +1,145 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit (ITK)
+  Module:    itkSmartPointer.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+Copyright (c) 2000 National Library of Medicine
+All rights reserved.
+
+See COPYRIGHT.txt for copyright details.
+
+=========================================================================*/
 #ifndef ITK_SMART_POINTER_H
 #define ITK_SMART_POINTER_H
 
-// Insert block comment here Copyright, doxegen stuff
-
-// To compile / test this class
-// Windows: cl itkSmartPointerTest.cxx; .\itkSmartPointerTest.exe
-// linux:   c++ itkSmartPointerTest.cxx ./a.out
-// other:   CCcompiler itkSmartPointerTest.cxx  ./a.out
+/// implements reference counting
+/** itkSmartPointer implements reference counting by overloading
+ *  operator -> (and *) among others. This allows natural interface
+ *  to the class referred to by the pointer without having to invoke
+ *  special Register()/UnRegister() methods directly.
+ *
+ *  To compile / test this class
+ *  Windows: cl itkSmartPointerTest.cxx; .\itkSmartPointerTest.exe
+ *  linux:   c++ itkSmartPointerTest.cxx ./a.out
+ *  other:   CCcompiler itkSmartPointerTest.cxx  ./a.out
+ */
 
 template <class T>
 class itkSmartPointer 
 {
 public:
-
+  /** Constructor */
   itkSmartPointer () 
-  {
+    {
     m_Pointer = 0;
-  }
+    }
 
+  /** Const constructor */
   itkSmartPointer (const itkSmartPointer<T> &p)
-  { 
+    { 
     m_Pointer = p.m_Pointer; 
     this->Register(); 
-  }
+    }
   
+  /** Constructor to pointer p */
   itkSmartPointer (T *p)
-  { 
+    { 
     m_Pointer = p; 
     this->Register(); 
-  }				
+    }                             
 
+  /** Destructor */
   ~itkSmartPointer ()
-  {
+    {
     this->UnRegister();
-  }
+    }
 
+  /** Overload operator -> */
   T *operator -> () const
-  { 
+    { 
     return m_Pointer; 
-  }
+    }
 
-  T *ptr () const 
-  { 
+  /** Access funtion to pointer */
+  T *GetPointer () const 
+    { 
     // This returns the pointer.  
     return m_Pointer; 
-  }
+    }
   
-    // Comparison of pointers
+  /** Comparison of pointers. Less than comparison. */
   bool operator < (const itkSmartPointer &r)
-  { return (void*)m_Pointer < (void*) r.m_Pointer; }
+    { 
+    return (void*)m_Pointer < (void*) r.m_Pointer; 
+    }
 
+  /** Comparison of pointers. Greater than comparison. */
   bool operator > (const itkSmartPointer &r)
-  { return (void*)m_Pointer > (void*) r.m_Pointer; }
+    { 
+    return (void*)m_Pointer > (void*) r.m_Pointer; 
+    }
 
+  /** Comparison of pointers. Less than or equal to comparison. */
   bool operator <= (const itkSmartPointer &r)
-  { return (void*)m_Pointer <= (void*) r.m_Pointer; }
+    { 
+    return (void*)m_Pointer <= (void*) r.m_Pointer; 
+    }
 
+  /** Comparison of pointers. Greater than or equal to comparison. */
   bool operator >= (const itkSmartPointer &r)
-  { return (void*)m_Pointer >= (void*) r.m_Pointer; }
+    { 
+    return (void*)m_Pointer >= (void*) r.m_Pointer; 
+    }
 
+  /** Overload operator assignment. */
   itkSmartPointer &operator = (const itkSmartPointer &r)
-  { 
-    return this->operator = (r.ptr()); 
-  }
+    { 
+    return this->operator = (r.GetPointer()); 
+    }
   
+  /** Overload operator assignment. */
   itkSmartPointer &operator = (T *r)
-  {								 
+    {                                                              
     if (m_Pointer != r)
       {
-	this->UnRegister();
-	m_Pointer = r;
-	this->Register();
+      this->UnRegister();
+      m_Pointer = r;
+      this->Register();
       }
     return *this;
-  }
+    }
 
-  // DeRegistererencing the pointer
-  T &operator * () const { return *m_Pointer; }
+  /** Return object by reference */
+  T &operator * () const 
+    { 
+    return *m_Pointer; 
+    }
 
-  // Cast to T * 
-  operator T * () const { return m_Pointer; }
+  /** Return pointer to object */
+  operator T * () const 
+    { 
+    return m_Pointer; 
+    }
 
 private:
-
   void Register()
     { 
-      if(m_Pointer)
-	{
-	m_Pointer->Register();
-	}
+    if (m_Pointer)
+      {
+      m_Pointer->Register();
+      }
     }
   
   void UnRegister()
     {
-      if(m_Pointer)
-	{
-	m_Pointer->UnRegister();
-	}
+    if (m_Pointer)
+      {
+      m_Pointer->UnRegister();
+      }
     }
   
 private:
--- Code/Common/itkSmartPointerTest.cxx
+++ Code/Common/itkSmartPointerTest.cxx
@@ -1,6 +1,20 @@
-#include "iostream.h"
-#include "itkSmartPointer.h"
+/*=========================================================================
 
+  Program:   Insight Segmentation & Registration Toolkit (ITK)
+  Module:    itkSmartPointerTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+Copyright (c) 2000 National Library of Medicine
+All rights reserved.
+
+See COPYRIGHT.txt for copyright details.
+
+=========================================================================*/
+#include <iostream>
+#include "itkSmartPointer.h"
 
 class itkTestObject
 {
@@ -9,34 +23,37 @@ public:
   static itkTestObject::Pointer New();
   void Register()
     {
-      cout << "Register " << *this << endl;
-      
-      m_ReferenceCount++;
+    std::cout << "Register " << *this << " count:" 
+              << (m_ReferenceCount+1) << "  " << std::endl;
+    m_ReferenceCount++;
     }
   void UnRegister()
     {
-      cout << "UnRegister " << *this << endl;
+    std::cout << "UnRegister " << *this << " count:" 
+              << (m_ReferenceCount-1) << "  " << std::endl;
       
-      m_ReferenceCount--;
-      if(m_ReferenceCount == 0)
-	{
-	delete this;
-	}
+    m_ReferenceCount--;
+    if ( m_ReferenceCount == 0 )
+      {
+      delete this;
+      }
     }
-  inline friend ostream &operator << (ostream &os, itkTestObject const& o) 
+  inline friend std::ostream &operator << (std::ostream &os, 
+                                           itkTestObject const& o) 
     {
-      os << "itkTestObject " << (void*)&o << " " << o.m_ReferenceCount; 
-      return os;
+    os << "itkTestObject " << (void*)&o << " " << o.m_ReferenceCount; 
+    return os;
     }
   
 protected:
   itkTestObject() 
-    { 
-      cout << "construct itkTestObject " << *this << endl; 
+    {
+    m_ReferenceCount = 0;
+    std::cout << "construct itkTestObject " << *this << std::endl; 
     }
   ~itkTestObject() 
     {
-      cout << "destruct itkTestObject " << *this << endl; 
+    std::cout << "destruct itkTestObject " << *this << std::endl; 
     }
 
 private:
@@ -52,25 +69,38 @@ itkTestObject::Pointer itkTestObject::New()
 
 int main()
 {
+  std::cout <<"second test" << std::endl;
+  {
   itkTestObject::Pointer o1 = itkTestObject::New();
-  cout << "o1 " << o1 << endl;
+  std::cout << "o1 " << o1 << std::endl;
   itkTestObject::Pointer o2 = itkTestObject::New();
-  cout << "o2 " << o2 << endl;
+  std::cout << "o2 " << o2 << std::endl;
   itkTestObject::Pointer o3 = itkTestObject::New();
-  cout << "o3 " << o3 << endl;
+  std::cout << "o3 " << o3 << std::endl;
   itkTestObject::Pointer o4 = itkTestObject::New();
-  cout << "o4 " << o4 << endl;
+  std::cout << "o4 " << o4 << std::endl;
   
   o1 = o2;
   o2 = o3;
   o4 = o1;
-  if(o1 < o2)
+  if ( o1 < o2 )
     {
-    cout << "o1 is < o2 " << o1 << " " << o2 << endl;
+    std::cout << "o1 is < o2 " << o1 << " " << o2 << std::endl;
     }
   else
     {
-    cout << "o1 is not < o2 " << o1 << " " << o2 << endl;
+    std::cout << "o1 is not < o2 " << o1 << " " << o2 << std::endl;
     }
+  }
+  std::cout <<"end second test" << std::endl << std::endl;
+
+  std::cout <<"first test" << std::endl;
+  {
+  itkTestObject::Pointer o1 = itkTestObject::New();
+  }
+  std::cout <<"end first test" << std::endl << std::endl;
+  
   return 0;
 }
+
+
