d1571bf0a955175356759da8c163c54bc575692b
Brad King <brad.king@kitware.com>
2000-06-20 07:35:09 -0400

    Adding tetrahedron cell type.

---
+++ Code/Common/itkTetrahedronCell.cxx
@@ -0,0 +1,166 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkTetrahedronCell.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) 2000 National Library of Medicine
+  All rights reserved.
+
+  See COPYRIGHT.txt for copyright details.
+
+=========================================================================*/
+// #include "itkTetrahedronCell.h"
+
+
+/**
+ *
+ */
+template <typename TPixelType, typename TMeshType>
+itkTetrahedronCell< TPixelType , TMeshType >::Pointer
+itkTetrahedronCell< TPixelType , TMeshType >
+::New(void)
+{
+  return new Self;
+}
+
+
+/**
+ * Get the number of boundary entities of the given dimension.
+ */
+template <typename TPixelType, typename TMeshType>
+itkTetrahedronCell< TPixelType , TMeshType >::CellFeatureCount
+itkTetrahedronCell< TPixelType , TMeshType >
+::GetNumberOfBoundaryEntities(int dimension)
+{
+  switch (dimension)
+    {
+    case 0: return GetNumberOfVertices();
+    case 1: return GetNumberOfEdges();
+    case 2: return GetNumberOfFaces();
+    default: return 0;
+    }
+}
+
+
+/**
+ * Standard itkCell API:
+ * Set the cell's internal point list to the list of identifiers provided.
+ */
+template <typename TPixelType, typename TMeshType>
+void
+itkTetrahedronCell< TPixelType , TMeshType >
+::SetCellPoints(PointIdentifier *ptList)
+{
+  for(int i=0; i < NumberOfPoints ; ++i)
+    m_PointIds[i] = ptList[i];
+}
+
+
+/**
+ * Tetrahedron-specific:
+ * Get the number of vertices defining the tetrahedron.
+ */
+template <typename TPixelType, typename TMeshType>
+itkTetrahedronCell< TPixelType , TMeshType >::CellFeatureCount
+itkTetrahedronCell< TPixelType , TMeshType >
+::GetNumberOfVertices(void)
+{
+  return NumberOfVertices;
+}
+
+
+/**
+ * Tetrahedron-specific:
+ * Get the number of edges defined for the tetrahedron.
+ */
+template <typename TPixelType, typename TMeshType>
+itkTetrahedronCell< TPixelType , TMeshType >::CellFeatureCount
+itkTetrahedronCell< TPixelType , TMeshType >
+::GetNumberOfEdges(void)
+{
+  return NumberOfEdges;
+}
+
+
+/**
+ * Tetrahedron-specific:
+ * Get the number of faces defined for the tetrahedron.
+ */
+template <typename TPixelType, typename TMeshType>
+itkTetrahedronCell< TPixelType , TMeshType >::CellFeatureCount
+itkTetrahedronCell< TPixelType , TMeshType >
+::GetNumberOfFaces(void)
+{
+  return NumberOfFaces;
+}
+
+
+/**
+ * Tetrahedron-specific:
+ * Get the vertex specified by the given cell feature ID.
+ */
+template <typename TPixelType, typename TMeshType>
+itkTetrahedronCell< TPixelType , TMeshType >::Vertex::Pointer
+itkTetrahedronCell< TPixelType , TMeshType >
+::GetCellVertex(CellFeatureID vertexId)
+{
+  Vertex::Pointer vert(Vertex::New());
+  vert->SetCellPoint(0, m_PointIds[vertexId]);
+  
+  return vert;
+}
+
+
+/**
+ * Tetrahedron-specific:
+ * Get the edge specified by the given cell feature ID.
+ */
+template <typename TPixelType, typename TMeshType>
+itkTetrahedronCell< TPixelType , TMeshType >::Edge::Pointer
+itkTetrahedronCell< TPixelType , TMeshType >
+::GetCellEdge(CellFeatureID edgeId)
+{
+  Edge::Pointer edge(Edge::New());
+
+  edge->SetCellPoint(0, m_PointIds[ m_Edges[edgeId][0] ]);
+  edge->SetCellPoint(1, m_PointIds[ m_Edges[edgeId][1] ]);
+  
+  return edge;
+}
+
+
+/**
+ * Tetrahedron-specific:
+ * Get the face specified by the given cell feature ID.
+ */
+template <typename TPixelType, typename TMeshType>
+itkTetrahedronCell< TPixelType , TMeshType >::Face::Pointer
+itkTetrahedronCell< TPixelType , TMeshType >
+::GetCellFace(CellFeatureID faceId)
+{
+  Face::Pointer face(Face::New());
+  
+  face->SetCellPoint(0, m_PointIds[ m_Faces[faceId][0] ]);
+  face->SetCellPoint(1, m_PointIds[ m_Faces[faceId][1] ]);
+  face->SetCellPoint(2, m_PointIds[ m_Faces[faceId][2] ]);  
+  
+  return face;
+}
+
+
+/**
+ * Define the tetrahedron's topology data.
+ */
+template <typename TPixelType, typename TMeshType>
+const int
+itkTetrahedronCell< TPixelType , TMeshType >
+::m_Edges[6][2] = { {0,1}, {1,2}, {2,0}, {0,3}, {1,3}, {2,3} };
+
+template <typename TPixelType, typename TMeshType>
+const int
+itkTetrahedronCell< TPixelType , TMeshType >
+::m_Faces[4][3] = { {0,1,3}, {1,2,3}, {2,0,3}, {0,2,1} };
---
+++ Code/Common/itkTetrahedronCell.h
@@ -0,0 +1,123 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkTetrahedronCell.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) 2000 National Library of Medicine
+  All rights reserved.
+
+  See COPYRIGHT.txt for copyright details.
+
+=========================================================================*/
+#ifndef __itkTetrahedronCell_h
+#define __itkTetrahedronCell_h
+
+#include "itkCell.h"
+#include "itkTriangleCell.h"
+
+/**
+ * itkTetrahedronCell represents a tetrahedron for itkMesh
+ */
+
+template <
+  /**
+   * The type associated with a point, cell, or boundary for use in storing
+   * its data.
+   */
+  typename TPixelType,
+
+  /**
+   * Type information of mesh containing cell.
+   */
+  typename TMeshType = itkMeshTypeDefault
+  >
+class itkTetrahedronCell: public itkCell< TPixelType , TMeshType >
+{
+public:
+  /**
+   * Smart pointer typedef support.
+   */
+  typedef itkTetrahedronCell        Self;
+  typedef itkSmartPointer<Self>  Pointer;
+
+  /**
+   * The type of our cell base class.
+   */
+  typedef itkCell< TPixelType , TMeshType >        Cell;
+  
+  /**
+   * The type of cells for this tetrahedron's vertices, edges, and faces.
+   */
+  typedef itkVertexCell< TPixelType , TMeshType >    Vertex;
+  typedef itkLineCell< TPixelType , TMeshType >      Edge;
+  typedef itkTriangleCell< TPixelType , TMeshType >  Face;
+  
+  /**
+   * Tetrahedron-specific topology numbers.
+   */
+  enum { NumberOfPoints   = 4,
+         NumberOfVertices = 4,
+         NumberOfEdges    = 6,
+         NumberOfFaces    = 4 };
+
+  /**
+   * Implement the standard cell API.
+   */
+  static Pointer New(void);
+  virtual CellFeatureCount GetNumberOfBoundaryEntities(int dimension);
+  virtual void SetCellPoints(PointIdentifier *ptList);
+
+  /**
+   * Tetrahedron-specific interface.
+   */
+  
+  virtual CellFeatureCount GetNumberOfVertices(void);
+  virtual CellFeatureCount GetNumberOfEdges(void);
+  virtual CellFeatureCount GetNumberOfFaces(void);
+
+  /**
+   * Get the cell vertex corresponding to the given ID.
+   * The ID can range from 0 to GetNumberOfVertices()-1.
+   */  
+  virtual Vertex::Pointer GetCellVertex(CellFeatureID);
+
+  /**
+   * Get the cell edge corresponding to the given ID.
+   * The ID can range from 0 to GetNumberOfEdges()-1.
+   */  
+  virtual Edge::Pointer GetCellEdge(CellFeatureID);  
+
+  /**
+   * Get the cell face corresponding to the given ID.
+   * The ID can range from 0 to GetNumberOfFaces()-1.
+   */  
+  virtual Face::Pointer GetCellFace(CellFeatureID);  
+
+  /**
+   * Standard part of itkObject class.  Used for debugging output.
+   */
+  itkTypeMacro(itkTetrahedronCell, itkCell);
+
+protected:
+  /**
+   * Allocate number of points needed for this cell type.
+   */
+  itkTetrahedronCell(): Cell(NumberOfPoints) {}  
+  
+  /**
+   * Tetrahedron topology data.
+   */
+  static const int m_Edges[6][2];
+  static const int m_Faces[4][3];
+};
+
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkTetrahedronCell.cxx"
+#endif
+
+#endif
