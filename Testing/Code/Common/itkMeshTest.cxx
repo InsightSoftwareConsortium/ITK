875c2f3b0f57709c50cf365b21b03ea44989f39d
Brad King <brad.king@kitware.com>
2000-06-20 07:32:19 -0400

    Changed mesh test to use tetrahedron cell.

--- Testing/Code/Common/itkMeshTest.cxx
+++ Testing/Code/Common/itkMeshTest.cxx
@@ -2,26 +2,28 @@
 #include <string>
 
 #include "itkMesh.h"
-#include "itkTriangleCell.h"
+#include "itkTetrahedronCell.h"
 
 typedef itkMesh<int>          Mesh;
-typedef itkTriangleCell<int>  TriangleCell;
+typedef itkTetrahedronCell<int>  TetraCell;
 typedef Mesh::Point           Point;
 
 int main(void)
 {
   double testPointCoords0[Point::PointDimension] = {2,3,4};
   double testPointCoords1[Point::PointDimension] = {3,3,3};
-  double testPointCoords2[Point::PointDimension] = {4,4,2};
-  unsigned long testPointList[3] = {0,1,2};  
+  double testPointCoords2[Point::PointDimension] = {4,3,2};
+  double testPointCoords3[Point::PointDimension] = {5,6,4};
+  unsigned long testPointList[4] = {0,1,2,3};
   
   Mesh::Pointer mesh(Mesh::New());  
 
   mesh->SetPoint(0, Point(testPointCoords0));
   mesh->SetPoint(1, Point(testPointCoords1));
   mesh->SetPoint(2, Point(testPointCoords2));
+  mesh->SetPoint(3, Point(testPointCoords3));
 
-  TriangleCell::Pointer testCell(TriangleCell::New());
+  TetraCell::Pointer testCell(TetraCell::New());
 
   testCell->SetCellPoints(testPointList);
   
