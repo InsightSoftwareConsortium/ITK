52a1076cd2c894d6e482ba5918b3595143ea858a
Luis Ibanez <luis.ibanez@kitware.com>
2000-09-14 07:44:55 -0400

    ImageLinearIterator is an Iterator type designed to go along a preselected direction in an image. It is useful for process that need line by line access to pixels

---
+++ Code/Common/itkImageLinearIterator.h
@@ -0,0 +1,147 @@
+/*==========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkImageLinearIterator.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2000 National Library of Medicine
+  All rights reserved.
+
+  See COPYRIGHT.txt for copyright details.
+  
+==========================================================================*/
+#ifndef __itkImageLinearIterator_h
+#define __itkImageLinearIterator_h
+
+#include "itkImageIteratorWithIndex.h"
+
+namespace itk
+{
+
+/**
+ * \class ImageLinearIterator
+ * \brief Multi-dimensional image iterator which only walks a region.
+ * 
+ * ImageLinearIterator is a templated class to represent a multi-dimensional
+ * iterator. ImageLinearIterator is templated over the dimension of the image
+ * and the data type of the image. ImageLinearIterator is constrained to walk
+ * only within the specified region.
+ *
+ * ImageLinearIterator is a multi-dimensional iterator, requiring more
+ * information be specified before the iterator can be used than conventional
+ * iterators. Whereas the std::vector::iterator from the STL only needs to be
+ * passed a pointer to establish the iterator, the multi-dimensional image
+ * iterator needs a pointer, the size of the buffer, the size of the region,
+ * the start index of the buffer, and the start index of the region. To gain
+ * access to this information, ImageLinearIterator holds a reference to the
+ * image over which it is traversing.
+ *
+ * ImageLinearIterator assumes a particular layout of the image data. The
+ * is arranged in a 1D array as if it were [][][][slice][row][col] with
+ * Index[0] = col, Index[1] = row, Index[2] = slice, etc.
+ *
+ * operator++ provides a simple syntax for walking around a region of
+ * a multidimensional image. operator++ iterates across a row, constraining
+ * the movement to within a region of image. When the iterator reaches
+ * the boundary of the region along a row, the iterator automatically
+ * wraps to the next row, starting at the first pixel in the row that is
+ * part of the region. This allows for simple processing loops of the form:
+ *
+ *      for (it = image->RegionBegin(); it != image->RegionEnd(); ++it)
+ *         {
+ *         *it += 100.0;
+ *         }
+ *
+ */
+template<typename TPixel, unsigned int VImageDimension=2>
+class ImageLinearIterator : public ImageIteratorWithIndex<TPixel, VImageDimension>
+{
+public:
+  /**
+   * Standard "Self" typedef.
+   */
+  typedef ImageLinearIterator Self;
+
+  /** 
+   * Index typedef support. While this was already typdef'ed in the superclass
+   * it needs to be redone here for this subclass to compile properly with gcc.
+   * Note that we have to rescope Index back to itk::Index to that is it not
+   * confused with ImageIterator::Index.
+   */
+  typedef itk::Index<VImageDimension> Index;
+
+  /**
+   * Image typedef support. While this was already typdef'ed in the superclass
+   * it needs to be redone here for this subclass to compile properly with gcc.
+   * Note that we have to rescope Index back to itk::Index to that is it not
+   * confused with ImageIterator::Index.
+   */
+  typedef itk::Image<TPixel, VImageDimension> Image;
+
+  /**
+   * Default constructor. Needed since we provide a cast constructor.
+   */
+  ImageLinearIterator() : ImageIteratorWithIndex<TPixel, VImageDimension>() {}
+  
+  /**
+   * Constructor establishes an iterator to walk a particular image and a
+   * particular region of that image.
+   */
+  ImageLinearIterator(const SmartPointer<Image> &ptr,
+                      const Index &start,
+                      const unsigned long size[VImageDimension])
+    : ImageIteratorWithIndex<TPixel, VImageDimension>(ptr, start, size) {}
+
+  /**
+   * Constructor that can be used to cast from an ImageIterator to an
+   * ImageLinearIterator. Many routines return an ImageIterator but for a
+   * particular task, you may want an ImageLinearIterator.  Rather than
+   * provide overloaded APIs that return different types of Iterators, itk
+   * returns ImageIterators and uses constructors to cast from an
+   * ImageIterator to a ImageLinearIterator.
+   */
+  ImageLinearIterator( const ImageIteratorWithIndex<TPixel, VImageDimension> &it)
+    { this->ImageIteratorWithIndex<TPixel, VImageDimension>::operator=(it); }
+
+  /**
+   * Go to the next line
+   * \sa operator++
+   * \sa EndOfLine
+   * \sa End
+   */
+  void NextLine(void);
+
+  /**
+   * Test if the index is at the end of line
+   */
+  bool IsAtEndOfLine(void);
+
+
+  /**
+   * Set the direction of movement
+   */
+  void SetDirection(unsigned int direction) ;
+
+
+  /**
+   * Increment (prefix) the selected dimension.
+   * No bounds checking is performed. 
+   * \sa GetIndex
+   */
+  Self & operator++();
+
+
+private:
+    unsigned long  m_Jump;
+    unsigned int   m_Direction;
+};
+
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkImageLinearIterator.txx"
+#endif
+
+#endif 
---
+++ Code/Common/itkImageLinearIterator.txx
@@ -0,0 +1,113 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkImageLinearIterator.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) 2000 National Library of Medicine
+  All rights reserved.
+
+  See COPYRIGHT.txt for copyright details.
+
+=========================================================================*/
+
+// #include "itkImageRegionSimpleIterator.h"
+
+namespace itk
+{
+
+
+
+
+//----------------------------------------------------------------------
+//  Constructor
+//----------------------------------------------------------------------
+template<class TPixel, unsigned int VImageDimension>
+void 
+ImageLinearIterator<TPixel, VImageDimension>
+::NextLine(void)
+{
+
+  m_PositionIndex[m_Direction] = m_BeginIndex[m_Direction];   
+  m_Position -= m_OffsetTable[ m_Direction+1 ]; 
+  
+
+  for( unsigned int n=0; n<VImageDimension; n++ )
+  {
+
+    m_Remaining = false;
+    
+    if( n == m_Direction ) 
+    {
+      continue;
+    }
+    
+    m_PositionIndex[ n  ]++;
+    if( m_PositionIndex[n] < m_Size[n] )
+    {
+      m_Position += m_OffsetTable[ n ];
+      m_Remaining = true;
+      break;
+    }
+    else 
+    {
+      m_Position -= m_OffsetTable[ n+1 ] - m_OffsetTable[ n ];
+      m_PositionIndex[ n ] = m_BeginIndex[ n ]; 
+    }
+  }
+}
+
+
+
+
+//----------------------------------------------------------------------
+//  Test for end of line
+//----------------------------------------------------------------------
+template<class TPixel, unsigned int VImageDimension>
+bool 
+ImageLinearIterator<TPixel, VImageDimension>
+::IsAtEndOfLine(void) 
+{
+  return m_PositionIndex[m_Direction] >= m_Size[m_Direction];
+}
+
+
+
+//----------------------------------------------------------------------
+//  Set direction of movement
+//----------------------------------------------------------------------
+template<class TPixel, unsigned int VImageDimension>
+void 
+ImageLinearIterator<TPixel, VImageDimension>
+::SetDirection(unsigned int direction) 
+{
+  if( direction >= VImageDimension )
+  {
+    throw itk::ExceptionObject();
+  }
+  m_Direction = direction;
+  m_Jump = m_OffsetTable[ m_Direction ];
+}
+ 
+
+
+//----------------------------------------------------------------------
+//  Advance along the line
+//----------------------------------------------------------------------
+template<class TPixel, unsigned int VImageDimension>
+ImageLinearIterator<TPixel, VImageDimension>  & 
+ImageLinearIterator<TPixel, VImageDimension>
+::operator++()
+{
+  m_PositionIndex[m_Direction]++;
+  m_Position += m_Jump;
+  return *this;
+}
+
+
+
+
+} // end namespace itk
