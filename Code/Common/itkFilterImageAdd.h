d84ae7cc523488b9a37e0f65f9cb7f0e5ecafbf9
Luis Ibanez <luis.ibanez@kitware.com>
2000-09-22 06:54:44 -0400

    ENH: itkFilterImageAdd now derives from itkFilterImageBinary. This last class is templated over a function object. itkFilterImageAdd just define this function object as a Add operator inlined.

--- Code/Common/itkFilterImageAdd.h
+++ Code/Common/itkFilterImageAdd.h
@@ -16,9 +16,7 @@
 #ifndef __itkFilterImageAdd_h
 #define __itkFilterImageAdd_h
 
-#include "itkImageSource.h"
-
-#include "itkImageRegionSimpleIterator.h"
+#include "itkFilterImageBinary.h"
 
 namespace itk
 {
@@ -32,9 +30,31 @@ namespace itk
  *
  */
 
+namespace function {  
+  
+  template< class TInput1, class TInput2, class TOutput>
+  class Add
+  {
+  public:
+    Add() {};
+    ~Add() {};
+    inline TOutput operator()( const TInput1 & A, const TInput2 & B)
+    {
+      return (TOutput)(A + B);
+    }
+  }; 
+
+}
+
 template <class TInputImage1, class TInputImage2, class TOutputImage>
 class ITK_EXPORT FilterImageAdd :
-    public ImageSource<TOutputImage> 
+    public
+    FilterImageBinary<TInputImage1,TInputImage2,TOutputImage, 
+    function::Add< 
+              typename TInputImage1::PixelType, 
+              typename TInputImage2::PixelType,
+              typename TOutputImage::PixelType >   >
+
 
 {
 public:
@@ -50,64 +70,14 @@ public:
 
 
   /**
-   *  Pointer type for first input image
-   */
-  typedef typename TInputImage1::Pointer Image1Pointer;
-
-  /**
-   *  Pointer type for second input image
-   */
-  typedef typename TInputImage2::Pointer Image2Pointer;
-
-  /**
-   *  Pointer type for output image
-   */
-  typedef typename TOutputImage::Pointer Image3Pointer;
-
-  /**
-   *  Iterator type for first input image
-   */
-  typedef itk::ImageRegionSimpleIterator< typename TInputImage1::PixelType, TInputImage1::ImageDimension> Image1Iterator;
-
-  /**
-   *  Iterator type for second input image
-   */
-  typedef itk::ImageRegionSimpleIterator< typename TInputImage2::PixelType, TInputImage2::ImageDimension> Image2Iterator;
-
-  /**
-   *  Iterator type for first input image
-   */
-  typedef itk::ImageRegionSimpleIterator< typename TOutputImage::PixelType, TOutputImage::ImageDimension> Image3Iterator;
-
-
-  /**
    * Method for creation through the object factory.
    */
   itkNewMacro(Self);
   
-  /**
-   * Method for execute the algorithm
-   */
-   void Execute(void);
-  
-  /**
-   * Connect one of the operands for pixel-wise addition
-   */
-   void SetInput1( Image1Pointer image1);
-
-  /**
-   * Connect one of the operands for pixel-wise addition
-   */
-  void SetInput2( Image2Pointer image2);
-
 protected:
 
-  Image1Pointer image1;
-  Image2Pointer image2;
-  Image3Pointer image3;
-
-  FilterImageAdd();
-  virtual ~FilterImageAdd() {};
+  FilterImageAdd() {}
+  virtual ~FilterImageAdd() {}
   FilterImageAdd(const Self&) {}
   void operator=(const Self&) {}
 
@@ -116,8 +86,5 @@ protected:
 
 } // end namespace itk
 
-#ifndef ITK_MANUAL_INSTANTIATION
-#include "itkFilterImageAdd.txx"
-#endif
 
 #endif
