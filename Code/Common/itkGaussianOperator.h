6266555c5674dc810aff7f7b413afbb21013fdea
Josh Cates <cates@cs.utah.edu>
2000-10-06 19:07:39 -0400

    ENH:

---
+++ Code/Common/itkGaussianOperator.h
@@ -0,0 +1,198 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkGaussianOperator.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) 2000 National Library of Medicine
+  All rights reserved.
+
+  See COPYRIGHT.txt for copyright details.
+
+  =========================================================================*/
+#ifndef __itkGaussianOperator_h
+#define __itkGaussianOperator_h
+
+#include "itkNeighborhoodOperator.h"
+namespace itk {
+
+/**
+ * \class GaussianOperator
+ * \brief A NeighborhoodOperator whose coefficients are a one
+ * dimensional, discrete Gaussian kernel.
+ *
+ * GaussianOperator can be used to perform Gaussian blurring
+ * by taking its inner product with to a Neighborhood
+ * (NeighborhooIterator) that is swept across an image region.
+ * It is a directional operator.  N successive applications
+ * oriented along each dimensional direction will effect separable,
+ * efficient, N-D Gaussian blurring of an image region.
+ *
+ * GaussianOperator takes two parameters:
+ 
+ * (1) The floating-point variance of the desired Gaussian function.
+ 
+ * (2) The "maximum error" allowed in the discrete Gaussian
+ * function.  "Maximum errror" is defined as the difference between the area
+ * under the discrete Gaussian curve and the area under the continuous
+ * Gaussian. Maximum error affects the Gaussian operator size. Care should
+ * be taken not to make this value too small relative to the variance
+ * lest the operator size become unreasonably large.
+ *
+ * References:
+ * The Gaussian kernel contained in this operator was described
+ * by Tony Lindeberg (Discrete Scale-Space Theory and the Scale-Space
+ * Primal Sketch.  Dissertation. Royal Institute of Technology, Stockholm,
+ * Sweden. May 1991.).
+ *
+ * \sa NeighborhoodOperator
+ * \sa NeighborhoodIterator
+ * \sa Neighborhood
+ *
+ */
+template<unsigned int VDimension=2>
+class GaussianOperator : public NeighborhoodOperator<float, VDimension>
+{
+public:
+  /**
+   * Run-time type information
+   */
+  itkTypeMacro(GaussianOperator, NeighborhoodOperator);
+
+  /**
+   * Standard "Self" typedef support.
+   */
+  typedef GaussianOperator Self;
+
+  /**
+   * NeighborhoodOperator typedef support.
+   */
+  typedef NeighborhoodOperator<float, VDimension> NeighborhoodOperator;
+
+  /**
+   * Constructor.
+   */
+  GaussianOperator() : m_Variance(1), m_MaximumError(.01) { }
+
+  /**
+   * Required to support anonymous copying of NeighborhoodOperators.
+   */
+  NeighborhoodOperator *New() const { return new Self; }
+
+  /**
+   * Required to support anonymous copying of NeighborhoodOperators.
+   */
+  NeighborhoodOperator *Copy() const { return new Self(*this); }
+  
+  /**
+   * Sets the desired variance of the Gaussian kernel.
+   */
+  void SetVariance(const float &variance)
+  {
+    m_Variance = variance;
+  }
+
+  /**
+   * Sets the desired maximum error of the gaussian approximation.  Maximum
+   * error is the difference between the area under the discrete Gaussian curve
+   * and the area under the continuous Gaussian. Maximum error affects the
+   * Gaussian operator size.
+   */
+  void SetMaximumError(const float &max_error)
+  {
+    // --- FIX EXCEPTION HANDLING ---
+    if (m_MaximumError >= 1 || m_MaximumError <= 0)
+      {
+        throw ExceptionObject();
+      }
+    
+    m_MaximumError = max_error;
+  }
+  
+  /**
+   * Returns the variance of the Gaussian (scale) for the operator.
+   */
+  float GetVariance()
+  {
+    return m_Variance;
+  }
+
+  /**
+   * Returns the maximum error of the gaussian approximation.  Maximum error is 
+   * the difference between the area under the discrete Gaussian curve and the
+   * area under the continuous Gaussian. Maximum error affects the Gaussian
+   * operator size.
+   */
+  float GetMaximumError()
+  {
+    return m_MaximumError;
+  }
+
+  /**
+   * Prints some debugging information.
+   */
+  void Print()                // Note: This method is for debugging/devel
+  {                           //  purposes and should probably be removed
+                              // at some point.  jc 10-06-00
+    NeighborhoodOperator::Print();
+    std::cout << "GaussianOperator" << std::endl;
+    std::cout << "\t Variance = " << m_Variance << std::endl;
+    std::cout << "\t MaximumError = " << m_MaximumError << std::endl;
+  }
+  
+protected:
+  /**
+   * Returns the value of the modified Bessel function I0(x) at a point x >= 0.
+   */
+  float ModifiedBesselI0(float);
+
+  /**
+   * Returns the value of the modified Bessel function I1(x) at a point x,
+   * x real. 
+   */
+  float ModifiedBesselI1(float);
+
+  /**
+   * Returns the value of the modified Bessel function Ik(x) at a point x>=0,
+   * where k>=2.
+   */
+  float ModifiedBesselI(int, float);
+
+  /**
+   * Calculates operator coefficients.
+   */
+  std::vector<float> GenerateCoefficients();
+
+  /**
+   * Arranges coefficients spatially in the memory buffer.
+   */
+  void Fill(const std::vector<float> &coeff)
+  {
+    this->FillCenteredDirectional(coeff);
+  }
+
+private:
+  /**
+   * Desired variance of the discrete Gaussian function.
+   */
+  float m_Variance;
+  
+  /**
+   * Difference between the areas under the curves of the continuous and
+   * discrete Gaussian functions.
+   */
+  float m_MaximumError;
+  
+};
+
+} // namespace itk
+
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkGaussianOperator.txx"
+#endif
+
+#endif
---
+++ Code/Common/itkGaussianOperator.txx
@@ -0,0 +1,162 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkGaussianOperator.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) 2000 National Library of Medicine
+  All rights reserved.
+
+  See COPYRIGHT.txt for copyright details.
+
+  =========================================================================*/
+namespace itk
+{
+
+template<unsigned int VDimension>
+std::vector<float>
+GaussianOperator<VDimension>
+::GenerateCoefficients()
+{
+  std::vector<float> coeff;
+  float sum;
+  int i;
+  int j;
+  std::size_t accum;
+  std::vector<float>::iterator it;
+
+  const float et           = exp(-m_Variance);
+  const float cap          = 1.0 - m_MaximumError;
+  
+ // Create the kernel coefficients as a std::vector
+  sum = 0.0f;
+  coeff.push_back(et * ModifiedBesselI0(m_Variance));
+  sum += coeff[0];
+  coeff.push_back(et * ModifiedBesselI1(m_Variance));
+  sum += coeff[1]* 2.0;
+
+  for (i=2; sum < cap; i++)
+	{
+	  coeff.push_back(et* ModifiedBesselI(i, m_Variance));
+	  sum += coeff[i] *2.0;
+      if (coeff[i] <= 0.0) break;  // failsafe
+	}
+
+  // Normalize the coefficients so that their sum is one.
+  for (it = coeff.begin(); it < coeff.end(); ++it)
+    {
+      *it /= sum;
+    }
+
+  // Make symmetric
+  j = coeff.size()-1;
+  coeff.insert(coeff.begin(), j, 0);
+  for (i=0, it = coeff.end()-1; i < j; --it, ++i)
+    {
+      coeff[i] = *it;
+    }
+  
+  return coeff;
+}
+
+template<unsigned int VDimension>
+float
+GaussianOperator<VDimension>
+::ModifiedBesselI0(float y)
+{
+  float d, accumulator;
+  double m;
+
+  if ((d=fabs(y)) < 3.75)
+	{
+	  m=y/3.75;
+	  m*=m;
+	  accumulator = 1.0 + m *(3.5156229+m*(3.0899424+m*(1.2067492
+			+ m*(0.2659732+m*(0.360768e-1 +m*0.45813e-2)))));
+	}
+  else
+	{
+	  m=3.5/d;
+	  accumulator =(exp(d)/sqrt(d))*(0.39894228+m*(0.1328592e-1
+	 +m*(0.225319e-2+m*(-0.157565e-2+m*(0.916281e-2
+     +m*(-0.2057706e-1+m*(0.2635537e-1+m*(-0.1647633e-1	  
+     +m*0.392377e-2))))))));
+	}
+  return accumulator;
+}
+ 
+
+template<unsigned int VDimension>
+float
+GaussianOperator<VDimension>
+::ModifiedBesselI1(float y)
+{
+  float d, accumulator;
+  double m;
+
+  if ((d=fabs(y)) < 3.75)
+	{
+	  m=y/3.75;
+	  m*=m;
+	  accumulator = d*(0.5+m*(0.87890594+m*(0.51498869+m*(0.15084934
+            +m*(0.2658733e-1+m*(0.301532e-2+m*0.32411e-3))))));
+	 }
+  else
+	{
+	  m=3.75/d;
+	  accumulator = 0.2282967e-1+m*(-0.2895312e-1+m*(0.1787654e-1
+       	 -m*0.420059e-2));
+	  accumulator = 0.39894228+m*(-0.3988024e-1+m*(-0.362018e-2
+			+m*(0.163801e-2+m*(-0.1031555e-1+m*accumulator))));
+
+	  accumulator *= (exp(d)/sqrt(d));
+	}
+
+  if (y<0.0) return -accumulator;
+  else return accumulator;
+  
+}
+
+template<unsigned int VDimension>
+float
+GaussianOperator<VDimension>
+::ModifiedBesselI(int n, float y)
+{
+  const float ACCURACY = 40.0f;
+  int j;
+  float qim, qi, qip, toy;
+  float accumulator;
+
+  if (n<2)
+	{
+      throw ExceptionObject();  // placeholder
+	}
+  if (y==0.0) return 0.0;
+  else
+	{
+	  toy=2.0/fabs(y);
+	  qip=accumulator=0.0;
+	  qi=1.0;
+	  for (j=2*(n+(int)sqrt(ACCURACY*n)); j>0 ; j--)
+		{
+		  qim=qip+j*toy*qi;
+		  qip=qi;
+		  qi=qim;
+		  if (fabs(qi) > 1.0e10)
+			{
+			  accumulator*=1.0e-10;
+			  qi *=1.0e-10;
+			  qip*=1.0e-10;
+			}
+		  if (j==n) accumulator=qip;
+		}
+	  accumulator *= ModifiedBesselI0(y)/qi;
+	  if (y<0.0 && (n&1)) return -accumulator;
+	  else return accumulator;
+	}
+}
+
+}// end namespace itk
