3fbea01c0b4a1d8ae53be7fdf79cef1ab3b85083
Josh Cates <cates@cs.utah.edu>
2000-10-06 11:37:26 -0400

    ENH:

---
+++ Code/Common/itkRegionNeighborhoodIterator.h
@@ -0,0 +1,142 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkRegionNeighborhoodIterator.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) 2000 National Library of Medicine
+  All rights reserved.
+
+  See COPYRIGHT.txt for copyright details.
+
+=========================================================================*/
+#ifndef __itkRegionNeighborhoodIterator_h
+#define __itkRegionNeighborhoodIterator_h
+
+#include "itkNeighborhoodIterator.h"
+
+namespace itk {
+
+/**
+ * \class RegionNeighborhoodIterator
+ * \brief Subclass of NeighborhoodIterator designed for fast iteration
+ * over an itk::Image region of interest that does no bounds checking.
+ *
+ * RegionNeighborhoodIterator is the simplest of the NeighborhoodIterators.
+ * It is optimized for fast iteration of a pixel neighborhood over an
+ * image.  It performs no bounds checking and does not handle boundary
+ * conditions on dereferencing.
+ *
+ * RegionNeighborhoodIterator only maintains counters for loop position and
+ * upper bounds, and so it is "unaware" when it is overlapping a region boundary.
+ * You can only safely use this iterator on regions sufficiently contained
+ * within the itk::Image buffer.
+ *
+ * \sa NeighborhoodIterator
+ * \sa RegionBoundaryNeighborhoodIterator
+ * \sa SmartRegionNeighborhoodIterator
+ * \sa Neighborhood
+ * \sa NeighborhoodAlgorithm
+ */
+template<class TPixel, unsigned int VDimension = 2>
+class RegionNeighborhoodIterator
+  :  public NeighborhoodIterator<TPixel, VDimension>
+{
+public:
+  /** 
+   * Standard "Self" typedef support.
+   */
+  typedef RegionNeighborhoodIterator Self;
+
+  /**
+   * Index, Image, & Neighborhood  typedef support. While these were already
+   * typdef'ed in the superclass, they need to be redone here for this subclass
+   * to compile properly with gcc. Note that we have to rescope back to
+   * itk:: so that it is not confused with ImageIterator::.
+   */
+  typedef itk::Image<TPixel, VDimension>        Image;
+  typedef itk::Index<VDimension>                Index;
+  typedef itk::Neighborhood<TPixel, VDimension> Neighborhood;
+  
+  /** 
+   * Run-time type information (and related methods).
+   */
+  itkTypeMacro(RegionNeighborhoodIterator, NeighborhoodIterator);
+
+  /**
+  * Constructor establishes a neighborhood of iterators of a specified
+  * dimension to walk a particular image and a particular region of
+  * that image.
+  */
+  RegionNeighborhoodIterator(const unsigned long radius[VDimension],
+                             Image *ptr,
+                             const Index &start,
+                             const unsigned long bound[VDimension])
+    : NeighborhoodIterator<TPixel, VDimension>(radius, ptr, start, bound)
+  {
+    this->SetBound(bound);
+  }
+
+  /**
+   * Return an iterator for the beginning of the region.
+   */
+  Self Begin();
+
+  /**
+   * Return an iterator for the end of the region.
+   */
+  Self End();
+
+  /**
+   * Returns a Neighborhood object with values of the image pixels that
+   * are referenced by the NeighborhoodIterator's internal pointers.
+   * \sa SetPixelValues
+   * \sa Neighborhood
+   */
+  Neighborhood GetNeighborhood();
+
+  /**
+   * Sets the values in the referenced image to the values contained in
+   * a Neighborhood object.  This method assumes that the Neighborhood object
+   * argument and the NeighborhoodPointer are equal size.
+   */
+  void SetNeighborhood(Neighborhood &);
+
+  /**
+   * Prints information about the neighborhood pointer structure to
+   * std::cout for debugging purposes.
+   */
+  void Print();
+
+
+  /**
+   * Calculates the inner product of the neighborhood of referenced pixel
+   * values with a valarray (and therefore also any NeighborhoodBase subclass).
+   * Returns a scalar value.  Innerproduct is re-implemented for
+   * the NeighborhoodPointer to avoid the penalty of creating a temporary
+   * Neighborhood when such a step is not needed by an algorithm.
+   *
+   * \sa Neighborhood
+   * \sa SlicedInnerProduct
+   */
+  TPixel InnerProduct(std::valarray<TPixel> &);
+  
+protected:
+  /**
+   * Sets the loop upper boundaries for iteration.
+   */
+  void SetBound(const unsigned long []);
+};
+  
+} // namespace itk
+
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkRegionNeighborhoodIterator.txx"
+#endif
+
+#endif 
+
---
+++ Code/Common/itkRegionNeighborhoodIterator.txx
@@ -0,0 +1,143 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkRegionNeighborhoodIterator.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) 2000 National Library of Medicine
+  All rights reserved.
+
+  See COPYRIGHT.txt for copyright details.
+
+  =========================================================================*/
+namespace itk {
+  
+template<class TPixel, unsigned int VDimension>
+Neighborhood<TPixel, VDimension>
+RegionNeighborhoodIterator<TPixel, VDimension>
+::GetNeighborhood()
+{
+  Neighborhood ans;
+  Neighborhood::Iterator ans_it;
+  Iterator this_it;
+  const Iterator _end = this->end();
+
+  ans.SetRadius( this->GetRadius() );
+
+  for (ans_it = ans.begin(), this_it = this->begin();
+       this_it < _end; ans_it++, this_it++)
+    {
+      *ans_it = **this_it;
+    }
+
+  return ans;
+}
+
+template<class TPixel, unsigned int VDimension>
+void
+RegionNeighborhoodIterator<TPixel, VDimension>
+::SetNeighborhood(Neighborhood &N)
+{
+  Iterator this_it;
+  const Iterator _end = this->end();
+  Neighborhood::Iterator N_it;
+  
+  for (this_it = this->begin(); this_it < _end; this_it++, N_it++)
+    {
+      **this_it = *N_it;
+    }
+
+}
+  
+template<class TPixel, unsigned int VDimension>
+void RegionNeighborhoodIterator<TPixel, VDimension>
+::Print()   // --- Note: This method is for development/debugging
+{           //           and should be removed at some point.
+  int i;    //           jc 10-05-00
+  //NeighborhoodBase<TPixel, VDimension>::Print();
+  std::cout << "RegionNeighborhoodIterator" << std::endl;
+  std::cout << "        this = " << this << std::endl;
+  std::cout << "this->size() = " << this->size() << std::endl;
+  std::cout << "    m_Bound = { ";
+  for (i = 0; i<VDimension; i++) std::cout << m_Bound[i] << " ";
+  std::cout << "}" << std::endl;
+  std::cout << "     m_Loop = { ";
+  for (i = 0; i<VDimension; i++) std::cout << m_Loop[i] << " ";
+  std::cout << "}" << std::endl;
+}
+  
+template<class TPixel, unsigned int VDimension>
+void RegionNeighborhoodIterator<TPixel, VDimension>
+::SetBound(const unsigned long bound[VDimension])
+{
+  const unsigned long *offset     = m_Image->GetOffsetTable();
+  const unsigned long *bufferSize = m_Image->GetBufferSize();
+
+  // Set the bounds and the wrapping offsets
+  for (int i=0; i<VDimension; ++i)
+    {
+      m_Bound[i]      = m_StartIndex[i]+bound[i];
+      m_WrapOffset[i] = (bufferSize[i] - (m_Bound[i] - m_StartIndex[i]))
+                        * offset[i];
+    }  
+}
+
+template<class TPixel, unsigned int VDimension>
+TPixel
+RegionNeighborhoodIterator<TPixel, VDimension>
+::InnerProduct(std::valarray<TPixel> &v)
+{
+  TPixel sum = 0;
+  TPixel *it;
+  Iterator this_it;
+  const TPixel *_end = &(v[v.size()]);
+  
+  for (it = &(v[0]), this_it = this->begin(); it < _end; ++it, ++this_it)
+    {
+      sum += *it * **this_it; 
+    }
+  return sum;
+}
+
+
+template<class TPixel, unsigned int VDimension>
+RegionNeighborhoodIterator<TPixel, VDimension>
+RegionNeighborhoodIterator<TPixel, VDimension>
+::Begin()
+{
+  //Copy the current iterator
+  Self it( *this );
+
+  // Set the position to the m_BeginOffset
+  it.SetLocation( this->m_StartIndex );
+
+  return it;
+}
+
+template<class TPixel, unsigned int VDimension>
+RegionNeighborhoodIterator<TPixel, VDimension>
+RegionNeighborhoodIterator<TPixel, VDimension>
+::End()
+{
+  Index endIndex;
+  
+  // Copy the current iterator
+  Self it( *this );
+
+  // Calculate the end index
+  endIndex.m_Index[0] = m_StartIndex[0] + m_Bound[0];
+  for (int i = 1; i< VDimension; ++i)
+    {
+      endIndex.m_Index[i] = m_StartIndex[i] + m_Bound[i] -1;
+    }
+  
+  // Set the position to the m_BeginOffset
+  it.SetLocation( endIndex );
+
+  return it;
+}
+
+} // namespace itk
