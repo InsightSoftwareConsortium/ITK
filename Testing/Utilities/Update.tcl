6c1f123a0717fc8534c6eee879bf0ded4b1e5536
Daniel Blezek <blezek.daniel@mayo.edu>
2000-10-31 14:56:27 -0500

    ENH: added date for Nightly checkout

--- Testing/Utilities/Update.tcl
+++ Testing/Utilities/Update.tcl
@@ -42,7 +42,7 @@ set UpdateCommand "$cvs update -d -P -A"
 
 if { $DateStamp != "" } \
 {
-  set UpdateCommand "$UpdateCommand -D $Year-$Month-$Day 23:00 GMT"
+  set UpdateCommand "$UpdateCommand -D \"$Year-$Month-$Day 23:00 GMT\""
 }
   
 set UpdateStatus [catch { eval exec $UpdateCommand >& update.tmp } result]
