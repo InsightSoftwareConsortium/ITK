a8c2273e8396df11892582376ba500d0e92c49e9
Bill Lorensen <bill.lorensen@gmail.com>
2000-11-29 19:00:18 -0500

    Nightly version

--- Code/Common/itkVersion.h
+++ Code/Common/itkVersion.h
@@ -23,7 +23,7 @@
 #define ITK_MAJOR_VERSION 0
 #define ITK_MINOR_VERSION 0
 #define ITK_BUILD_VERSION 2
-#define ITK_SOURCE_VERSION "itk version " ITK_VERSION ", itk source $Revision: 1.94 $, $Date: 2000-11-29 00:00:19 $ (GMT)"
+#define ITK_SOURCE_VERSION "itk version " ITK_VERSION ", itk source $Revision: 1.95 $, $Date: 2000-11-30 00:00:18 $ (GMT)"
 
 namespace itk
 {
