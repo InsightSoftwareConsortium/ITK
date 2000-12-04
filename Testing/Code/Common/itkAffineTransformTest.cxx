3c12f1eb2fd83448b85b17a12f5a953383bc1e1d
Paul Hughett <hughett@upenn.edu>
2000-12-04 11:21:06 -0500

    ENH: Added a class for defining, manipulating, and using affine transformations.  This initial version defines an initial draft of the interface but does not actually supply any internal implementation.

---
+++ Testing/Code/Common/itkAffineTransformTest.cxx
@@ -0,0 +1,37 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkAffineTransformTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) 2000 National Library of Medicine
+  All rights reserved.
+
+  See COPYRIGHT.txt for copyright details.
+
+=========================================================================*/
+
+// #include <stdlib.h>
+// #include <stdio.h>
+#include <iostream>
+
+//#include "itkImage.h"
+//#include "itkIndex.h"
+//#include "itkSize.h"
+//#include "itkImageMoments.h"
+
+#include "itkAffineTransform.h"
+
+int
+main(
+    int argc,
+    char *argv[])
+{
+    /* FIXME: Just an empty shell for testing */
+    std::cout << "The AffineTransform class is still being implemented\n";
+    //              << std::endl;
+    return 1;
+}
