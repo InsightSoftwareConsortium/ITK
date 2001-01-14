400fcb3854ddc115ae69c51cdd20ecb6c0b5b033
Luis Ibanez <luis.ibanez@kitware.com>
2001-01-14 11:18:03 -0500

    Accessor class to the components of the RGB type.      This classes are expected to be used as template parameters for      ImageAdaptors.

---
+++ Code/Common/itkDataAccessorRGBtoBlue.h
@@ -0,0 +1,80 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkDataAccessorRGBtoBlue.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) 2000 National Library of Medicine
+  All rights reserved.
+
+  See COPYRIGHT.txt for copyright details.
+
+=========================================================================*/
+#ifndef __itkDataAccessorRGBtoBlue_h
+#define __itkDataAccessorRGBtoBlue_h
+
+
+#include "itkRGB.h"
+
+
+namespace itk
+{
+
+/**
+ * \class DataAccessorRGBtoBlue
+ * \brief Give access to the Blue component of a RGB type 
+ *
+ * This class is intended to be used as parameter of 
+ * an ImageAdaptor to make an RGB image appear as being
+ * of scalar type T, showing only the Blue component.
+ *
+ * \sa ImageAdaptor
+ *
+ */
+
+template <class T>
+class ITK_EXPORT DataAccessorRGBtoBlue
+{
+public:
+ /**
+   * Standard "Self" typedef.
+   */
+  typedef   DataAccessorRGBtoBlue        Self;
+
+ /** 
+   * External typedef. It defines the external aspect
+   * that this class will exhibit
+   */
+  typedef T ExternalType;
+
+  /** 
+   * Internal typedef. It defines the internal real
+   * representation of data
+   */
+  typedef   RGB<T>    InternalType;
+
+
+  /** 
+   * Write access to the Blue component
+   */
+  static void Set( InternalType & output, const ExternalType & input ) 
+    { output.SetBlue( input ); }
+
+
+  /** 
+   * Read access to the Blue component
+   */
+  static ExternalType Get( const InternalType & input ) 
+    { return input.GetBlue(); }
+
+
+};
+
+  
+}  // end namespace itk
+
+#endif
+
---
+++ Code/Common/itkDataAccessorRGBtoRed.h
@@ -0,0 +1,82 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkDataAccessorRGBtoRed.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) 2000 National Library of Medicine
+  All rights reserved.
+
+  See COPYRIGHT.txt for copyright details.
+
+=========================================================================*/
+#ifndef __itkDataAccessorRGBtoRed_h
+#define __itkDataAccessorRGBtoRed_h
+
+
+#include "itkRGB.h"
+
+
+namespace itk
+{
+
+/**
+ * \class DataAccessorRGBtoRed
+ * \brief Give access to the red component of a RGB type 
+ *
+ * This class is intended to be used as parameter of 
+ * an ImageAdaptor to make an RGB image appear as being
+ * of scalar type T, showing only the Red component.
+ *
+ * \sa ImageAdaptor
+ *
+ */
+
+template <class T>
+class ITK_EXPORT DataAccessorRGBtoRed
+{
+public:
+ /**
+   * Standard "Self" typedef.
+   */
+  typedef   DataAccessorRGBtoRed        Self;
+
+ /** 
+   * External typedef. It defines the external aspect
+   * that this class will exhibit
+   */
+  typedef T ExternalType;
+
+  /** 
+   * Internal typedef. It defines the internal real
+   * representation of data
+   */
+  typedef     RGB<T>      InternalType;
+
+
+  /** 
+   * Write access to the Red component
+   */
+  static void Set( InternalType & output, const ExternalType & input ) 
+    { output.SetRed( input ); }
+
+
+  /** 
+   * Read access to the Red component
+   */
+  static ExternalType Get( const InternalType & input ) 
+    { return input.GetRed(); }
+
+
+};
+
+  
+  
+}  // end namespace itk
+
+
+#endif
+
