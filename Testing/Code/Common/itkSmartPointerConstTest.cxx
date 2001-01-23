5c9b705b45f9d37b36a13623798037987b7d555d
Brad King <brad.king@kitware.com>
2001-01-23 16:40:41 -0500

    ENH: Added test for smart pointers to const objects.  The test is minimal, and should probably be expanded.

---
+++ Testing/Code/Common/itkSmartPointerConstTest.cxx
@@ -0,0 +1,25 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit (ITK)
+  Module:    itkSmartPointerConstTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+Copyright (c) 2000 National Library of Medicine
+All rights reserved.
+
+See COPYRIGHT.txt for copyright details.
+
+=========================================================================*/
+#include <iostream>
+
+#include "itkLightObject.h"
+
+int main(void)
+{
+  itk::LightObject::ConstPointer lo = itk::LightObject::New();
+  
+  return 0;
+}
