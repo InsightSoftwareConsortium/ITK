484ebfe94884c0ebe5f95be1e6bbc6c02ac59627
Parag Chandra <chandra@cs.unc.edu>
2001-01-29 13:07:25 -0500

    STYLE - Modified to use new interface of FileIOMetaImage, mainly ::New() instead of new.

--- Testing/Code/Common/itkFileIOTest.cxx
+++ Testing/Code/Common/itkFileIOTest.cxx
@@ -1,23 +1,29 @@
 #include <itkImage.h>
-#include "itkFileIOMetaImage.h"
-#include "itkWriteMetaImage.h"
-#include "itkFilterFileIOToImage.h"
+#include <itkFileIOMetaImage.h>
+#include <itkWriteMetaImage.h>
+#include <itkFilterFileIOToImage.h>
 
 using namespace itk;
+using namespace std;
 
 int main ()
 {
-	typedef Image<unsigned short, 3> myImageType;
-	typedef WriteMetaImage<myImageType> myWriterType;
-	typedef FilterFileIOToImage<myImageType> myFilterType;
+  typedef Image<unsigned short, 3> myImageType;
+  typedef WriteMetaImage<myImageType> myWriterType;
+  typedef FilterFileIOToImage<myImageType> myFilterType;
 
-	myWriterType::Pointer writer = myWriterType::New();
-	myFilterType::Pointer filter = new myFilterType("D:/research/Angio002Series005.mhd");
+  ObjectFactoryBase::RegisterFactory(new FileIOMetaImageFactory);
+  myWriterType::Pointer writer = myWriterType::New();
+  myFilterType::Pointer filter = myFilterType::New();
 
-	filter->Update();
-	filter->Print(std::cout);
-	writer->SetFileName("D:/research/TestOutput.mhd");
-	writer->SetInput(filter->GetOutput());
-	writer->Print(std::cout);
-	writer->Write();
+	filter->SetFileToLoad("D:/research/Angio002Series005.mhd");
+  filter->Update();
+// Why does Print() cause an infinite loop?
+//  filter->Print(std::cout);
+  writer->SetFileName("D:/research/TestOutput.mhd");
+  writer->SetInput(filter->GetOutput());
+//  writer->Print(std::cout);
+  writer->Write();
+
+  return 0;
 }
