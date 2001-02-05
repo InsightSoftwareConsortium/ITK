169d94c1a7831012b7f8ce59d47f0054ef9b1e80
Will Schroeder <will.schroeder@kitware.com>
2001-02-05 16:44:36 -0500

    ENH:Added a stub test

---
+++ Testing/Code/Common/itkThresholdImageFilterTest.cxx
@@ -0,0 +1,60 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit (ITK)
+  Module:    itkThresholdImageFilterTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+Copyright (c) 2000 National Library of Medicine
+All rights reserved.
+
+See COPYRIGHT.txt for copyright details.
+
+=========================================================================*/
+#include <iostream>
+#include "itkImage.h"
+#include "itkScalar.h"
+#include "itkRandomImageSource.h"
+#include "itkThresholdImageFilter.h"
+#include "itkCommand.h"
+#include "itkOutputWindow.h"
+
+
+// this class is used to send output to stdout and not the itk window
+class TextOutput : public itk::OutputWindow
+{
+public:
+  virtual void DisplayText(const char* s)
+    {
+      std::cout << s << std::endl;
+    }
+};
+
+int main()
+{
+  // Comment the following if you want to use the itk text output window
+  itk::OutputWindow::SetInstance(new TextOutput);
+
+  // Uncomment the following if you want to see each message independently
+  // itk::OutputWindow::GetInstance()->PromptUserOn();
+
+//  typedef itk::Image<itk::Scalar<float>,2> FloatImage2DType;
+  typedef itk::Image<float,2> FloatImage2DType;
+
+  itk::RandomImageSource<FloatImage2DType>::Pointer random;
+  random = itk::RandomImageSource<FloatImage2DType>::New();
+  random->SetMin(0.0);
+  random->SetMax(1000.0);
+
+  itk::ThresholdImageFilter<FloatImage2DType>::Pointer threshold;
+  threshold = itk::ThresholdImageFilter<FloatImage2DType>::New();
+  threshold->SetInput(random->GetOutput());
+  threshold->Update();
+
+  return EXIT_SUCCESS;
+}
+
+
+
