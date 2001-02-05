a4f6b4659b1877c1b106fabc2dab045496427519
Josh Cates <cates@cs.utah.edu>
2001-02-05 17:11:16 -0500

    STYLE: File name change that reflect the change in the filter tested.

---
+++ Testing/Code/Common/itkCurvatureAnisotropicDiffusionImageFilterTest.cxx
@@ -0,0 +1,53 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkCurvatureAnisotropicDiffusionImageFilterTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) 2000 National Library of Medicine
+  All rights reserved.
+
+  See COPYRIGHT.txt for copyright details.
+
+=========================================================================*/
+
+#include <iostream>
+#include "itkCurvatureAnisotropicDiffusionImageFilter.h"
+#include "itkNullImageToImageFilterDriver.txx"
+
+/**
+ * This program tests the FilterImageAnisotropicDiffusion object by driving it
+ * with a null input and output.  Returns 0 on success and 1 on failure.
+ */
+main(int argc, char *argv[])
+{
+  try
+    {
+      // Set up filter
+      itk::CurvatureAnisotropicDiffusionImageFilter<float, 2>::Pointer
+        filter = itk::CurvatureAnisotropicDiffusionImageFilter<float,
+        2>::New();
+      filter->SetIterations(1);
+      filter->SetConductanceParameter(3.0f);
+      filter->SetTimeStep(0.125f);
+
+      // Run Test
+      itk::Size<2> sz;
+      sz[0] = 250;
+      sz[1] = 250;
+      itk::NullImageToImageFilterDriver< itk::Image<float, 2>,
+        itk::Image<float, 2> > test1;
+      test1.SetImageSize(sz);
+      test1.SetFilter(filter.GetPointer());
+      test1.Execute();
+    }
+  catch(itk::ExceptionObject &err)
+    {
+      (&err)->Print(std::cerr);
+      return 1;
+    } 
+  return 0;   
+}
---
+++ Testing/Code/Common/itkVectorAnisotropicDiffusionImageFilterTest.cxx
@@ -0,0 +1,62 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkVectorAnisotropicDiffusionImageFilterTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) 2000 National Library of Medicine
+  All rights reserved.
+
+  See COPYRIGHT.txt for copyright details.
+
+=========================================================================*/
+
+#include <iostream>
+#include "itkImage.h"
+#include "itkVectorAnisotropicDiffusionImageFilter.h"
+#include "itkNullImageToImageFilterDriver.txx"
+#include "itkVector.h"
+
+inline ostream& operator<<(ostream &o, const itk::Vector<float, 3> &v)
+{
+  o << "["<< v[0] << " " << v[1] << " " << v[2] << "]";
+  return o;
+}
+
+main(int argc, char *argv[])
+{
+  try
+    {
+      // Set up filter
+      itk::VectorAnisotropicDiffusionImageFilter<itk::Vector<float, 3>,
+        2>::Pointer 
+        filter =
+        itk::VectorAnisotropicDiffusionImageFilter<itk::Vector<float, 3>, 
+        2>::New();
+      filter->SetIterations(1);
+      filter->SetConductanceParameter(3.0f);
+      filter->SetTimeStep(0.125f);
+
+      // Run Test
+      itk::Size<2> sz;
+      sz[0] = 100 ; //atoi(argv[1]);
+      sz[1] = 100 ; // atoi(argv[2]);
+      //      sz[2] = 10;//atoi(argv[3]);
+      //      sz[3] = 5;//atoi(argv[4]);
+      itk::NullImageToImageFilterDriver< itk::Image<itk::Vector<float, 3>, 2>,
+                                        itk::Image<itk::Vector<float, 3>, 2> >
+        test1;
+      test1.SetImageSize(sz);
+      test1.SetFilter(filter.GetPointer());
+      test1.Execute();
+    }
+  catch(itk::ExceptionObject &err)
+    {
+      (&err)->Print(std::cerr);
+      return 2;
+    } 
+  return 1;   
+}
