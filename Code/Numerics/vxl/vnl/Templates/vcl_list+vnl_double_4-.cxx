0103f6ad04f2093352485cc567a61a0d8479f42e
Bill Hoffman <bill.hoffman@kitware.com>
2001-02-14 17:26:58 -0500

    SGI

--- Code/Numerics/vxl/vnl/Templates/vcl_list+vnl_double_4-.cxx
+++ Code/Numerics/vxl/vnl/Templates/vcl_list+vnl_double_4-.cxx
@@ -1,9 +1,6 @@
 #include <vcl_list.txx>
 #include <vnl/vnl_double_4.h>
 
-#ifdef VCL_SGI_CC_730
-#pragma do_not_instantiate void  std::list<vnl_double_4>::merge(std::list<vnl_double_4> &)
-#endif
 
 VCL_DO_NOT_INSTANTIATE(void vcl_list<vnl_double_4>::merge(vcl_list<vnl_double_4> &), VCL_VOID_RETURN);
 
