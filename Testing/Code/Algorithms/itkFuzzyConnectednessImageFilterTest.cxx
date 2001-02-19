d82a869aaf1f81ac19516e6aa2d7828663946ae3
Will Schroeder <will.schroeder@kitware.com>
2001-02-19 14:31:49 -0500

    ENH:Bad style; don't use printf

--- Testing/Code/Algorithms/itkFuzzyConnectednessImageFilterTest.cxx
+++ Testing/Code/Algorithms/itkFuzzyConnectednessImageFilterTest.cxx
@@ -20,86 +20,83 @@
 const int WIDTH = 20;
 const int HEIGHT = 20;
 
-
 int main(){
 
-	typedef itk::Image<bool,2> BinaryImage2D;
-	typedef itk::Image<unsigned short,2> UShortImage2D;
-	typedef itk::FuzzyConnectednessImageFilter<UShortImage2D,BinaryImage2D> FuzzyUShort;
+  typedef itk::Image<bool,2> BinaryImage2D;
+  typedef itk::Image<unsigned short,2> UShortImage2D;
+  typedef itk::FuzzyConnectednessImageFilter<UShortImage2D,BinaryImage2D> FuzzyUShort;
 
-	FuzzyUShort::Pointer testFuzzy=FuzzyUShort::New();
-	UShortImage2D::Pointer inputimg=UShortImage2D::New();
-	UShortImage2D::SizeType size={{HEIGHT,WIDTH}};
-	UShortImage2D::IndexType index=UShortImage2D::IndexType::ZeroIndex;
-	UShortImage2D::RegionType region;
+  FuzzyUShort::Pointer testFuzzy=FuzzyUShort::New();
+  UShortImage2D::Pointer inputimg=UShortImage2D::New();
+  UShortImage2D::SizeType size={HEIGHT,WIDTH};
+  UShortImage2D::IndexType index=UShortImage2D::IndexType::ZeroIndex;
+  UShortImage2D::RegionType region;
 
-	region.SetSize(size);
-	region.SetIndex(index);
+  region.SetSize(size);
+  region.SetIndex(index);
 
-	inputimg->SetLargestPossibleRegion( region );
-    inputimg->SetBufferedRegion( region );
-	inputimg->SetRequestedRegion( region );
+  inputimg->SetLargestPossibleRegion( region );
+  inputimg->SetBufferedRegion( region );
+  inputimg->SetRequestedRegion( region );
 
-    inputimg->Allocate();
+  inputimg->Allocate();
 
+  // background: uniform distributed random number ~(300-350) 
+  itk::SimpleImageRegionIterator <UShortImage2D> it(inputimg, region);
+  it.Begin();
+  while( !it.IsAtEnd())
+    {    
+    it.Set( (unsigned short)(50*rand()/RAND_MAX+300) );
+    ++it;
+    }
 
-	/* background: uniform distributed random number ~(300-350) */
-   itk::SimpleImageRegionIterator <UShortImage2D> it(inputimg, region);
-   it.Begin();
-   while( !it.IsAtEnd())
-   {    
-     it.Set( (unsigned short)(50*rand()/RAND_MAX+300) );
-	 ++it;
-   }
+  // object, value ~(270-320)
+  for(int i = 0;i < HEIGHT/2; i++)
+    {
+    for(int j = 0; j < WIDTH/2; j++)
+      {
+      index[0] = i;
+      index[1] = j;
+      inputimg->SetPixel(index,(unsigned short)(50 * rand() / RAND_MAX + 270));
+      }
+    }
 
-   /* object, value ~(270-320) */
-   for(int i = 0;i < HEIGHT/2; i++)
-   {
-     for(int j = 0; j < WIDTH/2; j++){
-		index[0] = i;
-		index[1] = j;
-		inputimg->SetPixel(index,(unsigned short)(50 * rand() / RAND_MAX + 270));
-	 }
-   }
+  // print the input image
+  std::cout << "\n\n The Input Image\n";
+  it.Begin();
+  for(int i = 0;i < HEIGHT; i++)
+    {
+    for (int j = 0; j < WIDTH; j++)
+      {
+      std::cout << "%3i ";
+      ++it;
+      }
+    std::cout << "\n";
+    }
 
-   /* print the input image */	
-   printf("\n\n The Input Image\n");
-   it.Begin();
-   for(int i = 0;i < HEIGHT; i++){
-	   for (int j = 0; j < WIDTH; j++){
-		   printf("%3i ",it.Get());
-		   ++it;
-	   }
-	   printf("\n");
-   }
+  // execute the segmentation subroutine
+  testFuzzy->SetInput(inputimg);
 
+  index[0] = 5;
+  index[1] = 5;
+  testFuzzy->SetSeed(index);
+  testFuzzy->SetParameters(270.0,50.0,1.0,1.0,1.0);
+  testFuzzy->SetThreshold(0.8);
+  testFuzzy->ExcuteSegment();
 
-	/* execute the segmentation subroutine*/
-	testFuzzy->SetInput(inputimg);
+  // printout the segmentation result
+  std::cout << "\n\n Segmentation Result:\n";
+  itk::SimpleImageRegionIterator <BinaryImage2D> ot(testFuzzy->GetOutput(), region);
 
-
-	index[0] = 5;
-	index[1] = 5;
-	testFuzzy->SetSeed(index);
-	testFuzzy->SetParameters(270.0,50.0,1.0,1.0,1.0);
-	testFuzzy->SetThreshold(0.8);
-
-	testFuzzy->ExcuteSegment();
-
-
- 
-	/* printout the segmentation result */
-   printf("\n\n Segmentation Result:\n");
-   itk::SimpleImageRegionIterator <BinaryImage2D> ot(testFuzzy->GetOutput(), region);
-
-   ot.Begin();
-   for(int i = 0;i < HEIGHT; i++){
-	   for (int j = 0; j < WIDTH; j++){
-		   printf("%i",ot.Get());
-		   ++ot;
-	   }
-	   printf("\n");
-   }
-	
-	return 1;
+  ot.Begin();
+  for(int i = 0;i < HEIGHT; i++)
+    {
+    for (int j = 0; j < WIDTH; j++)
+      {
+      std::cout << ot.Get();
+      ++ot;
+      }
+    std::cout << "\n";
+    }
+  return 1;
 }
