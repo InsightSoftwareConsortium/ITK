66ca2391af77368e1ba72e875d895d0ff05fae7e
Bill Lorensen <bill.lorensen@gmail.com>
2001-02-19 16:15:31 -0500

    ERR: was missing unsigned short and short.

--- Code/Numerics/vxl/vnl/vnl_numeric_limits.h
+++ Code/Numerics/vxl/vnl/vnl_numeric_limits.h
@@ -302,6 +302,75 @@ public:
   static const bool tinyness_before VCL_STATIC_CONST_INIT_INT(false);
   static const vnl_float_round_style round_style VCL_STATIC_CONST_INIT_INT(vnl_round_toward_zero);
 };
+
+VCL_DEFINE_SPECIALIZATION
+class vnl_numeric_limits<unsigned short > {
+public:
+  static const bool is_specialized VCL_STATIC_CONST_INIT_INT(true);
+  inline static unsigned int min() { return 0; }
+  inline static unsigned int max() { return 0xffff; }
+  static const int digits   VCL_STATIC_CONST_INIT_INT(sizeof(unsigned short) * 8 );
+  static const int digits10 VCL_STATIC_CONST_INIT_INT( (digits * 301) / 1000 );
+  static const bool is_signed  VCL_STATIC_CONST_INIT_INT(false);
+  static const bool is_integer VCL_STATIC_CONST_INIT_INT(true);
+  static const bool is_exact   VCL_STATIC_CONST_INIT_INT(true);
+  static const int radix VCL_STATIC_CONST_INIT_INT(2);
+  inline static int epsilon()     { return 0; }
+  inline static int round_error() { return 0; }
+  static const int min_exponent   VCL_STATIC_CONST_INIT_INT(-31);
+  static const int min_exponent10 VCL_STATIC_CONST_INIT_INT(-9);
+  static const int max_exponent   VCL_STATIC_CONST_INIT_INT(31);
+  static const int max_exponent10 VCL_STATIC_CONST_INIT_INT(9);
+  static const bool has_infinity      VCL_STATIC_CONST_INIT_INT(false);
+  static const bool has_quiet_NaN     VCL_STATIC_CONST_INIT_INT(false);
+  static const bool has_signaling_NaN VCL_STATIC_CONST_INIT_INT(false);
+  static const bool has_denorm        VCL_STATIC_CONST_INIT_INT(false);
+  static int infinity() { return max(); }
+  static int quiet_NaN();
+  static int signaling_NaN();
+  inline static int denorm_min()    { return min(); }
+  static const bool is_iec559  VCL_STATIC_CONST_INIT_INT(false);
+  static const bool is_bounded VCL_STATIC_CONST_INIT_INT(true);
+  static const bool is_modulo  VCL_STATIC_CONST_INIT_INT(true);
+  static const bool traps      VCL_STATIC_CONST_INIT_INT(false);
+  static const bool tinyness_before VCL_STATIC_CONST_INIT_INT(false);
+  static const vnl_float_round_style round_style VCL_STATIC_CONST_INIT_INT(vnl_round_toward_zero);
+};
+
+VCL_DEFINE_SPECIALIZATION
+class vnl_numeric_limits<short > {
+public:
+  static const bool is_specialized VCL_STATIC_CONST_INIT_INT(true);
+  inline static int min() { return -0x7fff; }
+  inline static int max() { return  0x7fff; }
+  static const int digits   VCL_STATIC_CONST_INIT_INT(15);
+  static const int digits10 VCL_STATIC_CONST_INIT_INT(5);
+  static const bool is_signed  VCL_STATIC_CONST_INIT_INT(true);
+  static const bool is_integer VCL_STATIC_CONST_INIT_INT(true);
+  static const bool is_exact   VCL_STATIC_CONST_INIT_INT(true);
+  static const int radix VCL_STATIC_CONST_INIT_INT(2);
+  inline static int epsilon()     { return 0; }
+  inline static int round_error() { return 0; }
+  static const int min_exponent   VCL_STATIC_CONST_INIT_INT(-15);
+  static const int min_exponent10 VCL_STATIC_CONST_INIT_INT(-5);
+  static const int max_exponent   VCL_STATIC_CONST_INIT_INT(15);
+  static const int max_exponent10 VCL_STATIC_CONST_INIT_INT(5);
+  static const bool has_infinity      VCL_STATIC_CONST_INIT_INT(false);
+  static const bool has_quiet_NaN     VCL_STATIC_CONST_INIT_INT(false);
+  static const bool has_signaling_NaN VCL_STATIC_CONST_INIT_INT(false);
+  static const bool has_denorm        VCL_STATIC_CONST_INIT_INT(false);
+  static int infinity() { return max(); }
+  static int quiet_NaN();
+  static int signaling_NaN();
+  inline static int denorm_min()    { return min(); }
+  static const bool is_iec559  VCL_STATIC_CONST_INIT_INT(false);
+  static const bool is_bounded VCL_STATIC_CONST_INIT_INT(true);
+  static const bool is_modulo  VCL_STATIC_CONST_INIT_INT(true);
+  static const bool traps      VCL_STATIC_CONST_INIT_INT(false);
+  static const bool tinyness_before VCL_STATIC_CONST_INIT_INT(false);
+  static const vnl_float_round_style round_style VCL_STATIC_CONST_INIT_INT(vnl_round_toward_zero);
+};
+
 // -------------------- #include <vnl/vnl_numeric_limits_float.h>
 
 // IEEE 754 single precision
