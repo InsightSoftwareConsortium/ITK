ea78214108dfe9bb0117737cafc32c6ee03bcdf3
Luis Ibanez <luis.ibanez@kitware.com>
2001-02-23 21:49:50 -0500

    Tests for Filters and Adaptors implementing basic mathematical functions

---
+++ Testing/Code/BasicFilters/itkLogImageFilterAndAdaptorTest.cxx
@@ -0,0 +1,200 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkLogImageFilterAndAdaptorTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) 2000 National Library of Medicine
+  All rights reserved.
+
+  See COPYRIGHT.txt for copyright details.
+
+=========================================================================*/
+
+
+#include <itkImage.h>
+#include <itkLogImageFilter.h>
+#include <itkLogImageAdaptor.h>
+#include <itkSimpleImageRegionIterator.h>
+#include <itkSubtractImageFilter.h>
+
+
+int main() 
+{
+
+  // Define the dimension of the images
+  const unsigned int ImageDimension = 3;
+
+  // Declare the types of the images
+  typedef itk::Image<float, ImageDimension>  InputImageType;
+  typedef itk::Image<float, ImageDimension>  OutputImageType;
+
+  
+  
+  // Declare Iterator types apropriated for each image 
+  typedef itk::SimpleImageRegionIterator<
+                                  InputImageType>  InputIteratorType;
+
+  typedef itk::SimpleImageRegionIterator<
+                                  OutputImageType>  OutputIteratorType;
+
+
+
+  // Declare the type of the index to access images
+  typedef itk::Index<ImageDimension>         IndexType;
+
+  // Declare the type of the size 
+  typedef itk::Size<ImageDimension>          SizeType;
+
+  // Declare the type of the Region
+  typedef itk::ImageRegion<ImageDimension>   RegionType;
+
+  // Create two images
+  InputImageType::Pointer inputImage  = InputImageType::New();
+  
+  // Define their size, and start index
+  SizeType size;
+  size[0] = 2;
+  size[1] = 2;
+  size[2] = 2;
+
+  IndexType start;
+  start[0] = 0;
+  start[1] = 0;
+  start[2] = 0;
+
+  RegionType region;
+  region.SetIndex( start );
+  region.SetSize( size );
+
+  // Initialize Image A
+  inputImage->SetLargestPossibleRegion( region );
+  inputImage->SetBufferedRegion( region );
+  inputImage->SetRequestedRegion( region );
+  inputImage->Allocate();
+  // Create one iterator for the Input Image (this is a light object)
+  InputIteratorType it( inputImage, inputImage->GetBufferedRegion() );
+
+  // Initialize the content of Image A
+  const double pi    = atan( 1.0 ) * 4.0;
+  const double value = pi / 6.0;
+  std::cout << "Content of the Input " << std::endl;
+  it.Begin();
+  while( !it.IsAtEnd() ) 
+  {
+    it.Set( value );
+    std::cout << it.Get() << std::endl;
+    ++it;
+  }
+
+  // Declare the type for the Log filter
+  typedef itk::LogImageFilter< InputImageType,
+                               OutputImageType  >  FilterType;
+            
+
+  // Create an ADD Filter                                
+  FilterType::Pointer filter = FilterType::New();
+
+
+  // Connect the input images
+  filter->SetInput( inputImage ); 
+
+  // Get the Smart Pointer to the Filter Output 
+  OutputImageType::Pointer outputImage = filter->GetOutput();
+
+  
+  // Execute the filter
+  filter->Update();
+
+  // Create an iterator for going through the image output
+  OutputIteratorType ot(outputImage, outputImage->GetRequestedRegion());
+  
+  //  Check the content of the result image
+  std::cout << "Verification of the output " << std::endl;
+  const OutputImageType::PixelType epsilon = 1e-10;
+  ot.Begin();
+  it.Begin();
+  while( !ot.IsAtEnd() ) 
+  {
+    std::cout <<  ot.Get() << " = ";
+    std::cout <<  log( it.Get() )  << std::endl; 
+    const InputImageType::PixelType  input  = it.Get();
+    const OutputImageType::PixelType output = ot.Get();
+    const OutputImageType::PixelType logarithm  = log(input);
+    if( fabs( logarithm - output ) > epsilon )
+    {
+      std::cerr << "Error in itkLogImageFilterTest " << std::endl;
+      std::cerr << " log( " << input << ") = " << logarithm << std::endl;
+      std::cerr << " differs from " << output;
+      std::cerr << " by more than " << epsilon << std::endl;
+      return 1;
+    }
+    ++ot;
+    ++it;
+  }
+
+
+  //---------------------------------------
+  // This section tests for LogImageAdaptor
+  //---------------------------------------
+
+  typedef itk::LogImageAdaptor<InputImageType,
+                          OutputImageType::PixelType>  AdaptorType;
+
+  AdaptorType::Pointer logAdaptor = AdaptorType::New();
+
+  logAdaptor->SetImage( inputImage );
+
+  typedef itk::SubtractImageFilter<
+                        OutputImageType,
+                        AdaptorType,
+                        OutputImageType   > DiffFilterType;
+
+  DiffFilterType::Pointer diffFilter = DiffFilterType::New();
+
+  diffFilter->SetInput1( outputImage );
+  diffFilter->SetInput2( logAdaptor  );
+
+  diffFilter->Update();
+
+  // Get the Smart Pointer to the Diff filter Output
+  OutputImageType::Pointer diffImage = diffFilter->GetOutput();
+
+  //  Check the content of the diff image
+  std::cout << "Comparing the results with those of an Adaptor" << std::endl;
+  std::cout << "Verification of the output " << std::endl;
+
+  // Create an iterator for going through the image output
+  OutputIteratorType dt(diffImage, diffImage->GetRequestedRegion());
+  
+  dt.Begin();
+  while( !dt.IsAtEnd() ) 
+  {
+    std::cout <<  dt.Get() << std::endl;
+    const OutputImageType::PixelType diff = dt.Get();
+    if( fabs( diff ) > epsilon )
+    {
+      std::cerr << "Error in itkLogImageFilterTest " << std::endl;
+      std::cerr << "Comparing results with Adaptors" << std::endl;
+      std::cerr << " difference = " << diff << std::endl;
+      std::cerr << " differs from 0 ";
+      std::cerr << " by more than " << epsilon << std::endl;
+      return 1;
+    }
+    ++dt;
+  }
+
+
+
+
+  
+  return 0;
+
+}
+
+
+
+
---
+++ Testing/Code/BasicFilters/itkTanImageFilterAndAdaptorTest.cxx
@@ -0,0 +1,199 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkTanImageFilterAndAdaptorTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) 2000 National Library of Medicine
+  All rights reserved.
+
+  See COPYRIGHT.txt for copyright details.
+
+=========================================================================*/
+
+
+
+#include <itkImage.h>
+#include <itkTanImageFilter.h>
+#include <itkTanImageAdaptor.h>
+#include <itkSimpleImageRegionIterator.h>
+#include <itkSubtractImageFilter.h>
+
+
+int main() 
+{
+
+  // Define the dimension of the images
+  const unsigned int ImageDimension = 3;
+
+  // Declare the types of the images
+  typedef itk::Image<float, ImageDimension>  InputImageType;
+  typedef itk::Image<float, ImageDimension>  OutputImageType;
+
+  
+  
+  // Declare Iterator types apropriated for each image 
+  typedef itk::SimpleImageRegionIterator<
+                                  InputImageType>  InputIteratorType;
+
+  typedef itk::SimpleImageRegionIterator<
+                                  OutputImageType>  OutputIteratorType;
+
+
+
+  // Declare the type of the index to access images
+  typedef itk::Index<ImageDimension>         IndexType;
+
+  // Declare the type of the size 
+  typedef itk::Size<ImageDimension>          SizeType;
+
+  // Declare the type of the Region
+  typedef itk::ImageRegion<ImageDimension>   RegionType;
+
+  // Create two images
+  InputImageType::Pointer inputImage  = InputImageType::New();
+  
+  // Define their size, and start index
+  SizeType size;
+  size[0] = 2;
+  size[1] = 2;
+  size[2] = 2;
+
+  IndexType start;
+  start[0] = 0;
+  start[1] = 0;
+  start[2] = 0;
+
+  RegionType region;
+  region.SetIndex( start );
+  region.SetSize( size );
+
+  // Initialize Image A
+  inputImage->SetLargestPossibleRegion( region );
+  inputImage->SetBufferedRegion( region );
+  inputImage->SetRequestedRegion( region );
+  inputImage->Allocate();
+  // Create one iterator for the Input Image (this is a light object)
+  InputIteratorType it( inputImage, inputImage->GetBufferedRegion() );
+
+  // Initialize the content of Image A
+  const double pi    = atan( 1.0 ) * 4.0;
+  const double value = pi / 6.0;
+  std::cout << "Content of the Input " << std::endl;
+  it.Begin();
+  while( !it.IsAtEnd() ) 
+  {
+    it.Set( value );
+    std::cout << it.Get() << std::endl;
+    ++it;
+  }
+
+  // Declare the type for the Tan filter
+  typedef itk::TanImageFilter< InputImageType,
+                               OutputImageType  >  FilterType;
+            
+
+  // Create an ADD Filter                                
+  FilterType::Pointer filter = FilterType::New();
+
+
+  // Connect the input images
+  filter->SetInput( inputImage ); 
+
+  // Get the Smart Pointer to the Filter Output 
+  OutputImageType::Pointer outputImage = filter->GetOutput();
+
+  
+  // Execute the filter
+  filter->Update();
+
+  // Create an iterator for going through the image output
+  OutputIteratorType ot(outputImage, outputImage->GetRequestedRegion());
+  
+  //  Check the content of the result image
+  std::cout << "Verification of the output " << std::endl;
+  const OutputImageType::PixelType epsilon = 1e-10;
+  ot.Begin();
+  it.Begin();
+  while( !ot.IsAtEnd() ) 
+  {
+    std::cout <<  ot.Get() << " = ";
+    std::cout <<  tan( it.Get() )  << std::endl; 
+    const InputImageType::PixelType  input  = it.Get();
+    const OutputImageType::PixelType output = ot.Get();
+    const OutputImageType::PixelType tangent  = tan(input);
+    if( fabs( tangent - output ) > epsilon )
+    {
+      std::cerr << "Error in itkTanImageFilterTest " << std::endl;
+      std::cerr << " tan( " << input << ") = " << tangent << std::endl;
+      std::cerr << " differs from " << output;
+      std::cerr << " by more than " << epsilon << std::endl;
+      return 1;
+    }
+    ++ot;
+    ++it;
+  }
+
+  //---------------------------------------
+  // This section tests for TanImageAdaptor
+  //---------------------------------------
+
+  typedef itk::TanImageAdaptor<InputImageType,
+                          OutputImageType::PixelType>  AdaptorType;
+
+  AdaptorType::Pointer tanAdaptor = AdaptorType::New();
+
+  tanAdaptor->SetImage( inputImage );
+
+  typedef itk::SubtractImageFilter<
+                        OutputImageType,
+                        AdaptorType,
+                        OutputImageType   > DiffFilterType;
+
+  DiffFilterType::Pointer diffFilter = DiffFilterType::New();
+
+  diffFilter->SetInput1( outputImage );
+  diffFilter->SetInput2( tanAdaptor  );
+
+  diffFilter->Update();
+
+  // Get the Smart Pointer to the Diff filter Output
+  OutputImageType::Pointer diffImage = diffFilter->GetOutput();
+
+  //  Check the content of the diff image
+  std::cout << "Comparing the results with those of an Adaptor" << std::endl;
+  std::cout << "Verification of the output " << std::endl;
+
+  // Create an iterator for going through the image output
+  OutputIteratorType dt(diffImage, diffImage->GetRequestedRegion());
+  
+  dt.Begin();
+  while( !dt.IsAtEnd() ) 
+  {
+    std::cout <<  dt.Get() << std::endl;
+    const OutputImageType::PixelType diff = dt.Get();
+    if( fabs( diff ) > epsilon )
+    {
+      std::cerr << "Error in itkTanImageFilterTest " << std::endl;
+      std::cerr << "Comparing results with Adaptors" << std::endl;
+      std::cerr << " difference = " << diff << std::endl;
+      std::cerr << " differs from 0 ";
+      std::cerr << " by more than " << epsilon << std::endl;
+      return 1;
+    }
+    ++dt;
+  }
+
+
+
+  
+  return 0;
+
+}
+
+
+
+
