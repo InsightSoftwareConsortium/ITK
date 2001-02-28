6ff90bbdb542f04fbe5ec1187a27b635c3ace7f6
Bill Lorensen <bill.lorensen@gmail.com>
2001-02-28 10:24:50 -0500

    ENH: more extensive tetsing.

--- Testing/Code/Algorithms/itkTraitsTest.cxx
+++ Testing/Code/Algorithms/itkTraitsTest.cxx
@@ -10,8 +10,10 @@
 int main ()
 {
   // Test some numeric traits
-  float min = itk::NumericTraits<float>::min();
-  unsigned short max = itk::NumericTraits<unsigned short>::max();
+  float floatMin = itk::NumericTraits<float>::min();
+  float floatMax = itk::NumericTraits<float>::max();
+  unsigned short unsignedShortMax = itk::NumericTraits<unsigned short>::max();
+  unsigned short unsignedShortMin;
 
   char adder=10;
   itk::NumericTraits<char>::AccumulateType addItUp = itk::NumericTraits<char>::Zero;
@@ -20,16 +22,28 @@ int main ()
     addItUp += adder;
     }
   
+  std::cout << "for (int = 0; i < 100; i++) { addItUp += adder;} = " << addItUp << std::endl;
+  
+  unsignedShortMin = unsignedShortMax + 1;
+  std::cout << "unsigned short min is " << unsignedShortMin << std::endl;
   // Test some pixel traits
-  itk::ScalarTraits<float>::ValueType foo = min; //should be float
+  itk::ScalarTraits<float>::ValueType foo = floatMax; //should be float
+  std::cout << "itk::ScalarTraits<float>::ValueType foo = floatMax;" << std::endl;
+
   itk::VectorTraits<long>::ValueType fooLong = itk::NumericTraits<long>::max();
   
+
   itk::Scalar<float> scalar;
+  scalar.SetScalar(23.0);
   itk::Scalar<float>::ValueType s = scalar.GetScalar();
-  itk::Vector<unsigned short> vector;
-  const itk::Vector<unsigned short>::VectorType v = vector;
-  
-  itk::Scalar<float>::ValueType scalarMin = min;
+  std::cout << "itk::Scalar<float>::ValueType s = scalar.GetScalar();" << std::endl;
+  std::cout << "s is " << s << std::endl;
+  itk::Vector<unsigned short,256> vector;
+  const itk::Vector<unsigned short,256>::VectorType v = vector;
+  std::cout << "const itk::Vector<unsigned short,256>::VectorType v = vector;" << std::endl;
+  vector.Fill(unsignedShortMax);
+  std::cout << "vector[255] is " << vector[255] << std::endl;
+  itk::Scalar<float>::ValueType scalarMin = floatMin;
   itk::Vector<long>::ValueType vectorMax = fooLong;
   
   itk::Image<unsigned char,2>::Pointer i2 = itk::Image<unsigned char,2>::New();
