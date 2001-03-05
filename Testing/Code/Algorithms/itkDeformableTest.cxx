0fa1130fad505d690e91e19a0d81b175f95130a5
Ting Chen <chenting@graphics.cis.upenn.edu>
2001-03-05 16:47:59 -0500

    fix some mistakes, add test on potential

--- Testing/Code/Algorithms/itkDeformableTest.cxx
+++ Testing/Code/Algorithms/itkDeformableTest.cxx
@@ -9,6 +9,11 @@
 #include "itkDeformableMesh.h"
 #include "itkBalloonForceFilter.h"
 #include "itkTriangleCell.h"
+#include "itkImage.h"
+#include "itkSimpleImageRegionIterator.h"
+
+const int WIDTH = 20;
+const int HEIGHT = 20;
 
 using namespace itk;
 
@@ -16,6 +21,7 @@ typedef itk::DeformableMesh<float>  DMesh;
 typedef itk::Mesh<float>  MyMesh;
 typedef itk::BalloonForceFilter<MyMesh, MyMesh> BFilter;
 typedef itk::TriangleCell<DMesh::PixelType, DMesh::CellType>	   TriCell;
+typedef itk::Image<unsigned short,3> IImage;
 
 int main(void)
 {
@@ -24,11 +30,35 @@ int main(void)
   MyMesh::Pointer force(MyMesh::New());
   MyMesh::Pointer displace(MyMesh::New());
   MyMesh::Pointer derive(MyMesh::New());
-  
+	
+  IImage::Pointer inputimg=IImage::New();
+  IImage::SizeType size={{HEIGHT,WIDTH,1}};
+  IImage::IndexType index=IImage::IndexType::ZeroIndex;
+  IImage::RegionType region;
+
+  region.SetSize(size);
+  region.SetIndex(index);
+
+  inputimg->SetLargestPossibleRegion( region );
+  inputimg->SetBufferedRegion( region );
+  inputimg->SetRequestedRegion( region );
+  inputimg->Allocate();
+  	
+  itk::SimpleImageRegionIterator <IImage> it(inputimg, region);
+  it.Begin();	
+  int k=0;
+  while( !it.IsAtEnd()) {    
+	if ( (k%WIDTH==5) || (k%WIDTH==15) || (k/HEIGHT==5) || (k/HEIGHT==15) ||
+		(k%WIDTH==4) || (k%WIDTH==16) || (k/HEIGHT==4) || (k/HEIGHT==16) ) it.Set(1);
+	else it.Set(0);
+	k++;
+	++it;
+  }
+
   m_Mesh->SetDefault();
-  m_Mesh->SetCenter(0, 0, 0);
-  m_Mesh->SetResolution(4, 9);
+  m_Mesh->SetResolution(1, 20);
   m_Mesh->SetScale(1.0, 1.0, 1.0);
+  m_Mesh->SetCenter(10, 10, 0);
   m_Mesh->Allocate();
 
 //////////////////////////////////////////////////
@@ -73,16 +103,42 @@ int main(void)
 //////////////////////////////////////////////////
 */
   m_Filter->SetInput(m_Mesh);
-  m_Filter->SetResolution(4, 9, 1);
+  m_Filter->SetPotential(inputimg);
+  m_Filter->SetResolution(1, 20, 1);
+  m_Filter->SetCenter(10, 10, 0);
   m_Filter->SetForces(force);
   m_Filter->SetDisplacements(displace);
   m_Filter->SetDerives(derive);
   m_Filter->Initialize();
   m_Filter->SetStiffnessMatrix();
-  m_Filter->ComputeForce();
-  m_Filter->ComputeDt();
-  m_Filter->Advance();
-  m_Filter->ComputeOutput();
+  int i, j, sign;
+  for (k=0; k<30; k++) {
+	m_Filter->Reset();
+	m_Filter->ComputeForce();
+	m_Filter->ComputeDt();
+	m_Filter->Advance();
+    m_Filter->ComputeOutput();
+	it.Begin();
+	for(int i = 0;i < HEIGHT; i++){
+	  for (int j = 0; j < WIDTH; j++){
+		sign = 0;
+		point = myPoints->Begin();
+		while(  point != myPoints->End() ) {
+		  ds = point.Value();
+		  ++point;
+		  if ((i == (int)ds[0]) && (j == (int)ds[1])) {
+			std::cout << "*" ;
+			sign = 1;
+			break;
+		  }
+		}
+		if (sign == 0) std::cout<<it.Get();
+		++it;
+	  }
+	  std::cout<<std::endl;
+	}
+    getchar();
+  }
 //  m_Filter->Delete();
 //  m_Mesh->Delete();
 //  force->Delete();
@@ -97,5 +153,25 @@ int main(void)
 	std::cout << "Point: " << ds[0] << ds[1] <<ds[2] << std::endl;
   }
 */
+	it.Begin();
+	for(int i = 0;i < HEIGHT; i++){
+	  for (int j = 0; j < WIDTH; j++){
+		sign = 0;
+		point = myPoints->Begin();
+		while(  point != myPoints->End() ) {
+		  ds = point.Value();
+		  ++point;
+		  if ((i == (int)ds[0]) && (j == (int)ds[1])) {
+			std::cout << "*" ;
+			sign = 1;
+			break;
+		  }
+		}
+		if (sign == 0) std::cout<<it.Get();
+		++it;
+	  }
+	  std::cout<<std::endl;
+	}
+
   return 0;
 }
