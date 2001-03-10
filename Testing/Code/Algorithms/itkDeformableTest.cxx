75a6befcdfbd1ab77fdaaf0ae491c0e282511d6a
Ting Chen <chenting@graphics.cis.upenn.edu>
2001-03-10 18:53:17 -0500

    delete the getchar()

--- Testing/Code/Algorithms/itkDeformableTest.cxx
+++ Testing/Code/Algorithms/itkDeformableTest.cxx
@@ -12,24 +12,28 @@
 #include "itkImage.h"
 #include "itkSimpleImageRegionIterator.h"
 
-const int WIDTH = 20;
-const int HEIGHT = 20;
+const int WIDTH = 25;
+const int HEIGHT = 25;
 
 using namespace itk;
 
 typedef itk::DeformableMesh<float>  DMesh;
 typedef itk::Mesh<float>  MyMesh;
-typedef itk::BalloonForceFilter<MyMesh, MyMesh> BFilter;
+typedef itk::BalloonForceFilter<DMesh, DMesh> BFilter;
 typedef itk::TriangleCell<DMesh::PixelType, DMesh::CellType>	   TriCell;
 typedef itk::Image<unsigned short,3> IImage;
 
 int main(void)
 {
+
+
   DMesh::Pointer m_Mesh(DMesh::New());
   BFilter::Pointer m_Filter = BFilter::New();
-  MyMesh::Pointer force(MyMesh::New());
-  MyMesh::Pointer displace(MyMesh::New());
-  MyMesh::Pointer derive(MyMesh::New());
+  DMesh::Pointer force(DMesh::New());
+  DMesh::Pointer displace(DMesh::New());
+  DMesh::Pointer derive(DMesh::New());
+  DMesh::Pointer normal(DMesh::New());
+  DMesh::Pointer location(DMesh::New());
 	
   IImage::Pointer inputimg=IImage::New();
   IImage::SizeType size={{HEIGHT,WIDTH,1}};
@@ -47,26 +51,27 @@ int main(void)
   itk::SimpleImageRegionIterator <IImage> it(inputimg, region);
   it.Begin();	
   int k=0;
-  while( !it.IsAtEnd()) {    
-	if ( (k%WIDTH==5) || (k%WIDTH==15) || (k/HEIGHT==5) || (k/HEIGHT==15) ||
-		(k%WIDTH==4) || (k%WIDTH==16) || (k/HEIGHT==4) || (k/HEIGHT==16) ) it.Set(1);
-	else it.Set(0);
+  while( !it.IsAtEnd()) {
+	if ((k%10 == 0) || 
+		((k/10)%10 == 0)) {
+	  it.Set(1);
+	} else {
+	  it.Set(0);
+	}
 	k++;
 	++it;
   }
 
   m_Mesh->SetDefault();
-  m_Mesh->SetResolution(1, 20);
+  m_Mesh->SetResolution(1, 10);
   m_Mesh->SetScale(1.0, 1.0, 1.0);
-  m_Mesh->SetCenter(10, 10, 0);
+  m_Mesh->SetCenter(13, 13, 0);
   m_Mesh->Allocate();
 
 //////////////////////////////////////////////////
 // for test
 
   MyMesh::PointType ds;
-  MyMesh::PixelType dc;
-  const unsigned long *dl;
   m_Mesh->SetDefault();
 
   MyMesh::PointsContainerPointer      myPoints = m_Mesh->GetPoints();
@@ -78,100 +83,40 @@ int main(void)
 	std::cout << "Point: " << ds[0] << ds[1] <<ds[2] << std::endl;
   }
 
-
-/*
-  MyMesh::CellDataContainerPointer      myCellData = m_Mesh->GetCellData();
-  MyMesh::CellDataContainer::Iterator   celldata   = myCellData->Begin();
-  while(  celldata != myCellData->End() )
-  {
-	  dc = celldata.Value();
-    ++celldata;
-  }
-
-  MyMesh::CellsContainerPointer      myCells = m_Mesh->GetCells();
-  MyMesh::CellsContainer::Iterator   cells   = myCells->Begin();
-  while(  cells != myCells->End() )
-  {
-	  dl = cells.Value()->GetPointIds();
-    ++cells;
-  }
-//  SetPoint(1, (PointType)d);
-
-//  m_Mesh->GetPoint(3, (ds));
-
-
-//////////////////////////////////////////////////
-*/
   m_Filter->SetInput(m_Mesh);
   m_Filter->SetPotential(inputimg);
-  m_Filter->SetResolution(1, 20, 1);
-  m_Filter->SetCenter(10, 10, 0);
+  m_Filter->SetResolution(1, 10, 1);
+  m_Filter->SetCenter(13, 13, 0);
+  m_Filter->SetLocations(location);
   m_Filter->SetForces(force);
+  m_Filter->SetNormals(normal);
   m_Filter->SetDisplacements(displace);
   m_Filter->SetDerives(derive);
   m_Filter->Initialize();
   m_Filter->SetStiffnessMatrix();
-  int i, j, sign;
-  for (k=0; k<30; k++) {
-	m_Filter->Reset();
+  int sign;
+
+
+  for (k=0; k<10; k++) {
+
 	m_Filter->ComputeForce();
 	m_Filter->ComputeDt();
 	m_Filter->Advance();
-    m_Filter->ComputeOutput();
-	it.Begin();
-	for(int i = 0;i < HEIGHT; i++){
-	  for (int j = 0; j < WIDTH; j++){
-		sign = 0;
-		point = myPoints->Begin();
-		while(  point != myPoints->End() ) {
-		  ds = point.Value();
-		  ++point;
-		  if ((i == (int)ds[0]) && (j == (int)ds[1])) {
-			std::cout << "*" ;
-			sign = 1;
-			break;
-		  }
-		}
-		if (sign == 0) std::cout<<it.Get();
-		++it;
-	  }
-	  std::cout<<std::endl;
-	}
-    getchar();
+
   }
-//  m_Filter->Delete();
-//  m_Mesh->Delete();
-//  force->Delete();
-//  displace->Delete();
-//  derive->Delete();
-/*
-  point    = myPoints->Begin();
-  while(  point != myPoints->End() )
+
+
+
+  m_Filter->GapSearch();
+  m_Filter->ComputeOutput();
+
+  MyMesh::PointsContainerPointer      myoutput = m_Filter->GetOutput()->GetPoints();
+  MyMesh::PointsContainer::Iterator   m_output = myoutput->Begin();
+  while(  m_output != myoutput->End() )
   {
 	  ds = point.Value();
-    ++point;
+    ++m_output;
 	std::cout << "Point: " << ds[0] << ds[1] <<ds[2] << std::endl;
   }
-*/
-	it.Begin();
-	for(int i = 0;i < HEIGHT; i++){
-	  for (int j = 0; j < WIDTH; j++){
-		sign = 0;
-		point = myPoints->Begin();
-		while(  point != myPoints->End() ) {
-		  ds = point.Value();
-		  ++point;
-		  if ((i == (int)ds[0]) && (j == (int)ds[1])) {
-			std::cout << "*" ;
-			sign = 1;
-			break;
-		  }
-		}
-		if (sign == 0) std::cout<<it.Get();
-		++it;
-	  }
-	  std::cout<<std::endl;
-	}
-
   return 0;
 }
--- Testing/Code/Algorithms/itkGibbsTest.cxx
+++ Testing/Code/Algorithms/itkGibbsTest.cxx
@@ -14,8 +14,8 @@
 #include "itkGibbsPriorFilter.h"
 
 
-#define   IMGWIDTH            256
-#define   IMGHEIGHT           256
+#define   IMGWIDTH            5
+#define   IMGHEIGHT           5
 #define   NFRAMES             1
 #define   NUMBANDS            1  
 #define   NDIMENSION          3
@@ -25,10 +25,6 @@
 //using namespace itk;
 
 int main(){
-  unsigned short TestImage[65535];
-  FILE *input;
-  input = fopen("34.raw", "rb");
-  fread(TestImage, 2, 65535, input);
   
   typedef itk::Image<itk::Vector<unsigned short,NUMBANDS>,NDIMENSION> VecImageType; 
 
@@ -62,136 +58,17 @@ int main(){
   //--------------------------------------------------------------------------
   //Manually create and store each vector
   //--------------------------------------------------------------------------
-  //Slice 1
-  //Vector 1
   int i = 0;
   while ( !outIt.IsAtEnd() ) { 
-    dblVec[0] = TestImage[i]; 
+    if ( ((i>5)&&(i<9)) || ((i>10)&&(i<14)) || ((i>15)&&(i<19)) )
+	dblVec[0] = 1;
+	else dblVec[0] = 0;
 	outIt.Set(dblVec); 
 	++outIt;
 	i++;
   }
-/*
-  //Vector 2
-  dblVec[0] = 0; 
-  outIt.Set(dblVec); 
-  ++outIt;
 
-  //Vector 3
-  dblVec[0] = 0; 
-  outIt.Set(dblVec); 
-  ++outIt;
 
-  //Vector 4
-  dblVec[0] = 0; 
-  outIt.Set(dblVec); 
-  ++outIt;
-
-  //Vector 5
-  dblVec[0] = 0; 
-  outIt.Set(dblVec); 
-  ++outIt;
-
-  //Vector 6
-  dblVec[0] = 0; 
-  outIt.Set(dblVec); 
-  ++outIt;
-
-  //Vector 7
-  dblVec[0] = 10; 
-  outIt.Set(dblVec); 
-  ++outIt;
-
-  //Vector 8
-  dblVec[0] = 10; 
-  outIt.Set(dblVec); 
-  ++outIt;
-  
-  //Vector 9
-  dblVec[0] = 10; 
-  outIt.Set(dblVec); 
-  ++outIt;
-  
-  //Vector 10
-  dblVec[0] = 0; 
-  outIt.Set(dblVec); 
-  ++outIt;
-  
-  //Vector 11
-  dblVec[0] = 0; 
-  outIt.Set(dblVec); 
-  ++outIt;
-  
-  //Vector 12
-  dblVec[0] = 10; 
-  outIt.Set(dblVec); 
-  ++outIt;
-  
-  //Vector 13
-  dblVec[0] = 8; 
-  outIt.Set(dblVec); 
-  ++outIt; 
-  
-  //Vector 14
-  dblVec[0] = 10; 
-  outIt.Set(dblVec); 
-  ++outIt;
-
-  //Vector 15
-  dblVec[0] = 0; 
-  outIt.Set(dblVec); 
-  ++outIt;
-
-  //Vector 16
-  dblVec[0] = 0; 
-  outIt.Set(dblVec); 
-  ++outIt;
-
-  //Vector 17
-  dblVec[0] = 10; 
-  outIt.Set(dblVec); 
-  ++outIt;
-
-  //Vector 18
-  dblVec[0] = 10; 
-  outIt.Set(dblVec); 
-  ++outIt;
-
-  //Vector 19
-  dblVec[0] = 10; 
-  outIt.Set(dblVec); 
-  ++outIt;
-
-  //Vector 20
-  dblVec[0] = 0; 
-  outIt.Set(dblVec); 
-  ++outIt;
-
-  //Vector 21
-  dblVec[0] = 0; 
-  outIt.Set(dblVec); 
-  ++outIt;
-
-  //Vector 22
-  dblVec[0] = 0; 
-  outIt.Set(dblVec); 
-  ++outIt;
-
-  //Vector 23
-  dblVec[0] = 0; 
-  outIt.Set(dblVec); 
-  ++outIt;
-
-  //Vector 24
-  dblVec[0] = 0; 
-  outIt.Set(dblVec); 
-  ++outIt;
-
-  //Vector 25
-  dblVec[0] = 0; 
-  outIt.Set(dblVec); 
-  ++outIt;
-*/
   //---------------------------------------------------------------
   //Generate the training data
   //---------------------------------------------------------------
@@ -226,116 +103,16 @@ int main(){
   //--------------------------------------------------------------------------
   //Manually create and store each vector
   //--------------------------------------------------------------------------
-  //Slice 1
-  //Pixel no. 1
   i = 0;
   while ( !classoutIt.IsAtEnd() ) {  
-	if ( (i%IMGWIDTH<160) && (i%IMGWIDTH>140) && 
-		(i/IMGWIDTH<140) && (i/IMGWIDTH>120)) {
+	if ( ((i>5)&&(i<9)) || ((i>10)&&(i<14)) || ((i>15)&&(i<19)) ){
 	  classoutIt.Set( 2 );
-//	  TestImage[i] = 256;
 	}
 	else classoutIt.Set( 1 );
     ++classoutIt;
 	i++;
   }
-/*
-  //Pixel no. 2 
-  classoutIt.Set( 1 );
-  ++classoutIt;
 
-  //Pixel no. 3
-  classoutIt.Set( 1 );
-  ++classoutIt;
-
-  //Pixel no. 4
-  classoutIt.Set( 1 );
-  ++classoutIt;
-
-  //Pixel no. 5
-  classoutIt.Set( 1 );
-  ++classoutIt;
-
-  //Pixel no. 6 
-  classoutIt.Set( 1 );
-  ++classoutIt;
-
-  //Pixel no. 7
-  classoutIt.Set( 2 );
-  ++classoutIt;
-
-  //Pixel no. 8
-  classoutIt.Set( 2 );
-  ++classoutIt;
-  
-  //Pixel no. 9
-  classoutIt.Set( 2 );
-  ++classoutIt;
-
-  //Pixel no. 10 
-  classoutIt.Set( 1 );
-  ++classoutIt;
-
-  //Pixel no. 11
-  classoutIt.Set( 1 );
-  ++classoutIt;
-
-  //Pixel no. 12
-  classoutIt.Set( 2 );
-  ++classoutIt;
-
-  //Pixel no. 13
-  classoutIt.Set( 1 );
-  ++classoutIt;
-
-  //Pixel no. 14
-  classoutIt.Set( 2);
-  ++classoutIt;
-
-  //Pixel no. 15
-  classoutIt.Set( 1 );
-  ++classoutIt;
-
-  //Pixel no. 16
-  classoutIt.Set( 1 );
-  ++classoutIt;
-
-  //Pixel no. 17
-  classoutIt.Set( 2 );
-  ++classoutIt;
-
-  //Pixel no. 18 
-  classoutIt.Set( 2 );
-  ++classoutIt;
-
-  //Pixel no. 19
-  classoutIt.Set( 2 );
-  ++classoutIt;
-
-  //Pixel no. 20
-  classoutIt.Set( 1 );
-  ++classoutIt;
-
-  //Pixel no. 21
-  classoutIt.Set( 1 );
-  ++classoutIt;
-
-  //Pixel no. 22 
-  classoutIt.Set( 1 );
-  ++classoutIt;
-
-  //Pixel no. 23
-  classoutIt.Set( 1 );
-  ++classoutIt;
-
-  //Pixel no. 24
-  classoutIt.Set( 1 );
-  ++classoutIt;  
-
-  //Pixel no. 25
-  classoutIt.Set( 1 );
-  ++classoutIt;
-*/
   //----------------------------------------------------------------------
   // Test code for the supervised classifier algorithm
   //----------------------------------------------------------------------
@@ -362,7 +139,8 @@ int main(){
   applyGibbsImageFilter->SetNumClasses(NUM_CLASSES);
   applyGibbsImageFilter->SetMaxNumIter(MAX_NUM_ITER);
   applyGibbsImageFilter->SetErrorTollerance(0.00);
-  applyGibbsImageFilter->SetBoundaryGradient(8);
+  applyGibbsImageFilter->SetClusterSize(0);
+  applyGibbsImageFilter->SetBoundaryGradient(1);
  
   applyGibbsImageFilter->SetInput(vecImage);
   applyGibbsImageFilter
@@ -382,25 +160,10 @@ int main(){
 
   i = 0;
   while ( !labeloutIt.IsAtEnd() ) {
-	TestImage[i] = labeloutIt.Get()*32000;
+	std::cout <<labeloutIt.Get() << std::endl;
 	i++;
 	++labeloutIt;
   }
-
-  fclose(input);
-  FILE *output=fopen("result.raw", "wb");
-  fwrite(TestImage, 2, IMGWIDTH*IMGHEIGHT, output);
-  fclose(output);
-  //Verify if the results were as per expectation
-  bool passTest = true;
-
-//  outIt.Begin();
-
-  std::cout<< std::endl;
-  if( passTest == true ) 
-    std::cout<< "Gibbs Prior Test Passed" << std::endl;
-  else 
-    std::cout<< "Gibbs Prior Test failed" << std::endl;
 
   return 0;
 }
