acfc869b8795495d0313914a2316cd2070deaa46
Josh Cates <cates@cs.utah.edu>
2001-03-13 12:42:20 -0500

    Added test for itkDiscreteGaussianImageFilter.h/.txx

---
+++ Testing/Code/BasicFilters/itkDiscreteGaussianImageFilterTest.cxx
@@ -0,0 +1,53 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkDiscreteGaussianImageFilterTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) 2000 National Library of Medicine
+  All rights reserved.
+
+  See COPYRIGHT.txt for copyright details.
+
+=========================================================================*/
+
+#include <iostream>
+#include "itkImage.h"
+#include "itkDiscreteGaussianImageFilter.h"
+#include "itkNullImageToImageFilterDriver.txx"
+#include "itkVector.h"
+
+int main(int argc, char *argv[])
+{
+  try
+    {
+      typedef itk::Image<float, 2> ImageType;
+      
+      // Set up filter
+      itk::DiscreteGaussianImageFilter<ImageType, ImageType>::Pointer 
+        filter =
+        itk::DiscreteGaussianImageFilter<ImageType, ImageType>::New();
+      filter->SetVariance(1.0f);
+      filter->SetMaximumError(.01f);
+      
+      // Run Test
+      itk::Size<2> sz;
+      sz[0] = 100 ; //atoi(argv[1]);
+      sz[1] = 100 ; // atoi(argv[2]);
+      //      sz[2] = 10;//atoi(argv[3]);
+      //      sz[3] = 5;//atoi(argv[4]);
+      itk::NullImageToImageFilterDriver< ImageType, ImageType > test1;
+      test1.SetImageSize(sz);
+      test1.SetFilter(filter.GetPointer());
+      test1.Execute();
+    }
+  catch(itk::ExceptionObject &err)
+    {
+      (&err)->Print(std::cerr);
+      return 1;
+    } 
+  return 0;   
+}
