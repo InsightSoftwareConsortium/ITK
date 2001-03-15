57399c5e06f1272102b1a1e17d1aa6f3fd3eb358
Luis Ibanez <luis.ibanez@kitware.com>
2001-03-15 17:18:21 -0500

    Test for the Gradient Recursive Gaussian Image Filter.

---
+++ Testing/Code/BasicFilters/itkGradientRecursiveGaussianFilterTest.cxx
@@ -0,0 +1,152 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkGradientRecursiveGaussianFilterTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) 2000 National Library of Medicine
+  All rights reserved.
+
+  See COPYRIGHT.txt for copyright details.
+
+=========================================================================*/
+
+
+
+
+#include <itkPhysicalImage.h>
+#include <itkGradientRecursiveGaussianImageFilter.h>
+#include <itkSimpleImageRegionIterator.h>
+#include <itkCovariantVector.h>
+
+
+int main() 
+{
+
+  // Define the dimension of the images
+  const unsigned int myDimension = 3;
+
+  // Declare gradient type
+  typedef itk::CovariantVector<float, myDimension> myGradientType;
+
+  // Declare the types of the images
+  typedef itk::PhysicalImage<float, myDimension>           myImageType;
+  typedef itk::PhysicalImage<myGradientType, myDimension>  myGradientImageType;
+
+  // Declare the type of the index to access images
+  typedef itk::Index<myDimension>             myIndexType;
+
+  // Declare the type of the size 
+  typedef itk::Size<myDimension>              mySizeType;
+
+  // Declare the type of the Region
+  typedef itk::ImageRegion<myDimension>        myRegionType;
+
+  // Create the image
+  myImageType::Pointer inputImage  = myImageType::New();
+
+  
+  // Define their size, and start index
+  mySizeType size;
+  size[0] = 8;
+  size[1] = 8;
+  size[2] = 8;
+
+  myIndexType start;
+  start = myIndexType::ZeroIndex;
+
+  myRegionType region;
+  region.SetIndex( start );
+  region.SetSize( size );
+
+  // Initialize Image A
+  inputImage->SetLargestPossibleRegion( region );
+  inputImage->SetBufferedRegion( region );
+  inputImage->SetRequestedRegion( region );
+  inputImage->Allocate();
+
+  // Declare Iterator types apropriated for each image 
+  typedef itk::SimpleImageRegionIterator<myImageType>  myIteratorType;
+
+  typedef itk::SimpleImageRegionIterator<
+                                 myGradientImageType>  myOutputIteratorType;
+
+  // Create one iterator for the Input Image A (this is a light object)
+  myIteratorType it( inputImage, inputImage->GetRequestedRegion() );
+
+  // Initialize the content of Image A
+  std::cout << "Input Image initialization " << std::endl;
+  it.Begin();
+  while( !it.IsAtEnd() ) 
+  {
+    it.Set( 0.0 );
+    ++it;
+  }
+
+  size[0] = 8;
+  size[1] = 8;
+  size[2] = 8;
+
+  start[0] = 2;
+  start[1] = 2;
+  start[2] = 2;
+
+  // Create one iterator for an internal region
+  region.SetSize( size );
+  region.SetIndex( start );
+  myIteratorType itb( inputImage, region );
+
+  // Initialize the content the internal region
+  itb.Begin();
+  while( !itb.IsAtEnd() ) 
+  {
+    itb.Set( 100.0 );
+    ++itb;
+  }
+
+  // Declare the type for the 
+  typedef itk::GradientRecursiveGaussianImageFilter<
+                                            myImageType,
+                                            myGradientImageType,
+                                            float       >  myFilterType;
+            
+
+  // Create a  Filter                                
+  myFilterType::Pointer filter = myFilterType::New();
+
+
+  // Connect the input images
+  filter->SetInput( inputImage ); 
+
+  // Get the Smart Pointer to the Filter Output 
+  myGradientImageType::Pointer outputImage = filter->GetOutput();
+
+  
+  // Execute the filter
+  filter->Update();
+
+  // Create an iterator for going through the output image
+  myOutputIteratorType itg( outputImage, 
+                            outputImage->GetRequestedRegion() );
+  
+  //  Print the content of the result image
+  std::cout << " Result " << std::endl;
+  itg.Begin();
+  while( !itg.IsAtEnd() ) 
+  {
+    std::cout << itg.Get() << std::endl;
+    ++itg;
+  }
+
+
+  // All objects should be automatically destroyed at this point
+  return 0;
+
+}
+
+
+
+
