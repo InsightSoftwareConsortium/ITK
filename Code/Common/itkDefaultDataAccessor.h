6deb169b7bb2ed4d6641326b72d859545b5f56b4
Luis Ibanez <luis.ibanez@kitware.com>
2001-03-16 12:12:52 -0500

    Class to be used a default data accessor in itkImage.  It should optimize access to pixel data when no adaptors are used.

---
+++ Code/Common/itkDefaultDataAccessor.h
@@ -0,0 +1,65 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    $RCSfile: itkDefaultDataAccessor.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) 2000 National Library of Medicine
+  All rights reserved.
+
+  See COPYRIGHT.txt for copyright details.
+
+=========================================================================*/
+#ifndef __itkDefaultDataAccessor_h
+#define __itkDefaultDataAccessor_h
+
+
+namespace itk
+{
+
+/**
+ * \class DefaultDataAccessor
+ * \brief Give access to partial aspects a type
+ *
+ * DefaultDataAccessor is templated over an internal type and an
+ * external type representation. This class encapsulates a
+ * customized convertion between the internal and external
+ * type representations.
+ *
+ */
+
+template <class TType>
+class ITK_EXPORT DefaultDataAccessor  
+{
+public:
+
+ /** 
+   * External typedef. It defines the external aspect
+   * that this class will exhibit.
+   */
+  typedef TType ExternalType ;
+
+  /** 
+   * Internal typedef. It defines the internal real
+   * representation of data.
+   */
+  typedef TType InternalType ;
+
+
+  inline void Set(TType & output, const TType & input) const
+    {output = input;}
+
+  inline const TType & Get( const TType & input ) const
+    {return input;}
+
+};
+
+  
+} // end namespace itk
+  
+
+#endif
+
