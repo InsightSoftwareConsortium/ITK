ee805d96d3257e38eb5007b8ceeda010e5f726fc
Brad King <brad.king@kitware.com>
2001-03-16 16:43:54 -0500

    ERR: inline keyword should not be used unless the actual definition is in the .h and not the .txx

--- Code/Common/itkRGBPixel.h
+++ Code/Common/itkRGBPixel.h
@@ -51,50 +51,50 @@ public:
   /**
    * Constructors.
    */
-  inline RGBPixel();
-  inline RGBPixel(const RGBPixel &);
-  inline RGBPixel( ComponentType red, ComponentType green, ComponentType blue );
-  
+  RGBPixel();
+  RGBPixel(const RGBPixel &);
+  RGBPixel( ComponentType red, ComponentType green, ComponentType blue );
+ 
  
   /**
    * Assignment Operator
    */
-  inline const RGBPixel & operator=( const RGBPixel & );
+  const RGBPixel & operator=( const RGBPixel & );
 
   /**
    * Set the Red component
    */
-  inline void SetRed( ComponentType red );
+  void SetRed( ComponentType red );
 
   /**
    * Set the Green component
    */
-  inline void SetGreen( ComponentType green );
+  void SetGreen( ComponentType green );
 
   /**
    * Set the Blue component
    */
-  inline void SetBlue( ComponentType blue );
+  void SetBlue( ComponentType blue );
 
   /**
    * Set the three components
    */
-  inline void Set( ComponentType red, ComponentType green, ComponentType blue );
+  void Set( ComponentType red, ComponentType green, ComponentType blue );
 
   /**
    * Get the Red component
    */
-  inline const ComponentType & GetRed( void ) const;
-  
+  const ComponentType & GetRed( void ) const;
+ 
   /**
    * Get the Green component
    */
-  inline const ComponentType & GetGreen( void ) const;
+  const ComponentType & GetGreen( void ) const;
   
   /**
    * Get the Blue component
    */
-  inline const ComponentType & GetBlue( void ) const;
+  const ComponentType & GetBlue( void ) const;
 
 private:
   /**
