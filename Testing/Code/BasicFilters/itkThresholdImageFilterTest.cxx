43512a1955fdd9d8a7d91c4051622b9a01812f03
Jim Miller <millerjv@crd.ge.com>
2001-03-20 22:42:23 -0500

    ENH: tests reference counting

--- Testing/Code/BasicFilters/itkThresholdImageFilterTest.cxx
+++ Testing/Code/BasicFilters/itkThresholdImageFilterTest.cxx
@@ -14,6 +14,7 @@ See COPYRIGHT.txt for copyright details.
 
 =========================================================================*/
 #include <iostream>
+#include <string>
 #include "itkPhysicalImage.h"
 #include "itkScalar.h"
 #include "itkRandomImageSource.h"
@@ -48,12 +49,89 @@ int main()
   random->SetMin(0.0);
   random->SetMax(1000.0);
 
+  char buffer[8];
+  std::string os;
+
+  // Test #1, filter goes out of scope
+  itk::OutputWindow::GetInstance()->DisplayText( "Test #1: Filter goes out of scope -----------------" );
+  {
   itk::ThresholdImageFilter<FloatImage2DType>::Pointer threshold;
   threshold = itk::ThresholdImageFilter<FloatImage2DType>::New();
   threshold->SetInput(random->GetOutput());
   threshold->Update();
 
-  std::cout << "threshold reference count = " << threshold->GetReferenceCount() << std::endl;
+  os = "Filter: " + std::string(itoa((int)(void *)(threshold), buffer, 10));
+  itk::OutputWindow::GetInstance()->DisplayText( os.c_str() );
+  os = "Output #0: " + std::string(itoa((int)(void *)(threshold->GetOutput(0)),
+                                        buffer, 10));
+  itk::OutputWindow::GetInstance()->DisplayText( os.c_str() );
+  os = "Output #1: " + std::string(itoa((int)(void *)(threshold->GetOutput(1)),
+                                        buffer, 10));
+  itk::OutputWindow::GetInstance()->DisplayText( os.c_str() );
+  threshold->GetOutput(0)->DebugOn();
+  threshold->GetOutput(1)->DebugOn();
+  threshold->DebugOn();
+
+  itk::OutputWindow::GetInstance()->DisplayText( "Ending Test #1: filter goes out of scope" );
+  itk::OutputWindow::GetInstance()->DisplayText( "End of Test #1 -----------------------------------" );
+  }
+  
+  // Test #2, user keeps an extra handle to an output
+  itk::OutputWindow::GetInstance()->DisplayText( "Test #2: User keeps an extra hold on an output  -----------------" );
+  {
+  FloatImage2DType::Pointer keep;
+
+  itk::ThresholdImageFilter<FloatImage2DType>::Pointer threshold;
+  threshold = itk::ThresholdImageFilter<FloatImage2DType>::New();
+  threshold->SetInput(random->GetOutput());
+  threshold->Update();
+
+  os = "Filter: " + std::string(itoa((int)(void *)(threshold), buffer, 10));
+  itk::OutputWindow::GetInstance()->DisplayText( os.c_str() );
+  os = "Output #0: " + std::string(itoa((int)(void *)(threshold->GetOutput(0)),
+                                        buffer, 10));
+  itk::OutputWindow::GetInstance()->DisplayText( os.c_str() );
+  os = "Output #1: " + std::string(itoa((int)(void *)(threshold->GetOutput(1)),
+                                        buffer, 10));
+  itk::OutputWindow::GetInstance()->DisplayText( os.c_str() );
+  threshold->GetOutput(0)->DebugOn();
+  threshold->GetOutput(1)->DebugOn();
+  threshold->DebugOn();
+
+  keep = threshold->GetOutput(0);
+
+  itk::OutputWindow::GetInstance()->DisplayText( "End of Test #2: last handle on output 0 should go out of scope");
+  }
+  itk::OutputWindow::GetInstance()->DisplayText( "End of Test #2 -----------------------------------");
+
+  // Test #3, user disconnects a data object from the pipeline
+  itk::OutputWindow::GetInstance()->DisplayText( "Test #3: application disconnects a data object from the pipeline  -----------------" );
+  {
+  FloatImage2DType::Pointer keep;
+
+  itk::ThresholdImageFilter<FloatImage2DType>::Pointer threshold;
+  threshold = itk::ThresholdImageFilter<FloatImage2DType>::New();
+  threshold->SetInput(random->GetOutput());
+  threshold->Update();
+
+  os = "Filter: " + std::string(itoa((int)(void *)(threshold), buffer, 10));
+  itk::OutputWindow::GetInstance()->DisplayText( os.c_str() );
+  os = "Output #0: " + std::string(itoa((int)(void *)(threshold->GetOutput(0)),
+                                        buffer, 10));
+  itk::OutputWindow::GetInstance()->DisplayText( os.c_str() );
+  os = "Output #1: " + std::string(itoa((int)(void *)(threshold->GetOutput(1)),
+                                        buffer, 10));
+  itk::OutputWindow::GetInstance()->DisplayText( os.c_str() );
+  threshold->GetOutput(0)->DebugOn();
+  threshold->GetOutput(1)->DebugOn();
+  threshold->DebugOn();
+
+  keep = threshold->GetOutput(0);
+  keep->DisconnectPipeline();
+
+  itk::OutputWindow::GetInstance()->DisplayText( "End of Test #3: last handle on output 0 should go out of scope");
+  }
+  itk::OutputWindow::GetInstance()->DisplayText( "End of Test #3 -----------------------------------");
   
   return EXIT_SUCCESS;
 }
