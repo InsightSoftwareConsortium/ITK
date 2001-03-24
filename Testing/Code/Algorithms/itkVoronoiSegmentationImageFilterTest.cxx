f58bbfc41e1adf09578577c9bc53349174a40893
Yinpeng Jin <yj76@columbia.edu>
2001-03-24 17:03:23 -0500

    ENH:fix the take a prior function

--- Testing/Code/Algorithms/itkVoronoiSegmentationImageFilterTest.cxx
+++ Testing/Code/Algorithms/itkVoronoiSegmentationImageFilterTest.cxx
@@ -25,87 +25,89 @@ typedef itk::Image<unsigned short,2> UShortImage;
 typedef itk::VoronoiSegmentationImageFilter<UShortImage, UShortImage> VorSeg;
 
 int main(void){
-  VorSeg::Pointer testVorseg(VorSeg::New());
-  UShortImage::Pointer inputIMG = UShortImage::New();
-  UShortImage::SizeType size={{WIDTH,HEIGHT}};
-  UShortImage::IndexType index=UShortImage::IndexType::ZeroIndex;
-  UShortImage::RegionType region;
+	VorSeg::Pointer testVorseg(VorSeg::New());
+	UShortImage::Pointer inputIMG = UShortImage::New();
+	UShortImage::SizeType size={{WIDTH,HEIGHT}};
+	UShortImage::IndexType index=UShortImage::IndexType::ZeroIndex;
+	UShortImage::RegionType region;
 
-  region.SetSize(size);
-  region.SetIndex(index);
+	region.SetSize(size);
+	region.SetIndex(index);
 
-  inputIMG->SetLargestPossibleRegion( region );
-  inputIMG->SetBufferedRegion( region );
-  inputIMG->SetRequestedRegion( region );
-  inputIMG->Allocate();
+	inputIMG->SetLargestPossibleRegion( region );
+	inputIMG->SetBufferedRegion( region );
+	inputIMG->SetRequestedRegion( region );
+	inputIMG->Allocate();
 
 
-  itk::SimpleImageRegionIterator <UShortImage> it(inputIMG, region);
+	itk::SimpleImageRegionIterator <UShortImage> it(inputIMG, region);
 
-  // background: random field with mean: 500, std: 50
-  it.Begin();
-  while( !it.IsAtEnd()) {    
-    it.Set((unsigned short)(vnl_sample_uniform(450,550)) );
-    ++it;
+	// background: random field with mean: 500, std: 50
+	it.Begin();
+	while( !it.IsAtEnd()) {    
+		it.Set((unsigned short)(vnl_sample_uniform(450,550)) );
+		++it;
 	}
 
-  // object (2): random field with mean: 520, std: 20;
-  int i;
-  int j;
-  for(i = 30; i < 94; i++){
-    index[0] = i;
-    for (j = 30; j< 94; j++){
-      index[1] = j;
-      inputIMG->SetPixel(index, (unsigned short)(vnl_sample_uniform(500,540)) );
+	// object (2): random field with mean: 520, std: 20;
+	unsigned int i;
+	unsigned int j;
+	for(i = 30; i < 94; i++){
+		index[0] = i;
+		for (j = 30; j< 94; j++){
+			index[1] = j;
+			inputIMG->SetPixel(index, (unsigned short)(vnl_sample_uniform(500,540)) );
 		}
 	}
 
-  for(i = 150; i < 214; i++){
-    index[0] = i;
-    for (j = 150; j< 214; j++){
-      index[1] = j;
-      inputIMG->SetPixel(index, (unsigned short)(vnl_sample_uniform(500,540)) );
+	for(i = 150; i < 214; i++){
+		index[0] = i;
+		for (j = 150; j< 214; j++){
+			index[1] = j;
+			inputIMG->SetPixel(index, (unsigned short)(vnl_sample_uniform(500,540)) );
 		}
 	}
 
-  testVorseg->SetInput(inputIMG);
-  testVorseg->SetMean(520);
-  testVorseg->SetVar(20);
-  testVorseg->SetMeanTolerance(10);
-  testVorseg->SetVarTolerance(20);
-  testVorseg->SetNumberOfSeeds(200);
-  testVorseg->SetSteps(5);
-  testVorseg->ExcuteSegment();
 
-  itk::SimpleImageRegionIterator <UShortImage> ot(testVorseg->GetOutput(), region);
+	testVorseg->SetInput(inputIMG);
+	testVorseg->InitializeSegment();
+	testVorseg->SetMean(520);
+	testVorseg->SetVar(20);
+	testVorseg->SetMeanTolerance(10);
+	testVorseg->SetVarTolerance(20);
+	testVorseg->SetNumberOfSeeds(200);
+	testVorseg->SetSteps(5);
 
-  unsigned short TestImg[65536];
-  ot.Begin();
-  int k=0;
-  while( !ot.IsAtEnd()){
-    TestImg[k]=ot.Get();
+	testVorseg->ExcuteSegment();
+
+	itk::SimpleImageRegionIterator <UShortImage> ot(testVorseg->GetOutput(), region);
+
+	unsigned short TestImg[65536];
+	ot.Begin();
+	int k=0;
+	while( !ot.IsAtEnd()){
+		TestImg[k]=ot.Get();
     k++;
-    ++ot;
+		++ot;
 	}
 
-  /* Test Ok on local machine.
-  FILE *imgfile = fopen("output.raw","wb");
-  fwrite(TestImg,2,65536,imgfile);
-  fclose(imgfile);
+	//* Test Ok on local machine.
+	FILE *imgfile = fopen("output.raw","wb");
+	fwrite(TestImg,2,65536,imgfile);
+	fclose(imgfile);
  
   imgfile = fopen("input.raw","wb");
-  it.Begin();
-  k=0;
-  while( !it.IsAtEnd()){
-    TestImg[k]=it.Get();
+	it.Begin();
+	k=0;
+	while( !it.IsAtEnd()){
+		TestImg[k]=it.Get();
     k++;
-    ++it;
-  }
-  fwrite(TestImg,2,65536,imgfile);
-  fclose(imgfile);
-  */
+		++it;
+	}
+	fwrite(TestImg,2,65536,imgfile);
+	fclose(imgfile);
+
 
   return 0;
 }
-
 
