4e174fe95ba22c9b1307f466c4512195912b180f
Luis Ibanez <luis.ibanez@kitware.com>
2001-03-28 22:30:43 -0500

    GradientDescent Optimizer with regular step size.

---
+++ Code/Numerics/itkRegularStepGradientDescentOptimizer.h
@@ -0,0 +1,206 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkRegularStepGradientDescentOptimizer.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) 2000 National Library of Medicine
+  All rights reserved.
+
+  See COPYRIGHT.txt for copyright details.
+
+=========================================================================*/
+#ifndef __itkRegularStepGradientDescentOptimizer_h
+#define __itkRegularStepGradientDescentOptimizer_h
+
+#include "itkSingleValuedNonLinearOptimizer.h"
+
+namespace itk
+{
+  
+/** \class RegularStepGradientDescentOptimizer
+ * \brief Implement a gradient descent optimizer
+ *
+ */
+
+  
+template <class TCostFunction>
+class ITK_EXPORT RegularStepGradientDescentOptimizer : 
+        public SingleValuedNonLinearOptimizer< TCostFunction >
+
+{
+public:
+  /**
+   * Standard "Self" typedef.
+   */
+  typedef RegularStepGradientDescentOptimizer  Self;
+
+  /**
+   * Standard "Superclass" typedef.
+   */
+  typedef SingleValuedNonLinearOptimizer<TCostFunction> Superclass;
+
+  /** 
+   * Smart pointer typedef support 
+   */
+  typedef SmartPointer<Self>   Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+
+
+  /**
+   * Cost Function  typedef.
+   */
+  typedef          TCostFunction                CostFunctionType;
+  typedef typename CostFunctionType::Pointer    CostFunctionPointer;
+
+  /**
+   * Dimension of the Search Space
+   */
+  enum { SpaceDimension = TCostFunction::SpaceDimension };
+ 
+
+  /**
+   *  Parameters type.
+   *  it defines a position in the optimization search space
+   */
+  typedef typename TCostFunction::ParametersType ParametersType;
+
+
+  /**
+   *  Measure type.
+   *  it defines a type used to return the cost function value 
+   */
+  typedef typename TCostFunction::MeasureType MeasureType;
+
+
+  /**
+   *  Derivative type.
+   *  it defines a type used to return the cost function derivative 
+   */
+  typedef typename TCostFunction::DerivativeType DerivativeType;
+
+
+
+ /** 
+   * Run-time type information (and related methods).
+   */
+  itkTypeMacro( RegularStepGradientDescentOptimizer, 
+      SingleValuedNonLinearOptimizer );
+
+
+  /**
+   * Method for creation through the object factory.
+   */
+  itkNewMacro(Self);
+  
+
+
+  /**
+   * Codes of stopping conditions
+   */
+  typedef enum {
+    RegularStepGradientMagnitudeTolerance,
+    StepTooSmall,
+    ImageNotAvailable,
+    SamplesNotAvailable,
+    MaximumNumberOfIterations
+  } StopConditionType;
+
+  /**
+   * Select to Minimize the cost function
+   */
+  void    SetMinimize(void) 
+              { m_Maximize = false; }
+
+  /**
+   * Select to Maximize the cost function
+   */
+  void    SetMaximize(void)
+              { m_Maximize = true; }
+
+  /**
+   * Advance one step following the gradient direction
+   */
+  void    AdvanceOneStep( void );
+
+  /**
+   * Test a precondition
+   */
+  bool    Precondition( void );
+
+  /**
+   * Start Optimization
+   */
+  void    StartOptimization( void );
+
+  /**
+   * Resume previously stopped optimization with current parameters
+   * \sa StopOptimization
+   */
+  void    ResumeOptimization( void );
+
+  /**
+   * Stop optimization
+   * \sa ResumeOptimization
+   */
+  void    StopOptimization( void );
+
+  itkSetMacro( MaximumStepLength, double );
+  itkSetMacro( MinimumStepLength, double );
+  itkSetMacro( MaximumNumberOfIterations, unsigned long );
+  itkSetMacro( RegularStepGradientMagnitudeTolerance, double );
+
+  itkGetConstMacro( CurrentStepLength, double);
+  itkGetConstMacro( MaximumStepLength, double );
+  itkGetConstMacro( MinimumStepLength, double );
+  itkGetConstMacro( MaximumNumberOfIterations, unsigned long );
+  itkGetConstMacro( RegularStepGradientMagnitudeTolerance, double );
+  itkGetConstMacro( CurrentNumberOfIterations, unsigned int );
+
+  void SetScale( const ParametersType & scale )
+          { m_Scale = scale; this->Modified(); }
+
+  itkSetObjectMacro( CostFunction, CostFunctionType );
+
+protected:
+
+  RegularStepGradientDescentOptimizer();
+  virtual ~RegularStepGradientDescentOptimizer() {};
+  RegularStepGradientDescentOptimizer(const Self&) {}
+  void operator=(const Self&) {}
+
+private:
+
+  ParametersType                m_RegularStepGradient; 
+  ParametersType                m_PreviousRegularStepGradient; 
+  ParametersType                m_Scale;
+
+  bool                          m_Stop;
+  bool                          m_Maximize;
+  double                        m_Value;
+  double                        m_RegularStepGradientMagnitudeTolerance;
+  double                        m_MaximumStepLength;
+  double                        m_MinimumStepLength;
+  double                        m_CurrentStepLength;
+  StopConditionType             m_StopCondition;
+  unsigned long                 m_MaximumNumberOfIterations;
+  unsigned long                 m_CurrentNumberOfIterations;
+
+  CostFunctionPointer           m_CostFunction;
+
+
+};
+
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkRegularStepGradientDescentOptimizer.txx"
+#endif
+
+#endif
+
+
+
---
+++ Code/Numerics/itkRegularStepGradientDescentOptimizer.txx
@@ -0,0 +1,192 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkRegularStepGradientDescentOptimizer.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) 2000 National Library of Medicine
+  All rights reserved.
+
+  See COPYRIGHT.txt for copyright details.
+
+=========================================================================*/
+#ifndef _itkRegularStepGradientDescentOptimizer_txx
+#define _itkRegularStepGradientDescentOptimizer_txx
+
+#include "itkRegularStepGradientDescentOptimizer.h"
+
+namespace itk
+{
+
+/**
+ * Constructor
+ */
+template <class TCostFunction>
+RegularStepGradientDescentOptimizer<TCostFunction>
+::RegularStepGradientDescentOptimizer()
+{
+}
+
+
+
+/**
+ * Start the optimization
+ */
+template <class TCostFunction>
+void
+RegularStepGradientDescentOptimizer<TCostFunction>
+::StartOptimization( void )
+{
+
+  m_CurrentStepLength         = m_MaximumStepLength;
+  m_CurrentNumberOfIterations   = 0;
+
+  this->SetCurrentPosition( GetInitialPosition() );
+  this->ResumeOptimization();
+
+}
+
+
+
+
+
+/**
+ * Resume the optimization
+ */
+template <class TCostFunction>
+void
+RegularStepGradientDescentOptimizer<TCostFunction>
+::ResumeOptimization( void )
+{
+  
+  m_Stop = false;
+
+  while( !m_Stop ) 
+  {
+    m_Value = m_CostFunction->GetValue( GetCurrentPosition() );
+
+    if( m_Stop )
+    {
+      break;
+    }
+
+    m_PreviousRegularStepGradient = m_RegularStepGradient;
+  
+    m_RegularStepGradient = m_CostFunction->GetDerivative( GetCurrentPosition() );
+
+    if( m_Stop )
+    {
+      break;
+    }
+
+    AdvanceOneStep();
+
+    m_CurrentNumberOfIterations++;
+
+    if( m_CurrentNumberOfIterations == m_MaximumNumberOfIterations )
+    {
+       m_StopCondition = MaximumNumberOfIterations;
+       StopOptimization();
+       break;
+    }
+    
+  }
+    
+
+}
+
+
+
+
+
+/**
+ * Stop optimization
+ */
+template <class TCostFunction>
+void
+RegularStepGradientDescentOptimizer<TCostFunction>
+::StopOptimization( void )
+{
+  m_Stop = true;
+}
+
+
+/**
+ * Advance one Step following the gradient direction
+ */
+template <class TCostFunction>
+void
+RegularStepGradientDescentOptimizer<TCostFunction>
+::AdvanceOneStep( void )
+{ 
+
+  double magnitudeSquare = 0;
+  for(unsigned int dim=0; dim<SpaceDimension; dim++)
+  {
+    const double weighted = m_RegularStepGradient[dim] * m_Scale[dim];
+    magnitudeSquare += weighted * weighted;
+  }
+    
+  const double gradientMagnitude = sqrt( magnitudeSquare );
+
+  if( gradientMagnitude < m_RegularStepGradientMagnitudeTolerance ) 
+  {
+    m_StopCondition = RegularStepGradientMagnitudeTolerance;
+    StopOptimization();
+    return;
+  }
+    
+  double scalarProduct = 0;
+
+  for(unsigned int i=0; i<SpaceDimension; i++)
+  {
+    const double weight1 = m_RegularStepGradient[i]         * m_Scale[i]; 
+    const double weight2 = m_PreviousRegularStepGradient[i] * m_Scale[i]; 
+    scalarProduct += weight1 * weight2;
+  }
+   
+  // If there is a direction change 
+  if( scalarProduct < 0 ) 
+  {
+    m_CurrentStepLength /= 2.0;
+  }
+
+  if( m_CurrentStepLength < m_MinimumStepLength )
+  {
+    m_StopCondition = StepTooSmall;
+    StopOptimization();
+    return;
+  }
+
+  double direction = 1.0;
+  if( this->m_Maximize ) 
+  {
+    direction = 1.0;
+  }
+  else 
+  {
+    direction = -1.0;
+  }
+
+  ParametersType newPosition;
+  const ParametersType & currentPosition = GetCurrentPosition();
+  const double factor = 
+    (direction * m_CurrentStepLength / gradientMagnitude);
+
+  for(unsigned int j=0; j<SpaceDimension; j++)
+  {
+    newPosition[j] = currentPosition[j] + m_RegularStepGradient[j] * factor;
+  }
+
+  SetCurrentPosition( newPosition );
+
+}
+
+
+
+} // end namespace itk
+
+#endif
