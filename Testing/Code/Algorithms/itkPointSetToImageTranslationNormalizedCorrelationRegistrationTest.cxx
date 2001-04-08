52537cb4b588a93bcd1e8582ad775e6cf98fb663
Luis Ibanez <luis.ibanez@kitware.com>
2001-04-08 16:00:26 -0400

    Test for PointSet to Image registration

---
+++ Testing/Code/Algorithms/itkPointSetToImageTranslationNormalizedCorrelationRegistrationTest.cxx
@@ -0,0 +1,141 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit (ITK)
+  Module:    itkPointSetToImageTranslationNormalizedCorrelationRegistrationTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+Copyright (c) 2000 National Library of Medicine
+All rights reserved.
+
+See COPYRIGHT.txt for copyright details.
+
+=========================================================================*/
+#include "itkPhysicalImage.h"
+#include "itkSimpleImageRegionIterator.h"
+#include "itkPointSetToImageTranslationNormalizedCorrelationRegistration.h"
+
+/** 
+ *  This test uses two 2D-Gaussians (standard deviation RegionSize/2)
+ *  One is shifted by 10 pixels from the other.
+ *  therefore the solution of the registration is |-5 0|
+ * 
+ *  One of the images is subsampled in order to obtain a PoinSet.
+ *  The PointSet is considered the Target and the other image is
+ *  considered the Reference.
+ */ 
+
+int main()
+{
+
+  // Number of pixels to take from the target image
+  unsigned long  numberOfSamples = 10000L;
+
+  /*Allocate Images*/
+  typedef itk::PhysicalImage<unsigned char,2>           ReferenceType;
+
+  typedef itk::DefaultStaticMeshTraits<unsigned char,   // pixel type
+                                       2,               // point dimension
+                                       3,               // max topological dim
+                                       double,          // coordinates type
+                                       double           // interpolation weight
+                                         >  DefaultPointSetTraits;
+
+  typedef itk::PointSet<unsigned char,DefaultPointSetTraits>    TargetType;
+
+  typedef itk::PointSetToImageTranslationNormalizedCorrelationRegistration<
+                                                                ReferenceType,
+                                                                TargetType> RegistrationType;
+
+  ReferenceType::SizeType size = {{100,100}};
+  ReferenceType::IndexType index = {{0,0}};
+  ReferenceType::RegionType region;
+  region.SetSize( size );
+  region.SetIndex( index );
+
+  ReferenceType::Pointer imgReference = ReferenceType::New();
+  imgReference->SetLargestPossibleRegion( region );
+  imgReference->SetBufferedRegion( region );
+  imgReference->SetRequestedRegion( region );
+  imgReference->Allocate();
+
+  ReferenceType::Pointer imgTarget = ReferenceType::New(); // type=Reference but this 
+                                                           // is the target image to subsample
+  imgTarget->SetLargestPossibleRegion( region );
+  imgTarget->SetBufferedRegion( region );
+  imgTarget->SetRequestedRegion( region );
+  imgTarget->Allocate();
+
+  /* Fill images with a 2D gaussian*/
+  typedef  itk::SimpleImageRegionIterator<ReferenceType> ReferenceIteratorType;
+
+
+  itk::Point<double,2> center;
+  center[0] = (double)region.GetSize()[0]/2.0;
+  center[1] = (double)region.GetSize()[1]/2.0;
+
+  const double s = (double)region.GetSize()[0]/2.0;
+
+  itk::Point<double,2>  p;
+  itk::Vector<double,2> d;
+
+  /* Set the displacement */
+  itk::Vector<double,2> displacement;
+  displacement[0] = 7;
+  displacement[1] =  3;
+
+  ReferenceIteratorType ri(imgReference,region);
+  ReferenceIteratorType ti(imgTarget,region);
+  ri.Begin();
+  while(!ri.IsAtEnd())
+  {
+    p[0] = ri.GetIndex()[0];
+    p[1] = ri.GetIndex()[1];
+    d = p-center;
+    d += displacement;
+    const double x = d[0];
+    const double y = d[1];
+    ri.Set( 200.0 * exp( - ( x*x + y*y )/(s*s) ) );
+    ++ri;
+  }
+
+
+  ti.Begin();
+  while(!ti.IsAtEnd())
+  {
+    p[0] = ti.GetIndex()[0];
+    p[1] = ti.GetIndex()[1];
+    d = p-center;
+    const double x = d[0];
+    const double y = d[1];
+    ti.Set( 200.0 * exp( - ( x*x + y*y )/(s*s) ) );
+    ++ti;
+  }
+
+  
+  // Subsample the target image to produce a point set
+  TargetType::Pointer pointSetTarget = TargetType::New();
+
+  pointSetTarget->GetPoints()->Reserve( numberOfSamples );
+  pointSetTarget->GetPointData()->Reserve( numberOfSamples );
+
+  RegistrationType::Pointer registrationMethod = RegistrationType::New();
+
+  registrationMethod->SetReference(imgReference);
+  registrationMethod->SetTarget(pointSetTarget);
+
+  registrationMethod->StartRegistration();
+
+
+  std::cout << "The correct answer should be : " << std::endl;
+  std::cout << -displacement << std::endl;
+  
+
+  return EXIT_SUCCESS;
+
+}
+
+
+
