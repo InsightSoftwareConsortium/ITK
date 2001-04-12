f8973134f9b40d855845b6c14f700f3835f19644
Lydia Ng <lydiang@gmail.com>
2001-04-12 13:38:01 -0400

    FIX: increase number of iterations to allow SunOS and IRIX precision to meet the convergence criteria

--- Testing/Code/Algorithms/itkImageToImageAffineMutualInformationGradientDescentRegistrationTest.cxx
+++ Testing/Code/Algorithms/itkImageToImageAffineMutualInformationGradientDescentRegistrationTest.cxx
@@ -123,7 +123,7 @@ int main()
   registrationMethod->SetReferenceTransformationCenter( transCenter );
 
   // set optimization related parameters
-  registrationMethod->SetNumberOfIterations( 250 );
+  registrationMethod->SetNumberOfIterations( 500 );
   registrationMethod->SetLearningRate( 0.2 );
 
   //
