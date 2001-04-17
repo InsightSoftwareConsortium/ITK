57dc50b101c529507c332a2698b29371ffc7e0c7
Luis Ibanez <luis.ibanez@kitware.com>
2001-04-17 10:45:26 -0400

    ENH: Quantitative test added at the end

--- Testing/Code/Algorithms/itkImageToImageAffineMeanSquaresGradientDescentRegistrationTest.cxx
+++ Testing/Code/Algorithms/itkImageToImageAffineMeanSquaresGradientDescentRegistrationTest.cxx
@@ -117,12 +117,37 @@ int main()
 
   registrationMethod->StartRegistration();
 
+  // get the results
+  RegistrationType::ParametersType solution = 
+    registrationMethod->GetParameters();
 
-  std::cout << "The correct answer should be : " << std::endl;
-  std::cout << " 1.0    0.0    0.0    1.0  ";
-  std::cout << -displacement << std::endl;
-  
+  std::cout << "Solution is: " << solution << std::endl;
 
+  //
+  // check results to see if it is within range
+  //
+  bool pass = true;
+  double trueParameters[6] = { 1, 0, 0, 1, -7, -3 };
+  for( unsigned int j = 0; j < 4; j++ )
+    {
+    if( vnl_math_abs( solution[j] - trueParameters[j] ) > 0.02 )
+      pass = false;
+    }
+  for( unsigned int j = 4; j < 6; j++ )
+    {
+    if( vnl_math_abs( solution[j] - trueParameters[j] ) > 1.0 )
+      pass = false;
+    }
+
+  if( !pass )
+    {
+    std::cout << "Test failed." << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  std::cout << "Test passed." << std::endl;
   return EXIT_SUCCESS;
+
+
 
 }
