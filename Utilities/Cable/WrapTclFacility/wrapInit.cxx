cbe5d372bc77f839194d542c9c716ee9d47c5229
Brad King <brad.king@kitware.com>
2001-04-18 19:23:19 -0400

    ENH: Added one-time-only initialization per interpreter so that wrapper libraries can automatically initialize the facility.

--- Utilities/Cable/WrapTclFacility/wrapInit.cxx
+++ Utilities/Cable/WrapTclFacility/wrapInit.cxx
@@ -16,10 +16,59 @@
 #include "wrapUtils.h"
 #include "wrapTypeInfo.h"
 
+#include <set>
+
 namespace _wrap_
 {
 extern Tcl_ObjType TclPointerType;
 extern Tcl_ObjType TclReferenceType;
+
+
+/**
+ * Initialization class to setup the facility for a Tcl interpreter.
+ */
+class TclWrapperFacility
+{
+public:
+  static int InitializeInterpreter(Tcl_Interp*);
+private:
+  static std::set<Tcl_Interp*> initializedInterpreters;
+};
+
+
+/**
+ * The set of interpreters that have been initialized.
+ */
+std::set<Tcl_Interp*> TclWrapperFacility::initializedInterpreters;
+
+
+/**
+ * Initialization function for a Tcl interpreter.   This will only execute
+ * exactly once for a given interpreter.
+ *
+ * This just registers the Pointer and Reference Tcl object types needed
+ * by the wrappers with the interpreter.
+ */
+int TclWrapperFacility::InitializeInterpreter(Tcl_Interp* interp)
+{
+  // Initialize predefined type information.  This only executes once,
+  // no matter how many interpreters are using it.
+  _wrap_::TypeInfo::Initialize();
+
+  // Only register our object types if the interpreter has not been
+  // initialized.
+  if(initializedInterpreters.find(interp) != initializedInterpreters.end())
+    {
+      Tcl_RegisterObjType(&_wrap_::TclPointerType);
+      Tcl_RegisterObjType(&_wrap_::TclReferenceType);
+    }
+  
+  initializedInterpreters.insert(interp);
+
+  return TCL_OK;
+}
+
+
 } // namespace _wrap_
 
 
@@ -27,18 +76,11 @@ extern Tcl_ObjType TclReferenceType;
 extern "C" { _wrap_EXPORT int Wrap_Init(Tcl_Interp*); }
 
 /**
- * When the WrapTclFacilitator library is loaded by a tcl interpreter,
- * this is called to initialize it.
- *
- * This just registers the Pointer and Reference Tcl object types needed
- * by the wrappers with the interpreter.
+ * When the WrapTclFacilitator library is loaded by a Tcl interpreter,
+ * this is called to initialize it.  This just passes the call to
+ * TclWrapperFacility::InitializeInterpreter.
  */
 _wrap_EXPORT int Wrap_Init(Tcl_Interp* interp)
 {
-  Tcl_RegisterObjType(&_wrap_::TclPointerType);
-  Tcl_RegisterObjType(&_wrap_::TclReferenceType);
-  
-  _wrap_::TypeInfo::Initialize();
-  
-  return TCL_OK;
+  return _wrap_::TclWrapperFacility::InitializeInterpreter(interp);
 }
