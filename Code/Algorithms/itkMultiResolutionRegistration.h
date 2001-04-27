653c41a1c35f30a942dae6d725c36c28a89dff02
Lydia Ng <lydiang@gmail.com>
2001-04-27 19:00:08 -0400

    ENH: base class for multi-resolution registration methods

---
+++ Code/Algorithms/itkMultiResolutionRegistration.h
@@ -0,0 +1,258 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkMultiResolutionRegistration.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+Copyright (c) 2001 Insight Consortium
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * The name of the Insight Consortium, nor the names of any consortium members,
+   nor of any contributors, may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+  * Modified source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+#ifndef __itkMultiResolutionRegistration_h
+#define __itkMultiResolutionRegistration_h
+
+#include "itkProcessObject.h"
+#include "vnl/vnl_matrix.h"
+
+namespace itk
+{
+
+/**
+ * \class MultiResolutionRegistration
+ * \brief Framework for performing multiresolution registration.
+ *
+ * MultiResolutionRegistration provides a generic framework to
+ * peform multi-resolution registration. This class over a set
+ * of traits.
+ * 
+ * The traits must consist of:
+ *
+ * a) the target image type
+ * b) the reference image type
+ * c) a registration method which register a reference image
+ *    onto a target image. It must have method StartRegistration()
+ *    which is used to evoke the registration at each resolution level.
+ *    For example, see class RegistrationMethod.
+ * d) the target image pyramid type. For example, see class
+ *    MultiResolutionImagePyramid.
+ * e) the reference image pyramid type
+ *
+ */
+template <class TTraits>
+class ITK_EXPORT MultiResolutionRegistration : public ProcessObject
+{
+public:
+  /**
+   * Standard "Self" typedef
+   */
+  typedef MultiResolutionRegistration  Self;
+
+  /**
+   * Standard "Superclass" typedef
+   */
+  typedef ProcessObject  Superclass;
+
+  /**
+   * SmartPointer typedef support
+   */
+  typedef SmartPointer<Self>  Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+
+  /**
+   * Method for creation through the object factory
+   */
+  itkNewMacro(Self);
+
+  /**
+   * Run-time type information (and related methods)
+   */
+  itkTypeMacro(MultiResolutionRegistration, ProcessObject);
+
+  /**
+   * RegistrationType typedef support
+   */
+  typedef typename TTraits::RegistrationType RegistrationType;
+
+  /**
+   * RegistrationPointer typedef support
+   */
+  typedef typename RegistrationType::Pointer  RegistrationPointer;
+
+  /**
+   * TargetType typedef support
+   */
+  typedef typename TTraits::TargetType  TargetType;
+
+  /**
+   * TargetPointer typedef support
+   */
+  typedef typename TargetType::Pointer TargetPointer;
+
+  /**
+   * ReferenceType typedef support
+   */
+  typedef typename TTraits::ReferenceType  ReferenceType;
+
+  /**
+   * ReferencePointer typedef support
+   */
+  typedef typename ReferenceType::Pointer  ReferencePointer;
+
+  /**
+   * TargetImageDimension enumeration
+   */
+  enum{ TargetImageDimension = TargetType::ImageDimension };
+
+  /**
+   * ReferenceImageDimension enumeration
+   */
+  enum{ ReferenceImageDimension = ReferenceType::ImageDimension };
+
+  /**
+   * TargetPyramid typedef support
+   */
+  typedef typename TTraits::TargetPyramidType TargetPyramidType;
+
+  /**
+   * TargetPyramidPointer typedef support
+   */
+  typedef typename TargetPyramidType::Pointer TargetPyramidPointer;
+
+  /**
+   * ReferencePyramid typedef support
+   */
+  typedef typename TTraits::ReferencePyramidType ReferencePyramidType;
+
+  /**
+   * ReferencePyramidPointer typedef support
+   */
+  typedef typename ReferencePyramidType::Pointer ReferencePyramidPointer;
+
+  /**
+   * Set the number of multi-resolution computation levels.
+   * This sets the levels for both the target and reference
+   * pyramids.
+   * If the argument is less than 1, it is clamped to the
+   * value of 1.
+   */
+  virtual void SetNumberOfLevels(unsigned int num);
+
+  /**
+   * Get the number of multi-resolution computation levels
+   */
+  itkGetConstMacro(NumberOfLevels, unsigned int);
+
+  /**
+   * Set the target
+   */
+  void SetTarget( TargetType * target );
+
+  /**
+   * Get the target
+   */
+  TargetPointer GetTarget( void );
+
+  /**
+   * Set the reference
+   */
+  void SetReference( ReferenceType * reference );
+
+  /**
+   * Get the reference
+   */
+  ReferencePointer GetReference( void );
+
+  /**
+   * Get the target pyramid
+   */
+  TargetPyramidPointer  GetTargetPyramid(void)
+  { return m_TargetPyramid; }
+
+  /**
+   * Get the reference pyramid
+   */
+  ReferencePyramidPointer GetReferencePyramid(void)
+  { return m_ReferencePyramid; }
+
+  /**
+   * Get the internal registrator
+   */
+  RegistrationPointer GetInternalRegistrationMethod(void)
+  { return m_InternalRegistrationMethod; }
+
+  /**
+   * Start the registration
+   */
+  void StartRegistration(void);
+
+protected:
+  MultiResolutionRegistration();
+  ~MultiResolutionRegistration() {};
+  MultiResolutionRegistration(const Self&) {};
+  void operator=(const Self&) {};
+  void PrintSelf(std::ostream&os, Indent indent);
+
+  /**
+   * Peform the necessary initialization before
+   * running the internal registration algorithm at this level.
+   * Subclasses should override this method.
+   */
+  virtual void OneLevelInitialize(unsigned int level);
+ 
+  /**
+   * Run the internal registration algorithm at the
+   * specified level. Subclasses may override this method.
+   */
+  virtual void OneLevelRegistration(unsigned int level);
+
+private:
+
+  unsigned int               m_NumberOfLevels;
+  unsigned int               m_CurrentLevel;
+  RegistrationPointer        m_InternalRegistrationMethod;
+  TargetPyramidPointer       m_TargetPyramid;
+  ReferencePyramidPointer    m_ReferencePyramid;
+
+};
+
+
+} // namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkMultiResolutionRegistration.txx"
+#endif
+
+#endif
+
+
---
+++ Code/Algorithms/itkMultiResolutionRegistration.txx
@@ -0,0 +1,225 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkMultiResolutionRegistration.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+Copyright (c) 2001 Insight Consortium
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * The name of the Insight Consortium, nor the names of any consortium members,
+   nor of any contributors, may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+  * Modified source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+#ifndef _itkMultiResolutionRegistration_txx
+#define _itkMultiResolutionRegistration_txx
+
+#include "itkMultiResolutionRegistration.h"
+
+namespace itk
+{
+
+
+/**
+ * Constructor
+ */
+template <class TTraits>
+MultiResolutionRegistration<TTraits>
+::MultiResolutionRegistration()
+{
+  m_TargetPyramid = TargetPyramidType::New();
+  m_ReferencePyramid = ReferencePyramidType::New();
+  m_InternalRegistrationMethod = RegistrationType::New();
+
+  this->SetNumberOfLevels( 2 );
+  m_CurrentLevel = 0;
+
+}
+
+
+/**
+ * Set the number of computation levels
+ */
+template <class TRegistrationMethod>
+void
+MultiResolutionRegistration<TRegistrationMethod>
+::SetNumberOfLevels(
+unsigned int num )
+{
+  // clamp value to be at least one
+  m_NumberOfLevels = num;
+  if( m_NumberOfLevels == 0 ) m_NumberOfLevels = 1;
+
+  m_TargetPyramid->SetNumberOfLevels( m_NumberOfLevels );
+  m_ReferencePyramid->SetNumberOfLevels( m_NumberOfLevels );
+
+  this->Modified();
+}
+
+
+/**
+ * Set the target
+ */
+template <class TRegistrationMethod>
+void
+MultiResolutionRegistration<TRegistrationMethod>
+::SetTarget(
+TargetType * ptr )
+{
+  if( m_TargetPyramid->GetInput() != ptr )
+    {
+    m_TargetPyramid->SetInput( ptr );
+    this->Modified();
+    }
+}
+
+
+/**
+ * Get the target
+ */
+template <class TRegistrationMethod>
+MultiResolutionRegistration<TRegistrationMethod>::TargetPointer
+MultiResolutionRegistration<TRegistrationMethod>
+::GetTarget()
+{
+  return m_TargetPyramid->GetInput();
+}
+
+
+/**
+ * Set the reference
+ */
+template <class TRegistrationMethod>
+void
+MultiResolutionRegistration<TRegistrationMethod>
+::SetReference(
+ReferenceType * ptr )
+{
+  if( m_ReferencePyramid->GetInput() != ptr )
+    {
+    m_ReferencePyramid->SetInput( ptr );
+    this->Modified();
+    }
+}
+
+
+
+/**
+ * Get the target
+ */
+template <class TRegistrationMethod>
+MultiResolutionRegistration<TRegistrationMethod>::ReferencePointer
+MultiResolutionRegistration<TRegistrationMethod>
+::GetReference()
+{
+  return m_ReferencePyramid->GetInput();
+}
+
+/**
+ * Start the registration
+ */
+template <class TRegistrationMethod>
+void
+MultiResolutionRegistration<TRegistrationMethod>
+::StartRegistration()
+{
+  for( m_CurrentLevel = 0; m_CurrentLevel < m_NumberOfLevels; 
+    m_CurrentLevel++ )
+    {
+    
+    this->OneLevelInitialize( m_CurrentLevel );
+    this->OneLevelRegistration( m_CurrentLevel );
+        
+    }
+
+}
+
+
+/**
+ * OneLevelInitialize
+ */
+template <class TRegistrationMethod>
+void
+MultiResolutionRegistration<TRegistrationMethod>
+::OneLevelInitialize(
+unsigned int level )
+{
+
+}
+
+
+/**
+ * OneLevelRegistration
+ */
+template <class TRegistrationMethod>
+void
+MultiResolutionRegistration<TRegistrationMethod>
+::OneLevelRegistration(
+unsigned int level )
+{
+
+  std::cout << "Registering level: " << level << std::endl;
+
+  m_TargetPyramid->SetCurrentLevel( level );
+  m_TargetPyramid->Update();
+  m_InternalRegistrationMethod->SetTarget( m_TargetPyramid->GetOutput() );
+
+  m_ReferencePyramid->SetCurrentLevel( level );
+  m_ReferencePyramid->Update();
+  m_InternalRegistrationMethod->SetReference( m_ReferencePyramid->GetOutput() );
+
+  m_InternalRegistrationMethod->StartRegistration();
+
+}
+
+
+/**
+ * 
+
+/**
+ * PrintSelf method
+ */
+template <class TRegistrationMethod>
+void
+MultiResolutionRegistration<TRegistrationMethod>
+::PrintSelf(std::ostream& os, Indent indent)
+{
+  Superclass::PrintSelf(os,indent);
+
+  os << indent << "No. levels: " << m_NumberOfLevels << std::endl;
+
+}
+
+
+
+} // namespace itk
+
+#endif
