5f28dfab79e73b51e799fde9eef261075d7ab93a
Paul Hughett <hughett@upenn.edu>
2001-05-02 14:28:56 -0400

    ENH: Added a basic filter for resampling an image via an affine transform using linear interpolation.

---
+++ Code/BasicFilters/itkResampleImageFilter.h
@@ -0,0 +1,236 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit (ITK)
+  Module:    itkResampleImageFilter.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 2001 Insight Consortium
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * The name of the Insight Consortium, nor the names of any consortium members,
+   nor of any contributors, may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+  * Modified source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+
+
+#ifndef __itkResampleImageFilter_h
+#define __itkResampleImageFilter_h
+
+#include "itkAffineTransform.h"
+#include "itkImageFunction.h"
+#include "itkImageRegionIterator.h"
+#include "itkImageToImageFilter.h"
+#include "itkLinearInterpolateImageFunction.h"
+#include "itkSize.h"
+
+namespace itk
+{
+
+/** \class ResampleImageFilter
+ * \brief Resample an image via a coordinate transform
+ *
+ * ResampleImageFilter resamples an existing image through some coordinate
+ * transform, interpolating via some image function.  (In the current
+ * version, the transform is always affine and the interpolation is
+ * always linear, but this can be expected to change in the future.)
+ *
+ * Since this filter produces an image which is a different size than
+ * its input, it needs to override several of the methods defined
+ * in ProcessObject in order to properly manage the pipeline execution model.
+ * In particular, this filter overrides
+ * ProcessObject::GenerateInputRequestedRegion() and
+ * ProcessObject::UpdateOutputInformation().
+ *
+ * This filter is implemented as a multithreaded filter.  It provides a 
+ * ThreadedGenerateData() method for its implementation.
+ */
+template <class TInputImage, class TOutputImage>
+class ITK_EXPORT ResampleImageFilter:
+    public ImageToImageFilter<TInputImage,TOutputImage>
+{
+public:
+  /**
+   * Standard "Self" typedef.
+   */
+  typedef ResampleImageFilter         Self;
+
+  /**
+   * Standard "Superclass" typedef.
+   */
+  typedef ImageToImageFilter<TInputImage,TOutputImage>  Superclass;
+
+  /** 
+   * Smart pointer typedef support.
+   */
+  typedef SmartPointer<Self>        Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+
+  /**
+   * Number of dimensions
+   */
+  enum {NDimensions = TOutputImage::ImageDimension};
+
+  /**
+   * Transform typedef
+   *
+   * FIXME: Generalize this to any sort of coordinate transformation.
+   * FIXME: Check that input and output images have the same number of 
+   * dimensions; this is required by the current implementation of 
+   * AffineTransform.
+   */
+  typedef AffineTransform<double, TInputImage::ImageDimension> TransformType;
+  typedef AffineTransform<double, TInputImage::ImageDimension> 
+      *TransformPointerType;
+
+  /**
+   * Interpolation typedef
+   *
+   * FIXME: Generalize to any sort of image function.
+   */
+  typedef LinearInterpolateImageFunction<TInputImage>   InterpolationType;
+  typedef InterpolationType::ConstPointer  InterpolationPointerType;
+
+  /**
+   * Image size typedef
+   */
+  typedef Size<TOutputImage::ImageDimension>  SizeType;
+
+  /**
+   * Image index typedef
+   */
+  typedef typename TOutputImage::IndexType IndexType;
+
+  /**
+   * Image point typedef
+   */
+  typedef Point<double, TOutputImage::ImageDimension>    PointType;
+
+  /**
+   * Image pixel value typedef
+   */
+  typedef typename TOutputImage::PixelType   PixelType;
+
+  /**
+   * Method for creation through the object factory.
+   */
+  itkNewMacro(Self);  
+
+  /**
+   * Typedef to describe the output image region type.
+   */
+  typedef typename TOutputImage::RegionType OutputImageRegionType;
+
+  /** 
+   * Run-time type information (and related methods).
+   */
+  itkTypeMacro(ResampleImageFilter, ImageToImageFilter);
+
+  /**
+   * Set the coordinate transformation
+   *
+   * Set the coordinate transform to use for resampling.  Note that this
+   * must be in index coordinates and is the output-to-input transform,
+   * NOT the input-to-output transform that you might naively expect.
+   */
+  void SetTransform(TransformPointerType &transform) {
+    m_Transform = transform;
+  }
+
+  /**
+   * Set the interpolation function
+   */
+  void SetInterpolation(InterpolationPointerType &interpolation) {
+    m_Interpolation = interpolation;
+  }
+
+  /**
+   * Set the size of the output image
+   */
+  void SetSize(SizeType &size) {
+    m_Size = size;
+  }
+      
+  /**
+   * ResampleImageFilter produces an image which is a different size than
+   * its input.  As such, it needs to provide an implementation for
+   * UpdateOutputInformation() in order to inform the pipeline execution model.
+   * The original documentation of this method is below.
+   *
+   * \sa ProcessObject::UpdateOutputInformaton()
+   */
+  virtual void UpdateOutputInformation();
+
+  /**
+   * ResampleImageFilter needs a different input requested region than
+   * the output requested region.  As such, ResampleImageFilter needs
+   * to provide an implementation for GenerateInputRequestedRegion()
+   * in order to inform the pipeline execution model.
+   *
+   * \sa ProcessObject::GenerateInputRequestedRegion() */
+  virtual void GenerateInputRequestedRegion();
+
+protected:
+
+  ResampleImageFilter();
+  ~ResampleImageFilter() {};
+  ResampleImageFilter(const Self&) {}
+  void operator=(const Self&) {}
+  void PrintSelf(std::ostream& os, Indent indent);
+
+  /**
+   * ResampleImageFilter can be implemented as a multithreaded filter.  Therefore,
+   * this implementation provides a ThreadedGenerateData() routine which
+   * is called for each processing thread. The output image data is allocated
+   * automatically by the superclass prior to calling ThreadedGenerateData().
+   * ThreadedGenerateData can only write to the portion of the output image
+   * specified by the parameter "outputRegionForThread"
+   *
+   * \sa ImageToImageFilter::ThreadedGenerateData(),
+   *     ImageToImageFilter::GenerateData()
+   */
+  void ThreadedGenerateData(const OutputImageRegionType& outputRegionForThread,
+                            int threadId );
+
+private:
+
+  SizeType m_Size;                      // Size of the output image
+  TransformPointerType m_Transform;     // Coordinate transform to use
+  InterpolationPointerType m_Interpolation;
+                                        // Image function for interpolation
+};
+
+  
+} // end namespace itk
+  
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkResampleImageFilter.txx"
+#endif
+  
+#endif
---
+++ Code/BasicFilters/itkResampleImageFilter.txx
@@ -0,0 +1,210 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit (ITK)
+  Module:    itkResampleImageFilter.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 2001 Insight Consortium
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * The name of the Insight Consortium, nor the names of any consortium members,
+   nor of any contributors, may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+  * Modified source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+
+
+#ifndef _itkResampleImageFilter_txx
+#define _itkResampleImageFilter_txx
+
+#include "itkResampleImageFilter.h"
+#include "itkObjectFactory.h"
+
+namespace itk
+{
+
+/**
+ * Initialize new instance to obviously undefined state
+ */
+template <class TInputImage, class TOutputImage>
+ResampleImageFilter<TInputImage,TOutputImage>
+::ResampleImageFilter()
+{
+  for (int i = 0; i < NDimensions; i++)
+    m_Size[i] = 0;
+  m_Transform = 0;
+  m_Interpolation = 0;
+}
+
+
+/**
+ * Print out a description of self
+ */
+template <class TInputImage, class TOutputImage>
+void 
+ResampleImageFilter<TInputImage,TOutputImage>
+::PrintSelf(std::ostream& os, Indent indent)
+{
+  Superclass::PrintSelf(os,indent);
+  // FIXME: Add details about this class?
+
+  return;
+}
+
+
+/**
+ * ThreadedGenerateData
+ */
+template <class TInputImage, class TOutputImage>
+void 
+ResampleImageFilter<TInputImage,TOutputImage>
+::ThreadedGenerateData(
+  const OutputImageRegionType& outputRegionForThread,
+  int threadId)
+{
+  int i;
+  
+  itkDebugMacro(<<"Actually executing");
+
+  // Get the input and output pointers
+  InputImagePointer  inputPtr  = this->GetInput();
+  OutputImagePointer outputPtr = this->GetOutput();
+
+  // Create an iterator that will walk the output region for this thread.
+  typedef
+    ImageRegionIterator<TOutputImage> OutputIterator;
+
+  OutputIterator outIt(outputPtr, outputRegionForThread);
+
+  // Define a few indices that will be used to translate from an input pixel
+  // to an output pixel
+  IndexType outputIndex;         // Index to current output pixel
+  PointType outputPoint;         // Coordinates of current output pixel
+  PointType inputPoint;          // Coordinates of current input pixel
+  double value;                  // Interpolated value of input
+
+  // Configure the interpolator
+  m_Interpolation->SetInputImage(this->GetInput);
+
+  // Estimate total work for progress methods/callbacks
+  unsigned long updateVisits = 0;
+  if ( threadId == 0 )
+    {
+    updateVisits = 
+      outputPtr->GetRequestedRegion().GetNumberOfPixels()/10;
+    }
+        
+  // Walk the output region
+  for (i=0; !outIt.IsAtEnd(); ++outIt, i++ )
+
+    // Update progress
+    {
+    if ( threadId == 0 && !(i % updateVisits ) )
+      {
+      this->UpdateProgress((float)i/(float(updateVisits)*10.0));
+      }
+    
+    // Determine the index of the current output pixel
+    outputIndex = outIt.GetIndex();
+    for (int ii = 0; ii < NDimensions; ++ii)
+      outputPoint[ii] = outputIndex[ii];
+
+    // Compute corresponding input pixel position
+    inputPoint = m_Transform->Transform(outputPoint);
+
+    // Evaluate input at right position and copy to the output
+    value = (PixelType)(m_Interpolation->Evaluate(inputPoint));
+    outIt.Set(value);
+    }
+
+  return;
+}
+
+
+/** 
+ * Inform pipeline of necessary input image region
+ *
+ * Determining the actual input region is non-trivial, especially
+ * when we cannot assume anything about the transform being used.
+ * So we do the easy thing and request the entire input image.
+ */
+template <class TInputImage, class TOutputImage>
+void 
+ResampleImageFilter<TInputImage,TOutputImage>
+::GenerateInputRequestedRegion()
+{
+  // call the superclass's implementation of this method
+  Superclass::GenerateInputRequestedRegion();
+
+  // get pointers to the input and output
+  InputImagePointer  inputPtr  = this->GetInput();
+  if ( !inputPtr )
+    {
+    return;
+    }
+
+  // Request the entire input image
+  InputImageRegionType inputRegion;
+  inputRegion = inputPtr.GetLargestPossibleRegion();
+  inputPtr.SetLargestPossibleRegion(inputRegion);
+  inputPtr.SetRequestedRegion(inputRegion);
+
+  return;
+}
+
+
+/** 
+ * Inform pipeline of required output region
+ */
+template <class TInputImage, class TOutputImage>
+void 
+ResampleImageFilter<TInputImage,TOutputImage>
+::UpdateOutputInformation()
+{
+  // call the superclass' implementation of this method
+  Superclass::UpdateOutputInformation();
+
+  // get pointers to the input and output
+  OutputImagePointer outputPtr = this->GetOutput();
+  if ( !outputPtr )
+    {
+    return;
+    }
+
+  // Set the size of the output region
+  typename TOutputImage::RegionType outputLargestPossibleRegion;
+  outputLargestPossibleRegion.SetSize( m_Size );
+  outputPtr->SetLargestPossibleRegion( outputLargestPossibleRegion );
+
+  return;
+}
+
+} // end namespace itk
+
+#endif
