e2c6dd5cf78bf77d33d730156d079f899a157198
Luis Ibanez <luis.ibanez@kitware.com>
2001-05-11 17:36:53 -0400

    A File with content for the main page generated by Doxygen

---
+++ Code/DoxygenMainPage.h
@@ -0,0 +1,26 @@
+/**
+ *
+ * \mainpage  Insight Toolkit
+ *
+ * \image logo.jpg
+ *
+ * \section Introduccion
+ *
+ * \section Installation
+ * \subsection  How to Download
+ * \subsection  How to Compile
+ * 
+ *
+ * \section Getting Started
+ * \subsection Hello World
+ * \subsection My First itk Filter use
+ * \subsection Using Algorithms
+ *
+ * \section Resources
+ * \subsection The mailing list
+ * \subsection The FAQ
+ *
+ * \defgroup Algorithms "Algorithms"
+ * \endgroup
+ *
+ */ 
