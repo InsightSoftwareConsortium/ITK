eaf505137f4477ee270163f08dac315ea65b5afc
Josh Cates <cates@cs.utah.edu>
2001-05-16 17:35:06 -0400

    Draft version of a unified inner product for all neighborhood iterator types.

---
+++ Draft/FiniteDifference/itkNeighborhoodInnerProduct.h
@@ -0,0 +1,100 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkNeighborhoodInnerProduct.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 2001 Insight Consortium
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * The name of the Insight Consortium, nor the names of any consortium members,
+   nor of any contributors, may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+  * Modified source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+#ifndef __itkNeighborhoodInnerProduct_h
+#define __itkNeighborhoodInnerProduct_h
+
+#include "itkRegionNeighborhoodIterator.h"
+#include "itkSmartRegionNeighborhoodIterator.h"
+#include "itkNeighborhoodOperator.h"
+#include "itkConstSliceIterator.h"
+#include "itkImageBoundaryCondition.h"
+
+namespace itk {
+
+/**
+ * \class NeighborhoodInnerProduct
+ *
+ * This class defines the inner product operation between an itk::Neighborhood
+ * and and itk::NeighborhoodOperator.  The operator() method is overloaded
+ * to support various types of neighborhoods as wells as inner products with
+ * slices of neighborhoods.
+ *
+ */
+template<class TImage>
+class NeighborhoodInnerProduct
+{
+public:
+  typedef typename TImage::PixelType PixelType;
+  enum {ImageDimension = TImage::ImageDimension};
+
+  /**
+   *
+   */
+  PixelType operator()(const RegionNeighborhoodIterator<TImage> &it,
+                       const NeighborhoodOperator<PixelType, ImageDimension>
+                       &op) const;
+  /**
+   *
+   */
+  PixelType operator()(const std::slice &s,
+                       const RegionNeighborhoodIterator<TImage> &it,
+                       const NeighborhoodOperator<PixelType, ImageDimension>
+                       &op) const;
+  /**
+   *
+   */
+ PixelType operator()(const std::slice &s,
+                      /*                      const ImageBoundaryCondition<TImage> *,*/
+                      const SmartRegionNeighborhoodIterator<TImage> &it,
+                      const NeighborhoodOperator<PixelType, ImageDimension>
+                      &op) const;
+
+
+
+};
+
+} // end namespace itk
+  
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkNeighborhoodInnerProduct.txx"
+#endif
+
+#endif
