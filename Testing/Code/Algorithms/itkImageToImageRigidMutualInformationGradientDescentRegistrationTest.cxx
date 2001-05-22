77a9290d6b63f48fb54ad6f67e2303a48c83b390
Lydia Ng <lydiang@gmail.com>
2001-05-22 02:49:00 -0400

    ENH: test for new rigid mutual information registration class

---
+++ Testing/Code/Algorithms/itkImageToImageRigidMutualInformationGradientDescentRegistrationTest.cxx
@@ -0,0 +1,213 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit (ITK)
+  Module:    itkImageToImageRigidMutualInformationGradientDescentRegistrationTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 2001 Insight Consortium
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * The name of the Insight Consortium, nor the names of any consortium members,
+   nor of any contributors, may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+  * Modified source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+#include "itkPhysicalImage.h"
+#include "itkImageRegionIterator.h"
+
+#include "itkImageToImageRigidMutualInformationGradientDescentRegistration.h"
+#include "vnl/vnl_math.h"
+
+#include <iostream>
+
+int main()
+{
+
+//------------------------------------------------------------
+// Create two simple images
+// Two Gaussians with one translated (7,3,0) pixels from another
+//------------------------------------------------------------
+
+  //Allocate Images
+  typedef itk::PhysicalImage<unsigned char,3>           ReferenceType;
+  typedef itk::PhysicalImage<unsigned char,3>           TargetType;
+  enum { ImageDimension = ReferenceType::ImageDimension };
+
+  ReferenceType::SizeType size = {{100,100,1}};
+  ReferenceType::IndexType index = {{0,0,0}};
+  ReferenceType::RegionType region;
+  region.SetSize( size );
+  region.SetIndex( index );
+
+  ReferenceType::Pointer imgReference = ReferenceType::New();
+  imgReference->SetLargestPossibleRegion( region );
+  imgReference->SetBufferedRegion( region );
+  imgReference->SetRequestedRegion( region );
+  imgReference->Allocate();
+
+  TargetType::Pointer imgTarget = TargetType::New();
+  imgTarget->SetLargestPossibleRegion( region );
+  imgTarget->SetBufferedRegion( region );
+  imgTarget->SetRequestedRegion( region );
+  imgTarget->Allocate();
+
+  // Fill images with a 2D gaussian
+  typedef  itk::ImageRegionIterator<ReferenceType>
+    ReferenceIteratorType;
+  typedef  itk::ImageRegionIterator<TargetType>
+    TargetIteratorType;
+
+  itk::Point<double,2> center;
+  center[0] = (double)region.GetSize()[0]/2.0;
+  center[1] = (double)region.GetSize()[1]/2.0;
+
+  const double s = (double)region.GetSize()[0]/2.0;
+
+  itk::Point<double,2>  p;
+  itk::Vector<double,2> d;
+
+  // Set the displacement
+  itk::Vector<double,2> displacement;
+  displacement[0] = 7;
+  displacement[1] =	3;
+
+  ReferenceIteratorType ri(imgReference,region);
+  TargetIteratorType ti(imgTarget,region);
+  ri.Begin();
+  while(!ri.IsAtEnd())
+  {
+    p[0] = ri.GetIndex()[0];
+    p[1] = ri.GetIndex()[1];
+	  d = p-center;
+	  d += displacement;
+	  const double x = d[0];
+	  const double y = d[1];
+    ri.Set( (unsigned char)( 200.0 * exp( - ( x*x + y*y )/(s*s) ) ) );
+    ++ri;
+  }
+
+
+  ti.Begin();
+  while(!ti.IsAtEnd())
+  {
+    p[0] = ti.GetIndex()[0];
+    p[1] = ti.GetIndex()[1];
+	d = p-center;
+	const double x = d[0];
+	const double y = d[1];
+    ti.Set( (unsigned char)( 200.0 * exp( - ( x*x + y*y )/(s*s) ) ) );
+    ++ti;
+  }
+
+  // set image origin to be center of the image
+  double transCenter[3];
+  for( unsigned int j = 0; j < 3; j++ )
+    {
+    transCenter[j] = -0.5 * double(size[j] - 1);
+    }
+
+  imgReference->SetOrigin( transCenter );
+  imgTarget->SetOrigin( transCenter );
+
+
+//-----------------------------------------------------------
+// Set up a the registrator
+//-----------------------------------------------------------
+  typedef itk::ImageToImageRigidMutualInformationGradientDescentRegistration<
+    ReferenceType,TargetType> RegistrationType;
+
+  RegistrationType::Pointer registrationMethod = RegistrationType::New();
+
+  // connect the images
+  registrationMethod->SetReference(imgReference);
+  registrationMethod->SetTarget(imgTarget);
+
+  // set translation scale
+  const double transScale = 100;
+  registrationMethod->SetTranslationScale( transScale );
+
+  // set metric related parameters
+  registrationMethod->GetMetric()->SetTargetStandardDeviation( 5.0 );
+  registrationMethod->GetMetric()->SetReferenceStandardDeviation( 5.0 );
+  registrationMethod->GetMetric()->SetNumberOfSpatialSamples( 50 );
+
+  // do the registration
+  // reduce learning rate as we go
+  registrationMethod->SetNumberOfIterations( 300 );
+  registrationMethod->SetLearningRate( 5e-5 );
+  registrationMethod->StartRegistration();
+
+  registrationMethod->SetNumberOfIterations( 300 );
+  registrationMethod->SetLearningRate( 1e-5 );
+  registrationMethod->StartRegistration();
+
+  registrationMethod->SetNumberOfIterations( 300 );
+  registrationMethod->SetLearningRate( 1e-6 );
+  registrationMethod->StartRegistration();
+
+
+  // get the results
+  RegistrationType::ParametersType solution = 
+    registrationMethod->GetParameters();
+
+  std::cout << "Solution is: " << solution << std::endl;
+
+  //
+  // check results to see if it is within range
+  //
+  bool pass = true;
+  double trueParameters[7] = { 0,0,0,1,0,0,0 };
+  trueParameters[4] = - displacement[0];
+  trueParameters[5] = - displacement[1];
+  trueParameters[6] = 0.0;
+  for( unsigned int j = 0; j < 4; j++ )
+    {
+    if( vnl_math_abs( solution[j] - trueParameters[j] ) > 0.02 )
+      {
+      pass = false;
+      }
+    }
+  for( unsigned int j = 4; j < 7; j++ )
+    {
+    if( vnl_math_abs( solution[j] * transScale - trueParameters[j] ) > 1.0 )
+      {
+      pass = false;
+      }
+    }
+
+  if( !pass )
+    {
+    std::cout << "Test failed." << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  std::cout << "Test passed." << std::endl;
+  return EXIT_SUCCESS;
+
+}
