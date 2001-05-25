fa3cc546194d102be1cdbe28aa3a4d7720e5231c
Jim Miller <millerjv@crd.ge.com>
2001-05-24 22:33:23 -0400

    Moved include and link directories to this file to facilitate building projects that use Insight but are not within Insights source tree

---
+++ itkCMakeOptions.cmake
@@ -0,0 +1,25 @@
+#
+# This file can be included by other projects that use or depend on Insight.
+# It sets up many default parameters and include paths.
+#
+
+# set project include directories
+INCLUDE_DIRECTORIES(
+${ITK_BINARY_DIR} 
+${ITK_SOURCE_DIR}/Code/Numerics
+${ITK_SOURCE_DIR}/Code/Common 
+${ITK_BINARY_DIR}/Code/Numerics/vxl 
+${ITK_SOURCE_DIR}/Code/Numerics/vxl 
+${ITK_BINARY_DIR}/Code/Numerics/vxl/vcl 
+${ITK_SOURCE_DIR}/Code/Numerics/vxl/vcl 
+)
+
+# set link libraries
+LINK_DIRECTORIES(
+${ITK_BINARY_DIR}/Code/Common 
+${ITK_BINARY_DIR}/Code/Numerics/vxl )
+
+# set link libraries for project
+IF(UNIX)
+  LINK_LIBRARIES(${CMAKE_THREAD_LIBS} ${CMAKE_DL_LIBS})
+ENDIF(UNIX)
