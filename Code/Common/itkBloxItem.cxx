d6b8b9e19032f0565c782e302129ac3a1005369f
Damion Shelton <dmshelto@andrew.cmu.edu>
2001-05-29 14:29:23 -0400

    ENH: Prototype of BloxItem

---
+++ Code/Common/itkBloxItem.cxx
@@ -0,0 +1,21 @@
+#ifndef __itkBloxItem_cxx
+#define __itkBloxItem_cxx
+
+#include "itkBloxItem.h"
+
+namespace itk
+{
+
+BloxItem::BloxItem()
+{
+
+}
+
+BloxItem::~BloxItem()
+{
+
+}
+
+} // end namespace itk
+
+#endif
