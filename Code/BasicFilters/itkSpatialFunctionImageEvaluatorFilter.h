2d11066dd085fe07f3b89dd53c0a9e3b41fed5ae
Damion Shelton <dmshelto@andrew.cmu.edu>
2001-05-29 15:10:46 -0400

    ENH: Ability to evaluate a spatial function over an input image

---
+++ Code/BasicFilters/itkSpatialFunctionImageEvaluatorFilter.h
@@ -0,0 +1,218 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkSpatialFunctionImageEvaluatorFilter.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 2001 Insight Consortium
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * The name of the Insight Consortium, nor the names of any consortium members,
+   nor of any contributors, may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+  * Modified source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+#ifndef __itkSpatialFunctionImageEvaluatorFilter_h
+#define __itkSpatialFunctionImageEvaluatorFilter_h
+
+#include "itkImageFunction.h"
+#include "itkImageRegionIterator.h"
+#include "itkImageToImageFilter.h"
+#include "itkSize.h"
+#include "itkSpatialFunction.h"
+
+namespace itk
+{
+
+/**
+ * \class SpatialFunctionImageEvaluatorFilter
+ * \brief Evaluates an SpatialFunction onto a source image
+ *
+ * SpatialFunctionImageEvaluatorFilter walks an input image and evaluates
+ * the function at every pixel location. Since implicit functions exist
+ * in "real" space, rather than index space, the source image must be a
+ * PhysicalImage.
+ *
+ * Like its parent ImageToImageFilter, this class functions in the filtering
+ * pipeline and produces a unique output image.
+ * */
+
+template<class TInputImage, class TOutputImage>
+class ITK_EXPORT SpatialFunctionImageEvaluatorFilter :
+   public ImageToImageFilter<TInputImage, TOutputImage>
+{
+public:
+   
+  /**
+   * Standard "Self" typedef.
+   */
+  typedef SpatialFunctionImageEvaluatorFilter Self;
+
+  /**
+   * Standard "Superclass" typedef.
+   */
+  typedef ImageToImageFilter<TInputImage,TOutputImage>  Superclass;
+
+  /** 
+   * Smart pointer typedef support.
+   */
+  typedef SmartPointer<Self>        Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+
+  /**
+   * Number of dimensions
+   */
+  enum {NDimensions = TInputImage::ImageDimension};
+
+  /**
+   * Image size typedef
+   */
+  typedef Size<TOutputImage::ImageDimension> SizeType;
+
+  /**
+   * Image index typedef
+   */
+  typedef typename TOutputImage::IndexType IndexType;
+
+  /**
+   * Image pixel value typedef
+   */
+  typedef typename TOutputImage::PixelType PixelType;
+
+  /**
+   * Typedef to describe the output image region type.
+   */
+  typedef typename TOutputImage::RegionType OutputImageRegionType;
+
+  /**
+  * Typedef describing vector info
+  */
+  typedef vnl_vector_fixed<double, NDimensions> TVectorType;
+
+  /** 
+   * Run-time type information (and related methods).
+   */
+  itkTypeMacro( SpatialFunctionImageEvaluatorFilter, ImageToImageFilter );
+
+  /**
+   * Method for creation through the object factory.
+   */
+  itkNewMacro(Self);  
+
+  /**
+   * Set the internal implicit function
+   */
+  void SetFunction( SpatialFunction<NDimensions>* pFunction )
+    {m_pFunction = pFunction;};
+
+  /**
+   * Set the evaluator behavior to function value. This flag means the
+   * the actual value of the function will be stored in the output image,
+   * to the best of the output images ability to handle double floating
+   * point numbers.
+   */
+  void SetBehaviorToFunctionValue()
+    {m_Behavior = 0;};
+
+  /**
+   * Set the evaluator behavior to interior/exterior. Assuming that the
+   * spatial function can provide information about whether or not a physical
+   * location is "inside" or "outside" a certain object (e.g. a sphere),
+   * then the interior is set to a certain pixel value and the exterior
+   * set to another pixel value.
+   *
+   * By conventions, functions which supplu interior/exterior information
+   * do so as follows:
+   *
+   * >0 means exterior
+   * 0 means the surface of the function
+   * <0 means interior
+   *
+   * This behavior is identical to VTK's implicit functions
+   */
+  void SetBehaviorToInteriorExterior()
+    {m_Behavior = 1;};
+
+  /**
+   * Method for evaluating the implicit function over the image.
+   */
+  void GenerateData();
+
+  /**
+   * Gets and sets for member variables
+   */
+
+  itkSetMacro( InteriorValue, PixelType );
+  itkGetMacro( InteriorValue, PixelType );
+
+  itkSetMacro( ExteriorValue, PixelType );
+  itkGetMacro( ExteriorValue, PixelType );
+
+protected:
+
+  SpatialFunctionImageEvaluatorFilter();
+  virtual ~SpatialFunctionImageEvaluatorFilter() {};
+
+  SpatialFunctionImageEvaluatorFilter(const Self&) {}
+  void operator=(const Self&) {}
+
+private:
+
+  /**
+   * The behavior of the evaluator
+   * 0 = evaluate the actual function value
+   * 1 = evaluate interior / exterior
+   */
+  unsigned int m_Behavior;
+
+  /**
+   * The function that will be evaluated over the image
+   */
+  SpatialFunction<NDimensions>* m_pFunction;
+
+  /**
+   * The value to be output if the function represents the "interior"
+   * or surface of the function.
+   */
+  PixelType m_InteriorValue;
+  
+  /**
+   * The value to be output if the function represents the "exterior"
+   * of the function.
+   */
+  PixelType m_ExteriorValue;
+};
+
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkSpatialFunctionImageEvaluatorFilter.txx"
+#endif
+
+#endif
---
+++ Code/BasicFilters/itkSpatialFunctionImageEvaluatorFilter.txx
@@ -0,0 +1,166 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkSpatialFunctionImageEvaluatorFilter.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 2001 Insight Consortium
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * The name of the Insight Consortium, nor the names of any consortium members,
+   nor of any contributors, may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+  * Modified source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+#ifndef __itkSpatialFunctionImageEvaluatorFilter_txx
+#define __itkSpatialFunctionImageEvaluatorFilter_txx
+
+#include <iostream>
+#include "itkSize.h"
+#include "itkPixelTraits.h"
+#include "itkImageRegion.h"
+#include "itkSpatialFunctionImageEvaluatorFilter.h"
+
+namespace itk
+{
+
+template< class TInputImage, class TOutputImage >
+SpatialFunctionImageEvaluatorFilter< TInputImage, TOutputImage >
+::SpatialFunctionImageEvaluatorFilter()
+{
+  std::cout << "SpatialFunctionImageEvaluatorFilter::SpatialFunctionImageEvaluatorFilter() called\n";
+  
+  // Set the internal function to null
+  this->m_pFunction = 0;
+
+  // Default behavior is to just evaluate the function
+  this->SetBehaviorToFunctionValue();
+
+  // Safe values for all data types
+  m_InteriorValue = 255;
+  m_ExteriorValue = 128;
+}
+
+
+template< class TInputImage, class TOutputImage >
+void
+SpatialFunctionImageEvaluatorFilter< TInputImage, TOutputImage >
+::GenerateData()
+{
+  std::cout << "SpatialFunctionImageEvaluatorFilter::GenerateData() called\n";
+
+  // Get the input and output pointers
+  InputImagePointer  inputPtr = this->GetInput(0);
+  OutputImagePointer outputPtr = this->GetOutput(0);
+
+  // Make sure we're getting everything
+  inputPtr->SetRequestedRegionToLargestPossibleRegion();
+
+  // How big is the input image?
+  TInputImage::SizeType inputSize = inputPtr->GetLargestPossibleRegion().GetSize();
+
+  // Create a region object native to the output image type
+  OutputImageRegionType outputRegion;
+
+  // Resize the output region
+  outputRegion.SetSize( inputSize );
+
+  // Set the largest legal region size (i.e. the size of the whole image)
+  // to what we just defined
+  outputPtr->SetLargestPossibleRegion( outputRegion );
+  outputPtr->SetBufferedRegion( outputRegion );
+  outputPtr->SetRequestedRegion( outputRegion );
+  outputPtr->Allocate();
+
+  // Create an iterator that will walk the output region
+  typedef ImageRegionIterator<TOutputImage> OutputIterator;
+
+  OutputIterator outIt = OutputIterator(outputPtr,
+                                        outputPtr->GetRequestedRegion());
+
+  // Define a few indices that will be used to translate from an input pixel
+  // to an output pixel
+  typename TOutputImage::IndexType outputIndex;
+
+  const double* origin;
+  const double* spacing;
+
+  // Get the origin and spacing from the input image
+  origin = inputPtr->GetOrigin();
+  spacing = inputPtr->GetSpacing();
+
+  // Set the origin and spacing of the output image
+  outputPtr->SetOrigin(origin);
+  outputPtr->SetSpacing(spacing);
+
+  TVectorType outputVector;
+
+  double value;
+  PixelType pixval;
+
+  // walk the output image, and sample the input image
+  for ( ; !outIt.IsAtEnd(); ++outIt)
+    {
+    // determine the index of the output pixel
+    outputIndex = outIt.GetIndex();
+
+    for (int ii = 0; ii < NDimensions; ++ii)
+      outputVector[ii] = outputIndex[ii]*spacing[ii]+origin[ii];
+
+    value = m_pFunction->EvaluateFunctionValue(&outputVector);
+
+    switch (m_Behavior)
+      {
+      case 0: // Use the function value
+        pixval.SetScalar(value);
+        outIt.Set(pixval);
+        break;
+
+      case 1: // Use the interior/exterior values
+        if (value <= 0)
+          pixval.SetScalar(m_InteriorValue);
+        else
+          pixval.SetScalar(m_ExteriorValue);
+        outIt.Set(pixval);
+        break;
+      
+      default: // Use the function value
+        pixval.SetScalar(value);
+        outIt.Set(pixval);
+    
+      } // end switch
+
+    }
+
+  std::cout << "SpatialFunctionImageEvaluatorFilter::GenerateData() finished\n";
+}
+
+} // end namespace
+
+#endif
