647a89eec2be70689521a5a5183d5048e7b99884
Luis Ibanez <luis.ibanez@kitware.com>
2001-05-30 12:44:35 -0400

    STYLE: fixed formula notation for Doxygen

--- Code/Numerics/itkGradientDescentOptimizer.h
+++ Code/Numerics/itkGradientDescentOptimizer.h
@@ -54,7 +54,11 @@ namespace itk
  *
  * p(n+1) = p(n) + learningRate * d f(p(n)) / d p(n)
  *
- * \$ p\{n+1} = p\n + learningRate * \frac{\partial f(p\n) }{\partial p\n} \$
+ * \f[ 
+ *        p\{n+1} = p\n 
+ *                + mbox{learningRate} 
+                  * \frac{\partial f(p\n) }{\partial p\n} 
+ * \f]
  *
  * The learning rate is a fixed scalar defined via SetLearningRate().
  * The optimizer steps through a user defined number of iterations;
