dca00249468563b7fe4059021b850f870d2f9bf8
Damion Shelton <dmshelto@andrew.cmu.edu>
2001-06-21 11:43:41 -0400

    BUG: fixed missing itkPhysicalImage include

--- Code/BasicFilters/itkDifferenceOfGaussiansGradientImageFilter.h
+++ Code/BasicFilters/itkDifferenceOfGaussiansGradientImageFilter.h
@@ -41,6 +41,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef __itkDifferenceOfGaussiansGradientImageFilter_h
 #define __itkDifferenceOfGaussiansGradientImageFilter_h
 
+#include "itkPhysicalImage.h"
 #include "itkImageToImageFilter.h"
 #include "itkPhysicalImage.h"
 #include "itkScalarVector.h"
