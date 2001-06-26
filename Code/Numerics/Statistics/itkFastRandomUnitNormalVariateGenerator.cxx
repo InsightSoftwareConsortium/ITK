70825dea4125eaa295fa9d28080016ee541ea5cb
Jisung Kim <bahrahm@yahoo.com>
2001-06-26 16:16:55 -0400

    BUG: replaced cmath with vnl_math.h

--- Code/Numerics/Statistics/itkFastRandomUnitNormalVariateGenerator.cxx
+++ Code/Numerics/Statistics/itkFastRandomUnitNormalVariateGenerator.cxx
@@ -38,7 +38,7 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 =========================================================================*/
-#include <cmath>
+#include <vnl/vnl_math.h>
 
 #include "itkFastRandomUnitNormalVariateGenerator.h"
 
