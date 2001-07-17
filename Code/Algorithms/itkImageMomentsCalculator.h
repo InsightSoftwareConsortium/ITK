336bab3be4797021b51e5d60ab9fdd6a733cec85
Jim Miller <millerjv@crd.ge.com>
2001-07-17 11:06:44 -0400

    FIX: CVS Headers

--- Code/Algorithms/itkImageMomentsCalculator.h
+++ Code/Algorithms/itkImageMomentsCalculator.h
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   Insight Segmentation & Registration Toolkit
-  Module:    $RCSFile: $
+  Module:    itkImageMomentsCalculator.h
   Language:  C++
   Date:      $Date$
   Version:   $Revision$
