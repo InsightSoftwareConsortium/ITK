8b10209df99378d28d6be6d58fb5ee99007028da
Luis Ibanez <luis.ibanez@kitware.com>
2001-07-26 21:27:50 -0400

    FIX: Modified to use new method names in itkSplineKernelTransform

--- Testing/Code/Common/itkSplineKernelTransformTest.cxx
+++ Testing/Code/Common/itkSplineKernelTransformTest.cxx
@@ -34,21 +34,21 @@ int main(int argc, char* argv[])
       {
       sourcePoint2D[0] = j;
       sourcePoint2D[1] = i;
-      ebs2D->Getp()->SetPoint(pointNumber, sourcePoint2D);
-      tps2D->Getp()->SetPoint(pointNumber, sourcePoint2D);
+      ebs2D->GetSourceLandmarks()->SetPoint(pointNumber, sourcePoint2D);
+      tps2D->GetSourceLandmarks()->SetPoint(pointNumber, sourcePoint2D);
       targetPoint2D[0] = 3*j;
       targetPoint2D[1] = 3*i;
-      ebs2D->Getq()->SetPoint(pointNumber, targetPoint2D);
-      tps2D->Getq()->SetPoint(pointNumber, targetPoint2D);
+      ebs2D->GetTargetLandmarks()->SetPoint(pointNumber, targetPoint2D);
+      tps2D->GetTargetLandmarks()->SetPoint(pointNumber, targetPoint2D);
       pointNumber++;
       }
     }
   std::cout << "EBS 2D Test:" << std::endl;
   ebs2D->SetAlpha(0.25);
-  ebs2D->ComputeW();
+  ebs2D->ComputeWMatrix();
   for (i = 0; i < 4; i++)
     {
-    ebs2D->Getp()->GetPoint(i, &sourcePoint2D);
+    ebs2D->GetSourceLandmarks()->GetPoint(i, &sourcePoint2D);
     targetPoint2D = ebs2D->TransformPoint(sourcePoint2D);
     std::cout << sourcePoint2D << " warps to: " << 
       targetPoint2D << std::endl;
@@ -56,10 +56,10 @@ int main(int argc, char* argv[])
   std::cout << std::endl;
   
   std::cout << "TPS 2D Test:" << std::endl;
-  tps2D->ComputeW();
+  tps2D->ComputeWMatrix();
   for (i = 0; i < 4; i++)
     {
-    tps2D->Getp()->GetPoint(i, &sourcePoint2D);
+    tps2D->GetSourceLandmarks()->GetPoint(i, &sourcePoint2D);
     targetPoint2D = tps2D->TransformPoint(sourcePoint2D);
     std::cout << sourcePoint2D << " warps to: " << 
       targetPoint2D << std::endl;
@@ -91,23 +91,23 @@ int main(int argc, char* argv[])
         sourcePoint3D[0] = k;
         sourcePoint3D[1] = j;
         sourcePoint3D[2] = i;
-        ebs3D->Getp()->SetPoint(pointNumber, sourcePoint3D);
-        tps3D->Getp()->SetPoint(pointNumber, sourcePoint3D);
+        ebs3D->GetSourceLandmarks()->SetPoint(pointNumber, sourcePoint3D);
+        tps3D->GetSourceLandmarks()->SetPoint(pointNumber, sourcePoint3D);
         targetPoint3D[0] = 3*k;
         targetPoint3D[1] = 3*j;
         targetPoint3D[2] = 3*i;
-        ebs3D->Getq()->SetPoint(pointNumber, targetPoint3D);
-        tps3D->Getq()->SetPoint(pointNumber, targetPoint3D);
+        ebs3D->GetTargetLandmarks()->SetPoint(pointNumber, targetPoint3D);
+        tps3D->GetTargetLandmarks()->SetPoint(pointNumber, targetPoint3D);
         pointNumber++;
         }
       }
     }
   std::cout << "EBS 3D Test:" << std::endl;
   ebs3D->SetAlpha(0.25);
-  ebs3D->ComputeW();
+  ebs3D->ComputeWMatrix();
   for (i = 0; i < 8; i++)
     {
-    ebs3D->Getp()->GetPoint(i, &sourcePoint3D);
+    ebs3D->GetSourceLandmarks()->GetPoint(i, &sourcePoint3D);
     targetPoint3D = ebs3D->TransformPoint(sourcePoint3D);
     std::cout << sourcePoint3D << " warps to: " << 
       targetPoint3D << std::endl;
@@ -115,10 +115,10 @@ int main(int argc, char* argv[])
   std::cout << std::endl;
   
   std::cout << "TPS 3D Test:" << std::endl;
-  tps3D->ComputeW();
+  tps3D->ComputeWMatrix();
   for (i = 0; i < 8; i++)
     {
-    tps3D->Getp()->GetPoint(i, &sourcePoint3D);
+    tps3D->GetSourceLandmarks()->GetPoint(i, &sourcePoint3D);
     targetPoint3D = tps3D->TransformPoint(sourcePoint3D);
     std::cout << sourcePoint3D << " warps to: " << 
       targetPoint3D << std::endl;
@@ -153,14 +153,14 @@ int main(int argc, char* argv[])
           sourcePoint4D[1] = k;
           sourcePoint4D[2] = j;
           sourcePoint4D[3] = i;
-          ebs4D->Getp()->SetPoint(pointNumber, sourcePoint4D);
-          tps4D->Getp()->SetPoint(pointNumber, sourcePoint4D);
+          ebs4D->GetSourceLandmarks()->SetPoint(pointNumber, sourcePoint4D);
+          tps4D->GetSourceLandmarks()->SetPoint(pointNumber, sourcePoint4D);
           targetPoint4D[0] = 3*l;
           targetPoint4D[1] = 3*k;
           targetPoint4D[2] = 3*j;
           targetPoint4D[3] = 3*i;
-          ebs4D->Getq()->SetPoint(pointNumber, targetPoint4D);
-          tps4D->Getq()->SetPoint(pointNumber, targetPoint4D);
+          ebs4D->GetTargetLandmarks()->SetPoint(pointNumber, targetPoint4D);
+          tps4D->GetTargetLandmarks()->SetPoint(pointNumber, targetPoint4D);
           pointNumber++;
           }
         }
@@ -168,10 +168,10 @@ int main(int argc, char* argv[])
     }
   std::cout << "EBS 4D Test:" << std::endl;
   ebs4D->SetAlpha(0.25);
-  ebs4D->ComputeW();
+  ebs4D->ComputeWMatrix();
   for (i = 0; i < 16; i++)
     {
-    ebs4D->Getp()->GetPoint(i, &sourcePoint4D);
+    ebs4D->GetSourceLandmarks()->GetPoint(i, &sourcePoint4D);
     targetPoint4D = ebs4D->TransformPoint(sourcePoint4D);
     std::cout << sourcePoint4D << " warps to: " << 
       targetPoint4D << std::endl;
@@ -179,10 +179,10 @@ int main(int argc, char* argv[])
   std::cout << std::endl;
 
   std::cout << "TPS 4D Test:" << std::endl;
-  tps4D->ComputeW();
+  tps4D->ComputeWMatrix();
   for (i = 0; i < 16; i++)
     {
-    tps4D->Getp()->GetPoint(i, &sourcePoint4D);
+    tps4D->GetSourceLandmarks()->GetPoint(i, &sourcePoint4D);
     targetPoint4D = tps4D->TransformPoint(sourcePoint4D);
     std::cout << sourcePoint4D << " warps to: " << 
       targetPoint4D << std::endl;
