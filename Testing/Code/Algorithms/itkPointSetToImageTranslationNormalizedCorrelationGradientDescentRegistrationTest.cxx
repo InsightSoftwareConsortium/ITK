2eb4be924771ce46b9202fb272996f986004349a
Luis Ibanez <luis.ibanez@kitware.com>
2001-07-31 10:51:17 -0400

    FIX: casting warning on Set()

--- Testing/Code/Algorithms/itkPointSetToImageTranslationNormalizedCorrelationGradientDescentRegistrationTest.cxx
+++ Testing/Code/Algorithms/itkPointSetToImageTranslationNormalizedCorrelationGradientDescentRegistrationTest.cxx
@@ -132,7 +132,8 @@ int main()
     d += displacement;
     const double x = d[0];
     const double y = d[1];
-    ri.Set( 200.0 * exp( - ( x*x + y*y )/(s*s) ) );
+    const double value = 200.0 * exp( - ( x*x + y*y )/(s*s) );
+    ri.Set( static_cast<ReferenceType::PixelType>( value ) );
     ++ri;
   }
 
@@ -145,7 +146,8 @@ int main()
     d = p-center;
     const double x = d[0];
     const double y = d[1];
-    ti.Set( 200.0 * exp( - ( x*x + y*y )/(s*s) ) );
+    const double value = 200.0 * exp( - ( x*x + y*y )/(s*s) );
+    ti.Set( static_cast<TargetType::PixelType>( value ) ); 
     ++ti;
   }
 
