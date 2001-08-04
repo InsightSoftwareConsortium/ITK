0c21db9e632b3a45e7ade6ea6d4cb557b4722ce3
Luis Ibanez <luis.ibanez@kitware.com>
2001-08-04 19:43:51 -0400

    ENH: Transforms use now SmartPointers

--- Testing/Code/Common/itkTranslationTransformTest.cxx
+++ Testing/Code/Common/itkTranslationTransformTest.cxx
@@ -73,8 +73,9 @@ int main(int argc,char *argv[])
             << std::endl;
 
   /* Create a 2D identity transformation and show its parameters */
-  itk::TranslationTransform<double,2> id2;
-  vector2 = id2.GetOffset();
+  typedef itk::TranslationTransform<double,2>  TransformType;
+  TransformType::Pointer  id2 = TransformType::New();
+  vector2 = id2->GetOffset();
   std::cout << "Vector from instantiating an identity transform:"
             << std::endl;
   PrintVector( vector2 );
@@ -82,7 +83,8 @@ int main(int argc,char *argv[])
   /* Create and show a simple 2D transform from given parameters */
   vector2[0] = 5;
   vector2[1] = 6;
-  itk::TranslationTransform<double,2> aff2(vector2);
+  TransformType::Pointer aff2 = TransformType::New();
+  aff2->SetOffset(vector2);
   for (i = 0; i < 2; i++) 
     {
     vector2[i]    = 0.0;
@@ -90,27 +92,26 @@ int main(int argc,char *argv[])
   std::cout << "Instantiation of a given 2D transform:" 
             << std::endl << aff2;
 
-  itk::TranslationTransform<double,2> inverse2;
-  inverse2 = aff2.Inverse();
+  TransformType::Pointer inverse2 = aff2->Inverse();
   std::cout << "Inverse of the given transform:"
             << std::endl << inverse2;
 
   /* Set parameters of a 2D transform */
   vector2[0] = 2;
   vector2[1] = 1;
-  aff2.SetOffset(vector2);
+  aff2->SetOffset(vector2);
   for (i = 0; i < 2; i++) 
     {
     vector2[i]    = 0.0;
     }
 
-  vector2 = aff2.GetOffset();
+  vector2 = aff2->GetOffset();
   std::cout << "Setting the offset in an existing  transform:"
             << std::endl;
   PrintVector( vector2 );
 
   /* Try composition of two transformations */
-  aff2.Compose(aff2);
+  aff2->Compose(aff2);
   std::cout << "Result of a composition:"
             << std::endl << aff2;
 
@@ -118,7 +119,7 @@ int main(int argc,char *argv[])
   VectorType trans;
   trans[0] = 1;
   trans[1] = 2;
-  aff2.Translate(trans);
+  aff2->Translate(trans);
   std::cout << "Result of a translation:"
             << std::endl << aff2;
 
@@ -126,12 +127,12 @@ int main(int argc,char *argv[])
   itk::Point<double, 2> u2, v2;
   u2[0] = 3;
   u2[1] = 5;
-  v2 = aff2.TransformPoint(u2);
+  v2 = aff2->TransformPoint(u2);
   std::cout << "Transform a point:" << std::endl
             << v2[0] << " , " << v2[1] << std::endl;
 
   /* Back transform a point */
-  v2 = aff2.BackTransform(u2);
+  v2 = aff2->BackTransform(u2);
   std::cout << "Back transform a point:" << std::endl
             << v2[0] << " , " << v2[1] << std::endl;
 
@@ -139,12 +140,12 @@ int main(int argc,char *argv[])
   vnl_vector_fixed<double, 2> x2, y2;
   x2[0] = 1;
   x2[1] = 2;
-  y2 = aff2.TransformVector(x2);
+  y2 = aff2->TransformVnlVector(x2);
   std::cout << "Transform a vnl_vector:" << std::endl
             << y2[0] << " , " << y2[1] << std::endl;
 
   /* Back transform a vector */
-  y2 = aff2.BackTransform(x2);
+  y2 = aff2->BackTransform(x2);
   std::cout << "Back transform a vnl_vector:" << std::endl
             << y2[0] << " , " << y2[1] << std::endl;
 
@@ -152,12 +153,12 @@ int main(int argc,char *argv[])
   itk::Vector<double, 2> u3, v3;
   u3[0] = 3;
   u3[1] = 5;
-  v3 = aff2.TransformVector(u3);
+  v3 = aff2->TransformVector(u3);
   std::cout << "Transform a vector:" << std::endl
             << v3[0] << " , " << v3[1] << std::endl;
 
   /* Back transform a vector */
-  v3 = aff2.BackTransform(u3);
+  v3 = aff2->BackTransform(u3);
   std::cout << "Back transform a vector :" << std::endl
             << v3[0] << " , " << v3[1] << std::endl;
 
@@ -165,12 +166,12 @@ int main(int argc,char *argv[])
   itk::Vector<double, 2> u4, v4;
   u4[0] = 3;
   u4[1] = 5;
-  v4 = aff2.TransformVector(u4);
+  v4 = aff2->TransformVector(u4);
   std::cout << "Transform a Covariant vector:" << std::endl
             << v4[0] << " , " << v4[1] << std::endl;
 
   /* Back transform a vector */
-  v4 = aff2.BackTransform(u4);
+  v4 = aff2->BackTransform(u4);
   std::cout << "Back transform a vector :" << std::endl
             << v4[0] << " , " << v4[1] << std::endl;
 
