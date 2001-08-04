a833e7809e9a45e2d464c741810b758e7aacfe18
Luis Ibanez <luis.ibanez@kitware.com>
2001-08-04 19:44:55 -0400

    ENH: AffineTransform uses SmartPointers now and support SetParameters() and      GetJacobian()

--- Testing/Code/Common/itkAffineTransformTest.cxx
+++ Testing/Code/Common/itkAffineTransformTest.cxx
@@ -80,9 +80,13 @@ int main(
               << std::endl;
 
     /* Create a 2D identity transformation and show its parameters */
-    itk::AffineTransform<double,2> id2;
-    matrix2 = id2.GetMatrix();
-    vector2 = id2.GetOffset();
+    typedef itk::Point<double,6> ParametersType;
+    typedef itk::Matrix<double,2,6> JacobianType;
+    
+    typedef itk::AffineTransform<double,2> Affine2DType;
+    Affine2DType::Pointer id2 = Affine2DType::New();
+    matrix2 = id2->GetMatrix();
+    vector2 = id2->GetOffset();
     std::cout << "Matrix from instantiating an identity transform:"
               << std::endl << matrix2;
     std::cout << "Vector from instantiating an identity transform:"
@@ -96,15 +100,18 @@ int main(
     matrix2[1][1] = 4;
     vector2[0] = 5;
     vector2[1] = 6;
-    itk::AffineTransform<double,2> aff2(matrix2, vector2);
+
+    Affine2DType::Pointer aff2 = Affine2DType::New();
+    aff2->SetMatrix( matrix2 );
+    aff2->SetOffset( vector2 );
     for (i = 0; i < 2; i++) {
         for (j = 0; j < 2; j++)
             matrix2[i][j] = 0.0;
         vector2[i]    = 0.0;
     }
     std::cout << "Instantiation of a given 2D transform:" 
-              << std::endl << aff2;
-    inverse2 = aff2.GetInverse();
+              << std::endl << *aff2;
+    inverse2 = aff2->GetInverse();
     std::cout << "Inverse matrix for the given transform:"
               << std::endl << inverse2;
 
@@ -115,15 +122,15 @@ int main(
     matrix2[1][1] = 3;
     vector2[0] = 2;
     vector2[1] = 1;
-    aff2.SetMatrix(matrix2);
-    aff2.SetOffset(vector2);
+    aff2->SetMatrix(matrix2);
+    aff2->SetOffset(vector2);
     for (i = 0; i < 2; i++) {
         for (j = 0; j < 2; j++)
             matrix2[i][j] = 0.0;
         vector2[i]    = 0.0;
     }
-    matrix2 = aff2.GetMatrix();
-    vector2 = aff2.GetOffset();
+    matrix2 = aff2->GetMatrix();
+    vector2 = aff2->GetOffset();
     std::cout << "Setting the matrix in an existing transform:"
               << std::endl << matrix2;
     std::cout << "Setting the offset in an existing  transform:"
@@ -131,56 +138,56 @@ int main(
     PrintVector( vector2 );
 
     /* Try composition of two transformations */
-    aff2.Compose(aff2);
+    aff2->Compose( aff2 );
     std::cout << "Result of a composition:"
-              << std::endl << aff2;
+              << std::endl << *aff2;
 
     /* Compose with a translation */
     VectorType trans;
     trans[0] = 1;
     trans[1] = 2;
-    aff2.Translate(trans);
+    aff2->Translate(trans);
     std::cout << "Result of a translation:"
-              << std::endl << aff2;
+              << std::endl << *aff2;
 
     /* Compose with an isotropic scaling */
-    aff2.Scale(.3, 1);
+    aff2->Scale(.3, 1);
     std::cout << "Result of isotropic scaling:"
-              << std::endl << aff2;
+              << std::endl << *aff2;
 
     /* Compose with an anisotropic scaling */
     VectorType scale;
     scale[0] = .3;
     scale[1] = .2;
-    aff2.Scale(scale);
+    aff2->Scale(scale);
     std::cout << "Result of anisotropic scaling:"
-              << std::endl << aff2;
+              << std::endl << *aff2;
 
     /* Compose with a general N-D rotation */
-    aff2.Rotate(0, 1, 0.57, 1);
+    aff2->Rotate(0, 1, 0.57, 1);
     std::cout << "Result of general rotation:"
-              << std::endl << aff2;
+              << std::endl << *aff2;
 
     /* Compose with a 2-D rotation */
-    aff2.Rotate(0, 1, -0.57, 1);
+    aff2->Rotate(0, 1, -0.57, 1);
     std::cout << "Result of 2-D rotation:"
-              << std::endl << aff2;
+              << std::endl << *aff2;
 
     /* Compose with a shear */
-    aff2.Shear(1, 0, .2);
+    aff2->Shear(1, 0, .2);
     std::cout << "Result of shear:"
-              << std::endl << aff2;
+              << std::endl << *aff2;
 
     /* Transform a point */
     itk::Point<double, 2> u2, v2;
     u2[0] = 3;
     u2[1] = 5;
-    v2 = aff2.TransformPoint(u2);
+    v2 = aff2->TransformPoint(u2);
     std::cout << "Transform a point:" << std::endl
               << v2[0] << " , " << v2[1] << std::endl;
 
     /* Back transform a point */
-    v2 = aff2.BackTransform(u2);
+    v2 = aff2->BackTransform(u2);
     std::cout << "Back transform a point:" << std::endl
               << v2[0] << " , " << v2[1] << std::endl;
 
@@ -188,12 +195,12 @@ int main(
     vnl_vector_fixed<double, 2> x2, y2;
     x2[0] = 1;
     x2[1] = 2;
-    y2 = aff2.TransformVector(x2);
+    y2 = aff2->TransformVector(x2);
     std::cout << "Transform a vnl_vector:" << std::endl
               << y2[0] << " , " << y2[1] << std::endl;
 
     /* Back transform a vector */
-    y2 = aff2.BackTransform(x2);
+    y2 = aff2->BackTransform(x2);
     std::cout << "Back transform a vnl_vector:" << std::endl
               << y2[0] << " , " << y2[1] << std::endl;
 
@@ -201,12 +208,12 @@ int main(
     itk::Vector<double, 2> u3, v3;
     u3[0] = 3;
     u3[1] = 5;
-    v3 = aff2.TransformVector(u3);
+    v3 = aff2->TransformVector(u3);
     std::cout << "Transform a vector:" << std::endl
               << v3[0] << " , " << v3[1] << std::endl;
 
     /* Back transform a vector */
-    v3 = aff2.BackTransform(u3);
+    v3 = aff2->BackTransform(u3);
     std::cout << "Back transform a vector :" << std::endl
               << v3[0] << " , " << v3[1] << std::endl;
 
@@ -214,30 +221,31 @@ int main(
     itk::Vector<double, 2> u4, v4;
     u4[0] = 3;
     u4[1] = 5;
-    v4 = aff2.TransformVector(u4);
+    v4 = aff2->TransformVector(u4);
     std::cout << "Transform a Covariant vector:" << std::endl
               << v4[0] << " , " << v4[1] << std::endl;
 
     /* Back transform a vector */
-    v4 = aff2.BackTransform(u4);
+    v4 = aff2->BackTransform(u4);
     std::cout << "Back transform a vector :" << std::endl
               << v4[0] << " , " << v4[1] << std::endl;
 
 
 
     /* Create a 3D transform and rotate in 3D */
-    itk::AffineTransform<double,3> aff3;
+    typedef itk::AffineTransform<double,3> Affine3DType;
+    Affine3DType::Pointer aff3 = Affine3DType::New();
     itk::Vector<double,3> axis;
     axis[0] = .707;
     axis[1] = .707;
     axis[2] = .707;
-    aff3.Rotate3D(axis, 1.0, 1);
+    aff3->Rotate3D(axis, 1.0, 1);
     std::cout << "Create and rotate a 3D transform:" << std::endl
-              << aff3;
+              << *aff3;
 
     /* Generate inverse transform */
-    itk::AffineTransform<double,3> inv3;
-    inv3 = aff3.Inverse();
+    Affine3DType::Pointer inv3;
+    inv3 = aff3->Inverse();
     std::cout << "Create an inverse transformation:"
               << std::endl << inv3;
 
@@ -252,16 +260,14 @@ int main(
     image->SetSpacing(spacing);
 
     /* Generate index-to-physical transform */
-    itk::AffineTransform<double, 3>
-        i2p = image->GetIndexToPhysicalTransform();
+    Affine3DType::Pointer i2p = image->GetIndexToPhysicalTransform();
     std::cout << "Index to physical transformation:" << std::endl;
-    std::cout << i2p << std::endl;
+    std::cout << *i2p << std::endl;
 
     /* Generate physical-to-index transform */
-    itk::AffineTransform<double, 3>
-        p2i = image->GetPhysicalToIndexTransform();
+    Affine3DType::Pointer p2i = image->GetPhysicalToIndexTransform();
     std::cout << "Physical to index transformation:" << std::endl;
-    std::cout << p2i << std::endl;
+    std::cout << *p2i << std::endl;
 
     return any;
 }
