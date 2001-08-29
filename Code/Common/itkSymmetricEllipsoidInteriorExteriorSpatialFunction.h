9e54a0daa848da87ecb8cf15e3f278423eabbaa8
Robert Tamburo <rjtst21@pitt.edu>
2001-08-29 11:12:43 -0400

    no message

---
+++ Code/Common/itkSymmetricEllipsoidInteriorExteriorSpatialFunction.h
@@ -0,0 +1,153 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkSymmetricEllipsoidInteriorExteriorSpatialFunction.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 2001 Insight Consortium
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * The name of the Insight Consortium, nor the names of any consortium members,
+   nor of any contributors, may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+  * Modified source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+#ifndef __itkSymmetricEllipsoidInteriorExteriorSpatialFunction_h
+#define __itkSymmetricEllipsoidInteriorExteriorSpatialFunction_h
+
+#include "itkInteriorExteriorSpatialFunction.h"
+
+namespace itk
+{
+
+/**
+ * \class EllipsoidSpatialFunction
+ * \brief Function implementation of an ellipsoid
+ *
+ * Similar to itkEllipsoidInteriorExteriorSpatialFunction in that it 
+ * implements a function that returns 1 for points inside or on the surface
+ * of a ellipsoid and 0 for points outside the ellipsoid. However, this
+ * ellipsoid is defined by a single orientation vector and deals
+ * only with symmetric ellipsoids. An n-dimensional symmetric ellipsoid 
+ * is one which has m axes of equal length and (n - m) unique axes lengths.
+ * Specifically, this class deals with the case where (n - m) = 1 and
+ * the ellipsoid's major axis is oriented along a singles orientation vector.
+ **/
+
+template <class T, unsigned int VImageDimension=3>
+class ITK_EXPORT SymmetricEllipsoidInteriorExteriorSpatialFunction : public InteriorExteriorSpatialFunction<VImageDimension>
+{
+public:
+
+  /**
+   * Standard "Self" typedef.
+   */
+  typedef SymmetricEllipsoidInteriorExteriorSpatialFunction Self;
+
+  /**
+   * Standard "Superclass" typedef.
+   */
+  typedef InteriorExteriorSpatialFunction<VImageDimension> Superclass;
+  
+  /**
+   * Vector typedef.
+   */
+  typedef T VectorType;
+
+  /** 
+   * Smart pointer typedef support.
+   */
+  typedef SmartPointer<Self>  Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;  
+  
+  typedef typename Superclass::TFunctionValueType TFunctionValueType;
+  typedef typename Superclass::TPositionType TPositionType;
+   
+  itkTypeMacro(SymmetricEllipsoidInteriorExteriorSpatialFunction,InteriorExteriorSpatialFunction);
+
+  /**
+   * Method for creation through the object factory.
+   */
+  itkNewMacro(Self);
+
+  /**
+   * Get and set the center of the ellipsoid.
+   */
+  itkGetMacro( Center, TPositionType);
+  itkSetMacro( Center, TPositionType);
+
+  /**
+   * Set the orientation vector of the ellipsoid's unique axis and axes lengths.
+   * Must be normalized!!!!!
+   */
+  void SetOrientation(Vector<VectorType> orientation, VectorType uniqueAxis, VectorType symmetricAxes);
+
+  /**
+   * Evaluates the function at a given position.
+   */
+  TFunctionValueType Evaluate(TPositionType position);
+     
+protected:
+
+  SymmetricEllipsoidInteriorExteriorSpatialFunction();
+  virtual ~SymmetricEllipsoidInteriorExteriorSpatialFunction();
+
+  SymmetricEllipsoidInteriorExteriorSpatialFunction(const SymmetricEllipsoidInteriorExteriorSpatialFunction&) {};
+  void operator=(const SymmetricEllipsoidInteriorExteriorSpatialFunction&) {};
+
+private:
+
+  /**
+   * The center of the ellipsoid.
+   */
+  TPositionType m_Center;
+
+  /**
+   * The unique axis length of the ellipsoid.
+   */
+  VectorType m_UniqueAxis;
+  
+  /**
+   * The symmetric axes lengths of the ellipsoid.
+   */
+  VectorType m_SymmetricAxes;
+
+  /**
+   * The orientation vector of the ellipsoid's unique axis.
+   */  
+  Vector<VectorType, VImageDimension> m_Orientation;
+};
+
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkSymmetricEllipsoidInteriorExteriorSpatialFunction.txx"
+#endif
+
+#endif
---
+++ Code/Common/itkSymmetricEllipsoidInteriorExteriorSpatialFunction.txx
@@ -0,0 +1,107 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkSymmetricEllipsoidInteriorExteriorSpatialFunction.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 2001 Insight Consortium
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * The name of the Insight Consortium, nor the names of any consortium members,
+   nor of any contributors, may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+  * Modified source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+#ifndef __itkSymmetricEllipsoidInteriorExteriorSpatialFunction_cpp
+#define __itkSymmetricEllipsoidInteriorExteriorSpatialFunction_cpp
+
+#include "itkSymmetricEllipsoidInteriorExteriorSpatialFunction.h"
+#include <math.h>
+
+namespace itk
+{
+
+template <class T, unsigned int VImageDimension>
+SymmetricEllipsoidInteriorExteriorSpatialFunction<T, VImageDimension>::SymmetricEllipsoidInteriorExteriorSpatialFunction()
+{
+  m_Center.Fill(0.0); // Origin of ellipsoid
+  m_Orientation.Fill(1.0);  // Orientation of unique axis
+  m_UniqueAxis = 10;  // Length of unique axis
+  m_SymmetricAxes = 5; // Length of symmetric axes
+}
+
+template <class T, unsigned int VImageDimension>
+SymmetricEllipsoidInteriorExteriorSpatialFunction<T, VImageDimension>::~SymmetricEllipsoidInteriorExteriorSpatialFunction()
+{
+
+}
+
+template <class T, unsigned int VImageDimension>
+SymmetricEllipsoidInteriorExteriorSpatialFunction<T, VImageDimension>::TFunctionValueType
+SymmetricEllipsoidInteriorExteriorSpatialFunction<T, VImageDimension>
+::Evaluate(TPositionType position)
+{
+  double uniqueTerm = 0;  // Term in ellipsoid equation for unique axis    
+  double symmetricTerm = 0;  // Term in ellipsoid equation for symmetric axes  
+  Vector<VectorType, VImageDimension> pointVector;
+  Vector<VectorType, VImageDimension> symmetricVector;
+
+  // Project the position onto the major axis, normalize by axis length, 
+  // and determine whether position is inside ellipsoid.
+  for(int i = 0; i < VImageDimension; i++)
+    {
+    pointVector[i] = position[i] - m_Center[i];
+    }
+
+  uniqueTerm = pow(((pointVector * m_Orientation)/(.5*m_UniqueAxis)),2);
+  symmetricVector = pointVector - (m_Orientation * (pointVector * m_Orientation));
+  symmetricTerm = pow(((symmetricVector.GetNorm())/(.5*m_SymmetricAxes)),2);
+
+  if((uniqueTerm + symmetricTerm) >= 0 && (uniqueTerm + symmetricTerm) <= 1)
+    {    
+    return 1; // Inside the ellipsoid.                                                                                                            
+    }
+  else 
+    return 0; // Outside the ellipsoid.
+}
+
+template <class T, unsigned int VImageDimension>
+
+void SymmetricEllipsoidInteriorExteriorSpatialFunction<T, VImageDimension>::SetOrientation(itk::Vector<VectorType> orientation,
+                                                                                           VectorType uniqueAxis,
+                                                                                           VectorType symmetricAxes)
+{
+  m_Orientation = orientation;  // Orientation of unique axis of ellipsoid
+  m_SymmetricAxes = symmetricAxes;  // Length of symmetric axes
+  m_UniqueAxis = uniqueAxis;  // Length of unique axis
+}
+
+} // end namespace itk
+
+#endif
