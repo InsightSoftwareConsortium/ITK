9b450e379ef57457e943757dcbe674b7be343ac9
Ting Chen <chenting@graphics.cis.upenn.edu>
2001-09-17 18:42:06 -0400

    an empty file at present

---
+++ Testing/Code/Algorithms/itkSphereSourceTest.cxx
@@ -0,0 +1,22 @@
+#ifdef _MSC_VER
+#pragma warning ( disable : 4786 )
+#endif
+
+#include <iostream>
+#include <string>
+#include <math.h>
+
+#define   IMGWIDTH            20
+#define   IMGHEIGHT           20
+#define   NFRAMES             1
+#define   NUMBANDS            1  
+#define   NDIMENSION          3
+#define   NUM_CLASSES         3
+#define   MAX_NUM_ITER        20
+
+int main(){
+  
+  return 0;
+
+}
+
