eeccde2691ddcb2c2a957f37c421bd1368edf416
Luis Ibanez <luis.ibanez@kitware.com>
2001-09-18 23:09:25 -0400

    Tests for the CovariantVector class

---
+++ Testing/Code/Common/itkCovariantVectorGeometryTest.cxx
@@ -0,0 +1,125 @@
+/**
+ *  
+ *  This program illustrates the use of Geometric objects
+ *
+ */
+
+
+#include "itkCovariantVector.h"
+#include <vnl/vnl_vector_ref.h>
+#include <iostream>
+
+  // Dimension & Type
+  const     unsigned int    N = 3;
+  typedef   double          ValueType;
+
+  //  Vector type
+  typedef    itk::CovariantVector< ValueType, N >    VectorType;
+
+
+
+//-------------------------
+//
+//   Main code
+//
+//-------------------------
+int main() 
+{
+
+  VectorType va;
+  va[0] = 1.0;
+  va[1] = 2.0;
+  va[2] = 7.0;
+
+  std::cout << "va = { 1.0, 2.0, 7.0 } = ";
+  std::cout << va << std::endl;
+
+  VectorType vb;
+  
+  vb[0] = 1.0;
+  vb[1] = 3.0;
+  vb[2] = 5.0;
+
+  std::cout << "vb = (1,3,5)   = ";
+  std::cout << vb << std::endl;
+
+  VectorType   vc  =  vb - va;
+  std::cout << "vc  =  vb - va  = ";
+  std::cout << vc << std::endl;
+
+  VectorType   vd  =  va * 5.0;
+  std::cout << "vd  =  va * 5.0 = ";
+  std::cout << vd << std::endl;
+
+  VectorType   ve  =  vd / 5.0;
+  std::cout << "ve  =  vd * 5.0 = ";
+  std::cout << ve << std::endl;
+
+  vd += va;
+  std::cout << "vd  +=  va      = ";
+  std::cout << vd << std::endl;
+
+  ve -= vb;
+  std::cout << "ve  -=  vb      = ";
+  std::cout << ve << std::endl;
+
+  VectorType   vh  =  vb;
+  std::cout << "vh   =  vb      = ";
+  std::cout << vh << std::endl;
+
+
+  VectorType   vg( va );
+  std::cout << "vg( va )        = ";
+  std::cout << vg << std::endl;
+
+
+  ValueType norm2 = vg.GetSquaredNorm();
+  std::cout << "vg squared norm = ";
+  std::cout << norm2 << std::endl;
+
+  ValueType norm  = vg.GetNorm();
+  std::cout << "vg norm = ";
+  std::cout << norm << std::endl;
+
+
+  // Test for vnl interface
+
+  // Test the no const version that returns an vnl_vector_ref
+  vnl_vector_ref< ValueType > vnlVector = va.Get_vnl_vector();
+  {
+    std::cout << "vnl_vector_ref = va ";
+    for( unsigned int i=0; i<N; i++ )
+    {
+      std::cout << vnlVector[i] << ", ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "vnl_vector_ref.begin() = va.Begin()";
+    std::cout << std::endl;
+    std::cout << vnlVector.begin() << " = ";
+    std::cout << va.Begin() << std::endl;
+  }
+
+  // Test the const version that returns an vnl_vector
+  const VectorType vf(va);
+  vnl_vector<ValueType> vnlVector2 = vf.Get_vnl_vector();
+  {
+    std::cout << "vnl_vector = va ";
+    for( unsigned int i=0; i<N; i++ )
+    {
+      std::cout << vnlVector2[i] << ", ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "vnl_vector.begin() != vf.Begin()";
+    std::cout << std::endl;
+    std::cout << vnlVector2.begin() << " = ";
+    std::cout << vf.Begin() << std::endl;
+  }
+
+
+  return 0;
+}
+
+
+
