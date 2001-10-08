9e37aa9260303ed7187b96d26e0e8d6c61813f13
Jim Miller <millerjv@crd.ge.com>
2001-10-08 15:54:01 -0400

    First checkin of an example filter that has two outputs.  This used to be the ThresholdImageFilter.

---
+++ Code/BasicFilters/itkTwoOutputExampleImageFilter.h
@@ -0,0 +1,182 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkTwoOutputExampleImageFilter.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 2001 Insight Consortium
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * The name of the Insight Consortium, nor the names of any consortium members,
+   nor of any contributors, may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+  * Modified source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+#ifndef __itkTwoOutputExampleImageFilter_h
+#define __itkTwoOutputExampleImageFilter_h
+
+#include "itkImageToImageFilter.h"
+
+namespace itk
+{
+
+/** \class TwoOutputExampleImageFilter
+ * \brief Example of a filter that produce two outputs. 
+ *
+ * TwoOutputExampleImageFilter sets image values to a user-specified "outside"
+ * value (by default, "black") if the image values are below, above, or
+ * between simple threshold values. The filter can produce two outputs,
+ * one the inverse of the other. (GetOutput() returns an image whose
+ * pixels satisfy the threshold values and are passed to the output 
+ * unchanged (and those that don't are marked with the outside user-value);
+ * GetInverseOutput() returns an image in which pixels satisfying the
+ * threshold are marked "outside", and the other pixel values are passed
+ * through.)
+ *
+ * The pixels must support the operators >= and <=.
+ * 
+ * \ingroup IntensityImageFilters
+ *
+ */
+template <class TImage>
+class ITK_EXPORT TwoOutputExampleImageFilter:public ImageToImageFilter<TImage,TImage>
+{
+public:
+  /**
+   * Standard "Self" typedef.
+   */
+  typedef TwoOutputExampleImageFilter         Self;
+
+  /**
+   * Standard "Superclass" typedef.
+   */
+  typedef ImageToImageFilter<TImage,TImage>  Superclass;
+
+  /** 
+   * Smart pointer typedef support.
+   */
+  typedef SmartPointer<Self>  Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+
+  /**
+   * Method for creation through the object factory.
+   */
+  itkNewMacro(Self);  
+
+  /**
+   * Typedef to describe the type of pixel.
+   */
+  typedef typename TImage::PixelType PixelType;
+  
+  /** 
+   * Run-time type information (and related methods).
+   */
+  itkTypeMacro(TwoOutputExampleImageFilter, ImageToImageFilter);
+
+  /** 
+   * Set the "outside" pixel value. The default value 
+   * NumericTraits<PixelType>::Zero;
+   */
+  itkSetMacro(OutsideValue,PixelType);
+  
+  /** 
+   * Get the "outside" pixel value.
+   */
+  itkGetMacro(OutsideValue,PixelType);
+                 
+  /**
+   * The values greater than or equal to the value are set to OutsideValue
+   */
+  void ThresholdAbove(PixelType &thresh);
+  
+  /**
+   * The values less than or equal to the value are set to OutsideValue
+   */
+  void ThresholdBelow(PixelType &thresh);
+
+  /**
+   * The values outside the range are set to OutsideValue
+   */
+  void ThresholdOutside(PixelType &lower, PixelType &upper);
+
+  /** 
+   * Some typedefs to handle the second output.
+   */
+  typedef TImage OutputImageType;
+  typedef typename OutputImageType::Pointer OutputImagePointer;
+  typedef typename OutputImageType::RegionType OutputImageRegionType;
+  typedef typename OutputImageType::PixelType OutputImagePixelType;
+  
+  /** 
+   * Get the image output of this process object. 
+   */
+  OutputImagePointer GetInverseOutput();
+
+  /** 
+   * Set the image output of this process object. 
+   */
+  void SetInverseOutput(OutputImageType *output)
+    { this->SetNthOutput(1, output); };
+
+protected:
+  TwoOutputExampleImageFilter();
+  ~TwoOutputExampleImageFilter() {};
+  TwoOutputExampleImageFilter(const Self&) {}
+  void operator=(const Self&) {}
+  void PrintSelf(std::ostream& os, Indent indent) const;
+
+  /**
+   * TwoOutputExampleImageFilter can be implemented as a multithreaded filter.
+   * Therefore, this implementation provides a ThreadedGenerateData() routine
+   * which is called for each processing thread. The output image data is
+   * allocated automatically by the superclass prior to calling
+   * ThreadedGenerateData().  ThreadedGenerateData can only write to the
+   * portion of the output image specified by the parameter
+   * "outputRegionForThread"
+   *
+   * \sa ImageToImageFilter::ThreadedGenerateData(),
+   *     ImageToImageFilter::GenerateData() 
+   */
+  void ThreadedGenerateData(const OutputImageRegionType& outputRegionForThread,
+                            int threadId );
+
+private:
+  PixelType m_OutsideValue;
+  PixelType m_Lower;
+  PixelType m_Upper;
+};
+
+  
+} // end namespace itk
+  
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkTwoOutputExampleImageFilter.txx"
+#endif
+  
+#endif
---
+++ Code/BasicFilters/itkTwoOutputExampleImageFilter.txx
@@ -0,0 +1,204 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkTwoOutputExampleImageFilter.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 2001 Insight Consortium
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * The name of the Insight Consortium, nor the names of any consortium members,
+   nor of any contributors, may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+  * Modified source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+#ifndef _itkTwoOutputExampleImageFilter_txx
+#define _itkTwoOutputExampleImageFilter_txx
+
+#include "itkTwoOutputExampleImageFilter.h"
+#include "itkNumericTraits.h"
+#include "itkObjectFactory.h"
+
+namespace itk
+{
+
+/**
+ *
+ */
+template <class TImage>
+TwoOutputExampleImageFilter<TImage>
+::TwoOutputExampleImageFilter()
+{
+  m_OutsideValue = NumericTraits<PixelType>::Zero;
+  m_Lower = NumericTraits<PixelType>::NonpositiveMin();
+  m_Upper = NumericTraits<PixelType>::max();
+
+  typename TImage::Pointer output = TImage::New();
+  this->ProcessObject::SetNumberOfRequiredOutputs(2);
+  this->ProcessObject::SetNthOutput(1, output.GetPointer());
+}
+
+
+/**
+ *
+ */
+template <class TImage>
+void 
+TwoOutputExampleImageFilter<TImage>
+::PrintSelf(std::ostream& os, Indent indent) const
+{
+  Superclass::PrintSelf(os,indent);
+
+  os << indent << "Outside Value: " << m_OutsideValue << std::endl;
+  os << indent << "Lower: " << m_Lower << std::endl;
+  os << indent << "Upper: " << m_Upper << std::endl;
+}
+
+/**
+ * The values greater than or equal to the value are set to OutsideValue
+ */
+template <class TImage>
+void 
+TwoOutputExampleImageFilter<TImage>
+::ThresholdAbove(PixelType &thresh)
+{
+  if (m_Upper != thresh
+      || m_Lower > NumericTraits<PixelType>::NonpositiveMin())
+    {
+    m_Lower = NumericTraits<PixelType>::NonpositiveMin();
+    m_Upper = thresh;
+    this->Modified();
+    }
+}
+
+
+
+/**
+ * The values less than or equal to the value are set to OutsideValue
+ */
+template <class TImage>
+void 
+TwoOutputExampleImageFilter<TImage>
+::ThresholdBelow(PixelType &thresh)
+{
+  if (m_Lower != thresh || m_Upper < NumericTraits<PixelType>::max())
+    {
+    m_Lower = thresh;
+    m_Upper = NumericTraits<PixelType>::max();
+    this->Modified();
+    }
+}
+
+
+/**
+ * The values outside the range are set to OutsideValue
+ */
+template <class TImage>
+void 
+TwoOutputExampleImageFilter<TImage>
+::ThresholdOutside(PixelType &lower, PixelType &upper)
+{
+  if (lower > upper)
+    {
+    itkErrorMacro(<<"Lower threshold cannot be greater than upper threshold.");
+    return;
+    }
+  
+  if (m_Lower != lower || m_Upper != upper)
+    {
+    m_Lower = lower;
+    m_Upper = upper;
+    this->Modified();
+    }
+}
+
+
+/**
+ *
+ */
+template <class TImage>
+void 
+TwoOutputExampleImageFilter<TImage>
+::ThreadedGenerateData(const OutputImageRegionType& outputRegionForThread,
+                       int threadId)
+{
+  unsigned long i;
+  itkDebugMacro(<<"Actually executing");
+
+  // Get the input and output pointers
+  InputImagePointer  inputPtr = this->GetInput();
+  OutputImagePointer outputPtr = this->GetOutput(0);
+  OutputImagePointer outputInversePtr = this->GetOutput(1);
+
+  // Define/declare an iterator that will walk the output region for this
+  // thread.
+  typedef
+    ImageRegionIterator<TImage> InputIterator;
+  typedef
+    ImageRegionIterator<TImage> OutputIterator;
+
+  InputIterator  inIt(inputPtr, outputRegionForThread);
+  OutputIterator outIt(outputPtr, outputRegionForThread);
+  OutputIterator outInverseIt(outputInversePtr, outputRegionForThread);
+
+  // support progress methods/callbacks
+  unsigned long updateVisits = 0;
+  if ( threadId == 0 )
+    {
+    updateVisits = outputRegionForThread.GetNumberOfPixels()/10;
+    if ( updateVisits < 1 ) updateVisits = 1;
+    }
+        
+  // walk the regions, threshold each pixel
+  for (i=0; !outIt.IsAtEnd(); ++inIt, ++outIt, ++outInverseIt, i++ )
+    {
+    if ( threadId == 0 && !(i % updateVisits ) )
+      {
+      this->UpdateProgress((float)i/(float(updateVisits)*10.0));
+      }
+
+    if (m_Lower <= inIt.Get() && inIt.Get() <= m_Upper)
+      {
+      // pixel passes to output unchanged and is replaced by m_OutsideValue in
+      // the inverse output image
+      outIt.Set( inIt.Get() );
+      outInverseIt.Set( m_OutsideValue );
+      }
+    else
+      {
+      outIt.Set( m_OutsideValue );
+      outInverseIt.Set( inIt.Get() );
+      }
+    }
+  
+}
+
+} // end namespace itk
+
+#endif
