1bf93ad8acf52062ec2fe7965735841bc04dc773
Xinwei Xue <xwxue@cs.utah.edu>
2001-12-03 11:42:23 -0500

    ENH

---
+++ Testing/Code/BasicFilters/itkZeroCrossingBasedEdgeDetectionImageFilterTest.cxx
@@ -0,0 +1,78 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkZeroCrossingBasedEdgeDetectionImageFilterTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 2001 Insight Consortium
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * The name of the Insight Consortium, nor the names of any consortium members,
+   nor of any contributors, may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+  * Modified source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+
+#include <iostream>
+#include "itkImage.h"
+#include "itkZeroCrossingBasedEdgeDetectionImageFilter.h"
+#include "itkNullImageToImageFilterDriver.txx"
+#include "itkVector.h"
+
+int main(int argc, char *argv[])
+{
+  try
+    {
+      typedef itk::Image<float, 2> ImageType;
+      
+      // Set up filter
+      itk::ZeroCrossingBasedEdgeDetectionImageFilter<ImageType, ImageType>::Pointer 
+        filter =
+        itk::ZeroCrossingBasedEdgeDetectionImageFilter<ImageType, ImageType>::New();
+      filter->SetVariance(1.0f);
+      filter->SetMaximumError(.01f);
+      
+      // Run Test
+      itk::Size<2> sz;
+      sz[0] = 100 ; //atoi(argv[1]);
+      sz[1] = 100 ; // atoi(argv[2]);
+      //      sz[2] = 10;//atoi(argv[3]);
+      //      sz[3] = 5;//atoi(argv[4]);
+      itk::NullImageToImageFilterDriver< ImageType, ImageType > test1;
+      test1.SetImageSize(sz);
+      test1.SetFilter(filter.GetPointer());
+      test1.Execute();
+    }
+  catch(itk::ExceptionObject &err)
+    {
+      (&err)->Print(std::cerr);
+      return 1;
+    } 
+  return 0;   
+}
