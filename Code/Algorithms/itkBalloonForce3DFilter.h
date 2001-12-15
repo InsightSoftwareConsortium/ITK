157264325152d42ec67c0c7c455093aa6c5c07b8
Ting Chen <chenting@graphics.cis.upenn.edu>
2001-12-14 23:44:27 -0500

    Documentation refined

--- Code/Algorithms/itkBalloonForce3DFilter.h
+++ Code/Algorithms/itkBalloonForce3DFilter.h
@@ -3,8 +3,12 @@
   Program:   Insight Segmentation & Registration Toolkit
   Module:    itkBalloonForce3DFilter.h
   Language:  C++
+<<<<<<< itkBalloonForce3DFilter.h
+  Date:      $Date$
+=======
   Date:      $Date$
   Version:   $Revision$
+>>>>>>> 1.12
 
 Copyright (c) 2001 Insight Consortium
 All rights reserved.
@@ -48,7 +52,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "itkVector.h"
 #include "itkTriangleCell.h"
 #include "itkImage.h"
-#include "itkImageRegionIteratorWithIndex.h"
+#include "itkSimpleImageRegionIterator.h"
+#include <itkCovariantVector.h>
 
 namespace itk
 {
@@ -56,32 +61,58 @@ namespace itk
 /** \class BalloonForce3DFilter
  * \brief 
  *
- * BalloonForce3DFilter is used to apply balloon force and the potential
- * force onto the deformable model.
- * The balloon force is vertical to the surface of the model. The potential 
- * force is given out by the estimated boundary points. These two will meet 
- * a balance at the boundary, thus the deformable model will fit to the 
- * boundary.
- * Users should use deformable model as input using setInput and also provide
- * the filter with a potential image which will provide the estimated bounday.
- * The image should be a binary image with object and background labelled 
- * differently. These images can be given by GibbsPriorFilter or any other
- * segmentation filters.
- * When the nodes on the model stops at the estimated boundary, using the
- * GapSearch method to add new nodes at region the object has a complicate
- * shape.
+ * BalloonForce3DFilter is a simplified form of the 3D deformable model.
+ * There are 2 forces applied on the surface of model. The balloon force
+ * and the gradient force.
+ * (1) The balloon force is a 2D force. In each slice, the balloon force is 
+ *     oriented along the normal direction of the model.
+ * (2) The gradient force is derived from boundary points locations in the 
+ *     original data. At boundary positions, the magnitude of the gradient 
+ *     is large and the gradient force, proportional to the gradient magnitude,
+ *     will attract the model to these boundary locations.
+ * The combination of the two forces will drive the model nodes towards the 
+ * boundary positions and stop them as they reach a boundary position 
+ * (i.e high gradient position) 
+ * The model motion is driven by the following equation::
+ *        d' + Kd = f_{int} + f_{ext}
+ * The model nodes positions are updated using the following iterative scheme:
+ *      d_{n+1} = d_{n} + timestep * d'
+ * Input parameters are:
+ *  (1) An original volume
+ *  (2) A Deformable mesh (it can be derived from ShpereSource) 
+ *  (3) A potential map. This is a binary volume with object and background 
+ *   labelled differently (equivalent to a mask). 
+ *   This volume can be derived from GibbsPriorFilter or any other 
+ *   segmentation method.
+ *
+ * A gradient volume is derived from a GaussainRecursiveFilter applied to 
+ * the original data. This filter first applies a smoothing Gaussian filter 
+ * to the volume and then computes the gradient in 2D for each slice.
+ * 
+ * In order to speed up the deformation process, this method initially 
+ * deforms the model with only the balloon force (BalloonForceFilter method) 
+ * so that the model expands inside the object binary mask. When the nodes on 
+ * the model get close to the object mask boundary, the gradient force is turned on
+ * (GradientFit method) to refine the the model nodes position using the gradient
+ * information in the original volume.
+ * The BalloonForce3DFilter treats the 3D segmentation as series of 2D slices.
  *
  * \ingroup MeshFilters
- * \ingroup MeshSegmentation
- */
+ * \ingroup MeshSegmentation */
 template <class TInputMesh, class TOutputMesh>
-class ITK_EXPORT BalloonForce3DFilter : public MeshToMeshFilter<TInputMesh, TOutputMesh>
+class BalloonForce3DFilter : public MeshToMeshFilter<TInputMesh, TOutputMesh>
 {
 public:
-  /** Standard class typedefs. */
+
+  /** Standard "Self" typedef. */
   typedef BalloonForce3DFilter  Self;
+
+  /** Standard "Superclass" typedef. */
   typedef MeshToMeshFilter<TInputMesh, TOutputMesh> Superclass;
+
+  /** Smart pointer typedef support */
   typedef SmartPointer<Self>  Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
 
   /** Method for creation through the object factory. */
   itkNewMacro(Self);
@@ -90,8 +121,8 @@ public:
   itkTypeMacro(BalloonForce3DFilter,MeshToMeshFilter);
 
   /** Some typedefs. */
-  typedef typename Superclass::OutputMeshType OutputMeshType;
   typedef TInputMesh InputMeshType;
+  typedef TOutputMesh OutputMeshType;
   typedef typename InputMeshType::PointsContainerPointer
       InputPointsContainerPointer;
   typedef typename InputMeshType::PointsContainer::Iterator
@@ -113,60 +144,88 @@ public:
   typedef typename OutputMeshType::PointsContainer::Iterator
       OutputPointsContainerIterator;
 
-  /** Typedefs for image and mesh types. */
-  typedef Image<unsigned short, 3>    ImageType;
-  typedef typename InputMeshType::Pointer InputMeshPointer;
-  typedef typename OutputMeshType::Pointer  OutputMeshPointer;
-  typedef typename ImageType::Pointer   ImagePointer;
-  typedef typename ImageType::IndexType   IndexType;
-  typedef ImageRegionIteratorWithIndex<ImageType> ImageIterator;
+  /** typedefs for cell and point.*/
+  typedef typename InputMeshType::Cell              Cell;
+  typedef typename InputMeshType::CellTraits        CellTraits;
+  typedef typename InputMeshType::PointType         InputPointType;
+  typedef typename InputMeshType::PixelType         PixelType;
+  typedef itk::TriangleCell<PixelType, CellTraits>  TriCell;
 
-  /** Typedefs for vectors. */
-  typedef itk::Vector<float, 3>       FloatVector;
+  /** image and image iterator definition */
+  typedef CovariantVector<PixelType, 3>        GradientType;
+  typedef Image<GradientType, 3>               GradientImageType;
+  typedef Image<unsigned short, 3>             ImageType;
+  typedef typename ImageType::Pointer          ImagePointer;
+  typedef typename ImageType::IndexType        IndexType;
+  typedef SimpleImageRegionIterator<ImageType> ImageIterator;
+  typedef SimpleImageRegionIterator<GradientImageType> GradientIterator;
+  typedef ImageType::SizeType PotentialSizeType;
+  typedef typename GradientImageType::Pointer       GradientImagePointer;
+
+  /** Two typedef for vectors.*/
+  typedef itk::Vector<float, 3>     FloatVector;
   typedef itk::Vector<int, 3>       IntVector;
 
-  /** Typedefs related to cells. */
-  typedef typename InputMeshType::Cell      Cell;
-  typedef typename InputMeshType::CellTraits  CellTraits;
-  typedef itk::TriangleCell<float, CellTraits>  TriCell;
-  typedef typename InputMeshType::PointType   IPT;
-  typedef typename InputMeshType::PixelType   PT;
+  /* Mesh pointer definition */
+  typedef typename InputMeshType::Pointer   InputMeshPointer;
+  typedef typename OutputMeshType::Pointer  OutputMeshPointer;
 
-  /** Some functions. */
+  /** Routines. */
   void ComputeForce();
   void Initialize();
   void SetStiffnessMatrix();
-  void Advance();
-  void SetStiffness(double a, double b);
-  void SetResolution(int a, int b, int c);
-  void SetCenter(int a, int b, int c);
-  void Reset();
-  void ComputeDt();
+  void Advance();     // update data for next iteration
+  void ComputeDt();   // compute point positions
   void ComputeOutput();
-  void SetForces(TInputMesh* force);
-  void SetLocations(TInputMesh* location);
-  void SetNormals(TInputMesh* normals);
-  void SetDisplacements(TInputMesh* displace);
-  void SetDerives(TInputMesh* derive);
-  void SetPotential(ImagePointer potential);
-  void SetGradient(ImagePointer gradient);
-  void NodeAddition(int i, int res, IPT z);
+  void NodeAddition();
   void NodesRearrange();
-  void GapSearch();
-  void GradientFit();
+  void GradientFit(); // fit the model with gradient information
   void ComputeNormals();
-  void ACDSearch();
+  void ACDSearch();   // remove weird structures on the model surface
+  void ComputeShrinkForce();  // force in case we shrink the model
+  void InitialFit();          // locate the model near the objects
 
-  /** Set the output image. */
+  /** Set/Get routines */
   itkSetMacro(ImageOutput, ImagePointer);
   itkGetMacro(ImageOutput, ImagePointer);
 
+  itkSetMacro(Potential, ImagePointer);
+  itkGetMacro(Potential, ImagePointer);
+
+  itkSetMacro(Gradient, GradientImagePointer);
+  itkGetMacro(Gradient, GradientImagePointer);
+
+  itkSetMacro(FirstSlice, int);
+  itkGetMacro(FirstSlice, int);
+
+  itkSetMacro(NeighborRadius, int);
+
+  itkSetMacro(StepThreshold1, int);
+  itkSetMacro(StepThreshold2, int);
+
+  itkSetMacro(XResolution, int);
+  itkSetMacro(YResolution, int);
+  itkSetMacro(ZResolution, int);
+
+  itkSetMacro(StiffnessV, double);
+  itkSetMacro(StiffnessH, double);
+
+  itkSetMacro(TimeStep, double);
+
+  itkSetMacro(Center, IndexType);
+
+  itkGetMacro(Normals, InputMeshPointer);
+
 protected:
   BalloonForce3DFilter();
-  ~BalloonForce3DFilter() {};
-  virtual void GenerateData();
-//  void PrintSelf(std::ostream& os, Indent indent);
+  ~BalloonForce3DFilter() {}
+  BalloonForce3DFilter(const Self&) {}
+  void operator=(const Self&) {}
+  void PrintSelf(std::ostream& os, Indent indent) const;
 
+  virtual void GenerateData();
+
+private:
   /** These meshes are defined to hold the vectors as force, etc. */
   InputMeshPointer  m_Forces; 
   InputMeshPointer  m_Normals;
@@ -182,44 +241,39 @@ protected:
   vnl_matrix_fixed<double, 4, 4> CStiffness;
   vnl_matrix_fixed<double, 4, 4> **K;
   
-  double  m_Stiffness[2];
-  double  TimeStep;
-  int   m_Resolution[3];
+  /** Parameters definition */
+  double    m_StiffnessV, m_StiffnessH;
+  double    m_TimeStep;      /** Time step of each iteration. */
+  int       m_XResolution, m_YResolution, m_ZResolution;
   IndexType m_Center;
-  float   m_MiniT;
-  int   m_Step;
-  int   m_NumNodes;
-  int   m_NumCells;
-  int   m_NumNewNodes;
-  int   *m_GapLocations;
-//  float   m_NewNodes[200][4];
-  float   **m_NewNodes;
-  int   m_NewNodesExisted;
-  int   m_NewNodeLimit;
-  int   m_imgWidth;
-  int   m_imgHeight;
-  int   m_imgDepth;
-  int   m_ModelXUpLimit;
-  int   m_ModelXDownLimit;
-  int   m_ModelYUpLimit;
-  int   m_ModelYDownLimit;
-  int   **m_ACD;
-  int   m_ModelRestart;
+  double    m_MiniT;         /** To stop nodes near a boundary position. */
+  int       m_Step;          /** Number of iterations. */
+  int       m_NumNodes;
+  int       m_NumCells;
+  int       m_NumNewNodes;   /** To add new nodes. */
+  int       *m_GapLocations;
+  int       m_imgWidth;      /** Image size. */
+  int       m_imgHeight;
+  int       m_imgDepth;
+  int       m_ModelXUpLimit; /** Records the size of the model. */
+  int       m_ModelXDownLimit;
+  int       m_ModelYUpLimit;
+  int       m_ModelYDownLimit;
+  int       m_NewNode;
+  int       **m_ACD;         /** To remove unstable structures on the model surface. */
+  int       m_StepThreshold1;/** This threshold decides when to switch from potential fit to gradient fit. */
+  int       m_StepThreshold2;/** This threshold decides when to stop the model. */
+  int       m_FirstSlice;    /** This variable helps relocating the model when trying to load it. */ 
+  int       m_NeighborRadius;/** Defines the spatial range for the gradient fit */
+  unsigned short        m_ObjectLabel;
 
-  ImagePointer    m_Potential;  // for calculate of image force from ptoential
-  ImagePointer    m_Gradient;   // for calculate of image force from gradient
-
-  // for Gibbs Prior Model parameters' recalculation 
-  ImagePointer    m_ImageOutput; 
-  unsigned short  m_ObjectLabel;
-
-  typedef ImageType::SizeType PotentialSizeType;
-
-private:
-  BalloonForce3DFilter(const Self&); //purposely not implemented
-  void operator=(const Self&); //purposely not implemented
+  /** To compute the force derived from the potential data.*/
+  ImagePointer          m_Potential;
+  /** To compute the force derived from the gradient data.*/
+  GradientImagePointer  m_Gradient; 
+  /** Output a binary mask with segmented object. */
+  ImagePointer          m_ImageOutput; 
   
-
 };
 
 } // end namespace itk
