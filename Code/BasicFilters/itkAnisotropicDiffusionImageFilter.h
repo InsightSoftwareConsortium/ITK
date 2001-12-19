a9944c0e5494ecd386f7615b0343baeba1e98650
Josh Cates <cates@cs.utah.edu>
2001-12-19 16:49:54 -0500

    ENH: Added more documentation.

--- Code/BasicFilters/itkAnisotropicDiffusionImageFilter.h
+++ Code/BasicFilters/itkAnisotropicDiffusionImageFilter.h
@@ -48,8 +48,44 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 namespace itk {
 
 /** \class AnisotropicDiffusionImageFilter
- * \ingroup ImageEnhancement
- */
+ *  This filter is the base class for a set of filters that iteratively diffuse
+ *  images by solving non-linear partial differential equations based on the
+ *  classical heat equation.  For an overview of the diffusion algorithm, see
+ *  itkAnisotropicImageFunction.  AnisotropicDiffusionImageFilter is a subclass
+ *  of itkFiniteDifferenceImageFilter and is part of the finite difference
+ *  solver hierarchy.
+ *
+ *  \par Inputs and Outputs
+ *  This is an image-to-image filter.  The requirements for data types and
+ *  dimensionality of the input and output are defined by subclasses.
+ *
+ *  \par Parameters
+ *  Set/GetIterations specifies the number of iterations (time-step updates)
+ *  that the solver will perform to produce a solution image.  The appropriate
+ *  number of iterations is dependent on the application and the image being
+ *  processed.  As a general rule, the more iterations performed, the more
+ *  diffused the image will become.
+ *
+ *  \par
+ *  Set/GetTimeStep sets the time step to be used for each iteration (update).
+ *  This parameter is described in detail in itkAnisotropicImageFunction.
+ *
+ *  \par
+ *  Set/GetConductanceParameter set a common parameter used by subclasses of
+ *  itkAnisotropicDiffusionFunction.   See itkAnisotropicDiffusionFunction for
+ *  detailed information.
+ *
+ *  \par How to use this filter
+ *  AnisotropicDiffusionImageFilter must be subclassed to be used.  This class
+ *  implements a generic framework for other diffusion filters.
+ *
+ *  \sa ScalarAnisotropicDiffusionImageFilter
+ *  \sa VectorAnisotropicDiffusionImageFilter
+ *  \sa GradientAnisotropicDiffusionImageFilter
+ *  \sa VectorGradientAnisotropicDiffusionImageFilter
+ *  \sa CurvatureAnisotropicDiffusionImageFilter
+ *  \sa VectorCurvatureAnisotropicDiffusionImageFilter
+ * \ingroup ImageEnhancement */
 template <class TInputImage, class TOutputImage>
 class AnisotropicDiffusionImageFilter
   : public DenseFiniteDifferenceImageFilter<TInputImage, TOutputImage>
@@ -95,11 +131,21 @@ public:
       conductance equation. */
   itkSetMacro(ConductanceParameter, double);
   itkGetMacro(ConductanceParameter, double);
+
+  /** The following parameters are not used at this time.  Setting them will
+      have no effect on the output */
   itkSetMacro(ConductanceScalingUpdateInterval, unsigned int);
   itkGetMacro(ConductanceScalingUpdateInterval, unsigned int);
   itkSetMacro(ConductanceScalingParameter, double);
   itkGetMacro(ConductanceScalingParameter, double);
 
+  /** Supplies a fixed value for the average gradient magnitude of the image to 
+      the AnisotropicDiffusionFunction at each iteration.  The average gradient 
+      magnitude is normally calculated over the entire image before each
+      iteration and is used as a scaling factor in the calculations of change
+      at a pixel.  This method is  useful in streaming applications to avoid
+      block artifacts by overriding the normal gradient magnitude calculation
+      (i.e. all image chunks are scaled uniformly). */
   void SetFixedAverageGradientMagnitude(double a)
     {
       m_FixedAverageGradientMagnitude= a;
