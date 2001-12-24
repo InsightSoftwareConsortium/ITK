1820430ee5954523b1ae4db509de3e0b57777d86
Bill Lorensen <bill.lorensen@gmail.com>
2001-12-24 08:03:09 -0500

    ERR: abstract class cannot have New.

--- Code/Common/itkSpatialFunction.h
+++ Code/Common/itkSpatialFunction.h
@@ -76,9 +76,6 @@ public:
   typedef SmartPointer<Self>  Pointer;
   typedef SmartPointer<const Self>  ConstPointer;
   
-  /** Method for creation through the object factory. */
-  itkNewMacro(Self);
-
   /** Run-time type information (and related methods). */
   itkTypeMacro(SpatialFunction, FunctionBase);
 
