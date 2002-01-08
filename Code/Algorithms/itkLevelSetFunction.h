3c9e325f81960cf5e894b081d2f80bd09d5aba89
Brad King <brad.king@kitware.com>
2002-01-08 10:16:38 -0500

    ERR: Removed New() method from abstract class.

--- Code/Algorithms/itkLevelSetFunction.h
+++ Code/Algorithms/itkLevelSetFunction.h
@@ -66,9 +66,6 @@ public:
   typedef SmartPointer<Self> Pointer;
   typedef SmartPointer<const Self> ConstPointer;
 
-  /** Method for creation through the object factory. */
-  itkNewMacro(Self);
-
   /** Run-time type information (and related methods) */
   itkTypeMacro( LevelSetFunction,  FiniteDifferenceFunction );
 
