816caae10103f111eccdfb1d8cebe5deb2355b96
Luis Ibanez <luis.ibanez@kitware.com>
2002-01-08 13:42:25 -0500

    FIX: #include for VectorContainer was missing

--- Code/Algorithms/itkRegistrationMapperProcrustes.h
+++ Code/Algorithms/itkRegistrationMapperProcrustes.h
@@ -42,6 +42,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define __itkRegistrationMapperProcrustes_h
 
 #include "itkRegistrationMapper.h"
+#include "itkVectorContainer.h"
 
 namespace itk
 {
