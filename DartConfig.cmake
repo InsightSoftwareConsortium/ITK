06a3b755f03935d0e15f68dd30f2d06eb22d4f52
Jim Miller <millerjv@crd.ge.com>
2002-01-09 10:28:23 -0500

    ENH: Changed urls to www.itk.org

--- DartConfig.cmake
+++ DartConfig.cmake
@@ -3,7 +3,7 @@
 SET (NIGHTLY_START_TIME "1:00:00 EST")
 
 # Dart server to submit results (used by client)
-SET (DROP_SITE "public.kitware.com")
+SET (DROP_SITE "www.itk.org")
 SET (DROP_LOCATION "/incoming")
 SET (DROP_SITE_USER "ftpuser")
 SET (DROP_SITE_PASSWORD "public")
@@ -11,7 +11,7 @@ SET (TRIGGER_SITE
        "http://${DROP_SITE}/cgi-bin/Submit-Insight-TestingResults.pl")
 
 # Dart server configuration 
-SET (CVS_WEB_URL "http://${DROP_SITE}/cgi-bin/itkcvsweb.cgi/Insight/")
+SET (CVS_WEB_URL "http://${DROP_SITE}/cgi-bin/cvsweb.cgi/Insight/")
 SET (CVS_WEB_CVSROOT "Insight")
 
 OPTION(BUILD_DOXYGEN "Build source documentation using doxygen" "On")
