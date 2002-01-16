939136a6158c82290651c3494fb55c9e0fafec09
Daniel Blezek <blezek.daniel@mayo.edu>
2002-01-16 00:51:02 -0500

    ENH: Updated to latest headers

--- Testing/Code/Common/itkCommonHeaderTest.cxx
+++ Testing/Code/Common/itkCommonHeaderTest.cxx
@@ -1,17 +1,16 @@
 /*=========================================================================
 
-  Program:   Insight Segmentation & Registration Toolkit
-  Module:    itkCommonHeaderTest.cxx
+  Program:   Insight Segmentation & Registration Toolkit (ITK)
+  Module:
   Language:  C++
-  Date:      $Date$
-  Version:   $Revision$
+  Date:
+  Version:
 
-  Copyright (c) 2002 Insight Consortium. All rights reserved.
-  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
 
-     This software is distributed WITHOUT ANY WARRANTY; without even 
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
-     PURPOSE.  See the above copyright notices for more information.
+Copyright (c) 2000 National Library of Medicine
+All rights reserved.
+
+See COPYRIGHT.txt for copyright details.
 
 =========================================================================*/
 #include <iostream>
