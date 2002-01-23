bb6c479a9f7ae3f58a03fa2273a5d97580aab10b
Luis Ibanez <luis.ibanez@kitware.com>
2002-01-22 23:24:10 -0500

    ENH: Test added for regions smaller than the LargestPossibleRegion.

--- Testing/Code/Common/itkImageSliceIteratorTest.cxx
+++ Testing/Code/Common/itkImageSliceIteratorTest.cxx
@@ -81,7 +81,7 @@ int main()
     it.NextSlice();
   }
 
-  
+  { 
   // Verification 
   std::cout << "Verifying for iterator...";
   IteratorType ot( myImage, region );
@@ -103,6 +103,8 @@ int main()
             << std::endl;
           std::cerr << "Test failed at index ";
           std::cerr << index << std::endl;
+          std::cerr << "It should be ";
+          std::cerr << ot.Get() << std::endl;
           return EXIT_FAILURE;
         }
         ++ot;
@@ -135,6 +137,8 @@ int main()
             << std::endl;
           std::cerr << "Test failed at index ";
           std::cerr << index << std::endl;
+          std::cerr << "It should be ";
+          std::cerr << cot.Get() << std::endl;
           return EXIT_FAILURE;
         }
         ++cot;
@@ -215,7 +219,160 @@ int main()
   }
   std::cout << "   Done ! " << std::endl;
 
+  }
 
+  { 
+  // Verification 
+  std::cout << "Test in a region < LargestPossibleRegion" << std::endl;
+  std::cout << "Verifying for iterator...";
+
+  size[0]  = 50;
+  size[1]  = 50;
+  size[2]  = 50;
+
+  start[0] = 25;
+  start[1] = 25;
+  start[2] = 25;
+
+  region.SetIndex( start );
+  region.SetSize( size );
+
+  IteratorType ot( myImage, region );
+
+  ot.GoToBegin();
+  ot.SetFirstDirection( 0 ); // 0=x, 1=y, 2=z
+  ot.SetSecondDirection( 1 ); // 0=x, 1=y, 2=z
+ 
+  while( !ot.IsAtEnd() )
+  {
+    while( !ot.IsAtEndOfSlice() )
+    {
+      while( !ot.IsAtEndOfLine() )
+      {
+        index = ot.GetIndex();
+        if( ot.Get() != index )
+        {
+          std::cerr << "Values don't correspond to what was stored "
+            << std::endl;
+          std::cerr << "Test failed at index ";
+          std::cerr << index << std::endl;
+          std::cerr << "It should be ";
+          std::cerr << ot.Get() << std::endl;
+          return EXIT_FAILURE;
+        }
+        ++ot;
+      }
+      ot.NextLine();
+    }
+    ot.NextSlice();
+  }
+  std::cout << "  Done !" << std::endl;
+
+
+  // Verification 
+  std::cout << "Verifying for const iterator...";
+  ConstIteratorType cot( myImage, region );
+
+  cot.GoToBegin();
+  cot.SetFirstDirection( 0 ); // 0=x, 1=y, 2=z
+  cot.SetSecondDirection( 1 ); // 0=x, 1=y, 2=z
+ 
+  while( !cot.IsAtEnd() )
+  {
+    while( !cot.IsAtEndOfSlice() )
+    {
+      while( !cot.IsAtEndOfLine() )
+      {
+        index = cot.GetIndex();
+        if( cot.Get() != index )
+        {
+          std::cerr << "Values don't correspond to what was stored "
+            << std::endl;
+          std::cerr << "Test failed at index ";
+          std::cerr << index << std::endl;
+          std::cerr << "It should be ";
+          std::cerr << cot.Get() << std::endl;
+          return EXIT_FAILURE;
+        }
+        ++cot;
+      }
+      cot.NextLine();
+    }
+    cot.NextSlice();
+  }
+  std::cout << "  Done !" << std::endl;
+
+
+
+
+  // Verification 
+  std::cout << "Verifying iterator in reverse direction... ";
+
+  IteratorType ior( myImage, region );
+
+  ior.GoToEnd();
+  ior.SetFirstDirection( 0 );  // 0=x, 1=y, 2=z
+  ior.SetSecondDirection( 1 ); // 0=x, 1=y, 2=z
+ 
+  while( !ior.IsAtBegin() )
+  {
+    while( !ior.IsAtBeginOfSlice() )
+    {
+      while( !ior.IsAtBeginOfLine() )
+      {
+        index = ior.GetIndex();
+        if( ior.Get() != index )
+        {
+          std::cerr << "Values don't correspond to what was stored "
+            << std::endl;
+          std::cerr << "Test failed at index ";
+          std::cerr << index << " value is " << ior.Get() <<  std::endl;
+          return EXIT_FAILURE;
+        }
+        --ior;
+      }
+      ior.PreviousLine();
+    }
+    ior.PreviousSlice();
+  }
+
+  std::cout << "   Done ! " << std::endl;
+
+
+
+  // Verification 
+  std::cout << "Verifying const iterator in reverse direction... ";
+
+  ConstIteratorType cor( myImage, region );
+
+  cor.GoToEnd();
+  cor.SetFirstDirection( 0 );  // 0=x, 1=y, 2=z
+  cor.SetSecondDirection( 1 ); // 0=x, 1=y, 2=z
+ 
+  while( !cor.IsAtBegin() )
+  {
+    while( !cor.IsAtBeginOfSlice() )
+    {
+      while( !cor.IsAtBeginOfLine() )
+      {
+        index = cor.GetIndex();
+        if( cor.Get() != index )
+        {
+          std::cerr << "Values don't correspond to what was stored "
+            << std::endl;
+          std::cerr << "Test failed at index ";
+          std::cerr << index << " value is " << cor.Get() <<  std::endl;
+          return EXIT_FAILURE;
+        }
+        --cor;
+      }
+      cor.PreviousLine();
+    }
+    cor.PreviousSlice();
+  }
+  std::cout << "   Done ! " << std::endl;
+
+  }
 
 
 
