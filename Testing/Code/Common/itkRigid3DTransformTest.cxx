c33b1c04409425131708c09c94c1831f432ee35d
Luis Ibanez <luis.ibanez@kitware.com>
2002-01-24 16:42:16 -0500

    ENH: Added tests for SetRotationMatrix() and rotation only transformation.

--- Testing/Code/Common/itkRigid3DTransformTest.cxx
+++ Testing/Code/Common/itkRigid3DTransformTest.cxx
@@ -207,6 +207,219 @@ int main(int argc,char *argv[])
   }
 
  
+  /* Create a Rigid 3D transform with a rotation given by a Matrix */
+  {
+    TransformType::Pointer  rotation = TransformType::New();
+    TransformType::MatrixType mrotation;
+   
+    mrotation.SetIdentity();
+
+    // 15 degrees in radians
+    const double angle = 15.0 * atan( 1.0f ) / 45.0; 
+    const double sinth = sin( angle );
+    const double costh = cos( angle );
+
+    // around the positive Z axis 
+    mrotation[0][0] =  costh;
+    mrotation[0][1] =  sinth;
+    mrotation[1][0] = -sinth;
+    mrotation[1][1] =  costh;
+
+    rotation->SetRotationMatrix( mrotation );
+
+    TransformType::OffsetType ioffset;
+    ioffset.Fill( 0.0f );
+
+    rotation->SetOffset( ioffset );
+
+    TransformType::Pointer rotationInverse = rotation->Inverse();
+    std::cout << "rotation: " << rotation;
+    std::cout << "rotationInverse: " << rotationInverse;
+
+    // Verify the Offset content
+    TransformType::OffsetType offset = rotation->GetOffset();
+    std::cout << "pure Rotation test:  " << std::endl;
+    std::cout << "Offset = " << offset << std::endl;
+
+    for(unsigned int i=0; i<N; i++)
+    {
+      if( fabs( offset[i]- ioffset[i] ) > epsilon )
+      {
+        Ok = false;
+        break;    
+      }
+    }
+    if( !Ok )
+    { 
+      std::cerr << "Get Offset  differs from SetOffset value " << std::endl;
+      return EXIT_FAILURE;
+    }
+
+    // Verify the Matrix content
+    TransformType::MatrixType matrix = rotation->GetRotationMatrix();
+    std::cout << "Rotation matrix:  " << std::endl;
+    std::cout << matrix << std::endl;
+
+    for(unsigned int i=0; i<N; i++)
+    {
+      for(unsigned int j=0; j<N; j++)
+      {
+        if( fabs( matrix[i][j]- mrotation[i][j] ) > epsilon )
+        {
+          Ok = false;
+          break;    
+        }
+      }
+    }
+    if( !Ok )
+    { 
+      std::cerr << "Get Rotation Matrix  differs " << std::endl;
+      std::cerr << "from SetRotationMatrix value " << std::endl;
+      return EXIT_FAILURE;
+    }
+
+    {
+      // Rotate an itk::Point
+      TransformType::InputPointType p;
+      p = 10,10,10;
+      TransformType::InputPointType q;
+
+      q[0] =  p[0] * costh + p[1] * sinth;
+      q[1] = -p[0] * sinth + p[1] * costh;
+      q[2] =  p[2];
+
+      TransformType::OutputPointType r;
+      r = rotation->TransformPoint( p );
+      for(unsigned int i=0; i<N; i++)
+      {
+        if( fabs( q[i]- r[i] ) > epsilon )
+        {
+          Ok = false;
+          break;    
+        }
+      }
+      if( !Ok )
+      { 
+        std::cerr << "Error rotating point   : " << p << std::endl;
+        std::cerr << "Result should be       : " << q << std::endl;
+        std::cerr << "Reported Result is     : " << r << std::endl;
+        return EXIT_FAILURE;
+      }
+      else
+      {
+        std::cout << "Ok translating an itk::Point " << std::endl;
+      }
+    }
+
+    {
+      // Rotate an itk::Vector
+      TransformType::InputVectorType p;
+      p = 10,10,10;
+
+      TransformType::InputPointType q;
+      q[0] =  p[0] * costh + p[1] * sinth;
+      q[1] = -p[0] * sinth + p[1] * costh;
+      q[2] =  p[2];
+
+      TransformType::OutputVectorType r;
+      r = rotation->TransformVector( p );
+      for(unsigned int i=0; i<N; i++)
+      {
+        if( fabs( q[i] - r[i] ) > epsilon )
+        {
+          Ok = false;
+          break;    
+        }
+      }
+      if( !Ok )
+      { 
+        std::cerr << "Error rotating vector  : " << p << std::endl;
+        std::cerr << "Result should be       : " << q << std::endl;
+        std::cerr << "Reported Result is     : " << r << std::endl;
+        return EXIT_FAILURE;
+      }
+      else
+      {
+        std::cout << "Ok rotating an itk::Vector " << std::endl;
+      }
+    }
+
+    {
+      // Rotate an itk::CovariantVector
+      TransformType::InputCovariantVectorType p;
+      p = 10,10,10;
+      TransformType::OutputCovariantVectorType q;
+
+      q[0] =  p[0] * costh + p[1] * sinth;
+      q[1] = -p[0] * sinth + p[1] * costh;
+      q[2] =  p[2];
+
+      TransformType::OutputCovariantVectorType r;
+      r = rotation->TransformCovariantVector( p );
+
+      for(unsigned int i=0; i<N; i++)
+      {
+        if( fabs( q[i] - r[i] ) > epsilon )
+        {
+          Ok = false;
+          break;    
+        }
+      }
+      if( !Ok )
+      { 
+        std::cerr << "Error Rotating covariant vector: " << p << std::endl;
+        std::cerr << "Result should be               : " << q << std::endl;
+        std::cerr << "Reported Result is             : " << r << std::endl;
+        return EXIT_FAILURE;
+      }
+      else
+      {
+        std::cout << "Ok translating an itk::CovariantVector " << std::endl;
+      }
+    }
+
+    
+    {
+      // Translate a vnl_vector
+      TransformType::InputVnlVectorType p;
+      p[0] = 11;
+      p[1] =  7;
+      p[2] = 15;
+
+      TransformType::OutputVnlVectorType q;
+
+      q[0] =  p[0] * costh + p[1] * sinth;
+      q[1] = -p[0] * sinth + p[1] * costh;
+      q[2] =  p[2];
+
+
+      TransformType::OutputVnlVectorType r;
+      r = rotation->TransformVector( p );
+      for(unsigned int i=0; i<N; i++)
+      {
+        if( fabs( q[i] - r[i] ) > epsilon )
+        {
+          Ok = false;
+          break;    
+        }
+      }
+      if( !Ok )
+      { 
+        std::cerr << "Error translating vnl_vector : " << p << std::endl;
+        std::cerr << "Result should be             : " << q << std::endl;
+        std::cerr << "Reported Result is           : " << r << std::endl;
+        return EXIT_FAILURE;
+      }
+      else
+      {
+        std::cout << "Ok translating an vnl_Vector " << std::endl;
+      }
+    }
+
+
+
+
+  }
 
  
   return EXIT_SUCCESS;
