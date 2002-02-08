5dac8fc4a885081512dc6876a7cf198917c316c1
Jim Miller <millerjv@crd.ge.com>
2002-02-08 11:20:24 -0500

    Added MeanImageFilter

---
+++ Testing/Code/BasicFilters/itkMeanImageFilterTest.cxx
@@ -0,0 +1,102 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkMeanImageFilterTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include <iostream>
+#include <strstream>
+#include "itkImage.h"
+#include "itkRandomImageSource.h"
+#include "itkMeanImageFilter.h"
+#include "itkOutputWindow.h"
+
+// this class is used to send output to stdout and not the itk window
+class TextOutput : public itk::OutputWindow
+{
+public:
+  typedef itk::SmartPointer<TextOutput> Pointer;
+  itkNewMacro(TextOutput);
+  virtual void DisplayText(const char* s)
+    {
+      std::cout << s << std::endl;
+    }
+};
+
+int main()
+{
+  // Comment the following if you want to use the itk text output window
+  itk::OutputWindow::SetInstance(TextOutput::New());
+
+
+  typedef itk::Image<float,2> FloatImage2DType;
+
+  itk::RandomImageSource<FloatImage2DType>::Pointer random;
+  random = itk::RandomImageSource<FloatImage2DType>::New();
+  random->SetMin(0.0);
+  random->SetMax(1000.0);
+
+  unsigned long randomSize[2];
+  randomSize[0] = randomSize[1] = 8;
+  random->SetSize(randomSize);
+  
+  float spacing[2] = {0.7, 2.1};
+  random->SetSpacing( spacing );
+  float origin[2] = {15, 400};
+  random->SetOrigin( origin );
+    
+  // Create a mean image
+  itk::MeanImageFilter<FloatImage2DType, FloatImage2DType>::Pointer mean;
+  mean = itk::MeanImageFilter<FloatImage2DType,FloatImage2DType>::New();
+  mean->SetInput(random->GetOutput());
+
+  // define the neighborhood size used for the mean filter (5x5)
+  FloatImage2DType::SizeType neighRadius;
+  neighRadius[0] = 1;
+  neighRadius[1] = 1;
+  mean->SetRadius(neighRadius);
+
+  // run the algorithm
+  mean->Update();
+
+  itk::ImageRegionIterator<FloatImage2DType> it;
+  it = itk::ImageRegionIterator<FloatImage2DType>(random->GetOutput(),
+                               random->GetOutput()->GetBufferedRegion());
+  std::cout << "Input image" << std::endl;
+  unsigned int i;
+  for (i=1; !it.IsAtEnd(); ++i, ++it)
+    {
+    std::cout << "\t" << it.Get();
+    if ((i % 8) == 0)
+      {
+      std::cout << std::endl;
+      }
+    }
+
+  std::cout << "Output image" << std::endl;
+  it = itk::ImageRegionIterator<FloatImage2DType>(mean->GetOutput(),
+                               mean->GetOutput()->GetBufferedRegion());
+  for (i=1; !it.IsAtEnd(); ++i, ++it)
+    {
+    std::cout << "\t" << it.Get();
+    if ((i % 8) == 0)
+      {
+      std::cout << std::endl;
+      }
+    }
+  
+  return EXIT_SUCCESS;
+}
+
+
+
