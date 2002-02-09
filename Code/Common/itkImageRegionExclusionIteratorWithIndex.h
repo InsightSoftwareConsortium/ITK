a85c78469e80938aef18c6e566e97dcb85398ee3
Luis Ibanez <luis.ibanez@kitware.com>
2002-02-08 20:40:46 -0500

    Iterator for walking a region of an image by excluding a second region      completly contained inside the first.

---
+++ Code/Common/itkImageRegionExclusionIteratorWithIndex.h
@@ -0,0 +1,187 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkImageRegionExclusionIteratorWithIndex.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkImageRegionExclusionIteratorWithIndex_h
+#define __itkImageRegionExclusionIteratorWithIndex_h
+
+#include "itkImageIteratorWithIndex.h"
+
+namespace itk
+{
+
+/** \class ImageRegionExclusionIteratorWithIndex
+ * \brief Multi-dimensional image iterator which walks a region 
+ * excluding a second region completly contained in the first.
+ * 
+ * ImageRegionExclusionIteratorWithIndex is a templated class to represent a
+ * multi-dimensional iterator. ImageRegionExclusionIteratorWithIndex is templated
+ * over the image type.  ImageRegionExclusionIteratorWithIndex is constrained to
+ * walk only within the specified region. The exclusion regions has to be set
+ * after construction. By default the exclusion region is empty and the 
+ * iterator will behave as the itk::ImageRegionIteratorWithIndex with a penalty
+ * in performace due to internal extra checking.
+ *
+ * ImageRegionExclusionIteratorWithIndex is a multi-dimensional iterator,
+ * requiring more information be specified before the iterator can be
+ * used than conventional iterators. Whereas the std::vector::iterator
+ * from the STL only needs to be passed a pointer to establish the
+ * iterator, the multi-dimensional image iterator needs a pointer, the
+ * size of the buffer, the size of the region, the start index of the
+ * buffer, and the start index of the region. To gain access to this
+ * information, ImageRegionExclusionIteratorWithIndex holds a reference to the
+ * image over which it is traversing.
+ *
+ * ImageRegionExclusionIteratorWithIndex assumes a particular layout of the image data. The
+ * is arranged in a 1D array as if it were [][][][slice][row][col] with
+ * Index[0] = col, Index[1] = row, Index[2] = slice, etc.
+ *
+ * operator++ provides a simple syntax for walking around a region of
+ * a multidimensional image. operator++ iterates across a row, constraining
+ * the movement to within a region of image. When the iterator reaches
+ * the boundary of the region along a row, the iterator automatically
+ * wraps to the next row, starting at the first pixel in the row that is
+ * part of the region. This allows for simple processing loops of the form:
+ *
+ * \example itkImageRegionExclusionIteratorWithIndex
+ *
+ * \code
+ * 
+ *  IteratorType it( image, image->GetRequestedRegion() );
+ *
+ *  it.SetExclusionRegion( exclusionRegion );
+ *
+ *  it.GoToBegin();
+ *
+ *  while( ! it.IsAtEnd() ) 
+ *  {  
+ *    it.Set( 100.0 + it.Get() );
+ *    ++it;
+ *  }
+ *
+ * \endcode
+ *
+ *  It also can be used for walking in the reverse direction like
+ *
+ * \code
+ * 
+ *  IteratorType it( image, image->GetRequestedRegion() );
+ *
+ *  it.SetExclusionRegion( exclusionRegion );
+ *  it.GoToEnd();
+ *
+ *  while( !it.IsAtBegin() ) 
+ *  {  
+ *    it.Set( 100.0 );
+ *    --it;
+ *  }
+ *
+ * \endcode
+ *
+ * \ingroup ImageIterators
+ */
+template<typename TImage>
+class ImageRegionExclusionIteratorWithIndex : public ImageIteratorWithIndex<TImage>
+{
+public:
+  /** Standard class typedefs. */
+  typedef ImageRegionExclusionIteratorWithIndex Self;
+  typedef ImageIteratorWithIndex<TImage>  Superclass;
+  
+  /** Index typedef support. While this was already typdef'ed in the superclass
+   * it needs to be redone here for this subclass to compile properly with gcc.
+   * Note that we have to rescope Index back to itk::Index to that is it not
+   * confused with ImageIterator::Index. */
+  typedef typename TImage::IndexType  IndexType;
+
+  /** Image typedef support. While this was already typdef'ed in the superclass
+   * it needs to be redone here for this subclass to compile properly with gcc.
+   * Note that we have to rescope Image back to itk::Image to that is it not
+   * confused with ImageIterator::Image. */
+  typedef TImage ImageType;
+
+  /** PixelContainer typedef support. Used to refer to the container for
+   * the pixel data. While this was already typdef'ed in the superclass
+   * it needs to be redone here for this subclass to compile properly with gcc. */
+  typedef typename TImage::PixelContainer PixelContainer;
+  typedef typename PixelContainer::Pointer PixelContainerPointer;
+  
+  /** Region typedef support. While this was already typdef'ed in the superclass
+   * it needs to be redone here for this subclass to compile properly with gcc.
+   * Note that we have to rescope Region back to itk::ImageRegion so that is
+   * it not confused with ImageIterator::Index. */
+  typedef typename TImage::RegionType RegionType;
+
+  /** Default constructor. Needed since we provide a cast constructor. */
+  ImageRegionExclusionIteratorWithIndex() : ImageIteratorWithIndex<TImage>() {}
+  
+  /** Constructor establishes an iterator to walk a particular image and a
+   * particular region of that image. */
+  ImageRegionExclusionIteratorWithIndex(TImage *ptr,
+                            const RegionType& region)
+    : ImageIteratorWithIndex<TImage>(ptr, region) {}
+
+  /** Constructor that can be used to cast from an ImageIterator to an
+   * ImageRegionExclusionIteratorWithIndex. Many routines return an ImageIterator but for a
+   * particular task, you may want an ImageRegionExclusionIteratorWithIndex.  Rather than
+   * provide overloaded APIs that return different types of Iterators, itk
+   * returns ImageIterators and uses constructors to cast from an
+   * ImageIterator to a ImageRegionExclusionIteratorWithIndex. */
+  ImageRegionExclusionIteratorWithIndex( const ImageIteratorWithIndex<TImage> &it)
+    { this->ImageIteratorWithIndex<TImage>::operator=(it); }
+
+  /** Increment (prefix) the fastest moving dimension of the iterator's index.
+   * This operator will constrain the iterator within the region (i.e. the
+   * iterator will automatically wrap from the end of the row of the region
+   * to the beginning of the next row of the region) up until the iterator
+   * tries to moves past the last pixel of the region.  Here, the iterator
+   * will be set to be one pixel past the end of the region.
+   * \sa operator++(int) */
+  Self & operator++();
+
+  /** Decrement (prefix) the fastest moving dimension of the iterator's index.
+   * This operator will constrain the iterator within the region (i.e. the
+   * iterator will automatically wrap from the beginning of the row of the region
+   * to the end of the next row of the region) up until the iterator
+   * tries to moves past the first pixel of the region.  Here, the iterator
+   * will be set to be one pixel past the beginning of the region.
+   * \sa operator--(int) */
+  Self & operator--();
+
+
+  /** Method to define the Exclusion region. The iterator will skip pixels 
+   * inside this region. 
+   * \warning The exclusion region must be completly containe inside the
+   * normal region used to construct the iterator. A border of at least on
+   * pixels should exist between the normal region and the exclusion region.
+   */
+  void SetExclusionRegion( const RegionType & region );
+
+private:
+
+  RegionType      m_ExclusionRegion;
+
+  IndexType       m_ExclusionBegin;
+  IndexType       m_ExclusionEnd;
+
+};
+
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkImageRegionExclusionIteratorWithIndex.txx"
+#endif
+
+#endif 
---
+++ Code/Common/itkImageRegionExclusionIteratorWithIndex.txx
@@ -0,0 +1,141 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkImageRegionExclusionIteratorWithIndex.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef _itkImageRegionExclusionIteratorWithIndex_txx
+#define _itkImageRegionExclusionIteratorWithIndex_txx
+
+#include "itkImageRegionExclusionIteratorWithIndex.h"
+
+namespace itk
+{
+  
+
+   
+//----------------------------------------------------------------------
+//  Set the region to exclude from the walk
+//----------------------------------------------------------------------
+template<class TImage>
+void
+ImageRegionExclusionIteratorWithIndex<TImage>
+::SetExclusionRegion( const RegionType & region )
+{
+  
+  m_ExclusionRegion      = region;
+  m_ExclusionBegin       = m_ExclusionRegion.GetIndex();
+  SizeType exclusionSize = m_ExclusionRegion.GetSize();
+  
+  for(unsigned int i=0; i<TImage::ImageDimension; ++i)
+    {
+    m_ExclusionEnd[i] = m_ExclusionBegin[i] + exclusionSize[i];
+    }
+
+}
+
+ 
+
+//----------------------------------------------------------------------
+//  Advance along the line
+//----------------------------------------------------------------------
+template<class TImage>
+ImageRegionExclusionIteratorWithIndex<TImage> &
+ImageRegionExclusionIteratorWithIndex<TImage>
+::operator++()
+{
+  
+  m_Remaining = false;
+  for( unsigned int in=0; in<TImage::ImageDimension; in++ )
+    {
+    m_PositionIndex[ in  ]++;
+    
+    // if entering the exclusion region... jump over it
+    if( m_PositionIndex[ in ] == m_ExclusionBegin[in] )
+      {
+      m_PositionIndex[ in ]  = m_ExclusionEnd[ in ]+1;
+      m_Position            += m_OffsetTable[in] * 
+                               ( m_ExclusionRegion.GetSize()[ in ] + 1 );
+      }
+
+    if( m_PositionIndex[ in ] < m_EndIndex[ in ] )
+      {
+      m_Position += m_OffsetTable[in];
+      m_Remaining = true;
+      break;
+      }
+    else 
+      {
+      m_Position -= m_OffsetTable[ in ] * ( static_cast<long>(m_Region.GetSize()[in])-1 );
+      m_PositionIndex[ in ] = m_BeginIndex[ in ]; 
+      }
+    }
+
+  if( !m_Remaining ) // It will not advance here otherwise
+    {
+    m_Position = m_End;
+    }
+
+  return *this;
+}
+
+
+//----------------------------------------------------------------------
+//  Advance along the line in reverse direction
+//----------------------------------------------------------------------
+template<class TImage>
+ImageRegionExclusionIteratorWithIndex<TImage> &
+ImageRegionExclusionIteratorWithIndex<TImage>
+::operator--()
+{
+  
+  m_Remaining = false;
+  for( unsigned int in=0; in<TImage::ImageDimension; in++ )
+    {
+      
+      if( m_PositionIndex[ in ] > m_BeginIndex[ in ] )
+        {
+
+        m_PositionIndex[ in  ]--;
+        m_Position -= m_OffsetTable[in];
+
+        // if entering the exclusion region... jump over it
+        if( m_PositionIndex[ in ] == m_ExclusionEnd[in] )
+          {
+          m_PositionIndex[ in ]  = m_ExclusionBegin[ in ]-1;
+          m_Position            -= m_OffsetTable[in] * m_ExclusionRegion.GetSize()[ in ];
+          }
+
+        m_Remaining = true;
+        break;
+        }
+      else 
+        {
+        m_PositionIndex[ in  ]--;
+        m_Position += m_OffsetTable[ in ] * ( static_cast<long>(m_Region.GetSize()[in])-1 );
+        m_PositionIndex[ in ] = m_EndIndex[ in ] - 1; 
+        }
+
+    }
+
+  if( !m_Remaining ) // It will not advance here otherwise
+    {
+    m_Position = m_End;
+    }
+
+  return *this;
+}
+
+} // end namespace itk
+
+#endif
