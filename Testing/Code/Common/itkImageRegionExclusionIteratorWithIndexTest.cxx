25e6afb3789e1674bf3c80f8d8374c54db96da6b
Luis Ibanez <luis.ibanez@kitware.com>
2002-02-08 20:43:00 -0500

    Test for the ImageRegionIterator capable of excluding and internal region.

---
+++ Testing/Code/Common/itkImageRegionExclusionIteratorWithIndexTest.cxx
@@ -0,0 +1,159 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkImageRegionExclusionIteratorWithIndexTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include <iostream>
+
+#include "itkImage.h"
+#include "itkImageRegionIteratorWithIndex.h"
+#include "itkImageRegionExclusionIteratorWithIndex.h"
+//#include "itkImageRegionExclusionConstIteratorWithIndex.h"
+
+
+
+
+int main()
+{
+
+  const unsigned int ImageDimension = 3;
+
+  typedef itk::Index< ImageDimension >             IndexPixelType;
+  typedef unsigned char                            ValuePixelType;
+
+  typedef itk::Image< IndexPixelType, ImageDimension >  IndexImageType;
+  typedef itk::Image< ValuePixelType, ImageDimension >  ValueImageType;
+
+  IndexImageType::Pointer myIndexImage = IndexImageType::New();
+  
+  IndexImageType::SizeType size;
+
+  size[0] = 100;
+  size[1] = 100;
+  size[2] = 100;
+
+  IndexImageType::IndexType start;
+  start = IndexImageType::IndexType::ZeroIndex;
+
+  IndexImageType::RegionType region;
+  region.SetIndex( start );
+  region.SetSize( size );
+
+  myIndexImage->SetLargestPossibleRegion( region );
+  myIndexImage->SetBufferedRegion( region );
+  myIndexImage->SetRequestedRegion( region );
+  myIndexImage->Allocate();
+
+  ValueImageType::Pointer  myValueImage  = ValueImageType::New();
+
+  myValueImage->SetLargestPossibleRegion( region );
+  myValueImage->SetBufferedRegion( region );
+  myValueImage->SetRequestedRegion( region );
+  myValueImage->Allocate();
+
+  typedef itk::ImageRegionIteratorWithIndex< ValueImageType >     ValueIteratorType;
+  typedef itk::ImageRegionIteratorWithIndex< IndexImageType >     IndexIteratorType;
+
+  const unsigned char normalRegionValue    = 100;
+  const unsigned char exclusionRegionValue = 200;
+
+  std::cout << "Initializing an image of indices and an image of values" << std::endl;
+
+  // Initialize the Image
+  IndexIteratorType ii( myIndexImage, region );
+  ValueIteratorType iv( myValueImage, region );
+
+  ii.GoToBegin();
+  iv.GoToBegin();
+
+  while( !ii.IsAtEnd() )
+    {
+    ii.Set( ii.GetIndex() );
+    iv.Set( normalRegionValue );  
+    ++ii;
+    ++iv;
+    }
+  
+  IndexImageType::RegionType exclusionRegion;
+ 
+  IndexImageType::SizeType exclusionSize;
+
+  exclusionSize[0] = 60;
+  exclusionSize[1] = 60;
+  exclusionSize[2] = 60;
+
+  IndexImageType::IndexType exclusionStart;
+
+  exclusionStart[0] = 15;
+  exclusionStart[1] = 15;
+  exclusionStart[2] = 15;
+  
+  exclusionRegion.SetIndex( exclusionStart );
+  exclusionRegion.SetSize(  exclusionSize  );
+  
+  std::cout << "Initializing the exclusion region on the image of values " << std::endl;
+  // Set a different value inside the exclusion region
+  ValueIteratorType ive( myValueImage, exclusionRegion );
+
+  ive.GoToBegin();
+  while( !ive.IsAtEnd() )
+    {
+    ive.Set( exclusionRegionValue );
+    ++ive;
+    }
+
+
+  std::cout << "Starting walk with the exclusion iterator " << std::endl;
+  typedef itk::ImageRegionExclusionIteratorWithIndex< IndexImageType > ExclusionIndexIteratorType;
+  typedef itk::ImageRegionExclusionIteratorWithIndex< ValueImageType > ExclusionValueIteratorType;
+
+  ExclusionValueIteratorType ev( myValueImage, region );
+  ExclusionIndexIteratorType ei( myIndexImage, region );
+
+  ev.SetExclusionRegion( exclusionRegion );
+  ei.SetExclusionRegion( exclusionRegion );
+  
+  ev.GoToBegin();
+  ei.GoToBegin();
+  while( ev.IsAtEnd() )
+    {
+
+    if( ei.Get() != ei.GetIndex() )
+      {
+      std::cout << "Error in exclusion index " << std::endl;
+      std::cout << "It should be at " << ei.GetIndex();
+      std::cout << " but it is at   " << ei.Get() << std::endl;
+      return EXIT_FAILURE;
+      }
+
+    if( ev.Get() != normalRegionValue )
+      {
+      std::cout << "Error in exclusion index " << std::endl;
+      std::cout << "It is stepping into the exclusion region " << std::endl;
+      std::cout << "Entry point = " << ev.GetIndex() << std::endl;
+      return EXIT_FAILURE;
+      }
+    
+    ++ei;
+    ++ev;
+    }
+
+  std::cout << "Test PASSED ! " << std::endl;
+
+  return EXIT_SUCCESS;
+
+  }
+
+
+
