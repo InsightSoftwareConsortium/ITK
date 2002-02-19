9aee796d7836f11d8d4dc796fdf9528036312ecb
Luis Ibanez <luis.ibanez@kitware.com>
2002-02-19 17:39:49 -0500

    ENH: Set() methods exercised. ArrayType used to declare external parameters.

--- Testing/Code/BasicFilters/itkGaussianImageSourceTest.cxx
+++ Testing/Code/BasicFilters/itkGaussianImageSourceTest.cxx
@@ -24,17 +24,41 @@ int main()
   const unsigned int dim = 3;
 
   // Image typedef
-  typedef itk::Image< unsigned char, dim > TImageType;
+  typedef itk::Image< unsigned char, dim > ImageType;
 
   // Create a gaussian image source
-  typedef itk::GaussianImageSource< TImageType > TGaussianSource;
-  TGaussianSource::Pointer pSource = TGaussianSource::New();
+  typedef itk::GaussianImageSource< ImageType > GaussianSourceType;
+  GaussianSourceType::Pointer pSource = GaussianSourceType::New();
+
+  float spacing[]       = { 1.2f, 1.3f, 1.4f };
+  float origin[]        = { 1.0f, 4.0f, 2.0f };
+  unsigned long size[]  = {  130,  150,  120 };
+
+  GaussianSourceType::ArrayType mean;
+  mean[0] = size[0]/2.0f + origin[0];
+  mean[1] = size[1]/2.0f + origin[1];
+  mean[2] = size[2]/2.0f + origin[2];
+  
+  GaussianSourceType::ArrayType sigma;
+  sigma[0] = 25.0f;
+  sigma[1] = 35.0f;
+  sigma[2] = 55.0f;
+  
+  pSource->SetSize( size );
+  pSource->SetOrigin( origin );
+  pSource->SetSpacing( spacing );
+  pSource->SetMean( mean );
+  pSource->SetSigma( sigma );
 
   // Get the output of the source
-  TImageType::Pointer pImage = pSource->GetOutput();
+  ImageType::Pointer pImage = pSource->GetOutput();
   
   // Run the pipeline
   pSource->Update();
 
+  std::cout << pImage << std::endl;
+
   return EXIT_SUCCESS;
 }
+
+
