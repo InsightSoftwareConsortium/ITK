249c0432bd9c694f391c1be33a69a063b80595db
Bill Lorensen <bill.lorensen@gmail.com>
2002-02-21 13:37:40 -0500

    ENH: Filter to change origin, spacing and start index. Ref GNATS PR 163.

---
+++ Code/BasicFilters/itkChangeInformationImageFilter.h
@@ -0,0 +1,218 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkChangeInformationImageFilter.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkChangeInformationImageFilter_h
+#define __itkChangeInformationImageFilter_h
+
+#include "itkImageToImageFilter.h"
+#include "itkFixedArray.h"
+
+namespace itk
+{
+
+/** \class ChangeInformationImageFilter
+ * \brief Change the origin, spacing and/or region of an Image.
+ *
+ * Change the origin, spacing and/or buffered region of an itkImage. This
+ * "Information" along with an Image's container comprise the
+ * itkImage. By default, the output's information is set to the input's
+ * information. The methods ChangeSpacingOn/Off, ChangeOriginOn/Off and
+ * ChangeRegionOn/Off control whether the default origin, spacing or
+ * buffered region should be changed. If On, the associated information
+ * will be replaced with either the ReferenceImage information (if
+ * UseReferenceImage is true) or the ivars OutputSpacing, OutputOrigin,
+ * OutputOffset.
+ * 
+ * In addition, the method CenterImageOn will recompute the output image
+ * origin (using the selected output spacing) the align the center of the
+ * image with the coordinate 0.
+ * 
+ * \ingroup GeometricTransforms
+ *
+ */
+template <class TInputImage>
+class ITK_EXPORT ChangeInformationImageFilter:
+    public ImageToImageFilter<TInputImage,TInputImage>
+{
+public:
+  /** Standard class typedefs. */
+  typedef ChangeInformationImageFilter         Self;
+  typedef ImageToImageFilter<TInputImage,TInputImage>  Superclass;
+  typedef SmartPointer<Self>  Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);  
+
+  /** Typedef to describe the output and input image region types. */
+  typedef typename TInputImage::RegionType OutputImageRegionType;
+  typedef typename TInputImage::RegionType InputImageRegionType;
+
+  /** Typedef to describe the pointer to the input. */  
+  typedef typename TInputImage::Pointer InputImagePointer;
+
+  /** Typedef to describe the type of pixel. */
+  typedef typename TInputImage::PixelType OutputImagePixelType;
+  typedef typename TInputImage::PixelType InputImagePixelType;
+
+  /** Typedef to describe the output and input image index and size types. */
+  typedef typename TInputImage::IndexType OutputImageIndexType;
+  typedef typename TInputImage::IndexType InputImageIndexType;
+  typedef typename TInputImage::SizeType OutputImageSizeType;
+  typedef typename TInputImage::SizeType InputImageSizeType;
+  typedef typename TInputImage::OffsetType OutputImageOffsetType;
+  typedef typename TInputImage::OffsetType InputImageOffsetType;
+
+  /** Typedef of double containers */
+  typedef FixedArray<double, ImageDimension> ArrayType;
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(ChangeInformationImageFilter, ImageToImageFilter);
+
+  /** ImageDimension enumeration. */
+  enum { ImageDimension = TInputImage::ImageDimension };
+
+  /** Copy the information from another Image.  By default,
+   *  the information is copied from the input image. */
+  void SetReferenceImage (InputImageType *image)
+    {
+    if (image != m_ReferenceImage)
+      {
+      m_ReferenceImage = image;
+      this->ProcessObject::SetNthInput(1, image);
+      this->Modified();
+      }
+    }
+  itkGetObjectMacro(ReferenceImage, TInputImage);
+
+  itkSetMacro(UseReferenceImage, bool);
+  itkBooleanMacro(UseReferenceImage);
+  itkGetMacro(UseReferenceImage, bool);
+
+  /** Specify a new data spacing explicitly.  The default is to
+   * use the spacing of the Input, or of the ReferenceImage
+   * if UseReferenceImage is set. */
+  itkSetMacro(OutputSpacing, ArrayType);
+  itkGetMacro(OutputSpacing, const ArrayType);
+
+  /** Specify a new data origin explicitly.  The default is to
+   *  use the origin of the Input, or of the ReferenceImage
+   *  if UseReferenceImage is true. */
+  itkSetMacro(OutputOrigin, ArrayType);
+  itkGetMacro(OutputOrigin, const ArrayType);
+
+  /** Specify an offset for the buffered region. The default is to
+   *  use the same buffered region as the input or an Offset computed from
+   *  the ReferenceImage's buffered region (if UseReferenceImage is true.)
+   *  NOTE: Changing the buffered region should not be done without a
+   *  corresponding change in the requested region. Of course, the pipeline
+   *  controls the requested region. Therefore, changing the buffered region
+   *  may mean the filter cannot produce the requested region.
+   */
+  itkSetVectorMacro(OutputOffset, long, ImageDimension);
+  itkGetVectorMacro(OutputOffset, const long, ImageDimension);
+
+  /** Change the origin, spacing and region of the output image. */
+  bool ChangeAll()
+    {
+    this->ChangeSpacingOn();
+    this->ChangeOriginOn();
+    this->ChangeRegionOn();
+    }
+
+  /** Change neither the origin nor spacing nor region of the output image. */
+
+  bool ChangeNone()
+    {
+    this->ChangeSpacingOff();
+    this->ChangeOriginOff();
+    this->ChangeRegionOff();
+    }
+
+  /** Change the Spacing of the output image. If false, the output
+   *  image spacing will be set to the input image spacing. If true, the
+   *  output image spacing will be set to:
+   *      the ReferenceImage spacing (if UseReferenceImage is true) or
+   *      OutputSpacing. */
+
+  itkSetMacro(ChangeSpacing, bool);
+  itkBooleanMacro(ChangeSpacing);
+  itkGetMacro(ChangeSpacing, bool);
+
+  /** Change the Origin of the output image. If false, the output
+   *  image origin will be set to the input image origin. If true, the
+   *  output image origin will be set to:
+   *      the ReferenceImage origin (if UseReferenceImage is true) or
+   *      OutputOrigin. */
+
+  itkSetMacro(ChangeOrigin, bool);
+  itkBooleanMacro(ChangeOrigin);
+  itkGetMacro(ChangeOrigin, bool);
+
+  /** Change the BufferedRegion of the output image. */
+
+  itkSetMacro(ChangeRegion, bool);
+  itkBooleanMacro(ChangeRegion);
+  itkGetMacro(ChangeRegion, bool);
+
+  /** Set the Origin of the output so that image coordinate (0,0,0)
+   * lies at the Center of the Image.  This will override 
+   * SetOutputOrigin. */
+  itkSetMacro(CenterImage, bool);
+  itkBooleanMacro(CenterImage);
+  itkGetMacro(CenterImage, bool);
+
+  /** Apply changes to the output image information. */
+  virtual void GenerateOutputInformation();
+
+  /** Apply changes to the input image requested region. */
+  virtual void GenerateInputRequestedRegion();
+
+  /** Copy the input buffer. */
+  void GenerateData();
+
+ protected:
+  ChangeInformationImageFilter();
+  ~ChangeInformationImageFilter() {};
+  void PrintSelf(std::ostream& os, Indent indent) const;
+
+private:
+  ChangeInformationImageFilter(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+  InputImagePointer m_ReferenceImage;
+
+  bool m_CenterImage;
+  bool m_ChangeSpacing;
+  bool m_ChangeOrigin;
+  bool m_ChangeRegion;
+  bool m_UseReferenceImage;
+  
+  ArrayType m_OutputSpacing;
+  ArrayType m_OutputOrigin;
+
+  long m_OutputOffset[ImageDimension];
+  OutputImageOffsetType m_Shift;
+};
+
+  
+} // end namespace itk
+  
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkChangeInformationImageFilter.txx"
+#endif
+  
+#endif
---
+++ Code/BasicFilters/itkChangeInformationImageFilter.txx
@@ -0,0 +1,233 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkChangeInformationImageFilter.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef _itkChangeInformationImageFilter_txx
+#define _itkChangeInformationImageFilter_txx
+
+#include "itkChangeInformationImageFilter.h"
+#include "itkImageRegionIterator.h"
+#include "itkObjectFactory.h"
+
+namespace itk
+{
+
+/**
+ *
+ */
+template <class TInputImage>
+ChangeInformationImageFilter<TInputImage>
+::ChangeInformationImageFilter()
+{
+  m_ReferenceImage = 0;
+
+  m_ChangeSpacing = false;
+  m_ChangeOrigin = false;
+  m_ChangeRegion = false;
+
+  m_CenterImage = false;
+  m_UseReferenceImage = false;
+  
+  m_OutputSpacing.Fill(1.0);
+  m_OutputOrigin.Fill(0.0);
+}
+
+template <class TInputImage>
+void 
+ChangeInformationImageFilter<TInputImage>
+::GenerateOutputInformation()
+{
+  Superclass::GenerateOutputInformation();
+
+  unsigned int i;
+
+  typename TInputImage::RegionType outputRegion;
+  typename TInputImage::SizeType  inputSize;
+  typename TInputImage::SizeType  outputSize;
+  typename TInputImage::IndexType outputIndex;
+  typename TInputImage::IndexType inputIndex;
+  double origin[ImageDimension];
+  double spacing[ImageDimension];
+
+  itkDebugMacro("GenerateOutputInformation Start");
+
+  // Get pointers to the input and output
+  OutputImagePointer output = this->GetOutput();
+  InputImagePointer input = this->GetInput();
+
+  inputIndex = input->GetLargestPossibleRegion().GetIndex();
+
+  // Default is to copy input's information
+  output->CopyInformation(input);
+  
+  // Output size is always the same as input size
+  inputSize = input->GetLargestPossibleRegion().GetSize();
+  outputSize = inputSize;
+
+  // Establish the source of the image information
+  if (m_UseReferenceImage && m_ReferenceImage)
+    {
+    outputIndex = m_ReferenceImage->GetLargestPossibleRegion().GetIndex();
+    for (i = 0; i < ImageDimension; i++)
+      {
+      origin[i] = m_ReferenceImage->GetOrigin()[i];
+      spacing[i] = m_ReferenceImage->GetSpacing()[i];
+      }
+    m_Shift = outputIndex - inputIndex;
+    }
+  else
+    {
+    outputIndex = input->GetLargestPossibleRegion().GetIndex();
+    for (i = 0; i < ImageDimension; i++)
+      {
+      origin[i] = m_OutputOrigin[i];
+      spacing[i] = m_OutputSpacing[i];
+      m_Shift[i] = m_OutputOffset[i];
+      }
+    }
+
+  // Change the output spacing
+  if (m_ChangeSpacing)
+    {
+    output->SetSpacing(spacing);
+    }
+
+  // Change the output origin
+  if (m_ChangeOrigin)
+    {
+    output->SetOrigin(origin);
+    }
+
+  // Center the image by changing its origin
+  if (m_CenterImage)  
+    {
+    for (i = 0; i < ImageDimension; i++)
+      {
+      origin[i] = -output->GetSpacing()[i] * static_cast<double>(outputSize[i] - 1) / 2.0;
+      }
+    output->SetOrigin(origin);
+    }
+
+  // Change the output's largest possible region
+  if (m_ChangeRegion)
+    {
+    outputRegion.SetSize(outputSize);
+    outputRegion.SetIndex(outputIndex + m_Shift);
+    output->SetLargestPossibleRegion(outputRegion);
+    }
+  else
+    {
+    m_Shift.Fill(0);
+    }
+  itkDebugMacro("GenerateOutputInformation End");
+}
+
+template <class TInputImage>
+void 
+ChangeInformationImageFilter<TInputImage>
+::GenerateInputRequestedRegion()
+{
+  Superclass::GenerateInputRequestedRegion();
+
+  if ( this->GetInput() )
+    {
+    typename TInputImage::RegionType region;
+    region.SetSize(this->GetOutput()->GetRequestedRegion().GetSize());
+    region.SetIndex(this->GetOutput()->GetRequestedRegion().GetIndex() - m_Shift);
+    this->GetInput()->SetRequestedRegion (region);
+    }
+}
+
+template <class TInputImage>
+void 
+ChangeInformationImageFilter<TInputImage>
+::GenerateData()
+{
+  // Get pointers to the input and output
+  InputImagePointer output = this->GetOutput();
+  InputImagePointer input = this->GetInput();
+  
+  // No need to copy the bulk data
+  output->SetPixelContainer(input->GetPixelContainer());
+
+  // Shift the output's buffer region
+  typename TInputImage::RegionType region;
+  region.SetSize(this->GetInput()->GetBufferedRegion().GetSize());
+  region.SetIndex(this->GetOutput()->GetBufferedRegion().GetIndex() + m_Shift);
+
+  output->SetBufferedRegion(region);
+}
+
+/**
+ *
+ */
+template <class TInputImage>
+void 
+ChangeInformationImageFilter<TInputImage>
+::PrintSelf(std::ostream& os, Indent indent) const
+{
+  Superclass::PrintSelf(os,indent);
+
+  os << indent << "CenterImage: " << (m_CenterImage ? "On" : "Off") << std::endl;
+  os << indent << "ChangeSpacing: " << (m_ChangeSpacing ? "On" : "Off") << std::endl;
+  os << indent << "ChangeOrigin: " << (m_ChangeOrigin ? "On" : "Off") << std::endl;
+  os << indent << "ChangeRegion: " << (m_ChangeRegion ? "On" : "Off") << std::endl;
+  if (m_ReferenceImage)
+    {
+    os << indent << "ReferenceImage: " << m_ReferenceImage.GetPointer() << std::endl;
+    }
+  else
+    {
+    os << indent << "ReferenceImage: 0" << std::endl;
+    }
+  os << indent << "OutputSpacing: [";
+  if (ImageDimension >= 1) 
+    {
+    os << m_OutputSpacing[0];
+    }
+  for( unsigned int j = 1; j < ImageDimension; j++ )
+    {
+    os << ", " << m_OutputSpacing[j];
+    } 
+  os << "]" << std::endl;
+ 
+  os << indent << "OutputOrigin: [";
+  if (ImageDimension >= 1) 
+    {
+    os << m_OutputOrigin[0];
+    }
+  for( unsigned int j = 1; j < ImageDimension; j++ )
+    {
+    os << ", " << m_OutputOrigin[j];
+    } 
+  os << "]" << std::endl;
+
+  os << indent << "OutputOffset: [";
+  if (ImageDimension >= 1) 
+    {
+    os << m_OutputOffset[0];
+    }
+  for( unsigned int j = 1; j < ImageDimension; j++ )
+    {
+    os << ", " << m_OutputOffset[j];
+    } 
+  os << "]" << std::endl;
+
+}
+
+
+} // end namespace itk
+
+#endif
