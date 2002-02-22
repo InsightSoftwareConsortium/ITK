262e2cde6c5cf9fc332ae396cd39e56c4ee7e9ca
Bill Lorensen <bill.lorensen@gmail.com>
2002-02-22 14:57:06 -0500

    ENH: new test.

---
+++ Testing/Code/BasicFilters/itkStatisticsImageFilterTest.cxx
@@ -0,0 +1,103 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkStatisticsImageFilterTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include <iostream>
+
+#include "itkImage.h"
+#include "itkImageRegionIterator.h"
+
+#include "itkStatisticsImageFilter.h"
+#include "itkRandomImageSource.h"
+
+int main()
+{
+  std::cout << "itkStatisticsImageFilterTest Start" << std::endl;
+
+  int status = 0;
+  typedef itk::Image<int,3> FloatImage;
+
+  FloatImage::Pointer    image  = FloatImage::New();
+  FloatImage::RegionType region;
+  FloatImage::SizeType   size; size.Fill(64);
+  FloatImage::IndexType  index; index.Fill(0);
+
+  region.SetIndex (index);
+  region.SetSize (size);
+
+  // first try a constant image
+  double fillValue = -100.0;
+  image->SetRegions( region );
+  image->Allocate();
+  image->FillBuffer(fillValue);
+
+  typedef itk::StatisticsImageFilter<FloatImage> FilterType;
+  FilterType::Pointer filter = FilterType::New();
+
+  filter->SetInput (image);
+  filter->UpdateLargestPossibleRegion();
+
+  if (filter->GetMinimum() != fillValue)
+    {
+    std::cerr << "GetMinimum failed! Got " << filter->GetMinimum() << " but expected " << fillValue << std::endl;
+    status++;
+    }
+  if (filter->GetMaximum() != fillValue)
+    {
+    std::cerr << "GetMaximum failed! Got " << filter->GetMaximum() << " but expected " << fillValue << std::endl;
+    status++;
+    }
+  if (filter->GetMean() != fillValue)
+    {
+    std::cerr << "GetMean failed! Got " << filter->GetMean() << " but expected " << fillValue << std::endl;
+    status++;
+    }
+  if (filter->GetVariance() != 0.0)
+    {
+    std::cerr << "GetVariance failed! Got " << filter->GetVariance() << " but expected " << 0.0 << std::endl;
+    status++;
+    }
+  std::cout << "Filter: " << filter;
+
+
+
+  // Now generate a real image
+
+  typedef itk::RandomImageSource<FloatImage> SourceType;
+  SourceType::Pointer source = SourceType::New();
+  unsigned long randomSize[3] = {17, 8, 20};
+
+  source->SetSize(randomSize);
+  double minValue = -100.0;
+  double maxValue = 1000.0;
+
+  source->SetMin(minValue);
+  source->SetMax(maxValue);
+
+  filter->SetInput(source->GetOutput());
+  filter->UpdateLargestPossibleRegion();
+  
+  double expectedSigma = sqrt((maxValue-minValue)*(maxValue-minValue)/12.0);
+  double epsilon = (maxValue - minValue) * .001;
+
+  if (abs(filter->GetSigma() - expectedSigma) > epsilon)
+    {
+    std::cerr << "GetSigma failed! Got " << filter->GetSigma() << " but expected " << expectedSigma << std::endl;
+    }
+
+  std::cout << "Filter: " << filter;
+
+  return status;
+}
