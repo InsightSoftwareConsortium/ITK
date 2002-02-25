8f1ed09e39fb7ae17e31a0d02c0ff2a3b46b6eeb
Will Schroeder <will.schroeder@kitware.com>
2002-02-25 12:31:58 -0500

    ENH:Raw image IO test

--- Testing/Code/IO/itkRawImageIOTest.cxx
+++ Testing/Code/IO/itkRawImageIOTest.cxx
@@ -15,66 +15,56 @@
 
 =========================================================================*/
 #include <iostream>
-#include "itkOutputWindow.h"
-#include "itkRawImageIO.h"
+#include "itkRandomImageSource.h"
+#include "itkImageFileWriter.h"
 #include "itkImageFileReader.h"
-#include "itkRGBPixel.h"
-#include "itkImage.h"
+#include "itkRawImageIO.h"
 
-// this class is used to send output to stdout and not the itk window
-class TextOutput : public itk::OutputWindow
+int main()
 {
-public:
-  typedef itk::SmartPointer<TextOutput> Pointer;
-  itkNewMacro(TextOutput);
-  virtual void DisplayText(const char* s)
-    {
-      std::cout << s << std::endl;
-    }
-};
+  typedef itk::Image<unsigned short,2> ImageType;
 
-int main(int argc, char *argv[])
-{
-  if ( argc < 2 )
+  // Create a source object (in this case a random image generator).
+  // The source object is templated on the output type.
+  //
+  unsigned long size[2];
+  size[0]=128; size[1]=64;
+  
+  itk::RandomImageSource<ImageType>::Pointer random;
+  random = itk::RandomImageSource<ImageType>::New();
+  random->SetMin(0.0);
+  random->SetMax(1.0);
+  random->SetSize(size);
+
+  // Create a mapper (in this case a writer). A mapper
+  // is templated on the input type.
+  //
+  itk::RawImageIO::Pointer vtkIO;
+  vtkIO = itk::VTKImageIO::New();
+
+  // Write out the image
+  itk::ImageFileWriter<ImageType>::Pointer writer;
+  writer = itk::ImageFileWriter<ImageType>::New();
+  writer->SetInput(random->GetOutput());
+  writer->SetFileName("junk.raw");
+  writer->SetImageIO(vtkIO);
+  writer->Write();
+
+  if ( !vtkIO->CanReadFile("junk.raw") )
     {
-    itkGenericOutputMacro(<<"Need a file to process");
     return 1;
     }
 
-
-  // Comment the following if you want to use the itk text output window
-  itk::OutputWindow::SetInstance(TextOutput::New());
-  // Uncomment the following if you want to see each message independently
-  // itk::OutputWindow::GetInstance()->PromptUserOn();
-
-  // We are reading a RGB pixel
-  typedef itk::RGBPixel<unsigned char> RGBPixelType;
-
   // Create a source object (in this case a reader)
-  itk::RawImageIO<RGBPixelType>::Pointer io;
-  io = itk::RawImageIO<RGBPixelType>::New();
-  unsigned int dim[3] = {570,670,1};
-  io->SetDimensions(0,dim[0]);
-  io->SetDimensions(1,dim[1]);
-  io->SetDimensions(2,dim[2]);
-  double spacing[3] = {0.8, 0.8, 1.5};
-  io->SetSpacing(0,spacing[0]);
-  io->SetSpacing(1,spacing[1]);
-  io->SetSpacing(2,spacing[2]);
-  double origin[3] = {0.0,0.0,0.0};
-  io->SetOrigin(0, origin[0]);
-  io->SetOrigin(1, origin[1]);
-  io->SetOrigin(2, origin[2]);
-  io->SetHeaderSize(0);
-  io->SetImageMask(0x7fff);
-  io->SetByteOrderToLittleEndian();
-
-  typedef itk::Image<RGBPixelType,2> RGBImage2DType;
-  itk::ImageFileReader<RGBImage2DType>::Pointer reader;
-  reader = itk::ImageFileReader<RGBImage2DType>::New();
-  reader->SetFileName(argv[1]);
-  reader->SetImageIO(io);
+  itk::ImageFileReader<ImageType>::Pointer reader;
+  reader = itk::ImageFileReader<ImageType>::New();
+  reader->SetImageIO(vtkIO);
+  reader->SetFileName("junk.raw");
   reader->Update();
+
+  writer->SetInput(reader->GetOutput());
+  writer->SetFileName("junk2.raw");
+  writer->Write();
 
   return EXIT_SUCCESS;
 }
