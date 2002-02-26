e76be8571ceb696365d7740dcb1d11993c72b25d
Luis Ibanez <luis.ibanez@kitware.com>
2002-02-25 19:48:55 -0500

    Adaptor class that will wrap and itk::SingleValuedCostFunction and      make it look like a vnl_cost_function that can be used with vnl optimizers.

---
+++ Code/Numerics/itkSingleValuedVnlCostFunctionAdaptor.cxx
@@ -0,0 +1,155 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkSingleValuedVnlCostFunctionAdaptor.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "itkSingleValuedVnlCostFunctionAdaptor.h"
+#include "itkExceptionObject.h"
+
+
+namespace itk
+{
+  
+/**  Constructor.  */
+SingleValuedVnlCostFunctionAdaptor 
+::SingleValuedVnlCostFunctionAdaptor(unsigned int spaceDimension):
+                             vnl_cost_function(spaceDimension) 
+{ 
+}    
+
+    
+
+
+/**  Delegate computation of the value to the CostFunction. */
+SingleValuedVnlCostFunctionAdaptor::InternalMeasureType 
+SingleValuedVnlCostFunctionAdaptor
+::f( const InternalParametersType & inparameters ) 
+{
+  if( !m_CostFunction )
+    {
+    ExceptionObject ex;
+    ex.SetLocation(__FILE__);
+    ex.SetDescription("Attempt to use a SingleValuedVnlCostFunctionAdaptor 
+        without any CostFunction plugged in");
+    throw ex;
+    }
+
+  ParametersType parameters( inparameters.size() );
+  ConvertInternalToExternalParameters( inparameters, parameters );
+  const InternalMeasureType value = 
+    static_cast<InternalMeasureType>( m_CostFunction->GetValue( parameters ));
+
+  return value;
+}
+  
+
+
+/**  Delegate computation of the gradient to the costfunction.  */
+void 
+SingleValuedVnlCostFunctionAdaptor
+::gradf(  const InternalParametersType   & inparameters,
+                InternalDerivativeType   & gradient       ) 
+{
+  if( !m_CostFunction )
+    {
+    ExceptionObject ex;
+    ex.SetLocation(__FILE__);
+    ex.SetDescription("Attempt to use a SingleValuedVnlCostFunctionAdaptor 
+        without any CostFunction plugged in");
+    throw ex;
+    }
+
+  ParametersType parameters( inparameters.size() );
+  ConvertInternalToExternalParameters( inparameters, parameters );
+  DerivativeType externalGradient = 
+                      m_CostFunction->GetDerivative( parameters );
+  ConvertExternalToInternalGradient( externalGradient, gradient);
+
+}
+  
+
+
+/**  Delegate computation of value and gradient to the costfunction.     */
+void 
+SingleValuedVnlCostFunctionAdaptor
+::compute( const InternalParametersType   & x,
+                 InternalMeasureType      * f, 
+                 InternalDerivativeType   * g   )
+{
+  // delegate the computation to the CostFunction
+  ParametersType parameters( x.size() );
+  ConvertInternalToExternalParameters( x, parameters );
+
+  *f = static_cast<InternalMeasureType>(
+                      m_CostFunction->GetValue( parameters ) );
+
+  DerivativeType externalGradient = 
+                        m_CostFunction->GetDerivative( parameters );
+
+  ConvertExternalToInternalGradient( externalGradient, *g );    
+}
+
+
+
+/**  Convert internal Parameters into external type.  */
+void 
+SingleValuedVnlCostFunctionAdaptor
+::ConvertInternalToExternalParameters( const InternalParametersType & input,
+                                             ParametersType         & output )
+{
+  const unsigned int size = input.size();
+  for( unsigned int i=0; i<size; i++ )
+    {
+    output[i] = input[i]; 
+    }
+}
+
+
+
+
+/**  Convert external Parameters into internal type  */
+void 
+SingleValuedVnlCostFunctionAdaptor
+::ConvertExternalToInternalParameters( const  ParametersType         & input,
+                                              InternalParametersType & output )
+{
+  const unsigned int size = input.size();
+  for( unsigned int i=0; i<size; i++ ) 
+    {
+    output[i] = input[i];
+    }
+}
+  
+
+/**  Convert external derviative measures into internal type  */
+void 
+SingleValuedVnlCostFunctionAdaptor
+::ConvertExternalToInternalGradient( const DerivativeType         & input,
+                                           InternalDerivativeType & output )
+{
+  const unsigned int size = input.size();
+  for( unsigned int i=0; i<size; i++ ) 
+    {
+    output[i] = input[i];
+    }
+}
+
+
+} // end namespace itk
+
+
+
+
+
---
+++ Code/Numerics/itkSingleValuedVnlCostFunctionAdaptor.h
@@ -0,0 +1,108 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkSingleValuedVnlCostFunctionAdaptor.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkSingleValuedVnlCostFunctionAdaptor_h
+#define __itkSingleValuedVnlCostFunctionAdaptor_h
+
+#include "itkSingleValuedCostFunction.h"
+#include "vnl/vnl_cost_function.h"
+
+
+namespace itk
+{
+  
+/** \class SingleValuedVnlCostFunctionAdaptor
+ * \brief This class is an Adaptor that allows to pass 
+ * itk::SingleValuedCostFunctions to vnl_optimizers expecting
+ * a vnl_cost_function.
+ * 
+ * This class returns a single valued.
+ *
+ * \ingroup Numerics Optimizers
+ */
+class SingleValuedVnlCostFunctionAdaptor : 
+          public vnl_cost_function
+{
+public:
+
+  /** InternalParametersType typedef. */
+  typedef   vnl_vector<double>     InternalParametersType;
+
+  /** InternalMeasureType typedef. */
+  typedef   double                 InternalMeasureType;
+
+  /** InternalGradientType typedef. */
+  typedef   vnl_vector<double>     InternalDerivativeType;
+
+  /** Parameters of the SingleValuedCostFunction */
+  typedef SingleValuedCostFunction::ParametersType ParametersType;
+
+  /** Derivatives of the SingleValuedCostFunction */
+  typedef SingleValuedCostFunction::DerivativeType DerivativeType;
+
+
+  /** Constructor with size */
+  SingleValuedVnlCostFunctionAdaptor(unsigned int spaceDimension);
+
+  /** Set the CostFunction deriving from SingleValuedCostFunction */
+  void SetCostFunction( SingleValuedCostFunction * costFunction )
+          { m_CostFunction = costFunction; }
+    
+  /** Get the CostFunction deriving from SingleValuedCostFunction */
+  const SingleValuedCostFunction * GetCostFunction( void ) const
+          { return m_CostFunction; }
+    
+  /**  Delegate computation of the value to the CostFunction. */
+  virtual InternalMeasureType f( const InternalParametersType & inparameters );
+    
+  /**  Delegate computation of the gradient to the costFunction.  */
+  virtual void gradf(const InternalParametersType   & inparameters,
+                           InternalDerivativeType   & gradient );
+    
+  /**  Delegate computation of value and gradient to the costFunction.     */
+  virtual void compute(const InternalParametersType   & x,
+                             InternalMeasureType      * f, 
+                             InternalDerivativeType   * g   );
+
+  /**  Convert internal Parameters into external type.  */
+  static void ConvertInternalToExternalParameters( 
+                            const InternalParametersType & input,
+                                  ParametersType         & output );
+
+  /**  Convert external Parameters into internal type  */
+  static void ConvertExternalToInternalParameters(
+                            const  ParametersType         & input,
+                                   InternalParametersType & output );
+    
+  /**  Convert external derviative measures into internal type   */
+  void ConvertExternalToInternalGradient(
+                            const DerivativeType         & input,
+                                  InternalDerivativeType & output );
+
+private:
+
+  SingleValuedCostFunction::Pointer   m_CostFunction;
+
+};  // end of Class CostFunction
+
+    
+} // end namespace itk
+
+
+#endif
+
+
+
