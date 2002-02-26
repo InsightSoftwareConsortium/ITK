419cd69c56c767dd31f7b1e54e0cefc5eac2f79c
Luis Ibanez <luis.ibanez@kitware.com>
2002-02-25 19:44:24 -0500

    Base class for the CostFunction used by Optimizers. ImageMetrics in      Registration Framework should derive from this class too.

---
+++ Code/Numerics/itkCostFunction.h
@@ -0,0 +1,100 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkCostFunction.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkCostFunction_h
+#define __itkCostFunction_h
+
+#include "itkObject.h"
+#include "itkObjectFactory.h"
+#include "itkArray.h"
+#include "itkExceptionObject.h"
+
+namespace itk
+{
+  
+/** \class CostFunction
+ * \brief Base class for cost functions intended to be used with Optimizers.
+ *
+ * \ingroup Numerics Optimizers
+ *
+ */
+
+class ITK_EXPORT CostFunction : public Object 
+{
+public:
+  /** Standard class typedefs. */
+  typedef CostFunction              Self;
+  typedef Object                    Superclass;
+  typedef SmartPointer<Self>        Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(CostFunction, Object);
+
+  /**  ParametersType typedef.
+   *  It defines a position in the optimization search space. */
+  typedef Array<double>                 ParametersType;
+
+ 
+  /** Return the number of parameters required to compute 
+   *  this cost function.     
+   *  This method MUST be overloaded by derived classes */
+  virtual unsigned int GetNumberOfParameters(void) const  { 
+    ExceptionObject ex;
+    ex.SetLocation(__FILE__);
+    ex.SetDescription("This method MUST be overloaded in derived classes");
+    throw ex;
+    return 0; }
+
+
+  /** Return the parameters used in the last call to GetValue or GetDerivative 
+   *  This is done by the ackward reason that vnl_optimizers do not return the
+   *  optimimal parameters of a search so the only way to have access to the 
+   *  solution of an optimization problem is to keep track of the evaluation 
+   *  in the cost function itself                                          */
+  itkGetConstMacro( Parameters, ParametersType );
+ 
+protected:
+  CostFunction() {};
+  virtual ~CostFunction() {};
+  void PrintSelf(std::ostream& os, Indent indent) const
+    { Superclass::PrintSelf(os,indent); }
+
+  /** Store the parameters in ivars because optimizers do not keep track
+   *  of the optimal values   */
+  void SetParameters( const ParametersType & parameters ) const
+     { itkDebugMacro("setting parameters to " << parameters); 
+        m_Parameters = parameters;  }
+
+
+private:
+  CostFunction(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+  mutable   ParametersType      m_Parameters;
+
+};
+
+} // end namespace itk
+
+
+#endif
+
+
+
