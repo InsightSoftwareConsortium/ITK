092b708cdb08e2fc44e5a4ff709c15e1e5202e3c
Will Schroeder <will.schroeder@kitware.com>
2002-02-26 07:54:50 -0500

    ERR:RawIO test belongs in IO

--- Testing/Code/IO/itkRawImageIOTest2.cxx
+++ Testing/Code/IO/itkRawImageIOTest2.cxx
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   Insight Segmentation & Registration Toolkit
-  Module:    itkRawImageIOTest.cxx
+  Module:    itkRawImageIOTest2.cxx
   Language:  C++
   Date:      $Date$
   Version:   $Revision$
