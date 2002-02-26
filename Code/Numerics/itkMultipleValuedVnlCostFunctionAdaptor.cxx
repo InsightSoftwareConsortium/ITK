ebae204ba023c1e96a8a25a55b940ade27309603
Luis Ibanez <luis.ibanez@kitware.com>
2002-02-25 19:50:23 -0500

    Adaptor class that wrap an itk::MultipleValuedCostFunction and makes it      look like a vnl_least_squares_function that can be used by vnl optimizers.

---
+++ Code/Numerics/itkMultipleValuedVnlCostFunctionAdaptor.cxx
@@ -0,0 +1,178 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkMultipleValuedVnlCostFunctionAdaptor.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "itkMultipleValuedVnlCostFunctionAdaptor.h"
+#include "itkExceptionObject.h"
+
+
+namespace itk
+{
+  
+/**  Constructor.  */
+MultipleValuedVnlCostFunctionAdaptor 
+::MultipleValuedVnlCostFunctionAdaptor(
+        unsigned int spaceDimension, unsigned int numberOfValues ):
+               vnl_least_squares_function(spaceDimension,numberOfValues) 
+{ 
+}    
+
+    
+
+
+/**  Delegate computation of the value to the CostFunction. */
+void
+MultipleValuedVnlCostFunctionAdaptor
+::f( const InternalParametersType & inparameters, 
+           InternalMeasureType    & measures        )
+{
+  if( !m_CostFunction )
+    {
+    ExceptionObject ex;
+    ex.SetLocation(__FILE__);
+    ex.SetDescription("Attempt to use a MultipleValuedVnlCostFunctionAdaptor 
+        without any CostFunction plugged in");
+    throw ex;
+    }
+
+  ParametersType parameters( inparameters.size() );
+  ConvertInternalToExternalParameters( inparameters, parameters );
+
+  InternalMeasureType values = 
+                        m_CostFunction->GetValue( parameters );
+
+  measures = values;
+
+}
+  
+
+
+/**  Delegate computation of the gradient to the costfunction.  */
+void 
+MultipleValuedVnlCostFunctionAdaptor
+::gradf(  const InternalParametersType   & inparameters,
+                InternalDerivativeType   & gradient       ) 
+{
+  if( !m_CostFunction )
+    {
+    ExceptionObject ex;
+    ex.SetLocation(__FILE__);
+    ex.SetDescription("Attempt to use a MultipleValuedVnlCostFunctionAdaptor 
+        without any CostFunction plugged in");
+    throw ex;
+    }
+
+  ParametersType parameters( inparameters.size() );
+  ConvertInternalToExternalParameters( inparameters, parameters );
+  DerivativeType externalGradient = 
+                      m_CostFunction->GetDerivative( parameters );
+  ConvertExternalToInternalGradient( externalGradient, gradient);
+
+}
+  
+
+
+/**  Delegate computation of value and gradient to the costfunction.     */
+void 
+MultipleValuedVnlCostFunctionAdaptor
+::compute( const InternalParametersType   & x,
+                 InternalMeasureType      * f, 
+                 InternalDerivativeType   * g   )
+{
+  // delegate the computation to the CostFunction
+  ParametersType parameters( x.size() );
+  ConvertInternalToExternalParameters( x, parameters );
+
+  *f = static_cast<InternalMeasureType>(
+                      m_CostFunction->GetValue( parameters ) );
+
+  DerivativeType externalGradient = 
+                        m_CostFunction->GetDerivative( parameters );
+
+  ConvertExternalToInternalGradient( externalGradient, *g );    
+}
+
+
+
+/**  Convert internal Parameters into external type.  */
+void 
+MultipleValuedVnlCostFunctionAdaptor
+::ConvertInternalToExternalParameters( const InternalParametersType & input,
+                                             ParametersType         & output )
+{
+  const unsigned int size = input.size();
+  for( unsigned int i=0; i<size; i++ )
+    {
+    output[i] = input[i]; 
+    }
+}
+
+
+
+
+/**  Convert external Parameters into internal type  */
+void 
+MultipleValuedVnlCostFunctionAdaptor
+::ConvertExternalToInternalParameters( const  ParametersType         & input,
+                                              InternalParametersType & output )
+{
+  const unsigned int size = input.size();
+  for( unsigned int i=0; i<size; i++ ) 
+    {
+    output[i] = input[i];
+    }
+}
+  
+
+/**  Convert external derviative measures into internal type  */
+void 
+MultipleValuedVnlCostFunctionAdaptor
+::ConvertExternalToInternalGradient( const DerivativeType         & input,
+                                           InternalDerivativeType & output )
+{
+  const unsigned int rows = input.rows();
+  const unsigned int cols = input.cols();
+  for( unsigned int i=0; i<cols; i++ ) 
+    {
+    for( unsigned int j=0; j<rows; j++ ) 
+      {
+      output(i,j) = input(i,j);
+      }
+    }
+}
+
+
+
+/**  Convert external Measures into internal type  */
+void 
+MultipleValuedVnlCostFunctionAdaptor
+::ConvertExternalToInternalMeasures( const  MeasureType         & input,
+                                            InternalMeasureType & output )
+{
+  const unsigned int size = input.size();
+  for( unsigned int i=0; i<size; i++ ) 
+    {
+    output[i] = input[i];
+    }
+}
+  
+
+} // end namespace itk
+
+
+
+
+
---
+++ Code/Numerics/itkMultipleValuedVnlCostFunctionAdaptor.h
@@ -0,0 +1,119 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkMultipleValuedVnlCostFunctionAdaptor.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkMultipleValuedVnlCostFunctionAdaptor_h
+#define __itkMultipleValuedVnlCostFunctionAdaptor_h
+
+#include "itkMultipleValuedCostFunction.h"
+#include "vnl/vnl_least_squares_function.h"
+
+
+namespace itk
+{
+  
+/** \class MultipleValuedVnlCostFunctionAdaptor
+ * \brief This class is an Adaptor that allows to pass 
+ * itk::MultipleValuedCostFunctions to vnl_optimizers expecting
+ * a vnl_cost_function.
+ * 
+ * This class returns a single valued.
+ *
+ * \ingroup Numerics Optimizers
+ */
+class MultipleValuedVnlCostFunctionAdaptor : 
+          public vnl_least_squares_function
+{
+public:
+
+  /** InternalParametersType typedef. */
+  typedef   vnl_vector<double>     InternalParametersType;
+
+  /** InternalMeasureType typedef. */
+  typedef   vnl_vector<double>     InternalMeasureType;
+
+  /** InternalGradientType typedef. */
+  typedef   vnl_matrix<double>     InternalDerivativeType;
+
+  /** MeasureType of the MultipleValuedCostFunction */
+  typedef MultipleValuedCostFunction::MeasureType   MeasureType;
+
+  /** Parameters of the MultipleValuedCostFunction */
+  typedef MultipleValuedCostFunction::ParametersType ParametersType;
+
+  /** Derivatives of the MultipleValuedCostFunction */
+  typedef MultipleValuedCostFunction::DerivativeType DerivativeType;
+
+
+  /** Constructor with size */
+  MultipleValuedVnlCostFunctionAdaptor( unsigned int spaceDimension,
+                                        unsigned int numberOfValues );
+
+  /** Set the CostFunction deriving from MultipleValuedCostFunction */
+  void SetCostFunction( MultipleValuedCostFunction * costFunction )
+          { m_CostFunction = costFunction; }
+    
+  /** Get the CostFunction deriving from MultipleValuedCostFunction */
+  const MultipleValuedCostFunction * GetCostFunction( void ) const
+          { return m_CostFunction; }
+    
+  /**  Delegate computation of the value to the CostFunction. */
+  virtual void f( const InternalParametersType & inparameters,
+                        InternalMeasureType    & measures      );
+    
+  /**  Delegate computation of the gradient to the costFunction.  */
+  virtual void gradf(const InternalParametersType   & inparameters,
+                           InternalDerivativeType   & gradient );
+    
+  /**  Delegate computation of value and gradient to the costFunction.     */
+  virtual void compute(const InternalParametersType   & x,
+                             InternalMeasureType      * f, 
+                             InternalDerivativeType   * g   );
+
+  /**  Convert internal Parameters into external type */
+  static void ConvertInternalToExternalParameters( 
+                            const InternalParametersType & input,
+                                  ParametersType         & output );
+
+  /**  Convert external Parameters into internal type  */
+  static void ConvertExternalToInternalParameters(
+                            const  ParametersType         & input,
+                                   InternalParametersType & output );
+    
+  /**  Convert external derviative measures  into internal type */
+  void ConvertExternalToInternalGradient(
+                            const DerivativeType         & input,
+                                  InternalDerivativeType & output );
+
+  /**  Convert external measures  into internal type */
+  void ConvertExternalToInternalMeasures(
+                            const MeasureType         & input,
+                                  InternalMeasureType & output );
+
+
+private:
+
+  MultipleValuedCostFunction::Pointer   m_CostFunction;
+
+};  // end of Class CostFunction
+
+    
+} // end namespace itk
+
+
+#endif
+
+
+
