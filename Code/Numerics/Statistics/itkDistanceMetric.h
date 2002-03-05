d7f153c2e746170c327db32f0ffdf5a0e7cecdff
Jisung Kim <bahrahm@yahoo.com>
2002-03-05 13:16:18 -0500

    ENH: new base class for distance metrics (functions).

---
+++ Code/Numerics/Statistics/itkDistanceMetric.h
@@ -0,0 +1,82 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkDistanceMetric.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkDistanceMetric_h
+#define __itkDistanceMetric_h
+
+#include "itkMembershipFunctionBase.h"
+
+namespace itk{ 
+namespace Statistics{
+
+/** \class DistanceMetric
+ * \brief this class declares common interfaces 
+ * for distance functions.
+ *
+ * As a function derived from MembershipFunctionBase, 
+ * users use Evaluate method to get result.
+ * 
+ * To use this function in the context of MembershipFunction,
+ * users should first set the origin by calling SetOrigin() function,
+ * then call Evaluate() method with a point to get the distance between
+ * the origin point and the evaluation point.
+ * 
+ * If users want to the distance between two points without setting
+ * the origin point. Use two argument version of Evaluate() function.
+ */
+
+template< class TVector >
+class ITK_EXPORT DistanceMetric : public MembershipFunctionBase< TVector >
+{
+public:
+  /** Standard typedefs */
+  typedef DistanceMetric Self;
+  typedef MembershipFunctionBase< TVector > Superclass;
+  
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(DistanceMetric, MembershipFunctionBase);
+
+  /** Sets the origin point that will be used for the single point 
+   * version Evaluate() function. This function is necessary part of
+   * implementing MembershipFunctionBase's Evaluate() interface */ 
+  void SetOrigin(const TVector &x) ;
+  
+  /** Gets the distance between the origin point and x. This function
+   * work with SetOrigin() function*/
+  virtual double Evaluate(const TVector &x) const = 0 ;
+  
+  /** Gets the distance between x1 and x2 points */
+  virtual double Evaluate(const TVector &x1, const TVector &x2) const = 0 ;
+  
+protected:
+  TVector m_Origin ;
+} ; // end of class
+
+} // end of namespace Statistics 
+} // end of namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkDistanceMetric.txx"
+#endif
+
+#endif
+
+
+
+
+
+
+
---
+++ Code/Numerics/Statistics/itkDistanceMetric.txx
@@ -0,0 +1,43 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkDistanceMetric.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkDistanceMetric_txx
+#define __itkDistanceMetric_txx
+
+#include "itkDistanceMetric.h"
+
+namespace itk{ 
+namespace Statistics{
+
+template< class TVector >
+void 
+DistanceMetric< TVector >
+::SetOrigin(const TVector &x)
+{
+  m_Origin = x ;
+}
+
+} // end of namespace Statistics 
+} // end of namespace itk
+
+#endif
+
+
+
+
+
+
+
