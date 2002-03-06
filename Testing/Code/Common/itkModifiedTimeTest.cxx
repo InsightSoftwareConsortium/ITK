97470cedae76e7b57efaa64a720b22b56e1856b6
Luis Ibanez <luis.ibanez@kitware.com>
2002-03-06 18:32:10 -0500

    Test for GetMTime()  in the Bounding Box class

---
+++ Testing/Code/Common/itkModifiedTimeTest.cxx
@@ -0,0 +1,75 @@
+#include <cstdio>
+
+#include "itkPoint.h"
+#include "itkVectorContainer.h"
+#include "itkBoundingBox.h"
+
+int main( void )
+{
+
+  typedef itk::Point< double, 3 > Point;
+  typedef itk::VectorContainer< unsigned long int, Point > PointsContainer;
+  typedef itk::BoundingBox< unsigned long int, 3, double, PointsContainer > BoundingBox;
+
+  Point p,q,r;
+  
+  p.Fill(0);
+  q.Fill(0);
+  r.Fill(0);
+
+  BoundingBox::Pointer bb = BoundingBox::New();
+  
+  PointsContainer::Pointer pc = bb->GetPoints();
+
+  if( !pc )
+    {
+    pc = PointsContainer::New();
+    pc->InsertElement(0,p);
+    pc->InsertElement(1,q);
+    pc->Modified();
+    bb->SetPoints(pc);
+    }
+
+  const unsigned long bbBeforeTime = bb->GetMTime();
+  const unsigned long pcBeforeTime = pc->GetMTime();
+
+  std::cout<<"BB time before modification: "<< bbBeforeTime <<std::endl;
+  std::cout<<"PC time before modification: "<< pcBeforeTime <<std::endl;
+  pc = bb->GetPoints(); // not necessary, but just to make sure we got the right pointer
+  pc->InsertElement(2,r);
+  pc->Modified(); // call the Modified function to update the modified time of the container
+  
+  const unsigned long bbAfterTime = bb->GetMTime();
+  const unsigned long pcAfterTime = pc->GetMTime();
+
+  std::cout<<"BB time after modification: "<< bbAfterTime <<std::endl;
+  std::cout<<"PC time after modification: "<< pcAfterTime <<std::endl;
+
+
+  if( pcAfterTime == pcBeforeTime )
+    {
+    std::cout << "Points Container Modified Time is not being " << std::endl;
+    std::cout << "updated by call to Modified()" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  if( bbAfterTime == bbBeforeTime )
+    {
+    std::cout << "Bounding Box Modified Time is not being " << std::endl;
+    std::cout << "updated by changes in the points" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+
+  if( bbAfterTime < pcAfterTime )
+    {
+    std::cout << "Bounding Box Modified Time is not as recent " << std::endl;
+    std::cout << "as the modifiction in the points" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  std::cout << "Test PASSED !" << std::endl;
+
+  return EXIT_SUCCESS;
+
+}
