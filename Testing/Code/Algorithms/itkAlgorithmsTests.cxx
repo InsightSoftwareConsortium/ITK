9fa01de5ad889c1b91f67f21f152254bb6064c64
Luis Ibanez <luis.ibanez@kitware.com>
2002-03-07 11:08:46 -0500

    ENH: <iostream> include was redundant. It is already done in itkMainTest.h in the right order.

--- Testing/Code/Algorithms/itkAlgorithmsTests.cxx
+++ Testing/Code/Algorithms/itkAlgorithmsTests.cxx
@@ -1,6 +1,5 @@
 // this file defines the itkBasicFiltersTest for the test driver
 // and all it expects is that you have a function called RegisterTests
-#include <iostream>
 #include "itkTestMain.h" 
 
 
