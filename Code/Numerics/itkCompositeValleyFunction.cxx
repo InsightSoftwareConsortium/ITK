bd62f110802fc5b9daaf10432cc025e8d2e97dfa
Luis Ibanez <luis.ibanez@kitware.com>
2002-03-09 15:37:30 -0500

    FIX: "i" in a for() until "length" requires "unsigned int" type.

--- Code/Numerics/itkCompositeValleyFunction.cxx
+++ Code/Numerics/itkCompositeValleyFunction.cxx
@@ -39,7 +39,7 @@ CompositeValleyFunction
     throw ex;
     }
   
-  for (int i = 0 ; i < length ; i++) 
+  for (unsigned int i = 0 ; i < length ; i++) 
     {
     this->AddNewClass(classMeans[i], classSigmas[i]) ;
     }
