3dca385a87df8ff0bf33e72ec2eff5b60d9c5d89
Luis Ibanez <luis.ibanez@kitware.com>
2002-03-11 08:40:01 -0500

    Driver for tests of the statistics framework.

---
+++ Testing/Code/Numerics/Statistics/itkStatisticsTests.cxx
@@ -0,0 +1,10 @@
+// this file defines the itkStatisticsTest for the test driver
+// and all it expects is that you have a function called RegisterTests
+#include <iostream>
+#include "itkTestMain.h" 
+
+
+void RegisterTests()
+{
+  REGISTER_TEST(itkHistogramTest);
+}
