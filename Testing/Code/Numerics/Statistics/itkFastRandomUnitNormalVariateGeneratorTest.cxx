be12744795dd744d935b0ff41fa6555e286b7a52
Luis Ibanez <luis.ibanez@kitware.com>
2002-03-11 09:59:49 -0500

    ENH: Numeric verification of Mean and Variance added.

--- Testing/Code/Numerics/Statistics/itkFastRandomUnitNormalVariateGeneratorTest.cxx
+++ Testing/Code/Numerics/Statistics/itkFastRandomUnitNormalVariateGeneratorTest.cxx
@@ -15,7 +15,8 @@
 
 =========================================================================*/
 
-#include <itkFastRandomUnitNormalVariateGenerator.h>
+#include "itkFastRandomUnitNormalVariateGenerator.h"
+#include "vnl/vnl_math.h"
 
 
 
@@ -34,24 +35,47 @@ int itkFastRandomUnitNormalVariateGeneratorTest(int, char**)
 
   generator->Initialize( randomSeed );
 
-  const unsigned int numberOfSamples = 100;
+  const unsigned long numberOfSamples = 1000000UL;
+  
+  double sum  = 0.0f;
+  double sum2 = 0.0f;
+
+  std::cout << "Generating " << numberOfSamples << " samples... " << std::endl;
+
   for(unsigned int i=0; i < numberOfSamples; i++)
     {
     const double variate = generator->GetNormalVariate();
-    std::cout << variate << std::endl;
+    sum  += variate;
+    sum2 += variate * variate;
     }
 
-  bool pass = true;
+  const double mean     = sum  / numberOfSamples;
+  const double variance = sum2 / numberOfSamples - mean * mean;
 
-  if( !pass )
+  std::cout << "Mean     = " << mean     << std::endl;
+  std::cout << "Variance = " << variance << std::endl;
+
+
+  // Test values for tolerance...
+  const double tolerance = 1e-2;
+
+  if( vnl_math_abs( mean - 0.0f ) > tolerance )
     {
+    std::cout << "Mean is out of tolerance = " << tolerance << std::endl;
     std::cout << "Test failed." << std::endl;
     return EXIT_FAILURE;
     }
 
-  std::cout << "Test passed." << std::endl;
-  return EXIT_SUCCESS;
 
+  if( vnl_math_abs( variance - 1.0f ) > tolerance )
+    {
+    std::cout << "Variance is out of tolerance = " << tolerance << std::endl;
+    std::cout << "Test failed." << std::endl;
+    return EXIT_FAILURE;
+    }
+
+
+  return EXIT_SUCCESS;
 
 }
 
