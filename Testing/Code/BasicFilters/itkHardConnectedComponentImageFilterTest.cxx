de05e8e4f335a12ca6e8907755f2e63a49e20af5
Ying Zhuge <zhuge@mipg.upenn.edu>
2002-03-11 15:11:18 -0500

    ENH: testing for the new checked in filter (itkHardConnectedComponentImageFilter )

---
+++ Testing/Code/BasicFilters/itkHardConnectedComponentImageFilterTest.cxx
@@ -0,0 +1,101 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkHardConnectedComponentImageFilterTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "itkHardConnectedComponentImageFilter.h"
+#include "itkImageRegionIterator.h"
+
+const int HEIGHT = 20;
+const int WIDTH = 20;
+
+main()
+{
+  typedef itk::Image<bool,2> InputImageType;
+  typedef itk::Image<unsigned short,2> OutputImageType;
+  typedef InputImageType::IndexType IndexType;
+
+  itk::HardConnectedComponentImageFilter<InputImageType, OutputImageType>::Pointer 
+        filter = itk::HardConnectedComponentImageFilter<InputImageType, OutputImageType>::New();
+  InputImageType::Pointer inputimg = InputImageType::New();
+  IndexType index = InputImageType::IndexType::ZeroIndex;
+  InputImageType::RegionType region;
+
+  itk::Size<2> sz;
+  sz[0] = WIDTH;
+  sz[1] = HEIGHT;
+  
+  region.SetSize(sz);
+  region.SetIndex(index);
+
+  inputimg->SetLargestPossibleRegion( region );
+  inputimg->SetBufferedRegion( region );
+  inputimg->SetRequestedRegion( region );
+  inputimg->Allocate();
+
+  int row, col;
+  IndexType myIndex;
+  for(row = 0;row<20;row++)
+    for(col = 0;col < 20;col++)
+      {
+        myIndex[1] = row;
+        myIndex[0] = col;
+        inputimg->SetPixel(myIndex,false);
+      }
+  for(row = 0;row<15;row++)
+    for(col = 0;col<20;col++)
+      {
+        myIndex[1] = row;
+        myIndex[0] = col;
+        inputimg->SetPixel(myIndex,true);
+      }
+
+  for(row = 0;row<10;row++)
+    for(col = 5;col<15;col++)
+      {
+        myIndex[1] = row;
+        myIndex[0] = col;
+        inputimg->SetPixel(myIndex,false);      
+      }
+
+  for(row = 0;row<7;row++)
+    for(col = 7;col<12;col++)
+      {
+        myIndex[1] = row;
+        myIndex[0] = col;
+        inputimg->SetPixel(myIndex,true);
+      }
+
+  //InputImageType::IndexType Seed = {10,2};
+
+  filter->SetInput(inputimg);
+  //filter->SetObjectSeed(Seed);
+  filter->Update();
+
+  typedef itk::ImageRegionIterator<OutputImageType> outputIterator;
+  outputIterator ot = outputIterator(filter->GetOutput(), region);
+
+  ot.GoToBegin();
+  for(int i = 0;i < HEIGHT*WIDTH; i++)
+  {
+    if((i%WIDTH) == 0)
+      std::cout<<std::endl;
+      std::cout<<ot.Get();
+      ++ot;
+  } 
+
+  std::cout<<std::endl;
+
+  return 0;
+}
