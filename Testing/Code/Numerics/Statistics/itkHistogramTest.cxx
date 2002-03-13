af3abb3d8bfc7e334770c367d88a85f714b76951
Jisung Kim <bahrahm@yahoo.com>
2002-03-13 11:39:06 -0500

    ENH: tests on various functions added.

--- Testing/Code/Numerics/Statistics/itkHistogramTest.cxx
+++ Testing/Code/Numerics/Statistics/itkHistogramTest.cxx
@@ -14,29 +14,116 @@
      PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
-
 #include "itkHistogram.h"
-
-
 
 int itkHistogramTest(int, char**) 
 {
   std::cout << "Histogram Test \n \n"; 
-  
-  typedef  float      MeasureType;
-  const unsigned int  NumberOfBins = 100;
-
-  typedef  itk::Statistics::Histogram< MeasureType,
-                                       NumberOfBins   >   HistogramType;
-
-
   bool pass = true;
+  std::string whereFail = "" ;
+  
+  typedef float MeasurementType ;
+
+  // creats a histogram with 3 components measurement vectors
+  typedef itk::Statistics::Histogram< MeasurementType, 3 > HistogramType ;
+  HistogramType::Pointer histogram = HistogramType::New() ;
+
+  // initializes a 64 x 64 x 64 histogram with equal size interval
+  HistogramType::SizeType size ;
+  size.Fill(64) ;
+  unsigned long totalSize = size[0] * size[1] * size[2] ;
+  HistogramType::MeasurementVectorType lowerBound ;
+  HistogramType::MeasurementVectorType upperBound ;
+  lowerBound.Fill(0.0) ;
+  upperBound.Fill(1024.0) ;
+  histogram->Initialize(size, lowerBound, upperBound ) ;
+  HistogramType::MeasurementType interval = 
+    (upperBound[0] - lowerBound[0]) / 
+    static_cast< HistogramType::MeasurementType >(size[0]) ;
+
+  // tests begin
+  HistogramType::MeasurementVectorType measurements ;
+  measurements.Fill(512.0) ;
+  HistogramType::IndexType index ;
+  index.Fill(32) ;
+  if (index != histogram->GetIndex(measurements))
+    {
+      pass = false ;
+      whereFail = "GetIndex(MeasurementVectorType&)" ;
+    }
+  
+  HistogramType::InstanceIdentifier id = 
+    histogram->GetInstanceIdentifier(index);
+  if (index != histogram->GetIndex(id))
+    {
+      pass = false ;
+      whereFail = "GetIndex(InstanceIdentifier&)" ;
+    }
+
+  index.Fill(100) ;
+  
+  if (!histogram->IsIndexOutOfBound(index))
+    {
+      pass = false ;
+      whereFail = "IsIndexOutOfBound(IndexType)" ;
+    }
+
+  if (totalSize != histogram->GetNumberOfInstances())
+    {
+      pass = false ;
+      whereFail = "GetNumberOfInstances()" ;
+    }
+
+  if (totalSize != histogram->Size())
+    {
+      pass = false ;
+      whereFail = "Size()" ;
+    }
+
+  if (size != histogram->GetSize())
+    {
+      pass = false ;
+      whereFail = "GetSize()" ;
+    }
+
+  if ((lowerBound[0] + interval * 31) != histogram->GetBinMin(0,31))
+    {
+      pass = false ;
+      whereFail = "GetBinMin(Dimension, nthBin)" ;
+    }
+
+  if ((lowerBound[0] + interval * 32) != histogram->GetBinMax(0,31))
+    {
+      pass = false ;
+      whereFail = "GetBinMax(Dimension, nthBin)" ;
+    }
+
+  for (id = 0 ; 
+       id < static_cast< HistogramType::InstanceIdentifier >(totalSize) ;
+       id++)
+    {
+      histogram->SetFrequency(id, 1) ;
+      histogram->IncreaseFrequency(id, 1) ;
+      if (histogram->GetFrequency(id) != 2)
+        {
+          pass = false ;
+          whereFail = 
+            "SetFrequency(InstanceIdentifier, 1) + IncreaseFrequency(InstanceIdentifier, 1) + GetFrequency(InstanceIdentifier)" ;
+        }
+    }
+
+  if (histogram->Quantile(0, 0.5) != 512.0)
+    {
+      pass = false ;
+      whereFail = "Quantile(Dimension, percent)" ;
+    }
 
   if( !pass )
     {
-    std::cout << "Test failed." << std::endl;
+      std::cout << "Test failed in" << whereFail << "." << std::endl;
     return EXIT_FAILURE;
     }
+
 
   std::cout << "Test passed." << std::endl;
   return EXIT_SUCCESS;
