afb389c3df034f3952b46fdec303262f2256b7f0
Luis Ibanez <luis.ibanez@kitware.com>
2002-03-18 18:45:12 -0500

    Test for profiling perfomance of run-time code using itkTimeProbe and TimeProbeCollectorBase

---
+++ Testing/Code/Common/itkTimeProbesTest.cxx
@@ -0,0 +1,74 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkTimeProbesTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "itkTimeProbesCollectorBase.h"
+
+//-------------------------
+//
+//   This file test the interface to the TimeProbe classes
+//
+//-------------------------
+int itkTimeProbesTest(int, char**) 
+{
+  
+  itk::TimeProbesCollectorBase   collector;
+
+  const unsigned int N =  1000L;
+  const unsigned int M = 10000L;
+
+  collector.Start("Loop1"); // label that identify the range of the probe
+
+  // Do slow stuff here...
+  {
+  for(unsigned int i=0; i<N; i++)
+    {
+    char * dummy = new char [ M ];
+    for(unsigned int j=0; j<M; j++)
+      {
+      dummy[j] = j; 
+      }
+    delete [] dummy; 
+    }
+  }
+  collector.Stop("Loop1");
+
+
+
+  collector.Start("Loop2"); // label that identify the range of the probe
+
+  // Do other slow stuff here...
+  {
+  for(unsigned int i=0; i<M; i++)
+    {
+    char * dummy = new char [ N ];
+    for(unsigned int j=0; j<N; j++)
+      {
+      dummy[j] = j; 
+      }
+    delete [] dummy; 
+    }
+  }
+  collector.Stop("Loop2");
+
+
+  // Print the results of the time probes
+  collector.Report();
+
+  return EXIT_SUCCESS;
+
+}
+
