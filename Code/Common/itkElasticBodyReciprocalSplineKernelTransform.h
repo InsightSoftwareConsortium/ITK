e200a0183a66a0456cd81d8e18c3eef49e7636ab
Luis Ibanez <luis.ibanez@kitware.com>
2002-04-10 00:10:22 -0400

    Implementation of the ElasticBody Spline with a potential = 1/r(x).

---
+++ Code/Common/itkElasticBodyReciprocalSplineKernelTransform.h
@@ -0,0 +1,120 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkElasticBodyReciprocalSplineKernelTransform.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkElasticBodyReciprocalSplineKernelTransform_h
+#define __itkElasticBodyReciprocalSplineKernelTransform_h
+
+#include "itkKernelTransform.h"
+
+
+namespace itk
+{
+
+/** \class ElasticBodyReciprocalSplineKernelTransform
+ * This class defines the elastic body spline (EBS) transformation.
+ * It is implemented in as straightforward a manner as possible from
+ * the IEEE TMI paper by Davis, Khotanzad, Flamig, and Harms,
+ * Vol. 16 No. 3 June 1997
+ * Taken from the paper:
+ * The EBS "is based on a physical model of a homogeneous, isotropic,
+ * three-dimensional elastic body. The model can approximate the way
+ * that some physical objects deform".
+ *
+ * \ingroup Transforms
+ */
+template <class TScalarType = double,   // Data type for scalars (float or double)
+          int NDimensions = 3>          // Number of dimensions
+class ElasticBodyReciprocalSplineKernelTransform : 
+          public KernelTransform<  TScalarType, NDimensions>
+{
+public:
+  /** Standard class typedefs. */
+  typedef ElasticBodyReciprocalSplineKernelTransform   Self;
+  typedef KernelTransform<  TScalarType, 
+                            NDimensions> Superclass;
+
+  typedef SmartPointer<Self>        Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+    
+  /** Run-time type information (and related methods). */
+  itkTypeMacro( ThinPlateSplineKernelTransform, KernelTransform );
+
+  /** New macro for creation of through a Smart Pointer */
+  itkNewMacro( Self );
+
+  /** Scalar type. */
+  typedef typename Superclass::ScalarType  ScalarType;
+
+  /** Parameters type. */
+  typedef typename Superclass::ParametersType  ParametersType;
+
+  /** Jacobian type. */
+  typedef typename Superclass::JacobianType  JacobianType;
+
+  /** Dimension of the domain space. */
+  enum { SpaceDimension = Superclass::SpaceDimension };
+
+  /** Set alpha */
+  itkSetMacro( Alpha, TScalarType );
+  
+  /** Get alpha */
+  itkGetMacro( Alpha, TScalarType );
+  
+  /** These (rather redundant) typedefs are needed because on SGI, typedefs
+   * are not inherited */
+  typedef typename Superclass::InputPointType  InputPointType;
+  typedef typename Superclass::OutputPointType  OutputPointType;
+  typedef typename Superclass::InputVectorType InputVectorType;
+  typedef typename Superclass::OutputVectorType OutputVectorType;
+  typedef typename Superclass::InputCovariantVectorType InputCovariantVectorType;
+  typedef typename Superclass::OutputCovariantVectorType OutputCovariantVectorType;
+    
+
+protected:
+  ElasticBodyReciprocalSplineKernelTransform();
+  virtual ~ElasticBodyReciprocalSplineKernelTransform();
+  
+  /** These (rather redundant) typedefs are needed because on SGI, typedefs
+   * are not inherited */
+  typedef typename Superclass::GMatrixType GMatrixType;
+
+  /** Compute G(x)
+   * For the elastic body spline, this is:
+   * G(x) = [alpha*r(x)*I - 3*x*x'/r(x)]
+   * \f$ G(x) = [\alpha*r(x)*I - 3*x*x'/r(x) ]\f$
+   * where
+   * alpha is a constant
+   * r(x) = Euclidean norm = sqrt[x1^2 + x2^2 + x3^2]
+   * \f[ r(x) = \sqrt{ x_1^2 + x_2^2 + x_3^2 }  \f]
+   * I = identity matrix */
+  const GMatrixType & ComputeG(const InputVectorType& x) const;
+
+  /** alpha, Poisson's ratio */
+  TScalarType m_Alpha;
+
+  private:
+  ElasticBodyReciprocalSplineKernelTransform(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+};
+
+} // namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkElasticBodyReciprocalSplineKernelTransform.txx"
+#endif
+
+#endif // __itkElasticBodyReciprocalSplineKernelTransform_h
---
+++ Code/Common/itkElasticBodyReciprocalSplineKernelTransform.txx
@@ -0,0 +1,65 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkElasticBodyReciprocalSplineKernelTransform.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef _itkElasticBodyReciprocalSplineKernelTransform_txx
+#define _itkElasticBodyReciprocalSplineKernelTransform_txx
+#include "itkElasticBodyReciprocalSplineKernelTransform.h"
+
+namespace itk
+{
+
+template <class TScalarType, int NDimensions>
+ElasticBodyReciprocalSplineKernelTransform<TScalarType, NDimensions>::
+ElasticBodyReciprocalSplineKernelTransform() 
+{
+  m_Alpha = .25;
+}
+
+template <class TScalarType, int NDimensions>
+ElasticBodyReciprocalSplineKernelTransform<TScalarType, NDimensions>::
+~ElasticBodyReciprocalSplineKernelTransform()
+{
+}
+
+template <class TScalarType, int NDimensions>
+const ElasticBodyReciprocalSplineKernelTransform<TScalarType, NDimensions>::GMatrixType &
+ElasticBodyReciprocalSplineKernelTransform<TScalarType, NDimensions>
+::ComputeG(const InputVectorType & x) const
+{
+  const TScalarType r       = x.GetNorm();
+  const TScalarType factor  = 
+         ( r > 1e-8 ) ? ( -1.0 / r ): NumericTraits<TScalarType>::Zero;
+  const TScalarType radial  = m_Alpha * r;
+  for(unsigned int i=0; i<NDimensions; i++)
+    {
+    const typename InputVectorType::ValueType xi = x[i] * factor;
+    // G is symmetric
+    for(unsigned int j=0; j<i; j++)
+      {
+        const TScalarType value = xi * x[j]; 
+        m_GMatrix[i][j] = value;
+        m_GMatrix[j][i] = value;
+      }
+    m_GMatrix[i][i] =  radial + xi * x[i];
+    }
+  
+  return m_GMatrix;
+}
+
+
+
+} // namespace itk
+#endif
