2f8715dfa027f38743e1f357b08cb90a9c7b8066
Brad King <brad.king@kitware.com>
2002-04-18 10:24:37 -0400

    ERR:  A functor type cannot be compared.

--- Code/BasicFilters/itkUnaryFunctorImageFilter.h
+++ Code/BasicFilters/itkUnaryFunctorImageFilter.h
@@ -73,11 +73,8 @@ public:
    * appropriate). */
   void SetFunctor(const FunctorType& functor)
     {
-      if ( m_Functor != functor )
-        {
-        m_Functor = functor;
-        this->Modified();
-        }
+      m_Functor = functor;
+      this->Modified();
     }
   
 protected:
