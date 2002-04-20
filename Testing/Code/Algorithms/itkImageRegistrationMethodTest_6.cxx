e5ac03412f8fefdd1096057ced19c8213fa1cd10
Luis Ibanez <luis.ibanez@kitware.com>
2002-04-19 21:34:27 -0400

    FIX: Actual registration performed now.  Parameters tunned.

--- Testing/Code/Algorithms/itkImageRegistrationMethodTest_6.cxx
+++ Testing/Code/Algorithms/itkImageRegistrationMethodTest_6.cxx
@@ -20,31 +20,44 @@
 #include "itkNormalizedCorrelationImageToImageMetric.h"
 #include "itkLinearInterpolateImageFunction.h"
 #include "itkRegularStepGradientDescentOptimizer.h"
+#include "itkCommandIterationUpdate.h"
+
+#include "itkImageRegistrationMethodImageSource.h"
 
 /** 
- *  This program test one instantiation of the itk::ImageRegistrationMethod class
+ *  This program tests one instantiation of the itk::ImageRegistrationMethod class
  * 
- *  Only typedef are tested in this file.
+ *  
  */ 
 
-int itkImageRegistrationMethodTest_6(int, char**)
+int itkImageRegistrationMethodTest_6(int argc, char** argv)
 {
 
   bool pass = true;
 
-  const unsigned int dimension = 3;
+  const unsigned int dimension = 2;
 
   // Fixed Image Type
   typedef itk::Image<float,dimension>               FixedImageType;
 
   // Moving Image Type
-  typedef itk::Image<char,dimension>                MovingImageType;
+  typedef itk::Image<float,dimension>               MovingImageType;
 
+  // Size Type
+  typedef MovingImageType::SizeType                 SizeType;
+
+
+  // ImageSource
+  typedef itk::testhelper::ImageRegistrationMethodImageSource<
+                                  FixedImageType::PixelType,
+                                  MovingImageType::PixelType,
+                                  dimension >         ImageSourceType;
   // Transform Type
   typedef itk::AffineTransform< double, dimension > TransformType;
+  typedef TransformType::ParametersType             ParametersType;
 
   // Optimizer Type
-  typedef itk::RegularStepGradientDescentOptimizer   OptimizerType;
+  typedef itk::RegularStepGradientDescentOptimizer      OptimizerType;
 
   // Metric Type
   typedef itk::NormalizedCorrelationImageToImageMetric< 
@@ -54,24 +67,38 @@ int itkImageRegistrationMethodTest_6(int, char**)
   // Interpolation technique
   typedef itk:: LinearInterpolateImageFunction< 
                                     MovingImageType,
-                                    double          >    InterpolatorType;
+                                    double >             InterpolatorType;
 
   // Registration Method
   typedef itk::ImageRegistrationMethod< 
                                     FixedImageType, 
                                     MovingImageType >    RegistrationType;
 
+  typedef itk::CommandIterationUpdate<  
+                                  OptimizerType >    CommandIterationType;
+
 
   MetricType::Pointer         metric        = MetricType::New();
   TransformType::Pointer      transform     = TransformType::New();
   OptimizerType::Pointer      optimizer     = OptimizerType::New();
   TransformType::Pointer      trasform      = TransformType::New();
-  FixedImageType::Pointer     fixedImage    = FixedImageType::New();  
-  MovingImageType::Pointer    movingImage   = MovingImageType::New();  
   InterpolatorType::Pointer   interpolator  = InterpolatorType::New();
   RegistrationType::Pointer   registration  = RegistrationType::New();
 
+  ImageSourceType::Pointer    imageSource   = ImageSourceType::New();
 
+  SizeType size;
+  size[0] = 100;
+  size[1] = 100;
+  
+  imageSource->GenerateImages( size );
+
+  FixedImageType::ConstPointer     fixedImage    = imageSource->GetFixedImage();
+  MovingImageType::ConstPointer    movingImage   = imageSource->GetMovingImage();
+
+  //
+  // Connect all the components required for Registratio
+  //
   registration->SetMetric(        metric        );
   registration->SetOptimizer(     optimizer     );
   registration->SetTransform(     transform     );
@@ -80,13 +107,115 @@ int itkImageRegistrationMethodTest_6(int, char**)
   registration->SetInterpolator(  interpolator  );
 
 
+  // Select the Region of Interest over which the Metric will be computed
+  // Registration time will be proportional to the number of pixels in this region.
+  metric->SetFixedImageRegion( fixedImage->GetBufferedRegion() );
+
+  // Instantiate an Observer to report the progress of the Optimization
+  CommandIterationType::Pointer iterationCommand = CommandIterationType::New();
+  iterationCommand->SetOptimizer(  optimizer.GetPointer() );
+
+  // Scale the translation components of the Transform in the Optimizer
+  OptimizerType::ScalesType scales( transform->GetNumberOfParameters() );
+  scales.Fill( 1.0 );
+
+  
+  unsigned long   numberOfIterations =     30;
+  double          translationScale   =   1e-8;
+  double          maximumStepLenght  =   30.0; // no step will be larger than this
+  double          minimumStepLenght  =   0.01; // convergence criterion
+  double          gradientTolerance  =   0.01; // convergence criterion
+
+  if( argc > 1 )
+    {
+    numberOfIterations = atol( argv[1] );
+    std::cout << "numberOfIterations = " << numberOfIterations << std::endl;
+    }
+  if( argc > 2 )
+    {
+    translationScale = atof( argv[2] );
+    std::cout << "translationScale = " << translationScale << std::endl;
+    }
+  if( argc > 3 )
+    {
+    maximumStepLenght = atof( argv[3] );
+    std::cout << "maximumStepLenght = " << maximumStepLenght << std::endl;
+    }
+  if( argc > 4 )
+    {
+    minimumStepLenght = atof( argv[4] );
+    std::cout << "minimumStepLenght = " << minimumStepLenght << std::endl;
+    }
+  if( argc > 5 )
+    {
+    gradientTolerance = atof( argv[5] );
+    std::cout << "gradientTolerance = " << gradientTolerance << std::endl;
+    }
+
+
+
+  for( unsigned int i=0; i<dimension; i++)
+    {
+    scales[ i + dimension * dimension ] = translationScale;
+    }
+
+  optimizer->SetScales( scales );
+  optimizer->SetNumberOfIterations( numberOfIterations );
+  optimizer->SetMaximize(false);
+  optimizer->SetMinimumStepLength( minimumStepLenght );
+  optimizer->SetMaximumStepLength( maximumStepLenght );
+  optimizer->SetGradientMagnitudeTolerance( gradientTolerance );
+
+  // Start from an Identity transform (in a normal case, the user 
+  // can probably provide a better guess than the identity...
+  transform->SetIdentity();
+  registration->SetInitialTransformParameters( transform->GetParameters() );
+
+  // Initialize the internal connections of the registration method. 
+  // This can potentially throw an exception
+  try
+    {
+    registration->StartRegistration();
+    }
+  catch( itk::ExceptionObject & e )
+    {
+    std::cerr << e << std::endl;
+    pass = false;
+    }
+
+  ParametersType actualParameters = imageSource->GetActualParameters();
+  ParametersType finalParameters  = registration->GetLastTransformParameters();
+
+  const unsigned int numbeOfParameters = actualParameters.Size();
+
+  // We know that for the Affine transform the Translation parameters are at 
+  // the end of the list of parameters.
+  const unsigned int offsetOrder = finalParameters.Size()-actualParameters.Size();
+  
+
+
+  const double tolerance = 1.0;  // equivalent to 1 pixel.
+
+  for(unsigned int i=0; i<numbeOfParameters; i++) 
+    {
+    // the parameters are negated in order to get the inverse transformation.
+    // this only works for comparing translation parameters....
+    std::cout << finalParameters[i+offsetOrder] << " == " << -actualParameters[i] << std::endl;
+    if( fabs ( finalParameters[i+offsetOrder] - (-actualParameters[i]) ) > tolerance )
+      {
+      std::cout << "Tolerance exceeded at component " << i << std::endl;
+      pass = false;
+      }
+    }
+
+
   if( !pass )
     {
-    std::cout << "Test failed." << std::endl;
+    std::cout << "Test FAILED." << std::endl;
     return EXIT_FAILURE;
     }
 
-  std::cout << "Test passed." << std::endl;
+  std::cout << "Test PASSED." << std::endl;
   return EXIT_SUCCESS;
 
 
