44067603a238d7618e9dba30400498b3999ddf28
Brad King <brad.king@kitware.com>
2002-04-30 16:47:29 -0400

    ENH: Added ITK copyright to config file.

--- Wrapping/Tcl/Common/wrap_ITKCommon.cxx
+++ Wrapping/Tcl/Common/wrap_ITKCommon.cxx
@@ -1,3 +1,19 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    wrap_ITKCommon.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
 #ifdef CABLE_CONFIGURATION
 #include "wrap_ITKCommon.h"
 namespace _cable_
@@ -6,6 +22,7 @@ namespace _cable_
   const char* const package_version = ITK_WRAP_PACKAGE_VERSION;
   const char* const groups[] =
   {
+    ITK_WRAP_GROUP(ITKUtils),
     ITK_WRAP_GROUP(ITKBase),
     ITK_WRAP_GROUP(itkImage)
   };
