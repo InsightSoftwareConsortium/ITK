b2e4123c65af4a476024474a1eb591eef43737ca
Brad King <brad.king@kitware.com>
2002-05-08 15:05:45 -0400

    ENH: Tcl example using image viewer and some filters.

---
+++ Wrapping/Tcl/Executable/randomImage.tcl
@@ -0,0 +1,39 @@
+# Require ITK package.
+package require itk
+
+# Initial sigma value.
+set sigma 1
+
+# Create a random image source.
+set source [itk::create RandomImageSourceUS2]
+$source SetMin 0
+$source SetMax 255
+$source SetSize {300 300}
+
+# Connect the smoothing filter.
+set filter [itk::create RecursiveGaussianImageFilterUS2]
+$filter SetInput [[$source GetOutput] ->]
+$filter SetSigma $sigma
+$filter SetNormalizeAcrossScale 1
+$filter SetDirection 0
+
+# Create the display windows.
+set randomV [itk::createImageViewer2D .random [[$source GetOutput] ->]]
+set smoothedV [itk::createImageViewer2D .smoothed [[$filter GetOutput] ->]]
+$randomV Draw
+$smoothedV Draw
+
+# Setup the GUI.
+button .exit -text "Exit" -command {exit}
+button .update -text "Update" -command {
+  # Set sigma on the smoothing filter and update the display.
+  $filter SetSigma $sigma
+  $smoothedV Draw
+  $randomV Draw
+}
+label .sigma_label -text "Sigma:" 
+entry .sigma -textvariable sigma
+button .interact -text "Interact" -command {wm deiconify .itkInteract}
+
+pack .exit .interact .update -side top
+pack .sigma_label .sigma -side left
