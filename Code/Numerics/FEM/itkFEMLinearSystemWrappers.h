771497bed978a655da89404ae35d829acb727cff
Tessa Sundaram <Tessa.Sundaram@uphs.upenn.edu>
2002-05-15 20:41:55 -0400

    ENH: includes all LinearSystemWrapper classes

---
+++ Code/Numerics/FEM/itkFEMLinearSystemWrappers.h
@@ -0,0 +1,27 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkFEMLinearSystemWrappers.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+/**
+ * \file itkFEMLinearSystemWrappers.h
+ * \brief Include all LinearSystemWrapper classes defined in FEM toolkit.
+ *
+ */
+
+#include "itkFEMLinearSystemWrapper.h"
+#include "itkFEMLinearSystemWrapperItpack.h"
+#include "itkFEMLinearSystemWrapperVNL.h"
+#include "itkFEMLinearSystemWrapperDenseVNL.h"
