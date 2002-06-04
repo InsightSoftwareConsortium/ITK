20a866adec7bea5344e628a74178fb92fc16b75c
Julien Jomier <julien.jomier@kitware.com>
2002-06-04 17:11:58 -0400

    ENH: Class that performs rigid transformations in 2D

---
+++ Code/Common/itkRigid2DTransform.h
@@ -0,0 +1,221 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkRigid2DTransform.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkRigid2DTransform_h
+#define __itkRigid2DTransform_h
+
+#include <iostream>
+#include "itkTransform.h"
+#include "itkExceptionObject.h"
+#include "vnl/vnl_quaternion.h"
+#include "itkMatrix.h"
+
+namespace itk
+{
+
+/** \brief Rigid2DTransform of a vector space (e.g. space coordinates)
+ *
+ * This transform applies a rotation and translation to the space
+ *
+ * \ingroup Transforms
+ */
+template < class TScalarType=double >    // Data type for scalars (float or double)
+class ITK_EXPORT Rigid2DTransform : 
+        public Transform< TScalarType, 2, 2> // Dimensions of input and output spaces
+{
+public:
+  /** Standard class typedefs. */
+  typedef Rigid2DTransform Self;
+  typedef Transform< TScalarType, 2, 2 > Superclass;
+  typedef SmartPointer<Self>        Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+  
+  /** Run-time type information (and related methods). */
+  itkTypeMacro( Rigid2DTransform, Transform );
+
+  /** New macro for creation of through a Smart Pointer */
+  itkNewMacro( Self );
+
+  /** Dimension of the space. */
+  enum  { InputSpaceDimension  = 2 };
+  enum  { OutputSpaceDimension = 2 };
+  enum  { ParametersDimension  = 4 };
+
+  /** Scalar type. */
+  typedef typename Superclass::ScalarType  ScalarType;
+
+  /** Parameters type. */
+  typedef typename Superclass::ParametersType  ParametersType;
+
+  /** Jacobian type. */
+  typedef typename Superclass::JacobianType  JacobianType;
+
+  /// Standard matrix type for this class
+  typedef Matrix<ScalarType, InputSpaceDimension, InputSpaceDimension> MatrixType;
+
+  /// Standard vector type for this class
+  typedef Vector<TScalarType, InputSpaceDimension> OffsetType;
+
+  /// Standard vector type for this class
+  typedef Vector<TScalarType, InputSpaceDimension> InputVectorType;
+  typedef Vector<TScalarType, OutputSpaceDimension> OutputVectorType;
+
+  /// Standard covariant vector type for this class
+  typedef CovariantVector<TScalarType, InputSpaceDimension> InputCovariantVectorType;
+  typedef CovariantVector<TScalarType, OutputSpaceDimension> OutputCovariantVectorType;
+
+  /// Standard vnl_vector type for this class
+  typedef vnl_vector_fixed<TScalarType, InputSpaceDimension> InputVnlVectorType;
+  typedef vnl_vector_fixed<TScalarType, OutputSpaceDimension> OutputVnlVectorType;
+
+  /// Standard coordinate point type for this class
+  typedef Point<TScalarType, InputSpaceDimension>    InputPointType;
+  typedef Point<TScalarType, OutputSpaceDimension>    OutputPointType;
+
+  /**
+   * Get offset of an Rigid2DTransform
+   *
+   * This method returns the value of the offset of the
+   * Rigid2DTransform.
+   **/
+   itkGetConstMacro( Offset, OffsetType );
+
+  /**
+   * Get rotation Matrix from an Rigid2DTransform
+   *
+   * This method returns the value of the rotation of the
+   * Rigid2DTransform.
+   **/
+   itkGetConstMacro( RotationMatrix, MatrixType );
+
+
+  /**
+   * Set offset of a Rigid2D Transform
+   *
+   * This method sets the offset of an Rigid2DTransform to a
+   * value specified by the user.
+   **/
+  itkSetMacro( Offset, OffsetType );
+
+
+  /**
+   * Set the rotation Matrix of a Rigid2D Transform
+   *
+   * This method sets the 2x2 matrix representing a rotation
+   * in the transform.  The Matrix is expected to be orthogonal
+   * with a certain tolerance.
+   * \warning This method will throw an exception is the matrix
+   * provided as argument is not orthogonal.
+   **/
+  void SetRotationMatrix(const MatrixType &matrix);
+
+
+  /**
+   * Compose with another Rigid2DTransform
+   *
+   **/
+  void Compose(const Self *other, bool pre=false);
+
+
+  /**
+   * Compose the transformation with a translation
+   *
+   * This method modifies self to include a translation of the
+   * origin.  The translation is precomposed with self if pre is
+   * true, and postcomposed otherwise.
+   **/
+  void Translate(const OffsetType &offset, bool pre=false);
+
+
+  /**
+   * Transform by an affine transformation
+   *
+   * This method applies the affine transform given by self to a
+   * given point or vector, returning the transformed point or
+   * vector.
+   **/
+  OutputPointType     TransformPoint(const InputPointType  &point ) const;
+  OutputVectorType    TransformVector(const InputVectorType &vector) const;
+  OutputVnlVectorType    TransformVector(const InputVnlVectorType &vector) const;
+
+  OutputCovariantVectorType TransformCovariantVector(
+                                 const InputCovariantVectorType &vector) const;
+
+  /**
+   * Back transform by an affine transformation
+   *
+   * This method finds the point or vector that maps to a given
+   * point or vector under the affine transformation defined by
+   * self.  If no such point exists, an exception is thrown.
+   **/
+  inline InputPointType      BackTransform(const OutputPointType  &point ) const;
+  inline InputVectorType     BackTransform(const OutputVectorType &vector) const;
+  inline InputVnlVectorType  BackTransform(const OutputVnlVectorType &vector) const;
+
+  inline InputCovariantVectorType BackTransform(
+                                     const OutputCovariantVectorType &vector) const;
+
+  /**
+   * Print contents of an Rigid2DTransform
+   **/
+  void PrintSelf(std::ostream &os, Indent indent) const;
+
+  /**
+   * Find inverse of an affine transformation
+   *
+   * This method creates and returns a new Rigid2DTransform object
+   * which is the inverse of self.  If self is not invertible,
+   * an exception is thrown.
+   **/
+  Pointer Inverse( void ) const;
+
+  /** Set the parameters to the IdentityTransform */
+  void SetIdentity(void);
+
+  /** Compute the Jacobian Matrix of the transformation at one point */
+  virtual const JacobianType & GetJacobian(const InputPointType  &point ) const;
+
+protected:
+  Rigid2DTransform();
+  ~Rigid2DTransform();
+
+  // matrix representation of the rotation
+  // Should be protected in order to be modified 
+  // by derived classes that instantiate an interface
+  // to rotation computation
+  MatrixType          m_RotationMatrix;   
+
+  // representation of the inverse rottion
+  MatrixType          m_InverseMatrix; 
+
+private:
+  Rigid2DTransform(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+  // Offset of the transformation
+  OffsetType          m_Offset;   
+
+}; //class Rigid2DTransform
+
+
+}  // namespace itk
+
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkRigid2DTransform.txx"
+#endif
+
+#endif /* __itkRigid2DTransform_h */
---
+++ Code/Common/itkRigid2DTransform.txx
@@ -0,0 +1,246 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkRigid2DTransform.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef _itkRigid2DTransform_txx
+#define _itkRigid2DTransform_txx
+
+#include "itkRigid2DTransform.h"
+
+
+namespace itk
+{
+
+// Constructor with default arguments
+template<class TScalarType>
+Rigid2DTransform<TScalarType>::
+Rigid2DTransform():Superclass(OutputSpaceDimension,ParametersDimension)
+{
+  m_Offset.Fill( 0 );
+  m_RotationMatrix.SetIdentity();
+  m_InverseMatrix.SetIdentity();
+}
+ 
+
+// Destructor
+template<class TScalarType>
+Rigid2DTransform<TScalarType>::
+~Rigid2DTransform()
+{
+}
+
+
+// Print self
+template<class TScalarType>
+void
+Rigid2DTransform<TScalarType>::
+PrintSelf(std::ostream &os, Indent indent) const
+{
+
+  Superclass::PrintSelf(os,indent);
+  
+  os << indent << "Offset: " << m_Offset   << std::endl;
+  os << indent << "RotationMatrix: " << m_RotationMatrix   << std::endl;
+}
+
+
+  
+// Compose with another affine transformation
+template<class TScalarType>
+void
+Rigid2DTransform<TScalarType>::
+SetRotationMatrix(const MatrixType & matrix )
+{
+  itkDebugMacro("setting  m_RotationMatrix  to " << matrix ); 
+  // The matrix must be orthogonal otherwise it is not
+  // representing a valid rotaion in 2D space
+  MatrixType::InternalMatrixType test = 
+                  matrix.GetVnlMatrix() * matrix.GetTranspose();
+
+  const double tolerance = 1e-10;
+  if( !test.is_identity( tolerance ) ) 
+  {
+    itk::ExceptionObject ex;
+    ex.SetDescription("Attempt to set a Non-Orthogonal matrix");
+    ex.SetLocation(__FILE__);
+    throw ex;
+  }
+
+  m_RotationMatrix = matrix;
+
+  this->Modified(); 
+}
+
+
+// Compose with another affine transformation
+template<class TScalarType>
+void
+Rigid2DTransform<TScalarType>::
+Compose(const Self * other, bool pre )
+{
+  if (pre) 
+    {
+    m_Offset         = m_RotationMatrix * other->m_Offset + m_Offset;
+    m_RotationMatrix = m_RotationMatrix * other->m_RotationMatrix;
+    }
+  else 
+    {
+    m_Offset         = other->m_RotationMatrix * m_Offset + other->m_Offset;
+    m_RotationMatrix = other->m_RotationMatrix * m_RotationMatrix;
+    }
+  m_InverseMatrix = m_RotationMatrix.GetTranspose();
+}
+
+
+// Compose with a translation
+template<class TScalarType>
+void
+Rigid2DTransform<TScalarType>::
+Translate(const OffsetType &offset, bool pre)
+{
+  m_Offset += offset;
+  return;
+}
+
+
+
+
+// Transform a point
+template<class TScalarType>
+Rigid2DTransform<TScalarType>::OutputPointType
+Rigid2DTransform<TScalarType>::
+TransformPoint(const InputPointType &point) const 
+{
+  return m_RotationMatrix * point + m_Offset; 
+}
+
+
+// Transform a vector
+template<class TScalarType>
+Rigid2DTransform<TScalarType>::OutputVectorType
+Rigid2DTransform<TScalarType>::
+TransformVector(const InputVectorType &vect) const 
+{
+  return  m_RotationMatrix * vect;
+}
+
+
+// Transform a vnl_vector_fixed
+template<class TScalarType>
+Rigid2DTransform<TScalarType>::OutputVnlVectorType
+Rigid2DTransform<TScalarType>::
+TransformVector(const InputVnlVectorType &vect) const 
+{
+  return  m_RotationMatrix * vect;
+}
+
+
+// Transform a CovariantVector
+template<class TScalarType>
+Rigid2DTransform<TScalarType>::OutputCovariantVectorType
+Rigid2DTransform<TScalarType>::
+TransformCovariantVector(const InputCovariantVectorType &vect) const 
+{
+  // Covariant vectors are transformed like contravariant
+  // vectors under orthogonal transformations.
+  return  m_RotationMatrix * vect;
+}
+
+
+
+// Back transform a point
+template<class TScalarType>
+Rigid2DTransform<TScalarType>::InputPointType
+Rigid2DTransform<TScalarType>::
+BackTransform(const OutputPointType &point) const 
+{
+    return m_InverseMatrix * (point - m_Offset);
+}
+
+// Back transform a vector
+template<class TScalarType>
+Rigid2DTransform<TScalarType>::InputVectorType
+Rigid2DTransform<TScalarType>::
+BackTransform(const OutputVectorType &vect ) const 
+{
+    return  m_InverseMatrix * vect;
+}
+
+// Back transform a vnl_vector
+template<class TScalarType>
+Rigid2DTransform<TScalarType>::InputVnlVectorType
+Rigid2DTransform<TScalarType>::
+BackTransform(const OutputVnlVectorType &vect ) const 
+{
+    return  m_InverseMatrix * vect;
+}
+
+
+// Back Transform a CovariantVector
+template<class TScalarType>
+Rigid2DTransform<TScalarType>::InputCovariantVectorType
+Rigid2DTransform<TScalarType>::
+BackTransform(const OutputCovariantVectorType &vect) const 
+{
+  return m_RotationMatrix * vect;
+}
+
+
+
+// Create and return an inverse transformation
+template<class TScalarType>
+Rigid2DTransform<TScalarType>::Pointer
+Rigid2DTransform<TScalarType>::
+Inverse( void ) const
+{
+  Pointer result = New();
+  result->m_RotationMatrix   =   m_InverseMatrix;
+  result->m_InverseMatrix    =   m_RotationMatrix;
+  result->m_Offset           = -(m_InverseMatrix * m_Offset);
+  return result;
+}
+
+
+  
+// Compute the Jacobian in one position 
+template<class TScalarType >
+void
+Rigid2DTransform< TScalarType >::
+SetIdentity( void ) 
+{
+  m_Offset.Fill( NumericTraits< TScalarType >::Zero );
+  m_RotationMatrix.SetIdentity();
+  m_InverseMatrix.SetIdentity();
+}
+
+
+  
+// Compute the Jacobian in one position 
+template<class TScalarType >
+const Rigid2DTransform<TScalarType>::JacobianType & 
+Rigid2DTransform< TScalarType >::
+GetJacobian( const InputPointType & p ) const
+{
+  
+  m_Jacobian.Fill( NumericTraits< ITK_TYPENAME JacobianType::ValueType >::Zero );
+  return m_Jacobian;
+
+}
+
+
+ 
+} // namespace
+
+#endif
