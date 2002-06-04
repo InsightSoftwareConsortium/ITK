90784c08ae3f6a3489c2a93123e5d3ff525cf557
Aljaz Noe <noe@grasp.cis.upenn.edu>
2002-06-04 12:16:43 -0400

    FIX: Warnings on MSVS.

--- Testing/Code/Numerics/FEM/itkFEMTests.cxx
+++ Testing/Code/Numerics/FEM/itkFEMTests.cxx
@@ -1,5 +1,11 @@
 // this file defines the itkFEMTests for the test driver
 // and all it expects is that you have a function called RegisterTests
+
+// disable debug warnings in MS compiler
+#ifdef _MSC_VER
+#pragma warning(disable: 4786)
+#endif
+
 #include <iostream>
 #include "itkTestMain.h"
 
