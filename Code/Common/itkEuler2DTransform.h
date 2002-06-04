d4b7626a2a97343138bfbfe3be79c65caf010af1
Julien Jomier <julien.jomier@kitware.com>
2002-06-04 17:12:53 -0400

    ENH: Class that performs a 2D rigid transformation using euler angle

---
+++ Code/Common/itkEuler2DTransform.h
@@ -0,0 +1,123 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkEuler2DTransform.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#ifndef __itkEuler2DTransform_h
+#define __itkEuler2DTransform_h
+
+#include <iostream>
+#include "itkRigid2DTransform.h"
+
+namespace itk
+{
+
+/** \brief Euler2DTransform of a vector space (e.g. space coordinates)
+ *
+ * This transform applies a rotation and translation to the space
+ * given one angle for rotation and a 2D offset for translation. 
+ *
+ * \ingroup Transforms
+ */
+template < class TScalarType=double >    // Data type for scalars (float or double)
+class ITK_EXPORT Euler2DTransform : 
+            public Rigid2DTransform< TScalarType > 
+{
+public:
+  /** Standard class typedefs. */
+  typedef Euler2DTransform Self;
+  typedef Rigid2DTransform< TScalarType >   Superclass;
+  typedef SmartPointer<Self>        Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+    
+  /** New macro for creation of through a Smart Pointer. */
+  itkNewMacro( Self );
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro( Euler2DTransform, Rigid2DTransform );
+
+  /** Dimension of parameters. */
+  enum { SpaceDimension = 2, 
+         ParametersDimension = 3 };
+
+  /** Scalar type. */
+  typedef typename Superclass::ScalarType  ScalarType;
+
+  /** Parameters type. */
+  typedef typename Superclass::ParametersType  ParametersType;
+
+  /** Jacobian type. */
+  typedef typename Superclass::JacobianType  JacobianType;
+  
+  /** Offset type. */
+  typedef typename Superclass::OffsetType  OffsetType;
+
+  /** Point type. */
+  typedef typename Superclass::InputPointType   InputPointType;
+  typedef typename Superclass::OutputPointType  OutputPointType;
+  
+  /** Vector type. */
+  typedef typename Superclass::InputVectorType   InputVectorType;
+  typedef typename Superclass::OutputVectorType  OutputVectorType;
+  
+  /** CovariantVector type. */
+  typedef typename Superclass::InputCovariantVectorType   InputCovariantVectorType;
+  typedef typename Superclass::OutputCovariantVectorType  OutputCovariantVectorType;
+  
+  /** VnlVector type. */
+  typedef typename Superclass::InputVnlVectorType   InputVnlVectorType;
+  typedef typename Superclass::OutputVnlVectorType  OutputVnlVectorType;
+  
+  /** Set the transformation from a container of parameters
+   * This is typically used by optimizers.
+   * There are 3 parameters. The first one represents the
+   * rotation and the last two represents the offset. */
+  void SetParameters( const ParametersType & parameters );
+
+  /** Set the rotational part of the transform. */
+  void SetRotation(TScalarType angle);
+  
+  /** This method computes the Jacobian matrix of the transformation.
+   * given point or vector, returning the transformed point or
+   * vector. The rank of the Jacobian will also indicate if the 
+   * transform is invertible at this point. */
+  const JacobianType & GetJacobian(const InputPointType  &point ) const;
+
+
+protected:
+  Euler2DTransform();
+  ~Euler2DTransform(){};
+  void PrintSelf(std::ostream &os, Indent indent) const;
+
+  /** Compute the components of the rotation matrix in the superclass. */
+  void ComputeMatrix(void);
+
+private:
+  Euler2DTransform(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+  TScalarType m_Angle; 
+
+}; //class Euler2DTransform
+
+
+}  // namespace itk
+
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkEuler2DTransform.txx"
+#endif
+
+#endif /* __itkEuler2DTransform_h */
---
+++ Code/Common/itkEuler2DTransform.txx
@@ -0,0 +1,137 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkEuler2DTransform.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef _itkEuler2DTransform_txx
+#define _itkEuler2DTransform_txx
+
+#include "itkEuler2DTransform.h"
+
+
+namespace itk
+{
+
+// Constructor with default arguments
+template <class TScalarType>
+Euler2DTransform<TScalarType>
+::Euler2DTransform()
+{
+ 
+}
+
+// Copy Constructor
+template <class TScalarType>
+Euler2DTransform<TScalarType>
+::Euler2DTransform( const Self & other )
+{
+  // call the superclass copy constructor
+}
+
+// Set Parameters
+template <class TScalarType>
+void
+Euler2DTransform<TScalarType>
+::SetParameters( const ParametersType & parameters )
+{
+  itkDebugMacro( << "Setting paramaters " << parameters );
+
+  // Set angles with parameters
+  m_Angle = parameters[0];
+ 
+  // Transfer the translation part
+  OffsetType offset;
+  for(unsigned int i=0; i < SpaceDimension; i++) 
+  {
+    offset[i] = parameters[i+1];
+  }
+
+  this->SetOffset( offset );
+
+  ComputeMatrix();
+
+  itkDebugMacro(<<"After setting paramaters ");
+}
+
+
+// Set Rotational Part
+template <class TScalarType>
+void
+Euler2DTransform<TScalarType>
+::SetRotation(TScalarType angle)
+{
+  m_Angle = angle;
+  ComputeMatrix();
+}
+
+
+// Compute the matrix
+template <class TScalarType>
+void
+Euler2DTransform<TScalarType>
+::ComputeMatrix( void )
+{
+  const double cx = cos(m_Angle);
+  const double sx = sin(m_Angle);
+
+  m_RotationMatrix[0][0]=cx;m_RotationMatrix[0][1]=sx;
+  m_RotationMatrix[1][0]=-sx;m_RotationMatrix[1][1]=cx;
+
+  m_InverseMatrix = m_RotationMatrix.GetTranspose();
+
+}
+
+
+// Set parameters
+template<class TScalarType>
+const Euler2DTransform<TScalarType>::JacobianType &
+Euler2DTransform<TScalarType>::
+GetJacobian( const InputPointType & p ) const
+{
+  // need to check if angles are in the right order
+  const double cx = cos(m_Angle);
+  const double sx = sin(m_Angle);
+
+  m_Jacobian.Fill(0.0);
+
+  m_Jacobian[0][0] = -sx; m_Jacobian[0][1] = cx;
+  m_Jacobian[1][0] = -cx; m_Jacobian[1][1] = -sx;
+
+  // compute derivatives for the translation part
+  unsigned int blockOffset = 2;  
+  for(unsigned int dim=0; dim < SpaceDimension; dim++ ) 
+  {
+    m_Jacobian[ dim ][ blockOffset + dim ] = 1.0;
+  }
+
+  return m_Jacobian;
+
+}
+  
+// Print self
+template<class TScalarType>
+void
+Euler2DTransform<TScalarType>::
+PrintSelf(std::ostream &os, Indent indent) const
+{
+
+  Superclass::PrintSelf(os,indent);
+  
+  os << indent << "Euler's angle: Angle=" << m_Angle  
+     << std::endl;
+}
+
+} // namespace
+
+#endif
