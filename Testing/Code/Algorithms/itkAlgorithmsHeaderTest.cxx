3eb1b7049c7414dcb455dea92877653f78ee6266
Luis Ibanez <luis.ibanez@kitware.com>
2002-06-04 23:41:52 -0400

    FIX: itkKmeansUnsupervisedClassifier.txx  removed.

--- Testing/Code/Algorithms/itkAlgorithmsHeaderTest.cxx
+++ Testing/Code/Algorithms/itkAlgorithmsHeaderTest.cxx
@@ -48,7 +48,6 @@ See COPYRIGHT.txt for copyright details.
 #include "itkImageToImageMetric.txx"
 #include "itkKLMRegionGrowImageFilter.txx"
 #include "itkKalmanLinearEstimator.h"
-#include "itkKmeansUnsupervisedClassifier.txx"
 #include "itkLevelSetImageFilter.txx"
 #include "itkLevelSetNeighborhoodExtractor.txx"
 #include "itkLevelSetVelocityNeighborhoodExtractor.txx"
