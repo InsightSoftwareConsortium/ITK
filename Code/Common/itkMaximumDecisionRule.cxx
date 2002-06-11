a519b05afaecc690e6ca1d5ce1d56e305c2c5a7a
Jisung Kim <bahrahm@yahoo.com>
2002-06-11 03:18:33 -0400

    ENH: new class that choose the index of the element with largest value

---
+++ Code/Common/itkMaximumDecisionRule.cxx
@@ -0,0 +1,34 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkMaximumDecisionRule.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "itkMaximumDecisionRule.h"
+
+namespace itk{ 
+
+
+void MaximumDecisionRule::PrintSelf(std::ostream& os, Indent indent) const 
+{ 
+  Superclass::PrintSelf(os, indent) ; 
+}
+ 
+} // end of namespace itk
+
+
+
+
+
+
+
---
+++ Code/Common/itkMaximumDecisionRule.h
@@ -0,0 +1,78 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkMaximumDecisionRule.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkMaximumDecisionRule_h
+#define __itkMaximumDecisionRule_h
+
+#include <vector>
+#include "itkDecisionRuleBase.h"
+
+namespace itk{ 
+
+/** \class MaxDecisionRule
+ *  \brief A Decision rule that choose the class that has maximum value
+ */
+ 
+class ITK_EXPORT MaximumDecisionRule : 
+      public DecisionRuleBase
+{
+public:
+ /** Standard class typedefs */ 
+  typedef MaximumDecisionRule Self ;
+  typedef DecisionRuleBase Superclass;
+  typedef SmartPointer<Self> Pointer;
+
+ /** Run-time type information (and related methods) */
+  itkTypeMacro(MaxDecisionRule, DecisionRuleBase);
+
+  /** Standard New() method support */
+  itkNewMacro(Self) ;
+
+  unsigned int Evaluate(std::vector< double > &discriminantScores) ;
+
+protected:
+  MaximumDecisionRule() {}
+  virtual ~MaximumDecisionRule() {}
+  void PrintSelf(std::ostream& os, Indent indent) const ;
+} ; // end of class
+
+inline unsigned int MaximumDecisionRule::Evaluate(std::vector< double > 
+                                                  &discriminantScores)
+{
+  double max = discriminantScores[0] ;
+  unsigned int maxIndex = 0 ;
+  unsigned int i ;
+  for (i = 1 ; i < discriminantScores.size() ; i++)
+    {
+      if (discriminantScores[i] > max) 
+        {
+          max = discriminantScores[i] ;
+          maxIndex = i ;
+        }
+    }
+  return maxIndex ;
+}
+
+} // end of namespace itk
+
+#endif
+
+
+
+
+
+
+
