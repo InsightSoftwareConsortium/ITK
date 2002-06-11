8954a610de4296a329476b87ad8cbc5174225706
Jisung Kim <bahrahm@yahoo.com>
2002-06-11 04:10:23 -0400

    ENH: new euclidean distance function for two vectors. It is also derived from MembershipFunctionBase, so it can be used for minimum distance type classification process.

---
+++ Code/Numerics/Statistics/itkEuclideanDistance.h
@@ -0,0 +1,70 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkEuclideanDistance.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkEuclideanDistance_h
+#define __itkEuclideanDistance_h
+
+#include "itkNumericTraits.h"
+#include "itkFixedArray.h"
+#include "itkDistanceMetric.h"
+
+namespace itk{ 
+namespace Statistics{
+
+template< class TVector >
+class ITK_EXPORT EuclideanDistance : 
+    public DistanceMetric< TVector >
+{
+public:
+  /** Standard "Self" typedef. */
+  typedef EuclideanDistance Self;
+  typedef DistanceMetric< TVector > Superclass;
+  typedef SmartPointer< Self > Pointer ; 
+
+  enum { VectorLength = TVector::Length } ; 
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(EuclideanDistance, DistanceMetric);
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self) ;
+
+  typedef typename TVector::ValueType ValueType ;
+
+  double Evaluate(const TVector &x) const ;
+  double Evaluate(const TVector &x1, const TVector &x2) const ;
+  double Evaluate(const ValueType &a, const ValueType &b) const ;
+
+protected:
+  EuclideanDistance() {}
+  virtual ~EuclideanDistance() {} 
+} ; // end of class
+
+} // end of namespace Statistics 
+} // end of namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkEuclideanDistance.txx"
+#endif
+
+#endif
+
+
+
+
+
+
+
---
+++ Code/Numerics/Statistics/itkEuclideanDistance.txx
@@ -0,0 +1,68 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkEuclideanDistance.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkEuclideanDistance_txx
+#define __itkEuclideanDistance_txx
+
+
+namespace itk{ 
+namespace Statistics{
+
+template< class TVector >
+inline double
+EuclideanDistance< TVector >
+::Evaluate(const TVector &x1, const TVector &x2) const
+{
+ double temp, distance = NumericTraits< double >::Zero ;
+
+ for(int i = 0 ; i < VectorLength ; i++ )
+ {
+   temp = x1[i] - x2[i] ;
+   distance += temp * temp ;
+ }
+
+ return sqrt(distance) ;
+}
+
+    
+template< class TVector >
+inline double
+EuclideanDistance< TVector >
+::Evaluate(const TVector &x) const
+{
+  return this->Evaluate(m_Origin, x) ;
+}
+
+template< class TVector >
+inline double
+EuclideanDistance< TVector >
+::Evaluate(const ValueType &a, const ValueType &b) const
+{
+  double temp = a - b ;
+  return sqrt(temp * temp) ;
+}
+
+} // end of namespace Statistics 
+} // end of namespace itk
+
+#endif
+
+
+
+
+
+
+
