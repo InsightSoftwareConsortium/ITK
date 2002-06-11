3333bdc74373045abca81a1c377d4b95229c269a
Jisung Kim <bahrahm@yahoo.com>
2002-06-11 04:21:17 -0400

    ENH: new class that alculates sample mean where each measurement vector has its associated weight value

---
+++ Code/Numerics/Statistics/itkWeightedMeanCalculator.h
@@ -0,0 +1,91 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkWeightedMeanCalculator.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#ifndef __itkWeightedMeanCalculator_h
+#define __itkWeightedMeanCalculator_h
+
+#include <vnl/vnl_vector.h>
+#include <vnl/vnl_matrix.h>
+
+#include "itkArray.h"
+#include "itkVector.h"
+#include "itkSampleAlgorithmBase.h"
+
+namespace itk{ 
+namespace Statistics{
+  
+/** \class WeightedMeanCalculator
+ * \brief calculates sample mean where each measurement vector has
+ * associated weight value
+ *
+ * You plug in the target sample data using SetSample method. Then call
+ * the Update method to run the alogithm.
+ *
+ * \sa MeanCalculator SampleAlgorithmBase
+ */
+
+template< class TSample >
+class WeightedMeanCalculator :
+    public SampleAlgorithmBase< TSample >
+{
+public:
+  /**Standard class typedefs. */
+  typedef WeightedMeanCalculator Self;
+  typedef SampleAlgorithmBase< TSample > Superclass ;
+  typedef SmartPointer<Self> Pointer;
+
+  /**Standard Macros */
+  itkTypeMacro(WeightedMeanCalculator, SampleAlgorithmBase);
+  itkNewMacro(Self) ;
+  
+  enum {MeasurementVectorSize = TSample::MeasurementVectorSize } ;
+
+  /** Sample typedefs alias */
+  typedef typename Superclass::SampleType SampleType ;
+  typedef typename Superclass::SamplePointer SamplePointer ;
+  
+  typedef Array< double > WeightArrayType ;
+  /** Typedef for the mean output */
+  typedef Vector< double, MeasurementVectorSize > OutputType ;
+  
+  void SetWeights(WeightArrayType* array) ;
+  WeightArrayType* GetWeights() ;
+
+  OutputType* GetOutput() ;
+
+protected:
+  WeightedMeanCalculator() ;
+  virtual ~WeightedMeanCalculator() {}
+  void PrintSelf(std::ostream& os, Indent indent) const;
+
+  /** Calculates the mean and save it */
+  void GenerateData() ;
+
+private:
+  WeightArrayType* m_Weights ;
+  OutputType m_Output ;
+} ; // end of class
+    
+} // end of namespace Statistics 
+} // end of namespace itk 
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkWeightedMeanCalculator.txx"
+#endif
+
+#endif
+
---
+++ Code/Numerics/Statistics/itkWeightedMeanCalculator.txx
@@ -0,0 +1,95 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkWeightedMeanCalculator.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkWeightedMeanCalculator_txx
+#define __itkWeightedMeanCalculator_txx
+
+namespace itk{ 
+namespace Statistics{
+
+template< class TSample >
+WeightedMeanCalculator< TSample >
+::WeightedMeanCalculator()
+{
+}
+
+template< class TSample >
+void
+WeightedMeanCalculator< TSample >
+::SetWeights(WeightArrayType* array)
+{
+  m_Weights = array ;
+}
+
+template< class TSample >
+WeightedMeanCalculator< TSample >::WeightArrayType*
+WeightedMeanCalculator< TSample >
+::GetWeights()
+{
+  return m_Weights ;
+}
+
+template< class TSample >
+void
+WeightedMeanCalculator< TSample >
+::GenerateData() 
+{
+  m_Output.Fill(0.0) ;
+
+  typename TSample::Iterator iter = this->GetSample()->Begin() ;
+  typename TSample::Iterator end = this->GetSample()->End() ;
+  double totalWeight = 0.0 ;
+  double weight = 0.0 ;
+  unsigned int dim = 0 ;
+  int measurementVectorIndex = 0 ;
+
+  while (iter != end)
+    {
+      weight = iter.GetFrequency() * (*m_Weights)[measurementVectorIndex] ;
+      totalWeight += weight ;
+      for (dim = 0 ; dim < MeasurementVectorSize ; dim++)
+        {
+          m_Output[dim] += iter.GetMeasurementVector()[dim] * weight ;
+        }
+      ++measurementVectorIndex ;
+      ++iter ;
+    }
+
+  m_Output /= totalWeight ;
+}
+
+template< class TSample >
+WeightedMeanCalculator< TSample >::OutputType*
+WeightedMeanCalculator< TSample >
+::GetOutput()
+{
+  return &m_Output ;
+}
+
+template< class TSample >
+void
+WeightedMeanCalculator< TSample >
+::PrintSelf(std::ostream& os, Indent indent) const
+{
+  Superclass::PrintSelf(os,indent);
+  
+  std::cout << "Output: " << m_Output << std::endl ;
+}
+  } // end of namespace Statistics 
+} // end of namespace itk
+
+#endif
+
