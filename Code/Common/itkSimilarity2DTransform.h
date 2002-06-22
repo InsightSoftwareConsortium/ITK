0a97ebb5530d43aaada73b3212216e78a2d6ac3e
Luis Ibanez <luis.ibanez@kitware.com>
2002-06-21 21:26:21 -0400

    Similarity Transform in 2D (Rotation/Scale/Translation)

---
+++ Code/Common/itkSimilarity2DTransform.h
@@ -0,0 +1,140 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkSimilarity2DTransform.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#ifndef __itkSimilarity2DTransform_h
+#define __itkSimilarity2DTransform_h
+
+#include <iostream>
+#include "itkRigid2DTransform.h"
+
+namespace itk
+{
+
+/** \brief Similarity2DTransform of a vector space (e.g. space coordinates)
+ *
+ * This transform applies a rotation, scale and translation to the space
+ * given one angle for rotation, a homogeneous scale and a 2D offset for translation. 
+ *
+ * \ingroup Transforms
+ */
+template < class TScalarType=double >    // Data type for scalars (float or double)
+class ITK_EXPORT Similarity2DTransform : 
+            public Rigid2DTransform< TScalarType > 
+{
+public:
+  /** Standard class typedefs. */
+  typedef Similarity2DTransform Self;
+  typedef Rigid2DTransform< TScalarType >   Superclass;
+  typedef SmartPointer<Self>        Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+    
+  /** New macro for creation of through a Smart Pointer. */
+  itkNewMacro( Self );
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro( Similarity2DTransform, Rigid2DTransform );
+
+  /** Dimension of parameters. */
+  enum { SpaceDimension = 2, 
+         ParametersDimension = 4 };
+
+  /** Scalar type. */
+  typedef typename Superclass::ScalarType  ScalarType;
+
+  /** Parameters type. */
+  typedef typename Superclass::ParametersType  ParametersType;
+
+  /** Jacobian type. */
+  typedef typename Superclass::JacobianType  JacobianType;
+  
+  /** Offset type. */
+  typedef typename Superclass::OffsetType  OffsetType;
+
+  /** Point type. */
+  typedef typename Superclass::InputPointType   InputPointType;
+  typedef typename Superclass::OutputPointType  OutputPointType;
+  
+  /** Vector type. */
+  typedef typename Superclass::InputVectorType   InputVectorType;
+  typedef typename Superclass::OutputVectorType  OutputVectorType;
+  
+  /** CovariantVector type. */
+  typedef typename Superclass::InputCovariantVectorType   InputCovariantVectorType;
+  typedef typename Superclass::OutputCovariantVectorType  OutputCovariantVectorType;
+  
+  /** VnlVector type. */
+  typedef typename Superclass::InputVnlVectorType   InputVnlVectorType;
+  typedef typename Superclass::OutputVnlVectorType  OutputVnlVectorType;
+  
+  /** Set the transformation from a container of parameters
+   * This is typically used by optimizers.
+   * There are 3 parameters. The first one represents the
+   * rotation and the last two represents the offset. */
+  void SetParameters( const ParametersType & parameters );
+
+  /** Set the rotational part of the transform. */
+  void SetRotation(TScalarType angle);
+
+  /** Set the Scale part of the transform. */
+  void SetScale(TScalarType scale);
+
+  /** Get the parameters that uniquely define the transform
+   * This is typically used by optimizers.
+   * There are 4 parameters. The first one represents the
+   * rotation, the second one the scale and the last 
+   * two represent the offset. */
+  const ParametersType & GetParameters( void ) const;
+
+ 
+  /** This method computes the Jacobian matrix of the transformation.
+   * given point or vector, returning the transformed point or
+   * vector. The rank of the Jacobian will also indicate if the 
+   * transform is invertible at this point. */
+  const JacobianType & GetJacobian(const InputPointType  &point ) const;
+
+  /** Set the transformation to an Identity
+   * This sets the matrix to identity and the Offset to null. */
+  virtual void SetIdentity( void );
+
+
+protected:
+  Similarity2DTransform();
+  ~Similarity2DTransform(){};
+  void PrintSelf(std::ostream &os, Indent indent) const;
+
+  /** Compute the components of the rotation matrix in the superclass. */
+  void ComputeMatrix(void);
+
+private:
+  Similarity2DTransform(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+  TScalarType m_Angle; 
+
+  TScalarType m_Scale; 
+
+}; //class Similarity2DTransform
+
+
+}  // namespace itk
+
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkSimilarity2DTransform.txx"
+#endif
+
+#endif /* __itkSimilarity2DTransform_h */
---
+++ Code/Common/itkSimilarity2DTransform.txx
@@ -0,0 +1,181 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkSimilarity2DTransform.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef _itkSimilarity2DTransform_txx
+#define _itkSimilarity2DTransform_txx
+
+#include "itkSimilarity2DTransform.h"
+
+
+namespace itk
+{
+
+// Constructor with default arguments
+template <class TScalarType>
+Similarity2DTransform<TScalarType>
+::Similarity2DTransform()
+{
+ 
+}
+
+// Copy Constructor
+template <class TScalarType>
+Similarity2DTransform<TScalarType>
+::Similarity2DTransform( const Self & other )
+{
+  // call the superclass copy constructor
+}
+
+// Set Parameters
+template <class TScalarType>
+void
+Similarity2DTransform<TScalarType>
+::SetParameters( const ParametersType & parameters )
+{
+  itkDebugMacro( << "Setting paramaters " << parameters );
+
+  // Set angles with parameters
+  m_Angle = parameters[0];
+ 
+  // Set angles with parameters
+  m_Scale = parameters[1];
+ 
+  // Transfer the translation part
+  OffsetType offset;
+  for(unsigned int i=0; i < SpaceDimension; i++) 
+  {
+    offset[i] = parameters[i+2];
+  }
+
+  this->SetOffset( offset );
+
+  ComputeMatrix();
+
+  itkDebugMacro(<<"After setting paramaters ");
+}
+
+
+// Get Parameters
+template <class TScalarType>
+const typename Similarity2DTransform<TScalarType>::ParametersType &
+Similarity2DTransform<TScalarType>
+::GetParameters( void ) const
+{
+  itkDebugMacro( << "Getting parameters ");
+
+  m_Parameters[0] = m_Angle;
+  m_Parameters[1] = m_Scale;
+ 
+  // Transfer the translation part
+  OffsetType offset = this->GetOffset();
+  for(unsigned int i=0; i < SpaceDimension; i++) 
+  {
+    m_Parameters[i+2] = offset[i];
+  }
+
+  itkDebugMacro(<<"After getting parameters " << m_Parameters );
+
+  return m_Parameters;
+}
+
+
+// Set Rotational Part
+template <class TScalarType>
+void
+Similarity2DTransform<TScalarType>
+::SetRotation(TScalarType angle)
+{
+  m_Angle = angle;
+  ComputeMatrix();
+}
+
+
+// Compute the matrix
+template <class TScalarType>
+void
+Similarity2DTransform<TScalarType>
+::ComputeMatrix( void )
+{
+  const double cx = cos(m_Angle);
+  const double sx = sin(m_Angle);
+
+  m_RotationMatrix[0][0]=cx;m_RotationMatrix[0][1]=sx;
+  m_RotationMatrix[1][0]=-sx;m_RotationMatrix[1][1]=cx;
+
+  m_InverseMatrix = m_RotationMatrix.GetTranspose();
+
+}
+
+
+// Set parameters
+template<class TScalarType>
+const Similarity2DTransform<TScalarType>::JacobianType &
+Similarity2DTransform<TScalarType>::
+GetJacobian( const InputPointType & p ) const
+{
+  // need to check if angles are in the right order
+  const double cx = cos(m_Angle);
+  const double sx = sin(m_Angle);
+
+  m_Jacobian.Fill(0.0);
+
+  m_Jacobian[0][0] = -sx; m_Jacobian[0][1] = cx;
+  m_Jacobian[1][0] = -cx; m_Jacobian[1][1] = -sx;
+
+  // compute derivatives for the translation part
+  unsigned int blockOffset = 2;  
+  for(unsigned int dim=0; dim < SpaceDimension; dim++ ) 
+  {
+    m_Jacobian[ dim ][ blockOffset + dim ] = 1.0;
+  }
+
+  return m_Jacobian;
+
+}
+
+ 
+// Set Identity
+template <class TScalarType>
+void
+Similarity2DTransform<TScalarType>
+::SetIdentity(void)
+{
+  m_Angle = NumericTraits< TScalarType >::Zero;
+  m_Scale = static_cast< TScalarType >( 1.0f );
+  ComputeMatrix();
+  OffsetType offset;
+  offset.Fill( NumericTraits<TScalarType>::Zero );
+  this->SetOffset( offset );
+}
+
+
+ 
+// Print self
+template<class TScalarType>
+void
+Similarity2DTransform<TScalarType>::
+PrintSelf(std::ostream &os, Indent indent) const
+{
+
+  Superclass::PrintSelf(os,indent);
+  
+  os << indent << "Euler's angle: Angle=" << m_Angle  
+     << std::endl;
+}
+
+} // namespace
+
+#endif
