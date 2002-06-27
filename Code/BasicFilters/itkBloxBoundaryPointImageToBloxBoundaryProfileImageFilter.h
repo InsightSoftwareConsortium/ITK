97b774c8e3b03ef5179f538dca1177c422d755fc
Robert Tamburo <rjtst21@pitt.edu>
2002-06-27 16:42:09 -0400

    no message

---
+++ Code/BasicFilters/itkBloxBoundaryPointImageToBloxBoundaryProfileImageFilter.h
@@ -0,0 +1,197 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkBloxBoundaryPointImageToBloxBoundaryProfileImageFilter.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkBloxBoundaryPointImageToBloxBoundaryProfileImageFilter_h
+#define __itkBloxBoundaryPointImageToBloxBoundaryProfileImageFilter_h
+
+#include "itkBloxBoundaryPointImage.h"
+#include "itkBloxBoundaryProfileImage.h"
+#include "itkImageToImageFilter.h"
+#include "itkSize.h"
+
+namespace itk
+{
+
+/** \class BloxBoundaryPointImageToBloxBoundaryProfileImageFilter
+ * \brief Converts a BloxImage of BloxBoundaryPoints to a BloxImage of BloxBoundaryProfiles
+ *
+ * Samples the boundary point image to produce
+ * a BloxBoundaryProfileImage
+ * 
+ * References:
+ * Robert J. Tamburo, George D. Stetten: Gradient-Oriented Profiles 
+ * for Boundary Parameterization and Their Application to Core Atoms 
+ * Towards Shape Analysis. International Journal of Image and 
+ * Graphics 1(4): 659-680 (2001) 
+ *
+ * Robert J.Tamburo and George D.Stetten,M.D.,Ph.D.: Gradient-Oriented 
+ * Profiles for Unsupervised Boundary Classification. Proceedings of 
+ * the 29th Applied Imagery Pattern Recognition Workshop. October 2000: 
+ * Washington, D.C.
+ *
+ * \ingroup ImageEnhancement
+ */
+template< typename TSourceImage >
+class ITK_EXPORT itkBloxBoundaryPointImageToBloxBoundaryProfileImageFilter :
+public ImageToImageFilter<TSourceImage,BloxBoundaryProfileImage<TSourceImage::ImageDimension> >
+{
+public:
+  /** Standard class typedefs */
+  typedef itkBloxBoundaryPointImageToBloxBoundaryProfileImageFilter  Self;
+  typedef ImageToImageFilter<TSourceImage,BloxBoundaryProfileImage<TSourceImage::ImageDimension> >  Superclass;
+  typedef SmartPointer<Self>   Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+
+  /** Method for creation through the object factory */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods) */
+  itkTypeMacro(itkBloxBoundaryPointImageToBloxBoundaryProfileImageFilter, ImageToImageFilter);
+
+  /** Number of dimensions */
+  enum {NDimensions = TSourceImage::ImageDimension};
+
+  /** Typedef for boundary point image */
+  typedef BloxBoundaryPointImage<NDimensions> BoundaryPointImageType;
+  typedef typename BoundaryPointImageType::Pointer BoundaryPointImagePointer;
+  typedef typename BoundaryPointImageType::RegionType BoundaryPointImageRegionType; 
+  typedef typename BoundaryPointImageType::PixelType BoundaryPointImagePixelType; 
+  typedef typename BoundaryPointImageType::ConstPointer BoundaryPointImageConstPointer;
+
+  /** Typedef for blurred source image */
+  typedef TSourceImage SourceImageType;
+  typedef typename SourceImageType::Pointer SourceImagePointer;
+  typedef typename SourceImageType::RegionType SourceImageRegionType; 
+  typedef typename SourceImageType::PixelType SourceImagePixelType; 
+  typedef typename SourceImageType::ConstPointer SourceImageConstPointer;
+
+  /** Typedef for profile image */
+  typedef BloxBoundaryProfileImage<NDimensions> OutputImageType;
+  typedef typename OutputImageType::Pointer OutputImagePointer;
+  typedef typename OutputImageType::RegionType OutputImageRegionType;
+  typedef typename OutputImageType::PixelType OutputImagePixelType;
+
+  /** Image index typedef */
+  typedef typename BloxBoundaryProfileImage<NDimensions>::IndexType IndexType;
+
+  /** Image pixel value typedef */
+  typedef typename BloxBoundaryProfileImage<NDimensions>::PixelType PixelType;
+
+  /** The type of vector used to convert between physical and blox space */
+  typedef Point<double, NDimensions> TPositionType;
+
+  /** Vector typedef */
+  typedef typename TPositionType::VectorType TVectorType;
+
+  /** Set the blurred original image */
+   void SetInput1( const SourceImageType * image1 );
+
+  /** Set the boundary point image */
+   void SetInput2( const BoundaryPointImageType * image2 );
+
+  /** Find maximum in accumulator */
+  double FindAccumulatorMaximum();
+  
+  /** Find minimum in accumulator */
+  double FindAccumulatorMinimum();
+
+  /** Find boundary profiles from input images and store them */
+  void FindBoundaryProfiles();
+
+  /** Find boundary profiles given a specific boundary point */
+  void FindBoundaryProfilesAtBoundaryPoint(BloxBoundaryPointItem<NDimensions>* pItem);
+  
+  /** Add weighted pixel value to appropriate bin number in splat accumulator and normalizer */
+  bool AddSplatToAccumulatorAndNormalizer(int binNumber, double weight, double sourcePixelValue);
+
+  /** Normalize the splat accumulator by the normalizer */
+  void NormalizeSplatAccumulator();
+
+  /** Fit the boundary profile to a cumulative Gaussian */
+  int FitProfile();
+  
+  /** Parameters required to find boundary profiles */
+  void Initialize(double setUniqueAxis, double setSymmetricAxes, unsigned int numberOfBins,
+                  unsigned int splatMethod, unsigned int spaceDimension)
+    {
+    m_UniqueAxis = setUniqueAxis;
+    m_SymmetricAxes = setSymmetricAxes;
+    m_NumberOfBins = numberOfBins;
+    m_SplatMethod = splatMethod;
+    m_SpaceDimension = spaceDimension;
+
+    m_Accumulator = new double[m_NumberOfBins];
+    m_Normalizer = new double[m_NumberOfBins];
+    }
+
+protected:
+  itkBloxBoundaryPointImageToBloxBoundaryProfileImageFilter();
+  virtual ~itkBloxBoundaryPointImageToBloxBoundaryProfileImageFilter() {};
+  void PrintSelf(std::ostream& os, Indent indent) const;
+
+  /** Method for forming the BloxBoundaryProfileImage */
+  void GenerateData();
+
+private:
+  itkBloxBoundaryProfileImageFilter(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+  typename BoundaryPointImagePointer m_BoundaryPointImage;
+  const double* m_BPImageOrigin;
+  const double* m_BPImageSpacing;
+    
+  typename TSourceImage::Pointer m_OriginalImage;
+  const double* m_OriginalImageOrigin;
+  const double* m_OriginalImageSpacing;
+   
+  /** Length of major axis of ellipsoid */
+  double m_UniqueAxis;
+
+  /** Lengths of minor axes of ellipsoid */
+  double m_SymmetricAxes;
+
+  /** Number of bins in splat accumulator and normalizer */
+  unsigned int m_NumberOfBins;
+
+  /** Type of method to splat. 0: Gaussian 1: Triangle  */
+  unsigned int m_SplatMethod;
+
+  /** Count of number of boundary profiles found */
+  unsigned long int m_NumBoundaryProfiles;
+
+  /** Weight pixel values */
+  double * m_Accumulator;
+  
+  /** Count of pixels added to accumulator */
+  double * m_Normalizer;
+
+  /** Normalized accumulator */
+  double * m_NormalizedAccumulator;
+
+  /** Final parameters delivered by FitProfile() */
+  double * m_FinalParameters;
+
+  /** Number of parameters in cost function */
+  unsigned int m_SpaceDimension;
+};
+
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkBloxBoundaryPointImageToBloxBoundaryProfileImageFilter.txx"
+#endif
+
+#endif
