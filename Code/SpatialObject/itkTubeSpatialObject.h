577b3df989a99ee39ba236414f55b500ea4fc9e8
Julien Jomier <julien.jomier@kitware.com>
2002-06-29 15:18:51 -0400

    FIX: missing typename

--- Code/SpatialObject/itkTubeSpatialObject.h
+++ Code/SpatialObject/itkTubeSpatialObject.h
@@ -61,7 +61,7 @@ public:
     typedef typename TubePointType::Pointer             TubePointPointer; 
     typedef std::list < TubePointPointer >              PointListType;
     typedef PointListType *                             PointListPointer;
-    typedef Superclass::PointType                       PointType;
+    typedef typename Superclass::PointType              PointType;
     typedef VectorContainer<unsigned long,PointType>    PointContainerType;
     typedef SmartPointer<PointContainerType>            PointContainerPointer;
 
