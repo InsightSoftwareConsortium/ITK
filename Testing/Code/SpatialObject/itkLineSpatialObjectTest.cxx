05afb053620f906228fc2954402932319285fd4d
Bill Lorensen <bill.lorensen@gmail.com>
2002-07-07 20:28:00 -0400

    ERR: MSDEV warning 4786.

--- Testing/Code/SpatialObject/itkLineSpatialObjectTest.cxx
+++ Testing/Code/SpatialObject/itkLineSpatialObjectTest.cxx
@@ -14,6 +14,9 @@
      PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
 
 /**
  * This is a test file for the itkLineSpatialObject class.
