413fca874f37576a1a95c2b5804301218d740f72
Julien Jomier <julien.jomier@kitware.com>
2002-07-13 13:16:35 -0400

    ENH: Dicom Reader Class

---
+++ Code/IO/itkDicomImageIO.h
@@ -0,0 +1,114 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkDicomImageIO.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $1.0$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkDicomImageIO_h
+#define __itkDicomImageIO_h
+
+#include <fstream>
+#include "itkImageIOBase.h"
+
+//this structure is used to creat a table of Tag
+typedef struct Bal
+{
+  char Balise1 [2]; 
+  char Balise2 [2];
+  int compt;
+} Tag;
+
+
+
+namespace itk
+{
+
+/** \brief Read DicomImage file format. */
+class ITK_EXPORT DicomImageIO : public ImageIOBase
+{
+public:
+  /** Standard class typedefs. */
+  typedef DicomImageIO            Self;
+  typedef ImageIOBase  Superclass;
+  typedef SmartPointer<Self>  Pointer;
+  
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(DicomImageIO, Superclass);
+
+  /*-------- This part of the interfaces deals with reading data. ----- */
+
+  /** Determine the file type. Returns true if this ImageIO can read the
+   * file specified. */
+  virtual bool CanReadFile(const char*) ;
+
+  /** Set the spacing and dimension information for the set filename. */
+  virtual void ReadImageInformation();
+  
+  /** Get the type of the pixel.  */
+  virtual const std::type_info& GetPixelType() const;
+
+  /** Reads the data from disk into the memory buffer provided. */
+  virtual void Read(void* buffer);
+
+  /** Compute the size (in bytes) of the components of a pixel. For
+   * example, and RGB pixel of unsigned char would have a 
+   * component size of 1 byte. */
+  virtual unsigned int GetComponentSize() const;
+  
+  /*-------- This part of the interfaces deals with writing data. ----- */
+
+  /** Determine the file type. Returns true if this ImageIO can write the
+   * file specified. */
+  virtual bool CanWriteFile(const char*);
+
+  /** Set the spacing and dimension information for the set filename. */
+  virtual void WriteImageInformation();
+  
+  /** Writes the data to disk from the memory buffer provided. Make sure
+   * that the IORegions has been set properly. */
+  virtual void Write(const void* buffer);
+
+
+  DicomImageIO();
+  ~DicomImageIO();
+  void PrintSelf(std::ostream& os, Indent indent) const;
+  
+private:
+  DicomImageIO(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+  void SwapBytesIfNecessary(void* buffer, unsigned long numberOfPixels);
+ 
+  /** This function build a list of tags required to read data in a dicom file*/
+  bool DicomImageIO::CheckTagTable(std::ifstream & inputStream, std::list <Tag> &TableOfTags) const;
+  /** This function put the cursor of the stream on the first byte after the last balise
+  /*  at the end of the header */
+  bool DicomImageIO::GoToTheEndOfHeader(std::ifstream & inputStream,long int& i,Tag & tagcurrent) const;
+  /** This function put the cursor of the stream on the first byte after the given balise */
+  bool DicomImageIO::GoToTag(std::ifstream & inputStream,int balise1,int balise2,long int & i,
+                                                            long int & max,Tag & tagcurrent) const;
+  /** return true if tag([0][1]) = tagvalue1 and tag([2][3]) = tagvalue2*/ 
+  bool DicomImageIO::IfEqual(unsigned char * tag, int tagvalue1, int tagvalue2) const;
+  
+  std::ifstream   m_Ifstream;
+
+  std::ofstream   m_Ofstream;
+  
+};
+
+} // end namespace itk
+
+#endif // __itkDicomImageIO_h
