08cfa8393485653b5d8e613189eec921f8b9787a
Brad King <brad.king@kitware.com>
2002-08-22 09:33:00 -0400

    ERR: operator != was missing from Modulus3 functor.

--- Code/BasicFilters/itkTernaryMagnitudeImageFilter.h
+++ Code/BasicFilters/itkTernaryMagnitudeImageFilter.h
@@ -43,6 +43,10 @@ namespace Function {
                                const TInput2 & B,
                                const TInput3 & C)
       { return (TOutput) sqrt( (double)(A*A + B*B + C*C) ); }
+    bool operator != (const Modulus3&) const
+      {
+      return false;
+      }
   }; 
 }
 
