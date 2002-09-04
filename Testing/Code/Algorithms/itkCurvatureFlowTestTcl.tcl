f966d0624225f94d3d38ebfbc94ef076023bd791
Brad King <brad.king@kitware.com>
2002-09-04 11:07:35 -0400

    ENH: Added image-based test for CurvatureFlowImageFilter.

--- Testing/Code/Algorithms/itkCurvatureFlowTestTcl.tcl
+++ Testing/Code/Algorithms/itkCurvatureFlowTestTcl.tcl
@@ -1,49 +1,72 @@
-# Load the packages with wrappers we need.
-package require ITKCommonTcl
-package require ITKBasicFiltersTcl
-package require ITKAlgorithmsTcl
+wm withdraw .
+package require itktesting
 
-# Get the image from a random image source.
-set source_ptr [itkRandomImageSourceitkImage_float_2 New]
-set source [$source_ptr ->]
-  $source SetSize {64 64}
-  $source SetMin 0
-  $source SetMax 1
+set l [file split [file rootname [info script]]]
+set name [lindex $l [expr [llength $l] - 1]]
+set dir "Algorithms"
 
-# Create a Tcl callback setup as a progress event.
-set progressEvent_ptr [itkTclCommand New]
-set progressEvent [$progressEvent_ptr ->]
-  $progressEvent SetInterpreter [wrap::Interpreter]
-  $progressEvent SetCommandString {
-    set progress [$denoiser GetProgress]
-    set percent [expr {$progress * 100}]
-    puts "Progress: $percent%"
-  }
+set reader [itk::create ImageFileReaderF2]
+  $reader SetFileName "${ITK_TEST_INPUT}/cthead1.png"
 
-set progressEventId [itkCommand GetEventIdFromString "ProgressEvent"]
+set cf [itk::create CurvatureFlowImageFilterF2F2]
+  $cf SetInput [$reader GetOutput]
+  $cf SetTimeStep 0.25
+  $cf SetNumberOfIterations 10
 
-# Create the CurvatureFlowImageFilter for Image<float, 2>.
-set denoiser_ptr [itkCurvatureFlowImageFilteritkImage_float_2itkImage_float_2 New]
-set denoiser [$denoiser_ptr ->]
-  $denoiser SetInput [$source GetOutput]
-  $denoiser SetTimeStep 0.15
-  $denoiser SetNumberOfIterations 8
-  $denoiser AddObserver $progressEventId $progressEvent
-  $denoiser Update
+set cfss [itk::create ShiftScaleImageFilterF2US2]
+  $cfss SetInput [$cf GetOutput]
+  $cfss SetShift 0.7
+  $cfss SetScale 0.9
 
-# End the pipeline with a VTKImageWriter to write out the image.
-set writer_ptr [itkVTKImageWriteritkImage_float_2 New]
-set writer [$writer_ptr ->]
-  $writer SetInput [$denoiser GetOutput]
-  $writer SetFileName itkCurvatureFlowTestTcl_out.vtk
-  $writer Write
+set valid [itk::create ImageFileReaderUS2]
+  $valid SetFileName "${ITK_TEST_BASELINE}/$dir/$name.png"
 
-# When the variables writer_ptr, denoiser_ptr, source_ptr, and
-# progressEvent_ptr are destroyed at the end of the program, the smart
-# pointers to which they refer are destroyed.  This causes the
-# reference counts on the objects to be decremented, thus cleaning up
-# the program automatically.
+set diff [itk::create DifferenceImageFilterUS2]
+  $diff SetValidInput [$valid GetOutput]
+  $diff SetTestInput [$cfss GetOutput]
+  $diff SetToleranceRadius 1
+  $diff SetDifferenceThreshold 0
 
-# The variables writer, denoiser, progressEvent, and source are also
-# destroyed, but since they refer only to pointers, no destructors are
-# called.
+$diff Update
+
+set meanDiff [$diff GetMeanDifference]
+set totalDiff [$diff GetTotalDifference]
+puts "<DartMeasurement name=\"MeanDifference\" type=\"numeric/double\">$meanDiff</DartMeasurement>"
+puts "<DartMeasurement name=\"TotalDifference\" type=\"numeric/double\">$totalDiff</DartMeasurement>"
+
+if {$meanDiff > 0.1} {
+  set convert [itk::create ShiftScaleImageFilterUS2UC2]
+  set rescale [itk::create RescaleIntensityImageFilterUS2UC2]
+    $rescale SetInput [$diff GetOutput]
+    $rescale SetOutputMinimum 0
+    $rescale SetOutputMaximum 255
+
+  set io [itk::create PNGImageIO]
+    $io SetUseCompression 1
+    $io SetCompressionLevel 9
+
+  set writer [itk::create ImageFileWriterUC2]
+    $writer SetImageIO $io
+    $writer SetInput [$convert GetOutput]
+
+    $writer SetFileName "${ITK_TEST_OUTPUT}/$name.test.png"
+    $convert SetInput [$cfss GetOutput]
+    $writer Write
+
+    $writer SetFileName "${ITK_TEST_OUTPUT}/$name.diff.png"
+    $writer SetInput [$rescale GetOutput]
+    $writer Write
+
+  file copy -force "${ITK_TEST_BASELINE}/$dir/$name.png" \
+                   "${ITK_TEST_OUTPUT}/$name.valid.png"
+
+  puts "<DartMeasurementFile name=\"TestImage\" type=\"image/png\">${ITK_TEST_OUTPUT}/$name.test.png</DartMeasurementFile>"
+  puts "<DartMeasurementFile name=\"DifferenceImage\" type=\"image/png\">${ITK_TEST_OUTPUT}/$name.diff.png</DartMeasurementFile>"
+  puts "<DartMeasurementFile name=\"ValidImage\" type=\"image/png\">${ITK_TEST_OUTPUT}/$name.valid.png</DartMeasurementFile>"
+  puts "<DartMeasurement name=\"DifferenceShift\" type=\"numeric/double\">[$rescale GetShift]</DartMeasurement>"
+  puts "<DartMeasurement name=\"DifferenceScale\" type=\"numeric/double\">[$rescale GetScale]</DartMeasurement>"
+
+  exit 1
+}
+
+exit 0
