00b040862f8ddedca58d6a592e260b20011bc615
Jisung Kim <bahrahm@yahoo.com>
2002-09-21 05:15:45 -0400

    ENH: A new class that calculates the goodness-of-fit statstics for multivarate mixture model

---
+++ Code/Numerics/Statistics/itkGoodnessOfFitMixtureModelCostFunction.h
@@ -0,0 +1,150 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkGoodnessOfFitMixtureModelCostFunction.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkGoodnessOfFitMixtureModelCostFunction_h
+#define __itkGoodnessOfFitMixtureModelCostFunction_h
+
+#include "itkSingleValuedCostFunction.h"
+#include "itkHistogram.h"
+#include "itkGoodnessOfFitComponentBase.h"
+#include "itkGoodnessOfFitFunctionBase.h"
+#include "itkFunctionBase.h"
+
+namespace itk{ 
+namespace Statistics{
+
+/** \class GoodnessOfFitMixtureModelCostFunction 
+ *  \brief calculates the goodness-of-fit statstics for multivarate 
+ *  mixture model 
+ *
+ * The goodness-of-fit statistics for a single model is discrepancy
+ * between the observed frequency and the expected frequency. 
+ * To reduce computational load of multivariate case, this class
+ * uses projective method. 
+ * 
+ * The projective multivariate goodness-of-fit statistics calculation follows 
+ * the following steps:
+ *   
+ *   1) creates a subsample that includes the measurement vectors that fall 
+ *      in a spherical kernel.
+ *   2) finds the base axes determined by the eigen vectors of the covariance 
+ *      matrix.
+ *   3) project the subsample on to one of the base axes (from step 2)
+ *   4) calculates the observed frequencies (in an 1D Histogram object) after 
+ *      projection (step 3) and the expected frequencies (in an 1D Histogram 
+ *      object)
+ *   5) calculates the discrepancy between the observed histogram and 
+ *      the expected histogram using a goodness-of-fit statistics
+ *   6) repeat step 3) - 5) and sum the goodness-of-fit values
+ *    
+ * For a mixture model, the above procedure is applied independently for each 
+ * model (module). The sum of the goodness-of-fit values of models is the
+ * goodness-of-fit statistics for the mixture model.
+ *
+ * The step 1) - 4) is done by the subclasses of GoodnessOfFitComponentBase, and
+ * the step 5) is done by the subclasses of GoodnessOfFitFunctionBase.
+ *
+ * To see how this class interacts GoodnessOfFitComponentBase objects and 
+ * GoodnessOfFitFunctionBase objects, please look at the implementation of
+ * the GetValue method of this class.
+ *
+ * Better fit means smaller goodness-of-fit value in this implementation. 
+ * This class is following the SingleValuedCostFunction interfaces so that
+ * users can uses this function with any subclasses of 
+ * SingleValuedNonLinearOptimizer class as long as they do not use
+ * GetDerivative and GetValueAndDerivative methods.
+ *
+ * \sa GoodnessOfFitFunctionBase, GoodnessOfFitComponentBase, 
+ * SingleValuedCostFunction, SingleValuedNonLinearOptimizer
+ */
+
+template< class TInputSample >
+class ITK_EXPORT GoodnessOfFitMixtureModelCostFunction 
+  : public SingleValuedCostFunction 
+{
+public:
+  /** Standard class typedefs */
+  typedef GoodnessOfFitMixtureModelCostFunction Self;
+  typedef SingleValuedCostFunction Superclass;
+  typedef SmartPointer< Self > Pointer;
+  typedef SmartPointer< const Self > ConstPointer;
+  
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(GoodnessOfFitMixtureModelCostFunction, SingleValuedCostFunction) ;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self) ;
+
+  typedef TInputSample InputSampleType ;
+  enum { MeasurementVectorSize = TInputSample::MeasurementVectorSize } ;
+  typedef typename TInputSample::MeasurementType MeasurementType ;
+  typedef typename TInputSample::MeasurementVectorType MeasurementVectorType ;
+
+  /**  ParametersType typedef.
+   *  It defines a position in the optimization search space. */
+  typedef SingleValuedCostFunction::ParametersType ParamtersType ;
+
+  /**  MeasureType typedef.
+   *  It defines a type used to return the cost function value. */
+  typedef SingleValuedCostFunction::MeasureType MeasureType ;
+
+  typedef GoodnessOfFitComponentBase< TInputSample > ComponentType ;
+  typedef std::vector< ComponentType* > ComponentVectorType ;
+
+  typedef GoodnessOfFitFunctionBase< ComponentType::HistogramType > 
+  FunctionType ;
+
+  /** aceesing methods for the sample manipulator */ 
+  void AddComponent(ComponentType* component) ;
+  
+  /** aceesing methods for the expected probability histogram */ 
+  void SetFunction(FunctionType* core) ;
+
+  FunctionType* GetFunction()
+  { return m_Function ; }
+
+  virtual unsigned int GetNumberOfParameters() const ;
+
+  /** This method returns the value of the cost function corresponding
+    * to the specified parameters. */ 
+  virtual MeasureType GetValue( const ParametersType & parameters ) const ;
+
+  /** This method returns the derivative of the cost function corresponding
+    * to the specified parameters.   */ 
+  virtual void GetDerivative( const ParametersType & parameters,
+                                    DerivativeType & derivative ) const 
+  { /* not implemented */ }
+
+protected:
+  GoodnessOfFitMixtureModelCostFunction() ;
+  virtual ~GoodnessOfFitMixtureModelCostFunction() ;
+  virtual void PrintSelf(std::ostream& os, Indent indent) const ;
+
+private:
+  /** helper classes */
+  ComponentVectorType m_Components ;
+  FunctionType* m_Function ;
+} ; // end of class
+
+} // end of namespace Statistics 
+} // end of namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkGoodnessOfFitMixtureModelCostFunction.txx"
+#endif
+
+#endif
+
---
+++ Code/Numerics/Statistics/itkGoodnessOfFitMixtureModelCostFunction.txx
@@ -0,0 +1,160 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkGoodnessOfFitMixtureModelCostFunction.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkGoodnessOfFitMixtureModelCostFunction_txx
+#define __itkGoodnessOfFitMixtureModelCostFunction_txx
+
+#include "itkGoodnessOfFitMixtureModelCostFunction.h"
+
+namespace itk{ 
+namespace Statistics{
+
+template< class TInputSample >
+GoodnessOfFitMixtureModelCostFunction< TInputSample >
+::GoodnessOfFitMixtureModelCostFunction()
+{
+  m_Function = 0 ;
+}
+
+template< class TInputSample >
+GoodnessOfFitMixtureModelCostFunction< TInputSample >
+::~GoodnessOfFitMixtureModelCostFunction()
+{
+}
+
+template< class TInputSample >
+void
+GoodnessOfFitMixtureModelCostFunction< TInputSample >
+::PrintSelf(std::ostream& os, Indent indent) const
+{
+  Superclass::PrintSelf(os,indent);
+  os << indent << "Function  " << m_Function << std::endl;
+  for ( int i = 0 ; i < m_Components.size() ; i++)
+    {
+      os << indent << "Components["<< i <<"]  "  << m_Components[i]  << std::endl;
+    }
+}
+
+template< class TInputSample >
+void
+GoodnessOfFitMixtureModelCostFunction< TInputSample >
+::AddComponent(ComponentType* component)
+{
+  m_Components.push_back(component) ;
+}
+
+template< class TInputSample >
+void
+GoodnessOfFitMixtureModelCostFunction< TInputSample >
+::SetFunction(FunctionType* core)
+{
+  if ( m_Function != core )
+    {
+      m_Function = core ;
+      this->Modified() ;
+    }
+}
+
+template< class TInputSample >
+unsigned int
+GoodnessOfFitMixtureModelCostFunction< TInputSample >
+::GetNumberOfParameters() const 
+{
+  unsigned int size = 0 ;
+  ComponentType* component ;
+  for ( int componentIndex = 0 ; componentIndex < m_Components.size() ;
+        componentIndex++ )
+    {
+      component = m_Components[componentIndex] ;
+      size += component->GetNumberOfParameters() ;
+    }
+
+  return size ;
+}
+
+template< class TInputSample >
+typename GoodnessOfFitMixtureModelCostFunction< TInputSample >::MeasureType
+GoodnessOfFitMixtureModelCostFunction< TInputSample >
+::GetValue(const ParametersType &parameters) const
+{
+  int i = 0 ; 
+  int j = 0 ;
+
+  double value = 0.0 ;
+ 
+  int index = 0 ;
+  unsigned int paramSize = 0 ;
+  ComponentType* component ;
+  for ( int componentIndex = 0 ; componentIndex < m_Components.size() ;
+        componentIndex++ )
+    {
+      component = m_Components[componentIndex] ;
+      paramSize = component->GetNumberOfParameters() ;
+      ParametersType params(paramSize) ;
+      for ( i = 0 ; i < paramSize ; i++)
+        {
+          params[i] = parameters[index] ;
+          index++ ;
+        }
+
+      component->SetParameters(params) ;
+      component->
+        SetUseExpectedHistogram(m_Function->GetUseExpectedHistogram()) ;
+      if ( component->GetObservedHistogram() == 0 )
+        {
+          component->CreateHistograms() ;
+        }
+
+      component->Resample() ;
+
+      if ( component->GetResampledSample()->GetTotalFrequency(0) == 0 ) 
+        {
+          return NumericTraits< double >::max() ;
+        }
+
+      component->CalculateProjectionAxes() ;
+      
+      m_Function->
+        SetTotalObservedScale(component->GetTotalObservedScale()) ;
+      
+      m_Function->SetObservedHistogram(component->GetObservedHistogram()) ;
+      
+      if ( m_Function->GetUseExpectedHistogram() )
+        {
+          m_Function->
+            SetExpectedHistogram(component->GetExpectedHistogram()) ;
+        }
+      
+      for (i = 0 ; i < MeasurementVectorSize ; i++)
+        {
+          component->Project(i) ;
+          if ( m_Function->GetUseExpectedHistogram() )
+            {
+              component->UpdateExpectedHistogram() ;
+            }
+          
+          m_Function->Update() ;
+          value += m_Function->GetOutput() ;
+        }
+    } // end of while ( iter ...
+
+  return value ;
+}
+
+} // end of namespace Statistics 
+} // end of namespace itk
+
+#endif
