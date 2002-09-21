c66a466aaed2692df962043e3e61318073513f66
Jisung Kim <bahrahm@yahoo.com>
2002-09-21 05:19:10 -0400

    ENH: A new class that wraps the vnl_symmetric_eigensystem algorithm

---
+++ Code/Numerics/itkSymmetricEigenSystem.h
@@ -0,0 +1,129 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkSymmetricEigenSystem.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkSymmetricEigenSystem_h
+#define __itkSymmetricEigenSystem_h
+
+#include "vnl/algo/vnl_symmetric_eigensystem.h"
+#include "itkFixedArray.h"
+#include "itkMatrix.h"
+
+namespace itk
+{
+  
+/** \class SymmetricEigenSystem
+ * \brief wrapper of the vnl_symmetric_eigensystem algorithm
+ *
+ * \ingroup Numerics
+ */
+
+template< class TMatrixElement, int VNumberOfRows >
+class ITK_EXPORT SymmetricEigenSystem : public Object
+{
+public:
+  /** Standard "Self" typedef. */
+  typedef SymmetricEigenSystem Self;
+  typedef Object Superclass;
+  typedef SmartPointer<Self> Pointer;
+  typedef SmartPointer<const Self> ConstPointer;
+  
+  /** Run-time type information (and related methods). */
+  itkTypeMacro( SymmetricEigenSystem, Object );
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** 1D array typedef */
+  typedef FixedArray< TMatrixElement, VNumberOfRows > ArrayType;
+
+  /** 2D array typedef */
+  typedef FixedArray< ArrayType, VNumberOfRows > Array2DType ;
+
+  /** Array for eigen vectors */
+  typedef Array2DType EigenVectorArrayType ;
+
+  /** Array type for eigen values */
+  typedef ArrayType EigenValueArrayType ;
+
+  /** Matrix Type */
+  typedef Matrix< TMatrixElement, VNumberOfRows, VNumberOfRows > MatrixType ;
+
+  /** Internal eigen sytem type. */
+  typedef vnl_symmetric_eigensystem< TMatrixElement > InternalEigenSystemType;
+
+  /** Set/Get the target matrix for the eigen analysis */
+  itkSetObjectMacro(Matrix, MatrixType) ;
+  MatrixType* GetMatrix()
+  { return m_Matrix ; }
+
+  /** Set/Get the absolute order flag.
+   * By setting this flag true, after the calculation of eigen vectors 
+   * and values, if the absolute eigen value of eigen vector[j > i] is
+   * greater that of eigen vector[i], reorder the eigen vectors so that
+   * every absolute eigen values of eigen vector[j < i] is always greater than or
+   * equal to that of the eigen vectors[i] */
+  itkSetMacro(UseAbsoluteOrder, bool) ;
+  itkGetMacro(UseAbsoluteOrder, bool) ;
+
+  /** returns the eigen vectors in a 2D array */
+  EigenVectorArrayType* GetEigenVectors()
+  { return &m_EigenVectors ; } 
+
+  /** returns the eigen values in an 1D array */
+  EigenValueArrayType* GetEigenValues()
+  { return &m_EigenValues ; }
+
+  /** dummy method that calls the GenerateData method to 
+   * produce the eigen vectors and values. */
+  void Update()
+  { this->GenerateData() ; }
+
+protected:
+  SymmetricEigenSystem();
+  virtual ~SymmetricEigenSystem();
+
+  /** Produces the eigen vectors and values. */
+  void GenerateData() ;
+
+private:
+  SymmetricEigenSystem(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+  void PrintSelf(std::ostream& os, Indent indent) const;
+
+  /** the target matrix */
+  MatrixType* m_Matrix ;
+
+  /** eigen vectors output */
+  EigenVectorArrayType m_EigenVectors ;
+  
+  /** eigen values output */
+  EigenValueArrayType m_EigenValues ;
+
+  /** flag for absolute ordering of eigen vectors and
+   * eigen values */
+  bool m_UseAbsoluteOrder ;
+};
+
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkSymmetricEigenSystem.txx"
+#endif
+
+#endif
+
+
+
---
+++ Code/Numerics/itkSymmetricEigenSystem.txx
@@ -0,0 +1,108 @@
+/*=========================================================================
+
+Program:   Insight Segmentation & Registration Toolkit
+Module:    itkSymmetricEigenSystem.txx
+Language:  C++
+Date:      $Date$
+Version:   $Revision$
+
+Copyright (c) 2002 Insight Consortium. All rights reserved.
+See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+This software is distributed WITHOUT ANY WARRANTY; without even 
+the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef _itkSymmetricEigenSystem_txx
+#define _itkSymmetricEigenSystem_txx
+
+#include "itkSymmetricEigenSystem.h"
+
+namespace itk
+{
+
+/**
+ * Constructor
+ */
+template< class TMatrixElement, int VNumberOfRows >
+SymmetricEigenSystem< TMatrixElement, VNumberOfRows >
+::SymmetricEigenSystem()
+{
+  m_Matrix = 0 ;
+  m_UseAbsoluteOrder = true ;
+}
+
+/**
+ * Destructor
+ */
+template< class TMatrixElement, int VNumberOfRows >
+SymmetricEigenSystem< TMatrixElement, VNumberOfRows >
+::~SymmetricEigenSystem()
+{
+}
+
+template< class TMatrixElement, int VNumberOfRows >
+void
+SymmetricEigenSystem< TMatrixElement, VNumberOfRows >
+::PrintSelf(std::ostream& os, Indent indent) const 
+{
+  Superclass::PrintSelf(os, indent) ;
+
+  os << indent << "Matrix         " << m_Matrix << std::endl ;
+  os << indent << "Eigen Vectors  " << m_EigenVectors << std::endl ;
+  os << indent << "Eigen Values   " << m_EigenValues << std::endl ;
+  os << indent << "Absolute order " << m_UseAbsoluteOrder << std::endl ;
+}
+
+
+/**
+ * Compute the eigen values and vectors
+ */
+template< class TMatrixElement, int VNumberOfRows >
+void
+SymmetricEigenSystem< TMatrixElement, VNumberOfRows >
+::GenerateData( void )
+{
+  int i, j, k ;
+
+  InternalEigenSystemType internalEigenSystem(m_Matrix->GetVnlMatrix()) ;
+
+  vnl_vector< double > tempVector ;
+
+  for (i = 0 ; i < VNumberOfRows ; i++)
+    {
+      tempVector = internalEigenSystem.get_eigenvector(i) ;
+      m_EigenValues[i] = internalEigenSystem.get_eigenvalue(i) ;
+      for (j = 0 ; j < VNumberOfRows ; j++)
+        {
+          m_EigenVectors[i][j] = tempVector[j] ;
+        }
+    }
+
+  double temp ;
+  for(i = 0 ; i < (VNumberOfRows - 1) ; i++)
+    {
+      for(j = i + 1 ; j < VNumberOfRows ; j++)
+        {
+          if( (m_EigenValues[j] > m_EigenValues[i] && !m_UseAbsoluteOrder) ||
+              ( (abs( m_EigenValues[j] ) > abs( m_EigenValues[i] ) ) 
+                && m_UseAbsoluteOrder) )
+            {
+              temp= m_EigenValues[i] ;
+              m_EigenValues[i] = m_EigenValues[j] ;
+              m_EigenValues[j] = temp;
+              for ( k = 0 ; k < VNumberOfRows ; k++ )
+                {
+                  temp = m_EigenVectors[i][k];
+                  m_EigenVectors[i][k] = m_EigenVectors[j][k];
+                  m_EigenVectors[j][k] = temp;
+                }
+            }
+        }
+    }
+}
+
+} // end namespace itk
+
+#endif
