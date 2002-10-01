a4b9f55ae2273e3157812202dcf6c9aa0069318e
Jisung Kim <bahrahm@yahoo.com>
2002-10-01 17:32:45 -0400

    ENH: BrainWeb and IBSR experiment shell (batch file) for users without GNU R.

---
+++ Validation/StatisticalClustering/Code/BrainWeb.experiment.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo /share/bin-linux/insight/bin/KdTreeBasedKmeansBrainWebValidationApp --images /share/work/data/brainweb/brainweb.T1.1mm.0.0.mhd /share/work/data/brainweb/brainweb.PD.1mm.0.0.mhd --mask /share/work/data/brainweb/phantom_discrete.mhd --parameters /share/work/src/Insight/Validation/StatisticalClustering/Inputs/BrainWeb.params.1ext.dat --bucket-size 100 --result BrainWeb.Kmeans.0pn.1ext.dat
+
+/share/bin-linux/insight/bin/KdTreeBasedKmeansBrainWebValidationApp --images /share/work/data/brainweb/brainweb.T1.1mm.0.0.mhd /share/work/data/brainweb/brainweb.PD.1mm.0.0.mhd --mask /share/work/data/brainweb/phantom_discrete.mhd --parameters /share/work/src/Insight/Validation/StatisticalClustering/Inputs/BrainWeb.params.1ext.dat --bucket-size 100 --result BrainWeb.Kmeans.0pn.1ext.dat
+
+echo /share/bin-linux/insight/bin/ExpectationMaximizationBrainWebValidationApp --images /share/work/data/brainweb/brainweb.T1.1mm.0.0.mhd /share/work/data/brainweb/brainweb.PD.1mm.0.0.mhd --mask /share/work/data/brainweb/phantom_discrete.mhd --parameters /share/work/src/Insight/Validation/StatisticalClustering/Inputs/BrainWeb.params.1ext.dat --result BrainWeb.EM.0pn.1ext.dat
+
+/share/bin-linux/insight/bin/ExpectationMaximizationBrainWebValidationApp --images /share/work/data/brainweb/brainweb.T1.1mm.0.0.mhd /share/work/data/brainweb/brainweb.PD.1mm.0.0.mhd --mask /share/work/data/brainweb/phantom_discrete.mhd --parameters /share/work/src/Insight/Validation/StatisticalClustering/Inputs/BrainWeb.params.1ext.dat --result BrainWeb.EM.0pn.1ext.dat
+
+echo /share/bin-linux/insight/bin/KdTreeBasedKmeansBrainWebValidationApp --images /share/work/data/brainweb/brainweb.T1.1mm.0.0.mhd /share/work/data/brainweb/brainweb.PD.1mm.0.0.mhd --mask /share/work/data/brainweb/phantom_discrete.mhd --parameters /share/work/src/Insight/Validation/StatisticalClustering/Inputs/BrainWeb.params.2ext.dat --bucket-size 100 --result BrainWeb.Kmeans.0pn.2ext.dat
+
+/share/bin-linux/insight/bin/KdTreeBasedKmeansBrainWebValidationApp --images /share/work/data/brainweb/brainweb.T1.1mm.0.0.mhd /share/work/data/brainweb/brainweb.PD.1mm.0.0.mhd --mask /share/work/data/brainweb/phantom_discrete.mhd --parameters /share/work/src/Insight/Validation/StatisticalClustering/Inputs/BrainWeb.params.2ext.dat --bucket-size 100 --result BrainWeb.Kmeans.0pn.2ext.dat
+
+echo /share/bin-linux/insight/bin/ExpectationMaximizationBrainWebValidationApp --images /share/work/data/brainweb/brainweb.T1.1mm.0.0.mhd /share/work/data/brainweb/brainweb.PD.1mm.0.0.mhd --mask /share/work/data/brainweb/phantom_discrete.mhd --parameters /share/work/src/Insight/Validation/StatisticalClustering/Inputs/BrainWeb.params.2ext.dat --result BrainWeb.EM.0pn.2ext.dat
+
+/share/bin-linux/insight/bin/ExpectationMaximizationBrainWebValidationApp --images /share/work/data/brainweb/brainweb.T1.1mm.0.0.mhd /share/work/data/brainweb/brainweb.PD.1mm.0.0.mhd --mask /share/work/data/brainweb/phantom_discrete.mhd --parameters /share/work/src/Insight/Validation/StatisticalClustering/Inputs/BrainWeb.params.2ext.dat --result BrainWeb.EM.0pn.2ext.dat
+
+echo /share/bin-linux/insight/bin/KdTreeBasedKmeansBrainWebValidationApp --images /share/work/data/brainweb/brainweb.T1.1mm.3.0.mhd /share/work/data/brainweb/brainweb.PD.1mm.3.0.mhd --mask /share/work/data/brainweb/phantom_discrete.mhd --parameters /share/work/src/Insight/Validation/StatisticalClustering/Inputs/BrainWeb.params.1ext.dat --bucket-size 100 --result BrainWeb.Kmeans.3pn.1ext.dat
+
+/share/bin-linux/insight/bin/KdTreeBasedKmeansBrainWebValidationApp --images /share/work/data/brainweb/brainweb.T1.1mm.3.0.mhd /share/work/data/brainweb/brainweb.PD.1mm.3.0.mhd --mask /share/work/data/brainweb/phantom_discrete.mhd --parameters /share/work/src/Insight/Validation/StatisticalClustering/Inputs/BrainWeb.params.1ext.dat --bucket-size 100 --result BrainWeb.Kmeans.3pn.1ext.dat
+
+echo /share/bin-linux/insight/bin/ExpectationMaximizationBrainWebValidationApp --images /share/work/data/brainweb/brainweb.T1.1mm.3.0.mhd /share/work/data/brainweb/brainweb.PD.1mm.3.0.mhd --mask /share/work/data/brainweb/phantom_discrete.mhd --parameters /share/work/src/Insight/Validation/StatisticalClustering/Inputs/BrainWeb.params.1ext.dat --result BrainWeb.EM.3pn.1ext.dat
+
+/share/bin-linux/insight/bin/ExpectationMaximizationBrainWebValidationApp --images /share/work/data/brainweb/brainweb.T1.1mm.3.0.mhd /share/work/data/brainweb/brainweb.PD.1mm.3.0.mhd --mask /share/work/data/brainweb/phantom_discrete.mhd --parameters /share/work/src/Insight/Validation/StatisticalClustering/Inputs/BrainWeb.params.1ext.dat --result BrainWeb.EM.3pn.1ext.dat
+
+echo /share/bin-linux/insight/bin/KdTreeBasedKmeansBrainWebValidationApp --images /share/work/data/brainweb/brainweb.T1.1mm.9.0.mhd /share/work/data/brainweb/brainweb.PD.1mm.9.0.mhd --mask /share/work/data/brainweb/phantom_discrete.mhd --parameters /share/work/src/Insight/Validation/StatisticalClustering/Inputs/BrainWeb.params.1ext.dat --bucket-size 100 --result BrainWeb.Kmeans.9pn.1ext.dat
+
+/share/bin-linux/insight/bin/KdTreeBasedKmeansBrainWebValidationApp --images /share/work/data/brainweb/brainweb.T1.1mm.9.0.mhd /share/work/data/brainweb/brainweb.PD.1mm.9.0.mhd --mask /share/work/data/brainweb/phantom_discrete.mhd --parameters /share/work/src/Insight/Validation/StatisticalClustering/Inputs/BrainWeb.params.1ext.dat --bucket-size 100 --result BrainWeb.Kmeans.9pn.1ext.dat
+
+echo /share/bin-linux/insight/bin/ExpectationMaximizationBrainWebValidationApp --images /share/work/data/brainweb/brainweb.T1.1mm.9.0.mhd /share/work/data/brainweb/brainweb.PD.1mm.9.0.mhd --mask /share/work/data/brainweb/phantom_discrete.mhd --parameters /share/work/src/Insight/Validation/StatisticalClustering/Inputs/BrainWeb.params.1ext.dat --result BrainWeb.EM.9pn.1ext.dat
+
+/share/bin-linux/insight/bin/ExpectationMaximizationBrainWebValidationApp --images /share/work/data/brainweb/brainweb.T1.1mm.9.0.mhd /share/work/data/brainweb/brainweb.PD.1mm.9.0.mhd --mask /share/work/data/brainweb/phantom_discrete.mhd --parameters /share/work/src/Insight/Validation/StatisticalClustering/Inputs/BrainWeb.params.1ext.dat --result BrainWeb.EM.9pn.1ext.dat
