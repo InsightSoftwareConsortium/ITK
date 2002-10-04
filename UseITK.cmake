4d81f44ad8812ce792b416a894e2a55c817bca1d
Brad King <brad.king@kitware.com>
2002-10-04 19:07:27 -0400

    ENH: Updated UseITK.cmake and provided ITK_USE_FILE pointing to it from ITKConfig.cmake.  This will work with the old FindITK.cmake module in CMake 1.4 for the binary tree only.  A new find should look for ITKConfig.cmake, include it, and then use ITK_USE_FILE.

--- UseITK.cmake
+++ UseITK.cmake
@@ -2,29 +2,26 @@
 # This module add the ITK include paths to a project
 # It should be included after the FindITK module
 #
-IF (ITK_BINARY_PATH)
-  LOAD_CACHE(${ITK_BINARY_PATH} 
-  EXCLUDE
-    BUILD_SHARED_LIBS
-    BUILD_TESTING
-    BUILD_EXAMPLES
-    LIBRARY_OUTPUT_PATH
-    EXECUTABLE_OUTPUT_PATH
-    MAKECOMMAND 
-    SITE
-    BUILDNAME
-    CVS_UPDATE_OPTIONS
-    DART_TESTING_TIMEOUT
-    VTK_INSTALL_PATH
-  INCLUDE_INTERNALS
-    ITK_BINARY_DIR
-    ITK_LIBRARY_PATH
-    ITK_BUILD_SHARED_LIBS
-    VTK_BINARY_DIR
-    VTK_BUILD_SHARED_LIBS
-    VTK_LIBRARY_PATH
-    )
-    INCLUDE (${ITK_SOURCE_DIR}/itkCMakeOptions.cmake)
-    LINK_DIRECTORIES(${ITK_LIBRARY_PATH})
-ENDIF (ITK_BINARY_PATH)
 
+# We must support both the FindITK.cmake module in CMake 1.4.x, and
+# the new configuration form based on ITKConfig.cmake.
+
+IF (ITK_BINARY_PATH)
+
+  # ITK_BINARY_PATH is set, so we are being included from the old
+  # CMake 1.4 module.  Load settings from the build tree's config file.
+  INCLUDE (${ITK_BINARY_PATH}/ITKConfig.cmake)
+  INCLUDE_DIRECTORIES(${ITK_INCLUDE_DIRS})
+  LINK_DIRECTORIES(${ITK_LIBRARY_DIRS})
+  ADD_DEFINITIONS(${ITK_DEFINITIONS})
+
+ELSE (ITK_BINARY_PATH)
+
+  # We are being included from the ITK_USE_FILE set in ITKConfig.cmake.
+  # The settings have already been loaded.  This is the preferred style
+  # because it supports using ITK from an installation.
+  INCLUDE_DIRECTORIES(${ITK_INCLUDE_DIRS})
+  LINK_DIRECTORIES(${ITK_LIBRARY_DIRS})
+  ADD_DEFINITIONS(${ITK_DEFINITIONS})
+
+ENDIF (ITK_BINARY_PATH)
