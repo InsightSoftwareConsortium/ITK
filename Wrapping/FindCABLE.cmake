83703ff69d94a2ad2a6dde5210dfcb4557810b79
Brad King <brad.king@kitware.com>
2002-10-04 13:19:55 -0400

    ENH: Fixed for CABLE's rename of UseCABLE.cmake to CABLEConfig.cmake.  Changed CABLE_USE_FILE to CABLE_DIR, and it is now set only to the directory containing CABLEConfig.cmake.

--- Wrapping/FindCABLE.cmake
+++ Wrapping/FindCABLE.cmake
@@ -1,10 +1,15 @@
 #
 # Find CABLE in an installation or a build tree.
 #
-# Sets CABLE_USE_FILE to the "UseCABLE.cmake".
+# Sets CABLE_DIR to the "CABLEConfig.cmake".
 #
 
-FIND_FILE(CABLE_USE_FILE UseCABLE.cmake
+FIND_PATH(CABLE_DIR CABLEConfig.cmake
   /usr/local/lib/Cable
   /usr/lib/Cable
 )
+
+IF(CABLE_DIR)
+  INCLUDE(${CABLE_DIR}/CABLEConfig.cmake)
+  SET(CABLE_FOUND 1)
+ENDIF(CABLE_DIR)
