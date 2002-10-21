659272a1dd5eaa3cf746f126299fa6897dfa8983
Daniel Blezek <blezek.daniel@mayo.edu>
2002-10-21 01:11:44 -0400

    Nightly version

--- Code/Common/itkVersion.h
+++ Code/Common/itkVersion.h
@@ -27,7 +27,7 @@
 #define ITK_MAJOR_VERSION 0
 #define ITK_MINOR_VERSION 0
 #define ITK_BUILD_VERSION 2
-#define ITK_SOURCE_VERSION "itk version " ITK_VERSION ", itk source $Revision: 1.725 $, $Date: 2002-10-19 05:10:09 $ (GMT)"
+#define ITK_SOURCE_VERSION "itk version " ITK_VERSION ", itk source $Revision: 1.726 $, $Date: 2002-10-21 05:11:44 $ (GMT)"
 
 namespace itk
 {
