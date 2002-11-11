5f1d15aae2be9f777fc00926d8ac487a43dce1ff
Luis Ibanez <luis.ibanez@kitware.com>
2002-11-11 15:35:31 -0500

    ENH: Image3.cxx added. It illustrates GetPixel()/SetPixel() methods

---
+++ Examples/DataRepresentation/Image/Image3.cxx
@@ -0,0 +1,41 @@
+#include "itkImage.h"
+
+int main()
+{
+
+  typedef itk::Image< unsigned short, 3 > ImageType;
+
+  ImageType::Pointer image = ImageType::New();
+
+  ImageType::IndexType start;
+  ImageType::SizeType  size;
+
+  size[0]  = 200;  // size along X
+  size[1]  = 200;  // size along Y
+  size[2]  = 200;  // size along Z
+
+  start[0] =   0;  // first index on X
+  start[1] =   0;  // first index on Y
+  start[2] =   0;  // first index on Z
+
+  ImageType::RegionType region;
+  
+  image->SetRegions( region );
+  image->Allocate();
+
+  image->FillBuffer( 0 );
+
+  ImageType::IndexType index;
+ 
+  index[0] = 27;
+  index[1] = 29;
+  index[2] = 37;
+
+  image->SetPixel( index, 41 );
+
+  ImageType::PixelType value = image->GetPixel( index );
+
+  return 0;
+
+}
+
