70d1bbd31bc018c790c455e13d3b95b1652957e0
Luis Ibanez <luis.ibanez@kitware.com>
2002-11-11 17:36:21 -0500

    ENH: Use of cells & points containers illustrated.

--- Examples/DataRepresentation/Mesh/Mesh2.cxx
+++ Examples/DataRepresentation/Mesh/Mesh2.cxx
@@ -1,20 +1,58 @@
 #include "itkMesh.h"
+#include "itkLineCell.h"
 
 int main()
 {
 
-  typedef itk::Mesh< unsigned short, 3 > MeshType;
-  typedef MeshType::PointsContainer      PointsContainer;
+  typedef float                             PixelType;
+  typedef itk::Mesh< PixelType, 3 >         MeshType;
 
-  PointsContainer::Pointer points = PointsContainer::New();
+  typedef MeshType::CellType                CellType;
+  typedef CellType::CellAutoPointer         CellAutoPointer;
 
-  MeshType::PointType p;
+  typedef itk::LineCell< CellType >         LineType;
 
-  points->InsertElement( 0, p );
+  typedef MeshType::PointsContainer         PointsContainer;
+  typedef MeshType::CellsContainer          CellsContainer;
 
   MeshType::Pointer  mesh = MeshType::New();
 
+  MeshType::PointType p0;
+  MeshType::PointType p1;
+  MeshType::PointType p2;
+
+  p0[0] = -1.0; p0[1] = 0.0; p0[2] = 0.0;
+  p1[0] =  1.0; p1[1] = 0.0; p1[2] = 0.0;
+  p2[0] =  1.0; p2[1] = 1.0; p2[2] = 0.0;
+
+  PointsContainer::Pointer points = PointsContainer::New();
+
+  points->InsertElement( 0, p0 );
+  points->InsertElement( 1, p1 );
+  points->InsertElement( 2, p2 );
+
+  CellAutoPointer line0;
+  CellAutoPointer line1;
+
+  line0.TakeOwnership(  new LineType );
+  line1.TakeOwnership(  new LineType );
+
+  line0->SetPointId( 0, 0 ); // line between points 0 and 1
+  line0->SetPointId( 1, 1 );
+
+  line1->SetPointId( 0, 1 ); // line between points 1 and 2
+  line1->SetPointId( 1, 2 );
+
+  CellsContainer::Pointer cells = CellsContainer::New();
+
+  cells->InsertElement( 0, line0.ReleaseOwnership() );
+  cells->InsertElement( 1, line1.ReleaseOwnership() );
+
   mesh->SetPoints( points );
+  mesh->SetCells( cells );
+
+  std::cout << "Points = " << mesh->GetNumberOfPoints() << std::endl;
+  std::cout << "Cells  = " << mesh->GetNumberOfCells()  << std::endl;
 
   return 0;
 
