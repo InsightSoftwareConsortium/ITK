5df9667ca8ef76b71dd01fbc821beac441a353bf
Luis Ibanez <luis.ibanez@kitware.com>
2002-11-11 16:12:34 -0500

    ENH: simplified to not expose the PointsContainer.

--- Examples/DataRepresentation/Mesh/PointSet1.cxx
+++ Examples/DataRepresentation/Mesh/PointSet1.cxx
@@ -4,22 +4,20 @@ int main()
 {
 
   typedef itk::PointSet< unsigned short, 3 > PointSetType;
-  typedef PointSetType::PointsContainer      PointsContainer;
-
-  PointsContainer::Pointer points = PointsContainer::New();
 
   PointSetType::PointType p1;
   PointSetType::PointType p2;
+  PointSetType::PointType p3;
 
   p1[0] = -1.0; p1[1] = 0.0; p1[2] = 0.0; // Point 1 = {-1,0,0 }
   p2[0] =  1.0; p2[1] = 0.0; p2[2] = 0.0; // Point 2 = { 1,0,0 }
-
-  points->InsertElement( 0, p1 );
-  points->InsertElement( 1, p2 );
+  p3[0] =  1.0; p3[1] = 1.0; p3[2] = 0.0; // Point 3 = { 1,1,0 }
 
   PointSetType::Pointer  pointsSet = PointSetType::New();
 
-  pointsSet->SetPoints( points );
+  pointsSet->SetPoint( 0, p1 );
+  pointsSet->SetPoint( 1, p2 );
+  pointsSet->SetPoint( 2, p3 );
 
   std::cout << pointsSet->GetNumberOfPoints() << std::endl;
 
