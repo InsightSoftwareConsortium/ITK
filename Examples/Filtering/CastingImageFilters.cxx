16b10475ef369f53aea982f693dfa22d3ce74877
Luis Ibanez <luis.ibanez@kitware.com>
2002-11-11 21:46:09 -0500

    ENH: ShiftScale and  Normalize filters added

--- Examples/Filtering/CastingImageFilters.cxx
+++ Examples/Filtering/CastingImageFilters.cxx
@@ -2,6 +2,8 @@
 #include "itkImageFileReader.h"
 #include "itkCastImageFilter.h"
 #include "itkRescaleIntensityImageFilter.h"
+#include "itkShiftScaleImageFilter.h"
+#include "itkNormalizeImageFilter.h"
 
 
 int main( int argc, char ** argv )
@@ -20,18 +22,36 @@ int main( int argc, char ** argv )
   typedef itk::RescaleIntensityImageFilter<
                InputImageType, OutputImageType >  RescaleFilterType;
 
+  typedef itk::ShiftScaleImageFilter<
+               InputImageType, OutputImageType >  ShiftScaleFilterType;
+
+  typedef itk::NormalizeImageFilter<
+               InputImageType, OutputImageType >  NormalizeFilterType;
+
   ReaderType::Pointer reader = ReaderType::New();
 
   CastFilterType::Pointer castFilter = CastFilterType::New();
   RescaleFilterType::Pointer rescaleFilter = RescaleFilterType::New();
+  ShiftScaleFilterType::Pointer shiftFilter = ShiftScaleFilterType::New();
+  NormalizeFilterType::Pointer normzalizeFilter = NormalizeFilterType::New();
 
   reader->SetFileName( argv[1] );
   
   castFilter->SetInput( reader->GetOutput() );
   rescaleFilter->SetInput( reader->GetOutput() );
+  shiftFilter->SetInput( reader->GetOutput() );
+  normzalizeFilter->SetInput( reader->GetOutput() );
+
+  rescaleFilter->SetOutputMinimum(  10 );
+  rescaleFilter->SetOutputMaximum( 250 );
+
+  shiftFilter->SetShift( 25 );
+  shiftFilter->SetScale( 1.2 );
 
   castFilter->Update();
   rescaleFilter->Update();
+  shiftFilter->Update();
+  normzalizeFilter->Update();
 
   return 0;
 
