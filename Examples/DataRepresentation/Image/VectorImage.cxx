f248f95fd289f5dd14ba156c71f634db8ed2d721
Luis Ibanez <luis.ibanez@kitware.com>
2002-11-15 17:52:08 -0500

    Example on how to create an image of vectors

---
+++ Examples/DataRepresentation/Image/VectorImage.cxx
@@ -0,0 +1,155 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    VectorImage.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+// Software Guide : BeginLatex
+//
+// Many image processing tasks require images of non-scalar 
+// pixel types. A typical example is the case of Images of
+// vectors. This is the image type required to represent the
+// gradient of a scalar image. The following code illustrates
+// how to instantiate and use an image whose pixels are of 
+// vector type.
+//
+// For convenience we use here the itk::Vector class to define a pixel type.
+// The Vector class is intended to represent a geometrical vector
+// in space, not as an array container like the \texttt{std::vector} in STL.
+// If you are interested in containers, the itk::VectorContainer 
+// class may provide the functionalities you are looking for.
+//
+// The first step is then to include the header file of the vector class.
+//
+// Software Guide : EndLatex 
+
+// Software Guide : BeginCodeSnippet
+#include "itkVector.h"
+// Software Guide : EndCodeSnippet
+
+
+
+#include "itkImage.h"
+
+int main()
+{
+
+  // Software Guide : BeginLatex
+  // 
+  // The \texttt{itk::Vector} is templated over the type used to 
+  // represent the coordinate in space and over the space dimension. 
+  // In this example we want the vector dimension to match the image
+  // dimension but this is by no means a requirement. We could have
+  // defined a four dimensional image with three dimensional vectors
+  // as pixels. 
+  //
+  // Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  typedef itk::Vector< float, 3 >       PixelType;
+  typedef itk::Image< PixelType, 3 >    ImageType;
+  // Software Guide : EndCodeSnippet
+
+  // Then the image object can be created
+  ImageType::Pointer image = ImageType::New();
+
+
+  // The image region should be initialized
+
+  ImageType::IndexType start;
+  ImageType::SizeType  size;
+
+  size[0]  = 200;  // size along X
+  size[1]  = 200;  // size along Y
+  size[2]  = 200;  // size along Z
+
+  start[0] =   0;  // first index on X
+  start[1] =   0;  // first index on Y
+  start[2] =   0;  // first index on Z
+
+  ImageType::RegionType region;
+  
+  // Pixel data is allocated
+  image->SetRegions( region );
+  image->Allocate();
+
+  // The image buffer is initialized to a particular value
+  ImageType::PixelType  initialValue;
+
+  // A vector can initialize all its components to the
+  // same value by using the Fill() method.
+  initialValue.Fill( 0.0 );
+
+  // Now the image buffer can be initialized with this
+  // vector value.
+  image->FillBuffer( initialValue );
+
+
+  ImageType::IndexType pixelIndex;
+ 
+  pixelIndex[0] = 27;   // x position
+  pixelIndex[1] = 29;   // y position
+  pixelIndex[2] = 37;   // z position
+
+
+  // Software Guide : BeginLatex
+  //
+  // The itk::Vector inherits the operator \texttt{[]} from the
+  // itk::Array class. It is then possible to access its components
+  // using index notation as follows.
+  //
+  // Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  ImageType::PixelType   pixelValue;
+
+  pixelValue[0] =  1.345;   // x component
+  pixelValue[0] =  6.841;   // y component
+  pixelValue[0] =  3.295;   // x component
+  // Software Guide : EndCodeSnippet
+
+
+
+  // Software Guide : BeginLatex
+  //
+  // We can now store this vector in one of the image pixels 
+  // by defining an index and invoking the \texttt{SetPixel()} method.
+  //
+  // Software Guide : EndLatex 
+  // Software Guide : BeginCodeSnippet
+  image->SetPixel(   pixelIndex,   pixelValue  );
+  // Software Guide : EndCodeSnippet
+
+
+
+
+
+  // The GetPixel method can also be used to read Vectors 
+  // pixels from the image
+  ImageType::PixelType value = image->GetPixel( pixelIndex );
+
+
+  //
+  // Lets repeat that both SetPixel and GetPixel are extremly 
+  // inefficient and should only be used for debugging purposes
+  // or for implementing interactions with a graphical user interface
+  // for supporting features like quering the content of a pixels
+  // by clicking with the mouse.
+  //
+
+
+  return 0;
+
+}
+
