e9dc8820b76f9f987dfa4954b0689eddde834254
Luis Ibanez <luis.ibanez@kitware.com>
2002-11-19 14:05:19 -0500

    DOC: Explanations on methods for accessing points were added.

--- Examples/DataRepresentation/Mesh/PointSet1.cxx
+++ Examples/DataRepresentation/Mesh/PointSet1.cxx
@@ -17,11 +17,11 @@
 
 //  Software Guide : BeginLatex
 //
-//  The itk::PointSet is a basic class intended to represent geometry.
-//  It is the base class for the itk::Mesh and provide the support for
-//  manipulating sets of points in N-Dimensional space. Points can have
+//  The \code{itk::PointSet} is a basic class intended to represent geometry.
+//  It is the base class for the \code{itk::Mesh} and provide the support for
+//  manipulating sets of points in $N-Dimensional$ space. Points can have
 //  values associated with them. The type of such values is defined by
-//  a template parameter of the PointSet class. Two basic styles of 
+//  a template parameter of the \code{itk::PointSet} class. Two basic styles of 
 //  PointSets are available in ITK. They are referred to as \emph{Static}
 //  and \emph{Dynamic}. The first one is used when the number of points
 //  in the set can be known in advance and it is not expected to change
@@ -32,7 +32,7 @@
 //  with the aim of optimizing performance and memory management.
 //
 //
-//  In order to use the PointSet class, the header file should be included.
+//  In order to use the PointSet class, its header file should be included.
 //
 //  Software Guide : EndLatex 
 
@@ -46,38 +46,170 @@ int main()
   //  Software Guide : BeginLatex
   //
   //  Then we must decide what type of value should be associated with the
-  //  points. This is called in general the PixelType in order to make a 
-  //  terminology consistent with the itk::Image. The PointSet is also 
+  //  points. This is called in general the \code{PixelType} in order to make a 
+  //  terminology consistent with the \code{itk::Image}. The PointSet is also 
   //  templated over the dimension of the space in which the points are
-  //  represented.
+  //  represented. The following declaration illustrates a typical
+  //  instantiation of the PointSet class.
   //
-  //  The following declaration illustrates a typical instantiation of the
-  //  PointeSet class.
-  //
+  //  Software Guide : EndLatex 
+
   // Software Guide : BeginCodeSnippet
   typedef itk::PointSet< unsigned short, 3 > PointSetType;
   // Software Guide : EndCodeSnippet
+
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  PointSet objects are created by invoking the \code{New()} method on its
+  //  type.  The resulting object must be assigned to a \code{SmartPointer}.
+  //  The PointSet is then reference counted and can be shared by multiple
+  //  SmartPointers. The memory allocated for the PointSet will be released
+  //  when the number of references to the object are reduced to zero. This
+  //  simply means that the user does not need to be concerned about invoking
+  //  the \code{Delete()} method on this class.  In fact, the \code{Delete()}
+  //  method should \textbf{never} be called directly in any of the reference
+  //  counted ITK classes.
+  //
+  //  Software Guide : EndLatex 
 
   // Software Guide : BeginCodeSnippet
   PointSetType::Pointer  pointsSet = PointSetType::New();
   // Software Guide : EndCodeSnippet
 
-  PointSetType::PointType p0;
-  PointSetType::PointType p1;
-  PointSetType::PointType p2;
-
-  p0[0] = -1.0; p0[1] = 0.0; p0[2] = 0.0; // Point 0 = {-1,0,0 }
-  p1[0] =  1.0; p1[1] = 0.0; p1[2] = 0.0; // Point 1 = { 1,0,0 }
-  p2[0] =  1.0; p2[1] = 1.0; p2[2] = 0.0; // Point 2 = { 1,1,0 }
 
 
+
+  //  Software Guide : BeginLatex
+  //
+  //  Following the principles of Generic Programming the PointSet class has a
+  //  set of associated defined types to ensure that interacting objects can be
+  //  declared with compatible types. This set of type definitionas are
+  //  commonly known as \emph{traits}.  Among them we can find for example the
+  //  \code{PointType} type.  This is the type used by the point set to
+  //  represent points in space.  The following declaration is taking the point
+  //  type as defined in the PointSet traits and renaming it to be conveniently
+  //  used in the global namespace.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  typedef PointSetType::PointType     PointType;
+  // Software Guide : EndCodeSnippet
+
+
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  The \code{PointType} can now be used to declare point objects to be
+  //  inserted in the PointSet. Points are fairly small objects and henceforth
+  //  it is not convenient to manage them with reference counting and smart
+  //  pointers. They are simply instantiated as typical C++ classes. The Point
+  //  class inheriths the \code{[]} operator from the \code{itk::Array} class.
+  //  This makes possible to access its components using index notation. For
+  //  efficiency sake no bound-testing is performed during index access. It is
+  //  the user's responsibility to ensure that the index used is in the range
+  //  $\{0,Dimension-1\}$. Each one of the components in the point are associated
+  //  with space coordinates. The following code illustrates how to instantiate
+  //  a point and initialize its components.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  PointType p0;
+
+  p0[0] = -1.0;     //  x coordinate
+  p0[1] = -1.0;     //  y coordinate
+  p0[2] =  0.0;     //  z coordinate
+  // Software Guide : EndCodeSnippet
+
+
+  PointType p1;
+
+  p1[0] =  1.0; // Point 1 = { 1,-1,0 } 
+  p1[1] = -1.0; 
+  p1[2] =  0.0;
+
+
+  PointType p2; // Point 2 = { 1,1,0 }
+  p2[0] =  1.0; 
+  p2[1] =  1.0; 
+  p2[2] =  0.0; 
+
+  //  Software Guide : BeginLatex
+  //
+  //  Points are inserted in the PointSet by using the \code{SetPoint()} method.
+  //  This method requires the user to provide a unique identifier for the
+  //  point. The identifier is simply an unsigned integer that will enumerate
+  //  the points as they are being inserted. The following code shows how three
+  //  points are inserted into the PointSet.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
   pointsSet->SetPoint( 0, p0 );
   pointsSet->SetPoint( 1, p1 );
   pointsSet->SetPoint( 2, p2 );
+  // Software Guide : EndCodeSnippet
 
-  std::cout << pointsSet->GetNumberOfPoints() << std::endl;
+
+  //  Software Guide : BeginLatex
+  //
+  // It is possible to query the PointSet in order to determine how many points
+  // have been inserted on it. This is done with the \code{GetNumberOfPoints()}
+  // method as illustrated below.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  const unsigned int numberOfPoints = pointsSet->GetNumberOfPoints();
+
+  std::cout << numberOfPoints << std::endl;
+  // Software Guide : EndCodeSnippet
+
+
+
+  //  Software Guide : BeginLatex
+  //
+  // Points can be read from the PointSet by using the \code{GetPoint()} method
+  // and the integer identifier. The point is stored in a pointer provided by
+  // the user. In the case the identifier provided does not match an
+  // existing point the method will return \code{false} and the content of the
+  // point will be invalid. The following code illustrates the access to points
+  // using defensive programming.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  PointType pp;
+
+  bool pointExists =  pointsSet->GetPoint( 1, & pp );
+
+  if( pointsSet ) {
+    std::cout << "Point is = " << pp << std::endl;
+    }
+  // Software Guide : EndCodeSnippet
+
+
+
+
+
+  //  Software Guide : BeginLatex
+  //
+  // \code{GetPoint()} and \code{SetPoint()} are not the most efficient methods
+  // to access points in the PointSet. It is prefereable to get direct access
+  // to the internal point container defined by the \emph{traits} and use
+  // Iterators to walk sequentially over the list of points.
+  //
+  //  Software Guide : EndLatex 
+
+
+
 
   return 0;
 
 }
+
 
--- Examples/DataRepresentation/Mesh/PointSet2.cxx
+++ Examples/DataRepresentation/Mesh/PointSet2.cxx
@@ -1,29 +1,245 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    PointSet2.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+//  Software Guide : BeginLatex
+//
+//  The PointSet class uses an internal container to manage the storage of
+//  Points. It is more efficient, in general, to manage points by using the
+//  access methods provided directly on the points container. The following
+//  example illustrates how to interact with the point container and how to use
+//  point iterators.
+//
+//  Software Guide : EndLatex 
+
+
 #include "itkPointSet.h"
 
 int main()
 {
 
   typedef itk::PointSet< unsigned short, 3 > PointSetType;
+
+  //  Software Guide : BeginLatex
+  //
+  //  The \code{PointContainer} type is defined among the \emph{traits} of the
+  //  PointSet class. The following line conveniently takes the
+  //  \code{PointContainer} type from the PointSet traits and declare it on the
+  //  global namespace.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
   typedef PointSetType::PointsContainer      PointsContainer;
+  // Software Guide : EndCodeSnippet
 
+  //  Software Guide : BeginLatex
+  //
+  //  The actual type of the \code{PointsContainer} depends on what style of
+  //  \code{PointSet} is being used. Dynamic PointSet use the
+  //  \code{itk::MapContainer} while the Static PointSet uses the
+  //  \code{itk::VectorContainer}. The vector and map container are basically
+  //  ITK wrappers around the STL classes \code{std::map} and
+  //  \code{std::vector}. By default, the PointSet uses a Static style, hence
+  //  the default type of point container is an \code{itk::VectorContainer}.
+  //  Both the map and vector container are templated over the type of the
+  //  elements they contain. In this case they are templated over
+  //  \code{PointType}. Containers are reference counted object. They are
+  //  then created with the \code{New()} method and assigned to a SmartPointer
+  //  after creation.  The following line creates a point container compatible
+  //  with the type of the PointSet from which the trait has been taken.
+  //
+  //  Software Guide : EndLatex 
+
+
+  // Software Guide : BeginCodeSnippet
   PointsContainer::Pointer points = PointsContainer::New();
+  // Software Guide : EndCodeSnippet
 
-  PointSetType::PointType p0;
-  PointSetType::PointType p1;
+
+
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  Points can now be defined using the \code{PointType} trait from the PointSet.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  typedef PointSetType::PointType   PointType;
+
+  PointType p0;
+  PointType p1;
 
   p0[0] = -1.0; p0[1] = 0.0; p0[2] = 0.0; // Point 0 = {-1,0,0 }
   p1[0] =  1.0; p1[1] = 0.0; p1[2] = 0.0; // Point 1 = { 1,0,0 }
+  // Software Guide : EndCodeSnippet
 
-  points->InsertElement( 0, p0 );
-  points->InsertElement( 1, p1 );
 
-  PointSetType::Pointer  pointsSet = PointSetType::New();
 
-  pointsSet->SetPoints( points );
 
-  std::cout << pointsSet->GetNumberOfPoints() << std::endl;
+
+  //  Software Guide : BeginLatex
+  //
+  //  The created points can be inserted in the PointsContainer using the generic
+  //  method \code{InsertElement()} which requires an identified to be provided
+  //  for each point.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  unsigned int pointId = 0;
+
+  points->InsertElement( pointId++ , p0 );
+  points->InsertElement( pointId++ , p1 );
+
+  // Software Guide : EndCodeSnippet
+
+  PointSetType::Pointer  pointSet = PointSetType::New();
+
+
+
+
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  Finally the PointsContainer can be assigned to the PointSet. This will
+  //  substitute any previously existing PointContainer on the PointSet. The
+  //  assignment is done using the \code{SetPoints()} method.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  pointSet->SetPoints( points );
+  // Software Guide : EndCodeSnippet
+
+
+
+
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  The PointContainer can be obtained from the PointSet using the
+  //  \code{GetPoints()} method.  This method returns a SmartPointer to the
+  //  actual container owned by the PointSet.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  PointsContainer::Pointer  points2 = pointSet->GetPoints();   
+  // Software Guide : EndCodeSnippet
+
+  
+
+
+
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  The most efficient way to sequentially visit the points is to use the
+  //  iterators provided by PointsContainer. The \code{Iterator} type belongs
+  //  to the traits of the PointsContainer classes. It behaves pretty much like
+  //  the STL iterators \footnote{If you dig deep enough into the code, you
+  //  will discover that these iterators are actually ITK wrappers around STL
+  //  iterators.}.  The Points iterator is not a reference counted class, so it
+  //  is just created directly from the traits without using SmartPointers.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  typedef PointsContainer::Iterator     PointsIterator;
+  // Software Guide : EndCodeSnippet
+
+
+
+
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  The subsequent use of the iterator follows what you may expect from a STL
+  //  iterator. The iterator to the first point is obtained from the container
+  //  with the \code{Begin()} method and assigned to another iterator.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  PointsIterator  pointIterator = points->Begin();  
+  // Software Guide : EndCodeSnippet
+
+
+
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  The \code{++} operator on the iterator can be used to advance from one
+  //  point to the next. The actual value of the Point to which the iterator is
+  //  pointing can be obtained with the \code{Value()} method. The loop for
+  //  walking through all the points can be controled by comparing the current
+  //  iterator with the iterator returned by the \code{End()} method of the
+  //  PointsContainer. The following lines illustrate the typical loop for
+  //  walking through the points.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  PointsIterator end = points->End();
+  while( pointIterator != end ) {
+    PointType p = pointIterator.Value();   // access the point
+    std::cout << p << std::endl;           // print the point
+    ++pointIterator;                       // advance to next point
+    }
+  // Software Guide : EndCodeSnippet
+
+
+
+
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  Note that as in STL, the iterator returned by the \code{End()} method is
+  //  not a valid iterator. This is called a past-end iterator in order to
+  //  indicate that it is the value resulting from advancing one step after
+  //  visiting the last element in the container.
+  // 
+  //  The number of elements stored in a container can be queried with the
+  //  \code{Size()} method. In the case of the PointSet, the following two
+  //  lines of code are equivalent, both of them returning the number of points
+  //  in the PointSet.
+
+  //
+  //  Software Guide : EndLatex 
+
+
+  // Software Guide : BeginCodeSnippet
+  std::cout << pointSet->GetNumberOfPoints() << std::endl;
+  std::cout << pointSet->GetPoints()->Size() << std::endl;
+  // Software Guide : EndCodeSnippet
+
+
 
   return 0;
 
 }
+
+
 
