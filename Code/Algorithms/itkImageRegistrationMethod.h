618b7435cd85a41025c4cf7a9a496c8263768c52
Lydia Ng <lydiang@gmail.com>
2002-12-12 10:31:51 -0500

    FIX: superclass in itkTypeMacro

--- Code/Algorithms/itkImageRegistrationMethod.h
+++ Code/Algorithms/itkImageRegistrationMethod.h
@@ -72,7 +72,7 @@ public:
   itkNewMacro(Self);
   
   /** Run-time type information (and related methods). */
-  itkTypeMacro(ImageRegistrationMethod, Object);
+  itkTypeMacro(ImageRegistrationMethod, ProcessObject);
 
   /**  Type of the Fixed image. */
   typedef          TFixedImage                     FixedImageType;
