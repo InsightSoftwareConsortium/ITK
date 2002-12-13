b3be78274414f04cfd072f5682727d46ab200874
Jim Miller <millerjv@crd.ge.com>
2002-12-13 12:47:02 -0500

    FIX: TypeMacro

--- Code/Common/itkElasticBodySplineKernelTransform.h
+++ Code/Common/itkElasticBodySplineKernelTransform.h
@@ -50,7 +50,7 @@ public:
   typedef SmartPointer<const Self>  ConstPointer;
     
   /** Run-time type information (and related methods). */
-  itkTypeMacro( ThinPlateSplineKernelTransform, KernelTransform );
+  itkTypeMacro( ElasticBodySplineKernelTransform, KernelTransform );
 
   /** New macro for creation of through a Smart Pointer */
   itkNewMacro( Self );
