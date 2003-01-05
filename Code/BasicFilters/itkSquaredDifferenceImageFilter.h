4c60b9662b2e862a0ef1ac46a805d545137d2ed1
Luis Ibanez <luis.ibanez@kitware.com>
2003-01-05 18:38:22 -0500

    Filter to compute squared differences pixel-wise.

---
+++ Code/BasicFilters/itkSquaredDifferenceImageFilter.h
@@ -0,0 +1,104 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkSquaredDifferenceImageFilter.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkSquaredDifferenceImageFilter_h
+#define __itkSquaredDifferenceImageFilter_h
+
+#include "itkBinaryFunctorImageFilter.h"
+
+namespace itk
+{
+  
+/** \class SquaredDifferenceImageFilter
+ * \brief Implements pixel-wise the computation of squared difference.
+ *
+ * This filter is parametrized over the types of the two 
+ * input images and the type of the output image. 
+ *
+ * Numeric conversions (castings) are done by the C++ defaults.
+ *
+ * The filter will walk over all the pixels in the two input images, and for
+ * each one of them it will do the following: 
+ *
+ * - cast the input 1 pixel value to \c double 
+ * - cast the input 2 pixel value to \c double 
+ * - compute the difference of the two pixel values
+ * - compute the square of the difference
+ * - cast the \c double value resulting from \c sqr() to the pixel type of the output image 
+ * - store the casted value into the output image.
+ * 
+ * The filter expect all images to have the same dimension 
+ * (e.g. all 2D, or all 3D, or all ND)
+ * 
+ * \ingroup IntensityImageFilters Multithreaded
+ */
+namespace Functor {  
+  
+  template< class TInput1, class TInput2, class TOutput>
+  class SquaredDifference2
+  {
+  public:
+    SquaredDifference2() {};
+    ~SquaredDifference2() {};
+    inline TOutput operator()( const TInput1 & A, 
+                               const TInput2 & B)
+    {
+      const double dA = static_cast<double>( A );
+      const double dB = static_cast<double>( B );
+      const double diff = dA - dB;
+      return static_cast<TOutput>( diff * diff );
+    }
+  }; 
+}
+
+template <class TInputImage1, class TInputImage2, class TOutputImage>
+class ITK_EXPORT SquaredDifferenceImageFilter :
+    public
+    BinaryFunctorImageFilter<TInputImage1,TInputImage2,TOutputImage, 
+            Functor::SquaredDifference2< 
+                      typename TInputImage1::PixelType, 
+                      typename TInputImage2::PixelType,
+                      typename TOutputImage::PixelType>   >
+{
+public:
+  /** Standard class typedefs. */
+  typedef SquaredDifferenceImageFilter  Self;
+  typedef BinaryFunctorImageFilter<TInputImage1,TInputImage2,TOutputImage, 
+                      Functor::SquaredDifference2< 
+                      typename TInputImage1::PixelType, 
+                      typename TInputImage2::PixelType,
+                      typename TOutputImage::PixelType>   
+                        >  Superclass;
+  typedef SmartPointer<Self>   Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+  
+protected:
+  SquaredDifferenceImageFilter() {}
+  virtual ~SquaredDifferenceImageFilter() {}
+
+private:
+  SquaredDifferenceImageFilter(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+};
+
+} // end namespace itk
+
+
+#endif
