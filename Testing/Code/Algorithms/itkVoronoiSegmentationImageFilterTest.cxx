1fb09e211356e7fc26e4e8cc030013d579a78922
Bill Lorensen <bill.lorensen@gmail.com>
2003-01-07 13:51:40 -0500

    ERR: On Borland, turn off floating exceptions.

--- Testing/Code/Algorithms/itkVoronoiSegmentationImageFilterTest.cxx
+++ Testing/Code/Algorithms/itkVoronoiSegmentationImageFilterTest.cxx
@@ -23,12 +23,16 @@
 #include "itkImage.h"
 #include "vnl/vnl_sample.h"
 #include <stdio.h>
-
-
+#ifdef __BORLANDC__
+#include <float.h>
+#endif
 int itkVoronoiSegmentationImageFilterTest(int, char**){
   const int WIDTH = 256;
   const int HEIGHT = 256;
 
+#ifdef __BORLANDC__
+  _control87(MCW_EM,MCW_EM);
+#endif
   typedef itk::Image<unsigned short,2> UShortImage;
   typedef itk::VoronoiSegmentationImageFilter<UShortImage, UShortImage> VorSeg;
 
