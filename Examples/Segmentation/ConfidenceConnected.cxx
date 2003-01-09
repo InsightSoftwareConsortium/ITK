69580674e25114b41199f486f6c78e9c238d7c0b
Luis Ibanez <luis.ibanez@kitware.com>
2003-01-09 01:39:17 -0500

    ENH: Added all the filters in the pipeline.

--- Examples/Segmentation/ConfidenceConnected.cxx
+++ Examples/Segmentation/ConfidenceConnected.cxx
@@ -34,6 +34,8 @@
 #include "itkImage.h"
 #include "itkCastImageFilter.h"
 
+#include "itkCurvatureFlowImageFilter.h"
+
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
 
@@ -49,9 +51,150 @@ int main( int argc, char **argv )
     std::cerr << " inputImage  outputImage seedX seedY " << std::endl;
     return 1;
     }
+
+
+
+
+  //  Software Guide : BeginLatex
+  //  
+  //  First, We declare the pixel type and image dimension.
+  //  With them we declare the image type.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  typedef float       InternalPixelType;
+  const unsigned int  Dimension = 2;
   
+  typedef itk::Image< InternalPixelType, Dimension >  InternalImageType;
+  // Software Guide : EndCodeSnippet
+
+
+  typedef unsigned short OutputPixelType;
+  typedef itk::Image< OutputPixelType, Dimension > OutputImageType;
+
+  typedef itk::CastImageFilter< 
+                        InternalImageType, 
+                        OutputImageType    >    CastingFilterType;
+  
+  CastingFilterType::Pointer caster = CastingFilterType::New();
+                        
+  //
+  // We instantiate reader and writer types
+  //
+  typedef  itk::ImageFileReader< InternalImageType > ReaderType;
+  typedef  itk::ImageFileWriter<  OutputImageType  > WriterType;
+
+  ReaderType::Pointer reader = ReaderType::New();
+  WriterType::Pointer writer = WriterType::New();
+
+  reader->SetFileName( argv[1] );
+  writer->SetFileName( argv[2] );
+
+
+
+  //  Software Guide : BeginLatex
+  //  
+  //  An edge-preservin smoothing filter is required in order to preprocess the
+  //  images. \code{CurvatureFlowImageFilter} is used here, but any of the
+  //  filter in section \ref{sec:EdgePreservingSmoothingFilers} could do it.
+  //  The filter type is instantiated using the image type as template
+  //  parameter.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  typedef   itk::CurvatureFlowImageFilter< 
+                 InternalImageType, 
+                 InternalImageType >     CurvatureFlowImageFilterType;
+  // Software Guide : EndCodeSnippet
+
+
+
+
+  //  Software Guide : BeginLatex
+  //  
+  //  The smoothing filter is created by invoking the \code{New()} method and
+  //  assigning the result to a \code{SmartPointer}.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  CurvatureFlowImageFilterType::Pointer smoothing = 
+                         CurvatureFlowImageFilterType::New();
+  // Software Guide : EndCodeSnippet
+
+
+  smoothing->SetInput( reader->GetOutput() );
+
+
+
+
+  //  Software Guide : BeginLatex
+  //  
+  //  We declare now the type of the region growing filter. In this case it is
+  //  the \code{itk::ConfidenceConectedImageFilter}. 
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  typedef  itk::ConfidenceConnectedImageFilter< 
+                                    InternalImageType, 
+                                    InternalImageType > ConnectedFilterType;
+  // Software Guide : EndCodeSnippet
+
+  //  Software Guide : BeginLatex
+  //  
+  //  With the type, we can construct one using the \code{New()} method. 
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  ConnectedFilterType::Pointer confidence = ConnectedFilterType::New();
+  // Software Guide : EndCodeSnippet
+
+
+  //  Software Guide : BeginLatex
+  //  
+  //  Now is time for connecting the pipeline. This is pretty linear in our
+  //  example.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  smoothing->SetInput( reader->GetOutput() );
+  confidence->SetInput( smoothing->GetOutput() );
+  caster->SetInput( confidence->GetOutput() );
+  writer->SetInput( caster->GetOutput() );
+  // Software Guide : EndCodeSnippet
+
+
+
+
+  //  Software Guide : BeginLatex
+  //  
+  //  The invokation of the \code{Update()} method on the writer triggers the
+  //  execution of the pipeline.  It is usually wise to put update calls in
+  //  \code{try/catch} block in case error ocurr and exceptions are thrown.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  try
+    {
+    writer->Update();
+    }
+  catch( itk::ExceptionObject & excep )
+    {
+    std::cerr << "Exception caught !" << std::endl;
+    std::cerr << excep << std::endl;
+    }
+  // Software Guide : EndCodeSnippet
 
   return 0;
 
 }
+
+
+
 
