3649f8381760079f2958b8544afe9fd946d2fdee
Luis Ibanez <luis.ibanez@kitware.com>
2003-01-14 19:16:48 -0500

    Helper class for initializing Centered Transforms in the Registration Framework.

---
+++ Code/Common/itkCenteredTransformInitializer.h
@@ -0,0 +1,143 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkCenteredTransformInitializer.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#ifndef __itkCenteredTransformInitializer_h
+#define __itkCenteredTransformInitializer_h
+
+#include <iostream>
+
+namespace itk
+{
+
+/** \brief CenteredTransformInitializer is a helper class intended to
+ * initialize the center of rotation and the translation of Transforms having
+ * the center of rotation among their parameters.
+ * 
+ * This class is connected to the fixed image, moving image and transform
+ * involved in the registration. Two modes of operation are possible:
+ * 
+ * - Geometrical, 
+ * - Center of mass
+ *
+ * In the first mode, the geometrical center of the moving image is passed as
+ * initial center of rotation to the transform and the vector from the center
+ * of the  fixed image to the center of the moving image is passed as the
+ * initial translation. This mode basically assumes that the anatomical objects
+ * to be registered are centered in their respective images. Hence the best
+ * initial guess for the registration is the one that superimpose those two
+ * centers.
+ *
+ * In the second mode, the moments of gray level values are computed for both
+ * images. The center of mass of the moving image is then used as center of
+ * rotation. The vector relation both centers of mass is passes as the initial
+ * translation to the transform. This second approach assumes that the moments
+ * of the anatomical object in the image is similar for both images and hence
+ * the best initial guess for registration is to superimpose both mass centers.
+ * Note that this assumption will probably not hold in multi-modality
+ * registration.
+ *
+ * 
+ * \ingroup Transforms
+ */
+template < class TTransform,     
+           class TFixedImage,
+           class TMovingImage > 
+class ITK_EXPORT CenteredTransformInitializer : 
+            public Object
+{
+public:
+  /** Standard class typedefs. */
+  typedef CenteredTransformInitializer     Self;
+  typedef Object                           Superclass;
+  typedef SmartPointer<Self>               Pointer;
+  typedef SmartPointer<const Self>         ConstPointer;
+    
+  /** New macro for creation of through a Smart Pointer. */
+  itkNewMacro( Self );
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro( CenteredTransformInitializer, Object );
+
+  /** Type of the transform to initialize */
+  typedef TTransform                        TransformType;
+  typedef typename TransformType::Pointer   TransformPointer;
+
+  /** Dimension of parameters. */
+  itkStaticConstMacro(SpaceDimension, unsigned int, TransformType::SpaceDimension);
+  itkStaticConstMacro(InputSpaceDimension, unsigned int, TransformType::InputSpaceDimension);
+  itkStaticConstMacro(OutputSpaceDimension, unsigned int, TransformType::OutputSpaceDimension);
+
+  
+  /** Image Types to use in the initialization of the transform */
+  typedef   TFixedImage              FixedImageType;
+  typedef   TMovingImage             MovingImageType;
+
+  typedef   typename FixedImageType::ConstPointer   FixedImagePointer;
+  typedef   typename MovingImageType::ConstPointer  MovingImagePointer;
+
+
+
+  /** Offset type. */
+  typedef typename TransformType::OffsetType  OffsetType;
+
+  /** Point type. */
+  typedef typename TransformType::InputPointType   InputPointType;
+  
+  /** Vector type. */
+  typedef typename TransformType::OutputVectorType  OutputVectorType;
+  
+  /** Set the transform to be initialized */
+  itkSetObjectMacro( Transform,   TransformType   );
+
+  /** Set the fixed image used in the registration process */
+  itkSetConstObjectMacro( FixedImage,  FixedImageType  );
+
+  /** Set the moving image used in the registration process */
+  itkSetConstObjectMacro( MovingImage, MovingImageType );
+
+
+  /** Initialize the transform using data from the images */
+  void InitializeTransform() const;
+
+
+protected:
+  CenteredTransformInitializer() {};
+  ~CenteredTransformInitializer(){};
+
+  void PrintSelf(std::ostream &os, Indent indent) const;
+
+private:
+  CenteredTransformInitializer(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+  TransformPointer    m_Transform;
+
+  FixedImagePointer   m_FixedImage;
+
+  MovingImagePointer  m_MovingImage;
+
+}; //class CenteredTransformInitializer
+
+
+}  // namespace itk
+
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkCenteredTransformInitializer.txx"
+#endif
+
+#endif /* __itkCenteredTransformInitializer_h */
---
+++ Code/Common/itkCenteredTransformInitializer.txx
@@ -0,0 +1,107 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkCenteredTransformInitializer.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#ifndef __itkCenteredTransformInitializer_txx
+#define __itkCenteredTransformInitializer_txx
+
+#include "itkCenteredTransformInitializer.h"
+#include "itkImageMomentsCalculator.h"
+
+namespace itk
+{
+
+template < class TTransform, class TFixedImage, class TMovingImage >
+void 
+CenteredTransformInitializer<TTransform, TFixedImage, TMovingImage >
+::InitializeTransform() const
+{
+  // Sanity check
+  if( !m_FixedImage )
+    {
+    return;
+    }
+  if( !m_MovingImage )
+    {
+    return;
+    }
+  if( !m_Transform )
+    {
+    return;
+    }
+
+  // If images come from filters, then update those filters.
+  if( m_FixedImage->GetSource() )
+    { 
+    m_FixedImage->GetSource()->Update();
+    }
+  if( m_MovingImage->GetSource() )
+    { 
+    m_MovingImage->GetSource()->Update();
+    }
+
+  typedef ImageMomentsCalculator< FixedImageType >   FixedImageCalculatorType;
+  typedef ImageMomentsCalculator< MovingImageType >  MovingImageCalculatorType;
+
+  FixedImageCalculatorType    fixedCalculator;
+  MovingImageCalculatorType   movingCalculator;
+
+  fixedCalculator.ComputeMoments(  m_FixedImage );
+  movingCalculator.ComputeMoments( m_MovingImage );
+  
+  FixedImageCalculatorType::VectorType fixedCenter =
+                        fixedCalculator.GetCenterOfGravity();
+
+  MovingImageCalculatorType::VectorType movingCenter =
+                        movingCalculator.GetCenterOfGravity();
+
+  InputPointType rotationCenter;
+  OutputVectorType translationVector;
+
+  for( unsigned int i=0; i<InputSpaceDimension; i++)
+    {
+    rotationCenter[i]    = movingCenter[i];
+    translationVector[i] = movingCenter[i] - fixedCenter[i];
+    }
+
+  m_Transform->SetCenterOfRotation( rotationCenter );
+
+  m_Transform->SetTranslation( translationVector );
+
+
+}
+  
+
+      
+
+template < class TTransform, class TFixedImage, class TMovingImage >
+void 
+CenteredTransformInitializer<TTransform, TFixedImage, TMovingImage >
+::PrintSelf(std::ostream& os, Indent indent) const
+{
+  Superclass::PrintSelf(os,indent);
+  os << indent << "Transform   = " << m_Transform   << std::endl;
+  os << indent << "FixedImage  = " << m_FixedImage  << std::endl;
+  os << indent << "MovingImage = " << m_MovingImage << std::endl;
+
+}
+
+
+
+ 
+}  // namespace itk
+
+#endif /* __itkCenteredTransformInitializer_txx */
