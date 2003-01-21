713a082e986b1f82b7701384e5e24e4ab3881486
Luis Ibanez <luis.ibanez@kitware.com>
2003-01-21 18:33:00 -0500

    ENH: Multiplicative factor added to the exponential.

--- Code/BasicFilters/itkExpNegativeImageFilter.h
+++ Code/BasicFilters/itkExpNegativeImageFilter.h
@@ -24,8 +24,10 @@ namespace itk
 {
   
 /** \class ExpNegativeImageFilter
- * \brief Computes the exp(x) pixel-wise
+ * \brief Computes the function exp(-K.x) pixel-wise
  *
+ * Every output pixel is equal to exp(-K.x ). where x is the intensity of the
+ * homologous input pixel, and K is a user-provided constant.
  * 
  * \ingroup IntensityImageFilters  Multithreaded
  *
@@ -36,12 +38,20 @@ namespace Function {
   class ExpNegative
   {
   public:
-    ExpNegative() {};
+    ExpNegative() { m_Factor = 1.0; }
     ~ExpNegative() {};
     inline TOutput operator()( const TInput & A )
     {
-      return static_cast<TOutput>( exp( - static_cast<double>(A) ) );
+      return static_cast<TOutput>( exp( - m_Factor * static_cast<double>(A) ) );
     }
+  void SetFactor( double factor ) {
+    m_Factor = factor;
+    }
+  double GetFactor() const {
+    return m_Factor;
+    }
+  private:
+    double  m_Factor;
   }; 
 }
 template <class TInputImage, class TOutputImage>
@@ -64,6 +74,15 @@ public:
   /** Method for creation through the object factory. */
   itkNewMacro(Self);
   
+  void SetFactor( double factor )
+    {
+    if( factor == this->GetFunctor().GetFactor() ) 
+      {
+      return;
+      }
+    this->GetFunctor().SetFactor( factor );
+    this->Modified();
+    }
 protected:
   ExpNegativeImageFilter() {}
   virtual ~ExpNegativeImageFilter() {}
@@ -71,7 +90,6 @@ protected:
 private:
   ExpNegativeImageFilter(const Self&); //purposely not implemented
   void operator=(const Self&); //purposely not implemented
-
 
 };
 
