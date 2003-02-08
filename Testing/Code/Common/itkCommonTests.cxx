ecace397822935589f538aaf93a318cc35e5611c
Will Schroeder <will.schroeder@kitware.com>
2003-02-07 20:26:43 -0500

    ENH:Renamed test to get bug list off my back

--- Testing/Code/Common/itkCommonTests.cxx
+++ Testing/Code/Common/itkCommonTests.cxx
@@ -10,7 +10,6 @@ REGISTER_TEST(itkModifiedTimeTest );
 REGISTER_TEST(itkAdaptorComparisonTest );
 REGISTER_TEST(itkAffineTransformTest );
 REGISTER_TEST(itkArrayTest );
-REGISTER_TEST(itkArray2Test );
 REGISTER_TEST(itkArray2DTest );
 REGISTER_TEST(itkAutoPointerTest );
 REGISTER_TEST(itkAzimuthElevationToCartesianTransformTest );
