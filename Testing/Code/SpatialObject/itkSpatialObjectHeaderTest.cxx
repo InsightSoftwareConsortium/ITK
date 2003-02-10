e8b6f7e4f4c1caa3a417686627f4bab2dcf133d1
Bill Lorensen <bill.lorensen@gmail.com>
2003-02-10 12:25:14 -0500

    ENH: Updated to latest headers

--- Testing/Code/SpatialObject/itkSpatialObjectHeaderTest.cxx
+++ Testing/Code/SpatialObject/itkSpatialObjectHeaderTest.cxx
@@ -28,6 +28,7 @@ See COPYRIGHT.txt for copyright details.
 #include "itkLandmarkSpatialObject.txx"
 #include "itkLineSpatialObject.txx"
 #include "itkLineSpatialObjectPoint.txx"
+#include "itkNDimensionalSpatialObject.txx"
 #include "itkPlaneSpatialObject.txx"
 #include "itkScene.txx"
 #include "itkSpatialObject.txx"
