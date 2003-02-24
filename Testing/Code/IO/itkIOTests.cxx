f3176c2dca5f27e6e963417dc62c5991b63c27d7
Brad King <brad.king@kitware.com>
2003-02-24 10:03:06 -0500

    BUG: Reverting to old manually implemented test drivers until a more flexible automated one can be implemented.

---
+++ Testing/Code/IO/itkIOTests.cxx
@@ -0,0 +1,21 @@
+// this file defines the itkBasicFiltersTest for the test driver
+// and all it expects is that you have a function called RegisterTests
+#include <iostream>
+#include "itkTestMain.h" 
+
+
+void RegisterTests()
+{
+  REGISTER_TEST(itkConvertBufferTest);
+  REGISTER_TEST(itkDicomImageIOTest);
+  REGISTER_TEST(itkAnalyzeImageIOTest);
+  REGISTER_TEST(itkGiplImageIOTest);
+  REGISTER_TEST(itkMetaImageIOTest);
+  REGISTER_TEST(itkPNGImageIOTest);
+  REGISTER_TEST(itkVOLImageIOTest);
+  REGISTER_TEST(itkVTKImageIOTest);
+  REGISTER_TEST(itkRawImageIOTest);
+  REGISTER_TEST(itkRawImageIOTest2);
+  REGISTER_TEST(itkRawImageIOTest3);
+  REGISTER_TEST(itkRawImageIOTest4);
+}
---
+++ Testing/Code/SpatialObject/itkSpatialObjectTests.cxx
@@ -0,0 +1,16 @@
+// this file defines the itkStatisticsTest for the test driver
+// and all it expects is that you have a function called RegisterTests
+#include <iostream>
+#include "itkTestMain.h" 
+
+
+void RegisterTests()
+{
+  REGISTER_TEST(itkImageSpatialObjectTest);
+  REGISTER_TEST(itkTubeSpatialObjectTest);
+  REGISTER_TEST(itkBlobSpatialObjectTest);
+  REGISTER_TEST(itkLandmarkSpatialObjectTest);
+  REGISTER_TEST(itkLineSpatialObjectTest);
+  REGISTER_TEST(itkEllipseSpatialObjectTest);
+  REGISTER_TEST(itkSurfaceSpatialObjectTest);
+}
