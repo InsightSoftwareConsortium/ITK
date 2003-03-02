53d486752ff0da24c1454fedc176596f75a32017
Mark Foskey <foskey@cs.unc.edu>
2003-03-02 09:56:54 -0500

    DOC: Changed /* to /** so that the Detailed Description would show up in the Doxygen docs.

--- Code/BasicFilters/itkReflectiveImageRegionConstIterator.h
+++ Code/BasicFilters/itkReflectiveImageRegionConstIterator.h
@@ -22,7 +22,7 @@
 namespace itk
 {
 
-/* \class ReflectiveImageRegionConstIterator
+/** \class ReflectiveImageRegionConstIterator
  * \brief Multi-dimensional image iterator which only walks a region.
  * 
  * ReflectiveImageRegionConstIterator is a templated class to represent a
