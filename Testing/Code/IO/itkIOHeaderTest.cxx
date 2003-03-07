9774b9819762dcba9236de933523c096f6625ef8
Bill Lorensen <bill.lorensen@gmail.com>
2003-03-06 20:45:20 -0500

    ENH: Updated to latest headers

--- Testing/Code/IO/itkIOHeaderTest.cxx
+++ Testing/Code/IO/itkIOHeaderTest.cxx
@@ -48,6 +48,8 @@ See COPYRIGHT.txt for copyright details.
 #include "itkPNGImageIOFactory.h"
 #include "itkRawImageIO.txx"
 #include "itkRawImageWriter.txx"
+#include "itkStimulateImageIO.h"
+#include "itkStimulateImageIOFactory.h"
 #include "itkVOLImageIO.h"
 #include "itkVOLImageIOFactory.h"
 #include "itkVTKImageIO.h"
