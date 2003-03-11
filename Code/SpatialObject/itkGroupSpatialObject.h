1b9895b7d12824907a22ab394ba2427d81a5f57c
Mark Foskey <foskey@cs.unc.edu>
2003-03-10 21:31:20 -0500

    DOC: Added detail to overview.

--- Code/SpatialObject/itkGroupSpatialObject.h
+++ Code/SpatialObject/itkGroupSpatialObject.h
@@ -29,6 +29,10 @@ namespace itk
 * \class GroupSpatialObject
 * \brief Representation of a group based on the spatial object classes.
 *
+* A GroupSpatialObject represents a group by serving as the parent of
+* the elements of the group.  Since any itk::SpatialObject can have
+* children (see SpatialObject::GetChildren()), this class needs no
+* additional methods.
 */
 
 template < unsigned int TDimension = 3 >
