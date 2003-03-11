5fc0ca730d26d0d03928955146ffd4e4f97e7ec7
Luis Ibanez <luis.ibanez@kitware.com>
2003-03-11 16:21:37 -0500

    Filter for performing pixel-wise intensity windowing.

---
+++ Code/BasicFilters/itkIntensityWindowingImageFilter.h
@@ -0,0 +1,167 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkIntensityWindowingImageFilter.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkIntensityWindowingImageFilter_h
+#define __itkIntensityWindowingImageFilter_h
+
+#include "itkUnaryFunctorImageFilter.h"
+
+namespace itk
+{
+
+// This functor class applies a linear transformation A.x + B inside a specified 
+// range. Values below the range are mapped to a constant. Values over the range
+// are mapped to another constant.
+namespace Functor {  
+ 
+ template< typename TInput, typename  TOutput>
+ class IntensityWindowingTransform
+ {
+ public:
+   typedef typename NumericTraits< TInput >::RealType RealType;
+   IntensityWindowingTransform() {}
+   ~IntensityWindowingTransform() {}
+   void SetFactor( RealType a ) { m_Factor = a; }
+   void SetOffset( RealType b ) { m_Offset = b; }
+   void SetOutputMinimum( TOutput min ) { m_OutputMinimum = min; }
+   void SetOutputMaximum( TOutput max ) { m_OutputMaximum = max; }
+   void SetWindowMinimum( TInput  min ) { m_WindowMinimum = min; }
+   void SetWindowMaximum( TInput  max ) { m_WindowMaximum = max; }
+   inline TOutput operator()( const TInput & x )
+   {
+     if( x < m_WindowMinimum )
+       {
+       return m_OutputMinimum;
+       }
+     if( x > m_WindowMaximum )
+       {
+       return m_OutputMaximum;
+       }
+     const RealType value  = static_cast<RealType>(x) * m_Factor + m_Offset;
+     const TOutput  result = static_cast<TOutput>( value );
+     return result;
+   }
+  private:
+    RealType m_Factor;
+    RealType m_Offset;
+    TOutput  m_OutputMaximum;
+    TOutput  m_OutputMinimum;
+    TInput   m_WindowMaximum;
+    TInput   m_WindowMinimum;
+ }; 
+
+}  // end namespace functor
+
+
+/** \class IntensityWindowingImageFilter
+ * \brief Applies a linear transformation to the intensity levels of the
+ * input Image that are inside a user-defined interval. Values below this
+ * interval are mapped to a constant. Values over the interval are mapped 
+ * to another constant. 
+ *
+ * IntensityWindowingImageFilter applies pixel-wise a linear transformation
+ * to the intensity values of input image pixels. The linear transformation
+ * is defined by the user in terms of the minimum and maximum values that 
+ * the output image should have and the lower and upper limits of the intensity
+ * window of the input image. This operation is very common in visualization,
+ * and can also be applied as a convenient preprocessing operation for image
+ * segmentation.
+ * 
+ * All computations are performed in the precison of the input pixel's 
+ * RealType. Before assigning the computed value to the output pixel. 
+ *
+ * \ingroup IntensityImageFilters  Multithreaded
+ *
+ */
+template <typename  TInputImage, typename  TOutputImage=TInputImage>
+class ITK_EXPORT IntensityWindowingImageFilter :
+    public
+    UnaryFunctorImageFilter<TInputImage,TOutputImage, 
+    Functor::IntensityWindowingTransform< 
+              typename TInputImage::PixelType, 
+              typename TOutputImage::PixelType>   >
+{
+public:
+  /** Standard class typedefs. */
+  typedef IntensityWindowingImageFilter  Self;
+  typedef UnaryFunctorImageFilter<TInputImage,TOutputImage, 
+    Functor::IntensityWindowingTransform< 
+                  typename TInputImage::PixelType, 
+                  typename TOutputImage::PixelType> >  Superclass;
+  typedef SmartPointer<Self>   Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+
+  typedef typename TOutputImage::PixelType OutputPixelType;
+  typedef typename TInputImage::PixelType  InputPixelType;
+  typedef typename NumericTraits<InputPixelType>::RealType RealType;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+  
+  /** Set/Get the values of the maximum and minimum 
+   *  intensities of the outputimage */
+  itkSetMacro( OutputMinimum, OutputPixelType );
+  itkSetMacro( OutputMaximum, OutputPixelType );
+  itkGetConstMacro( OutputMinimum, OutputPixelType );
+  itkGetConstMacro( OutputMaximum, OutputPixelType );
+
+  /** Set/Get the values of the maximum and minimum 
+   *  intensities of the input intensity window */
+  itkSetMacro( WindowMinimum, InputPixelType );
+  itkSetMacro( WindowMaximum, InputPixelType );
+  itkGetConstMacro( WindowMinimum, InputPixelType );
+  itkGetConstMacro( WindowMaximum, InputPixelType );
+
+  /** Get the Scale and Shift used for the linear transformation
+      of gray level values. 
+   \warning These Values are only valid after the filter has been updated */
+  itkGetConstMacro( Scale, RealType );
+  itkGetConstMacro( Shift, RealType );
+
+  /** Process to execute before entering the multithreaded section */
+  void BeforeThreadedGenerateData(void);
+
+  /** Print internal ivars */
+  void PrintSelf(std::ostream& os, Indent indent) const;
+
+protected:
+  IntensityWindowingImageFilter();
+  virtual ~IntensityWindowingImageFilter() {};
+
+private:
+  IntensityWindowingImageFilter(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+  RealType m_Scale;
+  RealType m_Shift;
+
+  InputPixelType        m_WindowMinimum;
+  InputPixelType        m_WindowMaximum;
+
+  OutputPixelType       m_OutputMinimum;
+  OutputPixelType       m_OutputMaximum;
+
+};
+
+
+  
+} // end namespace itk
+  
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkIntensityWindowingImageFilter.txx"
+#endif
+  
+#endif
---
+++ Code/BasicFilters/itkIntensityWindowingImageFilter.txx
@@ -0,0 +1,110 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkIntensityWindowingImageFilter.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+  Portions of this code are covered under the VTK copyright.
+  See VTKCopyright.txt or http://www.kitware.com/VTKCopyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef _itkIntensityWindowingImageFilter_txx
+#define _itkIntensityWindowingImageFilter_txx
+
+#include "itkIntensityWindowingImageFilter.h"
+#include "itkMinimumMaximumImageCalculator.h"
+
+namespace itk
+{
+
+/**
+ *
+ */
+template <class TInputImage, class TOutputImage>
+IntensityWindowingImageFilter<TInputImage, TOutputImage>
+::IntensityWindowingImageFilter()
+{
+  m_OutputMaximum   = NumericTraits<OutputPixelType>::Zero;
+  m_OutputMinimum   = NumericTraits<OutputPixelType>::max();
+
+  m_WindowMaximum   = NumericTraits<InputPixelType>::Zero;
+  m_WindowMinimum   = NumericTraits<InputPixelType>::max();
+
+  m_Scale = 1.0;
+  m_Shift = 0.0;
+}
+
+
+/**
+ *
+ */
+template <class TInputImage, class TOutputImage>
+void 
+IntensityWindowingImageFilter<TInputImage, TOutputImage>
+::PrintSelf(std::ostream& os, Indent indent) const
+{
+  Superclass::PrintSelf(os,indent);
+
+  os << indent << "Output Minimum: "
+     << static_cast<typename NumericTraits<OutputPixelType>::PrintType>(m_OutputMinimum)
+     << std::endl;
+  os << indent << "Output Maximum: "
+     << static_cast<typename NumericTraits<OutputPixelType>::PrintType>(m_OutputMaximum)
+     << std::endl;
+  os << indent << "Window Minimum: "
+     << static_cast<typename NumericTraits<InputPixelType>::PrintType>(m_WindowMinimum)
+     << std::endl;
+  os << indent << "Window Maximum: "
+     << static_cast<typename NumericTraits<InputPixelType>::PrintType>(m_WindowMaximum)
+     << std::endl;
+  os << indent << "Scale Factor: "
+     << static_cast<typename NumericTraits<RealType>::PrintType>(m_Scale)
+     << std::endl;
+  os << indent << "Shift offset: "
+     << static_cast<typename NumericTraits<RealType>::PrintType>(m_Shift)
+     << std::endl;
+
+}
+
+/**
+ *
+ */
+template <class TInputImage, class TOutputImage>
+void 
+IntensityWindowingImageFilter<TInputImage, TOutputImage>
+::BeforeThreadedGenerateData()
+{
+  m_Scale = 
+    (static_cast<RealType>( m_OutputMaximum )
+   - static_cast<RealType>( m_OutputMinimum )) /
+    (static_cast<RealType>( m_WindowMaximum )
+   - static_cast<RealType>( m_WindowMinimum ));
+
+  m_Shift =
+          static_cast<RealType>( m_OutputMinimum ) - 
+          static_cast<RealType>( m_WindowMinimum ) * m_Scale;
+  
+  // set up the functor values
+  this->GetFunctor().SetOutputMinimum( m_OutputMinimum );
+  this->GetFunctor().SetOutputMaximum( m_OutputMaximum );
+  
+  this->GetFunctor().SetWindowMinimum( m_WindowMinimum );
+  this->GetFunctor().SetWindowMaximum( m_WindowMaximum );
+  
+  this->GetFunctor().SetFactor( m_Scale );
+  this->GetFunctor().SetOffset( m_Shift );
+}
+
+
+} // end namespace itk
+
+#endif
