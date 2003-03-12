d2df19902ba33c6b94be2631a9a430b017909ca3
Robert Tamburo <rjtst21@pitt.edu>
2003-03-11 21:53:29 -0500

    FIX: Used Get/Set functions

--- Code/Common/itkBloxBoundaryProfileItem.h
+++ Code/Common/itkBloxBoundaryProfileItem.h
@@ -6,27 +6,10 @@
 
 namespace itk
 {
-   
 template <unsigned int VImageDimension>
-class BloxBoundaryProfileItem: public BloxItem
+class BloxBoundaryProfileItem : public BloxItem
 {
 public:
-  /** Standard "Self" typedef. */
-  typedef BloxBoundaryProfileItem  Self;
-
-  /** Standard "Superclass" typedef. */
-  typedef BloxItem Superclass;
-
-  /** Smart pointer typedef support */
-  typedef SmartPointer<Self>  Pointer;
-  typedef SmartPointer<const Self>  ConstPointer;
-
-  /** Method for creation through the object factory. */
-  itkNewMacro(Self);
-
-  /** Run-time type information (and related methods). */
-  itkTypeMacro(BloxBoundaryProfileItem, BloxItem);
-
   /** The point type used to store the position of the boundary profile */
   typedef Point<double, VImageDimension> PositionType;
 
@@ -40,63 +23,51 @@ public:
   typedef BloxBoundaryPointItem<VImageDimension> BPItemType;
 
   /** Set the position of the first boundary point in physical space */
-  void SetBoundaryPoint(BPItemType point) {m_BoundaryPoint = point;}
+  void SetBoundaryPoint(BPItemType * point);
 
   /** Set and get lower intensity estimates */
-  void SetLowerIntensity(double lowerIntensity) {m_LowerIntensity = lowerIntensity;}
-  double GetLowerIntensity(void) {return m_LowerIntensity;}
+  void SetLowerIntensity(double lowerIntensity);
+  double GetLowerIntensity(void);
 
   /** Set and get upper intensity estimates */
-  void SetUpperIntensity(double upperIntensity) {m_UpperIntensity = upperIntensity;}
-  double GetUpperIntensity(void) {return(m_UpperIntensity);}
+  void SetUpperIntensity(double upperIntensity);
+  double GetUpperIntensity(void);
 
   /** Set and get mean estimates */
-  void SetMean(double mean) {m_Mean = mean;}
-  double GetMean(void) {return(m_Mean);}
+  //void SetMean(double mean) {m_Mean = mean;}
+  void SetMean(double mean);
+  double GetMean(void);
 
   /** Set and get the length of profile */
-  void SetProfileLength(unsigned int profileLength) {m_ProfileLength = profileLength;}
-  unsigned int GetProfileLength(void) {return(m_ProfileLength);}
+  void SetProfileLength(unsigned int profileLength);
+  unsigned int GetProfileLength(void);
 
   /** Set and get mean normalized by profile length */
-  void SetMeanNormalized(void) {m_MeanNormalized = m_Mean - m_ProfileLength/2;}
-  double GetMeanNormalized(void) {return(m_MeanNormalized);}
+  void SetMeanNormalized(void);
+  double GetMeanNormalized(void);
   
   /** Set and get standard deviation */
-  void SetStandardDeviation(double standardDeviation) {m_StandardDeviation = standardDeviation;}
-  double GetStandardDeviation(void) {return(m_StandardDeviation);}
+  void SetStandardDeviation(double standardDeviation);
+  double GetStandardDeviation(void);
 
   /** Set and get standard deviation normalized by profile length */
-  void SetStandardDeviationNormalized(void)
-  {m_StandardDeviationNormalized = m_StandardDeviation / m_ProfileLength;}
+  void SetStandardDeviationNormalized(void);
 
-  double GetStandardDeviationNormalized(double) {return(m_StandardDeviationNormalized);}
+  double GetStandardDeviationNormalized(void);
 
   /** Set and get optimal boundary location */
-  void SetOptimalBoundaryLocation(VectorType spatialFunctionOriginVector, VectorType orientation)
-    {
-    VectorType optimalBoundaryLocation;
-    optimalBoundaryLocation = m_MeanNormalized * orientation;
-    optimalBoundaryLocation = spatialFunctionOriginVector + optimalBoundaryLocation;
-    for(unsigned int i = 0; i < VImageDimension; i++)
-      m_OptimalBoundaryLocation[i] = optimalBoundaryLocation[i];
-    }
-  PositionType GetOptimalBoundaryLocation(void) {return(m_OptimalBoundaryLocation);}
+  void SetOptimalBoundaryLocation(VectorType spatialFunctionOriginVector, VectorType orientation);
+
+  PositionType GetOptimalBoundaryLocation(void);
 
   /** Set and get the gradient of the boundary profile * */
-  void SetGradient(GradientType grad){m_Gradient = grad;};
-  GradientType GetGradient(){return m_Gradient;};
+  void SetGradient(GradientType * gradient);
+  GradientType GetGradient();
 
   BloxBoundaryProfileItem();
   ~BloxBoundaryProfileItem();
 
-protected:
-  BloxBoundaryProfileItem(const Self&) {}
-  void operator=(const Self&) {}
-  void PrintSelf(std::ostream& os, Indent indent) const;
-
 private:
-
   /** Lower estimated intensity */
   double m_LowerIntensity;
 
@@ -119,17 +90,16 @@ private:
   double m_StandardDeviationNormalized;
 
   /** The boundary point to construct a profile from */
-  BPItemType m_BoundaryPoint;
+  BPItemType * m_BoundaryPoint;
 
   /** The position of the estimated boundary location */
   PositionType m_OptimalBoundaryLocation;
 
   /** The gradient of the boundary point (non-normalized) */
-  GradientType m_Gradient;
+  GradientType * m_Gradient;
 };
 
 } // end namespace itk
-
 
 #ifndef ITK_MANUAL_INSTANTIATION
 #include "itkBloxBoundaryProfileItem.txx"
--- Code/Common/itkBloxBoundaryProfileItem.txx
+++ Code/Common/itkBloxBoundaryProfileItem.txx
@@ -5,7 +5,6 @@
 
 namespace itk
 {
-
 template <unsigned int VImageDimension>
 BloxBoundaryProfileItem<VImageDimension>
 ::BloxBoundaryProfileItem()
@@ -17,13 +16,171 @@ BloxBoundaryProfileItem<VImageDimension>
   m_MeanNormalized = 0;
   m_StandardDeviation = 0;
   m_StandardDeviationNormalized = 0;
+  m_BoundaryPoint = 0;
+  m_Gradient = 0;
 }
 
 template <unsigned int VImageDimension>
 BloxBoundaryProfileItem<VImageDimension>
 ::~BloxBoundaryProfileItem()
 {
-  
+
+}
+
+template <unsigned int VImageDimension>
+void
+BloxBoundaryProfileItem<VImageDimension>
+::SetBoundaryPoint(BPItemType * point) 
+{
+  m_BoundaryPoint = point;
+}
+
+template <unsigned int VImageDimension>
+void
+BloxBoundaryProfileItem<VImageDimension>
+::SetLowerIntensity(double lowerIntensity)
+{
+  m_LowerIntensity = lowerIntensity;
+}
+
+template <unsigned int VImageDimension>
+double
+BloxBoundaryProfileItem<VImageDimension>
+::GetLowerIntensity(void)
+{
+  return m_LowerIntensity;
+}
+
+template <unsigned int VImageDimension>
+void
+BloxBoundaryProfileItem<VImageDimension>
+::SetUpperIntensity(double upperIntensity)
+{
+  m_UpperIntensity = upperIntensity;
+}
+
+template <unsigned int VImageDimension>
+double
+BloxBoundaryProfileItem<VImageDimension>
+::GetUpperIntensity(void)
+{
+  return(m_UpperIntensity);
+}
+
+template <unsigned int VImageDimension>
+void
+BloxBoundaryProfileItem<VImageDimension>
+::SetMean(double mean)
+{
+  m_Mean = mean;
+}
+
+template <unsigned int VImageDimension>
+double
+BloxBoundaryProfileItem<VImageDimension>
+::GetMean(void)
+{
+  return(m_Mean);
+}
+
+template <unsigned int VImageDimension>
+void 
+BloxBoundaryProfileItem<VImageDimension>
+::SetProfileLength(unsigned int profileLength)
+{
+  m_ProfileLength = profileLength;
+}
+
+template <unsigned int VImageDimension>
+unsigned int
+BloxBoundaryProfileItem<VImageDimension>
+::GetProfileLength(void)
+{
+  return(m_ProfileLength);
+}
+
+template <unsigned int VImageDimension>
+void
+BloxBoundaryProfileItem<VImageDimension>
+::SetMeanNormalized(void)
+{
+  m_MeanNormalized = m_Mean - m_ProfileLength/2;
+}
+
+template <unsigned int VImageDimension>
+double
+BloxBoundaryProfileItem<VImageDimension>
+::GetMeanNormalized(void)
+{
+  return(m_MeanNormalized);
+}
+
+template <unsigned int VImageDimension>
+void
+BloxBoundaryProfileItem<VImageDimension>
+::SetStandardDeviation(double standardDeviation)
+{
+  m_StandardDeviation = standardDeviation;
+}
+
+template <unsigned int VImageDimension>
+double
+BloxBoundaryProfileItem<VImageDimension>
+::GetStandardDeviation(void)
+{
+  return(m_StandardDeviation);
+}
+
+template <unsigned int VImageDimension>
+void
+BloxBoundaryProfileItem<VImageDimension>
+::SetStandardDeviationNormalized(void)
+{
+  m_StandardDeviationNormalized = m_StandardDeviation / m_ProfileLength;
+}
+
+template <unsigned int VImageDimension>
+double
+BloxBoundaryProfileItem<VImageDimension>
+::GetStandardDeviationNormalized(void)
+{
+  return(m_StandardDeviationNormalized);
+}
+
+template <unsigned int VImageDimension>
+void
+BloxBoundaryProfileItem<VImageDimension>
+::SetOptimalBoundaryLocation(VectorType spatialFunctionOriginVector, VectorType orientation)
+{
+  VectorType optimalBoundaryLocation;
+  optimalBoundaryLocation = m_MeanNormalized * orientation;
+  optimalBoundaryLocation = spatialFunctionOriginVector + optimalBoundaryLocation;
+  for(unsigned int i = 0; i < VImageDimension; i++)
+    m_OptimalBoundaryLocation[i] = optimalBoundaryLocation[i];
+}
+
+template <unsigned int VImageDimension>
+Point<double, VImageDimension>
+BloxBoundaryProfileItem<VImageDimension>
+::GetOptimalBoundaryLocation(void)
+{
+  return(m_OptimalBoundaryLocation);
+}
+
+template <unsigned int VImageDimension>
+void
+BloxBoundaryProfileItem<VImageDimension>
+::SetGradient(GradientType * gradient)
+{
+  m_Gradient = gradient;
+}
+
+template <unsigned int VImageDimension>
+CovariantVector<double, VImageDimension>
+BloxBoundaryProfileItem<VImageDimension>
+::GetGradient()
+{
+  return m_Gradient;
 }
 
 } // end namespace itk
