6339c80343914118e4426273b961fd8fc9d85ece
Matt Turek <turek@crd.ge.com>
2003-03-13 13:58:48 -0500

    Tests itkDICOMImageIO2

---
+++ Testing/Code/IO/itkDICOMImageIO2Test.cxx
@@ -0,0 +1,84 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkDICOMImageIO2Test.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include <list>
+#include <fstream>
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkImage.h"
+#include "itkRescaleIntensityImageFilter.h"
+
+int itkDICOMImageIO2Test(int ac, char* av[])
+{
+
+  if(ac < 3)
+  {
+    std::cerr << "Usage: " << av[0] << " DicomImage OutputImage\n";
+    return EXIT_FAILURE;
+  }
+
+
+  // ATTENTION THIS IS THE PIXEL TYPE FOR 
+  // THE RESULTING IMAGE
+  typedef unsigned short PixelType;
+  
+  typedef itk::Image<PixelType, 2> myImage;
+
+  itk::ImageFileReader<myImage>::Pointer reader 
+                                  = itk::ImageFileReader<myImage>::New();
+  // Register on factory capable of creating DicomImage readers
+  itk::DICOMImageIO2Factory::RegisterOneFactory();
+  
+  reader->SetFileName(av[1]);
+  
+  try
+  {
+    reader->Update();
+  }
+  catch (itk::ExceptionObject & e)
+  {
+    std::cerr << "exception in file reader " << std::endl;
+    std::cerr << e.GetDescription() << std::endl;
+    std::cerr << e.GetLocation() << std::endl;
+    return EXIT_FAILURE;
+  }
+  
+  typedef unsigned char WritePixelType;
+
+  typedef itk::Image< WritePixelType, 2 > WriteImageType;
+
+  typedef itk::RescaleIntensityImageFilter< 
+               myImage, WriteImageType > RescaleFilterType;
+
+  RescaleFilterType::Pointer rescaler = RescaleFilterType::New();
+
+  rescaler->SetOutputMinimum(   0 );
+  rescaler->SetOutputMaximum( 255 );
+  
+
+  typedef itk::ImageFileWriter< WriteImageType >  WriterType;
+
+  WriterType::Pointer writer = WriterType::New();
+
+  writer->SetFileName( av[2] );
+ 
+  // Software Guide : BeginCodeSnippet
+  rescaler->SetInput( reader->GetOutput() );
+  writer->SetInput( rescaler->GetOutput() );
+  writer->Update();
+  return EXIT_SUCCESS;
+
+}
