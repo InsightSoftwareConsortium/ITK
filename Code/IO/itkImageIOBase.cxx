29a798bf5d03275c3ddb9157f64cd3954d08d86d
Bill Lorensen <bill.lorensen@gmail.com>
2003-03-13 23:24:57 -0500

    ERR: warnings.

--- Code/IO/itkImageIOBase.cxx
+++ Code/IO/itkImageIOBase.cxx
@@ -14,6 +14,9 @@
      PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
 #include "itkImageIOBase.h"
 #include "itkRGBPixel.h"
 #include "itkRGBAPixel.h"
