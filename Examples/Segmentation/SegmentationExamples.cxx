871db89c75fab932a0c9d1fb53d728ea38ad0f9a
Bill Lorensen <bill.lorensen@gmail.com>
2003-03-20 10:13:42 -0500

    ERR: warnings.

--- Examples/Segmentation/SegmentationExamples.cxx
+++ Examples/Segmentation/SegmentationExamples.cxx
@@ -1,5 +1,8 @@
 // this file defines the SegmentationExamples for the test driver
 // and all it expects is that you have a function called RegisterTests
+#ifdef _MSC_VER
+#pragma warning ( disable : 4786 )
+#endif
 #include <iostream>
 #include "itkTestMain.h" 
 
