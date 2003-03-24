9a65d7d7c7373a3a7c0099499335c9403a341a77
Luis Ibanez <luis.ibanez@kitware.com>
2003-03-24 11:59:38 -0500

    Example on the use of the DerivativeImageFilter.

---
+++ Examples/Filtering/DerivativeImageFilter.cxx
@@ -0,0 +1,166 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    DerivativeImageFilter.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+//  Software Guide : BeginLatex
+//
+//  The \doxygen{DerivativeImageFilter} is used for computing the derivative
+//  of an image along a particular direction.
+//
+//  \index{itk::DerivativeImageFilter|textbf}
+//
+//  Software Guide : EndLatex 
+
+
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+
+
+
+//  Software Guide : BeginLatex
+//
+//  The header file corresponding to this filter should be included first.
+//
+//  \index{itk::DerivativeImageFilter!header|textbf}
+//
+//  Software Guide : EndLatex 
+
+
+// Software Guide : BeginCodeSnippet
+#include "itkDerivativeImageFilter.h"
+// Software Guide : EndCodeSnippet
+
+
+
+
+int main( int argc, char * argv[] )
+{
+
+
+  if( argc < 5 )
+    {
+    std::cerr << "Usage: " << std::endl;
+    std::cerr << argv[0] << "  inputImageFile   outputImageFile  derivativeOrder direction" << std::endl;
+    return 1;
+    }
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  Then the pixel types for input and output image must be defined and with
+  //  them the image types can be instantiated. Note that it is important to
+  //  select a signed type for the image, since the values of the derivatives
+  //  will be positive as well as negative.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  typedef   float  InputPixelType;
+  typedef   float  OutputPixelType;
+
+  typedef itk::Image< InputPixelType,  2 >   InputImageType;
+  typedef itk::Image< OutputPixelType, 2 >   OutputImageType;
+  // Software Guide : EndCodeSnippet
+
+
+  typedef itk::ImageFileReader< InputImageType  >  ReaderType;
+  typedef itk::ImageFileWriter< OutputImageType >  WriterType;
+
+  ReaderType::Pointer reader = ReaderType::New();
+  WriterType::Pointer writer = WriterType::New();
+
+  reader->SetFileName( argv[1] );
+  writer->SetFileName( argv[2] );
+
+  //  Software Guide : BeginLatex
+  //
+  //  Using the image types it is now possible to instantiate the filter type
+  //  and create the filter object. 
+  //
+  //  \index{itk::DerivativeImageFilter!instantiation}
+  //  \index{itk::DerivativeImageFilter!New()}
+  //  \index{itk::DerivativeImageFilter!Pointer}
+  // 
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  typedef itk::DerivativeImageFilter<
+               InputImageType, OutputImageType >  FilterType;
+
+  FilterType::Pointer filter = FilterType::New();
+  // Software Guide : EndCodeSnippet
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  The order of the derivative is selected with the \code{SetOrder()} method.
+  //  The direction along which the derivative will be computed is selected with
+  //  the \code{SetDirection()} method.
+  //
+  //  \index{itk::DerivativeImageFilter!SetOrder()}
+  //  \index{itk::DerivativeImageFilter!SetDirection()}
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  filter->SetOrder(     atoi( argv[3] ) );
+  filter->SetDirection( atoi( argv[4] ) );
+  // Software Guide : EndCodeSnippet
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  The input to the filter can be taken from any other filter, for example a
+  //  reader. The output can be passed down the pipeline to other filters, for
+  //  example a writer. An update call on any downstream filter will trigger
+  //  the execution of the mean filter.
+  //
+  //  \index{itk::DerivativeImageFilter!SetInput()}
+  //  \index{itk::DerivativeImageFilter!GetOutput()}
+  //
+  //  Software Guide : EndLatex 
+
+
+  // Software Guide : BeginCodeSnippet
+  filter->SetInput( reader->GetOutput() );
+  writer->SetInput( filter->GetOutput() );
+  writer->Update();
+  // Software Guide : EndCodeSnippet
+
+
+  //  Software Guide : BeginLatex
+  // 
+  // \begin{figure}
+  // \center
+  // \includegraphics[width=6cm]{BrainProtonDensitySlice.eps}
+  // \includegraphics[width=6cm]{DerivativeImageFilterOutput.eps}
+  // \caption{Effect of the Derivative filter on a slice from a MRI
+  // Proton Density brain image.}
+  // \label{fig:DerivativeImageFilterOutput}
+  // \end{figure}
+  //
+  //  Figure \ref{fig:DerivativeImageFilterOutput} illustrate the effect of this
+  //  filter on a slice of MRI brain image. The derivative is taken along the X direction.
+  //  The sensibility to noise present in the image is evident from this result.
+  //
+  //  Software Guide : EndLatex 
+
+
+  return 0;
+
+}
+
