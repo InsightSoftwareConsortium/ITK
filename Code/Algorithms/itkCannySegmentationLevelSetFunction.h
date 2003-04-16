4291e1b6fc1f592bd8e5307baf5eb185fbde29ea
Josh Cates <cates@cs.utah.edu>
2003-04-16 17:00:45 -0400

    DOC: More documentation.

--- Code/Algorithms/itkCannySegmentationLevelSetFunction.h
+++ Code/Algorithms/itkCannySegmentationLevelSetFunction.h
@@ -22,9 +22,9 @@
 namespace itk {
 
 /**
- *
- *
- * 
+ * \brief A refinement of the standard level-set function which computes a
+ * speed term and advection term based on pseudo-Canny edges.  See
+ * CannySegmentationLevelSetImageFilter for complete information.
  */
 template <class TImageType, class TFeatureImageType = TImageType>
 class ITK_EXPORT CannySegmentationLevelSetFunction
