310a012724cad97b3ccd64181734bb5bbe2c09d7
Luis Ibanez <luis.ibanez@kitware.com>
2003-04-19 14:43:51 -0400

    Example on the use of the SmoothingRecursiveGaussianImageFilter.      This filter packages a mini-pipeline in order to smooth an image      along all the directions.

---
+++ Examples/Filtering/SmoothingRecursiveGaussianImageFilter2.cxx
@@ -0,0 +1,244 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    SmoothingRecursiveGaussianImageFilter2.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+//  Software Guide : BeginLatex
+//
+//  Setting up a pipeline of N filters in order to smooth an N-Dimensional
+//  image may be a lot of work to do for achieving a simple goal. In order to
+//  avoid this inconvenience, a filter packaging this N filters internally
+//  is available. This filter is the \doxygen{SmoothingRecursiveGaussianImageFilter}.
+//
+//  \index{itk::SmoothingRecursiveGaussianImageFilter}
+//
+//  Software Guide : EndLatex 
+
+
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkRescaleIntensityImageFilter.h"
+
+//  Software Guide : BeginLatex
+//
+//  In order to use this filter the following header file must be included
+//
+//  \index{itk::SmoothingRecursiveGaussianImageFilter!header}
+//
+//  Software Guide : EndLatex 
+
+// Software Guide : BeginCodeSnippet
+#include "itkSmoothingRecursiveGaussianImageFilter.h"
+// Software Guide : EndCodeSnippet
+
+
+
+
+int main( int argc, char * argv[] )
+{
+
+
+  if( argc < 4 ) 
+    { 
+    std::cerr << "Usage: " << std::endl;
+    std::cerr << argv[0] << "  inputImageFile  outputImageFile  sigma " << std::endl;
+    return 1;
+    }
+
+  
+  //  Software Guide : BeginLatex
+  //
+  //  Types should be choosen for the pixels of the input and output images.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  typedef    float    InputPixelType;
+  typedef    float    OutputPixelType;
+  // Software Guide : EndCodeSnippet
+
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  With them, the input and output image types can be instantiated.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  typedef itk::Image< InputPixelType,  2 >   InputImageType;
+  typedef itk::Image< OutputPixelType, 2 >   OutputImageType;
+  // Software Guide : EndCodeSnippet
+
+
+
+  typedef itk::ImageFileReader< InputImageType >  ReaderType;
+
+  
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  The filter type is now instantiated using both the input image and the
+  //  output image types. If the second template parameter is omitted, the
+  //  filter will assume that the output image has the same type as the input
+  //  image.
+  //
+  //  \index{itk::SmoothingRecursiveGaussianImageFilter!Instantiation}
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  typedef itk::SmoothingRecursiveGaussianImageFilter<
+                        InputImageType, OutputImageType >  FilterType;
+  // Software Guide : EndCodeSnippet
+
+
+
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( argv[1] );
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  Now a single filter is enough for smoothing the image along all the
+  //  dimensions.  The filter is created by invoking the \code{New()} method
+  //  and assigning the result to a \doxygen{SmartPointer}.
+  //
+  //  \index{itk::SmoothingRecursiveGaussianImageFilter!New()}
+  //  \index{itk::SmoothingRecursiveGaussianImageFilter!Pointer}
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  FilterType::Pointer filter = FilterType::New();
+  // Software Guide : EndCodeSnippet
+
+  //  Software Guide : BeginLatex
+  //
+  //  As in the previous examples we should decide what type of normalization
+  //  to use during the computation of the Gaussians. The method   
+  //  \code{SetNormalizeAcrossScale()} serves this purpose. 
+  //  \index{SmoothingRecursiveGaussianImageFilter!SetNormalizeAcrossScale()}
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  filter->SetNormalizeAcrossScale( false );
+  // Software Guide : EndCodeSnippet
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  The input image can be obtained from the output of another filter. Here,
+  //  an image reader is used as source. The image is passed directly to the 
+  //  smoothing filter.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  filter->SetInput( reader->GetOutput() );
+  // Software Guide : EndCodeSnippet
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  It is now time for selecting the sigma of the Gaussian to use for
+  //  smoothing the data. Note that sigma is considered to be in millimeters.
+  //  That is, at the moment of applying the smoothing process, the filter will
+  //  take into account the spacing values defined in the image.
+  //
+  //  \index{itk::SmoothingRecursiveGaussianImageFilter!SetSigma()}
+  //  \index{SetSigma()!itk::SmoothingRecursiveGaussianImageFilter}
+  //
+  //  Software Guide : EndLatex 
+
+  const double sigma = atof( argv[3] );
+
+  // Software Guide : BeginCodeSnippet
+  filter->SetSigma( sigma );
+  // Software Guide : EndCodeSnippet
+
+
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  Finally the pipeline is executed by invoking the \code{Update()} method.
+  //
+  //  \index{itk::SmoothingRecursiveGaussianImageFilter!Update()}
+  //
+  //  Software Guide : EndLatex 
+
+
+  // Software Guide : BeginCodeSnippet
+  filter->Update();
+  // Software Guide : EndCodeSnippet
+
+
+
+  typedef  unsigned char  WritePixelType;
+  typedef itk::Image< WritePixelType, 2 >    WriteImageType;
+
+  typedef itk::RescaleIntensityImageFilter< 
+                   OutputImageType, WriteImageType > RescaleFilterType;
+
+  RescaleFilterType::Pointer rescaler = RescaleFilterType::New();
+  
+  rescaler->SetOutputMinimum(   0 );
+  rescaler->SetOutputMaximum( 255 );
+
+  typedef itk::ImageFileWriter< WriteImageType >  WriterType;
+
+  WriterType::Pointer writer = WriterType::New();
+
+  writer->SetFileName( argv[2] );
+ 
+
+  rescaler->SetInput( filter->GetOutput() );
+  writer->SetInput( rescaler->GetOutput() );
+  writer->Update();
+  
+
+
+  //  Software Guide : BeginLatex
+  //  
+  // \begin{figure}
+  // \center
+  // \includegraphics[width=6cm]{SmoothingRecursiveGaussianImageFilterOutput3.eps}
+  // \includegraphics[width=6cm]{SmoothingRecursiveGaussianImageFilterOutput5.eps}
+  // \caption[SmoothingRecursiveGaussianImageFilter output]{Effect of the
+  // SmoothingRecursiveGaussianImageFilter on a slice from a MRI Proton Density image
+  // of the brain.}
+  // \label{fig:SmoothingRecursiveGaussianImageFilterInputOutput}
+  // \end{figure}
+  //
+  //  Figure \ref{fig:SmoothingRecursiveGaussianImageFilterInputOutput}
+  //  illustrates the effect of this filter on a MRI proton density image of
+  //  the brain using a sigma value of $3$ (left) and a value of $5$ (right).
+  //  The figure shows how the attenuation of noise can be regulated by
+  //  selecting an apropriate sigma.  This type of scale-tunable filter is
+  //  suitable for performing Scale-Space analysis.
+  //
+  //  Software Guide : EndLatex 
+
+
+
+
+  return 0;
+
+}
+
