dcb6b8bf829970901e4e916434a5963be3293cea
Julien Jomier <julien.jomier@kitware.com>
2003-04-23 18:08:57 -0400

    FIX: warnings, unused arguments

--- Testing/Code/IO/itkImageSeriesIOTest.cxx
+++ Testing/Code/IO/itkImageSeriesIOTest.cxx
@@ -21,7 +21,7 @@
 #include "itkImageSeriesWriter.h"
 #include "itkImage.h"
 
-int itkImageSeriesIOTest(int , char *)
+int itkImageSeriesIOTest(int , char * [])
 {
   // Create a typedef to make the code more digestable
   //
