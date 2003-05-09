abe744f978a6ff3387ca1244b7ab15257182db9d
Tessa Sundaram <Tessa.Sundaram@uphs.upenn.edu>
2003-05-09 16:29:12 -0400

    FIX: fixed typos

--- Examples/Segmentation/WatershedSegmentation1.cxx
+++ Examples/Segmentation/WatershedSegmentation1.cxx
@@ -22,9 +22,9 @@
 //
 // The following example illustrates how to preprocess and segment images using
 // the \code{itk::WatershedImageFilter}. Note that the care with which the data
-// prepared will greatly affect the quality of your result.  Typically, the
+// is prepared will greatly affect the quality of your result.  Typically, the
 // best results are obtained by preprocessing the original image with an
-// edge-preserving diffusion such as one of the anisotropic diffusion filters,
+// edge-preserving diffusion filter, such as one of the anisotropic diffusion filters,
 // or with the bilateral image filter.  As noted in
 // Section~\ref{sec:AboutWatersheds}, the height function used as input should
 // be created such that higher positive values correspond to object boundaries.
@@ -109,14 +109,16 @@ int main( int argc, char *argv[] )
   
   // Software Guide : BeginLatex
   //
-  // Next we instantiate the filters and set their parameters.  The first step
-  // in the image processing pipeline is diffusion of the color input image
-  // using an anisotropic diffusion filter.  For this class of filters, the CFL
-  // condition requires that the time step be no more than 0.25 for two-
-  // dimensional images, and no more than 0.125 for three-dimensional images.
-  // The number of iterations and the conductance term will be taken from the
-  // command line. See Section \ref{sec:EdgePreservingSmoothingFilters} for
-  // more information on the ITK anisotropic diffusion filters.
+  // Next we instantiate the filters and set their parameters.  The
+  // first step in the image processing pipeline is diffusion of the
+  // color input image using an anisotropic diffusion filter.  For
+  // this class of filters, the CFL condition requires that the time
+  // step be no more than 0.25 for two-dimensional images, and no more
+  // than 0.125 for three-dimensional images.  The number of
+  // iterations and the conductance term will be taken from the
+  // command line. See Section
+  // \ref{sec:EdgePreservingSmoothingFilters} for more information on
+  // the ITK anisotropic diffusion filters.
   //
   // Software Guide : EndLatex
   
@@ -130,7 +132,7 @@ int main( int argc, char *argv[] )
   // Software Guide : BeginLatex
   //
   // The ITK gradient magnitude filter for vector-valued images can optionally
-  // take several parameters.  Here we allow only enabling or disabling of 
+  // take several parameters.  Here we allow only enabling or disabling 
   // of principle component analysis.
 
   // Software Guide : BeginCodeSnippet
@@ -159,7 +161,7 @@ int main( int argc, char *argv[] )
   // The output of \code{itk::WatershedImageFilter} is an image of unsigned
   // long integer labels, where a label denotes membership of a pixel in a
   // particular segmented region.  This format is not practical for
-  // visualization, so, for the purposes of this example, we will
+  // visualization, so for the purposes of this example, we will
   // convert it to RGB pixels.  RGB images have the advantage that they can be saved as a simple png file and
   // viewed using any standard image viewer software.  The
   // \code{itk::Functor::ScalarToRGBPixelFunctor} class is a special function
