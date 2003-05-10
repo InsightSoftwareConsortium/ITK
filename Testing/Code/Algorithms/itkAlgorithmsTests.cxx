52be58e6380dabd523de5bca0e944f10fe2f9b7d
Bill Lorensen <bill.lorensen@gmail.com>
2003-05-10 07:40:17 -0400

    ENH: split AlgorithmsPrintTest into 2 pieces.

--- Testing/Code/Algorithms/itkAlgorithmsTests.cxx
+++ Testing/Code/Algorithms/itkAlgorithmsTests.cxx
@@ -7,6 +7,7 @@
 void RegisterTests()
 {
 REGISTER_TEST(itkAlgorithmsPrintTest  );
+REGISTER_TEST(itkAlgorithmsPrintTest2  );
 REGISTER_TEST(itkAntiAliasBinaryImageFilterTest  );
 REGISTER_TEST(itkBinaryMinMaxCurvatureFlowImageFilterTest  );  
 REGISTER_TEST(itkBinaryMask3DMeshSourceTest  );  
