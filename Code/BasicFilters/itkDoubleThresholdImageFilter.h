ae197ba41dd370752de71da4a04565227c2c7955
Jim Miller <millerjv@crd.ge.com>
2003-05-28 22:10:11 -0400

    New filter

---
+++ Code/BasicFilters/itkDoubleThresholdImageFilter.h
@@ -0,0 +1,151 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkDoubleThresholdImageFilter.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkDoubleThresholdImageFilter_h
+#define __itkDoubleThresholdImageFilter_h
+
+#include "itkImageToImageFilter.h"
+#include "itkConceptChecking.h"
+
+namespace itk
+{
+  
+/** \class DoubleThresholdImageFilter
+ * \brief Binarize an input image using double thresholding.
+ *
+ * Double threshold addresses the difficulty in selecting a threshold
+ * that will select the objects of interest without selecting
+ * extraneous objects. Double threshold considers two threshold
+ * ranges: a narrow range and a wide range (where the wide range
+ * encompasses the narrow range). If the wide range was used for a
+ * traditional threshold (where values inside the range map to the
+ * foreground and values outside the range map to the background),
+ * many extraneous pixels may survive the threshold operation. If the
+ * narrow range was used for a traditional threshold, then too few
+ * pixels may survive the threshold.
+ *
+ * Double threshold uses the narrow threshold image as a marker image
+ * and the wide threshold image as a mask image in the geodesic
+ * dilation. Essentially, the marker image (narrow threshold) is
+ * dilated but constrained to lie within the mask image (wide
+ * threshold). Thus, only the objects of interest (those pixels that
+ * survived the narrow threshold) are extracted but the those objects
+ * appear in the final image as they would have if the wide threshold
+ * was used.
+ *
+ * \sa GrayscaleGeodesicDilateImageFilter
+ * \sa MorphologyImageFilter, GrayscaleDilateImageFilter, GrayscaleFunctionDilateImageFilter, BinaryDilateImageFilter
+ * \ingroup ImageEnhancement  MathematicalMorphologyImageFilters
+ */
+template <class TInputImage, class TOutputImage>
+class ITK_EXPORT DoubleThresholdImageFilter :
+    public ImageToImageFilter<TInputImage, TOutputImage>
+{
+public:
+  /** Standard class typedefs. */
+  typedef DoubleThresholdImageFilter  Self;
+  typedef ImageToImageFilter<TInputImage,TOutputImage> Superclass;
+  typedef SmartPointer<Self>   Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(DoubleThresholdImageFilter, ImageToImageFilter);
+
+  /** Pixel types. */
+  typedef typename TInputImage::PixelType  InputPixelType;
+  typedef typename TOutputImage::PixelType OutputPixelType;
+
+  /** The input pixel type must support comparison operators. */
+  itkConceptMacro(PixelTypeComparable, (Concept::Comparable<InputPixelType>));
+
+  /** Set the "outside" pixel value. The default value 
+   * NumericTraits<OutputPixelType>::Zero. */
+  itkSetMacro(OutsideValue,OutputPixelType);
+  
+  /** Get the "outside" pixel value. */
+  itkGetMacro(OutsideValue,OutputPixelType);
+
+  /** Set the "inside" pixel value. The default value 
+   * NumericTraits<OutputPixelType>::max() */
+  itkSetMacro(InsideValue,OutputPixelType);
+  
+  /** Get the "inside" pixel value. */
+  itkGetMacro(InsideValue,OutputPixelType);
+
+  /** Set the thresholds. Four thresholds should be specified.  The
+   * two lower thresholds default to
+   * NumericTraits<InputPixelType>::NonpositiveMin(). The two upper
+   * thresholds default NumericTraits<InputPixelType>::max.
+   * Threshold1 <= Threshold2 <= Threshold3 <= Threshold4. */
+  itkSetMacro( Threshold1, InputPixelType );
+  itkSetMacro( Threshold2, InputPixelType );
+  itkSetMacro( Threshold3, InputPixelType );
+  itkSetMacro( Threshold4, InputPixelType );
+                 
+  /** Get the threshold values. */
+  itkGetMacro( Threshold1, InputPixelType );
+  itkGetMacro( Threshold2, InputPixelType );
+  itkGetMacro( Threshold3, InputPixelType );
+  itkGetMacro( Threshold4, InputPixelType );
+
+  /** Get the number of iterations used by the geodesic dilation to
+   * produce the current output. */
+  itkGetMacro( NumberOfIterationsUsed, unsigned long);
+  
+protected:
+  DoubleThresholdImageFilter();
+  virtual ~DoubleThresholdImageFilter() {}
+  void PrintSelf(std::ostream& os, Indent indent) const;
+
+  /** DoubleThresholdImageFilter needs all of the input. So it must
+   * provide an implementation of GenerateInputRequestedRegion() */
+  void GenerateInputRequestedRegion();
+
+  /** DoubleThresholdImageFilter produces all of the output and must
+   * provide an implementation of EnlargeOutputRequestedRegion() */
+  void EnlargeOutputRequestedRegion(DataObject *itkNotUsed(output));
+
+  /** Single threaded version of
+   * GenerateData(). DoubleThresholdImageFilter delegates its
+   * implementation to the GrayscaleGeodesicDilateImageFilter. */
+  void GenerateData();
+  
+private:
+  DoubleThresholdImageFilter(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+  InputPixelType      m_Threshold1;
+  InputPixelType      m_Threshold2;
+  InputPixelType      m_Threshold3;
+  InputPixelType      m_Threshold4;
+
+  OutputPixelType     m_InsideValue;
+  OutputPixelType     m_OutsideValue;
+
+  unsigned long       m_NumberOfIterationsUsed;
+
+};
+
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkDoubleThresholdImageFilter.txx"
+#endif
+
+#endif
---
+++ Code/BasicFilters/itkDoubleThresholdImageFilter.txx
@@ -0,0 +1,132 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkDoubleThresholdImageFilter.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkDoubleThresholdImageFilter_txx
+#define __itkDoubleThresholdImageFilter_txx
+
+#include "itkDoubleThresholdImageFilter.h"
+#include "itkGrayscaleGeodesicDilateImageFilter.h"
+#include "itkBinaryThresholdImageFilter.h"
+
+namespace itk {
+
+template <class TInputImage, class TOutputImage>
+DoubleThresholdImageFilter<TInputImage, TOutputImage>
+::DoubleThresholdImageFilter()
+  : m_NumberOfIterationsUsed( 0 )
+{
+  m_Threshold1 = NumericTraits<InputPixelType>::NonpositiveMin();
+  m_Threshold2 = NumericTraits<InputPixelType>::NonpositiveMin();
+  m_Threshold3 = NumericTraits<InputPixelType>::max();
+  m_Threshold4 = NumericTraits<InputPixelType>::max();
+
+  m_OutsideValue   = NumericTraits<OutputPixelType>::Zero;
+  m_InsideValue    = NumericTraits<OutputPixelType>::max();
+}
+
+template <class TInputImage, class TOutputImage>
+void 
+DoubleThresholdImageFilter<TInputImage, TOutputImage>
+::GenerateInputRequestedRegion()
+{
+  // call the superclass' implementation of this method
+  Superclass::GenerateInputRequestedRegion();
+  
+  // We need all the input.
+  InputImagePointer input = const_cast<InputImageType *>(this->GetInput());
+  
+  input->SetRequestedRegion( input->GetLargestPossibleRegion() );
+}
+
+
+template <class TInputImage, class TOutputImage>
+void 
+DoubleThresholdImageFilter<TInputImage, TOutputImage>
+::EnlargeOutputRequestedRegion(DataObject *)
+{
+  this->GetOutput()
+    ->SetRequestedRegion( this->GetOutput()->GetLargestPossibleRegion() );
+}
+
+
+template<class TInputImage, class TOutputImage>
+void
+DoubleThresholdImageFilter<TInputImage, TOutputImage>
+::GenerateData()
+{
+  // Allocate the output
+  this->AllocateOutputs();
+
+  // Build a mini-pipeline that involves two thresholds filters and a
+  // geodesic dilation.
+  typedef BinaryThresholdImageFilter<TInputImage, TOutputImage> ThresholdFilterType;
+  typedef GrayscaleGeodesicDilateImageFilter<TOutputImage, TOutputImage> DilationFilterType;
+
+  ThresholdFilterType::Pointer narrowThreshold = ThresholdFilterType::New();
+  narrowThreshold->SetLowerThreshold( m_Threshold2 );
+  narrowThreshold->SetUpperThreshold( m_Threshold3 );
+  narrowThreshold->SetInsideValue( m_InsideValue );
+  narrowThreshold->SetOutsideValue( m_OutsideValue );
+  narrowThreshold->SetInput( this->GetInput() );
+
+  ThresholdFilterType::Pointer wideThreshold = ThresholdFilterType::New();
+  wideThreshold->SetLowerThreshold( m_Threshold1 );
+  wideThreshold->SetUpperThreshold( m_Threshold4 );
+  wideThreshold->SetInsideValue( m_InsideValue );
+  wideThreshold->SetOutsideValue( m_OutsideValue );
+  wideThreshold->SetInput( this->GetInput() );
+  
+  DilationFilterType::Pointer dilate = DilationFilterType::New();
+  dilate->SetMarkerImage( narrowThreshold->GetOutput() );
+  dilate->SetMaskImage( wideThreshold->GetOutput() );
+  dilate->RunOneIterationOff();   // run to convergence
+  
+  // graft our output to the dilate filter to force the proper regions
+  // to be generated
+  dilate->GraftOutput( this->GetOutput() );
+
+  // reconstruction by dilation
+  dilate->Update();
+
+  // graft the output of the dilate filter back onto this filter's
+  // output. this is needed to get the appropriate regions passed
+  // back.
+  this->GraftOutput( dilate->GetOutput() );
+
+  // copy the number of iterations used
+  m_NumberOfIterationsUsed = dilate->GetNumberOfIterationsUsed();
+}
+
+
+template<class TInputImage, class TOutputImage>
+void
+DoubleThresholdImageFilter<TInputImage, TOutputImage>
+::PrintSelf(std::ostream &os, Indent indent) const
+{
+  Superclass::PrintSelf(os, indent);
+
+  os << indent << "Threshold1: " << m_Threshold1 << std::endl;
+  os << indent << "Threshold2: " << m_Threshold2 << std::endl;
+  os << indent << "Threshold3: " << m_Threshold3 << std::endl;
+  os << indent << "Threshold4: " << m_Threshold4 << std::endl;
+  os << indent << "InsideValue: " << m_InsideValue << std::endl;
+  os << indent << "OutsideValue: " << m_OutsideValue << std::endl;
+  os << indent << "Number of iterations used to produce current output: "
+     << m_NumberOfIterationsUsed << std::endl;
+}
+  
+}// end namespace itk
+#endif
