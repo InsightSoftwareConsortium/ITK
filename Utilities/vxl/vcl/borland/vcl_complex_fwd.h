07851754d6895138009afcbe6625fbfa531a8943
Bill Hoffman <bill.hoffman@kitware.com>
2003-05-29 14:42:27 -0400

    ENH: add include for forward reference of complex

--- Utilities/vxl/vcl/borland/vcl_complex_fwd.h
+++ Utilities/vxl/vcl/borland/vcl_complex_fwd.h
@@ -1,10 +1,6 @@
 #ifndef vcl_borland_complex_fwd_h_
 #define vcl_borland_complex_fwd_h_
 
-// forward declare the native std::complex<T> :
-namespace std { template <class T> class complex; }
-#ifndef vcl_complex
-# define vcl_complex std :: complex
-#endif
+#include <borland/vcl_complex.h>
 
 #endif // vcl_borland_complex_fwd_h_
