f1d29d2db869d625a04cabe802c91bd2e8a22b77
Bill Lorensen <bill.lorensen@gmail.com>
2003-06-18 11:27:55 -0400

    ENH: now supports an optional comment on constrcutor. Also thorws an exception if progress is not reported.

--- Testing/Code/BasicFilters/itkFilterWatcher.h
+++ Testing/Code/BasicFilters/itkFilterWatcher.h
@@ -19,13 +19,13 @@
 
 #include "itkCommand.h"
 #include <time.h>
-// The following class supports callbacks
-// on the filter in the pipeline that follows later
+// The following class is a convenience  to watch the progress of a filter
+
 class FilterWatcher
 {
 public:
-  FilterWatcher(itk::ProcessObject* o)
-    {m_Start = 0; m_End = 0; m_Process = o;
+  FilterWatcher(itk::ProcessObject* o, char *comment="")
+  {m_Start = 0; m_End = 0; m_Process = o; m_Steps = 0; m_Comment = comment;
     itk::SimpleMemberCommand<FilterWatcher>::Pointer startFilterCommand;
     itk::SimpleMemberCommand<FilterWatcher>::Pointer endFilterCommand;
     itk::SimpleMemberCommand<FilterWatcher>::Pointer progressFilterCommand;
@@ -50,23 +50,34 @@ public:
   virtual void ShowProgress()
     {
       std::cout << " | " << m_Process->GetProgress() << std::flush;
+      m_Steps++;
     }
   virtual void StartFilter()
     {
+    m_Steps = 0;
     m_Start = ::clock();
-    std::cout << "-------- Start Filter " << m_Process->GetNameOfClass()
+    std::cout << "-------- Start " << m_Process->GetNameOfClass()
+              << " \"" << m_Comment << "\" "
               << m_Process
               << "Progress " << std::flush;
     }
+  const char *GetNameOfClass () {return m_Process->GetNameOfClass();}
   virtual void EndFilter()
     {
     m_End = ::clock();
     std::cout << std::endl << "Filter took " << static_cast<double>(m_End - m_Start) / CLOCKS_PER_SEC << " seconds.";
-    std::cout << std::endl << std::endl << "-------- End Filter " << m_Process->GetNameOfClass()
-              << m_Process;
+    std::cout << std::endl << std::endl << "-------- End " << m_Process->GetNameOfClass()
+              << " \"" << m_Comment << "\" "
+              << m_Process << std::flush;
+    if (m_Steps <= 2)
+      {
+      itkExceptionMacro ("Filter does not have progress.");
+      }
     }
   clock_t m_Start;
   clock_t m_End;
+  int m_Steps;
+  std::string m_Comment;
   itk::ProcessObject::Pointer m_Process;
 };
 
