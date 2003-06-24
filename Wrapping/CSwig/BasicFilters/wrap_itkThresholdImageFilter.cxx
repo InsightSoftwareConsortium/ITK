6b76d2a54af32c3afc221057a35ad3b771dfa2ba
Luis Ibanez <luis.ibanez@kitware.com>
2003-06-24 18:44:13 -0400

    Cable swig configuration for type instantiation.

---
+++ Wrapping/CSwig/BasicFilters/wrap_itkThresholdImageFilter.cxx
@@ -0,0 +1,40 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    wrap_itkThresholdImageFilter.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "itkThresholdImageFilter.h"
+#include "itkImage.h"
+
+#ifdef CABLE_CONFIGURATION
+#include "itkCSwigMacros.h"
+#include "itkCSwigImages.h"
+
+namespace _cable_
+{
+  const char* const group = ITK_WRAP_GROUP(itkThresholdImageFilter);
+  namespace wrappers
+  {
+    ITK_WRAP_OBJECT1_WITH_SUPERCLASS(ThresholdImageFilter, image::F2,
+                                     itkThresholdImageFilterF2);
+    ITK_WRAP_OBJECT1_WITH_SUPERCLASS(ThresholdImageFilter, image::US2,
+                                     itkThresholdImageFilterUS2);
+    ITK_WRAP_OBJECT1_WITH_SUPERCLASS(ThresholdImageFilter, image::F3, 
+                                     itkThresholdImageFilterF3);
+    ITK_WRAP_OBJECT1_WITH_SUPERCLASS(ThresholdImageFilter, image::US3, 
+                                     itkThresholdImageFilterUS3);
+  }
+}
+
+#endif
