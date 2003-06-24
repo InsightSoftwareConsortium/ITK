a06ab9e2edbf1887dde517fe45f00e501b8a1deb
Luis Ibanez <luis.ibanez@kitware.com>
2003-06-24 17:28:04 -0400

    ENH: Fixed notation for initializing a Size type for SetRadius().

--- Examples/Filtering/MedianImageFilter.py
+++ Examples/Filtering/MedianImageFilter.py
@@ -19,9 +19,19 @@ writer.SetInput( filter.GetOutput() )
 reader.SetFileName( argv[1] )
 writer.SetFileName( argv[2] )
 
-radius = eval( argv[3] )
+sizeRadius = itkSize2()
 
-filter.SetRadius( radius )
+sizeValues = new_ULArray(2)
+
+ULArray_setitem( sizeValues, 0, eval( argv[3] ) )
+ULArray_setitem( sizeValues, 1, eval( argv[3] ) )
+
+print ULArray_getitem( sizeValues, 0, )
+print ULArray_getitem( sizeValues, 1, )
+
+sizeRadius.SetSize( sizeValues );
+
+filter.SetRadius( sizeRadius )
 
 
 writer.Update()
