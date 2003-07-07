f0a5bbfc7173fb8757d3462dc1bcda83049c8834
Julien Jomier <julien.jomier@kitware.com>
2003-07-07 09:16:07 -0400

    ENH: Added sample size for higher dimensionality

--- Testing/Code/BasicFilters/itkSpatialObjectToImageStatisticsCalculatorTest.cxx
+++ Testing/Code/BasicFilters/itkSpatialObjectToImageStatisticsCalculatorTest.cxx
@@ -24,6 +24,7 @@
 #include "itkSpatialObjectToImageFilter.h"
 #include "itkFloodFilledSpatialFunctionConditionalIterator.h"
 #include "itkEllipseSpatialObject.h"
+#include "itkImageSliceIteratorWithIndex.h"
 
 int itkSpatialObjectToImageStatisticsCalculatorTest(int, char * [] )
 {
@@ -103,18 +104,103 @@ int itkSpatialObjectToImageStatisticsCalculatorTest(int, char * [] )
 
   std::cout << "[PASSED]" << std::endl;
 
+  std::cout << " --- Ellipse and Image mismatched right --- " << std::endl;
+
   offset.Fill(30);
   ellipse->GetIndexToObjectTransform()->SetOffset(offset);
   ellipse->ComputeObjectToParentTransform();
   ellipse->Update();
   calculator->Update();
 
-  std::cout << " --- Ellipse and Image mismatched right --- " << std::endl;
   std::cout << "Sample mean = " << calculator->GetMean() << std::endl ;
   std::cout << "Sample covariance = " << calculator->GetCovarianceMatrix();
 
   if( (fabs(calculator->GetMean()[0]-140.0)>1.0) 
     || (fabs(calculator->GetCovarianceMatrix()[0][0]-16141.0)>1.0)
+    )
+    {
+    std::cout << "[FAILED]" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  std::cout << "[PASSED]" << std::endl;
+
+
+  std::cout << " --- Testing higher dimensionality --- " << std::endl;
+
+  // Create a new 3D image
+  typedef itk::Image<PixelType,3> Image3DType;
+  Image3DType::Pointer image3D = Image3DType::New();
+
+  typedef Image3DType::RegionType RegionType;
+  typedef Image3DType::SizeType   SizeType;
+  typedef Image3DType::IndexType  IndexType;
+ 
+  SizeType size3D;
+  size3D[0]=50;
+  size3D[1]=50;
+  size3D[2]=3;
+  IndexType start;
+  start.Fill( 0 );
+  RegionType region3D;
+  region3D.SetIndex( start );
+  region3D.SetSize(  size3D  );
+  image3D->SetRegions( region3D );
+  image3D->Allocate();
+  image3D->FillBuffer(255);
+
+  // Fill the image
+  typedef itk::ImageSliceIteratorWithIndex< Image3DType > SliceIteratorType;
+  SliceIteratorType it( image3D, region3D );
+
+  it.GoToBegin();
+  it.SetFirstDirection( 0 );  // 0=x, 1=y, 2=z
+  it.SetSecondDirection( 1 ); // 0=x, 1=y, 2=z
+  
+  unsigned int value = 0;
+  while( !it.IsAtEnd() )
+  {
+    while( !it.IsAtEndOfSlice() )
+    {
+      while( !it.IsAtEndOfLine() )
+      {
+        it.Set( value );
+        ++it;
+      }
+      it.NextLine();
+    }
+    it.NextSlice();
+    value++;
+  }
+
+  typedef itk::EllipseSpatialObject<3> Ellipse3DType;
+  Ellipse3DType::Pointer ellipse3D = Ellipse3DType::New();
+  double radius[3];
+  radius[0] = 10;
+  radius[1] = 10;
+  radius[2] = 0;
+  ellipse3D->SetRadius(radius);
+
+  Ellipse3DType::VectorType offset3D;
+  offset3D.Fill(25);
+  offset3D[2]=0; // first slice
+  ellipse3D->GetIndexToObjectTransform()->SetOffset(offset3D);
+  ellipse3D->ComputeObjectToParentTransform();
+
+  // Create a new calculator with a sample size of 3
+  typedef itk::SpatialObjectToImageStatisticsCalculator<Image3DType,Ellipse3DType,3> Calculator3DType;
+  Calculator3DType::Pointer calculator3D = Calculator3DType::New();
+  calculator3D->SetImage(image3D);
+  calculator3D->SetSpatialObject(ellipse3D);
+  calculator3D->Update();
+
+  std::cout << "Sample mean = " << calculator3D->GetMean() << std::endl ;
+  std::cout << "Sample covariance = " << calculator3D->GetCovarianceMatrix();
+
+
+  if( (calculator3D->GetMean()[0] != 0)
+    || (calculator3D->GetMean()[1] != 1)
+    || (calculator3D->GetMean()[2] != 2)
     )
     {
     std::cout << "[FAILED]" << std::endl;
