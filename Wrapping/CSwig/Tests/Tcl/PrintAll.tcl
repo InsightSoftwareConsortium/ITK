26d76eba3013c6447c4a30adc1de5e6a0b6b1ca4
Bill Lorensen <bill.lorensen@gmail.com>
2003-07-14 08:49:38 -0400

    ENH: Added print all test.

---
+++ Wrapping/CSwig/Tests/Tcl/PrintAll.tcl
@@ -0,0 +1,15 @@
+package require InsightToolkit
+#
+# Exersize the PrintSelf of each class
+#
+set b [itkStringStream]
+set allClasses [info command itk*_New]
+
+foreach class $allClasses {
+    puts "--------------- $class ---------------"
+    catch {set a [$class]; $a Print [$b GetStream]; puts "[$b GetString]"}
+}
+
+
+exit
+
