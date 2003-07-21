f5145f74f2cbfabc0112ecfbab562634e58ac87e
Luis Ibanez <luis.ibanez@kitware.com>
2003-07-21 09:12:37 -0400

    ENH: itkMahalanobisDistanceThresholdImageFunction test added.

--- Testing/Code/Algorithms/itkAlgorithmsTests.cxx
+++ Testing/Code/Algorithms/itkAlgorithmsTests.cxx
@@ -55,6 +55,7 @@ REGISTER_TEST(itkLaplacianSegmentationLevelSetImageFilterTest );
 REGISTER_TEST(itkLevelSetNeighborhoodExtractorTest );
 REGISTER_TEST(itkLevelSetVelocityNeighborhoodExtractorTest );
 REGISTER_TEST(itkMattesMutualInformationImageToImageMetricTest );
+REGISTER_TEST(itkMahalanobisDistanceThresholdImageFunctionTest );
 REGISTER_TEST(itkMeanSquaresImageMetricTest );
 REGISTER_TEST(itkMinMaxCurvatureFlowImageFilterTest );
 REGISTER_TEST(itkMRFImageFilterTest );
