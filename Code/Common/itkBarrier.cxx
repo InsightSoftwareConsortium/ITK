a44dea06be581fd0cfa933edc4b6f4c2c61af30e
Josh Cates <cates@cs.utah.edu>
2003-07-29 17:58:53 -0400

    ENH: Added sproc and fetchop (fast) implementation. Removed Break() method because we cannot easily support it on sproc.  Also some documentation.

--- Code/Common/itkBarrier.cxx
+++ Code/Common/itkBarrier.cxx
@@ -21,39 +21,100 @@
 #include "itkMacro.h"
 
 #ifdef ITK_USE_SPROC
-
+#include "itkMultiThreader.h"
 #endif
 
 namespace itk {
 
+#ifdef ITK_USE_FETCHOP_BARRIERS
+atomic_reservoir_t Barrier::m_Reservoir = 0;
+bool Barrier::m_ReservoirInitialized = false;
+int Barrier::m_MaxBarriers = 1024;
+#endif
+
 Barrier::Barrier()
 {
   m_NumberExpected = 0;
+#ifndef ITK_USE_SPROC
+#ifndef ITK_USE_FETCHOP_BARRIERS
   m_NumberArrived  = 0;
-  m_ConditionVariable = ConditionVariable::New(); 
+  m_ConditionVariable = ConditionVariable::New();
+#endif
+#endif
 }
 
 Barrier::~Barrier()
-{}
+{
+#if defined ITK_USE_FETCHOP_BARRIERS
+  if (m_Pvar !=0 && Barrier::m_Reservoir != 0 )
+    {
+    atomic_free_variable(Barrier::m_Reservoir, m_Pvar);
+    m_Pvar = 0;
+    }
+
+  if (Barrier::m_Reservoir != 0)
+    {
+    atomic_free_reservoir(Barrier::m_Reservoir);
+    Barrier::m_Reservoir = 0;
+    }
+#elif defined ITK_USE_SPROC
+  if (m_Barrier != 0)
+    {
+    // Note: free_barrier should be called here
+    // but is buggy and causes a seg fault. jc 7/29/03
+    
+    //    free_barrier( m_Barrier );
+    }
+#endif
+}
 
 void Barrier::Initialize( unsigned int n )
 {
-  m_Mutex.Lock();
   m_NumberExpected = n;
-  m_Mutex.Unlock();
-}
 
-void Barrier::Break()
-{
-  // Unblock all waiting threads.
-  m_Mutex.Lock();
-  m_ConditionVariable->Broadcast();
-  m_NumberArrived = 0;
-  m_Mutex.Unlock();
+#if defined ITK_USE_FETCHOP_BARRIERS
+  // Create the reservoir.
+  if (Barrier::m_ReservoirInitialized == false)
+    {
+    Barrier::m_Reservoir = atomic_alloc_reservoir(USE_DEFAULT_PM, m_MaxBarriers, 0);
+    if (Barrier::m_Reservoir != 0)
+      {
+      Barrier::m_ReservoirInitialized = true;
+      }
+    else
+      {
+      itkExceptionMacro( << "atomic_alloc_reservoir call failed!" );
+      }
+    }
+
+  m_FetchopFlag = 0;
+  m_Pvar = atomic_alloc_variable(Barrier::m_Reservoir, 0);
+  storeop_store(m_Pvar, 0);
+#elif defined ITK_USE_SPROC
+  if (MultiThreader::GetInitialized() == false)
+    {
+    MultiThreader::Initialize();
+    }
+  m_Barrier = new_barrier( MultiThreader::GetThreadArena() );
+#endif  
 }
 
 void Barrier::Wait()
 {
+#if defined ITK_USE_FETCHOP_BARRIERS
+  int gen = m_FetchopFlag;
+  atomic_var_t val = atomic_fetch_and_increment(m_Pvar);
+  if (val == m_NumberExpected - 1)
+    {
+    storeop_store(m_Pvar, 0);
+    m_FetchopFlag++;
+    }
+  while (m_FetchopFlag == gen)
+    { // spin
+    }
+#elif defined ITK_USE_SPROC
+  barrier(m_Barrier, m_NumberExpected);
+#else
   m_Mutex.Lock();
   m_NumberArrived++;
   if ( m_NumberArrived == m_NumberExpected )
@@ -68,6 +129,7 @@ void Barrier::Wait()
     m_ConditionVariable->Wait( &m_Mutex );
     }
   m_Mutex.Unlock();
+#endif
 }
 
 }// end namespace itk
--- Code/Common/itkBarrier.h
+++ Code/Common/itkBarrier.h
@@ -21,16 +21,38 @@
 #include "itkConditionVariable.h"
 #include "itkMutexLock.h"
 
-#ifdef ITK_USE_SPROC
+#ifdef ITK_USE_FETCHOP_BARRIERS
+extern "C" {
+#include <sys/pmo.h>
+#include <fetchop.h>
+}
 #endif
 
 namespace itk {
 
 /**
  * \class Barrier
+ * \brief Standard barrier class implementation for synchronizing the execution
+ * of threads.
  *
- * 
- */
+ * A barrier class is used to synchronize threaded execution by allowing
+ * threads to block until each has reached a desired state.  As each thread
+ * enters the barrier it blocks. When all threads have entered the barrier,
+ * all all released and continue to execute.
+ *
+ * A thread enters the barrier by calling Wait() on the barrier class.
+ * To set up a barrier class, call Initialize(n) where n is the number of
+ * waiting threads that will trigger a release of the barrier.
+ *
+ * \par NOTE FOR SGI USERS. You may optionally enable a fetchop library
+ * implementation of barriers that will give significantly faster performance
+ * over the sproc barrier class.  With the fetchop implementation, you are
+ * limited to Barrier::m_MaxBarriers separate barrier instantiations, although
+ * this limit can be safely raised if necessary.  To enable the fetchop
+ * implementation, set ITK_USE_FETCHOP_BARRIERS and link applications against
+ * -lfetchop.
+ *
+*/
 class ITKCommon_EXPORT Barrier : public LightObject
 {
 public:
@@ -39,7 +61,7 @@ public:
   typedef LightObject  Superclass;
   typedef SmartPointer<Self>  Pointer;
   typedef SmartPointer<const Self>  ConstPointer;
-  
+
   /** Method for creation through the object factory. */
   itkNewMacro(Self);
 
@@ -50,23 +72,37 @@ public:
       argument to this method is the number of threads that must Wait() on the
       barrier before it is cleared. */
   void Initialize(unsigned int);
-  
+
   /** A thread calling this method waits until m_NumberOfThreads have called
-      Wait() on the barrier.  When the  */
+   *  Wait() on the barrier.  When the final expected thread calls Wait(), all
+   *  threads are released. */
   void Wait();
 
-  /** Break the barrier.  Unblocks execution of all waiting threads. */
-  void Break();
-  
 private:
   Barrier();
   ~Barrier();
-  
-  unsigned int m_NumberExpected;
-  unsigned int m_NumberArrived;
 
-  SimpleMutexLock m_Mutex;
+#if defined ITK_USE_FETCHOP_BARRIERS
+  static bool  m_ReservoirInitialized;
+  static atomic_reservoir_t m_Reservoir;
+  static int m_MaxBarriers;
+  atomic_var_t *m_Pvar;
+
+  char pad1[128];              // Attempt to put
+  volatile int m_FetchopFlag;  // m_Fetchop on its
+  char pad2[128];              // own cache line.
+
+#elif defined ITK_USE_SPROC
+  barrier_t *m_Barrier;
+
+#else
   ConditionVariable::Pointer m_ConditionVariable;
+  unsigned int m_NumberArrived;
+  SimpleMutexLock m_Mutex;
+
+#endif
+
+  unsigned int m_NumberExpected;
 };
 
 } // end namespace itk
