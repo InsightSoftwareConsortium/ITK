0a3987325661c3e63ad17b7ff57f1c29ef0c05ec
Raul Estepar <rjosest@bwh.harvard.edu>
2003-08-01 13:15:05 -0400

    ENH: segmentation level set wrappers added.

---
+++ Wrapping/CSwig/Algorithms/wrap_itkCurvesLevelSetImageFilter.cxx
@@ -0,0 +1,36 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    wrap_itkCurvesLevelSetImageFilter.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "itkImage.h"
+#include "itkCurvesLevelSetImageFilter.h"
+
+#ifdef CABLE_CONFIGURATION
+#include "itkCSwigMacros.h"
+#include "itkCSwigImages.h"
+
+namespace _cable_
+{
+  const char* const group = ITK_WRAP_GROUP(itkCurvesLevelSetImageFilter);
+  namespace wrappers
+  {
+    ITK_WRAP_OBJECT2(CurvesLevelSetImageFilter, image::F2, image::F2,
+                     itkCurvesLevelSetImageFilterF2F2);
+    ITK_WRAP_OBJECT2(CurvesLevelSetImageFilter, image::F3, image::F3,
+                     itkCurvesLevelSetImageFilterF3F3);
+  }
+}
+
+#endif
