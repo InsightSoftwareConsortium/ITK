62db39ab86b4aac56d0fbaf6f83dfe8097ed6b91
Julien Jomier <julien.jomier@kitware.com>
2003-08-02 15:47:16 -0400

    ENH: improve coverage

--- Testing/Code/Common/itkSimilarity2DTransformTest.cxx
+++ Testing/Code/Common/itkSimilarity2DTransformTest.cxx
@@ -34,8 +34,75 @@ int itkSimilarity2DTransformTest(int ,char *[] )
 
   typedef itk::Similarity2DTransform<double>  SimilarityTransformType;
   SimilarityTransformType::Pointer transform = SimilarityTransformType::New();
+
+  // Test the identity transform
+  std::cout << "Testing Identity:";
+  transform->SetIdentity();
+ 
+  SimilarityTransformType::InputPointType::ValueType pInit[2] = {10,10};
+  SimilarityTransformType::InputPointType p = pInit;
+  SimilarityTransformType::OutputPointType r;
+
+
+  r = transform->TransformPoint( p );
+  for(unsigned int i=0; i<N; i++)
+  {
+     if( fabs( p[i]- r[i] ) > epsilon )
+     {
+        Ok = false;
+        break;    
+     }
+  }
+  if( !Ok )
+  { 
+    std::cerr << "Error with Identity transform" << std::endl;
+    return EXIT_FAILURE;
+  }
+  else
+  {
+    std::cout << " [ PASSED ] " << std::endl;
+  }
+
+  // Test the Set/Get Parameters
+  std::cout << "Testing Set/GetParameters():";
+  SimilarityTransformType::ParametersType params;
+  params.resize(6);
+
+  for(unsigned int i=0;i<6;i++)
+    {
+    params[i]=i;
+    }
+
+
+  transform->SetParameters(params);
+  SimilarityTransformType::ParametersType outputParams;
+
+  outputParams = transform->GetParameters();
+
+  for(unsigned int i=0; i<N; i++)
+  {
+     if( fabs( outputParams[i]-params[i] ) > epsilon )
+     {
+        Ok = false;
+        break;    
+     }
+  }
+  if( !Ok )
+  { 
+    std::cerr << "Error with Set/GetParameters:" << std::endl;
+    std::cerr << "Input:" << params << std::endl;
+    std::cerr << "Output:" << outputParams << std::endl;
+    return EXIT_FAILURE;
+  }
+  else
+  {
+    std::cout << " [ PASSED ] " << std::endl;
+  }
+
+
   
   // 15 degrees in radians
+  transform->SetIdentity();
   const double angle = 15.0 * atan( 1.0f ) / 45.0; 
   const double sinth = sin( angle );
   const double costh = cos( angle );
@@ -45,14 +112,10 @@ int itkSimilarity2DTransformTest(int ,char *[] )
   transform->SetAngle(angle);
 
   // Rotate an itk::Point
-  SimilarityTransformType::InputPointType::ValueType pInit[2] = {10,10};
-  SimilarityTransformType::InputPointType p = pInit;
   SimilarityTransformType::InputPointType q;
-
+  p = pInit;
   q[0] =  p[0] * costh - p[1] * sinth;
   q[1] =  p[0] * sinth + p[1] * costh;
-
-  SimilarityTransformType::OutputPointType r;
   r = transform->TransformPoint( p );
   for(unsigned int i=0; i<N; i++)
   {
@@ -96,16 +159,36 @@ int itkSimilarity2DTransformTest(int ,char *[] )
     }
   }
   if( !Ok )
-  { 
+    { 
     std::cerr << "Error translating point: " << p << std::endl;
     std::cerr << "Result should be       : " << q << std::endl;
     std::cerr << "Reported Result is     : " << r << std::endl;
     return EXIT_FAILURE;
-  }
+    }
   else
-  {
+    {
     std::cout << " [ PASSED ] " << std::endl;
-  }
+    }
+
+
+
+  // Testing the Jacobian
+  std::cout << "Testing Jacobian:";
+  SimilarityTransformType::JacobianType jacobian =  transform->GetJacobian(p);
+
+  if( 
+    (jacobian[0][0] != 10) || (jacobian[0][1] != -10) || (jacobian[0][2] != 0) || (jacobian[0][3] != 0) 
+    || (jacobian[0][4] != 1) || (jacobian[0][5] != 0) || (jacobian[1][0] != 10) || (jacobian[1][1] != 10)
+    || (jacobian[1][2] !=0 ) || (jacobian[1][3] != 0) || (jacobian[1][4] !=0) || (jacobian[1][5] != 1)
+    ) 
+    {
+    std::cerr << "Error with Jacobian: " << jacobian << std::endl;
+    return EXIT_FAILURE;
+    }
+  else
+    {
+    std::cout << " [ PASSED ] " << std::endl;
+    }
 
   return EXIT_SUCCESS;
 
