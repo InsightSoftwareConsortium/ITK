f53389de04f0ea3c18a0af8103b397f08f4bfcf5
Tolga Tasdizen <tolga@sci.utah.edu>
2003-08-04 12:25:10 -0400

    DOC: Added documentation.

--- Code/Algorithms/itkUnsharpMaskLevelSetImageFilter.h
+++ Code/Algorithms/itkUnsharpMaskLevelSetImageFilter.h
@@ -22,6 +22,46 @@
 
 namespace itk {
 
+/**
+ * \class UnsharpMaskLevelSetImageFilter
+ *
+ * \brief This class implements a detail enhancing filter by making use of the
+ * 4th-order level set isotropic diffusion (smoothing) PDE.
+ *
+ * \par INPUT and OUTPUT
+ * This is a volume to volume filter; however, it is meant to process (smooth)
+ * surfaces. The input surface is an isosurface of the input volume. The
+ * isosurface value to be processed can be set by calling SetIsoSurfaceValue
+ * (default is 0). The output surface is the 0-isosurface of the output volume,
+ * regardless of the input isosurface value. To visualize the input/output
+ * surfaces to this filter a mesh extraction method such as marching cubes can
+ * be used. 
+ *
+ * \par
+ * This filter is an example of how the 4th order level set PDE framework can
+ * be used for general purpose surface processing. It is motivated by unsharp
+ * masking from image processing which is a way of enhancing detail. This
+ * filter acts much like the IsotropicFourthOrderLevelSetImageFilter because it
+ * first smoothes the normal vectors via isotropic diffusion. However, as a
+ * post-processing step we extrapolate from the original normals in the
+ * direction opposite to the new processes normals. By refiting the surface to
+ * these extrapolated vectors we achieve detail enhancement. This process is
+ * not the same as running the isotropic diffusion process in reverse.
+ *
+ * \par IMPORTANT
+ * Because this filters enhances details on the surface, it will also amplify
+ * noise! This filter is provided only as an example of graphics oriented
+ * post-processing. Do not use it on noisy data. 
+ * 
+ * \par PARAMETERS
+ * As mentioned before, the IsoSurfaceValue parameter chooses which isosurface
+ * of the input to process. The MaxFilterIterations parameter determine the
+ * number of iterations for which this filter will run. Since, this filter
+ * enhances detail AND noise MaxFilterIterations above a couple of hundred are
+ * unreasonable. Finally NormalProcessUnsharpWeight controls the amount of
+ * extrapolation (or equivalently the amount of detail enhancement). This value
+ * should be in the range [0.1,1] for reasonable results.
+ */
 template <class TInputImage, class TOutputImage>
 class ITK_EXPORT UnsharpMaskLevelSetImageFilter
   : public SparseFieldFourthOrderLevelSetImageFilter <TInputImage, TOutputImage>
