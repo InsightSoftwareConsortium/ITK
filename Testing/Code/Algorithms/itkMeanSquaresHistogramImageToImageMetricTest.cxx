cb441ec50b66b5dbd0895a0a681283c137bc845d
Luis Ibanez <luis.ibanez@kitware.com>
2003-08-04 11:35:48 -0400

    Test for the itkMeanSquaresHistogramImageToImageMetric class.

---
+++ Testing/Code/Algorithms/itkMeanSquaresHistogramImageToImageMetricTest.cxx
@@ -0,0 +1,30 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkMeanSquaresHistogramImageToImageMetricTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+
+#include "itkMeanSquaresHistogramImageToImageMetric.h"
+
+
+
+int itkMeanSquaresHistogramImageToImageMetricTest(int argc, char* argv[] )
+{
+
+  std::cerr << "Test to be implemented " << std::endl;
+  return EXIT_FAILURE;
+}
+
