704f7a1583838d53bddc0c88d6069d322cd6f02a
Luis Ibanez <luis.ibanez@kitware.com>
2003-08-04 11:16:08 -0400

    ENH: dummy test for the HistogramImageToImageMetric.

---
+++ Testing/Code/Algorithms/itkHistogramImageToImageMetricTest.cxx
@@ -0,0 +1,30 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkHistogramImageToImageMetricTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+
+#include "itkHistogramImageToImageMetric.h"
+
+
+
+int itkHistogramImageToImageMetricTest(int argc, char* argv[] )
+{
+
+  std::cerr << "Test to be implemented " << std::endl;
+  return EXIT_FAILURE;
+}
+
