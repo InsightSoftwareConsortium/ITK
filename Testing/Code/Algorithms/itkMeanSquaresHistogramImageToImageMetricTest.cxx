706f4a5b52fec6ad47c23f6462b2354e5ce629f4
Raghavendra Chandrashekara <rc3@doc.ic.ac.uk>
2003-08-05 08:05:45 -0400

    ENH: Added test for itkMeanSquaresHistogramImageToImageMetric class.

--- Testing/Code/Algorithms/itkMeanSquaresHistogramImageToImageMetricTest.cxx
+++ Testing/Code/Algorithms/itkMeanSquaresHistogramImageToImageMetricTest.cxx
@@ -14,17 +14,140 @@
      PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
-
-
-
+#include "itkGaussianImageSource.h"
+#include "itkImage.h"
+#include "itkLinearInterpolateImageFunction.h"
 #include "itkMeanSquaresHistogramImageToImageMetric.h"
+#include "itkTranslationTransform.h"
 
-
-
-int itkMeanSquaresHistogramImageToImageMetricTest(int argc, char* argv[] )
+/** This test uses two 2D-Gaussians (standard deviation RegionSize/2).
+    This test computes the mean squared differences between the pixels in the
+    two images.
+*/
+int itkMeanSquaresHistogramImageToImageMetricTest(int argc, char* argv[])
 {
+  try {
+    // Create two simple images.
+    const unsigned int ImageDimension = 2;
+    typedef double PixelType;
+    typedef double CoordinateRepresentationType;
+    
+    //Allocate Images
+    typedef itk::Image<PixelType,ImageDimension> MovingImageType;
+    typedef itk::Image<PixelType,ImageDimension> FixedImageType;
+    
+    // Declare Gaussian Sources
+    typedef itk::GaussianImageSource<MovingImageType> MovingImageSourceType;
+    typedef itk::GaussianImageSource<FixedImageType> FixedImageSourceType;
+    typedef MovingImageSourceType::Pointer MovingImageSourcePointer;
+    typedef FixedImageSourceType::Pointer FixedImageSourcePointer;
+    
+    // Note: the following declarations are classical arrays
+    unsigned long fixedImageSize[] = {100,  100};
+    unsigned long movingImageSize[] = {100,  100}; 
+    
+    float fixedImageSpacing[]  = {1.0f, 1.0f}; 
+    float movingImageSpacing[] = {1.0f, 1.0f}; 
+    
+    float fixedImageOrigin[] = {0.0f, 0.0f}; 
+    float movingImageOrigin[] = {0.0f, 0.0f}; 
+    
+    MovingImageSourceType::Pointer movingImageSource =
+      MovingImageSourceType::New();
+    FixedImageSourceType::Pointer  fixedImageSource  =
+      FixedImageSourceType::New();
+    
+    movingImageSource->SetSize(movingImageSize);
+    movingImageSource->SetOrigin(movingImageOrigin);
+    movingImageSource->SetSpacing(movingImageSpacing);
+    movingImageSource->SetNormalized(false);
+    movingImageSource->SetScale(250.0f);
+    
+    fixedImageSource->SetSize(fixedImageSize);
+    fixedImageSource->SetOrigin(fixedImageOrigin);
+    fixedImageSource->SetSpacing(fixedImageSpacing);
+    fixedImageSource->SetNormalized(false);
+    fixedImageSource->SetScale(250.0f);
+    
+    movingImageSource->Update(); // Force the filter to run
+    fixedImageSource->Update();  // Force the filter to run
+    
+    MovingImageType::Pointer movingImage = movingImageSource->GetOutput();
+    FixedImageType::Pointer  fixedImage  = fixedImageSource->GetOutput();
+    
+    // Set up the metric.
+    typedef itk::MeanSquaresHistogramImageToImageMetric<FixedImageType,
+      MovingImageType> MetricType;
+    typedef MetricType::TransformType TransformBaseType;
+    typedef MetricType::ScalesType ScalesType;
+    typedef TransformBaseType::ParametersType ParametersType;
+    
+    MetricType::Pointer metric = MetricType::New();
+    
+    unsigned int nBins = 256;
+    MetricType::HistogramType::SizeType histSize;
+    histSize[0] = nBins;
+    histSize[1] = nBins;
+    metric->SetHistogramSize(histSize);
+    
+    // Plug the images into the metric.
+    metric->SetFixedImage(fixedImage);
+    metric->SetMovingImage(movingImage);
 
-  std::cerr << "Test to be implemented " << std::endl;
-  return EXIT_FAILURE;
+    // Set up a transform.
+    typedef itk::TranslationTransform<CoordinateRepresentationType,
+      ImageDimension> TransformType;
+    
+    TransformType::Pointer transform = TransformType::New();
+    metric->SetTransform(transform.GetPointer());
+    
+    // Set up an interpolator.
+    typedef itk::LinearInterpolateImageFunction<MovingImageType,
+      double> InterpolatorType;
+    
+    InterpolatorType::Pointer interpolator = InterpolatorType::New();
+    interpolator->SetInputImage(movingImage.GetPointer());
+    metric->SetInterpolator(interpolator.GetPointer());
+    
+    // Define the region over which the metric will be computed.
+    metric->SetFixedImageRegion(fixedImage->GetBufferedRegion());
+
+    // Set up transform parameters.
+    ParametersType parameters(transform->GetNumberOfParameters());
+
+    for (unsigned int k = 0; k < ImageDimension; k++)
+      parameters[k] = 0.0f;
+
+    // Set scales for derivative calculation.
+    ScalesType scales(transform->GetNumberOfParameters());
+
+    for (unsigned int k = 0; k < ImageDimension; k++)
+      scales[k] = 1;
+
+    metric->SetDerivativeStepLengthScales(scales);
+
+    // Initialize the metric.
+    metric->Initialize();
+
+    // Print out metric value and derivative.
+    MetricType::MeasureType measure = metric->GetValue(parameters);
+    MetricType::DerivativeType derivative;
+    metric->GetDerivative(parameters, derivative);
+
+    std::cout << "Metric value = " << measure << std::endl
+              << "Derivative = " << derivative << std::endl;
+ 
+    // Exercise Print() method.
+    metric->Print(std::cout);
+
+    std::cout << "Test passed." << std::endl;
+  }
+  catch (itk::ExceptionObject& ex)
+  {
+    std::cerr << "Exception caught!" << std::endl;
+    std::cerr << ex << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
 }
-
