9146985312ca3ab2d864bec25fc55699c670fbae
Bill Lorensen <bill.lorensen@gmail.com>
2003-08-07 07:57:45 -0400

    ERR: need pragma to disable warning 4786 on VC6.

--- Testing/Code/BasicFilters/itkHoughTransform2DCirclesImageTest.cxx
+++ Testing/Code/BasicFilters/itkHoughTransform2DCirclesImageTest.cxx
@@ -14,6 +14,9 @@
      PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
 #include "itkHoughTransform2DCirclesImageFilter.h"
 #include "itkImageRegionIterator.h"
 #include "itkThresholdImageFilter.h"
