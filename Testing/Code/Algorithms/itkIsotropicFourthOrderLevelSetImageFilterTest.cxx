0575a1b206084967d777a147cfc76f40318cad84
Bill Lorensen <bill.lorensen@gmail.com>
2003-08-07 09:29:14 -0400

    ERR: add pragma to supress 4786 warning on VC6.

--- Testing/Code/Algorithms/itkIsotropicFourthOrderLevelSetImageFilterTest.cxx
+++ Testing/Code/Algorithms/itkIsotropicFourthOrderLevelSetImageFilterTest.cxx
@@ -1,3 +1,23 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkIsotropicFourthOrderLevelSetImageFilterTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
 #include "itkIsotropicFourthOrderLevelSetImageFilter.h"
 #include "itkImage.h"
 #include <iostream>
--- Testing/Code/Algorithms/itkSimpleFuzzyConnectednessScalarImageFilterTest.cxx
+++ Testing/Code/Algorithms/itkSimpleFuzzyConnectednessScalarImageFilterTest.cxx
@@ -14,6 +14,9 @@
      PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
 #include "itkSimpleFuzzyConnectednessScalarImageFilter.h"
 #include "itkImageRegionIteratorWithIndex.h"
 #include <iomanip>
