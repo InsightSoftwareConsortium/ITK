88dce3381650c029b001d8d40c82f11045a753d8
Jisung Kim <bahrahm@yahoo.com>
2003-08-12 12:59:21 -0400

    ENH: Documentation updated.

--- Code/Numerics/Statistics/itkHypersphereKernelMeanShiftModeSeeker.h
+++ Code/Numerics/Statistics/itkHypersphereKernelMeanShiftModeSeeker.h
@@ -25,10 +25,12 @@
 
 namespace itk{ 
 namespace Statistics{
-  
+
 /** \class HypersphereKernelMeanShiftModeSeeker
- * \brief Calculates the covariance matrix of the target sample data.
+ * \brief Evolves the mode using a hyperspherical kernel defined by a
+ * radius (which is set by SetRadius) method.
  *
+ * \sa MeanShiftModeSeekerBase 
  */
 
 template< class TSample >
@@ -46,20 +48,28 @@ public:
                MeanShiftModeSeekerBase);
   itkNewMacro(Self) ;
   
+  /** Typedefs from the superclass */
   typedef typename Superclass::MeasurementVectorType MeasurementVectorType ;
   typedef typename Superclass::SearchResultVectorType SearchResultVectorType ;
   typedef typename Superclass::MeasurementType MeasurementType ;
 
+  /** Size of a measurement vector */
   itkStaticConstMacro( MeasurementVectorSize, unsigned int,
                        MeasurementVectorType::Length ) ;
 
+  /** Type for the sum of measurements */
   typedef double RealMeasurementType ;
+
+  /** Type fot the sum of measurement vectors used in computing the new
+    * mode */
   typedef FixedArray< RealMeasurementType,
                       itkGetStaticConstMacro( MeasurementVectorSize ) > 
   MeasurementVectorSumType ;
 
+  /** Sets the radius of the kernel */
   void SetSearchRadius(double radius) ;
   
+  /** Gets the radius of the kernel  */
   double GetSearchRadius()
   { return m_SearchRadius ; }
 
@@ -71,12 +81,19 @@ protected:
   virtual ~HypersphereKernelMeanShiftModeSeeker() ;
   void PrintSelf(std::ostream& os, Indent indent) const;
 
+  /** Computes the new mode with the given query point (queryPoint). The
+    * new mode will be stored in the newPoint */ 
   inline void ComputeMode(MeasurementVectorType queryPoint,
-                   MeasurementVectorType& newPoint) ;
+                          MeasurementVectorType& newPoint) ;
 
 private:
+  /** Radius of the kernel */
   double m_SearchRadius ;
+
+  /** Temporary measurement vector sum: internal use */ 
   MeasurementVectorSumType m_TempVectorSum ;
+  
+  /** Temporary measurement vector: internal use */
   MeasurementVectorType m_TempVector ;
 } ; // end of class
     
