dde8819bab9d57c8c5d52ca2e0ea484ef0dafb97
Luis Ibanez <luis.ibanez@kitware.com>
2003-08-17 13:03:53 -0400

    ENH: Improving coverage: now it is run on a Mesh generated with the      SphereMeshSource filter.

--- Testing/Code/Algorithms/itkExtractMeshConnectedRegionsTest.cxx
+++ Testing/Code/Algorithms/itkExtractMeshConnectedRegionsTest.cxx
@@ -19,35 +19,42 @@
 #endif
 #include "itkMesh.h"
 #include "itkConnectedRegionsMeshFilter.h"
+#include "itkSphereMeshSource.h"
 
 #include <iostream>
 #include <string>
 
-/**
- * Some typedefs to make things easier.
- */
-
-// A mesh with no pixel data.
-typedef itk::Mesh<int>  Mesh;
-typedef itk::ConnectedRegionsMeshFilter<Mesh,Mesh> Connect;
-typedef itk::Point<float,3> Point;
 
 /*
  * Test the mesh connectivity class.
  */
 int itkExtractMeshConnectedRegionsTest(int, char* [])
 {
+
+  /**
+   * Some typedefs to make things easier.
+   */
+
+  // A mesh with no pixel data.
+  typedef itk::Mesh< int >  MeshType;
+
+  typedef itk::ConnectedRegionsMeshFilter<MeshType,MeshType> ConnectFilterType;
+  typedef itk::Point<float,3> PointType;
+
+
+
   // Define a simple mesh of three connected pieces. The mesh consists
   // of several different cell types.
   //
-  Mesh::Pointer inMesh(Mesh::New());
-  Mesh::Pointer outMesh(Mesh::New());
+  MeshType::Pointer inMesh  = MeshType::New();
+  MeshType::Pointer outMesh = MeshType::New();
   
   // Pass the mesh through the filter in a variety of ways.
   //
-  Point::ValueType pInit[3] = {1,2,3};
-  Point p = pInit;
-  Connect::Pointer connect(Connect::New());
+  PointType::ValueType pInit[3] = {1,2,3};
+  PointType p = pInit;
+  ConnectFilterType::Pointer connect(ConnectFilterType::New());
+
   connect->SetInput(inMesh);
   connect->SetClosestPoint(p);
   connect->AddSeed(0);
@@ -56,8 +63,38 @@ int itkExtractMeshConnectedRegionsTest(int, char* [])
   connect->AddSeed(2);
   connect->DeleteSeed(1);
   connect->Update();
-  
 
-  return 0;  
+
+  // Create a Sphere for running the filter on real input data.  
+  typedef itk::SphereMeshSource< MeshType >  SphereMeshSourceType;
+
+  SphereMeshSourceType::Pointer meshSource = SphereMeshSourceType::New();
+
+  PointType center; center.Fill(0);
+  PointType::ValueType scaleInit[3] = {1,1,1};
+  PointType scale = scaleInit;
+  
+  meshSource->SetCenter(center);
+  meshSource->SetResolutionX( 10 );
+  meshSource->SetResolutionY( 10 );
+  meshSource->SetScale(scale);
+  meshSource->Modified();
+  meshSource->Update();
+
+
+  connect->SetInput( meshSource->GetOutput() );
+
+  try
+    {
+    connect->Update();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+    std::cerr << "Exception caught " << std::endl;
+    std::cerr << excp << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  return EXIT_SUCCESS;  
 }
 
