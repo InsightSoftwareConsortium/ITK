ef2ca68a5c44c7510cde700299f986b90b6f182c
Josh Cates <cates@cs.utah.edu>
2003-08-19 18:45:47 -0400

    FIX: Grammar, spelling.

--- Examples/Filtering/BinomialBlurImageFilter.cxx
+++ Examples/Filtering/BinomialBlurImageFilter.cxx
@@ -18,7 +18,7 @@
 //  Software Guide : BeginLatex
 //
 //  The \doxygen{BinomialBlurImageFilter} computes a nearest neighbor average
-//  along each dimension. The process is repeated the number of times as
+//  along each dimension. The process is repeated a number of times, as
 //  specified by the user. In principle, after a large number of iterations
 //  the result will approach the convolution with a Gaussian.
 //
@@ -57,7 +57,7 @@ int main( int argc, char * argv[] )
   
   //  Software Guide : BeginLatex
   //
-  //  Types should be choosen for the pixels of the input and output images.
+  //  Types should be chosen for the pixels of the input and output images.
   //  Image types can be instantiated using the pixel type and dimension.
   //
   //  Software Guide : EndLatex 
@@ -100,7 +100,7 @@ int main( int argc, char * argv[] )
   //  Software Guide : BeginLatex
   //
   //  The input image can be obtained from the output of another filter. Here,
-  //  an image reader is used as source. The number of repetitions is set with
+  //  an image reader is used as the source. The number of repetitions is set with
   //  the \code{SetRepetitions()} method. Computation time will
   //  increase linearly with the number of repetitions selected. Finally, the
   //  filter can be executed by calling the \code{Update()} method.
@@ -144,7 +144,7 @@ int main( int argc, char * argv[] )
   // \center
   // \includegraphics[width=0.44\textwidth]{BrainProtonDensitySlice.eps}
   // \includegraphics[width=0.44\textwidth]{BinomialBlurImageFilterOutput.eps}
-  // \itkcaption[BinomialBlurImageFilter output]{Effect of the
+  // \itkcaption[BinomialBlurImageFilter output.]{Effect of the
   // BinomialBlurImageFilter on a slice from a MRI proton density image of the
   // brain.}
   // \label{fig:BinomialBlurImageFilterInputOutput}
@@ -153,9 +153,9 @@ int main( int argc, char * argv[] )
   //  Figure \ref{fig:BinomialBlurImageFilterInputOutput} illustrates the
   //  effect of this filter on a MRI proton density image of the brain. 
   //  
-  //  Note that the $\sigma$ of the equivalent Gaussian is fixed. The effect of
-  //  every iteration of this filter on the spatial spectrum will be to
-  //  multiply it with a sinus cardinal function.
+  //  Note that the standard deviation $\sigma$ of the equivalent Gaussian is
+  //  fixed.  In the spatial spectrum, the effect of every iteration of this
+  //  filter is like a multiplication with a sinus cardinal function.
   //
   //  Software Guide : EndLatex 
 
