26349c351cd04210b85d912e3fb455a5bf865c97
Tessa Sundaram <Tessa.Sundaram@uphs.upenn.edu>
2003-08-20 13:14:55 -0400

    FIX: editing/proofreading

--- Examples/Registration/ImageRegistration5.cxx
+++ Examples/Registration/ImageRegistration5.cxx
@@ -19,10 +19,10 @@
 //
 // This example illustrates the use of the \doxygen{CenteredRigid2DTransform}
 // for performing rigid registration in $2D$. The example code is for
-// the most part identical to the one presented in
+// the most part identical to that presented in Section 
 // \ref{sec:IntroductionImageRegistration}.  The main difference is the use
-// of the \doxygen{CenteredRigid2DTransform} here instead of the
-// \doxygen{TranslationTransform}.
+// of the CenteredRigid2DTransform here instead of the
+// TranslationTransform.
 //
 // \index{itk::CenteredRigid2DTransform}
 //
@@ -188,7 +188,7 @@ int main( int argc, char *argv[] )
   //  
   //  In this example, the input images are taken from readers. The code
   //  below updates the readers in order to ensure that the image parameters
-  //  of size, origin and spacing are valid when used to initialize the
+  //  (size, origin and spacing) are valid when used to initialize the
   //  transform.  We intend to use the center of the fixed image as the
   //  rotation center and then use the vector between the fixed image center
   //  and the moving image center as the initial translation to be applied
@@ -249,8 +249,8 @@ int main( int argc, char *argv[] )
 
   //  Software Guide : BeginLatex
   //  
-  //   In order to initialize the transform parameters, the most
-  //   straightforward method is to configure the transform and then get its
+  //   The most
+  //   straightforward method of initializing the transform parameters is to configure the transform and then get its
   //   parameters with the method \code{GetParameters()}. Here we initialize
   //   the transform by passing the center of the fixed image as the rotation
   //   center with the \code{SetRotationCenter()} method. Then the
@@ -292,7 +292,7 @@ int main( int argc, char *argv[] )
 
   //  Software Guide : BeginLatex
   //  
-  //  Keeping in mind that the scale of units in rotation and translation are
+  //  Keeping in mind that the scale of units in rotation and translation is
   //  quite different, we take advantage of the scaling functionality
   //  provided by the optimizers. We know that the first element of the
   //  parameters array corresponds to the angle. For this reason we use small
@@ -319,7 +319,7 @@ int main( int argc, char *argv[] )
   //  Software Guide : BeginLatex
   //  
   //  Next we set the normal parameters of the optimization method. In this
-  //  case we are using an \doxygen{RegularStepGradientDescentOptimizer}.
+  //  case we are using an RegularStepGradientDescentOptimizer.
   //  Below, we define the optimization parameters like initial step length,
   //  minimal step length and number of iterations. These last two act as
   //  stopping criteria for the optimization.
@@ -380,8 +380,8 @@ int main( int argc, char *argv[] )
 
   //  Software Guide : BeginLatex
   //  
-  //  Let's execute this example over some of the images provided in
-  //  \code{Examples/Data}, for example:
+  //  Let's execute this example over two of the images provided in
+  //  \code{Examples/Data}:
   //  
   //  \begin{itemize}
   //  \item \code{BrainProtonDensitySliceBorder20.png} 
@@ -399,7 +399,7 @@ int main( int argc, char *argv[] )
   //  \end{verbatim}
   //  \end{center}
   //
-  //  That are interpreted as
+  //  These results are interpreted as
   //
   //  \begin{itemize}
   //  \item Angle         =                     $0.177491$   radians
@@ -407,8 +407,8 @@ int main( int argc, char *argv[] )
   //  \item Translation   = $(   0.0111713,   0.00250842 )$ millimeters
   //  \end{itemize}
   // 
-  //  As expected, these values pretty well match the misalignment
-  //  intentionally introduced into the moving image. Since $10$ degrees is
+  //  As expected, these values match the misalignment
+  //  intentionally introduced into the moving image quite well, since $10$ degrees is
   //  about $0.174532$ radians.
   //
   // \begin{figure}
@@ -416,7 +416,7 @@ int main( int argc, char *argv[] )
   // \includegraphics[width=0.44\textwidth]{BrainProtonDensitySliceBorder20.eps}
   // \includegraphics[width=0.44\textwidth]{BrainProtonDensitySliceRotated10.eps}
   // \itkcaption[Rigid2D Registration input images]{Fixed and moving images
-  // provided as input to the registration method using the CenteredRigid2D
+  // are provided as input to the registration method using the CenteredRigid2D
   // transform.}
   // \label{fig:FixedMovingImageRegistration5}
   // \end{figure}
@@ -434,27 +434,27 @@ int main( int argc, char *argv[] )
   // \end{figure}
   //
   // Figure \ref{fig:ImageRegistration5Outputs} shows the output of the
-  // registration. The right most image of this figure shows the squared
-  // magnitude of pixel differences between the fixed image and the resampled
+  // registration. The rightmost image of this figure shows the squared
+  // magnitude difference between the fixed image and the resampled
   // moving image. It can be seen on the difference image that the rotational
-  // component was solved but a centering misalignment persists.
+  // component has been solved but that the centering misalignment persists.
   //
   // \begin{figure}
   // \center
   // \includegraphics[height=0.32\textwidth]{ImageRegistration5TraceMetric.eps}
   // \includegraphics[height=0.32\textwidth]{ImageRegistration5TraceAngle.eps}
   // \includegraphics[height=0.32\textwidth]{ImageRegistration5TraceTranslations.eps} 
-  // \itkcaption[Rigid2D Registration output plots]{Plots of the Metric, rotation
-  // angle and translations during the registration using the CenteredRigid2D
+  // \itkcaption[Rigid2D Registration output plots]{Metric values, rotation
+  // angle and translations during registration with the CenteredRigid2D
   // transform.}
   // \label{fig:ImageRegistration5Plots}
   // \end{figure}
   //
   //  Figure \ref{fig:ImageRegistration5Plots} shows plots of the main output
   //  parameters produced from the registration process. The metric values at
-  //  every iteration are shown on the top. The angle values are shown in the
-  //  plot at left while the translation components of the registration are
-  //  presented in the plot at right.
+  //  every iteration are shown on the top. The angle values are shown on the
+  //  bottom left plot while the translation components of the registration are
+  //  presented on the bottom right plot.
   //
   //  Software Guide : EndLatex 
 
@@ -536,7 +536,7 @@ int main( int argc, char *argv[] )
   //  Software Guide : BeginLatex
   //  
   //  Let's now consider the case in which rotations and translations are
-  //  present in the initial registration. For example in the pair
+  //  present in the initial registration, as in the following pair
   //  of images:
   //  
   //  \begin{itemize}
@@ -545,15 +545,14 @@ int main( int argc, char *argv[] )
   //  \end{itemize}
   //
   //  The second image is the result of intentionally rotating the first image
-  //  by $10$ degrees and then translation it $13mm$ in $X$ and $17mm$ in $Y$.
+  //  by $10$ degrees and then translating it $13mm$ in $X$ and $17mm$ in $Y$.
   //  Both images have unit-spacing and are shown in Figure
   //  \ref{fig:FixedMovingImageRegistration5b}. In order to accelerate
-  //  convergence it is convenient here to use a larger step length. For
-  //  example, with the following change:
+  //  convergence it is convenient to use a larger step length as shown here.
   //
   //  \code{optimizer->SetMaximumStepLength( 1.0 );}
   //
-  //  The registration takes now $92$ iterations and produces the following
+  //  The registration now takes $92$ iterations and produces the following
   //  results:
   //
   //  \begin{center}
@@ -570,8 +569,8 @@ int main( int argc, char *argv[] )
   //  \item Translation   = $(  12.9044    ,  15.8459     )$ millimeters
   //  \end{itemize}
   //  
-  //  These values approximately match the initial alignment intentionally
-  //  introduced into the moving image. Since $10$ degrees is about $0.174532$
+  //  These values approximately match the initial misalignment intentionally
+  //  introduced into the moving image, since $10$ degrees is about $0.174532$
   //  radians. The horizontal translation is well resolved while the vertical
   //  translation ends up being off by a bit more than one millimeter.
   //
@@ -598,8 +597,8 @@ int main( int argc, char *argv[] )
   // \end{figure}
   //
   // Figure \ref{fig:ImageRegistration5Outputs2} shows the output of the
-  // registration. The right most image of this figure shows the squared
-  // magnitude of pixel differences between the fixed image and the resampled
+  // registration. The rightmost image of this figure shows the squared
+  // magnitude difference between the fixed image and the resampled
   // moving image. 
   //
   // \begin{figure}
@@ -607,20 +606,20 @@ int main( int argc, char *argv[] )
   // \includegraphics[height=0.32\textwidth]{ImageRegistration5TraceMetric2.eps}
   // \includegraphics[height=0.32\textwidth]{ImageRegistration5TraceAngle2.eps}
   // \includegraphics[height=0.32\textwidth]{ImageRegistration5TraceTranslations2.eps} 
-  // \itkcaption[Rigid2D Registration output plots]{Plots of the metric, rotation
+  // \itkcaption[Rigid2D Registration output plots]{Metric values, rotation
   // angle and translations during the registration using the CenteredRigid2D
-  // transform on an image with rotation and translation miss-registration.}
+  // transform on an image with rotation and translation mis-registration.}
   // \label{fig:ImageRegistration5Plots2}
   // \end{figure}
   //
   //  Figure \ref{fig:ImageRegistration5Plots2} shows plots of the main
   //  output registration parameters when the rotation and translations are
   //  combined. The metric values at every iteration are shown at the
-  //  top. The angle values are shown in the plot at left while the
-  //  translation components of the registration are presented in the plot at
-  //  right. It can be seen from the smoothness of these plots that a larger
-  //  step length could have been easily supported by the optimizer. You may
-  //  want to play with this value in order to get a better feeling of how to
+  //  top. The angle values are shown in the bottom left plot, while the
+  //  translation components of the registration are presented in the bottom 
+  //  right plot. It can be seen from the smoothness of these plots that a larger
+  //  step length could easily have been supported by the optimizer. You may
+  //  want to modify this value in order to get a better idea of how to
   //  tune the parameters.
   //
   //  Software Guide : EndLatex 
