1d2836914335b40df1650575f0b2c59a7a9e48ae
Bill Lorensen <bill.lorensen@gmail.com>
2003-10-18 08:31:09 -0400

    ERR: missing include functional.

--- Code/IO/itkIPLFileNameList.cxx
+++ Code/IO/itkIPLFileNameList.cxx
@@ -1,3 +1,19 @@
+/*=========================================================================
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkIPLFileNameList.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notices for more information.
+
+  =========================================================================*/
+
 #include <stdlib.h>
 #include "itkIPLFileNameList.h"
 #include <assert.h>
@@ -5,6 +21,7 @@
 #include <ctype.h>
 
 #include <list>
+#include <functional>
 namespace itk
 {
 class IPLFileSortInfo_ascend_compare :
