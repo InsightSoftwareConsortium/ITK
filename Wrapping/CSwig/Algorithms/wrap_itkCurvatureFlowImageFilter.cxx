ed07ec4bfe579f52e89490a3b6141cbc6ab9ddb8
Hans Johnson <hans-johnson@uiowa.edu>
2003-11-25 21:01:27 -0500

    FIX: Moved wrap_itkCurvatureFlowImageFilter and wrap_itkMinMaxCurvatureFlowImageFilter from BasicFilters to Algorithms.

--- Wrapping/CSwig/Algorithms/wrap_itkCurvatureFlowImageFilter.cxx
+++ Wrapping/CSwig/Algorithms/wrap_itkCurvatureFlowImageFilter.cxx
@@ -30,6 +30,11 @@ namespace _cable_
                      itkCurvatureFlowImageFilterF2F2);
     ITK_WRAP_OBJECT2(CurvatureFlowImageFilter, image::F3, image::F3,
                      itkCurvatureFlowImageFilterF3F3);
+
+    ITK_WRAP_OBJECT2(CurvatureFlowImageFilter, image::D2, image::D2,
+                     itkCurvatureFlowImageFilterD2D2);
+    ITK_WRAP_OBJECT2(CurvatureFlowImageFilter, image::D3, image::D3,
+                     itkCurvatureFlowImageFilterD3D3);
   }
 }
 
