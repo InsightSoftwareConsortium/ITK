cfddd491b397344f89dea96a19b6abb68fd3050d
Luis Ibanez <luis.ibanez@kitware.com>
2003-12-05 14:11:32 -0500

    Example on how to use the convenience class ScalarImageToHistogramGenerator.

---
+++ Examples/Statistics/ImageHistogram2.cxx
@@ -0,0 +1,96 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    ImageHistogram2.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+// Software Guide : BeginLatex
+//
+// This example shows how to compute the histogram of a scalar image.
+// using the helper class \doxygen{ScalarImageToHistogramGenerator}.
+//
+// Software Guide : EndLatex 
+
+
+// Software Guide : BeginCodeSnippet
+#include "itkScalarImageToHistogramGenerator.h"
+#include "itkImage.h"
+// Software Guide : EndCodeSnippet
+
+#include "itkImageFileReader.h"
+
+int main( int argc, char * argv [] )
+{
+
+  if( argc < 2 )
+    {
+    std::cerr << "Missing command line arguments" << std::endl;
+    std::cerr << "Usage :  ImageHistogram1  inputImageFileName " << std::endl;
+    return -1;
+    }
+
+
+  typedef unsigned char       PixelType;
+  const unsigned int          Dimension = 2;
+
+  typedef itk::Image<PixelType, Dimension > ImageType;
+
+  typedef itk::ImageFileReader< ImageType > ReaderType;
+
+  ReaderType::Pointer reader = ReaderType::New();
+
+  reader->SetFileName( argv[1] );
+
+  try
+    {
+    reader->Update();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+    std::cerr << "Problem encoutered while reading image file : " << argv[1] << std::endl;
+    std::cerr << excp << std::endl;
+    return -1;
+    }
+
+  typedef itk::Statistics::ScalarImageToHistogramGenerator< 
+                                                    ImageType 
+                                                          >   HistogramGeneratorType;
+
+  HistogramGeneratorType::Pointer histogramGenerator = HistogramGeneratorType::New();
+
+  histogramGenerator->SetInput(  reader->GetOutput() );
+
+  histogramGenerator->SetNumberOfBins( 255 );
+  histogramGenerator->SetMarginalScale( 10.0 );
+  histogramGenerator->Compute();
+
+  typedef HistogramGeneratorType::HistogramType  HistogramType;
+
+  const HistogramType * histogram = histogramGenerator->GetOutput();
+
+  const unsigned int histogramSize = histogram->Size();
+
+  std::cout << "Histogram size " << histogramSize << std::endl;
+
+  for( unsigned int bin=0; bin < histogramSize; bin++ )
+    {
+    std::cout << "bin = " << bin << " frequency = ";
+    std::cout << histogram->GetFrequency( bin, 0 ) << std::endl;
+    }
+
+  return 0;
+  
+}
+
+
