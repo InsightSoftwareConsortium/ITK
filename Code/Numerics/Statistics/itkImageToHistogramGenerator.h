569320cd8e7db8c81781dd3efd4c7dcedc4379e7
Luis Ibanez <luis.ibanez@kitware.com>
2003-12-08 09:10:15 -0500

    Helper class to facilitate the computation of histograms from a vector image.

---
+++ Code/Numerics/Statistics/itkImageToHistogramGenerator.h
@@ -0,0 +1,106 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkImageToHistogramGenerator.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkImageToHistogramGenerator_h
+#define __itkImageToHistogramGenerator_h
+
+
+#include "itkImageToListAdaptor.h"
+#include "itkListSampleToHistogramGenerator.h"
+#include "itkObject.h"
+
+
+namespace itk {
+namespace Statistics {
+
+template< class TImageType >
+class ImageToHistogramGenerator : public Object
+{
+public:
+  /** Standard typedefs */
+  typedef ImageToHistogramGenerator  Self ;
+  typedef Object Superclass;
+  typedef SmartPointer<Self> Pointer;
+  typedef SmartPointer<const Self> ConstPointer;
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(ImageToHistogramGenerator, Object) ;
+
+  /** standard New() method support */
+  itkNewMacro(Self) ;
+
+  typedef TImageType                                      ImageType;
+  typedef itk::Statistics::ImageToListAdaptor< 
+                                              ImageType 
+                                                      >   AdaptorType;
+  typedef typename AdaptorType::Pointer                   AdaptorPointer;
+  typedef typename ImageType::PixelType                   PixelType;
+  typedef typename PixelType::ValueType                   ValueType;
+
+  typedef itk::Statistics::ListSampleToHistogramGenerator< 
+                                                AdaptorType, 
+                                                ValueType > GeneratorType;
+
+  typedef typename GeneratorType::Pointer                   GeneratorPointer;
+
+  typedef typename GeneratorType::HistogramType             HistogramType;
+  typedef typename HistogramType::Pointer                   HistogramPointer;
+  typedef typename HistogramType::ConstPointer              HistogramConstPointer;
+  typedef typename HistogramType::SizeType                  SizeType;
+
+public:
+
+  /** Triggers the Computation of the histogram */
+  void Compute( void );
+
+  /** Connects the input image for which the histogram is going to be computed */
+  void SetInput( const ImageType * );
+  
+  /** Return the histogram. o
+   \warning This output is only valid after the Compute() method has been invoked 
+   \sa Compute */
+  const HistogramType * GetOutput() const;
+  
+  /** Set number of histogram bins */
+  void SetNumberOfBins( const SizeType & size );
+ 
+  /** Set marginal scale value to be passed to the histogram generator */
+  void SetMarginalScale( double marginalScale );
+
+
+protected:
+  ImageToHistogramGenerator();
+  virtual ~ImageToHistogramGenerator() {};
+  void PrintSelf(std::ostream& os, Indent indent) const;
+
+
+private:
+
+  AdaptorPointer      m_ImageToListAdaptor;
+
+  GeneratorPointer    m_HistogramGenerator;
+
+};
+
+
+} // end of namespace Statistics 
+} // end of namespace itk 
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkImageToHistogramGenerator.txx"
+#endif
+
+#endif
---
+++ Code/Numerics/Statistics/itkImageToHistogramGenerator.txx
@@ -0,0 +1,105 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkImageToHistogramGenerator.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef _itkImageToHistogramGenerator_txx
+#define _itkImageToHistogramGenerator_txx
+
+#include "itkImageToHistogramGenerator.h"
+
+
+namespace itk { 
+namespace Statistics {
+
+
+template < class TImage >
+ImageToHistogramGenerator< TImage >
+::ImageToHistogramGenerator() 
+{
+  m_ImageToListAdaptor = AdaptorType::New();
+  m_HistogramGenerator = GeneratorType::New();
+  m_HistogramGenerator->SetListSample( m_ImageToListAdaptor );
+}
+
+
+
+template < class TImage >
+void
+ImageToHistogramGenerator< TImage >
+::SetInput( const ImageType * image ) 
+{
+  m_ImageToListAdaptor->SetImage( image );
+}
+
+
+template < class TImage >
+const typename ImageToHistogramGenerator< TImage >::HistogramType *
+ImageToHistogramGenerator< TImage >
+::GetOutput() const
+{
+  return m_HistogramGenerator->GetOutput();
+}
+
+
+
+template < class TImage >
+void
+ImageToHistogramGenerator< TImage >
+::Compute() 
+{
+  m_HistogramGenerator->Update();
+}
+
+
+
+template < class TImage >
+void
+ImageToHistogramGenerator< TImage >
+::SetNumberOfBins( const SizeType & size ) 
+{
+  m_HistogramGenerator->SetNumberOfBins( size );
+}
+
+
+
+template < class TImage >
+void
+ImageToHistogramGenerator< TImage >
+::SetMarginalScale( double marginalScale )
+{
+  m_HistogramGenerator->SetMarginalScale( marginalScale );
+}
+
+
+
+
+template < class TImage >
+void
+ImageToHistogramGenerator< TImage >
+::PrintSelf(std::ostream& os, Indent indent) const
+{
+  Superclass::PrintSelf(os,indent);
+  os << "ImageToListSample adaptor = " << m_ImageToListAdaptor << std::endl;
+  os << "HistogramGenerator = " << m_HistogramGenerator << std::endl;
+}
+
+
+
+} // end of namespace Statistics 
+} // end of namespace itk
+
+#endif
+
+
