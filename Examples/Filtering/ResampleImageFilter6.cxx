6022c8aea0646b912bd50c2e4eee42e7be5776f0
Luis Ibanez <luis.ibanez@kitware.com>
2003-12-12 14:21:14 -0500

    Example on the use of the VectorResampleImageFilter.

---
+++ Examples/Filtering/ResampleImageFilter6.cxx
@@ -0,0 +1,128 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    ResampleImageFilter6.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+//  Software Guide : BeginLatex
+//
+//  Resampling can also be performed in multi-component images.
+//
+//  \index{itk::VectorResampleImageFilter!Image internal transform}
+//
+//  Software Guide : EndLatex 
+
+
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkVectorResampleImageFilter.h"
+#include "itkIdentityTransform.h"
+#include "itkVectorLinearInterpolateImageFunction.h"
+#include "itkRGBPixel.h"
+
+
+int main( int argc, char * argv[] )
+{
+  if( argc < 4 )
+    {
+    std::cerr << "Usage: " << std::endl;
+    std::cerr << argv[0] << "  inputImageFile  outputImageFile" << std::endl;
+    return 1;
+    }
+
+  const     unsigned int   Dimension = 2;
+  typedef   unsigned char  PixelComponentType;
+  typedef   itk::RGBPixel< PixelComponentType > PixelType;
+
+  typedef itk::Image< PixelType,  Dimension >   ImageType;
+
+
+  typedef itk::ImageFileReader< ImageType >  ReaderType;
+  typedef itk::ImageFileWriter< ImageType >  WriterType;
+
+  ReaderType::Pointer reader = ReaderType::New();
+  WriterType::Pointer writer = WriterType::New();
+
+  reader->SetFileName( argv[1] );
+  writer->SetFileName( argv[2] );
+
+
+  typedef itk::VectorResampleImageFilter<
+                            ImageType, ImageType >  FilterType;
+
+  FilterType::Pointer filter = FilterType::New();
+
+  typedef itk::VectorLinearInterpolateImageFunction< 
+                       ImageType, double >  InterpolatorType;
+
+  InterpolatorType::Pointer interpolator = InterpolatorType::New();
+
+  filter->SetInterpolator( interpolator );
+
+
+  typedef itk::IdentityTransform< double, Dimension >  TransformType;
+  TransformType::Pointer transform = TransformType::New();
+
+  filter->SetTransform( transform );
+
+
+  // Software Guide : BeginCodeSnippet
+  filter->SetDefaultPixelValue( 50 );
+  // Software Guide : EndCodeSnippet
+
+
+  // Software Guide : BeginCodeSnippet
+  ImageType::SpacingType spacing;
+  spacing[0] = 1.0; // pixel spacing in millimeters along X
+  spacing[1] = 1.0; // pixel spacing in millimeters along Y
+
+  filter->SetOutputSpacing( spacing );
+  // Software Guide : EndCodeSnippet
+
+
+  // Software Guide : BeginCodeSnippet
+  ImageType::PointType origin;
+  origin[0] = 30.0;  // X space coordinate of origin
+  origin[1] = 40.0;  // Y space coordinate of origin
+  filter->SetOutputOrigin( origin );
+  // Software Guide : EndCodeSnippet
+
+
+  ImageType::SizeType   size;
+
+  size[0] = 300;  // number of pixels along X
+  size[1] = 300;  // number of pixels along Y
+
+  filter->SetSize( size );
+
+  filter->SetInput( reader->GetOutput() );
+  writer->SetInput( filter->GetOutput() );
+
+
+  
+  try
+    {
+    writer->Update();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+    std::cerr << "Exception thrown " << std::endl;
+    std::cerr << excp << std::endl;
+    }
+
+  return 0;
+
+}
+
