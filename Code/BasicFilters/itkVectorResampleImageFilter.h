adcaee6a75655faaa780b806061ed1398698f47c
Luis Ibanez <luis.ibanez@kitware.com>
2003-12-12 14:21:03 -0500

    Filter for resampling images of pixel type vector.

---
+++ Code/BasicFilters/itkVectorResampleImageFilter.h
@@ -0,0 +1,226 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkVectorResampleImageFilter.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkVectorResampleImageFilter_h
+#define __itkVectorResampleImageFilter_h
+
+#include "itkTransform.h"
+#include "itkImageFunction.h"
+#include "itkImageRegionIterator.h"
+#include "itkImageToImageFilter.h"
+#include "itkVectorInterpolateImageFunction.h"
+#include "itkFixedArray.h"
+#include "itkSize.h"
+
+namespace itk
+{
+
+/** \class VectorResampleImageFilter
+ * \brief Resample an image via a coordinate transform
+ *
+ * VectorResampleImageFilter resamples an existing image through some coordinate
+ * transform, interpolating via some image function.  The class is templated
+ * over the types of the input and output images.
+ *
+ * Note that the choice of interpolator function can be important.
+ * This function is set via SetInterpolator().  The default is
+ * itk::VectorLinearInterpolateImageFunction<InputImageType, double>, which
+ * is reasonable for ordinary medical images.  However, some synthetic
+ * images have pixels drawn from a finite prescribed set.  An example
+ * would be a mask indicating the segmentation of a brain into a small
+ * number of tissue types.  For such an image, one does not want to
+ * interpolate between different pixel values, and so
+ * itk::NearestNeighborInterpolateImageFunction< InputImageType,
+ * TCoordRep > would be a better choice.
+ *
+ * Since this filter produces an image which is a different size than
+ * its input, it needs to override several of the methods defined
+ * in ProcessObject in order to properly manage the pipeline execution model.
+ * In particular, this filter overrides
+ * ProcessObject::GenerateInputRequestedRegion() and
+ * ProcessObject::GenerateOutputInformation().
+ *
+ * This filter is implemented as a multithreaded filter.  It provides a 
+ * ThreadedGenerateData() method for its implementation.
+ *
+ * \ingroup GeometricTransforms
+ */
+template <class TInputImage, class TOutputImage>
+class ITK_EXPORT VectorResampleImageFilter:
+    public ImageToImageFilter<TInputImage, TOutputImage>
+{
+public:
+  /** Standard class typedefs. */
+  typedef VectorResampleImageFilter         Self;
+  typedef ImageToImageFilter<TInputImage,TOutputImage>  Superclass;
+  typedef SmartPointer<Self>        Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+  typedef TInputImage InputImageType;
+  typedef TOutputImage OutputImageType;
+  typedef typename InputImageType::Pointer InputImagePointer;
+  typedef typename OutputImageType::Pointer OutputImagePointer;
+  typedef typename InputImageType::RegionType InputImageRegionType;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);  
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(VectorResampleImageFilter, ImageToImageFilter);
+
+  /** Number of dimensions. */
+  itkStaticConstMacro(ImageDimension, unsigned int,
+                      TOutputImage::ImageDimension);
+
+  /** Transform typedef.
+   *
+   * \todo Check that input and output images have the same number of 
+   * dimensions; this is required by the current implementation of 
+   * AffineTransform. */
+  typedef Transform<double, itkGetStaticConstMacro(ImageDimension), itkGetStaticConstMacro(ImageDimension)> TransformType;
+  typedef typename TransformType::Pointer TransformPointerType;
+
+  /** Interpolator typedef. */
+  typedef VectorInterpolateImageFunction<InputImageType, double> InterpolatorType;
+  typedef typename InterpolatorType::Pointer  InterpolatorPointerType;
+
+  /** Image size typedef. */
+  typedef Size<itkGetStaticConstMacro(ImageDimension)> SizeType;
+
+  /** Image index typedef. */
+  typedef typename TOutputImage::IndexType IndexType;
+
+  /** Image point typedef. */
+  typedef typename InterpolatorType::PointType    PointType;
+
+  /** Image pixel value typedef. */
+  typedef typename TOutputImage::PixelType   PixelType;
+  typedef typename PixelType::ValueType      PixelComponentType;
+
+  /** Typedef to describe the output image region type. */
+  typedef typename TOutputImage::RegionType OutputImageRegionType;
+
+  /** Image spacing typedef */
+  typedef typename TOutputImage::SpacingType SpacingType;
+  typedef typename TOutputImage::PointType   OriginPointType;
+  
+  /** Set the coordinate transformation.
+   * Set the coordinate transform to use for resampling.  Note that this
+   * must be in index coordinates and is the output-to-input transform,
+   * NOT the input-to-output transform that you might naively expect.
+   * The default is itk::AffineTransform<double, ImageDimension>. */
+  itkSetObjectMacro( Transform, TransformType ); 
+
+  /** Get a pointer to the coordinate transform. */
+  itkGetObjectMacro( Transform, TransformType );
+
+  /** Set the interpolator function.  The default is
+   * itk::VectorLinearInterpolateImageFunction<InputImageType, double>. Some
+   * other options are itk::NearestNeighborInterpolateImageFunction
+   * (useful for binary masks and other images with a small number of
+   * possible pixel values), and itk::BSplineInterpolateImageFunction
+   * (which provides a higher order of interpolation).  */
+  itkSetObjectMacro( Interpolator, InterpolatorType );
+
+  /** Get a pointer to the interpolator function. */
+  itkGetObjectMacro( Interpolator, InterpolatorType );
+
+  /** Set the size of the output image. */
+  itkSetMacro( Size, SizeType );
+
+  /** Get the size of the output image. */
+  itkGetConstReferenceMacro( Size, SizeType );
+     
+  /** Set the pixel value when a transformed pixel is outside of the
+   * image.  The default default pixel value is 0. */
+  itkSetMacro(DefaultPixelValue,PixelType);
+
+  /** Get the pixel value when a transformed pixel is outside of the image */
+  itkGetMacro(DefaultPixelValue,PixelType);
+
+  /** Set the output image spacing. */
+  itkSetMacro(OutputSpacing, SpacingType);
+  virtual void SetOutputSpacing( const double values[ImageDimension] );
+
+  /** Get the output image spacing. */
+  itkGetConstReferenceMacro( OutputSpacing, SpacingType );
+
+  /** Set the output image origin. */
+  itkSetMacro(OutputOrigin, PointType);
+  virtual void SetOutputOrigin( const double values[ImageDimension] );
+
+  /** Get the output image origin. */
+  itkGetConstReferenceMacro( OutputOrigin, PointType );
+
+  /** VectorResampleImageFilter produces an image which is a different size
+   * than its input.  As such, it needs to provide an implementation
+   * for GenerateOutputInformation() in order to inform the pipeline
+   * execution model.  The original documentation of this method is
+   * below. \sa ProcessObject::GenerateOutputInformaton() */
+  virtual void GenerateOutputInformation();
+
+  /** VectorResampleImageFilter needs a different input requested region than
+   * the output requested region.  As such, VectorResampleImageFilter needs
+   * to provide an implementation for GenerateInputRequestedRegion()
+   * in order to inform the pipeline execution model.
+   * \sa ProcessObject::GenerateInputRequestedRegion() */
+  virtual void GenerateInputRequestedRegion();
+
+  /** This method is used to set the state of the filter before 
+   * multi-threading. */
+  virtual void BeforeThreadedGenerateData();
+
+  /** Method Compute the Modified Time based on changed to the components. */
+  unsigned long GetMTime( void ) const;
+
+protected:
+  VectorResampleImageFilter();
+  ~VectorResampleImageFilter() {};
+  void PrintSelf(std::ostream& os, Indent indent) const;
+
+  /** VectorResampleImageFilter can be implemented as a multithreaded filter.  Therefore,
+   * this implementation provides a ThreadedGenerateData() routine which
+   * is called for each processing thread. The output image data is allocated
+   * automatically by the superclass prior to calling ThreadedGenerateData().
+   * ThreadedGenerateData can only write to the portion of the output image
+   * specified by the parameter "outputRegionForThread"
+   * \sa ImageToImageFilter::ThreadedGenerateData(),
+   *     ImageToImageFilter::GenerateData() */
+  void ThreadedGenerateData(const OutputImageRegionType& outputRegionForThread,
+                            int threadId );
+
+private:
+  VectorResampleImageFilter(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+  SizeType                m_Size;       // Size of the output image
+  TransformPointerType    m_Transform;  // Coordinate transform to use
+  InterpolatorPointerType m_Interpolator;
+  // Image function for interpolation
+  PixelType               m_DefaultPixelValue; 
+  // default pixel value if the point 
+  // is outside the image
+  SpacingType             m_OutputSpacing; // output image spacing
+  PointType               m_OutputOrigin;  // output image origin
+};
+
+  
+} // end namespace itk
+  
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkVectorResampleImageFilter.txx"
+#endif
+  
+#endif
---
+++ Code/BasicFilters/itkVectorResampleImageFilter.txx
@@ -0,0 +1,303 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkVectorResampleImageFilter.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#ifndef _itkVectorResampleImageFilter_txx
+#define _itkVectorResampleImageFilter_txx
+
+#include "itkVectorResampleImageFilter.h"
+#include "itkObjectFactory.h"
+#include "itkIdentityTransform.h"
+#include "itkVectorLinearInterpolateImageFunction.h"
+#include "itkProgressReporter.h"
+
+namespace itk
+{
+
+/**
+ * Initialize new instance
+ */
+template <class TInputImage, class TOutputImage>
+VectorResampleImageFilter<TInputImage, TOutputImage>
+::VectorResampleImageFilter()
+{
+  m_OutputSpacing.Fill(1.0);
+  m_OutputOrigin.Fill(0.0);
+  for (unsigned int i = 0; i < ImageDimension; i++)
+    {
+    m_Size[i] = 0;
+    }
+  
+  m_Transform = IdentityTransform<double, ImageDimension>::New();
+  m_Interpolator = VectorLinearInterpolateImageFunction<InputImageType, double>::New();
+  m_DefaultPixelValue = 0;
+}
+
+
+/**
+ * Print out a description of self
+ *
+ * \todo Add details about this class
+ */
+template <class TInputImage, class TOutputImage>
+void 
+VectorResampleImageFilter<TInputImage, TOutputImage>
+::PrintSelf(std::ostream& os, Indent indent) const
+{
+  Superclass::PrintSelf(os,indent);
+
+  unsigned int j;
+  
+  os << indent << "DefaultPixelValue: "
+     << static_cast<typename NumericTraits<PixelType>::PrintType>(m_DefaultPixelValue)
+     << std::endl;
+  os << indent << "Size: [";
+  for( j = 0; j < ImageDimension - 1; j++ )
+    {
+    os << m_Size[j] << ", ";
+    }
+  os << m_Size[j] << "]" << std::endl;
+
+  os << indent << "OutputSpacing: " << m_OutputSpacing << std::endl;
+  os << indent << "OutputOrigin: " << m_OutputOrigin << std::endl;
+  os << indent << "Transform: " << m_Transform.GetPointer() << std::endl;
+  os << indent << "Interpolator: " << m_Interpolator.GetPointer() << std::endl;
+
+  return;
+}
+
+
+
+/**
+ * Set the output image spacing.
+ */
+template <class TInputImage, class TOutputImage>
+void 
+VectorResampleImageFilter<TInputImage,TOutputImage>
+::SetOutputSpacing(
+  const double spacing[ImageDimension] )
+{
+  SpacingType s(spacing);
+  this->SetOutputSpacing( s );
+}
+
+
+/**
+ * Set the output image origin.
+ */
+template <class TInputImage, class TOutputImage>
+void 
+VectorResampleImageFilter<TInputImage,TOutputImage>
+::SetOutputOrigin(
+  const double origin[ImageDimension] )
+{
+  PointType p(origin);
+  this->SetOutputOrigin( p );
+}
+
+
+/**
+ * Set up state of filter before multi-threading.
+ * InterpolatorType::SetInputImage is not thread-safe and hence
+ * has to be set up before ThreadedGenerateData
+ */
+template <class TInputImage, class TOutputImage>
+void 
+VectorResampleImageFilter<TInputImage,TOutputImage>
+::BeforeThreadedGenerateData()
+{
+
+  if( !m_Interpolator )
+    {
+    itkExceptionMacro(<< "Interpolator not set");
+    }
+
+  // Connect input image to interpolator
+  m_Interpolator->SetInputImage( this->GetInput() );
+
+}
+
+
+/**
+ * ThreadedGenerateData
+ */
+template <class TInputImage, class TOutputImage>
+void 
+VectorResampleImageFilter<TInputImage,TOutputImage>
+::ThreadedGenerateData(
+  const OutputImageRegionType& outputRegionForThread,
+  int threadId)
+{
+  unsigned int i;
+  
+  itkDebugMacro(<<"Actually executing");
+
+  // Get the output pointers
+  OutputImagePointer      outputPtr = this->GetOutput();
+
+  // Create an iterator that will walk the output region for this thread.
+  typedef
+    ImageRegionIterator<TOutputImage> OutputIterator;
+
+  OutputIterator outIt(outputPtr, outputRegionForThread);
+
+  typedef VectorLinearInterpolateImageFunction<InputImageType, double>::OutputType OutputType;
+
+  // Define a few indices that will be used to translate from an input pixel
+  // to an output pixel
+  IndexType outputIndex;         // Index to current output pixel
+  PointType outputPoint;         // Coordinates of current output pixel
+  PointType inputPoint;          // Coordinates of current input pixel
+
+  // Support for progress methods/callbacks
+  ProgressReporter progress(this, threadId, outputRegionForThread.GetNumberOfPixels());
+        
+  const int numberOfComponents = PixelType::Dimension;
+        
+  // Walk the output region
+  for (i=0; !outIt.IsAtEnd(); ++outIt, i++ )
+    {
+    // Determine the index of the current output pixel
+    outputIndex = outIt.GetIndex();
+    outputPtr->TransformIndexToPhysicalPoint( outputIndex, outputPoint );
+
+    // Compute corresponding input pixel position
+    inputPoint = m_Transform->TransformPoint(outputPoint);
+
+    // Evaluate input at right position and copy to the output
+    if( m_Interpolator->IsInsideBuffer(inputPoint) )
+      {
+      PixelType   pixval; 
+      OutputType  output = m_Interpolator->Evaluate( inputPoint );
+      for( unsigned int i=0; i< numberOfComponents; i++ )
+        {
+        pixval[i] = static_cast<PixelComponentType>( output[i] );
+        }
+      outIt.Set( pixval );      
+      }
+    else
+      {
+      outIt.Set(m_DefaultPixelValue); // default background value
+      }
+
+    progress.CompletedPixel();
+    }
+
+  return;
+}
+
+
+/** 
+ * Inform pipeline of necessary input image region
+ *
+ * Determining the actual input region is non-trivial, especially
+ * when we cannot assume anything about the transform being used.
+ * So we do the easy thing and request the entire input image.
+ */
+template <class TInputImage, class TOutputImage>
+void 
+VectorResampleImageFilter<TInputImage,TOutputImage>
+::GenerateInputRequestedRegion()
+{
+  // call the superclass's implementation of this method
+  Superclass::GenerateInputRequestedRegion();
+
+  if ( !this->GetInput() )
+    {
+    return;
+    }
+
+  // get pointers to the input and output
+  InputImagePointer  inputPtr  = 
+    const_cast< TInputImage *>( this->GetInput() );
+
+  // Request the entire input image
+  InputImageRegionType inputRegion;
+  inputRegion = inputPtr->GetLargestPossibleRegion();
+  inputPtr->SetLargestPossibleRegion(inputRegion);
+  inputPtr->SetRequestedRegion(inputRegion);
+
+  return;
+}
+
+
+/** 
+ * Inform pipeline of required output region
+ */
+template <class TInputImage, class TOutputImage>
+void 
+VectorResampleImageFilter<TInputImage,TOutputImage>
+::GenerateOutputInformation()
+{
+  // call the superclass' implementation of this method
+  Superclass::GenerateOutputInformation();
+
+  // get pointers to the input and output
+  OutputImagePointer outputPtr = this->GetOutput();
+  if ( !outputPtr )
+    {
+    return;
+    }
+
+  // Set the size of the output region
+  typename TOutputImage::RegionType outputLargestPossibleRegion;
+  outputLargestPossibleRegion.SetSize( m_Size );
+  outputPtr->SetLargestPossibleRegion( outputLargestPossibleRegion );
+
+  // Set spacing and origin
+  outputPtr->SetSpacing( m_OutputSpacing );
+  outputPtr->SetOrigin( m_OutputOrigin );
+
+  return;
+}
+
+
+
+/** 
+ * Verify if any of the components has been modified.
+ */
+template <class TInputImage, class TOutputImage>
+unsigned long 
+VectorResampleImageFilter<TInputImage,TOutputImage>
+::GetMTime( void ) const
+{
+  unsigned long latestTime = Object::GetMTime(); 
+
+  if( m_Transform )
+    {
+    if( latestTime < m_Transform->GetMTime() )
+      {
+      latestTime = m_Transform->GetMTime();
+      }
+    }
+
+  if( m_Interpolator )
+    {
+    if( latestTime < m_Interpolator->GetMTime() )
+      {
+      latestTime = m_Interpolator->GetMTime();
+      }
+    }
+
+  return latestTime;
+}
+
+
+
+} // end namespace itk
+
+#endif
