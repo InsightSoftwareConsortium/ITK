0d98376524c358eda64a6731e6e90a0feff10bce
Luis Ibanez <luis.ibanez@kitware.com>
2003-12-27 07:39:44 -0500

    Example on how to read an image series into a volume.

---
+++ Examples/IO/RGBImageSeriesReadWrite.cxx
@@ -0,0 +1,144 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    RGBImageSeriesReadWrite.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+//  Software Guide : BeginLatex
+//
+//  RGB images are commonly used for representing data acquired from
+//  cryogenic sections, optical microscopy and endoscopy. This example
+//  illustrates how to read and write RGB color images to and from a file.
+//  This requires the following headers as shown.
+//
+//  \index{itk::RGBPixel!Image}
+//  \index{RGB!writing Image}
+//  \index{RGB!reading Image}
+//
+//  Software Guide : EndLatex 
+
+// Software Guide : BeginCodeSnippet
+#include "itkRGBPixel.h"
+#include "itkImage.h"
+#include "itkImageSeriesReader.h"
+#include "itkImageFileWriter.h"
+#include "itkNumericSeriesFileNames.h"
+// Software Guide : EndCodeSnippet
+
+
+int main( int argc, char ** argv )
+{
+  // Verify the number of parameters in the command line
+  if( argc < 3 )
+    {
+    std::cerr << "Usage: " << std::endl;
+    std::cerr << argv[0] << " inputRGBImageFile  outputRGBImageFile " << std::endl;
+    return -1;
+    }
+
+
+  // Software Guide : BeginLatex
+  //
+  // The \doxygen{RGBPixel} class is templated over the type used to
+  // represent each one of the red, green and Blue components. A typical
+  // instantiation of the RGB image class might be as follows.
+  //
+  //  \index{itk::RGBPixel!Instantiation}
+  //
+  // Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  typedef itk::RGBPixel< unsigned char >   PixelType;
+  typedef itk::Image< PixelType, 2 >       ImageType;
+  // Software Guide : EndCodeSnippet
+
+
+  // Software Guide : BeginLatex
+  //
+  // The image type is used as a template parameter to instantiate
+  // the reader and writer.
+  //
+  // \index{itk::ImageSeriesReader!RGB Image}
+  // \index{itk::ImageFileWriter!RGB Image}
+  //
+  // Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  typedef itk::ImageSeriesReader< ImageType >  ReaderType;
+  typedef itk::ImageFileWriter<   ImageType >  WriterType;
+
+  ReaderType::Pointer reader = ReaderType::New();
+  WriterType::Pointer writer = WriterType::New();
+  // Software Guide : EndCodeSnippet
+
+
+  const char * inputFilename  = argv[1];
+  const char * outputFilename = argv[2];
+
+
+  // Software Guide : BeginCodeSnippet
+  typedef itk::NumericSeriesFileNames    NameGeneratorType;
+
+  NameGeneratorType::Pointer nameGenerator = NameGeneratorType::New();
+   
+  nameGenerator->SetStartIndex( 0 );
+  nameGenerator->SetEndIndex( 100 );
+  nameGenerator->SetIncrementIndex( 1 );
+  nameGenerator->SetSeriesFormat("file%4d.png");
+  // Software Guide : EndCodeSnippet
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  The filenames of the input and output files must be provided to the
+  //  reader and writer respectively.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  reader->SetFileNames( nameGenerator->GetFileNames()  );
+  writer->SetFileName( outputFilename );
+  // Software Guide : EndCodeSnippet
+
+
+  ImageType::Pointer image = reader->GetOutput();
+  writer->SetInput( image );
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  Finally, execution of the pipeline can be triggered by invoking the
+  //  Update() method in the writer.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  writer->Update();
+  // Software Guide : EndCodeSnippet
+
+  //  Software Guide : BeginLatex
+  //
+  //  You may have noticed that apart from the declaraton of the
+  //  \code{PixelType} there is nothing in this code that is specific for RGB
+  //  images. All the actions required to support color images are implemented
+  //  internally in the \doxygen{ImageIO} objects.
+  //
+  //  Software Guide : EndLatex 
+
+
+  return 0;
+}
+
+
+
