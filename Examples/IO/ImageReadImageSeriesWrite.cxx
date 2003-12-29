f58f7b717fe7c64cff96eda061ab54cec4b2c0d2
Luis Ibanez <luis.ibanez@kitware.com>
2003-12-29 14:50:34 -0500

    DOC: Comment added for the SoftwareGuide.

--- Examples/IO/ImageReadImageSeriesWrite.cxx
+++ Examples/IO/ImageReadImageSeriesWrite.cxx
@@ -39,41 +39,100 @@ int main( int argc, char *argv[] )
     return EXIT_FAILURE;
     }
 
-  typedef itk::Image<unsigned char,3> ImageType;
-  typedef itk::Image<unsigned char,2> Image2DType;
-  typedef itk::ImageFileReader<ImageType> ReaderType;
 
+  //  Software Guide : BeginLatex
+  //
+  //  The type of the input image is declared here and it is used for declaring
+  //  the type of the reader. This will be a conventional 3D image reader. 
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
+  typedef itk::Image< unsigned char, 3 >      ImageType;
+  typedef itk::ImageFileReader< ImageType >   ReaderType;
+  // Software Guide : EndCodeSnippet
+
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  The reader object is constructed using the \code{New()} operator and
+  //  assigning the result to a \code{SmartPointer}. The filename of the 3D
+  //  volume to be read is taken from the command line arguments and passed to
+  //  the reader using the \code{SetFileName()} method.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
   ReaderType::Pointer reader = ReaderType::New();
   reader->SetFileName( argv[1] );
+  // Software Guide : EndCodeSnippet
   
-  try
-    {
-    reader->Update();
-    }
-  catch( itk::ExceptionObject & excp )
-    {
-    std::cerr << "Exception thrown while reading the image" << std::endl;
-    std::cerr << excp << std::endl;
-    }
 
-  reader->GetOutput()->Print( std::cout );
+
+  //  Software Guide : BeginLatex
+  //
+  //  The type of the writer must be instantiated taking into account that the
+  //  input file is a 3D volume and the output files are 2D images.
+  //  Additionally, the output of the reader is connected as input to the
+  //  writer.
+  //
+  //  Software Guide : EndLatex 
+
+
+  // Software Guide : BeginCodeSnippet
+  typedef itk::Image< unsigned char, 2 >     Image2DType;
 
   typedef itk::ImageSeriesWriter< ImageType, Image2DType > WriterType;
 
   WriterType::Pointer writer = WriterType::New();
 
   writer->SetInput( reader->GetOutput() );
-
-  char format[500];
-  sprintf( format, "%s%03d.png", argv[2] );
+  // Software Guide : EndCodeSnippet
 
 
-  writer->DebugOn();
+  //  Software Guide : BeginLatex
+  //
+  //  The writer requires a string in the form of a \code{printf()} format in
+  //  order to have a template for generating the filenames of all the output
+  //  slices. Here we compose this string using a prefix taken from the command
+  //  line arguments and adding the extension for PNG files.
+  //
+  //  Software Guide : EndLatex 
 
-  writer->SetSeriesFormat( format );
+
+  // Software Guide : BeginCodeSnippet
+  std::string format = argv[2];
+  format += "%03d.png";
+  // Software Guide : EndCodeSnippet
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  Finally we trigger the execution of the pipeline with the Update()
+  //  method on the writer. At this point the slices of the image will be
+  //  saved in individual files containing a single slice per file.
+  //
+  //  Software Guide : EndLatex 
+
+
+  // Software Guide : BeginCodeSnippet
+  writer->SetSeriesFormat( format.c_str() );
   writer->SetStartIndex( 0 );
   writer->SetIncrementIndex( 1 );
+  // Software Guide : EndCodeSnippet
 
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  Finally we trigger the execution of the pipeline with the Update()
+  //  method on the writer. At this point the slices of the image will be
+  //  saved in individual files containing a single slice per file.
+  //
+  //  Software Guide : EndLatex 
+
+  // Software Guide : BeginCodeSnippet
   try
     {
     writer->Update();
@@ -83,6 +142,7 @@ int main( int argc, char *argv[] )
     std::cerr << "Exception thrown while reading the image" << std::endl;
     std::cerr << excp << std::endl;
     }
+  // Software Guide : EndCodeSnippet
 
 
   return EXIT_SUCCESS;
