685eec246acc3df83414dc9e57fcec1accfac486
Luis Ibanez <luis.ibanez@kitware.com>
2003-12-30 16:13:04 -0500

    ENH: Improved for printing out all the DICOM series found in the directory      and accepting a third optional argument specifying what series to read.

--- Examples/IO/DicomSeriesReadImageWrite.cxx
+++ Examples/IO/DicomSeriesReadImageWrite.cxx
@@ -33,7 +33,7 @@ int main( int argc, char* argv[] )
 
   if( argc < 3 )
     {
-    std::cerr << "Usage: " << argv[0] << " DicomDirectory  outputFileName" << std::endl;
+    std::cerr << "Usage: " << argv[0] << " DicomDirectory  outputFileName  [seriesName]" << std::endl;
     return EXIT_FAILURE;
     }
 
@@ -46,8 +46,44 @@ int main( int argc, char* argv[] )
   itk::DICOMSeriesFileNames::Pointer nameGenerator = itk::DICOMSeriesFileNames::New();
   nameGenerator->SetDirectory( argv[1] );
   
+
+  typedef std::vector<std::string> seriesIdContainer;
+  const seriesIdContainer & seriesUID = nameGenerator->GetSeriesUIDs();
+
+  seriesIdContainer::const_iterator seriesItr = seriesUID.begin();
+  seriesIdContainer::const_iterator seriesEnd = seriesUID.end();
+  
+  std::cout << std::endl << "The directory: " << std::endl;
+  std::cout << std::endl << argv[1] << std::endl << std::endl;
+  std::cout << "Contains the following DICOM Series: ";
+  std::cout << std::endl << std::endl;
+
+  while( seriesItr != seriesEnd )
+    {
+    std::cout << seriesItr->c_str() << std::endl;
+    seriesItr++;
+    }
+  
+  std::cout << std::endl << std::endl;
+  std::cout << "Now reading series: " << std::endl << std::endl;
+
+  typedef std::vector<std::string> fileNamesContainer;
+  fileNamesContainer fileNames;
+
+  if( argc < 4 ) // If no optional third argument
+    {
+    std::cout << seriesUID.begin()->c_str() << std::endl;
+    fileNames = nameGenerator->GetFileNames();
+    }
+  else
+    {
+    std::cout << argv[3] << std::endl;
+    fileNames = nameGenerator->GetFileNames( argv[3] );
+    }
+  std::cout << std::endl << std::endl;
+
   ReaderType::Pointer reader = ReaderType::New();
-  reader->SetFileNames( nameGenerator->GetFileNames());
+  reader->SetFileNames( fileNames );
   reader->SetImageIO( dicomIO );
 
   try
@@ -62,6 +98,9 @@ int main( int argc, char* argv[] )
 
   typedef itk::ImageFileWriter< ImageType > WriterType;
   WriterType::Pointer writer = WriterType::New();
+
+  std::cout  << "Writing the image as " << std::endl << std::endl;
+  std::cout  << argv[2] << std::endl << std::endl;
 
   writer->SetFileName( argv[2] );
 
