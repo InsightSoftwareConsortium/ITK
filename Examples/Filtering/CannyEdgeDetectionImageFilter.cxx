fccd4cb869453f1a73c35ffc70ad93ff925131aa
Luis Ibanez <luis.ibanez@kitware.com>
2004-01-08 11:14:25 -0500

    Example on the use of the CannyEdgeDetectionImageFilter.

---
+++ Examples/Filtering/CannyEdgeDetectionImageFilter.cxx
@@ -0,0 +1,152 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    CannyEdgeDetectionImageFilter.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+//  Software Guide : BeginLatex
+//
+//  This example introduces the use of the
+//  \doxygen{CannyEdgeDetectionImageFilter}. This filter is widely used for
+//  edge detection since it is the optimal solution satisfying the constraints
+//  of good sensitivity, localization and noise robustness.
+//
+//  \index{itk::CannyEdgeDetectionImageFilter|textbf}
+//
+//  Software Guide : EndLatex 
+
+
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkCastImageFilter.h"
+#include "itkRescaleIntensityImageFilter.h"
+
+
+//  Software Guide : BeginLatex
+//
+//  The first step required for using this filter is to include its header file
+//
+//  \index{itk::CannyEdgeDetectionImageFilter!header}
+//
+//  Software Guide : EndLatex 
+
+
+// Software Guide : BeginCodeSnippet
+#include "itkCannyEdgeDetectionImageFilter.h"
+// Software Guide : EndCodeSnippet
+
+
+int main(int argc, char** argv)
+{
+  if( argc < 3 )
+    {
+    std::cerr << "Usage: " << std::endl;
+    std::cerr << argv[0] << " inputImage outputImage [variance]" << std::endl;
+    return -1;
+    }
+   
+  const char * inputFilename  = argv[1];
+  const char * outputFilename = argv[2];
+  float variance = 2.0;
+
+  if( argc > 3 )
+    {
+    variance = atof( argv[3] );
+    }
+
+  typedef unsigned char    CharPixelType;  //  IO
+  typedef double           RealPixelType;  //  Operations
+  const   unsigned int     Dimension = 2;
+
+  typedef itk::Image<CharPixelType, Dimension>    CharImageType;
+  typedef itk::Image<RealPixelType, Dimension>    RealImageType;
+
+  typedef itk::ImageFileReader< CharImageType >  ReaderType;
+  typedef itk::ImageFileWriter< CharImageType >  WriterType;
+
+
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  This filter operates on image of pixel type float. It is then necessary
+  //  to cast the type of the input images that are usually of integer type.
+  //  The \doxygen{CastImageFilter} is used here for that purpose. Its image 
+  //  template parameters are defined for casting from the input type to the
+  //  float type using for processing.
+  //
+  //  Software Guide : EndLatex
+
+  // Software Guide : BeginCodeSnippet
+  typedef itk::CastImageFilter< CharImageType, RealImageType> CastToRealFilterType;
+  // Software Guide : EndCodeSnippet
+
+
+
+  typedef itk::RescaleIntensityImageFilter<RealImageType, CharImageType > RescaleFilter;
+
+
+  //  Software Guide : BeginLatex
+  //
+  //  The \doxygen{CannyEdgeDetectionImageFilter} is instantiated using the float image type.
+  //
+  //  \index{itk::CannyEdgeDetectionImageFilter|textbf}
+  //
+  //  Software Guide : EndLatex
+
+
+  typedef itk::CannyEdgeDetectionImageFilter<RealImageType, RealImageType> CannyFilter;
+
+  //Setting the IO
+
+  ReaderType::Pointer reader = ReaderType::New();
+  WriterType::Pointer writer = WriterType::New();
+
+  CastToRealFilterType::Pointer toReal = CastToRealFilterType::New();
+  RescaleFilter::Pointer rescale = RescaleFilter::New();
+
+  //Setting the ITK pipeline filter
+
+  CannyFilter::Pointer cannyFilter = CannyFilter::New();
+
+  reader->SetFileName( inputFilename  );
+  writer->SetFileName( outputFilename );
+
+  //The output of an edge filter is 0 or 1
+  rescale->SetOutputMinimum(   0 );
+  rescale->SetOutputMaximum( 255 );
+
+  toReal->SetInput( reader->GetOutput() );
+
+  cannyFilter->SetInput( toReal->GetOutput() );
+  cannyFilter->SetVariance( variance );
+  rescale->SetInput( cannyFilter->GetOutput() );
+  writer->SetInput( rescale->GetOutput() );
+
+  try 
+    {
+    writer->Update();
+    }
+  catch( itk::ExceptionObject & err ) 
+    { 
+    std::cout << "ExceptionObject caught !" << std::endl; 
+    std::cout << err << std::endl; 
+    return -1;
+    } 
+
+
+  return 0;
+
+}
