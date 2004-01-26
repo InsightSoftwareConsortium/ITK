9e6a472b06467e33b329671ce00fd42c55ebb68c
Luis Ibanez <luis.ibanez@kitware.com>
2004-01-26 18:30:48 -0500

    CableSwig Tcl version of the CannyEdgeDetection example.

---
+++ Examples/Filtering/CannyEdgeDetectionImageFilter.tcl
@@ -0,0 +1,35 @@
+#
+#  Example on the use of the CannyEdgeDetection filter
+#
+package require InsightToolkit
+
+set reader [ itkImageFileReaderF2_New ]
+set writer [ itkImageFileWriterUS2_New ]
+
+set outputCast [ itkRescaleIntensityImageFilterF2US2_New ]
+
+set filter [ itkCannyEdgeDetectionImageFilterF2F2_New ]
+
+$filter     SetInput [ $reader     GetOutput ]
+$outputCast SetInput [ $filter     GetOutput ]
+$writer     SetInput [ $outputCast GetOutput ]
+
+
+$reader SetFileName [lindex $argv 0]
+$writer SetFileName [lindex $argv 1]
+
+
+$outputCast SetOutputMinimum       0
+$outputCast SetOutputMaximum   65535
+
+set variance   [expr [lindex $argv 2]]
+set threshold  [expr [lindex $argv 3]]
+
+
+$filter SetVariance     $variance
+$filter SetThreshold    $threshold
+
+
+$writer Update
+
+
