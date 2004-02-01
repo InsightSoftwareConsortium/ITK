916a22b0a292dccb436a48cea784975afe7a57bf
Luis Ibanez <luis.ibanez@kitware.com>
2004-01-31 23:18:30 -0500

    Example on the combination of DeformationFieldSource and WarpImageFilter.

---
+++ Examples/Registration/LandmarkWarping2.cxx
@@ -0,0 +1,195 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    LandmarkWarping2.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+//  Software Guide : BeginLatex
+//
+//  This example illustrates how to deform an image using a KernelBase spline
+//  and two sets of landmarks.
+// 
+//  \index{WarpImageFilter}
+//  \index{DeformationFieldSource}
+//
+//  Software Guide : EndLatex 
+
+
+
+// Software Guide : BeginCodeSnippet
+#include "itkImageFileReader.h" 
+#include "itkImageFileWriter.h" 
+
+#include "itkVector.h"
+#include "itkImage.h"
+#include "itkImageRegionIteratorWithIndex.h"
+#include "itkDeformationFieldSource.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkWarpImageFilter.h"
+#include "itkLinearInterpolateImageFunction.h"
+
+#include <fstream>
+
+
+int main( int argc, char * argv[] )
+{
+
+  if( argc < 3 )
+    {
+    std::cerr << "Missing Parameters " << std::endl;
+    std::cerr << "Usage: " << argv[0];
+    std::cerr << " landmarksFile fixedImage ";
+    std::cerr << "movingImage deformedMovingImage" << std::endl;
+    return 1;
+    }
+
+  const     unsigned int   Dimension = 2;
+  typedef   float          VectorComponentType;
+
+  typedef   itk::Vector< VectorComponentType, Dimension >    VectorType;
+
+  typedef   itk::Image< VectorType,  Dimension >   DeformationFieldType;
+
+
+  typedef   unsigned char  PixelType;
+  typedef   itk::Image< PixelType, Dimension >       FixedImageType;
+  typedef   itk::Image< PixelType, Dimension >       MovingImageType;
+
+  typedef   itk::ImageFileReader< FixedImageType  >  FixedReaderType;
+  typedef   itk::ImageFileReader< MovingImageType >  MovingReaderType;
+
+  typedef   itk::ImageFileWriter< MovingImageType >  MovingWriterType;
+
+
+  FixedReaderType::Pointer fixedReader = FixedReaderType::New();
+  fixedReader->SetFileName( argv[2] );
+
+  MovingReaderType::Pointer movingReader = MovingReaderType::New();
+  MovingWriterType::Pointer movingWriter = MovingWriterType::New();
+
+  movingReader->SetFileName( argv[3] );
+  movingWriter->SetFileName( argv[4] );
+
+
+  FixedImageType::ConstPointer fixedImage = fixedReader->GetOutput();
+
+
+  typedef itk::DeformationFieldSource<
+                                DeformationFieldType 
+                                             >  DeformationSourceType;
+
+  DeformationSourceType::Pointer deformer = DeformationSourceType::New();
+
+
+
+  deformer->SetOutputSpacing( fixedImage->GetSpacing() );
+  deformer->SetOutputOrigin(  fixedImage->GetOrigin() );
+  deformer->SetOutputRegion(  fixedImage->GetLargestPossibleRegion() );
+
+
+
+  //  Create source and target landmarks.
+  //  
+  typedef DeformationSourceType::LandmarkContainerPointer   LandmarkContainerPointer;
+  typedef DeformationSourceType::LandmarkContainer          LandmarkContainerType;
+  typedef DeformationSourceType::LandmarkPointType          LandmarkPointType;
+
+  LandmarkContainerType::Pointer sourceLandmarks = LandmarkContainerType::New();
+  LandmarkContainerType::Pointer targetLandmarks = LandmarkContainerType::New();
+
+  LandmarkPointType sourcePoint;
+  LandmarkPointType targetPoint;
+
+  std::ifstream pointsFile;
+  pointsFile.open( argv[1] );
+
+  unsigned int pointId = 0;
+
+  pointsFile >> sourcePoint;
+  pointsFile >> targetPoint;
+
+  while( !pointsFile.fail() )
+    {
+    sourceLandmarks->InsertElement( pointId, sourcePoint );
+    targetLandmarks->InsertElement( pointId, targetPoint );
+    pointId++;
+
+    pointsFile >> sourcePoint;
+    pointsFile >> targetPoint;
+
+    }
+
+  pointsFile.close();
+
+  
+  deformer->SetSourceLandmarks( sourceLandmarks.GetPointer() );
+  deformer->SetTargetLandmarks( targetLandmarks.GetPointer() );
+
+  try
+    {
+    deformer->UpdateLargestPossibleRegion();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+    std::cerr << "Exception thrown " << std::endl;
+    std::cerr << excp << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  DeformationFieldType::ConstPointer deformationField = deformer->GetOutput();
+
+  typedef itk::WarpImageFilter< MovingImageType, 
+                                MovingImageType, 
+                                DeformationFieldType  >  FilterType;
+
+  FilterType::Pointer warper = FilterType::New();
+
+  typedef itk::LinearInterpolateImageFunction< 
+                       MovingImageType, double >  InterpolatorType;
+
+  InterpolatorType::Pointer interpolator = InterpolatorType::New();
+
+  warper->SetInterpolator( interpolator );
+
+
+  warper->SetOutputSpacing( deformationField->GetSpacing() );
+  warper->SetOutputOrigin(  deformationField->GetOrigin() );
+
+  warper->SetDeformationField( deformationField );
+
+  warper->SetInput( movingReader->GetOutput() );
+  
+  movingWriter->SetInput( warper->GetOutput() );
+
+
+  
+  try
+    {
+    movingWriter->Update();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+    std::cerr << "Exception thrown " << std::endl;
+    std::cerr << excp << std::endl;
+    }
+
+
+  return EXIT_SUCCESS;
+
+//  Software Guide : EndLatex
+
+}
+
