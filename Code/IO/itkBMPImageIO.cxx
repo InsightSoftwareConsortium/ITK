db322f8250ad5dbfececd1b561809508cb40459d
Julien Jomier <julien.jomier@kitware.com>
2004-02-20 12:04:32 -0500

    ENH: First checkin

---
+++ Code/IO/itkBMPImageIO.cxx
@@ -0,0 +1,670 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkBMPImageIO.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$  
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "itkBMPImageIO.h"
+#include "itkExceptionObject.h"
+#include "itkByteSwapper.h"
+#include <iostream>
+#include <list>
+#include <string>
+#include <math.h>
+
+namespace itk
+{
+
+/** Constructor */
+BMPImageIO::BMPImageIO()
+{
+  m_ByteOrder = BigEndian;
+  this->SetNumberOfDimensions(2);
+  m_PixelType = UCHAR;
+  m_Spacing[0] = 1.0;
+  m_Spacing[1] = 1.0;
+  
+  m_Origin[0] = 0.0;
+  m_Origin[1] = 0.0;
+  m_FileLowerLeft = 0;
+  m_Depth = 8;
+  m_Allow8BitBMP = true;
+}
+
+
+/** Destructor */
+BMPImageIO::~BMPImageIO()
+{
+  m_Ifstream.close();
+}
+
+
+bool BMPImageIO::CanReadFile( const char* filename ) 
+{ 
+  // First check the filename extension
+  std::string fname = filename;
+  if ( fname == "" )
+    {
+    itkDebugMacro(<< "No filename specified.");
+    }
+
+  bool extensionFound = false;
+  std::string::size_type BMPPos = fname.rfind(".bmp");
+  if ((BMPPos != std::string::npos)
+      && (BMPPos == fname.length() - 4))
+    {
+    extensionFound = true;
+    }
+
+  BMPPos = fname.rfind(".BMP");
+  if ((BMPPos != std::string::npos)
+      && (BMPPos == fname.length() - 4))
+    {
+    extensionFound = true;
+    }
+
+  if( !extensionFound )
+    {
+    itkDebugMacro(<<"The filename extension is not recognized");
+    return false;
+    }
+
+  // Now check the content
+  std::ifstream inputStream;
+  inputStream.open( filename, std::ios::in || std::ios::binary );
+  if( inputStream.fail() )
+    {
+    return false;
+    }
+ 
+  char magic_number1, magic_number2;
+  inputStream.read((char*)&magic_number1,sizeof(char));
+  inputStream.read((char*)&magic_number2,sizeof(char));
+
+  if ((magic_number1 != 'B')||(magic_number2 != 'M'))
+    {
+    std::cout << "BMPImageIO : Magic Number Fails = " << magic_number1 << " : " << magic_number2 << std::endl;
+    inputStream.close();
+    return false;
+    }
+
+  long tmp;
+  long infoSize;
+  int  iinfoSize;  // in case we are on a 64bit machine
+  int  itmp;       // in case we are on a 64bit machine
+
+  // get the size of the file
+  int sizeLong = sizeof(long);
+  if (sizeLong == 4)
+    {
+    inputStream.read((char*)&tmp,4);
+    // skip 4 bytes
+    inputStream.read((char*)&tmp,4);
+    // read the offset
+    inputStream.read((char*)&tmp,4);
+    }
+  else
+    {
+    inputStream.read((char*)&itmp,4);
+    // skip 4 bytes
+    inputStream.read((char*)&itmp,4);
+    // read the offset
+    inputStream.read((char*)&itmp,4);
+    }
+
+  // get size of header
+  if (sizeLong == 4)   // if we are on a 32 bit machine
+    {
+    inputStream.read((char*)&infoSize,sizeof(long));
+    ByteSwapper<long>::SwapFromSystemToLittleEndian(&infoSize);                      
+    // error checking
+    if ((infoSize != 40)&&(infoSize != 12))
+      {
+      inputStream.close();
+      return false;
+      }
+    }
+  else    // else we are on a 64bit machine
+    {
+    inputStream.read((char*)&iinfoSize,sizeof(int));
+    ByteSwapper<int>::SwapFromSystemToLittleEndian(&iinfoSize);
+    infoSize = iinfoSize;
+    
+    // error checking
+    if ((infoSize != 40)&&(infoSize != 12))
+      {
+      inputStream.close();
+      return false;
+      }
+    }
+
+  inputStream.close();
+  return true;
+
+}
+  
+
+
+bool BMPImageIO::CanWriteFile( const char * name )
+{
+  std::string filename = name;
+  if ( filename == "" )
+    {
+    itkDebugMacro(<< "No filename specified.");
+    }
+
+  bool extensionFound = false;
+  std::string::size_type BMPPos = filename.rfind(".bmp");
+  if ((BMPPos != std::string::npos)
+      && (BMPPos == filename.length() - 4))
+    {
+    extensionFound = true;
+    }
+  
+  BMPPos = filename.rfind(".BMP");
+  if ((BMPPos != std::string::npos)
+      && (BMPPos == filename.length() - 4))
+    {
+    extensionFound = true;
+    }
+
+  if( !extensionFound )
+    {
+    itkDebugMacro(<<"The filename extension is not recognized");
+    return false;
+    }
+
+  if( extensionFound )
+    {
+    return true;
+    }
+  return false;
+
+}
+
+
+const std::type_info& BMPImageIO::GetPixelType() const
+{
+  switch(m_PixelType)
+    {
+    case SHORT:
+      return typeid(short);
+    case USHORT:
+      return typeid(unsigned short);
+    case CHAR:
+      return typeid(char);
+    case UCHAR:
+      return typeid(unsigned char);
+    default:
+      ExceptionObject exception(__FILE__, __LINE__);
+      exception.SetDescription("Pixel Type Unknown");
+      throw exception;
+    }
+}
+
+unsigned int BMPImageIO::GetComponentSize() const
+{
+  switch(m_PixelType)
+    {
+    case SHORT:
+      return sizeof(short);
+    case USHORT:
+      return sizeof(unsigned short);
+    case CHAR:
+      return sizeof(char);
+    case UCHAR:
+      return sizeof(unsigned char);
+    default:
+      ExceptionObject exception(__FILE__, __LINE__);
+      exception.SetDescription("Pixel Type Unknown");
+      throw exception;
+    }
+  return 1;
+}
+ 
+ 
+void BMPImageIO::Read(void* buffer)
+{
+
+  char * p = static_cast<char *>(buffer);
+
+  unsigned char inc = m_Depth/8;
+  unsigned long l=0;
+  unsigned long step = 1;
+  long streamRead = m_Dimensions[0]*m_Depth/8;
+
+    for(unsigned int id=0;id<m_Dimensions[1];id++)
+      {
+    char* value = new char[streamRead+1];
+    m_Ifstream.seekg(-streamRead*(id+1),std::ios::end);
+    m_Ifstream.read((char *)value, streamRead);
+    
+    for(unsigned long i=0;i<streamRead;i+=step)
+      { 
+      p[l++]=value[i];  
+      }
+ 
+  delete value;
+      }
+  m_Ifstream.close();
+}
+
+/** 
+ *  Read Information about the BMP file
+ *  and put the cursor of the stream just before the first data pixel
+ */
+void BMPImageIO::ReadImageInformation()
+{
+
+  int xsize, ysize;
+  long tmp;
+  short stmp;
+  long infoSize;
+  int  iinfoSize;  // in case we are on a 64bit machine
+  int  itmp;       // in case we are on a 64bit machine
+
+  // Now check the content
+  m_Ifstream.open( m_FileName.c_str(), std::ios::in || std::ios::binary );
+  if( m_Ifstream.fail() )
+    {
+    return;
+    }
+ 
+  char magic_number1, magic_number2;
+  m_Ifstream.read((char*)&magic_number1,sizeof(char));
+  m_Ifstream.read((char*)&magic_number2,sizeof(char));
+
+  if ((magic_number1 != 'B')||(magic_number2 != 'M'))
+    {
+    std::cout << "BMPImageIO : Magic Number Fails = " << magic_number1 << " : " << magic_number2 << std::endl;
+    m_Ifstream.close();
+    return;
+    }
+
+  // get the size of the file
+  int sizeLong = sizeof(long);
+  if (sizeLong == 4)
+    {
+    m_Ifstream.read((char*)&tmp,4);
+    // skip 4 bytes
+    m_Ifstream.read((char*)&tmp,4);
+    // read the offset
+    m_Ifstream.read((char*)&tmp,4);
+    }
+  else
+    {
+    m_Ifstream.read((char*)&itmp,4);
+    // skip 4 bytes
+    m_Ifstream.read((char*)&itmp,4);
+    // read the offset
+    m_Ifstream.read((char*)&itmp,4);
+    }
+
+  // get size of header
+  if (sizeLong == 4)   // if we are on a 32 bit machine
+    {
+    m_Ifstream.read((char*)&infoSize,sizeof(long));
+    ByteSwapper<long>::SwapFromSystemToLittleEndian(&infoSize);
+                       
+    // error checking
+    if ((infoSize != 40)&&(infoSize != 12))
+      {
+      itkExceptionMacro(<<"Unknown file type! " << m_FileName.c_str() 
+                    <<" is not a Windows BMP file!");
+      m_Ifstream.close();
+      return;
+      }
+  
+    // there are two different types of BMP files
+    if (infoSize == 40)
+      {
+      // now get the dimensions
+      m_Ifstream.read((char*)&xsize,sizeof(long));
+      ByteSwapper<int>::SwapFromSystemToLittleEndian(&xsize);
+      m_Ifstream.read((char*)&ysize,sizeof(long));
+      ByteSwapper<int>::SwapFromSystemToLittleEndian(&ysize);
+      }
+    else
+      {
+      m_Ifstream.read((char*)&stmp,sizeof(short));
+      ByteSwapper<short>::SwapFromSystemToLittleEndian(&stmp);
+      xsize = stmp;
+      m_Ifstream.read((char*)&stmp,sizeof(short));
+      ByteSwapper<short>::SwapFromSystemToLittleEndian(&stmp);
+      ysize = stmp;
+      }
+    }
+  else // else we are on a 64bit machine
+    {
+    m_Ifstream.read((char*)&iinfoSize,sizeof(int));
+    ByteSwapper<int>::SwapFromSystemToLittleEndian(&iinfoSize);
+    
+    infoSize = iinfoSize;
+    
+    // error checking
+    if ((infoSize != 40)&&(infoSize != 12))
+      {
+      itkExceptionMacro(<<"Unknown file type! " << m_FileName.c_str() 
+                    <<" is not a Windows BMP file!");
+      m_Ifstream.close();
+      return;
+      }
+  
+    // there are two different types of BMP files
+    if (infoSize == 40)
+      {
+      // now get the dimensions
+      m_Ifstream.read((char*)&xsize,sizeof(int));
+      ByteSwapper<int>::SwapFromSystemToLittleEndian(&xsize);
+      m_Ifstream.read((char*)&ysize,sizeof(int));
+      ByteSwapper<int>::SwapFromSystemToLittleEndian(&ysize);
+      }
+    else
+      {
+      stmp =0;
+      m_Ifstream.read((char*)&xsize,sizeof(short));
+      ByteSwapper<short>::SwapFromSystemToLittleEndian(&stmp);
+      xsize = stmp;
+      m_Ifstream.read((char*)&xsize,sizeof(short));
+      ByteSwapper<short>::SwapFromSystemToLittleEndian(&stmp);
+      ysize = stmp;
+      }
+    }
+  
+  this->SetNumberOfDimensions(2);
+  m_Dimensions[0] = xsize;
+  m_Dimensions[1] = ysize;
+  
+  // is corner in upper left or lower left
+  if (ysize < 0)
+    {
+    ysize = ysize*-1;
+    m_FileLowerLeft = 0;
+    }
+  else
+    {
+    m_FileLowerLeft = 1;
+    }
+    
+  // ignore planes
+  m_Ifstream.read((char*)&stmp,sizeof(short));
+  // read depth
+  m_Ifstream.read((char*)&m_Depth,sizeof(short));
+  ByteSwapper<short>::SwapFromSystemToLittleEndian(&m_Depth);
+
+  if ((m_Depth != 8)&&(m_Depth != 24))
+    {
+    std::cout <<"Only BMP depths of (8,24) are supported. Not " << m_Depth << std::endl;
+    m_Ifstream.close();
+    return;
+    }
+  
+  // skip over rest of info for long format
+  if (infoSize == 40)
+    {
+    m_Ifstream.read((char*)&tmp,4);
+    m_Ifstream.read((char*)&tmp,4);
+    m_Ifstream.read((char*)&tmp,4);
+    m_Ifstream.read((char*)&tmp,4);
+    m_Ifstream.read((char*)&tmp,4);
+    }
+
+  if ((m_Depth == 8) && m_Allow8BitBMP)
+    {
+    this->SetNumberOfComponents(1);
+    }
+  else
+    {
+    this->SetNumberOfComponents(3);
+    }
+}
+
+
+void 
+BMPImageIO
+::SwapBytesIfNecessary( void* buffer, unsigned long numberOfPixels )
+{
+  switch(m_PixelType)
+    {
+    case CHAR:
+    {
+    if ( m_ByteOrder == LittleEndian )
+      {
+      ByteSwapper<char>::SwapRangeFromSystemToLittleEndian(
+        (char*)buffer, numberOfPixels );
+      }
+    else if ( m_ByteOrder == BigEndian )
+      {
+      ByteSwapper<char>::SwapRangeFromSystemToBigEndian(
+        (char *)buffer, numberOfPixels );
+      }
+    break;
+    }
+    case UCHAR:
+    {
+    if ( m_ByteOrder == LittleEndian )
+      {
+      ByteSwapper<unsigned char>::SwapRangeFromSystemToLittleEndian(
+        (unsigned char*)buffer, numberOfPixels );
+      }
+    else if ( m_ByteOrder == BigEndian )
+      {
+      ByteSwapper<unsigned char>::SwapRangeFromSystemToBigEndian(
+        (unsigned char *)buffer, numberOfPixels );
+      }
+    break;
+    }
+    case SHORT:
+    {
+    if ( m_ByteOrder == LittleEndian )
+      {
+      ByteSwapper<short>::SwapRangeFromSystemToLittleEndian(
+        (short*)buffer, numberOfPixels );
+      }
+    else if ( m_ByteOrder == BigEndian )
+      {
+      ByteSwapper<short>::SwapRangeFromSystemToBigEndian(
+        (short *)buffer, numberOfPixels );
+      }
+    break;
+    }
+    case USHORT:
+    {
+    if ( m_ByteOrder == LittleEndian )
+      {
+      ByteSwapper<unsigned short>::SwapRangeFromSystemToLittleEndian(
+        (unsigned short*)buffer, numberOfPixels );
+      }
+    else if ( m_ByteOrder == BigEndian )
+      {
+      ByteSwapper<unsigned short>::SwapRangeFromSystemToBigEndian(
+        (unsigned short *)buffer, numberOfPixels );
+      }
+    break; 
+    }
+    default:
+      ExceptionObject exception(__FILE__, __LINE__);
+      exception.SetDescription("Pixel Type Unknown");
+      throw exception;
+    }
+}
+
+
+void 
+BMPImageIO
+::WriteImageInformation(void)
+{
+  
+}
+
+
+/** The write function is not implemented */
+void 
+BMPImageIO
+::Write( const void* buffer) 
+{
+ 
+  unsigned int nDims = this->GetNumberOfDimensions();
+
+  if(nDims != 2)
+    {
+    ExceptionObject exception(__FILE__, __LINE__);
+    exception.SetDescription("BMPImageIO cannot write images with a dimension != 2");
+    throw exception;
+    }
+
+#ifdef __sgi
+  // Create the file. This is required on some older sgi's
+  std::ofstream tFile(m_FileName.c_str(),std::ios::out);
+  tFile.close();                    
+#endif
+
+  m_Ofstream.open(m_FileName.c_str(), std::ios::binary | std::ios::out);
+  if( m_Ofstream.fail() )
+    {
+    ExceptionObject exception(__FILE__, __LINE__);
+    exception.SetDescription("File cannot be write");
+    throw exception;
+    }
+
+  // Write the header  
+  // spit out the BMP header
+  char tmp = 66;
+  m_Ofstream.write(&tmp,sizeof(char));
+  tmp = 77;
+  m_Ofstream.write(&tmp,sizeof(char));
+
+  long temp = (long)(m_Dimensions[0]*m_Dimensions[1]) + 54L;
+
+  tmp = temp%256;
+  m_Ofstream.write(&tmp,sizeof(char));
+  tmp = (temp%65536L)/256;
+  m_Ofstream.write(&tmp,sizeof(char));
+  tmp = temp/65536L;
+  m_Ofstream.write(&tmp,sizeof(char));
+  tmp =0;
+
+  int row;
+  for(row = 0; row < 5; row++)
+    {
+    m_Ofstream.write(&tmp,sizeof(char));
+    }
+  tmp =54;
+  m_Ofstream.write(&tmp,sizeof(char));
+  tmp =0;
+  m_Ofstream.write(&tmp,sizeof(char));
+  m_Ofstream.write(&tmp,sizeof(char));
+  m_Ofstream.write(&tmp,sizeof(char));
+  
+  // info header
+  tmp = 40;
+  m_Ofstream.write(&tmp,sizeof(char));
+  tmp =0;
+  m_Ofstream.write(&tmp,sizeof(char));
+  m_Ofstream.write(&tmp,sizeof(char));
+  m_Ofstream.write(&tmp,sizeof(char));
+  
+  tmp = m_Dimensions[0]%256;
+  m_Ofstream.write(&tmp,sizeof(char));
+  tmp = m_Dimensions[0]/256;
+  m_Ofstream.write(&tmp,sizeof(char));
+  tmp =0;
+  m_Ofstream.write(&tmp,sizeof(char));
+  m_Ofstream.write(&tmp,sizeof(char));
+  
+  tmp = m_Dimensions[1]%256;
+  m_Ofstream.write(&tmp,sizeof(char));
+  tmp = m_Dimensions[1]/256;
+  m_Ofstream.write(&tmp,sizeof(char));
+  tmp =0;
+  m_Ofstream.write(&tmp,sizeof(char));
+  m_Ofstream.write(&tmp,sizeof(char));
+  
+  tmp =1;
+  m_Ofstream.write(&tmp,sizeof(char));
+  tmp =0;
+  m_Ofstream.write(&tmp,sizeof(char));
+  tmp =24;
+  m_Ofstream.write(&tmp,sizeof(char));
+  tmp =0;
+  for(row = 0; row < 25; row++)
+    {
+    m_Ofstream.write(&tmp,sizeof(char));
+    }
+
+  int bpp = this->GetNumberOfComponents(); 
+
+  int i=0;
+  for (int h = 0; h < m_Dimensions[1]; h++)
+    {  
+      const char * ptr = static_cast<const char*>(buffer);
+      ptr += (m_Dimensions[1]-(h+1))*m_Dimensions[0]*bpp;
+      if (bpp == 1)
+        {
+        for (i = 0; i < m_Dimensions[0]; i++)
+          {
+          m_Ofstream.write(ptr,sizeof(char));
+          m_Ofstream.write(ptr,sizeof(char));
+          m_Ofstream.write(ptr,sizeof(char));
+          ptr++;
+          }
+        }
+      if (bpp == 3)
+        {
+        for (i = 0; i < m_Dimensions[0]; i++)
+          {
+           m_Ofstream.write(ptr,sizeof(char));
+           ptr++;
+           m_Ofstream.write(ptr,sizeof(char));
+           ptr++;
+           m_Ofstream.write(ptr,sizeof(char));
+           ptr++;
+         
+          }
+        }
+      if (bpp == 4)
+        {
+        for (i = 0; i < m_Dimensions[0]; i++)
+          {
+           m_Ofstream.write(ptr,sizeof(char));
+           ptr++;
+           m_Ofstream.write(ptr,sizeof(char));
+           ptr++;
+           m_Ofstream.write(ptr,sizeof(char));
+           ptr++;
+           ptr++; // skip the alpha value
+          }
+        }
+      }
+}
+
+/** Print Self Method */
+void BMPImageIO::PrintSelf(std::ostream& os, Indent indent) const
+{
+  Superclass::PrintSelf(os, indent);
+  os << indent << "PixelType " << m_PixelType << "\n";
+  os << indent << "Depth " << m_Depth << "\n";
+  os << indent << "FileLowerLeft " << m_FileLowerLeft << "\n";
+  if(m_Allow8BitBMP)
+    {
+    os << indent << "m_Allow8BitBMP : True" << "\n";
+    }
+  else
+    {
+    os << indent << "m_Allow8BitBMP : False" << "\n";
+    }
+}
+
+
+
+} // end namespace itk
---
+++ Code/IO/itkBMPImageIO.h
@@ -0,0 +1,103 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkBMPImageIO.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $1.0$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkBMPImageIO_h
+#define __itkBMPImageIO_h
+
+#ifdef _MSC_VER
+#pragma warning ( disable : 4786 )
+#endif
+
+#include <fstream>
+#include "itkImageIOBase.h"
+#include <stdio.h>
+
+namespace itk
+{
+
+/** \brief Read BMPImage file format. 
+ *
+ *  \ingroup IOFilters
+ *
+ */
+class ITK_EXPORT BMPImageIO : public ImageIOBase
+{
+public:
+  /** Standard class typedefs. */
+  typedef BMPImageIO            Self;
+  typedef ImageIOBase  Superclass;
+  typedef SmartPointer<Self>  Pointer;
+  
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(BMPImageIO, Superclass);
+
+  /*-------- This part of the interfaces deals with reading data. ----- */
+
+  /** Determine the file type. Returns true if this ImageIO can read the
+   * file specified. */
+  virtual bool CanReadFile(const char*) ;
+
+  /** Set the spacing and dimension information for the set filename. */
+  virtual void ReadImageInformation();
+  
+  /** Get the type of the pixel.  */
+  virtual const std::type_info& GetPixelType() const;
+
+  /** Reads the data from disk into the memory buffer provided. */
+  virtual void Read(void* buffer);
+
+  /** Compute the size (in bytes) of the components of a pixel. For
+   * example, and RGB pixel of unsigned char would have a 
+   * component size of 1 byte. */
+  virtual unsigned int GetComponentSize() const;
+  
+  /*-------- This part of the interfaces deals with writing data. ----- */
+
+  /** Determine the file type. Returns true if this ImageIO can write the
+   * file specified. */
+  virtual bool CanWriteFile(const char*);
+
+  /** Set the spacing and dimension information for the set filename. */
+  virtual void WriteImageInformation();
+  
+  /** Writes the data to disk from the memory buffer provided. Make sure
+   * that the IORegions has been set properly. */
+  virtual void Write(const void* buffer);
+
+
+  BMPImageIO();
+  ~BMPImageIO();
+  void PrintSelf(std::ostream& os, Indent indent) const;
+  
+private:
+  BMPImageIO(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+  void SwapBytesIfNecessary(void* buffer, unsigned long numberOfPixels);
+ 
+  std::ifstream   m_Ifstream;
+  std::ofstream   m_Ofstream;
+  bool            m_FileLowerLeft;
+  short           m_Depth;
+  bool            m_Allow8BitBMP;
+};
+
+} // end namespace itk
+
+#endif // __itkBMPImageIO_h
