5e8beb26d2a2ad20f3e81416bb82598cfadd8d52
Julien Jomier <julien.jomier@kitware.com>
2004-02-23 20:20:47 -0500

    ENH: First checkin

---
+++ Code/SpatialObject/itkMetaEvent.h
@@ -0,0 +1,46 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkMetaEvent.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkMetaEvent__h
+#define __itkMetaEvent__h
+
+#include "metaEvent.h"
+
+
+namespace itk 
+{
+
+class MetaEvent : public metaEvent
+{
+
+public:
+
+  MetaEvent(){};
+  ~MetaEvent(){};
+
+  virtual void StartReading(unsigned int n) {metaEvent::StartReading(n);}
+  virtual void StopReading() {metaEvent::StopReading();}
+  virtual void SetCurrentIteration(unsigned int n) {metaEvent::SetCurrentIteration(n);}
+
+private:
+
+
+};
+
+} // end namespace itk
+
+
+#endif
