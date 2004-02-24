f570feafc9622de025f62672c706174dd93ccba9
Julien Jomier <julien.jomier@kitware.com>
2004-02-23 20:12:24 -0500

    ENH: First checkin

---
+++ Utilities/MetaIO/metaEvent.h
@@ -0,0 +1,43 @@
+#ifndef METAEvent_H
+#define METAEvent_H
+
+/*!    MetaEvent (.h)
+ *
+ * Description:
+ *    Event abstract class
+ *
+ * \author Julien Jomier
+ * February 20, 2003
+ *
+ */
+
+
+class metaEvent
+{
+ 
+public:
+
+  metaEvent(){m_Level = -1;};
+  ~metaEvent(){};
+
+  virtual void SetCurrentIteration(unsigned int n) {m_CurrentIteration = n;}
+  virtual void StartReading(unsigned int n) 
+    {
+    m_NumberOfIterations = n;
+    m_Level++;
+    };
+  virtual void StopReading() 
+    {
+    m_Level--;
+    };
+
+protected:
+
+  unsigned int m_CurrentIteration;
+  unsigned int m_NumberOfIterations;
+  int m_Level;
+
+};
+
+
+#endif
