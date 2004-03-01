9904ab19df198f9a79352e03bf3e0c59a7f31bb0
Luis Ibanez <luis.ibanez@kitware.com>
2004-02-29 22:22:41 -0500

    Example on the use of the BinaryDilateImageFilter from Python.

---
+++ Examples/Filtering/BinaryDilateImageFilter.py
@@ -0,0 +1,37 @@
+#
+#  Example on the use of the BinaryDilateImageFilter
+#
+
+from InsightToolkit import *
+
+from sys import argv
+
+
+reader = itkImageFileReaderUC2_New()
+writer = itkImageFileWriterUC2_New()
+
+
+filter  = itkBinaryDilateImageFilterUC2UC2_New()
+
+filter.SetInput( reader.GetOutput() )
+writer.SetInput( filter.GetOutput() )
+
+reader.SetFileName( argv[1] )
+writer.SetFileName( argv[2] )
+
+
+element = itkBinaryBallStructuringElementUC2()
+
+element.SetRadius( 1 )
+element.CreateStructuringElement()
+
+filter.SetKernel( element )
+
+sout = itkStringStream()
+filter.Print( sout.GetStream() )
+print sout.GetString()
+
+writer.Update()
+
+
+
