58cd662585d7623fd75115d81e904382d8939f52
Bill Lorensen <bill.lorensen@gmail.com>
2004-03-08 07:59:37 -0500

    ERR: missing copyright. ERR: warnings.

--- Testing/Code/Algorithms/itkSTAPLEImageFilterTest.cxx
+++ Testing/Code/Algorithms/itkSTAPLEImageFilterTest.cxx
@@ -1,3 +1,23 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkSTAPLEImageFilterTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
 #include <iostream>
 #include <string>
 //#include <iomanip>
@@ -135,7 +155,7 @@ int Stapler<VDimension>::Execute()
 
 int itkSTAPLEImageFilterTest( int argc, char * argv[])
 {
-  unsigned int i;
+  int i;
   StaplerBase *stapler;
 
   if (argc < 5)
