e6f32e75434662e872369cb8d50dc58b97d39482
Luis Ibanez <luis.ibanez@kitware.com>
2004-03-20 13:14:30 -0500

    ENH: Example on how to convert an Image to a PointSet.

---
+++ Examples/DataRepresentation/Mesh/ImageToPointSet.cxx
@@ -0,0 +1,113 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    ImageToPointSet.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+//  Software Guide : BeginLatex
+//
+//  This example illustrates how to convert an ITK Image into a PointSet.
+//
+//  Software Guide : EndLatex 
+
+// Software Guide : BeginCodeSnippet
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+
+
+#include "itkImage.h"
+#include "itkPointSet.h"
+#include "itkImageRegionConstIterator.h"
+
+
+int main( int argc, char ** argv )
+{
+  // Verify the number of parameters in the command line
+  if( argc < 2 )
+    {
+    std::cerr << "Usage: " << std::endl;
+    std::cerr << argv[0] << " inputImageFile  " << std::endl;
+    return -1;
+    }
+
+
+  typedef unsigned char      PixelType;
+  const   unsigned int       Dimension = 2;
+
+  typedef itk::Image< PixelType, Dimension >    ImageType;
+
+  typedef itk::PointSet< PixelType, Dimension > PointSetType;
+
+
+
+  typedef itk::ImageFileReader< ImageType >  ReaderType;
+
+  ReaderType::Pointer reader = ReaderType::New();
+
+  const char * inputFilename  = argv[1];
+  reader->SetFileName( inputFilename  );
+
+  try 
+    { 
+    reader->Update(); 
+    } 
+  catch( itk::ExceptionObject & err ) 
+    { 
+    std::cout << "ExceptionObject caught !" << std::endl; 
+    std::cout << err << std::endl; 
+    return -1;
+    } 
+
+  PointSetType::Pointer  pointSet = PointSetType::New();
+
+
+  typedef itk::ImageRegionConstIterator< ImageType > IteratorType;
+
+  const ImageType * image = reader->GetOutput();
+
+  IteratorType it( image, image->GetBufferedRegion() );
+
+  it.GoToBegin();
+
+
+  typedef PointSetType::PointType     PointType;
+  PointType point;
+
+  unsigned long pointId = 0;
+
+  while( !it.IsAtEnd() )
+    {
+    
+    // Convert the pixel position into a Point
+    image->TransformIndexToPhysicalPoint( it.GetIndex() , point );
+    pointSet->SetPoint( pointId, point );
+
+    // Transfer the pixel data to the value associated with the point.
+    pointSet->SetPointData( pointId, it.Get() );
+
+    ++it;
+    ++pointId;
+    }
+
+  
+  std::cout << "Number Of Points = ";
+  std::cout << pointSet->GetNumberOfPoints() << std::endl;
+
+
+// Software Guide : EndCodeSnippet
+  return 0;
+}
+
+
+
