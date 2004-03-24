1fd14056e5060086f748c4b6d4ea6aae2643ea96
Julien Jomier <julien.jomier@kitware.com>
2004-03-24 15:34:03 -0500

    ENH: Orientation is now of type double

--- Testing/Code/IO/testMetaObject.cxx
+++ Testing/Code/IO/testMetaObject.cxx
@@ -38,7 +38,7 @@ int testMetaObject(int , char *[])
   tObj.Position(0, 1);
   tObj.Position(1, 2);
   tObj.Position(2, 3);
-  float orient[9];
+  double orient[9];
   int i;
   for(i=0; i<9; i++)
     {
