39a716882d4b2cc65923932f77371d03717f7f45
Charl Botha <c.p.botha@tudelft.nl>
2004-03-26 07:59:58 -0500

    ENH: Added unsigned long image types.  The itkWatershedImageFilter has unsigned long output.

--- Wrapping/CSwig/itkCSwigImages.h
+++ Wrapping/CSwig/itkCSwigImages.h
@@ -8,11 +8,11 @@ namespace image
   typedef itk::Image<unsigned char , 2> UC2;
   typedef itk::Image<unsigned short, 2> US2;
   typedef itk::Image<unsigned int  , 2> UI2;
-  //typedef itk::Image<unsigned long  longint, 2> UL2;
+  typedef itk::Image<unsigned long , 2> UL2;
   typedef itk::Image<signed char   , 2> SC2;
   typedef itk::Image<signed short  , 2> SS2;
   typedef itk::Image<signed int    , 2> SI2;
-  //typedef itk::Image<signed long  longint, 2> SL2;
+  typedef itk::Image<signed long   , 2> SL2;
 
 
   typedef itk::Image<float         , 3> F3;
@@ -20,9 +20,9 @@ namespace image
   typedef itk::Image<unsigned char , 3> UC3;
   typedef itk::Image<unsigned short, 3> US3;
   typedef itk::Image<unsigned int  , 3> UI3;
-  //typedef itk::Image<unsigned long long int, 3> UL3;
+  typedef itk::Image<unsigned long , 3> UL3;
   typedef itk::Image<signed char   , 3> SC3;
   typedef itk::Image<signed short  , 3> SS3;
   typedef itk::Image<signed int    , 3> SI3;
-  //typedef itk::Image<signed long long int, 3> SL3;
+  typedef itk::Image<signed long   , 3> SL3;
 }
