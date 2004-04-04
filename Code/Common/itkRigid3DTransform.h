a288c1ceae023b357d70ad34d3fa2b96c093fc99
Julien Jomier <julien.jomier@kitware.com>
2004-04-04 19:13:19 -0400

    FIX: SetRotationMatrix() and Compose() are now virtual

--- Code/Common/itkRigid3DTransform.h
+++ Code/Common/itkRigid3DTransform.h
@@ -128,14 +128,14 @@ public:
    * \warning This method will throw an exception is the matrix
    * provided as argument is not orthogonal.
    **/
-  void SetRotationMatrix(const MatrixType &matrix);
+  virtual void SetRotationMatrix(const MatrixType &matrix);
 
 
   /**
    * Compose with another Rigid3DTransform
    *
    **/
-  void Compose(const Self *other, bool pre=false);
+  virtual void Compose(const Self *other, bool pre=false);
 
 
   /**
