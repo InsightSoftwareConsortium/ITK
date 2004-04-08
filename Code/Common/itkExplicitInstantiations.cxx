e2b26b027c1851b86e7c777b4301b55e5a20f9e8
Hans Johnson <hans-johnson@uiowa.edu>
2004-04-08 09:56:38 -0400

    ENH: Changes to facilitate explicit template instatiation of very common templated classes.

--- Code/Common/itkExplicitInstantiations.cxx
+++ Code/Common/itkExplicitInstantiations.cxx
@@ -17,7 +17,8 @@
      PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
-#undef ITK_EXPLICIT_INSTANTIATION
+#ifdef ITK_EXPLICIT_INSTANTIATION  //This is a strange construct, only compile this file if ITK_EXPLICIT_INSTANTIATION is defined
+#undef ITK_EXPLICIT_INSTANTIATION  //and this file requires that the headers are included with ITK_EXPLICIT_INSTANTIATION undefined!!
 //NOTE: If this file is compiled, then this flag must not have been already defined.
 //The ITK_EXPLICIT_INSTANTIATION flag turns off the  instantiation in the itkImage.h so that these classes are built once.  With out undef'ing ITK_EXPLICIT_INSTANTIATION in this file, the compiler will refuse to instatiate the objects, expeting that to be done elsewhere.
 
@@ -28,17 +29,48 @@
 
 //The example for doing this was taken from the VC6.0 documentation, so I assume that it would work there also.
 
+#include "itkSize.h"
+#include "itkIndex.h"
 #include "itkImageBase.h"
 #include "itkImage.h"
 #include "itkImageSource.h"
+#include "itkImageToImageFilter.h"
 #include "itkPoint.h"
 #include "itkConstNeighborhoodIterator.h"
 
 namespace itk
 {
+   //Compilations from itkSize.h
+   template class Size<1>;
+   template class Size<2>;
+   template class Size<3>;
+   template class Size<4>;
+   template class Size<5>;
+   template std::ostream & operator<<(std::ostream &os, const Size<1> &size);
+   template std::ostream & operator<<(std::ostream &os, const Size<2> &size);
+   template std::ostream & operator<<(std::ostream &os, const Size<3> &size);
+   template std::ostream & operator<<(std::ostream &os, const Size<4> &size);
+   template std::ostream & operator<<(std::ostream &os, const Size<5> &size);
+
+   //Compilations from itkIndex.h
+   template class Index<1>;
+   template class Index<2>;
+   template class Index<3>;
+   template class Index<4>;
+   template class Index<5>;
+   template std::ostream & operator<<(std::ostream &os, const Index<1> &ind);
+   template std::ostream & operator<<(std::ostream &os, const Index<2> &ind);
+   template std::ostream & operator<<(std::ostream &os, const Index<3> &ind);
+   template std::ostream & operator<<(std::ostream &os, const Index<4> &ind);
+   template std::ostream & operator<<(std::ostream &os, const Index<5> &ind);
+
+   //Compilations from itkImageBase.h
    template class ImageBase<2>;
    template class ImageBase<3>;
+   template class ImageBase<4>;
+   template class ImageBase<5>;
 
+   //Compilations from itkImage.h
    template class Image<float         ,2>;
    template class Image<double        ,2>;
    template class Image<unsigned char ,2>;
@@ -56,6 +88,7 @@ namespace itk
    template class Image<signed short  ,3>;
    template class Image<signed int    ,3>;
 
+   //Compilations from itkImageSource.h
    template class ImageSource<Image<float         ,2> >;
    template class ImageSource<Image<double        ,2> >;
    template class ImageSource<Image<unsigned char ,2> >;
@@ -73,10 +106,54 @@ namespace itk
    template class ImageSource<Image<signed short  ,3> >;
    template class ImageSource<Image<signed int    ,3> >;
 
+   //Compilations from itkImageToImageFilter.h
+   template class ImageToImageFilter<Image<float         ,2>, Image<float         ,2> >;
+   template class ImageToImageFilter<Image<double        ,2>, Image<double        ,2> >;
+   template class ImageToImageFilter<Image<unsigned char ,2>, Image<unsigned char ,2> >;
+   template class ImageToImageFilter<Image<unsigned short,2>, Image<unsigned short,2> >;
+   template class ImageToImageFilter<Image<signed char   ,2>, Image<signed char   ,2> >;
+   template class ImageToImageFilter<Image<signed short  ,2>, Image<signed short  ,2> >;
+   template class ImageToImageFilter<Image<signed int    ,2>, Image<signed int    ,2> >;
+
+   template class ImageToImageFilter<Image<double        ,2>, Image<float         ,2> >;
+   template class ImageToImageFilter<Image<unsigned char ,2>, Image<float         ,2> >;
+   template class ImageToImageFilter<Image<unsigned short,2>, Image<float         ,2> >;
+   template class ImageToImageFilter<Image<signed char   ,2>, Image<float         ,2> >;
+   template class ImageToImageFilter<Image<signed short  ,2>, Image<float         ,2> >;
+
+   template class ImageToImageFilter<Image<float         ,2>, Image<double        ,2> >;
+   template class ImageToImageFilter<Image<float         ,2>, Image<unsigned char ,2> >;
+   template class ImageToImageFilter<Image<float         ,2>, Image<unsigned short,2> >;
+   template class ImageToImageFilter<Image<float         ,2>, Image<signed char   ,2> >;
+   template class ImageToImageFilter<Image<float         ,2>, Image<signed short  ,2> >;
+
+   template class ImageToImageFilter<Image<float         ,3>, Image<float         ,3> >;
+   template class ImageToImageFilter<Image<double        ,3>, Image<double        ,3> >;
+   template class ImageToImageFilter<Image<unsigned char ,3>, Image<unsigned char ,3> >;
+   template class ImageToImageFilter<Image<unsigned short,3>, Image<unsigned short,3> >;
+   template class ImageToImageFilter<Image<signed char   ,3>, Image<signed char   ,3> >;
+   template class ImageToImageFilter<Image<signed short  ,3>, Image<signed short  ,3> >;
+   template class ImageToImageFilter<Image<signed int    ,3>, Image<signed int    ,3> >;
+
+   template class ImageToImageFilter<Image<double        ,3>, Image<float         ,3> >;
+   template class ImageToImageFilter<Image<unsigned char ,3>, Image<float         ,3> >;
+   template class ImageToImageFilter<Image<unsigned short,3>, Image<float         ,3> >;
+   template class ImageToImageFilter<Image<signed char   ,3>, Image<float         ,3> >;
+   template class ImageToImageFilter<Image<signed short  ,3>, Image<float         ,3> >;
+
+   template class ImageToImageFilter<Image<float         ,3>, Image<double        ,3> >;
+   template class ImageToImageFilter<Image<float         ,3>, Image<unsigned char ,3> >;
+   template class ImageToImageFilter<Image<float         ,3>, Image<unsigned short,3> >;
+   template class ImageToImageFilter<Image<float         ,3>, Image<signed char   ,3> >;
+   template class ImageToImageFilter<Image<float         ,3>, Image<signed short  ,3> >;
+
+   //Compilations from itkPoint.h
    template class Point<float         ,2>;
    template class Point<double        ,2>;
    template class Point<float         ,3>;
    template class Point<double        ,3>;
 
+   //Compilations from xxxx
 } // end namespace itk
 
+#endif
