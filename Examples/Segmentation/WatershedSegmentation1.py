698a73827034f1b0cb76cd38278d9b4b31180605
Luis Ibanez <luis.ibanez@kitware.com>
2004-04-22 13:55:46 -0400

    Example on the use of the Watershed filter. Modified from file posted      by Tsai at sjtu edu cn on the users list.

---
+++ Examples/Segmentation/WatershedSegmentation1.py
@@ -0,0 +1,28 @@
+
+import InsightToolkit as itk
+import sys
+
+reader = itk.itkImageFileReaderF2_New()
+reader.SetFileName( sys.argv[1] )
+
+diffusion = itk.itkGradientAnisotropicDiffusionImageFilterF2F2_New()
+diffusion.SetInput(reader.GetOutput())
+diffusion.SetTimeStep(0.0625)
+diffusion.SetConductanceParameter(9.0)
+diffusion.SetNumberOfIterations(  5 );
+
+gradient = itk.itkGradientMagnitudeImageFilterF2F2_New()
+gradient.SetInput(diffusion.GetOutput())
+
+watershed = itk.itkWatershedImageFilterF2_New()
+watershed.SetInput(gradient.GetOutput())
+watershed.SetThreshold(0.1)
+watershed.SetLevel(0.6)
+
+writer = itk.itkImageFileWriterUL2_New()
+writer.SetFileName( sys.argv[2] )
+writer.SetInput( watershed.GetOutput()  )
+writer.Update()
+
+
+
