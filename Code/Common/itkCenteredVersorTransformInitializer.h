007ac7723728f506ff4889f40d0990f9df6d32b3
Luis Ibanez <luis.ibanez@kitware.com>
2004-04-25 15:11:57 -0400

    ENH: First draft of the Centered initializer that also uses the image      moments in order to set the orientation on the Transform.

---
+++ Code/Common/itkCenteredVersorTransformInitializer.h
@@ -0,0 +1,116 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkCenteredVersorTransformInitializer.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#ifndef __itkCenteredVersorTransformInitializer_h
+#define __itkCenteredVersorTransformInitializer_h
+
+
+#include "itkCenteredTransformInitializer.h"
+#include "itkVersorRigid3DTransform.h"
+
+
+namespace itk
+{
+
+/** \brief CenteredVersorTransformInitializer is a helper class intended to
+ * initialize the center of rotation, versor, and translation of the 
+ * VersorRigid3DTransform.
+ * 
+ * This class derived from the CenteredTransformInitializer and uses it in
+ * a more constrained context. It always uses the Moments mode, and also
+ * takes advantage of the second order moments in order to initialize the
+ * Versor representing rotation.
+ * 
+ * \ingroup Transforms
+ */
+template < class TFixedImage,
+           class TMovingImage > 
+class ITK_EXPORT CenteredVersorTransformInitializer : 
+                      public CenteredTransformInitializer< 
+                                    VersorRigid3DTransform<double>, 
+                                    TFixedImage,TMovingImage> 
+{
+public:
+  /** Standard class typedefs. */
+  typedef CenteredVersorTransformInitializer      Self;
+  typedef CenteredTransformInitializer< 
+               VersorRigid3DTransform<double>, 
+               TFixedImage,TMovingImage>          Superclass;
+  typedef SmartPointer<Self>                      Pointer;
+  typedef SmartPointer<const Self>                ConstPointer;
+    
+  /** New macro for creation of through a Smart Pointer. */
+  itkNewMacro( Self );
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro( CenteredVersorTransformInitializer, Object );
+
+  /** Type of the transform to initialize */
+  typedef typename Superclass::TransformType        TransformType;
+  typedef typename Superclass::TransformPointer     TransformPointer;
+
+  /** Dimension of parameters. */
+  itkStaticConstMacro(SpaceDimension, unsigned int, Superclass::SpaceDimension);
+  itkStaticConstMacro(InputSpaceDimension, unsigned int, Superclass::InputSpaceDimension);
+  itkStaticConstMacro(OutputSpaceDimension, unsigned int, Superclass::OutputSpaceDimension);
+
+  
+  /** Image Types to use in the initialization of the transform */
+  typedef   typename Superclass::FixedImageType   FixedImageType;
+  typedef   typename Superclass::MovingImageType  MovingImageType;
+
+  typedef   typename Superclass::FixedImagePointer   FixedImagePointer;
+  typedef   typename Superclass::MovingImagePointer  MovingImagePointer;
+
+
+
+  /** Offset type. */
+  typedef typename Superclass::OffsetType  OffsetType;
+
+  /** Point type. */
+  typedef typename Superclass::InputPointType   InputPointType;
+  
+  /** Vector type. */
+  typedef typename Superclass::OutputVectorType  OutputVectorType;
+  
+
+  /** Initialize the transform using data from the images */
+  void InitializeTransform() const;
+
+
+protected:
+  CenteredVersorTransformInitializer();
+  ~CenteredVersorTransformInitializer(){};
+
+  void PrintSelf(std::ostream &os, Indent indent) const;
+
+private:
+  CenteredVersorTransformInitializer(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+
+}; //class CenteredVersorTransformInitializer
+
+
+}  // namespace itk
+
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkCenteredVersorTransformInitializer.txx"
+#endif
+
+#endif /* __itkCenteredVersorTransformInitializer_h */
---
+++ Code/Common/itkCenteredVersorTransformInitializer.txx
@@ -0,0 +1,62 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkCenteredVersorTransformInitializer.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#ifndef __itkCenteredVersorTransformInitializer_txx
+#define __itkCenteredVersorTransformInitializer_txx
+
+#include "itkCenteredVersorTransformInitializer.h"
+
+namespace itk
+{
+
+template < class TFixedImage, class TMovingImage >
+CenteredVersorTransformInitializer<TFixedImage, TMovingImage >
+::CenteredVersorTransformInitializer() 
+{
+  // Force to use Moments computation since we need here the second
+  // order moments in order to estimate a rotation
+  this->Superclass::MomentsOn();
+}
+
+
+
+template < class TFixedImage, class TMovingImage >
+void 
+CenteredVersorTransformInitializer<TFixedImage, TMovingImage >
+::InitializeTransform() const
+{
+
+  // Compute moments and initialize center of rotaion and translation
+  this->Superclass::InitializeTransform();
+
+  
+}
+  
+
+      
+
+template < class TFixedImage, class TMovingImage >
+void 
+CenteredVersorTransformInitializer<TFixedImage, TMovingImage >
+::PrintSelf(std::ostream& os, Indent indent) const
+{
+  this->Superclass::PrintSelf(os,indent);
+}
+ 
+}  // namespace itk
+
+#endif /* __itkCenteredVersorTransformInitializer_txx */
