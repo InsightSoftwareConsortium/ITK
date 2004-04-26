3ab475e596073525df4e43ffdf265698b21e7b46
Luis Ibanez <luis.ibanez@kitware.com>
2004-04-25 20:02:49 -0400

    ENH: Reconfigures after the model of the CenteredTransfromInitializer test.      start adding test for orientation.

--- Testing/Code/Common/itkCenteredVersorTransformInitializerTest.cxx
+++ Testing/Code/Common/itkCenteredVersorTransformInitializerTest.cxx
@@ -22,6 +22,8 @@
 #include "itkCenteredVersorTransformInitializer.h"
 #include "itkImage.h"
 
+#include "itkImageRegionIterator.h"
+
 
 /** 
  *  This program tests the use of the CenteredVersorTransformInitializer class
@@ -29,24 +31,25 @@
  *  
  */ 
 
-int itkCenteredVersorTransformInitializerTest(int argc, char* argv[] )
+int itkCenteredVersorTransformInitializerTest(int , char* [] )
 {
 
   bool pass = true;
 
-  const unsigned int dimension = 3;
+  const unsigned int Dimension = 3;
 
   // Fixed Image Type
-  typedef itk::Image<unsigned char ,dimension>      FixedImageType;
+  typedef itk::Image<unsigned char, Dimension>      FixedImageType;
 
   // Moving Image Type
-  typedef itk::Image<unsigned char,dimension>       MovingImageType;
+  typedef itk::Image<unsigned char, Dimension>       MovingImageType;
 
   // Size Type
-  typedef MovingImageType::SizeType                 SizeType;
-  typedef MovingImageType::SpacingType              SpacingType;
-  typedef MovingImageType::PointType                PointType;
-  typedef MovingImageType::IndexType                IndexType;
+  typedef FixedImageType::SizeType                 SizeType;
+  typedef FixedImageType::SpacingType              SpacingType;
+  typedef FixedImageType::PointType                PointType;
+  typedef FixedImageType::IndexType                IndexType;
+  typedef FixedImageType::RegionType               RegionType;
 
 
   // Transform Type
@@ -56,15 +59,212 @@ int itkCenteredVersorTransformInitializerTest(int argc, char* argv[] )
   SizeType size;
   size[0] = 100;
   size[1] = 100;
-  size[2] = 100;
+  size[2] =  60;
   
   PointType fixedOrigin;
+  fixedOrigin[0] = 0.0;
+  fixedOrigin[1] = 0.0;
+  fixedOrigin[2] = 0.0;
+
+  PointType movingOrigin;
+  movingOrigin[0] = 29.0;
+  movingOrigin[1] = 17.0;
+  movingOrigin[2] = 13.0;
+
+  SpacingType spacing;
+  spacing[0] = 1.5;
+  spacing[1] = 1.5;
+  spacing[2] = 2.5;
+
+  IndexType index;
+  index[0] = 0;
+  index[1] = 0;
+  index[2] = 0;
+
+  RegionType region;
+  region.SetSize( size );
+  region.SetIndex( index );
 
 
   FixedImageType::Pointer     fixedImage    = FixedImageType::New();
   MovingImageType::Pointer    movingImage   = MovingImageType::New();
 
+  fixedImage->SetRegions( region );
+  fixedImage->SetSpacing( spacing );
+  fixedImage->SetOrigin(  fixedOrigin );
+  fixedImage->Allocate();
+  fixedImage->FillBuffer( 0 );
 
+  movingImage->SetRegions( region );
+  movingImage->SetSpacing( spacing );
+  movingImage->SetOrigin(  movingOrigin );
+  movingImage->Allocate();
+  movingImage->FillBuffer( 0 );
+  
+  RegionType internalRegion;
+  SizeType  internalSize;
+  IndexType internalIndex;
+
+  internalIndex[0] = index[0] + 20;
+  internalIndex[1] = index[1] + 30;
+  internalIndex[2] = index[2] + 40;
+  
+  internalSize[0]  = size[0] - 2 * 20;
+  internalSize[1]  = size[1] - 2 * 30;
+  internalSize[2]  = size[2] - 2 * 40;
+
+
+  internalRegion.SetSize(  internalSize  );
+  internalRegion.SetIndex( internalIndex );
+
+  typedef itk::ImageRegionIterator< FixedImageType > FixedIterator;
+  FixedIterator fi( fixedImage, internalRegion );
+
+  fi.GoToBegin();
+  while( !fi.IsAtEnd() )
+    {
+    fi.Set( 200 );
+    ++fi;
+    }
+   
+
+  internalIndex[0] = index[0] + 40;
+  internalIndex[1] = index[1] + 20;
+  internalIndex[2] = index[2] + 30;
+  
+  internalSize[0]  = size[0] - 2 * 40;
+  internalSize[1]  = size[1] - 2 * 20;
+  internalSize[2]  = size[2] - 2 * 30;
+
+
+  internalRegion.SetSize(  internalSize  );
+  internalRegion.SetIndex( internalIndex );
+
+
+  typedef itk::ImageRegionIterator< MovingImageType > MovingIterator;
+  MovingIterator mi( movingImage, internalRegion );
+
+  mi.GoToBegin();
+  while( !mi.IsAtEnd() )
+    {
+    mi.Set( 200 );
+    ++mi;
+    }
+   
+
+
+  
+  TransformType::Pointer transform = TransformType::New();
+
+
+
+  typedef itk::CenteredVersorTransformInitializer< 
+                                  FixedImageType, 
+                                  MovingImageType >
+                                            InitializerType;
+
+  InitializerType::Pointer initializer = InitializerType::New();
+
+  initializer->SetFixedImage( fixedImage );
+  initializer->SetMovingImage( movingImage );
+  initializer->SetTransform( transform );
+                                    
+  transform->SetIdentity();
+  initializer->GeometryOn();
+  initializer->InitializeTransform();
+
+  std::cout << std::endl << std::endl;
+  std::cout << "Testing Geometric Mode " << std::endl;
+  transform->Print( std::cout );
+
+  TransformType::InputPointType   center1      = transform->GetCenter();
+  TransformType::OutputVectorType translation1 = transform->GetTranslation();
+  TransformType::OffsetType       offset1      = transform->GetOffset();
+
+  const double tolerance = 1e-3;
+  
+  { // Verfications for the Geometry Mode
+  TransformType::InputPointType   fixedCenter;
+  TransformType::InputPointType   movingCenter;
+
+  for(unsigned int j=0; j < Dimension; j++ )
+    {
+    fixedCenter[j]  = fixedOrigin[j]  + size[j] * spacing[j] / 2.0 ;
+    movingCenter[j] = movingOrigin[j] + size[j] * spacing[j] / 2.0 ;
+    }
+  
+  TransformType::InputVectorType relativeCenter = movingCenter - fixedCenter;
+
+  for(unsigned int k=0; k < Dimension; k++ )
+    {
+    if( fabs( translation1[k] - relativeCenter[k] ) > tolerance )
+      {
+      std::cerr << "Translation differs from expected value" << std::endl;
+      std::cerr << "It should be " << relativeCenter << std::endl;
+      std::cerr << "but it is    " << translation1 << std::endl;
+      pass = false;
+      break;
+      }
+    if( fabs( offset1[k] - relativeCenter[k] ) > tolerance )
+      {
+      std::cerr << "Offset differs from expected value" << std::endl;
+      std::cerr << "It should be " << relativeCenter << std::endl;
+      std::cerr << "but it is    " << offset1 << std::endl;
+      pass = false;
+      break;
+      }
+    }
+  }
+
+  transform->SetIdentity();
+  initializer->MomentsOn();
+  initializer->InitializeTransform();
+
+  std::cout << std::endl << std::endl;
+  std::cout << "Testing Moments Mode " << std::endl;
+  transform->Print( std::cout );
+
+  TransformType::InputPointType   center2      = transform->GetCenter();
+  TransformType::OutputVectorType translation2 = transform->GetTranslation();
+  TransformType::OffsetType       offset2      = transform->GetOffset();
+
+  { // Verfications for the Moments Mode
+  TransformType::InputPointType   fixedCenter;
+  TransformType::InputPointType   movingCenter;
+
+  for(unsigned int j=0; j < Dimension; j++ )
+    {
+    fixedCenter[j]  = fixedOrigin[j]  + size[j] * spacing[j] / 2.0 ;
+    movingCenter[j] = movingOrigin[j] + size[j] * spacing[j] / 2.0 ;
+    }
+  
+  TransformType::InputVectorType relativeCenter = movingCenter - fixedCenter;
+
+
+
+  for(unsigned int k=0; k < Dimension; k++ )
+    {
+    if( fabs( translation2[k] - relativeCenter[k] ) > tolerance )
+      {
+      std::cerr << "Translation differs from expected value" << std::endl;
+      std::cerr << "It should be " << relativeCenter << std::endl;
+      std::cerr << "but it is    " << translation2 << std::endl;
+      pass = false;
+      break;
+      }
+    if( fabs( offset2[k] - relativeCenter[k] ) > tolerance )
+      {
+      std::cerr << "Offset differs from expected value" << std::endl;
+      std::cerr << "It should be " << relativeCenter << std::endl;
+      std::cerr << "but it is    " << offset2 << std::endl;
+      pass = false;
+      break;
+      }
+    }
+  }
+
+
+  
   if( !pass )
     {
     std::cout << "Test FAILED." << std::endl;
