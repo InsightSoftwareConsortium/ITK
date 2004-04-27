764f959b9edff54967281c047a2a7e5c458710a6
Luis Ibanez <luis.ibanez@kitware.com>
2004-04-27 09:37:26 -0400

    Example on how to perform Iterative Closest Point (ICP) with ITK      registration framework.

---
+++ Examples/Registration/IterativeClosestPoint1.cxx
@@ -0,0 +1,215 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    IterativeClosestPoint1.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifdef _WIN32
+#pragma warning ( disable : 4786 )
+#endif
+
+// Software Guide : BeginLatex
+//
+// This example illustrates how to perform Iterative Closest Point (ICP) 
+// registration in ITK. The main class featured in this section is the 
+// \doxygen{IterativeClosestPointMetric}.
+//
+// Software Guide : EndLatex 
+
+
+
+// Software Guide : BeginCodeSnippet
+#include "itkTranslationTransform.h"
+#include "itkIterativeClosestPointMetric.h"
+#include "itkLevenbergMarquardtOptimizer.h"
+#include "itkPointSet.h"
+#include "itkPointSetToPointSetRegistrationMethod.h"
+#include <iostream>
+#include <fstream>
+
+
+int main(int argc, char * argv[] )
+{
+
+  if( argc < 3 )
+    {
+    std::cerr << "Arguments Missing. " << std::endl;
+    std::cerr << "Usage:  IterativeClosestPoint1   fixedPointsFile  movingPointsFile " << std::endl;
+    return 1;
+    }
+
+
+  typedef itk::PointSet< float, 2 >   PointSetType;
+
+  PointSetType::Pointer fixedPointSet  = PointSetType::New();
+  PointSetType::Pointer movingPointSet = PointSetType::New();
+
+  typedef PointSetType::PointType     PointType;
+
+  typedef PointSetType::PointsContainer  PointsContainer;
+
+  PointsContainer::Pointer fixedPointContainer  = fixedPointSet->GetPoints();
+  PointsContainer::Pointer movingPointContainer = movingPointSet->GetPoints();
+
+  PointType fixedPoint;
+  PointType movingPoint;
+
+
+  // Read the file containing coordinates of fixed points.
+  std::ifstream   fixedFile;
+  fixedFile.open( argv[1] );
+  if( fixedFile.fail() )
+    {
+    std::cerr << "Error opening points file with name : " << std::endl;
+    std::cerr << argv[1] << std::endl;
+    return 2;
+    }
+
+  unsigned int pointId = 0;
+  fixedFile >> fixedPoint;
+  while( fixedFile.eof() );
+    {
+    fixedPointContainer->InsertElement( pointId, fixedPoint );
+    fixedFile >> fixedPoint;
+    }
+
+
+
+  // Read the file containing coordinates of moving points.
+  std::ifstream   movingFile;
+  movingFile.open( argv[1] );
+  if( movingFile.fail() )
+    {
+    std::cerr << "Error opening points file with name : " << std::endl;
+    std::cerr << argv[2] << std::endl;
+    return 2;
+    }
+
+  pointId = 0;
+  movingFile >> movingPoint;
+  while( movingFile.eof() );
+    {
+    movingPointContainer->InsertElement( pointId, movingPoint );
+    movingFile >> movingPoint;
+    }
+
+
+
+//-----------------------------------------------------------
+// Set up  the Metric
+//-----------------------------------------------------------
+  typedef itk::IterativeClosestPointMetric<  
+                                    PointSetType, 
+                                    PointSetType>
+                                                    MetricType;
+
+  typedef MetricType::TransformType                 TransformBaseType;
+  typedef TransformBaseType::ParametersType         ParametersType;
+  typedef TransformBaseType::JacobianType           JacobianType;
+
+  MetricType::Pointer  metric = MetricType::New();
+
+
+//-----------------------------------------------------------
+// Set up a Transform
+//-----------------------------------------------------------
+
+  typedef itk::TranslationTransform< double, 2 >      TransformType;
+
+  TransformType::Pointer transform = TransformType::New();
+
+
+  // Optimizer Type
+  typedef itk::LevenbergMarquardtOptimizer OptimizerType;
+
+  OptimizerType::Pointer      optimizer     = OptimizerType::New();
+  optimizer->SetUseCostFunctionGradient(false);
+
+  // Registration Method
+  typedef itk::PointSetToPointSetRegistrationMethod< 
+                                            PointSetType, 
+                                            PointSetType >
+                                                    RegistrationType;
+
+
+  RegistrationType::Pointer   registration  = RegistrationType::New();
+
+  // Scale the translation components of the Transform in the Optimizer
+  OptimizerType::ScalesType scales( transform->GetNumberOfParameters() );
+  scales.Fill( 1.0 );
+
+  
+  unsigned long   numberOfIterations =   100;
+  double          gradientTolerance  =  1e-1; // convergence criterion
+  double          valueTolerance =  1e-1; // convergence criterion
+  double          epsilonFunction =  1e-9; // convergence criterion
+
+
+  optimizer->SetScales( scales );
+  optimizer->SetNumberOfIterations( numberOfIterations );
+  optimizer->SetValueTolerance(valueTolerance);
+  optimizer->SetGradientTolerance(gradientTolerance);
+  optimizer->SetEpsilonFunction(epsilonFunction);
+
+  // Start from an Identity transform (in a normal case, the user 
+  // can probably provide a better guess than the identity...
+  transform->SetIdentity();
+
+  registration->SetInitialTransformParameters( transform->GetParameters() );
+
+  //------------------------------------------------------
+  // Connect all the components required for Registration
+  //------------------------------------------------------
+  registration->SetMetric(        metric        );
+  registration->SetOptimizer(     optimizer     );
+  registration->SetTransform(     transform     );
+  registration->SetFixedPointSet( fixedPointSet );
+  registration->SetMovingPointSet(   movingPointSet   );
+
+
+//------------------------------------------------------------
+// Set up transform parameters
+//------------------------------------------------------------
+  ParametersType parameters( transform->GetNumberOfParameters() );
+
+  // initialize the offset/vector part
+  for( unsigned int k = 0; k < 2; k++ )
+    {
+    parameters[k]= 10.0;
+    }
+
+  transform->SetParameters( parameters );
+  registration->SetInitialTransformParameters( transform->GetParameters() );
+
+  try 
+    {
+    registration->StartRegistration();
+    }
+  catch( itk::ExceptionObject & e )
+    {
+    std::cout << e << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  std::cout << "Solution = " << transform->GetParameters() << std::endl;
+
+
+
+
+// Software Guide : EndCodeSnippet
+
+
+  return EXIT_SUCCESS;
+
+}
+
