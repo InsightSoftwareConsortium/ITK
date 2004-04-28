c1d6bd29989c5633bb911f67eb28969789c25553
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2004-04-28 15:13:00 -0400

    NEW: Adding gdcm dicom library into ITk, very alpha for now

---
+++ Code/IO/itkGDCMImageIO.cxx
@@ -0,0 +1,334 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkGDCMImageIO.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+  Portions of this code are covered under the VTK copyright.
+  See VTKCopyright.txt or http://www.kitware.com/VTKCopyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#include "itkGDCMImageIO.h"
+#include "gdcmFile.h"
+#include "gdcmHeaderHelper.h"
+#include <fstream>
+
+namespace itk
+{
+
+GDCMImageIO::GDCMImageIO()
+{
+//  this->SetNumberOfDimensions(2); //default
+  m_ByteOrder = LittleEndian; //default
+  m_FileType = Binary;  //default...always true
+  m_RescaleSlope = 1.0;
+  m_RescaleIntercept = 0.0;
+}
+
+GDCMImageIO::~GDCMImageIO()
+{
+}
+
+bool GDCMImageIO::OpenGDCMFileForReading(std::ifstream& os, 
+                                         const char* filename)
+                                       
+{
+  // Make sure that we have a file to 
+  if ( filename == "" )
+    {
+    itkExceptionMacro(<<"A FileName must be specified.");
+    return false;
+    }
+
+  // Close file from any previous image
+  if ( os.is_open() )
+    {
+    os.close();
+    }
+  
+  // Open the new file for reading
+  itkDebugMacro(<< "Initialize: opening file " << filename);
+
+  // Actually open the file
+  os.open( filename, std::ios::in | std::ios::binary );
+
+  if ( os.fail() )
+    {
+    return false;
+    }
+
+  return true;
+}
+
+
+bool GDCMImageIO::OpenGDCMFileForWriting(std::ofstream& os, 
+                                         const char* filename)
+                                       
+{
+  // Make sure that we have a file to 
+  if ( filename == "" )
+    {
+    itkExceptionMacro(<<"A FileName must be specified.");
+    return false;
+    }
+
+  // Close file from any previous image
+  if ( os.is_open() )
+    {
+    os.close();
+    }
+  
+  // Open the new file for writing
+  itkDebugMacro(<< "Initialize: opening file " << filename);
+
+#ifdef __sgi
+  // Create the file. This is required on some older sgi's
+  std::ofstream tFile(filename,std::ios::out);
+  tFile.close();                    
+#endif
+
+  // Actually open the file
+  os.open( filename, std::ios::out | std::ios::binary );
+
+  if( os.fail() )
+    {
+    itkExceptionMacro(<< "Could not open file for writing: " << filename);
+    return false;
+    }
+
+
+  return true;
+}
+
+// This method will only test if the header looks like a
+//GDCM image file.
+bool GDCMImageIO::CanReadFile(const char* filename) 
+{ 
+  std::ifstream file;
+  char buffer[256];
+  std::string fname(filename);
+
+  if(  fname == "" )
+    {
+    itkDebugMacro(<<"No filename specified.");
+    return false;
+    }
+
+  bool extensionFound = false;
+  std::string::size_type sprPos = fname.rfind(".dcm");  //acr nema ??
+  if ((sprPos != std::string::npos)
+      && (sprPos == fname.length() - 4))
+    {
+    extensionFound = true;
+    }
+
+  if( !extensionFound )
+    {
+    itkDebugMacro(<<"The filename extension is not recognized");
+    return false;
+    }
+
+  //Check for file existence:
+  if ( ! this->OpenGDCMFileForReading(file, filename))
+    {
+    return false;
+    }
+
+  // Check to see if its a valid dicom file gdcm is able to parse:
+  //We are parsing the header one time here:
+  gdcmHeaderHelper GdcmHeader(filename);
+  if (!GdcmHeader.IsReadable())
+    {
+    itkExceptionMacro("Gdcm cannot parse file " << filename );
+    return false;
+    }
+
+  //For later : For 2d stack dicom:
+  //we should not mix insoncistent on pixel type
+  //GdcmHeader.GetPixelType();
+  //we should not mix insoncistent on image size
+  //GdcmHeader.GetXSize(); GdcmHeader.GetYSize(); GdcmHeader.GetZSize();
+  //spacing:
+  //GdcmHeader.GetXSpacing(); GdcmHeader.GetYSpacing(); ...
+  //GdcmHeader.GetXOrigin() ...
+
+  return true;
+}
+
+void GDCMImageIO::Read(void* buffer)
+{
+  std::ifstream file;
+
+  //read header information file:
+  this->InternalReadImageInformation(file);
+  
+  //Should I handle differently dicom lut ?
+  //GdcmHeader.HasLUT()
+
+  if( !this->ReadBufferAsBinary( file, buffer, this->GetImageSizeInBytes()) )
+    {
+    itkExceptionMacro(<<"Read failed: Wanted " 
+                      << this->GetImageSizeInBytes()
+                      << " bytes, but read " 
+                      << file.gcount() << " bytes.");
+    }
+
+  gdcmFile GdcmFile(m_FileName);
+  size_t size = GdcmFile.GetImageDataSize();
+  //== this->GetImageSizeInComponents()
+  unsigned char *Source = (unsigned char*)GdcmFile.GetImageData();
+  memcpy((void*)buffer, (void*)Source, size);
+
+  //closing files:
+  file.close();
+}
+
+void GDCMImageIO::InternalReadImageInformation(std::ifstream& file)
+{
+  //read header
+  if ( ! this->OpenGDCMFileForReading(file, m_FileName.c_str()) )
+    {
+    itkExceptionMacro(<< "Cannot read requested file");
+    }
+
+  gdcmHeaderHelper GdcmHeader(m_FileName.c_str());
+
+  // We don't need to positionate the Endian related stuff (by using
+  // this->SetDataByteOrderToBigEndian() or SetDataByteOrderToLittleEndian()
+  // since the reading of the file is done by gdcm.
+  // But we do need to set up the data type for downstream filters:
+
+  std::string type = GdcmHeader.GetPixelType();
+  if( type == "8U")
+    {
+    SetPixelType(UCHAR);
+    SetComponentType(UCHAR);
+    }
+  else if( type == "8S")
+    {
+    SetPixelType(CHAR);
+    SetComponentType(CHAR);
+    }
+  else if( type == "16U")
+    {
+    SetPixelType(USHORT);
+    SetComponentType(USHORT);
+    }
+  else if( type == "16S")
+    {
+    SetPixelType(SHORT);
+    SetComponentType(SHORT);
+    }
+  else if( type == "32U")
+    {
+    SetPixelType(UINT);
+    SetComponentType(UINT);
+    }
+  else if( type == "32S")
+    {
+    SetPixelType(INT);
+    SetComponentType(INT);
+    }
+  else if ( type == "FD" )
+    {
+    //64 bits Double image
+    SetPixelType(DOUBLE);
+    SetComponentType(DOUBLE);
+    }
+  else
+    {
+    itkExceptionMacro(<<"Unrecognized type:" << type);
+    }
+
+  // set values in case we don't find them
+  this->SetNumberOfDimensions(2);
+  m_Dimensions[0] = GdcmHeader.GetXSize();
+  m_Dimensions[1] = GdcmHeader.GetYSize();
+  //m_Dimensions[2] = GdcmHeader.GetZSize();
+  m_Dimensions[2] = 0;
+
+  m_Spacing[0] = GdcmHeader.GetXSpacing();
+  m_Spacing[1] = GdcmHeader.GetYSpacing();
+  //  m_Spacing[2] = GdcmHeader.GetZSpacing();
+  m_Spacing[2] = 0;
+
+  m_Origin[0] = GdcmHeader.GetXOrigin();
+  m_Origin[1] = GdcmHeader.GetYOrigin();
+  m_Origin[2] = GdcmHeader.GetZOrigin();
+  m_Origin[2] = 0;
+  
+  //For grayscale image :
+  m_RescaleSlope = GdcmHeader.GetRescaleSlope();
+  m_RescaleIntercept = GdcmHeader.GetRescaleIntercept();
+  
+}
+
+void GDCMImageIO::ReadImageInformation()
+{
+  std::ifstream file;
+  this->InternalReadImageInformation(file);
+}
+
+
+bool GDCMImageIO::CanWriteFile( const char* name )
+{
+  std::string filename = name;
+
+  if(  filename == "" )
+    {
+    itkDebugMacro(<<"No filename specified.");
+    return false;
+    }
+
+  bool extensionFound = false;
+  std::string::size_type sprPos = filename.rfind(".dcm");
+  if ((sprPos != std::string::npos)
+      && (sprPos == filename.length() - 4))
+    {
+    extensionFound = true;
+    }
+
+  if( !extensionFound )
+    {
+    itkDebugMacro(<<"The filename extension is not recognized");
+    return false;
+    }
+
+  return true;
+}
+
+void GDCMImageIO::WriteImageInformation() 
+{
+}
+
+void GDCMImageIO::Write(const void* buffer)
+{
+  std::ofstream file;
+  if ( ! this->OpenGDCMFileForWriting(file, m_FileName.c_str()) )
+    {
+    return;
+    }
+
+  //gdcmFile GdcmFile();
+  //GdcmFile.WriteDcmExplVR(m_FileName);
+  //GdcmFile.WriteDcmImplVR(m_FileName);
+  //GdcmFile.WriteRawData(m_FileName);
+  file.close();
+}
+
+void GDCMImageIO::PrintSelf(std::ostream& os, Indent indent) const
+{
+  Superclass::PrintSelf(os, indent);
+}
+
+
+} // end namespace itk
---
+++ Code/IO/itkGDCMImageIO.h
@@ -0,0 +1,103 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkGDCMImageIO.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+  Portions of this code are covered under the VTK copyright.
+  See VTKCopyright.txt or http://www.kitware.com/VTKCopyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkGDCMImageIO_h
+#define __itkGDCMImageIO_h
+
+#include "itkImageIOBase.h"
+
+namespace itk
+{
+
+/** \brief ImageIO class for reading DICOM v3 and ACR/NEMA images
+ *  This class is only an adaptor to the gdcm library:
+ *  
+ *  http://creatis-www.insa-lyon.fr/Public/Gdcm/
+ *
+ *  CREATIS INSA - Lyon 2004
+ *    http://www.creatis.insa-lyon.fr
+ *
+ * \ingroup IOFilters
+ *
+ */
+class ITK_EXPORT GDCMImageIO : public ImageIOBase
+{
+public:
+  /** Standard class typedefs. */
+  typedef GDCMImageIO Self;
+  typedef ImageIOBase Superclass;
+  typedef SmartPointer<Self>  Pointer;
+  
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(GDCMImageIO, Superclass);
+
+  /*-------- This part of the interface deals with reading data. ------ */
+
+  /** Determine the file type. Returns true if this ImageIO can read the
+   * file specified. */
+  virtual bool CanReadFile(const char*);
+  
+  /** Set the spacing and dimesion information for the current filename. */
+  virtual void ReadImageInformation();
+  
+  /** Reads the data from disk into the memory buffer provided. */
+  virtual void Read(void* buffer);
+
+  /*-------- This part of the interfaces deals with writing data. ----- */
+
+  /** Determine the file type. Returns true if this ImageIO can read the
+   * file specified. */
+  virtual bool CanWriteFile(const char*);
+
+  /** Writes the spacing and dimentions of the image.
+   * Assumes SetFileName has been called with a valid file name. */
+  virtual void WriteImageInformation();
+  
+  /** Writes the data to disk from the memory buffer provided. Make sure
+   * that the IORegion has been set properly. */
+  virtual void Write(const void* buffer);
+  
+  // Useful for rescaling graylevel, 
+  // TODO: temporary solution
+  itkGetMacro(RescaleSlope, double);
+  itkGetMacro(RescaleIntercept, double);
+
+protected:
+  GDCMImageIO();
+  ~GDCMImageIO();
+  void PrintSelf(std::ostream& os, Indent indent) const;
+
+  bool OpenGDCMFileForReading(std::ifstream& os, const char* filename);
+  bool OpenGDCMFileForWriting(std::ofstream& os, const char* filename);
+  void InternalReadImageInformation(std::ifstream& file);
+
+  double m_RescaleSlope;
+  double m_RescaleIntercept;
+
+private:
+  GDCMImageIO(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+};
+
+} // end namespace itk
+
+#endif // __itkGDCMImageIO_h
