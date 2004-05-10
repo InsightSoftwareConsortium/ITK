dc7041e1247b04e96bd2c6a8a2b0142f336a1786
Luis Ibanez <luis.ibanez@kitware.com>
2004-05-10 10:56:29 -0400

    ENH: Parameters fine tunned for the rigid transform. Translation and Rotation      should use different scalings.

--- Examples/Registration/IterativeClosestPoint2.cxx
+++ Examples/Registration/IterativeClosestPoint2.cxx
@@ -151,8 +151,16 @@ int main(int argc, char * argv[] )
 
   // Scale the translation components of the Transform in the Optimizer
   OptimizerType::ScalesType scales( transform->GetNumberOfParameters() );
-  scales.Fill( 0.001 );
 
+  const double translationScale = 1000.0;   // dynamic range of translations
+  const double rotationScale    =    1.0;   // dynamic range of rotations
+
+  scales[0] = 1.0 / rotationScale;
+  scales[1] = 1.0 / rotationScale;
+  scales[2] = 1.0 / rotationScale;
+  scales[3] = 1.0 / translationScale; 
+  scales[4] = 1.0 / translationScale; 
+  scales[5] = 1.0 / translationScale;
   
   unsigned long   numberOfIterations =  2000;
   double          gradientTolerance  =  1e-4;   // convergence criterion
