8b5b6f8324bfa706dffff3d50b7f54a9acba0488
Brad King <brad.king@kitware.com>
2004-05-27 11:55:12 -0400

    ERR: Fix for CVS version of vxl.

--- Code/Common/itkSimplexMeshGeometry.cxx
+++ Code/Common/itkSimplexMeshGeometry.cxx
@@ -18,6 +18,12 @@
 #include "itkSimplexMeshGeometry.h"
 #include "itkNumericTraits.h"
 
+#include <vxl_version.h>
+#if VXL_VERSION_DATE_FULL > 20040406
+# include <vnl/vnl_cross.h>
+# define cross_3d vnl_cross_3d
+#endif
+
 namespace itk
 {
   
