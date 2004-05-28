a1e38122997ea6a139788d64beb524e5e6ff5039
Luis Ibanez <luis.ibanez@kitware.com>
2004-05-28 10:14:04 -0400

    ENH: import was missing for argv. some prints added.

--- Examples/DataRepresentation/Image/ImageToArray.py
+++ Examples/DataRepresentation/Image/ImageToArray.py
@@ -1,5 +1,6 @@
 from InsightToolkit import *
 from numarray import *
+from sys import argv
 
 reader = itkImageFileReaderUC2_New()
 
@@ -9,11 +10,15 @@ reader.SetFileName( argv[1] )
 
 reader.Update()
 
+print "ready to convert image into array"
+
 buffer = connector.GetArrayFromImage( reader.GetOutput() )
 
 writer = itkImageFileWriterUC2_New()
 
 writer.SetFileName( argv[2] )
+
+print "ready to convert array into image"
 
 writer.SetInput( connector.GetImageFromArray( buffer ) )
 
