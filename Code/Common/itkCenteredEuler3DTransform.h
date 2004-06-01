850a8c4839ed74fb394bb86f23fd7a13f656c6c3
John Hipwell <john.hipwell@kcl.ac.uk>
2004-06-01 06:23:31 -0400

    ENH: New 3D Euler transform derived class with center of rotation.

---
+++ Code/Common/itkCenteredEuler3DTransform.h
@@ -0,0 +1,180 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkCenteredEuler3DTransform.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkCenteredEuler3DTransform_h
+#define __itkCenteredEuler3DTransform_h
+
+#include <iostream>
+#include "itkEuler3DTransform.h"
+#include "itkExceptionObject.h"
+#include "vnl/vnl_quaternion.h"
+#include "itkMatrix.h"
+#include "itkVersor.h"
+
+namespace itk
+{
+
+/** \brief CenteredEuler3DTransform of a vector space (e.g. space coordinates)
+ *
+ * This transform applies a rotation about a specific coordinate or
+ * centre of rotation followed by a translation.
+ *
+ * \ingroup Transforms
+ */
+template < class TScalarType=double >    // Data type for scalars (float or double)
+class ITK_EXPORT CenteredEuler3DTransform : 
+        public Euler3DTransform< TScalarType >
+{
+public:
+  /** Standard class typedefs. */
+  typedef CenteredEuler3DTransform Self;
+  typedef Euler3DTransform< TScalarType > Superclass;
+  typedef SmartPointer<Self>        Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+  
+  /** Run-time type information (and related methods). */
+  itkTypeMacro( CenteredEuler3DTransform, Euler3DTransform );
+
+  /** New macro for creation of through a Smart Pointer */
+  itkNewMacro( Self );
+
+  /** Dimension of parameters. */
+  enum { SpaceDimension = 3, 
+         ParametersDimension = 6 };
+
+  /** Scalar type. */
+  typedef typename Superclass::ScalarType  ScalarType;
+
+  /** Parameters type. */
+  typedef typename Superclass::ParametersType  ParametersType;
+
+  /** Jacobian type. */
+  typedef typename Superclass::JacobianType  JacobianType;
+
+  /** VnlQuaternion type. */
+  typedef typename Superclass::VnlQuaternionType  VnlQuaternionType;
+
+  /** Versor type. */
+  typedef typename Superclass::VersorType  VersorType;
+  typedef typename VersorType::VectorType  AxisType;
+  typedef typename VersorType::ValueType   AngleType;
+  
+  /** Offset type. */
+  typedef typename Superclass::OffsetType  OffsetType;
+
+  /** Point type. */
+  typedef typename Superclass::InputPointType   InputPointType;
+  typedef typename Superclass::OutputPointType  OutputPointType;
+  
+  /** Vector type. */
+  typedef typename Superclass::InputVectorType   InputVectorType;
+  typedef typename Superclass::OutputVectorType  OutputVectorType;
+  
+  /** CovariantVector type. */
+  typedef typename Superclass::InputCovariantVectorType   InputCovariantVectorType;
+  typedef typename Superclass::OutputCovariantVectorType  OutputCovariantVectorType;
+  
+  /** VnlVector type. */
+  typedef typename Superclass::InputVnlVectorType   InputVnlVectorType;
+  typedef typename Superclass::OutputVnlVectorType  OutputVnlVectorType;
+  
+  /** Set the transformation from a container of parameters
+   * This is typically used by optimizers.
+   * There are six parameters. The first three represent the
+   * rotation and the last three represent the translation. */
+  void SetParameters( const ParametersType & parameters );
+
+  /** Get the parameters that uniquely define the transform
+   * This is typically used by optimizers.
+   * There are six parameters. The first three represent the
+   * rotation and the last three represent the translation. */
+  const ParametersType & GetParameters( void ) const;
+
+  /** Set and Get the center of rotation */
+  void SetCenter( const InputPointType & center );
+  itkGetConstReferenceMacro( Center, InputPointType );
+
+  /** Set and Get the Translation to be applied after rotation */
+  void SetTranslation( const OutputVectorType & translation );
+  itkGetConstReferenceMacro( Translation, OutputVectorType );
+
+  /**
+   * Back transform by an affine transformation
+   *
+   * This method finds the point or vector that maps to a given
+   * point or vector under the affine transformation defined by
+   * self.  If no such point exists, an exception is thrown.
+   **/
+  inline InputPointType      BackTransform(const OutputPointType  &point ) const;
+  inline InputVectorType     BackTransform(const OutputVectorType &vector) const;
+  inline InputVnlVectorType  BackTransform(const OutputVnlVectorType &vector) const;
+
+  inline InputCovariantVectorType BackTransform(
+                                     const OutputCovariantVectorType &vector) const;
+
+  /**
+   * Find inverse of an affine transformation
+   *
+   * This method creates and returns a new CenteredEuler3DTransform object
+   * which is the inverse of self.  If self is not invertible,
+   * an exception is thrown.
+   **/
+  Pointer Inverse( void ) const;
+
+  /** Set the parameters to the IdentityTransform */
+  virtual void SetIdentity(void);
+
+protected:
+  CenteredEuler3DTransform();
+  ~CenteredEuler3DTransform();
+
+  
+  /**
+   * Print contents of an CenteredEuler3DTransform
+   **/
+  void PrintSelf(std::ostream &os, Indent indent) const;
+
+
+  /** Compute the components of the rotation matrix in the superclass. */
+  virtual void ComputeMatrix(void);
+
+private:
+  CenteredEuler3DTransform(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+  /**
+   * Transformation translation (applied after rotation).
+   * Not to be confused with member m_Offset in base class
+   * Rigid3DTransform. m_Offset is recomputed to take into account
+   * translation to the centre of rotation pre- (negative) and post-
+   * (positive) rotation, and the subsequent translation m_Translation.
+   **/
+  OutputVectorType        m_Translation;   
+
+  // The center of rotation coordinate 
+  InputPointType          m_Center;   
+
+}; //class CenteredEuler3DTransform
+
+
+}  // namespace itk
+
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkCenteredEuler3DTransform.txx"
+#endif
+
+#endif /* __itkCenteredEuler3DTransform_h */
---
+++ Code/Common/itkCenteredEuler3DTransform.txx
@@ -0,0 +1,223 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkCenteredEuler3DTransform.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef _itkCenteredEuler3DTransform_txx
+#define _itkCenteredEuler3DTransform_txx
+
+#include "itkCenteredEuler3DTransform.h"
+
+
+namespace itk
+{
+
+// Constructor with default arguments
+template<class TScalarType>
+CenteredEuler3DTransform<TScalarType>::
+CenteredEuler3DTransform()
+{
+  m_Center.Fill( 0 );
+  m_Translation.Fill( 0 );
+}
+ 
+// Destructor
+template<class TScalarType>
+CenteredEuler3DTransform<TScalarType>::
+~CenteredEuler3DTransform()
+{
+}
+
+
+// Set the center of rotation and recompute the matrix (and the offset
+// or translation relative to [0,0,0]).
+template <class TScalarType>
+void
+CenteredEuler3DTransform<TScalarType>
+::SetCenter( const InputPointType & center )
+{
+  m_Center = center;
+  this->ComputeMatrix();
+}
+
+
+// Set the translation of the centre of rotation and recompute the
+// matrix and the corresponding offset or translation relative to [0,0,0].
+template <class TScalarType>
+void
+CenteredEuler3DTransform<TScalarType>
+::SetTranslation( const OutputVectorType & translation )
+{
+  m_Translation = translation;
+  this->ComputeMatrix();
+}
+
+
+
+//
+// Set Parameters
+// 
+// Parameters are ordered as:
+//
+// p[0:2] = rotations about x, y and z axes
+// p[3:5} = translation components
+//
+//
+template <class TScalarType>
+void
+CenteredEuler3DTransform<TScalarType>
+::SetParameters( const ParametersType & parameters )
+{
+  itkDebugMacro( << "Setting paramaters " << parameters );
+
+  // Set the translations
+  m_Translation[0] = parameters[3];
+  m_Translation[1] = parameters[4];
+  m_Translation[2] = parameters[5];
+
+  // Set the rotations (NB. This function will call the over-ridden
+  // ComputeMatrix() function)
+  this->SetRotation(parameters[0], parameters[1], parameters[2]);
+
+  itkDebugMacro(<<"After setting parameters ");
+}
+
+//
+// Get Parameters
+// 
+// Parameters are ordered as:
+//
+// p[0:2] = rotations about x, y and z axes
+// p[3:5} = translation components
+//
+
+template <class TScalarType>
+const typename CenteredEuler3DTransform<TScalarType>::ParametersType &
+CenteredEuler3DTransform<TScalarType>
+::GetParameters( void ) const
+{
+  ParametersType parameters = Superclass::GetParameters();
+
+  m_Parameters[0] = parameters[0];
+  m_Parameters[1] = parameters[1];
+  m_Parameters[2] = parameters[2];
+
+  for( unsigned int i=0; i < SpaceDimension; i++ )
+    {
+    m_Parameters[i+3] = m_Translation[i];
+    }
+
+  return m_Parameters;
+}
+
+
+// Print self
+template<class TScalarType>
+void
+CenteredEuler3DTransform<TScalarType>::
+PrintSelf(std::ostream &os, Indent indent) const
+{
+
+  Superclass::PrintSelf(os,indent);
+  
+  os << indent << "Center of Rotation: " << m_Center        << std::endl;
+  os << indent << "Translation: "        << m_Translation   << std::endl;
+}
+
+
+// Create and return an inverse transformation
+template<class TScalarType>
+typename CenteredEuler3DTransform<TScalarType>::Pointer
+CenteredEuler3DTransform<TScalarType>::
+Inverse( void ) const
+{
+  Pointer result = New();
+  
+  result->SetMatrix( this->GetMatrix() );
+  result->SetOffset( this->GetOffset() );
+  result->Superclass::Inverse();
+  
+  result->m_Center      =   m_Center;
+  result->m_Translation =  -m_Translation;
+  
+  result->ComputeMatrix();
+
+  return result;
+}
+
+
+  
+// Compute the Jacobian in one position 
+template<class TScalarType >
+void
+CenteredEuler3DTransform< TScalarType >::
+SetIdentity( void ) 
+{
+  this->Superclass::SetIdentity();
+  m_Center.Fill( 0.0 );
+  m_Translation.Fill( 0.0 );
+  this->ComputeMatrix();
+  this->Modified();  
+}
+ 
+
+// Compute the matrix
+template <class TScalarType>
+void
+CenteredEuler3DTransform<TScalarType>
+::ComputeMatrix( void )
+{
+  Superclass::ComputeMatrix();
+
+  ParametersType parameters = this->GetParameters();
+
+  const double cx = cos(parameters[0]);
+  const double sx = sin(parameters[0]);
+  const double cy = cos(parameters[1]);
+  const double sy = sin(parameters[1]); 
+  const double cz = cos(parameters[2]);
+  const double sz = sin(parameters[2]);
+
+  const double ox = m_Center[0];
+  const double oy = m_Center[1];
+  const double oz = m_Center[2];
+
+  const double tx = m_Translation[0];
+  const double ty = m_Translation[1];
+  const double tz = m_Translation[2];
+
+  OffsetType          offset;   
+
+  /** Recompute the offset according to the centre of rotation and
+    * translation */
+  if(this->GetComputeZYX())
+    {
+    offset[0] = cz*cy*(tx - ox) + (-sz*cx + cz*sy*sx)*(ty - oy) + ( sz*sx + cz*sy*cx)*(tz - oz) + ox;
+    offset[1] = sz*cy*(tx - ox) + ( cz*cx + sz*sy*sx)*(ty - oy) + (-cz*sx + sz*sy*cx)*(tz - oz) + oy;
+    offset[2] =                               -sy*(tx - ox) + cy*sx*(ty - oy) + cy*cx*(tz - oz) + oz;
+    }
+  else
+    {
+    offset[0] = (cz*cy - sz*sy*sx)*(tx - ox) - sz*cx*(ty - oy) + (cz*sy + sz*sx*cy)*(tz - oz) + ox;
+    offset[1] = (sz*cy + cz*sy*sx)*(tx - ox) + cz*cx*(ty - oy) + (sz*sy - cz*sx*cy)*(tz - oz) + oy;
+    offset[2] =                             -cx*sy*(tx - ox) + sx*(ty - oy) + cy*cx*(tz - oz) + oz;
+    }
+
+  this->SetOffset( offset );
+  this->Modified();
+}
+
+} // namespace
+
+#endif
