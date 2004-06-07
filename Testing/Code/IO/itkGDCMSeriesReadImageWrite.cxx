4599bd1819f6991b4212eb106c06ea644a5ab8bd
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2004-06-07 14:13:32 -0400

    ENH: Adding testing for the new class itkGDCMSeriesFileNames

---
+++ Testing/Code/IO/itkGDCMSeriesReadImageWrite.cxx
@@ -0,0 +1,118 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkGDCMSeriesReadImageWrite.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+//  Software Guide : BeginLatex
+//
+//  This example illustrates how to read a DICOM series into a volume
+//  and then save this volume into another DICOM series using the 
+//  exact same name.
+//  It makes use of the GDCM library
+//
+//  Software Guide : EndLatex 
+
+
+#include "itkImageSeriesReader.h"
+#include "itkImageFileWriter.h"
+#include "itkImageSeriesWriter.h"
+#include "itkGDCMImageIO.h"
+#include "itkGDCMSeriesFileNames.h"
+#include <vector>
+#include "gdcm.h"
+
+int main( int argc, char* argv[] )
+{
+  if( argc < 3 )
+    {
+    std::cerr << "Usage: " << argv[0] << 
+      " DicomDirectory  outputFile OutputDicomDirectory" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  typedef itk::Image<unsigned short,3>            ImageType;
+  typedef itk::ImageSeriesReader< ImageType >     ReaderType;
+  typedef itk::GDCMImageIO                        ImageIOType;
+  typedef itk::GDCMSeriesFileNames                SeriesFileNames;
+
+  ImageIOType::Pointer gdcmIO = ImageIOType::New();
+  SeriesFileNames::Pointer it = SeriesFileNames::New();
+
+  // Get the DICOM filenames from the directory
+  it->SetInputDirectory( argv[1] );
+
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileNames( it->GetInputFileNames() );
+  reader->SetImageIO( gdcmIO );
+
+  try
+    {
+    reader->Update();
+    }
+  catch (itk::ExceptionObject &excp)
+    {
+    std::cerr << "Exception thrown while writing the image" << std::endl;
+    std::cerr << excp << std::endl;
+
+    return EXIT_FAILURE;
+    }
+
+  typedef itk::ImageFileWriter< ImageType > WriterType;
+  WriterType::Pointer writer = WriterType::New();
+
+  //std::cout  << "Writing the image as " << std::endl << std::endl;
+  //std::cout  << argv[2] << std::endl << std::endl;
+
+  writer->SetFileName( argv[2] );
+  writer->SetInput( reader->GetOutput() );
+
+  try
+    {
+    writer->Update();
+    }
+  catch (itk::ExceptionObject &excp)
+    {
+    std::cerr << "Exception thrown while writing the image" << std::endl;
+    std::cerr << excp << std::endl;
+
+    return EXIT_FAILURE;
+    }
+      
+  // Ok saving as a known format worked now try to save as a serie of DICOM file
+    
+  typedef itk::Image<unsigned short,2>            Image2DType;
+  typedef itk::ImageSeriesWriter< ImageType, Image2DType > SeriesWriterType;
+    
+  SeriesWriterType::Pointer swriter = SeriesWriterType::New();
+  swriter->SetInput( reader->GetOutput() );
+  swriter->SetImageIO( gdcmIO );
+
+  it->SetOutputDirectory( argv[3] );
+  swriter->SetFileNames( it->GetOutputFileNames() );
+
+  // Try to write the serie:
+  try
+    {
+    swriter->Update();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+    std::cerr << "Exception thrown while writing the series " << std::endl;
+    std::cerr << excp << std::endl;
+    }
+
+  return EXIT_SUCCESS;
+}
+
