182ba5a762d450a1bc8195e1681f47d2424dce30
Luis Ibanez <luis.ibanez@kitware.com>
2004-06-11 21:07:02 -0400

    ENH: Temporarily removing the radius creation. The class doesn't seem to      be available at the Java level.

--- Examples/Filtering/MeanImageFilter.java
+++ Examples/Filtering/MeanImageFilter.java
@@ -15,20 +15,13 @@ public class MeanImageFilter
     itkImageFileReaderUS2_Pointer reader = itkImageFileReaderUS2.itkImageFileReaderUS2_New();
     itkImageFileWriterUS2_Pointer writer = itkImageFileWriterUS2.itkImageFileWriterUS2_New();
 
-    itkMeanImageFilterUS2US2_Pointer filter  = itkMeanImageFilterUS2US2.itkMeanImageFilterUS2US2_New();
+    itkMeanImageFilterUS2US2_Pointer filter = itkMeanImageFilterUS2US2.itkMeanImageFilterUS2US2_New();
 
     filter.SetInput( reader.GetOutput() );
     writer.SetInput( filter.GetOutput() );
 
     reader.SetFileName( argv[1] );
     writer.SetFileName( argv[2] );
-
-
-    sizeRadius = itkSize2();
-    sizeRadius.SetElement( 0, Integer.valueOf( argv[3] ) );
-    sizeRadius.SetElement( 1, Integer.valueOf( argv[3] ) );
-
-    filter.SetRadius( sizeRadius );
 
     writer.Update();
   }
