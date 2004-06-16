743f53e70c4b52b5e0ea3c606bcbb9653e36674a
Jim Miller <millerjv@crd.ge.com>
2004-06-16 10:25:55 -0400

    New filter to sharpen an image using a laplacian.

---
+++ Examples/Filtering/LaplacianSharpeningImageFilter.cxx
@@ -0,0 +1,85 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    LaplacianSharpeningImageFilter.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "itkLaplacianSharpeningImageFilter.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkRescaleIntensityImageFilter.h"
+
+int main(int argc, char* argv[])
+{
+  if( argc < 3)
+    {
+    std::cerr << "Usage: " << std::endl;
+    std::cerr << argv[0] << " inputImage outputImage " << std::endl;
+    return -1;
+    }
+   
+  const char * inputFilename  = argv[1];
+  const char * outputFilename = argv[2];
+
+  typedef unsigned char    CharPixelType; 
+  const    unsigned int    Dimension = 2;
+
+  typedef itk::Image<CharPixelType, Dimension>    CharImageType;
+  
+  typedef itk::ImageFileReader< CharImageType >  ReaderType;
+  typedef itk::ImageFileWriter< CharImageType >  WriterType;
+
+  typedef itk::RescaleIntensityImageFilter<CharImageType, CharImageType> RescaleFilter;
+
+  typedef itk::LaplacianSharpeningImageFilter< 
+                              CharImageType, 
+                              CharImageType >    LaplacianSharpeningFilter;
+
+
+  //Setting the IO
+  ReaderType::Pointer reader = ReaderType::New();
+  WriterType::Pointer writer = WriterType::New();
+  RescaleFilter::Pointer rescale = RescaleFilter::New();
+
+  //Setting the ITK pipeline filter
+  
+  LaplacianSharpeningFilter::Pointer lapFilter = LaplacianSharpeningFilter::New();
+  
+  reader->SetFileName( inputFilename  );
+  writer->SetFileName( outputFilename );
+
+  //Sharpen with the laplacian
+  lapFilter->SetInput( reader->GetOutput() );
+
+  // Rescale and cast to unsigned char
+  rescale->SetInput( lapFilter->GetOutput() );
+  writer->SetInput( rescale->GetOutput() );
+
+  rescale->SetOutputMinimum(   0 );
+  rescale->SetOutputMaximum( 255 );
+
+  try
+    {
+    writer->Update();
+    }
+  catch( itk::ExceptionObject & err )
+    { 
+    std::cout << "ExceptionObject caught !" << std::endl; 
+    std::cout << err << std::endl; 
+    return -1;
+    } 
+
+  return 0;
+
+}
