10e7142393a108617e42bc01928d87bcd6b7ea7a
Luis Ibanez <luis.ibanez@kitware.com>
2004-06-23 20:06:20 -0400

    Class intended to compute the coorcurrence matrix from a scalar image was added.      This class was contributed by Zachary Pincus      http://public.kitware.com/pipermail/insight-users/2004-June/008926.html

---
+++ Testing/Code/Numerics/Statistics/itkScalarImageToGreyLevelCooccurrenceMatrixGeneratorTest.cxx
@@ -0,0 +1,245 @@
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+// Insight classes
+#include "itkImage.h"
+#include "itkImageRegionIterator.h"
+
+
+#include "itkScalarImageToGreyLevelCooccurrenceMatrixGenerator.h"
+
+// Un-comment to run this test standalone:
+//int itkScalarImageToGreyLevelCooccurrenceMatrixGeneratorTest(int, char* [] );
+//int main(int c, char * v[])
+//  {
+//  return itkScalarImageToGreyLevelCooccurrenceMatrixGeneratorTest(c, v);
+//  }
+
+int itkScalarImageToGreyLevelCooccurrenceMatrixGeneratorTest(int, char* [] )
+{
+
+  //Data definitions 
+  const unsigned int  IMGWIDTH         =  5;
+  const unsigned int  IMGHEIGHT        =  5;
+  const unsigned int  NDIMENSION       =  2;
+
+
+  //------------------------------------------------------
+  //Create a simple test images
+  //------------------------------------------------------
+  typedef itk::Image<unsigned char, NDIMENSION> InputImageType;
+
+  typedef itk::ImageRegionIterator< InputImageType > InputImageIterator;
+
+   
+  InputImageType::Pointer image = InputImageType::New();
+  
+  InputImageType::SizeType inputImageSize = {{ IMGWIDTH, IMGHEIGHT }};
+
+  InputImageType::IndexType index;
+  index.Fill(0);
+  InputImageType::RegionType region;
+
+  region.SetSize( inputImageSize );
+  region.SetIndex( index );
+
+  //--------------------------------------------------------------------------
+  // Set up the image first. It looks like:
+  //  1 2 1 2 1
+  //  1 2 1 2 1
+  //  1 2 1 2 1
+  //  1 2 1 2 1
+  //  1 2 1 2 1
+  //--------------------------------------------------------------------------
+
+  image->SetRegions( region );
+  image->Allocate();
+
+  // setup the iterator
+  InputImageIterator imageIt( image, image->GetBufferedRegion() );
+
+  for(int i = 0; i < 5; i++)
+    for(int j = 0; j < 5; j++, ++imageIt)
+      {
+      imageIt.Set(j % 2 + 1);
+      }
+
+  
+  //--------------------------------------------------------------------------
+  // Generate the histogram. The un-normalized histogram should look like this:
+  // 
+  //     0 1 2 ...
+  //     -----
+  //  0 |0 0 0
+  //  1 |0 6 9
+  //  2 |0 9 12
+  //  3 |0 0 0
+  //  .
+  //  .
+  // with zeroes elsewhere.
+  //--------------------------------------------------------------------------
+  
+  try {
+  
+  typedef itk::Statistics::ScalarImageToGreyLevelCooccurrenceMatrixGenerator< 
+    InputImageType> GLCMGeneratorType;
+  
+  GLCMGeneratorType::Pointer glcmGen = GLCMGeneratorType::New();
+  
+  glcmGen->SetInput(image);
+  InputImageType::OffsetType offset1 = {{0, 1}};
+  InputImageType::OffsetType offset2 = {{1, 0}};
+  GLCMGeneratorType::OffsetVectorPointer offsetV = 
+  GLCMGeneratorType::OffsetVector::New();
+  offsetV->push_back(offset1);
+  offsetV->push_back(offset2);
+  
+  glcmGen->SetOffsets(offsetV);
+  glcmGen->Compute();
+  GLCMGeneratorType::HistogramPointer hist = glcmGen->GetOutput();
+  
+  //--------------------------------------------------------------------------
+  // Test the histogram.
+  //--------------------------------------------------------------------------
+  bool passed = true;
+  
+  // First make sure the bins are sized properly:
+  
+  float max = hist->GetBinMax(0,255);
+  float min = hist->GetBinMin(0,255);
+  
+  if(max != 256 || min != 255)
+    {
+    std::cerr << "Error" << std::endl;
+    std::cerr << "The calculated bin sizes are incorrect" << std::endl;
+    std::cerr << "Expected [255, 256), got [" << min << ", " << max << ")" << std::endl << std::endl;
+    passed = false;
+    }
+  
+  // Now make sure the contents of the bins are correct:
+  typedef GLCMGeneratorType::HistogramType::IndexType IndexType;
+  IndexType one_one = {{1, 1}}, one_two= {{1, 2}}, two_one= {{2, 1}}, two_two= {{2, 2}};
+  float ooF, otF, toF, ttF, totalF;
+  ooF = hist->GetFrequency(one_one);
+  otF = hist->GetFrequency(one_two);
+  toF = hist->GetFrequency(two_one);
+  ttF = hist->GetFrequency(two_two);
+  totalF = hist->GetTotalFrequency();
+  
+  if( ooF != 6 || ttF != 12 || otF != 9 || toF != 9 || ooF + ttF + otF + toF != totalF)
+    {
+    std::cerr << "Error:" << std::endl;
+    std::cerr << "The histogram was calculated incorrectly" << std::endl;
+    std::cerr << "Expected 6, 12, 9, 9, 36 got " << ooF << ", " << ttF  << ", " <<
+    otF  << ", " << toF  << ", " << totalF << std::endl << std::endl;
+    passed = false;
+    }
+  
+  //--------------------------------------------------------------------------
+  // Test the histogram with normalization
+  //--------------------------------------------------------------------------
+  
+
+  GLCMGeneratorType::Pointer glcmGen0 = GLCMGeneratorType::New();
+  
+  glcmGen0->SetInput(image);
+  glcmGen0->SetOffsets(offsetV);
+  glcmGen0->NormalizeOn();
+  glcmGen0->Compute();
+  GLCMGeneratorType::HistogramPointer hist0 = glcmGen0->GetOutput();
+
+  ooF = hist0->GetFrequency(one_one);
+  otF = hist0->GetFrequency(one_two);
+  toF = hist0->GetFrequency(two_one);
+  ttF = hist0->GetFrequency(two_two);
+  
+  if( (ooF - 6/36.) > 0.001 || (ttF - 12/36.) > 0.001 || (otF - 9/36.) > 0.001 || 
+      (toF - 9/36.) > 0.001 || (ooF + ttF + otF + toF - 1) > 0.001 )
+    {
+    std::cerr << "Error:" << std::endl;
+    std::cerr << "The histogram was calculated incorrectly" << std::endl;
+    std::cerr << "Expected 0.166, 0.333, 0.25, 0.25 got " << ooF << ", " << ttF  << ", " <<
+    otF  << ", " << toF << std::endl << std::endl;
+    passed = false;
+    }
+  
+  
+  //--------------------------------------------------------------------------
+  // Generate some variant histograms and test them
+  //--------------------------------------------------------------------------
+  
+  // First a histogram with 2 bins per axis
+  GLCMGeneratorType::Pointer glcmGen2 = GLCMGeneratorType::New();
+  
+  glcmGen2->SetInput(image);
+  InputImageType::OffsetType offset3 = {{0, 1}};
+
+  glcmGen2->SetOffset(offset3);
+  glcmGen2->SetNumberOfBinsPerAxis( 2 );
+  glcmGen2->Compute();
+  GLCMGeneratorType::HistogramPointer hist2 = glcmGen2->GetOutput();
+  
+  IndexType zero_zero = {{0, 0}};
+  float zzF;
+  zzF = hist2->GetFrequency(zero_zero);
+  totalF = hist2->GetTotalFrequency();
+  
+  if( zzF != 18 || zzF != totalF)
+    {
+    std::cerr << "Error:" << std::endl;
+    std::cerr << "The degenerate histogram was calculated incorrectly" << std::endl;
+    std::cerr << "Expected 18, 18 got " << zzF  << ", " << totalF << std::endl << std::endl;
+    passed = false;
+    }
+  
+  
+  // Next a histogram that has many fewer bins.
+  GLCMGeneratorType::Pointer glcmGen3 = GLCMGeneratorType::New();
+  
+  glcmGen3->SetInput(image);
+  InputImageType::OffsetType offset4 = {{1, 1}};
+  
+  glcmGen3->SetOffset(offset4);
+  
+  glcmGen3->SetPixelValueMinMax(1, 2);
+  glcmGen3->SetNumberOfBinsPerAxis( 2 );
+
+  glcmGen3->Compute();
+  GLCMGeneratorType::HistogramPointer hist3 = glcmGen3->GetOutput();
+
+  IndexType zero_one = {{0, 1}}, one_zero= {{1, 0}};
+  float zoF, ozF;
+  zzF = hist3->GetFrequency(zero_zero);
+  zoF = hist3->GetFrequency(zero_one);
+  ozF = hist3->GetFrequency(one_zero);
+  ooF = hist3->GetFrequency(one_one);
+  totalF = hist3->GetTotalFrequency();
+  
+  if( zzF != 0 || zoF != 9 || ozF != 9 || ooF != 0 || zzF + zoF + ozF + ooF != totalF)
+    {
+    std::cerr << "Error:" << std::endl;
+    std::cerr << "The small size histogram was calculated incorrectly" << std::endl;
+    std::cerr << "Expected 6, 12, 9, 9, 36 got " << zzF << ", " << zoF  << ", " <<
+    ozF  << ", " << ooF  << ", " << totalF << std::endl << std::endl;
+    passed = false;
+    }
+
+  if (!passed)
+    {
+    std::cerr << "Test failed" << std::endl;
+    return EXIT_FAILURE;
+    }
+  else
+    {
+    std::cerr << "Test succeeded" << std::endl;
+    return EXIT_SUCCESS;
+    }
+  
+  } catch( itk::ExceptionObject & err ) { 
+    std::cerr << "ExceptionObject caught !" << std::endl; 
+    std::cerr << err << std::endl; 
+    std::cerr << "Test failed" << std::endl;
+    return EXIT_FAILURE;
+  }
+}
+
--- Testing/Code/Numerics/Statistics/itkStatisticsTests.cxx
+++ Testing/Code/Numerics/Statistics/itkStatisticsTests.cxx
@@ -31,6 +31,7 @@ void RegisterTests()
   REGISTER_TEST(itkSampleSelectiveMeanShiftBlurringFilterTest) ;
   REGISTER_TEST(itkSelectiveSubsampleGeneratorTest) ;
   REGISTER_TEST(itkScalarImageToHistogramGeneratorTest);
+  REGISTER_TEST(itkScalarImageToGreyLevelCooccurrenceMatrixGeneratorTest);
   REGISTER_TEST(itkStatisticsAlgorithmTest);
   REGISTER_TEST(itkSubsampleTest);
   REGISTER_TEST(itkWeightedMeanCalculatorTest);
