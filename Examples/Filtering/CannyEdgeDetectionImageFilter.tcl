620e1b0552fe59e1f0f0c1a454f662559d115b89
Luis Ibanez <luis.ibanez@kitware.com>
2004-07-05 13:08:08 -0400

    ENH: Better selection of types, helps to reduce intermediate casting.

--- Examples/Filtering/CannyEdgeDetectionImageFilter.tcl
+++ Examples/Filtering/CannyEdgeDetectionImageFilter.tcl
@@ -4,9 +4,9 @@
 package require InsightToolkit
 
 set reader [ itkImageFileReaderF2_New ]
-set writer [ itkImageFileWriterUS2_New ]
+set writer [ itkImageFileWriterUC2_New ]
 
-set outputCast [ itkRescaleIntensityImageFilterF2US2_New ]
+set outputCast [ itkRescaleIntensityImageFilterF2UC2_New ]
 
 set filter [ itkCannyEdgeDetectionImageFilterF2F2_New ]
 
@@ -19,12 +19,11 @@ $reader SetFileName [lindex $argv 0]
 $writer SetFileName [lindex $argv 1]
 
 
-$outputCast SetOutputMinimum       0
-$outputCast SetOutputMaximum   65535
+$outputCast SetOutputMinimum     0
+$outputCast SetOutputMaximum   255
 
 set variance   [expr [lindex $argv 2]]
 set threshold  [expr [lindex $argv 3]]
-
 
 $filter SetVariance     $variance
 $filter SetThreshold    $threshold
