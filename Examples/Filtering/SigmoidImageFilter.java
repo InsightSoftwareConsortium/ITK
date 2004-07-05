09b149ca3839329baeed27358d75529a30153357
Luis Ibanez <luis.ibanez@kitware.com>
2004-07-05 12:51:10 -0400

    ENH: Translated from the Python example/test with the same name.

---
+++ Examples/Filtering/SigmoidImageFilter.java
@@ -0,0 +1,36 @@
+/**
+ *  Example on the use of the SigmoidImageFilter
+ *
+ */
+
+import InsightToolkit.*;
+
+public class SigmoidImageFilter
+{
+  public static void main( String argv[] )
+  {
+    System.out.println("SigmoidImageFilter Example");
+
+    itkImageFileReaderUC2_Pointer reader = itkImageFileReaderUC2.itkImageFileReaderUC2_New();
+    itkImageFileWriterUC2_Pointer writer = itkImageFileWriterUC2.itkImageFileWriterUC2_New();
+
+    itkSigmoidImageFilterUC2UC2_Pointer filter = itkSigmoidImageFilterUC2UC2.itkSigmoidImageFilterUC2UC2_New();
+
+    filter.SetInput( reader.GetOutput() );
+    writer.SetInput( filter.GetOutput() );
+
+    reader.SetFileName( argv[0] );
+    writer.SetFileName( argv[1] );
+
+    filter.SetOutputMinimum( Integer.parseInt( argv[2] ) );
+    filter.SetOutputMaximum( Integer.parseInt( argv[3] ) );
+
+    filter.SetAlpha( Float.parseFloat( argv[4] ) );
+    filter.SetBeta(  Float.parseFloat( argv[5] ) );
+
+    writer.Update();
+  }
+
+}
+
+
