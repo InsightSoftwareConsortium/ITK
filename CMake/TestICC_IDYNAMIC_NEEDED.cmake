bfc1ee780faabe520f795d97de9860f5029afa1f
Hans Johnson <hans-johnson@uiowa.edu>
2004-07-06 11:10:33 -0400

    FIX:  Fixed cmake testing for intel compiler -i_dynamic flag based on feedback from Andy Cedilnik.

---
+++ CMake/TestICC_IDYNAMIC_NEEDED.cmake
@@ -0,0 +1,31 @@
+#
+# Check if the system is big endian or little endian
+#
+# VARIABLE - variable to store the result to
+#
+
+MACRO(TEST_ICC_IDYNAMIC_NEEDED VARIABLE LOCAL_TEST_DIR)
+  IF("HAVE_${VARIABLE}" MATCHES "^HAVE_${VARIABLE}$")
+    TRY_RUN(${VARIABLE} HAVE_${VARIABLE}
+      ${CMAKE_BINARY_DIR}
+      ${LOCAL_TEST_DIR}/TestICC_IDYNAMIC_NEEDED.cxx
+      OUTPUT_VARIABLE OUTPUT)
+    MESSAGE(STATUS "Check if using the Intel icc compiler, and if -i_dynamic is needed... COMPILE_RESULT...${HAVE_${VARIABLE}} RUN_RESULT...${VARIABLE}\n")
+    IF(HAVE_${VARIABLE}) #Test compiled, either working intel w/o -i_dynamic, or another compiler
+      IF(${VARIABLE})   #Intel icc compiler, -i_dynamic not needed
+        FILE(APPEND ${CMAKE_BINARY_DIR}/CMakeError.log
+                    "Intel icc compiler found, -i_dynamic not needed")
+        MESSAGE(STATUS "Intel icc compiler found, -i_dynamic not needed")
+      ELSE(${VARIABLE}) #The compiler is not Intel icc
+        FILE(APPEND ${CMAKE_BINARY_DIR}/CMakeError.log
+                    "The compiler is not Intel icc")
+        MESSAGE(STATUS "The compiler is not Intel icc")
+      ENDIF(${VARIABLE})
+    ELSE(HAVE_${VARIABLE})  #Test did not compile, either badly broken compiler, or intel -i_dynamic needed
+      FILE(APPEND ${CMAKE_BINARY_DIR}/CMakeError.log
+        "\tThe -i_dynamic compiler flag is needed for the Intel icc compiler on this platform.\n")
+      MESSAGE("The -i_dynamic compiler flag is needed for the Intel icc compiler on this platform.")
+    ENDIF(HAVE_${VARIABLE})
+    FILE(APPEND ${CMAKE_BINARY_DIR}/CMakeError.log "TestICC_IDYNAMIC_NEEDED produced following output:\n${OUTPUT}\n\n")
+  ENDIF("HAVE_${VARIABLE}" MATCHES "^HAVE_${VARIABLE}$")
+ENDMACRO(TEST_ICC_IDYNAMIC_NEEDED)
