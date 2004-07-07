9ff66dabe965048d96987a3bf8032f7fc3436698
Hans Johnson <hans-johnson@uiowa.edu>
2004-07-07 10:03:31 -0400

    EHN: Syncronized message that is put into CMakeError.txt to the message that is written to the terminal.

--- CMake/TestNO_ICC_IDYNAMIC_NEEDED.cmake
+++ CMake/TestNO_ICC_IDYNAMIC_NEEDED.cmake
@@ -14,16 +14,16 @@ MACRO(TESTNO_ICC_IDYNAMIC_NEEDED VARIABLE LOCAL_TEST_DIR)
     IF(HAVE_${VARIABLE}) #Test compiled, either working intel w/o -i_dynamic, or another compiler
       IF(${VARIABLE})   #Intel icc compiler, -i_dynamic not needed
         FILE(APPEND ${CMAKE_BINARY_DIR}/CMakeError.log
-                    "-i_dynamic not needed, (Not Intel icc, or this version of Intel icc does not conflict with OS glibc.")
-        MESSAGE(STATUS "Intel icc compiler found, -i_dynamic not needed")
+                       "-i_dynamic not needed, (Not Intel icc, or this version of Intel icc does not conflict with OS glibc.")
+        MESSAGE(STATUS "-i_dynamic not needed, (Not Intel icc, or this version of Intel icc does not conflict with OS glibc.")
       ELSE(${VARIABLE}) #The compiler is not Intel icc
-        FILE(APPEND ${CMAKE_BINARY_DIR}/CMakeError.log
-                    "The compiler ERROR--This should never happen")
+        FILE(APPEND ${CMAKE_BINARY_DIR}/CMakeError.log 
+                       "The compiler ERROR--This should never happen")
         MESSAGE(STATUS "The compiler ERROR--This should never happen")
       ENDIF(${VARIABLE})
     ELSE(HAVE_${VARIABLE})  #Test did not compile, either badly broken compiler, or intel -i_dynamic needed
       FILE(APPEND ${CMAKE_BINARY_DIR}/CMakeError.log
-        "\tThe -i_dynamic compiler flag is needed for the Intel icc compiler on this platform.\n")
+            "\tThe -i_dynamic compiler flag is needed for the Intel icc compiler on this platform.\n")
       MESSAGE("The -i_dynamic compiler flag is needed for the Intel icc compiler on this platform.")
     ENDIF(HAVE_${VARIABLE})
     FILE(APPEND ${CMAKE_BINARY_DIR}/CMakeError.log "TestNO_ICC_IDYNAMIC_NEEDED produced following output:\n${OUTPUT}\n\n")
