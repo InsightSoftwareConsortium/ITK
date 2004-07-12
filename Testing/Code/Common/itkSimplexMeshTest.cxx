71a4ffc869dd8d2278e0c4c88542317d53f004cb
Luis Ibanez <luis.ibanez@kitware.com>
2004-07-12 10:19:14 -0400

    FIX: Some content was added to the mesh.

--- Testing/Code/Common/itkSimplexMeshTest.cxx
+++ Testing/Code/Common/itkSimplexMeshTest.cxx
@@ -24,6 +24,7 @@ PURPOSE.  See the above copyright notices for more information.
 #include <time.h>
 
 #include "itkMesh.h"
+#include "itkHexahedronCell.h"
 #include "itkSimplexMesh.h"
 #include "itkSimplexMeshGeometry.h"
 #include "itkDefaultDynamicMeshTraits.h"
@@ -35,30 +36,78 @@ int itkSimplexMeshTest(int , char *[] )
   // Declare the type of the input and output mesh
   typedef itk::DefaultDynamicMeshTraits<double, 3, 3, double, double, double> MeshTraits;
 
-  typedef itk::Mesh<double,3,MeshTraits> TriangleMeshType;
-  typedef itk::SimplexMesh<double,3,MeshTraits> SimplexMeshType;
+  typedef itk::SimplexMesh<double,3,MeshTraits>           SimplexMeshType;
 
+  typedef SimplexMeshType::CellType                       CellInterfaceType;
+  typedef itk::HexahedronCell< CellInterfaceType >        HexaCellType;
 
   SimplexMeshType::Pointer simplexMesh = SimplexMeshType::New();
 
   typedef  SimplexMeshType::NeighborListType              NeighborsListType;
-  NeighborsListType* neighbors = 0;
+
+  NeighborsListType* neighbors;
+
+  /**
+   * Define the 3d geometric positions for 8 points in a cube.
+   */
+  SimplexMeshType::CoordRepType testPointCoords[8][3]
+    = { {0,0,0}, {9,0,0}, {9,0,9}, {0,0,9},
+        {0,9,0}, {9,9,0}, {9,9,9}, {0,9,9} };
   
-  for (int i=0; i < 9; i++)
-    {  
-    itk::TimeProbe * timeProbe = new itk::TimeProbe(); 
-    
-    timeProbe->Start();
-    for (int pointIndex = 0; pointIndex < simplexMesh->GetPoints()->Size(); pointIndex++)
+  /**
+   * List the points that the hexahedron will use from the mesh.
+   */
+  unsigned long hexaPoints[8] = {0,1,2,3,4,5,6,7};
+ 
+
+  /**
+   * Typedef the generic cell type for the mesh.  It is an abstract class,
+   * so we can only use information from it, like get its pointer type.
+   */
+  typedef SimplexMeshType::CellType       CellType;
+  typedef CellType::CellAutoPointer       CellAutoPointer;
+  typedef SimplexMeshType::PointType      PointType;
+
+  /**
+   * Add our test points to the mesh.
+   * simplexMesh->SetPoint(pointId, point)
+   * Note that the constructor for Point is public, and takes an array
+   * of coordinates for the point.
+   */
+  for(int i=0; i < 8 ; ++i)
     {
-      neighbors = simplexMesh->GetNeighbors( pointIndex, i );
+    simplexMesh->SetPoint(i, PointType(testPointCoords[i]));
     }
-    timeProbe->Stop();
+
+  /** 
+   * Specify the method used for allocating cells
+   */
+   simplexMesh->SetCellsAllocationMethod( SimplexMeshType::CellsAllocatedDynamicallyCellByCell );
+
+  /**
+   * Create an hexahedron cell
+   */
+  CellAutoPointer testCell; 
+  testCell.TakeOwnership( new HexaCellType ); // polymorphism
+  testCell->SetPointIds(hexaPoints);
+  simplexMesh->SetCell(1, testCell ); // Internally transfers ownership to the mesh
+
+
+  itk::TimeProbe timeProbe;
+
+  for (int i=0; i < 2; i++)
+    {  
+    timeProbe.Start();
+    for (int pointIndex = 0; pointIndex < simplexMesh->GetPoints()->Size(); pointIndex++)
+      {
+      neighbors = simplexMesh->GetNeighbors( pointIndex, i );
+      }
+    timeProbe.Stop();
     if (neighbors)
       {
       std::cout << "Rigidity: " << i << ", neighbor list size: " << neighbors->size() << std::endl;
       }
-    std::cout << ", Elapsed time (for getting neighbors): " << timeProbe->GetMeanTime() << std::endl;
+    std::cout << ", Elapsed time (for getting neighbors): " << timeProbe.GetMeanTime() << std::endl;
     }
 
     
