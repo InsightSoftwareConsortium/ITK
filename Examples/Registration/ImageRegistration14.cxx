fa78b3fb6a0e6ec1ebd300b3f61051dfd387654a
Luis Ibanez <luis.ibanez@kitware.com>
2004-07-13 18:33:23 -0400

    Example on the use of NormalizedMutualInformation histogram metric.

---
+++ Examples/Registration/ImageRegistration14.cxx
@@ -0,0 +1,291 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    ImageRegistration14.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+// Software Guide : BeginLatex
+//
+//  This example illustrates how to do registration with a 2D Rigid Transform
+//  and with the Normalized Mutual Information metric.
+//
+// Software Guide : EndLatex 
+
+
+// Software Guide : BeginCodeSnippet
+#include "itkImageRegistrationMethod.h"
+
+#include "itkCenteredRigid2DTransform.h"
+#include "itkCenteredTransformInitializer.h"
+
+#include "itkNormalizedMutualInformationHistogramImageToImageMetric.h"
+#include "itkLinearInterpolateImageFunction.h"
+#include "itkRegularStepGradientDescentOptimizer.h"
+#include "itkImage.h"
+
+
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+
+#include "itkResampleImageFilter.h"
+#include "itkCastImageFilter.h"
+
+
+//  The following section of code implements a Command observer
+//  used to monitor the evolution of the registration process.
+//
+#include "itkCommand.h"
+class CommandIterationUpdate : public itk::Command 
+{
+public:
+  typedef  CommandIterationUpdate   Self;
+  typedef  itk::Command             Superclass;
+  typedef itk::SmartPointer<Self>  Pointer;
+  itkNewMacro( Self );
+protected:
+  CommandIterationUpdate() {};
+public:
+  typedef itk::RegularStepGradientDescentOptimizer     OptimizerType;
+  typedef   const OptimizerType   *    OptimizerPointer;
+
+  void Execute(itk::Object *caller, const itk::EventObject & event)
+    {
+      Execute( (const itk::Object *)caller, event);
+    }
+
+  void Execute(const itk::Object * object, const itk::EventObject & event)
+    {
+      OptimizerPointer optimizer = 
+        dynamic_cast< OptimizerPointer >( object );
+      if( typeid( event ) != typeid( itk::IterationEvent ) )
+        {
+        return;
+        }
+      std::cout << optimizer->GetCurrentIteration() << "   ";
+      std::cout << optimizer->GetValue() << "   ";
+      std::cout << optimizer->GetCurrentPosition() << std::endl;
+    }
+};
+
+
+int main( int argc, char *argv[] )
+{
+  if( argc < 3 )
+    {
+    std::cerr << "Missing Parameters " << std::endl;
+    std::cerr << "Usage: " << argv[0];
+    std::cerr << " fixedImageFile  movingImageFile ";
+    std::cerr << "outputImagefile [differenceImage]" << std::endl;
+    return 1;
+    }
+  
+  const    unsigned int    Dimension = 2;
+  typedef  unsigned char   PixelType;
+  
+  typedef itk::Image< PixelType, Dimension >  FixedImageType;
+  typedef itk::Image< PixelType, Dimension >  MovingImageType;
+
+  typedef itk::CenteredRigid2DTransform< double > TransformType;
+
+  typedef itk::RegularStepGradientDescentOptimizer       OptimizerType;
+  typedef itk::LinearInterpolateImageFunction< 
+                                    MovingImageType,
+                                    double             > InterpolatorType;
+  typedef itk::ImageRegistrationMethod< 
+                                    FixedImageType, 
+                                    MovingImageType    > RegistrationType;
+
+
+  typedef itk::NormalizedMutualInformationHistogramImageToImageMetric< 
+                                          FixedImageType, 
+                                          MovingImageType >    MetricType;
+
+
+
+  TransformType::Pointer      transform     = TransformType::New();
+  OptimizerType::Pointer      optimizer     = OptimizerType::New();
+  InterpolatorType::Pointer   interpolator  = InterpolatorType::New();
+  RegistrationType::Pointer   registration  = RegistrationType::New();
+
+  registration->SetOptimizer(     optimizer     );
+  registration->SetTransform(     transform     );
+  registration->SetInterpolator(  interpolator  );
+  
+
+
+  MetricType::Pointer metric = MetricType::New();
+  registration->SetMetric( metric  );
+
+
+  unsigned int numberOfBins = 256;
+  MetricType::HistogramType::SizeType histogramSize;
+  histogramSize[0] = numberOfBins;
+  histogramSize[1] = numberOfBins;
+  metric->SetHistogramSize( histogramSize );
+ 
+
+  const unsigned int numberOfParameters = transform->GetNumberOfParameters();
+
+  typedef MetricType::ScalesType ScalesType;
+  ScalesType scales( numberOfParameters );
+
+  scales.Fill( 1.0 );
+    
+  metric->SetDerivativeStepLengthScales(scales);
+
+
+
+  typedef itk::ImageFileReader< FixedImageType  > FixedImageReaderType;
+  typedef itk::ImageFileReader< MovingImageType > MovingImageReaderType;
+
+  FixedImageReaderType::Pointer  fixedImageReader  = FixedImageReaderType::New();
+  MovingImageReaderType::Pointer movingImageReader = MovingImageReaderType::New();
+
+  fixedImageReader->SetFileName(  argv[1] );
+  movingImageReader->SetFileName( argv[2] );
+
+  registration->SetFixedImage(    fixedImageReader->GetOutput()    );
+  registration->SetMovingImage(   movingImageReader->GetOutput()   );
+
+  fixedImageReader->Update();
+
+  registration->SetFixedImageRegion( 
+       fixedImageReader->GetOutput()->GetBufferedRegion() );
+
+
+  typedef itk::CenteredTransformInitializer< 
+                                    TransformType, 
+                                    FixedImageType, 
+                                    MovingImageType >  TransformInitializerType;
+  TransformInitializerType::Pointer initializer = TransformInitializerType::New();
+
+  initializer->SetTransform(   transform );
+  initializer->SetFixedImage(  fixedImageReader->GetOutput() );
+  initializer->SetMovingImage( movingImageReader->GetOutput() );
+  initializer->GeometryOn();
+  initializer->InitializeTransform();
+
+  transform->SetAngle( 0.0 );
+
+
+  registration->SetInitialTransformParameters( transform->GetParameters() );
+
+  typedef OptimizerType::ScalesType       OptimizerScalesType;
+  OptimizerScalesType optimizerScales( transform->GetNumberOfParameters() );
+
+  const double translationScale = 1.0 / 1000.0;
+
+  optimizerScales[0] = 1.0;
+  optimizerScales[1] = translationScale;
+  optimizerScales[2] = translationScale;
+  optimizerScales[3] = translationScale;
+  optimizerScales[4] = translationScale;
+
+  optimizer->SetScales( optimizerScales );
+
+  optimizer->SetMaximumStepLength( 0.1    ); 
+  optimizer->SetMinimumStepLength( 0.001 );
+  optimizer->SetNumberOfIterations( 400 );
+
+  optimizer->MaximizeOn();
+
+
+  // Create the Command observer and register it with the optimizer.
+  //
+  CommandIterationUpdate::Pointer observer = CommandIterationUpdate::New();
+  optimizer->AddObserver( itk::IterationEvent(), observer );
+
+
+  try 
+    { 
+    registration->StartRegistration(); 
+    } 
+  catch( itk::ExceptionObject & err ) 
+    { 
+    std::cout << "ExceptionObject caught !" << std::endl; 
+    std::cout << err << std::endl; 
+    return -1;
+    } 
+
+  typedef RegistrationType::ParametersType ParametersType;
+
+  ParametersType finalParameters = registration->GetLastTransformParameters();
+  
+  const double finalAngle           = finalParameters[0];
+  const double finalRotationCenterX = finalParameters[1];
+  const double finalRotationCenterY = finalParameters[2];
+  const double finalTranslationX    = finalParameters[3];
+  const double finalTranslationY    = finalParameters[4];
+
+  unsigned int numberOfIterations = optimizer->GetCurrentIteration();
+  
+  double bestValue = optimizer->GetValue();
+
+
+  // Print out results
+  //
+
+  const double finalAngleInDegrees = finalAngle * 45.0 / atan(1.0);
+
+  std::cout << "Result = " << std::endl;
+  std::cout << " Angle (radians) " << finalAngle  << std::endl;
+  std::cout << " Angle (degrees) " << finalAngleInDegrees  << std::endl;
+  std::cout << " Center X      = " << finalRotationCenterX  << std::endl;
+  std::cout << " Center Y      = " << finalRotationCenterY  << std::endl;
+  std::cout << " Translation X = " << finalTranslationX  << std::endl;
+  std::cout << " Translation Y = " << finalTranslationY  << std::endl;
+  std::cout << " Iterations    = " << numberOfIterations << std::endl;
+  std::cout << " Metric value  = " << bestValue          << std::endl;
+
+
+  typedef itk::ResampleImageFilter< 
+                            MovingImageType, 
+                            FixedImageType >    ResampleFilterType;
+
+  TransformType::Pointer finalTransform = TransformType::New();
+
+  finalTransform->SetParameters( finalParameters );
+
+  ResampleFilterType::Pointer resample = ResampleFilterType::New();
+
+  resample->SetTransform( finalTransform );
+  resample->SetInput( movingImageReader->GetOutput() );
+
+  FixedImageType::Pointer fixedImage = fixedImageReader->GetOutput();
+
+  resample->SetSize(    fixedImage->GetLargestPossibleRegion().GetSize() );
+  resample->SetOutputOrigin(  fixedImage->GetOrigin() );
+  resample->SetOutputSpacing( fixedImage->GetSpacing() );
+  resample->SetDefaultPixelValue( 100 );
+
+
+  typedef itk::Image< PixelType, Dimension > OutputImageType;
+
+  typedef itk::ImageFileWriter< OutputImageType >  WriterType;
+
+  WriterType::Pointer      writer =  WriterType::New();
+
+  writer->SetFileName( argv[3] );
+
+  writer->SetInput( resample->GetOutput() );
+  writer->Update();
+
+// Software Guide : EndCodeSnippet
+
+
+
+  return 0;
+}
+
