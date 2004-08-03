fb65b07ad004838ebeee9f91e30effbf977ea686
Luis Ibanez <luis.ibanez@kitware.com>
2004-08-02 22:32:03 -0400

    ENH: Test added for the conversion of a triagle mesh into a simplex mesh.      Changes contributed by Thomas Boettger.

--- Testing/Code/BasicFilters/itkSimplexMeshAdaptTopologyFilterTest.cxx
+++ Testing/Code/BasicFilters/itkSimplexMeshAdaptTopologyFilterTest.cxx
@@ -22,20 +22,53 @@
 #include "itkMesh.h"
 #include "itkSimplexMesh.h"
 #include "itkRegularSphereMeshSource.h"
+#include <itkTriangleMeshToSimplexMeshFilter.h>
 #include "itkSimplexMeshAdaptTopologyFilter.h"
 #include "itkDefaultDynamicMeshTraits.h"
 
 int itkSimplexMeshAdaptTopologyFilterTest( int , char * [] )
 { 
 
-   // Declare the type of the input and output mesh
-  typedef itk::DefaultStaticMeshTraits<double, 3, 3, double, double, double>   MeshTraits;
-  typedef itk::SimplexMesh< double, 3, MeshTraits > MeshType;
- 
-  typedef itk::SimplexMeshAdaptTopologyFilter< MeshType, MeshType > FilterType;
+  // Declare the type of the input and output mesh
+  typedef itk::DefaultDynamicMeshTraits<double, 3, 3,double,double> TriangleMeshTraits;
+  typedef itk::DefaultDynamicMeshTraits<double, 3, 3, double,double> SimplexMeshTraits;
+  typedef itk::Mesh<double,3, TriangleMeshTraits> TriangleMeshType;
+  typedef itk::SimplexMesh<double,3, SimplexMeshTraits> SimplexMeshType;
+
+  // declare triangle mesh source
+  typedef itk::RegularSphereMeshSource<TriangleMeshType>  SphereMeshSourceType;
+  typedef SphereMeshSourceType::PointType PointType;
+  typedef SphereMeshSourceType::VectorType VectorType;
+
+  // declare the triangle to simplex mesh filter
+  typedef itk::TriangleMeshToSimplexMeshFilter<TriangleMeshType, SimplexMeshType> SimplexFilterType;
+
+  SphereMeshSourceType::Pointer  mySphereMeshSource = SphereMeshSourceType::New();
+  PointType center; 
+  center.Fill(10);
+  PointType::ValueType scaleInit[3] = {3,3,3};
+  VectorType scale = scaleInit;
+  
+  mySphereMeshSource->SetCenter(center);
+  mySphereMeshSource->SetResolution(2); 
+  mySphereMeshSource->SetScale(scale);
+
+  std::cout << "Triangle mesh created. " << std::endl;
+  
+  SimplexFilterType::Pointer simplexFilter = SimplexFilterType::New();
+  simplexFilter->SetInput( mySphereMeshSource->GetOutput() );
+  simplexFilter->Update();
+  
+  SimplexMeshType::Pointer simplexMesh = simplexFilter->GetOutput();
+  simplexMesh->DisconnectPipeline();
+  
+  std::cout << "Simplex Mesh: " << simplexMesh << std::endl;
+
+  typedef itk::SimplexMeshAdaptTopologyFilter< SimplexMeshType, SimplexMeshType > FilterType;
 
   FilterType::Pointer filter = FilterType::New();
-
+  filter->SetInput(simplexMesh);
+  filter->Update();
 
   std::cout << "[TEST DONE]" << std::endl;
 
