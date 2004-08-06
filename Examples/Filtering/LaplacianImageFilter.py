5e6eb6b2e8c6d04d0f2a78668013cd7fe42b2f95
Luis Ibanez <luis.ibanez@kitware.com>
2004-08-05 22:29:24 -0400

    ENH: RescaleIntensity filter added for writing the image as Unsigned Char in PNG.

--- Examples/Filtering/LaplacianImageFilter.py
+++ Examples/Filtering/LaplacianImageFilter.py
@@ -7,12 +7,18 @@ from InsightToolkit import *
 from sys import argv
 
 reader = itkImageFileReaderF2_New()
-writer = itkImageFileWriterF2_New()
+writer = itkImageFileWriterUC2_New()
 
 filter  = itkLaplacianImageFilterF2F2_New()
 
+caster  = itkRescaleIntensityImageFilterF2UC2_New()
+
+caster.SetOutputMinimum(   0 );
+caster.SetOutputMaximum( 255 );
+
 filter.SetInput( reader.GetOutput() )
-writer.SetInput( filter.GetOutput() )
+caster.SetInput( filter.GetOutput() )
+writer.SetInput( caster.GetOutput() )
 
 reader.SetFileName( argv[1] )
 writer.SetFileName( argv[2] )
