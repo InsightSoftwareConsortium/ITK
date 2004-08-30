0471837f8d8cc63931c5de440093fb6c44744c60
Julien Jomier <julien.jomier@kitware.com>
2004-08-30 19:13:49 -0400

    FIX: Warning: unused parameter.

--- Code/Common/itkTransform.h
+++ Code/Common/itkTransform.h
@@ -171,7 +171,7 @@ public:
 
   /** Return the inverse of the transform.
    *  The inverse is recomputed if it has been modified */
-  virtual bool GetInverse(Self* inverse) const {return false;}
+  virtual bool GetInverse(Self*) const {return false;}
 
 
 protected:
