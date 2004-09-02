18f6e15019c80e4ccbca1efe746971b3c2b09459
Bill Lorensen <bill.lorensen@gmail.com>
2004-09-02 11:43:53 -0400

    ERR: partial override of method.

--- Code/SpatialObject/itkPolygonSpatialObject.h
+++ Code/SpatialObject/itkPolygonSpatialObject.h
@@ -84,8 +84,16 @@ public:
 
   /** Test whether a point is inside or outside the object. */ 
   virtual bool IsInside( const PointType & point,
-                         unsigned int depth=0,
-                         char * name=NULL) const;
+                         unsigned int depth,
+                         char * name) const;
+
+  /** Test whether a point is inside or outside the object 
+   *  For computational speed purposes, it is faster if the method does not
+   *  check the name of the class and the current depth */ 
+  virtual bool IsInside( const PointType & point) const
+  {
+    return this->IsInside(point, 0, NULL);
+  };
 
 private:
   PolygonGroupOrientation m_Orientation;
