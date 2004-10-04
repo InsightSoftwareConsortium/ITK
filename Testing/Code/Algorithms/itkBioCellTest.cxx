3a555510b75430fe9d1b82b20609e1116129a377
Luis Ibanez <luis.ibanez@kitware.com>
2004-10-04 13:43:02 -0400

    BUG: radius tmp variable was not being used: Borland warned.      A test section for 4D was added.

--- Testing/Code/Algorithms/itkBioCellTest.cxx
+++ Testing/Code/Algorithms/itkBioCellTest.cxx
@@ -41,6 +41,7 @@ int itkBioCellTest( int, char * [] )
    egg->AddForce( forceVector );
 
    const double radius = egg->GetRadius();
+   std::cout << "radius = " << radius << std::endl;
 
    CellType::SetGrowthRadiusLimit( 2.0 );
    CellType::SetGrowthRadiusIncrement( 0.1 );
@@ -60,7 +61,7 @@ int itkBioCellTest( int, char * [] )
 
    }
 
-   // Test for 2D 
+   // Test for 3D 
    {
    std::cout << "Testing 3D " << std::endl;
    typedef itk::bio::Cell<3> CellType;
@@ -77,6 +78,7 @@ int itkBioCellTest( int, char * [] )
    egg->AddForce( forceVector );
 
    const double radius = egg->GetRadius();
+   std::cout << "radius = " << radius << std::endl;
 
    CellType::SetGrowthRadiusLimit( 2.0 );
    CellType::SetGrowthRadiusIncrement( 0.1 );
@@ -97,7 +99,43 @@ int itkBioCellTest( int, char * [] )
    }
 
 
+   // Test for 4D 
+  {
+   std::cout << "Testing 4D " << std::endl;
+   typedef itk::bio::Cell<4> CellType;
+    
+   CellType * egg = CellType::CreateEgg();
 
+   CellType::VectorType forceVector;
+
+   forceVector[0] = 10.5;
+   forceVector[1] = 20.5;
+   forceVector[2] = 30.5;
+   forceVector[3] = 15.5;
+
+   egg->ClearForce();
+   egg->AddForce( forceVector );
+
+   const double radius = egg->GetRadius();
+   std::cout << "radius = " << radius << std::endl;
+
+   CellType::SetGrowthRadiusLimit( 2.0 );
+   CellType::SetGrowthRadiusIncrement( 0.1 );
+   CellType::SetEnergySelfRepairLevel( 1.0 );
+   CellType::SetNutrientSelfRepairLevel( 1.0 );
+
+   CellType::SetGrowthMaximumLatencyTime( 10 );
+   CellType::SetDivisionMaximumLatencyTime( 10 );
+
+   std::cout << "Growth Latency = " << 
+     CellType::GetGrowthMaximumLatencyTime() << std::endl;
+
+   std::cout << "Division Latency = " << 
+     CellType::GetDivisionMaximumLatencyTime() << std::endl;
+
+   delete egg;
+
+   }
 
    std::cout << "Test Passed !" << std::endl;
    return EXIT_SUCCESS;
