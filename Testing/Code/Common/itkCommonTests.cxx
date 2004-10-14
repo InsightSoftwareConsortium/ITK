6cdc8bd43f4af18c7bf83885048384bcf8efdf44
Luis Ibanez <luis.ibanez@kitware.com>
2004-10-14 11:13:13 -0400

    ENH: Added test that was missing for the itkCenteredEuler3DTransformTest.

--- Testing/Code/Common/itkCommonTests.cxx
+++ Testing/Code/Common/itkCommonTests.cxx
@@ -29,6 +29,7 @@ REGISTER_TEST(itkByteSwapTest );
 REGISTER_TEST(itkCenteredRigid2DTransformTest );
 REGISTER_TEST(itkCenteredAffineTransformTest );
 REGISTER_TEST(itkCentralDifferenceImageFunctionTest );
+REGISTER_TEST(itkCenteredEuler3DTransformTest );
 REGISTER_TEST(itkCenteredTransformInitializerTest   );
 REGISTER_TEST(itkCenteredVersorTransformInitializerTest   );
 REGISTER_TEST(itkChainCodePath2DTest );
