07733cccef0288fa3f9ab9d9b7d3a0208287d50d
Julien Jomier <julien.jomier@kitware.com>
2004-10-18 17:19:38 -0400

    COMP: typename not needed

--- Code/BasicFilters/itkInterpolateImagePointsFilter.h
+++ Code/BasicFilters/itkInterpolateImagePointsFilter.h
@@ -69,7 +69,7 @@ namespace itk
 
 template <class TInputImage, 
           class TOutputImage, 
-          class TCoordType = typename TInputImage::PixelType, 
+          class TCoordType = TInputImage::PixelType, 
           class InterpolatorType = BSplineInterpolateImageFunction<TInputImage, TCoordType> > 
 class ITK_EXPORT InterpolateImagePointsFilter : 
     public ImageToImageFilter<TInputImage, TOutputImage>
