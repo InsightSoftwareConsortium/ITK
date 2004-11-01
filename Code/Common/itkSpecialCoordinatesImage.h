845e008039cf1ca2b254244281e27e80f7a1b711
Jim Miller <millerjv@crd.ge.com>
2004-11-01 11:32:46 -0500

    BUG: checked in too soon

--- Code/Common/itkSpecialCoordinatesImage.h
+++ Code/Common/itkSpecialCoordinatesImage.h
@@ -100,7 +100,6 @@ public:
   typedef ImageBase<VImageDimension>  Superclass;
   typedef SmartPointer<Self>  Pointer;
   typedef SmartPointer<const Self>  ConstPointer;
-  typedef WeakPointer<const Self>  ConstWeakPointer;
 
   /** Method for creation through the object factory. */
   itkNewMacro(Self);
