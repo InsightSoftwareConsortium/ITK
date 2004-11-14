bfbe2876624c5847a5a4df5a49b5e8ac6e294c94
Luis Ibanez <luis.ibanez@kitware.com>
2004-11-14 15:19:24 -0500

    BUG: 1349. Example added for illustrating the use of the FFT filters.      It reads an scalar (real) image and computes its Fourier Transfrom,      that produces a complex image. The real and imaginary parts of this      image are extracted and saved as independent scalar images.

---
+++ Examples/Filtering/FFTImageFilter.cxx
@@ -0,0 +1,210 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    FFTImageFilter.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+//  Software Guide : BeginLatex
+//
+//  In this section we assume that you are familiar with Spectral Analysis, in
+//  particular with the concepts of the Fourier Transform and the numerical
+//  implementation of the Fast Fourier transform. If you are not familiar with
+//  these concepts you may want to consult first any of the many available
+//  introductory books to spectral analysis.
+//
+//  This example illustrates how to use the Fast Fourier Transform filter (FFT)
+//  for processing an image in the spectral domain. Given that FFT computation
+//  can be CPU intensive, there are multiple hardware specific impelmentations
+//  of FFT. IT is convenient in many cases to delegate the actual computation
+//  of the transform to local available libraries. Particular examples of those
+//  libraries are fftw and the VXL implementation of FFT. For this reason ITK
+//  provides a base abstract class that factorizes the interface to multiple
+//  specific implementations of FFT. This base class is the
+//  \doxygen{FFTRealToComplexConjugateImageFilter}, and two of its derived
+//  classes are \doxygen{VnlFFTRealToComplexConjugateImageFilter} and
+//  \doxygen{FFTWRealToComplexConjugateImageFilter}.
+//  
+//  
+//  \index{itk::FFTRealToComplexConjugateImageFilter}
+//  \index{itk::VnlFFTRealToComplexConjugateImageFilter}
+//  \index{itk::FFTWRealToComplexConjugateImageFilter}
+//
+//  Software Guide : EndLatex 
+
+
+// Software Guide : BeginCodeSnippet
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkRescaleIntensityImageFilter.h"
+#include "itkVnlFFTRealToComplexConjugateImageFilter.h"
+#include "itkComplexToRealImageAdaptor.h"
+// Software Guide : EndCodeSnippet
+
+
+int main( int argc, char * argv [] )
+{
+  if( argc < 3 )
+    {
+    std::cerr << "Usage: " << argv[0] << " inputScalarImage  outputRealPartOfComplexImage" << std::endl;
+    }
+
+
+// Software Guide : BeginLatex
+//
+// The first decision to make is related to the pixel type and dimension of the
+// images on which we want to compute the Fourier transform. 
+//
+// Software Guide : EndLatex 
+
+// Software Guide : BeginCodeSnippet
+  typedef float  PixelType;
+  const unsigned int Dimension = 2;
+
+  typedef itk::Image< PixelType, Dimension > ImageType;
+// Software Guide : EndCodeSnippet
+
+
+  typedef itk::ImageFileReader< ImageType >  ReaderType;
+
+  ReaderType::Pointer reader = ReaderType::New();
+
+  reader->SetFileName( argv[1] );
+
+
+
+
+// Software Guide : BeginLatex
+//
+// We use the same image type in order to instantiate the FFT filter. In this
+// case the \doxygen{VnlFFTRealToComplexConjugateImageFilter}. Note that
+// contrary to most ITK filters, the FFT filter is instantiated using the Pixel
+// type and the image dimension explicitly. Once the filter type is
+// instantiated, we can use it for creating one object by invoking the
+// \code{New()} method and assigning the result to a SmartPointer.
+// 
+// Software Guide : EndLatex 
+
+
+// Software Guide : BeginCodeSnippet
+  typedef itk::VnlFFTRealToComplexConjugateImageFilter< PixelType, Dimension >  FFTFilterType;
+
+  FFTFilterType::Pointer filter = FFTFilterType::New();
+
+  filter->SetInput( reader->GetOutput() );
+// Software Guide : EndCodeSnippet
+
+
+
+
+// Software Guide : BeginLatex
+//
+// The execution of the filter can be triggered by invoking the \code{Update()}
+// method.  Since this invokation can eventually throw and exception, the call
+// must be placed inside a try/catch block.
+//
+// Software Guide : EndLatex 
+
+// Software Guide : BeginCodeSnippet
+  try
+    {
+    filter->Update();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+    std::cerr << "Error: " << std::endl;
+    std::cerr << excp << std::endl;
+    return EXIT_FAILURE;
+    }
+// Software Guide : EndCodeSnippet
+
+
+
+
+// Software Guide : BeginLatex
+//
+// We instantiate now the ImageAdaptor that will help us to extract the real
+// part from the complex image without having toalocate additional memory.  The
+// Adaptor we use here is the \doxygen{ComplexToRealImageAdaptor}. It takes as
+// first template parameter the type of the complex image and as second
+// template parameter it takes the type of the output image pixel.
+//
+// Software Guide : EndLatex 
+
+// Software Guide : BeginCodeSnippet
+  typedef FFTFilterType::OutputImageType    ComplexImageType;
+
+  typedef itk::ComplexToRealImageAdaptor< ComplexImageType, PixelType > RealAdaptorType;
+// Software Guide : EndCodeSnippet
+
+  RealAdaptorType::Pointer realAdaptor = RealAdaptorType::New();
+
+  realAdaptor->SetImage( filter->GetOutput() );
+
+
+  typedef unsigned char WritePixelType;
+  typedef itk::Image< WritePixelType, Dimension > WriteImageType;
+
+
+
+
+// Software Guide : BeginLatex
+//
+// We instantiate now the filter type that will be used for rescaling the
+// intensities of the \code{real} image into a range suitable for writing in a
+// file. This is done with the \doxygen{RescaleIntensityImageFilter}. Note that
+// the input image type in the instantiation must be directly the Adaptor type.
+// For most information on image adaptors you may want to look at section
+// \ref{sec:ImageAdaptors}.
+//
+// Software Guide : EndLatex 
+
+// Software Guide : BeginCodeSnippet
+  typedef itk::RescaleIntensityImageFilter< 
+                                RealAdaptorType, 
+                                WriteImageType > RescaleFilterType;
+// Software Guide : EndCodeSnippet
+
+
+
+  RescaleFilterType::Pointer intensityRescaler = RescaleFilterType::New();
+
+  intensityRescaler->SetInput( realAdaptor );
+
+  intensityRescaler->SetOutputMinimum(  0  );
+  intensityRescaler->SetOutputMaximum( 255 );
+
+  typedef itk::ImageFileWriter< WriteImageType > WriterType;
+
+  WriterType::Pointer writer = WriterType::New();
+
+  writer->SetFileName( argv[2] );
+
+  writer->SetInput( intensityRescaler->GetOutput() );
+
+  try
+    {
+//    writer->Update();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+    std::cerr << "Error: " << std::endl;
+    std::cerr << excp << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  return EXIT_SUCCESS;
+}
+
