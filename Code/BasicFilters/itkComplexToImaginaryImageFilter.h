af5ae5dd68578a886c70df6e66935ac5b94b275b
Luis Ibanez <luis.ibanez@kitware.com>
2004-11-14 15:16:34 -0500

    BUG: 1349. An examples was missing for illustrating how to use the FFT filters      in images. Such example requires convenience filter for extracting the      real an imaginary parts of a complex image. Both ImageAdaptors and ImageFilter      are created here for extracting the real and imaginary part of a complex image.

---
+++ Code/BasicFilters/itkComplexToImaginaryImageFilter.h
@@ -0,0 +1,77 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkComplexToImaginaryImageFilter.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkComplexToImaginaryImageFilter_h
+#define __itkComplexToImaginaryImageFilter_h
+
+#include "itkUnaryFunctorImageFilter.h"
+#include "vnl/vnl_math.h"
+
+namespace itk
+{
+  
+/** \class ComplexToImaginaryImageFilter
+ * \brief Computes pixel-wise the imaginary part of a complex image.
+ * 
+ * \ingroup IntensityImageFilters  Multithreaded
+ */
+namespace Function {  
+  
+template< class TInput, class TOutput>
+class ComplexToImaginary
+{
+public:
+  ComplexToImaginary() {}
+  ~ComplexToImaginary() {}
+  inline TOutput operator()( const TInput & A )
+  { return (TOutput)( A.imag() ); }
+}; 
+}
+
+template <class TInputImage, class TOutputImage>
+class ITK_EXPORT ComplexToImaginaryImageFilter :
+    public
+UnaryFunctorImageFilter<TInputImage,TOutputImage, 
+                        Function::ComplexToImaginary< 
+  typename TInputImage::PixelType, 
+  typename TOutputImage::PixelType>   >
+{
+public:
+  /** Standard class typedefs. */
+  typedef ComplexToImaginaryImageFilter  Self;
+  typedef UnaryFunctorImageFilter<TInputImage,TOutputImage, 
+                                  Function::ComplexToImaginary< typename TInputImage::PixelType, 
+                                                 typename TOutputImage::PixelType> >  Superclass;
+  typedef SmartPointer<Self>   Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+  
+protected:
+  ComplexToImaginaryImageFilter() {}
+  virtual ~ComplexToImaginaryImageFilter() {}
+
+private:
+  ComplexToImaginaryImageFilter(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+};
+
+} // end namespace itk
+
+
+#endif
---
+++ Code/BasicFilters/itkComplexToRealImageFilter.h
@@ -0,0 +1,77 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkComplexToRealImageFilter.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkComplexToRealImageFilter_h
+#define __itkComplexToRealImageFilter_h
+
+#include "itkUnaryFunctorImageFilter.h"
+#include "vnl/vnl_math.h"
+
+namespace itk
+{
+  
+/** \class ComplexToRealImageFilter
+ * \brief Computes pixel-wise the real(x) part of a complex image.
+ * 
+ * \ingroup IntensityImageFilters  Multithreaded
+ */
+namespace Function {  
+  
+template< class TInput, class TOutput>
+class ComplexToReal
+{
+public:
+  ComplexToReal() {}
+  ~ComplexToReal() {}
+  inline TOutput operator()( const TInput & A )
+  { return (TOutput)( A.real() ); }
+}; 
+}
+
+template <class TInputImage, class TOutputImage>
+class ITK_EXPORT ComplexToRealImageFilter :
+    public
+UnaryFunctorImageFilter<TInputImage,TOutputImage, 
+                        Function::ComplexToReal< 
+  typename TInputImage::PixelType, 
+  typename TOutputImage::PixelType>   >
+{
+public:
+  /** Standard class typedefs. */
+  typedef ComplexToRealImageFilter  Self;
+  typedef UnaryFunctorImageFilter<TInputImage,TOutputImage, 
+                                  Function::ComplexToReal< typename TInputImage::PixelType, 
+                                                 typename TOutputImage::PixelType> >  Superclass;
+  typedef SmartPointer<Self>   Pointer;
+  typedef SmartPointer<const Self>  ConstPointer;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+  
+protected:
+  ComplexToRealImageFilter() {}
+  virtual ~ComplexToRealImageFilter() {}
+
+private:
+  ComplexToRealImageFilter(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+};
+
+} // end namespace itk
+
+
+#endif
