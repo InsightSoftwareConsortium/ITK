0db0c610c11ac8d1c85da0121199d18d6c3564da
Luis Ibanez <luis.ibanez@kitware.com>
2004-11-15 10:26:14 -0500

    COMP: Adding many instantiations of the InPlaceImageFilter that are now       necessary after the UnaryFunctorImageFilter has been made to derive       from the InPlaceImageFilter.

--- Wrapping/CSwig/Common/wrap_itkInPlaceImageFilter.cxx
+++ Wrapping/CSwig/Common/wrap_itkInPlaceImageFilter.cxx
@@ -35,7 +35,7 @@ namespace _cable_
     ITK_WRAP_OBJECT2(InPlaceImageFilter, image::SC2, image::SC2, itkInPlaceImageFilterSC2SC2);
     ITK_WRAP_OBJECT2(InPlaceImageFilter, image::SS2, image::SS2, itkInPlaceImageFilterSS2SS2);
     ITK_WRAP_OBJECT2(InPlaceImageFilter, image::SI2, image::SI2, itkInPlaceImageFilterSI2SI2);
-    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::F2 , image::VF2 ,itkInPlaceImageFilterF2VF2 );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::F2 , image::VF2, itkInPlaceImageFilterF2VF2 );
     ITK_WRAP_OBJECT2(InPlaceImageFilter, image::US2, image::VF2, itInPlaceeImageFilterUS2VF2);
 
     //===========Same type 3D Wrapped Filters==============
@@ -47,7 +47,7 @@ namespace _cable_
     ITK_WRAP_OBJECT2(InPlaceImageFilter, image::SC3, image::SC3, itkInPlaceImageFilterSC3SC3);
     ITK_WRAP_OBJECT2(InPlaceImageFilter, image::SS3, image::SS3, itkInPlaceImageFilterSS3SS3);
     ITK_WRAP_OBJECT2(InPlaceImageFilter, image::SI3, image::SI3, itkInPlaceImageFilterSI3SI3);
-    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::F3 , image::VF3 ,itkInPlaceImageFilterF3VF3 );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::F3 , image::VF3, itkInPlaceImageFilterF3VF3 );
     ITK_WRAP_OBJECT2(InPlaceImageFilter, image::US3, image::VF3, itInPlaceeImageFilterUS3VF3);
 
     //===========Same type 2D->3D Wrapped Filters==============
@@ -59,7 +59,7 @@ namespace _cable_
     ITK_WRAP_OBJECT2(InPlaceImageFilter, image::SC2, image::SC3, itkInPlaceImageFilterSC2SC3);
     ITK_WRAP_OBJECT2(InPlaceImageFilter, image::SS2, image::SS3, itkInPlaceImageFilterSS2SS3);
     ITK_WRAP_OBJECT2(InPlaceImageFilter, image::SI2, image::SI3, itkInPlaceImageFilterSI2SI3);
-    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::F2 , image::VF3 ,itkInPlaceImageFilterF2VF3 );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::F2 , image::VF3, itkInPlaceImageFilterF2VF3 );
     ITK_WRAP_OBJECT2(InPlaceImageFilter, image::US2, image::VF3, itInPlaceeImageFilterUS2VF3);
 
     //===========Same type 3D->2D Wrapped Filters==============
@@ -71,19 +71,41 @@ namespace _cable_
     ITK_WRAP_OBJECT2(InPlaceImageFilter, image::SC3, image::SC2, itkInPlaceImageFilterSC3SC2);
     ITK_WRAP_OBJECT2(InPlaceImageFilter, image::SS3, image::SS2, itkInPlaceImageFilterSS3SS2);
     ITK_WRAP_OBJECT2(InPlaceImageFilter, image::SI3, image::SI2, itkInPlaceImageFilterSI3SI2);
-    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::F3 , image::VF2 ,itkInPlaceImageFilterF3VF2 );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::F3 , image::VF2, itkInPlaceImageFilterF3VF2 );
     ITK_WRAP_OBJECT2(InPlaceImageFilter, image::US3, image::VF2, itInPlaceeImageFilterUS3VF2);
 
     //===== Mixed types InPlaceImageFilters 2D ImageFilters
-    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::F2 , image::UC2 , itkInPlaceImageFilterF2UC2  );
-    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::F2 , image::US2 , itkInPlaceImageFilterF2US2  );
-    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::US2 , image::F2 , itkInPlaceImageFilterUS2F2  );
-    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::UC2 , image::F2 , itkInPlaceImageFilterUC2F2  );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::F2,  image::UC2, itkInPlaceImageFilterF2UC2  );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::F2,  image::US2, itkInPlaceImageFilterF2US2  );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::F2,  image::UI2, itkInPlaceImageFilterF2UI2  );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::F2,  image::SC2, itkInPlaceImageFilterF2SC2  );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::F2,  image::SI2, itkInPlaceImageFilterF2SI2  );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::F2,  image::SS2, itkInPlaceImageFilterF2SS2  );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::F2 , image::D2 , itkInPlaceImageFilterF2D2   );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::D2 , image::F2 , itkInPlaceImageFilterD2F2   );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::US2, image::F2,  itkInPlaceImageFilterUS2F2  );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::US2, image::UC2,  itkInPlaceImageFilterUS2UC2);
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::UC2, image::F2,  itkInPlaceImageFilterUC2F2  );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::UC2, image::US2, itkInPlaceImageFilterUC2US2 );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::SC2, image::F2,  itkInPlaceImageFilterSC2F2  );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::SS2, image::F2,  itkInPlaceImageFilterSS2F2  );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::SI2, image::F2,  itkInPlaceImageFilterSI2F2  );
     //===== Mixed types InPlaceImageFilters 3D ImageFilters
-    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::F3 , image::UC3 , itkInPlaceImageFilterF3UC3  );
-    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::F3 , image::US3 , itkInPlaceImageFilterF3US3  );
-    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::US3 , image::F3 , itkInPlaceImageFilterUS3F3  );
-    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::UC3 , image::F3 , itkInPlaceImageFilterUC3F3  );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::F3,  image::UC3, itkInPlaceImageFilterF3UC3  );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::F3,  image::US3, itkInPlaceImageFilterF3US3  );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::F3,  image::UI3, itkInPlaceImageFilterF3UI3  );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::F3,  image::SC3, itkInPlaceImageFilterF3SC3  );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::F3,  image::SI3, itkInPlaceImageFilterF3SI3  );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::F3,  image::SS3, itkInPlaceImageFilterF3SS3  );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::F3 , image::D3 , itkInPlaceImageFilterF3D3   );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::D3 , image::F3 , itkInPlaceImageFilterD3F3   );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::US3, image::F3,  itkInPlaceImageFilterUS3F3  );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::US3, image::UC3,  itkInPlaceImageFilterUS3UC3);
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::UC3, image::F3,  itkInPlaceImageFilterUC3F3  );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::UC3, image::US3, itkInPlaceImageFilterUC3US3 );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::SC3, image::F3,  itkInPlaceImageFilterSC3F3  );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::SS3, image::F3,  itkInPlaceImageFilterSS3F3  );
+    ITK_WRAP_OBJECT2(InPlaceImageFilter, image::SI3, image::F3,  itkInPlaceImageFilterSI3F3  );
   }
 }
 #endif
