936c052710ffbafd4595e0a5721bae18233fa56e
Bill Lorensen <bill.lorensen@gmail.com>
2005-01-05 10:00:40 -0500

    ENH: new examples to look at and manipulate DICOM headers.

---
+++ Examples/IO/DicomImageReadChangeHeaderWrite.cxx
@@ -0,0 +1,120 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    DicomImageReadChangeHeaderWrite.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+//  Software Guide : BeginLatex
+//
+//  This example illustrates how to read a single DICOM slice and write it back
+//  with some changed header information as another DICOM
+//  slice. Header Key/Value pairs can be specified on the command
+//  line. The keys are defined in Insight/Utilities/gdcm/Dicts/dicomV3.dic.
+//
+//  Software Guide : EndLatex 
+
+
+// Software Guide : BeginCodeSnippet
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkImage.h"
+#include "itkMetaDataDictionary.h"
+#include "itkMetaDataObject.h"
+#include "itkGDCMImageIO.h"
+// Software Guide : EndCodeSnippet
+
+#include <list>
+#include <fstream>
+
+int main(int ac, char* av[])
+{
+
+  if(ac < 5)
+    {
+    std::cerr << "Usage: " << av[0] << " DicomImage OutputDicomImage Entry Value\n";
+    return EXIT_FAILURE;
+    }
+
+
+// Software Guide : BeginCodeSnippet
+  typedef short InputPixelType;
+
+  typedef itk::Image< InputPixelType, 2 > InputImageType;
+
+  typedef itk::ImageFileReader< InputImageType > ReaderType;
+// Software Guide : EndCodeSnippet
+
+  ReaderType::Pointer reader = ReaderType::New();
+
+  reader->SetFileName( av[1] );
+  
+// Software Guide : BeginCodeSnippet
+  typedef itk::GDCMImageIO        ImageIOType;
+
+  ImageIOType::Pointer gdcmImageIO = ImageIOType::New();
+
+  reader->SetImageIO( gdcmImageIO );
+// Software Guide : EndCodeSnippet
+
+  try
+    {
+    reader->Update();
+    }
+  catch (itk::ExceptionObject & e)
+    {
+    std::cerr << "exception in file reader " << std::endl;
+    std::cerr << e.GetDescription() << std::endl;
+    std::cerr << e.GetLocation() << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  // Change some header information
+  typedef itk::MetaDataDictionary   DictionaryType;
+  DictionaryType & dictionary = reader->GetOutput()->GetMetaDataDictionary();
+
+  for (int i = 3; i < ac; i+=2)
+    {
+    std::string entryId(av[i]);
+    std::string value(av[i+1]);
+    itk::EncapsulateMetaData<std::string>(dictionary,entryId, value);
+    }
+
+  // Rewrite the image in DICOM format
+  //
+  typedef itk::ImageFileWriter< InputImageType >  Writer1Type;
+
+  Writer1Type::Pointer writer1 = Writer1Type::New();
+
+  writer1->SetFileName( av[2] );
+ 
+  writer1->SetInput( reader->GetOutput() );
+  
+  writer1->SetImageIO( gdcmImageIO );
+
+  try
+    {
+    writer1->Update();
+    }
+  catch (itk::ExceptionObject & e)
+    {
+    std::cerr << "exception in file writer " << std::endl;
+    std::cerr << e.GetDescription() << std::endl;
+    std::cerr << e.GetLocation() << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  return EXIT_SUCCESS;
+
+}
+
---
+++ Examples/IO/DicomImageReadPrintTags.cxx
@@ -0,0 +1,112 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    DicomImageReadPrintTags.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+//  Software Guide : BeginLatex
+//
+//  This example illustrates how to read a DICOM file and then print
+//  most of the DICOM header information (binary field are skipped).
+//
+//  Software Guide : EndLatex 
+
+
+#include "itkImageFileReader.h"
+#include "itkGDCMImageIO.h"
+#include "itkMetaDataDictionary.h"
+#include "itkMetaDataObject.h"
+
+int main( int argc, char* argv[] )
+{
+
+  if( argc < 2 )
+    {
+    std::cerr << "Usage: " << argv[0] << " DicomFile " << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  typedef itk::Image<unsigned short,3>          ImageType;
+  typedef itk::ImageFileReader< ImageType >     ReaderType;
+
+  typedef itk::GDCMImageIO                      ImageIOType;
+  ImageIOType::Pointer dicomIO = ImageIOType::New();
+
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( argv[1] );
+  reader->SetImageIO( dicomIO );
+
+  try
+    {
+    reader->Update();
+    }
+  catch (itk::ExceptionObject &ex)
+    {
+    std::cout << ex << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  
+  typedef itk::MetaDataDictionary   DictionaryType;
+
+  const  DictionaryType & dictionary = dicomIO->GetMetaDataDictionary();
+
+  DictionaryType::ConstIterator itr = dictionary.Begin();
+  DictionaryType::ConstIterator end = dictionary.End();
+
+  
+  typedef itk::MetaDataObject< std::string > MetaDataStringType;
+
+  while( itr != end )
+    {
+    itk::MetaDataObjectBase::Pointer  entry = itr->second;
+
+    MetaDataStringType::Pointer entryvalue = 
+      dynamic_cast<MetaDataStringType *>( entry.GetPointer() ) ;
+
+    if( entryvalue )
+      {
+      std::string tagkey   = itr->first;
+      std::string tagvalue = entryvalue->GetMetaDataObjectValue();
+      std::cout << tagkey <<  " = " << tagvalue << std::endl;
+      }
+    ++itr;
+    }
+  
+//  Software Guide : BeginLatex
+//
+//  In order to read a specific tag, the string of the entry can be used
+//  for quering the MetaDataDictionary.
+//
+//  Software Guide : EndLatex 
+
+
+    //std::string entryId = "Patient Name"; //DICOM 2002
+    std::string entryId = "Patient's Name"; //DICOM 2003
+    itk::MetaDataObjectBase::ConstPointer  entry = dictionary[entryId];
+
+    MetaDataStringType::ConstPointer entryvalue = 
+      dynamic_cast<const MetaDataStringType *>( entry.GetPointer() ) ;
+
+    if( entryvalue )
+      {
+      std::string tagvalue = entryvalue->GetMetaDataObjectValue();
+      std::cout << entryId <<  " = " << tagvalue << std::endl;
+      }
+
+
+  return EXIT_SUCCESS;
+
+}
+
