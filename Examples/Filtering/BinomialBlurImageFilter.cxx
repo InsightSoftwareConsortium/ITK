abec8ba0cfead046f5aa16a46113a7291771b44a
Karthik Krishnan <karthik.krishnan@kitware.com>
2005-01-07 09:12:21 -0500

    ENH: Command line tags for automatic generation of figures in Software guide

--- Examples/Filtering/BinomialBlurImageFilter.cxx
+++ Examples/Filtering/BinomialBlurImageFilter.cxx
@@ -14,6 +14,11 @@
      PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
+//  Software Guide : BeginCommandLineArgs
+//    INPUTS: {BrainProtonDensitySlice.png}
+//    OUTPUTS: {BinomialBlurImageFilterOutput.png}
+//    5
+//  Software Guide : EndCommandLineArgs
 
 //  Software Guide : BeginLatex
 //
