5bc5ca05260a00a1478c40099e435bcce5123b25
Luis Ibanez <luis.ibanez@kitware.com>
2005-01-10 19:24:30 -0500

    ENH: Test for the itkContourDirectedMeanDistanceImageFilter contributed by      Teo Popa from the ISIS Center at Georgetown University.

--- Testing/Code/BasicFilters/itkBasicFiltersTests2.cxx
+++ Testing/Code/BasicFilters/itkBasicFiltersTests2.cxx
@@ -32,6 +32,7 @@ void RegisterTests()
   REGISTER_TEST(itkComplexToImaginaryFilterAndAdaptorTest );
   REGISTER_TEST(itkConstrainedValueAdditionImageFilterTest );
   REGISTER_TEST(itkConstrainedValueDifferenceImageFilterTest );
+  REGISTER_TEST(itkContourDirectedMeanDistanceImageFilterTest );
   REGISTER_TEST(itkGrayscaleMorphologicalClosingImageFilterTest );
   REGISTER_TEST(itkGrayscaleMorphologicalOpeningImageFilterTest );
   REGISTER_TEST(itkSimpleContourExtractorImageFilterTest );
