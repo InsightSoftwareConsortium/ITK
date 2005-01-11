122064eba066c66d42efc22c3aa288ac1458d384
Luis Ibanez <luis.ibanez@kitware.com>
2005-01-10 20:23:32 -0500

    ENH: Test added for the itkContourMeanDistanceImageFilter.

--- Testing/Code/BasicFilters/itkBasicFiltersTests2.cxx
+++ Testing/Code/BasicFilters/itkBasicFiltersTests2.cxx
@@ -33,6 +33,7 @@ void RegisterTests()
   REGISTER_TEST(itkConstrainedValueAdditionImageFilterTest );
   REGISTER_TEST(itkConstrainedValueDifferenceImageFilterTest );
   REGISTER_TEST(itkContourDirectedMeanDistanceImageFilterTest );
+  REGISTER_TEST(itkContourMeanDistanceImageFilterTest );
   REGISTER_TEST(itkGrayscaleMorphologicalClosingImageFilterTest );
   REGISTER_TEST(itkGrayscaleMorphologicalOpeningImageFilterTest );
   REGISTER_TEST(itkSimpleContourExtractorImageFilterTest );
