19981ee34684b0956e9666beee49dfe85f82d27a
Stephen Aylward <stephen.aylward@kitware.com>
2005-01-21 15:18:10 -0500

    BUG: itkTypeMacro must be passed the class's actual name - you cannot use Self or Superclass typedefs.

--- Code/Algorithms/itkSimplexMeshVolumeCalculator.h
+++ Code/Algorithms/itkSimplexMeshVolumeCalculator.h
@@ -69,7 +69,7 @@ public:
   itkNewMacro(Self);
 
   /** Run-time type information (and related methods). */
-  itkTypeMacro(Self, Object);
+  itkTypeMacro(SimplexMeshVolumeCalculator, Object);
 
   typedef TInputMesh                                              InputMeshType;
   typedef typename InputMeshType::Pointer                         InputMeshPointer;
