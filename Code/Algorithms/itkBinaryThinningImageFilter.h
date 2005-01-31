c7ec4a642e662751d9ffa4d00c120931d3be48c2
Bill Lorensen <bill.lorensen@gmail.com>
2005-01-31 14:39:38 -0500

    ENH: updated documentation.

--- Code/Algorithms/itkBinaryThinningImageFilter.h
+++ Code/Algorithms/itkBinaryThinningImageFilter.h
@@ -26,26 +26,29 @@ namespace itk
 
 /** \class BinaryThinningImageFilter
 *
+* \brief This filter computes one-pixel-wide edges of the input image.
+*
 * This class is parametrized over the type of the input image
 * and the type of the output image.
-*
-* This filter computes one pixel wide edges of the input image.
 * 
+* The input is assumed to be a binary image.  If the foreground pixels
+* of the input image do not have a value of 1, they are rescaled to 1
+* internally to simplify the computation.
+* 
+* The filter will produce a skeleton of the object.  The output
+* background values are 0, and the foreground values are 1.
 *
-* The input is assumed to be a binary image.
-* The filter will produce a skeleton of the object:
-
-*
-* This filter is sequential thinning algorithm and known to be computational time
-* dependable of the image size.  The algorithm is the N-dimensional version
-* of the  given for two dimensions in:
+* This filter is a sequential thinning algorithm and known to be computational time
+* dependable on the image size.  The algorithm corresponds with the 2D
+* implementation described in:
 * 
 * Rafael C. Gonzales and Richard E. Woods. 
 * Digital Image Processing. 
 * Addison Wesley, 491-494, (1993).
 *
+* To do: Make this filter ND.
+*
 * \sa MorphologyImageFilter
-* \sa BinaryThinningImageFilter
 * \ingroup ImageEnhancement MathematicalMorphologyImageFilters
 */
 
