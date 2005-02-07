d4715be604cf988d5e38c844fec677fd9424d01d
Karthik Krishnan <karthik.krishnan@kitware.com>
2005-02-07 13:58:51 -0500

    STYLE: Sofware Guide tags added

--- Examples/IO/DicomSeriesReadImageWrite2.cxx
+++ Examples/IO/DicomSeriesReadImageWrite2.cxx
@@ -37,31 +37,61 @@ int main( int argc, char* argv[] )
     return EXIT_FAILURE;
     }
 
+  // Software Guide : BeginLatex
+  // The dimensionality of the ImageType is 3 for a conventional 3D image reader.
+  // Software Guide : EndLatex
+  // Software Guide : BeginCodeSnippet
   typedef itk::Image<unsigned short,3>            ImageType;
   typedef itk::ImageSeriesReader< ImageType >     ReaderType;
+  // Software Guide : EndCodeSnippet
 
+  // Software Guide : BeginLatex
+  // GDCMImageIO is an ImageIO class for reading and writing DICOM v3 and 
+  // ACR/NEMA images. It uses GDCM, an open source package developed by the 
+  // Creatis team at INSA-Lyon \cite{CreatisINSA-Lyon}. 
+  // Software Guide : EndLatex
+
+
+  // Software Guide : BeginCodeSnippet
   typedef itk::GDCMImageIO                        ImageIOType;
-    
   ImageIOType::Pointer dicomIO = ImageIOType::New();
+  // Software Guide : EndCodeSnippet
 
   // Get the DICOM filenames from the directory
+  // Software Guide : BeginLatex
+  // GDCMSeriesFileNames generates a sequence of filenames for DICOM files 
+  // for one study/series. The files pertinent to the study are contained
+  // in the directory specified by the \code{SetInputDirectory()} method.
+  // Software Guide : EndLatex
+  // Software Guide : BeginCodeSnippet
   typedef itk::GDCMSeriesFileNames NamesGeneratorType;
   NamesGeneratorType::Pointer nameGenerator = NamesGeneratorType::New();
   nameGenerator->SetInputDirectory( argv[1] );
+  // Software Guide : EndCodeSnippet
   
 
   try
     {
+    // Software Guide : BeginLatex
+    // The \code{GetInputFileNames()} method returns a vector container 
+    // containing an ordered list of input file names in the specified 
+    // directory.
+    // Software Guide : EndLatex
+      
+    // Software Guide : BeginCodeSnippet
     typedef std::vector<std::string> fileNamesContainer;
     fileNamesContainer fileNames;
     fileNames = nameGenerator->GetInputFileNames();
     ReaderType::Pointer reader = ReaderType::New();
     reader->SetFileNames( fileNames );
     reader->SetImageIO( dicomIO );
+    // Software Guide : EndCodeSnippet
 
     try
       {
+      // Software Guide : BeginCodeSnippet
       reader->Update();
+      // Software Guide : EndCodeSnippet
       }
     catch (itk::ExceptionObject &ex)
       {
@@ -69,19 +99,28 @@ int main( int argc, char* argv[] )
       return EXIT_FAILURE;
       }
 
+
+    // Software Guide : BeginLatex
+    // Now we will save the files in another specified file format.
+    // Software Guide : EndLatex
+    // Software Guide : BeginCodeSnippet    
     typedef itk::ImageFileWriter< ImageType > WriterType;
     WriterType::Pointer writer = WriterType::New();
+    // Software Guide : EndCodeSnippet    
 
     std::cout  << "Writing the image as " << std::endl << std::endl;
     std::cout  << argv[2] << std::endl << std::endl;
 
+    // Software Guide : BeginCodeSnippet    
     writer->SetFileName( argv[2] );
-    
     writer->SetInput( reader->GetOutput() );
+    // Software Guide : EndCodeSnippet    
 
     try
       {
+      // Software Guide : BeginCodeSnippet    
       writer->Update();
+      // Software Guide : EndCodeSnippet    
       }
     catch (itk::ExceptionObject &ex)
       {
