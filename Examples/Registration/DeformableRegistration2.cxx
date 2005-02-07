6622087f4a13c3ac225ec0c58a08ae7882c336f6
Karthik Krishnan <karthik.krishnan@kitware.com>
2005-02-07 14:00:48 -0500

    STYLE: Software Guide: Command line tags added

--- Examples/Registration/DeformableRegistration2.cxx
+++ Examples/Registration/DeformableRegistration2.cxx
@@ -14,6 +14,11 @@
      PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
+//  Software Guide : BeginCommandLineArgs
+//    RatLungSlice1.mha RatLungSlice2.mha DeformableRegistration2Output.mha 
+//    DeformableRegistration2Field.mha
+//  Software Guide : EndCommandLineArgs
+
 
 #include "itkImageFileReader.h" 
 #include "itkImageFileWriter.h" 
