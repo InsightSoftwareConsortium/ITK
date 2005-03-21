8c086c64941ce85c3d9a3fa108659e5dabb0bbe5
Julien Jomier <julien.jomier@kitware.com>
2005-03-21 14:31:51 -0500

    ENH: Added CenteredSimilarity2DTransform

--- Wrapping/CSwig/Common/wrap_ITKSimilarityTransforms.cxx
+++ Wrapping/CSwig/Common/wrap_ITKSimilarityTransforms.cxx
@@ -15,6 +15,7 @@
 
 =========================================================================*/
 #include "itkSimilarity2DTransform.h"
+#include "itkCentereddSimilarity2DTransform.h"
 
 #ifdef CABLE_CONFIGURATION
 #include "itkCSwigMacros.h"
@@ -28,6 +29,7 @@ namespace _cable_
   namespace wrappers
   {
     ITK_WRAP_TRANSFORM_1(Similarity2DTransform);
+    ITK_WRAP_TRANSFORM_1(CenteredSimilarity2DTransform);
   }
 }
 #endif
