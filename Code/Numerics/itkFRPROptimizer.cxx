f7043737afabf1808717e0a116642199609c0693
Stephen Aylward <stephen.aylward@kitware.com>
2005-03-21 19:13:48 -0500

    ENH: Added the Fletcher-Reeves & Polak-Ribier optimizer from NRC

---
+++ Code/Numerics/itkFRPROptimizer.cxx
@@ -0,0 +1,191 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkFRPROptimizer.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef _itkFRPROptimizer_cxx
+#define _itkFRPROptimizer_cxx
+
+#include "itkFRPROptimizer.h"
+
+#define FRPR_TINY 1e-20
+
+namespace itk
+{
+
+FRPROptimizer
+::FRPROptimizer()
+{
+}
+
+FRPROptimizer
+::~FRPROptimizer() 
+{
+}
+
+void
+FRPROptimizer
+::GetValueAndDerivative(ParametersType p, double * val,
+                        ParametersType * xi)
+{
+  this->m_CostFunction->GetValueAndDerivative(p, *val, *xi);
+  if(this->GetMaximize())
+    {
+    (*val) = -(*val);
+    for(int i=0; i<this->GetSpaceDimension(); i++)
+      {
+      (*xi)[i] = -(*xi)[i];
+      }
+    }
+}
+
+void
+FRPROptimizer
+::LineOptimize(ParametersType * p, ParametersType xi, double * val)
+{
+  this->SetLine(*p, xi);
+
+  double ax = 0.0;
+  double fa = (*val);
+  double xx = this->GetStepLength();
+  double fx;
+  double bx;
+  double fb;
+
+  ParametersType pp = (*p);
+
+  this->LineBracket(&ax, &xx, &bx, &fa, &fx, &fb);
+  this->SetCurrentLinePoint(xx, fx);
+
+  double extX = 0;
+  double extVal = 0;
+
+  this->BracketedLineOptimize(ax, xx, bx, fa, fx, fb, &extX, &extVal);
+  this->SetCurrentLinePoint(extX, extVal);
+
+  (*p) = this->GetCurrentPosition();
+  (*val) = extVal;
+}
+
+void
+FRPROptimizer
+::StartOptimization()
+{
+  if( m_CostFunction.IsNull() )
+    {
+    return ;
+    }
+
+  this->InvokeEvent( StartEvent() );
+  this->SetStop(false);
+
+  this->SetSpaceDimension(m_CostFunction->GetNumberOfParameters());
+
+  double gg, gam, dgg;
+  FRPROptimizer::ParametersType g(this->GetSpaceDimension());
+  FRPROptimizer::ParametersType h(this->GetSpaceDimension());
+  FRPROptimizer::ParametersType xi(this->GetSpaceDimension());
+
+  FRPROptimizer::ParametersType p(this->GetSpaceDimension());
+  p = this->GetInitialPosition();
+
+  double fp;
+  this->GetValueAndDerivative(p, &fp, &xi);
+
+  for(int i=0; i<this->GetSpaceDimension(); i++)
+    {
+    g[i] = -xi[i];
+    xi[i] = g[i];
+    h[i] = g[i];
+    }
+
+  unsigned int limitCount = 0;
+
+  for (int currentIteration = 0;
+       currentIteration <= this->GetMaximumIteration();
+       currentIteration++)
+    {
+    std::cout << currentIteration << " : " << p << " : " << fp << std::endl;
+    this->SetCurrentIteration(currentIteration);
+
+    double fret;
+    fret = fp;
+    this->LineOptimize(&p, xi, &fret);
+
+    if(2.0 * vcl_abs(fret - fp) <= 
+       this->GetStepTolerance() * (vcl_abs(fret)*vcl_abs(fp) + FRPR_TINY))
+      {
+      if(limitCount < this->GetSpaceDimension())
+        {
+        this->GetValueAndDerivative(p, &fp, &xi);
+        xi[limitCount] = 1;
+        limitCount++;
+        }
+      else
+        {
+        this->SetCurrentPosition(p);
+        this->InvokeEvent( EndEvent() );
+        return;
+        }
+      }
+    else
+      {
+      limitCount = 0;
+      this->GetValueAndDerivative(p, &fp, &xi);
+      }
+
+    gg = 0.0;
+    dgg = 0.0;
+    for(int i=0; i<this->GetSpaceDimension(); i++)
+      {
+      gg += g[i] * g[i];
+      //dgg += xi[i] * xi[i];  // Uncomment for Fletcher-Reeves
+      dgg += (xi[i] + g[i]) * xi[i];    // Uncoment for Polak-Ribiere
+      }
+
+    if(gg == 0)
+      {
+      this->SetCurrentPosition(p);
+      this->InvokeEvent( EndEvent() );
+      return;
+      }
+
+    gam = dgg/gg;
+    for(int i=0; i<this->GetSpaceDimension(); i++)
+      {
+      g[i] = -xi[i];
+      xi[i] = g[i] + gam * h[i];
+      h[i] = xi[i];
+      }
+
+    this->SetCurrentPosition(p);
+    this->InvokeEvent( IterationEvent() );
+    }
+
+  this->SetCurrentPosition(p);
+  this->InvokeEvent( EndEvent() );
+}
+
+/**
+ *
+ */
+void
+FRPROptimizer
+::PrintSelf(std::ostream& os, Indent indent) const
+{
+  Superclass::PrintSelf(os,indent);
+}
+
+} // end of namespace itk
+#endif
---
+++ Code/Numerics/itkFRPROptimizer.h
@@ -0,0 +1,99 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkFRPROptimizer.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkFRPROptimizer_h
+#define __itkFRPROptimizer_h
+
+#include <itkVector.h>
+#include <itkMatrix.h>
+#include <itkPowellOptimizer.h>
+
+namespace itk
+{
+
+/** \class FRPROptimizer
+ * \brief Implements Fletch-Reeves & Polak-Ribiere optimization using dBrent
+ * line search - adapted from Numerical Recipes in C (first edition).
+ *
+ * This optimizer needs a cost function.
+ * This optimizer needs to be able to compute partial derivatives of the 
+ *    cost function with respect to each parameter.
+ *
+ * The SetStepLength determines the initial distance to step in a line direction
+ * when bounding the minimum (using bracketing triple spaced using a 
+ * derivative-based search strategy).
+ *
+ * The StepTolerance terminates optimization when the parameter values are
+ * known to be within this (scaled) distance of the local extreme.
+ *
+ * The ValueTolerance terminates optimization when the cost function values at
+ * the current parameters and at the local extreme are likely (within a second
+ * order approximation) to be within this is tolerance.
+ *
+ * \ingroup Numerics Optimizers
+ *
+ */
+
+class ITK_EXPORT FRPROptimizer: 
+    public PowellOptimizer
+{
+public:
+  /** Standard "Self" typedef. */
+  typedef FRPROptimizer                       Self ;
+  typedef PowellOptimizer                     Superclass;
+  typedef SmartPointer<Self>                  Pointer;
+  typedef SmartPointer<const Self>            ConstPointer;
+
+  typedef SingleValuedNonLinearOptimizer::ParametersType
+                                              ParametersType;
+  
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+ 
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(FRPROptimizer, PowellOptimizer );
+  
+  /** Type of the Cost Function   */
+  typedef  SingleValuedCostFunction         CostFunctionType;
+  typedef  CostFunctionType::Pointer        CostFunctionPointer;
+
+  /** Start optimization. */
+  void StartOptimization() ;
+
+protected:
+  FRPROptimizer() ;
+  virtual ~FRPROptimizer() ; 
+
+  void PrintSelf(std::ostream& os, Indent indent) const;
+
+  /** Get the value of the n-dimensional cost function at this scalar step
+   * distance along the current line direction from the current line origin.
+   * Line origin and distances are set via SetLine */
+  virtual void GetValueAndDerivative(ParametersType p, double * val,
+                                     ParametersType * xi);
+
+  virtual void   LineOptimize(ParametersType * p, ParametersType xi,
+                              double * val );
+
+
+private:
+  FRPROptimizer(const FRPROptimizer&) ; // not implemented
+
+} ; // end of class
+
+} // end of namespace itk
+
+#endif
