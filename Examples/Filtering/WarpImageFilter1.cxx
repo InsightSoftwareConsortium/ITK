1093a36a2ca5f6a243f522e21bf1c7606392eb1d
Karthik Krishnan <karthik.krishnan@kitware.com>
2005-04-02 17:30:07 -0500

    STYLE: Comments for Software Guide

--- Examples/Filtering/WarpImageFilter1.cxx
+++ Examples/Filtering/WarpImageFilter1.cxx
@@ -29,12 +29,15 @@
 //  Software Guide : EndLatex 
 
 
-// Software Guide : BeginCodeSnippet
 #include "itkImage.h"
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
+
+
+// Software Guide : BeginCodeSnippet
 #include "itkWarpImageFilter.h"
 #include "itkLinearInterpolateImageFunction.h"
+// Software Guide : EndCodeSnippet
 
 
 int main( int argc, char * argv[] )
@@ -48,17 +51,35 @@ int main( int argc, char * argv[] )
 
   const     unsigned int   Dimension = 2;
 
+  // Software Guide : BeginLatex
+  // The deformation field is represented as an image of vector pixel types. The
+  // dimenstion of the vectors is the same as the dimension of the input image.
+  // Each vector in the deformation field represents the distance between a 
+  // geometric point in the input space and a point in the output space such that:
+  // \begin{equation}
+  // p_{in} = p_{out} + distance
+  // \end{equation}
+  // Software Guide : EndLatex
+  // Software Guide : BeginCodeSnippet
   typedef   float VectorComponentType;
   typedef   itk::Vector< VectorComponentType, Dimension > VectorPixelType;
   typedef   itk::Image< VectorPixelType,  Dimension >   DeformationFieldType;
   
   typedef   unsigned char  PixelType;
   typedef   itk::Image< PixelType,  Dimension >   ImageType;
+  // Software Guide : EndCodeSnippet
 
 
   typedef   itk::ImageFileReader< ImageType >  ReaderType;
   typedef   itk::ImageFileWriter< ImageType >  WriterType;
+  
+  // Software Guide : BeginLatex
+  // The field is read from a file, through a reader instantiated over the
+  // vector pixel types.
+  // Software Guide : EndLatex
+  // Software Guide : BeginCodeSnippet
   typedef   itk::ImageFileReader< DeformationFieldType >  FieldReaderType;
+  // Software Guide : EndCodeSnippet
 
   ReaderType::Pointer reader = ReaderType::New();
   WriterType::Pointer writer = WriterType::New();
@@ -66,32 +87,54 @@ int main( int argc, char * argv[] )
   reader->SetFileName( argv[1] );
   writer->SetFileName( argv[3] );
 
+  // Software Guide : BeginCodeSnippet
   FieldReaderType::Pointer fieldReader = FieldReaderType::New();
   fieldReader->SetFileName( argv[2] );
   fieldReader->Update();
 
   DeformationFieldType::ConstPointer deformationField = fieldReader->GetOutput();
+  // Software Guide : EndCodeSnippet
 
 
 
+  // Software Guide : BeginLatex
+  // The \doxygen{WarpImageFilter} is templated over the input image type, output
+  // image type and the deformation field type. 
+  // Software Guide : EndLatex
+  // Software Guide : BeginCodeSnippet
   typedef itk::WarpImageFilter< ImageType, 
                                 ImageType, 
                                 DeformationFieldType  >  FilterType;
 
   FilterType::Pointer filter = FilterType::New();
+  // Software Guide : EndCodeSnippet
 
+  // Software Guide : BeginLatex
+  // Typically the mapped position does not correspond to an integer pixel position 
+  // in the input image. Interpolation via an image function is used to compute 
+  // values at non-integer positions.
+  // This is done via the \code{SetInterpolator()} method.
+  // \index{itk::Warp\-Image\-Filter!SetInterpolator()}
+  // Software Guide : EndLatex
+  // Software Guide : BeginCodeSnippet
   typedef itk::LinearInterpolateImageFunction< 
                        ImageType, double >  InterpolatorType;
 
   InterpolatorType::Pointer interpolator = InterpolatorType::New();
 
   filter->SetInterpolator( interpolator );
+  // Software Guide : EndCodeSnippet
 
-
+  // SoftwareGuide : BeginLatex
+  // The output image spacing and origin may be set via SetOutputSpacing(), 
+  // SetOutputOrigin(). This is taken from the deformation field.
+  // Software Guide : EndLatex
+  // Software Guide : BeginCodeSnippet
   filter->SetOutputSpacing( deformationField->GetSpacing() );
   filter->SetOutputOrigin(  deformationField->GetOrigin() );
 
   filter->SetDeformationField( deformationField );
+  // Software Guide : EndCodeSnippet
 
   filter->SetInput( reader->GetOutput() );
   writer->SetInput( filter->GetOutput() );
@@ -110,6 +153,5 @@ int main( int argc, char * argv[] )
 
   return EXIT_SUCCESS;
 
-  // Software Guide : EndCodeSnippet
 }
 
