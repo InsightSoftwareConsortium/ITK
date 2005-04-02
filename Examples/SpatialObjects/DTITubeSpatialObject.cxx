4e5c50a6452ebfb081d1116c042ead6de75811b7
Julien Jomier <julien.jomier@kitware.com>
2005-04-02 14:42:29 -0500

    ENH: DTITubeSpatialObject has now dynamic fields

--- Examples/SpatialObjects/DTITubeSpatialObject.cxx
+++ Examples/SpatialObjects/DTITubeSpatialObject.cxx
@@ -66,14 +66,10 @@ int main( int , char *[] )
 // \item The position of each point in the local coordinate system using the 
 // \code{SetPosition()} method.
 // \item The radius of the tube at this position using \code{SetRadius()}.
-// \item The FA value using \code{SetFA()}.
-// \item The ADC value using \code{SetADC()}.
-// \item The GA value using \code{SetGA()}.
-// \item The Eigen values value using \code{SetLambda1()},\code{SetLambda2()} and \code{SetLambda3()}.
-// \item The Eigen vectors value using \code{SetMinEigenVector()},\code{SetMedEigenVector()} and \code{SetMaxEigenVector()}.
-// \item The MRI value using \code{SetMRI()}.
+// \item The FA value using \code{AddField(DTITubePointType::FA)}.
+// \item The ADC value using \code{AddField(DTITubePointType::ADC)}.
+// \item The GA value using \code{AddField(DTITubePointType::GA)}.
 // \item The full tensor matrix supposed to be symetric definite positive value using \code{SetTensorMatrix()}.
-// \item The interpolation value using \code{SetInterpolation()}.
 // \item The color of the point is set to red in our case.
 // \end{enumerate}
 //
@@ -86,35 +82,19 @@ int main( int , char *[] )
     DTITubePointType p;
     p.SetPosition(i,i+1,i+2);
     p.SetRadius(1);
-    p.SetFA(i);
-    p.SetADC(2*i);
-    p.SetGA(3*i);
-    p.SetLambda1(4*i);
-    p.SetLambda2(5*i);
-    p.SetLambda3(6*i);
-    float* v = new float[3];
-    v[0] = i;
-    v[1] = 2*i;
-    v[2] = 3*i;
-    p.SetMinEigenVector(v);
-    p.SetMedEigenVector(v);
-    p.SetMaxEigenVector(v);
-    delete v;
-    v = new float[5];
-    for(unsigned int j=0;j<5;j++)
-      {
-      v[j] = j;
-      }
-    p.SetMRI(v);
-    delete v;
-    v = new float[6];
+    p.AddField(DTITubePointType::FA,i);
+    p.AddField(DTITubePointType::ADC,2*i);
+    p.AddField(DTITubePointType::GA,3*i);
+    p.AddField("Lambda1",4*i);
+    p.AddField("Lambda2",5*i);
+    p.AddField("Lambda3",6*i);
+    float* v = new float[6];
     for(unsigned int k=0;k<6;k++)
       {
       v[k] = k;
       }
     p.SetTensorMatrix(v);
     delete v;
-    p.SetInterpolation(1);
     p.SetColor(1,0,0,1);
     list.push_back(p);
     }
@@ -164,23 +144,18 @@ int main( int , char *[] )
     std::cout << "Point #" << i << std::endl;
     std::cout << "Position: " << (*it).GetPosition() << std::endl;
     std::cout << "Radius: " << (*it).GetRadius() << std::endl;
-    std::cout << "FA: " << (*it).GetFA() << std::endl;
-    std::cout << "ADC: " << (*it).GetADC() << std::endl;
-    std::cout << "GA: " << (*it).GetGA() << std::endl;
-    std::cout << "Lambda1: " << (*it).GetLambda1() << std::endl;
-    std::cout << "Lambda2: " << (*it).GetLambda2() << std::endl;
-    std::cout << "Lambda3: " << (*it).GetLambda3() << std::endl;
-    std::cout << "GetMinEigenVector: " << (*it).GetMinEigenVector()[0] << " : " << (*it).GetMinEigenVector()[1] << " : " << (*it).GetMinEigenVector()[2] << std::endl;
-    std::cout << "GetMedEigenVector: " << (*it).GetMedEigenVector()[0] << " : " << (*it).GetMedEigenVector()[1] << " : " << (*it).GetMedEigenVector()[2] << std::endl;
-    std::cout << "GetMaxEigenVector: " << (*it).GetMaxEigenVector()[0] << " : " << (*it).GetMaxEigenVector()[1] << " : " << (*it).GetMaxEigenVector()[2] << std::endl;   
-    std::cout << "MRI: " << (*it).GetMRI()[0] << " : " << (*it).GetMRI()[1] << " : " << (*it).GetMRI()[2] << " : " << (*it).GetMRI()[3] << " : " << (*it).GetMRI()[4] << std::endl;
+    std::cout << "FA: " << (*it).GetField(DTITubePointType::FA) << std::endl;
+    std::cout << "ADC: " << (*it).GetField(DTITubePointType::ADC) << std::endl;
+    std::cout << "GA: " << (*it).GetField(DTITubePointType::GA) << std::endl;
+    std::cout << "Lambda1: " << (*it).GetField("Lambda1") << std::endl;
+    std::cout << "Lambda2: " << (*it).GetField("Lambda2") << std::endl;
+    std::cout << "Lambda3: " << (*it).GetField("Lambda3") << std::endl;
     std::cout << "TensorMatrix: " << (*it).GetTensorMatrix()[0] << " : ";
     std::cout << (*it).GetTensorMatrix()[1] << " : ";
     std::cout << (*it).GetTensorMatrix()[2] << " : ";
     std::cout << (*it).GetTensorMatrix()[3] << " : ";
     std::cout << (*it).GetTensorMatrix()[4] << " : ";
     std::cout << (*it).GetTensorMatrix()[5] << std::endl;
-    std::cout << "Interpolation: " <<(*it).GetInterpolation() << std::endl;  
     std::cout << "Color = " << (*it).GetColor() << std::endl;
     it++;
     i++;
