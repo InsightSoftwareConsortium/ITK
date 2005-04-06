5545cffa85b5b1b8315b7fd1f120288a197cefec
Karthik Krishnan <karthik.krishnan@kitware.com>
2005-04-06 11:06:37 -0400

    STYLE: Missing copyright header

--- Code/Numerics/Statistics/itkHistogramToImageFilter.h
+++ Code/Numerics/Statistics/itkHistogramToImageFilter.h
@@ -1,3 +1,19 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkHistogramToImageFilter.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
 #ifndef __itkHistogramToImageFilter_h
 #define __itkHistogramToImageFilter_h
 
