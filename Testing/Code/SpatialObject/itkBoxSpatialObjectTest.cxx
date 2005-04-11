752f8befcaac4d5fef3db80087d01ed39fb476b9
Karthik Krishnan <karthik.krishnan@kitware.com>
2005-04-11 15:30:44 -0400

    COMP: Avoid rotation transforms with floating point computations, tests fail on some platforms

--- Testing/Code/SpatialObject/itkBoxSpatialObjectTest.cxx
+++ Testing/Code/SpatialObject/itkBoxSpatialObjectTest.cxx
@@ -33,126 +33,128 @@
 
 int itkBoxSpatialObjectTest( int argc, char *argv[] )
 {
-    const int Dimension = 2;
-    typedef itk::GroupSpatialObject< Dimension >       SceneType;
-    typedef itk::BoxSpatialObject< Dimension >         BoxType;
-    typedef itk::Image< unsigned char, Dimension >     OutputImageType;
-    typedef itk::ImageFileWriter< OutputImageType >    WriterType;
-    typedef itk::SpatialObjectToImageFilter< SceneType, OutputImageType >     
-                                        SpatialObjectToImageFilterType;
+  const int Dimension = 2;
+  typedef itk::GroupSpatialObject< Dimension >       SceneType;
+  typedef itk::BoxSpatialObject< Dimension >         BoxType;
+  typedef itk::Image< unsigned char, Dimension >     OutputImageType;
+  typedef itk::ImageFileWriter< OutputImageType >    WriterType;
+  typedef itk::SpatialObjectToImageFilter< SceneType, OutputImageType >     
+                                      SpatialObjectToImageFilterType;
 
-    SceneType::Pointer scene =  SceneType::New();
-    BoxType::Pointer box1 =     BoxType::New();
-    BoxType::Pointer box2 =     BoxType::New();
-    box1->SetId(1);
+  SceneType::Pointer scene =  SceneType::New();
+  BoxType::Pointer box1 =     BoxType::New();
+  BoxType::Pointer box2 =     BoxType::New();
+  box1->SetId(1);
 
-    scene->AddSpatialObject(box1);
-    scene->AddSpatialObject(box2);
+  scene->AddSpatialObject(box1);
+  scene->AddSpatialObject(box2);
 
-    BoxType::SizeType  boxsize1;
-    BoxType::SizeType  boxsize2;
+  BoxType::SizeType  boxsize1;
+  BoxType::SizeType  boxsize2;
 
-    boxsize1[0] = 30;
-    boxsize1[1] = 30;
-    box1->SetSize( boxsize1 );
-    boxsize2[0] = 30;
-    boxsize2[1] = 30;
-    box2->SetSize( boxsize2 );
+  boxsize1[0] = 30;
+  boxsize1[1] = 30;
+  box1->SetSize( boxsize1 );
+  boxsize2[0] = 30;
+  boxsize2[1] = 30;
+  box2->SetSize( boxsize2 );
 
-    BoxType::TransformType::OffsetType offset1;
-    BoxType::TransformType::OffsetType offset2;
+  BoxType::TransformType::OffsetType offset1;
+  BoxType::TransformType::OffsetType offset2;
 
-    offset1[0] =  29.0;
-    offset1[1] =  29.0;
-    box1->GetObjectToParentTransform()->SetOffset( offset1 );
-    box1->ComputeObjectToWorldTransform();
+  offset1[0] =  29.0;
+  offset1[1] =  29.0;
+  box1->GetObjectToParentTransform()->SetOffset( offset1 );
+  box1->ComputeObjectToWorldTransform();
 
-    offset2[0] = 71.0;
-    offset2[1] = 29.0;
-    box2->GetObjectToParentTransform()->SetOffset( offset2 );
-    box2->ComputeObjectToWorldTransform();
-    
-    box1->ComputeBoundingBox();
-    box2->ComputeBoundingBox();
-    
-    std::cout <<"Test ComputeBoundingBox: " << std::endl;
+  offset2[0] = 50.0;
+  offset2[1] = 50.0;
+  box2->GetObjectToParentTransform()->SetOffset( offset2 );
+  box2->ComputeObjectToWorldTransform();
+  
+  box1->ComputeBoundingBox();
+  box2->ComputeBoundingBox();
+  
+  std::cout <<"Test ComputeBoundingBox: " << std::endl;
+  std::cout << box1->GetBoundingBox()->GetBounds() << std::endl;
+  std::cout << box2->GetBoundingBox()->GetBounds() << std::endl;
+  BoxType::BoundingBoxType * boundingBox = box1->GetBoundingBox();
+  
+  if(     (boundingBox->GetBounds()[0]!= 29) 
+      ||  (boundingBox->GetBounds()[1]!= 59) 
+      ||  (boundingBox->GetBounds()[2]!= 29) 
+      ||  (boundingBox->GetBounds()[3]!= 59) )
+    {
+    std::cout << "[FAILED] Test returned" << std::endl;
     std::cout << box1->GetBoundingBox()->GetBounds() << std::endl;
-    std::cout << box2->GetBoundingBox()->GetBounds() << std::endl;
-    BoxType::BoundingBoxType * boundingBox = box1->GetBoundingBox();
-    
-    if(     (boundingBox->GetBounds()[0]!= 29) 
-        ||  (boundingBox->GetBounds()[1]!= 59) 
-        ||  (boundingBox->GetBounds()[2]!= 29) 
-        ||  (boundingBox->GetBounds()[3]!= 59) )
-      {
-      std::cout << "[FAILED] Test returned" << std::endl;
-      std::cout << box1->GetBoundingBox()->GetBounds() << std::endl;
-      std::cout << "Instead of [29 59 29 59]" << std::endl;
-      return EXIT_FAILURE;
-      }
-    std::cout << "[PASSED]" << std::endl;
-     
-    // Point consistency
-    std::cout << "Test Is Inside: ";
-    itk::Point<double,2> in;
-    in[0]=30.0;in[1]=30.0;
-    itk::Point<double,2> out;
-    out[0]=0;out[1]=4;
-
-    if(!box1->IsInside(in))
-    {
-      std::cout<<"[FAILED]"<<std::endl;
-      return EXIT_FAILURE;
+    std::cout << "Instead of [29 59 29 59]" << std::endl;
+    return EXIT_FAILURE;
     }
-    if(box1->IsInside(out))
+  std::cout << "[PASSED]" << std::endl;
+   
+  // Point consistency
+  std::cout << "Test Is Inside: ";
+  itk::Point<double,2> in;
+  in[0]=30.0;in[1]=30.0;
+  itk::Point<double,2> out;
+  out[0]=0;out[1]=4;
+
+  if(!box1->IsInside(in))
+  {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  }
+  if(box1->IsInside(out))
+  {
+    std::cout<<"[FAILED]"<<std::endl;
+    return EXIT_FAILURE;
+  } 
+  std::cout << "[PASSED]" << std::endl;
+       
+  std::cout << "Test ObjectToWorldTransform " << std::endl;
+ 
+  BoxType::TransformType::OffsetType translation;
+  translation[0] =  5.0;
+  translation[1] =  5.0;
+  box1->GetObjectToParentTransform()->Translate( translation );
+  box1->GetObjectToParentTransform()->SetOffset( offset1 );
+  box1->ComputeObjectToWorldTransform();
+  box2->GetObjectToParentTransform()->Translate( translation );
+  box2->GetObjectToParentTransform()->SetOffset( offset2 );
+  box2->ComputeObjectToWorldTransform();
+
+  SpatialObjectToImageFilterType::Pointer imageFilter = 
+                            SpatialObjectToImageFilterType::New();
+  imageFilter->SetInput(  scene  );
+
+  OutputImageType::SizeType size;
+  size[ 0 ] = 100;
+  size[ 1 ] = 100;
+  imageFilter->SetSize( size );
+
+  SpatialObjectToImageFilterType::PointType origin;
+  origin[0]=0; 
+  origin[1]=0;
+  imageFilter->SetOrigin( origin );
+  
+  imageFilter->SetInsideValue( 255 );
+  imageFilter->SetOutsideValue( 0 );
+  imageFilter->Update();
+
+  const char * outputFilename = argv[1];
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetFileName( outputFilename );
+  writer->SetInput( imageFilter->GetOutput() );
+  try
     {
-      std::cout<<"[FAILED]"<<std::endl;
-      return EXIT_FAILURE;
-    } 
-    std::cout << "[PASSED]" << std::endl;
-         
-    std::cout << "Test ObjectToWorldTransform " << std::endl;
-    
-    box1->GetObjectToParentTransform()->Rotate2D( 2*PI/360* 45 );
-    box1->GetObjectToParentTransform()->SetOffset( offset1 );
-    box1->ComputeObjectToWorldTransform();
-    box2->GetObjectToParentTransform()->Rotate2D( 2*PI/360* (45) );
-    box2->GetObjectToParentTransform()->SetOffset( offset2 );
-    box2->ComputeObjectToWorldTransform();
+    writer->Update();
+    }
+  catch( itk::ExceptionObject & err )
+    {
+    std::cout << "ExceptionObject caught !" << std::endl;
+    std::cout << err << std::endl;
+    return -1;
+    }
 
-    SpatialObjectToImageFilterType::Pointer imageFilter = 
-                              SpatialObjectToImageFilterType::New();
-    imageFilter->SetInput(  scene  );
-
-    OutputImageType::SizeType size;
-    size[ 0 ] = 100;
-    size[ 1 ] = 100;
-    imageFilter->SetSize( size );
-
-    SpatialObjectToImageFilterType::PointType origin;
-    origin[0]=0; 
-    origin[1]=0;
-    imageFilter->SetOrigin( origin );
-    
-    imageFilter->SetInsideValue( 255 );
-    imageFilter->SetOutsideValue( 0 );
-    imageFilter->Update();
-
-    const char * outputFilename = argv[1];
-    WriterType::Pointer writer = WriterType::New();
-    writer->SetFileName( outputFilename );
-    writer->SetInput( imageFilter->GetOutput() );
-    try
-      {
-      writer->Update();
-      }
-    catch( itk::ExceptionObject & err )
-      {
-      std::cout << "ExceptionObject caught !" << std::endl;
-      std::cout << err << std::endl;
-      return -1;
-      }
-
-  return 0;
-
+return EXIT_SUCCESS;
 }
