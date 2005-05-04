8a8beabd3c16f7571a4c122ca2a598471fd72015
Luis Ibanez <luis.ibanez@kitware.com>
2005-05-04 09:13:30 -0400

    ENH: Added check for the Trace computation.

--- Testing/Code/Common/itkSymmetricSecondRankTensorTest.cxx
+++ Testing/Code/Common/itkSymmetricSecondRankTensorTest.cxx
@@ -370,8 +370,46 @@ int itkSymmetricSecondRankTensorTest(int, char* [] )
 
     }
 
+  } // end of Test Eigen values computation
 
-  }
+
+  // Test GetTrace() method
+  {
+
+    typedef itk::SymmetricSecondRankTensor<double,3>         Double3DTensorType;
+    typedef Double3DTensorType::AccumulateValueType          AccumulateValueType;
+
+    Double3DTensorType tensor;
+
+    tensor(0,0) =  19.0;
+    tensor(0,1) =   0.0; 
+    tensor(0,2) =   0.0;
+    tensor(1,0) =   0.0; // overrides (0,1)
+    tensor(1,1) =  23.0;
+    tensor(1,2) =   0.0; 
+    tensor(2,0) =   7.0; // overrides (0,2)
+    tensor(2,1) =   0.0; // overrides (1,2)
+    tensor(2,2) =  29.0;
+
+    AccumulateValueType expectedTrace = 
+              itk::NumericTraits< AccumulateValueType >::Zero;
+
+    expectedTrace += tensor(0,0);
+    expectedTrace += tensor(1,1);
+    expectedTrace += tensor(2,2);
+
+    const double tolerance = 1e-4;
+
+    AccumulateValueType computedTrace = tensor.GetTrace();
+    if( fabs( computedTrace - expectedTrace ) > tolerance )
+      {
+      std::cerr << "Error computing the Trace" << std::endl;
+      std::cerr << "Expected trace = " << expectedTrace << std::endl;
+      std::cerr << "Computed trace = " << computedTrace << std::endl;
+      return EXIT_FAILURE;
+      }
+
+  } // end of Test GetTrace() method
 
   return EXIT_SUCCESS;
 }
