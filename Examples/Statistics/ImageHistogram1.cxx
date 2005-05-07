82085cc0e664cbb109f16d5f1b22c3041f296b5b
Luis Ibanez <luis.ibanez@kitware.com>
2005-05-07 14:15:05 -0400

    STYLE: Added comments for the Software Guide.

--- Examples/Statistics/ImageHistogram1.cxx
+++ Examples/Statistics/ImageHistogram1.cxx
@@ -20,18 +20,40 @@
 
 // Software Guide : BeginLatex
 //
-// This example shows how to compute the histogram of a scalar image.
+// This example shows how to compute the histogram of a scalar image.  Since
+// the classes of the Statistics framework operate on Samples and
+// ListOfSamples, we need to introduce a class that will make the image look
+// like a list of samples. This class is the \subdoxygen{Statistics}{ScalarImageToListAdaptor}.
+// Once we have connected this adaptor to an image, we can proceed to use the
+// \subdoxygen{Statistics}{ListSampleToHistogramGenerator} in order to compute the
+// \subdoxygen{Statistics}{Histogram} of the image.
+//
+// First, we need to include the headers for the
+// \subdoxygen{Statistics}{ScalarImageToListAdaptor} and the \doxygen{Image} classes.
+//
+// \index{itk::Statistics::ScalarImageToListAdaptor!header}
+// \index{Statistics!Images}
 //
 // Software Guide : EndLatex 
-
 
 // Software Guide : BeginCodeSnippet
 #include "itkScalarImageToListAdaptor.h"
 #include "itkImage.h"
 // Software Guide : EndCodeSnippet
 
+// Software Guide : BeginLatex
+//
+// Now we include the headers for the \code{ListSampleToHistogramGenerator} and
+// the reader that we will use for reading the image from a file.
+//
+// \index{itk::Statistics::ListSampleToHistogramGenerator!header}
+//
+// Software Guide : EndLatex 
+
+// Software Guide : BeginCodeSnippet
 #include "itkImageFileReader.h"
 #include "itkListSampleToHistogramGenerator.h"
+// Software Guide : EndCodeSnippet
 
 int main( int argc, char * argv [] )
 {
@@ -44,35 +66,105 @@ int main( int argc, char * argv [] )
     }
 
 
+
+// Software Guide : BeginLatex
+//
+// The image type must be defined using the typical pair of pixel type and
+// dimension specification.
+//
+// Software Guide : EndLatex 
+
+
+// Software Guide : BeginCodeSnippet
   typedef unsigned char       PixelType;
   const unsigned int          Dimension = 2;
 
   typedef itk::Image<PixelType, Dimension > ImageType;
+// Software Guide : EndCodeSnippet
 
+
+
+// Software Guide : BeginLatex
+//
+// Using the same image type we instantiate the type of the image reader that
+// will provide the image source for our example.
+//
+// Software Guide : EndLatex 
+
+
+// Software Guide : BeginCodeSnippet
   typedef itk::ImageFileReader< ImageType > ReaderType;
 
   ReaderType::Pointer reader = ReaderType::New();
 
   reader->SetFileName( argv[1] );
+// Software Guide : EndCodeSnippet
 
+
+// Software Guide : BeginLatex
+//
+// Now we introduce the central piece of this example, which is the use of the
+// adaptor that will present the \doxygen{Image} as if it was a list of
+// samples. We instantiate the type of the adaptor by using the actual image
+// type. Then construct the adaptor by invoking its \code{New()} method and
+// assigning the result to the corresponding smart pointer. Finally we connect
+// the output of the image reader to the input of the adaptor. 
+//
+// \index{itk::Statistics::ScalarImageToListAdaptor!instantiation}
+//
+// Software Guide : EndLatex 
+
+
+// Software Guide : BeginCodeSnippet
+  typedef itk::Statistics::ScalarImageToListAdaptor< ImageType >   AdaptorType;
+
+  AdaptorType::Pointer adaptor = AdaptorType::New();
+
+  adaptor->SetImage(  reader->GetOutput() );
+// Software Guide : EndCodeSnippet
+
+
+
+
+// Software Guide : BeginLatex
+//
+// You must keep in mind that adaptors are not pipeline objects. This means
+// that they do not propagate update calls. It is therefore your responsibility
+// to make sure that you invoke the \code{Update()} method of the reader before
+// you attempt to use the output of the adaptor. As usual, this must be done
+// inside a try/catch block because the read operation can potentially throw
+// exceptions.
+//
+// Software Guide : EndLatex 
+
+// Software Guide : BeginCodeSnippet
   try
     {
     reader->Update();
     }
   catch( itk::ExceptionObject & excp )
     {
-    std::cerr << "Problem encoutered while reading image file : " << argv[1] << std::endl;
+    std::cerr << "Problem reading image file : " << argv[1] << std::endl;
     std::cerr << excp << std::endl;
     return -1;
     }
+// Software Guide : EndCodeSnippet
 
 
-  typedef itk::Statistics::ScalarImageToListAdaptor< ImageType >   AdaptorType;
 
-  AdaptorType::Pointer adaptor = AdaptorType::New();
+// Software Guide : BeginLatex
+//
+// At this point, we are ready for instantiating the type of the histogram
+// generator. Note that the adaptor type is used as template parameter of the
+// generator. Having instantiated this type, we proceed to create one generator
+// by invoking its \code{New()} method.
+//
+// \index{itk::Statistics::ListSampleToHistogramGenerator!instantiation}
+//
+// Software Guide : EndLatex 
 
-  adaptor->SetImage(  reader->GetOutput() );
 
+// Software Guide : BeginCodeSnippet
   typedef PixelType        HistogramMeasurementType;
 
   typedef itk::Statistics::ListSampleToHistogramGenerator< 
@@ -81,7 +173,25 @@ int main( int argc, char * argv [] )
                                                                 > GeneratorType;
 
   GeneratorType::Pointer generator = GeneratorType::New();
+// Software Guide : EndCodeSnippet
 
+
+
+// Software Guide : BeginLatex
+//
+// We define now the characteristics of the Histogram that we want to compute.
+// This typically includes the size of each one of the component, but given
+// that in this simple example we are dealing with a scalar image, then our
+// histogram will have a single component. For the sake of generality, however,
+// we use the \code{HistogramType} as defined inside of the Generator type. We
+// define also the marginal scale factor that will control the precision used
+// when assigning values to histogram bines. Finally we invoke the
+// \code{Update()} method in the generator.
+//
+// Software Guide : EndLatex 
+
+
+// Software Guide : BeginCodeSnippet
   typedef GeneratorType::HistogramType  HistogramType;
 
   HistogramType::SizeType size;
@@ -90,10 +200,37 @@ int main( int argc, char * argv [] )
   generator->SetListSample( adaptor );
   generator->SetNumberOfBins( size );
   generator->SetMarginalScale( 10.0 );
+
   generator->Update();
+// Software Guide : EndCodeSnippet
 
+
+
+
+// Software Guide : BeginLatex
+//
+// Now we are ready for using the image histogram for any further processing.
+// The histogram is obtained from the generator by invoking the
+// \code{GetOutput()} method.
+//
+// Software Guide : EndLatex 
+
+
+// Software Guide : BeginCodeSnippet
   HistogramType::ConstPointer histogram = generator->GetOutput();
+// Software Guide : EndCodeSnippet
 
+
+
+// Software Guide : BeginLatex
+//
+// In this current example we simply print out the frequency values of all the
+// bins in the image histogram.
+//
+// Software Guide : EndLatex 
+
+
+// Software Guide : BeginCodeSnippet
   const unsigned int histogramSize = histogram->Size();
 
   std::cout << "Histogram size " << histogramSize << std::endl;
@@ -103,6 +240,7 @@ int main( int argc, char * argv [] )
     std::cout << "bin = " << bin << " frequency = ";
     std::cout << histogram->GetFrequency( bin, 0 ) <<std::endl;
     }
+// Software Guide : EndCodeSnippet
 
   return 0;
   
