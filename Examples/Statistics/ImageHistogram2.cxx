40a2ada5aae8450f49bee14ebe3f77ba09e26b27
Luis Ibanez <luis.ibanez@kitware.com>
2005-05-07 15:07:25 -0400

    STYLE: Added comment for the Software Guide.

--- Examples/Statistics/ImageHistogram2.cxx
+++ Examples/Statistics/ImageHistogram2.cxx
@@ -20,11 +20,28 @@
 
 // Software Guide : BeginLatex
 //
-// This example shows how to compute the histogram of a scalar image.
-// using the helper class \doxygen{ScalarImageToHistogramGenerator}.
+// From the previous example you would have noticed that there is a significant
+// number of operations to perform in order to compute the simple histogram of
+// a scalar image. Given that this is a relatively common operation, it is
+// convenient to encapsulate many of these operations in a single helper class. 
+//
+// The \subdoxygen{Statistics}{ScalarImageToHistogramGenerator} is the result
+// of such encapsulation.  This example illustrates how to compute the
+// histogram of a scalar image using this helper class.
 //
 // Software Guide : EndLatex 
 
+
+
+
+// Software Guide : BeginLatex
+//
+// We should first include the header of the histogram generator and the image
+// class.
+//
+// \index{itk::Statistics::ScalarImageToHistogramGenerator!header}
+//
+// Software Guide : EndLatex 
 
 // Software Guide : BeginCodeSnippet
 #include "itkScalarImageToHistogramGenerator.h"
@@ -44,10 +61,24 @@ int main( int argc, char * argv [] )
     }
 
 
+
+
+// Software Guide : BeginLatex
+//
+// The image type must be defined using the typical pair of pixel type and
+// dimension specification.
+//
+// Software Guide : EndLatex 
+
+
+// Software Guide : BeginCodeSnippet
   typedef unsigned char       PixelType;
   const unsigned int          Dimension = 2;
 
   typedef itk::Image<PixelType, Dimension > ImageType;
+// Software Guide : EndCodeSnippet
+
+
 
   typedef itk::ImageFileReader< ImageType > ReaderType;
 
@@ -61,26 +92,97 @@ int main( int argc, char * argv [] )
     }
   catch( itk::ExceptionObject & excp )
     {
-    std::cerr << "Problem encoutered while reading image file : " << argv[1] << std::endl;
+    std::cerr << "Problem reading image file : " << argv[1] << std::endl;
     std::cerr << excp << std::endl;
     return -1;
     }
 
+
+
+
+// Software Guide : BeginLatex
+//
+// We use now the image type in order to instantiate the type of the
+// corresponding histogram generator class, and invoke its \code{New()} method
+// in order to construct one.
+//
+// \index{itk::Statistics::ScalarImageToHistogramGenerator!header}
+//
+// Software Guide : EndLatex 
+
+// Software Guide : BeginCodeSnippet
   typedef itk::Statistics::ScalarImageToHistogramGenerator< 
-                                                    ImageType 
-                                                          >   HistogramGeneratorType;
+                                 ImageType >   HistogramGeneratorType;
 
-  HistogramGeneratorType::Pointer histogramGenerator = HistogramGeneratorType::New();
+  HistogramGeneratorType::Pointer histogramGenerator =
+                                        HistogramGeneratorType::New();
+// Software Guide : EndCodeSnippet
 
+
+
+
+
+// Software Guide : BeginLatex
+//
+// The image to be passed as input to the histogram generator is taken in this
+// case from the output of an image reader.
+//
+// Software Guide : EndLatex 
+
+// Software Guide : BeginCodeSnippet
   histogramGenerator->SetInput(  reader->GetOutput() );
+// Software Guide : EndCodeSnippet
 
+
+
+
+
+// Software Guide : BeginLatex
+//
+// We define also the typical parameters that specify the characteristics of
+// the histogram to be computed.
+//
+// Software Guide : EndLatex 
+
+// Software Guide : BeginCodeSnippet
   histogramGenerator->SetNumberOfBins( 255 );
   histogramGenerator->SetMarginalScale( 10.0 );
-  histogramGenerator->Compute();
+// Software Guide : EndCodeSnippet
 
+
+
+// Software Guide : BeginLatex
+//
+// Finally we trigger the computation of the histogram by invoking the
+// \code{Compute()} method of the generator. Note again, that a generator is
+// not a pipeline object and therefore it is up to you to make sure that the
+// filters providing the input image have been updated.
+//
+// \index{itk::Statistics::ScalarImageToHistogramGenerator!Compute()}
+//
+// Software Guide : EndLatex 
+
+// Software Guide : BeginCodeSnippet
+  histogramGenerator->Compute();
+// Software Guide : EndCodeSnippet
+
+
+
+// Software Guide : BeginLatex
+//
+// The resulting histogram can be obtained from the generator by invoking its
+// \code{GetOutput()} method. It is also convenient to get the Histogram type
+// from the traits of the generator type itself as shown in the code below.
+//
+// Software Guide : EndLatex 
+
+// Software Guide : BeginCodeSnippet
   typedef HistogramGeneratorType::HistogramType  HistogramType;
 
   const HistogramType * histogram = histogramGenerator->GetOutput();
+// Software Guide : EndCodeSnippet
+
+
 
   const unsigned int histogramSize = histogram->Size();
 
@@ -93,25 +195,31 @@ int main( int argc, char * argv [] )
     std::cout << histogram->GetFrequency( bin, 0 ) << std::endl;
     }
 
+
+
+
 // Software Guide : BeginLatex
 //
-// It is also possible to use iterators in order to visit the values
-// of the histogram bins.
+// In this case we simply print out the frequency values of the histogram.
+// These values can be accessed by using iterators.
+//
+// \index{itk::Statistics::Histogram!Iterators}
 //
 // Software Guide : EndLatex 
 
-
+// Software Guide : BeginCodeSnippet
   HistogramType::ConstIterator itr = histogram->Begin();
   HistogramType::ConstIterator end = histogram->End();
  
-  bin = 0;
+  unsigned int binNumber = 0;
   while( itr != end )
     {
-    std::cout << "bin = " << bin << " frequency = ";
+    std::cout << "bin = " << binNumber << " frequency = ";
     std::cout << itr.GetFrequency() << std::endl;     
     ++itr;
-    ++bin;
+    ++binNumber;
     }
+// Software Guide : EndCodeSnippet
 
 
   return 0;
