e4e83594fba66fd6a2720155027e45b355d4e2df
Luis Ibanez <luis.ibanez@kitware.com>
2005-05-15 20:53:31 -0400

    ENH: Added test for using itkMatrix or itkSymmetricSecondRankTensor as      template arguments.

--- Testing/Code/Common/itkSymmetricEigenAnalysisTest.cxx
+++ Testing/Code/Common/itkSymmetricEigenAnalysisTest.cxx
@@ -24,12 +24,14 @@
 #include "vnl/vnl_matrix.h"
 #include "itkFixedArray.h"
 #include "itkMatrix.h"
+#include "itkSymmetricSecondRankTensor.h"
 
 
 int itkSymmetricEigenAnalysisTest(int, char* [] )
 {
   // Test SymmetricEigenAnalysis class with symmetric matrices 
 
+  {
   // Test using vnl_matrix
   std::cout << "Testing ComputeEigenValuesAndVectors() "
     << "with SymmetricEigenAnalysis< vnl_matrix, itk::FixedArray, itk::Matrix >" 
@@ -78,7 +80,129 @@ int itkSymmetricEigenAnalysisTest(int, char* [] )
       return EXIT_FAILURE;
       }
     }
+  }
+
+  {
+  // Test using itk Matrix
+  std::cout << "Testing ComputeEigenValuesAndVectors() "
+    << "with SymmetricEigenAnalysis< itk::Matrix, itk::FixedArray, itk::Matrix >" 
+    << std::endl;
+  typedef itk::Matrix< double, 6, 6 > InputMatrixType;
+  typedef itk::FixedArray< double, 6 > EigenValuesArrayType;
+  typedef itk::Matrix< double, 6, 6 > EigenVectorMatrixType;
+  typedef itk::SymmetricEigenAnalysis< InputMatrixType,  
+      EigenValuesArrayType, EigenVectorMatrixType > SymmetricEigenAnalysisType;
+   
+  double Sdata[36] = {
+   30.0000,   -3.4273,   13.9254,   13.7049,   -2.4446,   20.2380,
+   -3.4273,   13.7049,   -2.4446,    1.3659,    3.6702,   -0.2282,
+   13.9254,   -2.4446,   20.2380,    3.6702,   -0.2282,   28.6779,
+   13.7049,    1.3659,    3.6702,   12.5273,   -1.6045,    3.9419,
+   -2.4446,    3.6702,   -0.2282,   -1.6045,    3.9419,    2.5821,
+   20.2380,   -0.2282,   28.6779,    3.9419,    2.5821,   44.0636,
+  };
+  
+  InputMatrixType S;
+
+  for(unsigned int row=0; row<6; row++)
+    {
+    for(unsigned int col=0; col<6; col++)
+      {
+      S[row][col] = Sdata[ row * 6 + col ];
+      }
+    }
+
+  EigenValuesArrayType eigenvalues;
+  EigenVectorMatrixType eigenvectors;
+  SymmetricEigenAnalysisType symmetricEigenSystem(6);
+  
+  symmetricEigenSystem.ComputeEigenValuesAndVectors(S, eigenvalues, eigenvectors );
+
+  std::cout << "EigenValues: " << eigenvalues << std::endl;
+  std::cout << "EigenVectors (each row is an an eigen vector): " << std::endl;
+  std::cout << eigenvectors << std::endl;
+
+  double eigvec3[6] = { 0.5236407,  -0.0013422,  -0.4199706,  -0.5942299,   0.4381326,   0.0659837 };
+  double eigvals[6]= {0.170864, 2.16934, 3.79272, 15.435, 24.6083, 78.2994}; 
+  
+  double tolerance = 0.01;
+  for( unsigned int i=0; i<6; i++ )
+    {
+    if (vnl_math_abs( eigvals[i] - eigenvalues[i] ) > tolerance)
+      {
+      std::cout << "Eigen value computation failed" << std::endl;
+      return EXIT_FAILURE;
+      }
+    
+     if (vnl_math_abs( eigvec3[i] - eigenvectors[2][i] ) > tolerance)
+      {
+      std::cout << "Eigen vector computation failed" << std::endl;
+      return EXIT_FAILURE;
+      }
+    }
+  }
+
+  {
+  // Test using itk SymmetricSecondRankTensor
+  std::cout << "Testing ComputeEigenValuesAndVectors() "
+    << "with SymmetricEigenAnalysis< itk::SymmetricSecondRankTensor, itk::FixedArray, itk::Matrix >" 
+    << std::endl;
+  typedef itk::SymmetricSecondRankTensor< double, 6 > InputMatrixType;
+  typedef itk::FixedArray< double, 6 > EigenValuesArrayType;
+  typedef itk::Matrix< double, 6, 6 > EigenVectorMatrixType;
+  typedef itk::SymmetricEigenAnalysis< InputMatrixType,  
+      EigenValuesArrayType, EigenVectorMatrixType > SymmetricEigenAnalysisType;
+   
+  double Sdata[36] = {
+   30.0000,   -3.4273,   13.9254,   13.7049,   -2.4446,   20.2380,
+   -3.4273,   13.7049,   -2.4446,    1.3659,    3.6702,   -0.2282,
+   13.9254,   -2.4446,   20.2380,    3.6702,   -0.2282,   28.6779,
+   13.7049,    1.3659,    3.6702,   12.5273,   -1.6045,    3.9419,
+   -2.4446,    3.6702,   -0.2282,   -1.6045,    3.9419,    2.5821,
+   20.2380,   -0.2282,   28.6779,    3.9419,    2.5821,   44.0636,
+  };
+  
+  InputMatrixType S;
+
+  for(unsigned int row=0; row<6; row++)
+    {
+    for(unsigned int col=0; col<6; col++)
+      {
+      S(row,col) = Sdata[ row * 6 + col ];
+      }
+    }
+
+  EigenValuesArrayType eigenvalues;
+  EigenVectorMatrixType eigenvectors;
+  SymmetricEigenAnalysisType symmetricEigenSystem(6);
+  
+  symmetricEigenSystem.ComputeEigenValuesAndVectors(S, eigenvalues, eigenvectors );
+
+  std::cout << "EigenValues: " << eigenvalues << std::endl;
+  std::cout << "EigenVectors (each row is an an eigen vector): " << std::endl;
+  std::cout << eigenvectors << std::endl;
+
+  double eigvec3[6] = { 0.5236407,  -0.0013422,  -0.4199706,  -0.5942299,   0.4381326,   0.0659837 };
+  double eigvals[6]= {0.170864, 2.16934, 3.79272, 15.435, 24.6083, 78.2994}; 
+  
+  double tolerance = 0.01;
+  for( unsigned int i=0; i<6; i++ )
+    {
+    if (vnl_math_abs( eigvals[i] - eigenvalues[i] ) > tolerance)
+      {
+      std::cout << "Eigen value computation failed" << std::endl;
+      return EXIT_FAILURE;
+      }
+    
+     if (vnl_math_abs( eigvec3[i] - eigenvectors[2][i] ) > tolerance)
+      {
+      std::cout << "Eigen vector computation failed" << std::endl;
+      return EXIT_FAILURE;
+      }
+    }
+  }
   std::cout << "[TEST PASSED]" << std::endl;
+
 return EXIT_SUCCESS;
 
 }
