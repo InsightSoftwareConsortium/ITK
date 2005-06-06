03d4c0631fd3c017d1eb8c1a75eda3af89151c3b
Luis Ibanez <luis.ibanez@kitware.com>
2005-06-06 15:51:53 -0400

    STYLE: Adding comments for the Software Guide.

--- Examples/IO/VectorImageReadWrite.cxx
+++ Examples/IO/VectorImageReadWrite.cxx
@@ -20,17 +20,30 @@
 
 //  Software Guide : BeginLatex
 //
-//  This example illustrates how to read and write an image of pixel type vector.
+//  This example illustrates how to read and write an image of pixel type
+//  \doxygen{Vector}.
 //
 //  \index{itk::ImageFileRead!Vector images}
+//  \index{itk::ImageFileWrite!Vector images}
+//  \index{Vector images!Reading}
+//  \index{Vector images!Writing}
 //
 //  Software Guide : EndLatex 
 
+
+
+// Software Guide : BeginLatex
+//
+// We should include the header files for the Image, the ImageFileReader and
+// the ImageFileWriter.
+//
+// Software Guide : EndLatex 
 
 // Software Guide : BeginCodeSnippet
 #include "itkImage.h"
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
+// Software Guide : EndCodeSnippet
 
 
 int main( int argc, char * argv [] )
@@ -44,26 +57,107 @@ int main( int argc, char * argv [] )
     return EXIT_FAILURE;
     }
 
+// Software Guide : BeginLatex
+//
+// Then we define the specific type of vector to be used as pixel type.
+//
+// Software Guide : EndLatex 
 
-  const unsigned int Dimension = 2;
+// Software Guide : BeginCodeSnippet
+  const unsigned int VectorDimension = 3;
 
-  typedef itk::Vector< float, Dimension >    PixelType;
-  typedef itk::Image< PixelType, Dimension > ImageType;
+  typedef itk::Vector< float, VectorDimension >    PixelType;
+// Software Guide : EndCodeSnippet
 
+
+// Software Guide : BeginLatex
+//
+// We define the image dimension, and along with the pixel type we use it for
+// fully instantiating the image type.
+//
+// Software Guide : EndLatex 
+
+// Software Guide : BeginCodeSnippet
+  const unsigned int ImageDimension = 2;
+  
+  typedef itk::Image< PixelType, ImageDimension > ImageType;
+// Software Guide : EndCodeSnippet
+
+
+
+// Software Guide : BeginLatex
+//
+// Having the image type at hand, we can instantiate the reader and writer
+// types, and use them for creating one object of each type.
+//
+// Software Guide : EndLatex 
+
+// Software Guide : BeginCodeSnippet
   typedef itk::ImageFileReader< ImageType > ReaderType;
   typedef itk::ImageFileWriter< ImageType > WriterType;
 
   ReaderType::Pointer reader = ReaderType::New();
   WriterType::Pointer writer = WriterType::New();
+// Software Guide : EndCodeSnippet
 
+
+// Software Guide : BeginLatex
+//
+// Filename must be provided to both the reader and the writer. In this
+// particular case we take those filenames from the command line arguments.
+//
+// Software Guide : EndLatex 
+
+// Software Guide : BeginCodeSnippet
   reader->SetFileName( argv[1] );
   writer->SetFileName( argv[2] );
-
-  writer->SetInput( reader->GetOutput() );
-
-  writer->Update();
-
 // Software Guide : EndCodeSnippet
+
+
+  
+// Software Guide : BeginLatex
+//
+// Being this a minimal example, we create a short pipeline where we simply
+// connect the output of the reader to the input of the writer.
+//
+// Software Guide : EndLatex 
+
+// Software Guide : BeginCodeSnippet
+  writer->SetInput( reader->GetOutput() );
+// Software Guide : EndCodeSnippet
+
+
+
+// Software Guide : BeginLatex
+//
+// The execution of this short pipeline is triggered by invoking the Update()
+// method of the writer. This invokation must be placed inside a try/catch
+// block since its execution may result in exeptions being thrown.
+//
+// Software Guide : EndLatex 
+
+// Software Guide : BeginCodeSnippet
+  try
+    {
+    writer->Update();
+    }
+  catch( itk::ExceptionObject & err ) 
+    { 
+    std::cout << "ExceptionObject caught !" << std::endl; 
+    std::cout << err << std::endl; 
+    return EXIT_FAILURE;
+    } 
+// Software Guide : EndCodeSnippet
+
+
+
+// Software Guide : BeginLatex
+//
+// Of course, you could envision the addition of filters in between the reader
+// and the writer. Those filters could perform operations on the vector image.
+//
+// Software Guide : EndLatex 
+
 
 
   return EXIT_SUCCESS;
