6664f0d3218e515e1b6d85014b25231e05824662
Luis Ibanez <luis.ibanez@kitware.com>
2005-06-08 14:19:40 -0400

    STYLE: Add better comments for the software guide.

--- Examples/IO/ImageSeriesReadWrite.cxx
+++ Examples/IO/ImageSeriesReadWrite.cxx
@@ -26,8 +26,8 @@
 //  of filenames that will provide a list of files to be read. In this
 //  particular example we use the \doxygen{NumericSeriesFileNames} class as
 //  filename generator. This generator uses a \code{printf} style of string format
-//  with a ``\%d'' field that will be succesively replaced by a number specified
-//  by the user. Here we will use a format like ``file\%04d.png'' for reading 
+//  with a ``\code{\%d}'' field that will be succesively replaced by a number specified
+//  by the user. Here we will use a format like ``\code{file\%04d.png}'' for reading 
 //  PNG files named file0001.png, file0002.png, file0003.png... and so on.
 //
 //  This requires the following headers as shown.
@@ -57,38 +57,38 @@ int main( int argc, char ** argv )
     }
 
 
-  // Software Guide : BeginLatex
-  //
-  // We start by defining the \code{PixelType} and \code{ImageType}.
-  //
-  //
-  // Software Guide : EndLatex 
+// Software Guide : BeginLatex
+//
+// We start by defining the \code{PixelType} and \code{ImageType}.
+//
+//
+// Software Guide : EndLatex 
 
-  // Software Guide : BeginCodeSnippet
+// Software Guide : BeginCodeSnippet
   typedef unsigned char                       PixelType;
   const unsigned int Dimension = 3;
 
   typedef itk::Image< PixelType, Dimension >  ImageType;
-  // Software Guide : EndCodeSnippet
+// Software Guide : EndCodeSnippet
 
 
-  // Software Guide : BeginLatex
-  //
-  // The image type is used as a template parameter to instantiate
-  // the reader and writer.
-  //
-  // \index{itk::ImageSeriesReader!Instantiation}
-  // \index{itk::ImageFileWriter!Instantiation}
-  //
-  // Software Guide : EndLatex 
+// Software Guide : BeginLatex
+//
+// The image type is used as a template parameter to instantiate
+// the reader and writer.
+//
+// \index{itk::ImageSeriesReader!Instantiation}
+// \index{itk::ImageFileWriter!Instantiation}
+//
+// Software Guide : EndLatex 
 
-  // Software Guide : BeginCodeSnippet
+// Software Guide : BeginCodeSnippet
   typedef itk::ImageSeriesReader< ImageType >  ReaderType;
   typedef itk::ImageFileWriter<   ImageType >  WriterType;
 
   ReaderType::Pointer reader = ReaderType::New();
   WriterType::Pointer writer = WriterType::New();
-  // Software Guide : EndCodeSnippet
+// Software Guide : EndCodeSnippet
 
 
   const unsigned int first = atoi( argv[1] );
@@ -96,71 +96,104 @@ int main( int argc, char ** argv )
 
   const char * outputFilename = argv[3];
 
-  // Software Guide : BeginCodeSnippet
+
+// Software Guide : BeginLatex
+//
+// Then, we declare the filenames generator type and create one instance of it.
+//
+// Software Guide : EndLatex 
+
+// Software Guide : BeginCodeSnippet
   typedef itk::NumericSeriesFileNames    NameGeneratorType;
 
   NameGeneratorType::Pointer nameGenerator = NameGeneratorType::New();
+// Software Guide : EndCodeSnippet
+
+
    
+// Software Guide : BeginLatex
+//
+// The filenames generator requires us to provide a pattern of text for the
+// filenames, and numbers for the initial value, last value and increment to be
+// used for generating the names of the files.
+//
+// Software Guide : EndLatex 
+
+// Software Guide : BeginCodeSnippet
+  nameGenerator->SetSeriesFormat( "vwe%04d.png" );
+
   nameGenerator->SetStartIndex( first );
   nameGenerator->SetEndIndex( last );
   nameGenerator->SetIncrementIndex( 1 );
-
-  nameGenerator->SetSeriesFormat( "vwe%04d.png" );
-  // Software Guide : EndCodeSnippet
+// Software Guide : EndCodeSnippet
 
 
 
-  //  Software Guide : BeginLatex
-  //
-  //  The ImageIO object that actually performs the read process
-  //  is now connected to the ImageSeriesReader.
-  //
-  //  Software Guide : EndLatex 
+//  Software Guide : BeginLatex
+//
+//  The ImageIO object that actually performs the read process is now connected
+//  to the ImageSeriesReader. This is the safest way of making sure that we use
+//  an ImageIO object that is appropriate for the type of files that we want to
+//  read.
+//
+//  Software Guide : EndLatex 
 
-  // Software Guide : BeginCodeSnippet
+// Software Guide : BeginCodeSnippet
   reader->SetImageIO( itk::PNGImageIO::New() );
-  // Software Guide : EndCodeSnippet
+// Software Guide : EndCodeSnippet
 
 
 
 
-  //  Software Guide : BeginLatex
-  //
-  //  The filenames of the input and output files must be provided to the
-  //  reader and writer respectively.
-  //
-  //  Software Guide : EndLatex 
+//  Software Guide : BeginLatex
+//
+//  The filenames of the input files must be provided to the reader. While the
+//  writer is instructed to write the same volume dataset in a single file.
+//  
+//
+//  Software Guide : EndLatex 
 
-  // Software Guide : BeginCodeSnippet
+// Software Guide : BeginCodeSnippet
   reader->SetFileNames( nameGenerator->GetFileNames()  );
 
   writer->SetFileName( outputFilename );
-  // Software Guide : EndCodeSnippet
+// Software Guide : EndCodeSnippet
 
-  try
+
+
+// Software Guide : BeginLatex
+//
+// We connect the output of the reader to the input of the writer.
+//
+// Software Guide : EndLatex 
+
+// Software Guide : BeginCodeSnippet
+  writer->SetInput( reader->GetOutput() );
+// Software Guide : EndCodeSnippet
+      
+
+
+//  Software Guide : BeginLatex
+//
+//  Finally, execution of the pipeline can be triggered by invoking the
+//  Update() method in the writer. This call must be placed in a try/catch
+//  block since exceptions be potentially be thrown in the process of reading
+//  or writing the images.
+//
+//  Software Guide : EndLatex 
+
+// Software Guide : BeginCodeSnippet
+  try 
     { 
-    reader->Update();
-    }
-  catch( itk::ExceptionObject & excp )
-    {
-    std::cerr << "Error reading the series " << std::endl;
-    std::cerr << excp << std::endl;
-    }
-
-  ImageType::Pointer image = reader->GetOutput();
-  writer->SetInput( image );
-
-
-  //  Software Guide : BeginLatex
-  //
-  //  Finally, execution of the pipeline can be triggered by invoking the
-  //  Update() method in the writer.
-  //
-  //  Software Guide : EndLatex 
-
-  // Software Guide : BeginCodeSnippet
-  writer->Update();
-  // Software Guide : EndCodeSnippet
+    writer->Update(); 
+    } 
+  catch( itk::ExceptionObject & err ) 
+    { 
+    std::cout << "ExceptionObject caught !" << std::endl; 
+    std::cout << err << std::endl; 
+    return EXIT_FAILURE;
+    } 
+// Software Guide : EndCodeSnippet
 
 
   return EXIT_SUCCESS;
