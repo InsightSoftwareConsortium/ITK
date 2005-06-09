a16d1be0e7bc626614788e333bdd6caba6428b04
Luis Ibanez <luis.ibanez@kitware.com>
2005-06-09 05:16:59 -0400

    STYLE: Added a longer introductory comment. Note however that this example        is the obsolete form of reading DICOM series. Therefore it is not        being included in the text of the Software Guide.

--- Examples/IO/DicomSeriesReadImageWrite.cxx
+++ Examples/IO/DicomSeriesReadImageWrite.cxx
@@ -20,8 +20,16 @@
 
 //  Software Guide : BeginLatex
 //
-//  This example illustrates how to read a DICOM series into a volume
-//  and then save this volume in another file format.
+//  Probably the most common representation of datasets in clinical
+//  applications is the one that uses sets of DICOM slices in order to compose
+//  tridimensional images. This is the case for CT, MRI and PET scanners. It is
+//  very common therefore for image analysists to have to process volumetric
+//  images that are stored in the form of a set of DICOM files belonging to a
+//  common DICOM series. 
+//
+//  The following example illustrates how to use ITK functionalities in order
+//  to read a DICOM series into a volume and then save this volume in another
+//  file format.
 //
 //  Software Guide : EndLatex 
 
