e3aa76d7a6fa90fc749b65839a4b9d6c8067485e
Luis Ibanez <luis.ibanez@kitware.com>
2005-06-11 14:49:46 -0400

    ENH: Example on the use of the CurvatureRegistrationFilter in a Multiresolution context.

---
+++ Examples/Registration/DeformableRegistration10.cxx
@@ -0,0 +1,218 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    DeformableRegistration10.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#ifndef USE_FFTW
+#error "This program needs FFTW to work."
+#endif
+
+
+#include "itkImageFileReader.h" 
+#include "itkImageFileWriter.h" 
+
+#include "itkCurvatureRegistrationFilter.h"
+#include "itkFastSymmetricForcesDemonsRegistrationFunction.h"
+#include "itkMultiResolutionPDEDeformableRegistration.h"
+
+#include "itkHistogramMatchingImageFilter.h"
+#include "itkCastImageFilter.h"
+#include "itkWarpImageFilter.h"
+#include "itkLinearInterpolateImageFunction.h"
+#include "itkExceptionObject.h"
+
+const unsigned int Dimension = 3;
+
+//  The following section of code implements a Command observer
+//  that will monitor the evolution of the registration process.
+//
+  class CommandIterationUpdate : public itk::Command 
+  {
+  public:
+    typedef  CommandIterationUpdate   Self;
+    typedef  itk::Command             Superclass;
+    typedef  itk::SmartPointer<CommandIterationUpdate>  Pointer;
+    itkNewMacro( CommandIterationUpdate );
+  protected:
+    CommandIterationUpdate() {};
+
+    typedef itk::Image< float, Dimension > InternalImageType;
+    typedef itk::Vector< float, Dimension >    VectorPixelType;
+    typedef itk::Image<  VectorPixelType, Dimension > DeformationFieldType;
+
+    typedef itk::CurvatureRegistrationFilter<
+                                InternalImageType,
+                                InternalImageType,
+                                DeformationFieldType,
+                                itk::FastSymmetricForcesDemonsRegistrationFunction<InternalImageType,InternalImageType,DeformationFieldType> >   RegistrationFilterType;
+
+  public:
+
+    void Execute(itk::Object *caller, const itk::EventObject & event)
+      {
+        Execute( (const itk::Object *)caller, event);
+      }
+
+    void Execute(const itk::Object * object, const itk::EventObject & event)
+      {
+         const RegistrationFilterType * filter = 
+          dynamic_cast< const RegistrationFilterType * >( object );
+        if( typeid( event ) != typeid( itk::IterationEvent ) )
+          {
+          return;
+          }
+        std::cout << filter->GetMetric() << std::endl;
+      }
+  };
+
+
+int main( int argc, char *argv[] )
+{
+  if( argc < 4 )
+    {
+    std::cerr << "Missing Parameters " << std::endl;
+    std::cerr << "Usage: " << argv[0];
+    std::cerr << " fixedImageFile movingImageFile ";
+    std::cerr << " outputImageFile " << std::endl;
+    return 1;
+    }
+
+  typedef short PixelType;
+
+  typedef itk::Image< PixelType, Dimension >  FixedImageType;
+  typedef itk::Image< PixelType, Dimension >  MovingImageType;
+  typedef itk::Image< float, Dimension > JacobianImageType;
+
+  typedef itk::ImageFileReader< FixedImageType  > FixedImageReaderType;
+  typedef itk::ImageFileReader< MovingImageType > MovingImageReaderType;
+
+  FixedImageReaderType::Pointer fixedImageReader   = 
+    FixedImageReaderType::New();
+  MovingImageReaderType::Pointer movingImageReader = 
+    MovingImageReaderType::New();
+
+  fixedImageReader->SetFileName( argv[1] );
+  movingImageReader->SetFileName( argv[2] );
+
+  typedef float InternalPixelType;
+  typedef itk::Image< InternalPixelType, Dimension > InternalImageType;
+  typedef itk::CastImageFilter< FixedImageType, 
+                                InternalImageType > FixedImageCasterType;
+  typedef itk::CastImageFilter< MovingImageType, 
+                                InternalImageType > MovingImageCasterType;
+
+  FixedImageCasterType::Pointer fixedImageCaster   = 
+    FixedImageCasterType::New();
+  MovingImageCasterType::Pointer movingImageCaster = 
+    MovingImageCasterType::New();
+
+  fixedImageCaster->SetInput( fixedImageReader->GetOutput() );
+  movingImageCaster->SetInput( movingImageReader->GetOutput() ); 
+
+  typedef itk::HistogramMatchingImageFilter<
+                                    InternalImageType,
+                                    InternalImageType >   MatchingFilterType;
+  MatchingFilterType::Pointer matcher = MatchingFilterType::New();
+
+  matcher->SetInput( movingImageCaster->GetOutput() );
+  matcher->SetReferenceImage( fixedImageCaster->GetOutput() );
+  matcher->SetNumberOfHistogramLevels( 1024 );
+  matcher->SetNumberOfMatchPoints( 7 );
+  matcher->ThresholdAtMeanIntensityOn();
+
+  typedef itk::Vector< float, Dimension >    VectorPixelType;
+  typedef itk::Image<  VectorPixelType, Dimension > DeformationFieldType;
+  typedef itk::CurvatureRegistrationFilter<
+                                InternalImageType,
+                                InternalImageType,
+                                DeformationFieldType,
+                                itk::FastSymmetricForcesDemonsRegistrationFunction<InternalImageType,InternalImageType,DeformationFieldType> >   RegistrationFilterType;
+  RegistrationFilterType::Pointer filter = RegistrationFilterType::New();
+  filter->SetTimeStep( 1 );
+  filter->SetConstraintWeight( 0.1 );
+
+  CommandIterationUpdate::Pointer observer = CommandIterationUpdate::New();
+  filter->AddObserver( itk::IterationEvent(), observer );
+
+  typedef itk::MultiResolutionPDEDeformableRegistration<
+                                InternalImageType,
+                                InternalImageType,
+                                DeformationFieldType >   MultiResRegistrationFilterType;
+  MultiResRegistrationFilterType::Pointer multires = MultiResRegistrationFilterType::New();
+  multires->SetRegistrationFilter( filter );
+  multires->SetNumberOfLevels( 3 );
+  multires->SetFixedImage( fixedImageCaster->GetOutput() );
+  multires->SetMovingImage( matcher->GetOutput() );
+
+  unsigned int nIterations[4] = { 10, 20, 50, 50 };
+  multires->SetNumberOfIterations( nIterations );
+  multires->Update();
+
+  typedef itk::WarpImageFilter<
+                          MovingImageType, 
+                          MovingImageType,
+                          DeformationFieldType  >     WarperType;
+  typedef itk::LinearInterpolateImageFunction<
+                                   MovingImageType,
+                                   double          >  InterpolatorType;
+  WarperType::Pointer warper = WarperType::New();
+  InterpolatorType::Pointer interpolator = InterpolatorType::New();
+  FixedImageType::Pointer fixedImage = fixedImageReader->GetOutput();
+
+  warper->SetInput( movingImageReader->GetOutput() );
+  warper->SetInterpolator( interpolator );
+  warper->SetOutputSpacing( fixedImage->GetSpacing() );
+  warper->SetOutputOrigin( fixedImage->GetOrigin() );
+  warper->SetDeformationField( multires->GetOutput() );
+
+  // Write warped image out to file
+  typedef unsigned short  OutputPixelType;
+  typedef itk::Image< OutputPixelType, Dimension > OutputImageType;
+  typedef itk::CastImageFilter< 
+                        MovingImageType,
+                        OutputImageType > CastFilterType;
+  typedef itk::ImageFileWriter< OutputImageType >  WriterType;
+
+  WriterType::Pointer      writer =  WriterType::New();
+  CastFilterType::Pointer  caster =  CastFilterType::New();
+
+  writer->SetFileName( argv[3] );
+  
+  caster->SetInput( warper->GetOutput() );
+  writer->SetInput( caster->GetOutput() );
+  writer->Update();
+
+  if( argc > 4 ) // if a fourth line argument has been provided...
+    {
+
+    typedef itk::ImageFileWriter< DeformationFieldType > FieldWriterType;
+
+    FieldWriterType::Pointer fieldWriter = FieldWriterType::New();
+    fieldWriter->SetFileName( argv[4] );
+    fieldWriter->SetInput( multires->GetOutput() );
+
+    try
+      {
+      fieldWriter->Update();
+      }
+    catch ( itk::ExceptionObject e )
+      {
+      e.Print( std::cerr );
+      }
+    }
+
+  return 0;
+}
+
