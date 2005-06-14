4ae470b5be48912a397aa527c4552841faa3fc74
Karthik Krishnan <karthik.krishnan@kitware.com>
2005-06-14 13:20:04 -0400

    ENH: Filters to compute vesselness measures for tubular structures based on eigen values

---
+++ Code/BasicFilters/itkHessian3DToVesselnessMeasureImageFilter.h
@@ -0,0 +1,118 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkHessian3DToVesselnessMeasureImageFilter.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkHessian3DToVesselnessMeasureImageFilter_h
+#define __itkHessian3DToVesselnessMeasureImageFilter_h
+
+#include "itkSymmetricSecondRankTensor.h"
+#include "itkSymmetricEigenAnalysisImageFilter.h"
+
+namespace itk
+{
+/** \class Hessian3DToVesselnessMeasureImageFilter
+ * \brief 
+ * Line filter to provide a vesselness measure for tubular objects from the
+ * hessian matrix. The filter takes as input an image of hessian pixels
+ * (SymmetricSecondRankTensor pixels) and preserves pixels that have
+ * eigen values \f[\lambda_3\f] close to 0 and \f[\lambda_2\f] and \f[\lambda_1\f] as
+ * large negative values. (for bright tubular structures).
+ * 
+ * \f[ \lambda_1 < \lambda_2 < \lambda_3 \f]
+ *  
+ *
+ * \par References: 
+ * "3D Multi-scale line filter for segmentation and visualization of 
+ * curvilinear structures in medical images", 
+ * Yoshinobu Sato, Shin Nakajima, Hideki Atsumi, Thomas Koller,
+ * Guido Gerig, Shigeyuki Yoshida, Ron Kikinis.
+ *
+ * http://splweb.bwh.harvard.edu:8000/pages/papers/yoshi/cr.html
+ *
+ * 
+ * \sa HessianRecursiveGaussianImageFilter 
+ * \sa SymmetricEigenAnalysisImageFilter
+ * \sa SymmetricSecondRankTensor
+ * 
+ * \ingroup IntensityImageFilters TensorObjects
+ *
+ */
+  
+template < typename  TPixel >
+class ITK_EXPORT Hessian3DToVesselnessMeasureImageFilter : public
+ImageToImageFilter< Image< SymmetricSecondRankTensor< double, 3 >, 3 >, 
+                                                  Image< TPixel, 3 > >
+{
+public:
+  /** Standard class typedefs. */
+  typedef Hessian3DToVesselnessMeasureImageFilter Self;
+  typedef ImageToImageFilter< 
+          Image< SymmetricSecondRankTensor< double, 3 >, 3 >, 
+          Image< TPixel, 3 > >                 Superclass;
+  typedef SmartPointer<Self>                   Pointer;
+  typedef SmartPointer<const Self>        ConstPointer;
+  
+  typedef typename Superclass::InputImageType            InputImageType;
+  typedef typename Superclass::OutputImageType           OutputImageType;
+  typedef typename InputImageType::PixelType             InputPixelType;
+  typedef TPixel                                         OutputPixelType;
+  
+  /** Image dimension = 3. */
+  itkStaticConstMacro(ImageDimension, unsigned int,
+                      ::itk::GetImageDimension<InputImageType>::ImageDimension);
+
+  typedef   itk::FixedArray< double, InputPixelType::Dimension >
+                                                          EigenValueArrayType;
+  typedef  itk::Image< EigenValueArrayType, InputImageType::ImageDimension >
+                                                          EigenValueImageType;
+  typedef   SymmetricEigenAnalysisImageFilter< 
+              InputImageType, EigenValueImageType >     EigenAnalysisFilterType;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+  
+  /** Set/Get macros for alpha_1 */
+  itkSetMacro(Alpha1, double);
+  itkGetMacro(Alpha1, double);
+  
+  /** Set/Get macros for alpha_1 */
+  itkSetMacro(Alpha2, double);
+  itkGetMacro(Alpha2, double);
+    
+    
+protected:
+  Hessian3DToVesselnessMeasureImageFilter();
+  ~Hessian3DToVesselnessMeasureImageFilter() {};
+  void PrintSelf(std::ostream& os, Indent indent) const;
+  
+  /** Generate Data */
+  void GenerateData( void );
+
+private:
+  Hessian3DToVesselnessMeasureImageFilter(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+  typename EigenAnalysisFilterType::Pointer         m_SymmetricEigenValueFilter;
+  double m_Alpha1;
+  double m_Alpha2;
+};
+
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkHessian3DToVesselnessMeasureImageFilter.txx"
+#endif
+  
+#endif
---
+++ Code/BasicFilters/itkHessian3DToVesselnessMeasureImageFilter.txx
@@ -0,0 +1,129 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkHessian3DToVesselnessMeasureImageFilter.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef _itkHessian3DToVesselnessMeasureImageFilter_txx
+#define _itkHessian3DToVesselnessMeasureImageFilter_txx
+
+#include "itkHessian3DToVesselnessMeasureImageFilter.h"
+#include "itkImageRegionIterator.h"
+#include "itkImageRegionConstIterator.h"
+#include "vnl/vnl_math.h"
+
+namespace itk
+{
+
+/**
+ * Constructor
+ */
+template < typename TPixel >
+Hessian3DToVesselnessMeasureImageFilter< TPixel >
+::Hessian3DToVesselnessMeasureImageFilter()
+{
+  m_Alpha1 = 0.5;
+  m_Alpha2 = 2.0;
+
+  // Hessian( Image ) = Jacobian( Gradient ( Image ) )  is symmetric
+  m_SymmetricEigenValueFilter = EigenAnalysisFilterType::New();
+  m_SymmetricEigenValueFilter->SetDimension( ImageDimension );
+  m_SymmetricEigenValueFilter->OrderEigenValuesBy( 
+      EigenAnalysisFilterType::FunctorType::OrderByValue );
+  
+}
+
+
+template < typename TPixel >
+void 
+Hessian3DToVesselnessMeasureImageFilter< TPixel >
+::GenerateData()
+{
+  itkDebugMacro(<< "Hessian3DToVesselnessMeasureImageFilter generating data ");
+
+  m_SymmetricEigenValueFilter->SetInput( this->GetInput() );
+  
+  typename OutputImageType::Pointer output = this->GetOutput();
+
+  typedef typename EigenAnalysisFilterType::OutputImageType
+                                                    EigenValueImageType;
+
+  m_SymmetricEigenValueFilter->Update();
+  
+  const typename EigenValueImageType::ConstPointer eigenImage = 
+                    m_SymmetricEigenValueFilter->GetOutput();
+  
+  // walk the region of eigen values and get the vesselness measure
+  EigenValueArrayType eigenValue;
+  ImageRegionConstIterator<EigenValueImageType> it;
+  it = ImageRegionConstIterator<EigenValueImageType>(
+      eigenImage, eigenImage->GetRequestedRegion());
+  ImageRegionIterator<OutputImageType> oit;
+  this->AllocateOutputs();
+  oit = ImageRegionIterator<OutputImageType>(output,
+                                             output->GetRequestedRegion());
+  oit.GoToBegin();
+  it.GoToBegin();
+  while (!it.IsAtEnd())
+    {
+    // Get the eigen value
+    eigenValue = it.Get();
+    
+    // normalizeValue <= 0 for bright line structures
+    double normalizeValue = vnl_math_min( -1.0 * eigenValue[1], -1.0 * eigenValue[0]);
+
+    // Similarity measure to a line structure
+    if( normalizeValue > 0 )
+      {
+      double lineMeasure = 0;
+      if( eigenValue[3] <= 0 )
+        {
+        lineMeasure = 
+          exp( -0.5 * vnl_math_sqr( eigenValue[2] / (m_Alpha1 * normalizeValue)));
+        }
+      else
+        {
+        lineMeasure = 
+          exp( -0.5 * vnl_math_sqr( eigenValue[2] / (m_Alpha1 * normalizeValue)));
+        }
+      
+      lineMeasure *= normalizeValue;
+      oit.Set( static_cast< OutputPixelType >(lineMeasure) );
+      }
+    else
+      {
+      oit.Set( NumericTraits< OutputPixelType >::Zero );
+      }    
+    
+
+    ++it;
+    ++oit;
+    }
+    
+}
+
+template < typename TPixel >
+void
+Hessian3DToVesselnessMeasureImageFilter< TPixel >
+::PrintSelf(std::ostream& os, Indent indent) const
+{
+  Superclass::PrintSelf(os, indent);
+  
+  os << indent << "Alpha1: " << m_Alpha1 << std::endl;
+  os << indent << "Alpha2: " << m_Alpha2 << std::endl;
+}
+
+
+} // end namespace itk
+  
+#endif
