bcf52f9be71831291532a9a10dcbff716f8a539f
Andinet Enquobahrie <andinet.enqu@kitware.com>
2005-07-13 16:14:55 -0400

    ENH: New test program for 2D polyline guided segmentation

---
+++ Testing/Code/BasicFilters/itkPolylineMask2DImageFilterTest.cxx
@@ -0,0 +1,159 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkPolylineMask2DImageFilterTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#include <itkImage.h>
+#include <itkImageRegionIteratorWithIndex.h>
+#include <itkPolylineMask2DImageFilter.h>
+#include <itkPolyLineParametricPath.h>
+#include <itkImageFileWriter.h>
+
+int itkPolylineMask2DImageFilterTest(int argc, char* argv [] ) 
+{
+
+ // Declare the types of the images
+  typedef itk::Image<unsigned char, 2>     inputImageType;
+  typedef itk::Image<unsigned char, 2>     outputImageType;
+  typedef itk::PolyLineParametricPath<2>     inputPolylineType;
+
+  // Declare the type of the index to access images
+  typedef inputImageType::IndexType         inputIndexType;
+
+  // Declare the type of the size 
+  typedef inputImageType::SizeType          inputSizeType;
+
+  // Declare the type of the Region
+  typedef inputImageType::RegionType         inputRegionType;
+
+  // Create images
+  inputImageType::Pointer inputImage    = inputImageType::New();
+ 
+  // Create polyline
+  inputPolylineType::Pointer inputPolyline   = inputPolylineType::New();
+
+  if (argc < 3)
+    {
+    std::cerr << "Usage: " << std::endl;
+    std::cerr << argv[0] << "  inputfilename outputfilename" << std::endl;
+    return 1;
+    }
+
+  // Define their size, and start index
+  inputSizeType size;
+  size[0] = 512;
+  size[1] = 512;
+
+
+  inputIndexType start;
+  start[0] = 0;
+  start[1] = 0;
+
+
+  inputRegionType region;
+  region.SetIndex( start );
+  region.SetSize( size );
+
+  // Initialize input image
+  inputImage->SetLargestPossibleRegion( region );
+  inputImage->SetBufferedRegion( region );
+  inputImage->SetRequestedRegion( region );
+  inputImage->Allocate();
+  inputImage->FillBuffer(0);
+
+  // Declare Iterator types apropriated for each image 
+  typedef itk::ImageRegionIteratorWithIndex<inputImageType>  inputIteratorType;
+
+  // Create one iterator for Image A (this is a light object)
+  inputIteratorType it( inputImage, inputImage->GetBufferedRegion() );
+  it.GoToBegin();
+  while( !it.IsAtEnd() ) 
+    {
+    /* fill in only the upper part of the image */
+      if(it.GetIndex()[1] > 256)
+        {
+        it.Set( 255 );
+        }
+      ++it;
+    }
+  
+ // Write out the input image for testing
+  typedef itk::ImageFileWriter< outputImageType >  WriterType;
+  WriterType::Pointer in_writer = WriterType::New();
+  const char * inputFilename = argv[1];
+  in_writer->SetFileName( inputFilename );
+  in_writer->SetInput( inputImage );
+  in_writer->Update();
+  
+
+  // Initialize the polyline 
+  typedef inputPolylineType::VertexType VertexType;
+    
+  // Add vertices to the polyline
+  VertexType v;
+  v[0] = 128;
+  v[1] = 256;
+  inputPolyline->AddVertex(v);
+  
+  v[0] = 256;
+  v[1] = 394;
+  inputPolyline->AddVertex(v);
+  
+  v[0] = 394;
+  v[1] = 256;
+  inputPolyline->AddVertex(v);
+
+  v[0] = 256;
+  v[1] = 128;
+  inputPolyline->AddVertex(v);
+  
+  // Declare the type for the Mask image filter
+  typedef itk::PolylineMask2DImageFilter<
+                           inputImageType, inputPolylineType,   
+                           outputImageType  >     inputFilterType;
+            
+
+  // Create a mask  Filter                                
+  inputFilterType::Pointer filter = inputFilterType::New();
+
+  // Connect the input image
+  filter->SetInput    ( inputImage ); 
+ 
+  // Connect the Polyline 
+  filter->SetInput    ( inputPolyline ); 
+
+  // Get the Smart Pointer to the Filter Output 
+  // outputImageType::Pointer outputImage = filter->GetOutput();
+
+  // Execute the filter
+  // filter->Update();
+
+  // Write out the image
+  WriterType::Pointer writer = WriterType::New();
+  const char * outputFilename = argv[2];
+  writer->SetFileName( outputFilename );
+  writer->SetInput( filter->GetOutput() );
+  writer->Update();
+  
+  return 0;
+
+}
+
+
+
+
