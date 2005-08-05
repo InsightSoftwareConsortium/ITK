76cfd4a1a1a7984fa44f91f033994ad45876d8bc
Gordon Kindlmann <gk@bwh.harvard.edu>
2005-08-05 19:21:54 -0400

    ENH: added tests for reading of NRRD RGB data

--- Testing/Code/IO/itkIOTests.cxx
+++ Testing/Code/IO/itkIOTests.cxx
@@ -43,6 +43,7 @@ void RegisterTests()
   REGISTER_TEST(itkMetaImageIOTest);
   REGISTER_TEST(itkNrrdImageIOTest);
   REGISTER_TEST(itkNrrdImageReadWriteTest);
+  REGISTER_TEST(itkNrrdRGBImageReadWriteTest);
   REGISTER_TEST(itkNumericSeriesFileNamesTest);
   REGISTER_TEST(itkPolygonGroupSpatialObjectXMLFileTest);
   REGISTER_TEST(itkPNGImageIOTest);
