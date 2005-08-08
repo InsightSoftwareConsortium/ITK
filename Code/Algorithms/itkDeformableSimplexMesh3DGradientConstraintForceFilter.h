faa043da8fbddedd73db3d6716ba57cb09904b9f
Leila Baghdadi <baghdadi@phenogenomics.ca>
2005-08-08 16:03:26 -0400

    BUG: replaced the old scan-line algorithm with a better one

--- Code/Algorithms/itkDeformableSimplexMesh3DGradientConstraintForceFilter.h
+++ Code/Algorithms/itkDeformableSimplexMesh3DGradientConstraintForceFilter.h
@@ -25,16 +25,63 @@
 #include "itkCovariantVector.h"
 
 #include <set>
-
+#include <vector>
 namespace itk
 {
+class ImageVoxel 
+      {
+        
+      public:
+        // voxel coordinates
+        unsigned int vpos[3];
+        // subvoxel coordinates (in cartesian space)
+        double  spos[3];
+        // voxel value converted to a double
+        double value;
+        // distance from line origin
+        double distance;
+        // index
+        unsigned int index;
 
+        ImageVoxel() {};
+        ImageVoxel(int *pos, double *subpos, double value, double distance, unsigned int index)
+          {
+            vpos[0] = pos[0];
+            vpos[1] = pos[1];
+            vpos[2] = pos[2];
+            spos[0] = subpos[0];
+            spos[1] = subpos[1];
+            spos[2] = subpos[2];
+            value = value;
+            distance = distance;
+            index = index;
+          };
+
+        /// returns voxel X coordinate (voxel column)
+        unsigned int getX(void) const { return vpos[0]; }
+        /// returns voxel Y coordinate (voxel row)
+        unsigned int getY(void) const { return vpos[1]; }
+        /// returns voxel Z coordinate (voxel plane)
+        unsigned int getZ(void) const { return vpos[2]; }
+        /// returns voxel distance to origin
+        double getDistance(void) const { return distance; }
+        /// returns voxel value
+        double getValue(void) const { return value; }
+        /// returns voxel position
+        
+        /// set the value of the voxel
+        void setValue(const double val) { value = val; }
+  
+      };
+  
 /** \class DeformableSimplexMesh3DGradientConstraintForceFilter
  * \brief
- * Additional to its superclass this class reimplemets the external forces 
- * in which the highest gradient is found in the direction of the normal to eac vertex.
+ * Additional to its superclass this class reimplemets the external forces methos
+ * in which the scan line algorithm is used to find highest gradient is found in 
+ * the direction of the normal to each vertex within a specified range.
  *
  * \author Leila Baghdadi. Mouse Imaging Centre, Hospital for Sick Children, Toronto, Ontario,Canada.
+ *  I acknowledge the helpful insights of Herve Delingette of INRIA, France.
  */
 
 template <class TInputMesh, class TOutputMesh>
@@ -72,10 +119,29 @@ public:
 
   typedef Image<PixelType, 3>                                   GradientIntensityImageType;
   typedef typename GradientIntensityImageType::Pointer          GradientIntensityImagePointer;
+
+  typedef Image<float, 3>                        OriginalImageType;
+  typedef typename OriginalImageType::IndexType           OriginalImageIndexType;
+  typedef typename OriginalImageIndexType::IndexValueType      ImageIndexValueType;
+  typedef typename OriginalImageType::Pointer             OriginalImagePointer;
   /** control the range of search for Bresenham at normal line */
   itkSetMacro(Range, int);
   itkGetMacro(Range, int);
+  
+  // full segment or half segment direction
+  enum SIDE {
+   // half segment in direction
+   NORMAL,
+   // half segment in -direction
+   INVERSE,
+   // complete segment
+   BOTH
+  };
 
+  /**
+   * Set Original image
+   */ 
+  itkSetMacro(Image, OriginalImagePointer);
 
 protected:
   DeformableSimplexMesh3DGradientConstraintForceFilter();
@@ -98,11 +164,21 @@ protected:
    */
   int m_Range;
 
- private:
-/**
-   *
-   */
-  GradientIndexType BresenhamLine(GradientIndexType a,GradientIndexType b);
+private:
+  double NextVoxel(const double* pp, int *ic, double *x, double *y, double *z);
+    
+  int signi(double a);
+
+  void clear();
+
+ // line starting voxel
+  ImageVoxel *startvoxel;
+  // line voxels in direction
+  std::vector<ImageVoxel *> positive;
+  // line voxels in -direction
+  std::vector<ImageVoxel *> negative;
+
+  OriginalImagePointer m_Image;
 
 }; // end of class
 
