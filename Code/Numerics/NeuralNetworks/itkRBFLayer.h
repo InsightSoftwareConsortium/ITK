8970abacb9cae6cfb1643a9864f495730aac71e5
Bill Lorensen <bill.lorensen@gmail.com>
2005-08-20 08:36:46 -0400

    BUG: improper signature for overriden virtual method.

--- Code/Numerics/NeuralNetworks/itkRBFLayer.h
+++ Code/Numerics/NeuralNetworks/itkRBFLayer.h
@@ -68,7 +68,7 @@ public:
   void ForwardPropagate(TVector input);
 
   void BackwardPropagate();
-  void BackwardPropagate(TVector errors){};
+  void BackwardPropagate(TOutput errors){};
 
   void SetOutputErrorValues(TOutput);
   ValueType GetOutputErrorValue(unsigned int node_id);
