4c57c6ec27f2d2a5278d5d126beeb4c58e06c082
Bill Lorensen <bill.lorensen@gmail.com>
2005-08-22 16:09:18 -0400

    ENH: try a few more random numbers.

--- Testing/Code/Numerics/Statistics/itkMersenneTwisterRandomVariateGeneratorTest.cxx
+++ Testing/Code/Numerics/Statistics/itkMersenneTwisterRandomVariateGeneratorTest.cxx
@@ -36,8 +36,8 @@ int itkMersenneTwisterRandomVariateGeneratorTest(int, char* [] )
 
   bool pass = true;
   
+  pass &= ComputeMeanAndVariance( 1000000UL );
   pass &= ComputeMeanAndVariance( 100000UL );
-  pass &= ComputeMeanAndVariance( 10000UL );
 
   if( pass )
     {
