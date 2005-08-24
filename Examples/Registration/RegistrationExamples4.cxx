d852ed396f848cd1c5e0b1be7e386e8e58f54b21
Bill Lorensen <bill.lorensen@gmail.com>
2005-08-24 07:26:37 -0400

    COMP: ITK_LEAN_AND_MEAN for Borland.

--- Examples/Registration/RegistrationExamples4.cxx
+++ Examples/Registration/RegistrationExamples4.cxx
@@ -3,6 +3,10 @@
 #ifdef _MSC_VER
 #pragma warning ( disable : 4786 )
 #endif
+#ifdef __BORLANDC__
+#define ITK_LEAN_AND_MEAN
+#endif
+
 #include <iostream>
 #include "vnl/vnl_sample.h"
 #include "itkTestMain.h" 
--- Examples/Segmentation/SegmentationExamples9.cxx
+++ Examples/Segmentation/SegmentationExamples9.cxx
@@ -3,6 +3,10 @@
 #if defined(_MSC_VER)
 #pragma warning ( disable : 4786 )
 #endif
+#ifdef __BORLANDC__
+#define ITK_LEAN_AND_MEAN
+#endif
+
 #include "itkTestMain.h" 
 #include <iostream>
 
