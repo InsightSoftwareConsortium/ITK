1ad80084d9b4bce0c56e026f90a3b0a6714a9c5f
Bill Lorensen <bill.lorensen@gmail.com>
2005-08-28 11:45:34 -0400

    COMP: Borland can now handle all Examples since itkDebugMacro is empty for the Borland compiler.

--- Examples/Registration/RegistrationExamples7.cxx
+++ Examples/Registration/RegistrationExamples7.cxx
@@ -3,9 +3,6 @@
 #ifdef _MSC_VER
 #pragma warning ( disable : 4786 )
 #endif
-#ifdef __BORLANDC__
-#define ITK_LEAN_AND_MEAN
-#endif
 
 #include <iostream>
 #include "vnl/vnl_sample.h"
@@ -15,15 +12,11 @@
 void RegisterTests()
 {
   vnl_sample_reseed(8775070);
-#ifndef __BORLANDC__
   REGISTER_TEST(ImageRegistration13Test);
-#endif
 }
 
-#ifndef __BORLANDC__
 #undef main
 #define main  ImageRegistration13Test
 #undef CommandIterationUpdate
 #define CommandIterationUpdate CommandIterationUpdate5
 #include "ImageRegistration13.cxx"
-#endif
