d7e36d65caef3fd90abcd53a380c9282135294b0
Bill Lorensen <bill.lorensen@gmail.com>
2005-09-05 12:56:30 -0400

    ENH: a ew class to accelerate image-related computations.

---
+++ Code/Common/itkImageHelper.h
@@ -0,0 +1,87 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkImageHelper.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkImageHelper_h
+#define __itkImageHelper_h
+
+#include "itkImageBase.h"
+#include "itkConceptChecking.h"
+
+namespace itk
+{
+
+/** \class ImageHelper
+ *  \brief Fast Index/Offset computation
+ */
+template <unsigned int NImageDimension, unsigned int NLoop>
+class ImageHelper
+{
+public:
+  typedef ImageBase<NImageDimension>         ImageType;
+  typedef typename ImageType::IndexType      IndexType;
+  typedef typename ImageType::OffsetType     OffsetType;
+  typedef typename ImageType::IndexValueType     IndexValueType;
+  typedef typename ImageType::OffsetValueType     OffsetValueType;
+  typedef Concept::Detail::UniqueType_bool<false> UniqueTypeBoolFalse;
+  typedef Concept::Detail::UniqueType_bool<true> UniqueTypeBoolTrue;
+
+  // ComputeIndex
+  //
+  inline static void ComputeIndex(const IndexType &bufferedRegionIndex,
+                                  OffsetValueType offset,
+                                  const OffsetValueType offsetTable[],
+                                  IndexType &index)
+    {
+      ImageHelper<NImageDimension,NLoop-1>::        
+      ComputeIndexInner(bufferedRegionIndex,
+                        offset,
+                        offsetTable,
+                        index,
+                        Concept::Detail::UniqueType_bool<(NLoop==1)>());
+    }
+
+  inline static void ComputeIndexInner(const IndexType &bufferedRegionIndex,    
+                                       OffsetValueType &offset,
+                                       const OffsetValueType offsetTable[],
+                                       IndexType &index,
+                                       const UniqueTypeBoolFalse& )
+  {
+    index[NLoop] = static_cast<IndexValueType>(offset / offsetTable[NLoop]);
+    offset -= (index[NLoop] * offsetTable[NLoop]);
+    index[NLoop] += bufferedRegionIndex[NLoop];
+    ImageHelper<NImageDimension, NLoop-1>::        
+      ComputeIndexInner(bufferedRegionIndex,
+                        offset,
+                        offsetTable,
+                        index,
+                        Concept::Detail::UniqueType_bool<(NLoop==1)>());
+    
+  }
+
+  inline static void ComputeIndexInner(const IndexType &bufferedRegionIndex,
+                                       OffsetValueType &offset,
+                                       const OffsetValueType offsetTable[],
+                                       IndexType &index,
+                                       const UniqueTypeBoolTrue& )
+  {
+    // Do last 
+    index[0] = bufferedRegionIndex[0] + static_cast<IndexValueType>(offset);
+  }
+};
+} // end namespace itk
+
+#endif
+
