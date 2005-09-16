bd0c81c2aeaa488e3d5d1cab812f93fa43fdba75
Karthik Krishnan <karthik.krishnan@kitware.com>
2005-09-16 11:00:27 -0400

    ENH: Pixel types to handle Vectors whose lengths can be specified at run-time, (meant to represent multi-variate vectors, providing mathematical operators much like itkVector

---
+++ Code/Common/itkVariableLengthVector.h
@@ -0,0 +1,419 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkVariableLengthVector.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef __itkVariableLengthVector_h
+#define __itkVariableLengthVector_h
+
+#include "itkMacro.h"
+
+namespace itk
+{
+/** \class VariableLengthVector 
+ * \brief VariableLengthVector is intended to reperesent an array whose
+ * length can be defined at run-time. 
+ *
+ * This class is templated over the data type. This data-type is meant
+ * to a scalar, such as float, double etc...
+ *
+ * \note
+ * ITK itself provides several classes that can serve as \c Arrays.
+ * 1. FixedArray - Compile time fixed length arrays that's intended to 
+ * represent an enumerated collection of \c n entities. 
+ * 2. Array - Run time resizeable array that is intended to hold a collection
+ * of \c n entities
+ * 3. Vector - Compile time fixed length array that is intended to hold
+ * a collection of \c n data types. A vector usually has a mathematical meaning. 
+ * It should only be used when mathematical operations such as addition, 
+ * multiplication by a scalar, product etc make sense. 
+ * 4. VariableLengthVector - Run time array that is intended to hold a collection
+ * of scalar data types. Again, it should be used only when mathematical
+ * operations on it are relevant. If not, use an Array. 
+ * 5. Point - Represents the spatial coordinates of a spatial location. Operators
+ * on Point reflect geometrical concepts.
+ *
+ * \par For the reasons listed above, you cannot instantiate 
+ * \code VariableLengthVector< bool > \endcode.
+ *
+ * \par
+ * Design Considerations: We do not derive from \c vnl_vector to avoid being
+ * limited by the explicit template instantiations of vnl_vector and other 
+ * hacks that vnl folks have been forced to use.
+ * 
+ * \note
+ * This work is part of the National Alliance for Medical Image Computing 
+ * (NAMIC), funded by the National Institutes of Health through the NIH Roadmap
+ * for Medical Research, Grant U54 EB005149.
+ *
+ * \sa CovariantVector
+ * \sa SymmetricSecondRankTensor
+ * \sa RGBPixel
+ * \sa DiffusionTensor3D
+ * \ingroup DataRepresentation 
+ */
+template <typename TValueType >
+class VariableLengthVector
+{
+public:
+ 
+  /** The element type stored at each location in the Array. */
+  typedef TValueType  ValueType;
+  typedef VariableLengthVector Self;
+
+  /** Typedef used to indicate the number of elements in the vector */
+  typedef unsigned int ElementIdentifier;
+  
+  /** Default constructor. It is created with an empty array
+   *  it has to be allocated later by assignment              */
+  VariableLengthVector(); 
+
+  /** Constructor with size. Size can only be changed by assignment */
+  VariableLengthVector(unsigned int dimension);
+
+  /** Constructor that initializes array with contents from a user supplied
+   * buffer. The pointer to the buffer and the length is specified. By default,
+   * the array does not manage the memory of the buffer. It merely points to 
+   * that location and it is the user's responsibility to delete it. 
+   * If "LetArrayManageMemory" is true, then this class will free the
+   * memory when this object is destroyed. */ 
+  VariableLengthVector( ValueType* data, unsigned int sz, 
+                                        bool LetArrayManageMemory = false);
+  
+  /** Constructor that initializes array with contents from a user supplied
+   * buffer. The pointer to the buffer and the length is specified. By default,
+   * the array does not manage the memory of the buffer. It merely points to 
+   * that location and it is the user's responsibility to delete it. 
+   * If "LetArrayManageMemory" is true, then this class will free the
+   * memory when this object is destroyed. */ 
+  VariableLengthVector( const ValueType* data, unsigned int sz, 
+                                        bool LetArrayManageMemory = false);
+
+
+  /** Copy constructor. The reason why the copy constructor and the assignment
+   * operator are templated is that it will allow implicit casts to be 
+   * performed. For instance
+   * \code
+   * VariableLengthVector< int > vI;
+   * VariableLengthVector< float > vF( vI );
+   * or for instance vF = static_cast< VariableLengthVector< float > >( vI );
+   * \endcode
+   **/
+  template< class T >
+  VariableLengthVector(const VariableLengthVector< T > & v)
+    {
+    m_NumElements = v.Size();
+    m_Data = this->AllocateElements(m_NumElements);
+    m_LetArrayManageMemory = true;
+    for( ElementIdentifier i=0; i< v.Size(); i++ )
+      {
+      this->m_Data[i] = static_cast< ValueType >( v[i] );
+      }
+    }
+
+  /** Copy constructer.. Override the default non-templated copy constructor
+   * that the compiler provides */
+  VariableLengthVector(const VariableLengthVector< TValueType > & v);
+  
+  /** Set the all the elements of the array to the specified value */
+  void Fill (TValueType const& v); 
+
+  /** Assignment operator  **/
+  const Self & operator=(const Self & v);
+  
+  /** Assignment operator  **/
+  template< class T >
+  const VariableLengthVector< TValueType > & operator= 
+                          (const VariableLengthVector< T > & v)
+    {
+    if( m_Data == static_cast< void * >(const_cast< T * >
+              ((const_cast< VariableLengthVector< T > & >(v)).GetDataPointer())) )
+      {
+      return *this;
+      }
+    this->SetSize( v.Size() );
+    for( ElementIdentifier i=0; i< v.Size(); i++ )
+      {
+      this->m_Data[i] = static_cast< ValueType >( v[i] );
+      }
+    return *this;
+    }
+      
+  /** Return the number of elements in the Array  */
+  inline unsigned int Size (void ) const 
+      { return m_NumElements; }
+  inline unsigned int GetNumberOfElements(void) const 
+      { return m_NumElements; }
+
+  /** Return reference to the element at specified index. No range checking. */
+  TValueType       & operator[](unsigned int i) { return this->m_Data[i]; }
+  /** Return reference to the element at specified index. No range checking. */
+  TValueType const & operator[](unsigned int i) const { return this->m_Data[i]; }
+
+  /** Get one element */
+  inline const TValueType & GetElement( unsigned int i ) const
+    { return m_Data[i]; }
+
+  /** Set one element */
+  void SetElement( unsigned int i, const TValueType & value )
+    { m_Data[i] = value; }
+
+  /** Set the size to that given. 
+   * 
+   * If \c destroyExistingData is \c false:
+   * If the array already contains data, the existing data is copied over and 
+   * new space is allocated, if necessary. If the length to reserve is less 
+   * than the current number of elements, then an appropriate number of elements
+   * are discarded. 
+   *    If \c true, the size is set destructively to the length given. If the 
+   * length is different from the current length, existing data will be lost.
+   * The default is \c true. */
+  void SetSize(unsigned int sz, bool destroyExistingData=true);
+  inline unsigned int GetSize(void) const 
+      { return m_NumElements; }
+
+  /** Set the pointer from which the data is imported.
+   * If "LetArrayManageMemory" is false, then the application retains
+   * the responsibility of freeing the memory for this data.  If
+   * "LetArrayManageMemory" is true, then this class will free the
+   * memory when this object is destroyed. */
+  void SetData(TValueType* data,bool LetArrayManageMemory = false);
+
+  /** Similar to the previous method. In the above method, the size must be 
+   * seperately set prior to using user-supplied data. This introduces an
+   * unnecessary allocation step to be performed. This method avoids it 
+   * and should be used to import data whereever possible to avoid this.
+   * Set the pointer from which the data is imported.
+   * If "LetArrayManageMemory" is false, then the application retains
+   * the responsibility of freeing the memory for this data.  If
+   * "LetArrayManageMemory" is true, then this class will free the
+   * memory when this object is destroyed. */
+  void SetData(TValueType* data, unsigned int sz, bool LetArrayManageMemory = false);
+
+  
+  /** This destructor is not virtual for performance reasons. However, this
+   * means that subclasses cannot allocate memory. */
+  ~VariableLengthVector();
+
+
+  /** Reserves memory of a certain length. 
+   *
+   * If the array already contains data, the existing data is copied over and 
+   * new space is allocated, if necessary. If the length to reserve is less 
+   * than the current number of elements, then an appropriate number of elements
+   * are discarded. */
+  void Reserve( ElementIdentifier );
+
+  /** Allocate memory of certain size and return it.  */
+  TValueType * AllocateElements( ElementIdentifier size ) const;
+
+  const TValueType* GetDataPointer() { return m_Data; }
+
+  /** Mathematical operators.
+   * \note For efficiency, the operators do not check to see of the length of 
+   * the vectors are the same. For instance it is assumed that if you are adding
+   * VariableLengthVector a and b, they are of the same length. */
+  template< class T > 
+  inline Self operator+(const VariableLengthVector< T > &v) const
+    {
+    // if( m_NumElements != v.GetSize() ) 
+    //   { 
+    //   itkGenericExceptionMacro( << "Cannot add VariableLengthVector of length " 
+    //                             << m_NumElements " and " << v.GetSize() );
+    //   }      
+    const ElementIdentifier length = v.Size();
+    Self result( length );
+    for( ElementIdentifier i=0; i< length; i++ )
+      {
+      result[i] = (*this)[i] + static_cast< ValueType >( v[i] );
+      }
+    return result;
+    }
+  template< class T > 
+  inline Self operator-(const VariableLengthVector< T > &v) const
+    {
+    // if( m_NumElements != v.GetSize() ) 
+    //   { 
+    //   itkGenericExceptionMacro( << "Cannot add VariableLengthVector of length " 
+    //                             << m_NumElements " and " << v.GetSize() );
+    //   }      
+    const ElementIdentifier length = v.Size();
+    Self result( length );
+    for( ElementIdentifier i=0; i< length; i++ )
+      {
+      result[i] = (*this)[i] - static_cast< ValueType >( v[i] );
+      }
+    return result;
+    }
+  template< class T > inline Self operator*( T s ) const
+    {
+    Self result( m_NumElements );
+    for( ElementIdentifier i=0; i< m_NumElements; i++ )
+      {
+      result[i] = m_Data[i] * static_cast< ValueType >( s );
+      }
+    return result;
+    }
+  template< class T > inline Self operator/( T s ) const
+    {
+    Self result( m_NumElements );
+    for( ElementIdentifier i=0; i< m_NumElements; i++ )
+      {
+      result[i] = m_Data[i] / (static_cast< ValueType >( s ));
+      }
+    return result;
+    }
+  template< class T > inline Self operator+( T s ) const
+    {
+    Self result( m_NumElements );
+    for( ElementIdentifier i=0; i< m_NumElements; i++ )
+      {
+      result[i] = m_Data[i] + (static_cast< ValueType >( s ));
+      }
+    return result;
+    }
+  template< class T > inline Self operator-( T s ) const
+    {
+    Self result( m_NumElements );
+    for( ElementIdentifier i=0; i< m_NumElements; i++ )
+      {
+      result[i] = m_Data[i] - (static_cast< ValueType >( s ));
+      }
+    return result;
+    }
+  inline Self& operator--()
+    {
+    for( ElementIdentifier i=0; i< m_NumElements; i++ )
+      {
+      this->m_Data[i] -= static_cast< ValueType >( 1.0 );
+      }
+    return *this;
+    }
+  inline Self& operator++() // prefix operator ++v;
+    {
+    for( ElementIdentifier i=0; i< m_NumElements; i++ )
+      {
+      this->m_Data[i] += static_cast< ValueType >( 1.0 );
+      }
+    return *this;
+    }
+  inline Self& operator--(int)
+    {
+    for( ElementIdentifier i=0; i< m_NumElements; i++ )
+      {
+      this->m_Data[i] -= static_cast< ValueType >( 1.0 );
+      }
+    return *this;
+    }
+  inline Self& operator++(int) // postfix operator v++;
+    {
+    for( ElementIdentifier i=0; i< m_NumElements; i++ )
+      {
+      this->m_Data[i] += static_cast< ValueType >( 1.0 );
+      }
+    return *this;
+    }
+   template< class T > inline Self& operator-=
+                  ( const VariableLengthVector< T > &v )
+    {
+    for( ElementIdentifier i=0; i< m_NumElements; i++ )
+      {
+      m_Data[i] -= static_cast< ValueType >( v[i] );
+      }
+    return *this;
+    }
+  template< class T > inline Self& operator-=( T s )
+    {
+    for( ElementIdentifier i=0; i< m_NumElements; i++ )
+      {
+      m_Data[i] -= (static_cast< ValueType >( s ));
+      }
+    return *this;
+    }
+  template< class T > inline Self& operator+=
+                  ( const VariableLengthVector< T > &v )
+    {
+    for( ElementIdentifier i=0; i< m_NumElements; i++ )
+      {
+      m_Data[i] += static_cast< ValueType >( v[i] );
+      }
+    return *this;
+    }
+  template< class T > inline Self& operator+=( T s )
+    {
+    for( ElementIdentifier i=0; i< m_NumElements; i++ )
+      {
+      m_Data[i] += (static_cast< ValueType >( s ));
+      }
+    return *this;
+    }
+  template< class T > inline Self& operator*=( T s )
+    {
+    for( ElementIdentifier i=0; i< m_NumElements; i++ )
+      {
+      m_Data[i] *= (static_cast< ValueType >( s ));
+      }
+    return *this;
+    }
+  template< class T > inline Self& operator/=( T s )
+    {
+    for( ElementIdentifier i=0; i< m_NumElements; i++ )
+      {
+      m_Data[i] /= (static_cast< ValueType >( s ));
+      }
+    return *this;
+    }
+  Self & operator- (); // negation operator
+  bool operator==( const Self &v) const;
+  bool operator!=( const Self &v) const;
+    
+
+
+private:
+
+  bool m_LetArrayManageMemory; // if true, the array is responsible for memory 
+                               // of data
+  TValueType *m_Data; // Array to hold data
+  ElementIdentifier m_NumElements;
+};
+
+
+  
+template <typename TValueType >
+std::ostream & operator<<(std::ostream &os, const VariableLengthVector<TValueType> &arr)
+{
+  const unsigned int length = arr.Size();
+  const signed int last   = (unsigned int) length - 1;
+
+  os << "[";
+  for (signed int i=0; i < last; ++i)
+    {
+    os << arr[i] << ", ";
+    }
+  if (length >= 1)
+    {
+    os << arr[last];
+    }
+  os << "]";
+  return os;
+}
+
+} // namespace itk
+
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkVariableLengthVector.txx"
+#endif
+
+#endif
---
+++ Code/Common/itkVariableLengthVector.txx
@@ -0,0 +1,298 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkVariableLengthVector.txx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#ifndef _itkVariableLengthVector_txx
+#define _itkVariableLengthVector_txx
+
+#include "itkVariableLengthVector.h"
+#include "itkNumericTraitsVariableLengthVectorPixel.h"
+
+namespace itk
+{
+
+/** Default constructor  */
+template < typename TValueType >
+VariableLengthVector<TValueType >
+::VariableLengthVector() : 
+  m_LetArrayManageMemory( true ),
+  m_Data( 0 ),
+  m_NumElements( 0 ) {}
+
+
+/** Constructor with size */
+template < typename TValueType >
+VariableLengthVector<TValueType >
+::VariableLengthVector(unsigned int length) :
+  m_LetArrayManageMemory( true ),
+  m_Data( 0 )
+{
+  Reserve( length );
+}
+
+/** Constructor with user specified data */
+template < typename TValueType >
+VariableLengthVector<TValueType >
+::VariableLengthVector( ValueType *datain, unsigned int sz, bool LetArrayManageMemory) :
+  m_LetArrayManageMemory( LetArrayManageMemory ),
+  m_Data( datain ),
+  m_NumElements( sz ) {}
+
+/** Constructor with user specified data */
+template < typename TValueType >
+VariableLengthVector<TValueType >
+::VariableLengthVector( const ValueType *datain, unsigned int sz, bool LetArrayManageMemory)
+  : m_LetArrayManageMemory( LetArrayManageMemory )
+{
+  m_Data = const_cast< ValueType * >(datain);
+  m_NumElements = sz;
+}
+
+/** Copy constructer.. Override the default non-templated copy constructor
+ * that the compiler provides */
+template < typename TValueType >
+VariableLengthVector<TValueType >
+::VariableLengthVector(const VariableLengthVector< TValueType > & v)
+{
+  m_NumElements = v.Size();
+  m_Data = this->AllocateElements(m_NumElements);
+  m_LetArrayManageMemory = true;
+  for( ElementIdentifier i=0; i< v.Size(); i++ )
+    {
+    this->m_Data[i] = v[i];
+    }
+}
+
+
+/** Destructor*/
+template < typename TValueType >
+VariableLengthVector<TValueType >
+::~VariableLengthVector()
+{
+  // if data exists and we are responsible for its memory, get rid of it.. 
+  if(m_LetArrayManageMemory && m_Data)
+    {
+    delete [] m_Data;
+    }
+}
+
+/** Reserve memory of certain size for m_Data */
+template < typename TValueType >
+void VariableLengthVector<TValueType >
+::Reserve( ElementIdentifier size )
+{
+  if( m_Data )
+    {
+    TValueType *temp = this->AllocateElements( size );
+    if( size> m_NumElements )
+      { 
+      // only copy the portion of the data used in the old buffer
+      memcpy(temp, m_Data, m_NumElements*sizeof(TValueType));
+      if (m_Data && m_LetArrayManageMemory)
+        {
+        delete[] m_Data;
+        }
+      m_Data = temp;
+      m_LetArrayManageMemory = true;
+      m_NumElements = size;
+      }
+    }
+  else
+    {
+    m_Data = this->AllocateElements(size);
+    m_NumElements = size;
+    m_LetArrayManageMemory = true;
+    }
+}
+        
+      
+/** Allocate memory of certain size and return it */
+template < typename TValueType >
+TValueType * VariableLengthVector< TValueType >
+::AllocateElements( ElementIdentifier size ) const
+{
+  TValueType *data;
+  try 
+    {
+    data = new TValueType[ size ];
+    }
+  catch(...)
+    {
+    data = 0;
+    }
+  if( !data )
+    {
+    itkGenericExceptionMacro( << "Failed to allocate memory of length " << size 
+                              << " for VariableLengthVector.");
+    }
+  return data;
+}
+
+
+/** Set the pointer from which the data is imported.
+ * If "LetArrayManageMemory" is false, then the application retains
+ * the responsibility of freeing the memory for this data.  If
+ * "LetArrayManageMemory" is true, then this class will free the
+ * memory when this object is destroyed. Note that you need to explicitly
+ * set the number of elements.*/
+template < typename TValueType >
+void 
+VariableLengthVector<TValueType >
+::SetData(TValueType* datain,bool LetArrayManageMemory)
+{
+  // Free any existing data if we manage its memory
+  if(m_LetArrayManageMemory && m_Data)   
+    {
+    delete [] m_Data;
+    }  
+  
+  m_LetArrayManageMemory = LetArrayManageMemory;
+  m_Data = datain;
+}
+
+
+/** Similar to the previous method. In the above method, the size must be 
+ * seperately set prior to using user-supplied data. This introduces an
+ * unnecessary allocation step to be performed. This method avoids it 
+ * and should be used to import data whereever possible to avoid this.
+ * Set the pointer from which the data is imported.
+ * If "LetArrayManageMemory" is false, then the application retains
+ * the responsibility of freeing the memory for this data.  If
+ * "LetArrayManageMemory" is true, then this class will free the
+ * memory when this object is destroyed. */
+template < typename TValueType >
+void 
+VariableLengthVector<TValueType >
+::SetData(TValueType* datain, unsigned int sz, bool LetArrayManageMemory)
+{
+  // Free any existing data if we manage its memory
+  if(m_LetArrayManageMemory && m_Data)
+    {
+    delete [] m_Data;
+    }
+
+  m_LetArrayManageMemory = LetArrayManageMemory;
+  m_Data = datain;
+  m_NumElements = sz;
+}
+
+
+template < typename TValueType >
+void VariableLengthVector<TValueType >
+::SetSize(unsigned int sz, bool destroyExistingData)
+{
+  if( destroyExistingData )
+    {
+    // Free any existing data if we manage its memory and if we need to destroy
+    if(!m_LetArrayManageMemory)
+      {
+      m_Data=0;
+      m_NumElements = 0;
+      }
+    else if( m_Data )
+      {
+      if( m_NumElements != sz )
+        {
+        delete [] m_Data;
+        }
+      else return;
+      }
+    }
+
+  if ( m_NumElements != sz )
+    {
+    Reserve( sz );
+    }
+}
+
+/** Set the all the elements of the array to the specified value */
+template < typename TValueType > 
+void VariableLengthVector<TValueType >
+::Fill (TValueType const& v) 
+{
+  for( ElementIdentifier i=0; i< m_NumElements; i++ )
+    {
+    this->m_Data[i] = v;
+    }
+}
+
+/** Assignment operator  **/
+template < typename TValueType > const VariableLengthVector< TValueType >& 
+VariableLengthVector<TValueType >
+::operator=(const Self & v)
+{
+  if( this == &v )
+    {
+    return *this;
+    }
+  this->SetSize( v.Size() );
+  for( ElementIdentifier i=0; i< v.Size(); i++ )
+    {
+    this->m_Data[i] = v[i];
+    }
+  return *this;
+}
+
+template < typename TValueType > VariableLengthVector< TValueType >& 
+VariableLengthVector<TValueType >
+::operator- ()
+{
+  for( ElementIdentifier i=0; i< m_NumElements; i++ )
+    {
+    m_Data[i] = -m_Data[i];
+    }
+  return *this;
+}
+
+template < typename TValueType > bool 
+VariableLengthVector<TValueType >
+::operator==( const Self & v) const
+{
+  if( m_NumElements != v.Size() ) 
+    {
+    return false;
+    }
+  for( ElementIdentifier i=0; i< m_NumElements; i++ )
+    {
+    if( m_Data[i] != v[i] ) 
+      { 
+      return false; 
+      }
+    }
+  return true;
+}
+
+template < typename TValueType > bool 
+VariableLengthVector<TValueType >
+::operator!=( const Self & v) const
+{
+  if( m_NumElements != v.Size() ) 
+    {
+    return true;
+    }
+  for( ElementIdentifier i=0; i< m_NumElements; i++ )
+    {
+    if( m_Data[i] != v[i] ) 
+      { 
+      return true; 
+      }
+    }
+  return false;
+}
+
+
+} // namespace itk
+
+#endif
