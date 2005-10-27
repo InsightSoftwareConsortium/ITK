12692db6db03a72322785e75245fd4ef07685cbb
Bill Lorensen <bill.lorensen@gmail.com>
2005-10-27 00:08:09 -0400

    BUG: Signature was wrong for main program. BUG: Missing copyright.

--- Testing/Code/Common/itkQuaternionOrientationAdapterTest.cxx
+++ Testing/Code/Common/itkQuaternionOrientationAdapterTest.cxx
@@ -1,3 +1,23 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkQuaternionOrientationAdapterTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
 #include <iostream>
 #include <itkSpatialOrientation.h>
 #include <itkSpatialOrientationAdapter.h>
@@ -199,7 +219,7 @@ void printDirections(const std::string &prompt,
 }
 
 
-int itkQuaternionOrientationAdapterTest(int argc, char **argv)
+int itkQuaternionOrientationAdapterTest(int argc, char *argv[])
 {
   
   for(unsigned i = 0; i < sizeof(allOrientations)/sizeof(SO_OrientationType); i++)
