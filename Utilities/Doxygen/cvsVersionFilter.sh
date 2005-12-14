4b0a83c8c2b00b177ed351465f735cbe9e375300
Luis Ibanez <luis.ibanez@kitware.com>
2005-12-14 12:44:49 -0500

    ENH: adding the CVSROOT specification.  Still holds the limitation that it has      to be launched from the checkout directory.

--- Utilities/Doxygen/cvsVersionFilter.sh
+++ Utilities/Doxygen/cvsVersionFilter.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cvs status $1 | sed -n 's/^[ \]*Working revision:[ \t]*\([0-9][0-9\.]*\).*/\1/p'
+cvs -d :pserver:anonymous@www.itk.org:/cvsroot/Insight status $1 | sed -n 's/^[ \]*Working revision:[ \t]*\([0-9][0-9\.]*\).*/\1/p'
 
