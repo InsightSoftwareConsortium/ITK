fbc427ec15b095219dccf9e5990ce0b84fdec203
Luis Ibanez <luis.ibanez@kitware.com>
2006-03-06 08:40:58 -0500

    ENH: Support files for testing the classes that are under the review process.

---
+++ Testing/Code/Review/itkReviewPrintTest.cxx
@@ -0,0 +1,39 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkReviewPrintTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#include "itkThresholdMaximumConnectedComponentsImageFilter.h"
+
+int main(int , char* [])
+{
+  typedef itk::Image<float,2> InputType; 
+  typedef itk::Image<float,2> OutputType;
+  typedef itk::Image<unsigned short,2> UShortImageType;
+  typedef itk::Image<unsigned char,2> CharType;
+
+  typedef itk::Vector<float,2> VectorType;
+  typedef itk::Image<VectorType, 2> VectorImageType;
+
+  itk::ThresholdMaximumConnectedComponentsImageFilter<InputType>::Pointer ThresholdMaximumConnectedComponentsImageFilterObj =
+  itk::ThresholdMaximumConnectedComponentsImageFilter<InputType,InputType>::New();
+  std:: cout << "-------------ThresholdMaximumConnectedComponentsImageFilter " << ThresholdMaximumConnectedComponentsImageFilterObj;
+
+  return 0;
+
+}
---
+++ Testing/Code/Review/itkReviewTests.cxx
@@ -0,0 +1,13 @@
+// this file defines the itkBasicFiltersTest for the test driver
+// and all it expects is that you have a function called RegisterTests
+
+#include "vnl/vnl_sample.h"
+#include "itkTestMain.h" 
+
+
+void RegisterTests()
+{
+  vnl_sample_reseed(8775070);
+  REGISTER_TEST(itkThresholdMaximumConnectedComponentsImageFilterTest);
+}
+
