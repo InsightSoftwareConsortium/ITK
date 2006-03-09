3abde90b93bae73f841ce374cf633784a8e602fe
Bill Lorensen <bill.lorensen@gmail.com>
2006-03-08 22:37:16 -0500

    STYLE: missing copyright.

--- Testing/Code/BasicFilters/itkDiffusionTensor3DReconstructionImageFilterTest.cxx
+++ Testing/Code/BasicFilters/itkDiffusionTensor3DReconstructionImageFilterTest.cxx
@@ -1,3 +1,19 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkDiffusionTensor3DReconstructionImageFilterTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
 #include "itkDiffusionTensor3DReconstructionImageFilter.h"
 #include "itkImageRegionIteratorWithIndex.h"
 #include <iostream>
