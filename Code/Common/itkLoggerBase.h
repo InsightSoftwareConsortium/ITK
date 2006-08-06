f445aa5e6bb21d1494ce0cbcf9ee1932ec23c074
Stephen Aylward <stephen.aylward@kitware.com>
2006-08-06 16:37:55 -0400

    STYLE: Fixed line lengths and indentation errors

--- Code/Common/itkLoggerBase.h
+++ Code/Common/itkLoggerBase.h
@@ -36,105 +36,125 @@ namespace itk
 */
 
 class ITKCommon_EXPORT LoggerBase : public Object
-{
-
-public:
-
-  typedef LoggerBase  Self;
-  typedef Object  Superclass;
-  typedef SmartPointer<Self>  Pointer;
-  typedef SmartPointer<const Self>  ConstPointer;
-
-  /** Run-time type information (and related methods). */
-  itkTypeMacro( LoggerBase, Object );
-
-  typedef  MultipleLogOutput::OutputType   OutputType;
-
-  /** Definition of types of messages. These codes will be used to regulate the
-   * level of detail of messages reported to the final outputs */
-  typedef enum
   {
-    MUSTFLUSH=0,
-    FATAL,
-    CRITICAL,
-    WARNING,
-    INFO,
-    DEBUG,
-    NOTSET
-  } PriorityLevelType;
 
-  itkSetStringMacro(Name);
-  itkGetStringMacro(Name);
-
-  /** Provides a default formatted log entry */
-  virtual std::string BuildFormattedEntry(PriorityLevelType level, std::string const & content);
-
-  /** Set the priority level for the current logger. Only messages that have
-   * priorities equal or greater than the one set here will be posted to the
-   * current outputs */
-  virtual void SetPriorityLevel( PriorityLevelType level )
-  {
-    m_PriorityLevel = level;
-  }
-
-  /** Get the priority level for the current logger. Only messages that have
-   * priorities equal or greater than the one set here will be posted to the
-   * current outputs */
-  virtual PriorityLevelType GetPriorityLevel() const
-  {
-    return m_PriorityLevel;
-  }
-
-  virtual void SetLevelForFlushing( PriorityLevelType level )
-  {
-    m_LevelForFlushing = level;
-  }
-
-  virtual PriorityLevelType GetLevelForFlushing() const
-  {
-    return m_LevelForFlushing;
-  }
-
-  /** Registers another output stream with the multiple output. */
-  virtual void AddLogOutput( OutputType* output ) ;
-
-
-  virtual void Write(PriorityLevelType level, std::string const & content) ;
-  /** Helper methods */
-  void Debug ( std::string const& message ) { this->Write ( LoggerBase::DEBUG, message ); }
-  void Info ( std::string const& message ) { this->Write ( LoggerBase::INFO, message ); }
-  void Warning ( std::string const& message ) { this->Write ( LoggerBase::WARNING, message ); }
-  void Critical ( std::string const& message ) { this->Write ( LoggerBase::CRITICAL, message ); }
-  void Error ( std::string const& message ) { this->Write ( LoggerBase::CRITICAL, message ); }
-  void Fatal ( std::string const& message ) { this->Write ( LoggerBase::FATAL, message ); }
-
-  virtual void Flush();
-
-protected:
-
-  /** Constructor */
-  LoggerBase();
-
-  /** Destructor */
-  virtual ~LoggerBase();
-
-  /** Print contents of a LoggerBase */
-  virtual void PrintSelf(std::ostream &os, Indent indent) const;
-
-protected:
-
-  PriorityLevelType m_PriorityLevel;
-
-  PriorityLevelType m_LevelForFlushing;
-
-  MultipleLogOutput::Pointer  m_Output;
-
-  RealTimeClock::Pointer  m_Clock;
-private:
-
-  std::string m_Name;
-
-};  // class LoggerBase
+  public:
+  
+    typedef LoggerBase  Self;
+    typedef Object  Superclass;
+    typedef SmartPointer<Self>  Pointer;
+    typedef SmartPointer<const Self>  ConstPointer;
+  
+    /** Run-time type information (and related methods). */
+    itkTypeMacro( LoggerBase, Object );
+  
+    typedef  MultipleLogOutput::OutputType   OutputType;
+  
+    /** Definition of types of messages. These codes will be used to regulate
+     * the level of detail of messages reported to the final outputs */
+    typedef enum
+      {
+      MUSTFLUSH=0,
+      FATAL,
+      CRITICAL,
+      WARNING,
+      INFO,
+      DEBUG,
+      NOTSET
+      } PriorityLevelType;
+  
+    itkSetStringMacro(Name);
+    itkGetStringMacro(Name);
+  
+    /** Provides a default formatted log entry */
+    virtual std::string BuildFormattedEntry(PriorityLevelType level,
+                                            std::string const & content);
+  
+    /** Set the priority level for the current logger. Only messages that have
+     * priorities equal or greater than the one set here will be posted to the
+     * current outputs */
+    virtual void SetPriorityLevel( PriorityLevelType level )
+      {
+      m_PriorityLevel = level;
+      }
+  
+    /** Get the priority level for the current logger. Only messages that have
+     * priorities equal or greater than the one set here will be posted to the
+     * current outputs */
+    virtual PriorityLevelType GetPriorityLevel() const
+      {
+      return m_PriorityLevel;
+      }
+  
+    virtual void SetLevelForFlushing( PriorityLevelType level )
+      {
+      m_LevelForFlushing = level;
+      }
+  
+    virtual PriorityLevelType GetLevelForFlushing() const
+      {
+      return m_LevelForFlushing;
+      }
+  
+    /** Registers another output stream with the multiple output. */
+    virtual void AddLogOutput( OutputType* output ) ;
+  
+  
+    virtual void Write(PriorityLevelType level, std::string const & content) ;
+    /** Helper methods */
+    void Debug ( std::string const& message ) 
+      { 
+      this->Write ( LoggerBase::DEBUG, message ); 
+      }
+    void Info ( std::string const& message ) 
+      { 
+      this->Write ( LoggerBase::INFO, message ); 
+      }
+    void Warning ( std::string const& message ) 
+      { 
+      this->Write ( LoggerBase::WARNING, message ); 
+      }
+    void Critical ( std::string const& message ) 
+      { 
+      this->Write ( LoggerBase::CRITICAL, message ); 
+      }
+    void Error ( std::string const& message ) 
+      { 
+      this->Write ( LoggerBase::CRITICAL, message ); 
+      }
+    void Fatal ( std::string const& message ) 
+      { 
+      this->Write ( LoggerBase::FATAL, message ); 
+      }
+  
+    virtual void Flush();
+  
+  protected:
+  
+    /** Constructor */
+    LoggerBase();
+  
+    /** Destructor */
+    virtual ~LoggerBase();
+  
+    /** Print contents of a LoggerBase */
+    virtual void PrintSelf(std::ostream &os, Indent indent) const;
+  
+  protected:
+  
+    PriorityLevelType m_PriorityLevel;
+  
+    PriorityLevelType m_LevelForFlushing;
+  
+    MultipleLogOutput::Pointer  m_Output;
+  
+    RealTimeClock::Pointer  m_Clock;
+  
+  private:
+  
+    std::string m_Name;
+  
+  };  // class LoggerBase
 
 
 } // namespace itk
--- Code/Common/itkLoggerThreadWrapper.h
+++ Code/Common/itkLoggerThreadWrapper.h
@@ -18,7 +18,8 @@
 #ifndef __itkLoggerThreadWrapper_h
 #define __itkLoggerThreadWrapper_h
 
-#if ! ( defined(_MSC_VER) || ( defined(__GNUC__) && (__GNUC__ <= 2) ))//NOTE: This class does not work under MSVS6, or gnu 2.95
+//NOTE: This class does not work under MSVS6, or gnu 2.95
+#if ! ( defined(_MSC_VER) || ( defined(__GNUC__) && (__GNUC__ <= 2) ))
 
 #include "itkMacro.h"
 #include "itkMultiThreader.h"
@@ -31,7 +32,8 @@ namespace itk
 {
 
 /** \class LoggerThreadWrapper
- *  \brief Class LoggerThreadWrapper is meant for providing logging service as a separate thread.
+ *  \brief Class LoggerThreadWrapper is meant for providing logging service 
+ *  as a separate thread.
  *
  * \author Hee-Su Kim, Compute Science Dept. Kyungpook National University,
  *                     ISIS Center, Georgetown University.
@@ -43,101 +45,100 @@ namespace itk
 template < class SimpleLoggerType >
 class LoggerThreadWrapper : public SimpleLoggerType
 {
-
-public:
-
-  typedef LoggerThreadWrapper  Self;
-  typedef SimpleLoggerType  Superclass;
-  typedef SmartPointer<Self>  Pointer;
-  typedef SmartPointer<const Self>  ConstPointer;
-
-  /** Run-time type information (and related methods). */
-  itkTypeMacro( LoggerThreadWrapper, SimpleLoggerType );
-
-  /** New macro for creation of through a Smart Pointer */
-  itkNewMacro( Self );
-
-  typedef  typename SimpleLoggerType::OutputType   OutputType;
-  typedef  typename SimpleLoggerType::PriorityLevelType  PriorityLevelType;
-
-  /** Definition of types of operations for LoggerThreadWrapper. */
-  typedef enum
-  {
-    SET_PRIORITY_LEVEL,
-    SET_LEVEL_FOR_FLUSHING,
-    ADD_LOG_OUTPUT,
-    WRITE,
-    FLUSH
-  } OperationType;
-
-
-  /** Set the priority level for the current logger. Only messages that have
-   * priorities equal or greater than the one set here will be posted to the
-   * current outputs */
-  virtual void SetPriorityLevel( PriorityLevelType level );
-
-  /** Get the priority level for the current logger. Only messages that have
-   * priorities equal or greater than the one set here will be posted to the
-   * current outputs */
-  virtual PriorityLevelType GetPriorityLevel() const;
-
-  virtual void SetLevelForFlushing( PriorityLevelType level );
-  virtual PriorityLevelType GetLevelForFlushing() const;
-
-  /** Registers another output stream with the multiple output. */
-  virtual void AddLogOutput( OutputType* output );
-
-  virtual void Write(PriorityLevelType level, std::string const & content);
-
-  virtual void Flush();
-
-protected:
-
-  /** Constructor */
-  LoggerThreadWrapper();
-
-  /** Destructor */
-  virtual ~LoggerThreadWrapper();
-
-  /** Print contents of a LoggerThreadWrapper */
-  virtual void PrintSelf(std::ostream &os, Indent indent) const;
-
-  static ITK_THREAD_RETURN_TYPE ThreadFunction(void*);
-
-private:
-
-  typedef std::queue<OperationType> OperationContainerType;
-
-  typedef std::queue<std::string>  MessageContainerType;
-
-  typedef std::queue<PriorityLevelType>  LevelContainerType;
-
-  typedef std::queue<typename OutputType::Pointer>  OutputContainerType;
-
-  MultiThreader::Pointer  m_Threader;
-
-  int m_ThreadID;
-
-  OperationContainerType  m_OperationQ;
-
-  MessageContainerType  m_MessageQ;
-
-  LevelContainerType  m_LevelQ;
-
-  OutputContainerType m_OutputQ;
-
-  SimpleFastMutexLock m_Mutex;
-
-  SimpleFastMutexLock m_WaitMutex;
-
-};  // class LoggerThreadWrapper
+  public:
+  
+    typedef LoggerThreadWrapper  Self;
+    typedef SimpleLoggerType  Superclass;
+    typedef SmartPointer<Self>  Pointer;
+    typedef SmartPointer<const Self>  ConstPointer;
+  
+    /** Run-time type information (and related methods). */
+    itkTypeMacro( LoggerThreadWrapper, SimpleLoggerType );
+  
+    /** New macro for creation of through a Smart Pointer */
+    itkNewMacro( Self );
+  
+    typedef  typename SimpleLoggerType::OutputType   OutputType;
+    typedef  typename SimpleLoggerType::PriorityLevelType  PriorityLevelType;
+  
+    /** Definition of types of operations for LoggerThreadWrapper. */
+    typedef enum
+      {
+      SET_PRIORITY_LEVEL,
+      SET_LEVEL_FOR_FLUSHING,
+      ADD_LOG_OUTPUT,
+      WRITE,
+      FLUSH
+      } OperationType;
+  
+  
+    /** Set the priority level for the current logger. Only messages that have
+     * priorities equal or greater than the one set here will be posted to the
+     * current outputs */
+    virtual void SetPriorityLevel( PriorityLevelType level );
+  
+    /** Get the priority level for the current logger. Only messages that have
+     * priorities equal or greater than the one set here will be posted to the
+     * current outputs */
+    virtual PriorityLevelType GetPriorityLevel() const;
+  
+    virtual void SetLevelForFlushing( PriorityLevelType level );
+    virtual PriorityLevelType GetLevelForFlushing() const;
+  
+    /** Registers another output stream with the multiple output. */
+    virtual void AddLogOutput( OutputType* output );
+  
+    virtual void Write(PriorityLevelType level, std::string const & content);
+  
+    virtual void Flush();
+  
+  protected:
+  
+    /** Constructor */
+    LoggerThreadWrapper();
+  
+    /** Destructor */
+    virtual ~LoggerThreadWrapper();
+  
+    /** Print contents of a LoggerThreadWrapper */
+    virtual void PrintSelf(std::ostream &os, Indent indent) const;
+  
+    static ITK_THREAD_RETURN_TYPE ThreadFunction(void*);
+  
+  private:
+  
+    typedef std::queue<OperationType> OperationContainerType;
+  
+    typedef std::queue<std::string>  MessageContainerType;
+  
+    typedef std::queue<PriorityLevelType>  LevelContainerType;
+  
+    typedef std::queue<typename OutputType::Pointer>  OutputContainerType;
+  
+    MultiThreader::Pointer  m_Threader;
+  
+    int m_ThreadID;
+  
+    OperationContainerType  m_OperationQ;
+  
+    MessageContainerType  m_MessageQ;
+  
+    LevelContainerType  m_LevelQ;
+  
+    OutputContainerType m_OutputQ;
+  
+    SimpleFastMutexLock m_Mutex;
+  
+    SimpleFastMutexLock m_WaitMutex;
+  
+  };  // class LoggerThreadWrapper
 } // namespace itk
-
+  
 #ifndef ITK_MANUAL_INSTANTIATION
 #include "itkLoggerThreadWrapper.txx"
 #endif
-
-
-
+  
+  
+  
 #endif  // !defined (_MSC_VER)
 #endif  // __itkLoggerThreadWrapper_h
