552829fc49f37e5660d011cf2f47a30e23322c66
Luis Ibanez <luis.ibanez@kitware.com>
2006-09-25 17:10:28 -0400

    BUG: It was creating headers with the copyright assigned to the National Library      of Medicine, instead of the Insight Software Consortium.

--- Utilities/Dart/BuildHeaderTest.tcl
+++ Utilities/Dart/BuildHeaderTest.tcl
@@ -41,17 +41,15 @@
 
 set Header {/*=========================================================================
 
-  Program:   Insight Segmentation & Registration Toolkit (ITK)
-  Module:
+  Program:   Insight Segmentation & Registration Toolkit
   Language:  C++
-  Date:
-  Version:
 
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
 
-Copyright (c) 2000 National Library of Medicine
-All rights reserved.
-
-See COPYRIGHT.txt for copyright details.
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
 #if defined(_MSC_VER)
