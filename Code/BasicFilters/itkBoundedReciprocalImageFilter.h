9a1b258dce7ec62c9bcd4824d63c750a804bd6a6
Bill Lorensen <bill.lorensen@gmail.com>
2006-09-26 08:05:05 -0400

    STYLE: consistent copyright text.

--- Code/BasicFilters/itkBoundedReciprocalImageFilter.h
+++ Code/BasicFilters/itkBoundedReciprocalImageFilter.h
@@ -6,8 +6,11 @@
   Date:      $Date$
   Version:   $Revision$
 
-  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  Copyright (c) Insight Software Consortium. All rights reserved.
   See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+  Portions of this code are covered under the VTK copyright.
+  See VTKCopyright.txt or http://www.kitware.com/VTKCopyright.htm for details.
 
      This software is distributed WITHOUT ANY WARRANTY; without even 
      the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
--- Code/BasicFilters/itkScalarConnectedComponentImageFilter.h
+++ Code/BasicFilters/itkScalarConnectedComponentImageFilter.h
@@ -6,6 +6,12 @@
   Date:      $Date$
   Version:   $Revision$
 
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+  Portions of this code are covered under the VTK copyright.
+  See VTKCopyright.txt or http://www.kitware.com/VTKCopyright.htm for details.
+
      This software is distributed WITHOUT ANY WARRANTY; without even 
      the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
      PURPOSE.  See the above copyright notices for more information.
