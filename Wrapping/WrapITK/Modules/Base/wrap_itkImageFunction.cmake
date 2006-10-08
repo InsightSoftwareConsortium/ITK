08a9e610e42f63657d5b2213ea97d26de1b5a48e
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-10-08 07:54:11 -0400

    ENH: wrap unsigned char type required by spatial object

--- Wrapping/WrapITK/Modules/Base/wrap_itkImageFunction.cmake
+++ Wrapping/WrapITK/Modules/Base/wrap_itkImageFunction.cmake
@@ -1,6 +1,8 @@
 WRAP_CLASS("itk::ImageFunction" POINTER)
   FOREACH(d ${WRAP_ITK_DIMS})
-    FOREACH(t ${WRAP_ITK_SCALAR})
+    # UC is required for InterpolateImageFunction
+    UNIQUE(types "${WRAP_ITK_SCALAR};UC")
+    FOREACH(t ${types})
       WRAP_TEMPLATE("${ITKM_I${t}${d}}${ITKM_D}${ITKM_F}"  "${ITKT_I${t}${d}},${ITKT_D},${ITKT_F}")
       WRAP_TEMPLATE("${ITKM_I${t}${d}}${ITKM_D}${ITKM_D}"  "${ITKT_I${t}${d}},${ITKT_D},${ITKT_D}")
     ENDFOREACH(t)
