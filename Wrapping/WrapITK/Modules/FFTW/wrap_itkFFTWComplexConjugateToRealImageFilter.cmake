d641d59602ff9aa97abd9c2bc0667e8f3c5b58b7
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-10-09 14:06:55 -0400

    ENH: move FFTW wrappers to a FFTW module.

--- Wrapping/WrapITK/Modules/FFTW/wrap_itkFFTWComplexConjugateToRealImageFilter.cmake
+++ Wrapping/WrapITK/Modules/FFTW/wrap_itkFFTWComplexConjugateToRealImageFilter.cmake
@@ -1,12 +1,12 @@
 WRAP_CLASS("itk::FFTWComplexConjugateToRealImageFilter" POINTER)
   FOREACH(d ${WRAP_ITK_DIMS})
-    IF(WRAP_complex_float AND WRAP_float)
+    IF(WRAP_complex_float AND WRAP_float AND FFTWF_LIB)
       WRAP_TEMPLATE("${ITKM_F}${d}" "${ITKT_F},${d}")
-    ENDIF(WRAP_complex_float AND WRAP_float)
+    ENDIF(WRAP_complex_float AND WRAP_float AND FFTWF_LIB)
   
-    IF(WRAP_complex_double AND WRAP_double)
+    IF(WRAP_complex_double AND WRAP_double AND FFTWD_LIB)
       WRAP_TEMPLATE("${ITKM_D}${d}" "${ITKT_D},${d}")
-    ENDIF(WRAP_complex_double AND WRAP_double)
+    ENDIF(WRAP_complex_double AND WRAP_double AND FFTWD_LIB)
   ENDFOREACH(d)
 END_WRAP_CLASS()
 
