ab8f24e5c90d1c00c1be745641fe4f19e146e1c9
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2006-10-17 06:42:05 -0400

    ENH: mask image has integral pixel type ENH: add complex type to be able to filter Fourier spectrum ENH: use a more general macro to wrap scalars and all types

--- Wrapping/WrapITK/Modules/PixelMath/wrap_itkMultiplyImageFilter.cmake
+++ Wrapping/WrapITK/Modules/PixelMath/wrap_itkMultiplyImageFilter.cmake
@@ -1,7 +1,15 @@
 WRAP_CLASS("itk::MultiplyImageFilter" POINTER_WITH_SUPERCLASS)
+  WRAP_IMAGE_FILTER_SCALAR(3)
+  WRAP_IMAGE_FILTER_COMPLEX_REAL(3)
 
-  WRAP_IMAGE_FILTER_USIGN_INT(3)
-  WRAP_IMAGE_FILTER_SIGN_INT(3)
-  WRAP_IMAGE_FILTER_REAL(3)
+  FOREACH(d ${WRAP_ITK_DIMS})
+    IF(WRAP_complex_float AND WRAP_float)
+      WRAP_TEMPLATE("${ITKM_ICF${d}}${ITKM_IF${d}}${ITKM_ICF${d}}" "${ITKT_ICF${d}},${ITKT_IF${d}},${ITKT_ICF${d}}")
+    ENDIF(WRAP_complex_float AND WRAP_float)
+  
+    IF(WRAP_complex_double AND WRAP_double)
+      WRAP_TEMPLATE("${ITKM_ICD${d}}${ITKM_ID${d}}${ITKM_ICD${d}}" "${ITKT_ICD${d}},${ITKT_ID${d}},${ITKT_ICD${d}}")
+    ENDIF(WRAP_complex_double AND WRAP_double)
+  ENDFOREACH(d)
 
 END_WRAP_CLASS()
