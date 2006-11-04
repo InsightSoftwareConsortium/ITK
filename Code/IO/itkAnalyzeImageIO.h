904592055522a8969b8f720982ab521b83c9d18c
Bill Lorensen <bill.lorensen@gmail.com>
2006-11-04 08:53:52 -0500

    COMP: include files have moved. Some external projects not seeing the new includes.

--- Code/IO/itkAnalyzeImageIO.h
+++ Code/IO/itkAnalyzeImageIO.h
@@ -35,7 +35,7 @@ PURPOSE.  See the above copyright notices for more information.
 
 #include <fstream>
 #include "itkImageIOBase.h"
-#include "itkAnalyzeDbh.h"
+#include "itkExtHdrs/itkAnalyzeDbh.h"
 
 namespace itk
 {
