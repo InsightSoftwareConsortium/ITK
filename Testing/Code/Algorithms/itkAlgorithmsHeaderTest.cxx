1a53bd81ca4f30723536838ab46168d5a09d3209
Luis Ibanez <luis.ibanez@kitware.com>
2006-12-29 11:08:12 -0500

    COMP: Reordering the FFT related headers in order to fix an INTERNAL COMPILER       ERROR (ICE) in Visual Studio 6.0.

--- Testing/Code/Algorithms/itkAlgorithmsHeaderTest.cxx
+++ Testing/Code/Algorithms/itkAlgorithmsHeaderTest.cxx
@@ -61,13 +61,19 @@
 #include "itkExtensionVelocitiesImageFilter.txx"
 #include "itkFEMFiniteDifferenceFunctionLoad.txx"
 #include "itkFEMRegistrationFilter.txx"
-#if defined(USE_FFTWF) || defined(USE_FFTWD)
+
 #include "itkFFTComplexConjugateToRealImageFilter.txx"
 #include "itkFFTRealToComplexConjugateImageFilter.txx"
+
+#include "itkVnlFFTRealToComplexConjugateImageFilter.txx"
+#include "itkVnlFFTComplexConjugateToRealImageFilter.txx"
+
+#if defined(USE_FFTWF) || defined(USE_FFTWD)
 #include "itkFFTWCommon.h"
 #include "itkFFTWComplexConjugateToRealImageFilter.txx"
 #include "itkFFTWRealToComplexConjugateImageFilter.txx"
 #endif
+
 #include "itkFastChamferDistanceImageFilter.txx"
 #include "itkFastMarchingExtensionImageFilter.txx"
 #include "itkFastMarchingImageFilter.txx"
@@ -177,8 +183,6 @@
 #include "itkUnsharpMaskLevelSetImageFilter.txx"
 #include "itkVectorThresholdSegmentationLevelSetFunction.txx"
 #include "itkVectorThresholdSegmentationLevelSetImageFilter.txx"
-#include "itkVnlFFTComplexConjugateToRealImageFilter.txx"
-#include "itkVnlFFTRealToComplexConjugateImageFilter.txx"
 #include "itkVoronoiDiagram2D.txx"
 #include "itkVoronoiDiagram2DGenerator.txx"
 #include "itkVoronoiPartitioningImageFilter.txx"
