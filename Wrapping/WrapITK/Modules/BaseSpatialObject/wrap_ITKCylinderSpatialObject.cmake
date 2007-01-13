5f9b7f7023ced348c73cefe6e511cfee9541f998
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2007-01-13 15:50:00 -0500

    COMP: ITKCylinderSpatialObject is not different enough of itkCylinderSpatialObject with a case insensitive file system (windows, mac os x, ...). Move the code to another file.

--- Wrapping/WrapITK/Modules/BaseSpatialObject/wrap_ITKCylinderSpatialObject.cmake
+++
@@ -1,9 +0,0 @@
-# the file must have a different name than the class name for java
-# that's why ITK is uppercase in the file name
-
-# only available for dim 3
-
-FILTER_DIMS(d 3)
-IF(d)
-  WRAP_NON_TEMPLATE_CLASS("itk::CylinderSpatialObject" POINTER)
-ENDIF(d)
--- Wrapping/WrapITK/Modules/BaseSpatialObject/wrap_itkSpatialObjectBase.cmake
+++ Wrapping/WrapITK/Modules/BaseSpatialObject/wrap_itkSpatialObjectBase.cmake
@@ -3,3 +3,13 @@ WRAP_CLASS("itk::SpatialObject" POINTER)
     WRAP_TEMPLATE("${d}" "${d}")
   ENDFOREACH(d)
 END_WRAP_CLASS()
+
+# the file must have a different name than the class name for java
+# that's why it is here
+
+# only available for dim 3
+
+FILTER_DIMS(d 3)
+IF(d)
+  WRAP_NON_TEMPLATE_CLASS("itk::CylinderSpatialObject" POINTER)
+ENDIF(d)
