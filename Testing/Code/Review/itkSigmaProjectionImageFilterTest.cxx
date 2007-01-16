18c5c818ca122fe216eab14e83786f3c82429f22
Andinet Enquobahrie <andinet.enqu@kitware.com>
2007-01-16 16:21:02 -0500

    STYLE: code style fix

--- Testing/Code/Review/itkSigmaProjectionImageFilterTest.cxx
+++ Testing/Code/Review/itkSigmaProjectionImageFilterTest.cxx
@@ -35,20 +35,20 @@ int itkSigmaProjectionImageFilterTest(int argc, char * argv[])
 
   const int dim = 3;
   
-  typedef unsigned char PType;
-  typedef itk::Image< PType, dim > IType;
+  typedef unsigned char PixelType;
+  typedef itk::Image< PixelType, dim > ImageType;
 
-  typedef itk::ImageFileReader< IType > ReaderType;
+  typedef itk::ImageFileReader< ImageType > ReaderType;
   ReaderType::Pointer reader = ReaderType::New();
   reader->SetFileName( argv[1] );
 
-  typedef itk::SigmaProjectionImageFilter< IType, IType > FilterType;
+  typedef itk::SigmaProjectionImageFilter< ImageType, ImageType > FilterType;
   FilterType::Pointer filter = FilterType::New();
   filter->SetInput( reader->GetOutput() );
 
   itk::SimpleFilterWatcher watcher(filter, "filter");
 
-  typedef itk::ImageFileWriter< IType > WriterType;
+  typedef itk::ImageFileWriter< ImageType > WriterType;
   WriterType::Pointer writer = WriterType::New();
   writer->SetInput( filter->GetOutput() );
   writer->SetFileName( argv[2] );
