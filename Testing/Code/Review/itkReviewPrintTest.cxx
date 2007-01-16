eb5ed4fb6bf76cc793d33bd5e0cfd791e8a67e6c
Luis Ibanez <luis.ibanez@kitware.com>
2007-01-16 09:01:14 -0500

    ENH: Adding test for the ContourExtractor2DImageFilter and the LabelOverlayImageFilter.

--- Testing/Code/Review/itkReviewPrintTest.cxx
+++ Testing/Code/Review/itkReviewPrintTest.cxx
@@ -18,23 +18,35 @@
 #pragma warning ( disable : 4786 )
 #endif
 
-#include "itkThresholdMaximumConnectedComponentsImageFilter.h"
+#include "itkContourExtractor2DImageFilter.h"
+#include "itkLabelOverlayImageFilter.h"
+#include "itkRGBPixel.h"
 
 int main(int , char* [])
 {
-  typedef itk::Image<float,2>          InputType; 
-  typedef itk::Image<float,2>          OutputType;
-  typedef itk::Image<unsigned short,2> UShortImageType;
-  typedef itk::Image<unsigned char,2>  CharType;
+  typedef itk::Image<float,2>           InputType; 
+  typedef itk::Image<float,2>           OutputType;
+  typedef itk::Image<unsigned short,2>  UShortImageType;
+  typedef itk::Image<unsigned char,2>   CharType;
+  typedef itk::RGBPixel<unsigned char>  RGBPixelType;
+  typedef itk::Image< RGBPixelType, 2 > RGBImageType;
+  typedef itk::Image<unsigned char,2>   CharType;
 
   typedef itk::Vector<float,2>      VectorType;
   typedef itk::Image<VectorType, 2> VectorImageType;
 
-  itk::ThresholdMaximumConnectedComponentsImageFilter<InputType>::Pointer
-    ThresholdMaximumConnectedComponentsImageFilterObj =
-    itk::ThresholdMaximumConnectedComponentsImageFilter<InputType>::New();
-  std:: cout << "-------------ThresholdMaximumConnectedComponentsImageFilter "
-             << ThresholdMaximumConnectedComponentsImageFilterObj;
+  itk::ContourExtractor2DImageFilter<InputType>::Pointer
+    ContourExtractor2DImageFilterObj =
+    itk::ContourExtractor2DImageFilter<InputType>::New();
+  std:: cout << "-------------ContourExtractor2DImageFilter "
+             << ContourExtractor2DImageFilterObj;
+
+  itk::LabelOverlayImageFilter<InputType,CharType,RGBImageType>::Pointer
+    LabelOverlayImageFilterObj =
+    itk::LabelOverlayImageFilter<InputType,CharType,RGBImageType>::New();
+  std:: cout << "-------------LabelOverlayImageFilter "
+             << LabelOverlayImageFilterObj;
+
 
   return 0;
 
