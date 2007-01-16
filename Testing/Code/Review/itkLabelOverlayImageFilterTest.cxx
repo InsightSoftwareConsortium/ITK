4480502ebba6f7ae574168cff0ba63d36080f487
Luis Ibanez <luis.ibanez@kitware.com>
2007-01-16 08:26:25 -0500

    ENH: Adding code for exercising all the methods. It should get close to 100% code coverage now.

--- Testing/Code/Review/itkLabelOverlayImageFilterTest.cxx
+++ Testing/Code/Review/itkLabelOverlayImageFilterTest.cxx
@@ -38,11 +38,11 @@ int itkLabelOverlayImageFilterTest(int argc, char * argv[])
     return 1;
     }
  
-  typedef unsigned char                           PType;
-  typedef itk::Image< PType, Dimension >          IType;
-  typedef itk::RGBPixel<unsigned char>            CPType;
-  typedef itk::Image< CPType, Dimension >         CIType;
-  typedef itk::ImageFileReader< IType >           ReaderType;
+  typedef unsigned char                               PixelType;
+  typedef itk::Image< PixelType, Dimension >          ImageType;
+  typedef itk::RGBPixel<unsigned char>                ColorPixelType;
+  typedef itk::Image< ColorPixelType, Dimension >     ColorImageType;
+  typedef itk::ImageFileReader< ImageType >           ReaderType;
 
   //Read in the input image
   ReaderType::Pointer reader = ReaderType::New();
@@ -53,14 +53,72 @@ int itkLabelOverlayImageFilterTest(int argc, char * argv[])
   reader2->SetFileName( argv[2] );
 
   //Instantiate the filter
-  typedef itk::LabelOverlayImageFilter< IType, IType, CIType> FilterType;
+  typedef itk::LabelOverlayImageFilter< 
+    ImageType, ImageType, ColorImageType> FilterType;
   FilterType::Pointer filter = FilterType::New();
 
   //Use the background
   filter->SetUseBackground( true );
+  if( filter->GetUseBackground() != true )
+    {
+    std::cerr << "UseBackground Set/Get Problem" << std::endl;
+    return EXIT_FAILURE;
+    }
+   
+  filter->SetUseBackground( false );
+  if( filter->GetUseBackground() != false )
+    {
+    std::cerr << "UseBackground Set/Get Problem" << std::endl;
+    return EXIT_FAILURE;
+    }
+ 
+  filter->UseBackgroundOn();
+  if( filter->GetUseBackground() != true )
+    {
+    std::cerr << "UseBackground Set/Get Problem" << std::endl;
+    return EXIT_FAILURE;
+    }
+ 
+  filter->UseBackgroundOff();
+  if( filter->GetUseBackground() != false )
+    {
+    std::cerr << "UseBackground Set/Get Problem" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+
+
+  filter->SetUseBackground( true );
+
+  // Exercising Background Value methods
+  filter->SetBackgroundValue( 200 );
+  if( filter->GetUseBackground() != true )
+    {
+    std::cerr << "UseBackground Set/Get Problem" << std::endl;
+    return EXIT_FAILURE;
+    }
+ 
+
   //Set the filter input and label images
   filter->SetInput( reader->GetOutput() );
   filter->SetLabelImage( reader2->GetOutput() );
+
+  
+  filter->SetOpacity( 2 );
+  if( filter->GetOpacity() != 2 )
+    {
+    std::cerr << "Opacity Set/Get Problem" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+ 
+  filter->SetOpacity( 3 );
+  if( filter->GetOpacity() != 3 )
+    {
+    std::cerr << "Opacity Set/Get Problem" << std::endl;
+    return EXIT_FAILURE;
+    }
+
 
   //Set opacity 
   filter->SetOpacity( atof(argv[3]) );
@@ -68,12 +126,21 @@ int itkLabelOverlayImageFilterTest(int argc, char * argv[])
   itk::SimpleFilterWatcher watcher(filter, "filter");
 
   //Instantiate output image
-  typedef itk::ImageFileWriter< CIType > WriterType;
+  typedef itk::ImageFileWriter< ColorImageType > WriterType;
   WriterType::Pointer writer = WriterType::New();
 
   writer->SetInput( filter->GetOutput() );
   writer->SetFileName( argv[4] );
-  writer->Update();
+
+  try
+    {
+    writer->Update();
+    }
+  catch( itk::ExceptionObject & excp )
+    {
+    std::cerr << excp << std::endl;
+    return EXIT_FAILURE;
+    }
 
   return EXIT_SUCCESS;
 }
