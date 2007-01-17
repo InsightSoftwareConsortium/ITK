9b3e6cac18cb51368f5e3a802598dc3e7defcd32
Andinet Enquobahrie <andinet.enqu@kitware.com>
2007-01-17 17:32:27 -0500

    STYLE: add copyright headers

--- Testing/Code/Review/itkValuedRegionalMinimaImageFilterTest.cxx
+++ Testing/Code/Review/itkValuedRegionalMinimaImageFilterTest.cxx
@@ -1,3 +1,20 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkValuedRegionalMinimaImageFilterTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
 // a test routine for regional extrema using flooding
 #include "itkValuedRegionalMinimaImageFilter.h"
 #include "itkMaximumImageFilter.h"
