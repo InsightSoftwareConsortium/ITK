9328977924202119d61830d10f01852a9fd9f7e5
Luis Ibanez <luis.ibanez@kitware.com>
2007-01-17 08:32:35 -0500

    ENH: Fixing the types for coordinate representation and interpolatino weights.      Before the PixelType was being "reused" for that purpose. That previous      use would make impossible to create meshes with actual customizations      of the pixel types.

--- Code/Review/itkQuadEdgeMeshTraits.h
+++ Code/Review/itkQuadEdgeMeshTraits.h
@@ -1,9 +1,9 @@
 // -------------------------------------------------------------------------
 // itkQuadEdgeMeshTraits.h
-// $Revision: 1.2 $
+// $Revision: 1.3 $
 // $Author: ibanez $
 // $Name:  $
-// $Date: 2007-01-16 22:30:06 $
+// $Date: 2007-01-17 13:32:35 $
 // -------------------------------------------------------------------------
 // This code is an implementation of the well known quad edge (QE) data
 // structure in the ITK library. Although the original QE can handle non
@@ -40,83 +40,84 @@ template< int VPointDimension, typename TCoordRep,
           typename TUsingCellsContainer, typename TQE >
 class QuadEdgeMeshCellTraitsInfo
 {
-    public:
-    itkStaticConstMacro( PointDimension, unsigned int, VPointDimension );
-    typedef TCoordRep               CoordRepType;
-    typedef TInterpolationWeight    InterpolationWeightType;
-    typedef TPointIdentifier        PointIdentifier;
-    typedef TCellIdentifier         CellIdentifier;
-    typedef TCellFeatureIdentifier  CellFeatureIdentifier;
-    typedef TPoint                  PointType;
-    typedef TPointsContainer        PointsContainer;
-    typedef TUsingCellsContainer    UsingCellsContainer;
+public:
+  itkStaticConstMacro( PointDimension, unsigned int, VPointDimension );
+  typedef TCoordRep               CoordRepType;
+  typedef TInterpolationWeight    InterpolationWeightType;
+  typedef TPointIdentifier        PointIdentifier;
+  typedef TCellIdentifier         CellIdentifier;
+  typedef TCellFeatureIdentifier  CellFeatureIdentifier;
+  typedef TPoint                  PointType;
+  typedef TPointsContainer        PointsContainer;
+  typedef TUsingCellsContainer    UsingCellsContainer;
 
-    /** Iterator types. */
-    typedef TQE                             QuadEdgeType;
-    typedef typename TQE::IteratorGeom      PointIdIterator;
-    typedef typename TQE::ConstIteratorGeom PointIdConstIterator;
+  /** Iterator types. */
+  typedef TQE                             QuadEdgeType;
+  typedef typename TQE::IteratorGeom      PointIdIterator;
+  typedef typename TQE::ConstIteratorGeom PointIdConstIterator;
 };
 
 /**
- * Traits for a QE-based itk::Mesh.
+ * Traits for a QuadEdgeMesh
  */
 template< typename TPixel, unsigned int VPointDimension,
-          typename TPData, typename TDData >
+          typename TPData, typename TDData, 
+          typename TCoordRep=float, typename TInterpolationWeight=float >
 class QuadEdgeMeshTraits
 {
-    public:
-    /** Basic types for a mesh trait class. */
-    typedef QuadEdgeMeshTraits Self;
-    typedef TPixel     PixelType;
-    typedef TPixel     CellPixelType;
-    typedef TPixel     CoordRepType;
-    typedef TPixel     InterpolationWeightType;
+public:
+  /** Basic types for a mesh trait class. */
+  typedef QuadEdgeMeshTraits                Self;
+  typedef TPixel                            PixelType;
+  typedef TPixel                            CellPixelType;
+  typedef TCoordRep                         CoordRepType;
+  typedef TInterpolationWeight              InterpolationWeightType;
 
-    itkStaticConstMacro( PointDimension, unsigned int, VPointDimension );
-    itkStaticConstMacro( MaxTopologicalDimension, unsigned int,
-                         VPointDimension );
+  itkStaticConstMacro( PointDimension, unsigned int, VPointDimension );
+  itkStaticConstMacro( MaxTopologicalDimension, unsigned int,
+                       VPointDimension );
 
-    typedef unsigned long PointIdentifier;
-    typedef unsigned long CellIdentifier;
-    typedef unsigned long CellFeatureIdentifier;
+  typedef unsigned long PointIdentifier;
+  typedef unsigned long CellIdentifier;
+  typedef unsigned long CellFeatureIdentifier;
 
-    typedef std::set< CellIdentifier > UsingCellsContainer;
-    typedef std::set< CellIdentifier > PointCellLinksContainer;
+  typedef std::set< CellIdentifier > UsingCellsContainer;
+  typedef std::set< CellIdentifier > PointCellLinksContainer;
 
-    /** Quad edge typedefs. */
-    typedef TPData PrimalDataType;
-    typedef TDData DualDataType;
-    typedef GeometricalQuadEdge< PointIdentifier, CellIdentifier,
-                          PrimalDataType, DualDataType > QEPrimal;
-    //typedef QEPrimal QEType;
-    typedef typename QEPrimal::Dual             QEDual;
-    /// FOR LEO typedef typename QEPrimal::Superclass     QEType;
-    /// FOR LEO typedef typename QEPrimal::Dual           QEDual;
-    typedef typename QEPrimal::OrgRefType     VertexRefType;
-    typedef typename QEPrimal::DualOrgRefType FaceRefType;
+  /** Quad edge typedefs. */
+  typedef TPData PrimalDataType;
+  typedef TDData DualDataType;
+  typedef GeometricalQuadEdge< PointIdentifier, CellIdentifier,
+                        PrimalDataType, DualDataType > QEPrimal;
+  //typedef QEPrimal QEType;
+  typedef typename QEPrimal::Dual             QEDual;
+  /// FOR LEO typedef typename QEPrimal::Superclass     QEType;
+  /// FOR LEO typedef typename QEPrimal::Dual           QEDual;
+  typedef typename QEPrimal::OrgRefType     VertexRefType;
+  typedef typename QEPrimal::DualOrgRefType FaceRefType;
 
-    /** Points have an entry in the Onext ring */
-    typedef QuadEdgeMeshPoint< CoordRepType, VPointDimension, QEPrimal >  PointType;
-    typedef itk::MapContainer< PointIdentifier, PointType >   PointsContainer;
+  /** Points have an entry in the Onext ring */
+  typedef QuadEdgeMeshPoint< CoordRepType, VPointDimension, QEPrimal >  PointType;
+  typedef MapContainer< PointIdentifier, PointType >   PointsContainer;
 
-    /** Standard itk cell interface. */
-    typedef QuadEdgeMeshCellTraitsInfo< VPointDimension,         CoordRepType,
-                            InterpolationWeightType, PointIdentifier,
-                            CellIdentifier,          CellFeatureIdentifier,
-                            PointType,               PointsContainer,
-                            UsingCellsContainer,     QEPrimal > CellTraits;
-    typedef itk::CellInterface< CellPixelType, CellTraits > CellType;
-    typedef typename CellType::CellAutoPointer              CellAutoPointer;
+  /** Standard cell interface. */
+  typedef QuadEdgeMeshCellTraitsInfo< VPointDimension, CoordRepType,
+                          InterpolationWeightType, PointIdentifier,
+                          CellIdentifier,          CellFeatureIdentifier,
+                          PointType,               PointsContainer,
+                          UsingCellsContainer,     QEPrimal > CellTraits;
+  typedef CellInterface< CellPixelType, CellTraits > CellType;
+  typedef typename CellType::CellAutoPointer              CellAutoPointer;
 
-    /** Containers types. */
-    typedef itk::MapContainer< PointIdentifier,
-                               PointCellLinksContainer >       CellLinksContainer;
-    typedef itk::MapContainer< CellIdentifier, CellType* >     CellsContainer;
-    typedef itk::MapContainer< PointIdentifier, PixelType >    PointDataContainer;
-    typedef itk::MapContainer< CellIdentifier, CellPixelType > CellDataContainer;
+  /** Containers types. */
+  typedef MapContainer< PointIdentifier,
+                        PointCellLinksContainer >       CellLinksContainer;
+  typedef MapContainer< CellIdentifier, CellType* >     CellsContainer;
+  typedef MapContainer< PointIdentifier, PixelType >    PointDataContainer;
+  typedef MapContainer< CellIdentifier, CellPixelType > CellDataContainer;
 
-    /** Other useful types. */
-    typedef typename PointType::VectorType VectorType;
+  /** Other useful types. */
+  typedef typename PointType::VectorType VectorType;
 };
 
 } 
