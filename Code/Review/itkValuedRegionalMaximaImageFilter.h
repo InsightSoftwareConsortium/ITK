c408cdfca8cda3bdece4ea62704b5ca8ec8097bb
Andinet Enquobahrie <andinet.enqu@kitware.com>
2007-01-17 17:32:59 -0500

    STYLE: add copyright header

--- Code/Review/itkValuedRegionalMaximaImageFilter.h
+++ Code/Review/itkValuedRegionalMaximaImageFilter.h
@@ -1,3 +1,20 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkValuedRegionalMaximaImageFilter.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
 #ifndef __itkValuedRegionalMaximaImageFilter_h
 #define __itkValuedRegionalMaximaImageFilter_h
 
