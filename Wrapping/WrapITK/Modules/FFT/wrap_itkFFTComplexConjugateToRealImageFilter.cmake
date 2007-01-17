b6bd65a58bb33414cced13740554d6051200f3be
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2007-01-17 08:13:07 -0500

    COMP: workaround the linking problem with vnl classes with mac os x by wrapping the vnl fft filters in the same files than the fft base classes

--- Wrapping/WrapITK/Modules/FFT/wrap_itkFFTComplexConjugateToRealImageFilter.cmake
+++ Wrapping/WrapITK/Modules/FFT/wrap_itkFFTComplexConjugateToRealImageFilter.cmake
@@ -10,3 +10,18 @@ WRAP_CLASS("itk::FFTComplexConjugateToRealImageFilter" POINTER)
   ENDFOREACH(d)
 END_WRAP_CLASS()
 
+
+# wrapped here to avoid linking error on mac os x
+
+WRAP_CLASS("itk::VnlFFTComplexConjugateToRealImageFilter" POINTER)
+  FOREACH(d ${WRAP_ITK_DIMS})
+    IF(WRAP_complex_float AND WRAP_float)
+      WRAP_TEMPLATE("${ITKM_F}${d}" "${ITKT_F},${d}")
+    ENDIF(WRAP_complex_float AND WRAP_float)
+  
+    IF(WRAP_complex_double AND WRAP_double)
+      WRAP_TEMPLATE("${ITKM_D}${d}" "${ITKT_D},${d}")
+    ENDIF(WRAP_complex_double AND WRAP_double)
+  ENDFOREACH(d)
+END_WRAP_CLASS()
+
