11d0f9deb148e8643e8dc88312a942ece416a710
Luis Ibanez <luis.ibanez@kitware.com>
2007-01-24 10:53:37 -0500

    ENH: Test added for GetLprev() methods.

--- Testing/Code/Review/itkQuadEdgeTest1.cxx
+++ Testing/Code/Review/itkQuadEdgeTest1.cxx
@@ -431,6 +431,73 @@ int itkQuadEdgeTest1( int , char* [] )
 
   std::cout << "GetOprev() Test passed ! " << std::endl;
   }
+
+
+  // Tests for the GetLprev() methods
+  // returns the previous edge with same left face
+  { // create a local scope for these tests
+  QuadEdgeType * quadEdge1 = new QuadEdgeType;
+  QuadEdgeType * quadEdge2 = new QuadEdgeType;
+  QuadEdgeType * quadEdge3 = new QuadEdgeType;
+  QuadEdgeType * quadEdge4 = new QuadEdgeType;
+
+  QuadEdgeType * quadEdgeA = new QuadEdgeType;
+  QuadEdgeType * quadEdgeB = new QuadEdgeType;
+  QuadEdgeType * quadEdgeC = new QuadEdgeType;
+  QuadEdgeType * quadEdgeD = new QuadEdgeType;
+
+  const QuadEdgeType * quadEdge1c = quadEdge1;
+
+  quadEdge1->GetLprev(); // testing null case
+
+  //
+  //                        ^/^
+  //                        /
+  //                       / quadEdge1
+  //          Face        /
+  //                     /
+  //                    /
+  //   --------------->O
+  //       quadEdgeA
+  //
+ 
+  quadEdgeA->SetRot( quadEdgeB );
+  quadEdgeB->SetRot( quadEdgeC );
+  quadEdgeC->SetRot( quadEdgeD );
+  quadEdgeD->SetRot( quadEdgeA );
+
+  quadEdge1->SetRot( quadEdge2 );
+  quadEdge2->SetRot( quadEdge3 );
+  quadEdge3->SetRot( quadEdge4 );
+  quadEdge4->SetRot( quadEdge1 );
+
+  quadEdge1->SetOnext( quadEdgeC );
+
+  if( quadEdge1->GetLprev() != quadEdgeA )
+    {
+    std::cerr << "Error in GetLprev()" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  if( quadEdge1c->GetLprev() != quadEdgeA )
+    {
+    std::cerr << "Error in const GetLprev()" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  delete quadEdge1;
+  delete quadEdge2;
+  delete quadEdge3;
+  delete quadEdge4;
+
+  delete quadEdgeA;
+  delete quadEdgeB;
+  delete quadEdgeC;
+  delete quadEdgeD;
+
+  std::cout << "GetLprev() Test passed ! " << std::endl;
+  }
+
   return EXIT_SUCCESS;
 }
 
