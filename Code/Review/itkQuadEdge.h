f70c12458357a6699c56d09334ff499255b882b3
Luis Ibanez <luis.ibanez@kitware.com>
2007-01-29 10:08:44 -0500

    STYLE: Fixing style issues.

--- Code/Review/itkQuadEdge.h
+++ Code/Review/itkQuadEdge.h
@@ -1,24 +1,19 @@
-// -------------------------------------------------------------------------
-// itkQuadEdge.h
-// $Revision: 1.10 $
-// $Author: ibanez $
-// $Name:  $
-// $Date: 2007-01-24 23:58:14 $
-// -------------------------------------------------------------------------
-// This code is an implementation of the well known quad edge (QE) data
-// structure in the ITK library. Although the original QE can handle non
-// orientable 2-manifolds and its dual and its mirror, this implementation
-// is specifically dedicated to handle orientable 2-manifolds along with
-// their dual.
-//
-// Any comment, criticism and/or donation is welcome.
-//
-// Please contact any member of the team:
-//
-// - The frog master (Eric Boix)       eboix@ens-lyon.fr
-// - The duck master (Alex Gouaillard) alexandre.gouaillard@sun.com
-// - The cow  master (Leonardo Florez) florez@creatis.insa-lyon.fr
-// -------------------------------------------------------------------------
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkQuadEdge.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
 
 #ifndef __itkQuadEdge_h
 #define __itkQuadEdge_h
@@ -31,22 +26,22 @@
 // FIXME: Maybe variations of these macros should be moved into
 // itkMacro.h
 //
-#define itkQEDebugMacro( x )                                            \
-    {                                                                   \
-        ::itk::OStringStream itkmsg;                                    \
-        itkmsg << "Debug: In " __FILE__ ", line " << __LINE__ << "\n"   \
-               << " (" << this << "): " x                               \
-               << "\n\n";                                               \
-        ::itk::OutputWindowDisplayDebugText( itkmsg.str( ).c_str( ) );  \
-    }
-#define itkQEWarningMacro( x )                                          \
-    {                                                                   \
-        ::itk::OStringStream itkmsg;                                    \
-        itkmsg << "WARNING: In " __FILE__ ", line " << __LINE__ << "\n" \
-               << " (" << this << "): " x                               \
-               << "\n\n";                                               \
-        ::itk::OutputWindowDisplayWarningText( itkmsg.str( ).c_str( ) ); \
-    }
+#define itkQEDebugMacro( x )                                        \
+{                                                                   \
+    ::itk::OStringStream itkmsg;                                    \
+    itkmsg << "Debug: In " __FILE__ ", line " << __LINE__ << "\n"   \
+           << " (" << this << "): " x                               \
+           << "\n\n";                                               \
+    ::itk::OutputWindowDisplayDebugText( itkmsg.str( ).c_str( ) );  \
+}
+#define itkQEWarningMacro( x )                                      \
+{                                                                   \
+    ::itk::OStringStream itkmsg;                                    \
+    itkmsg << "WARNING: In " __FILE__ ", line " << __LINE__ << "\n" \
+           << " (" << this << "): " x                               \
+           << "\n\n";                                               \
+    ::itk::OutputWindowDisplayWarningText( itkmsg.str( ).c_str( ) );\
+}
 
 
 // -------------------------------------------------------------------------
