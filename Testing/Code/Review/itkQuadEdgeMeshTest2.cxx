922478a14ee02406bc8b0ac75ecfb1078a8035d8
Luis Ibanez <luis.ibanez@kitware.com>
2007-02-24 07:31:12 -0500

    STYLE: Fixing Header and indentations.

--- Testing/Code/Review/itkQuadEdgeMeshTest2.cxx
+++ Testing/Code/Review/itkQuadEdgeMeshTest2.cxx
@@ -1,89 +1,98 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkQuadEdgeMeshTest2.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
 
 #include "itkQuadEdgeMesh.h"
 #include "itkQuadEdgeMeshLineCell.h"
 
 int itkQuadEdgeMeshTest2( int , char* [] )
 {
-    std::cout << "Testing points and simple edges... " << std::ends;
+  std::cout << "Testing points and simple edges... " << std::ends;
 
-    typedef double                                   PixelType;
-    typedef itk::QuadEdgeMesh< PixelType, 3 >        MeshType;
-    typedef MeshType::CellType                       CellType;
-    typedef itk::QuadEdgeMeshLineCell< CellType >    LineType;
-    typedef CellType::CellAutoPointer                CellAutoPointer;
+  typedef double                                   PixelType;
+  typedef itk::QuadEdgeMesh< PixelType, 3 >        MeshType;
+  typedef MeshType::CellType                       CellType;
+  typedef itk::QuadEdgeMeshLineCell< CellType >    LineType;
+  typedef CellType::CellAutoPointer                CellAutoPointer;
 
-    MeshType::Pointer  mesh = MeshType::New( );
+  MeshType::Pointer  mesh = MeshType::New( );
 
-    MeshType::PointType p0;
-    MeshType::PointType p1;
-    MeshType::PointType p2;
+  MeshType::PointType p0;
+  MeshType::PointType p1;
+  MeshType::PointType p2;
 
-    p0[ 0 ] = -1.0; p0[ 1 ] = 0.0; p0[ 2 ] = 0.0;
-    p1[ 0 ] =  1.0; p1[ 1 ] = 0.0; p1[ 2 ] = 0.0;
-    p2[ 0 ] =  1.0; p2[ 1 ] = 1.0; p2[ 2 ] = 0.0;
+  p0[ 0 ] = -1.0; p0[ 1 ] = 0.0; p0[ 2 ] = 0.0;
+  p1[ 0 ] =  1.0; p1[ 1 ] = 0.0; p1[ 2 ] = 0.0;
+  p2[ 0 ] =  1.0; p2[ 1 ] = 1.0; p2[ 2 ] = 0.0;
 
-    mesh->SetPoint( 0, p0 );
-    mesh->SetPoint( 1, p1 );
-    mesh->SetPoint( 2, p2 );
+  mesh->SetPoint( 0, p0 );
+  mesh->SetPoint( 1, p1 );
+  mesh->SetPoint( 2, p2 );
 
-    CellAutoPointer line0;
-    line0.TakeOwnership( new LineType );
-    line0->SetPointId( 0, 0 );
-    line0->SetPointId( 1, 1 );
-    mesh->SetCell( 0, line0 );
+  CellAutoPointer line0;
+  line0.TakeOwnership( new LineType );
+  line0->SetPointId( 0, 0 );
+  line0->SetPointId( 1, 1 );
+  mesh->SetCell( 0, line0 );
 
-    CellAutoPointer line1;
-    line1.TakeOwnership( new LineType );
-    line1->SetPointId( 0, 1 );
-    line1->SetPointId( 1, 2 );
-    mesh->SetCell( 1, line1 );
+  CellAutoPointer line1;
+  line1.TakeOwnership( new LineType );
+  line1->SetPointId( 0, 1 );
+  line1->SetPointId( 1, 2 );
+  mesh->SetCell( 1, line1 );
 
-    CellAutoPointer line2;
-    line2.TakeOwnership( new LineType );
-    line2->SetPointId( 0, 2 );
-    line2->SetPointId( 1, 0 );
-    mesh->SetCell( 2, line2 );
+  CellAutoPointer line2;
+  line2.TakeOwnership( new LineType );
+  line2->SetPointId( 0, 2 );
+  line2->SetPointId( 1, 0 );
+  mesh->SetCell( 2, line2 );
 
-    if( mesh->GetNumberOfPoints( ) != 3 ) {
+  if( mesh->GetNumberOfPoints( ) != 3 )
+    {
+    std::cout << "Not all points added." << std::endl;
+    return EXIT_FAILURE;
+    }
 
-        std::cout << "Not all points added." << std::endl;
-        return( 1 );
+  if( mesh->GetNumberOfCells( ) != 3 )
+    {
+    std::cout << "Not all cells added." << std::endl;
+    return EXIT_FAILURE;
+    }
 
-    } // fi
+  typedef MeshType::CellsContainer::Iterator CellIterator;
+  CellIterator cellIterator = mesh->GetCells( )->Begin( );
+  unsigned int ids[ ] = { 0, 1, 2, 1, 2, 0, 2, 0, 1 };
+  int itIds = 0;
 
-    if( mesh->GetNumberOfCells( ) != 3 ) {
+  for( ; cellIterator != mesh->GetCells( )->End( ); cellIterator++ )
+    {
+    MeshType::CellType* cellptr = cellIterator.Value( );
+    LineType* line = dynamic_cast< LineType* >( cellptr );
+    LineType::IteratorGeom git = line->BeginGeomLnext( );
+    for( ; git != line->EndGeomLnext( ); git++ )
+      {
+      if( ids[ itIds ] != *git )
+        {
+        std::cout << "Problem with splicing edges." << std::endl;
+        return EXIT_FAILURE;
+        }
+      itIds++;
+      }
+    }
 
-        std::cout << "Not all cells added." << std::endl;
-        return( 1 );
-
-    } // fi
-
-    typedef MeshType::CellsContainer::Iterator CellIterator;
-    CellIterator cellIterator = mesh->GetCells( )->Begin( );
-    unsigned int ids[ ] = { 0, 1, 2, 1, 2, 0, 2, 0, 1 };
-    int itIds = 0;
-
-    for( ; cellIterator != mesh->GetCells( )->End( ); cellIterator++ ) {
-
-        MeshType::CellType* cellptr = cellIterator.Value( );
-        LineType* line = dynamic_cast< LineType* >( cellptr );
-        LineType::IteratorGeom git = line->BeginGeomLnext( );
-        for( ; git != line->EndGeomLnext( ); git++ ) {
-
-            if( ids[ itIds ] != *git ) {
-
-                std::cout << "Problem with splicing edges." << std::endl;
-                return( 1 );
-
-            } // fi
-            itIds++;
-
-        } // rof
-
-    } // rof
-
-    std::cout << "done!" << std::endl;
-    return( 0 );
+  std::cout << "done!" << std::endl;
+  return EXIT_SUCCESS;
 }
-
-// eof - itkQuadEdgeMeshTest2.cxx
