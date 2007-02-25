de03036a209e96b5e1047f93581f6f2d4fea0821
Luis Ibanez <luis.ibanez@kitware.com>
2007-02-25 09:54:57 -0500

    STYLE: Fixing copyright message.

--- Code/Review/itkQuadEdge.cxx
+++ Code/Review/itkQuadEdge.cxx
@@ -1,24 +1,19 @@
-// -------------------------------------------------------------------------
-// itkQuadEdge.cxx
-// $Revision: 1.11 $
-// $Author: ibanez $
-// $Name:  $
-// $Date: 2007-01-24 18:22:08 $
-// -------------------------------------------------------------------------
-// This code is an implementation of the well known quad edge (QE) data
-// structure in the ITK library. Although the original QE can handle non
-// orientable 2-manifolds and its dual and its mirror, this implementation
-// is specifically dedicated to handle orientable 2-manifolds along with
-// their dual.
-//
-// Any comment, criticism and/or donation is welcome.
-//
-// Please contact any member of the team:
-//
-// - The frog master (Eric Boix)       eboix@ens-lyon.fr
-// - The duck master (Alex Gouaillard) gouaillard@creatis.insa-lyon.fr
-// - The cow  master (Leonardo Florez) florez@creatis.insa-lyon.fr
-// -------------------------------------------------------------------------
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkQuadEdge.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
 
 #include "itkQuadEdge.h"
 
