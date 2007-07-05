5c893f76845bceb6afff5c4be01ac61bd2558c7a
Bill Lorensen <bill.lorensen@gmail.com>
2007-07-05 18:18:40 -0400

    ENH: FORCE

--- Code/Common/itkVersion.h
+++ Code/Common/itkVersion.h
@@ -28,7 +28,7 @@
 #define ITK_VERSION ITK_VERSION_TO_STRING(ITK_VERSION_MAJOR) "." \
                     ITK_VERSION_TO_STRING(ITK_VERSION_MINOR) "." \
                     ITK_VERSION_TO_STRING(ITK_VERSION_PATCH)
-#define ITK_SOURCE_VERSION "itk version " ITK_VERSION ", itk source $Revision: 1.2364 $, $Date: 2007-07-05 21:32:30 $ (GMT)"
+#define ITK_SOURCE_VERSION "itk version " ITK_VERSION ", itk source $Revision: 1.2365 $, $Date: 2007-07-05 22:18:40 $ (GMT)"
 
 namespace itk
 {
