d54d50c14311980be387a65de2c39bed8ec37d41
Kent Williams <norman-k-williams@uiowa.edu>
2007-07-24 13:52:03 -0400

    COMP: Added test for header size

---
+++ Utilities/nifti/Testing/niftilib/nifti_headersize_test.c
@@ -0,0 +1,19 @@
+#include "nifti1_io.h"
+
+int main(int argc, char **argv)
+{
+  int error = 0;
+  int argdims[] = { 2, 2, 2 };
+  nifti_1_header *hdr = nifti_make_new_header(argdims,NIFTI_TYPE_UINT8);
+  if(sizeof(*hdr) != 348)
+    {
+    printf("NIfTI Header size not 348: %d\n", sizeof(*hdr));
+    error++;
+    }
+  if(hdr->sizeof_hdr != 348)
+    {
+    printf("NIfTI sizeof_hdr != 348: %d\n", hdr->sizeof_hdr);
+    error++;
+    }
+  exit(error);
+}
