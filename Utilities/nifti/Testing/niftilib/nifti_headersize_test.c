24da1b8b5884e20801f134aad0be2b426e7af465
Kent Williams <norman-k-williams@uiowa.edu>
2007-07-27 17:35:52 -0400

    COMP: fixed compiler warning

--- Utilities/nifti/Testing/niftilib/nifti_headersize_test.c
+++ Utilities/nifti/Testing/niftilib/nifti_headersize_test.c
@@ -7,7 +7,7 @@ int main(int argc, char **argv)
   nifti_1_header *hdr = nifti_make_new_header(argdims,NIFTI_TYPE_UINT8);
   if(sizeof(*hdr) != 348)
     {
-    printf("NIfTI Header size not 348: %d\n", sizeof(*hdr));
+    printf("NIfTI Header size not 348: %lu\n", sizeof(*hdr));
     error++;
     }
   if(hdr->sizeof_hdr != 348)
