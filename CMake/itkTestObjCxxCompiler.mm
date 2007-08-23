e34781e09520e7037e7768797b795023be6ca6f6
Mathieu Coursolle <mcoursolle@rogue-research.com>
2007-08-23 10:28:53 -0400

    COMP: [5255] Add TRY_COMPILE directive and tests to make sure no conflicting id is use in headers.

---
+++ CMake/itkTestObjCxxCompiler.mm
@@ -0,0 +1,27 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Language:  ObjC++
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+#import <objc/objc.h>
+
+@interface MyObject
+{
+  id foo;
+}
+@end
+
+int main()
+{
+  MyObject* object = nil;
+  
+  return 0;
+}
