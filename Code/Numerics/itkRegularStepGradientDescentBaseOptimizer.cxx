69b7c4679382db4828dc05c40f8dbca1fd600d95
Luis Ibanez <luis.ibanez@kitware.com>
2007-09-10 12:22:23 -0400

    BUG: 4893. Adding validity check for GradientMagnitudeTolerance value.

--- Code/Numerics/itkRegularStepGradientDescentBaseOptimizer.cxx
+++ Code/Numerics/itkRegularStepGradientDescentBaseOptimizer.cxx
@@ -65,6 +65,13 @@ RegularStepGradientDescentBaseOptimizer
 
   m_StopCondition = Unknown;
 
+  // validity check for the value of GradientMagnitudeTolerance
+  if( m_GradientMagnitudeTolerance < 0.0 )
+      {
+      itkExceptionMacro(<< "Gradient magnitude tolerance must be"
+      "greater or equal 0.0. Current value is " << m_GradientMagnitudeTolerance );
+      }
+
   const unsigned int spaceDimension = m_CostFunction->GetNumberOfParameters();
 
   m_Gradient = DerivativeType( spaceDimension );
