693f196a4c2d234f902edca17201b0c58b7cbb69
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2007-10-23 07:10:54 -0400

    BUG: The OutputWindow's New() method returns a Win32OutputWindow on windows, so it have to be excluded of the GetNameOfClass test ENH: More verobose output ENH: Also exlcude the non templated classes

--- Wrapping/WrapITK/Python/Tests/getNameOfClass.py
+++ Wrapping/WrapITK/Python/Tests/getNameOfClass.py
@@ -9,39 +9,44 @@ itk.auto_progress(2)
 itk.force_load()
 # itk.ImageToImageFilter
 
-# a list of classes to exclude
-exclude = ["FFTRealToComplexConjugateImageFilter", "FFTComplexConjugateToRealImageFilter"]
+# a list of classes to exclude. Typically, the classes with a custom New() method, which return a subclass
+# of the current class
+exclude = ["FFTRealToComplexConjugateImageFilter", "FFTComplexConjugateToRealImageFilter", "OutputWindow"]
 
 wrongName = False
 
 for t in dir(itk):
-  T = itk.__dict__[t]
-  # first case - that's a templated class
-  if t not in exclude and isinstance(T, itk.Vector.__class__) and len(T)>0:
-    # use only the first specialization - all of them return the same name
-    i = T.values()[0]
-    # GetNameOfClass() is a virtual method of the LightObject class, so we must
-    # instantiate an object with the New() method
-    if 'New' in dir(i):
-      I = i.New()
-      # be sure that the type of the instantiated object is the same than the
-      # one of the class. It can be different if the class is an "abstract" one
-      # and don't provide any New() method. In that case, the one of the superclass
-      # is used.
-      if 'GetNameOfClass' in dir(I):
-        n = I.GetNameOfClass()
-        if n != t and itk.class_(I) == i:
-          print t
-          wrongName = True
-  else:
-    if 'New' in dir(T):
-      I = T.New()
-      if 'GetNameOfClass' in dir(I):
-        n = I.GetNameOfClass()
-        if n != t and itk.class_(I) == T:
-          print t
-          wrongName = True
+  if t not in exclude:
+    T = itk.__dict__[t]
+    # first case - that's a templated class
+    if isinstance(T, itk.Vector.__class__) and len(T)>0:
+      # use only the first specialization - all of them return the same name
+      i = T.values()[0]
+      # GetNameOfClass() is a virtual method of the LightObject class, so we must
+      # instantiate an object with the New() method
+      if 'New' in dir(i):
+        I = i.New()
+        # be sure that the type of the instantiated object is the same than the
+        # one of the class. It can be different if the class is an "abstract" one
+        # and don't provide any New() method. In that case, the one of the superclass
+        # is used.
+        if 'GetNameOfClass' in dir(I):
+          print "Checking", t
+          n = I.GetNameOfClass()
+          if n != t and itk.class_(I) == i:
+            print >> sys.stderr, t, "doesn't provide the right name."
+            wrongName = True
+    else:
+      if 'New' in dir(T):
+        I = T.New()
+        if 'GetNameOfClass' in dir(I):
+          print "Checking", t
+          n = I.GetNameOfClass()
+          if n != t and itk.class_(I) == T:
+            print >> sys.stderr, t, "doesn't provide the right name."
+            wrongName = True
 
 
 if wrongName:
+  print >> sys.stderr, "Some classes are not providing the correct name."
   sys.exit(1)
