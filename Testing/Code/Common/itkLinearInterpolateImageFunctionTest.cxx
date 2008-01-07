d6857aed88aa9795430564a6f2227911c9f48241
Luis Ibanez <luis.ibanez@kitware.com>
2008-01-06 19:56:16 -0500

    ENH: Fixing the initialization of the image (all regions).      Added numerical verification of the values returned by the interpolator.      Removed checks of extrapolation.

--- Testing/Code/Common/itkLinearInterpolateImageFunctionTest.cxx
+++ Testing/Code/Common/itkLinearInterpolateImageFunctionTest.cxx
@@ -28,29 +28,53 @@
 int itkLinearInterpolateImageFunctionTest( int , char*[] )
  {
 
-  const    unsigned int                                  Dimension = 2;
-  typedef  float                                         PixelType;
-  typedef  itk::Image< PixelType, Dimension >            ImageType;
+ const   unsigned int                                  Dimension = 2;
+ typedef float                                         PixelType;
+ typedef itk::Image< PixelType, Dimension >            ImageType;
+ typedef ImageType::RegionType                         RegionType;
+ typedef RegionType::SizeType                          SizeType;
+ typedef ImageType::IndexType                          IndexType;
 
- typedef itk::Image< PixelType, Dimension >                ImageType;
- typedef ImageType::RegionType                             RegionType;
- typedef RegionType::SizeType                              SizeType;
- typedef ImageType::IndexType                              IndexType;
- //typedef ImageType::ContinuousIndexType                    ContinuousIndexType;
- typedef itk::ContinuousIndex<float, 2>                     ContinuousIndexType;
+ typedef itk::ContinuousIndex<float, 2>                ContinuousIndexType;
+ typedef itk::Point<float,2>                           PointType;
+
  typedef itk::LinearInterpolateImageFunction< ImageType >  InterpolatorType;
 
  ImageType::Pointer image = ImageType::New();
+
+ IndexType start;
+ start.Fill( 0 );
+
  SizeType size;
  size.Fill( 3 );
- RegionType region( size );
 
- image->SetLargestPossibleRegion( region );
+ RegionType region;
+ region.SetSize( size );
+ region.SetIndex( start );
+
+ image->SetRegions( region );
  image->Allocate();
+
+ ImageType::PointType     origin;
+ ImageType::SpacingType   spacing;
+
+ origin.Fill( 0.0 );
+ spacing.Fill( 1.0 );
+
+ image->SetOrigin( origin );
+ image->SetSpacing( spacing );
+
+ image->Print( std::cout );
 
  unsigned int maxx = 3;
  unsigned int maxy = 3;
 
+ //
+ // Fill up the image values with the function
+ //
+ //   Intensity = f(x,y) = x + 3 * y
+ //
+ //
  for (unsigned int y = 0; y < maxy; y++)
    {
    for (unsigned int x = 0; x < maxx; x++)
@@ -69,23 +93,42 @@ int itkLinearInterpolateImageFunctionTest( int , char*[] )
  InterpolatorType::Pointer interpolator = InterpolatorType::New();
  interpolator->SetInputImage( image );
 
- double incr = 0.1;
+ const double incr = 0.1;
+ 
+ const double tolerance = 1e-6;
 
- for (double yy = 0; yy < static_cast<double>(maxy); yy++)
+ PointType point;
+
+ for (double yy = 0; yy < static_cast<double>(maxy-1); yy++)
    {
-   for (double xx = 0; xx < static_cast<double>(maxx); xx++)
+   for (double xx = 0; xx < static_cast<double>(maxx-1); xx++)
      {
-     for (double yyy = 0; yyy <= yy + 1.01; yyy += incr)
+     for (double yyy = yy; yyy < yy + 1.01; yyy += incr)
        {
-       for (double xxx = 0; xxx <= xx + 1.01; xxx += incr)
+       for (double xxx = xx; xxx < xx + 1.01; xxx += incr)
          {
-         ContinuousIndexType cindex;
-         cindex[0] = xxx;
-         cindex[1] = yyy;
-         double val = interpolator->EvaluateAtContinuousIndex( cindex );
-         std::cout << cindex << " Value : " << val << std::endl;
+         point[0] = xxx;
+         point[1] = yyy;
+
+         if( interpolator->IsInsideBuffer( point ) )
+           {
+           const double expectedValue = xxx + 3.0 * yyy;
+           
+           const double computedValue = interpolator->Evaluate( point );
+
+           const double difference = expectedValue - computedValue;
+
+           if( vcl_fabs( difference ) > tolerance )
+             {
+             std::cerr << "Error found while computing interpolation " << std::endl;
+             std::cerr << "Point = " << point << std::endl; 
+             std::cerr << "Expected value = " << expectedValue << std::endl;
+             std::cerr << "Computed value = " << computedValue << std::endl;
+             std::cerr << "Difference     = " << difference << std::endl;
+             return EXIT_FAILURE;
+             }
+           }
          }
-       std::cout << " -- " << std::endl;
        }
      }
    }
