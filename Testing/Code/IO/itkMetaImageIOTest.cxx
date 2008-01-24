d2a8f2f25cc6ca4caa8f8af2781a6fadd29d6b27
Bill Lorensen <bill.lorensen@gmail.com>
2008-01-24 09:01:22 -0500

    COMP: trying to track down segfault on 64 bit icc.

--- Testing/Code/IO/itkMetaImageIOTest.cxx
+++ Testing/Code/IO/itkMetaImageIOTest.cxx
@@ -47,8 +47,7 @@ int itkMetaImageIOTest(int ac, char* av[])
   catch (itk::ExceptionObject & e)
     {
     std::cerr << "exception in file reader " << std::endl;
-    std::cerr << e.GetDescription() << std::endl;
-    std::cerr << e.GetLocation() << std::endl;
+    std::cerr << e << std::endl;
     if(ac == 3) // should fail
       {
       return EXIT_SUCCESS;
