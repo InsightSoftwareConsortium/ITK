ec7f92d6c3ac5a74c398fc11f2c8068798027a89
Bill Lorensen <bill.lorensen@gmail.com>
2008-02-07 13:32:27 -0500

    BUG: revert bogus checkin.

--- Code/Review/itkStandardDeviationProjectionImageFilter.h
+++ Code/Review/itkStandardDeviationProjectionImageFilter.h
@@ -49,7 +49,7 @@ template <class TInputPixel, class TAccumulate>
 class StandardDeviationAccumulator
 {
 public:
-  typedef NumericTraits<TInputPixel>::RealType RealType;
+  typedef typename NumericTraits<TInputPixel>::RealType RealType;
 
   StandardDeviationAccumulator( unsigned long size )
     {
