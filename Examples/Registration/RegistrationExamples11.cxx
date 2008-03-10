fa35dcefd54dc43de913ccda215b5331977d5736
Luis Ibanez <luis.ibanez@kitware.com>
2008-03-10 14:01:16 -0400

    ENH: Adding optional tests for 3D image registration using the BrainWeb data      from http://public.kitware.com/pub/itk/Data/BrainWeb/

--- Examples/Registration/RegistrationExamples11.cxx
+++ Examples/Registration/RegistrationExamples11.cxx
@@ -26,18 +26,23 @@
 #endif
 
 #include <iostream>
-#include "vnl/vnl_sample.h"
 #include "itkTestMain.h" 
 
 
 void RegisterTests()
 {
-  vnl_sample_reseed(8775070);
+  REGISTER_TEST(ImageRegistration8Test);
   REGISTER_TEST(ImageRegistration9Test);
 }
 
 #undef main
+#define main  ImageRegistration8Test
+#undef CommandIterationUpdate
+#define CommandIterationUpdate CommandIterationUpdate08
+#include "ImageRegistration8.cxx"
+
+#undef main
 #define main  ImageRegistration9Test
 #undef CommandIterationUpdate
-#define CommandIterationUpdate CommandIterationUpdate11
+#define CommandIterationUpdate CommandIterationUpdate09
 #include "ImageRegistration9.cxx"
