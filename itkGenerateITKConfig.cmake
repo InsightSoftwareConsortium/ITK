bbc0e7abb5a34fb1bb85c4ab66466d64a88d35eb
Luis Ibanez <luis.ibanez@kitware.com>
2008-04-07 18:55:10 -0400

    ENH: Harmonizing with VTK the process for making the installation of ITK to      be relocatable. This makes possible to move the binare tree that results      from installing ITK, and still build projects againts the relocated binary tree.      Brad and Luis compared the previous ITK process with the one in VTK and      made them homogeneous (e.g. now ITK follows better the VTK model).

--- itkGenerateITKConfig.cmake
+++ itkGenerateITKConfig.cmake
@@ -16,29 +16,38 @@ ELSE(ITK_USE_SYSTEM_GDCM)
 ENDIF(ITK_USE_SYSTEM_GDCM)
 
 
-#-----------------------------------------------------------------------------
-# Settings common to the build and installation tree.
-
-# The "use" file.
-SET(ITK_USE_FILE                  UseITK.cmake)
-
-# The build settings file.
-SET(ITK_BUILD_SETTINGS_FILE       ITKBuildSettings.cmake)
-
 
 #-----------------------------------------------------------------------------
 # Settings specific to the build tree.
 
+# Generate CMake lines that will define the ITK_SOURCE_DIR in the ITKConfig.cmake.
+# We want this to happen only in the ITKConfig.cmake of the build dir, not in the
+# installed or relocatable one.
+SET(ITK_CONFIG_CODE "
+# The ITK source tree.
+# For backward compatibility issues we still need to define this variable, although
+# it is highly probable that it will cause more harm than being useful. 
+# Use ITK_INCLUDE_DIRS instead, since ITK_SOURCE_DIR may point to non-existent directory
+IF(NOT ITK_LEGACY_REMOVE)
+  SET(ITK_SOURCE_DIR \"${ITK_SOURCE_DIR}\")
+ENDIF(NOT ITK_LEGACY_REMOVE)"
+)
+
 # The library dependencies file.
-SET(ITK_LIBRARY_DEPENDS_FILE       ITKLibraryDepends.cmake)
+SET(ITK_LIBRARY_DEPENDS_FILE  ${ITK_BINARY_DIR}/ITKLibraryDepends.cmake)
+
+# The "use" file.
+SET(ITK_USE_FILE ${ITK_BINARY_DIR}/UseITK.cmake)
+
+# The build settings file.
+SET(ITK_BUILD_SETTINGS_FILE ${ITK_BINARY_DIR}/ITKBuildSettings.cmake)
 
 # Library directory.
-SET(ITK_LIBRARY_DIRS_CONFIG ${ITK_BUILD_LIB_DIR})
+SET(ITK_LIBRARY_DIRS_CONFIG ${ITK_LIBRARY_PATH})
 
 # Determine the include directories needed.
 SET(ITK_INCLUDE_DIRS_CONFIG
   ${ITK_INCLUDE_DIRS_BUILD_TREE}
-  ${ITK_INCLUDE_DIRS_SOURCE_TREE}
   ${ITK_INCLUDE_DIRS_SYSTEM}
 )
 
@@ -60,21 +69,25 @@ CONFIGURE_FILE(${ITK_SOURCE_DIR}/ITKConfig.cmake.in
 # Settings specific to the install tree.
 
 # The library dependencies file.
-SET(ITK_LIBRARY_DEPENDS_FILE      ITKLibraryDepends.cmake)
+SET(ITK_LIBRARY_DEPENDS_FILE "\${ITK_INSTALL_PREFIX}${ITK_INSTALL_PACKAGE_DIR}/ITKLibraryDepends.cmake")
+
+# The "use" file.
+SET(ITK_USE_FILE \${ITK_INSTALL_PREFIX}${ITK_INSTALL_PACKAGE_DIR}/UseITK.cmake)
+
+# The build settings file.
+SET(ITK_BUILD_SETTINGS_FILE \${ITK_INSTALL_PREFIX}${ITK_INSTALL_PACKAGE_DIR}/ITKBuildSettings.cmake)
 
 # Include directories.
-SET(ITK_INCLUDE_DIRS_CONFIG
-  ${ITK_INCLUDE_DIRS_INSTALL_TREE}
-  ${ITK_INCLUDE_DIRS_SYSTEM}
-)
+SET(ITK_INCLUDE_DIRS_CONFIG \${ITK_INSTALL_PREFIX}${ITK_INSTALL_INCLUDE_DIR})
+FOREACH(DIR ${ITK_INCLUDE_RELATIVE_DIRS})
+  LIST(APPEND ITK_INCLUDE_DIRS_CONFIG \${ITK_INSTALL_PREFIX}${ITK_INSTALL_INCLUDE_DIR}/${DIR})
+ENDFOREACH(DIR)
+IF(ITK_INCLUDE_DIRS_SYSTEM)
+  LIST(APPEND ITK_INCLUDE_DIRS_CONFIG ${ITK_INCLUDE_DIRS_SYSTEM})
+ENDIF(ITK_INCLUDE_DIRS_SYSTEM)
 
 # Link directories.
-# The install tree will use the directory where ITKConfig.cmake is found, which
-# happens to be "INSTALLATION/lib/InsightToolkit". That is, it is already the
-# same directory where the libraries are installed. Therefore this variable
-# must be empty here. See ITKConfig.cmake.in for details on how this variable
-# is used.
-SET(ITK_LIBRARY_DIRS_CONFIG "")  
+SET(ITK_LIBRARY_DIRS_CONFIG "\${ITK_INSTALL_PREFIX}${ITK_INSTALL_LIB_DIR}")
 
 # The CableSwig configuration directory.
 IF(ITK_NEED_CableSwig)
@@ -93,5 +106,21 @@ ENDIF(ITK_NEED_CableSwig)
 
 #-----------------------------------------------------------------------------
 # Configure ITKConfig.cmake for the install tree.
+
+# Construct the proper number of GET_FILENAME_COMPONENT(... PATH)
+# calls to compute the installation prefix.
+STRING(REGEX REPLACE "/" ";" ITK_INSTALL_PACKAGE_DIR_COUNT
+  "${ITK_INSTALL_PACKAGE_DIR}")
+SET(ITK_CONFIG_CODE "
+# Compute the installation prefix from this ITKConfig.cmake file location.
+GET_FILENAME_COMPONENT(ITK_INSTALL_PREFIX \"\${CMAKE_CURRENT_LIST_FILE}\" PATH)")
+FOREACH(p ${ITK_INSTALL_PACKAGE_DIR_COUNT})
+  SET(ITK_CONFIG_CODE
+    "${ITK_CONFIG_CODE}\nGET_FILENAME_COMPONENT(ITK_INSTALL_PREFIX \"\${ITK_INSTALL_PREFIX}\" PATH)"
+    )
+ENDFOREACH(p)
+
+
 CONFIGURE_FILE(${ITK_SOURCE_DIR}/ITKConfig.cmake.in
                ${ITK_BINARY_DIR}/Utilities/ITKConfig.cmake @ONLY IMMEDIATE)
+
