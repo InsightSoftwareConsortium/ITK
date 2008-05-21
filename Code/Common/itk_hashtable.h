c1e6b9870ab8c1628aedc1cfc541e44136bc12e1
Luis Ibanez <luis.ibanez@kitware.com>
2008-05-21 15:30:45 -0400

    COMP: Restoring the previous code for the case of Borland, while we look for       a common solution between VS9 and bcc55.

--- Code/Common/itk_hashtable.h
+++ Code/Common/itk_hashtable.h
@@ -450,7 +450,11 @@ public:
 
   const_iterator end() const { return const_iterator((node*)0, this); }
 
+#ifndef __BORLANDC__
   friend bool operator== <self>(const self&,const self&);
+#else
+  friend bool operator==VCL_NULL_TMPL_ARGS(const self&,const self&);
+#endif
 
 public:
 
