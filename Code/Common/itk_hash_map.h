df3f58f615d7c903e7f9f4fa1bdfe6801174b183
Luis Ibanez <luis.ibanez@kitware.com>
2008-05-21 14:25:24 -0400

    COMP: Fixing warning in Visual Studio 9 (2008), regarding friend templated function.       the construction:   friend bool function<>(type arg)       was replaced with   friend bool function<type>(type arg)

--- Code/Common/itk_hash_map.h
+++ Code/Common/itk_hash_map.h
@@ -178,8 +178,7 @@ public:
   bool empty() const { return rep.empty(); }
   void swap(self& hs) { rep.swap(hs.rep); }
 #ifndef __BORLANDC__
-  friend bool operator==VCL_NULL_TMPL_ARGS(const hash_map<Key,T,HashFcn,EqualKey,Alloc>&,
-                         const hash_map<Key,T,HashFcn,EqualKey,Alloc>&);
+  friend bool operator==<self>(const self &, const self &);
 #endif
   iterator begin() { return rep.begin(); }
   iterator end() { return rep.end(); }
@@ -282,8 +281,7 @@ public:
   size_type max_size() const { return rep.max_size(); }
   bool empty() const { return rep.empty(); }
   void swap(self& hs) { rep.swap(hs.rep); }
-  friend bool operator==VCL_NULL_TMPL_ARGS(const hash_multimap<Key,T,HashFcn,EqualKey,Alloc>&,
-                         const hash_multimap<Key,T,HashFcn,EqualKey,Alloc>&);
+  friend bool operator==<self>(const self &, const self &);
 
   iterator begin() { return rep.begin(); }
   iterator end() { return rep.end(); }
--- Code/Common/itk_hashtable.h
+++ Code/Common/itk_hashtable.h
@@ -450,9 +450,8 @@ public:
 
   const_iterator end() const { return const_iterator((node*)0, this); }
 
-  //  friend IUEi_STL_INLINE bool operator== VCL_NULL_TMPL_ARGS (const
-  //  self&,const self&);
-  friend bool operator== VCL_NULL_TMPL_ARGS (const self&,const self&);
+  friend bool operator== <self>(const self&,const self&);
+
 public:
 
   size_type bucket_count() const { return this->buckets.size(); }
