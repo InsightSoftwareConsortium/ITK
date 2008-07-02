87f9d145c68fbc5fc3852de56c3f0c3f48623ad3
Luis Ibanez <luis.ibanez@kitware.com>
2008-07-02 18:01:48 -0400

    COMP: Fixing the absence of initialization of the Zero and One static members       of the NumericTraits.

--- Code/Common/itkNumericTraitsRGBPixel.cxx
+++ Code/Common/itkNumericTraitsRGBPixel.cxx
@@ -14,13 +14,36 @@
      PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
-#include "itkNumericTraitsRGBPixel.h"
+#include "itkRGBPixel.h"
 
 namespace itk
 {
 
-
 // All the specializations that were here previously have now been 
 // replaced with a single template in the header file.
+//
+
+//
+// Helper macro for initializing the Zero and One static member of the NumericTraits<>.
+//
+#define RGBPIXELSTATICTRAITSMACRO( T ) \
+template<> \
+const RGBPixel<T>  NumericTraits< RGBPixel<T> >::Zero = RGBPixel<T>( NumericTraits<T>::Zero ); \
+template<> \
+const RGBPixel<T>  NumericTraits< RGBPixel<T> >::One = RGBPixel<T>( NumericTraits<T>::One );
+
+//
+// List here the specializations of the Traits:
+//
+RGBPIXELSTATICTRAITSMACRO( char );
+RGBPIXELSTATICTRAITSMACRO( unsigned char );
+RGBPIXELSTATICTRAITSMACRO( short );
+RGBPIXELSTATICTRAITSMACRO( unsigned short );
+RGBPIXELSTATICTRAITSMACRO( int );
+RGBPIXELSTATICTRAITSMACRO( unsigned int );
+RGBPIXELSTATICTRAITSMACRO( long );
+RGBPIXELSTATICTRAITSMACRO( unsigned long );
+RGBPIXELSTATICTRAITSMACRO( float );
+RGBPIXELSTATICTRAITSMACRO( double );
 
 } // end namespace itk
