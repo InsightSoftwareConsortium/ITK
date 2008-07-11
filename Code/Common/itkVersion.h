6169818943fa0a0036f242e91f336eb39c322506
Luis Ibanez <luis.ibanez@kitware.com>
2008-07-10 21:52:32 -0400

    ENH: Version number update

--- Code/Common/itkVersion.h
+++ Code/Common/itkVersion.h
@@ -28,7 +28,7 @@
 #define ITK_VERSION ITK_VERSION_TO_STRING(ITK_VERSION_MAJOR) "." \
                     ITK_VERSION_TO_STRING(ITK_VERSION_MINOR) "." \
                     ITK_VERSION_TO_STRING(ITK_VERSION_PATCH)
-#define ITK_SOURCE_VERSION "itk version " ITK_VERSION ", itk source $Revision: 1.2732 $, $Date: 2008-07-10 01:52:38 $ (GMT)"
+#define ITK_SOURCE_VERSION "itk version " ITK_VERSION ", itk source $Revision: 1.2733 $, $Date: 2008-07-11 01:52:32 $ (GMT)"
 
 namespace itk
 {
