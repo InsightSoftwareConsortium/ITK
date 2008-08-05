2d96c0d019d0e7c992b37d38dfbf4fa50930b281
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2008-08-05 08:30:34 -0400

    ENH: Implement MedianImageFilter as a subclass of BoxImageFilter

--- Code/Review/itkOptMedianImageFilter.h
+++ Code/Review/itkOptMedianImageFilter.h
@@ -1,7 +1,7 @@
 /*=========================================================================
 
   Program:   Insight Segmentation & Registration Toolkit
-  Module:    itkMedianImageFilter.h
+  Module:    itkOptMedianImageFilter.h
   Language:  C++
   Date:      $Date$
   Version:   $Revision$
@@ -14,20 +14,10 @@
      PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
-#ifndef __itkMedianImageFilter_h
-#define __itkMedianImageFilter_h
+#ifndef __itkOptMedianImageFilter_h
+#define __itkOptMedianImageFilter_h
 
-
-// First make sure that the configuration is available.
-// This line can be removed once the optimized versions
-// gets integrated into the main directories.
-#include "itkConfigure.h"
-
-#ifdef ITK_USE_CONSOLIDATED_MORPHOLOGY
-#include "itkOptMedianImageFilter.h"
-#else
-
-#include "itkImageToImageFilter.h"
+#include "itkBoxImageFilter.h"
 #include "itkImage.h"
 
 namespace itk
@@ -53,7 +43,7 @@ namespace itk
  */
 template <class TInputImage, class TOutputImage>
 class ITK_EXPORT MedianImageFilter :
-    public ImageToImageFilter< TInputImage, TOutputImage >
+    public BoxImageFilter< TInputImage, TOutputImage >
 {
 public:
   /** Extract dimension from input and output image. */
@@ -63,43 +53,29 @@ public:
                       TOutputImage::ImageDimension);
 
   /** Convenient typedefs for simplifying declarations. */
-  typedef TInputImage InputImageType;
-  typedef TOutputImage OutputImageType;
+  typedef TInputImage                                          InputImageType;
+  typedef TOutputImage                                         OutputImageType;
 
   /** Standard class typedefs. */
-  typedef MedianImageFilter Self;
+  typedef MedianImageFilter                                    Self;
   typedef ImageToImageFilter< InputImageType, OutputImageType> Superclass;
-  typedef SmartPointer<Self> Pointer;
-  typedef SmartPointer<const Self>  ConstPointer;
+  typedef SmartPointer<Self>                                   Pointer;
+  typedef SmartPointer<const Self>                             ConstPointer;
 
   /** Method for creation through the object factory. */
   itkNewMacro(Self);
 
   /** Run-time type information (and related methods). */
-  itkTypeMacro(MedianImageFilter, ImageToImageFilter);
+  itkTypeMacro(MedianImageFilter, BoxImageFilter);
   
   /** Image typedef support. */
-  typedef typename InputImageType::PixelType InputPixelType;
-  typedef typename OutputImageType::PixelType OutputPixelType;
+  typedef typename InputImageType::PixelType                   InputPixelType;
+  typedef typename OutputImageType::PixelType                  OutputPixelType;
 
-  typedef typename InputImageType::RegionType InputImageRegionType;
-  typedef typename OutputImageType::RegionType OutputImageRegionType;
+  typedef typename InputImageType::RegionType                  InputImageRegionType;
+  typedef typename OutputImageType::RegionType                 OutputImageRegionType;
 
-  typedef typename InputImageType::SizeType InputSizeType;
-
-  /** Set the radius of the neighborhood used to compute the median. */
-  itkSetMacro(Radius, InputSizeType);
-
-  /** Get the radius of the neighborhood used to compute the median */
-  itkGetConstReferenceMacro(Radius, InputSizeType);
-  
-  /** MedianImageFilter needs a larger input requested region than
-   * the output requested region.  As such, MedianImageFilter needs
-   * to provide an implementation for GenerateInputRequestedRegion()
-   * in order to inform the pipeline execution model.
-   *
-   * \sa ImageToImageFilter::GenerateInputRequestedRegion() */
-  virtual void GenerateInputRequestedRegion() throw(InvalidRequestedRegionError);
+  typedef typename InputImageType::SizeType                    InputSizeType;
 
 #ifdef ITK_USE_CONCEPT_CHECKING
   /** Begin concept checking */
@@ -115,7 +91,6 @@ public:
 protected:
   MedianImageFilter();
   virtual ~MedianImageFilter() {}
-  void PrintSelf(std::ostream& os, Indent indent) const;
 
   /** MedianImageFilter can be implemented as a multithreaded filter.
    * Therefore, this implementation provides a ThreadedGenerateData()
@@ -133,16 +108,12 @@ protected:
 private:
   MedianImageFilter(const Self&); //purposely not implemented
   void operator=(const Self&); //purposely not implemented
-
-  InputSizeType m_Radius;
 };
   
 } // end namespace itk
 
 #ifndef ITK_MANUAL_INSTANTIATION
-#include "itkMedianImageFilter.txx"
-#endif
-
+#include "itkOptMedianImageFilter.txx"
 #endif
 
 #endif
