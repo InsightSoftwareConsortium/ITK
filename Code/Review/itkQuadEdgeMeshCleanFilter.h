67c3523221664507c87f5a33da348c902f0d096b
Luis Ibanez <luis.ibanez@kitware.com>
2008-09-30 18:08:42 -0400

    STYLE: Fixing indentations, and replacing for loops of iterators with while loops.

--- Code/Review/itkQuadEdgeMeshCleanFilter.h
+++ Code/Review/itkQuadEdgeMeshCleanFilter.h
@@ -1,8 +1,25 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkQuadEdgeMeshCleanFilter.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
 #ifndef __itkQuadEdgeMeshCleanFilter_h
 #define __itkQuadEdgeMeshCleanFilter_h
 
-#include <itkQuadEdgeMeshToQuadEdgeMeshFilter.h>
-#include <itkBoundingBox.h>
+#include "itkQuadEdgeMeshToQuadEdgeMeshFilter.h"
+#include "itkBoundingBox.h"
 
 #include "itkQuadEdgeMeshSquaredEdgeLengthDecimation.h"
 #include "itkQuadEdgeMeshDecimationCriteria.h"
@@ -12,102 +29,97 @@ namespace itk
 /**
  * \class QuadEdgeMeshCleanFilter
  * \brief
-*/
+ */
 template< class TInput, class TOutput >
 class QuadEdgeMeshCleanFilter :
   public QuadEdgeMeshToQuadEdgeMeshFilter< TInput, TOutput >
 {
 public:
-  typedef QuadEdgeMeshCleanFilter Self;
-  typedef SmartPointer< Self > Pointer;
-  typedef SmartPointer< const Self > ConstPointer;
-  typedef QuadEdgeMeshToQuadEdgeMeshFilter< TInput, TOutput > Superclass;
+  typedef QuadEdgeMeshCleanFilter                                 Self;
+  typedef SmartPointer< Self >                                    Pointer;
+  typedef SmartPointer< const Self >                              ConstPointer;
+  typedef QuadEdgeMeshToQuadEdgeMeshFilter< TInput, TOutput >     Superclass;
 
   /** Run-time type information (and related methods).   */
   itkTypeMacro( QuadEdgeMeshCleanFilter, QuadEdgeMeshToQuadEdgeMeshFilter );
+
   /** New macro for creation of through a Smart Pointer   */
   itkNewMacro( Self );
 
-  typedef TInput InputMeshType;
-  typedef typename Superclass::InputMeshPointer InputMeshPointer;
-  typedef typename Superclass::InputCoordRepType    InputCoordRepType;
-  typedef typename Superclass::InputPointType       InputPointType;
-  typedef typename Superclass::InputPointIdentifier InputPointIdentifier;
-  typedef typename Superclass::InputQEPrimal        InputQEPrimal;
-  typedef typename Superclass::InputVectorType      InputVectorType;
+  typedef TInput                                                InputMeshType;
+  typedef typename Superclass::InputMeshPointer                 InputMeshPointer;
+  typedef typename Superclass::InputCoordRepType                InputCoordRepType;
+  typedef typename Superclass::InputPointType                   InputPointType;
+  typedef typename Superclass::InputPointIdentifier             InputPointIdentifier;
+  typedef typename Superclass::InputQEPrimal                    InputQEPrimal;
+  typedef typename Superclass::InputVectorType                  InputVectorType;
 
-  typedef typename Superclass::InputEdgeCellType InputEdgeCellType; 
-  typedef typename Superclass::InputPolygonCellType InputPolygonCellType;
-  typedef typename Superclass::InputPointIdList InputPointIdList;
-  typedef typename Superclass::InputCellTraits InputCellTraits;
-  typedef typename Superclass::InputPointsIdInternalIterator
-    InputPointsIdInternalIterator;
-  typedef typename Superclass::InputQEIterator InputQEIterator;
+  typedef typename Superclass::InputEdgeCellType                InputEdgeCellType; 
+  typedef typename Superclass::InputPolygonCellType             InputPolygonCellType;
+  typedef typename Superclass::InputPointIdList                 InputPointIdList;
+  typedef typename Superclass::InputCellTraits                  InputCellTraits;
+  typedef typename Superclass::InputPointsIdInternalIterator    InputPointsIdInternalIterator;
+  typedef typename Superclass::InputQEIterator                  InputQEIterator;
 
-  typedef typename InputMeshType::PointsContainer InputPointsContainer;
-  typedef typename InputMeshType::PointsContainerPointer
-    InputPointsContainerPointer;
-  typedef typename InputMeshType::PointsContainerIterator
-    InputPointsContainerIterator;
+  typedef typename InputMeshType::PointsContainer               InputPointsContainer;
+  typedef typename InputMeshType::PointsContainerPointer        InputPointsContainerPointer;
+  typedef typename InputMeshType::PointsContainerIterator       InputPointsContainerIterator;
 
-  typedef typename InputMeshType::CellsContainerIterator
-      InputCellsContainerIterator;
+  typedef typename InputMeshType::CellsContainerIterator        InputCellsContainerIterator;
 
-  itkStaticConstMacro( PointDimension, unsigned int,
-                       InputMeshType::PointDimension );
+  itkStaticConstMacro( PointDimension, unsigned int, InputMeshType::PointDimension );
 
-  typedef TOutput OutputMeshType;
-  typedef typename Superclass::OutputMeshPointer     OutputMeshPointer;
-  typedef typename Superclass::OutputCoordRepType    OutputCoordRepType;
-  typedef typename Superclass::OutputPointType       OutputPointType;
-  typedef typename Superclass::OutputPointIdentifier OutputPointIdentifier;
-  typedef typename Superclass::OutputQEPrimal        OutputQEPrimal;
-  typedef typename Superclass::OutputVectorType      OutputVectorType;
-  typedef typename OutputMeshType::QEType            OutputQEType;
+  typedef TOutput                                               OutputMeshType;
+  typedef typename Superclass::OutputMeshPointer                OutputMeshPointer;
+  typedef typename Superclass::OutputCoordRepType               OutputCoordRepType;
+  typedef typename Superclass::OutputPointType                  OutputPointType;
+  typedef typename Superclass::OutputPointIdentifier            OutputPointIdentifier;
+  typedef typename Superclass::OutputQEPrimal                   OutputQEPrimal;
+  typedef typename Superclass::OutputVectorType                 OutputVectorType;
 
-  typedef typename OutputMeshType::PointsContainer OutputPointsContainer;
+  typedef typename OutputMeshType::QEType                       OutputQEType;
+  typedef typename OutputMeshType::PointsContainer              OutputPointsContainer;
+  typedef typename OutputMeshType::PointsContainerPointer       OutputPointsContainerPointer;
+  typedef typename OutputMeshType::PointsContainerIterator      OutputPointsContainerIterator;
 
-  typedef typename OutputMeshType::PointsContainerPointer
-    OutputPointsContainerPointer;
-
-  typedef typename OutputMeshType::PointsContainerIterator
-    OutputPointsContainerIterator;
-
-  typedef typename OutputMeshType::CellsContainerIterator
-      OutputCellsContainerIterator;
+  typedef typename OutputMeshType::CellsContainerIterator      OutputCellsContainerIterator;
 
   typedef BoundingBox< InputPointIdentifier, itkGetStaticConstMacro(PointDimension),
     InputCoordRepType, InputPointsContainer > BoundingBoxType;
 
-  typedef typename BoundingBoxType::Pointer BoundingBoxPointer;
+  typedef typename BoundingBoxType::Pointer                 BoundingBoxPointer;
 
-  typedef MaxMeasureBoundCriterion< OutputMeshType > CriterionType;
-  typedef typename CriterionType::Pointer CriterionPointer;
+  typedef MaxMeasureBoundCriterion< OutputMeshType >        CriterionType;
+  typedef typename CriterionType::Pointer                   CriterionPointer;
 
   typedef QuadEdgeMeshSquaredEdgeLengthDecimation< InputMeshType,
-    InputMeshType, CriterionType > DecimationType;
-  typedef typename DecimationType::Pointer DecimationPointer;
+    InputMeshType, CriterionType >                          DecimationType;
+  typedef typename DecimationType::Pointer                  DecimationPointer;
 
   itkSetMacro( AbsoluteTolerance, InputCoordRepType );
   itkSetMacro( RelativeTolerance, InputCoordRepType );
 
 protected:
-  QuadEdgeMeshCleanFilter() : Superclass(),
-    m_AbsoluteTolerance2( static_cast< InputCoordRepType >( 0. ) ),
-    m_AbsoluteTolerance( static_cast< InputCoordRepType >( 0. ) ),
-    m_RelativeTolerance( static_cast< InputCoordRepType >( 0. ) )
-   {}
-  ~QuadEdgeMeshCleanFilter() {}
+  QuadEdgeMeshCleanFilter()
+    {
+    this->m_AbsoluteTolerance2 = itk::NumericTraits< InputCoordRepType >::Zero;
+    this->m_AbsoluteTolerance  = itk::NumericTraits< InputCoordRepType >::Zero;
+    this->m_RelativeTolerance  = itk::NumericTraits< InputCoordRepType >::Zero;
+    }
+
+  virtual ~QuadEdgeMeshCleanFilter() {}
 
   InputCoordRepType m_AbsoluteTolerance2;
   InputCoordRepType m_AbsoluteTolerance;
   InputCoordRepType m_RelativeTolerance;
 
   void GenerateData()
-  {
-    if( ( m_AbsoluteTolerance == 0. ) && ( m_RelativeTolerance != 0. ) )
+    {
+    InputCoordRepType zeroValue = itk::NumericTraits< InputCoordRepType >::Zero;
+
+    if( ( m_AbsoluteTolerance == zeroValue ) && ( m_RelativeTolerance != zeroValue ) )
       {
-      assert( ( m_RelativeTolerance > 0. ) && ( m_RelativeTolerance < 1. ) );
+      assert( ( m_RelativeTolerance > zeroValue ) && ( m_RelativeTolerance < 1. ) );
       BoundingBoxPointer bounding_box = BoundingBoxType::New();
       bounding_box->SetPoints( this->GetInput()->GetPoints() );
       bounding_box->ComputeBoundingBox();
@@ -115,15 +127,18 @@ protected:
       m_AbsoluteTolerance2 = m_RelativeTolerance * m_RelativeTolerance *
         bounding_box->GetDiagonalLength2();
       }
-    if( m_AbsoluteTolerance != 0. )
-      m_AbsoluteTolerance2 = m_AbsoluteTolerance * m_AbsoluteTolerance;
 
-    MergePoints();
-    CleanPoints();
-  }
+    if( m_AbsoluteTolerance != zeroValue )
+      {
+      m_AbsoluteTolerance2 = m_AbsoluteTolerance * m_AbsoluteTolerance;
+      }
+
+    this->MergePoints();
+    this->CleanPoints();
+    }
 
   void MergePoints()
-  {
+    {
     OutputMeshPointer output = this->GetOutput();
 
     CriterionPointer criterion = CriterionType::New();
@@ -142,62 +157,67 @@ protected:
 
     OutputPointType pOut;
 
-    for( ; p_it != p_end; ++p_it )
+    while( p_it != p_end )
       {
       pOut.CastFrom( p_it.Value() );
       output->SetPoint( p_it.Index(), pOut );
+      ++p_it;
       }
 
     // Copy cells
-  InputCellsContainerIterator c_it = temp->GetCells()->Begin();
-  InputCellsContainerIterator c_end = temp->GetCells()->End();
-  InputEdgeCellType* qe;
-  InputPolygonCellType* pe;
-  InputQEPrimal* QEGeom;
+    InputCellsContainerIterator c_it = temp->GetCells()->Begin();
+    InputCellsContainerIterator c_end = temp->GetCells()->End();
+    InputEdgeCellType* qe;
+    InputPolygonCellType* pe;
+    InputQEPrimal* QEGeom;
 
-  for( ; c_it != c_end; ++c_it )
-    {
-    if( ( qe = dynamic_cast< InputEdgeCellType* >( c_it.Value() ) ) )
+    while( c_it != c_end )
       {
-      QEGeom = qe->GetQEGeom( );
-      output->AddEdgeWithSecurePointList( QEGeom->GetOrigin(),
-        QEGeom->GetDestination() );
-      }
-    else
-      {
-      pe = dynamic_cast< InputPolygonCellType* >( c_it.Value());
-      if( pe )
+      if( ( qe = dynamic_cast< InputEdgeCellType* >( c_it.Value() ) ) )
         {
-        InputPointIdList points;
-
-        for( InputPointsIdInternalIterator pit = pe->InternalPointIdsBegin();
-             pit != pe->InternalPointIdsEnd( ); ++pit )
+        QEGeom = qe->GetQEGeom( );
+        output->AddEdgeWithSecurePointList( QEGeom->GetOrigin(),
+          QEGeom->GetDestination() );
+        }
+      else
+        {
+        pe = dynamic_cast< InputPolygonCellType* >( c_it.Value());
+        if( pe )
           {
-          points.push_back( ( *pit ) );
-          }
-        output->AddFaceWithSecurePointList( points );
-        } 
+          InputPointIdList points;
+
+          for( InputPointsIdInternalIterator pit = pe->InternalPointIdsBegin();
+               pit != pe->InternalPointIdsEnd( ); ++pit )
+            {
+            points.push_back( ( *pit ) );
+            }
+          output->AddFaceWithSecurePointList( points );
+          } 
+        }
+      ++c_it;
       }
     }
-  }
 
   void CleanPoints()
-  {
+    {
     OutputMeshPointer output = this->GetOutput();
 
     OutputPointsContainerIterator p_it = output->GetPoints()->Begin();
     OutputPointsContainerIterator p_end = output->GetPoints()->End();
     OutputPointIdentifier id( 0 );
 
-    for( ; p_it != p_end; ++p_it )
+    while( p_it != p_end )
       {
       id = p_it->Index();
       if( output->FindEdge( id ) == 0 )
+        {
         output->DeletePoint( id );
+        }
+      ++p_it;
       }
 
     output->SqueezePointsIds( );
-  }
+    }
 
 private:
   QuadEdgeMeshCleanFilter( const Self& );
