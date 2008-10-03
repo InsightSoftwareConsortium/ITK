eb714ca0df53626a45a502063735f9843537bd10
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2008-10-03 16:59:38 -0400

    ENH: Wrap ZeroCrossingBasedEdgeDetectionImageFilter - it has been turned off a long time ago because of a bug which seem to have disapear now. The current code is based on Dan's Mueller code, probably from ManagedITK.

--- Wrapping/WrapITK/Modules/EdgesAndContours/wrap_itkZeroCrossingBasedEdgeDetectionImageFilter.cmake
+++ Wrapping/WrapITK/Modules/EdgesAndContours/wrap_itkZeroCrossingBasedEdgeDetectionImageFilter.cmake
@@ -1,8 +1,6 @@
-# and again, this boring problem of FixedArray typemap with python
+WRAP_CLASS("itk::ZeroCrossingBasedEdgeDetectionImageFilter" POINTER)
 
-# WRAP_CLASS("itk::ZeroCrossingBasedEdgeDetectionImageFilter" POINTER)
-# 
-#   WRAP_IMAGE_FILTER_SIGN_INT(2)
-#   WRAP_IMAGE_FILTER_REAL(2)
-# 
-# END_WRAP_CLASS()
+  WRAP_IMAGE_FILTER_REAL(2 2+)
+  
+END_WRAP_CLASS()
+  
