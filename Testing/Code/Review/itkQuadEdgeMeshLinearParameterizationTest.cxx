0f623e17b745be52529e7071ead9db1541bd6652
Bill Lorensen <bill.lorensen@gmail.com>
2008-10-04 13:41:56 -0400

    STYLE: Added copyright.

--- Testing/Code/Review/itkQuadEdgeMeshLinearParameterizationTest.cxx
+++ Testing/Code/Review/itkQuadEdgeMeshLinearParameterizationTest.cxx
@@ -1,4 +1,19 @@
-// BELONG TO \REVIEW
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkQuadEdgeMeshLinearParameterizationTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
 #include <itkVTKPolyDataReader.h>
 #include <itkVTKPolyDataWriter.h>
 #include <itkQuadEdgeMesh.h>
