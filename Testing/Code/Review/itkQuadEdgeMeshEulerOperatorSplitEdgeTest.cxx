29a90a8bedab923c8bf4016e3e687468a5b489db
Luis Ibanez <luis.ibanez@kitware.com>
2008-10-04 10:48:56 -0400

    STYLE: Fixing copyright header. Removing unused variables argc,argv.

--- Testing/Code/Review/itkQuadEdgeMeshEulerOperatorSplitEdgeTest.cxx
+++ Testing/Code/Review/itkQuadEdgeMeshEulerOperatorSplitEdgeTest.cxx
@@ -1,3 +1,20 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkQuadEdgeMeshEulerOperatorSplitEdgeTest.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
 #if defined(_MSC_VER)
 #pragma warning ( disable : 4786 )
 #endif
@@ -12,10 +29,8 @@
 #include "itkQuadEdgeMeshEulerOperatorSplitEdgeFunction.h"
 #include "itkQuadEdgeMeshEulerOperatorsTestHelper.h"
 
-int itkQuadEdgeMeshEulerOperatorSplitEdgeTest(int argc, char* argv[] )
+int itkQuadEdgeMeshEulerOperatorSplitEdgeTest( int, char * [] )
 {
-  (void)argc;
-  (void)argv;
   
   typedef itk::QuadEdgeMesh< double, 3 >                      MeshType;
   typedef MeshType::Pointer                                   MeshPointer;
