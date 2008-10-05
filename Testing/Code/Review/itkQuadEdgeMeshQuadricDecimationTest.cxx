3f1978c06c74e4be55f62af106f88d3f6070ea0c
Luis Ibanez <luis.ibanez@kitware.com>
2008-10-05 18:47:47 -0400

    STYLE: Fixing many indentations.

--- Testing/Code/Review/itkQuadEdgeMeshQuadricDecimationTest.cxx
+++ Testing/Code/Review/itkQuadEdgeMeshQuadricDecimationTest.cxx
@@ -27,63 +27,63 @@ using namespace itk;
 
 int itkQuadEdgeMeshQuadricDecimationTest( int argc, char* argv[] )
 {
-    // ** ERROR MESSAGE AND HELP ** //
-    if( argc < 3 )
+  // ** ERROR MESSAGE AND HELP ** //
+  if( argc < 3 )
     {
-        std::cout <<"Requires 3 argument: " <<std::endl;
-        std::cout <<"1-Input file name " <<std::endl;
-        std::cout <<"2-Number of Faces " <<std::endl;
-        std::cout <<"3-Output file name " <<std::endl;
-        return EXIT_FAILURE;
+    std::cout << "Requires 3 argument: " << std::endl;
+    std::cout << "1-Input file name " << std::endl;
+    std::cout << "2-Number of Faces " << std::endl;
+    std::cout << "3-Output file name " << std::endl;
+    return EXIT_FAILURE;
     }
 
-  // ** TYPEDEF **
-    typedef double Coord;
-    const unsigned int Dimension = 3;
+// ** TYPEDEF **
+  typedef double        CoordType;
+  const unsigned int    Dimension = 3;
 
-    typedef QuadEdgeMesh< Coord, Dimension > MeshType;
-    typedef VTKPolyDataReader< MeshType > ReaderType;
-    typedef VTKPolyDataWriter< MeshType > WriterType;
+  typedef QuadEdgeMesh< CoordType, Dimension >      MeshType;
+  typedef VTKPolyDataReader< MeshType >             ReaderType;
+  typedef VTKPolyDataWriter< MeshType >             WriterType;
 
-    // ** READ THE FILE IN **
-    ReaderType::Pointer reader = ReaderType::New( );
-    reader->SetFileName( argv[1] );
-    try
+  // ** READ THE FILE IN **
+  ReaderType::Pointer reader = ReaderType::New( );
+  reader->SetFileName( argv[1] );
+  try
     {
-      reader->Update( );
+    reader->Update( );
     }
-    catch( itk::ExceptionObject & exp )
+  catch( itk::ExceptionObject & exp )
     {
-      std::cerr << "Exception thrown while reading the input file " << std::endl;
-      std::cerr << exp << std::endl;
-      return EXIT_FAILURE;
+    std::cerr << "Exception thrown while reading the input file " << std::endl;
+    std::cerr << exp << std::endl;
+    return EXIT_FAILURE;
     }
 
-    MeshType::Pointer mesh = reader->GetOutput( );
+  MeshType::Pointer mesh = reader->GetOutput( );
 
-    typedef NumberOfFacesCriterion< MeshType > CriterionType;
-    typedef QuadEdgeMeshQuadricDecimation< MeshType, MeshType, CriterionType >
-      DecimationType;
+  typedef NumberOfFacesCriterion< MeshType > CriterionType;
+  typedef QuadEdgeMeshQuadricDecimation< MeshType, MeshType, CriterionType >
+    DecimationType;
 
-    long N;
-    std::stringstream ssout( argv[2] );
-    ssout >>N;
+  long N;
+  std::stringstream ssout( argv[2] );
+  ssout >>N;
 
-    CriterionType::Pointer criterion = CriterionType::New();
-    criterion->SetTopologicalChange( true );
-    criterion->SetNumberOfElements( N );
+  CriterionType::Pointer criterion = CriterionType::New();
+  criterion->SetTopologicalChange( true );
+  criterion->SetNumberOfElements( N );
 
-    DecimationType::Pointer decimate = DecimationType::New();
-    decimate->SetInput( mesh );
-    decimate->SetCriterion( criterion );
-    decimate->Update();
+  DecimationType::Pointer decimate = DecimationType::New();
+  decimate->SetInput( mesh );
+  decimate->SetCriterion( criterion );
+  decimate->Update();
 
-    // ** WRITE OUTPUT **
-    WriterType::Pointer writer = WriterType::New( );
-    writer->SetInput( decimate->GetOutput( ) );
-    writer->SetFileName( argv[3] );
-    writer->Update( );
+  // ** WRITE OUTPUT **
+  WriterType::Pointer writer = WriterType::New( );
+  writer->SetInput( decimate->GetOutput( ) );
+  writer->SetFileName( argv[3] );
+  writer->Update( );
 
-    return EXIT_SUCCESS;
-  
+  return EXIT_SUCCESS;
+
 }
