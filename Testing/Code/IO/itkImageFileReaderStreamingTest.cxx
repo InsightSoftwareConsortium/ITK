e6bcb1a5bf4470f459d6d5d099368c164825931e
Luis Ibanez <luis.ibanez@kitware.com>
2009-02-11 17:30:23 -0500

    STYLE: Fixing coding style: missing brackets in single line ifs, inserting spaces inside parenthesis.

--- Testing/Code/IO/itkImageFileReaderStreamingTest.cxx
+++ Testing/Code/IO/itkImageFileReaderStreamingTest.cxx
@@ -9,8 +9,8 @@
   Copyright (c) Insight Software Consortium. All rights reserved.
   See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
 
-     This software is distributed WITHOUT ANY WARRANTY; without even 
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
      PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
@@ -30,54 +30,58 @@
 int itkImageFileReaderStreamingTest(int argc, char* argv[])
 {
   if( argc < 2 )
-    { 
+    {
     std::cerr << "Usage: " << argv[0] << " input [expected-to-stream 1|0 [ force-no-streaming 1|0] ]" << std::endl;
     return EXIT_FAILURE;
     }
-  
+
   unsigned int numberOfDataPieces = 4;
-  
+
   bool expectedToStream = true;
-  if (argc > 2) 
+  if( argc > 2 )
     {
-    if (atoi(argv[2]) != 1) 
+    if( atoi(argv[2]) != 1 )
+      {
       expectedToStream = false;
+      }
     }
 
   bool forceNoStreamingInput = false;
-  if (argc > 3) 
+  if( argc > 3 )
     {
-    if (atoi(argv[3]) == 1) 
+    if( atoi(argv[3]) == 1 )
+      {
       forceNoStreamingInput = true;
+      }
     }
-  
-  
+
+
   typedef unsigned char             PixelType;
   typedef itk::Image<PixelType,3>   ImageType;
-  
+
   typedef itk::ImageFileReader<ImageType>         ReaderType;
   ReaderType::Pointer reader = ReaderType::New();
   reader->SetFileName( argv[1] );
   reader->SetUseStreaming( true );
-  
+
   typedef itk::PipelineMonitorImageFilter<ImageType> MonitorFilter;
   MonitorFilter::Pointer monitor = MonitorFilter::New();
   monitor->SetInput(reader->GetOutput());
-  
-  
+
+
   typedef itk::StreamingImageFilter<ImageType, ImageType> StreamingFilter;
   StreamingFilter::Pointer streamer = StreamingFilter::New();
   streamer->SetInput(monitor->GetOutput());
   streamer->SetNumberOfStreamDivisions(numberOfDataPieces);
 
-    
+
   try
     {
     if (forceNoStreamingInput)
       {
       monitor->UpdateLargestPossibleRegion();
       }
-    else 
+    else
       {
       streamer->Update();
       }
@@ -88,27 +92,31 @@ int itkImageFileReaderStreamingTest(int argc, char* argv[])
     std::cerr << err << std::endl;
     return EXIT_FAILURE;
     }
-  
+
 
   bool passed = true;
-  if (expectedToStream) 
+  if( expectedToStream )
     {
-    if (!monitor->VerifyAllInputCanStream(numberOfDataPieces))
+    if( !monitor->VerifyAllInputCanStream(numberOfDataPieces) )
+      {
       passed = false;
+      }
     }
-  else 
+  else
     {
-    if (!monitor->VerifyAllIputCanNotStream())
+    if( !monitor->VerifyAllIputCanNotStream() )
+      {
       passed = false;
+      }
     }
 
-  
-  if (!passed) 
+
+  if( !passed )
     {
     std::cout << monitor << std::endl;
     std::cout << "pipeline did not execute as expected!" << std::endl;
     return EXIT_FAILURE;
     }
-  
+
   return EXIT_SUCCESS;
 }
