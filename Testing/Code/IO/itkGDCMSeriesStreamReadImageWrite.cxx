999f0a8591b33d5a590b31c2103614f312596cd6
Luis Ibanez <luis.ibanez@kitware.com>
2009-02-11 17:31:02 -0500

    ENH: Inserting PipelineMonitor and StreamingFilter, following the model of the itkImageFileReaderStreamingTest.cxx.

--- Testing/Code/IO/itkGDCMSeriesStreamReadImageWrite.cxx
+++ Testing/Code/IO/itkGDCMSeriesStreamReadImageWrite.cxx
@@ -9,8 +9,8 @@
   Copyright (c) Insight Software Consortium. All rights reserved.
   See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
 
-     This software is distributed WITHOUT ANY WARRANTY; without even 
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
      PURPOSE.  See the above copyright notices for more information.
 
 =========================================================================*/
@@ -20,17 +20,18 @@
 
 #include "itkImageSeriesReader.h"
 #include "itkImageFileWriter.h"
-#include "itkImageSeriesWriter.h"
-#include "itkRescaleIntensityImageFilter.h"
 #include "itkGDCMImageIO.h"
 #include "itkGDCMSeriesFileNames.h"
+#include "itkPipelineMonitorImageFilter.h"
+#include "itkStreamingImageFilter.h"
 
 int main( int argc, char* argv[] )
 {
   if( argc < 3 )
     {
-    std::cerr << "Usage: " << argv[0] << 
-      " DicomDirectory  outputFile " << std::endl;
+    std::cerr << "Usage: " << argv[0];
+    std::cerr << " DicomDirectory  outputFile ";
+    std::cerr << " [expected-to-stream 1|0 [ force-no-streaming 1|0] ]" << std::endl;
     return EXIT_FAILURE;
     }
 
@@ -40,37 +41,93 @@ int main( int argc, char* argv[] )
   typedef itk::GDCMSeriesFileNames                SeriesFileNames;
 
   ImageIOType::Pointer gdcmIO = ImageIOType::New();
-  SeriesFileNames::Pointer it = SeriesFileNames::New();
+  SeriesFileNames::Pointer filenameGenerator = SeriesFileNames::New();
 
-  // Get the DICOM filenames from the directory
-  // First add a restriction *before* selecting the input directory
-  // since SetInputDirectory has a side effect of executing
-  gdcm::SerieHelper *sh = it->GetSeriesHelper( );
-  it->SetInputDirectory( argv[1] );
+  filenameGenerator->SetInputDirectory( argv[1] );
 
   ReaderType::Pointer reader = ReaderType::New();
 
-  const ReaderType::FileNamesContainer & filenames = it->GetInputFileNames();
+  const ReaderType::FileNamesContainer & filenames =
+    filenameGenerator->GetInputFileNames();
+
   unsigned int numberOfFilenames =  filenames.size();
-  std::cout << numberOfFilenames << std::endl; 
+  std::cout << numberOfFilenames << std::endl;
   for(unsigned int fni = 0; fni<numberOfFilenames; fni++)
     {
     std::cout << "filename # " << fni << " = ";
     std::cout << filenames[fni] << std::endl;
     }
-  
+
   reader->SetFileNames( filenames );
   reader->SetImageIO( gdcmIO );
 
+  unsigned int numberOfDataPieces = 4;
+
+  bool expectedToStream = true;
+  if( argc > 3 )
+    {
+    if( atoi(argv[3]) != 1 )
+      {
+      expectedToStream = false;
+      }
+    }
+
+  bool forceNoStreamingInput = false;
+  if( argc > 4 )
+    {
+    if( atoi( argv[4] ) == 1 )
+      {
+      forceNoStreamingInput = true;
+      }
+    }
+
+  typedef itk::PipelineMonitorImageFilter<ImageType> MonitorFilter;
+  MonitorFilter::Pointer monitor = MonitorFilter::New();
+  monitor->SetInput( reader->GetOutput() );
+
+  typedef itk::StreamingImageFilter<ImageType, ImageType> StreamingFilter;
+  StreamingFilter::Pointer streamer = StreamingFilter::New();
+  streamer->SetInput( monitor->GetOutput() );
+  streamer->SetNumberOfStreamDivisions( numberOfDataPieces );
+
   try
     {
-    reader->Update();
+    if( forceNoStreamingInput )
+      {
+      monitor->UpdateLargestPossibleRegion();
+      }
+    else
+      {
+      streamer->Update();
+      }
     }
-  catch (itk::ExceptionObject &excp)
+  catch( itk::ExceptionObject & err )
     {
-    std::cerr << "Exception thrown while writing the image" << std::endl;
-    std::cerr << excp << std::endl;
+    std::cerr << "ExceptionObject caught !" << std::endl;
+    std::cerr << err << std::endl;
+    return EXIT_FAILURE;
+    }
 
+  bool passed = true;
+  if( expectedToStream )
+    {
+    if( !monitor->VerifyAllInputCanStream(numberOfDataPieces) )
+      {
+      passed = false;
+      }
+    }
+  else
+    {
+    if( !monitor->VerifyAllIputCanNotStream() )
+      {
+      passed = false;
+      }
+    }
+
+  if( !passed )
+    {
+    std::cerr << monitor << std::endl;
+    std::cerr << "pipeline did not execute as expected!" << std::endl;
     return EXIT_FAILURE;
     }
 
@@ -88,10 +145,8 @@ int main( int argc, char* argv[] )
     {
     std::cerr << "Exception thrown while writing the image" << std::endl;
     std::cerr << excp << std::endl;
-
     return EXIT_FAILURE;
     }
 
   return EXIT_SUCCESS;
 }
-
