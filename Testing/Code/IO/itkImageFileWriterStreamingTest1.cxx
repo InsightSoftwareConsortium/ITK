2857cef25f20be09822813501593fa68d11800d7
Bradley Lowekamp <blowekamp@mail.nih.gov>
2009-02-18 11:13:23 -0500

    ENH: changed to PipelineMonitor::Verify methods SYTLE: was improved

--- Testing/Code/IO/itkImageFileWriterStreamingTest1.cxx
+++ Testing/Code/IO/itkImageFileWriterStreamingTest1.cxx
@@ -38,7 +38,7 @@ int itkImageFileWriterStreamingTest1(int argc, char* argv[])
   // We remove the output file
   if (argc == 3)
     {
-      itksys::SystemTools::RemoveFile(argv[2]); 
+      itksys::SystemTools::RemoveFile( argv[2] ); 
     } 
   else 
     {
@@ -51,9 +51,9 @@ int itkImageFileWriterStreamingTest1(int argc, char* argv[])
  
 
   bool forceNoStreamingInput = false;
-  if (argc > 4) 
+  if ( argc > 4 ) 
     {
-      if (atoi(argv[4]) == 1) 
+      if ( atoi( argv[4] ) == 1 ) 
           forceNoStreamingInput = true;
     }
 
@@ -72,12 +72,15 @@ int itkImageFileWriterStreamingTest1(int argc, char* argv[])
   MonitorFilter::Pointer monitor = MonitorFilter::New();
   monitor->SetInput(reader->GetOutput());
 
-  if (forceNoStreamingInput)
+  if ( forceNoStreamingInput )
+    {
     monitor->UpdateLargestPossibleRegion();
+    monitor->VerifyAllIputCanNotStream();
+    }
 
   // Setup the writer
   WriterType::Pointer writer = WriterType::New();
-  writer->SetFileName(argv[2]);
+  writer->SetFileName( argv[2] );
   writer->SetInput(monitor->GetOutput());
   writer->SetNumberOfStreamDivisions(numberOfDataPieces);
   
@@ -93,13 +96,30 @@ int itkImageFileWriterStreamingTest1(int argc, char* argv[])
       return EXIT_FAILURE;
     }
    
-  //check that the pipeline executed as expected
-  if (!forceNoStreamingInput && monitor->GetNumberOfUpdates() <= 1) {
-    std::cerr << "pipeline did not execute as expected" << std::endl;
-    std::cerr << monitor;
+
+  bool passed = true;
+  if( !forceNoStreamingInput )
+    {
+    if( !monitor->VerifyAllInputCanStream(numberOfDataPieces) )
+      {
+      passed = false;
+      }
+    }
+  else
+    {
+    if( !monitor->VerifyAllNoUpdate() )
+      {
+      passed = false;
+      }
+    }
+
+
+  if( !passed )
+    {
+    std::cout << monitor << std::endl;
+    std::cout << "pipeline did not execute as expected!" << std::endl;
     return EXIT_FAILURE;
-  }
-      
-     
+    }
+
   return EXIT_SUCCESS;
 }
