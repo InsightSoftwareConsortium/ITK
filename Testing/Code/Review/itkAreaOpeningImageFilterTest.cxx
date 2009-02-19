4ce048d613b22077644d5d1500f03fd51227eaa7
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2009-02-19 06:07:34 -0500

    ENH: Enhance code coverage of area opening/closing filters.

--- Testing/Code/Review/itkAreaOpeningImageFilterTest.cxx
+++ Testing/Code/Review/itkAreaOpeningImageFilterTest.cxx
@@ -28,9 +28,9 @@
 int itkAreaOpeningImageFilterTest(int argc, char * argv[])
 {
 
-  if( argc != 5 )
+  if( argc != 6 )
     {
-    std::cerr << "usage: " << argv[0] << " inputImage outputImage lambda conn" << std::endl;
+    std::cerr << "usage: " << argv[0] << " inputImage outputImage lambda conn use_spacing" << std::endl;
     std::cerr << "  inputImage: The input image." << std::endl;
     std::cerr << "  outputImage: The output image." << std::endl;
     exit(1);
@@ -47,9 +47,61 @@ int itkAreaOpeningImageFilterTest(int argc, char * argv[])
 
   typedef itk::AreaOpeningImageFilter< IType, IType > FilterType;
   FilterType::Pointer filter = FilterType::New();
+  
+  // 
+  // Tests for raising code coverage
+  //
+  filter->FullyConnectedOn();
+  if( !filter->GetFullyConnected() )
+    {
+    std::cerr << "Set/GetFullyConnected() error" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  filter->FullyConnectedOff();
+  if( filter->GetFullyConnected() )
+    {
+    std::cerr << "Set/GetFullyConnected() error" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  filter->UseImageSpacingOn();
+  if( !filter->GetUseImageSpacing() )
+    {
+    std::cerr << "Set/GetUseImageSpacing() error" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  filter->UseImageSpacingOff();
+  if( filter->GetUseImageSpacing() )
+    {
+    std::cerr << "Set/GetUseImageSpacing() error" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+
   filter->SetInput( reader->GetOutput() );
+  
   filter->SetLambda( atoi(argv[3]) );
+  if ( filter->GetLambda( ) != atoi(argv[3]) )
+    {
+    std::cerr << "Set/Get Lambda problem." << std::endl;
+    return EXIT_FAILURE;
+    }
+  
   filter->SetFullyConnected( atoi(argv[4]) );
+  if ( filter->GetFullyConnected( ) != (bool)atoi(argv[4]) )
+    {
+    std::cerr << "Set/Get FullyConnected problem." << std::endl;
+    return EXIT_FAILURE;
+    }
+  
+  filter->SetUseImageSpacing( atoi(argv[5]) );
+  if ( filter->GetUseImageSpacing( ) != (bool)atoi(argv[5]) )
+    {
+    std::cerr << "Set/Get UseImageSpacing problem." << std::endl;
+    return EXIT_FAILURE;
+    }
 
   itk::SimpleFilterWatcher watcher(filter, "filter");
 
