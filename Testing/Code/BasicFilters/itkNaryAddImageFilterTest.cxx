f40b2e062ca8159af969d98cd847cbdb7d1ee8c9
Tom Vercauteren <tom.vercauteren@gmail.com>
2009-03-04 14:24:13 -0500

    ENH: Enhanced code coverage by checking the use of the filter inplace

--- Testing/Code/BasicFilters/itkNaryAddImageFilterTest.cxx
+++ Testing/Code/BasicFilters/itkNaryAddImageFilterTest.cxx
@@ -92,7 +92,7 @@ void InitializeImage( myInputImageType * image, double value   )
 
 
 // Function for image printing
-void PrintImage( myInputImageType * image, const char *)
+void PrintImage( myInputImageType * image, const char * text)
 {
   // Create an iterator for going through the image
   myImageIteratorType it( image, 
@@ -100,7 +100,7 @@ void PrintImage( myInputImageType * image, const char *)
   
   it.GoToBegin();
   //  Print the content of the image
-  //std::cout << text << std::endl;
+  std::cout << text << std::endl;
   while( !it.IsAtEnd() ) 
   {
     std::cout << it.Get() << std::endl;
@@ -120,12 +120,15 @@ int itkNaryAddImageFilterTest(int, char* [] )
   // Create two images
   myInputImageType::Pointer inputImageA  = myInputImageType::New();
   myInputImageType::Pointer inputImageB  = myInputImageType::New();
+  myInputImageType::Pointer inputImageC  = myInputImageType::New();
   
   InitializeImage( inputImageA, 12 );
   InitializeImage( inputImageB, 17 );
-
+  InitializeImage( inputImageC, -4 );
+  
   PrintImage( inputImageA, "Input image A" ); 
   PrintImage( inputImageB, "Input image B" ); 
+  PrintImage( inputImageC, "Input image C" ); 
 
   // Create an ADD Filter                                
   myFilterType::Pointer filter = myFilterType::New();
@@ -134,6 +137,7 @@ int itkNaryAddImageFilterTest(int, char* [] )
   // Connect the input images
   filter->SetInput( 0, inputImageA ); 
   filter->SetInput( 1, inputImageB );
+  filter->SetInput( 2, inputImageC );
 
   // Get the Smart Pointer to the Filter Output 
   myOutputImageType::Pointer outputImage = filter->GetOutput();
@@ -143,8 +147,62 @@ int itkNaryAddImageFilterTest(int, char* [] )
   filter->Update();
   filter->SetFunctor(filter->GetFunctor());
 
-  PrintImage( outputImage, "Resulting image" ); 
+  PrintImage( outputImage, "Resulting image" );
 
+  
+  // Test the validity of the output
+  myRegionType region = inputImageA->GetRequestedRegion();
+  
+  typedef itk::ImageRegionConstIterator<myInputImageType>  IteratorIn;
+  typedef itk::ImageRegionConstIterator<myOutputImageType> IteratorOut;
+  
+  IteratorIn  iterA( inputImageA, region );
+  IteratorIn  iterB( inputImageB, region );
+  IteratorIn  iterC( inputImageC, region );
+  IteratorOut iterO( outputImage, region );
+
+  const double epsilon = 1e-9;
+  unsigned int failures = 0;
+  while ( !iterO.IsAtEnd() )
+    {
+    if ( vcl_abs( iterO.Get() - (iterA.Get() + iterB.Get() + iterC.Get()) ) > epsilon ) ++failures;
+    ++iterA;
+    ++iterB;
+    ++iterC;
+    ++iterO;
+    }
+
+  if ( failures > 0 )
+    {
+    std::cout << "Got " << failures << " different pixels." << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  
+  // Execute the filter in place
+  filter->InPlaceOn();
+  filter->Update();
+
+  PrintImage( outputImage, "Resulting image" );
+
+  
+  // Test the validity of the output
+  IteratorOut iterO2( outputImage, region );
+  failures = 0;
+  while ( !iterO2.IsAtEnd() )
+    {
+    // Here we cannot test using the input iterators anymore since
+    // inputImageA should have been overwritten
+    if ( vcl_abs( iterO2.Get() - (12+17-4) ) > epsilon ) ++failures;
+    ++iterO2;
+    }
+
+  if ( failures > 0 )
+    {
+    std::cout << "Got " << failures << " different pixels." << std::endl;
+    return EXIT_FAILURE;
+    }
+  
   // Testing with vector Images
   typedef itk::Image< itk::Vector<double,2>, 2> VectorImageType;
   typedef itk::NaryAddImageFilter< VectorImageType, VectorImageType > NaryAdderType;
