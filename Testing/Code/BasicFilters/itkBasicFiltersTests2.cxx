dc435956be9c1041f5c7f032a69a6823c2d6a7d0
Bradley Lowekamp <blowekamp@mail.nih.gov>
2009-03-27 10:53:06 -0400

    ENH: added a test for streaming in itkShrinkImageFilter

--- Testing/Code/BasicFilters/itkBasicFiltersTests2.cxx
+++ Testing/Code/BasicFilters/itkBasicFiltersTests2.cxx
@@ -47,6 +47,7 @@ void RegisterTests()
   REGISTER_TEST(itkShiftScaleInPlaceImageFilterTest );
   REGISTER_TEST(itkShrinkImageTest );
   REGISTER_TEST(itkShrinkImagePreserveObjectPhysicalLocations);
+  REGISTER_TEST(itkShrinkImageStreamingTest );
   REGISTER_TEST(itkSigmoidImageFilterTest );
   REGISTER_TEST(itkSignedDanielssonDistanceMapImageFilterTest );
   REGISTER_TEST(itkSimilarityIndexImageFilterTest );
