9e9e4f298bb6c4f361b9c870f5bea6ced917c991
Wes Turner <wes.turner@kitware.com>
2009-05-13 11:33:10 -0400

    ENH: Adding classes from IJ Article 176, See conversation at: http://www.itk.org/mailman/private/insight-developers/2009-May/012445.html

--- Testing/Code/Review/itkReviewTests2.cxx
+++ Testing/Code/Review/itkReviewTests2.cxx
@@ -27,6 +27,8 @@ void RegisterTests()
 
   REGISTER_TEST(itkDirectFourierReconstructionImageToImageFilterTest);
   REGISTER_TEST(itkHeavisideStepFunctionTest1);
+  REGISTER_TEST(itkLabelObjectLineComparatorTest);
+  REGISTER_TEST(itkLabelObjectLineTest);
   REGISTER_TEST(itkMultiphaseFiniteDifferenceImageFilterTest);
   REGISTER_TEST(itkMultiphaseDenseFiniteDifferenceImageFilterTest);
   REGISTER_TEST(itkMultiphaseSparseFiniteDifferenceImageFilterTest);
