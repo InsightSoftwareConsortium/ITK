082752a31f3bf8d74c9a73546bb9387e4706df4e
Bradley Lowekamp <blowekamp@mail.nih.gov>
2009-05-15 11:25:04 -0400

    ENH: added ITK copyright header

--- Testing/Code/IO/itkPipelineMonitorImageFilter.h
+++ Testing/Code/IO/itkPipelineMonitorImageFilter.h
@@ -1,3 +1,21 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkPipelineMonitorImageFilter.h
+  Language:  C++
+  Date:      $Date$
+
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
 #include "itkInPlaceImageFilter.h"
 
 namespace itk {
