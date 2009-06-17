c7c2aebe4038914b48218701fb09f2e87f5ef0db
Luis Ibanez <luis.ibanez@kitware.com>
2009-06-17 08:17:54 -0400

    ENH: Adding reference to the Insight Journal paper.

--- Code/Review/itkAreaOpeningImageFilter.h
+++ Code/Review/itkAreaOpeningImageFilter.h
@@ -45,6 +45,12 @@ namespace itk
  * included, but operation appears incorrect. Check the ifdefs if you
  * are interested.
  *
+ * This code was contributed in the Insight Journal paper
+ *
+ * http://hdl.handle.net/1926/1316
+ * http://www.insight-journal.org/browse/publication/203
+ *
+ *
  * \author Richard Beare. Department of Medicine, Monash University, Melbourne, Australia.
  *
  */
