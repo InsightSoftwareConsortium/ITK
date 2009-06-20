cacba9888049a8cf6478da6a939aa14166f22b48
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2009-06-20 04:06:34 -0400

    ENH: There's no need to know the input type of the output filter, so use ImageSource as output filter type.

--- Code/Review/itkSliceBySliceImageFilter.h
+++ Code/Review/itkSliceBySliceImageFilter.h
@@ -74,7 +74,7 @@ template<class TInputImage,
   class TInputFilter=ImageToImageFilter< 
     Image< ITK_TYPENAME TInputImage::PixelType,  ::itk::GetImageDimension<TInputImage >::ImageDimension - 1 >,
     Image< ITK_TYPENAME TOutputImage::PixelType, ::itk::GetImageDimension<TOutputImage>::ImageDimension - 1 > >,
-  class TOutputFilter=TInputFilter,
+  class TOutputFilter=ITK_TYPENAME TInputFilter::Superclass,
   class TInternalInputImage=ITK_TYPENAME TInputFilter::InputImageType,
   class TInternalOutputImage=ITK_TYPENAME TOutputFilter::OutputImageType >
 class ITK_EXPORT SliceBySliceImageFilter : 
