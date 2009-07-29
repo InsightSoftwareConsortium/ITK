7df68363c680a8d141b8d153629f707476036c5c
Sophie Chen <sophie.chen@kitware.com>
2009-07-29 17:44:31 -0400

    ENH: Adding itkBinaryImageToShapeLabelMapFilter contributed      by Gaetan Lehmann from the Insight Journal paper:
           http://hdl.handle.net/1926/584  or
           http://www.insight-journal.org/browse/publication/176
    
    Including baseline image for this filter.

---
+++ Testing/Code/Review/itkBinaryImageToShapeLabelMapFilterTest1.cxx
@@ -0,0 +1,73 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkBinaryImageToShapeLabelMapFilterTest1.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) Insight Software Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkSimpleFilterWatcher.h"
+
+#include "itkLabelObject.h"
+#include "itkLabelMap.h"
+#include "itkBinaryImageToShapeLabelMapFilter.h"
+#include "itkLabelMapToLabelImageFilter.h"
+
+#include "itkTestingMacros.h"
+
+int itkBinaryImageToShapeLabelMapFilterTest1(int argc, char * argv[])
+{
+
+  if( argc != 6 )
+    {
+    std::cerr << "usage: " << argv[0];
+    std::cerr << " inputBinaryImage outputShapeLabelMap";
+    std::cerr << " fullyConnected(0/1) foregroundValue backgroundValue";
+    return EXIT_FAILURE;
+    }
+
+  const unsigned int dim = 2;
+  
+  typedef itk::Image< unsigned char, dim > ImageType;
+
+  typedef itk::ShapeLabelObject< unsigned char, dim > LabelObjectType;
+  typedef itk::LabelMap< LabelObjectType >            LabelMapType;
+  
+  typedef itk::ImageFileReader< ImageType > ReaderType;
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( argv[1] );
+  
+  typedef itk::BinaryImageToShapeLabelMapFilter< ImageType, LabelMapType> I2LType;
+  I2LType::Pointer i2l = I2LType::New();
+  i2l->SetInput( reader->GetOutput() );
+  i2l->SetFullyConnected( true );
+
+  i2l->SetFullyConnected( atoi(argv[3]) );
+  i2l->SetInputForegroundValue( atoi(argv[4]) );
+  i2l->SetOutputBackgroundValue( atoi(argv[5]) );
+
+  typedef itk::LabelMapToLabelImageFilter< LabelMapType, ImageType> L2IType;
+  L2IType::Pointer l2i = L2IType::New();
+  l2i->SetInput( i2l->GetOutput() );
+
+  typedef itk::ImageFileWriter< ImageType > WriterType;
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetInput( l2i->GetOutput() );
+  writer->SetFileName( argv[2] );
+  writer->UseCompressionOn();
+
+  TRY_EXPECT_NO_EXCEPTION( writer->Update() );
+
+  return EXIT_SUCCESS;
+}
