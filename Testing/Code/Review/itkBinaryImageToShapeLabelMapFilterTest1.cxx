1a2e0509a72094508b9f4061f7944ed0dffc0bfb
Sophie Chen <sophie.chen@kitware.com>
2009-08-03 15:31:11 -0400

    ENH: Added additional code coverage for Set/Get and Boolean macros. itkTypeMacro test coverage is incomplete.

--- Testing/Code/Review/itkBinaryImageToShapeLabelMapFilterTest1.cxx
+++ Testing/Code/Review/itkBinaryImageToShapeLabelMapFilterTest1.cxx
@@ -44,19 +44,63 @@ int itkBinaryImageToShapeLabelMapFilterTest1(int argc, char * argv[])
 
   typedef itk::ShapeLabelObject< unsigned char, dim > LabelObjectType;
   typedef itk::LabelMap< LabelObjectType >            LabelMapType;
-  
+ 
+  //reading image to file 
   typedef itk::ImageFileReader< ImageType > ReaderType;
   ReaderType::Pointer reader = ReaderType::New();
   reader->SetFileName( argv[1] );
-  
+ 
+  //converting binary image to shape label map 
   typedef itk::BinaryImageToShapeLabelMapFilter< ImageType, LabelMapType> I2LType;
   I2LType::Pointer i2l = I2LType::New();
   i2l->SetInput( reader->GetOutput() );
-  i2l->SetFullyConnected( true );
 
-  i2l->SetFullyConnected( atoi(argv[3]) );
-  i2l->SetInputForegroundValue( atoi(argv[4]) );
-  i2l->SetOutputBackgroundValue( atoi(argv[5]) );
+  //testing get/set FullyConnected macro
+  bool fullyConnected =  atoi(argv[3]);
+  i2l->SetFullyConnected( fullyConnected );
+  TEST_SET_GET_VALUE( fullyConnected, i2l->GetFullyConnected() );
+
+  //testing boolean FullyConnected macro
+  i2l->FullyConnectedOff();
+  TEST_SET_GET_VALUE( false, i2l->GetFullyConnected() );
+
+  i2l->FullyConnectedOn();
+  TEST_SET_GET_VALUE( true, i2l->GetFullyConnected() );
+
+  //testing get/set InputForegroundValue macro
+  int inputForegroundValue = ( atoi(argv[4]) );
+  i2l->SetInputForegroundValue( inputForegroundValue );
+  TEST_SET_GET_VALUE( inputForegroundValue, i2l->GetInputForegroundValue() );
+
+  //testing get/set OutputBackgroundValue macro
+  int outputBackgroundValue = ( atoi(argv[5]) );
+  i2l->SetOutputBackgroundValue( outputBackgroundValue );
+  TEST_SET_GET_VALUE( outputBackgroundValue, i2l->GetOutputBackgroundValue() );
+
+  //testing get/set ComputeFeretDiameter macro
+  bool computeFeretDiameter =  atoi(argv[6]);
+  i2l->SetComputeFeretDiameter( computeFeretDiameter );
+  TEST_SET_GET_VALUE( computeFeretDiameter, i2l->GetComputeFeretDiameter() );
+
+  //testing boolean ComputeFeretDiameter macro
+  i2l->ComputeFeretDiameterOff();
+  TEST_SET_GET_VALUE( false, i2l->GetComputeFeretDiameter() );
+
+  i2l->ComputeFeretDiameterOn();
+  TEST_SET_GET_VALUE( true, i2l->GetComputeFeretDiameter() );
+ 
+
+  //testing get/set ComputePerimeter macro
+  bool computePerimeter =  atoi(argv[6]);
+  i2l->SetComputePerimeter( computePerimeter );
+  TEST_SET_GET_VALUE( computePerimeter, i2l->GetComputePerimeter() );
+
+  //testing boolean ComputePerimeter macro
+  i2l->ComputePerimeterOff();
+  TEST_SET_GET_VALUE( false, i2l->GetComputePerimeter() );
+
+  i2l->ComputePerimeterOn();
+  TEST_SET_GET_VALUE( true, i2l->GetComputePerimeter() );
 
   typedef itk::LabelMapToLabelImageFilter< LabelMapType, ImageType> L2IType;
   L2IType::Pointer l2i = L2IType::New();
