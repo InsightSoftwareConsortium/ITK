9d965699143916f803f589b1df49d2181bf8b41c
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2009-08-07 08:18:38 -0400

    ENH: Add new class LabelStatisticsKeepNObjectsImageFilter from http://hdl.handle.net/1926/584.

--- Testing/Code/Review/itkReviewTests3.cxx
+++ Testing/Code/Review/itkReviewTests3.cxx
@@ -57,4 +57,5 @@ void RegisterTests()
   REGISTER_TEST(itkLabelShapeKeepNObjectsImageFilterTest1);
   REGISTER_TEST(itkLabelShapeOpeningImageFilterTest1);
   REGISTER_TEST(itkBinaryStatisticsKeepNObjectsImageFilterTest1);
+  REGISTER_TEST(itkLabelStatisticsKeepNObjectsImageFilterTest1);
 }
