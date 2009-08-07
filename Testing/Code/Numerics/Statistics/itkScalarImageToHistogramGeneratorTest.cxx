6a5e56f04dc52934728d8450447ee7c2c6f1396b
Hans Johnson <hans-johnson@uiowa.edu>
2009-08-06 23:23:57 -0400

    BUG:  Identified bug that causes Otsu thresholds to fail on images with very large number of background voxels that all fit in the first bin of the histogram.

--- Testing/Code/Numerics/Statistics/itkScalarImageToHistogramGeneratorTest.cxx
+++ Testing/Code/Numerics/Statistics/itkScalarImageToHistogramGeneratorTest.cxx
@@ -27,79 +27,180 @@ int itkScalarImageToHistogramGeneratorTest( int , char * [] )
 {
 
 
-  typedef unsigned char                         PixelType;
-  const unsigned int                            Dimension = 2;
-
-  typedef itk::Image< PixelType, Dimension >    ImageType;
-
-
-  ImageType::Pointer image = ImageType::New();
-
-  ImageType::RegionType region;
-  ImageType::SizeType   size;
-  ImageType::IndexType  start;
-
-  size[0] = 255;
-  size[1] = 255;
-  
-  start[0] = 0;
-  start[1] = 0;
-
-  region.SetIndex( start );
-  region.SetSize( size );
-
-  image->SetRegions( region );
-  image->Allocate();
-  
-  
-  //  Now fill up the image will all the combinations of  
-  //  values from 0-255.
-  itk::ImageRegionIteratorWithIndex< ImageType >  it( image, region );
-  it.GoToBegin();
-
-  while( !it.IsAtEnd() )
     {
-    it.Set( it.GetIndex()[0] );
-    ++it;
+    typedef unsigned long                         PixelType;
+    const unsigned int imageSize=256;
+    const unsigned int                            Dimension = 2;
+
+    typedef itk::Image< PixelType, Dimension >    ImageType;
+
+
+    ImageType::Pointer image = ImageType::New();
+
+    ImageType::RegionType region;
+    ImageType::SizeType   size;
+    ImageType::IndexType  start;
+
+    size[0] = imageSize;
+    size[1] = imageSize;
+
+    start[0] = 0;
+    start[1] = 0;
+
+    region.SetIndex( start );
+    region.SetSize( size );
+
+    image->SetRegions( region );
+    image->Allocate();
+
+    //  Now fill up the image will all the combinations of  
+    //  values from 0-imageSize.
+    itk::ImageRegionIteratorWithIndex< ImageType >  it( image, region );
+    it.GoToBegin();
+
+    while( !it.IsAtEnd() )
+      {
+      it.Set( it.GetIndex()[0] );
+      ++it;
+      }
+
+
+
+    typedef itk::Statistics::ScalarImageToHistogramGenerator< 
+      ImageType 
+      >   HistogramGeneratorType;
+
+
+    HistogramGeneratorType::Pointer histogramGenerator = HistogramGeneratorType::New();
+
+    histogramGenerator->SetInput(  image  );
+
+    histogramGenerator->SetNumberOfBins( imageSize );
+    histogramGenerator->SetMarginalScale( 10.0 );
+    histogramGenerator->Compute();
+
+    typedef HistogramGeneratorType::HistogramType  HistogramType;
+
+    const HistogramType * histogram = histogramGenerator->GetOutput();
+
+    const unsigned int histogramSize = histogram->Size();
+
+    std::cout << "Histogram size " << histogramSize << std::endl;
+
+    const unsigned int channel = 0;  // the only channel
+
+    for( unsigned int bin=0; bin < histogramSize; bin++ )
+      {
+      if( histogram->GetFrequency( bin, channel ) != imageSize ) 
+        {
+        std::cerr << "Error in bin= " << bin << " channel = " << channel << std::endl;
+        std::cerr << "Frequency was= " <<  histogram->GetFrequency( bin, channel ) << " Instead of the expected " << imageSize << std::endl;
+        return EXIT_FAILURE;
+        }
+      }
     }
-
-
-  typedef itk::Statistics::ScalarImageToHistogramGenerator< 
-                                                    ImageType 
-                                                          >   HistogramGeneratorType;
-
-
-  HistogramGeneratorType::Pointer histogramGenerator = HistogramGeneratorType::New();
-
-  histogramGenerator->SetInput(  image  );
-
-  histogramGenerator->SetNumberOfBins( 255 );
-  histogramGenerator->SetMarginalScale( 10.0 );
-  histogramGenerator->Compute();
-
-  typedef HistogramGeneratorType::HistogramType  HistogramType;
-
-  const HistogramType * histogram = histogramGenerator->GetOutput();
-
-  const unsigned int histogramSize = histogram->Size();
-
-  std::cout << "Histogram size " << histogramSize << std::endl;
-
-  unsigned int channel = 0;  // the only channel
-
-  for( unsigned int bin=0; bin < histogramSize; bin++ )
     {
-    if( histogram->GetFrequency( bin, channel ) != 255 ) 
-       {
-       std::cerr << "Error in bin= " << bin << " channel = " << channel << std::endl;
-       std::cerr << "Frequency was= " <<  histogram->GetFrequency( bin, channel ) << " Instead of the expected " << 255 << std::endl;
-       return EXIT_FAILURE;
-       }
-    }
+    const unsigned int NumberOfBins=4;
+    typedef unsigned long                         PixelType;
+    // NOTE :  it is not uncommon to have a 3D image with more than 4096 * 4096 voxels in the background (i.e. 0'th histogram bin), and this causes an overflow event
+    const unsigned int imageSize=4096+NumberOfBins;  // Note 4096^2 = 16777216, which is greater than the number that can be accurately incremented by a floating point number.
+    //const unsigned int imageSize=406+NumberOfBins;  // Note 4096^2 = 16777216, which is greater than the number that can be accurately incremented by a floating point number.
+    const unsigned int                            Dimension = 2;
 
+    typedef itk::Image< PixelType, Dimension >    ImageType;
+
+
+    ImageType::Pointer image = ImageType::New();
+
+    ImageType::RegionType region;
+    ImageType::SizeType   size;
+    ImageType::IndexType  start;
+
+    size[0] = imageSize;
+    size[1] = imageSize;
+
+    start[0] = 0;
+    start[1] = 0;
+
+    region.SetIndex( start );
+    region.SetSize( size );
+
+    image->SetRegions( region );
+    image->Allocate();
+
+    //  Now fill up the image will all the combinations of  
+    //  values from 0-imageSize.
+    itk::ImageRegionIteratorWithIndex< ImageType >  it( image, region );
+
+    it.GoToBegin();
+    for(unsigned int temp=0;temp<NumberOfBins;temp++)
+      {
+      it.Set((temp+1)*1000); //Make it have a range
+      ++it;
+      }
+
+    while( !it.IsAtEnd() )
+      {
+      it.Set( 0 );
+      ++it;
+      }
+
+
+
+    typedef itk::Statistics::ScalarImageToHistogramGenerator< 
+      ImageType 
+      >   HistogramGeneratorType;
+
+
+    HistogramGeneratorType::Pointer histogramGenerator = HistogramGeneratorType::New();
+
+    histogramGenerator->SetInput(  image  );
+
+    histogramGenerator->SetNumberOfBins( 4 );
+    histogramGenerator->SetMarginalScale( 10.0 );
+    histogramGenerator->Compute();
+
+    typedef HistogramGeneratorType::HistogramType  HistogramType;
+
+    const HistogramType * histogram = histogramGenerator->GetOutput();
+
+    const unsigned int histogramSize = histogram->Size();
+
+    std::cout << "Histogram size " << histogramSize << std::endl;
+
+    const unsigned int channel = 0;  // the only channel
+
+    int status=EXIT_SUCCESS;
+      {
+      const unsigned int bin=0;
+      if( histogram->GetFrequency( bin, channel ) == 16777216 ) {
+        std::cerr << "Error in bin= " << bin << " channel = " << channel << std::endl;
+        std::cerr << "Frequency was= " <<  (unsigned int)histogram->GetFrequency( bin, channel ) 
+          << " which is the maximum integer value representable by a 32bit floating point number " << std::endl;
+        status=EXIT_FAILURE;
+      }
+
+      if( histogram->GetFrequency( bin, channel ) != imageSize*imageSize - NumberOfBins +1)
+        {
+        std::cerr << "Error in bin= " << bin << " channel = " << channel << std::endl;
+        std::cerr << "Frequency was= " <<  (unsigned int)histogram->GetFrequency( bin, channel ) << " Instead of the expected " << imageSize*imageSize - NumberOfBins +1 << std::endl;
+        status=EXIT_FAILURE;
+        }
+      }
+    for( unsigned int bin=1; bin < histogramSize; bin++ )
+      {
+      if( histogram->GetFrequency( bin, channel ) != 1 )
+        {
+        std::cerr << "Error in bin= " << bin << " channel = " << channel << std::endl;
+        std::cerr << "Frequency was= " <<  (unsigned int)histogram->GetFrequency( bin, channel ) << " Instead of the expected " << 1 << std::endl;
+        status=EXIT_FAILURE;
+        }
+      }
+    return status;
+    }
 
   return EXIT_SUCCESS;
-  
+
 }
 
 
