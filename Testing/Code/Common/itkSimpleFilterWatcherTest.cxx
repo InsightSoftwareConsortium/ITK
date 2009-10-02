3dfb826d49deccd895a5ef96fc4172be0848bfb0
Bill Lorensen <bill.lorensen@gmail.com>
2009-10-02 09:35:53 -0400

    ENH: More informative error reporting. BUG: AddConstantToImageFilter replaced with InvertintensityImageFilter. The former is still in Review and may not be available on all builds.

--- Testing/Code/Common/itkSimpleFilterWatcherTest.cxx
+++ Testing/Code/Common/itkSimpleFilterWatcherTest.cxx
@@ -20,7 +20,7 @@
 
 #include <iostream>
 #include "itkSimpleFilterWatcher.h"
-#include "itkAddConstantToImageFilter.h"
+#include "itkInvertIntensityImageFilter.h"
 #include "itkImage.h"
 
 
@@ -29,10 +29,13 @@ int itkSimpleFilterWatcherTest (int, char*[])
   // Test out the code
   typedef itk::SimpleFilterWatcher        WatcherType;
   typedef itk::Image<char,3>              ImageType;
-  typedef itk::AddConstantToImageFilter<
-    ImageType, char, ImageType>           FilterType;
+  typedef itk::InvertIntensityImageFilter<
+    ImageType, ImageType>                 FilterType;
   FilterType::Pointer filter = FilterType::New();
   const char * comment = "comment";
+
+  // Test empty constructor.
+  WatcherType watcher0();
 
   // Test constructor that takes a ProcessObject.
   WatcherType watcher1( filter, comment );
@@ -40,15 +43,16 @@ int itkSimpleFilterWatcherTest (int, char*[])
   // Test copy constructor.
   WatcherType watcher2( watcher1 );
   if ( watcher1.GetNameOfClass() != watcher2.GetNameOfClass()
-    || watcher1.GetProcess() != watcher2.GetProcess()
-    || watcher1.GetSteps() != watcher2.GetSteps()
-    || watcher1.GetIterations() != watcher2.GetIterations()
-    || watcher1.GetQuiet() != watcher2.GetQuiet()
-    || watcher1.GetComment() != watcher2.GetComment() )
+       || watcher1.GetProcess() != watcher2.GetProcess()
+       || watcher1.GetSteps() != watcher2.GetSteps()
+       || watcher1.GetIterations() != watcher2.GetIterations()
+       || watcher1.GetQuiet() != watcher2.GetQuiet()
+       || watcher1.GetComment() != watcher2.GetComment() )
     //|| watcher1.GetTimeProbe() != watcher2.GetTimeProbe() )
-  {
+    {
+    std::cout << "Copy constructor failed." << std::endl;    
     return EXIT_FAILURE;
-  }
+    }
 
   // Test default constructor.
   WatcherType watcher3;
@@ -56,47 +60,54 @@ int itkSimpleFilterWatcherTest (int, char*[])
   // Test assignment operator.
   watcher3 = watcher2;
   if ( watcher3.GetNameOfClass() != watcher2.GetNameOfClass()
-    || watcher3.GetProcess() != watcher2.GetProcess()
-    || watcher3.GetSteps() != watcher2.GetSteps()
-    || watcher3.GetIterations() != watcher2.GetIterations()
-    || watcher3.GetQuiet() != watcher2.GetQuiet()
-    || watcher3.GetComment() != watcher2.GetComment() )
+       || watcher3.GetProcess() != watcher2.GetProcess()
+       || watcher3.GetSteps() != watcher2.GetSteps()
+       || watcher3.GetIterations() != watcher2.GetIterations()
+       || watcher3.GetQuiet() != watcher2.GetQuiet()
+       || watcher3.GetComment() != watcher2.GetComment() )
     //|| watcher3.GetTimeProbe() != watcher2.GetTimeProbe() )
-  {
+    {
+    std::cout << "Operator= failed." << std::endl;
     return EXIT_FAILURE;
-  }
+    }
 
   // Test GetNameOfClass().
   std::string name = watcher3.GetNameOfClass();
-  if ( name != "AddConstantToImageFilter" )
-  {
+  if ( name != filter->GetNameOfClass() )
+    {
+    std::cout << "GetNameOfClass failed. Expected: " 
+              << filter->GetNameOfClass() 
+              << " but got: " 
+              << watcher3.GetNameOfClass() << std::endl;
     return EXIT_FAILURE;
-  }
+    }
 
   // Test Quiet functions.
   watcher3.QuietOff();
   watcher3.QuietOn();
   watcher3.SetQuiet( false );
   if ( watcher3.GetQuiet() != false )
-  {
+    {
+    std::cout << "GetQuiet() failed." << std::endl;
     return EXIT_FAILURE;
-  }
+    }
 
   // Test comment.
   if ( watcher3.GetComment() != comment )
-  {
+    {
+    std::cout << "GetComment() failed." << std::endl;
     return EXIT_FAILURE;
-  }
+    }
 
   // Check Pointer
   if ( watcher3.GetProcess() != filter )
-  {
+    {
+    std::cout << "GetProcess() failed." << std::endl;
     return EXIT_FAILURE;
-  }
+    }
 
   // Return success.
   std::cout << "SimpleFilterWatcher test PASSED ! " << std::endl;
   return EXIT_SUCCESS;
 
 }
-
