7bd5e5e04a3c5f5e65138a4da53eed6113dc8b01
Bradley Lowekamp <blowekamp@mail.nih.gov>
2009-11-18 07:42:03 -0500

    BUG: Sun Studio compiler needs to include stdio.h before fenv when cstdio is included

--- Code/Common/itkMathDetail.h
+++ Code/Common/itkMathDetail.h
@@ -25,6 +25,9 @@
 #include "itkNumericTraits.h"
 
 #ifdef HAVE_FENV_H
+// The Sun Studio CC compiler seems to have a bug where if cstdio is
+// included stdio.h must also be included before fenv.h
+#include <stdio.h>
 #include <fenv.h>
 #endif
 
