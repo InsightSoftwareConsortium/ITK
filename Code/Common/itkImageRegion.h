a872710c63ab223e78f45de8d3e4bc435172867d
Luis Ibanez <luis.ibanez@kitware.com>
2009-12-18 14:56:48 -0500

    STYLE: Documenting the behavior of the IsInside() method when the argument        region has zero size in any of its dimensions.

--- Code/Common/itkImageRegion.h
+++ Code/Common/itkImageRegion.h
@@ -224,7 +224,10 @@ public:
     return true;
     }
 
-  /** Test if a region (the argument) is completely inside of this region */
+  /** Test if a region (the argument) is completely inside of this region. If
+   * the region that is passed as argument to this method, has a size of value
+   * zero, then it will not be considered to be inside of the current region,
+   * even its starting index is inside. */
   bool
   IsInside(const Self &region) const
     {
