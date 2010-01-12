94f117f490cc78911f0d7874380fe7584d816a0a
Michel Audette <michel.audette@kitware.com>
2010-01-12 13:47:40 -0500

    ENH: Update to Nrrd library 2009 (Teem 1.10). No change to IO ctest.

--- Utilities/NrrdIO/privateAir.h
+++ Utilities/NrrdIO/privateAir.h
@@ -1,6 +1,27 @@
+/*
+  Teem: Tools to process and visualize scientific data and images              
+  Copyright (C) 2008, 2007, 2006, 2005  Gordon Kindlmann
+  Copyright (C) 2004, 2003, 2002, 2001, 2000, 1999, 1998  University of Utah
 
-#include "teemEndian.h"
-#include "teemQnanhibit.h"
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public License
+  (LGPL) as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+  The terms of redistributing and/or modifying this software also
+  include exceptions to the LGPL that facilitate static linking.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this library; if not, write to Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include <teemEndian.h>
+#include <teemQnanhibit.h>
 
 /* miscAir.c */
 extern double _airSanityHelper(double val);
@@ -56,5 +77,4 @@ typedef union {
   } c;
   double v;
 } _airDouble;
-
 
