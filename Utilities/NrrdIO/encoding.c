49ef53240e8f0a26c3c3749f2293ea85098bdd78
Michel Audette <michel.audette@kitware.com>
2010-01-28 14:52:39 -0500

    ENH: Update to Nrrd library 2009 (Teem 1.10). No change to IO ctest.

--- Utilities/NrrdIO/encoding.c
+++ Utilities/NrrdIO/encoding.c
@@ -1,25 +1,23 @@
 /*
-  NrrdIO: stand-alone code for basic nrrd functionality
-  Copyright (C) 2005  Gordon Kindlmann
+  Teem: Tools to process and visualize scientific data and images              
+  Copyright (C) 2008, 2007, 2006, 2005  Gordon Kindlmann
   Copyright (C) 2004, 2003, 2002, 2001, 2000, 1999, 1998  University of Utah
- 
-  This software is provided 'as-is', without any express or implied
-  warranty.  In no event will the authors be held liable for any
-  damages arising from the use of this software.
- 
-  Permission is granted to anyone to use this software for any
-  purpose, including commercial applications, and to alter it and
-  redistribute it freely, subject to the following restrictions:
- 
-  1. The origin of this software must not be misrepresented; you must
-     not claim that you wrote the original software. If you use this
-     software in a product, an acknowledgment in the product
-     documentation would be appreciated but is not required.
- 
-  2. Altered source versions must be plainly marked as such, and must
-     not be misrepresented as being the original software.
- 
-  3. This notice may not be removed or altered from any source distribution.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public License
+  (LGPL) as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+  The terms of redistributing and/or modifying this software also
+  include exceptions to the LGPL that facilitate static linking.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this library; if not, write to Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 #include "NrrdIO.h"
@@ -37,7 +35,7 @@
 **     or vice versa.
 ** -- respect nrrdStateVerboseIO with messages to stderr, if possible
 ** -- in case of error, put text error messages into biff via
-**    biffAdd(NRRD, <error char*>)
+**    biffAddf(NRRD, <error char*> ...)
 **
 ** The "unknown" encoding below is intended to serve as a template for 
 ** any new encodings being developed.
@@ -55,7 +53,7 @@ int
 _nrrdEncodingUnknown_read(FILE *file, void *data,
                           size_t elementNum, Nrrd *nrrd,
                           struct NrrdIoState_t *nio) {
-  char me[]="_nrrdEncodingUnknown_read", err[AIR_STRLEN_MED];
+  static const char me[]="_nrrdEncodingUnknown_read";
 
   /* insert code here, and remove error handling below */
   AIR_UNUSED(file);
@@ -64,8 +62,7 @@ _nrrdEncodingUnknown_read(FILE *file, void *data,
   AIR_UNUSED(nrrd);
   AIR_UNUSED(nio);
 
-  sprintf(err, "%s: ERROR!!! trying to read unknown encoding", me);
-  biffAdd(NRRD, err);
+  biffAddf(NRRD, "%s: ERROR!!! trying to read unknown encoding", me);
   return 1;
 }
 
@@ -73,7 +70,7 @@ int
 _nrrdEncodingUnknown_write(FILE *file, const void *data,
                            size_t elementNum, const Nrrd *nrrd,
                            struct NrrdIoState_t *nio) {
-  char me[]="_nrrdEncodingUnknown_write", err[AIR_STRLEN_MED];
+  static const char me[]="_nrrdEncodingUnknown_write";
 
   /* insert code here, and remove error handling below */
   AIR_UNUSED(file);
@@ -82,8 +79,7 @@ _nrrdEncodingUnknown_write(FILE *file, const void *data,
   AIR_UNUSED(nrrd);
   AIR_UNUSED(nio);
 
-  sprintf(err, "%s: ERROR!!! trying to write unknown encoding", me);
-  biffAdd(NRRD, err);
+  biffAddf(NRRD, "%s: ERROR!!! trying to write unknown encoding", me);
   return 1;
 }
 
