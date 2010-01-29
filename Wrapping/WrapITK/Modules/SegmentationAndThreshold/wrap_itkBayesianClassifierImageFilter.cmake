60e844b555b6baf2d8ce8c11189a36b7d02cada6
Hans Johnson <hans-johnson@uiowa.edu>
2010-01-29 17:08:16 -0500

    BUG:0010205:  Avoiding compile time failure caused by attempting to instantiate invalid template combinations.

--- Wrapping/WrapITK/Modules/SegmentationAndThreshold/wrap_itkBayesianClassifierImageFilter.cmake
+++ Wrapping/WrapITK/Modules/SegmentationAndThreshold/wrap_itkBayesianClassifierImageFilter.cmake
@@ -1,11 +1,14 @@
+### NOTE:  This assumes taht the 3rd and 4th template arguments are both the same floating point type.
+### NOTE:  Removed scalar types for the vector image type, because it must match the 3rd lement type.
+### NOTE:  This change was necessary to avoid itk BUG number 0010205 that demonstrates that the only valid
+###        vector type is one that has floating point voxel elements that are same as the PriorType.
+###        To preserve backwards compatibility, the types are re-used multiple times in an unconventional way.
 WRAP_CLASS("itk::BayesianClassifierImageFilter" POINTER)
   FOREACH(d ${WRAP_ITK_DIMS})
-    FOREACH(t ${WRAP_ITK_SCALAR})
       FOREACH(t2 ${WRAP_ITK_INT})
         FOREACH(t3 ${WRAP_ITK_REAL})
-          WRAP_TEMPLATE("${ITKM_VI${t}${d}}${ITKM_${t2}}${ITKM_${t3}}" "${ITKT_VI${t}${d}}, ${ITKT_${t2}}, ${ITKT_${t3}}")
+          WRAP_TEMPLATE("${ITKM_VI${t3}${d}}${ITKM_${t2}}${ITKM_${t3}}" "${ITKT_VI${t3}${d}}, ${ITKT_${t2}}, ${ITKT_${t3}}, ${ITKT_${t3}}")
         ENDFOREACH(t3)
       ENDFOREACH(t2)
-    ENDFOREACH(t)
   ENDFOREACH(d)
 END_WRAP_CLASS()
