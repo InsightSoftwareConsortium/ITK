17269cbe9e620c308bf614a13f6acbbb4cc89e48
Bradley Lowekamp <blowekamp@mail.nih.gov>
2010-02-24 10:05:56 -0500

    DOC: enabled comments to be parsed by doxygen, and other doxygen improvements

--- Code/Common/itkMath.h
+++ Code/Common/itkMath.h
@@ -35,42 +35,42 @@ namespace Math
 // These constants originate from VXL's vnl_math.h. They have been
 // moved here to improve visibility, and to ensure that the constants
 // are available during compile time ( as opposed to static const
-// member vaiables).
+// member vaiables ).
 
-// \f[e] The base of the natural logarithm or Euler's number
+/// \brief \f[e] The base of the natural logarithm or Euler's number
 static const double e                = 2.7182818284590452354;
-// \f[ \log_2 e \f]
+/// \brief  \f[ \log_2 e \f]
 static const double log2e            = 1.4426950408889634074;
-// \f[ \log_10 e \f]
+/// \brief \f[ \log_10 e \f]
 static const double log10e           = 0.43429448190325182765;
-// \f[ \log_e 2 \f]
+/// \brief \f[ \log_e 2 \f]
 static const double ln2              = 0.69314718055994530942;
-// \f[ \log_e 10 \f]
+/// \brief \f[ \log_e 10 \f]
 static const double ln10             = 2.30258509299404568402;
-// \f[ \pi ] 
+/// \brief \f[ \pi ] 
 static const double pi               = 3.14159265358979323846;
-// \f[ \frac{\pi}{2} \f] 
+/// \brief \f[ \frac{\pi}{2} \f] 
 static const double pi_over_2        = 1.57079632679489661923;
-// \f[ \frac{\pi}{4} \f] 
+/// \brief \f[ \frac{\pi}{4} \f] 
 static const double pi_over_4        = 0.78539816339744830962;
-// \f[ \frac{1}{\pi} \f] 
+/// \brief \f[ \frac{1}{\pi} \f] 
 static const double one_over_pi      = 0.31830988618379067154;
-// \f[ \frac{2}{\pi} \f] 
+/// \brief \f[ \frac{2}{\pi} \f] 
 static const double two_over_pi      = 0.63661977236758134308;
-// \f[ \frac{2}{\sqrt{\pi}} \f] 
+/// \brief \f[ \frac{2}{\sqrt{\pi}} \f] 
 static const double two_over_sqrtpi  = 1.12837916709551257390;
-// \f[ \frac{2}{\sqrt{2\pi}} \f] 
+/// \brief \f[ \frac{2}{\sqrt{2\pi}} \f] 
 static const double one_over_sqrt2pi = 0.39894228040143267794;
-// \f[ \sqrt{2} \f] 
+/// \brief \f[ \sqrt{2} \f] 
 static const double sqrt2            = 1.41421356237309504880;
-// \f[ \sqrt{ \frac{1}{2}} \f]
+/// \brief \f[ \sqrt{ \frac{1}{2}} \f]
 static const double sqrt1_2          = 0.70710678118654752440;
 
 
 #ifdef ITK_HAS_INT_64
-// A useful macro to generate a template floating point to integer conversion
-// templated on the return type and using either the 32 bit, the 64 bit or
-// the vanilla version
+/// A useful macro to generate a template floating point to integer conversion
+/// templated on the return type and using either the 32 bit, the 64 bit or
+/// the vanilla version
 #define itkTemplateFloatingToIntegerMacro(name)                         \
   template <typename TReturn,typename TInput>                           \
   inline TReturn name(TInput x)                                         \
@@ -105,79 +105,80 @@ static const double sqrt1_2          = 0.70710678118654752440;
    }
 #endif // end ITK_HAS_INT_64
 
-// \brief Define TReturn itk::Math::RoundHalfIntegerToEven<TReturn,TInput>(TInput x) 
-// Round towards nearest integer
-//
-// \tparam TReturn a type convertable from integer types
-// \tparam TInput must be float or double
-//
-//         halfway cases are rounded towards the nearest even integer, e.g.
-//         RoundHalfIntegerToEven( 1.5) ==  2
-//         RoundHalfIntegerToEven(-1.5) == -2
-//         RoundHalfIntegerToEven( 2.5) ==  2
-//         RoundHalfIntegerToEven( 3.5) ==  4
-//
-// The behavior of overflow is undefined due to numerous implementations.
-//
-// \warning We assume that the rounding mode is not changed from the default
-// one (or at least that it is always restored to the default one).
+/// \brief Round towards nearest integer
+///
+/// \tparam TReturn must be an interger type
+/// \tparam TInput must be float or double
+///
+///         halfway cases are rounded towards the nearest even
+///         integer, e.g.
+/// \code
+///         RoundHalfIntegerToEven( 1.5) ==  2
+///         RoundHalfIntegerToEven(-1.5) == -2
+///         RoundHalfIntegerToEven( 2.5) ==  2
+///         RoundHalfIntegerToEven( 3.5) ==  4
+/// \endcode
+///
+/// The behavior of overflow is undefined due to numerous implementations.
+///
+/// \warning We assume that the rounding mode is not changed from the default
+/// one (or at least that it is always restored to the default one).
 itkTemplateFloatingToIntegerMacro(RoundHalfIntegerToEven);
 
-// \brief Define TReturn itk::Math::RoundHalfIntegerUp<TReturn,TInput>(TInput x) 
-//  Round towards nearest integer
-//
-//         halfway cases are rounded upward, e.g.
-//         RoundHalfIntegerUp( 1.5) ==  2
-//         RoundHalfIntegerUp(-1.5) == -1
-//         RoundHalfIntegerUp( 2.5) ==  3
-//
-// The behavior of overflow is undefined due to numerous implementations.
-//
-// \warning argument absolute value must be less than INT_MAX/2
-// for RoundHalfIntegerUp to be guaranteed to work.
-// We also assume that the rounding mode is not changed from the default
-// one (or at least that it is always restored to the default one).
+/// \brief Round towards nearest integer
+///
+/// \tparam TReturn must be an interger type
+/// \tparam TInput must be float or double
+///
+///         halfway cases are rounded upward, e.g.
+/// \code
+///         RoundHalfIntegerUp( 1.5) ==  2
+///         RoundHalfIntegerUp(-1.5) == -1
+///         RoundHalfIntegerUp( 2.5) ==  3
+/// \endcode
+///
+/// The behavior of overflow is undefined due to numerous implementations.
+///
+/// \warning The argument absolute value must be less than
+/// NumbericTraits<TReturn>::max()/2 for RoundHalfIntegerUp to be
+/// guaranteed to work. 
+///
+/// \warning We also assume that the rounding mode is not changed from
+/// the default one (or at least that it is always restored to the
+/// default one). 
 itkTemplateFloatingToIntegerMacro(RoundHalfIntegerUp);
 
-// \brief Define TReturn itk::Math::Round<TReturn,TInput>(TInput x) 
-// Round towards nearest integer (This is a synonym for RoundHalfIntegerUp)
-//
-//         halfway cases are rounded upward, e.g.
-//         RoundHalfIntegerUp( 1.5) ==  2
-//         RoundHalfIntegerUp(-1.5) == -1
-//         RoundHalfIntegerUp( 2.5) ==  3
-//
-// The behavior of overflow is undefined due to numerous implementations.
-//
-// \warning argument absolute value must be less than INT_MAX/2
-// for RoundHalfIntegerUp to be guaranteed to work.
-// We also assume that the rounding mode is not changed from the default
-// one (or at least that it is always restored to the default one).
-//
-// \sa RoundHalfIntegerUp
+/// \brief Round towards nearest integer (This is a synonym for RoundHalfIntegerUp)
+///
+/// \tparam TReturn must be an interger type
+/// \tparam TInput must be float or double
+///
+/// \sa RoundHalfIntegerUp<TReturn, TInput>()
 template <typename TReturn, typename TInput>
 inline TReturn Round(TInput x) { return RoundHalfIntegerUp<TReturn,TInput>(x); }
 
-// \brief Define TReturn itk::Math::Floor<TReturn,TInput>(TInput x) 
-// Round towards minus infinity
-//
-// The behavior of overflow is undefined due to numerous implementations.
-//
-// \warning argument absolute value must be less than INT_MAX/2
-// for vnl_math_floor to be guaranteed to work.
-// We also assume that the rounding mode is not changed from the default
-// one (or at least that it is always restored to the default one).
+/// \brief Round towards minus infinity
+///
+/// The behavior of overflow is undefined due to numerous implementations.
+///
+/// \warning argument absolute value must be less than
+/// NumbericTraits<TReturn>::max()/2 for vnl_math_floor to be
+/// guaranteed to work. 
+///
+/// \warning We also assume that the rounding mode is not changed from
+/// the default one (or at least that it is always restored to the
+/// default one). 
 itkTemplateFloatingToIntegerMacro(Floor);
 
-// \brief Define TReturn itk::Math::Ceil<TReturn,TInput>(TInput x)
-// Round towards plus infinity
-//
-// The behavior of overflow is undefined due to numerous implementations.
-//
-// \warning argument absolute value must be less than INT_MAX/2
-// for vnl_math_ceil to be guaranteed to work.
-// We also assume that the rounding mode is not changed from the default
-// one (or at least that it is always restored to the default one)
+/// \brief Round towards plus infinity
+///
+/// The behavior of overflow is undefined due to numerous implementations.
+///
+/// \warning argument absolute value must be less than INT_MAX/2
+/// for vnl_math_ceil to be guaranteed to work.
+/// \warning We also assume that the rounding mode is not changed from
+/// the default one (or at least that it is always restored to the
+/// default one).
 itkTemplateFloatingToIntegerMacro(Ceil);
 
 
@@ -189,12 +190,13 @@ itkTemplateFloatingToIntegerMacro(Ceil);
 // can not handle function overloading with templated and
 // non-templated methods, ie the templated functions matches too often
 
-/**
- * These methods have been deprecated as of ITK 3.16
- * Please use the templated method
- * TReturn itk::Math::XXX<TReturn,TInput>(TInput x) instead.
- */
 
+//@{
+/// \deprecated These methods have been precated as of ITK 3.16. Please
+/// use the templated methods of the form
+/// itk::Math::XXX<TReturn,TINput(TInput x) instead.
+///
+/// \sa RoundHalfIntegerUp<TReturn, TInput>()
 inline int RoundHalfIntegerToEven(double x) { return Detail::RoundHalfIntegerToEven_32(x); }
 inline int RoundHalfIntegerToEven(float  x) { return Detail::RoundHalfIntegerToEven_32(x); }
 
@@ -209,6 +211,7 @@ inline int Floor(float  x) { return Detail::Floor_32(x); }
 
 inline int Ceil(double x) { return Detail::Ceil_32(x); }
 inline int Ceil(float  x) { return Detail::Ceil_32(x); }
+//@}
 
 #endif // end of ITK_LEGACY_REMOVE
 
