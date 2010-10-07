c26050e5a75613a4280651e2970e535c4976d3e0
Matt McCormick (thewtex) <matt@mmmccormick.com>
2010-10-07 16:51:58 -0500

    Add Utilities/SetupForDevelopment.sh script.
    Runs all the git development setup scripts in Utilities/DevelopmentSetupScripts
    along with downloading submodules.
    
    Change-Id: I4367529d41adfe7f0c8eb5556881fc1070f1aee3

---
+++ Utilities/SetupForDevelopment.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+#=============================================================================
+# Copyright (c) 2010 Insight Software Consortium. All rights reserved.
+# See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even
+# the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the above copyright notices for more information.
+#=============================================================================
+
+
+# Run this script to set up development with git.
+
+die() {
+	echo 'failure during git development setup' 1>&2
+	echo '------------------------------------' 1>&2
+	echo '' 1>&2
+	echo "$@" 1>&2
+	exit 1
+}
+
+# Make sure we are inside the repository.
+cd "$(echo "$0"|sed 's/[^/]*$//')"/..
+
+if test -d .git/.git; then
+  die "The directory '.git/.git' exists, indicating a configuration error.
+
+Please 'rm -rf' this directory."
+fi
+
+echo "Downloading submodules..."
+git submodule update --init || die "Failure downloading submodules."
+echo -e "Done.\n"
+
+echo "Configuring push urls..."
+git config remote.origin.pushurl git@itk.org:ITK.git
+
+cd Testing/Data
+git config remote.origin.pushurl git@itk.org:ITKData.git
+cd ../..
+echo -e "Done.\n"
+
+cd Utilities/DevelopmentSetupScripts
+
+echo "Setting up git hooks..."
+./SetupHooks.sh || exit 1
+echo
+
+echo "Setting up Gerrit..."
+./SetupGerrit.sh || exit 1
+echo
+
+echo "Setting up the topic stage..."
+./SetupTopicStage.sh || exit 1
+echo
+
+echo "Suggesting git tips..."
+./GitTips.sh || exit 1
+echo
