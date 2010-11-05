d5ea08ec94b0b72afa87df989f04102fed2bed8c
Alex. Gouaillard <agouaillard@gmail.com>
2010-11-05 23:01:24 +0800

    COMP: Java wrapping 2D
    Change-Id: If0b05b112df9d53e6c9984118f68ffcd8e9830fe

--- Wrapping/WrapITK/Libraries/Mesh/wrap_itkDefaultStaticMeshTraits.cmake
+++ Wrapping/WrapITK/Libraries/Mesh/wrap_itkDefaultStaticMeshTraits.cmake
@@ -3,5 +3,4 @@ WRAP_CLASS("itk::DefaultStaticMeshTraits")
     WRAP_TEMPLATE("${ITKM_D}${d}${d}${ITKM_D}${ITKM_D}" "${ITKT_D},${d},${d},${ITKT_D},${ITKT_D}")
     WRAP_TEMPLATE("${ITKM_D}${d}${d}${ITKM_D}" "${ITKT_D},${d},${d},${ITKT_D}")
   endforeach(d)
-
 END_WRAP_CLASS()
