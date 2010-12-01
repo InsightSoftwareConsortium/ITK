53415a5748e5252f277144f27cac38a4a3bef987
Brad King <brad.king@kitware.com>
2010-12-01 16:45:38 -0500

    COMP: Disable vnl test_svd_fixed since it needs vul
    This test was merged in from vxl-upstream but needs vul.  Disable it
    since we do not build vul.

--- Utilities/vxl/core/vnl/algo/tests/test_driver.cxx
+++ Utilities/vxl/core/vnl/algo/tests/test_driver.cxx
@@ -25,7 +25,7 @@ DECLARE( test_rnpoly_roots );
 DECLARE( test_sparse_matrix );
 DECLARE( test_integral );
 DECLARE( test_svd );
-DECLARE( test_svd_fixed );
+//DECLARE( test_svd_fixed );
 //DECLARE( test_symmetric_eigensystem );
 DECLARE( test_algo );
 DECLARE( test_solve_qp );
@@ -61,7 +61,7 @@ register_tests()
   REGISTER( test_rnpoly_roots );
   REGISTER( test_sparse_matrix );
   REGISTER( test_svd );
-  REGISTER( test_svd_fixed );
+  //REGISTER( test_svd_fixed );
   //REGISTER( test_symmetric_eigensystem );
   REGISTER( test_algo );
   REGISTER( test_solve_qp );
