7b16d242e9e6ff4aaf2c7192022c34d41575a7bd
Xiaoxiao Liu <xiaoxiao.liu@kitware.com>
2011-03-10 10:56:04 -0500

    ENH: Transition from monolithic to modular ITK
    Move source files from their former monolithic ITK location to their new
    location in modular ITK without modification.  This preserves enough
    information for "git blame -M" and "git log --follow" to connect
    modularized ITK files to their original location and history.  Also add
    per-module CMakeLists.txt and HeaderTest source files generated by the
    modularization script.
    
    The modular ITK build system is not yet mature.  Document the current
    status and known limitations in "TODO-Modularization.txt".

---
+++ ITK/Nonunit/Deprecated/test/itkDeprecatedHeaderTest.cxx
@@ -0,0 +1,52 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#if defined(_MSC_VER)
+#pragma warning ( disable : 4786 )
+#endif
+
+#include <iostream>
+
+#include "itkTestDriverIncludeAnalyzeIOFactory.h"
+#include "itkAnalyzeDbh.h"
+#include "itkScalarVector.h"
+#include "itkDICOMImageIO2Factory.h"
+#include "itkTestDriverIncludeDeprecatedIOFactories.h"
+#include "itkNonThreadedShrinkImageFilter.h"
+#include "itkDefaultImageTraits.h"
+#include "itkDICOMSeriesFileNames.h"
+#include "itkReflectImageFilter.txx"
+#include "itkTwoOutputExampleImageFilter.txx"
+#include "itkDicomImageIOFactory.h"
+#include "itkShiftScaleInPlaceImageFilter.txx"
+#include "itkAnalyzeImageIOTest.h"
+#include "itkDeformableMesh3DFilter.txx"
+#include "itkAnalyzeImageIO.h"
+#include "itkNonUniformBSpline.txx"
+#include "itkDICOMImageIO2.h"
+#include "itkBalloonForceFilter.txx"
+#include "itkDicomImageIO.h"
+#include "itkAnalyzeImageIOFactory.h"
+#include "itkQuaternionOrientationAdapter.h"
+
+
+
+int itkDeprecatedHeaderTest ( int , char * [] )
+{
+
+  return EXIT_SUCCESS;
+}
