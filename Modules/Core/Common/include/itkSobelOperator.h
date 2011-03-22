a9ea39badc08d29a28945be8d13e4eb1b84049c2
Bill Lorensen <bill.lorensen@gmail.com>
2011-03-22 18:49:22 -0400

    STYLE: Replaced CreateOperator with CreateToRadius
    Change-Id: I5046f994e815604a1179f7b7fe61815d6c940ae3

--- Modules/Core/Common/include/itkSobelOperator.h
+++ Modules/Core/Common/include/itkSobelOperator.h
@@ -33,12 +33,17 @@ namespace itk
  * method. To create the operator:
  *
  * 1) Set the direction by calling  \code SetDirection \endcode
- * 2) call \code CreateOperator() \endcode
+ * 2) call
+ * \code
+ * itk::Size<2> radius;
+ * radius.Fill(1);
+ * sobelOperator.CreateToRadius(radius);
+ * \endcode
  * 3) You may optionally scale the coefficients of this operator using the
  * \code ScaleCoefficients \endcode method.  This is useful if you
  * want to take the spacing of the image into account when computing
  * the edge strength.  Apply the scaling only after calling to
- * \code CreateOperator \endcode.
+ * \code CreateToRadius \endcode.
  *
  * The Sobel Operator in vertical direction for 2 dimensions is
  * \verbatim
