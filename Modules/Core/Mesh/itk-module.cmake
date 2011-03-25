c40845598accc1e859e3c551e27862bcefb8a5f3
Bill Lorensen <bill.lorensen@gmail.com>
2011-03-25 18:40:35 -0400

    BUG: Typo in module name.
    IO-Spatialobjects -> IO-SpatialObjects
    
    Change-Id: Iea01b5571b36a497e22328e1137348c691e9385c

--- Modules/Core/Mesh/itk-module.cmake
+++ Modules/Core/Mesh/itk-module.cmake
@@ -1,2 +1,2 @@
-itk_module(ITK-Mesh DEPENDS ITK-Transform TEST_DEPENDS ITK-TestKernel ITK-IO-Spatialobjects)
-# Extra test dependecy on IO-SpatialObjects is cuased by itkMeshSpatialObjectIOTest.
+itk_module(ITK-Mesh DEPENDS ITK-Transform TEST_DEPENDS ITK-TestKernel ITK-IO-SpatialObjects)
+# Extra test dependency on IO-SpatialObjects is caused by itkMeshSpatialObjectIOTest.
