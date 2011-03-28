162a0e0bddf105f57be371501dfa46a32ca9e4e1
Luis Ibanez <luis.ibanez@kitware.com>
2011-03-28 16:54:37 -0400

    ENH: Fixing TestDriver for Examples and Wrapping.
    The itkTestDriver.cxx in TestKernel module is now suitable for
    testing the Wrapping.
    
    Change-Id: I8c09dbff4fca0c856f4a39dabb9f155bfd2afe7e

--- Modules/Core/TestKernel/src/itkTestDriver.cxx
+++ Modules/Core/TestKernel/src/itkTestDriver.cxx
@@ -28,15 +28,123 @@
 
 #include "itkTestDriverIncludeRequiredIOFactories.h"
 
+// define some itksys* things to make ShareForward.h happy
+#define itksys_SHARED_FORWARD_DIR_BUILD ""
+#define itksys_SHARED_FORWARD_PATH_BUILD ""
+#define itksys_SHARED_FORWARD_PATH_INSTALL ""
+#define itksys_SHARED_FORWARD_EXE_BUILD ""
+#define itksys_SHARED_FORWARD_EXE_INSTALL ""
+
+// include SharedForward to avoid duplicating the code which find the
+// library path variable name and the path separator
+#include "itksys/SharedForward.h"
+#include "itksys/Process.h"
+
+
 int main(int ac, char *av[])
 {
- RegisterRequiredFactories();
+  RegisterRequiredFactories();
+
+  // parse the command line
+  int  i = 1;
+  bool skip = false;
+  while ( i < ac )
+    {
+    if ( !skip && strcmp(av[i], "--add-before-libpath") == 0 )
+      {
+      if ( i + 1 >= ac )
+        {
+        usage();
+        return 1;
+        }
+      std::string libpath = KWSYS_SHARED_FORWARD_LDPATH;
+      libpath += "=";
+      libpath += av[i + 1];
+      char *oldenv = getenv(KWSYS_SHARED_FORWARD_LDPATH);
+      if ( oldenv )
+        {
+        libpath += KWSYS_SHARED_FORWARD_PATH_SEP;
+        libpath += oldenv;
+        }
+      itksys::SystemTools::PutEnv( libpath.c_str() );
+      // on some 64 bit systems, LD_LIBRARY_PATH_64 is used before
+      // LD_LIBRARY_PATH if it is set. It can lead the test to load
+      // the system library instead of the expected one, so this
+      // var must also be set
+      if ( std::string(KWSYS_SHARED_FORWARD_LDPATH) == "LD_LIBRARY_PATH" )
+        {
+        std::string libpath64 = "LD_LIBRARY_PATH_64";
+        libpath64 += "=";
+        libpath64 += av[i + 1];
+        char *oldenv64 = getenv("LD_LIBRARY_PATH_64");
+        if ( oldenv64 )
+          {
+          libpath64 += KWSYS_SHARED_FORWARD_PATH_SEP;
+          libpath64 += oldenv64;
+          }
+        itksys::SystemTools::PutEnv( libpath64.c_str() );
+        }
+      av += 2;
+      ac -= 2;
+      }
+    else if ( !skip && strcmp(av[i], "--add-before-env") == 0 )
+      {
+      if ( i + 2 >= ac )
+        {
+        usage();
+        return 1;
+        }
+      std::string env = av[i + 1];
+      env += "=";
+      env += av[i + 2];
+      char *oldenv = getenv(av[i + 1]);
+      if ( oldenv )
+        {
+        env += KWSYS_SHARED_FORWARD_PATH_SEP;
+        env += oldenv;
+        }
+      itksys::SystemTools::PutEnv( env.c_str() );
+      av += 3;
+      ac -= 3;
+      }
+    else if ( !skip && strcmp(av[i], "--add-before-env-with-sep") == 0 )
+      {
+      if ( i + 3 >= ac )
+        {
+        usage();
+        return 1;
+        }
+      std::string env = av[i + 1];
+      env += "=";
+      env += av[i + 2];
+      char *oldenv = getenv(av[i + 1]);
+      if ( oldenv )
+        {
+        env += av[i + 3];
+        env += oldenv;
+        }
+      itksys::SystemTools::PutEnv( env.c_str() );
+      av += 4;
+      ac -= 4;
+      }
+    else
+      {
+      i += 1;
+      }
+    }
 
  int result = ProcessArguments(&ac, &av);
 
- if (result == 0){
+ if (result == 0)
+   {
    #include "itkTestDriverBeforeTest.inc"
    #include "itkTestDriverAfterTest.inc"
- }
-return result;
+   }
+
+
+  // We call this function here just to silence a warning.
+  // It should not have any effect on the execution of the test driver.
+  itksys_shared_forward_to_real(0, NULL);
+
+  return result;
 }
