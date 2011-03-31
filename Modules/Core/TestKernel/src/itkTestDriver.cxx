e42e605c899dec4b525ccb5f00daa514d007dc9a
Luis Ibanez <luis.ibanez@kitware.com>
2011-03-31 19:01:00 -0400

    BUG: Fixes to Test Driver.
    Fixed invocation of processes when not needed.
    
    Recent changes to the itkTestDriverInclude.h that were introduced
    to support the wrapping testing by making possible to call from
    the test driver an external executable (e.g. Python interpreter)
    resulted in unnecessary attempts to interpret every set of test
    arguments as a potential external executable.
    
    In this patch, we introduce an explicit flag "--process" to be
    used to indicate when it is the developer's intention to actually
    call an external program.
    
    The ImageIOFactory::RegisterBuiltInFactories() was been removed
    in a previous commit. Here we remove the declaration that was
    left behind. Otherwise wrapping tests will segfault at runtime
    due to the absence of this symbol in the shared libraries.
    
    Change-Id: Ia8bd13eb88b8ef361e6e3d99a8a943e38002d3f1

--- Modules/Core/TestKernel/src/itkTestDriver.cxx
+++ Modules/Core/TestKernel/src/itkTestDriver.cxx
@@ -28,123 +28,271 @@
 
 #include "itkTestDriverIncludeRequiredIOFactories.h"
 
-// define some itksys* things to make ShareForward.h happy
-#define itksys_SHARED_FORWARD_DIR_BUILD ""
-#define itksys_SHARED_FORWARD_PATH_BUILD ""
-#define itksys_SHARED_FORWARD_PATH_INSTALL ""
-#define itksys_SHARED_FORWARD_EXE_BUILD ""
-#define itksys_SHARED_FORWARD_EXE_INSTALL ""
-
-// include SharedForward to avoid duplicating the code which find the
-// library path variable name and the path separator
-#include "itksys/SharedForward.h"
 #include "itksys/Process.h"
+#include "itksys/SystemTools.hxx"
+
+/* Select the environment variable holding the shared library runtime
+   search path for this platform.  */
+
+/* Linux */
+#if defined(__linux) || defined(__FreeBSD__) || defined(__OpenBSD__)
+# define ITK_TEST_DRIVER_LDPATH "LD_LIBRARY_PATH"
+# define ITK_TEST_DRIVER_LDPATH64 ITK_TEST_DRIVER_LDPATH
+
+/* OSX */
+#elif defined(__APPLE__)
+# define ITK_TEST_DRIVER_LDPATH "DYLD_LIBRARY_PATH"
+# define ITK_TEST_DRIVER_LDPATH64 ITK_TEST_DRIVER_LDPATH
+
+/* AIX */
+#elif defined(_AIX)
+# define ITK_TEST_DRIVER_LDPATH "LIBPATH"
+# define ITK_TEST_DRIVER_LDPATH64 ITK_TEST_DRIVER_LDPATH
+
+/* SUN */
+#elif defined(__sun)
+#  define ITK_TEST_DRIVER_LDPATH "LD_LIBRARY_PATH"
+#  define ITK_TEST_DRIVER_LDPATH64 "LD_LIBRARY_PATH_64"
+
+/* HP-UX */
+#elif defined(__hpux)
+#  define ITK_TEST_DRIVER_LDPATH "SHLIB_PATH"
+#  define ITK_TEST_DRIVER_LDPATH64 "LD_LIBRARY_PATH"
+
+/* SGI MIPS */
+#elif defined(__sgi) && defined(_MIPS_SIM)
+# if _MIPS_SIM == _ABIO32
+#  define ITK_TEST_DRIVER_LDPATH "LD_LIBRARY_PATH"
+# elif _MIPS_SIM == _ABIN32
+#  define ITK_TEST_DRIVER_LDPATH "LD_LIBRARYN32_PATH"
+# endif
+#  define ITK_TEST_DRIVER_LDPATH64 "LD_LIBRARY64_PATH"
+
+/* Cygwin */
+#elif defined(__CYGWIN__)
+# define ITK_TEST_DRIVER_LDPATH "PATH"
+# define ITK_TEST_DRIVER_LDPATH64 ITK_TEST_DRIVER_LDPATH
+
+/* Windows */
+#elif defined(_WIN32)
+# define ITK_TEST_DRIVER_LDPATH "PATH"
+# define ITK_TEST_DRIVER_LDPATH64 ITK_TEST_DRIVER_LDPATH
+
+/* Guess on this unknown system.  */
+#else
+# define ITK_TEST_DRIVER_LDPATH "LD_LIBRARY_PATH"
+# define ITK_TEST_DRIVER_LDPATH64 ITK_TEST_DRIVER_LDPATH
+#endif
+
+#if defined(_WIN32) && !defined(__CYGWIN__)
+# define ITK_TEST_DRIVER_PATH_SEP ';'
+# define ITK_TEST_DRIVER_PATH_SLASH '\\'
+#else
+# define ITK_TEST_DRIVER_PATH_SEP ':'
+# define ITK_TEST_DRIVER_PATH_SLASH '/'
+#endif
+
+
+void AddEntriesBeforeLibraryPath( const ArgumentsList & args )
+{
+  unsigned int i = 0;
+
+  while( i < args.size() )
+    {
+    std::string libpath = ITK_TEST_DRIVER_LDPATH;
+    libpath += "=";
+    libpath += args[i];
+    char *oldenv = getenv(ITK_TEST_DRIVER_LDPATH);
+    if ( oldenv )
+      {
+      libpath += ITK_TEST_DRIVER_PATH_SEP;
+      libpath += oldenv;
+      }
+    itksys::SystemTools::PutEnv( libpath.c_str() );
+    // on some 64 bit systems, LD_LIBRARY_PATH_64 is used before
+    // LD_LIBRARY_PATH if it is set. It can lead the test to load
+    // the system library instead of the expected one, so this
+    // var must also be set
+    if ( std::string(ITK_TEST_DRIVER_LDPATH) != ITK_TEST_DRIVER_LDPATH64 )
+      {
+      std::string libpath64 = ITK_TEST_DRIVER_LDPATH64;
+      libpath64 += "=";
+      libpath64 += args[i];
+      char *oldenv64 = getenv(ITK_TEST_DRIVER_LDPATH64);
+      if ( oldenv64 )
+        {
+        libpath64 += ITK_TEST_DRIVER_PATH_SEP;
+        libpath64 += oldenv64;
+        }
+      itksys::SystemTools::PutEnv( libpath64.c_str() );
+      }
+
+    i++;
+    }
+}
+
+
+void AddEntriesBeforeEnvironment( const ArgumentsList & args )
+{
+  unsigned int i = 0;
+
+  while( i < args.size() )
+    {
+    std::string env = args[i];
+    env += "=";
+    env += args[i+1];
+    char *oldenv = getenv( args[i] );
+    if ( oldenv )
+      {
+      env += ITK_TEST_DRIVER_PATH_SEP;
+      env += oldenv;
+      }
+    itksys::SystemTools::PutEnv( env.c_str() );
+
+    i += 2;
+    }
+}
+
+
+void AddEntriesBeforeEnvironmentWithSeparator( const ArgumentsList & args )
+{
+  unsigned int i = 0;
+
+  while( i < args.size() )
+    {
+    std::string env = args[i];
+    env += "=";
+    env += args[i+1];
+    char *oldenv = getenv( args[i] );
+    if ( oldenv )
+      {
+      env += args[i+2];
+      env += oldenv;
+      }
+    itksys::SystemTools::PutEnv( env.c_str() );
+
+    i += 3;
+    }
+}
+
+
+int TestDriverInvokeProcess( const ArgumentsList & args )
+{
+  // a NULL is required at the end of the table
+  char ** argv = new char *[args.size() + 1];
+  for ( unsigned int i = 0; i < args.size(); i++ )
+    {
+    argv[i] = args[i];
+    }
+  argv[args.size()] = NULL;
+
+  itksysProcess *process = itksysProcess_New();
+  itksysProcess_SetCommand(process, argv);
+  itksysProcess_SetPipeShared(process, itksysProcess_Pipe_STDOUT, true);
+  itksysProcess_SetPipeShared(process, itksysProcess_Pipe_STDERR, true);
+  itksysProcess_Execute(process);
+  itksysProcess_WaitForExit(process, NULL);
+
+  delete[] argv;
+
+  int state = itksysProcess_GetState(process);
+  switch( state )
+    {
+    case itksysProcess_State_Error:
+      {
+      std::cerr << "itkTestDriver: Process error: " << itksysProcess_GetErrorString(process) << std::endl;
+      return 1;
+      break;
+      }
+    case itksysProcess_State_Exception:
+      {
+      std::cerr << "itkTestDriver: Process exception: " << itksysProcess_GetExceptionString(process) << std::endl;
+      return 1;
+      break;
+      }
+    case itksysProcess_State_Executing:
+      {
+      // this is not a possible state after itksysProcess_WaitForExit
+      std::cerr << "itkTestDriver: Internal error: process can't be in Executing State." << std::endl;
+      return 1;
+      break;
+      }
+    case itksysProcess_State_Exited:
+      {
+      // this is the normal case - it is treated later
+      break;
+      }
+    case itksysProcess_State_Expired:
+      {
+      // this is not a possible state after itksysProcess_WaitForExit
+      std::cerr << "itkTestDriver: Internal error: process can't be in Expired State." << std::endl;
+      return 1;
+      break;
+      }
+    case itksysProcess_State_Killed:
+      {
+      std::cerr << "itkTestDriver: The process has been killed." << std::endl;
+      return 1;
+      break;
+      }
+    case itksysProcess_State_Disowned:
+      {
+      std::cerr << "itkTestDriver: Process disowned." << std::endl;
+      return 1;
+      break;
+      }
+    default:
+      {
+      // this is not a possible state after itksysProcess_WaitForExit
+      std::cerr << "itkTestDriver: Internal error: unknown State." << std::endl;
+      return 1;
+      break;
+      }
+    }
+
+  int retCode = itksysProcess_GetExitValue(process);
+  if ( retCode != 0 )
+    {
+    std::cerr << "itkTestDriver: Process exited with return value: " << retCode << std::endl;
+    }
+
+return retCode;
+}
 
 
 int main(int ac, char *av[])
 {
   RegisterRequiredFactories();
 
-  // parse the command line
-  int  i = 1;
-  bool skip = false;
-  while ( i < ac )
+  ProcessedOutputType po;
+
+  int result = 0;
+
+  result = ProcessArguments(&ac, &av, &po);
+
+  if ( po.args.empty() )
     {
-    if ( !skip && strcmp(av[i], "--add-before-libpath") == 0 )
-      {
-      if ( i + 1 >= ac )
-        {
-        usage();
-        return 1;
-        }
-      std::string libpath = KWSYS_SHARED_FORWARD_LDPATH;
-      libpath += "=";
-      libpath += av[i + 1];
-      char *oldenv = getenv(KWSYS_SHARED_FORWARD_LDPATH);
-      if ( oldenv )
-        {
-        libpath += KWSYS_SHARED_FORWARD_PATH_SEP;
-        libpath += oldenv;
-        }
-      itksys::SystemTools::PutEnv( libpath.c_str() );
-      // on some 64 bit systems, LD_LIBRARY_PATH_64 is used before
-      // LD_LIBRARY_PATH if it is set. It can lead the test to load
-      // the system library instead of the expected one, so this
-      // var must also be set
-      if ( std::string(KWSYS_SHARED_FORWARD_LDPATH) == "LD_LIBRARY_PATH" )
-        {
-        std::string libpath64 = "LD_LIBRARY_PATH_64";
-        libpath64 += "=";
-        libpath64 += av[i + 1];
-        char *oldenv64 = getenv("LD_LIBRARY_PATH_64");
-        if ( oldenv64 )
-          {
-          libpath64 += KWSYS_SHARED_FORWARD_PATH_SEP;
-          libpath64 += oldenv64;
-          }
-        itksys::SystemTools::PutEnv( libpath64.c_str() );
-        }
-      av += 2;
-      ac -= 2;
-      }
-    else if ( !skip && strcmp(av[i], "--add-before-env") == 0 )
-      {
-      if ( i + 2 >= ac )
-        {
-        usage();
-        return 1;
-        }
-      std::string env = av[i + 1];
-      env += "=";
-      env += av[i + 2];
-      char *oldenv = getenv(av[i + 1]);
-      if ( oldenv )
-        {
-        env += KWSYS_SHARED_FORWARD_PATH_SEP;
-        env += oldenv;
-        }
-      itksys::SystemTools::PutEnv( env.c_str() );
-      av += 3;
-      ac -= 3;
-      }
-    else if ( !skip && strcmp(av[i], "--add-before-env-with-sep") == 0 )
-      {
-      if ( i + 3 >= ac )
-        {
-        usage();
-        return 1;
-        }
-      std::string env = av[i + 1];
-      env += "=";
-      env += av[i + 2];
-      char *oldenv = getenv(av[i + 1]);
-      if ( oldenv )
-        {
-        env += av[i + 3];
-        env += oldenv;
-        }
-      itksys::SystemTools::PutEnv( env.c_str() );
-      av += 4;
-      ac -= 4;
-      }
-    else
-      {
-      i += 1;
-      }
+    usage();
+    return 1;
     }
 
- int result = ProcessArguments(&ac, &av);
 
- if (result == 0)
-   {
-   #include "itkTestDriverBeforeTest.inc"
-   #include "itkTestDriverAfterTest.inc"
-   }
+  if ( !result && po.externalProcessMustBeCalled )
+    {
+    AddEntriesBeforeLibraryPath( po.add_before_libpath );
 
+    AddEntriesBeforeEnvironment( po.add_before_env );
 
-  // We call this function here just to silence a warning.
-  // It should not have any effect on the execution of the test driver.
-  itksys_shared_forward_to_real(0, NULL);
+    AddEntriesBeforeEnvironmentWithSeparator( po.add_before_env_with_sep );
+
+    result = TestDriverInvokeProcess( po.args );
+    }
+
+  if (result == 0)
+    {
+    #include "itkTestDriverBeforeTest.inc"
+    #include "itkTestDriverAfterTest.inc"
+    }
 
   return result;
 }
