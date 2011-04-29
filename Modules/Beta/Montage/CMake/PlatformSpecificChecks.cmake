b58a5094e471a627c29d8eabdd650d2c373ffedb
Luis Ibanez <luis.ibanez@kitware.com>
2011-04-29 15:43:52 -0400

    ENH: Disable Windows deprecated warnings.

---
+++ Modules/Beta/Montage/CMake/PlatformSpecificChecks.cmake
@@ -0,0 +1,24 @@
+
+# On Visual Studio 8 MS deprecated C. This removes all 1.276E1265 security
+# warnings
+if(WIN32)
+   if(NOT CYGWIN)
+     if(NOT MINGW)
+       if(NOT ITK_ENABLE_VISUAL_STUDIO_DEPRECATED_C_WARNINGS)
+         add_definitions(
+           -D_CRT_FAR_MAPPINGS_NO_DEPRECATE
+           -D_CRT_IS_WCTYPE_NO_DEPRECATE
+           -D_CRT_MANAGED_FP_NO_DEPRECATE
+           -D_CRT_NONSTDC_NO_DEPRECATE
+           -D_CRT_SECURE_NO_DEPRECATE
+           -D_CRT_SECURE_NO_DEPRECATE_GLOBALS
+           -D_CRT_SETERRORMODE_BEEP_SLEEP_NO_DEPRECATE
+           -D_CRT_TIME_FUNCTIONS_NO_DEPRECATE
+           -D_CRT_VCCLRIT_NO_DEPRECATE
+           -D_SCL_SECURE_NO_DEPRECATE
+           )
+       endif(NOT ITK_ENABLE_VISUAL_STUDIO_DEPRECATED_C_WARNINGS)
+     endif(NOT MINGW)
+   endif(NOT CYGWIN)
+endif(WIN32)
+
