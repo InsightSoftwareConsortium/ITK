8b4d6db7c322586326b9ba4e717a1f46ea848b96
Gabe Hart <gabe.hart@kitware.com>
2011-05-04 14:36:20 -0400

    ENH: Uncomment parts of FileListVideoIOTest

--- Modules/VideoIO/FileList/test/itkFileListVideoIOTest.cxx
+++ Modules/VideoIO/FileList/test/itkFileListVideoIOTest.cxx
@@ -66,7 +66,6 @@ int test_FileListVideoIO ( char* input, char* nonVideoInput, char* output, char*
     std::cerr << "Should have failed to open \"" << nonVideoInput << "\"" << std::endl;
     ret = EXIT_FAILURE; 
     }
-/*
 
   //////
   // ReadImageInformation
@@ -358,7 +357,7 @@ int test_FileListVideoIO ( char* input, char* nonVideoInput, char* output, char*
   //std::cout << "I263 = " << CV_FOURCC('I', '2', '6', '3') << std::endl;
   //std::cout << "FLV1 = " << CV_FOURCC('F', 'L', 'V', '1') << std::endl;
 
-*/
+
 
   std::cout<<"Done !"<<std::endl;
   return ret;
