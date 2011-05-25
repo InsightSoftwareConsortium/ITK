b3c2e083f2ab1b3f0b0aac9727890db959ac4d0b
Luis Ibanez <luis.ibanez@kitware.com>
2011-05-25 14:22:38 -0400

    DOC: Added Doxygen documentation for the module.
    Change-Id: I92d742fea69e851800fe043dc8eff2892e04bb09

--- Modules/GPU/Common/itk-module-TBD.cmake
+++ Modules/GPU/Common/itk-module-TBD.cmake
@@ -1,1 +1,10 @@
-itk_module(ITK-GPUCommon DEPENDS  ITK-Common OpenCL TEST_DEPENDS ITK-TestKernel)
+set("DOCUMENTATION  This module contains the basic infrastructure to support
+GPU based operations in ITK. The essential classes here allow to make
+transparent use of an itkImage with memory managed either by the CPU or the
+GPU. They also make possible to provide GPU-based filters that act as
+replacements for existing standard CPU filters. This replacement is done by
+taking advantage of the ITK Factories, and have the advantage that the code of
+existing applications do not have to be modified in order to take advantage of
+this GPU support.")
+
+itk_module(ITK-GPUCommon DEPENDS  ITK-Common OpenCL TEST_DEPENDS ITK-TestKernel DESCRIPTION "${DOCUMENTATION}")
