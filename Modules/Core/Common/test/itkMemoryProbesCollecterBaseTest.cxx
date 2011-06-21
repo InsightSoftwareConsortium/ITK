72377308cceb7905a25c04dc2cbc505473e09ad8
Kent Williams <norman-k-williams@uiowa.edu>
2011-06-21 10:12:20 -0500

    BUG: 10252 - MemoryProbesCollecterBase reports zero.
    I added a test based on the example given in
    www.itk.org/BUg/view.php?id=10252 and it does show that
    after allocating a megabyte, the probe reports zero memory
    usage.
    
    I did some googling to try and find solutions that actually
    work on OSX and Linux and incorporated them.
    
    This code also had some suspect usage of preprocessor
    symbols. It didn't actually work; On OS X  it
    was performing arithmetic on undefined preprocessor
    variables.
    
    Change-Id: Id9e09219f26bc4d9527e4209150bb68bc5806552

---
+++ Modules/Core/Common/test/itkMemoryProbesCollecterBaseTest.cxx
@@ -0,0 +1,51 @@
+#include "itkMemoryProbesCollectorBase.h"
+#include "itkMemoryProbe.h"
+
+#if defined(TEST_WITH_SLEEP)
+#if defined(_WIN32) && !defined(__CYGWIN__)
+#include <windows.h>
+#else
+#include <unistd.h>
+inline
+void Sleep(unsigned int milleseconds)
+{
+  sleep(milleseconds / 1000);
+}
+#endif
+#else
+#define Sleep(x) // Empty
+#endif //defined(TEST_WITH_SLEEP)
+
+#include <stdlib.h>
+
+int itkMemoryProbesCollecterBaseTest(int, char *[])
+{
+  const size_t megabyte = 1024L * 1024L;
+
+  itk::MemoryProbesCollectorBase mcollecter;
+  itk::MemoryProbe probe;
+  mcollecter.Start("Update");
+  Sleep(5000);
+  mcollecter.Stop("Update");
+  mcollecter.Report();
+  mcollecter.Clear();
+  mcollecter.Start("Update");
+  probe.Start();
+  char *buf = new char[megabyte];
+  for(unsigned int i = 0; i < megabyte; i++)
+    {
+    buf[i] = static_cast<char>(i & 0xff);
+    }
+  Sleep(5000);
+  mcollecter.Stop("Update");
+  probe.Stop();
+  size_t total = probe.GetTotal();
+  std::cout << " Total Value " << probe.GetTotal() << std::endl;
+  if(total == 0)
+    {
+    std::cout << "Total memory usage should be greater than zero" << std::endl;
+    return EXIT_FAILURE;
+    }
+  mcollecter.Report();
+  return EXIT_SUCCESS;
+}
