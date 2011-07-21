6c02a82d19c5821ca9f6ea2de247332547ef4384
Cory Quammen <cquammen@cs.unc.edu>
2011-07-21 16:54:16 -0400

    BUG: WrapPadImageFilter couldn't be used with VectorImage
    Fixed itk::ImageRegionExclusionIteratorWithIndex and
    itk::PeriodicBoundaryCondition to work on VectorImages.  Thanks to
    Brad Lowekamp for the ImageRegionExclusionIteratorWithIndex fix.
    
    Added test of VectorImages in itkWrapPadImageTest.
    
    Change-Id: I70ca4a5c8f32cf11fef10fa8bc5c318e3ee3d677

--- Modules/Filtering/ImageGrid/test/itkWrapPadImageTest.cxx
+++ Modules/Filtering/ImageGrid/test/itkWrapPadImageTest.cxx
@@ -19,23 +19,22 @@
 #pragma warning ( disable : 4786 )
 #endif
 
+#include <iostream>
+#include "itkFileOutputWindow.h"
 #include "itkImage.h"
 #include "itkImage.h"
 #include "itkImageRegionIterator.h"
-#include "itkWrapPadImageFilter.h"
-#include "itkFileOutputWindow.h"
 #include "itkMultiThreader.h"
 #include "itkStreamingImageFilter.h"
-#include <iostream>
+#include "itkWrapPadImageFilter.h"
+#include "itkVectorImage.h"
 
 //
 // Check that val represents the correct pixel value.  This routine
 // allows the pad region to extend to twice the size of the input.
 //
-int VerifyPixel(int row, int col, int val)
+int VerifyPixel(int row, int col, short val, short & expected)
 {
-  int nextVal;
-
   if (row < 0) row += 8;
   if (row < 0) row += 8;
   if (row > 7) row -= 8;
@@ -44,234 +43,283 @@ int VerifyPixel(int row, int col, int val)
   if (col < 0) col += 12;
   if (col > 11) col -= 12;
   if (col > 11) col -= 12;
-  nextVal = 8*col+row;
-  return (val == nextVal);
+  expected = 8*col+row;
+  return (val == expected);
 }
 
 
 int itkWrapPadImageTest(int, char* [] )
 {
-//  itk::FileOutputWindow::Pointer fow = itk::FileOutputWindow::New();
-//  fow->SetInstance(fow);
-//
-//  itk::MultiThreader::SetGlobalDefaultNumberOfThreads(1);
-
   // typedefs to simplify the syntax
-  typedef itk::Image<short, 2>   SimpleImage;
-  SimpleImage::Pointer simpleImage = SimpleImage::New();
-  std::cout << "Simple image spacing: " << simpleImage->GetSpacing()[0] << ", "
-            << simpleImage->GetSpacing()[1] << std::endl;
-
-  // typedefs to simplify the syntax
-  typedef itk::Image<short, 2>   ShortImage;
+  typedef itk::Image< short, 2 >       ShortImage;
+  typedef itk::VectorImage< short, 2 > VectorImage;
 
   // Test the creation of an image with native type
-  ShortImage::Pointer if2 = ShortImage::New();
-
-  // fill in an image
+  ShortImage::Pointer    image = ShortImage::New();
   ShortImage::IndexType  index = {{0, 0}};
   ShortImage::SizeType   size = {{8, 12}};
-  ShortImage::RegionType region;
-  int row, column;
-  region.SetSize( size );
-  region.SetIndex( index );
-  if2->SetLargestPossibleRegion( region );
-  if2->SetBufferedRegion( region );
-  if2->Allocate();
+  ShortImage::RegionType region( index, size );
+  image->SetRegions( region );
+  image->Allocate();
 
-  itk::ImageRegionIterator<ShortImage> iterator(if2, region);
+  VectorImage::Pointer vectorImage = VectorImage::New();
+  vectorImage->SetRegions( region );
+  vectorImage->SetNumberOfComponentsPerPixel( 3 );
+  vectorImage->Allocate();
 
-  short i=0;
-  for (; !iterator.IsAtEnd(); ++iterator, ++i)
+  itk::ImageRegionIterator< ShortImage > it( image, region );
+  itk::ImageRegionIterator< VectorImage > vit( vectorImage, region );
+
+  ShortImage::PixelType i = 0;
+  for (; !it.IsAtEnd(); ++it, ++vit, ++i)
     {
-      iterator.Set( i );
+    it.Set( i );
+    VectorImage::PixelType vectorPixel( 3 );
+    vectorPixel = i;
+    vectorPixel[1] = i+1;
+    vit.Set( vectorPixel );
     }
 
   // Create a filter
   itk::WrapPadImageFilter< ShortImage, ShortImage >::Pointer wrapPad;
   wrapPad = itk::WrapPadImageFilter< ShortImage, ShortImage >::New();
-  wrapPad->SetInput( if2 );
+  wrapPad->SetInput( image );
 
-  ShortImage::SizeValueType upperfactors[2] = { 0, 0};
-  ShortImage::SizeValueType lowerfactors[2] = { 0, 0};
+  itk::WrapPadImageFilter< VectorImage, VectorImage >::Pointer vectorWrapPad;
+  vectorWrapPad = itk::WrapPadImageFilter< VectorImage, VectorImage >::New();
+  vectorWrapPad->SetInput( vectorImage );
 
-  wrapPad->SetPadLowerBound(lowerfactors);
-  wrapPad->SetPadUpperBound(upperfactors);
+  ShortImage::SizeValueType upperBound[2] = { 0, 0};
+  ShortImage::SizeValueType lowerBound[2] = { 0, 0};
+
+  wrapPad->SetPadLowerBound( lowerBound );
+  wrapPad->SetPadUpperBound( upperBound );
   wrapPad->UpdateLargestPossibleRegion();
 
+  vectorWrapPad->SetPadLowerBound( lowerBound );
+  vectorWrapPad->SetPadUpperBound( upperBound );
+  vectorWrapPad->UpdateLargestPossibleRegion();
+
   std::cout << wrapPad << std::endl;
+  std::cout << vectorWrapPad << std::endl;
 
-  std::cout << "Input spacing: " << if2->GetSpacing()[0] << ", "
-            << if2->GetSpacing()[1] << std::endl;
+  std::cout << "Input spacing: " << image->GetSpacing()[0] << ", "
+            << image->GetSpacing()[1] << std::endl;
   std::cout << "Output spacing: " << wrapPad->GetOutput()->GetSpacing()[0]
-            << ", "
-            << wrapPad->GetOutput()->GetSpacing()[1] << std::endl;
-
+            << ", " << wrapPad->GetOutput()->GetSpacing()[1] << std::endl;
 
   ShortImage::RegionType requestedRegion;
   bool passed;
 
   // CASE 1
-  lowerfactors[0] = 1; lowerfactors[1] = 3;
-  upperfactors[0] = 2; upperfactors[1] = 4;
-  wrapPad->SetPadLowerBound(lowerfactors);
-  wrapPad->SetPadUpperBound(upperfactors);
+  lowerBound[0] = 1; lowerBound[1] = 3;
+  upperBound[0] = 2; upperBound[1] = 4;
+  wrapPad->SetPadLowerBound( lowerBound );
+  wrapPad->SetPadUpperBound( upperBound );
   wrapPad->UpdateLargestPossibleRegion();
+
+  vectorWrapPad->SetPadLowerBound( lowerBound );
+  vectorWrapPad->SetPadUpperBound( upperBound );
+  vectorWrapPad->UpdateLargestPossibleRegion();
+
   requestedRegion = wrapPad->GetOutput()->GetRequestedRegion();
 
-  itk::ImageRegionIterator<ShortImage>
-    iteratorIn1(wrapPad->GetOutput(), requestedRegion);
+  itk::ImageRegionIterator< ShortImage > itIn1( wrapPad->GetOutput(), requestedRegion );
+  itk::ImageRegionIterator< VectorImage > vitIn1( vectorWrapPad->GetOutput(), requestedRegion );
 
   passed = true;
   size = requestedRegion.GetSize();
   index = requestedRegion.GetIndex();
-  if ((index[0] != (0 - (long) lowerfactors[0]))
-      || (index[1] != (0 - (long) lowerfactors[1]))
-      || (size[0] != (8 + lowerfactors[0] + upperfactors[0]))
-      || (size[1] != (12 + lowerfactors[1] + upperfactors[1]))) {
+  if ( ( index[0] != (0 - (long) lowerBound[0] ) )
+      || ( index[1] != (0 - (long) lowerBound[1] ) )
+      || ( size[0] != (8 + lowerBound[0] + upperBound[0] ) )
+      || ( size[1] != (12 + lowerBound[1] + upperBound[1] ) ) )
+    {
     passed = false;
-  } else {
-
-    for (; !iteratorIn1.IsAtEnd(); ++iteratorIn1)
-      {
-  row = iteratorIn1.GetIndex()[0];
-  column = iteratorIn1.GetIndex()[1];
-  if (!VerifyPixel(row, column, iteratorIn1.Get()))
-    {
-      std::cout << "Error: (" << row << ", " << column
-          << "), got "
-          << iteratorIn1.Get() << std::endl;
-      passed = false;
-    }
-      }
-  }
-
-  if (passed)
-    {
-      std::cout << "wrapPadImageFilter case 1 passed." << std::endl;
     }
   else
     {
-      std::cout << "wrapPadImageFilter case 1 failed." << std::endl;
-      return EXIT_FAILURE;
+    for (; !itIn1.IsAtEnd(); ++itIn1, ++vitIn1)
+      {
+      int row = itIn1.GetIndex()[0];
+      int column = itIn1.GetIndex()[1];
+      ShortImage::PixelType expected = 0;
+
+      if ( !VerifyPixel( row, column, itIn1.Get(), expected ) )
+        {
+        std::cout << "Error in wrapPad: index (" << row << ", " << column
+                  << "). Got " << itIn1.Get() << ", expected " << expected << std::endl;
+        passed = false;
+        }
+
+      if ( !VerifyPixel( row, column, vitIn1.Get()[1] - 1, expected ) )
+        {
+        std::cout << "Error in vectorWrapPad: index (" << row << ", " << column
+                  << "). Got " << vitIn1.Get()[1] - 1 << ", expected " << expected << std::endl;
+        passed = false;
+        }
+      }
+    }
+
+  if (passed)
+    {
+    std::cout << "wrapPadImageFilter case 1 passed." << std::endl;
+    }
+  else
+    {
+    std::cout << "wrapPadImageFilter case 1 failed." << std::endl;
+    return EXIT_FAILURE;
     }
 
 
   // CASE 2
-  lowerfactors[0] = 10;
-  upperfactors[1] = 15;
-  wrapPad->SetPadLowerBound(lowerfactors);
-  wrapPad->SetPadUpperBound(upperfactors);
+  lowerBound[0] = 10;
+  upperBound[1] = 15;
+  wrapPad->SetPadLowerBound( lowerBound );
+  wrapPad->SetPadUpperBound( upperBound );
+  vectorWrapPad->SetPadLowerBound( lowerBound );
+  vectorWrapPad->SetPadUpperBound( upperBound );
 
-  if ((wrapPad->GetPadUpperBound()[0] != upperfactors[0])
-      || (wrapPad->GetPadUpperBound()[1] != upperfactors[1])
-      || (wrapPad->GetPadLowerBound()[0] != lowerfactors[0])
-      || (wrapPad->GetPadLowerBound()[1] != lowerfactors[1]))
+  if ((wrapPad->GetPadUpperBound()[0] != upperBound[0])
+      || (wrapPad->GetPadUpperBound()[1] != upperBound[1])
+      || (wrapPad->GetPadLowerBound()[0] != lowerBound[0])
+      || (wrapPad->GetPadLowerBound()[1] != lowerBound[1]))
     {
-      passed = false;
+    passed = false;
     }
   else
     {
-      wrapPad->UpdateLargestPossibleRegion();
-      requestedRegion = wrapPad->GetOutput()->GetRequestedRegion();
+    wrapPad->UpdateLargestPossibleRegion();
+    vectorWrapPad->UpdateLargestPossibleRegion();
+    requestedRegion = wrapPad->GetOutput()->GetRequestedRegion();
 
-      itk::ImageRegionIterator<ShortImage>
-  iteratorIn2(wrapPad->GetOutput(), requestedRegion);
+    itk::ImageRegionIterator< ShortImage > itIn2( wrapPad->GetOutput(), requestedRegion );
+    itk::ImageRegionIterator< VectorImage > vitIn2( vectorWrapPad->GetOutput(), requestedRegion );
 
-      passed = true;
-      size = requestedRegion.GetSize();
-      index = requestedRegion.GetIndex();
-      if ((index[0] != (0 - (long) lowerfactors[0]))
-    || (index[1] != (0 - (long) lowerfactors[1]))
-    || (size[0] != (8 + lowerfactors[0] + upperfactors[0]))
-    || (size[1] != (12 + lowerfactors[1] + upperfactors[1]))) {
-  passed = false;
-      } else {
-  for (; !iteratorIn2.IsAtEnd(); ++iteratorIn2)
-    {
-      row = iteratorIn2.GetIndex()[0];
-      column = iteratorIn2.GetIndex()[1];
-      if (!VerifyPixel(row, column, iteratorIn2.Get()))
+    passed = true;
+    size = requestedRegion.GetSize();
+    index = requestedRegion.GetIndex();
+    if ( ( index[0] != (0 - (long) lowerBound[0] ) )
+        || ( index[1] != (0 - (long) lowerBound[1] ) )
+        || ( size[0] != (8 + lowerBound[0] + upperBound[0] ) )
+        || ( size[1] != (12 + lowerBound[1] + upperBound[1] ) ) )
+      {
+      passed = false;
+      }
+    else
+      {
+      for (; !itIn2.IsAtEnd(); ++itIn2, ++vitIn2 )
         {
-    std::cout << "Error: (" << row << ", " << column
-        << "), got "
-        << iteratorIn2.Get() << std::endl;
-    passed = false;
+        int row = itIn2.GetIndex()[0];
+        int column = itIn2.GetIndex()[1];
+        ShortImage::PixelType expected = 0;
+
+        if ( !VerifyPixel( row, column, itIn2.Get(), expected ) )
+          {
+          std::cout << "Error in wrapPad: index (" << row << ", " << column
+                    << "). Got " << itIn2.Get() << ", expected " << expected << std::endl;
+          passed = false;
+          }
+
+        if ( !VerifyPixel( row, column, vitIn2.Get()[1] - 1, expected ) )
+          {
+          std::cout << "Error in vectorWrapPad: index (" << row << ", " << column
+                    << "). Got " << vitIn2.Get()[1] - 1 << ", expected " << expected << std::endl;
+          passed = false;
+          }
         }
-    }
       }
     }
 
   if (passed)
     {
-      std::cout << "wrapPadImageFilter case 2 passed." << std::endl;
+    std::cout << "wrapPadImageFilter case 2 passed." << std::endl;
     }
   else
     {
-      std::cout << "wrapPadImageFilter case 2 failed." << std::endl;
-      return EXIT_FAILURE;
+    std::cout << "wrapPadImageFilter case 2 failed." << std::endl;
+    return EXIT_FAILURE;
     }
 
 
   // CASE 3
-  lowerfactors[1] = 16;
-  upperfactors[0] = 9;
-  wrapPad->SetPadLowerBound(lowerfactors);
-  wrapPad->SetPadUpperBound(upperfactors);
+  lowerBound[1] = 16;
+  upperBound[0] = 9;
+  wrapPad->SetPadLowerBound( lowerBound );
+  wrapPad->SetPadUpperBound( upperBound );
+  vectorWrapPad->SetPadLowerBound( lowerBound );
+  vectorWrapPad->SetPadUpperBound( upperBound );
 
   // Create a stream
-  itk::StreamingImageFilter< ShortImage, ShortImage >::Pointer stream;
-  stream = itk::StreamingImageFilter< ShortImage, ShortImage >::New();
+  itk::StreamingImageFilter< ShortImage, ShortImage >::Pointer stream =
+    itk::StreamingImageFilter< ShortImage, ShortImage >::New();
   stream->SetInput( wrapPad->GetOutput() );
-  stream->SetNumberOfStreamDivisions(3);
+  stream->SetNumberOfStreamDivisions( 3 );
 
-  if ((wrapPad->GetPadUpperBound()[0] != upperfactors[0])
-      || (wrapPad->GetPadUpperBound()[1] != upperfactors[1])
-      || (wrapPad->GetPadLowerBound()[0] != lowerfactors[0])
-      || (wrapPad->GetPadLowerBound()[1] != lowerfactors[1]))
+  itk::StreamingImageFilter< VectorImage, VectorImage >::Pointer vectorStream =
+    itk::StreamingImageFilter< VectorImage, VectorImage >::New();
+  vectorStream->SetInput( vectorWrapPad->GetOutput() );
+  vectorStream->SetNumberOfStreamDivisions( 3 );
+
+  if ( ( wrapPad->GetPadUpperBound()[0] != upperBound[0] )
+      || ( wrapPad->GetPadUpperBound()[1] != upperBound[1] )
+      || ( wrapPad->GetPadLowerBound()[0] != lowerBound[0] )
+      || ( wrapPad->GetPadLowerBound()[1] != lowerBound[1] ) )
     {
-      passed = false;
+    passed = false;
     }
   else
     {
-      stream->UpdateLargestPossibleRegion();
-      requestedRegion = stream->GetOutput()->GetRequestedRegion();
+    stream->UpdateLargestPossibleRegion();
+    vectorStream->UpdateLargestPossibleRegion();
+    requestedRegion = stream->GetOutput()->GetRequestedRegion();
 
-      itk::ImageRegionIterator<ShortImage>
-  iteratorIn3(stream->GetOutput(), requestedRegion);
+    itk::ImageRegionIterator< ShortImage > itIn3(stream->GetOutput(), requestedRegion);
+    itk::ImageRegionIterator< VectorImage > vitIn3(vectorStream->GetOutput(), requestedRegion);
 
-      passed = true;
-      size = requestedRegion.GetSize();
-      index = requestedRegion.GetIndex();
-      if ((index[0] != (0 - (long) lowerfactors[0]))
-    || (index[1] != (0 - (long) lowerfactors[1]))
-    || (size[0] != (8 + lowerfactors[0] + upperfactors[0]))
-    || (size[1] != (12 + lowerfactors[1] + upperfactors[1]))) {
-  passed = false;
-      } else {
-  for (; !iteratorIn3.IsAtEnd(); ++iteratorIn3)
-    {
-      row = iteratorIn3.GetIndex()[0];
-      column = iteratorIn3.GetIndex()[1];
-      if (!VerifyPixel(row, column, iteratorIn3.Get()))
+    passed = true;
+    size = requestedRegion.GetSize();
+    index = requestedRegion.GetIndex();
+    if ( ( index[0] != (0 - (long) lowerBound[0] ) )
+        || ( index[1] != (0 - (long) lowerBound[1] ) )
+        || ( size[0] != (8 + lowerBound[0] + upperBound[0] ) )
+        || ( size[1] != (12 + lowerBound[1] + upperBound[1] ) ) )
+      {
+      passed = false;
+      }
+    else
+      {
+      for (; !itIn3.IsAtEnd(); ++itIn3, ++vitIn3 )
         {
-    std::cout << "Error: (" << row << ", " << column
-        << "), got "
-        << iteratorIn3.Get() << std::endl;
-    passed = false;
+        int row = itIn3.GetIndex()[0];
+        int column = itIn3.GetIndex()[1];
+        ShortImage::PixelType expected = 0;
+
+        if ( !VerifyPixel( row, column, itIn3.Get(), expected ) )
+          {
+          std::cout << "Error in wrapPad: index (" << row << ", " << column
+                    << "). Got " << itIn3.Get() << ", expected " << expected << std::endl;
+          passed = false;
+          }
+
+        if ( !VerifyPixel( row, column, vitIn3.Get()[1] - 1, expected ) )
+          {
+          std::cout << "Error in vectorWrapPad: index (" << row << ", " << column
+                    << "). Got " << vitIn3.Get()[1] - 1 << ", expected " << expected << std::endl;
+          passed = false;
+          }
         }
-    }
       }
     }
 
   if (passed)
     {
-      std::cout << "wrapPadImageFilter case 3 passed." << std::endl;
+    std::cout << "wrapPadImageFilter case 3 passed." << std::endl;
     }
   else
     {
-      std::cout << "wrapPadImageFilter case 3 failed." << std::endl;
-      return EXIT_FAILURE;
+    std::cout << "wrapPadImageFilter case 3 failed." << std::endl;
+    return EXIT_FAILURE;
     }
 
   return EXIT_SUCCESS;
