b3e8e94e7dc757ce8c758746a5b54135db5f6647
Brad King <brad.king@kitware.com>
2011-09-13 15:05:09 -0400

    ENH: Support ExternalData local object stores for ITK
    This squashes the following commits together for review.
    
    ENH: ExternalData: Support local object stores
    
    Add configuration variable ExternalData_OBJECT_STORES to specify one or
    more local directories that store objects.  This allows local stores to
    be shared among multiple build trees and multiple projects.  Since the
    stores use content-addressed storage there will be no conflicts.
    
    ENH: Configure ExternalData local object stores for ITK
    
    Expose ExternalData option ExternalData_OBJECT_STORES as a cache entry
    defaulting to the empty string.  If not set, use a directory in the
    build tree as the main store.  Either way, add the .ExternalData
    directory at the top of the source tree as a local object store.  Remove
    the latter from the URL templates list so that the objects can be used
    without copying to another local store.
    
    Change-Id: I0b454872ad4855e28bd03351c41b6cd02f107204

--- CMake/ITKExternalData.cmake
+++ CMake/ITKExternalData.cmake
@@ -1,13 +1,23 @@
 get_filename_component(_ITKExternalData_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 include(${_ITKExternalData_DIR}/ExternalData.cmake)
+
+set(ExternalData_OBJECT_STORES "" CACHE STRING
+  "Semicolon-separated list of local directories holding data objects in the layout %(algo)/%(hash).")
+mark_as_advanced(ExternalData_OBJECT_STORES)
+if(NOT ExternalData_OBJECT_STORES)
+  set(ExternalData_OBJECT_STORES "${CMAKE_BINARY_DIR}/ExternalData/Objects")
+  file(MAKE_DIRECTORY "${ExternalData_OBJECT_STORES}")
+endif()
+list(APPEND ExternalData_OBJECT_STORES
+  # Local data store populated by the ITK pre-commit hook
+  "${CMAKE_SOURCE_DIR}/.ExternalData"
+  )
+
 set(ExternalData_URL_TEMPLATES "" CACHE STRING
   "Additional URL templates for the ExternalData CMake script to look for testing data. E.g.
 file:///var/bigharddrive/%(algo)/%(hash)")
 mark_as_advanced(ExternalData_URL_TEMPLATES)
 list(APPEND ExternalData_URL_TEMPLATES
-  # Local data store populated by the ITK pre-commit hook
-  "file:///${CMAKE_SOURCE_DIR}/.ExternalData/%(algo)/%(hash)"
-
   # Data published by MIDAS
   "http://midas.kitware.com/api/rest/midas.bitstream.by.hash?hash=%(hash)&algorithm=%(algo)"
 
