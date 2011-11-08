6fed567dcfeb8e71e119253403d2fa680bd3b0a1
Hans Johnson <hans-johnson@uiowa.edu>
2011-11-07 18:27:50 -0600

    COMP:  Intel compiler option settings.
    The default -Wall option on intel compiler was really verbose.
    Additionally, it was necessary to create custom "Check*CompilerFlag.cmake"
    files address the intel compiler error syntax.
    
    Change-Id: I3516f0ba6f53cdc31c091accbea02513fb779d61

--- CMake/ITKSetStandardCompilerFlags.cmake
+++ CMake/ITKSetStandardCompilerFlags.cmake
@@ -19,14 +19,14 @@
 #  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CXX_WARNING_FLAGS}")
 
 
-include(CheckCCompilerFlag)
-include(CheckCXXCompilerFlag)
+include(ITK_CheckCCompilerFlag)
+include(ITK_CheckCXXCompilerFlag)
 
 function(check_c_compiler_warning_flags c_flag_var)
   set(local_c_flags "")
   set(flag_list "${ARGN}")
   foreach(flag IN LISTS flag_list)
-    CHECK_C_COMPILER_FLAG(${flag} C_HAS_WARNING${flag})
+    ITK_CHECK_C_COMPILER_FLAG(${flag} C_HAS_WARNING${flag})
     if(${C_HAS_WARNING${flag}})
       set(local_c_flags "${local_c_flags} ${flag}")
     endif()
@@ -39,7 +39,7 @@ function(check_cxx_compiler_warning_flags cxx_flag_var)
   set(local_cxx_flags "")
   set(flag_list "${ARGN}")
   foreach(flag IN LISTS flag_list)
-    CHECK_CXX_COMPILER_FLAG(${flag} CXX_HAS_WARNING${flag})
+    ITK_CHECK_CXX_COMPILER_FLAG(${flag} CXX_HAS_WARNING${flag})
     if(${CXX_HAS_WARNING${flag}})
       set(local_cxx_flags "${local_cxx_flags} ${flag}")
     endif()
@@ -68,7 +68,30 @@ function(check_compiler_warning_flags c_warning_flags_var cxx_warning_flags_var)
     ## and then disable warnings one by one
     ## set(VerboseWarningsFlag -Wall -wd4820 -wd4682 )
   else()
-    set(VerboseWarningsFlag -Wall )
+    ## with Intel compiler, the -Wall compiler options
+    ## is reporting 1000's of remarks of trivial items
+    ## that will only slow day-to-day operations
+    ## specify -w2 to restrict to only warnings and errors
+    if (${CMAKE_C_COMPILER} MATCHES "icc.*$")
+      set(USING_INTEL_ICC_COMPILER TRUE)
+    endif()
+    if (${CMAKE_CXX_COMPILER} MATCHES "icpc.*$")
+      set(USING_INTEL_ICC_COMPILER TRUE)
+    endif()
+    if(USING_INTEL_ICC_COMPILER)
+      # NOTE -w2 is close to gcc's -Wall warning level, -w5 is intels -Wall warning level, and it is too verbose.
+      set(VerboseWarningsFlag -w2 -wd1268 -wd981 -wd383 -wd1418 -wd1419 -wd2259 -wd1572 -wd424 )
+      #-wd424  #Needed for Intel compilers with remarki  #424: extra ";" ignored
+      #-wd383  #Needed for Intel compilers with remark   #383: value copied to temporary, reference to temporary used
+      #-wd981  #Needed for Intel compilers with remark   #981: operands are evaluated in unspecified order
+      #-wd1418 #Needed for Intel compilers with remark  #1418: external function definition with no prior declaration
+      #-wd1419 #Needed for Intel compilers with remark  #1419: external declaration in primary source file
+      #-wd1572 #Needed for Intel compilers with remark  #1572: floating-point equality and inequality comparisons are unreliable
+      #-wd2259 #Needed for Intel compilers with remark  #2259: non-pointer conversion from "itk::SizeValueType={unsigned long}" to "double" may lose significant bits
+      #-wd1268 #Needed for Intel compliers with warning #1268: support for exported templates is disabled
+    else()
+      set(VerboseWarningsFlag -Wall )
+    endif ()
   endif()
 
   # Check this list on both C and C++ compilers
