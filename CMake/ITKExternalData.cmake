d80e341a589c826018b603fcc02aaceb42b342af
Brad King <brad.king@kitware.com>
2011-11-28 13:08:28 -0500

    ENH: Initialize ExternalData_OBJECT_STORES from environment
    The intended use case for ExternalData_OBJECT_STORES is to share
    persistent object stores among multiple build trees.  Simplify
    configuration of build trees by taking the list of object stores from
    the environment.  Convert the list from the native path format.
    For example, in UNIX-like environments:
    
     ExternalData_OBJECT_STORES=$HOME/.ExternalData:/media/ExternalData
     export ExternalData_OBJECT_STORES
     cmake ../ITK
    
    and in Windows-like environments:
    
     set ExternalData_OBJECT_STORES=%USERPROFILE%\ExternalData;c:\ExternalData
     cmake ..\ITK
    
    Change-Id: I887f821b82f5cf8818828c81235fc714f7533832

--- CMake/ITKExternalData.cmake
+++ CMake/ITKExternalData.cmake
@@ -1,7 +1,15 @@
 get_filename_component(_ITKExternalData_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 include(${_ITKExternalData_DIR}/ExternalData.cmake)
 
-set(ExternalData_OBJECT_STORES "" CACHE STRING
+if(NOT ExternalData_OBJECT_STORES)
+  # Use ExternalData_OBJECT_STORES from environment as default.
+  set(ExternalData_OBJECT_STORES_DEFAULT "")
+  if(DEFINED "ENV{ExternalData_OBJECT_STORES}")
+    file(TO_CMAKE_PATH "$ENV{ExternalData_OBJECT_STORES}" ExternalData_OBJECT_STORES_DEFAULT)
+  endif()
+endif()
+
+set(ExternalData_OBJECT_STORES "${ExternalData_OBJECT_STORES_DEFAULT}" CACHE STRING
   "Semicolon-separated list of local directories holding data objects in the layout %(algo)/%(hash).")
 mark_as_advanced(ExternalData_OBJECT_STORES)
 if(NOT ExternalData_OBJECT_STORES)
