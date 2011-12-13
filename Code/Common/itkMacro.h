aaf1b133e5c79d22e2d7585b2628465d32d54ff7
Sean McBride <sean@rogue-research.com>
2011-12-13 14:51:00 -0500

    COMP: removed invalid use of noreturn attribute
    Change-Id: Ie93b999497693e70affac2441832861b3f553bf1

--- Code/Common/itkMacro.h
+++ Code/Common/itkMacro.h
@@ -961,22 +961,6 @@ private:
 #endif
 
 
-//----------------------------------------------------------------------------
-// Macro to declare that a function does not return. __attribute__((noreturn))
-//    On some compiler, functions that do not return (ex: exit(0)) must
-//    have the noreturn attribute. Otherwise, a warning is raised. Use
-//    that macro to avoid those warnings. GCC defines the attribute
-//    noreturn for versions 2.5 and higher.
-#if defined(__GNUC__)
-#  if (((__GNUC__ == 2) && (__GNUC_MINOR__ >= 5)) || (__GNUC__ >= 3))
-#    define ITK_NO_RETURN \
-       __attribute__ ((noreturn))
-#  endif
-#else
-#  define ITK_NO_RETURN
-#endif
-
-
 #ifdef ITK_USE_TEMPLATE_META_PROGRAMMING_LOOP_UNROLLING
 //--------------------------------------------------------------------------------
 //  Helper macros for Template Meta-Programming techniques of for-loops unrolling
