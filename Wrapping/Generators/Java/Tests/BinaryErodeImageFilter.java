15d62ddf632f00ffbcb9864ea51137a02c4d21df
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2011-12-16 21:26:54 +0100

    COMP: fix java tests
    Change-Id: I04a2332e36b617903891f254abdb43a01703dda7

--- Wrapping/Generators/Java/Tests/BinaryErodeImageFilter.java
+++ Wrapping/Generators/Java/Tests/BinaryErodeImageFilter.java
@@ -24,6 +24,7 @@
 import org.itk.itkcommon.*;
 import org.itk.itkioimagebase.*;
 import org.itk.itkmathematicalmorphology.*;
+import org.itk.itkbinarymathematicalmorphology.*;
 
 public class BinaryErodeImageFilter
 {
