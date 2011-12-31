49a5a943a7c739e2b96c6dafcf2be0ca055e7af8
Hans Johnson <hans-johnson@uiowa.edu>
2011-12-31 11:34:37 -0600

    DOC: Fixed documentation to match code behavior
    This has further documentation and conversation at:
    http://www.itk.org/pipermail/insight-users/2010-February/035297.html
    
    Change-Id: I5850a6d53aae15ac75a88a57c5edc6365c60a5d2

--- Modules/Filtering/AnisotropicSmoothing/include/itkAnisotropicDiffusionFunction.h
+++ Modules/Filtering/AnisotropicSmoothing/include/itkAnisotropicDiffusionFunction.h
@@ -100,10 +100,11 @@ namespace itk
  * information).  Appropriate time steps for solving this type of p.d.e. depend
  * on the dimensionality of the image and the order of the equation.  Typical
  * values are less than 0.250.  A stable value for most 2 and 3d functions is
- * 0.125.  In general, you should keep the time step below 1/2^N, where N is
- * the number of image dimensions.  A filter will automatically attempt to
- * constrain its time step to a stable  value and generate a run-time warning
- * if the time step is set too high.
+ * 0.125.  In general, you should keep the time step below
+ * \f$(PixelSpacing)/2^(N+1)\f$, where \f$N\f$ is * the number of image
+ * dimensions.  A filter will automatically attempt to constrain its time step
+ * to a stable  value and generate a run-time warning if the time step is set
+ * too high.
  *
  * \par Conductance Parameter
  * The conductance parameter controls the sensitivity of the conductance term
