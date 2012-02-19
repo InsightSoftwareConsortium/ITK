913e2164cca521f791aac6d20bce34cba9831e96
Bill Lorensen <bill.lorensen@gmail.com>
2012-02-19 08:21:41 -0500

    BUG: Older version of VTK cannot be used with ITK
    Versions of VTK prior to April 19, 2011 cannot be used with ITKv4
    because of issues with MetaIO. Note that VTK 5.9 contains this error.
    These older version will produce the compile errors:
    MetaObject does not name a type
    MetaObjectType does not name a type
    plus others.
    
    The errors are caused by a change in the way MetaIO configures
    itself. Since VTK commit eb532553, (MetaIO: Avoid requiring parent
    projects to provide "../metaIOConfig.h", 2011-04-06) MetaIO configures
    a header into it's build tree.)
    
    The earliest VTK commit tag that will work is
    be4c4f115793598f6c8937a98bdd1f227d471540
    
    Change-Id: I58506c09b53344a8550df9aed5a9ceda844597e4

--- Modules/Segmentation/LevelSetsv4Visualization/itk-module-init.cmake
+++ Modules/Segmentation/LevelSetsv4Visualization/itk-module-init.cmake
@@ -1,1 +1,5 @@
 find_package(VTK REQUIRED)
+set(VERSION_MIN "5.9.20110419")
+if (${VTK_VERSION} VERSION_LESS ${VERSION_MIN})
+  message(ERROR " LevelSetsv4Visualization requires VTK version ${VERSION_MIN} or newer but the current version is ${VTK_VERSION}")
+endif()
