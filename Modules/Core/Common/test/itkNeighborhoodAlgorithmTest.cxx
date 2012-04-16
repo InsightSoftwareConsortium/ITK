fe3b5696f5289a202329ea26488b299fa4e88222
Kris Zygmunt <krismz@sci.utah.edu>
2012-04-16 16:57:35 -0600

    BUG: BoundaryFacesCalculator returns duplicate regions sometimes
    If the size of one dimension of an image is 1, the boundary faces
    calculator will repeat the final image region twice.  The test has
    been modified to demonstrate this condition.
    
    The patch solved the mentioned issue. In addition is solved
    the case that requestToProcessRegion does not overlaped with
    image bufferedRegion.
    
    The test carries out the below tests:
    1. requestToProcessRegion is the same as bufferedRegion.
    2. requestToProcessRegion is part of the bufferedRegion.
    3. requestToProcessRegion matches the bufferedRegion, but the
    whole region is the boundary. It is the bug aforementioned.
    4. requestToProcessRegion has partial overlap with
    the image bufferedRegion.
    5. requestToProcessRegion is inside the boundary part of
    the bufferedRegion.
    6. enforce the guarantee that a neighborhood iterator constructed with
    the first region and the given radius will not overlap the boundary of
    the image.
    
    Change-Id: If0e53d2bf9ab68641ba0f1a898c8171ccfac0a26

--- Modules/Core/Common/test/itkNeighborhoodAlgorithmTest.cxx
+++ Modules/Core/Common/test/itkNeighborhoodAlgorithmTest.cxx
@@ -18,9 +18,10 @@
 
 
 #include "itkNeighborhoodAlgorithm.h"
+#include "itkConstNeighborhoodIterator.h"
 
 template<class TImage>
-bool ImageBoundaryFaceCalculatorTest(TImage * image, const typename TImage::RegionType & region, const typename TImage::SizeType & radius)
+bool ImageBoundaryFaceCalculatorTest(TImage * image, typename TImage::RegionType & region, const typename TImage::SizeType & radius)
 {
   typedef itk::NeighborhoodAlgorithm::ImageBoundaryFacesCalculator<TImage> FaceCalculatorType;
   typedef typename FaceCalculatorType::FaceListType FaceListType;
@@ -32,11 +33,33 @@ bool ImageBoundaryFaceCalculatorTest(TImage * image, const typename TImage::Regi
     {
     std::cout<<"Number of pixels : "<<fit->GetNumberOfPixels()<<std::endl;
     std::cout<<*fit<<std::endl;
+    if( !region.IsInside( *fit ) && (*fit).GetNumberOfPixels() > 0 )
+      {
+      std::cerr<<"face region is outside of requestToProcessRegion "<<std::endl;
+      return false;
+      }
     }
+
+  if( !faceList.size() )
+    return true;
 
   image->FillBuffer(0);
   for(typename FaceListType::iterator fit = faceList.begin(); fit != faceList.end(); ++fit)
     {
+      if (fit == faceList.begin())
+      {
+        typedef itk::ConstNeighborhoodIterator<TImage> NeighborhoodIteratorType;
+        NeighborhoodIteratorType nIt(radius, image, *fit);
+        // a neighborhood iterator with radius and the first region should never overlap the boundary of the image
+        if (!nIt.InBounds() && fit->GetNumberOfPixels() > 0)
+        {
+          std::cerr << "Error!  Violation of the constraint that a neighborhood iterator of radius "
+                    << radius << " constructed from the first region (\n" << *fit
+                    << ") returned by the boundary face calculator should never overlap the boundary of the image.\n";
+          image->Print(std::cerr);
+          return false;
+        }
+      }
     itk::ImageRegionIterator<TImage> it(image, *fit);
     for(it.GoToBegin(); !it.IsAtEnd(); ++it)
       {
@@ -44,12 +67,15 @@ bool ImageBoundaryFaceCalculatorTest(TImage * image, const typename TImage::Regi
       }
     }
 
+  //to test the case that region is outside of bufferedRegion
+  region.Crop(image->GetBufferedRegion());
+
   itk::ImageRegionIterator<TImage> iter1(image, region);
   for(iter1.GoToBegin(); !iter1.IsAtEnd(); ++iter1)
     {
     if(iter1.Get() != 1)
       {
-      std::cerr<<"pixel at Duplication or empty region found"<<std::endl;
+        std::cerr<<"pixel at Duplication or empty region found, pixel = "<<iter1.Get()<<std::endl;
       return false;
       }
     }
@@ -79,6 +105,7 @@ bool NeighborhoodAlgorithmTest()
   image->SetRegions(region);
   image->Allocate();
 
+  //test 1: requestToProcessRegion match the bufferedRegion
   if ( !ImageBoundaryFaceCalculatorTest( image.GetPointer(), region, radius ))
     return false;
 
@@ -87,8 +114,81 @@ bool NeighborhoodAlgorithmTest()
   region.SetIndex(ind);
   region.SetSize(size);
 
+  //test 2: requestToProcessRegion is part of bufferedRegion
   if ( !ImageBoundaryFaceCalculatorTest( image.GetPointer(), region, radius ))
     return false;
+
+  ind.Fill(0);
+  region.SetIndex(ind);
+
+  size.Fill(5);
+  if (VDimension > 1)
+  {
+    size[VDimension-1] = 1;
+  }
+  region.SetSize(size);
+  image->Initialize();
+  image->SetRegions(region);
+  image->Allocate();
+
+  //test 3: requestToProcessRegion match the bufferedRegion, but all the bufferedRegion is inside the boundary
+  if ( !ImageBoundaryFaceCalculatorTest( image.GetPointer(), region, radius ))
+    return false;
+
+  size.Fill(5);
+  region.SetSize(size);
+  image->Initialize();
+  image->SetRegions(region);
+  image->Allocate();
+
+  ind.Fill(1);
+  size.Fill(6);
+  region.SetIndex(ind);
+  region.SetSize(size);
+
+  //test 4: bufferedRegion is part of the requestToProcessRegion
+  if ( !ImageBoundaryFaceCalculatorTest( image.GetPointer(), region, radius ))
+    return false;
+
+  ind.Fill(0);
+  size.Fill(10);
+  region.SetIndex(ind);
+  region.SetSize(size);
+  image->Initialize();
+  image->SetRegions(region);
+  image->Allocate();
+  radius.Fill(4);
+
+  ind.Fill(1);
+  size.Fill(2);
+  region.SetIndex(ind);
+  region.SetSize(size);
+  //test 5: requestToProcessRegion is part of boundary of bufferedRegion
+  if ( !ImageBoundaryFaceCalculatorTest( image.GetPointer(), region, radius ))
+    return false;
+
+  if (VDimension == 2)
+  {
+    ind[0] = 0;
+    ind[1] = 249;
+    size[0] = 256;
+    size[1] = 7;
+    region.SetIndex(ind);
+    region.SetSize(size);
+    image->Initialize();
+    image->SetRegions(region);
+    image->Allocate();
+
+    ind[1] = 253;
+    size[1] = 3;
+    region.SetIndex(ind);
+    region.SetSize(size);
+    radius.Fill(4);
+    // test 6: test condition encountered by BoxMeanImageFilterTest with 24 threads
+    if (!ImageBoundaryFaceCalculatorTest(image.GetPointer(), region, radius))
+      return false;
+  }
+
 
   return true;
 }
