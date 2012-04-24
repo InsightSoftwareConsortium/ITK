2f0f2b46264ba6d5e6c88adead4e8f8cb1de4ba4
Matt McCormick <matt.mccormick@kitware.com>
2012-04-24 06:16:37 -0400

    ENH:  Add the LesionSizingToolkit as a Remote Module.
    Change-Id: Ie01d3bb3d2dbf984c3c7276967b9e342db608e2e

---
+++ Modules/Remote/LesionSizingToolkit.remote.cmake
@@ -0,0 +1,5 @@
+itk_fetch_module(LesionSizingToolkit
+  "Framework for determining the sizes of lesions in medical images."
+  GIT_REPOSITORY http://public.kitware.com/LesionSizingToolkit.git
+  GIT_TAG 76235ea768b
+  )
