ea87880367b31ab9831041d56917c34cbafe6f93
Jim Miller <millerjv@ge.com>
2012-05-07 14:49:34 -0400

    DOC: correct documentation of Directions
    Main component of this patch is to correct the documentation of the
    Directions.  It is the columns of the Directions that provide the
    direction traversed by each dimension.
    
    Also performed some general cleanup of the documentation.
    
    Also moved the method SetRegions() out of the subclasses of ImageBase
    and put it directly in ImageBase. All the implementations were identical
    with the exception of some variation of const-ness of the parameters.
    
    Change-Id: Id340b4bb66d20c79b4dd825853b4f74649cdd12e

--- Modules/Core/Common/include/itkVectorImage.h
+++ Modules/Core/Common/include/itkVectorImage.h
@@ -201,25 +201,6 @@ public:
    * already be set, e.g. by calling SetRegions(). */
   void Allocate();
 
-  /** Convenience methods to set the LargestPossibleRegion,
-   *  BufferedRegion and RequestedRegion. Allocate must still be called.
-   */
-  void SetRegions(RegionType region)
-  {
-    this->SetLargestPossibleRegion(region);
-    this->SetBufferedRegion(region);
-    this->SetRequestedRegion(region);
-  }
-
-  void SetRegions(SizeType size)
-  {
-    RegionType region; region.SetSize(size);
-
-    this->SetLargestPossibleRegion(region);
-    this->SetBufferedRegion(region);
-    this->SetRequestedRegion(region);
-  }
-
   /** Restore the data object to its initial state. This means releasing
    * memory. */
   virtual void Initialize();
