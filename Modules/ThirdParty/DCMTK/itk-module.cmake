0bf6f58dfb2d51946682bb195e69433679d5cad5
Bill Lorensen <bill.lorensen@gmail.com>
2012-11-24 11:51:15 -0500

    COMP: Remove DCMTK from default modules
    Turn DCMTK off by default until after the 4.3.0 release. DCMTK
    requires cmake 2.8.8. After the 4.3.0 release, we will add it back to
    the default modules.
    
    Change-Id: Ib979461a049e2bf0ca32b07db22decc3203f7e24

--- Modules/ThirdParty/DCMTK/itk-module.cmake
+++ Modules/ThirdParty/DCMTK/itk-module.cmake
@@ -2,12 +2,6 @@ set(DOCUMENTATION "This module builds the DCMTK library <a
 href=\"http://dicom.offis.de/dcmtk.php.en\">DCMTK</a> DICOM
 library suite.")
 
-if(WIN32)
-  set(ITKDCMTK_EXCLUDE_FROM_ALL "EXCLUDE_FROM_ALL")
-else()
-  set(ITKDCMTK_EXCLUDE_FROM_ALL "")
-endif()
-
 itk_module(ITKDCMTK
   DEPENDS
     ITKZLIB
@@ -15,5 +9,5 @@ itk_module(ITKDCMTK
     ITKTIFF
   DESCRIPTION
     "${DOCUMENTATION}"
-  ${ITKDCMTK_EXCLUDE_FROM_ALL}
+  EXCLUDE_FROM_ALL
 )
