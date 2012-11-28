f4bf71192403d7a1df837d78ad4113380777b3f5
Matt McCormick <matt.mccormick@kitware.com>
2012-11-28 22:43:35 +0000

    STYLE: Fix style of CleanQuadEdgeMeshFilter.
    Put method implementations into .hxx file.
    
    Use TInputMesh and TOutputMesh instead of TInput/TOutput.
    
    Do not use itk:: inside namespace itk { }.
    
    Remove m_AbsoluteTolerance2 member in favor of stack variable.  I believe that
    this change also fixes a bug in how the AbsoluteTolerance was used.
    
    Rename variables to ITK convention.
    
    Use itkGetConstMacro and itkSetClampMacro.
    
    Make help objects members and create them in constructor -- may also help
    performance.
    
    Change-Id: I2214ee9f9509b6b3a216754601c4b21c5280a97a

--- Modules/Filtering/QuadEdgeMeshFiltering/include/itkCleanQuadEdgeMeshFilter.h
+++ Modules/Filtering/QuadEdgeMeshFiltering/include/itkCleanQuadEdgeMeshFilter.h
@@ -32,15 +32,16 @@ namespace itk
  * \brief TODO
  * \ingroup ITKQuadEdgeMeshFiltering
  */
-template< class TInput, class TOutput=TInput >
+template< class TInputMesh, class TOutputMesh=TInputMesh >
 class ITK_EXPORT CleanQuadEdgeMeshFilter:
-  public QuadEdgeMeshToQuadEdgeMeshFilter< TInput, TOutput >
+  public QuadEdgeMeshToQuadEdgeMeshFilter< TInputMesh, TOutputMesh >
 {
 public:
   typedef CleanQuadEdgeMeshFilter                             Self;
   typedef SmartPointer< Self >                                Pointer;
   typedef SmartPointer< const Self >                          ConstPointer;
-  typedef QuadEdgeMeshToQuadEdgeMeshFilter< TInput, TOutput > Superclass;
+  typedef QuadEdgeMeshToQuadEdgeMeshFilter< TInputMesh, TOutputMesh >
+                                                              Superclass;
 
   /** Run-time type information (and related methods).   */
   itkTypeMacro(CleanQuadEdgeMeshFilter, QuadEdgeMeshToQuadEdgeMeshFilter);
@@ -48,7 +49,7 @@ public:
   /** New macro for creation of through a Smart Pointer   */
   itkNewMacro(Self);
 
-  typedef TInput                                    InputMeshType;
+  typedef TInputMesh                                InputMeshType;
   typedef typename Superclass::InputMeshPointer     InputMeshPointer;
   typedef typename Superclass::InputCoordRepType    InputCoordRepType;
   typedef typename Superclass::InputPointType       InputPointType;
@@ -71,7 +72,7 @@ public:
 
   itkStaticConstMacro(PointDimension, unsigned int, InputMeshType::PointDimension);
 
-  typedef TOutput                                    OutputMeshType;
+  typedef TOutputMesh                                OutputMeshType;
   typedef typename Superclass::OutputMeshPointer     OutputMeshPointer;
   typedef typename Superclass::OutputCoordRepType    OutputCoordRepType;
   typedef typename Superclass::OutputPointType       OutputPointType;
@@ -99,146 +100,43 @@ public:
                                                    CriterionType >                          DecimationType;
   typedef typename DecimationType::Pointer DecimationPointer;
 
+  /** TODO */
   itkSetMacro(AbsoluteTolerance, InputCoordRepType);
-  itkSetMacro(RelativeTolerance, InputCoordRepType);
+  itkGetConstMacro(AbsoluteTolerance, InputCoordRepType);
+
+  /** TODO */
+  itkSetClampMacro(RelativeTolerance, InputCoordRepType, 0.0, 1.0);
+  itkGetConstMacro(RelativeTolerance, InputCoordRepType);
 
 protected:
-  CleanQuadEdgeMeshFilter()
-  {
-    this->m_AbsoluteTolerance2 = itk::NumericTraits< InputCoordRepType >::Zero;
-    this->m_AbsoluteTolerance  = itk::NumericTraits< InputCoordRepType >::Zero;
-    this->m_RelativeTolerance  = itk::NumericTraits< InputCoordRepType >::Zero;
-  }
+  CleanQuadEdgeMeshFilter();
 
   virtual ~CleanQuadEdgeMeshFilter() {}
 
-  InputCoordRepType m_AbsoluteTolerance2;
+  virtual void GenerateData();
+
+  virtual void MergePoints( const InputCoordRepType absoluteToleranceSquared );
+
+  virtual void CleanPoints();
+
+  void PrintSelf(std::ostream & os, Indent indent) const;
+
+private:
+  CleanQuadEdgeMeshFilter(const Self &); // purposely not implemented
+  void operator=(const Self &); // purposely not implemented
+
   InputCoordRepType m_AbsoluteTolerance;
   InputCoordRepType m_RelativeTolerance;
 
-  void GenerateData()
-  {
-    InputCoordRepType zeroValue = itk::NumericTraits< InputCoordRepType >::Zero;
+  BoundingBoxPointer m_BoundingBox;
+  CriterionPointer   m_Criterion;
+  DecimationPointer  m_Decimation;
 
-    if ( ( m_AbsoluteTolerance == zeroValue ) && ( m_RelativeTolerance != zeroValue ) )
-      {
-      itkAssertOrThrowMacro( ( m_RelativeTolerance > zeroValue ) && ( m_RelativeTolerance < 1. ),
-                             "Relative tolerance out of range" );
-      BoundingBoxPointer bounding_box = BoundingBoxType::New();
-      bounding_box->SetPoints( this->GetInput()->GetPoints() );
-      bounding_box->ComputeBoundingBox();
-
-      m_AbsoluteTolerance2 = m_RelativeTolerance * m_RelativeTolerance
-                             * bounding_box->GetDiagonalLength2();
-      }
-
-    if ( m_AbsoluteTolerance != zeroValue )
-      {
-      m_AbsoluteTolerance2 = m_AbsoluteTolerance * m_AbsoluteTolerance;
-      }
-
-    this->MergePoints();
-    this->CleanPoints();
-  }
-
-  void MergePoints()
-  {
-    OutputMeshPointer output = this->GetOutput();
-
-    CriterionPointer criterion = CriterionType::New();
-
-    criterion->SetTopologicalChange(false);
-    criterion->SetMeasureBound(m_AbsoluteTolerance2);
-
-    DecimationPointer decimate = DecimationType::New();
-    decimate->SetInput( this->GetInput() );
-    decimate->SetCriterion(criterion);
-    decimate->Update();
-
-    InputMeshPointer temp = decimate->GetOutput();
-
-    InputPointsContainerIterator p_it = temp->GetPoints()->Begin();
-    InputPointsContainerIterator p_end = temp->GetPoints()->End();
-
-    OutputPointType pOut;
-
-    while ( p_it != p_end )
-      {
-      pOut.CastFrom( p_it.Value() );
-      output->SetPoint(p_it.Index(), pOut);
-      ++p_it;
-      }
-
-    // Copy Edge Cells
-    InputCellsContainerIterator c_it = temp->GetEdgeCells()->Begin();
-    InputCellsContainerIterator c_end = temp->GetEdgeCells()->End();
-    InputEdgeCellType *         qe;
-    InputQEPrimal *             QEGeom;
-
-    while ( c_it != c_end )
-      {
-      qe = dynamic_cast< InputEdgeCellType * >( c_it.Value() );
-      QEGeom = qe->GetQEGeom();
-      output->AddEdgeWithSecurePointList( QEGeom->GetOrigin(),
-                                          QEGeom->GetDestination() );
-      ++c_it;
-      }
-
-    // Copy cells
-    c_it = temp->GetCells()->Begin();
-    c_end = temp->GetCells()->End();
-    InputPolygonCellType *pe;
-
-    while ( c_it != c_end )
-      {
-      pe = dynamic_cast< InputPolygonCellType * >( c_it.Value() );
-      if ( pe )
-        {
-        InputPointIdList points;
-
-        for ( InputPointsIdInternalIterator pit = pe->InternalPointIdsBegin();
-              pit != pe->InternalPointIdsEnd(); ++pit )
-          {
-          points.push_back( ( *pit ) );
-          }
-        output->AddFaceWithSecurePointList(points);
-        }
-      ++c_it;
-      }
-  }
-
-  void CleanPoints()
-  {
-    OutputMeshPointer output = this->GetOutput();
-
-    OutputPointsContainerIterator p_it = output->GetPoints()->Begin();
-    OutputPointsContainerIterator p_end = output->GetPoints()->End();
-    OutputPointIdentifier         id(0);
-
-    while ( p_it != p_end )
-      {
-      id = p_it->Index();
-      if ( output->FindEdge(id) == 0 )
-        {
-        output->DeletePoint(id);
-        }
-      ++p_it;
-      }
-
-    output->SqueezePointsIds();
-  }
-
-  void PrintSelf(std::ostream & os, Indent indent) const
-  {
-    Superclass::PrintSelf(os, indent);
-    os << indent << "AbsoluteTolerance2: " << m_AbsoluteTolerance2 << std::endl;
-    os << indent << "AbsoluteTolerance: " << m_AbsoluteTolerance << std::endl;
-    os << indent << "RelativeTolerance: " << m_RelativeTolerance << std::endl;
-  }
-
-private:
-  CleanQuadEdgeMeshFilter(const Self &);
-  void operator=(const Self &);
 };
-}
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkCleanQuadEdgeMeshFilter.hxx"
+#endif
+
 #endif
---
+++ Modules/Filtering/QuadEdgeMeshFiltering/include/itkCleanQuadEdgeMeshFilter.hxx
@@ -0,0 +1,167 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef __itkCleanQuadEdgeMeshFilter_hxx
+#define __itkCleanQuadEdgeMeshFilter_hxx
+
+#include "itkCleanQuadEdgeMeshFilter.h"
+
+namespace itk
+{
+
+template< class TInputMesh, class TOutputMesh >
+CleanQuadEdgeMeshFilter< TInputMesh, TOutputMesh >
+::CleanQuadEdgeMeshFilter()
+{
+  this->m_AbsoluteTolerance  = NumericTraits< InputCoordRepType >::Zero;
+  this->m_RelativeTolerance  = NumericTraits< InputCoordRepType >::Zero;
+
+  this->m_BoundingBox = BoundingBoxType::New();
+
+  this->m_Criterion = CriterionType::New();
+  this->m_Criterion->SetTopologicalChange( false );
+
+  this->m_Decimation = DecimationType::New();
+  this->m_Decimation->SetCriterion( this->m_Criterion );
+}
+
+
+template< class TInputMesh, class TOutputMesh >
+void
+CleanQuadEdgeMeshFilter< TInputMesh, TOutputMesh >
+::GenerateData()
+{
+  InputCoordRepType zeroValue = NumericTraits< InputCoordRepType >::Zero;
+
+  InputCoordRepType absoluteToleranceSquared = this->m_AbsoluteTolerance * this->m_AbsoluteTolerance;
+  if ( ( this->m_AbsoluteTolerance == zeroValue ) && ( this->m_RelativeTolerance != zeroValue ) )
+    {
+    this->m_BoundingBox->SetPoints( this->GetInput()->GetPoints() );
+    this->m_BoundingBox->ComputeBoundingBox();
+
+    absoluteToleranceSquared = this->m_RelativeTolerance * this->m_RelativeTolerance
+                           * this->m_BoundingBox->GetDiagonalLength2();
+    }
+
+  this->MergePoints( absoluteToleranceSquared );
+  this->CleanPoints();
+}
+
+
+template< class TInputMesh, class TOutputMesh >
+void
+CleanQuadEdgeMeshFilter< TInputMesh, TOutputMesh >
+::MergePoints( const InputCoordRepType absoluteToleranceSquared )
+{
+  OutputMeshPointer output = this->GetOutput();
+
+  this->m_Criterion->SetMeasureBound( absoluteToleranceSquared );
+
+  this->m_Decimation->SetInput( this->GetInput() );
+  this->m_Decimation->Update();
+
+  InputMeshPointer decimatedMesh = this->m_Decimation->GetOutput();
+
+  InputPointsContainerIterator pointsIt = decimatedMesh->GetPoints()->Begin();
+  InputPointsContainerIterator pointsItEnd = decimatedMesh->GetPoints()->End();
+
+  OutputPointType outputPoint;
+
+  while ( pointsIt != pointsItEnd )
+    {
+    outputPoint.CastFrom( pointsIt.Value() );
+    output->SetPoint(pointsIt.Index(), outputPoint);
+    ++pointsIt;
+    }
+
+  // Copy Edge Cells
+  InputCellsContainerIterator cellIt = decimatedMesh->GetEdgeCells()->Begin();
+  InputCellsContainerIterator cellItEnd = decimatedMesh->GetEdgeCells()->End();
+  InputEdgeCellType *         qeCell;
+  InputQEPrimal *             qeGeometry;
+
+  while ( cellIt != cellItEnd )
+    {
+    qeCell = dynamic_cast< InputEdgeCellType * >( cellIt.Value() );
+    qeGeometry = qeCell->GetQEGeom();
+    output->AddEdgeWithSecurePointList( qeGeometry->GetOrigin(),
+                                        qeGeometry->GetDestination() );
+    ++cellIt;
+    }
+
+  // Copy cells
+  cellIt = decimatedMesh->GetCells()->Begin();
+  cellItEnd = decimatedMesh->GetCells()->End();
+  InputPolygonCellType *polygonCell;
+
+  while ( cellIt != cellItEnd )
+    {
+    polygonCell = dynamic_cast< InputPolygonCellType * >( cellIt.Value() );
+    if ( polygonCell )
+      {
+      InputPointIdList points;
+
+      for ( InputPointsIdInternalIterator pit = polygonCell->InternalPointIdsBegin();
+            pit != polygonCell->InternalPointIdsEnd(); ++pit )
+        {
+        points.push_back( ( *pit ) );
+        }
+      output->AddFaceWithSecurePointList(points);
+      }
+    ++cellIt;
+    }
+}
+
+
+template< class TInputMesh, class TOutputMesh >
+void
+CleanQuadEdgeMeshFilter< TInputMesh, TOutputMesh >
+::CleanPoints()
+{
+  OutputMeshPointer output = this->GetOutput();
+
+  OutputPointsContainerIterator p_it = output->GetPoints()->Begin();
+  OutputPointsContainerIterator p_end = output->GetPoints()->End();
+  OutputPointIdentifier         id(0);
+
+  while ( p_it != p_end )
+    {
+    id = p_it->Index();
+    if ( output->FindEdge(id) == 0 )
+      {
+      output->DeletePoint(id);
+      }
+    ++p_it;
+    }
+
+  output->SqueezePointsIds();
+}
+
+
+template< class TInputMesh, class TOutputMesh >
+void
+CleanQuadEdgeMeshFilter< TInputMesh, TOutputMesh >
+::PrintSelf( std::ostream & os, Indent indent ) const
+{
+  Superclass::PrintSelf(os, indent);
+  os << indent << "AbsoluteTolerance: " << m_AbsoluteTolerance << std::endl;
+  os << indent << "RelativeTolerance: " << m_RelativeTolerance << std::endl;
+}
+
+} // end namespace itk
+
+#endif
