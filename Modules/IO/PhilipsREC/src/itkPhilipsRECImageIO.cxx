InsightSoftwareConsortium/ITK

commit 966fcb62c859229f9b421403c4faf3e6ef5e2b81

COMP: add new gdcm test for SeriesReader + MetaDataDictionary
This addresses bug reported here:

https://itk.icts.uiowa.edu/jira/browse/HISTITK-812

The problem comes up because the ImageSeriesReader re-uses one
instance of the ImageIO to read all files, and the GDCMImageIO uses
its own in-built MetaDataDictionary to record all the DICOM tags. It
was populating the dictionary without first clearing it, so if file #1
had a particular DICOM tag, but file #2 did not, the tag would still
show up in the MetaDataDictionary for file #2.

This is fixed by adding a Clear method to MetaDataDictionary, and
calling it in GDCMImageio::GetImageInformation before populating the
MetaDataDictionary with DICOM tags.

Change-Id: I26c6c16719ceb848ae69f6220fa3c78d524a8316

Modules/IO/PhilipsREC/src/itkPhilipsRECImageIO.cxx
@@ -740,6 +740,8 @@ void PhilipsRECImageIO::ReadImageInformation()
   //Get Dictionary Information
   //Important hk fields.
   MetaDataDictionary & thisDic = this->GetMetaDataDictionary();
+  // Necessary to clear dict if ImageIO object is re-used
+  metaDict->Clear();
   std::string          classname( this->GetNameOfClass() );
   EncapsulateMetaData< std::string >(thisDic, ITK_InputFilterName, classname);
 

Modules/Core/Common/include/itkMetaDataDictionary.h
@@ -110,6 +110,8 @@ public:
   ConstIterator  Find(const std::string & key) const;
 
 #endif
+  /** remove all MetaObjects from dictionary */
+  void Clear();
 
 private:
   MetaDataDictionaryMapType *m_Dictionary;

Modules/Core/Common/src/itkMetaDataDictionary.cxx
@@ -162,4 +162,12 @@ MetaDataDictionary
 {
   return m_Dictionary->find(key);
 }
+
+void
+MetaDataDictionary
+::Clear()
+{
+  this->m_Dictionary->clear();
+}
+
 } // namespace

Modules/IO/GDCM/src/itkGDCMImageIO.cxx
@@ -453,6 +453,10 @@ void GDCMImageIO::InternalReadImageInformation(std::ifstream & file)
   //Now copying the gdcm dictionary to the itk dictionary:
   MetaDataDictionary & dico = this->GetMetaDataDictionary();
 
+  // in the case of re-use by ImageSeriesReader, clear the dictionary
+  // before populating it.
+  dico.Clear();
+
   gdcm::StringFilter sf;
   sf.SetFile(f);
   gdcm::DataSet::ConstIterator it = ds.Begin();

Modules/IO/GDCM/test/CMakeLists.txt
@@ -3,6 +3,7 @@ set(ITKIOGDCMTests
 itkGDCMImageIOTest.cxx
 itkGDCMImageIOTest2.cxx
 itkGDCMSeriesReadImageWrite.cxx
+itkGDCMSeriesMissingDicomTagTest.cxx
 itkGDCMSeriesStreamReadImageWrite.cxx
 itkGDCMImagePositionPatientTest.cxx
 )
@@ -52,3 +53,12 @@ set_property(TEST itkGDCMSeriesStreamReadImageWrite2 APPEND PROPERTY DEPENDS ITK
 itk_add_test(NAME itkGDCMImagePositionPatientTest
       COMMAND ITKIOGDCMTestDriver itkGDCMImagePositionPatientTest
               ${ITK_TEST_OUTPUT_DIR})
+
+ExternalData_expand_arguments(ITKData VAR_IMAGE0075_2 DATA{${ITK_DATA_ROOT}/Input/DicomSeries2/Image0075.dcm})
+ExternalData_expand_arguments(ITKData VAR_IMAGE0076_2 DATA{${ITK_DATA_ROOT}/Input/DicomSeries2/Image0076-missingTag.dcm})
+get_filename_component(DICOMSERIES2_DIR ${VAR_IMAGE0075_2} PATH)
+
+itk_add_test(NAME itkGDCMSeriesMissingDicomTagTest
+  COMMAND ITKIOGDCMTestDriver itkGDCMSeriesMissingDicomTagTest ${DICOMSERIES2_DIR})
+
+set_property(TEST itkGDCMSeriesMissingDicomTagTest APPEND PROPERTY DEPENDS ITKData)

Modules/IO/GDCM/test/itkGDCMSeriesMissingDicomTagTest.cxx
@@ -1,0 +1,110 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+//
+//  This example illustrates how to read a DICOM series into a volume
+//  and then save this volume into another DICOM series using the
+//  exact same name.
+//  It makes use of the GDCM library
+//
+
+#include "itkImageSeriesReader.h"
+#include "itkImageSeriesWriter.h"
+#include "itkRescaleIntensityImageFilter.h"
+#include "itkGDCMImageIO.h"
+#include "itkGDCMSeriesFileNames.h"
+
+int itkGDCMSeriesMissingDicomTagTest( int argc, char* argv[] )
+{
+  if( argc < 2)
+    {
+    std::cerr << "Usage: " << argv[0] <<
+      " DicomDirectory" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  typedef itk::Image<unsigned short,3>            ImageType;
+  typedef itk::ImageSeriesReader< ImageType >     ReaderType;
+  typedef itk::GDCMImageIO                        ImageIOType;
+  typedef itk::GDCMSeriesFileNames                SeriesFileNames;
+
+  ImageIOType::Pointer gdcmIO = ImageIOType::New();
+  SeriesFileNames::Pointer it = SeriesFileNames::New();
+
+  // Get the DICOM filenames from the directory
+  // First add a restriction *before* selecting the input directory
+  // since SetInputDirectory has a side effect of executing
+  gdcm::SerieHelper *sh = it->GetSeriesHelper( );
+  sh->AddRestriction(0x0010, 0x0010, "Wes Turner", gdcm::GDCM_EQUAL);
+  sh->AddRestriction(0x0020, 0x0013, "75", gdcm::GDCM_GREATEROREQUAL);
+  sh->AddRestriction(0x0020, 0x0013, "77", gdcm::GDCM_LESSOREQUAL);
+  it->SetInputDirectory( argv[1] );
+
+  ReaderType::Pointer reader = ReaderType::New();
+
+  const ReaderType::FileNamesContainer & filenames = it->GetInputFileNames();
+  unsigned int numberOfFilenames =  filenames.size();
+  std::cout << numberOfFilenames << std::endl;
+  for(unsigned int fni = 0; fni<numberOfFilenames; fni++)
+    {
+    std::cout << "filename # " << fni << " = ";
+    std::cout << filenames[fni] << std::endl;
+    }
+
+  reader->SetFileNames( filenames );
+  reader->SetImageIO( gdcmIO );
+
+  try
+    {
+    reader->Update();
+    }
+  catch (itk::ExceptionObject &excp)
+    {
+    std::cerr << "Exception thrown while writing the image" << std::endl;
+    std::cerr << excp << std::endl;
+
+    return EXIT_FAILURE;
+    }
+
+  ReaderType::DictionaryArrayRawPointer dictArray =
+    reader->GetMetaDataDictionaryArray();
+  unsigned dictSize = dictArray->size();
+  if(dictSize != 2)
+    {
+    std::cerr << "Expected 2 elements in MetaDataDictionary array, found "
+              << dictArray->size() << std::endl;
+    return EXIT_FAILURE;
+    }
+  // This key artificially removed from the second file in the series.
+  std::string missingKey("0008|0033");
+  std::string val;
+  if(!itk::ExposeMetaData<std::string>( *((*dictArray)[0]) ,missingKey, val ))
+    {
+    std::cerr << "Missing data object " << missingKey
+              << "in first slice" << std::endl;
+    return EXIT_FAILURE;
+    }
+  if(itk::ExposeMetaData<std::string>( *((*dictArray)[1]) ,missingKey, val ))
+    {
+    std::cerr << "DataObject " << missingKey
+              << "found in second slice where it should be missing" << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  return EXIT_SUCCESS;
+}

Modules/IO/HDF5/src/itkHDF5ImageIO.cxx
@@ -745,7 +745,8 @@ HDF5ImageIO
     //
     // read out metadata
     MetaDataDictionary & metaDict = this->GetMetaDataDictionary();
-
+    // Necessary to clear dict if ImageIO object is re-used
+    metaDict.Clear();
     std::string MetaDataGroupName(groupName);
     MetaDataGroupName += MetaDataName;
     MetaDataGroupName += "/";

Modules/IO/NIFTI/src/itkNiftiImageIO.cxx
@@ -767,6 +767,8 @@ void NiftiImageIO::SetImageIOMetadataFromNIfTI()
     {
     // Encapsulate as many header information as possible.
     MetaDataDictionary & thisDic = this->GetMetaDataDictionary();
+    // Necessary to clear dict if ImageIO object is re-used
+    thisDic.Clear();
 
     std::ostringstream dim_info;
     dim_info << header->dim_info;

commit f444637092fa0f5261a5880a6ad97e957d758a1e

COMP: Fix compile error in PhilipsRECImageIO
The recent change 966fcb62c859229f9b421403c4faf3e6ef5e2b81 introduced
a compile error in PhilipsRECImageIO.  This was not detected because
no dashboards have this module enabled.

Change-Id: Ida1ceaa4a8106754244843b3392660d2883eed75

Modules/IO/PhilipsREC/src/itkPhilipsRECImageIO.cxx
@@ -741,7 +741,7 @@ void PhilipsRECImageIO::ReadImageInformation()
   //Important hk fields.
   MetaDataDictionary & thisDic = this->GetMetaDataDictionary();
   // Necessary to clear dict if ImageIO object is re-used
-  metaDict->Clear();
+  thisDic.Clear();
   std::string          classname( this->GetNameOfClass() );
   EncapsulateMetaData< std::string >(thisDic, ITK_InputFilterName, classname);
 