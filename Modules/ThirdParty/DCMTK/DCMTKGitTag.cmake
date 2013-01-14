b9c473e241d46bc79d6750ffae73a112b0dcae7c
Kent Williams <norman-k-williams@uiowa.edu>
2013-01-14 14:00:38 -0600

    COMP: Update DCMTK version to incorporate new fixes.
    In particular there was a fix to allow compiling with CLang
    we needed.  The rest of the new commits are innocuous with
    respect to how ITK uses DCMTK.
    
    WARNING: DCMTK is not turned on by default. Set
    Module_ITKDCMTK:BOOL=ON to test this patch.
    
    Change-Id: I6815f5c8f66563f09a3eaaefd6ac9e3b127a92ab

---
+++ Modules/ThirdParty/DCMTK/DCMTKGitTag.cmake
@@ -0,0 +1,6 @@
+# Set GIT tag here for use in multiple CMakeLists.txt
+#
+# DCMTK tag brought up to 2013-01-11 to get incorporated
+# compiler fixes and bug fixes
+set(DCMTK_GIT_REPOSITORY "http://git.dcmtk.org/dcmtk.git")
+set(DCMTK_GIT_TAG "8f6a406044ec8d521d3ccd19001f254d4545b2fc")
