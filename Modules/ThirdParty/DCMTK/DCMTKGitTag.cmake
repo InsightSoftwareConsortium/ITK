31bc608a1fd8b72388d619484a3137a4d3065e62
Kent Williams <norman-k-williams@uiowa.edu>
2013-02-11 09:26:14 -0600

    BUG: Regressions with respect to building DCMTK
    1. The wrong hash tag was given to check out DCMTK
    from https://github.com/InsightSoftwareConsortium/DCMTK
    
    2. Erroneous conditional inclusion of statements in
    Modules/ThirdParty/DCMTK/itk-module-init.cmake
    
    Change-Id: I313bdf5c1b3dacc81a2d8b2f73dd8a9b5ad3e322

--- Modules/ThirdParty/DCMTK/DCMTKGitTag.cmake
+++ Modules/ThirdParty/DCMTK/DCMTKGitTag.cmake
@@ -3,4 +3,7 @@
 # DCMTK tag brought up to 2013-01-11 to get incorporated
 # compiler fixes and bug fixes
 set(DCMTK_GIT_REPOSITORY "http://github.com/InsightSoftwareConsortium/DCMTK.git")
-set(DCMTK_GIT_TAG "8f6a406044ec8d521d3ccd19001f254d4545b2fc")
+# this is a specific tag.  It corresponds to the symbolic tag
+# 20130129_DCMTK_PATCHES_FOR_ITK which incorporates changes needed to build and deploy
+# with ITK
+set(DCMTK_GIT_TAG "257e800c42f40ff3207a9242fa225c850c56dd07")
