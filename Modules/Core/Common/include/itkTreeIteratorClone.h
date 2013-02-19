a1bfd292c08cd38ae739e2bde432f9ae76be6dbb
Nicolas Rey <nicolasreyv@gmail.com>
2013-02-18 21:17:52 -0600

    STYLE: Minor style fixes
    Removed a commented line in itkGradientVectorFlowImageFilter, fixed a
    typo referrred to referred, and removed an extra indent when printing
    the information on ImageBase class.
    
    Change-Id: Iaa0227584d995125a63868c82e746fca5061caa4

--- Modules/Core/Common/include/itkTreeIteratorClone.h
+++ Modules/Core/Common/include/itkTreeIteratorClone.h
@@ -152,7 +152,7 @@ public:
   }
 
 private:
-  /** The pointer to the object referrred to by this smart pointer. */
+  /** The pointer to the object referred to by this smart pointer. */
   ObjectType *m_Pointer;
 };
 
