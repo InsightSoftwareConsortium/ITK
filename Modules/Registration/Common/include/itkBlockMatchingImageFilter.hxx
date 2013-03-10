8d436a795b74268bfae07d3e8d1af59c9f8fa79e
David Fuentes <fuentesdt@gmail.com>
2013-03-10 12:08:52 -0500

    ENH: control spread of feature selection, different dimesion in block match
    Change-Id: I052ea81ec5b4ed7e7beed36200cf2091ee53c6e1

--- Modules/Registration/Common/include/itkBlockMatchingImageFilter.hxx
+++ Modules/Registration/Common/include/itkBlockMatchingImageFilter.hxx
@@ -257,8 +257,9 @@ BlockMatchingImageFilter< TFixedImage, TMovingImage, TFeatures, TDisplacements, 
   for ( SizeValueType idx = first, last = first + count; idx < last; idx++ )
     {
     FeaturePointsPhysicalCoordinates originalLocation = featurePoints->GetPoint( idx );
-    ImageIndexType index;
-    fixedImage->TransformPhysicalPointToIndex( originalLocation, index );
+    ImageIndexType fixedIndex,floatIndex;
+    fixedImage->TransformPhysicalPointToIndex(    originalLocation, fixedIndex );
+    floatingImage->TransformPhysicalPointToIndex( originalLocation, floatIndex );
 
     // the block is selected for a minimum similarity metric
     SimilaritiesValue  similarity = NumericTraits< SimilaritiesValue >::Zero;
@@ -267,9 +268,9 @@ BlockMatchingImageFilter< TFixedImage, TMovingImage, TFeatures, TDisplacements, 
     DisplacementsVector displacement;
 
     // set centers of window and center regions to current location
-    ImageIndexType start = index - this->m_SearchRadius;
+    ImageIndexType start = fixedIndex - this->m_SearchRadius;
     window.SetIndex( start );
-    center.SetIndex( index );
+    center.SetIndex( floatIndex );
 
     // iterate over neighborhoods in region window, for each neighborhood: iterate over voxels in blockRadius
     ConstNeighborhoodIterator< FixedImageType > windowIterator( m_BlockRadius, fixedImage, window );
