02db47b376de35639f2c2f2a28f2b924ab0739f1
Bradley Lowekamp <blowekamp@mail.nih.gov>
2013-04-17 11:34:53 -0400

    ENH: instantiate vnl types with "long long" on all platforms
    This works toward portable instantiation of images with int64_t
    types.
    
    Change-Id: Id228cbd38794336898b0b0df06cf9524641d01b0

--- Modules/ThirdParty/VNLInstantiation/src/vnl_vector+longlong-.cxx
+++ Modules/ThirdParty/VNLInstantiation/src/vnl_vector+longlong-.cxx
@@ -17,10 +17,8 @@
  *=========================================================================*/
 #include <vxl_version.h>
 #if VXL_VERSION_DATE_FULL < 20120316
-#ifdef _WIN64
 #include "vnl/vnl_vector.txx"
 VNL_VECTOR_INSTANTIATE(long long);
-#endif
 #else
 void vnl_vector_longlong_dummy(void) {}
 #endif
