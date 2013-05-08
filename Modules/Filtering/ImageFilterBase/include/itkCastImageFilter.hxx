22fa5692ed5ff4ea49b51c58cddcece77a1e5f14
Bradley Lowekamp <blowekamp@mail.nih.gov>
2013-05-08 08:24:34 -0400

    PERF: Use ImageAlgorithm::Copy in CastImageFilter
    Overload and "specialize" the ThreadedGenerateData method to use this
    optimized procedure instead of the standard functor
    implementation. This results in a ~5-25X performance increase when
    compared to ITKv4.3.2, when combined with the optimized copy
    method. However, this does remove incremental progress reporting.
    
    Change-Id: I8a2af5c4ec4726967bf6a0cdccaa5f2a4e82d156

---
+++ Modules/Filtering/ImageFilterBase/include/itkCastImageFilter.hxx
@@ -0,0 +1,79 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef __itkCastImageFilter_hxx
+#define __itkCastImageFilter_hxx
+
+#include "itkCastImageFilter.h"
+#include "itkProgressReporter.h"
+#include "itkImageAlgorithm.h"
+
+namespace itk
+{
+
+
+template< class TInputImage, class TOutputImage >
+CastImageFilter< TInputImage, TOutputImage >
+::CastImageFilter()
+{
+  this->SetNumberOfRequiredInputs(1);
+  this->InPlaceOff();
+}
+
+template< class TInputImage, class TOutputImage >
+void
+CastImageFilter< TInputImage, TOutputImage >
+::GenerateData()
+{
+  if ( this->GetInPlace() && this->CanRunInPlace() )
+    {
+    // nothing to do, so avoid iterating over all the pixels
+    // for nothing! Allocate the output, generate a fake progress and exit
+    this->AllocateOutputs();
+    ProgressReporter progress(this, 0, 1);
+    return;
+    }
+  Superclass::GenerateData();
+}
+
+
+template< class TInputImage, class TOutputImage >
+void
+CastImageFilter< TInputImage, TOutputImage >
+::ThreadedGenerateData(const OutputImageRegionType & outputRegionForThread,
+                       ThreadIdType threadId)
+{
+  const TInputImage *inputPtr = this->GetInput();
+  TOutputImage *outputPtr = this->GetOutput(0);
+
+  // Define the portion of the input to walk for this thread, using
+  // the CallCopyOutputRegionToInputRegion method allows for the input
+  // and output images to be different dimensions
+  typename TInputImage::RegionType inputRegionForThread;
+
+  this->CallCopyOutputRegionToInputRegion(inputRegionForThread, outputRegionForThread);
+
+  // when progress is destroyed it'll set progress to 1.0
+  ProgressReporter progress(this, threadId, 1);
+
+  ImageAlgorithm::Copy( inputPtr, outputPtr, inputRegionForThread, outputRegionForThread );
+}
+
+
+} // end namespace itk
+
+#endif
