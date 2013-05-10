d3c9e57ee05fc68126d73869a9324563bbe82d17
Mark Hiner <hiner@wisc.edu>
2013-05-10 12:19:19 +0000

    ENH: Add SCIFIOImageIO.
    This package provides an ITK ImageIO plugin that uses SCIFIO
    to read and write supported life sciences file formats.
    
    The SCIFIO ImageIO plugin was developed by Gaetan Lehmann, Mark Hiner,
    Curtis Rueden, Melissa Linkert and Matt McCormick. Development of this
    module was funded in part by the [FARSIGHT
    project](http://farsight-toolkit.org/), as well as the [Open Microscopy
    Environment](http://openmicroscopy.org/).
    
    Special thanks to Alex Gouaillard, Sebastien Barre, Luis Ibanez and
    Jim Miller for fixes and suggestions.
    
    For further details, including usage instructions, see the [Bio-Formats
    ITK page](http://openmicroscopy.org/site/support/bio-formats/users/itk).
    
    Please [direct any questions to the OME
    team](http://www.openmicroscopy.org/site/community).
    
    To build the module, set
    
      Fetch_SCIFIO to ON in the ITK CMake configuration.
    
    Change-Id: Idafa221438f1faec75f48e7ec04fc7b435e80f24

---
+++ Modules/Remote/SCIFIO.remote.cmake
@@ -0,0 +1,5 @@
+itk_fetch_module(SCIFIO
+  "SCIFIO (Bioformats) ImageIO plugin for ITK"
+  GIT_REPOSITORY https://github.com/scifio/scifio-imageio.git
+  GIT_TAG 29a6ac4b7a8a82c8ec64ffe3baa670ebc5e31b83
+  )
