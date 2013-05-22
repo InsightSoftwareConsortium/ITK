cbbe2d272f3363ac680bccc961d2c38ad2b048de
Mark Hiner <hinerm@gmail.com>
2013-05-22 13:36:40 -0500

    BUG: Wrong classpath separator on Windows
    Updated the SCIFIO remote commit that is checked out. This new commit
    contains logic to select the correct path separator on Windows and
    OSX/Linux.
    
    Previously this was hard-coded to the OSX path separator, which would
    cause the itk-bridge-pipes jar to not be found.
    
    Also includes cquammen's fixes for the ImageInfo test and reading
    newlines on Windows.
    
    Change-Id: I4c4a864443fedbe6439c18e8a18469c8560a9541

--- Modules/Remote/SCIFIO.remote.cmake
+++ Modules/Remote/SCIFIO.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(SCIFIO
   "SCIFIO (Bioformats) ImageIO plugin for ITK"
   GIT_REPOSITORY https://github.com/scifio/scifio-imageio.git
-  GIT_TAG b4fe2d2ba4b5f0116a89cf3705e2900b0de926ee
+  GIT_TAG d096456994e0ac0e0cde7f3356e5fdbfac55c95f
   )
