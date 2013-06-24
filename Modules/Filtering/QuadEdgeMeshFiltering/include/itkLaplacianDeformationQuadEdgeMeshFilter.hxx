6c86b9f18e4b31d0053ad74be77535c8dd6c6439
Arnaud Gelas <arnaudgelas@gmail.com>
2013-06-24 19:07:32 +0200

    ENH: Importing Laplacian Deformation for QuadEdgeMesh
    Code from IJ paper Laplacian Mesh Deformation
    
      http://www.insight-journal.org/browse/publication/890
      http://hdl.handle.net/10380/3410
    
    Change-Id: I170de75843605e51f79f59db04d5f3c6db24f111

---
+++ Modules/Filtering/QuadEdgeMeshFiltering/include/itkLaplacianDeformationQuadEdgeMeshFilter.hxx
@@ -0,0 +1,299 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef __itkLaplacianDeformationQuadEdgeMeshFilter_hxx
+#define __itkLaplacianDeformationQuadEdgeMeshFilter_hxx
+
+#include "itkLaplacianDeformationQuadEdgeMeshFilter.h"
+
+namespace itk
+{
+template< typename TInputMesh, typename TOutputMesh, typename TSolverTraits >
+LaplacianDeformationQuadEdgeMeshFilter< TInputMesh, TOutputMesh, TSolverTraits >
+::LaplacianDeformationQuadEdgeMeshFilter():
+  m_CoefficientsMethod( NULL ), m_Order(1), m_AreaComputationType( NONE )
+{}
+
+template< typename TInputMesh, typename TOutputMesh, typename TSolverTraits >
+void
+LaplacianDeformationQuadEdgeMeshFilter< TInputMesh, TOutputMesh, TSolverTraits >
+::SolveLinearSystems(const MatrixType & iM,
+                     const VectorType & iBx,
+                     const VectorType & iBy,
+                     const VectorType & iBz,
+                     VectorType & oX,
+                     VectorType & oY,
+                     VectorType & oZ)
+{
+  SolverTraits::Solve(iM, iBx, iBy, iBz, oX, oY, oZ);
+}
+
+template< typename TInputMesh, typename TOutputMesh, typename TSolverTraits >
+typename
+LaplacianDeformationQuadEdgeMeshFilter< TInputMesh, TOutputMesh, TSolverTraits >::OutputCoordRepType
+LaplacianDeformationQuadEdgeMeshFilter< TInputMesh, TOutputMesh, TSolverTraits >
+::ComputeMixedAreaForGivenVertex(OutputPointIdentifier iId)
+{
+  OutputMeshType* output = this->GetOutput();
+  OutputQEPrimal *    qe = output->FindEdge(iId);
+  OutputQEPrimal *    qeIt = qe;
+  OutputQEPrimal *    qeIt2 = qe->GetOnext();
+
+  OutputCoordRepType oW = NumericTraits< OutputCoordRepType >::Zero;
+
+  do
+    {
+    oW += this->ComputeMixedArea(qeIt, qeIt2);
+
+    qeIt = qeIt2;
+    qeIt2 = qeIt2->GetOnext();
+    }
+  while ( qe != qeIt );
+
+  return oW;
+}
+
+template< typename TInputMesh, typename TOutputMesh, typename TSolverTraits >
+typename
+LaplacianDeformationQuadEdgeMeshFilter< TInputMesh, TOutputMesh, TSolverTraits >::OutputCoordRepType
+LaplacianDeformationQuadEdgeMeshFilter< TInputMesh, TOutputMesh, TSolverTraits >
+::ComputeMixedArea(OutputQEPrimal *iQE1, OutputQEPrimal *iQE2)
+{
+  if ( iQE1->IsLeftSet() )
+    {
+    OutputMeshType* output = this->GetOutput();
+
+    typename OutputMeshType::PointsContainer* points = output->GetPoints();
+
+    OutputPointIdentifier vId[3];
+
+    vId[0] = iQE1->GetOrigin();
+    vId[1] = iQE1->GetDestination();
+    vId[2] = iQE2->GetDestination();
+
+    OutputPointType p[3];
+
+    for ( int i = 0; i < 3; ++i )
+      {
+      p[i] = points->GetElement( vId[i] );
+      }
+
+    OutputCoordRepType area = TriangleType::ComputeMixedArea(p[0], p[1], p[2]);
+
+    if ( area < vnl_math::eps )
+      {
+      return NumericTraits< OutputCoordRepType >::Zero;
+      }
+    else
+      {
+      return ( 1. / ( 2. * area ) );
+      }
+    }
+  else
+    {
+    return NumericTraits< OutputCoordRepType >::Zero;
+    }
+}
+
+template< typename TInputMesh, typename TOutputMesh, typename TSolverTraits >
+void
+LaplacianDeformationQuadEdgeMeshFilter< TInputMesh, TOutputMesh, TSolverTraits >
+::ComputeVertexIdMapping()
+{
+  OutputMeshType* output   = this->GetOutput();
+
+  typename OutputMeshType::PointsContainer* points = output->GetPoints();
+
+  typename OutputMeshType::PointsContainerIterator pIt = points->Begin();
+  const typename OutputMeshType::PointsContainerIterator pEnd = points->End();
+
+  OutputPointIdentifier k = 0;
+
+  while ( pIt != pEnd )
+    {
+    this->m_InternalMap.insert( typename OutputMapPointIdentifier::value_type(pIt->Index(), k++) );
+    ++pIt;
+    }
+}
+
+template< typename TInputMesh, typename TOutputMesh, typename TSolverTraits >
+void
+LaplacianDeformationQuadEdgeMeshFilter< TInputMesh, TOutputMesh, TSolverTraits >
+::SetConstrainedNode(OutputPointIdentifier vId, const OutputPointType & iP)
+{
+  const InputMeshType*  input = this->GetInput();
+  InputPointType        pOrg = input->GetPoint( vId );
+
+  this->SetDisplacement( vId, iP - pOrg);
+}
+
+template< typename TInputMesh, typename TOutputMesh, typename TSolverTraits >
+void
+LaplacianDeformationQuadEdgeMeshFilter< TInputMesh, TOutputMesh, TSolverTraits >
+::SetDisplacement(OutputPointIdentifier vId, const OutputVectorType & iV)
+{
+  m_Constraints[ vId ] = iV;
+}
+
+template< typename TInputMesh, typename TOutputMesh, typename TSolverTraits >
+bool
+LaplacianDeformationQuadEdgeMeshFilter< TInputMesh, TOutputMesh, TSolverTraits >
+::GetDisplacement(OutputPointIdentifier vId,
+                  OutputVectorType& oV ) const
+{
+  ConstraintMapConstIterator it = m_Constraints.find( vId );
+  const ConstraintMapConstIterator end = m_Constraints.end();
+
+  if( it != end )
+    {
+    oV( it->second );
+    return true;
+    }
+  else
+    {
+    return false;
+    }
+}
+
+template< typename TInputMesh, typename TOutputMesh, typename TSolverTraits >
+void
+LaplacianDeformationQuadEdgeMeshFilter< TInputMesh, TOutputMesh, TSolverTraits >
+::ClearConstraints()
+{
+  if ( !m_Constraints.empty() )
+    {
+    m_Constraints.clear();
+    }
+
+  if ( !m_InternalMap.empty() )
+    {
+    m_InternalMap.clear();
+    }
+}
+
+template< typename TInputMesh, typename TOutputMesh, typename TSolverTraits >
+void
+LaplacianDeformationQuadEdgeMeshFilter< TInputMesh, TOutputMesh, TSolverTraits >
+::FillMatrixRow(OutputPointIdentifier iId,
+                unsigned int iDegree,
+                OutputCoordRepType iWeight,
+                RowType &ioRow)
+{
+  OutputMeshType* output = this->GetOutput();
+
+  std::list< Triple > todo;
+
+  Triple t(iId, iWeight, iDegree);
+  todo.push_back(t);
+
+  OutputPointIdentifier vId;
+  unsigned int          degree;
+  OutputQEPrimal          *qe, *temp;
+
+  while ( !todo.empty() )
+    {
+    t = todo.back();
+    todo.pop_back();
+
+    vId = t.m_Id;
+    degree = t.m_Degree;
+
+    if ( degree == 0 )
+      {
+      RowIterator rIt = ioRow.find(vId);
+
+      if ( rIt == ioRow.end() )
+        {
+        ioRow.insert( std::pair< OutputPointIdentifier, OutputCoordRepType >(vId, t.m_Weight) );
+        }
+      else
+        {
+        rIt->second += t.m_Weight;
+        }
+      }
+    else
+      {
+      OutputCoordRepType ww  = NumericTraits< OutputCoordRepType >::Zero;
+      OutputCoordRepType w   = NumericTraits< OutputCoordRepType >::Zero;
+
+      qe = output->FindEdge(vId);
+      if ( qe )
+        {
+        temp = qe;
+
+        do
+          {
+          CoefficientMapConstIterator coeffIt = m_CoefficientMap.find( temp );
+
+          if( coeffIt != m_CoefficientMap.end() )
+            {
+            w = coeffIt->second;
+            }
+          else
+            {
+            w = ( *this->m_CoefficientsMethod )(output, temp);
+            m_CoefficientMap.insert( typename CoefficientMapType::value_type( temp, w ) );
+            }
+
+          if ( degree < iDegree )
+            {
+            if( m_AreaComputationType != NONE )
+              {
+              AreaMapConstIterator mixedIt = m_MixedAreaMap.find( vId );
+              OutputCoordRepType mixedArea = NumericTraits< OutputCoordRepType >::One;
+
+              if( mixedIt != m_MixedAreaMap.end() )
+                {
+                mixedArea = mixedIt->second;
+                }
+              else
+                {
+                if( m_AreaComputationType == MIXEDAREA )
+                  {
+                  mixedArea = this->ComputeMixedAreaForGivenVertex(vId);
+                  }
+                m_MixedAreaMap.insert( typename AreaMapType::value_type( vId, mixedArea ) );
+                }
+              w *= mixedArea;
+              }
+            }
+
+          w *= t.m_Weight;
+          ww -= w;
+
+          todo.push_back( Triple(temp->GetDestination(), w, degree - 1) );
+
+          temp = temp->GetOnext();
+          }
+        while ( temp != qe );
+
+        todo.push_back( Triple(vId, ww, degree - 1) );
+        }
+      }
+    }
+}
+
+template< typename TInputMesh, typename TOutputMesh, typename TSolverTraits >
+void
+LaplacianDeformationQuadEdgeMeshFilter< TInputMesh, TOutputMesh, TSolverTraits >
+::PrintSelf(std::ostream & os, Indent indent) const
+{
+  Superclass::PrintSelf(os, indent);
+}
+} // end namespace itk
+
+#endif
---
+++ Modules/Filtering/QuadEdgeMeshFiltering/include/itkLaplacianDeformationQuadEdgeMeshFilterWithHardConstraints.hxx
@@ -0,0 +1,187 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef __itkLaplacianDeformationQuadEdgeMeshFilterWithHardConstraints_hxx
+#define __itkLaplacianDeformationQuadEdgeMeshFilterWithHardConstraints_hxx
+
+#include "itkLaplacianDeformationQuadEdgeMeshFilterWithHardConstraints.h"
+
+namespace itk
+{
+template< typename TInputMesh, typename TOutputMesh, typename TSolverTraits >
+LaplacianDeformationQuadEdgeMeshFilterWithHardConstraints< TInputMesh, TOutputMesh, TSolverTraits >
+::LaplacianDeformationQuadEdgeMeshFilterWithHardConstraints()
+{}
+
+template< typename TInputMesh, typename TOutputMesh, typename TSolverTraits >
+void
+LaplacianDeformationQuadEdgeMeshFilterWithHardConstraints< TInputMesh, TOutputMesh, TSolverTraits >
+::ComputeVertexIdMapping()
+{
+  OutputMeshType* output   = this->GetOutput();
+
+  typename OutputMeshType::PointsContainer* points = output->GetPoints();
+
+  typedef typename OutputMeshType::PointsContainerIterator PointsContainerIterator;
+  PointsContainerIterator pIt = points->Begin();
+  const PointsContainerIterator pEnd = points->End();
+
+  OutputPointIdentifier k = 0;
+
+  while ( pIt != pEnd )
+    {
+    const OutputPointIdentifier vId = pIt->Index();
+
+    if ( this->m_Constraints.find(vId) == this->m_Constraints.end() )
+      {
+      this->m_InternalMap.insert( typename OutputMapPointIdentifier::value_type(vId, k++) );
+      }
+    ++pIt;
+    }
+}
+
+template< typename TInputMesh, typename TOutputMesh, typename TSolverTraits >
+void
+LaplacianDeformationQuadEdgeMeshFilterWithHardConstraints< TInputMesh, TOutputMesh, TSolverTraits >
+::FillMatrix(MatrixType & iM, VectorType & iBx, VectorType & iBy, VectorType & iBz)
+{
+  OutputMapPointIdentifierConstIterator it = this->m_InternalMap.begin();
+  const OutputMapPointIdentifierConstIterator end = this->m_InternalMap.end();
+
+  while ( it != end )
+    {
+    const OutputPointIdentifier vId1 = it->first;
+    const unsigned int internalId1 = static_cast< unsigned int >( it->second );
+
+    RowType row;
+    this->FillMatrixRow(vId1, this->m_Order, NumericTraits< OutputCoordRepType >::One, row);
+
+    RowConstIterator rIt = row.begin();
+    const RowConstIterator rEnd = row.end();
+
+    while( rIt != rEnd )
+      {
+      const OutputPointIdentifier vId2 = rIt->first;
+      const OutputCoordRepType weight = rIt->second;
+
+      ConstraintMapConstIterator cIt = this->m_Constraints.find(vId2);
+      if ( cIt != this->m_Constraints.end() )
+        {
+        iBx[internalId1] -= weight * ( cIt->second )[0];
+        iBy[internalId1] -= weight * ( cIt->second )[1];
+        iBz[internalId1] -= weight * ( cIt->second )[2];
+        }
+      else
+        {
+        const unsigned int internalId2 = static_cast< unsigned int >( this->m_InternalMap[vId2] );
+        SolverTraits::AddToMatrix(iM, internalId1, internalId2, weight);
+        }
+      ++rIt;
+      }
+    ++it;
+    }
+}
+
+template< typename TInputMesh, typename TOutputMesh, typename TSolverTraits >
+void
+LaplacianDeformationQuadEdgeMeshFilterWithHardConstraints< TInputMesh, TOutputMesh, TSolverTraits >
+::GenerateData()
+{
+  this->CopyInputMeshToOutputMesh();
+
+  if ( !this->m_Constraints.empty() )
+    {
+    OutputMeshType* output = this->GetOutput();
+
+    this->m_CoefficientMap.clear();
+    this->m_MixedAreaMap.clear();
+
+    this->ComputeVertexIdMapping();
+
+    const unsigned int N = static_cast< unsigned int >( this->m_InternalMap.size() );
+
+    MatrixType M = SolverTraits::InitializeSparseMatrix(N, N);
+
+    VectorType Bx = SolverTraits::InitializeVector(N);
+    Bx.fill(0.);
+
+    VectorType By = SolverTraits::InitializeVector(N);
+    By.fill(0.);
+
+    VectorType Bz = SolverTraits::InitializeVector(N);
+    Bz.fill(0.);
+
+    this->FillMatrix(M, Bx, By, Bz);
+
+    VectorType X = SolverTraits::InitializeVector(N);
+    X.fill(0.);
+
+    VectorType Y = SolverTraits::InitializeVector(N);
+    Y.fill(0.);
+
+    VectorType Z = SolverTraits::InitializeVector(N);
+    Z.fill(0.);
+
+    this->SolveLinearSystems(M, Bx, By, Bz, X, Y, Z);
+
+    typename OutputMeshType::PointsContainer*  points = output->GetPoints();
+
+    OutputMapPointIdentifierConstIterator it = this->m_InternalMap.begin();
+    const OutputMapPointIdentifierConstIterator end = this->m_InternalMap.end();
+
+    while ( it != end )
+      {
+      const OutputPointIdentifier vId = it->first;
+      const unsigned int internalId = static_cast< unsigned int >( it->second );
+
+      OutputPointType& p = points->ElementAt(vId);
+
+      OutputCoordRepType dx = static_cast< OutputCoordRepType >( X[internalId] );
+      p[0] += dx;
+
+      OutputCoordRepType dy = static_cast< OutputCoordRepType >( Y[internalId] );
+      p[1] += dy;
+
+      OutputCoordRepType dz = static_cast< OutputCoordRepType >( Z[internalId] );
+      p[2] += dz;
+
+      ++it;
+      }
+
+    ConstraintMapConstIterator cIt  = this->m_Constraints.begin();
+    const ConstraintMapConstIterator cEnd = this->m_Constraints.end();
+
+    while( cIt != cEnd )
+      {
+      const OutputPointIdentifier vId = cIt->first;
+      OutputPointType& p = points->ElementAt(vId);
+      p += cIt->second;
+      ++cIt;
+      }
+    }
+}
+
+template< typename TInputMesh, typename TOutputMesh, typename TSolverTraits >
+void
+LaplacianDeformationQuadEdgeMeshFilterWithHardConstraints< TInputMesh, TOutputMesh, TSolverTraits >
+::PrintSelf(std::ostream & os, Indent indent) const
+{
+  Superclass::PrintSelf(os, indent);
+}
+} // end namespace itk
+
+#endif
