0aa1d8fa4202eb0987b142d4f0e27edd4f11b880
Dirk Padfield <padfield@ge.com>
2013-07-11 13:15:07 -0400

    ENH: Otsu code replaced with OtsuMultiple call
    Replaced all of the computation code in the GenerateData method of
    OtsuThresholdCalculator with an instantiation of the
    OtsuMultipleThresholdsCalculator with one threshold.  This ensures
    that the two calculators output the same results and promotes code
    reuse.  It also enables further enhancements to the computation to be
    made in one place instead of two.
    
    It was not possible to make Otsu inherit from OtsuMultiple because
    they inherit from very different trees.  This is a separate issue, why
    two calculators that compute the same thing have entirely separate
    inheritance trees, but this is beyond the scope of this patch.
    
    Change-Id: I4f54c4085bbce3c5eddf62624d4254fe9b294708

--- Modules/Filtering/Thresholding/include/itkOtsuThresholdCalculator.hxx
+++ Modules/Filtering/Thresholding/include/itkOtsuThresholdCalculator.hxx
@@ -19,7 +19,6 @@
 #define __itkOtsuThresholdCalculator_hxx
 
 #include "itkOtsuThresholdCalculator.h"
-#include "itkProgressReporter.h"
 #include "vnl/vnl_math.h"
 
 namespace itk
@@ -29,92 +28,14 @@ void
 OtsuThresholdCalculator< THistogram, TOutput >
 ::GenerateData(void)
 {
-  const HistogramType * histogram = this->GetInput();
-
-  if ( histogram->GetTotalFrequency() == 0 )
-    {
-    itkExceptionMacro(<< "Histogram is empty");
-    }
-  ProgressReporter progress(this, 0, histogram->GetSize(0)*2 );
-  if( histogram->GetSize(0) == 1 )
-    {
-    this->GetOutput()->Set( static_cast<OutputType>(histogram->GetMeasurement(0,0)) );
-    }
-
-  SizeValueType size = histogram->GetSize(0);
-
-  // normalize the frequencies
-  std::vector< double > relativeFrequency;
-  relativeFrequency.resize(size);
-  double totalMean = 0.0;
-  for ( SizeValueType j = 0; j < size; j++ )
-    {
-    relativeFrequency[j] = histogram->GetFrequency(j,0);
-    relativeFrequency[j] /= histogram->GetTotalFrequency();
-    totalMean += j * relativeFrequency[j];
-    progress.CompletedPixel();
-    }
-
-  // compute Otsu's threshold by maximizing the between-class
-  // variance
-  double freqLeft = relativeFrequency[0];
-  double meanLeft = 0.0;
-  double meanRight = 0.0;
-
-  if ( freqLeft < 1.0 )
-    {
-    meanRight = ( totalMean - meanLeft * freqLeft )
-                / ( 1.0 - freqLeft );
-    }
-
-  double maxVarBetween = freqLeft * ( 1.0 - freqLeft )
-                         * vnl_math_sqr(meanLeft - meanRight);
-  SizeValueType  maxBinNumber = 0;
-
-  double freqLeftOld = freqLeft;
-  double meanLeftOld = meanLeft;
-
-  for ( SizeValueType j = 1; j < size; j++ )
-    {
-    freqLeft += relativeFrequency[j];
-
-    if ( freqLeft > 0.0 )
-      {
-      meanLeft = ( meanLeftOld * freqLeftOld
-                   + j * relativeFrequency[j] ) / freqLeft;
-      }
-
-    if ( freqLeft >= 1.0 )
-      {
-      meanRight = 0.0;
-      }
-    else
-      {
-      meanRight = ( totalMean - meanLeft * freqLeft )
-                  / ( 1.0 - freqLeft );
-      }
-    double varBetween = freqLeft * ( 1.0 - freqLeft )
-                        * vnl_math_sqr(meanLeft - meanRight);
-
-    // for portability - different compilers seem to produce results
-    // that differ by one bin, presumable because of the precision of
-    // calculating varBetween
-    const double tolerance = 0.00001;
-    if ( (varBetween - tolerance) > maxVarBetween )
-      {
-      maxVarBetween = varBetween;
-      maxBinNumber = j;
-      }
-
-    // cache old values
-    freqLeftOld = freqLeft;
-    meanLeftOld = meanLeft;
-    progress.CompletedPixel();
-    }
-
-  this->GetOutput()->Set( static_cast<OutputType>( histogram->GetMeasurement( maxBinNumber, 0 ) ) );
+  this->UpdateProgress(0.0);
+  // Compute the Otsu threshold using the OtsuMultipleThresholdsCalculator to ensure code reusability.
+  m_OtsuMultipleThresholdsCalculator->SetInputHistogram( this->GetInput() );
+  m_OtsuMultipleThresholdsCalculator->SetNumberOfThresholds( 1 );
+  m_OtsuMultipleThresholdsCalculator->Compute();
+  this->GetOutput()->Set( static_cast<OutputType>( m_OtsuMultipleThresholdsCalculator->GetOutput()[0] ) );
+  this->UpdateProgress(1.0);
 }
-
 } // end namespace itk
 
 #endif
