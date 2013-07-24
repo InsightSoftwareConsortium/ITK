ef8069f20c0648cd45bae6ccd8dee02e081bb1b6
Irina Vidal-Migallon <irina.vidal-migallon@inria.fr>
2013-07-24 18:40:57 +0200

    ENH: Remote module: GPU and CPU Smoothing recursive YVV Gaussian Filter
    GPU and CPU implementations (independent of each other) of Young & Van Vliet's smoothing recursive Gaussian filter.
    Details in InsightJournal submission: http://hdl.handle.net/10380/3425
    
    Change-Id: I89783f19e21d2ca25de7e9cdd36d126223b65c76

---
+++ Modules/Remote/SmoothingRecursiveYvvGaussianFilter.remote.cmake
@@ -0,0 +1,6 @@
+#Contact: I. Vidal-Migallon <irina.vidal-migallon@inria.fr>
+itk_fetch_module(SmoothingRecursiveYvvGaussianFilter
+  "GPU and CPU Young & Van Vliet Recursive Gaussian Smoothing Filter: http://hdl.handle.net/10380/3425"
+  GIT_REPOSITORY https://github.com/Inria-Asclepios/SmoothingRecursiveYvvGaussianFilter
+  GIT_TAG 632750e079cf7572bf9a05bc3b1e006776b0724d
+  )
