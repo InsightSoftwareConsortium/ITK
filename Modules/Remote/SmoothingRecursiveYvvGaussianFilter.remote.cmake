29e8b584a83d79815a37246f83456074c69083a8
Irina Vidal-Migallon <irina.vidal-migallon@inria.fr>
2013-07-25 15:10:19 +0200

    ENH: YVV remote module - Add EXCLUDE_FROM_ALL option to itk_module.
    Change-Id: I439b64fc589f007f1feb8e48fb78d6443ba024fa

--- Modules/Remote/SmoothingRecursiveYvvGaussianFilter.remote.cmake
+++ Modules/Remote/SmoothingRecursiveYvvGaussianFilter.remote.cmake
@@ -2,5 +2,5 @@
 itk_fetch_module(SmoothingRecursiveYvvGaussianFilter
   "GPU and CPU Young & Van Vliet Recursive Gaussian Smoothing Filter: http://hdl.handle.net/10380/3425"
   GIT_REPOSITORY https://github.com/Inria-Asclepios/SmoothingRecursiveYvvGaussianFilter
-  GIT_TAG 632750e079cf7572bf9a05bc3b1e006776b0724d
+  GIT_TAG 28a247fe07765652cdbd51dd9583f4d773fdbb18
   )
