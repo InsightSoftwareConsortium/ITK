cf265e51c7bcd6cf66e375d69567e9a878ecb767
Xiaoxiao Liu <xiaoxiao.liu@kitware.com>
2013-08-15 13:56:30 -0400

    ENH: Turn remote module LSTK off by default.
    In the updated version of LSTK repo,"EXCLUDE_FROM_ALL"
    tag is added to the module dependency.Also the module name
    no longer has ITK prefix in it.
    
    Change-Id: Ia4467a0b2861a7e89e97028fe89d2ca61cb88c57

--- Modules/Remote/LesionSizingToolkit.remote.cmake
+++ Modules/Remote/LesionSizingToolkit.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(LesionSizingToolkit
   "Framework for determining the sizes of lesions in medical images."
   GIT_REPOSITORY http://public.kitware.com/LesionSizingToolkit.git
-  GIT_TAG 60cc50ff7
+  GIT_TAG 962ae6d7a
   )
