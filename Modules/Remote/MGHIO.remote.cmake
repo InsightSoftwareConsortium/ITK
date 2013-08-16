544bbb9035c0655063079fb0a2e1a6ae02a34431
Xiaoxiao Liu <xiaoxiao.liu@kitware.com>
2013-08-16 11:36:01 -0400

    ENH: Remote module names need to be consistent.
    To ensure the usage of "Module_${remote module name}" to enable
    a remote module,  the remote module name appeared
    in the corresponding *.remote.cmake file needs to be consistent
    with the module name defined in each module's itk-module.cmake
    file.
    
    Change-Id: Ia240e54d707d4000f8cd95e795fe6aab8cd681f7

--- Modules/Remote/MGHIO.remote.cmake
+++ Modules/Remote/MGHIO.remote.cmake
@@ -1,4 +1,4 @@
-itk_fetch_module(MGHIO
+itk_fetch_module(ITKIOMGH
   "MGHIO ImageIO plugin for ITK"
   GIT_REPOSITORY https://github.com/Slicer/itkMGHImageIO.git
   GIT_TAG 65a531ba9f5220cbaa05892d9eabb5a2d395364e
--- Modules/Remote/SCIFIO.remote.cmake
+++ Modules/Remote/SCIFIO.remote.cmake
@@ -1,4 +1,4 @@
-itk_fetch_module(SCIFIO
+itk_fetch_module(ITKIOSCIFIO
   "SCIFIO (Bioformats) ImageIO plugin for ITK"
   GIT_REPOSITORY https://github.com/scifio/scifio-imageio.git
   GIT_TAG 9cb93a368e2d431ee5b03e061ed210a07adb98a7
