026fbaa94ff911626b52c678692b10ada23adc08
Brian Helba <brian.helba@kitware.com>
2013-08-21 01:39:49 -0400

    ENH: Refactor KWStyle testing
    All ITK Modules C++ source and test files will now be checked with KWStyle,
    except External, Remote, and ThirdParty. A separate test for Review code
    is no longer needed, and is removed.
    
    Support for GCC-format output of KWStyle is added.
    
    The list of KWStyle error exceptions (ITKOverwrite.txt) is greatly
    reduced in length.
    
    A bug preventing the KWStyle Make target from running is fixed.
    
    Finding the KWStyle program is factored out into an independent CMake
    module.
    
    Several unnecessary files are removed, including a C shell script
    that wasn't portable and only checked individual directories.
    
    Change-Id: I089dbfe3273d32bb980d2c902835e980d741c553

---
+++ CMake/FindKWStyle.cmake
@@ -0,0 +1,47 @@
+# - Finds the KWStyle utility
+# This module looks for KWStyle. If KWStyle is found,
+# the following variables are defined:
+#  KWSTYLE_FOUND - Set if KWStyle is found
+#  KWSTYLE_EXECUTABLE - Path to the KWStyle executable
+#  KWSTYLE_VERSION_STRING - A human-readable string containing the version of KWStyle
+
+#=============================================================================
+# Copyright 2008-2013 Kitware, Inc.
+# Copyright 2013 Brian Helba
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+
+find_program(
+  KWSTYLE_EXECUTABLE
+  NAMES KWStyle
+  DOC "Path to the KWStyle executable"
+  )
+mark_as_advanced(KWSTYLE_EXECUTABLE)
+
+if(KWSTYLE_EXECUTABLE)
+  execute_process(
+    COMMAND ${KWSTYLE_EXECUTABLE} -version
+    OUTPUT_VARIABLE KWSTYLE_VERSION_STRING
+    ERROR_QUIET
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+  string(REPLACE
+    "Version: "
+    ""
+    KWSTYLE_VERSION_STRING
+    ${KWSTYLE_VERSION_STRING}
+    )
+endif(KWSTYLE_EXECUTABLE)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+  KWStyle
+  REQUIRED_VARS KWSTYLE_EXECUTABLE
+  VERSION_VAR KWSTYLE_VERSION_STRING
+  )
