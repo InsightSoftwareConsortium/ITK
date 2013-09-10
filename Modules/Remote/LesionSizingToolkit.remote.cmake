08ebd7bf9a765c0a56e25f2731661275b86ac156
Xiaoxiao Liu <xiaoxiao.liu@kitware.com>
2013-09-10 14:07:10 -0400

    COMP: Fix LSTK warning.
    EXCLUDE_FROM_ALL is replaced by EXCLUDE_FROM_DEFAULT in the latest
    LSTK commit:c03168c0ff69386d7f59b00b46875273852068c7.
    
    Change-Id: I3ff3e6528fc9cbff4b4a30214a28b07ca2d4785e

--- Modules/Remote/LesionSizingToolkit.remote.cmake
+++ Modules/Remote/LesionSizingToolkit.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(LesionSizingToolkit
   "Framework for determining the sizes of lesions in medical images."
   GIT_REPOSITORY http://public.kitware.com/LesionSizingToolkit.git
-  GIT_TAG  4c4a381f8dd302fbc3418d882fe1a78539b605da
+  GIT_TAG  c03168c0ff69386d7f59b00b46875273852068c7
   )
