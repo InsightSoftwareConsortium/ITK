c38b44bf36802491c5d809f8fdba704aa5af7718
Xiaoxiao Liu <xiaoxiao.liu@kitware.com>
2013-09-12 11:26:15 -0400

    COMP: Fix cmake warnings on EXCLUDE_FROM_ALL.
    EXCLUDE_FROM_ALL is replaced by EXCLUDE_FROM_DEFAULT
    in MGHIO remote module:
    1beeaa5abb18188ded17bf6337b31c572d2594cf .
    
    Change-Id: I7e0aa6b4fcec4a631faded0949d425de7cf9d3ba

--- Modules/Remote/MGHIO.remote.cmake
+++ Modules/Remote/MGHIO.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(MGHIO
   "MGHIO ImageIO plugin for ITK"
   GIT_REPOSITORY https://github.com/Slicer/itkMGHImageIO.git
-  GIT_TAG 841832fd94889d0793b606c0a5b9be981b98cec3
+  GIT_TAG 1beeaa5abb18188ded17bf6337b31c572d2594cf
   )
