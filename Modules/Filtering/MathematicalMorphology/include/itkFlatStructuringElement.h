789954b2612050f5f9261506bd9024c6b0317795
Dirk Padfield <padfield@ge.com>
2013-10-23 11:40:39 -0400

    ENH: Added radiusIsParametric  mode to FlatStructuringElement
    This patch introduces an optional flag called
    "radiusIsParametric" to the "ball" and "annulus" structuring
    elements of FlatStructuringElement.  Setting this flag to true will
    generate structuring elements with more accurate areas, which can be
    especially important when morphological operations are intended to
    remove or retain objects of particular sizes.
    
    This mode was introduced because the original ball and annulus
    structuring elements have a systematic bias in the radius of +0.5
    voxels in each dimension relative to the analytic definition of the
    radius.
    
    We recommend using this mode for more accurate structuring elements.
    However, this mode is turned off by default for backward
    compatibility.
    
    Examples:
    For a 3D ball of radius 5 should have an area of 523. With this mode
    turned on, the number of "on" pixels is 515 (error 1.6%), but with it
    turned off, the area is 739 (error 41%).
    
    For a 3D annulus of radius 5 and thickness 2, the area should be 410.
    With this mode turned on, the area is 392 (error 4.5%),  but when
    turned off it is 560 (error 36%).
    
    This same trend holds for balls and annuli of any radius or dimension.
    For more detailed experiments with this mode, please refer to the
    results of the test itkFlatStructuringElementTest.cxx or the wiki
    example.
    
    Change-Id: If7b2f654de396771beb49b6736eae57b28445eed

--- Modules/Filtering/MathematicalMorphology/include/itkFlatStructuringElement.h
+++ Modules/Filtering/MathematicalMorphology/include/itkFlatStructuringElement.h
@@ -38,10 +38,49 @@ namespace itk
  * "Flat" refers to binary as opposed to grayscale structuring elements. Flat
  * structuring elements can be used for both binary and grayscale images.
  *
+ * A Neighborhood has an N-dimensional \em radius.  The radius is defined
+ * separately for each dimension as the number of pixels that the neighborhood
+ * extends outward from the center pixel.  For example, a 2D Neighborhood
+ * object with a radius of 2x3 has sides of length 5x7.
+ * However, in the case of balls and annuli, this definition is slightly
+ * different from the parametric definition of those objects.
+ * For example, an ellipse of radius 2x3 has a diameter of 4x6, not 5x7.
+ * To have a diameter of 5x7, the radius would need to increase by 0.5
+ * in each dimension.
+ * Thus, the "radius" of the neighborhood and the "radius" of the
+ * object should be distinguished.
+ *
+ * To accomplish this, the "ball" and "annulus" structuring elements
+ * have an optional flag called "radiusIsParametric" (off by default).
+ * Setting this flag to true will use the parametric definition of the object
+ * and will generate structuring elements with more accurate areas,
+ * which can be especially important when morphological operations are
+ * intended to remove or retain objects of particular sizes.
+ * When the mode is turned off (default), the radius is the same, but the object
+ * diameter is set to (radius*2)+1, which is the size of the neighborhood region.
+ * Thus, the original ball and annulus structuring
+ * elements have a systematic bias in the radius of +0.5 voxels in each dimension
+ * relative to the parametric definition of the radius.
+ * Thus, we recommend turning this mode on for more accurate structuring elements,
+ * but this mode is turned off by default for backward compatibility.
+ *
+ * As an example, a 3D ball of radius 5 should have an area of 523.
+ * With this mode turned on, the number of "on" pixels is 515 (error 1.6%),
+ * but with it turned off, the area is 739 (error 41%).
+ * For a 3D annulus of radius 5 and thickness 2, the area should be 410.
+ * With this mode turned on, the area is 392 (error 4.5%),
+ * but when turned off it is 560 (error 36%).
+ * This same trend holds for balls and annuli of any radius or dimension.
+ * For more detailed experiments with this mode, please refer to the results of the
+ * test itkFlatStructuringElementTest.cxx or the wiki example.
+ *
  * \ingroup ITKMathematicalMorphology
  *
  * \wiki
  * \wikiexample{Morphology/FlatStructuringElement,Erode a binary image using a flat (box) structuring element}
+ * \endwiki
+ * \wiki
+ * \wikiexample{Morphology/FlatStructuringElementRadiusIsParametric,Generate structuring elements with accurate area}
  * \endwiki
  */
 
@@ -81,8 +120,12 @@ public:
   /** Default destructor. */
   virtual ~FlatStructuringElement() {}
 
-  /** Default consructor. */
-  FlatStructuringElement() { m_Decomposable = false; }
+  /** Default constructor. */
+  FlatStructuringElement()
+  {
+    m_Decomposable = false;
+    m_RadiusIsParametric = false;
+  }
 
   /** Various constructors */
 
@@ -93,7 +136,7 @@ public:
   static Self Box(RadiusType radius);
 
   /** Create a ball structuring element */
-  static Self Ball(RadiusType radius);
+  static Self Ball(RadiusType radius, bool radiusIsParametric = false);
 
   /** Create a cross structuring element */
   static Self Cross(RadiusType radius);
@@ -101,7 +144,8 @@ public:
   /** Create an annulus structuring element */
   static Self Annulus(RadiusType radius,
                       unsigned int thickness = 1,
-                      bool includeCenter = false);
+                      bool includeCenter = false,
+                      bool radiusIsParametric = false);
 
   /**
    * Create a polygon structuring element. The structuring element is
@@ -111,7 +155,7 @@ public:
   static Self Polygon(RadiusType radius, unsigned lines);
 
   /**
-   * Returns wether the structuring element is decomposable or not. If the
+   * Returns whether the structuring element is decomposable or not. If the
    * structuring is decomposable, the set of lines associated with the
    * structuring may be used by an algorithm instead of the standard buffer.
    */
@@ -142,6 +186,22 @@ public:
    */
   void ComputeBufferFromLines();
 
+  /**
+   * The RadiusIsParametric mode ensures that the area of the foreground
+   * corresponds to the radius that was specified.
+   * This defaults to "off" for backward compatibility.
+   */
+  bool GetRadiusIsParametric() const
+  {
+    return m_RadiusIsParametric;
+  }
+  void SetRadiusIsParametric( bool flag )
+  {
+    m_RadiusIsParametric = flag;
+  }
+  itkBooleanMacro(RadiusIsParametric);
+
+
 protected:
 
   void PrintSelf(std::ostream & os, Indent indent) const;
@@ -166,6 +226,8 @@ private:
 
   typedef Vector< float, 3 >           LType3;
   typedef StructuringElementFacet< 3 > FacetType3;
+
+  bool m_RadiusIsParametric;
 
 };
 } // namespace itk
--- Modules/Filtering/MathematicalMorphology/test/itkFlatStructuringElementTest.cxx
+++ Modules/Filtering/MathematicalMorphology/test/itkFlatStructuringElementTest.cxx
@@ -18,18 +18,47 @@
 
 #include "itkFlatStructuringElement.h"
 
+// Helper function
+template< class SEType>
+bool ComputeAreaError(SEType k, unsigned int thickness = 0);
+
 int itkFlatStructuringElementTest(int, char *[])
 {
+  int scalarRadius = 5;
+  int scalarThickness = 2;
+  bool radiusIsParametric = true;
+
   typedef itk::FlatStructuringElement< 2 > SE2Type;
   SE2Type::RadiusType r2;
-  r2.Fill( 3 );
+  r2.Fill( scalarRadius );
   SE2Type k2;
 
   k2 = SE2Type::Box( r2 );
   k2.Print(std::cout);
 
+  std::cout << "2D ball of radius " << scalarRadius
+  << " with radiusIsParametric mode off:" << std::endl;
   k2 = SE2Type::Ball( r2 );
-  k2.Print(std::cout);
+  ComputeAreaError( k2 );
+
+  // Test the radiusIsParametric mode.
+  std::cout << "2D ball of radius " << scalarRadius
+  << " with radiusIsParametric mode on:" << std::endl;
+  k2 = SE2Type::Ball( r2, radiusIsParametric );
+  ComputeAreaError( k2 );
+
+  std::cout << "2D annulus of radius " << scalarRadius
+  << " and thickness " <<  scalarThickness
+  << " with radiusIsParametric mode off:" << std::endl;
+  k2 = SE2Type::Annulus( r2, scalarThickness, false );
+  ComputeAreaError( k2, scalarThickness );
+
+  // Test the radiusIsParametric mode.
+  std::cout << "2D annulus of radius " << scalarRadius
+  << " and thickness " <<  scalarThickness
+  << " with radiusIsParametric mode on:" << std::endl;
+  k2 = SE2Type::Annulus( r2, scalarThickness, false, radiusIsParametric );
+  ComputeAreaError( k2, scalarThickness );
 
   k2 = SE2Type::Polygon( r2, 2 );
   k2.Print(std::cout);
@@ -41,14 +70,35 @@ int itkFlatStructuringElementTest(int, char *[])
 
   typedef itk::FlatStructuringElement< 3 > SE3Type;
   SE3Type::RadiusType r3;
-  r3.Fill( 3 );
+  r3.Fill( scalarRadius );
   SE3Type k3;
 
   k3 = SE3Type::Box( r3 );
-  k3.Print(std::cout);
+  //k3.Print(std::cout);
 
+  std::cout << "3D ball of radius " << scalarRadius
+  << " with radiusIsParametric mode off:" << std::endl;
   k3 = SE3Type::Ball( r3 );
-  k3.Print(std::cout);
+  ComputeAreaError( k3 );
+
+  // Test the radiusIsParametric mode.
+  std::cout << "3D ball of radius " << scalarRadius
+  << " with radiusIsParametric mode on:" << std::endl;
+  k3 = SE3Type::Ball( r3, radiusIsParametric );
+  ComputeAreaError( k3 );
+
+  std::cout << "3D annulus of radius " << scalarRadius
+  << " and thickness " <<  scalarThickness
+  << " with radiusIsParametric mode off:" << std::endl;
+  k3 = SE3Type::Annulus( r3, scalarThickness, false );
+  ComputeAreaError( k3, scalarThickness );
+
+  // Test the radiusIsParametric mode.
+  std::cout << "3D annulus of radius " << scalarRadius
+  << " and thickness " <<  scalarThickness
+  << " with radiusIsParametric mode on:" << std::endl;
+  k3 = SE3Type::Annulus( r3, scalarThickness, false, radiusIsParametric );
+  ComputeAreaError( k3, scalarThickness );
 
   k3 = SE3Type::Polygon( r3, 6 );
   k3.Print(std::cout);
@@ -77,14 +127,35 @@ int itkFlatStructuringElementTest(int, char *[])
 
   typedef itk::FlatStructuringElement< 4 > SE4Type;
   SE4Type::RadiusType r4;
-  r4.Fill( 3 );
+  r4.Fill( scalarRadius );
   SE4Type k4;
 
   k4 = SE4Type::Box( r4 );
-  k4.Print(std::cout);
+  //k4.Print(std::cout);
 
+  std::cout << "4D ball of radius " << scalarRadius
+  << " with radiusIsParametric mode off:" << std::endl;
   k4 = SE4Type::Ball( r4 );
-  k4.Print(std::cout);
+  ComputeAreaError( k4 );
+
+  // Test the radiusIsParametric mode.
+  std::cout << "4D ball of radius " << scalarRadius
+  << " with radiusIsParametric mode on:" << std::endl;
+  k4 = SE4Type::Ball( r4, radiusIsParametric );
+  ComputeAreaError( k4 );
+
+  std::cout << "4D annulus of radius " << scalarRadius
+  << " and thickness " <<  scalarThickness
+  << " with radiusIsParametric mode off:" << std::endl;
+  k4 = SE4Type::Annulus( r4, scalarThickness, false );
+  ComputeAreaError( k4, scalarThickness );
+
+  // Test the radiusIsParametric mode.
+  std::cout << "4D annulus of radius " << scalarRadius
+  << " and thickness " <<  scalarThickness
+  << " with radiusIsParametric mode on:" << std::endl;
+  k4 = SE4Type::Annulus( r4, scalarThickness, false, radiusIsParametric );
+  ComputeAreaError( k4, scalarThickness );
 
   catched = false;
   try
@@ -103,4 +174,79 @@ int itkFlatStructuringElementTest(int, char *[])
     }
 
   return EXIT_SUCCESS;
+}
+
+template< class SEType >
+bool ComputeAreaError(SEType k, unsigned int thickness)
+{
+  float expectedOuterForegroundArea = 1;
+  float expectedInnerForegroundArea;
+  if( thickness == 0 )
+    {
+    // Circle/Ellipse has no inner area to subract.
+    expectedInnerForegroundArea = 0;
+    }
+  else
+    {
+    // Annulus does have inner area to subract.
+    expectedInnerForegroundArea = 1;
+    }
+  if( SEType::NeighborhoodDimension == 2)
+    {
+    expectedOuterForegroundArea *= vnl_math::pi;
+    expectedInnerForegroundArea *= vnl_math::pi;
+    }
+  else if( SEType::NeighborhoodDimension == 3 )
+    {
+    expectedOuterForegroundArea *= 4.0/3.0 * vnl_math::pi;
+    expectedInnerForegroundArea *= 4.0/3.0 * vnl_math::pi;
+    }
+  else if ( SEType::NeighborhoodDimension == 4 )
+    {
+    expectedOuterForegroundArea *= 0.5 * vnl_math::pi * vnl_math::pi;
+    expectedInnerForegroundArea *= 0.5 * vnl_math::pi * vnl_math::pi;
+    }
+  else
+    {
+    return EXIT_FAILURE;
+    }
+  for( unsigned int i = 0; i < SEType::NeighborhoodDimension; i++ )
+    {
+    expectedOuterForegroundArea *= k.GetRadius()[i];
+    expectedInnerForegroundArea *= (k.GetRadius()[i] - thickness);
+    }
+
+  float expectedForegroundArea = expectedOuterForegroundArea - expectedInnerForegroundArea;
+
+  // Show the neighborhood if it is 2D.
+  typename SEType::Iterator SEIt;
+  if( SEType::NeighborhoodDimension == 2 )
+    {
+    for( SEIt = k.Begin(); SEIt != k.End(); ++SEIt )
+      {
+      std::cout << *SEIt << "\t";
+      if( (SEIt - k.Begin()+1) % k.GetSize()[0] == 0 )
+        {
+        std::cout << std::endl;
+        }
+      }
+    }
+
+  // Compute the area/volume.
+  float computedForegroundArea = 0;
+  for( SEIt = k.Begin(); SEIt != k.End(); ++SEIt )
+    {
+    if( *SEIt )
+      {
+      computedForegroundArea++;
+      }
+    }
+
+  std::cout << "Expected foreground area: " << expectedForegroundArea << std::endl;
+  std::cout << "Computed foreground area: " << computedForegroundArea << std::endl;
+  std::cout << "Foreground area error: "
+  << 100 * vnl_math_abs(expectedForegroundArea-computedForegroundArea)/expectedForegroundArea
+  << "%" << "\n\n";
+
+  return EXIT_FAILURE;
 }
