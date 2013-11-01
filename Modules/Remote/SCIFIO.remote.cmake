2a09bea27785598151d8f52687308664b52be4bb
Mark Hiner <hinerm@gmail.com>
2013-11-01 08:59:26 -0500

    BUG: Update loci_tools to 4.4.9
    The scifio-itk-bridge jar was being compiled against the 4.4.9
    Bio-Formats release, but the 4.4.5 loci_tools.jar was being downloaded,
    causing missing classes at runtime.
    
    Updated to the latest scifio-imageio head, which downloads the latest
    4.4.9 loci_tools.

--- Modules/Remote/SCIFIO.remote.cmake
+++ Modules/Remote/SCIFIO.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(SCIFIO
   "SCIFIO (Bioformats) ImageIO plugin for ITK"
   GIT_REPOSITORY https://github.com/scifio/scifio-imageio.git
-  GIT_TAG 67be5138c10769e2d9b09e96e2656ccf1d8c7a79
+  GIT_TAG 3e76055918e70c8e97730d1e321ec59f15007f4c
   )
