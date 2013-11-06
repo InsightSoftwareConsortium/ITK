a53f2447a8accf1f2af4ab06a41b7362801fa6b8
Arnaud Gelas <arnaudgelas@gmail.com>
2013-11-06 15:56:27 +0100

    PERF: fix cppcheck issues in itk::ImageRegion
    [Modules/Core/Common/include/itkImageRegion.h:184]: (performance) Variable 'same' is reassigned a value before the old one has been used.
    [Modules/Core/Common/include/itkImageRegion.h:195]: (performance) Variable 'same' is reassigned a value before the old one has been used.
    
    Change-Id: Ib03a09a37e92216d8a7ed05891eb2a5bd578858d

--- Modules/Core/Common/include/itkImageRegion.h
+++ Modules/Core/Common/include/itkImageRegion.h
@@ -179,9 +179,7 @@ public:
   bool
   operator==(const Self & region) const
   {
-    bool same = 1;
-
-    same = ( m_Index == region.m_Index );
+    bool same = ( m_Index == region.m_Index );
     same = same && ( m_Size == region.m_Size );
     return same;
   }
@@ -190,9 +188,7 @@ public:
   bool
   operator!=(const Self & region) const
   {
-    bool same = 1;
-
-    same = ( m_Index == region.m_Index );
+    bool same = ( m_Index == region.m_Index );
     same = same && ( m_Size == region.m_Size );
     return !same;
   }
