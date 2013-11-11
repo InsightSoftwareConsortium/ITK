bd336450f2cc61ad2ccd83e6a11a5700f87934dd
Matt McCormick <matt.mccormick@kitware.com>
2013-11-11 15:57:17 -0500

    BUG: Incorrect CDash URL in CTestConfig.cmake.
    CDash has moved from www.cdash.org/CDash to open.cdash.org/.
    
    Change-Id: I0b751eda4fa6945104cc0a1d5443a213b0a91755

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -2,6 +2,6 @@ set(CTEST_PROJECT_NAME "ITK")
 set(CTEST_NIGHTLY_START_TIME "1:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "www.cdash.org")
-set(CTEST_DROP_LOCATION "/CDash/submit.php?project=Insight")
+set(CTEST_DROP_SITE "open.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=Insight")
 set(CTEST_DROP_SITE_CDASH TRUE)
