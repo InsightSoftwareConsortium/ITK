07f48b65739b94ad2600ccdf739d94559e71919d
Matt McCormick <matt.mccormick@kitware.com>
2013-11-12 13:11:27 -0500

    BUG: Update CDash URL.
    Change-Id: I5ef11e6a4d6a7fef9cc240419ba44988daa8e0ef

--- itk_common.cmake
+++ itk_common.cmake
@@ -251,8 +251,8 @@ endif()
   # CTestConfig.cmake info here.
   set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
   set(CTEST_DROP_METHOD "http")
-  set(CTEST_DROP_SITE "www.cdash.org")
-  set(CTEST_DROP_LOCATION "/CDash/submit.php?project=Insight")
+  set(CTEST_DROP_SITE "open.cdash.org")
+  set(CTEST_DROP_LOCATION "/submit.php?project=Insight")
   set(CTEST_DROP_SITE_CDASH TRUE)
 endif()
 
