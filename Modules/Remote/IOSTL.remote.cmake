79037828a6a16d94297c85b97ea644e603973ba9
Luis Ibanez <luis.ibanez@kitware.com>
2014-01-18 09:37:56 -0500

    COMP: Fixing instantiation of templated functions.
    To address compilation errors from MacOSX-gcc4.2-Release-IOSTL.
    
    The build in the Mac does not honor the instantiation of templated
    functions based on the type of the  argument. This change added the
    explicit use of the template argument in the name of the function.
    
    Change-Id: I94d09775f6e1d645e1a60746e71522697470f6c4

--- Modules/Remote/IOSTL.remote.cmake
+++ Modules/Remote/IOSTL.remote.cmake
@@ -3,5 +3,5 @@ itk_fetch_module(IOSTL
   "This module contains classes for reading and writing QuadEdgeMeshes using
   the STL (STereoLithography)file format. http://hdl.handle.net/10380/3452"
   GIT_REPOSITORY https://github.com/luisibanez/ITKSTLMeshIO.git
-  GIT_TAG 76f994628acab8f32d0638ac845f7692c5bdc096
+  GIT_TAG dd92020fa5b2c17e32f7b90ef8d285832ada8b0f
   )
