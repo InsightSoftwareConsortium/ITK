fbabe3edb889019c0868559c8bef0e9627079fbb
Brian Helba <brian.helba@kitware.com>
2014-01-23 15:30:58 -0500

    BUG: Fix uninitialized variable in GradientRecursiveGaussianImageFilter
    In GradientRecursiveGaussianImageFilter::TransformOutputPixel, the
    correctedGradient variable is not fully initialized until the outer loop
    completes.
    
    Change-Id: Ia2a70086b83958320f7e159658ada7df98a753b2

--- Modules/Filtering/ImageGradient/include/itkGradientRecursiveGaussianImageFilter.h
+++ Modules/Filtering/ImageGradient/include/itkGradientRecursiveGaussianImageFilter.h
@@ -219,8 +219,8 @@ private:
         DefaultConvertPixelTraits<OutputPixelType>::SetNthComponent( nc*ImageDimension+dim, correctedGradient,
                                                                      correctedComponentGradient[dim] );
         }
-      it.Set(correctedGradient);
       }
+    it.Set(correctedGradient);
   }
 
   template <template<typename, unsigned int> class P, class T, unsigned int N>
