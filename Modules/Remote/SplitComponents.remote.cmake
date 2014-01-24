dbffa3d3ea0b719f017b96af4e4e8f7ae25cfc30
Matt McCormick <matt.mccormick@kitware.com>
2014-01-24 02:22:11 -0500

    ENH: Add SplitComponents Remote Module.
    From:
    
    http://hdl.handle.net/10380/3230
    
    Change-Id: I7589a3cb7141320a50823a2b7576fa956a8f117e

---
+++ Modules/Remote/SplitComponents.remote.cmake
@@ -0,0 +1,9 @@
+# Contact: Matt McCormick <matt.mccormick@kitware.com>
+itk_fetch_module(SplitComponents
+"This module contains filter called
+itk::SplitComponentsImageFilter.  This filter generates component images from an
+itk::Image of, for example, itk::Vector, itk::CovariantVector, or
+itk::SymmetricSecondRankTensor. http://hdl.handle.net/10380/3230"
+  GIT_REPOSITORY https://github.com/thewtex/ITKSplitComponents/
+  GIT_TAG 91d9a5035001a31a5d0c6f76684338648cce8446
+  )
