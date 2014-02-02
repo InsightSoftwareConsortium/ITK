c8573737c12e34c51b532ac3c0a63be6228dc749
Brian Helba <brian.helba@kitware.com>
2014-02-01 22:57:01 -0500

    STYLE: Coverity 40ee44a9: Self assignment in VoronoiSegmentationImageFilterTest
    This should prevent warnings in both GCC and Coverity.
    
    Change-Id: Ic18e7b560fdb0bc7defdfda49476f32bde933771

--- Modules/Segmentation/Voronoi/test/itkVoronoiSegmentationImageFilterTest.cxx
+++ Modules/Segmentation/Voronoi/test/itkVoronoiSegmentationImageFilterTest.cxx
@@ -93,10 +93,10 @@ int itkVoronoiSegmentationImageFilterTest(int, char* [] ){
   k=0;
   while( !ot.IsAtEnd()){
     TestImg[k]=ot.Get();
-    TestImg[k] = TestImg[k];  // dummy line to eliminate warning
     k++;
     ++ot;
   }
+  (void)TestImg; // prevents "set but not used" warning
 
   /* Test Ok on local machine.
   FILE *imgfile = fopen("output.raw","wb");
