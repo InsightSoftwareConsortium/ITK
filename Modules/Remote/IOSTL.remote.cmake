669d43890d16536fb1c0b71cd9652910e59ac136
Brian Helba <brian.helba@kitware.com>
2014-02-01 19:28:40 -0500

    BUG: Update IOSTL to include new bug fixes upstream
    Change-Id: Ibd91ce1bc1873cbf909985e82f149fb67e2507a8

--- Modules/Remote/IOSTL.remote.cmake
+++ Modules/Remote/IOSTL.remote.cmake
@@ -3,5 +3,5 @@ itk_fetch_module(IOSTL
   "This module contains classes for reading and writing QuadEdgeMeshes using
   the STL (STereoLithography)file format. http://hdl.handle.net/10380/3452"
   GIT_REPOSITORY https://github.com/luisibanez/ITKSTLMeshIO.git
-  GIT_TAG dd92020fa5b2c17e32f7b90ef8d285832ada8b0f
+  GIT_TAG ebb492ceeb443b9b41845a5a7d48f3e317a37579
   )
