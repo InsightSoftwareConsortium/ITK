a5fa8d81cf7476ff6a095384478d89f00dcd94bb
Brian Helba <brian.helba@kitware.com>
2014-02-06 16:21:28 -0500

    BUG: Update SmoothingRecursiveYvvGaussianFilter remote module
    This fixes build errors and warnings on MSVC.
    
    Conflicts:
    	Modules/Remote/SmoothingRecursiveYvvGaussianFilter.remote.cmake
    
    Change-Id: I6d4cc2c4b6e911d67a7afb078dedf423acd7b57e

--- Modules/Remote/SmoothingRecursiveYvvGaussianFilter.remote.cmake
+++ Modules/Remote/SmoothingRecursiveYvvGaussianFilter.remote.cmake
@@ -2,5 +2,5 @@
 itk_fetch_module(SmoothingRecursiveYvvGaussianFilter
   "GPU and CPU Young & Van Vliet Recursive Gaussian Smoothing Filter: http://hdl.handle.net/10380/3425"
   GIT_REPOSITORY https://github.com/Inria-Asclepios/SmoothingRecursiveYvvGaussianFilter
-  GIT_TAG d66cc15f97f98a6ecbf6e6ef8a5b52615ffb883e
+  GIT_TAG a5504a0e3849cf9e0e86af92221cae61e5c1c62b
   )
