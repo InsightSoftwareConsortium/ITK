cba0a8d2332a7281e9ece2ce177bb8f0115dc43f
Brian Helba <brian.helba@kitware.com>
2014-02-06 16:38:03 -0500

    BUG: Update SplitComponents remote module
    This fixes a missing EXCLUDE_FROM_DEFAULT parameter in the module config.
    
    Change-Id: Ic28cf0a476efb6d4626f1db13cbb86007613f518

--- Modules/Remote/SplitComponents.remote.cmake
+++ Modules/Remote/SplitComponents.remote.cmake
@@ -5,5 +5,5 @@ itk::SplitComponentsImageFilter.  This filter generates component images from an
 itk::Image of, for example, itk::Vector, itk::CovariantVector, or
 itk::SymmetricSecondRankTensor. http://hdl.handle.net/10380/3230"
   GIT_REPOSITORY https://github.com/thewtex/ITKSplitComponents/
-  GIT_TAG 91d9a5035001a31a5d0c6f76684338648cce8446
+  GIT_TAG 7bede8f7efa83b2e3199fa651462ff657388ae23
   )
