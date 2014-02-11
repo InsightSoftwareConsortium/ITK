6ff7e43632d425a99f9bbf0e8d595fbe20d39c13
Brian Helba <brian.helba@kitware.com>
2014-02-11 16:07:52 -0500

    COMP: Update LesionSizingToolkit remote module from upstream
    This fixes a compiler warning on Visual Studio 9.
    
    Change-Id: I44c387aa5e66b0165130d989c3c56ce132e159c9

--- Modules/Remote/LesionSizingToolkit.remote.cmake
+++ Modules/Remote/LesionSizingToolkit.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(LesionSizingToolkit
   "Framework for determining the sizes of lesions in medical images."
   GIT_REPOSITORY http://public.kitware.com/LesionSizingToolkit.git
-  GIT_TAG  c03168c0ff69386d7f59b00b46875273852068c7
+  GIT_TAG  cb8c368a391199a2cba9d01747a2f96186f6772a
   )
