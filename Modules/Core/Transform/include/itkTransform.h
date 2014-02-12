8b9ca623e7b9f658d07c10336d5c421e16b47b6f
Brian Helba <brian.helba@kitware.com>
2014-02-12 00:18:30 -0500

    STYLE: Coverity 1081570-1081578: Structurally dead code
    Change-Id: I9aa6a9d577a140b6d195b53bbefad6215b6ba7b9

--- Modules/Core/Transform/include/itkTransform.h
+++ Modules/Core/Transform/include/itkTransform.h
@@ -192,7 +192,6 @@ public:
   {
     itkExceptionMacro( "TransformVector(const InputVectorType &)"
                        "is unimplemented for " << this->GetNameOfClass() );
-    return OutputVectorType();
   }
 
   /** Method to transform a vector at a given location.
@@ -208,7 +207,6 @@ public:
   {
     itkExceptionMacro( "TransformVector( const InputVnlVectorType & ) is "
                        "unimplemented for " << this->GetNameOfClass() );
-    return OutputVnlVectorType();
   }
 
   /** Method to transform a vnl_vector, at a point.
@@ -225,7 +223,6 @@ public:
   {
     itkExceptionMacro( "TransformVector( const InputVectorPixelType & ) is "
                        "unimplemented for " << this->GetNameOfClass() );
-    return OutputVectorPixelType();
   }
 
   /** Method to transform a vector stored in a VectorImage, at a point.
@@ -241,7 +238,6 @@ public:
   {
     itkExceptionMacro( "TransformCovariantVector( const InputCovariantVectorType & ) is "
                        "unimplemented for " << this->GetNameOfClass() );
-    return OutputCovariantVectorType();
   }
   /** Method to transform a CovariantVector, using a point. Global transforms
    * can ignore the \c point parameter. Local transforms (e.g. deformation
@@ -259,7 +255,6 @@ public:
   {
     itkExceptionMacro( "TransformCovariantVector(const InputVectorPixelType &)"
                        "is unimplemented for " << this->GetNameOfClass() );
-    return OutputVectorPixelType();
   }
 
   /** Method to transform a CovariantVector, using a point. Global transforms
@@ -279,7 +274,6 @@ public:
     itkExceptionMacro(
       "TransformDiffusionTensor3D( const InputDiffusionTensor3DType & ) is "
       "unimplemented for " << this->GetNameOfClass() );
-    return OutputDiffusionTensor3DType();
   }
 
   /** Method to transform a diffusion tensor at a point. Global transforms
@@ -298,7 +292,6 @@ public:
     itkExceptionMacro(
       "TransformDiffusionTensor( const InputVectorPixelType & ) is "
       "unimplemented for " << this->GetNameOfClass() );
-    return OutputVectorPixelType();
   }
 
   virtual OutputVectorPixelType TransformDiffusionTensor3D(
@@ -321,7 +314,6 @@ public:
     itkExceptionMacro(
       "TransformSymmetricSecondRankTensor( const InputSymmetricSecondRankTensorType & ) is "
       "unimplemented for " << this->GetNameOfClass() );
-    return OutputSymmetricSecondRankTensorType();
   }
 
   /** Method to transform a ssr tensor stored in a VectorImage */
@@ -331,7 +323,6 @@ public:
     itkExceptionMacro(
       "TransformSymmetricSecondRankTensor( const InputVectorPixelType & ) is "
       "unimplemented for " << this->GetNameOfClass() );
-    return OutputVectorPixelType();
   }
 
   /** Method to transform a diffusion tensor stored in a VectorImage, at
