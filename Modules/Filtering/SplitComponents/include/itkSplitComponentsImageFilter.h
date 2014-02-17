7380489e1004f3aa5391615a2c13cd75aaecd16a
Matt McCormick <matt.mccormick@kitware.com>
2014-02-17 11:35:50 -0500

    STYLE: Remove unused ITK_EXPORT.

--- Modules/Filtering/SplitComponents/include/itkSplitComponentsImageFilter.h
+++ Modules/Filtering/SplitComponents/include/itkSplitComponentsImageFilter.h
@@ -45,7 +45,7 @@ namespace itk
  * \sa NthElementImageAdaptor
  */
 template <class TInputImage, class TOutputImage, unsigned int TComponents = TInputImage::ImageDimension>
-class ITK_EXPORT SplitComponentsImageFilter : public ImageToImageFilter<TInputImage, TOutputImage>
+class SplitComponentsImageFilter : public ImageToImageFilter<TInputImage, TOutputImage>
 {
 public:
   /** ImageDimension enumeration. */
