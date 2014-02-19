2bcd704bb0c767707ba9416fe22f88f14f050ef2
Matt McCormick <matt.mccormick@kitware.com>
2014-02-19 10:18:49 -0500

    ENH: Bump SCIFIO to add wrapping.
    This bump to the SCIFIO remote adds wrapping support.
    
    Change-Id: Ib49290d9fa3d6f1cf665134a07b58eb9f5bbc237

--- Modules/Remote/SCIFIO.remote.cmake
+++ Modules/Remote/SCIFIO.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(SCIFIO
   "SCIFIO (Bioformats) ImageIO plugin for ITK"
   GIT_REPOSITORY https://github.com/scifio/scifio-imageio.git
-  GIT_TAG 19f2ca398d9565aef99ed739c14c7de1e3daacfa
+  GIT_TAG 48f72ed9781f0a8dc8e535e3bab704d39147f4c8
   )
