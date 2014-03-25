6d3c08ff43d611759bcd3b6c4c68b373c154db46
Michka Popoff <michkapopoff@gmail.com>
2014-03-25 13:32:00 +0100

    ENH: Bump SCIFIO for OS X installation
    See :
    https://github.com/scifio/scifio-imageio/commit/be0b3ecf40a8aaa817ac3ff25edbe2e5ceb36c6c
    https://github.com/scifio/scifio-imageio/commit/de3ce1e5c5c6a50ea99817265602c6733aa500ae
    
    Change-Id: Idc6e4aa67ff19a103e68f77b75676ce2b9a47f75

--- Modules/Remote/SCIFIO.remote.cmake
+++ Modules/Remote/SCIFIO.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(SCIFIO
   "SCIFIO (Bioformats) ImageIO plugin for ITK"
   GIT_REPOSITORY https://github.com/scifio/scifio-imageio.git
-  GIT_TAG 48f72ed9781f0a8dc8e535e3bab704d39147f4c8
+  GIT_TAG be0b3ecf40a8aaa817ac3ff25edbe2e5ceb36c6c
   )
