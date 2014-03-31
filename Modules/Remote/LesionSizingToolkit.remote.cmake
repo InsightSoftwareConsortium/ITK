f0c0ee30032bb9a76354a0d44351359d034d4cb8
Matt McCormick <matt.mccormick@kitware.com>
2014-03-31 17:10:24 -0400

    COMP: Bump LesionSizingToolkit for Doxygen warnings.
    Change-Id: I89f4704948b2148f10ae0fd86f66cc0d91e3c6ec

--- Modules/Remote/LesionSizingToolkit.remote.cmake
+++ Modules/Remote/LesionSizingToolkit.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(LesionSizingToolkit
   "Framework for determining the sizes of lesions in medical images."
   GIT_REPOSITORY http://public.kitware.com/LesionSizingToolkit.git
-  GIT_TAG  cb8c368a391199a2cba9d01747a2f96186f6772a
+  GIT_TAG fd9cfcef3bb4f7678bc50ecbe6ad51c6998cc3db
   )
