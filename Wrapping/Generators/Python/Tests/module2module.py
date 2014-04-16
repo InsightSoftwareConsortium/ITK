5e229e3e3e3c1ef66558e3da6f078fb02efbf083
Matt McCormick <matt.mccormick@kitware.com>
2014-04-16 02:06:36 -0400

    BUG: Clean up Python module2module test.
    UpdateLargestPossibleRegion() instead of Update() needs to be called to
    avoid pipeline region errors.
    
    binaryDilateFilter/dbinaryDilateFilter need to have different names.
    
    Improve variable names.
    
    Remove commented code.
    
    Use print function.
    
    Change-Id: I6c86a10ff46ab431e9c70dfa355f399340b0265c

--- Wrapping/Generators/Python/Tests/module2module.py
+++ Wrapping/Generators/Python/Tests/module2module.py
@@ -16,10 +16,7 @@
 #
 #==========================================================================*/
 
-# def custom_callback(name):
-#     print "loading %s submodule..." % name
-# import itkConfig
-# itkConfig.ImportCallback = custom_callback
+from __future__ import print_function
 
 import itk
 import sys
@@ -30,122 +27,109 @@ import ITKImageStatistics
 import ITKSmoothing
 import ITKDistanceMap
 import ITKImageIntensity
-import ITKIOImageBase
 import ITKThresholding
 import ITKImageGrid
 
 inputImage = sys.argv[1]
 radiusValue = int(sys.argv[2])
 
-PType = itk.UC
+PixelType = itk.UC
 Dimension = 2
-IType = itk.Image[PType, Dimension]
+ImageType = itk.Image[PixelType, Dimension]
 
 StructuringElementType = itk.FlatStructuringElement[Dimension]
-structuringElement = StructuringElementType.Ball(radiusValue)
+radius = (radiusValue, radiusValue)
+structuringElement = StructuringElementType.Ball(radius)
 
 # create the reader
-reader = itk.ImageFileReader[IType].New(FileName=inputImage)
+reader = itk.ImageFileReader[ImageType].New(FileName=inputImage)
 
 sources = []
-image = ITKCommon.Image[PType, Dimension].New()
-r = itk.ImageRegion._2()
-r.SetSize((10, 10))
-image.SetRegions(r)
+image = ITKCommon.Image[PixelType, Dimension].New()
+region = itk.ImageRegion._2()
+region.SetSize((10, 10))
+image.SetRegions(region)
 image.Allocate()
 
 sources.append(("ITKCommon", image))
 
 sources.append(("ITKIOImageBase", reader.GetOutput()))
 
-otsu = ITKThresholding.OtsuThresholdImageFilter[IType, IType].New(reader)
+OtsuType = ITKThresholding.OtsuThresholdImageFilter[ImageType, ImageType]
+otsu = OtsuType.New(reader)
 sources.append(("ITKThresholding", otsu.GetOutput()))
 
-flip = ITKImageGrid.FlipImageFilter[IType].New(reader)
+flip = ITKImageGrid.FlipImageFilter[ImageType].New(reader)
 sources.append(("ITKImageGrid", flip.GetOutput()))
 
-abs = ITKImageIntensity.AbsImageFilter[IType, IType].New(reader)
-sources.append(("ITKImageIntensity", abs.GetOutput()))
+absFilter = ITKImageIntensity.AbsImageFilter[ImageType, ImageType].New(reader)
+sources.append(("ITKImageIntensity", absFilter.GetOutput()))
+absFilter.InPlaceOff()
 
-binarydilateType = ITKBinaryMathematicalMorphology.BinaryDilateImageFilter[
-    IType, IType, StructuringElementType]
-binarydilateFilter = binarydilateType.New()
-binarydilateFilter.SetInput(reader.GetOutput())
-binarydilateFilter.SetKernel(structuringElement)
+BinaryDilateType = ITKBinaryMathematicalMorphology.BinaryDilateImageFilter[
+    ImageType, ImageType, StructuringElementType]
+binaryDilateFilter = BinaryDilateType.New()
+binaryDilateFilter.SetInput(reader.GetOutput())
+binaryDilateFilter.SetKernel(structuringElement)
 
-output = binarydilateFilter.GetOutput()
+output = binaryDilateFilter.GetOutput()
 sources.append(("ITKBinaryMathematicalMorphology", output))
 
-minmax = ITKImageStatistics.MinimumMaximumImageFilter[IType].New(reader)
+minmax = ITKImageStatistics.MinimumMaximumImageFilter[ImageType].New(reader)
 sources.append(("ITKImageStatistics", minmax.GetOutput()))
 
-median = ITKSmoothing.MedianImageFilter[IType, IType].New(reader)
+median = ITKSmoothing.MedianImageFilter[ImageType, ImageType].New(reader)
 sources.append(("ITKSmoothing", median.GetOutput()))
 
 distance = ITKDistanceMap.DanielssonDistanceMapImageFilter[
-    IType, IType].New(reader)
+    ImageType, ImageType].New(reader)
 sources.append(("ITKDistanceMap", distance.GetOutput()))
-
-# sobel = EdgesAndContours.SobelEdgeDetectionImageFilter[IType, IType].New
-# (reader)
-# sources.append(("EdgesAndContours", sobel.GetOutput()))
-
-# laplacian = Filtering.LaplacianImageFilter[IType, IType].New(reader)
-# sources.append(("Filtering", laplacian.GetOutput()))
 
 
 dests = []
 
-dotsu = ITKThresholding.OtsuThresholdImageFilter[IType, IType].New(reader)
+dotsu = OtsuType.New()
 dests.append(("ITKThresholding", dotsu))
 
-dflip = ITKImageGrid.FlipImageFilter[IType].New()
+dflip = ITKImageGrid.FlipImageFilter[ImageType].New()
 dests.append(("ITKImageGrid", dflip))
 
-dabs = ITKImageIntensity.AbsImageFilter[IType, IType].New()
+dabs = ITKImageIntensity.AbsImageFilter[ImageType, ImageType].New()
 dests.append(("ITKImageIntensity", dabs))
 
-binarydilateType = ITKBinaryMathematicalMorphology.BinaryDilateImageFilter[
-    IType, IType, StructuringElementType]
-binarydilateFilter = binarydilateType.New()
-binarydilateFilter.SetKernel(structuringElement)
-dests.append(("ITKBinaryMathematicalMorphology", binarydilateFilter))
+dbinaryDilateFilter = BinaryDilateType.New()
+dbinaryDilateFilter.SetKernel(structuringElement)
+dests.append(("ITKBinaryMathematicalMorphology", dbinaryDilateFilter))
 
-dminmax = ITKImageStatistics.MinimumMaximumImageFilter[IType].New()
+dminmax = ITKImageStatistics.MinimumMaximumImageFilter[ImageType].New()
 dests.append(("ITKImageStatistics", dminmax))
 
-dmedian = ITKSmoothing.MedianImageFilter[IType, IType].New()
+dmedian = ITKSmoothing.MedianImageFilter[ImageType, ImageType].New()
 dests.append(("ITKSmoothing", dmedian))
 
-ddistance = ITKDistanceMap.DanielssonDistanceMapImageFilter[IType, IType].New()
+DistanceMapType = ITKDistanceMap.DanielssonDistanceMapImageFilter[ImageType,
+                                                                  ImageType]
+ddistance = DistanceMapType.New()
 dests.append(("ITKDistanceMap", ddistance))
-
-# dsobel = EdgesAndContours.SobelEdgeDetectionImageFilter[IType, IType].New()
-# dests.append(("EdgesAndContours", dsobel))
-
-# dlaplacian = Filtering.LaplacianImageFilter[IType, IType].New()
-# dests.append(("Filtering", dlaplacian))
-
-writer = ITKIOImageBase.ImageFileWriter[IType].New(FileName='out.png')
-dests.append(("ITKIOImageBase", writer))
 
 
 nb = 0
 failList = []
-for sname, s in sources:
-    for dname, d in dests:
+for sourceName, source in sources:
+    for destinationName, destination in dests:
         nb += 1
-        d.SetInput(s)
+        destination.SetInput(source)
         try:
-            d.Update()
-            print "%s -> %s pass" % (sname, dname)
+            destination.UpdateLargestPossibleRegion()
+            print("%s -> %s pass" % (sourceName, destinationName))
         except RuntimeError as e:
-            print "%s -> %s fail" % (sname, dname)
-            failList.append((sname, dname))
+            print("%s -> %s fail" % (sourceName, destinationName))
+            print(e)
+            failList.append((sourceName, destinationName))
 
 
-print
-print "%i tests succeed" % (nb - len(failList))
-print "%i tests failed" % len(failList)
+print()
+print("%i tests succeed" % (nb - len(failList)))
+print("%i tests failed" % len(failList))
 
 sys.exit(len(failList))
