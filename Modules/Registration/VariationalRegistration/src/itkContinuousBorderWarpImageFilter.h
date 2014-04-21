d549475d7b793b504991433c29159587d5548ace
Jan <jan@ErnieLinux.(none)>
2014-04-21 16:34:58 +0200

    STYLE: Renamed ContinuousBorderWarpImageFilter.txx to .hxx

--- Modules/Registration/VariationalRegistration/src/itkContinuousBorderWarpImageFilter.h
+++ Modules/Registration/VariationalRegistration/src/itkContinuousBorderWarpImageFilter.h
@@ -96,7 +96,7 @@ private:
 } // end namespace itk
 
 #ifndef ITK_MANUAL_INSTANTIATION
-#  include "itkContinuousBorderWarpImageFilter.txx"
+#  include "itkContinuousBorderWarpImageFilter.hxx"
 #endif
 
 #endif
