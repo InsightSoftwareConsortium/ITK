4d3b91ff6c9e306179b96a6a5cc836425feb8bed
Hans Johnson <hans-johnson@uiowa.edu>
2014-04-22 18:39:40 -0500

    COMP: Update AnalyzeObjectMapIO replacing deprecated
    The Begin() iterators were replaced with GoToBegin() for
    future ITKv4 compliance.
    
    Change-Id: I74a86eeaa721b16775b1ab3bc3d2798e778130fd

--- Modules/Remote/AnalyzeObjectMapIO.remote.cmake
+++ Modules/Remote/AnalyzeObjectMapIO.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(AnalyzeObjectMapIO
   "AnalyzeObjectMapIO plugin for ITK. From Insight Journal article with handle: http://hdl.handle.net/1926/593"
   GIT_REPOSITORY https://github.com/InsightSoftwareConsortium/itkAnalyzeObjectMap.git
-  GIT_TAG 9609e86b99a0a3dd46a62d07297027d3744d0f0a
+  GIT_TAG ea14a5547de995c3f2c7f10948a3df2091de3034
   )
