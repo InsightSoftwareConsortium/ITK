26ac09522c2ac8dabff0e7f516bf8442c8bbf172
Alexander Schmidt-Richberg <a.schmidt-richberg@imperial.ac.uk>
2014-05-09 19:05:08 +0100

    ENH: Added *.remote.cmake for remote module VariationalRegistration
    This version uses bd3ae2f8a37f76dc1700e7369e8edb03c31ff6dd.
    
    Change-Id: I0dd31de20480934229efc019ef95416245286224

---
+++ Modules/Remote/VariationalRegistration.remote.cmake
@@ -0,0 +1,9 @@
+# Insight Journal Handle: http://hdl.handle.net/10380/3460
+# Contact: Alexander Schmidt-Richberg <a.schmidt-richberg@imperial.ac.uk>
+#          Jan Ehrhardt <ehrhardt@imi.uni-luebeck.de>
+#          Rene Werner <r.werner@uke.de>
+itk_fetch_module(VariationalRegistration
+  "A module to perform variational image registration. http://hdl.handle.net/10380/3460"
+  GIT_REPOSITORY https://github.com/VariationalRegistration/VariationalRegistration.git
+  GIT_TAG bd3ae2f8a37f76dc1700e7369e8edb03c31ff6dd
+  )
