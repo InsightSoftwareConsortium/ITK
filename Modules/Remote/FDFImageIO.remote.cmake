cbcd1d8555cd696423d39bd41044f7d9861944a6
Kent Williams <norman-k-williams@uiowa.edu>
2014-05-15 14:27:32 -0500

    ENH: Add FDFImageIO as a remote module.
    This adds a  read-only image IO for FDF format files.
    
    Initial work was done by Glenn Pierce (glennpierce@gmail.com) Code
    brought up to current ITK standards by Nicholas Tustison
    (mtustison@gmail.com) Converted to an ITK Remote module by Kent
    Williams (norman-k-williams@uiowa.edu)
    
    Change-Id: Ib4d0de862fe4dff5351e14991fd28af7762fcd21

---
+++ Modules/Remote/FDFImageIO.remote.cmake
@@ -0,0 +1,5 @@
+itk_fetch_module(FDFImageIO
+  "FDFImageIO plugin for ITK. Authors Gleen Pierce/Nick Tustison/Kent Williams"
+  GIT_REPOSITORY http://github.com/InsightSoftwareConsortium/itkFDFImageIO.git
+  GIT_TAG df4888e4f843c18605da1229c28553933994ddbd
+  )
