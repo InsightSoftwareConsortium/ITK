d048b97c99089331d9d5ab3a212a5f677076e3cf
Ali Ghayoor <ali-ghayoor@uiowa.edu>
2014-05-21 14:08:31 -0500

    ENH: Add OnePlusOneEvolutionaryOptimizerv4 to ITKv4 registration
    OnePlusOne evolutionary optimizer is converted from the ITKv3 to v4 registration
    framework. A test is designed for this newly converted optimizer
    and is passed successfully.
    
    Change-Id: I836c3de67f180320435a8ffa95b1672def44090f

---
+++ Modules/Numerics/Optimizersv4/test/itkOnePlusOneEvolutionaryOptimizerv4Test.cxx
@@ -0,0 +1,272 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#include "itkOnePlusOneEvolutionaryOptimizerv4.h"
+#include "itkNormalVariateGenerator.h"
+#include "itkCommand.h"
+#include "vnl/vnl_math.h"
+
+namespace itk
+{
+
+/**
+ * \class OnePlusOneMetric
+ *
+ *  The objectif function is the quadratic form:
+ *
+ *  1/2 x^T A x - b^T x
+ *
+ *  Where A is a matrix and b is a vector
+ *  The system in this example is:
+ *
+ *     | 3  2 ||x|   | 2|   |0|
+ *     | 2  6 ||y| + |-8| = |0|
+ *
+ *
+ *   the solution is the vector | 2 -2 |
+ *
+ */
+class OnePlusOneMetric : public itk::ObjectToObjectMetricBase
+{
+public:
+
+  typedef OnePlusOneMetric                Self;
+  typedef itk::ObjectToObjectMetricBase   Superclass;
+  typedef itk::SmartPointer<Self>         Pointer;
+  typedef itk::SmartPointer<const Self>   ConstPointer;
+  itkNewMacro( Self );
+
+  enum { SpaceDimension=2 };
+
+  typedef Superclass::ParametersType      ParametersType;
+  typedef Superclass::DerivativeType      DerivativeType;
+  typedef Superclass::MeasureType         MeasureType;
+
+
+  OnePlusOneMetric()
+  {
+    m_HasLocalSupport = false;
+  }
+
+  virtual MeasureType  GetValue() const ITK_OVERRIDE
+  {
+    double x = m_Parameters[0];
+    double y = m_Parameters[1];
+
+    std::cout << "GetValue( ";
+    std::cout << x << " ";
+    std::cout << y << ") = ";
+
+    MeasureType measure = 0.5*(3*x*x+4*x*y+6*y*y) - 2*x + 8*y;
+
+    std::cout << measure << std::endl;
+
+    return measure;
+  }
+
+  virtual void GetDerivative( DerivativeType & ) const ITK_OVERRIDE
+  {
+    itkGenericExceptionMacro("OnePlusOneEvolutionaryOptimizerv4 is not supposed to call GetDerivative()");
+  }
+
+  void GetValueAndDerivative( MeasureType & value,
+                             DerivativeType & derivative ) const ITK_OVERRIDE
+  {
+    value = GetValue();
+    GetDerivative( derivative );
+  }
+
+  virtual void Initialize(void) throw ( itk::ExceptionObject ) ITK_OVERRIDE
+  {
+    m_Parameters.SetSize( SpaceDimension );
+  }
+
+  virtual unsigned int GetNumberOfLocalParameters() const ITK_OVERRIDE
+  {
+    return SpaceDimension;
+  }
+
+  virtual unsigned int GetNumberOfParameters(void) const ITK_OVERRIDE
+  {
+    return SpaceDimension;
+  }
+
+  virtual void SetParameters( ParametersType & parameters ) ITK_OVERRIDE
+  {
+    m_Parameters = parameters;
+  }
+
+  virtual const ParametersType & GetParameters() const ITK_OVERRIDE
+  {
+    return m_Parameters;
+  }
+
+  virtual bool HasLocalSupport() const ITK_OVERRIDE
+  {
+    return m_HasLocalSupport;
+  }
+
+  void SetHasLocalSupport(bool hls)
+  {
+    m_HasLocalSupport = hls;
+  }
+
+  virtual void UpdateTransformParameters( const DerivativeType &, ParametersValueType ) ITK_OVERRIDE
+  {
+  }
+
+private:
+  ParametersType  m_Parameters;
+  bool            m_HasLocalSupport;
+};
+
+
+class OnePlusOneCommandIterationUpdate : public itk::Command
+{
+public:
+  typedef  OnePlusOneCommandIterationUpdate   Self;
+  typedef  itk::Command                       Superclass;
+  typedef itk::SmartPointer<Self>             Pointer;
+  itkNewMacro( Self );
+
+protected:
+  OnePlusOneCommandIterationUpdate() { m_LastMetricValue = 0.0; };
+
+public:
+  typedef itk::OnePlusOneEvolutionaryOptimizerv4<double>    OptimizerType;
+  typedef   const OptimizerType *                           OptimizerPointer;
+
+  virtual void Execute(itk::Object *caller, const itk::EventObject & event) ITK_OVERRIDE
+    {
+    Execute( (const itk::Object *)caller, event);
+    }
+
+  virtual void Execute(const itk::Object * object, const itk::EventObject & event) ITK_OVERRIDE
+    {
+      OptimizerPointer optimizer =
+        dynamic_cast< OptimizerPointer >( object );
+      if( ! itk::IterationEvent().CheckEvent( &event ) )
+        {
+        return;
+        }
+      double currentValue = optimizer->GetValue();
+      // Only print out when the Metric value changes
+      if( std::fabs( m_LastMetricValue - currentValue ) > 1e-7 )
+        {
+        std::cout << optimizer->GetCurrentIteration() << "   ";
+        std::cout << currentValue << "   ";
+        std::cout << optimizer->GetCurrentPosition() << std::endl;
+        m_LastMetricValue = currentValue;
+        }
+    }
+
+private:
+  double m_LastMetricValue;
+};
+}
+
+int itkOnePlusOneEvolutionaryOptimizerv4Test(int, char* [] )
+{
+  std::cout << "OnePlusOne Evolutionary Optimizer Test ";
+  std::cout << std::endl << std::endl;
+
+  typedef  itk::OnePlusOneEvolutionaryOptimizerv4<double>  OptimizerType;
+
+  typedef OptimizerType::ScalesType        ScalesType;
+
+  // Declaration of a itkOptimizer
+  OptimizerType::Pointer  itkOptimizer = OptimizerType::New();
+
+  itk::OnePlusOneCommandIterationUpdate::Pointer observer = itk::OnePlusOneCommandIterationUpdate::New();
+  itkOptimizer->AddObserver( itk::IterationEvent(), observer );
+
+  // Declaration of the CostFunction
+  itk::OnePlusOneMetric::Pointer metric = itk::OnePlusOneMetric::New();
+  itkOptimizer->SetMetric( metric.GetPointer() );
+
+  typedef itk::OnePlusOneMetric::ParametersType    ParametersType;
+
+  const unsigned int spaceDimension = metric->GetNumberOfParameters();
+
+  // We start not so far from  | 2 -2 |
+  ParametersType  initialPosition( spaceDimension );
+
+  initialPosition[0] =  100;
+  initialPosition[1] = -100;
+
+  itkOptimizer->Initialize( 10 );
+  itkOptimizer->SetEpsilon( 0.1 );
+  itkOptimizer->SetMaximumIteration( 8000 );
+
+  typedef itk::Statistics::NormalVariateGenerator  GeneratorType;
+  GeneratorType::Pointer generator = GeneratorType::New();
+  itkOptimizer->SetNormalVariateGenerator( generator );
+
+  // Set the initial position by setting the metric
+  // parameters.
+  std::cout << "Set metric parameters." << std::endl;
+  metric->SetParameters( initialPosition );
+
+  try
+    {
+    itkOptimizer->StartOptimization();
+    }
+  catch( itk::ExceptionObject & e )
+    {
+    std::cout << "Exception thrown ! " << std::endl;
+    std::cout << "An error occurred during Optimization" << std::endl;
+    std::cout << "Location    = " << e.GetLocation()    << std::endl;
+    std::cout << "Description = " << e.GetDescription() << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  ParametersType finalPosition = itkOptimizer->GetCurrentPosition();
+  std::cout << "Solution        = (";
+  std::cout << finalPosition[0] << ",";
+  std::cout << finalPosition[1] << ")" << std::endl;
+
+  //
+  // check results to see if it is within range
+  //
+  bool pass = true;
+  double trueParameters[2] = { 2, -2 };
+  for( unsigned int j = 0; j < 2; j++ )
+    {
+    if( vnl_math_abs( finalPosition[j] - trueParameters[j] ) > 0.01 )
+      {
+      pass = false;
+      }
+    }
+
+  // Exercise various member functions.
+  std::cout << "Epsilon: " << itkOptimizer->GetEpsilon() << std::endl;
+  std::cout << "NumberOfIterations: " << itkOptimizer->GetMaximumIteration() << std::endl;
+
+  itkOptimizer->Print( std::cout );
+  std::cout << "Stop description   = " << itkOptimizer->GetStopConditionDescription() << std::endl;
+
+  if( !pass )
+    {
+    std::cout << "Test failed." << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  std::cout << "Test passed." << std::endl;
+  return EXIT_SUCCESS;
+
+}
