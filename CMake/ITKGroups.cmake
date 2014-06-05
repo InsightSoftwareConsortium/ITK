6973ffdc8a2992eaaf3d8eb199befb57d8e252ff
Matt McCormick <matt.mccormick@kitware.com>
2014-06-05 16:17:03 -0400

    BUG: Exclude Remote Modules the default ON Group values.
    When a Remote Module is explicitly enabled, with
    
    -DModule_<remote-name>:BOOL=ON
    
    it will initially download the sources into the ITK source tree.
    
    If a fresh build has been created, and the module was previously enabled,
    it will be automatically enabled. In this patch, we ensure that all
    Remote Modules are OFF during initial CMake configuration to generate a
    consistent, predictable user experience.
    
    We also fix the indexing of modules for Doxygen. Bridge modules are now
    visible in Doxygen, and Remote Modules, that do not have "ITK" in their
    module
    name, are added to the appropriate Doxygen Group.
    
    Change-Id: I38cecaec345fb7f17d1812682c5e69934563d3d6

--- CMake/ITKGroups.cmake
+++ CMake/ITKGroups.cmake
@@ -67,22 +67,23 @@ The modules are OFF by default in ITK's CMake configuration.
 Note that these modules do get the same level of support and backwards
 compatibility as other modules in the toolkit.")
 
-# Set a module name list for each group
+#------------------------------------------------
+# Find the modules in each group and the module name line in itk-module.cmake
 foreach( group ${group_list} )
-  set( ${group}_module_list )
-  file( GLOB_RECURSE _module_files ${ITK_SOURCE_DIR}/Modules/${group}/itk-module.cmake )
-  foreach( _module_f ${_module_files} )
-    get_filename_component (module_file_dir ${_module_f} PATH)
-    if (NOT EXISTS "${module_file_dir}/itk-module-init.cmake")
-      file( STRINGS ${_module_f} _module_line REGEX "itk_module[ \n]*\\([ \n]*ITK[A-Za-z0-9]*" )
-      string( REGEX MATCH "ITK[A-Za-z0-9]*" _module_name ${_module_line} )
-      list( APPEND ${group}_module_list ${_module_name} )
-    endif()
+  set( _${group}_module_list )
+  file( GLOB_RECURSE _${group}_module_files ${ITK_SOURCE_DIR}/Modules/${group}/itk-module.cmake )
+  foreach( _module_file ${_${group}_module_files} )
+    file( STRINGS ${_module_file} _module_line REGEX "itk_module[ \n]*\\([ \n]*[A-Za-z0-9]*" )
+    string( REGEX MATCH "(\\([ \n]*)([A-Za-z0-9]*)" _module_name ${_module_line} )
+    set( _module_name ${CMAKE_MATCH_2} )
+    set( _${_module_name}_module_line ${_module_line} )
+    list( APPEND _${group}_module_list ${_module_name} )
   endforeach()
 endforeach()
 
 #------------------------------------------------
-#------------------------------------------------
+# Set up Doxygen Group descriptions
+
 set( group_list_dox )
 foreach(group ${group_list} )
   set( group_list_dox
@@ -93,7 +94,7 @@ foreach(group ${group_list} )
 ${${group}_documentation} */\n"
     )
 
-  foreach(mod ${${group}_module_list} )
+  foreach(mod ${_${group}_module_list} )
     set( group_list_dox
 "${group_list_dox}
 /** \\defgroup ${mod} Module ${mod}
@@ -110,6 +111,27 @@ configure_file(
 
 #------------------------------------------------
 # Turn on the ITK_BUILD option for each group
+
+# Set a module name list for each group and exclude
+# Modules that should be OFF
+foreach( group ${group_list} )
+  set( _${group}_on_module_list )
+  list( LENGTH _${group}_module_files _num_modules )
+  set( _current_module 0 )
+  while( ${_current_module} LESS ${_num_modules} )
+    list( GET _${group}_module_files ${_current_module} _module_file )
+    get_filename_component (module_file_dir ${_module_file} PATH)
+    if (NOT EXISTS "${module_file_dir}/itk-module-init.cmake")
+      list( GET _${group}_module_list ${_current_module} _module_name )
+      # Remote Modules do not start with "ITK", and should not be ON by default.
+      if( "${_module_name}" MATCHES "^ITK" )
+        list( APPEND _${group}_on_module_list ${_module_name} )
+      endif()
+    endif()
+  math( EXPR _current_module "${_current_module} + 1" )
+  endwhile()
+endforeach()
+
 if("$ENV{DASHBOARD_TEST_FROM_CTEST}" STREQUAL "")
   # developer build
   option(ITKGroup_Core "Request building core modules" ON)
@@ -117,7 +139,7 @@ endif()
 foreach( group ${group_list})
     option(ITKGroup_${group} "Request building ${group} modules" OFF)
     if (ITKGroup_${group})
-      foreach (itk-module ${${group}_module_list} )
+      foreach (itk-module ${_${group}_on_module_list} )
          list(APPEND ITK_MODULE_${itk-module}_REQUEST_BY ITKGroup_${group})
       endforeach()
     endif()
