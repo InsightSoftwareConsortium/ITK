584ceecd52d0b498fc515ebe3ecfcc81fa07d055
Matt McCormick <matt.mccormick@kitware.com>
2014-06-08 16:36:25 -0400

    DOC: Add links to IJ article in module documentation.

--- Modules/Filtering/PhaseSymmetry/itk-module.cmake
+++ Modules/Filtering/PhaseSymmetry/itk-module.cmake
@@ -1,7 +1,11 @@
 set(
   DOCUMENTATION
   "This module contains classes for performing phase symmetry
-filtration."
+filtration. For more information, see
+\"Multi-scale Steerable Phase-Symmetry Filters for ITK\" by C. Hatt.
+http://www.insight-journal.org/browse/publication/846
+http://hdl.handle.net/10380/3330
+"
 )
 
 itk_module(
