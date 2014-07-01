ad5da8af28d0888bfbdf9d58b282185a9a0e6dac
Matt McCormick <matt.mccormick@kitware.com>
2014-06-30 23:05:02 -0400

    ENH: Add test input data from IJ article.

--- Modules/Filtering/PhaseSymmetry/test/itkPhaseSymmetryImageFilterTest.cxx
+++ Modules/Filtering/PhaseSymmetry/test/itkPhaseSymmetryImageFilterTest.cxx
@@ -17,9 +17,41 @@
  *=========================================================================*/
 
 #include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkPhaseSymmetryImageFilter.h"
 
 int
-itkPhaseSymmetryImageFilterTest(int, char *[])
+itkPhaseSymmetryImageFilterTest(int argc, char * argv[])
 {
+  if (argc < 3)
+  {
+    std::cerr << "Usage: " << argv[0] << " <InputImage> <OutputImage>" << std::endl;
+    return EXIT_FAILURE;
+  }
+  const char * inputImageFileName = argv[1];
+  const char * outputImageFileName = argv[2];
+
+  typedef float                            PixelType;
+  const unsigned int                       Dimension = 2;
+  typedef itk::Image<PixelType, Dimension> ImageType;
+
+  typedef itk::ImageFileReader<ImageType> ReaderType;
+  ReaderType::Pointer                     reader = ReaderType::New();
+  reader->SetFileName(inputImageFileName);
+
+  try
+  {
+    reader->Update();
+  }
+  catch (itk::ExceptionObject & error)
+  {
+    std::cerr << "Error: " << error << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  typedef itk::ImageFileWriter<ImageType> WriterType;
+  WriterType::Pointer                     writer = WriterType::New();
+  writer->SetFileName(outputImageFileName);
+
   return EXIT_SUCCESS;
 }
