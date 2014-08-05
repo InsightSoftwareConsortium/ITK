06013c532c8c974facd1eb68730e8e4631c80761
Girish Mallya <indianzeppelin@gmail.com>
2014-08-05 12:51:24 +0100

    BUG: Tests added for BinaryImageToLabelMapFilter for single-row images.
    Change-Id: I910e787471871bb063c2f30a8454d2a89a6ecf3c

---
+++ Modules/Filtering/LabelMap/test/itkBinaryImageToLabelMapFilterTest2.cxx
@@ -0,0 +1,73 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkBinaryImageToLabelMapFilter.h"
+#include "itkLabelMapToLabelImageFilter.h"
+
+
+int itkBinaryImageToLabelMapFilterTest2( int argc, char * argv [] )
+{
+
+  if( argc != 6 )
+  {
+    std::cerr << "usage: " << argv[0];
+    std::cerr << "inputBinaryImage outputLabelImage";
+    std::cerr << "foregroundValue backgroundValue NumThreads";
+    std::cerr << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  const unsigned int Dimension = 2;
+
+  typedef unsigned char BinaryPixelType;
+  typedef unsigned char LabelPixelType;
+
+  typedef itk::Image< BinaryPixelType, Dimension > ImageType;
+  typedef itk::LabelObject< LabelPixelType, Dimension >   LabelObjectType;
+  typedef itk::LabelMap< LabelObjectType >                LabelMapType;
+
+  typedef itk::ImageFileReader< ImageType > ReaderType;
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( argv[1] );
+
+  typedef itk::BinaryImageToLabelMapFilter< ImageType, LabelMapType > ImageToLabelType;
+  ImageToLabelType::Pointer imageToLabel = ImageToLabelType::New();
+
+  imageToLabel->SetInput( reader->GetOutput() );
+  imageToLabel->SetFullyConnected(true);
+  imageToLabel->SetInputForegroundValue( atoi(argv[3]) );
+  imageToLabel->SetOutputBackgroundValue( atoi(argv[4]) );
+  imageToLabel->SetNumberOfThreads( atoi(argv[5]) );
+  imageToLabel->Update();
+
+  std::cout << "There are " << imageToLabel->GetOutput()->GetNumberOfLabelObjects() << " objects." << std::endl;
+
+  typedef itk::LabelMapToLabelImageFilter< LabelMapType, ImageType> LabelToImageType;
+  LabelToImageType::Pointer labelToImage = LabelToImageType::New();
+  labelToImage->SetInput( imageToLabel->GetOutput() );
+
+  typedef itk::ImageFileWriter< ImageType > WriterType;
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetFileName( argv[2] );
+  writer->SetInput( labelToImage->GetOutput() );
+  writer->Update();
+
+  return EXIT_SUCCESS;
+}
