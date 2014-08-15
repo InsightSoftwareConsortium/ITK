d9d8fe8fc2e51c11134f310be3a3bf73183a8af1
Matt McCormick <matt.mccormick@kitware.com>
2014-08-15 00:14:45 -0400

    ENH: Add src files.

---
+++ Modules/IO/IOTransformDCMTK/src/itkDCMTKTransformIOFactory.cxx
@@ -0,0 +1,74 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#include "itkDCMTKTransformIOFactory.h"
+#include "itkCreateObjectFunction.h"
+#include "itkDCMTKTransformIO.h"
+#include "itkVersion.h"
+
+namespace itk
+{
+
+void
+DCMTKTransformIOFactory::PrintSelf(std::ostream &, Indent) const
+{}
+
+DCMTKTransformIOFactory::DCMTKTransformIOFactory()
+{
+  this->RegisterOverride("itkTransformIOBaseTemplate",
+                         "itkDCMTKTransformIO",
+                         "DCMTK Transform float IO",
+                         1,
+                         CreateObjectFunction<DCMTKTransformIO<float>>::New());
+
+  this->RegisterOverride("itkTransformIOBaseTemplate",
+                         "itkDCMTKTransformIO",
+                         "DCMTK Transform double IO",
+                         1,
+                         CreateObjectFunction<DCMTKTransformIO<double>>::New());
+}
+
+DCMTKTransformIOFactory::~DCMTKTransformIOFactory() {}
+
+const char *
+DCMTKTransformIOFactory::GetITKSourceVersion(void) const
+{
+  return ITK_SOURCE_VERSION;
+}
+
+const char *
+DCMTKTransformIOFactory::GetDescription() const
+{
+  return "DCMTK TransformIO Factory, allows the"
+         " loading of DICOM transforms into Insight";
+}
+
+// Undocumented API used to register during static initialization.
+// DO NOT CALL DIRECTLY.
+static bool DCMTKTransformIOFactoryHasBeenRegistered;
+
+void
+DCMTKTransformIOFactoryRegister__Private(void)
+{
+  if (!DCMTKTransformIOFactoryHasBeenRegistered)
+  {
+    DCMTKTransformIOFactoryHasBeenRegistered = true;
+    DCMTKTransformIOFactory::RegisterOneFactory();
+  }
+}
+
+} // end namespace itk
