a14caee044ba037a08730a632cac7d70947ab8ad
Mark Hiner <hinerm@gmail.com>
2014-09-09 11:30:03 -0500

    ENH: Update SCIFIO remote module hash
    Updates to the latest scifio-imageio commit.
    
    Changes:
    * Bio-Formats version 4.4.9 > 5.0.4
    * Pin to release scifio-itk-bridge (old build system was fragile)
    * Java package rename, "loci.scifio.itk" > "io.scif.itk"
    * Fixes race condition when writing
    * Fixes metadata population when reading multi-series datasets
    
    Change-Id: I65d4f675d9162ff4e05fa4308035c48c4e74856d

--- Modules/Remote/SCIFIO.remote.cmake
+++ Modules/Remote/SCIFIO.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(SCIFIO
   "SCIFIO (Bioformats) ImageIO plugin for ITK"
   GIT_REPOSITORY https://github.com/scifio/scifio-imageio.git
-  GIT_TAG da6edd6bcefd8f107a3ed34ed663bfc69b3dc167
+  GIT_TAG 99ccab489f9cd64ef4926cc4a539175e9207ac8d
   )
