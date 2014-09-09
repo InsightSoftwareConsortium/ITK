InsightSoftwareConsortium/ITK

commit f910d5e752dba02fab17bfd80014b73a0a3ba3ab

ENH: Use if(DEFINED) to simplify conditions
Replace old hacks of the form 'if("${VAR}" MATCHES "^${VAR}$")'
with the much simpler 'if(NOT DEFINED ${VAR})'.

Change-Id: I6d21632ccfdbdd8dda0cec515231311f47a4ce7a

Modules/ThirdParty/VNL/src/vxl/config/cmake/config/vxl_config_macros.cmake
@@ -12,7 +12,7 @@ MACRO(PERFORM_CMAKE_TEST FILE TEST)
   IF( VXL_UPDATE_CONFIGURATION )
     SET( ${TEST} "${TEST}" )
   ENDIF( VXL_UPDATE_CONFIGURATION )
-  IF("${TEST}" MATCHES "^${TEST}$")
+  IF(NOT DEFINED "${TEST}")
     # Perform test
     SET(MACRO_CHECK_FUNCTION_DEFINITIONS
         "-D${TEST} ${CMAKE_REQUIRED_FLAGS}")
@@ -38,12 +38,12 @@ MACRO(PERFORM_CMAKE_TEST FILE TEST)
                  "Performing Test ${TEST} failed with the following output:\n"
                  "${OUTPUT}\n")
     ENDIF(${TEST})
-  ELSE("${TEST}" MATCHES "^${TEST}$")
+  ELSE()
     # Have result
     #FOREACH(tst ${TEST})
     #  MESSAGE("Test ${TEST} resulted in ${${tst}}")
     #ENDFOREACH(tst ${TEST})
-  ENDIF("${TEST}" MATCHES "^${TEST}$")
+  ENDIF()
 ENDMACRO(PERFORM_CMAKE_TEST FILE TEST)
 
 #
@@ -59,7 +59,7 @@ MACRO(PERFORM_CMAKE_TEST_CUSTOM DIR TEST)
   IF( VXL_UPDATE_CONFIGURATION )
     SET( ${TEST} "${TEST}" )
   ENDIF( VXL_UPDATE_CONFIGURATION )
-  IF("${TEST}" MATCHES "^${TEST}$")
+  IF(NOT DEFINED "${TEST}")
     # Perform test
     SET(MACRO_CHECK_FUNCTION_DEFINITIONS
         "-D${TEST} ${CMAKE_REQUIRED_FLAGS}")
@@ -88,7 +88,7 @@ MACRO(PERFORM_CMAKE_TEST_CUSTOM DIR TEST)
                  "Performing Test ${TEST} failed with the following output:\n"
                  "${OUTPUT}\n")
     ENDIF(${TEST})
-  ENDIF("${TEST}" MATCHES "^${TEST}$")
+  ENDIF()
   FILE(REMOVE_RECURSE ${CMAKE_BINARY_DIR}/config/${DIR})
 ENDMACRO(PERFORM_CMAKE_TEST_CUSTOM DIR TEST)
 
@@ -103,7 +103,7 @@ MACRO(PERFORM_CMAKE_TEST_RUN FILE TEST)
   IF( VXL_UPDATE_CONFIGURATION )
     SET( ${TEST} "${TEST}" )
   ENDIF( VXL_UPDATE_CONFIGURATION )
-  IF("${TEST}" MATCHES "^${TEST}$")
+  IF(NOT DEFINED "${TEST}")
     # Perform test
     SET(MACRO_CHECK_FUNCTION_DEFINITIONS
         "-D${TEST} ${CMAKE_REQUIRED_FLAGS}")
@@ -140,12 +140,12 @@ MACRO(PERFORM_CMAKE_TEST_RUN FILE TEST)
                  "Performing Try-Run Test ${TEST} failed to compile with the following output:\n"
                  "${OUTPUT}\n")
     ENDIF(${TEST}_COMPILED)
-  ELSE("${TEST}" MATCHES "^${TEST}$")
+  ELSE()
     # Have result
     #FOREACH(tst ${TEST})
     #  MESSAGE("Test ${TEST} resulted in ${${tst}}")
     #ENDFOREACH(tst ${TEST})
-  ENDIF("${TEST}" MATCHES "^${TEST}$")
+  ENDIF()
 ENDMACRO(PERFORM_CMAKE_TEST_RUN FILE TEST)
 
 #
@@ -157,9 +157,9 @@ MACRO(PERFORM_CHECK_HEADER FILE VARIABLE)
     UNSET(${VARIABLE})
   ENDIF( VXL_UPDATE_CONFIGURATION )
   CHECK_INCLUDE_FILE_CXX(${FILE} ${VARIABLE})
-  IF("${VARIABLE}" MATCHES "^$")
+  IF("x${VARIABLE}" STREQUAL "x")
     SET(${VARIABLE} 0)
-  ENDIF("${VARIABLE}" MATCHES "^$")
+  ENDIF()
 ENDMACRO(PERFORM_CHECK_HEADER FILE VARIABLE)
 
 #
@@ -197,7 +197,7 @@ MACRO(CHECK_TYPE_EXISTS TYPE FILES VARIABLE)
   IF( VXL_UPDATE_CONFIGURATION )
     SET( ${VARIABLE} "${VARIABLE}" )
   ENDIF( VXL_UPDATE_CONFIGURATION )
-  IF("${VARIABLE}" MATCHES "^${VARIABLE}$")
+  IF(NOT DEFINED "${VARIABLE}")
     SET(CHECK_TYPE_EXISTS_CONTENT "/* */\n")
     SET(MACRO_CHECK_TYPE_EXISTS_FLAGS ${CMAKE_REQUIRED_FLAGS})
     IF(CMAKE_REQUIRED_LIBRARIES)
@@ -239,7 +239,7 @@ MACRO(CHECK_TYPE_EXISTS TYPE FILES VARIABLE)
            "${OUTPUT}\nFile ${CMAKE_BINARY_DIR}/CMakeTmp/CheckTypeExists.c:\n"
            "${CHECK_TYPE_EXISTS_CONTENT}\n")
     ENDIF(${VARIABLE})
-  ENDIF("${VARIABLE}" MATCHES "^${VARIABLE}$")
+  ENDIF()
 ENDMACRO(CHECK_TYPE_EXISTS TYPE FILES VARIABLE)
 
 #

Modules/Core/Common/CMake/CheckCPPDirective.cmake
@@ -7,7 +7,7 @@
 get_filename_component(_CheckCPPDirective_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 
 macro(CHECK_CPP_DIRECTIVE_EXISTS DIRECTIVE VARIABLE)
- if("HAVE_${VARIABLE}" MATCHES "^HAVE_${VARIABLE}$")
+ if(NOT DEFINED "HAVE_${VARIABLE}")
   message(STATUS "Checking to see if this platform has the ${DIRECTIVE} C-Preprocessor directive")
   set(DIRECTIVE ${DIRECTIVE})
   configure_file(${_CheckCPPDirective_DIR}/CheckCPPDirectiveExists.cxx.in

Modules/ThirdParty/GDCM/src/gdcm/CMake/gdcmPlatformCxxTests.cmake
@@ -8,7 +8,7 @@
 # Macro to centralize all the plateform specific
 # tests.
 MACRO(GDCM_PLATFORM_CXX_TEST var description invert)
-  IF("${var}_COMPILED" MATCHES "^${var}_COMPILED$")
+  IF(NOT DEFINED "${var}_COMPILED")
     MESSAGE(STATUS "${description}")
     TRY_COMPILE(${var}_COMPILED
       ${CMAKE_CURRENT_BINARY_DIR}
@@ -35,7 +35,7 @@ MACRO(GDCM_PLATFORM_CXX_TEST var description invert)
         MESSAGE(STATUS "${description} - no")
       ENDIF(${var}_COMPILED)
     ENDIF(${invert} MATCHES INVERT)
-  ENDIF("${var}_COMPILED" MATCHES "^${var}_COMPILED$")
+  ENDIF()
   IF(${invert} MATCHES INVERT)
     IF(${var}_COMPILED)
       SET(${var} 0)

Modules/ThirdParty/HDF5/src/itkhdf5/config/cmake/CheckTypeSize.cmake
@@ -9,7 +9,7 @@
 
 MACRO (HDF_CHECK_TYPE_SIZE TYPE VARIABLE)
   SET (CMAKE_ALLOW_UNKNOWN_VARIABLE_READ_ACCESS 1)
-  IF ("HAVE_${VARIABLE}" MATCHES "^HAVE_${VARIABLE}$")
+  IF (NOT DEFINED "HAVE_${VARIABLE}")
     SET (MACRO_CHECK_TYPE_SIZE_FLAGS 
         "-DCHECK_TYPE_SIZE_TYPE=\"${TYPE}\" ${CMAKE_REQUIRED_FLAGS}"
     )
@@ -43,6 +43,6 @@ MACRO (HDF_CHECK_TYPE_SIZE TYPE VARIABLE)
           "Determining size of ${TYPE} failed with the following output:\n${OUTPUT}\n\n"
       )
     ENDIF (HAVE_${VARIABLE})
-  ENDIF ("HAVE_${VARIABLE}" MATCHES "^HAVE_${VARIABLE}$")
+  ENDIF ()
   SET (CMAKE_ALLOW_UNKNOWN_VARIABLE_READ_ACCESS)
 ENDMACRO (HDF_CHECK_TYPE_SIZE)

Modules/ThirdParty/HDF5/src/itkhdf5/config/cmake/ConfigureChecks.cmake
@@ -440,7 +440,7 @@ ENDIF (NOT H5_HAVE_SIGSETJMP)
 #  usual places. On MSVC we are just going to use ::clock()
 #-----------------------------------------------------------------------------
 IF (NOT MSVC)
-  IF ("H5_HAVE_TIME_GETTIMEOFDAY" MATCHES "^H5_HAVE_TIME_GETTIMEOFDAY$")
+  IF (NOT DEFINED H5_HAVE_TIME_GETTIMEOFDAY)
     TRY_COMPILE (HAVE_TIME_GETTIMEOFDAY
         ${CMAKE_BINARY_DIR}
         ${HDF5_RESOURCES_DIR}/GetTimeOfDayTest.cpp
@@ -451,9 +451,9 @@ IF (NOT MSVC)
       SET (H5_HAVE_TIME_GETTIMEOFDAY "1" CACHE INTERNAL "H5_HAVE_TIME_GETTIMEOFDAY")
       SET (H5_HAVE_GETTIMEOFDAY "1" CACHE INTERNAL "H5_HAVE_GETTIMEOFDAY")
     ENDIF (HAVE_TIME_GETTIMEOFDAY STREQUAL "TRUE")
-  ENDIF ("H5_HAVE_TIME_GETTIMEOFDAY" MATCHES "^H5_HAVE_TIME_GETTIMEOFDAY$")
+  ENDIF ()
 
-  IF ("H5_HAVE_SYS_TIME_GETTIMEOFDAY" MATCHES "^H5_HAVE_SYS_TIME_GETTIMEOFDAY$")
+  IF (NOT DEFINED H5_HAVE_SYS_TIME_GETTIMEOFDAY)
     TRY_COMPILE (HAVE_SYS_TIME_GETTIMEOFDAY
         ${CMAKE_BINARY_DIR}
         ${HDF5_RESOURCES_DIR}/GetTimeOfDayTest.cpp
@@ -464,7 +464,7 @@ IF (NOT MSVC)
       SET (H5_HAVE_SYS_TIME_GETTIMEOFDAY "1" CACHE INTERNAL "H5_HAVE_SYS_TIME_GETTIMEOFDAY")
       SET (H5_HAVE_GETTIMEOFDAY "1" CACHE INTERNAL "H5_HAVE_GETTIMEOFDAY")
     ENDIF (HAVE_SYS_TIME_GETTIMEOFDAY STREQUAL "TRUE")
-  ENDIF ("H5_HAVE_SYS_TIME_GETTIMEOFDAY" MATCHES "^H5_HAVE_SYS_TIME_GETTIMEOFDAY$")
+  ENDIF ()
 
   IF (NOT HAVE_SYS_TIME_GETTIMEOFDAY AND NOT H5_HAVE_GETTIMEOFDAY)
     MESSAGE (STATUS "---------------------------------------------------------------")
@@ -500,7 +500,7 @@ ENDIF (HDF5_STREAM_VFD)
 
 # For other other specific tests, use this MACRO.
 MACRO (HDF5_FUNCTION_TEST OTHER_TEST)
-  IF ("H5_${OTHER_TEST}" MATCHES "^H5_${OTHER_TEST}$")
+  IF (NOT DEFINED "H5_${OTHER_TEST}")
     SET (MACRO_CHECK_FUNCTION_DEFINITIONS "-D${OTHER_TEST} ${CMAKE_REQUIRED_FLAGS}")
     SET (OTHER_TEST_ADD_LIBRARIES)
     IF (CMAKE_REQUIRED_LIBRARIES)
@@ -547,7 +547,7 @@ MACRO (HDF5_FUNCTION_TEST OTHER_TEST)
           "${OUTPUT}\n"
       )
     ENDIF (${OTHER_TEST})
-  ENDIF ("H5_${OTHER_TEST}" MATCHES "^H5_${OTHER_TEST}$")
+  ENDIF ()
 ENDMACRO (HDF5_FUNCTION_TEST)
 
 #-----------------------------------------------------------------------------
@@ -590,7 +590,7 @@ ENDIF (NOT WINDOWS)
 #-----------------------------------------------------------------------------
 IF (WINDOWS)
   MESSAGE (STATUS "Checking for InitOnceExecuteOnce:")
-  IF("${H5_HAVE_IOEO}" MATCHES "^${H5_HAVE_IOEO}$")
+  IF(NOT DEFINED "${H5_HAVE_IOEO}")
     IF (LARGEFILE)
       SET (CMAKE_REQUIRED_DEFINITIONS
           "${CURRENT_TEST_DEFINITIONS} -D_FILE_OFFSET_BITS=64 -D_LARGEFILE64_SOURCE -D_LARGEFILE_SOURCE"
@@ -645,7 +645,7 @@ IF (WINDOWS)
         "${OUTPUT}\n"
         "Return value: ${HAVE_IOEO_EXITCODE}\n")
     ENDIF("${HAVE_IOEO_EXITCODE}" EQUAL 0)
-  ENDIF("${H5_HAVE_IOEO}" MATCHES "^${H5_HAVE_IOEO}$")
+  ENDIF()
 ENDIF (WINDOWS)
 	
 
@@ -750,7 +750,7 @@ ENDIF (HDF5_ENABLE_HSIZET)
 # Macro to determine the various conversion capabilities
 #-----------------------------------------------------------------------------
 MACRO (H5ConversionTests TEST msg)
-  IF ("${TEST}" MATCHES "^${TEST}$")
+  IF (NOT DEFINED "${TEST}")
    # MESSAGE (STATUS "===> ${TEST}")
     TRY_RUN (${TEST}_RUN   ${TEST}_COMPILE
         ${HDF5_BINARY_DIR}/CMake
@@ -777,14 +777,14 @@ MACRO (H5ConversionTests TEST msg)
       )
     ENDIF (${TEST}_COMPILE)
 
-  ENDIF("${TEST}" MATCHES "^${TEST}$")
+  ENDIF()
 ENDMACRO (H5ConversionTests)
 
 #-----------------------------------------------------------------------------
 # Macro to make some of the conversion tests easier to write/read
 #-----------------------------------------------------------------------------
 MACRO (H5MiscConversionTest  VAR TEST msg)
-  IF ("${TEST}" MATCHES "^${TEST}$")
+  IF (NOT DEFINED "${TEST}")
     IF (${VAR})
       SET (${TEST} 1 CACHE INTERNAL ${msg})
       MESSAGE (STATUS "${msg}... yes")
@@ -792,7 +792,7 @@ MACRO (H5MiscConversionTest  VAR TEST msg)
       SET (${TEST} "" CACHE INTERNAL ${msg})
       MESSAGE (STATUS "${msg}... no")
     ENDIF (${VAR})
-  ENDIF ("${TEST}" MATCHES "^${TEST}$")
+  ENDIF ()
 ENDMACRO (H5MiscConversionTest)
 
 #-----------------------------------------------------------------------------
@@ -869,19 +869,19 @@ H5ConversionTests (H5_LDOUBLE_TO_UINT_ACCURATE "Checking IF correctly converting
 # 'unsigned long long' to 'float' and 'double' typecasts.
 # (This flag should be set for all machines.)
 #
-IF (H5_ULLONG_TO_FP_CAST_WORKS MATCHES ^H5_ULLONG_TO_FP_CAST_WORKS$)
+IF (NOT DEFINED H5_ULLONG_TO_FP_CAST_WORKS)
   SET (H5_ULLONG_TO_FP_CAST_WORKS 1 CACHE INTERNAL "Checking IF compiling unsigned long long to floating-point typecasts work")
   MESSAGE (STATUS "Checking IF compiling unsigned long long to floating-point typecasts work... yes")
-ENDIF (H5_ULLONG_TO_FP_CAST_WORKS MATCHES ^H5_ULLONG_TO_FP_CAST_WORKS$)
+ENDIF ()
 # ----------------------------------------------------------------------
 # Set the flag to indicate that the machine can _compile_
 # 'long long' to 'float' and 'double' typecasts.
 # (This flag should be set for all machines.)
 #
-IF (H5_LLONG_TO_FP_CAST_WORKS MATCHES ^H5_LLONG_TO_FP_CAST_WORKS$)
+IF (NOT DEFINED H5_LLONG_TO_FP_CAST_WORKS)
   SET (H5_LLONG_TO_FP_CAST_WORKS 1 CACHE INTERNAL "Checking IF compiling long long to floating-point typecasts work")
   MESSAGE (STATUS "Checking IF compiling long long to floating-point typecasts work... yes")
-ENDIF (H5_LLONG_TO_FP_CAST_WORKS MATCHES ^H5_LLONG_TO_FP_CAST_WORKS$)
+ENDIF ()
 # ----------------------------------------------------------------------
 # Set the flag to indicate that the machine can convert from
 # 'unsigned long long' to 'long double' without precision loss.

Modules/ThirdParty/NrrdIO/src/NrrdIO/CMake/TestQnanhibit.cmake
@@ -7,7 +7,7 @@
 #
 
 macro(TEST_QNANHIBIT VARIABLE LOCAL_TEST_DIR)
-  if("HAVE_${VARIABLE}" MATCHES "^HAVE_${VARIABLE}$")
+  if(NOT DEFINED "HAVE_${VARIABLE}")
     try_run(${VARIABLE} HAVE_${VARIABLE}
       ${CMAKE_BINARY_DIR}
       ${LOCAL_TEST_DIR}/TestQnanhibit.c
@@ -32,5 +32,5 @@ macro(TEST_QNANHIBIT VARIABLE LOCAL_TEST_DIR)
       message(STATUS "Failed to compile a test (TestQnanhibit.c) necessary to configure for proper handling of IEEE floating point NaN's")
     endif(HAVE_${VARIABLE})
     file(APPEND ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeError.log "TestQnanhibit.c produced following output:\n${OUTPUT}\n\n")
-  endif("HAVE_${VARIABLE}" MATCHES "^HAVE_${VARIABLE}$")
+  endif()
 endmacro(TEST_QNANHIBIT)

Modules/ThirdParty/VNL/src/vxl/config/cmake/Modules/CheckPrototypeExistsCXX.cmake
@@ -10,7 +10,7 @@
 # context, so we use the C++ compiler to check.
 #
 MACRO(CHECK_PROTOTYPE_EXISTS_CXX FUNC INCLUDE VARIABLE)
-  IF("${VARIABLE}" MATCHES "^${VARIABLE}$")
+  IF(NOT DEFINED "${VARIABLE}")
     SET( CHECK_PROTOTYPE_EXISTS_CXX_FILE_IN "${VXL_CMAKE_DIR}/CheckPrototypeExists.cxx.in" )
     SET( CHECK_PROTOTYPE_EXISTS_CXX_FILE "${CMAKE_BINARY_DIR}/CMakeTmp/CheckPrototypeExists.cxx" )
     SET( CHECK_PROTOTYPE_EXISTS_CXX_EXTERNC_BEGIN "extern \"C\" {\n" )
@@ -63,5 +63,5 @@ MACRO(CHECK_PROTOTYPE_EXISTS_CXX FUNC INCLUDE VARIABLE)
       MESSAGE(STATUS "Looking for prototype of ${FUNC} - not found")
       SET(${VARIABLE} "" CACHE INTERNAL "Have prototype ${VARIABLE}")
     ENDIF(${VARIABLE})
-  ENDIF("${VARIABLE}" MATCHES "^${VARIABLE}$")
+  ENDIF()
 ENDMACRO(CHECK_PROTOTYPE_EXISTS_CXX)

commit 6c56c8bd88a345e40c5b4ffc766b7a64df4152a6

COMP: Fix one more vxl_config_macros check broken by refactoring
In commit f910d5e7 (Use if(DEFINED) to simplify conditions, 2014-09-08)
we removed sevaral uses of implicit variable evaluation.  However, the
CHECK_TYPE_EXISTS macro was abusing details of the prior implementation
and depended on setting a check result variable to its own name to reset
the check.  Use unset() instead.

Change-Id: I801bd9daab63b73b30d2d639e9f2d52b3e0b534b

Modules/ThirdParty/VNL/src/vxl/config/cmake/config/vxl_config_macros.cmake
@@ -195,7 +195,7 @@ ENDMACRO(SET_INVERT VAR VALUE)
 
 MACRO(CHECK_TYPE_EXISTS TYPE FILES VARIABLE)
   IF( VXL_UPDATE_CONFIGURATION )
-    SET( ${VARIABLE} "${VARIABLE}" )
+    UNSET( ${VARIABLE} )
   ENDIF( VXL_UPDATE_CONFIGURATION )
   IF(NOT DEFINED "${VARIABLE}")
     SET(CHECK_TYPE_EXISTS_CONTENT "/* */\n")