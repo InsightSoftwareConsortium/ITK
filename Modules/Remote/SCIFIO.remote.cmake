c7c85edd4853350abef6145549bd637d29d3185f
Mark Hiner <hinerm@gmail.com>
2014-09-19 08:36:28 -0500

    BUG: Fix duplicate variables in Windows
    Updates the scifio-imageio hash to account for a fix from mabruce, removing
    duplicate variable declarations in the Write method on Windows.
    
    Change-Id: Id07006d70822fa689be71bb045033708e1cc7234

--- Modules/Remote/SCIFIO.remote.cmake
+++ Modules/Remote/SCIFIO.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(SCIFIO
   "SCIFIO (Bioformats) ImageIO plugin for ITK"
   GIT_REPOSITORY https://github.com/scifio/scifio-imageio.git
-  GIT_TAG 99ccab489f9cd64ef4926cc4a539175e9207ac8d
+  GIT_TAG 71a701054bdb6e19fb21d7195e5878fc01179be4
   )
