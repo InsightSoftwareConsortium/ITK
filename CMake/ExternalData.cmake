13f6744c14bc1c973a6b7846e4bed70519f3013f
Michka Popoff <michkapopoff@gmail.com>
2014-10-01 09:39:40 +0200

    STYLE: Remove old cmake < 2.8.5 specific code
    We are requiring at least cmake 2.8.6
    
    Change-Id: I46183691bea6496967ebb0181b5de577e7320510

--- CMake/ExternalData.cmake
+++ CMake/ExternalData.cmake
@@ -135,8 +135,7 @@
 # Variables ExternalData_TIMEOUT_INACTIVITY and ExternalData_TIMEOUT_ABSOLUTE
 # set the download inactivity and absolute timeouts, in seconds.  The defaults
 # are 60 seconds and 300 seconds, respectively.  Set either timeout to 0
-# seconds to disable enforcement.  The inactivity timeout is enforced only
-# with CMake >= 2.8.5.
+# seconds to disable enforcement.
 
 #=============================================================================
 # Copyright 2010-2013 Kitware, Inc.
@@ -307,13 +306,7 @@ function(_ExternalData_compute_hash var_hash algo file)
 endfunction()
 
 function(_ExternalData_random var)
-  if(NOT ${CMAKE_VERSION} VERSION_LESS 2.8.5)
-    string(RANDOM LENGTH 6 random)
-  elseif(EXISTS /dev/urandom)
-    file(READ /dev/urandom random LIMIT 4 HEX)
-  else()
-    message(FATAL_ERROR "CMake >= 2.8.5 required in this environment")
-  endif()
+  string(RANDOM LENGTH 6 random)
   set("${var}" "${random}" PARENT_SCOPE)
 endfunction()
 
