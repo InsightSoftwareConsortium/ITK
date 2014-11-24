0dc92b4868c47f43f2a56f2ea38b90b30e6b22dc
Matt McCormick <matt.mccormick@kitware.com>
2014-11-23 19:14:13 -0500

    DOC: Index Wiki examples with Doxygen.
    The wiki examples are indexed by doxygen so they will automatically be listed
    with the class documentation and show up on the "Examples" tab page of
    Doxygen.
    
    ITK-3309.
    
    Change-Id: I44245e1189c6d1fe7b585ba153c9d7f78efe58f5

--- Utilities/Doxygen/GenerateExamplesDox.cmake
+++ Utilities/Doxygen/GenerateExamplesDox.cmake
@@ -10,12 +10,55 @@
 # referenced files.
 
 # glob the examples for each language
-file( GLOB_RECURSE EXAMPLES_LIST
-  RELATIVE "${PROJECT_SOURCE_DIR}/Examples"
-  "*.cxx"
-  "*.py"
-  "*.java"
+file( GLOB_RECURSE examples_list_fullpath
+  FOLLOW_SYMLINKS
+  "${PROJECT_SOURCE_DIR}/Examples/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Examples/*.py"
+  "${PROJECT_SOURCE_DIR}/Examples/*.java"
 )
+file( GLOB_RECURSE wiki_examples_list_fullpath
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/Conversions/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/Curves/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/Developer/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/DICOM/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/EdgesAndGradients/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/Functions/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/ImageProcessing/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/Images/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/ImageSegmentation/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/Inspection/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/Instructions/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/IO/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/Iterators/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/Math/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/Meshes/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/Metrics/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/Morphology/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/Operators/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/PointSet/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/Registration/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/Segmentation/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/SimpleOperations/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/Smoothing/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/SpatialObjects/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/SpectralAnalysis/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/Statistics/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/VectorImages/*.cxx"
+  "${PROJECT_SOURCE_DIR}/Modules/Remote/WikiExamples/Visualization/*.cxx"
+)
+
+# Use relative paths
+set( examples_list )
+string( LENGTH "${PROJECT_SOURCE_DIR}/" root_length )
+foreach( _example ${examples_list_fullpath} )
+  string( SUBSTRING ${_example} ${root_length} -1 example_relative )
+  list( APPEND examples_list ${example_relative} )
+endforeach()
+string( LENGTH "${PROJECT_SOURCE_DIR}/Modules/Remote/" root_length )
+foreach( _example ${wiki_examples_list_fullpath} )
+  string( SUBSTRING ${_example} ${root_length} -1 example_relative )
+  list( APPEND examples_list ${example_relative} )
+endforeach()
 
 # remove the file before we begin appending
 file( REMOVE ${OUTPUT_FILE} )
@@ -27,9 +70,9 @@ file( APPEND ${OUTPUT_FILE} "/**\n")
 # of the \example to be utilized
 file( APPEND ${OUTPUT_FILE} " \n")
 
-FOREACH( f IN LISTS EXAMPLES_LIST )
+foreach( f IN LISTS examples_list )
   file( APPEND ${OUTPUT_FILE}  "\\example ${f}\n" )
-ENDFOREACH( )
+endforeach()
 
 # end comment
 file( APPEND ${OUTPUT_FILE}  "*/\n" )
