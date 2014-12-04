da17ae4d4955bb92d5259c6ac47bfce6c474be18
Arnaud Gelas <arnaudgelas@gmail.com>
2014-12-04 10:37:58 +0100

    ENH: add MinimalPathExtraction as a remote module
    source code is imported from
    http://www.insight-journal.org/browse/publication/213
    
    Change-Id: I8ff9c85c22f672c5c8cf92af66a6bf92d4dd8509

---
+++ Modules/Remote/MinimalPathExtraction.remote.cmake
@@ -0,0 +1,7 @@
+# Contact: Arnaud Gelas <arnaudgelas@gmail.com>
+itk_fetch_module(MinimalPathExtraction
+ "http://www.insight-journal.org/browse/publication/213
+"
+  GIT_REPOSITORY https://github.com/InsightSoftwareConsortium/MinimalPathExtraction.git
+  GIT_TAG 1065027b1fea00ca2353facdb2db9d1a5e724d27
+  )
