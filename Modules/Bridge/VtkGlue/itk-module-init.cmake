6274e5fddd6741e4250ab9fcb9672612f6f2f148
Cory Quammen <cory.quammen@kitware.com>
2014-12-06 15:44:12 -0500

    COMP: Avoid including VTK_USE_FILE within ITK
    A convenient way to load VTK for usage in a project's CMakeLists.txt
    file is
    
    find_package(VTK)
    include(${VTK_USE_FILE})
    
    Doing this within ITK can cause mysterious linker and include path
    problems for projects that use ITK and perform their own inclusion of
    VTK_USE_FILE, a file that is meant to be included only once in a CMake
    project.
    
    This commit changes ITK's inclusion of VTK in the VtkGlue and
    LevelSetsv4Visualization modules to not include VTK_USE_FILE.
    Instead, it uses the vtk_module_config() mechanism to load the
    necessary VTK modules and sets up only the necessary libraries,
    include paths, and compiler definitions needed for each ITK module.
    
    This change should make it possible for projects that use ITK with
    VtkGlue and/or LevelSetsv4Visualization enabled with the simplified
    idiom
    
    find_package(VTK)
    include(${VTK_USE_FILE})
    
    as long as ITK is found prior to VTK, i.e.,
    
    find_package(ITK)
    find_package(VTK)
    
    Change-Id: Ic54e09890617bf8b81f0aba672b573a5bdf5e7bf

--- Modules/Bridge/VtkGlue/itk-module-init.cmake
+++ Modules/Bridge/VtkGlue/itk-module-init.cmake
@@ -5,40 +5,5 @@
 # Needed VTK version
 set(VERSION_MIN "5.10.0")
 
-set(_wrap_module)
-if(ITK_WRAP_PYTHON)
-  set(_wrap_module vtkWrappingPythonCore)
-endif()
 # Look for VTK
-find_package(VTK REQUIRED
-  COMPONENTS
-  vtkCommonCore
-  vtkRenderingCore
-  vtkRenderingOpenGL
-  vtkRenderingFreeType
-  vtkInteractionStyle
-  vtkIOImage
-  vtkImagingSources
-  ${_wrap_module}
-  )
-
-# Older versions of VTK (VTK 5.5 for example) do not have VTK_VERSION, in this
-# case it needs to be defined manually
-if(NOT VTK_VERSION)
-set(VTK_VERSION "${VTK_MAJOR_VERSION}.${VTK_MINOR_VERSION}.${VTK_BUILD_VERSION}")
-endif()
-
-if (${VTK_VERSION} VERSION_LESS ${VERSION_MIN})
-  message(ERROR " VtkGlue requires VTK version ${VERSION_MIN} or newer but the current version is ${VTK_VERSION}")
-endif()
-
-# The VTK DICOMParser and vtkmetaio includes conflict with the ITK
-# versions. Here we remove them from the include directories.
-#
-string(REGEX REPLACE "[^;]*MetaIO;"
-         "" VTK_INCLUDE_DIRS "${VTK_INCLUDE_DIRS}")
-string(REGEX REPLACE "[^;]*vtkmetaio;"
-         "" VTK_INCLUDE_DIRS "${VTK_INCLUDE_DIRS}")
-string(REGEX REPLACE "[^;]*DICOMParser;"
-         "" VTK_INCLUDE_DIRS "${VTK_INCLUDE_DIRS}")
-include(${VTK_USE_FILE})
+find_package(VTK NO_MODULE REQUIRED COMPONENTS vtkCommonCore)
