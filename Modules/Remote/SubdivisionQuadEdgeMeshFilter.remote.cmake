f5f9a94a5f6c399d4340dd32c659a32d673ace00
Arnaud Gelas <arnaudgelas@gmail.com>
2014-12-08 09:21:33 +0100

    ENH: Adding SubdivisionQuadEdgeMeshFilters to ITK as remote module
    Added triangle and edge based subdivision QuadEdgeMesh
    Filters to Modules/Filtering/QuadEdgeMeshFiltering and
    related tests.
    
    Code is imported from the following Insight Journal's publication:
    
    Wanlin Zhu, "Triangle Mesh Subdivision"
    http://www.insight-journal.org/browse/publication/831
    
    Co-authored-by: Wanlin Zhu <wanlinzhu@gmail.com>
    
    Change-Id: I5ed91f149567d833ca22d22bf2d8135e51167ece

---
+++ Modules/Remote/SubdivisionQuadEdgeMeshFilter.remote.cmake
@@ -0,0 +1,11 @@
+itk_fetch_module(SubdivisionQuadEdgeMeshFilter
+"Triangle and edge based QuadEdgeMesh subdivision filters.
+
+See the following Insight Journal's publication:
+
+  Wanlin Zhu, \"Triangle Mesh Subdivision\"
+  http://www.insight-journal.org/browse/publication/831
+  http://hdl.handle.net/10380/3307"
+  GIT_REPOSITORY http://github.com/InsightSoftwareConsortium/itkSubdivisionQuadEdgeMeshFilter
+  GIT_TAG 7911e2c888acb695ee5e16faa7b8024a2e36136c
+  )
