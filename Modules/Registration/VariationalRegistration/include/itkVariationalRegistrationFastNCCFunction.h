71b76f800c7dfef61f1145f57259fdd6f7fc830a
Jan Ehrhardt <ehrhardt@imi.uni-luebeck.de>
2015-01-17 13:25:54 +0100

    ENH: added missing files from last commit

---
+++ Modules/Registration/VariationalRegistration/include/itkVariationalRegistrationFastNCCFunction.h
@@ -0,0 +1,174 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef __itkVariationalRegistrationFastNCCFunction_h
+#define __itkVariationalRegistrationFastNCCFunction_h
+
+#include "itkVariationalRegistrationNCCFunction.h"
+#include "itkCovariantVector.h"
+#include "itkInterpolateImageFunction.h"
+#include "itkLinearInterpolateImageFunction.h"
+#include "itkCentralDifferenceImageFunction.h"
+
+namespace itk
+{
+
+/** \class VariationalRegistrationFastNCCFunction
+ *
+ *  \brief This class computes NCC forces in the variational registration framework.
+ *
+ *  This class implements NCC forces as given in <em> Hermosillo, Chefd'Hotel, and Faugeras.
+ *  "Variational methods for multimodal image matching." IJCV 50(3), 2002: 329-343</em>
+ *  and <em>Avants et al. "Symmetric diffeomorphic image registration with cross-correlation:
+ *  evaluating automated labeling of elderly and neurodegenerative brain." Medical image analysis
+ *  12(1), 2008: 26-41</em> (except Jacobian term). We define the derivative of NCC between two
+ *  images as:
+ *  \f[
+ *    f^{NCC}(x)=\tau\kappa\frac{2\sum_w (F-\bar{F})(M-\bar{M})}{\sum_w (F-\bar{F})^2\
+ *    \sum_w (M-\bar{M})^2}\left((M-\bar{M}) -
+ *    \frac{\sum_w (F-\bar{F})(M-\bar{M})}{\sum_w (F-\bar{F})^2}(F-\bar{F})\right)
+ *    \nabla M(x+u(x))
+ *  \f]
+ *  \f$\tau\f$ is the step size and \f$\kappa\f$ is the mean squared spacing. Use SetRadius()
+ *  (see FiniteDifferenceFunction) to set the size of the neighbourhood to compute local mean values
+ *  \f$\bar{F}\f$ and \f$\bar{M}\f$ and the local sums.
+ *  Alternative, the classical gradient \f$\nabla M(x+u(x))\f$ can be replaced by \f$\nabla F(x)\f$
+ *  or \f$\frac{\nabla F(x) + \nabla M(x+u(x))}{2}\f$.
+ *
+ *  \sa VariationalRegistrationFilter
+ *  \sa VariationalRegistrationFunction
+ *
+ *  \ingroup FiniteDifferenceFunctions
+ *  \ingroup VariationalRegistration
+ *
+ *  \note This class was developed with funding from the German Research
+ *  Foundation (DFG: EH 224/3-1 and HA 235/9-1).
+ *  \author Alexander Schmidt-Richberg
+ *  \author Rene Werner
+ *  \author Jan Ehrhardt
+ */
+template <class TFixedImage, class TMovingImage, class TDisplacementField>
+class ITK_EXPORT VariationalRegistrationFastNCCFunction
+  : public VariationalRegistrationNCCFunction<TFixedImage, TMovingImage, TDisplacementField>
+{
+public:
+  /** Standard class typedefs. */
+  typedef VariationalRegistrationFastNCCFunction                                            Self;
+  typedef VariationalRegistrationNCCFunction<TFixedImage, TMovingImage, TDisplacementField> Superclass;
+  typedef SmartPointer<Self>                                                                Pointer;
+  typedef SmartPointer<const Self>                                                          ConstPointer;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(VariationalRegistrationFastNCCFunction, VariationalRegistrationNCCFunction);
+
+  /** Get image dimension. */
+  itkStaticConstMacro(ImageDimension, unsigned int, Superclass::ImageDimension);
+
+  /** MovingImage image type. */
+  typedef typename Superclass::MovingImageType    MovingImageType;
+  typedef typename Superclass::MovingImagePointer MovingImagePointer;
+
+  /** FixedImage image type. */
+  typedef typename Superclass::FixedImageType    FixedImageType;
+  typedef typename Superclass::FixedImagePointer FixedImagePointer;
+
+  /** MaskImage image type. */
+  typedef typename Superclass::MaskImageType    MaskImageType;
+  typedef typename Superclass::MaskImagePointer MaskImagePointer;
+
+  typedef typename FixedImageType::IndexType   IndexType;
+  typedef typename FixedImageType::SizeType    SizeType;
+  typedef typename FixedImageType::SpacingType SpacingType;
+
+  /** Deformation field type. */
+  typedef typename Superclass::DisplacementFieldType        DisplacementFieldType;
+  typedef typename Superclass::DisplacementFieldTypePointer DisplacementFieldTypePointer;
+
+  /** Inherit some types from the superclass. */
+  typedef typename Superclass::PixelType        PixelType;
+  typedef typename Superclass::RadiusType       RadiusType;
+  typedef typename Superclass::NeighborhoodType NeighborhoodType;
+  typedef typename Superclass::FloatOffsetType  FloatOffsetType;
+
+  /** Gradient calculator type. */
+  typedef typename Superclass::GradientCalculatorType GradientCalculatorType;
+  typedef typename GradientCalculatorType::Pointer    GradientCalculatorPointer;
+
+  /** This method is called by a finite difference solver image filter at
+   * each pixel that does not lie on a data set boundary */
+  virtual PixelType
+  ComputeUpdate(const NeighborhoodType & neighborhood,
+                void *                   globalData,
+                const FloatOffsetType &  offset = FloatOffsetType(0.0));
+
+  /** Return a pointer to a global data structure that is passed to
+   * this object from the solver at each calculation.  */
+  virtual void *
+  GetGlobalDataPointer() const;
+
+  /** Release memory for global data structure. */
+  virtual void
+  ReleaseGlobalDataPointer(void * GlobalData) const;
+
+protected:
+  VariationalRegistrationFastNCCFunction();
+  ~VariationalRegistrationFastNCCFunction() {}
+
+  /** Print information about the filter. */
+  virtual void
+  PrintSelf(std::ostream & os, Indent indent) const;
+
+  typedef typename Superclass::GlobalDataStruct GlobalDataStruct;
+
+  /** A global data type for this class of equation. Used to store
+   * information for computing the metric. */
+  struct NCCGlobalDataStruct
+  {
+    double            m_SumOfMetricValues;
+    SizeValueType     m_NumberOfPixelsProcessed;
+    double            m_SumOfSquaredChange;
+    IndexType         m_LastIndex;
+    bool              bValuesAreValid;
+    std::list<double> sfSliceValueList;
+    std::list<double> smSliceValueList;
+    std::list<double> sffSliceValueList;
+    std::list<double> smmSliceValueList;
+    std::list<double> sfmSliceValueList;
+    double            sfLastValue;
+    double            smLastValue;
+    double            sffLastValue;
+    double            smmLastValue;
+    double            sfmLastValue;
+  };
+
+private:
+  VariationalRegistrationFastNCCFunction(const Self &); // purposely not implemented
+  void
+  operator=(const Self &); // purposely not implemented
+};
+
+
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#  include "itkVariationalRegistrationFastNCCFunction.hxx"
+#endif
+
+#endif
---
+++ Modules/Registration/VariationalRegistration/include/itkVariationalRegistrationFastNCCFunction.hxx
@@ -0,0 +1,433 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef __itkVariationalRegistrationFastNCCFunction_hxx
+#define __itkVariationalRegistrationFastNCCFunction_hxx
+
+#include "itkVariationalRegistrationFastNCCFunction.h"
+#include "itkExceptionObject.h"
+#include "vnl/vnl_math.h"
+
+namespace itk
+{
+
+/**
+ * Default constructor
+ */
+template <class TFixedImage, class TMovingImage, class TDisplacementField>
+VariationalRegistrationFastNCCFunction<TFixedImage, TMovingImage, TDisplacementField>::
+  VariationalRegistrationFastNCCFunction()
+{}
+
+/*
+ * Standard "PrintSelf" method.
+ */
+template <class TFixedImage, class TMovingImage, class TDisplacementField>
+void
+VariationalRegistrationFastNCCFunction<TFixedImage, TMovingImage, TDisplacementField>::PrintSelf(std::ostream & os,
+                                                                                                 Indent indent) const
+{
+  Superclass::PrintSelf(os, indent);
+}
+
+/*
+ * Compute update at a non boundary neighbourhood
+ */
+template <class TFixedImage, class TMovingImage, class TDisplacementField>
+typename VariationalRegistrationFastNCCFunction<TFixedImage, TMovingImage, TDisplacementField>::PixelType
+VariationalRegistrationFastNCCFunction<TFixedImage, TMovingImage, TDisplacementField>::ComputeUpdate(
+  const NeighborhoodType & it,
+  void *                   gd,
+  const FloatOffsetType &  itkNotUsed(offset))
+{
+  NCCGlobalDataStruct * globalData = (NCCGlobalDataStruct *)gd;
+  assert(globalData != NULL);
+
+  // initialize update value to compute with zero
+  PixelType update;
+  update.Fill(0.0);
+
+  // Get the index at current location
+  const IndexType index = it.GetIndex();
+
+  // Check if index lies inside mask
+  const MaskImageType * mask = this->GetMaskImage();
+  if (mask)
+    if (mask->GetPixel(index) <= this->GetMaskBackgroundThreshold())
+    {
+      return update;
+    }
+
+  // flag whether boundary conditions have to be processed here
+  const bool bNeedToUseBoundaryCondition = it.GetNeedToUseBoundaryCondition();
+
+  // flag to show whether we can speed up computation
+  bool bProcessFollowingPixel = false;
+
+  // never speed up in boundary regions
+  if (!bNeedToUseBoundaryCondition)
+  {
+    //
+    // Check if we have already processed the previous pixel
+    //
+    if (index[0] == (globalData->m_LastIndex[0] + 1))
+    {
+      bProcessFollowingPixel = true;
+      for (unsigned int d = 1; d < FixedImageType::GetImageDimension(); d++)
+        if (index[d] != (globalData->m_LastIndex[d]))
+          bProcessFollowingPixel = false;
+    }
+  }
+  // save the last processed pixel index
+  globalData->m_LastIndex = index;
+
+  // We compute the CC on the fixed and warped moving image
+  // VariationalRegistrationFunction::WarpMovingImage() has to be called before
+  FixedImagePointer fixedImage = this->GetFixedImage();
+  FixedImagePointer warpedImage = this->GetWarpedImage();
+
+  //
+  // Get fixed image information and neighborhood radius
+  //
+  const RadiusType radius = it.GetRadius(); // should be same as this->GetRadius()
+
+  //
+  // Compute sums and mean in local neighborhood
+  // of current position index
+  // Iterate in current neighborhood to compute the following values:
+  // mean of fixed (f) and warped moving (m) image
+  // Sum f*f, Sum m*m, Sum m*f
+  double             sf = 0.0;
+  double             sm = 0.0;
+  double             sff = 0.0;
+  double             smm = 0.0;
+  double             sfm = 0.0;
+  unsigned int       pixelCounter = 0;
+  const unsigned int hoodSize = it.Size();
+  if (!bNeedToUseBoundaryCondition)
+  {
+    if (!bProcessFollowingPixel || !globalData->bValuesAreValid)
+    {
+      //
+      // Start a new line
+      //
+
+      // reset values in global data struct
+      globalData->sfSliceValueList.clear();
+      globalData->smSliceValueList.clear();
+      globalData->sffSliceValueList.clear();
+      globalData->smmSliceValueList.clear();
+      globalData->sfmSliceValueList.clear();
+
+      const unsigned int numNeighborhoodSlices = it.GetSize(0);
+      const unsigned int neighborhoodSliceSize = it.Size() / numNeighborhoodSlices;
+      const unsigned int neighborhoodSliceStride = it.GetSize(0);
+
+      // go over all slices
+      for (unsigned int slice = 0; slice < numNeighborhoodSlices; slice++)
+      {
+        // initialize values for this slice
+        double sfTemp = 0.0;
+        double smTemp = 0.0;
+        double sffTemp = 0.0;
+        double smmTemp = 0.0;
+        double sfmTemp = 0.0;
+
+        // go over all pixels in this slice
+        unsigned int indct = slice;
+        for (unsigned int i = 0; i < neighborhoodSliceSize; i++)
+        {
+          const IndexType neighIndex = it.GetIndex(indct);
+          const double    fixedNeighValue = static_cast<double>(fixedImage->GetPixel(neighIndex));
+          const double    movingNeighValue = static_cast<double>(warpedImage->GetPixel(neighIndex));
+
+          sfTemp += fixedNeighValue;
+          smTemp += movingNeighValue;
+          sffTemp += fixedNeighValue * fixedNeighValue;
+          smmTemp += movingNeighValue * movingNeighValue;
+          sfmTemp += fixedNeighValue * movingNeighValue;
+
+          // increment neighbour index by stride in direction 0
+          indct += neighborhoodSliceStride;
+          pixelCounter++;
+        }
+        // store the values for this slice in the lists
+        globalData->sfSliceValueList.push_back(sfTemp);
+        globalData->smSliceValueList.push_back(smTemp);
+        globalData->sffSliceValueList.push_back(sffTemp);
+        globalData->smmSliceValueList.push_back(smmTemp);
+        globalData->sfmSliceValueList.push_back(sfmTemp);
+        // add slice-values to the sum
+        sf += sfTemp;
+        sm += smTemp;
+        sff += sffTemp;
+        smm += smmTemp;
+        sfm += sfmTemp;
+      } // for slice=0 ..
+      globalData->sfLastValue = sf;
+      globalData->smLastValue = sm;
+      globalData->sffLastValue = sff;
+      globalData->smmLastValue = smm;
+      globalData->sfmLastValue = sfm;
+      globalData->bValuesAreValid = true;
+    }
+    else
+    {
+      //
+      // Continue the current line
+      //
+      // Inside a line we only have to modify the sum values of the last pixel.
+      // We have to subtract the values of slice leaving the neighborhood and we have
+      // to add the values of the slice entering the neighborhood
+
+      // subtract neighborhood slice going out of the neighborhood region
+      sf = globalData->sfLastValue - globalData->sfSliceValueList.front();
+      sm = globalData->smLastValue - globalData->smSliceValueList.front();
+      sff = globalData->sffLastValue - globalData->sffSliceValueList.front();
+      smm = globalData->smmLastValue - globalData->smmSliceValueList.front();
+      sfm = globalData->sfmLastValue - globalData->sfmSliceValueList.front();
+      // remove values from list of the leaving slice
+      globalData->sfSliceValueList.pop_front();
+      globalData->smSliceValueList.pop_front();
+      globalData->sffSliceValueList.pop_front();
+      globalData->smmSliceValueList.pop_front();
+      globalData->sfmSliceValueList.pop_front();
+
+      const unsigned int numNeighborhoodSlices = it.GetSize(0);
+      const unsigned int neighborhoodSliceSize = it.Size() / numNeighborhoodSlices;
+      const unsigned int neighborhoodSliceStride = it.GetSize(0);
+
+      // initialize values for this slice
+      double sfTemp = 0.0;
+      double smTemp = 0.0;
+      double sffTemp = 0.0;
+      double smmTemp = 0.0;
+      double sfmTemp = 0.0;
+
+      // go over all pixels in the last slice of the neighborhood
+      unsigned int indct = numNeighborhoodSlices - 1;
+      for (unsigned int i = 0; i < neighborhoodSliceSize; i++)
+      {
+        const IndexType neighIndex = it.GetIndex(indct);
+        const double    fixedNeighValue = static_cast<double>(fixedImage->GetPixel(neighIndex));
+        const double    movingNeighValue = static_cast<double>(warpedImage->GetPixel(neighIndex));
+
+        sfTemp += fixedNeighValue;
+        smTemp += movingNeighValue;
+        sffTemp += fixedNeighValue * fixedNeighValue;
+        smmTemp += movingNeighValue * movingNeighValue;
+        sfmTemp += fixedNeighValue * movingNeighValue;
+
+        // increment neighbour index by stride in direction 0
+        indct += neighborhoodSliceStride;
+      }
+      // add slice-values of the last slice to the sum
+      sf += sfTemp;
+      sm += smTemp;
+      sff += sffTemp;
+      smm += smmTemp;
+      sfm += sfmTemp;
+      // store the values for the last slice in the lists
+      globalData->sfSliceValueList.push_back(sfTemp);
+      globalData->smSliceValueList.push_back(smTemp);
+      globalData->sffSliceValueList.push_back(sffTemp);
+      globalData->smmSliceValueList.push_back(smmTemp);
+      globalData->sfmSliceValueList.push_back(sfmTemp);
+      globalData->sfLastValue = sf;
+      globalData->smLastValue = sm;
+      globalData->sffLastValue = sff;
+      globalData->smmLastValue = smm;
+      globalData->sfmLastValue = sfm;
+      globalData->bValuesAreValid = true;
+
+      pixelCounter = hoodSize;
+    }
+  }
+  else
+  {
+    //
+    // We have to account for boundry conditions
+    //
+    for (unsigned int indct = 0; indct < hoodSize; indct++)
+    {
+      const IndexType neighIndex = it.GetIndex(indct);
+      if (fixedImage->GetLargestPossibleRegion().IsInside(neighIndex))
+      {
+        const double fixedNeighValue = static_cast<double>(fixedImage->GetPixel(neighIndex));
+        const double movingNeighValue = static_cast<double>(warpedImage->GetPixel(neighIndex));
+
+        sf += fixedNeighValue;
+        sm += movingNeighValue;
+        sff += fixedNeighValue * fixedNeighValue;
+        smm += movingNeighValue * movingNeighValue;
+        sfm += fixedNeighValue * movingNeighValue;
+        pixelCounter++;
+      }
+    }
+    // values in global data struct are not valid if boundary computation occurs
+    globalData->bValuesAreValid = false;
+  }
+
+  const double fixedMean = sf / static_cast<double>(pixelCounter);
+  const double movingMean = sm / static_cast<double>(pixelCounter);
+
+  // Compute Sum_i (f-meanF)^2 , Sum_i (m-meanM)^2, Sum_i (f-meanF)(m-meanM)
+  // Sum_i (f-meanF)^2 = Sum_i f*f - 2* meanF*Sum_i f + Sum_i meanF*meanF
+  // Sum_i (f-meanF)(m-meanM) = Sum_i f*m - meanF*Sum_i m - meanM*Sum_i f + Sum_i meanF*meanM
+  const double SumFF = sff - 2 * fixedMean * sf + pixelCounter * fixedMean * fixedMean;
+  const double SumMM = smm - 2 * movingMean * sm + pixelCounter * movingMean * movingMean;
+  const double SumFM = sfm - fixedMean * sm - movingMean * sf + pixelCounter * movingMean * fixedMean;
+
+  // Compute cross correlation and derivative only for non-homogeneous regions
+  // cross correlation, if one region is homogeneous is here defined as 1
+  double localCrossCorrelation = 1.0;
+
+  // check for homogeneous region
+  const double SumFFMultSumMM = SumFF * SumMM;
+  // if(SumFF != 0.0 && SumMM != 0.0)
+  if (SumFFMultSumMM > 1.e-5)
+  {
+    // compute local cross correlation
+    localCrossCorrelation = SumFM * SumFM / SumFFMultSumMM;
+
+    // Get grayvalues for fixed and warped images
+    const double warpedValue = static_cast<double>(warpedImage->GetPixel(index));
+    const double fixedValue = static_cast<double>(fixedImage->GetPixel(index));
+
+    const double centerWarpedValue = warpedValue - movingMean;
+    const double centerFixedValue = fixedValue - fixedMean;
+
+    typename GradientCalculatorType::OutputType gradient;
+
+    // Compute the gradient of either fixed or warped moving image or as the mean of both (symmetric)
+    if (this->m_GradientType ==
+        VariationalRegistrationNCCFunction<TFixedImage, TMovingImage, TDisplacementField>::GRADIENT_TYPE_WARPED)
+    {
+      gradient = this->m_WarpedImageGradientCalculator->EvaluateAtIndex(index);
+    }
+    else if (this->m_GradientType ==
+             VariationalRegistrationNCCFunction<TFixedImage, TMovingImage, TDisplacementField>::GRADIENT_TYPE_FIXED)
+    {
+      gradient = this->m_FixedImageGradientCalculator->EvaluateAtIndex(index);
+    }
+    else if (this->m_GradientType ==
+             VariationalRegistrationNCCFunction<TFixedImage, TMovingImage, TDisplacementField>::GRADIENT_TYPE_SYMMETRIC)
+    {
+      gradient = 0.5 * (this->m_WarpedImageGradientCalculator->EvaluateAtIndex(index) +
+                        this->m_FixedImageGradientCalculator->EvaluateAtIndex(index));
+    }
+    else
+    {
+      itkExceptionMacro(<< "Unknown gradient type!");
+    }
+
+    // compute the gradient magnitude
+    // const double gradientMagnitude = gradient.GetSquaredNorm();
+
+    // Compute the derivative of LCC as given in Hermosillo et al., IJCV 50(3), 2002
+    // and Avants et al., Med Image Anal 12(1), 2008 (except Jacobian term):
+    //
+    //       2* Sum_i (f-meanF)(m-meanM)        (             Sum_i (f-meanF)(m-meanM)          )
+    //    -------------------------------------*( (m-meanM) - ------------------------*(f-meanF)) * gradient f
+    //    Sum_i (f-meanF)^2 * Sum_i (m-meanM)^2 (             Sum_i (f-meanF)^2                 )
+    //
+    const double preComputeFactor =
+      (2.0 * SumFM / SumFFMultSumMM) * (centerWarpedValue - SumFM / SumFF * centerFixedValue);
+
+    for (unsigned int dim = 0; dim < ImageDimension; dim++)
+    {
+      update[dim] = (-1) * preComputeFactor * gradient[dim];
+    }
+  }
+
+  // Update the global data (metric etc.)
+  if (globalData)
+  {
+    globalData->m_NumberOfPixelsProcessed += 1;
+    // use 1 - CC to get a decreasing metric value
+    globalData->m_SumOfMetricValues += 1.0 - localCrossCorrelation;
+    globalData->m_SumOfSquaredChange += update.GetSquaredNorm();
+  }
+
+  return update;
+}
+
+/**
+ * Returns an empty struct that is used by the threads to include the
+ * required update information for each thread.
+ */
+template <class TFixedImage, class TMovingImage, class TDisplacementField>
+void *
+VariationalRegistrationFastNCCFunction<TFixedImage, TMovingImage, TDisplacementField>::GetGlobalDataPointer() const
+{
+  NCCGlobalDataStruct * globalData = new NCCGlobalDataStruct();
+
+  globalData->m_SumOfMetricValues = 0.0;
+  globalData->m_NumberOfPixelsProcessed = 0L;
+  globalData->m_SumOfSquaredChange = 0;
+
+  globalData->sfSliceValueList.clear();
+  globalData->smSliceValueList.clear();
+  globalData->sffSliceValueList.clear();
+  globalData->smmSliceValueList.clear();
+  globalData->sfmSliceValueList.clear();
+  globalData->sfLastValue = 0;
+  globalData->smLastValue = 0;
+  globalData->sffLastValue = 0;
+  globalData->smmLastValue = 0;
+  globalData->sfmLastValue = 0;
+  globalData->bValuesAreValid = false;
+
+  // set the last index to a value outside the image
+  SizeType size = this->GetFixedImage()->GetLargestPossibleRegion().GetSize();
+  globalData->m_LastIndex = this->GetFixedImage()->GetLargestPossibleRegion().GetIndex();
+  for (unsigned int d = 0; d < FixedImageType::GetImageDimension(); d++)
+    globalData->m_LastIndex[d] += size[d];
+
+  return globalData;
+}
+
+/**
+ * Update the metric and release the per-thread-global data.
+ */
+template <class TFixedImage, class TMovingImage, class TDisplacementField>
+void
+VariationalRegistrationFastNCCFunction<TFixedImage, TMovingImage, TDisplacementField>::ReleaseGlobalDataPointer(
+  void * gd) const
+{
+  NCCGlobalDataStruct * globalData = (NCCGlobalDataStruct *)gd;
+
+  GlobalDataStruct * baseRegFunctionGlobalData = new GlobalDataStruct();
+  baseRegFunctionGlobalData->m_SumOfMetricValues = globalData->m_SumOfMetricValues;
+  baseRegFunctionGlobalData->m_NumberOfPixelsProcessed = globalData->m_NumberOfPixelsProcessed;
+  baseRegFunctionGlobalData->m_SumOfSquaredChange = globalData->m_SumOfSquaredChange;
+
+  VariationalRegistrationFunction<TFixedImage, TMovingImage, TDisplacementField>::ReleaseGlobalDataPointer(
+    baseRegFunctionGlobalData);
+
+  globalData->sfSliceValueList.clear();
+  globalData->smSliceValueList.clear();
+  globalData->sffSliceValueList.clear();
+  globalData->smmSliceValueList.clear();
+  globalData->sfmSliceValueList.clear();
+
+  delete globalData;
+}
+
+} // end namespace itk
+
+#endif
