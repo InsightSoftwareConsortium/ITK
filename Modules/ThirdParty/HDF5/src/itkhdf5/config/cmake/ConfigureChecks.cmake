149eace5aa95c675b77beb7b7abcbfaf49dff712
Matt McCormick <matt.mccormick@kitware.com>
2015-02-12 20:46:52 -0500

    BUG: HDF5 do not force CMake CheckTypeSize on include.
    This allows the version shipped with CMake to be overridden.
    
    Change-Id: I018a451abcca8555ed4d5649c52d9602c77fe1da

--- Modules/ThirdParty/HDF5/src/itkhdf5/config/cmake/ConfigureChecks.cmake
+++ Modules/ThirdParty/HDF5/src/itkhdf5/config/cmake/ConfigureChecks.cmake
@@ -1,15 +1,15 @@
 #-----------------------------------------------------------------------------
 # Include all the necessary files for macros
 #-----------------------------------------------------------------------------
-INCLUDE (${CMAKE_ROOT}/Modules/CheckFunctionExists.cmake)
-INCLUDE (${CMAKE_ROOT}/Modules/CheckIncludeFile.cmake)
-INCLUDE (${CMAKE_ROOT}/Modules/CheckIncludeFileCXX.cmake)
-INCLUDE (${CMAKE_ROOT}/Modules/CheckIncludeFiles.cmake)
-INCLUDE (${CMAKE_ROOT}/Modules/CheckLibraryExists.cmake)
-INCLUDE (${CMAKE_ROOT}/Modules/CheckSymbolExists.cmake)
-INCLUDE (${CMAKE_ROOT}/Modules/CheckTypeSize.cmake)
-INCLUDE (${CMAKE_ROOT}/Modules/CheckVariableExists.cmake)
-INCLUDE (${CMAKE_ROOT}/Modules/CheckFortranFunctionExists.cmake)
+INCLUDE (CheckFunctionExists)
+INCLUDE (CheckIncludeFile)
+INCLUDE (CheckIncludeFileCXX)
+INCLUDE (CheckIncludeFiles)
+INCLUDE (CheckLibraryExists)
+INCLUDE (CheckSymbolExists)
+INCLUDE (CheckTypeSize)
+INCLUDE (CheckVariableExists)
+INCLUDE (CheckFortranFunctionExists)
 
 #-----------------------------------------------------------------------------
 # Always SET this for now IF we are on an OS X box
