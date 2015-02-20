be4cc59a17b6e8f6557e159e0d26ffbb37aa6ca1
Matt McCormick <matt.mccormick@kitware.com>
2015-02-20 13:06:29 -0500

    BUG: Fix linking SmoothingRecursiveYvvGaussianFilter with install tree.
    This creates the missing library when ITK_USE_GPU is ON.
    
    Change-Id: I092f3b111bd02c2d03fed3a15583ba35a6d3a9a4

--- Modules/Remote/SmoothingRecursiveYvvGaussianFilter.remote.cmake
+++ Modules/Remote/SmoothingRecursiveYvvGaussianFilter.remote.cmake
@@ -2,5 +2,5 @@
 itk_fetch_module(SmoothingRecursiveYvvGaussianFilter
   "GPU and CPU Young & Van Vliet Recursive Gaussian Smoothing Filter: http://hdl.handle.net/10380/3425"
   GIT_REPOSITORY https://github.com/Inria-Asclepios/SmoothingRecursiveYvvGaussianFilter
-  GIT_TAG b3b6cab4848b4ad88b31384ada303ea2eeb663e4
+  GIT_TAG 69a6d9569c3a160872ba579f96db789df2ef07e6
   )
