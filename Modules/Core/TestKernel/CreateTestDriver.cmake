0c82b97b749cea9f409aba99c918b94295ed24c7
Matt McCormick <matt.mccormick@kitware.com>
2015-02-22 02:26:36 -0500

    ENH: Mount local filesystem in the test driver for Emscripten.
    This allows the local source tree and binary tree to be seen by system when
    tests are run in node.js.
    
    Do not mount /home because emscripten automatically creates this directory
    on startup.
    
    Change-Id: I71143025353375d6545cc35bdfd7f108ab080c13

--- Modules/Core/TestKernel/CreateTestDriver.cmake
+++ Modules/Core/TestKernel/CreateTestDriver.cmake
@@ -9,9 +9,48 @@
 #   ADDITIONAL_SRC (optional) - additional source files, which don't contain tests
 
 macro(CreateTestDriver KIT KIT_LIBS KitTests)
-  set( ADDITIONAL_SRC ${ARGN} )
-  set(CMAKE_TESTDRIVER_BEFORE_TESTMAIN "#include \"itkTestDriverBeforeTest.inc\"")
-  set(CMAKE_TESTDRIVER_AFTER_TESTMAIN "#include \"itkTestDriverAfterTest.inc\"")
+  set(ADDITIONAL_SRC ${ARGN})
+  if(EMSCRIPTEN)
+    set(emscripten_before "
+EM_ASM(
+  var cmake_source_dir = '${CMAKE_SOURCE_DIR}'.split('/');
+  // This is intentionally global so it can be unmounted at the end.
+  source_mount_dir = null;
+  if(cmake_source_dir[1] === 'home') {
+    source_mount_dir = cmake_source_dir.slice(0, 3).join('/');
+    }
+  else {
+    source_mount_dir = cmake_source_dir.slice(0, 2).join('/');
+    }
+  FS.mkdir(source_mount_dir);
+  FS.mount(NODEFS, { root: source_mount_dir }, source_mount_dir);
+
+  var cmake_binary_dir = '${CMAKE_BINARY_DIR}'.split('/');
+  // This is intentionally global so it can be unmounted at the end.
+  binary_mount_dir = null;
+  if(cmake_binary_dir[1] === 'home') {
+    binary_mount_dir = cmake_binary_dir.slice(0, 3).join('/');
+    }
+  else {
+    binary_mount_dir = cmake_binary_dir.slice(0, 2).join('/');
+    }
+  if(source_mount_dir != binary_mount_dir) {
+    FS.mkdir(binary_mount_dir);
+    FS.mount(NODEFS, { root: binary_mount_dir }, binary_mount_dir);
+    }
+  );
+")
+    set(emscripten_after "
+EM_ASM(
+  FS.unmount(source_mount_dir);
+  if(source_mount_dir != binary_mount_dir) {
+    FS.unmount(binary_mount_dir);
+    }
+  );
+")
+  endif()
+  set(CMAKE_TESTDRIVER_BEFORE_TESTMAIN "${emscripten_before}#include \"itkTestDriverBeforeTest.inc\"")
+  set(CMAKE_TESTDRIVER_AFTER_TESTMAIN "#include \"itkTestDriverAfterTest.inc\"${emscripten_after}")
   create_test_sourcelist(Tests ${KIT}TestDriver.cxx
     ${KitTests}
     EXTRA_INCLUDE itkTestDriverIncludeRequiredIOFactories.h
@@ -24,15 +63,54 @@ endmacro()
 
 
 macro(CreateTestDriver_SupportBuildInIOFactories KIT KIT_LIBS KitTests)
-   set( ADDITIONAL_SRC ${ARGN} )
-   set(CMAKE_TESTDRIVER_BEFORE_TESTMAIN  "#include \"itkTestDriverBeforeTest.inc\"")
-   set(CMAKE_TESTDRIVER_AFTER_TESTMAIN "#include \"itkTestDriverAfterTest.inc\"")
-   create_test_sourcelist(Tests ${KIT}TestDriver.cxx
-     ${KitTests}
-     EXTRA_INCLUDE  itkTestDriverIncludeBuiltInIOFactories.h
-     FUNCTION  ProcessArgumentsAndRegisterBuiltInFactories
-     )
-   add_executable(${KIT}TestDriver ${KIT}TestDriver.cxx ${Tests} ${ADDITIONAL_SRC})
-   target_link_libraries(${KIT}TestDriver ${KIT_LIBS} ${ITKTestKernel_LIBRARIES})
-   itk_module_target_label(${KIT}TestDriver)
+  set(ADDITIONAL_SRC ${ARGN} )
+  if(EMSCRIPTEN)
+    set(emscripten_before "
+EM_ASM(
+  var cmake_source_dir = '${CMAKE_SOURCE_DIR}'.split('/');
+  // This is intentionally global so it can be unmounted at the end.
+  source_mount_dir = null;
+  if(cmake_source_dir[1] === 'home') {
+    source_mount_dir = cmake_source_dir.slice(0, 3).join('/');
+    }
+  else {
+    source_mount_dir = cmake_source_dir.slice(0, 2).join('/');
+    }
+  FS.mkdir(source_mount_dir);
+  FS.mount(NODEFS, { root: source_mount_dir }, source_mount_dir);
+
+  // This is intentionally global so it can be unmounted at the end.
+  binary_mount_dir = null;
+  var cmake_binary_dir = '${CMAKE_BINARY_DIR}'.split('/');
+  if(cmake_binary_dir[1] === 'home') {
+    binary_mount_dir = cmake_binary_dir.slice(0, 3).join('/');
+    }
+  else {
+    binary_mount_dir = cmake_binary_dir.slice(0, 2).join('/');
+    }
+  if(source_mount_dir != binary_mount_dir) {
+    FS.mkdir(binary_mount_dir);
+    FS.mount(NODEFS, { root: binary_mount_dir }, binary_mount_dir);
+    }
+  );
+")
+    set(emscripten_after "
+EM_ASM(
+  FS.unmount(source_mount_dir);
+  if(source_mount_dir != binary_mount_dir) {
+    FS.unmount(binary_mount_dir);
+    }
+  );
+")
+  endif()
+  set(CMAKE_TESTDRIVER_BEFORE_TESTMAIN "${emscripten_before}#include \"itkTestDriverBeforeTest.inc\"")
+  set(CMAKE_TESTDRIVER_AFTER_TESTMAIN "#include \"itkTestDriverAfterTest.inc\"${emscripten_after}")
+  create_test_sourcelist(Tests ${KIT}TestDriver.cxx
+    ${KitTests}
+    EXTRA_INCLUDE  itkTestDriverIncludeBuiltInIOFactories.h
+    FUNCTION  ProcessArgumentsAndRegisterBuiltInFactories
+    )
+  add_executable(${KIT}TestDriver ${KIT}TestDriver.cxx ${Tests} ${ADDITIONAL_SRC})
+  target_link_libraries(${KIT}TestDriver ${KIT_LIBS} ${ITKTestKernel_LIBRARIES})
+  itk_module_target_label(${KIT}TestDriver)
 endmacro()
