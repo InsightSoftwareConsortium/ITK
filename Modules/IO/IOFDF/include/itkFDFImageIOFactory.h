dcf3c193db6127081c0d2805176f33f37a4279d1
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2015-02-26 08:57:02 +0100

    BUG: fix factory class name

--- Modules/IO/IOFDF/include/itkFDFImageIOFactory.h
+++ Modules/IO/IOFDF/include/itkFDFImageIOFactory.h
@@ -45,7 +45,7 @@ public:
   itkFactorylessNewMacro(Self);
 
   /** Run-time type information (and related methods). */
-  itkTypeMacro(DicomImageIOFactory, ObjectFactoryBase);
+  itkTypeMacro(FDFImageIOFactory, ObjectFactoryBase);
 
   /** Register one factory of this type  */
   static void
