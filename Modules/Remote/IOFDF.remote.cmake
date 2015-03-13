b6534f041a760f23a647d6dffbf67309db048c8d
Bradley Lowekamp <blowekamp@mail.nih.gov>
2015-03-13 10:42:10 -0400

    STYLE: Update IOFDF to address KWStyle issues
    Change-Id: I32d97002d17f6b3eef146566e89e516e06636beb

--- Modules/Remote/IOFDF.remote.cmake
+++ Modules/Remote/IOFDF.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(IOFDF
   "FDFImageIO plugin for ITK. Authors Gleen Pierce/Nick Tustison/Kent Williams"
   GIT_REPOSITORY http://github.com/InsightSoftwareConsortium/itkFDFImageIO.git
-  GIT_TAG  d92ed1cceb7a4b393795998f34fda58a44fcbe28
+  GIT_TAG  0805a5e7ff0bd60cf3e5e59830223a62dc988d2e
   )
