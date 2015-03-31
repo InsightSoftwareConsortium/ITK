cf7325da3d7e64b9a7138fc2d3e186c4b60ff1b1
Bradley Lowekamp <blowekamp@mail.nih.gov>
2015-03-31 16:28:01 -0400

    BUG: Update MGHImageIO for header guarder style
    Addresses failing KWStyle test.
    
    Remove usage of reserved "__" prefix.
    
    Change-Id: Ic92657fa61d1c78c0168b8fa7317d2de6bb98ec2

--- Modules/Remote/MGHIO.remote.cmake
+++ Modules/Remote/MGHIO.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(MGHIO
   "MGHIO ImageIO plugin for ITK"
   GIT_REPOSITORY https://github.com/Slicer/itkMGHImageIO.git
-  GIT_TAG f4e475c47f5c246b1ebf0529e052f65b73c7d13d
+  GIT_TAG a7fe91bbc8f06e57c3d988862ae02ba313049042
   )
