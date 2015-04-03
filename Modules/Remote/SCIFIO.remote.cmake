8da468fe19f966f2b7661d1729d39e617773b0b9
Bradley Lowekamp <blowekamp@mail.nih.gov>
2015-04-03 11:36:52 -0400

    STYLE: Update SCIFIO for KWStyke failures
    Change-Id: I59746f951013cd19fdff9b5aea6900d67f050253

--- Modules/Remote/SCIFIO.remote.cmake
+++ Modules/Remote/SCIFIO.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(SCIFIO
   "SCIFIO (Bioformats) ImageIO plugin for ITK"
   GIT_REPOSITORY https://github.com/scifio/scifio-imageio.git
-  GIT_TAG 789c08071bbcd4341eba76fda10582a8d5fb7626
+  GIT_TAG 9346dbbf30b64645ebd9546fbf82f5438dc88a48
   )
