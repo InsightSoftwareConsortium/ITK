db56773985ef368726a0e3edeb720b3a37acdf21
Bradley Lowekamp <blowekamp@mail.nih.gov>
2015-04-03 08:59:30 -0400

    BUG: Fix itkFDFImageIOTest link error on windows
    Update IOFDF remote module. Use correct main signature on test for
    windows.
    
    Change-Id: I468dbd131f9a79f3dd466172ec1ff4087a2a3ffb

--- Modules/Remote/IOFDF.remote.cmake
+++ Modules/Remote/IOFDF.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(IOFDF
   "FDFImageIO plugin for ITK. Authors Gleen Pierce/Nick Tustison/Kent Williams"
   GIT_REPOSITORY http://github.com/InsightSoftwareConsortium/itkFDFImageIO.git
-  GIT_TAG  eff61805983c8a92b7a2043b94272d7b865c5a06
+  GIT_TAG 145e6342bd340919574003e4e324b0b9ebf49d26
   )
