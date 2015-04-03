c48c8fe046a72ccaf82d5f4005a93ee59e48e22f
Matt McCormick <matt.mccormick@kitware.com>
2015-04-03 10:58:56 -0400

    STYLE: Update IOSTL module for KWStyle test.
    Addresses __ in header guards.
    
    Change-Id: Iee45e8a9e3c6a9a019a1a8ba7130633b305c8edd

--- Modules/Remote/IOSTL.remote.cmake
+++ Modules/Remote/IOSTL.remote.cmake
@@ -3,5 +3,5 @@ itk_fetch_module(IOSTL
   "This module contains classes for reading and writing QuadEdgeMeshes using
   the STL (STereoLithography)file format. http://hdl.handle.net/10380/3452"
   GIT_REPOSITORY https://github.com/InsightSoftwareConsortium/ITKSTLMeshIO.git
-  GIT_TAG 933359a5fd1bac792a3c0af5f4e05e291e153677
+  GIT_TAG 300ffffbe07a42182d54b67660eb7058f893ffa2
   )
