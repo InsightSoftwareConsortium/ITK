097e0a8dce2e35a94acd82780bd00e3438d368dc
Richard Beare <richard.beare@monash.edu>
2015-04-06 20:23:45 +1000

    COMP: cmake stuff in top level

---
+++ Modules/Filtering/ParabolicMorphology/itk-module.cmake
@@ -0,0 +1,15 @@
+set(
+  DOCUMENTATION
+  "This module contains classes for mathematical morphology using parabolic functions.
+Parabolic functions can be used to build fast distance transforms, and
+binary morphology using spheres."
+)
+itk_module(
+  ParabolicMorphology
+  DEPENDS
+    ITKIOImageBase
+  TEST_DEPENDS
+    ITKTestKernel
+  EXCLUDE_FROM_DEFAULT
+  DESCRIPTION "${DOCUMENTATION}"
+)
