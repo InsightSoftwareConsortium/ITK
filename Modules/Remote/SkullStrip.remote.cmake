5081fad4c7ebf827c90ba3fb2a4c95c585981f0c
Bill Lorensen <bill.lorensen@gmail.com>
2015-04-07 11:40:18 -0700

    ENH: Update to KWStyle conformant version
    SkullStrip is now KWStyle conformant.
    
    Change-Id: Ic125b6f1aacdc1fabf550cec0a43d1ac2ecb05fd

--- Modules/Remote/SkullStrip.remote.cmake
+++ Modules/Remote/SkullStrip.remote.cmake
@@ -1,5 +1,6 @@
 itk_fetch_module(SkullStrip
   "A class to perform automatic skull-stripping for neuroimage analysis."
   GIT_REPOSITORY https://github.com/lorensen/skullStrip.git
-  GIT_TAG master
+  # April 7, 2015 Fixed KWStyle issues
+  GIT_TAG 2c4ded1afd24607de921e37cf56693cc5fa05570
   )
