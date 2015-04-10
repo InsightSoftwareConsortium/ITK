331fcfa931a4fa4f22009779d99b14e8045f5ee2
Bill Lorensen <bill.lorensen@gmail.com>
2015-04-10 12:31:57 -0700

    ENH: WikiExamples QuickView Update
    Improvements to QuickView.
    
    Superbuild updated to latest VTK and ITK.
    
    Some new examples.
    
    Change-Id: I84f0a9cc1a50285f7792ee7e15ba666b1644155d

--- Modules/Remote/WikiExamples.remote.cmake
+++ Modules/Remote/WikiExamples.remote.cmake
@@ -1,13 +1,10 @@
 #
 # ITK WikiExamples
-#  This remote module require a VTK build
-#  The following CMake variable must be set for the ITK build
 #
-# Check for requires cmake variables
 
 itk_fetch_module(WikiExamples
   "A collection of examples that illustrate how to use ITK."
   GIT_REPOSITORY https://github.com/InsightSoftwareConsortium/ITKWikiExamples.git
-  # January 05, 2015
-  GIT_TAG 42c129a898641934a84339c9a0ab69c5295f7dbe
+  # April 10, 2015
+  GIT_TAG e7c9adef49e30b7cb2cd15cd5a857e5ac25c745b
   )
