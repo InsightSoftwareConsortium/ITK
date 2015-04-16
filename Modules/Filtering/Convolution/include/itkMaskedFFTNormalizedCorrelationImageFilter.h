a8257f98ede40593f69258ba0a4b7835ab297c9a
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2015-04-16 16:12:45 +0200

    BUG: fix runtime information for cross correlation filters
    Change-Id: I3774da5c0cf9bd628bbe34a21a6a0cc0b0b468f6

--- Modules/Filtering/Convolution/include/itkMaskedFFTNormalizedCorrelationImageFilter.h
+++ Modules/Filtering/Convolution/include/itkMaskedFFTNormalizedCorrelationImageFilter.h
@@ -150,7 +150,7 @@ public:
   itkNewMacro(Self);
 
   /** Run-time type information (and related methods). */
-  itkTypeMacro(MaskedFFTNormalizedCorrelationImageFilter, MaskedFFTNormalizedCrossCorrelationImageFilter);
+  itkTypeMacro(MaskedFFTNormalizedCorrelationImageFilter, ImageToImageFilter);
 
   /** Extract some information from the image types.  Dimensionality
    * of the two images is assumed to be the same. */
