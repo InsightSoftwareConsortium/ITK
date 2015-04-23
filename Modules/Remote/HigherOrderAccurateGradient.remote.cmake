d6f0695569f6c24a9d7cb73096695cc7895f40d0
Matt McCormick <matt.mccormick@kitware.com>
2015-04-23 15:46:48 -0400

    STYLE: Update HigherOrderAccurateGradient module for style fixes.
    Addresses double underscore header guard KWStyle test failures.
    
    Change-Id: Ibad57ef302b317f25276bd9e7c4b1be566acd72c

--- Modules/Remote/HigherOrderAccurateGradient.remote.cmake
+++ Modules/Remote/HigherOrderAccurateGradient.remote.cmake
@@ -7,5 +7,5 @@ itk_fetch_module(HigherOrderAccurateGradient
   http://www.insight-journal.org/browse/publication/775
   http://hdl.handle.net/10380/3231"
   GIT_REPOSITORY https://github.com/InsightSoftwareConsortium/ITKHigherOrderAccurateGradient.git
-  GIT_TAG 011136180ec1779a4a6eb2294efd06deefe0fe7a
+  GIT_TAG 25e3ff135678fad2492db6aa5aa9fadc26d725e8
   )
