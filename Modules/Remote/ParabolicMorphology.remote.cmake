2b3190575b82e8457c028040c3bfd0f69fa1ef35
Richard Beare <Richard.Beare@gmail.com>
2015-05-12 20:30:05 +1000

    ENH: remote.cmake for parabolic morphology
    Change-Id: I7fb384db4ce1df58f6205d73bae62dbd22004666
    
    DOC: extended description
    
    Change-Id: Ie8049b63cb36c0fccd6bb70b25e1d07cdd39fd1c
    
    DOC: added link to IJ and changed git repo to ITK consortium fork
    
    Change-Id: Ic443ee0a7ad595b3d237afaec8a6f6f3a53c0891
    
    ENH: changed git referenced to SHA ready for merge

---
+++ Modules/Remote/ParabolicMorphology.remote.cmake
@@ -0,0 +1,9 @@
+itk_fetch_module(ParabolicMorphology
+  "Classes performing morphology using parabolic functions.
+  Fast distance transforms and binary erosions/dilations/openings/closings
+  by spheres, sharpenings and grayscale operations.
+  http://www.insight-journal.org/browse/publication/228
+  http://hdl.handle.net/1926/1370"
+  GIT_REPOSITORY https://github.com/InsightSoftwareConsortium/parabolicMorphology.git
+  GIT_TAG 25ce7abb79e2a250c23654b479872c0cc289ce12
+  )
