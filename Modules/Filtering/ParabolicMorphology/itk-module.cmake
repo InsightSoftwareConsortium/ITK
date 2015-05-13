b7f4b76b61352bdf979c9e22cfbc68a03bb92c35
Richard Beare <richard.beare@monash.edu>
2015-05-13 21:28:01 +1000

    ENH: put the links in thr group documentation

--- Modules/Filtering/ParabolicMorphology/itk-module.cmake
+++ Modules/Filtering/ParabolicMorphology/itk-module.cmake
@@ -2,7 +2,9 @@ set(
   DOCUMENTATION
   "This module contains classes for mathematical morphology using parabolic functions.
 Parabolic functions can be used to build fast distance transforms, and
-binary morphology using spheres."
+binary morphology using spheres.
+  http://www.insight-journal.org/browse/publication/228
+  http://hdl.handle.net/1926/1370"
 )
 itk_module(
   ParabolicMorphology
