5b158490c01b3e3a68816fdbe321751609746444
Richard Beare <Richard.Beare@gmail.com>
2015-05-13 21:34:01 +1000

    ENH: LabelErodeDilate as a remote module
    Change-Id: I2b4407253f4aa004565853c710525d7d4afec1de

---
+++ Modules/Remote/LabelErodeDilate.remote.cmake
@@ -0,0 +1,11 @@
+itk_fetch_module(LabelErodeDilate
+  "Classes performing morphology efficiently on label images.
+  Label collisions are consistently handled, and
+  operations are efficient (approximately constant time wrt
+  structuring element size). Only circular/spherical/hyperspherical
+  structuring elements are supported.
+  http://www.insight-journal.org/browse/publication/228
+  http://hdl.handle.net/10380/3399"
+  GIT_REPOSITORY /home/richardb/Projects/ITK/LabelErodeDilate/
+  GIT_TAG RemoteModule
+  )
