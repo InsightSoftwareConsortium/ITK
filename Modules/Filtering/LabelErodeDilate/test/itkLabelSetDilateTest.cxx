ba7cb4f0c1554d2182fd04558f21396a5658d696
Richard Beare <richard.beare@monash.edu>
2015-05-14 21:44:55 +1000

    readimageinfo

--- Modules/Filtering/LabelErodeDilate/test/itkLabelSetDilateTest.cxx
+++ Modules/Filtering/LabelErodeDilate/test/itkLabelSetDilateTest.cxx
@@ -49,6 +49,20 @@ int doDilate(char *In, char *Out, int radius)
 }
 
 /////////////////////////////////
+int readImageInfo(std::string filename, itk::ImageIOBase::IOComponentType *ComponentType, int *dim)
+{
+  itk::ImageIOBase::Pointer imageIO = itk::ImageIOFactory::CreateImageIO(filename.c_str(), itk::ImageIOFactory::ReadMode);
+  if (imageIO.IsNull())
+    return 0;
+
+
+  imageIO->SetFileName(filename.c_str());
+  imageIO->ReadImageInformation();
+
+  *ComponentType = imageIO->GetComponentType();
+  *dim = imageIO->GetNumberOfDimensions();
+  return(1);
+}
 
 int itkLabelSetDilateTest(int argc, char * argv[])
 {
