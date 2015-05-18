4524131bbd4b4fd8b0ad128b200398a06b011732
Richard Beare <richard.beare@monash.edu>
2015-05-18 18:30:04 +1000

    moved old tests, uncrustified

--- Modules/Filtering/LabelErodeDilate/test/read_info.h
+++
@@ -1,9 +0,0 @@
-#ifndef _read_info_h
-#define _read_info_h
-/////////////////////////////////
-#include "itkImageFileReader.h"
-
-int readImageInfo(char *filename, itk::ImageIOBase::IOComponentType *ComponentType, int *dim);
-
-/////////////////////////////////
-#endif
