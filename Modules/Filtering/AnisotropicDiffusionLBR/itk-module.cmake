8f642535acdd5e71b5b236b298ea1b95eb59c970
Matt McCormick <matt.mccormick@kitware.com>
2015-05-27 21:40:28 -0400

    DOC: Add link to the IJ article in the module documentation.

--- Modules/Filtering/AnisotropicDiffusionLBR/itk-module.cmake
+++ Modules/Filtering/AnisotropicDiffusionLBR/itk-module.cmake
@@ -1,4 +1,11 @@
-set(DOCUMENTATION "This module implements Anisotropic Diffusion, using Lattice Basis Reduction.")
+set(DOCUMENTATION "This module implements Anisotropic Diffusion, using Lattice Basis Reduction.
+
+Documentation can be found in the Insight Journal article, 'Anisotropic
+Diffusion in ITK', by Mirebeau J., Fehrenbach J., Risser L., Tobji S.
+
+http://insight-journal.org/browse/publication/953
+http://hdl.handle.net/10380/3505
+")
 
 # itk_module() defines the module dependencies in ITKAnisotropicFastMarchingLBR;
 # ITKAnisotropicFastMarchingLBR depends on ITKCommon;
