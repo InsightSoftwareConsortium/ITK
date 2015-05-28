27c8b2256ebe95c221e724a6dff3f866f7b44995
Matt McCormick <matt.mccormick@kitware.com>
2015-05-27 23:25:29 -0400

    BUG: Add missing license headers.

--- Modules/Filtering/AnisotropicDiffusionLBR/include/itkLinearAnisotropicDiffusionLBRImageFilter.h
+++ Modules/Filtering/AnisotropicDiffusionLBR/include/itkLinearAnisotropicDiffusionLBRImageFilter.h
@@ -1,6 +1,20 @@
-//
-//  LinearAnisotropicDiffusionLBRImageFilter.h
-//  itkDiffusion
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
 //
 //  Created by Jean-Marie Mirebeau on 28/02/2014.
 //
@@ -15,6 +29,8 @@
 namespace itk
 {
 /**
+ * \class LinearAnisotropicDiffusionLBRImageFilter
+ *
  Implementation of Anisotropic Diffusion
  \f[\partial_t u = {\rm div} (D \nabla u),\f]
  with Neumann boundary conditions. The numerical scheme is stable and satisfies the maximum principle, even for strongly anisotropic tensors, thanks to an adaptive discretization using arithmetic techniques (Lattice Basis Reduction, LBR).
