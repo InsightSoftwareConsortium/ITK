859cbde3218e8b6fcb587790e7d6b44da44228d5
Matt McCormick <matt.mccormick@kitware.com>
2015-06-27 22:04:04 -0400

    DOC: Update Wiki Examples Remote to June 26, 2015.
    Change-Id: If4752aca9d34e6ea661ba8894354ac7fcc21dc75

--- Modules/Remote/WikiExamples.remote.cmake
+++ Modules/Remote/WikiExamples.remote.cmake
@@ -5,6 +5,6 @@
 itk_fetch_module(WikiExamples
   "A collection of examples that illustrate how to use ITK."
   GIT_REPOSITORY https://github.com/InsightSoftwareConsortium/ITKWikiExamples.git
-  # April 10, 2015
-  GIT_TAG e7c9adef49e30b7cb2cd15cd5a857e5ac25c745b
+  # June 26, 2015
+  GIT_TAG 74e09f3e88cf2168e35af197e4b27a8686aa8f22
   )
