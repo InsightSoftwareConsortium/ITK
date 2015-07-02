2539320ce8266ee837a71994b22c0556afda3962
Luc Hermitte <luc.hermitte@c-s.fr>
2015-07-02 19:08:02 +0200

    PERF: Performance enhancements on VariableLengthVectors 1/...
    VariableLengthVector pixels induce many inefficiencies on algorithms and
    filters working on VectorImages.
    
    This commit is the first of a series on the subject.
    More explanations on what's to come are available on the draft page
    (http://wiki.orfeo-toolbox.org/index.php/OptimizingVectorImageProcessing).
    
    tl;dr: the cost of transformations like LinearInterpolateImageFunction on
    monoband itk::VectorImage<> shall be as close as the cost of the same
    processing while the image is loaded as a itk::Image. And it can be done.
    
    First things first.
    Today implementation of VLV assignment operator induce two things that 99% of
    the time never make sense:
    - we only need to allocate memory the first time -- within loops this cost
      makes no sense; I assume the size of a pixel is stable.
    - we never need to keep old values
    
    Moreover, there wasn't any version of SetSize() that permits to avoid
    reallocations or to duplicate values when we know we don't need that.
    
    The choices I've made here are:
    - To provide a very-low cost overload of SetSize() that permits end user to
      choose without any ambiguity what he exactly wants to acheive.
      Hence the two policies about Reallocations, and about KeepingOldValues.
    - The behaviour of the current overload hasn't been changed
    - The code has been factorized, and IMO simplified (this may be subjective).
    
    The difficulties I had are related to the fact that sometimes VLV are acting as
    proxies (to memory hold elsewhere). I've tried to express the contracts (as in
    Design By Contract) of the various operations that implies reallocations or
    assignments.
    
    In an ideal world, we should not be permitted to assign values to Proxies. In
    other words, proxies should be restricted to types like
    VariableLengthVector<const double> instead of VariableLengthVector<double>.
    This way, we could have easily asserted that the size of proxy VLVs cannot be
    changed, and that proxy VLV could not be assigned. I'm afraid it's to late to
    proceed to such radical changes.
    
    Notes:
    * I've proceeded to other optimizations. For instance, I've got rid of the
    auto-assignment pessimistic test. As the assignment is written to provide a
    Strong Exception Guarantee, the only thing the auto assignment test does is to
    have a us pay for a test that tries to optimize a situation that seldom, if
    never, occur in normal code. Thanks to the Strong Exception Guarantee (assuming
    that copy operations on the value type are noexcept), the code will resist to
    auto-assignment.
    
    * I've hesitated to provide move assignment and move copy. It would require
    some testing and macros, but it should be easy to add. For another patch may
    be?
    
    Change-Id: I0b07c5157cf5b8ae1b5b1de1181d7db4364596ed

---
+++ Modules/Core/Common/include/itkStaticAssert.h
@@ -0,0 +1,64 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#ifndef itkStaticAssert_h
+#define itkStaticAssert_h
+
+#include "itkMacro.h"
+
+#if defined(ITK_HAS_CXX11_STATIC_ASSERT)
+//  Use the C++11 static_assert if available
+
+/** Static assertion.
+ * This macro will either emulate static assertions on the current compiler or
+ * use C++11 \c static_assert() keyword.
+ * \param expr compile-time expression to check
+ * \param str  string literal that will appear (C++11, GCC 4.3+) as compiler
+ * error if \c expr is \c false.
+ * \ingroup ITKCommon
+ */
+#   define itkStaticAssert(expr, str) static_assert(expr, str)
+#elif ((__GNUC__ * 100) + __GNUC_MINOR__ ) >= 403
+//  GCC 4.3 is enough for this trick
+#   define itkStaticAssert(expr,str)                                  \
+      ({extern int __attribute__((error(str))) StaticAssertFailure(); \
+       ((void)((expr) ? 0: StaticAssertFailure()), 0);                \
+       })
+#else
+//  Usual trick (boost, clang, ...), but it will loose the error message on the
+//  way
+#   define ITK_JOIN(X,Y)     ITK_DO_JOIN(X,Y)
+#   define ITK_DO_JOIN(X,Y)  ITK_DO_JOIN2(X,Y)
+#   define ITK_DO_JOIN2(X,Y) X##Y
+
+namespace itk {
+/** \cond HIDE_META_PROGRAMMING */
+/** Internal class to emulate static assertions of pre-C++11 compilers.
+ * \sa \c itkStaticAssert
+ * \ingroup Common
+ */
+template <bool V> struct StaticAssertFailure;
+template <> struct StaticAssertFailure<true>{};
+/** \endcond */
+} // itk namespace
+
+#   define itkStaticAssert(expr,str) \
+      enum { ITK_JOIN(static_assert_typedef, __LINE__) = sizeof(itk::StaticAssertFailure<((expr) == 0 ? false : true)>) };
+#endif
+
+#endif // itkStaticAssert_h
--- Modules/Core/Common/test/itkVariableLengthVectorTest.cxx
+++ Modules/Core/Common/test/itkVariableLengthVectorTest.cxx
@@ -19,6 +19,10 @@
 #include <iostream>
 #include "itkVariableLengthVector.h"
 
+#define ASSERT(cond, text) \
+  if (!(cond))             \
+    std::cout << __FILE__ << ":" << __LINE__ << ":" << "Assertion failed: " << #cond << ": " << (text) << std::endl;
+
 int itkVariableLengthVectorTest(int, char*[])
 {
   typedef itk::VariableLengthVector<float>   FloatVariableLengthVectorType;
@@ -57,27 +61,178 @@ int itkVariableLengthVectorTest(int, char*[])
     DoubleVariableLengthVectorType x( d, 3, false );
     if( (d[0] != 0.1) || (x[0] != 0.1) )
       {
-      std::cerr << "Memory management: [FAILED]" << std::endl;
+      std::cerr << "Memory management(1): [FAILED]" << std::endl;
       }
     std::cout << x << std::endl;
     x.SetSize( 5 , false);
     x[3] = 3.0;
     x[4] = 4.0;
-    std::cout << x << std::endl;
+    std::cout << d[0] << "->" << x << std::endl;
     if( (d[0] != 0.1) || (x[0] != 0.1) ) // increase length but preserve existing data
       {
-      std::cerr << "Memory management: [FAILED]" << std::endl;
+      std::cerr << "Memory management(2): [FAILED]" << std::endl;
       }
     x.SetSize( 2 , false); // reduce length but preserve existing data
     std::cout << x << std::endl;
     if( (x.GetSize() != 2) || (d[0] != 0.1) || (x[0] != 0.1) )
       {
-      std::cerr << "Memory management: [FAILED]" << std::endl;
+      std::cerr << "Memory management(3): [FAILED]" << std::endl;
       }
-     x.SetSize( 5 , true); // increase size, destroy data.
-     x.SetSize( 7 , true); // increase size, destroy data.
-     x.SetSize( 6 , true); // decrease size, destroy data.
-     }
+    x.SetSize( 5 , true); // increase size, destroy data.
+    x.SetSize( 7 , true); // increase size, destroy data.
+    x.SetSize( 6 , true); // decrease size, destroy data.
+    }
+
+  // Tests for SetSize(size, allocation policy, values keeping policy)
+    {
+    DoubleVariableLengthVectorType ref( d, 3, false );
+    ASSERT( ref.IsAProxy(), "Unexpected Reference VLV value")
+    ASSERT( (ref[0] == 0.1) && (d[0] == 0.1), "Unexpected Reference VLV value")
+
+    DoubleVariableLengthVectorType x( d, 3, false );
+    ASSERT( x.IsAProxy(), "Unexpected VLV value")
+    ASSERT( (x[0] == 0.1) && (x[0] == 0.1), "Unexpected VLV value")
+
+    // ===[ Keep old values
+    // ---[ Shrink To Fit
+    x.SetSize(5, DoubleVariableLengthVectorType::ShrinkToFit(), DoubleVariableLengthVectorType::KeepOldValues());
+    ASSERT( ! x.IsAProxy(), "After resizing a proxy, it shall not be a proxy anymore")
+    ASSERT( ref[0]==x[0] && ref[1]==x[1] && ref[2]==x[2], "Old Values shall have been kept")
+    x[3] = 3.0;
+    x[4] = 4.0;
+    double * start = &x[0];
+
+    x.SetSize(3, DoubleVariableLengthVectorType::ShrinkToFit(), DoubleVariableLengthVectorType::KeepOldValues());
+    ASSERT( ! x.IsAProxy(), "After resizing, it shall never be a proxy");
+    ASSERT( x == ref, "Values haven't been preserved");
+    ASSERT(&x[0] != start, "ShrinkToFit shall induce a resizing");
+    start = &x[0];
+    x.SetSize(3, DoubleVariableLengthVectorType::ShrinkToFit(), DoubleVariableLengthVectorType::KeepOldValues());
+    ASSERT(&x[0] == start, "ShrinkToFit on the same size shall not induce a reallocation");
+
+    // ---[ Don't Shrink To Fit
+    x.SetSize(5, DoubleVariableLengthVectorType::DontShrinkToFit(), DoubleVariableLengthVectorType::KeepOldValues());
+    ASSERT( ! x.IsAProxy(), "After resizing, it shall never be a proxy");
+    ASSERT( ref[0]==x[0] && ref[1]==x[1] && ref[2]==x[2], "Old Values shall have been kept")
+    ASSERT(&x[0] != start, "DontShrinkToFit shall induce a resizing when the size grows");
+    x[3] = 3.0;
+    x[4] = 4.0;
+    start = &x[0];
+
+    x.SetSize(3, DoubleVariableLengthVectorType::DontShrinkToFit(), DoubleVariableLengthVectorType::KeepOldValues());
+    ASSERT( ! x.IsAProxy(), "After resizing, it shall never be a proxy");
+    ASSERT( ref == x, "Old Values shall have been kept")
+    ASSERT(&x[0] == start, "DontShrinkToFit shall not induce a resizing when the size diminishes");
+    start = &x[0];
+
+    x.SetSize(3, DoubleVariableLengthVectorType::DontShrinkToFit(), DoubleVariableLengthVectorType::KeepOldValues());
+    ASSERT( ! x.IsAProxy(), "After resizing, it shall never be a proxy");
+    ASSERT( ref == x, "Old Values shall have been kept")
+    ASSERT(&x[0] == start, "DontShrinkToFit shall not induce a resizing when the size stays the same");
+
+    // ---[ Always Reallocate
+    x.SetSize(5, DoubleVariableLengthVectorType::AlwaysReallocate(), DoubleVariableLengthVectorType::KeepOldValues());
+    ASSERT( ! x.IsAProxy(), "After resizing, it shall never be a proxy");
+    ASSERT( ref[0]==x[0] && ref[1]==x[1] && ref[2]==x[2], "Old Values shall have been kept")
+    ASSERT(&x[0] != start, "AlwaysReallocate shall induce a reallocation when resizing");
+    start = &x[0];
+
+    x.SetSize(3, DoubleVariableLengthVectorType::AlwaysReallocate(), DoubleVariableLengthVectorType::KeepOldValues());
+    ASSERT( ! x.IsAProxy(), "After resizing, it shall never be a proxy");
+    ASSERT( ref[0]==x[0] && ref[1]==x[1] && ref[2]==x[2], "Old Values shall have been kept")
+    ASSERT(&x[0] != start, "AlwaysReallocate shall induce a reallocation when resizing");
+    start = &x[0];
+
+    x.SetSize(3, DoubleVariableLengthVectorType::AlwaysReallocate(), DoubleVariableLengthVectorType::KeepOldValues());
+    ASSERT( ! x.IsAProxy(), "After resizing, it shall never be a proxy");
+    ASSERT( ref[0]==x[0] && ref[1]==x[1] && ref[2]==x[2], "Old Values shall have been kept")
+    ASSERT(&x[0] != start, "AlwaysReallocate shall induce a reallocation when resizing, even with the same size");
+    start = &x[0];
+
+    // ===[ Don't keep old values
+    // ---[ ShrinkToFit
+    x.SetSize(5, DoubleVariableLengthVectorType::ShrinkToFit(), DoubleVariableLengthVectorType::DumpOldValues());
+    ASSERT(&x[0] != start, "ShrintToFit(bigger) => reallocate");
+    ASSERT( ref[0] != x[0], "Resize + Dump Old Value --> Old values shall be lost");
+    x[0] = ref[0];
+    start = &x[0];
+
+    x.SetSize(3, DoubleVariableLengthVectorType::ShrinkToFit(), DoubleVariableLengthVectorType::DumpOldValues());
+    ASSERT(&x[0] != start, "ShrintToFit(smaller) => reallocate");
+    ASSERT( ref[0] != x[0], "Resize + Dump Old Value --> Old values shall be lost");
+    x[0] = ref[0];
+    start = &x[0];
+
+    x.SetSize(5, DoubleVariableLengthVectorType::DontShrinkToFit(), DoubleVariableLengthVectorType::DumpOldValues());
+    ASSERT(&x[0] != start, "DontShrintToFit(bigger) => reallocate");
+    ASSERT( ref[0] != x[0], "Resize + Dump Old Value --> Old values shall be lost");
+    x[0] = ref[0];
+    start = &x[0];
+    }
+
+  // Test on assignments
+    {
+    // We won't be able to test that old values are dumped.
+    // Only when reallocations will be avoided.
+    DoubleVariableLengthVectorType ref1(3);
+    ref1[0] = 0.1; ref1[1] = 0.2; ref1[2] = 0.3;
+    DoubleVariableLengthVectorType ref2(3);
+    ref2[0] = 1.1; ref2[1] = 1.2; ref2[2] = 1.3;
+    DoubleVariableLengthVectorType ref4(4);
+    ref4[0] = 1.1; ref4[1] = 1.2; ref4[2] = 1.3; ref4[3] = 1.4;
+
+    DoubleVariableLengthVectorType x;
+    ASSERT(x != ref1, "New VLV shall be empty");
+
+    x = ref1;
+    ASSERT(x == ref1, "Ref1 is expected to be copied into x");
+    double * start = &x[0];
+
+    x = ref2;
+    ASSERT(x == ref2, "Ref2 is expected to be copied into x");
+    ASSERT(start == &x[0], "Assignment doesn't imply reallocation when the new size is identical to the current one");
+
+    x = ref4;
+    ASSERT(x == ref4, "Ref4 is expected to be copied into x");
+    ASSERT(start != &x[0], "Assignment implies reallocation when the current size is insufficient to hold the new value");
+    start = &x[0];
+
+    x = ref1;
+    ASSERT(x == ref1, "Ref1 is expected to be copied into x");
+    ASSERT(start == &x[0], "Assignment doesn't imply reallocation when the current size is enough");
+
+    // NB: From here, x=ref4; will induce a reallocation even if enough memory has already
+    // been allocated.
+    }
+
+  // Test FastAssign
+    {
+    DoubleVariableLengthVectorType ref1(3);
+    ref1[0] = 0.1; ref1[1] = 0.2; ref1[2] = 0.3;
+    DoubleVariableLengthVectorType ref2(3);
+    ref2[0] = 1.1; ref2[1] = 1.2; ref2[2] = 1.3;
+    DoubleVariableLengthVectorType ref4(4);
+    ref4[0] = 1.1; ref4[1] = 1.2; ref4[2] = 1.3; ref4[3] = 1.4;
+
+    DoubleVariableLengthVectorType x(3);
+    // FastAssign pre conditions
+    assert(x.GetSize() == ref1.GetSize());
+    assert(!x.IsAProxy());
+    double * start = &x[0];
+    x.FastAssign(ref1);
+    ASSERT(start == &x[0], "FastAssign shall never reallocate");
+    ASSERT(x == ref1, "FastAssign shall ... assign VLVs");
+
+    assert(x.GetSize() == ref2.GetSize());
+    x.FastAssign(ref2);
+    ASSERT(start == &x[0], "FastAssign shall never reallocate");
+    ASSERT(x == ref2, "FastAssign shall ... assign VLVs");
+
+    // As ref4.GetSize() is different from x.GetSize(),
+    //    x.FastAssign(ref4);
+    // is an invalid instruction: Indeed FastAssign preconditions are not met.
+    }
+
   delete[] d;
   }
 
