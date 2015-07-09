48912c62757509e536ef7f161d09f9549d502e45
Hans Johnson <hans-johnson@uiowa.edu>
2015-07-09 09:00:55 -0500

    STYLE: Minor code style issues for MGHIO
    MGHIO now conforms to the header guard style, so bumping
    the remote module to use that version.
    
    Change-Id: Idf14f0c0db10bfce6a57dc3cdf93b99db2f0c487

--- Modules/Remote/MGHIO.remote.cmake
+++ Modules/Remote/MGHIO.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(MGHIO
   "MGHIO ImageIO plugin for ITK"
   GIT_REPOSITORY https://github.com/Slicer/itkMGHImageIO.git
-  GIT_TAG b3ce10a218ee9db5927536d45448d14f6b188cde
+  GIT_TAG 662e79fe283ed7ee85fb6d80dab3739fc75eb763
   )
