3f9b3273b319d879da93040c5f460c36746df18c
Hans Johnson <hans-johnson@uiowa.edu>
2015-07-09 20:28:45 -0500

    ENH: Update Sphinx for ITK 4.8.0
    Change-Id: I55573c60466a88a805c6e2a096021e4c49933f91

--- Modules/Remote/SphinxExamples.remote.cmake
+++ Modules/Remote/SphinxExamples.remote.cmake
@@ -2,6 +2,6 @@
 itk_fetch_module(SphinxExamples
   "This module builds the examples found at http://itk.org/ITKExamples/"
   GIT_REPOSITORY http://itk.org/ITKExamples.git
-  # 2015-07-02
-  GIT_TAG 890667eaa7dbab6111aabd51432d1aeadca738f9
+  # 2015-07-09
+  GIT_TAG 2bc08a52aa0437da776cc777157f25b7b525b46f
   )
