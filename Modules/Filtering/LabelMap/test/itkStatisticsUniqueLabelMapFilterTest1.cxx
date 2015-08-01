a18b3cf00e3812c3831efecd0703e73f0134ec0b
Jon Haitz Legarreta <jhlegarreta@vicomtech.org>
2015-08-01 18:44:59 +0200

    BUG: Test for StatisticsUniqueLabelMapFilter stability
    This patch set adds to the test the output of the dilation filter in the
    itkStatisticsUniqueLabelMapFilter pipeline. A comparison to the
    corresponding baseline is also added to the CMakeLists.txt.
    This additional output will help in ruling out the dilation operation, as
    it seems to be related to the stability issues of the filter.
    This branch is related to the JIRA issue 3370.
    
    Change-Id: I9303d8b9c2058aaa2bb4d6ebcc3daa9066a90dc9

--- Modules/Filtering/LabelMap/test/itkStatisticsUniqueLabelMapFilterTest1.cxx
+++ Modules/Filtering/LabelMap/test/itkStatisticsUniqueLabelMapFilterTest1.cxx
@@ -31,10 +31,12 @@
 
 int itkStatisticsUniqueLabelMapFilterTest1(int argc, char * argv[])
 {
-  if( argc != 6 )
+  // ToDo: remove dilationOutput once the JIRA issue 3370 has been solved
+  // Then, argc != 6
+  if( argc != 7 )
     {
     std::cerr << "Usage: " << argv[0];
-    std::cerr << " input feature output";
+    std::cerr << " input feature output dilationOutput";
     std::cerr << " reverseOrdering attribute";
     std::cerr << std::endl;
     return EXIT_FAILURE;
@@ -88,39 +90,52 @@ int itkStatisticsUniqueLabelMapFilterTest1(int argc, char * argv[])
   i2l->SetFeatureImage( reader2->GetOutput() );
 
   typedef itk::StatisticsUniqueLabelMapFilter< LabelMapType > LabelUniqueType;
-  LabelUniqueType::Pointer Unique = LabelUniqueType::New();
+  LabelUniqueType::Pointer unique = LabelUniqueType::New();
 
   //testing boolean macro for ReverseOrdering
-  Unique->ReverseOrderingOn();
-  TEST_SET_GET_VALUE( true, Unique->GetReverseOrdering() );
+  unique->ReverseOrderingOn();
+  TEST_SET_GET_VALUE( true, unique->GetReverseOrdering() );
 
-  Unique->ReverseOrderingOff();
-  TEST_SET_GET_VALUE( false, Unique->GetReverseOrdering() );
+  unique->ReverseOrderingOff();
+  TEST_SET_GET_VALUE( false, unique->GetReverseOrdering() );
 
   //testing get and set macros for ReverseOrdering
-  bool reverseOrdering = atoi( argv[4] );
-  Unique->SetReverseOrdering( reverseOrdering );
-  TEST_SET_GET_VALUE( reverseOrdering , Unique->GetReverseOrdering() );
+  // ToDo: decrease reverseOrdering argv index once the JIRA issue 3370 has been solved
+  // Then, argv[4]
+  bool reverseOrdering = atoi( argv[5] );
+  unique->SetReverseOrdering( reverseOrdering );
+  TEST_SET_GET_VALUE( reverseOrdering , unique->GetReverseOrdering() );
 
 
   //testing get and set macros for Attribute
-  LabelUniqueType::AttributeType attribute = atoi( argv[5] );
-  Unique->SetAttribute( attribute );
-  TEST_SET_GET_VALUE( attribute, Unique->GetAttribute() );
+  // ToDo: decrease attribute argv index once the JIRA issue 3370 has been solved
+  // Then, argv[5]
+  LabelUniqueType::AttributeType attribute = atoi( argv[6] );
+  unique->SetAttribute( attribute );
+  TEST_SET_GET_VALUE( attribute, unique->GetAttribute() );
 
-  Unique->SetInput( i2l->GetOutput() );
+  unique->SetInput( i2l->GetOutput() );
 
-  itk::SimpleFilterWatcher watcher(Unique, "filter");
+  itk::SimpleFilterWatcher watcher(unique, "filter");
 
   typedef itk::LabelMapToLabelImageFilter< LabelMapType, ImageType> L2IType;
   L2IType::Pointer l2i = L2IType::New();
-  l2i->SetInput( Unique->GetOutput() );
+  l2i->SetInput( unique->GetOutput() );
 
   typedef itk::ImageFileWriter< ImageType > WriterType;
 
   WriterType::Pointer writer = WriterType::New();
   writer->SetInput( l2i->GetOutput() );
   writer->SetFileName( argv[3] );
+  writer->UseCompressionOn();
+
+  TRY_EXPECT_NO_EXCEPTION( writer->Update() );
+
+
+  // WARNING: TEMPORARY: JIRA ISSUE 3370
+  // Writing an additional output of just the dilated label
+  writer->SetInput( grayscaleDilateFilter->GetOutput() );
+  writer->SetFileName( argv[4] );
   writer->UseCompressionOn();
 
   TRY_EXPECT_NO_EXCEPTION( writer->Update() );
