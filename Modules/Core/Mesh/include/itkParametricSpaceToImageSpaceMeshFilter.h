4f4c724701cfef1d0eccb24acbd5864847d5433c
Jon Haitz Legarreta <jhlegarreta@vicomtech.org>
2015-08-01 20:16:14 +0200

    ENH: Add test for itkParametricStpaceToImageSpaceFilter
    This patch set adds a test to improve the code coverage of the
    itkParametricSpaceToImageSpaceFilter.
    
    Change-Id: Ib75afc6009e72aa1de2c186a3a37b23a83461b24

--- Modules/Core/Mesh/include/itkParametricSpaceToImageSpaceMeshFilter.h
+++ Modules/Core/Mesh/include/itkParametricSpaceToImageSpaceMeshFilter.h
@@ -32,10 +32,9 @@ namespace itk
  *
  * The input mesh is assumed to represent pixels in some parametric space.
  * The output mesh is suitable to be superimposed as an overlay to the
- * associated image.
- * by the user.
+ * associated image specified by the user.
  *
- * The additional content of the mesh is passed untouched. Including the
+ * The additional content of the mesh is passed untouched, including the
  * connectivity and the additional information contained on cells and points.
  *
  * \warning This filter also assumes that the Output Mesh has as type
