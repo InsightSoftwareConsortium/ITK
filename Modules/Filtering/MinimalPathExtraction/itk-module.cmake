db6e43769af87845703e5eff1ad6f852ff604aa0
Matt McCormick <matt.mccormick@kitware.com>
2015-09-17 17:00:18 -0400

    DOC: Add a README.
    Including a link to the CircleCI page. Also, add more content to the module
    description.

--- Modules/Filtering/MinimalPathExtraction/itk-module.cmake
+++ Modules/Filtering/MinimalPathExtraction/itk-module.cmake
@@ -1,6 +1,21 @@
 set(
   DOCUMENTATION
-  "http://www.insight-journal.org/browse/publication/213
+  "This module implements a minimal path extraction framework
+based on Fast Marching arrival functions.
+
+A more detailed description can be found in the Insight Journal article::
+
+  Mueller, D. "
+  Fast
+  Marching
+  Minimal
+  Path
+  Extraction
+  in
+  ITK"
+  http://hdl.handle.net/1926/1332
+  http://www.insight-journal.org/browse/publication/213
+  March, 2008.
 "
 )
 
