6c2453d6d1c00c628510cd8535e2e7819a22255a
Lucas Gandel <lucas.gandel@gmail.com>
2015-09-21 08:13:41 -0400

    STYLE: Fix KWStyle dashboard errors
    Change-Id: I0eca00756c607af98c69cd8112f1be0913172a66

--- Modules/Remote/MinimalPathExtraction.remote.cmake
+++ Modules/Remote/MinimalPathExtraction.remote.cmake
@@ -2,6 +2,6 @@
 itk_fetch_module(MinimalPathExtraction
  "http://www.insight-journal.org/browse/publication/213
 "
-  GIT_REPOSITORY https://github.com/InsightSoftwareConsortium/MinimalPathExtraction.git
-  GIT_TAG 1065027b1fea00ca2353facdb2db9d1a5e724d27
+  GIT_REPOSITORY https://github.com/InsightSoftwareConsortium/ITKMinimalPathExtraction.git
+  GIT_TAG 50380b8142cb470403997979544fc555b9ed3448
   )
