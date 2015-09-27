da5be631f3c93cce73e7d79280ccfb41b66b5bd5
Bill Lorensen <bill.lorensen@gmail.com>
2015-09-27 12:33:24 -0400

    ENH: Bump tag for wiki examples
    Wiki examples now support the vtk rendering backend selection added to
    vtk 6.
    
    Change-Id: I8f07e8cd57d33075132c69d6be0d8e6642b4a0c5

--- Modules/Remote/WikiExamples.remote.cmake
+++ Modules/Remote/WikiExamples.remote.cmake
@@ -5,6 +5,6 @@
 itk_fetch_module(WikiExamples
   "A collection of examples that illustrate how to use ITK."
   GIT_REPOSITORY https://github.com/InsightSoftwareConsortium/ITKWikiExamples.git
-  # July 09, 2015
-  GIT_TAG 1c53a9346e914959f1afb8df79cf449ff99ac7fa
+  # September 27, 2015
+  GIT_TAG 29f456945d7ff9a35ac8bfaf8985601f8ddf9cba
   )
