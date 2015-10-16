77ab0f22b573c335f29e55bb04cc0847a4c37977
Matt McCormick <matt.mccormick@kitware.com>
2015-10-16 17:35:26 -0400

    DOC: Fix description typo.

--- Modules/Beta/Cuberille/itk-module.cmake
+++ Modules/Beta/Cuberille/itk-module.cmake
@@ -1,7 +1,7 @@
 set(DOCUMENTATION "This module implements cuberille implicit surface
 polygonization for ITK. This method operates by diving the surface into a
 number of small cubes called cuberilles. Each cuberille is centered at a
-pixel lying on th eiso-surface and then quadrilaterals are generated for each
+pixel lying on the iso-surface and then quadrilaterals are generated for each
 face. The original approach is improved by projecting the vertices of each
 cuberille onto the implicit surface, smoothing the typical block-like
 resultant mesh.
