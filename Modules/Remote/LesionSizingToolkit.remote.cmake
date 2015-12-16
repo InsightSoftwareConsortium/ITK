b43c146f4cc6e92f2d238cd71873a75ae88235ea
Matt McCormick <matt.mccormick@kitware.com>
2015-12-16 16:31:37 -0500

    COMP: Update LesionSizingToolkit for external build and style test failures.
    The protocol also needs to be http because https is not yet supported.
    
    Change-Id: Idff3ddc72c55920ade7847f1978f0c90b5ce5945

--- Modules/Remote/LesionSizingToolkit.remote.cmake
+++ Modules/Remote/LesionSizingToolkit.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(LesionSizingToolkit
   "Framework for determining the sizes of lesions in medical images."
-  GIT_REPOSITORY ${git_protocol}://public.kitware.com/LesionSizingToolkit.git
-  GIT_TAG fd9cfcef3bb4f7678bc50ecbe6ad51c6998cc3db
+  GIT_REPOSITORY http://public.kitware.com/LesionSizingToolkit.git
+  GIT_TAG 100ec226866cca13a6ed0f5c2d3520f45750424d
   )
