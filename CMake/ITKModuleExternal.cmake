910837d139a5209aee9b307c9ae8174259b9f54d
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2016-02-05 15:11:45 -0500

    COMP: Allow use of multiple "ITK external modules" in the same project.
    This changes will facilitate building application by aggregating
    multiple "External ITK module" like "ITKMinimalPathExtraction" or
    "ITKCuberille".
    
    Change-Id: I5c5be9d24e2af70287de46e88a85a14b4de97032
    Co-authored-by: Lucas Gandel <lucas.gandel@kitware.com>

--- CMake/ITKModuleExternal.cmake
+++ CMake/ITKModuleExternal.cmake
@@ -132,4 +132,6 @@ endif()
 
 # Create target to download data from the ITKData group.  This must come after
 # all tests have been added that reference the group, so we put it last.
-ExternalData_Add_Target(ITKData)
+if(NOT TARGET ITKData)
+  ExternalData_Add_Target(ITKData)
+endif()
