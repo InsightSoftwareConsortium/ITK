1c8e019855bd45365b43c8b8132c59fdc1e8d803
Matt McCormick <matt.mccormick@kitware.com>
2016-03-31 16:33:53 -0400

    BUG: midas3.kitware.com only supports the https protocol.
    midas3.kitware.com now only supports https -- previously it supported http and
    https.  This addresses errors like:
    
    error: downloading
      'http://midas3.kitware.com/midas/download/bitstream/457776/castxml-linux.tar.gz'
      failed
    
        status_code: 1
        status_string: "Unsupported protocol"
        log:   Trying 50.58.123.181...
    
      Connected to midas3.kitware.com (50.58.123.181) port 80 (#0)
    
    Change-Id: I29a9b87172c4dac380fc6cc43043b21c5afe1481

--- Utilities/Maintenance/ArchiveTestingDataOnMidas.py
+++ Utilities/Maintenance/ArchiveTestingDataOnMidas.py
@@ -19,7 +19,7 @@ import sys
 
 import pydas
 
-def connect_to_midas(email=None, api_key=None, midas_url='http://midas3.kitware.com/midas/'):
+def connect_to_midas(email=None, api_key=None, midas_url='https://midas3.kitware.com/midas/'):
     if not api_key:
         print('Please enter your login information for ' + midas_url)
         pydas.login(url=midas_url, email=email)
@@ -101,7 +101,7 @@ def upload_to_midas(content_link, externaldata_object_store,
 
 
 def run(itk_source_dir, externaldata_object_store,
-        email=None, api_key=None, midas_url='http://midas3.kitware.com/midas/'):
+        email=None, api_key=None, midas_url='https://midas3.kitware.com/midas/'):
     session, communicator = connect_to_midas(email, api_key, midas_url)
 
     md5files = []
@@ -158,7 +158,7 @@ if __name__ == '__main__':
             + 'ExternalData/Objects/ in a build tree.')
     parser.add_argument('--midas-url', '-u',
             help='URL of the Midas server.',
-            default='http://midas3.kitware.com/midas/')
+            default='https://midas3.kitware.com/midas/')
     args = parser.parse_args()
 
     if args.api_key_file:
