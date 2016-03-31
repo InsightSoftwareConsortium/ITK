afa116ff28d94a381eb993d1081f129b2f6891df
Dženan Zukić <dzenan.zukic@kitware.com>
2016-03-31 18:13:07 -0400

    COMP: Removing 2D test which now does not compile

--- Modules/Filtering/MorphologicalContourInterpolation/test/itkMorphologicalContourInterpolationTest.cxx
+++ Modules/Filtering/MorphologicalContourInterpolation/test/itkMorphologicalContourInterpolationTest.cxx
@@ -111,12 +111,6 @@ itkMorphologicalContourInterpolationTest(int argc, char * argv[])
   try
   {
     // unused cases are not instantiated because they greatly increase compile time
-    // 2-dim version causes compile errors with Maurer filter
-    // if (numDimensions==2 && pixelType == ScalarPixelType::UCHAR)
-    //   {
-    //   doTest<itk::Image<unsigned char, 2>>(inputImageFileName, outputImageFileName, dt, ball, axis, label);
-    //   return EXIT_SUCCESS;
-    //   }
     if (numDimensions == 3 && (pixelType == ScalarPixelType::SHORT || pixelType == ScalarPixelType::USHORT))
     {
       doTest<itk::Image<short, 3>>(inputImageFileName, outputImageFileName, dt, ball, axis, label);
