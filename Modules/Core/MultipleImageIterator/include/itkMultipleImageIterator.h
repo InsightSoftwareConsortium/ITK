b579170f3f27e29e6c615a46777a98df7d76bf17
Dženan Zukić <dzenan.zukic@kitware.com>
2016-05-24 13:20:16 -0400

    COMP: Compiles and passes style check

--- Modules/Core/MultipleImageIterator/include/itkMultipleImageIterator.h
+++ Modules/Core/MultipleImageIterator/include/itkMultipleImageIterator.h
@@ -1,16 +1,35 @@
-#ifndef __itkMultipleImageIterator_h
-#define __itkMultipleImageIterator_h
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#ifndef itkMultipleImageIterator_h
+#define itkMultipleImageIterator_h
 #include <vector>
 #include <itkImageRegionIterator.h>
 
 
 namespace itk
 {
-/** class MultipleImageIterator
+/** \class MultipleImageIterator
  * \brief An wrapper around image iterators to iterate over several images simultaneously
  * All iterators must
  *  - point to images of the same type
- *  - be of the same size (number of values from begin to end) */
+ *  - be of the same size (number of values from begin to end)
+ * \ingroup MultipleImageIterator */
 template <typename TIterator>
 class MultipleImageIterator
 {
@@ -22,19 +41,19 @@ public:
   IteratorType &
   operator[](const int i)
   {
-    return m_iterators[i];
+    return m_Iterators[i];
   }
   /// Add a new iterator
   void
   AddIterator(const IteratorType & it)
   {
-    m_iterators.push_back(it);
+    m_Iterators.push_back(it);
   }
   /// Advance all iterators
   Self &
   operator++()
   {
-    for (typename std::vector<IteratorType>::iterator it = m_iterators.begin(); it != m_iterators.end(); ++it)
+    for (typename std::vector<IteratorType>::iterator it = m_Iterators.begin(); it != m_Iterators.end(); ++it)
     {
       ++(*it);
     }
@@ -44,7 +63,7 @@ public:
   void
   GoToBegin()
   {
-    for (typename std::vector<IteratorType>::iterator it = m_iterators.begin(); it != m_iterators.end(); ++it)
+    for (typename std::vector<IteratorType>::iterator it = m_Iterators.begin(); it != m_Iterators.end(); ++it)
     {
       it->GoToBegin();
     }
@@ -56,12 +75,12 @@ public:
   IsAtEnd()
   {
 #ifdef NDEBUG
-    return m_iterators[0].IsAtEnd();
+    return m_Iterators[0].IsAtEnd();
 #else
-    assert(m_iterators.size());
-    bool retval = m_iterators[0].IsAtEnd();
-    for (unsigned int i = 0; i < m_iterators.size(); ++i)
-      assert(m_iterators[i].IsAtEnd() == retval);
+    assert(m_Iterators.size());
+    bool retval = m_Iterators[0].IsAtEnd();
+    for (unsigned int i = 0; i < m_Iterators.size(); ++i)
+      assert(m_Iterators[i].IsAtEnd() == retval);
     return retval;
 #endif
   }
@@ -69,11 +88,11 @@ public:
   unsigned int
   Size() const
   {
-    return m_iterators.size();
+    return m_Iterators.size();
   }
 
 protected:
-  std::vector<IteratorType> m_iterators;
+  std::vector<IteratorType> m_Iterators;
 };
 } // namespace itk
-#endif
+#endif // itkMultipleImageIterator_h
--- Modules/Core/MultipleImageIterator/test/DumpIntensities.cxx
+++ Modules/Core/MultipleImageIterator/test/DumpIntensities.cxx
@@ -1,3 +1,21 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -9,7 +27,7 @@
 using namespace std;
 
 int
-main(int argc, char * argv[])
+DumpIntensities(int argc, char * argv[])
 {
   if (argc < 2)
   {
@@ -23,7 +41,7 @@ main(int argc, char * argv[])
   itk::MultipleImageIterator<IteratorType>    it;
 
   vector<ImageType::Pointer> images; // Need to keep a reference as iterators only have weak references
-  for (unsigned int i = 1; i < argc; ++i)
+  for (int i = 1; i < argc; ++i)
   {
     ReaderType::Pointer r = ReaderType::New();
     r->SetFileName(argv[i]);
@@ -49,4 +67,5 @@ main(int argc, char * argv[])
       f << endl;
     }
   }
+  return 0;
 }
