c29e418c03d38891d4f7ec5489d5892529027d46
Jon Haitz Legarreta <jhlegarreta@vicomtech.org>
2016-05-25 11:46:06 +0200

    ENH: Improve itkVideoFileReader coverage
    Exercise basic object methods.
    
    Change-Id: Ic18b569c259e8251ceba890ae72ff62d905d992f

--- Modules/Video/IO/test/itkVideoFileReaderWriterTest.cxx
+++ Modules/Video/IO/test/itkVideoFileReaderWriterTest.cxx
@@ -20,6 +20,7 @@
 #include "itkVideoFileReader.h"
 #include "itkVideoFileWriter.h"
 #include "itkFileListVideoIOFactory.h"
+#include "itkTestingMacros.h"
 
 int itkVideoFileReaderWriterTest( int argc, char *argv[] )
 {
@@ -48,6 +49,9 @@ int itkVideoFileReaderWriterTest( int argc, char *argv[] )
       }
     }
   VideoReaderType::Pointer reader = VideoReaderType::New();
+
+  EXERCISE_BASIC_OBJECT_METHODS( reader, VideoFileReader, VideoSource );
+
   reader->SetFileName(inFile.c_str() );
 
   // I'm still not sure how to handle this right, but for now, just manually
