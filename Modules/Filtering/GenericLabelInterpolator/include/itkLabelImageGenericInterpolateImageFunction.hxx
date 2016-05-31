fea973734f6801032d65f5418f90b2c48b7c14ba
Francois Budin <francois.budin@gmail.com>
2016-05-31 08:39:47 -0400

    ENH: Remove unused variables warning
    Some parameters declared in function definitions are not used. To avoid compiler warning,
    we use the macro itkNotUsed().

--- Modules/Filtering/GenericLabelInterpolator/include/itkLabelImageGenericInterpolateImageFunction.hxx
+++ Modules/Filtering/GenericLabelInterpolator/include/itkLabelImageGenericInterpolateImageFunction.hxx
@@ -24,31 +24,35 @@
 namespace itk
 {
 
-template<typename TInputImage, template<class, typename> class TInterpolator , typename TCoordRep>
-LabelImageGenericInterpolateImageFunction<TInputImage, TInterpolator, TCoordRep>
-::LabelImageGenericInterpolateImageFunction()
-{
-}
+template <typename TInputImage, template <class, typename> class TInterpolator, typename TCoordRep>
+LabelImageGenericInterpolateImageFunction<TInputImage, TInterpolator, TCoordRep>::
+  LabelImageGenericInterpolateImageFunction()
+{}
 
-template<typename TInputImage, template<class, typename> class TInterpolator , typename TCoordRep>
-void LabelImageGenericInterpolateImageFunction<TInputImage,TInterpolator, TCoordRep>
-::SetInputImage( const TInputImage *image ) {
+template <typename TInputImage, template <class, typename> class TInterpolator, typename TCoordRep>
+void
+LabelImageGenericInterpolateImageFunction<TInputImage, TInterpolator, TCoordRep>::SetInputImage(
+  const TInputImage * image)
+{
   /** We have one adaptor and one interpolator per label to keep the class thread-safe:
    *  changing the adaptor's accepted value wouldn't work when called from a multi-threaded filter */
-  if (image) {
+  if (image)
+  {
     m_Labels.clear();
     typedef itk::ImageRegionConstIterator<TInputImage> IteratorType;
-    IteratorType it(image,image->GetLargestPossibleRegion());
+    IteratorType                                       it(image, image->GetLargestPossibleRegion());
     it.GoToBegin();
-    for (; !it.IsAtEnd(); ++it) {
+    for (; !it.IsAtEnd(); ++it)
+    {
       m_Labels.insert(it.Get());
     }
     m_InternalInterpolators.clear();
     m_LabelSelectionAdaptors.clear();
-    for(typename LabelSetType::const_iterator i=m_Labels.begin(); i != m_Labels.end(); ++i) {
+    for (typename LabelSetType::const_iterator i = m_Labels.begin(); i != m_Labels.end(); ++i)
+    {
       typename LabelSelectionAdaptorType::Pointer adapt = LabelSelectionAdaptorType::New();
       // This adaptor doesn't implement Set() so this should be safe
-      adapt->SetImage(const_cast<TInputImage*>(image));
+      adapt->SetImage(const_cast<TInputImage *>(image));
       adapt->SetAcceptedValue(*i);
       m_LabelSelectionAdaptors.push_back(adapt);
       typename InternalInterpolatorType::Pointer interp = InternalInterpolatorType::New();
@@ -59,27 +63,28 @@ void LabelImageGenericInterpolateImageFunction<TInputImage,TInterpolator, TCoord
   Superclass::SetInputImage(image);
 }
 
-template<typename TInputImage, template<class, typename> class TInterpolator , typename TCoordRep>
-typename LabelImageGenericInterpolateImageFunction<TInputImage, TInterpolator, TCoordRep>
-::OutputType
-LabelImageGenericInterpolateImageFunction<TInputImage, TInterpolator, TCoordRep>
-::EvaluateAtContinuousIndex( const ContinuousIndexType & cindex, OutputType * grad ) const
+template <typename TInputImage, template <class, typename> class TInterpolator, typename TCoordRep>
+typename LabelImageGenericInterpolateImageFunction<TInputImage, TInterpolator, TCoordRep>::OutputType
+LabelImageGenericInterpolateImageFunction<TInputImage, TInterpolator, TCoordRep>::EvaluateAtContinuousIndex(
+  const ContinuousIndexType & cindex,
+  OutputType *                itkNotUsed(grad)) const
 {
   /** Interpolate the binary mask corresponding to each label and return the label
    * with the highest value */
-  double value=0;
-  typename TInputImage::PixelType best_label=0;
+  double                                value = 0;
+  typename TInputImage::PixelType       best_label = 0;
   typename LabelSetType::const_iterator it;
-  int i;
-  for(it=m_Labels.begin(), i = 0;
-    it != m_Labels.end(); ++it, ++i) {
+  int                                   i;
+  for (it = m_Labels.begin(), i = 0; it != m_Labels.end(); ++it, ++i)
+  {
     double tmp = m_InternalInterpolators[i]->EvaluateAtContinuousIndex(cindex);
-    if( tmp > value) {
+    if (tmp > value)
+    {
       value = tmp;
       best_label = (*it);
     }
   }
-    return best_label;
+  return best_label;
 }
 
 } // namespace itk
