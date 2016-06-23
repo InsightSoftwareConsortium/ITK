ec58517c18848d043cb1874dec4868ea25d10ff9
Francois Budin <francois.budin@gmail.com>
2016-06-23 09:51:27 -0400

    BUG: Example (VectorKernelPCA) was missing dependencies
    ITKMesh, ITKIOMesh, and ITKIOImageBase modules are required to build
    VectorKernelPCA.

--- Modules/Numerics/PrincipalComponentsAnalysis/itk-module.cmake
+++ Modules/Numerics/PrincipalComponentsAnalysis/itk-module.cmake
@@ -8,9 +8,10 @@ itk_module(
   PrincipalComponentsAnalysis
   DEPENDS
     ITKCommon
-  TEST_DEPENDS
-    ITKIOMesh
     ITKMesh
+    ITKIOMesh
+    ITKIOImageBase
+  TEST_DEPENDS
     ITKTestKernel
   EXCLUDE_FROM_DEFAULT
   DESCRIPTION "Module ingested from upstream."
