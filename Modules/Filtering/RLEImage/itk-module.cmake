33618791cb67f4ff9d96a6a611b029ff2f7fb93b
Dženan Zukić <dzenan.zukic@kitware.com>
2016-07-15 16:17:40 -0400

    COMP: Adding ITK build infrastructure

---
+++ Modules/Filtering/RLEImage/itk-module.cmake
@@ -0,0 +1,18 @@
+set(
+  DOCUMENTATION
+  "The module provides run-length encoded storage for
+image content, iterators for efficient reading and writing, and a
+specialization of region of interest filter which can also be used to
+convert to and from regular itk::Image."
+)
+
+itk_module(
+  RLEImage
+  DEPENDS
+    ITKCommon
+  TEST_DEPENDS
+    ITKIOImageBase
+    ITKTestKernel
+  EXCLUDE_FROM_DEFAULT
+  DESCRIPTION "${DOCUMENTATION}"
+)
