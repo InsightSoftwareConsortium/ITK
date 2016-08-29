393b6c37c7357fc3abbd928296ca54c0b1c4f8cc
Francois Budin <francois.budin@kitware.com>
2016-08-29 17:41:33 -0400

    ENH: Update SCIFIO to latest version on 08/29/2016
    https://github.com/scifio/scifio-imageio/compare/d6ce39d855d5078589efed93f785c7289769c3c6...eb391a74e77b161e0f070539237193528c628f7b
    
    Change-Id: If7a96c28eca18ff6164795145cb80500b7b115f6

--- Modules/Remote/SCIFIO.remote.cmake
+++ Modules/Remote/SCIFIO.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(SCIFIO
   "SCIFIO (Bioformats) ImageIO plugin for ITK"
   GIT_REPOSITORY ${git_protocol}://github.com/scifio/scifio-imageio.git
-  GIT_TAG d6ce39d855d5078589efed93f785c7289769c3c6
+  GIT_TAG eb391a74e77b161e0f070539237193528c628f7b
   )
