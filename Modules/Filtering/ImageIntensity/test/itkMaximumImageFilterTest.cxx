fab1bc1fe8ab3c109d5565e52fbbc346c7f65b05
Jon Haitz Legarreta <jhlegarreta@vicomtech.org>
2016-09-15 09:27:45 +0200

    ENH: Improve MaximumImageFilter coverage.
    Exercise basic object methods.
    
    Refactor the test to make it cleaner.
    
    Set the function BEFORE updating the filter.
    
    Write out the result so that the output can be compared to the baseline.
    
    Change-Id: I01aeceac7ea0589523b5aa49e1fbdda3fcb338f2

--- Modules/Filtering/ImageIntensity/test/itkMaximumImageFilterTest.cxx
+++ Modules/Filtering/ImageIntensity/test/itkMaximumImageFilterTest.cxx
@@ -18,56 +18,49 @@
 
 #include "itkMaximumImageFilter.h"
 #include "itkImageRegionIteratorWithIndex.h"
+#include "itkTestingMacros.h"
 
 
-int itkMaximumImageFilterTest(int, char* [] )
+int itkMaximumImageFilterTest( int, char*[] )
 {
 
   // Define the dimension of the images
-  const unsigned int myDimension = 3;
+  const unsigned int Dimension = 3;
+
+  typedef unsigned char PixelType;
 
   // Declare the types of the images
-  typedef itk::Image<float, myDimension>  myImageType1;
-  typedef itk::Image<float, myDimension>  myImageType2;
-  typedef itk::Image<float, myDimension>  myImageType3;
+  typedef itk::Image< PixelType, Dimension >  ImageType;
 
   // Declare the type of the index to access images
-  typedef itk::Index<myDimension>         myIndexType;
+  typedef itk::Index< Dimension >             IndexType;
 
   // Declare the type of the size
-  typedef itk::Size<myDimension>          mySizeType;
+  typedef itk::Size< Dimension >              SizeType;
 
-  // Declare the type of the Region
-  typedef itk::ImageRegion<myDimension>        myRegionType;
+  // Declare the type of the region
+  typedef itk::ImageRegion< Dimension >       RegionType;
 
-  // Declare the type for the MULTIPLY filter
-  typedef itk::MaximumImageFilter<
-                                myImageType1,
-                                myImageType2,
-                                myImageType3  >       myFilterType;
-
-  // Declare the pointers to images
-  typedef myImageType1::Pointer   myImageType1Pointer;
-  typedef myImageType2::Pointer   myImageType2Pointer;
-  typedef myImageType3::Pointer   myImageType3Pointer;
-  typedef myFilterType::Pointer   myFilterTypePointer;
+  // Declare the type for the filter
+  typedef itk::MaximumImageFilter< ImageType, ImageType,
+    ImageType > MaximumImageFilterType;
 
   // Create two images
-  myImageType1Pointer inputImageA  = myImageType1::New();
-  myImageType2Pointer inputImageB  = myImageType2::New();
+  ImageType::Pointer inputImageA = ImageType::New();
+  ImageType::Pointer inputImageB = ImageType::New();
 
   // Define their size, and start index
-  mySizeType size;
+  SizeType size;
   size[0] = 2;
   size[1] = 2;
   size[2] = 2;
 
-  myIndexType start;
+  IndexType start;
   start[0] = 0;
   start[1] = 0;
   start[2] = 0;
 
-  myRegionType region;
+  RegionType region;
   region.SetIndex( start );
   region.SetSize( size );
 
@@ -83,64 +76,59 @@ int itkMaximumImageFilterTest(int, char* [] )
   inputImageB->SetRequestedRegion( region );
   inputImageB->Allocate();
 
+  // Define the pixel values for each image
+  PixelType largePixelValue = 3.0;
+  PixelType smallPixelValue = 2.0;
 
   // Declare Iterator types apropriated for each image
-  typedef itk::ImageRegionIteratorWithIndex<myImageType1>  myIteratorType1;
-  typedef itk::ImageRegionIteratorWithIndex<myImageType2>  myIteratorType2;
-  typedef itk::ImageRegionIteratorWithIndex<myImageType3>  myIteratorType3;
+  typedef itk::ImageRegionIteratorWithIndex< ImageType > IteratorType;
 
   // Create one iterator for Image A (this is a light object)
-  myIteratorType1 it1( inputImageA, inputImageA->GetBufferedRegion() );
+  IteratorType it1( inputImageA, inputImageA->GetBufferedRegion() );
 
   // Initialize the content of Image A
-  std::cout << "First operand " << std::endl;
   while( !it1.IsAtEnd() )
   {
-    it1.Set( 2.0 );
-    std::cout << it1.Get() << std::endl;
+    it1.Set( smallPixelValue );
     ++it1;
   }
 
   // Create one iterator for Image B (this is a light object)
-  myIteratorType2 it2( inputImageB, inputImageB->GetBufferedRegion() );
+  IteratorType it2( inputImageB, inputImageB->GetBufferedRegion() );
 
   // Initialize the content of Image B
-  std::cout << "Second operand " << std::endl;
   while( !it2.IsAtEnd() )
   {
-    it2.Set( 3.0 );
-    std::cout << it2.Get() << std::endl;
+    it2.Set( largePixelValue );
     ++it2;
   }
 
+  // Create the filter
+  MaximumImageFilterType::Pointer maximumImageFilter = MaximumImageFilterType::New();
 
-  // Create an MULTIPLY Filter
-  myFilterTypePointer filter = myFilterType::New();
-
+  EXERCISE_BASIC_OBJECT_METHODS( maximumImageFilter, MaximumImageFilter,
+    BinaryFunctorImageFilter);
 
   // Connect the input images
-  filter->SetInput1( inputImageA );
-  filter->SetInput2( inputImageB );
+  maximumImageFilter->SetInput1( inputImageA );
+  maximumImageFilter->SetInput2( inputImageB );
 
-  // Get the Smart Pointer to the Filter Output
-  myImageType3Pointer outputImage = filter->GetOutput();
+  // Get the Smart Pointer to the filter output
+  ImageType::Pointer outputImage = maximumImageFilter->GetOutput();
 
+  maximumImageFilter->SetFunctor( maximumImageFilter->GetFunctor() );
 
   // Execute the filter
-  filter->Update();
-  filter->SetFunctor(filter->GetFunctor());
+  maximumImageFilter->Update();
 
-  // Create an iterator for going through the image output
-  myIteratorType3 it3(outputImage, outputImage->GetBufferedRegion());
+  // Test some pixels in the result image
+  // Note that we are not comparing the entirety of the filter output in order
+  // to keep compile time as small as possible
 
-  //  Print the content of the result image
-  std::cout << " Result " << std::endl;
-  while( !it3.IsAtEnd() )
-  {
-    std::cout << it3.Get() << std::endl;
-    ++it3;
-  }
+  ImageType::IndexType pixelIndex = {{0, 1, 1}};
 
+  TEST_EXPECT_EQUAL( outputImage->GetPixel( start ), largePixelValue );
+  TEST_EXPECT_EQUAL( outputImage->GetPixel( pixelIndex ), largePixelValue );
 
   // All objects should be automatically destroyed at this point
   return EXIT_SUCCESS;
