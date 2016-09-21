15775f503503be9f8209460d1d6da9da37d84244
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2016-09-21 20:34:29 +1200

    ENH: [WIP] Add Shannon Isotropic Wavelet.

---
+++ Modules/Filtering/IsotropicWavelets/include/itkShannonIsotropicWavelet.h
@@ -0,0 +1,89 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef itkShannonIsotropicWavelet_h
+#define itkShannonIsotropicWavelet_h
+
+#include <itkIsotropicWaveletFrequencyFunction.h>
+
+namespace itk
+{
+/** \class ShannonIsotropicWavelet
+ *
+ * Shannon Wavelet
+ *
+ * Implement function in frequency space.
+ *
+ \f{equation}
+   h(\omega) =
+     \begin{cases}
+     \begin{aligned}
+       &1, &\omega \in [\frac{\pi}{2} , \pi] \\
+       &0, &\text{otherwise}
+     \end{aligned}
+     \end{cases}
+ \f{equation}
+ *
+ * \ingroup SpatialFunctions
+ * \ingroup IsotropicWavelets
+ */
+template <typename TFunctionValue = double,
+          unsigned int VImageDimension = 3,
+          typename TInput = Point<SpacePrecisionType, VImageDimension>>
+class ShannonIsotropicWavelet : public IsotropicWaveletFrequencyFunction<TFunctionValue, VImageDimension, TInput>
+{
+public:
+  /** Standard class typedefs. */
+  typedef ShannonIsotropicWavelet                                                    Self;
+  typedef IsotropicWaveletFrequencyFunction<TFunctionValue, VImageDimension, TInput> Superclass;
+  typedef SmartPointer<Self>                                                         Pointer;
+  typedef SmartPointer<const Self>                                                   ConstPointer;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(ShannonIsotropicWavelet, SpatialFunction);
+
+  /** Input type for the function. */
+  typedef typename Superclass::InputType InputType;
+
+  /** FunctionValue type for the function. */
+  typedef typename Superclass::FunctionValueType FunctionValueType;
+
+  /** Evaluate the function */
+  FunctionValueType
+  EvaluateMagnitude(const FunctionValueType & freq_norm_in_hz) const ITK_OVERRIDE;
+
+protected:
+  ShannonIsotropicWavelet();
+  virtual ~ShannonIsotropicWavelet();
+  void
+  PrintSelf(std::ostream & os, Indent indent) const ITK_OVERRIDE;
+
+private:
+  ShannonIsotropicWavelet(const Self &) ITK_DELETE_FUNCTION;
+  void
+  operator=(const Self &) ITK_DELETE_FUNCTION;
+};
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#  include "itkShannonIsotropicWavelet.hxx"
+#endif
+
+#endif
---
+++ Modules/Filtering/IsotropicWavelets/include/itkShannonIsotropicWavelet.hxx
@@ -0,0 +1,54 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef itkShannonIsotropicWavelet_hxx
+#define itkShannonIsotropicWavelet_hxx
+
+#include <cmath>
+#include <itkMath.h>
+#include <itkShannonIsotropicWavelet.h>
+
+namespace itk
+{
+template <typename TFunctionValue, unsigned int VImageDimension, typename TInput>
+ShannonIsotropicWavelet<TFunctionValue, VImageDimension, TInput>::ShannonIsotropicWavelet()
+{}
+
+template <typename TFunctionValue, unsigned int VImageDimension, typename TInput>
+ShannonIsotropicWavelet<TFunctionValue, VImageDimension, TInput>::~ShannonIsotropicWavelet()
+{}
+
+template <typename TFunctionValue, unsigned int VImageDimension, typename TInput>
+void
+ShannonIsotropicWavelet<TFunctionValue, VImageDimension, TInput>::PrintSelf(std::ostream & os, Indent indent) const
+{
+  Superclass::PrintSelf(os, indent);
+}
+
+template <typename TFunctionValue, unsigned int VImageDimension, typename TInput>
+typename ShannonIsotropicWavelet<TFunctionValue, VImageDimension, TInput>::FunctionValueType
+ShannonIsotropicWavelet<TFunctionValue, VImageDimension, TInput>::EvaluateMagnitude(
+  const FunctionValueType & freq_norm_in_hz) const
+{
+  // freq_in_rad_per_sec = freq_norm_in_hz * 2 * pi
+  if (freq_norm_in_hz >= 0.25 && freq_norm_in_hz <= 0.5)
+    return 1;
+  return 0;
+}
+} // end namespace itk
+
+#endif
