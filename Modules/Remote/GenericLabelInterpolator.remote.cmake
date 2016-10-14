3c7957957b1efee4d78e5ad57dc930e17a49d1f2
Francois Budin <francois.budin@gmail.com>
2016-10-14 13:31:59 -0400

    ENH: Updating ITKGenericLabelInterpolator remote module
    Updating remote repository to repository in InsightSoftwareConsortium
    Github and updating version to compile and pass tests with current ITK
    version.
    
    https://github.com/InsightSoftwareConsortium/ITKGenericLabelInterpolator/compare/b057e698644fcaa1f9f07882e3a2fcd5a0823a45...210c3e6eeece4875ac7d331460b3a4d2e4f6db60
    
    Change-Id: Id3b3a2aa6ab5c3b4f52b10653f1476b096d7c1aa

--- Modules/Remote/GenericLabelInterpolator.remote.cmake
+++ Modules/Remote/GenericLabelInterpolator.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(GenericLabelInterpolator
   "A generic interpolator for multi-label images."
-  GIT_REPOSITORY ${git_protocol}://github.com/fbudin69500/ITKGenericLabelInterpolator.git
-  GIT_TAG b057e698644fcaa1f9f07882e3a2fcd5a0823a45
+  GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKGenericLabelInterpolator.git
+  GIT_TAG 210c3e6eeece4875ac7d331460b3a4d2e4f6db60
   )
