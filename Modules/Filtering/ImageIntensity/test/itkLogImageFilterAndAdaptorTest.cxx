c91943eeda54e4686d3a1cfdc96a9be1af4a3662
Jon Haitz Legarreta <jhlegarreta@vicomtech.org>
2016-11-01 10:08:04 +0100

    ENH: Improve itkLogImageFilter coverage.
    Exercise basic object methods.
    
    Set the functor before updating the filter.
    
    Update the filter before getting its output.
    
    Use itk::Math::FloatAlmostEqual for floating point comparisons.
    
    Remove standard output messages.
    
    Minor style changes (variable names, white spaces, etc.) for the sake of
    readability.
    
    Change-Id: I770a780934333bbaa8f75b201608726d692f9300

--- Modules/Filtering/ImageIntensity/test/itkLogImageFilterAndAdaptorTest.cxx
+++ Modules/Filtering/ImageIntensity/test/itkLogImageFilterAndAdaptorTest.cxx
@@ -18,36 +18,40 @@
 
 #include "itkLogImageFilter.h"
 #include "itkLogImageAdaptor.h"
+#include "itkMath.h"
 #include "itkSubtractImageFilter.h"
+#include "itkTestingMacros.h"
 
 
-int itkLogImageFilterAndAdaptorTest(int, char* [] )
+int itkLogImageFilterAndAdaptorTest( int, char* [] )
 {
-
   // Define the dimension of the images
   const unsigned int ImageDimension = 3;
 
-  // Declare the types of the images
-  typedef itk::Image<float, ImageDimension>  InputImageType;
-  typedef itk::Image<float, ImageDimension>  OutputImageType;
+  // Declare the pixel types of the images
+  typedef float                PixelType;
 
-  // Declare Iterator types apropriated for each image
+  // Declare the types of the images
+  typedef itk::Image< PixelType, ImageDimension> InputImageType;
+  typedef itk::Image< PixelType, ImageDimension> OutputImageType;
+
+  // Declare appropriate Iterator types for each image
   typedef itk::ImageRegionIteratorWithIndex<
-                                  InputImageType>  InputIteratorType;
+                                  InputImageType > InputIteratorType;
   typedef itk::ImageRegionIteratorWithIndex<
-                                  OutputImageType> OutputIteratorType;
+                                  OutputImageType > OutputIteratorType;
 
   // Declare the type of the index to access images
-  typedef itk::Index<ImageDimension>         IndexType;
+  typedef itk::Index< ImageDimension >         IndexType;
 
   // Declare the type of the size
-  typedef itk::Size<ImageDimension>          SizeType;
+  typedef itk::Size< ImageDimension >          SizeType;
 
   // Declare the type of the Region
-  typedef itk::ImageRegion<ImageDimension>   RegionType;
+  typedef itk::ImageRegion< ImageDimension >   RegionType;
 
-  // Create two images
-  InputImageType::Pointer inputImage  = InputImageType::New();
+  // Create the input image
+  InputImageType::Pointer inputImage = InputImageType::New();
 
   // Define their size, and start index
   SizeType size;
@@ -69,57 +73,54 @@ int itkLogImageFilterAndAdaptorTest(int, char* [] )
   inputImage->SetBufferedRegion( region );
   inputImage->SetRequestedRegion( region );
   inputImage->Allocate();
+
   // Create one iterator for the Input Image (this is a light object)
   InputIteratorType it( inputImage, inputImage->GetBufferedRegion() );
 
   // Initialize the content of Image A
   const double value = itk::Math::pi / 6.0;
-  std::cout << "Content of the Input " << std::endl;
   it.GoToBegin();
   while( !it.IsAtEnd() )
     {
     it.Set( value );
-    std::cout << it.Get() << std::endl;
     ++it;
     }
 
   // Declare the type for the Log filter
-  typedef itk::LogImageFilter< InputImageType,
-                               OutputImageType  >  FilterType;
+  typedef itk::LogImageFilter< InputImageType, OutputImageType > FilterType;
 
-
-  // Create an ADD Filter
+  // Create the filter
   FilterType::Pointer filter = FilterType::New();
 
+  EXERCISE_BASIC_OBJECT_METHODS( filter, LogImageFilter,
+    UnaryFunctorImageFilter );
 
-  // Connect the input images
+  // Set the input image
   filter->SetInput( inputImage );
 
-  // Get the Smart Pointer to the Filter Output
-  OutputImageType::Pointer outputImage = filter->GetOutput();
-
+  filter->SetFunctor( filter->GetFunctor() );
 
   // Execute the filter
   filter->Update();
-  filter->SetFunctor(filter->GetFunctor());
+
+  // Get the filter output
+  OutputImageType::Pointer outputImage = filter->GetOutput();
 
   // Create an iterator for going through the image output
-  OutputIteratorType ot(outputImage, outputImage->GetRequestedRegion());
+  OutputIteratorType ot( outputImage, outputImage->GetRequestedRegion() );
 
   //  Check the content of the result image
-  std::cout << "Verification of the output " << std::endl;
   const OutputImageType::PixelType epsilon = 1e-6;
   ot.GoToBegin();
   it.GoToBegin();
   while( !ot.IsAtEnd() )
     {
-    std::cout <<  ot.Get() << " = ";
-    std::cout <<  std::log( it.Get() )  << std::endl;
     const InputImageType::PixelType  input  = it.Get();
     const OutputImageType::PixelType output = ot.Get();
     const OutputImageType::PixelType logarithm  = std::log(input);
-    if( std::fabs( logarithm - output ) > epsilon )
+    if( !itk::Math::FloatAlmostEqual( logarithm, output, 10, epsilon ) )
       {
+      std::cerr.precision( static_cast< int >( itk::Math::abs( std::log10( epsilon ) ) ) );
       std::cerr << "Error in itkLogImageFilterTest " << std::endl;
       std::cerr << " std::log( " << input << ") = " << logarithm << std::endl;
       std::cerr << " differs from " << output;
@@ -131,46 +132,48 @@ int itkLogImageFilterAndAdaptorTest(int, char* [] )
     }
 
 
-  //---------------------------------------
-  // This section tests for LogImageAdaptor
-  //---------------------------------------
+  //
+  // Test the itk::LogImageAdaptor
+  //
 
-  typedef itk::LogImageAdaptor<InputImageType,
-                          OutputImageType::PixelType>  AdaptorType;
+  typedef itk::LogImageAdaptor< InputImageType,
+                          OutputImageType::PixelType > AdaptorType;
 
   AdaptorType::Pointer logAdaptor = AdaptorType::New();
+
+  EXERCISE_BASIC_OBJECT_METHODS( logAdaptor, LogImageAdaptor,
+    ImageAdaptor );
 
   logAdaptor->SetImage( inputImage );
 
   typedef itk::SubtractImageFilter<
                         OutputImageType,
                         AdaptorType,
-                        OutputImageType   > DiffFilterType;
+                        OutputImageType > DiffFilterType;
 
   DiffFilterType::Pointer diffFilter = DiffFilterType::New();
 
   diffFilter->SetInput1( outputImage );
-  diffFilter->SetInput2( logAdaptor  );
+  diffFilter->SetInput2( logAdaptor );
 
   diffFilter->Update();
 
-  // Get the Smart Pointer to the Diff filter Output
+  // Get the filter output
   OutputImageType::Pointer diffImage = diffFilter->GetOutput();
 
   //  Check the content of the diff image
-  std::cout << "Comparing the results with those of an Adaptor" << std::endl;
-  std::cout << "Verification of the output " << std::endl;
+  //
 
   // Create an iterator for going through the image output
-  OutputIteratorType dt(diffImage, diffImage->GetRequestedRegion());
+  OutputIteratorType dt( diffImage, diffImage->GetRequestedRegion() );
 
   dt.GoToBegin();
   while( !dt.IsAtEnd() )
     {
-    std::cout <<  dt.Get() << std::endl;
     const OutputImageType::PixelType diff = dt.Get();
     if( std::fabs( diff ) > epsilon )
       {
+      std::cerr.precision( static_cast< int >( itk::Math::abs( std::log10( epsilon ) ) ) );
       std::cerr << "Error in itkLogImageFilterTest " << std::endl;
       std::cerr << "Comparing results with Adaptors" << std::endl;
       std::cerr << " difference = " << diff << std::endl;
