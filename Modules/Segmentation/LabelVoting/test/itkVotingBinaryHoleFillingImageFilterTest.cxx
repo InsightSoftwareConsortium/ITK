9715815a6b73e7ac9a3f11a7529bf598fa30daa1
Jon Haitz Legarreta <jhlegarreta@vicomtech.org>
2016-11-24 11:28:42 +0100

    ENH: Improve itkVotingBinaryHoleFillingImageFilter coverage.
    Exercise basic object methods.
    
    Exercise Set/Get methods using the TEST_SET_GET_VALUE macro.
    
    Make the test quantitative: compare to a baseline using the MD5 hash.
    
    Prefer initialization to assignment.
    
    Fix some oversights in the comments.
    
    Improve the test style: use typedefs, improve variable naming, etc.
    
    Change-Id: Ibae51c8f5b8ea904339d8b42e1095b6cb1902ff1

--- Modules/Segmentation/LabelVoting/test/itkVotingBinaryHoleFillingImageFilterTest.cxx
+++ Modules/Segmentation/LabelVoting/test/itkVotingBinaryHoleFillingImageFilterTest.cxx
@@ -16,110 +16,94 @@
  *
  *=========================================================================*/
 
-#include "itkRandomImageSource.h"
+
 #include "itkBinaryThresholdImageFilter.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
 #include "itkVotingBinaryHoleFillingImageFilter.h"
 #include "itkTextOutput.h"
+#include "itkTestingMacros.h"
 
 
-int itkVotingBinaryHoleFillingImageFilterTest(int, char* [] )
+int itkVotingBinaryHoleFillingImageFilterTest( int argc, char* argv[] )
 {
-  // Comment the following if you want to use the itk text output window
-  itk::OutputWindow::SetInstance(itk::TextOutput::New());
 
+  if ( argc != 3 )
+  {
+    std::cerr << "Missing arguments" << std::endl;
+    std::cerr << "Usage: " << argv[0] << " Inputimage OutputImage" << std::endl;
+    return EXIT_FAILURE;
+  }
 
-  typedef itk::Image<unsigned short,2> ImageType;
+  // Define the dimension of the images
+  const unsigned int Dimension = 2;
 
-  itk::RandomImageSource<ImageType>::Pointer random;
-  random = itk::RandomImageSource<ImageType>::New();
-  random->SetMin(   0 );
-  random->SetMax( 100 );
+  // Declare the pixel types of the images
+  typedef unsigned short                            InputPixelType;
+  typedef unsigned char                             OutputPixelType;
+  typedef itk::Image< InputPixelType, Dimension >   InputImageType;
+  typedef itk::Image< OutputPixelType, Dimension >  OutputImageType;
+  typedef itk::ImageFileReader< InputImageType >    ReaderType;
+  typedef itk::ImageFileWriter< OutputImageType >   WriterType;
 
-  ImageType::SizeValueType randomSize[2];
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( argv[1] );
 
-  randomSize[0] = randomSize[1] = 8;
+  TRY_EXPECT_NO_EXCEPTION( reader->Update() );
 
-  random->SetSize(randomSize);
+  InputImageType::PixelType foreground = 97; // Prime numbers are good testers
+  InputImageType::PixelType background = 29;
 
-  ImageType::SpacingValueType spacing[2] = {0.7, 2.1};
-  random->SetSpacing( spacing );
+  itk::BinaryThresholdImageFilter< InputImageType, InputImageType >::Pointer thresholder =
+    itk::BinaryThresholdImageFilter< InputImageType, InputImageType >::New();
 
-  ImageType::PointValueType origin[2] = {15, 400};
-  random->SetOrigin( origin );
-
-  ImageType::PixelType foreground =  97; // prime numbers are good testers
-  ImageType::PixelType background =  29;
-
-  itk::BinaryThresholdImageFilter<ImageType,ImageType>::Pointer thresholder;
-  thresholder =  itk::BinaryThresholdImageFilter<ImageType,ImageType>::New();
-  thresholder->SetInput( random->GetOutput() );
+  thresholder->SetInput( reader->GetOutput() );
   thresholder->SetLowerThreshold(  30 );
   thresholder->SetUpperThreshold( 100 );
   thresholder->SetInsideValue( foreground );
   thresholder->SetOutsideValue( background );
 
-  // Create a voting image
-  itk::VotingBinaryHoleFillingImageFilter<ImageType, ImageType>::Pointer voting;
-  voting = itk::VotingBinaryHoleFillingImageFilter<ImageType,ImageType>::New();
-  voting->SetInput( thresholder->GetOutput());
+  // Define the voting binary hole filling filter
+  typedef itk::VotingBinaryHoleFillingImageFilter< InputImageType, OutputImageType >
+    VotingBinaryHoleFillingImageFilterType;
+
+  VotingBinaryHoleFillingImageFilterType::Pointer voting =
+    VotingBinaryHoleFillingImageFilterType::New();
+
+  EXERCISE_BASIC_OBJECT_METHODS( voting, VotingBinaryHoleFillingImageFilter,
+    VotingBinaryImageFilter );
+
+  // Define the neighborhood size used for the voting filter (3x3)
+  InputImageType::SizeType neighRadius;
+  neighRadius[0] = 1;
+  neighRadius[1] = 1;
+  voting->SetRadius( neighRadius );
+
+  // Set the number of pixels over 50% that will tip the decision about
+  // switching a pixel
+  unsigned int majorityThreshold = 1;
+  voting->SetMajorityThreshold( majorityThreshold );
+  TEST_SET_GET_VALUE( majorityThreshold, voting->GetMajorityThreshold() );
+
   voting->SetForegroundValue( foreground );
   voting->SetBackgroundValue( background );
 
+  voting->SetInput( thresholder->GetOutput());
 
-  // define the neighborhood size used for the voting filter (5x5)
-  ImageType::SizeType neighRadius;
-  neighRadius[0] = 1;
-  neighRadius[1] = 1;
-  voting->SetRadius(neighRadius);
+  // Execute the filter
+  TRY_EXPECT_NO_EXCEPTION( voting->Update() );
 
 
-  // Set the number of pixels over 50% that will tip the decision about
-  // switching a pixel.
-  voting->SetMajorityThreshold( 1 );
+  std::cout << "Number of pixels changed: "
+    << voting->GetNumberOfPixelsChanged() << std::endl;
 
-  // run the algorithm
-  voting->Update();
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetInput( voting->GetOutput() );
+  writer->SetFileName( argv[2] );
 
-  itk::ImageRegionIterator<ImageType> it;
-  it = itk::ImageRegionIterator<ImageType>(random->GetOutput(),
-                               random->GetOutput()->GetBufferedRegion());
-  std::cout << "Input image" << std::endl;
-  unsigned int i;
-  for (i=1; !it.IsAtEnd(); ++i, ++it)
-    {
-    std::cout << "\t" << it.Get();
-    if ((i % 8) == 0)
-      {
-      std::cout << std::endl;
-      }
-    }
-
-  it = itk::ImageRegionIterator<ImageType>(thresholder->GetOutput(),
-                               thresholder->GetOutput()->GetBufferedRegion());
-  std::cout << "Binary image" << std::endl;
-
-  for (i=1; !it.IsAtEnd(); ++i, ++it)
-    {
-    std::cout << "\t" << it.Get();
-    if ((i % 8) == 0)
-      {
-      std::cout << std::endl;
-      }
-    }
+  TRY_EXPECT_NO_EXCEPTION( writer->Update() );
 
 
-  std::cout << "Output image" << std::endl;
-  it = itk::ImageRegionIterator<ImageType>(voting->GetOutput(),
-                               voting->GetOutput()->GetBufferedRegion());
-  for (i=1; !it.IsAtEnd(); ++i, ++it)
-    {
-    std::cout << "\t" << it.Get();
-    if ((i % 8) == 0)
-      {
-      std::cout << std::endl;
-      }
-    }
-
-  std::cout << "Number Of Pixels Changed = " << voting->GetNumberOfPixelsChanged() << std::endl;
+  std::cout << "Test finished" << std::endl;
   return EXIT_SUCCESS;
 }
