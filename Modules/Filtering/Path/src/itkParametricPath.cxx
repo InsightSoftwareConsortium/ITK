0aa2e6fe7411c46ec21c27cbcde8eb42f4cdc4b4
Bradley Lowekamp <blowekamp@mail.nih.gov>
2016-11-25 13:56:43 -0500

    COMP: Correct TEMPLATE_EXPLICIT definition in Path module
    Address compilation with VS9 2008 with static library. The declaration
    of the explicit template with extern causes the following compilation
    error:
    itkParametricPath.cxx(34) : error C2960: 'itk::ParametricPath' :
    inconsistent explicit instantiations, a previous explicit
    instantiation specified 'extern template'
    
    Change-Id: If85a4fb5f78f6a11e8be858ce541329d859dd4a1

--- Modules/Filtering/Path/src/itkParametricPath.cxx
+++ Modules/Filtering/Path/src/itkParametricPath.cxx
@@ -15,7 +15,7 @@
  *  limitations under the License.
  *
  *=========================================================================*/
-#define ITK_TEMPLATE_EXPLICIT_MetaDataObject
+#define ITK_TEMPLATE_EXPLICIT_ParametricPath
 #include "itkParametricPath.h"
 
 namespace itk
