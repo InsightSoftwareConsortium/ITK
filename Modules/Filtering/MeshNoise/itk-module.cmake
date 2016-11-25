d7a300c0c8a980d269abce9f0e38227e2bdbafc3
Davis Vigneault <davis.vigneault@gmail.com>
2016-11-25 10:30:49 +0000

    Add link to Insight Journal submission.

--- Modules/Filtering/MeshNoise/itk-module.cmake
+++ Modules/Filtering/MeshNoise/itk-module.cmake
@@ -1,5 +1,7 @@
 set(DOCUMENTATION "This module contains classes to perturb
-itk::Mesh and itk::QuadEdgeMesh classes with Gaussian noise.")
+itk::Mesh and itk::QuadEdgeMesh objects with Gaussian noise.
+This module is introduced in the Insight Journal article
+http://hdl.handle.net/10380/3567")
 
 itk_module(DVMeshNoise
   DEPENDS
