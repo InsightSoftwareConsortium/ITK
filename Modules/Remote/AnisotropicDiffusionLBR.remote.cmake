cdfe243633225ffcc601db22dd63182a05a76121
Dženan Zukić <dzenan.zukic@kitware.com>
2016-11-26 15:59:24 -0500

    ENH: updating version of AnisotropicDiffusionLBR
    Commit messages:
    * COMP: Add missing header to itkStructureTensorImageFilter.hxx
    * COMP: Disable Python in the Docker tests
    A timeout in the CircleCI Docker build appears to be related to CircleCI
    infrastructure.
    * ENH: More reliable Debian package fetching
    * ENH: Use default Python package version
    * STYLE: Rename itk::CoherenceEnhancingDiffusionImageFilter
    Follow ITK style naming conventions (image filters should end with
    ImageFilter).
    * ENH: Add Python wrapping
    * ENH: Add better default template argument for TScalar (RealType)
    * ENH: Rename the testing image to insighttoolkit/anisotropicdiffusionlbr-test
    
    Change-Id: I4747fdbf528647645f31990fc415cc6716ebf6db

--- Modules/Remote/AnisotropicDiffusionLBR.remote.cmake
+++ Modules/Remote/AnisotropicDiffusionLBR.remote.cmake
@@ -20,5 +20,5 @@ itk_fetch_module(AnisotropicDiffusionLBR
     http://insight-journal.org/browse/publication/953
   "
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKAnisotropicDiffusionLBR.git
-  GIT_TAG 54e1bb25033218b5ec27be4cda03fc47be0e6001
+  GIT_TAG c1e74cc933fe86e0738b4df2def965df04c64d3a
   )
