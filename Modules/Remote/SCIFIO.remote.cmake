034c3c663b0525cee8866ea97336453ab883297c
Francois Budin <francois.budin@gmail.com>
2016-12-01 16:12:05 -0500

    ENH: Update SCIFIO to lastest (2016.12.01)
    Francois Budin (1):
          BUG: m_Argv memory was allocated with 'new[]' and deleted with 'delete'
    
    Matt McCormick (1):
          Merge pull request #30 from fbudin69500/memory_leak_argv
    
    https://github.com/scifio/scifio-imageio/compare/0af019131839807d963c41641df86702b091816a...233880a49125afdd980bb8d7ca511cb08aab8dd8
    
    Change-Id: I945af1645f97284447b0e911dbf5feed204bccf5

--- Modules/Remote/SCIFIO.remote.cmake
+++ Modules/Remote/SCIFIO.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(SCIFIO
   "SCIFIO (Bioformats) ImageIO plugin for ITK"
   GIT_REPOSITORY ${git_protocol}://github.com/scifio/scifio-imageio.git
-  GIT_TAG 0af019131839807d963c41641df86702b091816a
+  GIT_TAG 233880a49125afdd980bb8d7ca511cb08aab8dd8
   )
