02fbcf8b6007705557c1bebbda3103a15038296f
Francois Budin <francois.budin@gmail.com>
2017-01-17 10:25:22 -0500

    ENH: Update SCIFIO to latest revision (2017.01.17)
    Francois Budin (4):
          BUG: Remove CMAKE_INSTALL_PREFIX from install directory
          ENH: Improve usage of JAVA_HOME and SCIFIO_PATH environment variables
          ENH: Uses https instead of http to download jre and *.jars
          ENH: Remove extra slash (or backslash) in paths to facilitate concatenation
    
    Matt McCormick (3):
          Merge pull request #31 from fbudin69500/remove_CMAKE_INSTALL_PREFIX
          Merge pull request #32 from fbudin69500/ENH_Uses_https_to_download_jre_and_jars
          Merge pull request #33 from fbudin69500/Improve_Path_handling
    
    Change-Id: I407603553947c5c88e0c29b6db4f3990217b6936

--- Modules/Remote/SCIFIO.remote.cmake
+++ Modules/Remote/SCIFIO.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(SCIFIO
   "SCIFIO (Bioformats) ImageIO plugin for ITK"
   GIT_REPOSITORY ${git_protocol}://github.com/scifio/scifio-imageio.git
-  GIT_TAG 233880a49125afdd980bb8d7ca511cb08aab8dd8
+  GIT_TAG 4391143cbf7696dad5c8a15d52dbc46daafe7aaa
   )
