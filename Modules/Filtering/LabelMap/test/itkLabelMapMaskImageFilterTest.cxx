347c3b4514835547dbaaa7380892e16f917646c5
Jon Haitz Legarreta <jhlegarreta@vicomtech.org>
2017-01-20 19:43:09 +0100

    ENH: Improve itkLabelMapMaskImageFilter coverage.
    Exercise basic object methods.
    
    Check all Set/Get methods using the TEST_SET_GET_VALUE macro.
    
    A few changes to improve the test style.
    
    Change-Id: I61645ac51225776f9b9a96ac6604e1e43e91691a

--- Modules/Filtering/LabelMap/test/itkLabelMapMaskImageFilterTest.cxx
+++ Modules/Filtering/LabelMap/test/itkLabelMapMaskImageFilterTest.cxx
@@ -21,33 +21,34 @@
 
 #include "itkLabelImageToLabelMapFilter.h"
 #include "itkLabelMapMaskImageFilter.h"
+#include "itkTestingMacros.h"
 
 
-int itkLabelMapMaskImageFilterTest(int argc, char * argv[])
+int itkLabelMapMaskImageFilterTest( int argc, char * argv[] )
 {
 
   if( argc != 9 )
     {
     std::cerr << "usage: " << argv[0] << " labelImage input output label bg neg crop cropBorder" << std::endl;
-    // std::cerr << "  : " << std::endl;
-    exit(1);
+    return EXIT_FAILURE;
     }
 
-  // the filters are able to work in any dimension. Lets choose 3, so the program can be tested on
-  // 2D and 2D image.
-  const int dim = 3;
+  const unsigned int Dimension = 3;
 
-  // declare the input image type
-  typedef itk::Image< unsigned char, dim > ImageType;
+  // Declare the pixel type
+  typedef unsigned char PixelType;
 
-  // and the label object type to use. The input image is a label image, so the
+  // Declare the input image type
+  typedef itk::Image< PixelType, Dimension > ImageType;
+
+  // And the label object type to use. The input image is a label image, so the
   // type of the label can be the same type than the pixel type. itk::LabelObject is
   // chosen, because only the mask feature is tested here, so we don't need any
   // attribute.
-  typedef itk::LabelObject< unsigned char, dim > LabelObjectType;
-  typedef itk::LabelMap< LabelObjectType >       LabelMapType;
+  typedef itk::LabelObject< PixelType, Dimension >  LabelObjectType;
+  typedef itk::LabelMap< LabelObjectType >          LabelMapType;
 
-  // read the label image and the input image to be masked.
+  // Read the label image and the input image to be masked.
   typedef itk::ImageFileReader< ImageType > ReaderType;
   ReaderType::Pointer reader = ReaderType::New();
   reader->SetFileName( argv[1] );
@@ -55,13 +56,13 @@ int itkLabelMapMaskImageFilterTest(int argc, char * argv[])
   ReaderType::Pointer reader2 = ReaderType::New();
   reader2->SetFileName( argv[2] );
 
-  // convert the label image to a label collection image.
+  // Convert the label image to a label collection image.
   typedef itk::LabelImageToLabelMapFilter< ImageType, LabelMapType> I2LType;
   I2LType::Pointer i2l = I2LType::New();
   i2l->SetInput( reader->GetOutput() );
-//  i2l->SetUseBackground( true );
+  //i2l->SetUseBackground( true );
 
-  // then mask the image. Two inputs are required (the label collection image, and
+  // Then mask the image. Two inputs are required (the label collection image, and
   // the image to be masked). The label used to mask the image is passed with the
   // SetLabel() method. The background in the output image, where the image is masked,
   // is passed with SetBackground(). The user can choose to mask the image outside the
@@ -70,25 +71,67 @@ int itkLabelMapMaskImageFilterTest(int argc, char * argv[])
   // masked region, by calling SetCrop( true ), or to a region padded by a border, by
   // calling both SetCrop() and SetCropBorder(). The crop border defaults to 0, and the
   // image is not cropped by default.
-  typedef itk::LabelMapMaskImageFilter< LabelMapType, ImageType > MaskType;
-  MaskType::Pointer mask = MaskType::New();
-  mask->SetInput( i2l->GetOutput() );
-  mask->SetFeatureImage( reader2->GetOutput() );
-  mask->SetLabel( atoi(argv[4]) );
-  mask->SetBackgroundValue( atoi(argv[5]) );
-  mask->SetNegated( atoi(argv[6]) );
-  mask->SetCrop( atoi(argv[7]) );
-  MaskType::SizeType border;
+
+  typedef itk::LabelMapMaskImageFilter< LabelMapType, ImageType > MaskFilterType;
+  MaskFilterType::Pointer maskFilter = MaskFilterType::New();
+
+  EXERCISE_BASIC_OBJECT_METHODS( maskFilter, LabelMapMaskImageFilter,
+    LabelMapFilter );
+
+  maskFilter->SetInput( i2l->GetOutput() );
+
+  maskFilter->SetFeatureImage( reader2->GetOutput() );
+
+  MaskFilterType::InputImagePixelType label = atoi( argv[4] );
+  maskFilter->SetLabel( label );
+  TEST_SET_GET_VALUE( label, maskFilter->GetLabel() );
+
+  MaskFilterType::OutputImagePixelType backgroundValue = atoi( argv[5] );
+  maskFilter->SetBackgroundValue( backgroundValue );
+  TEST_SET_GET_VALUE( backgroundValue, maskFilter->GetBackgroundValue() );
+
+  bool negated = static_cast< bool >(atoi( argv[6] ) );
+  maskFilter->SetNegated( negated );
+  TEST_SET_GET_VALUE( negated, maskFilter->GetNegated() );
+  if( negated )
+    {
+    maskFilter->NegatedOn();
+    TEST_SET_GET_VALUE( true, maskFilter->GetNegated() );
+    }
+  else
+    {
+    maskFilter->NegatedOff();
+    TEST_SET_GET_VALUE( false, maskFilter->GetNegated() );
+    }
+
+  bool crop = static_cast< bool >( atoi( argv[7] ) );
+  maskFilter->SetCrop( crop );
+  TEST_SET_GET_VALUE( crop, maskFilter->GetCrop() );
+  if( crop )
+    {
+    maskFilter->CropOn();
+    TEST_SET_GET_VALUE( true, maskFilter->GetCrop() );
+    }
+  else
+    {
+    maskFilter->CropOff();
+    TEST_SET_GET_VALUE( false, maskFilter->GetCrop() );
+    }
+
+  MaskFilterType::SizeType border;
   border.Fill( atoi(argv[8]) );
-  mask->SetCropBorder( border );
-  itk::SimpleFilterWatcher watcher6(mask, "filter");
+  maskFilter->SetCropBorder( border );
+  TEST_SET_GET_VALUE( border, maskFilter->GetCropBorder() );
+
+  itk::SimpleFilterWatcher watcher( maskFilter, "LabelMapMaskImageFilter" );
 
   // Finally, save the output image.
   typedef itk::ImageFileWriter< ImageType > WriterType;
   WriterType::Pointer writer = WriterType::New();
-  writer->SetInput( mask->GetOutput() );
+  writer->SetInput( maskFilter->GetOutput() );
   writer->SetFileName( argv[3] );
-  writer->Update();
 
-  return 0;
+  TRY_EXPECT_NO_EXCEPTION( writer->Update() );
+
+  return EXIT_SUCCESS;
 }
