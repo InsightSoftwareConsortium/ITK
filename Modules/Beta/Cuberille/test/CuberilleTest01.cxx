654f04c6932edd60cfe10324c50588f077ee9ad1
Jon Haitz Legarreta <jhlegarreta@vicomtech.org>
2017-02-19 10:26:25 +0100

    ENH: Improve the itk::CuberilleImageToMeshFilter coverage.
    Exercise basic object methods.
    
    Exercise all Get methods using the TEST_SET_GET_VALUE macro.
    
    Style changes to the test in order to improve the readability.

--- Modules/Beta/Cuberille/test/CuberilleTest01.cxx
+++ Modules/Beta/Cuberille/test/CuberilleTest01.cxx
@@ -34,16 +34,27 @@
 #include "itkBSplineInterpolateImageFunction.h"
 #include "itkQuadricDecimationQuadEdgeMeshFilter.h"
 #include "itkQuadEdgeMeshDecimationCriteria.h"
+#include "itkTestingMacros.h"
 
-int CuberilleTest01(int argc, char * argv [])
+
+int CuberilleTest01 (int argc, char * argv [] )
 {
-try
-  {
+  if( argc < 6 )
+    {
+    std::cout << "Usage: " << argv[0];
+    std::cout << "InputImage OutputMesh IsoSurfaceValue ExpectedNumberOfPoints ExpectedNumberOfCells";
+    std::cout << "[GenerateTriangleFaces] [ProjectToIsoSurface] ";
+    std::cout << "[SurfaceDistanceThreshold] [StepLength] [StepLengthRelax] [MaximumNumberOfSteps]";
+    std::cout << std::endl;
+    return EXIT_FAILURE;
+    }
+
   // Typedefs
   const unsigned int Dimension = 3;
-  typedef unsigned char PixelType;
+  typedef unsigned char                       PixelType;
   //typedef signed short PixelType;
-  typedef itk::Image< PixelType, Dimension > ImageType;
+  typedef itk::Image< PixelType, Dimension >  ImageType;
+
 #if USE_QUAD_EDGE_MESH | USE_DECIMATION
   typedef itk::QuadEdgeMesh< PixelType, Dimension > MeshType;
 #else
@@ -58,40 +69,57 @@ try
 #endif
   typedef itk::CuberilleImageToMeshFilter< ImageType, MeshType, InterpolatorType > CuberilleType;
 
+
   // Read command-line parameters
-  if ( argc < 6 )
-    {
-      std::cout << "USAGE: " << argv[0];
-      std::cout << "InputImage OutputMesh IsoSurfaceValue ExpectedNumberOfPoints ExpectedNumberOfCells";
-      std::cout << "[GenerateTriangleFaces] [ProjectToIsoSurface] ";
-      std::cout << "[SurfaceDistanceThreshold] [StepLength] [StepLengthRelax] [MaximumNumberOfSteps]";
-      std::cout << std::endl;
-      return EXIT_FAILURE;
-    }
   int arg = 1;
-  char * FilenameInputImage = argv[arg++];
-  char * FilenameOutputMesh = argv[arg++];
-  PixelType IsoSurfaceValue = atoi( argv[arg++] );
-  unsigned int ExpectedNumberOfPoints = atoi( argv[arg++] );
-  unsigned int ExpectedNumberOfCells = atoi( argv[arg++] );
-  bool GenerateTriangleFaces = true;
-  if (argc > arg) GenerateTriangleFaces = atoi( argv[arg++] );
-  bool ProjectToIsoSurface = true;
-  if (argc > arg) ProjectToIsoSurface = atoi( argv[arg++] );
-  double SurfaceDistanceThreshold = 0.5;
-  if (argc > arg) SurfaceDistanceThreshold = atof( argv[arg++] );
-  double StepLength = 0.25;
-  if (argc > arg) StepLength = atof( argv[arg++] );
-  double StepLengthRelax = 0.95;
-  if (argc > arg) StepLengthRelax = atof( argv[arg++] );
-  unsigned int MaximumNumberOfSteps = 50;
-  if (argc > arg) MaximumNumberOfSteps = atoi( argv[arg++] );
+  char * filenameInputImage = argv[arg++];
+  char * filenameOutputMesh = argv[arg++];
+  PixelType isoSurfaceValue = atoi( argv[arg++] );
+  unsigned int expectedNumberOfPoints = atoi( argv[arg++] );
+  unsigned int expectedNumberOfCells = atoi( argv[arg++] );
+
+  bool generateTriangleFaces = true;
+  if( argc > arg )
+    {
+    generateTriangleFaces = atoi( argv[arg++] );
+    }
+
+  bool projectToIsoSurface = true;
+  if( argc > arg )
+    {
+    projectToIsoSurface = atoi( argv[arg++] );
+    }
+
+  double surfaceDistanceThreshold = 0.5;
+  if( argc > arg )
+    {
+    surfaceDistanceThreshold = atof( argv[arg++] );
+    }
+
+  double stepLength = 0.25;
+  if( argc > arg )
+    {
+    stepLength = atof( argv[arg++] );
+    }
+
+  double stepLengthRelax = 0.95;
+  if( argc > arg )
+    {
+    stepLengthRelax = atof( argv[arg++] );
+    }
+
+  unsigned int maximumNumberOfSteps = 50;
+  if( argc > arg )
+    {
+    maximumNumberOfSteps = atoi( argv[arg++] );
+    }
 
   // Read input image
-  std::cout << "Reading input image: " << FilenameInputImage << std::endl;
   ImageFileReaderType::Pointer reader = ImageFileReaderType::New();
-  reader->SetFileName( FilenameInputImage );
-  reader->UpdateLargestPossibleRegion();
+  reader->SetFileName( filenameInputImage );
+
+  TRY_EXPECT_NO_EXCEPTION( reader->UpdateLargestPossibleRegion() );
+
   ImageType::Pointer input = reader->GetOutput();
   input->DisconnectPipeline();
 
@@ -100,8 +128,7 @@ try
   itk::TimeProbe time;
 #if USE_MARCHING_CUBES
 
-  // Create marching cubes
-  std::cout << "Creating marching cubes mesh..." << std::endl;
+  // Create marching cubes mesh
   BinaryThresholdFilterType::Pointer threshold = BinaryThresholdFilterType::New();
   threshold->SetInput( input );
   threshold->SetLowerThreshold( IsoSurfaceValue );
@@ -109,36 +136,72 @@ try
   threshold->SetInsideValue( itk::NumericTraits<PixelType>::One );
   threshold->SetOutsideValue( itk::NumericTraits<PixelType>::Zero );
   threshold->UpdateLargestPossibleRegion();
+
   MarchingCubesType::Pointer marching = MarchingCubesType::New();
   marching->SetInput( threshold->GetOutput() );
+
   time.Start();
-  marching->Update();
+
+  TRY_EXPECT_NO_EXCEPTION( marching->Update() );
+
   time.Stop();
+
   outputMesh = marching->GetOutput();
   outputMesh->DisconnectPipeline();
 #else
 
   // Create cuberille mesh filter
-  std::cout << "Creating cuberille mesh..." << std::endl;
   CuberilleType::Pointer cuberille = CuberilleType::New();
+
+  EXERCISE_BASIC_OBJECT_METHODS( cuberille, CuberilleImageToMeshFilter,
+    ImageToMeshFilter );
+
   cuberille->SetInput( input );
-  cuberille->SetIsoSurfaceValue( IsoSurfaceValue );
+
+  cuberille->SetIsoSurfaceValue( isoSurfaceValue );
+  TEST_SET_GET_VALUE( isoSurfaceValue, cuberille->GetIsoSurfaceValue() );
+
   InterpolatorType::Pointer interpolator = InterpolatorType::New();
 #if USE_BSPLINE_INTERPOLATOR
-  interpolator->SetSplineOrder( 3 );
+  unsigned int splineOrder = 3;
+  interpolator->SetSplineOrder( splineOrder );
 #endif
   cuberille->SetInterpolator( interpolator );
-  cuberille->SetGenerateTriangleFaces( GenerateTriangleFaces );
-  cuberille->SetProjectVerticesToIsoSurface( ProjectToIsoSurface );
-  cuberille->SetProjectVertexSurfaceDistanceThreshold( SurfaceDistanceThreshold );
-  cuberille->SetProjectVertexStepLength( StepLength );
-  cuberille->SetProjectVertexStepLengthRelaxationFactor( StepLengthRelax );
-  cuberille->SetProjectVertexMaximumNumberOfSteps( MaximumNumberOfSteps );
+  TEST_SET_GET_VALUE( interpolator, cuberille->GetInterpolator() );
+
+  cuberille->SetGenerateTriangleFaces( generateTriangleFaces );
+  TEST_SET_GET_VALUE( generateTriangleFaces,
+    cuberille->GetGenerateTriangleFaces() );
+
+  cuberille->SetProjectVerticesToIsoSurface( projectToIsoSurface );
+  TEST_SET_GET_VALUE( projectToIsoSurface,
+    cuberille->GetProjectVerticesToIsoSurface() );
+
+  cuberille->SetProjectVertexSurfaceDistanceThreshold( surfaceDistanceThreshold );
+  TEST_SET_GET_VALUE( surfaceDistanceThreshold,
+    cuberille->GetProjectVertexSurfaceDistanceThreshold() );
+
+  cuberille->SetProjectVertexStepLength( stepLength );
+  TEST_SET_GET_VALUE( stepLength, cuberille->GetProjectVertexStepLength() );
+
+  cuberille->SetProjectVertexStepLengthRelaxationFactor( stepLengthRelax );
+  TEST_SET_GET_VALUE( stepLengthRelax,
+    cuberille->GetProjectVertexStepLengthRelaxationFactor() );
+
+  cuberille->SetProjectVertexMaximumNumberOfSteps( maximumNumberOfSteps );
+  TEST_SET_GET_VALUE( maximumNumberOfSteps,
+    cuberille->GetProjectVertexMaximumNumberOfSteps() );
+
   time.Start();
-  cuberille->Update();
+
+  TRY_EXPECT_NO_EXCEPTION( cuberille->Update() );
+
   time.Stop();
+
   outputMesh = cuberille->GetOutput();
+
   outputMesh->DisconnectPipeline();
+
 #endif
 
 #if USE_DECIMATION
@@ -147,46 +210,47 @@ try
   DecimationCriterionType::Pointer decimateCriterion = DecimationCriterionType::New();
   decimateCriterion->SetTopologicalChange( false );
   decimateCriterion->SetNumberOfElements( 2000 );
-  typedef itk::QuadricDecimationQuadEdgeMeshFilter< MeshType, MeshType, DecimationCriterionType > DecimationType;
+
+  typedef itk::QuadricDecimationQuadEdgeMeshFilter< MeshType, MeshType, DecimationCriterionType >
+    DecimationType;
   DecimationType::Pointer decimate = DecimationType::New();
-  decimate->SetInput( outputMesh );
   decimate->SetCriterion( decimateCriterion );
-  decimate->Update();
+
+  decimate->SetInput( outputMesh );
+
+  TRY_EXPECT_NO_EXCEPTION( decimate->Update() );
 #endif
 
   // Write mesh
-  std::cout << "Writing output mesh: " << FilenameOutputMesh << std::endl;
   MeshFileWriterType::Pointer writer = MeshFileWriterType::New();
 #if USE_DECIMATION
   writer->SetInput( decimate->GetOutput() );
 #else
   writer->SetInput( outputMesh );
 #endif
-  writer->SetFileName( FilenameOutputMesh );
-  writer->Update();
+  writer->SetFileName( filenameOutputMesh );
+
+  TRY_EXPECT_NO_EXCEPTION( writer->Update() );
 
   // Assert number of points/cells
   std::cout << "Polygonization took " << time.GetMean() << " seconds" << std::endl;
   std::cout << "Mesh has " << outputMesh->GetNumberOfPoints() << " vertices ";
   std::cout << "and " << outputMesh->GetNumberOfCells() << " cells" << std::endl;
-  if ( ExpectedNumberOfPoints > 0 && outputMesh->GetNumberOfPoints() != ExpectedNumberOfPoints )
+  if( expectedNumberOfPoints > 0 && outputMesh->GetNumberOfPoints() != expectedNumberOfPoints )
     {
-    std::cerr << "ERROR: Expected mesh with " << ExpectedNumberOfPoints
+    std::cerr << "Test failed!" << std::endl;
+    std::cerr << "Error: Expected mesh with " << expectedNumberOfPoints
               << " points, but found " << outputMesh->GetNumberOfPoints() << std::endl;
     return EXIT_FAILURE;
     }
-  if ( ExpectedNumberOfCells > 0 && outputMesh->GetNumberOfCells() != ExpectedNumberOfCells )
+  if( expectedNumberOfCells > 0 && outputMesh->GetNumberOfCells() != expectedNumberOfCells )
     {
-    std::cerr << "ERROR: Expected mesh with " << ExpectedNumberOfCells
+    std::cerr << "Test failed!" << std::endl;
+    std::cerr << "Error: Expected mesh with " << expectedNumberOfCells
               << " cells, but found " << outputMesh->GetNumberOfCells() << std::endl;
     return EXIT_FAILURE;
     }
+
+  std::cout << "Test finished" << std::endl;
   return EXIT_SUCCESS;
-  }
-catch (itk::ExceptionObject & err)
-  {
-  std::cerr << "ExceptionObject caught !" << std::endl; 
-  std::cerr << err << std::endl; 
-  return EXIT_FAILURE;
-  }
 }
