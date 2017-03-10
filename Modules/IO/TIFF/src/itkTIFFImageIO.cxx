e9921ee6b52432219faf9aa3eaf810bc926657ba
Yann Le Poul <le-poul@biologie.uni-muenchen.de>
2017-03-10 14:34:05 +0100

    BUG: ExpandRGBPalette used instead of IsReadAsScalarPlusPalette
    In ReadGenericImage, the information about the image is alreadyextracted.
    GetIsReadAsScalarPlusPalette() should therefore be used in addition of
    GetExpandRGBPalette().
    
    This commit doesn't affect the current behavior of tiff reading,
    but make the logic of tiff reading more coherent
    and robust to future changes.
    
    Change-Id: I4b124e3f68471a38ddbf1731cd1f29a8922bdc59

--- Modules/IO/TIFF/src/itkTIFFImageIO.cxx
+++ Modules/IO/TIFF/src/itkTIFFImageIO.cxx
@@ -1337,7 +1337,7 @@ void TIFFImageIO::ReadGenericImage(void *_out,
           }
         break;
       case TIFFImageIO::PALETTE_RGB:
-        if ( GetExpandRGBPalette() )
+        if ( this->GetExpandRGBPalette() || (!this->GetIsReadAsScalarPlusPalette()) )
           {
           switch ( m_InternalImage->m_BitsPerSample )
             {
