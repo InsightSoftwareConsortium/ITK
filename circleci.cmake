014cdf330dce7e90ec13cc0a86cc4f64edc3b658
Bradley Lowekamp <blowekamp@mail.nih.gov>
2017-03-14 12:17:00 -0400

    ENH: Improve set_from_env function and use it
    The organization of setting CTest variables is improve by grouping
    them. Conditional are remove by making use of REQUIRE and DEFAULT
    options for the set_from_env function.
    
    The custom MAKEJ environment variable should be replaced by the
    standard CTEST_BUILD_FLAGS being mapped from an environment variable
    to a CTEST variable. MAKEJ remains to ensure the release branch still
    compiles on CircleCI.
    
    These changes follow improvements committed to SimpleITK.

--- circleci.cmake
+++ circleci.cmake
@@ -1,45 +1,49 @@
-set(CTEST_SITE "CircleCI")
 
+# set_from_env
+# ------------
+#
+# Sets a CMake variable from an environment variable. If the
+# environment variable  is not  defined then the CMake variable is not
+# modified. If DEFAULT is specified then if the environment variable
+# is not defined the default value is used. Alternatively, if REQUIRED
+# is specified then a FATAL_ERROR is generated.
+#
+# set_from_env( <variable> <environment variable> [REQUIRED|DEFAULT value] )
 function(set_from_env var env_var)
   if(NOT DEFINED ENV{${env_var}})
-    message(FATAL_ERROR "Required environment variable \"${env_var}\" not defined.")
+    if (ARGV2 STREQUAL "REQUIRED")
+      message(FATAL_ERROR "Required environment variable \"${env_var}\" not defined.")
+    elseif (ARGV2 STREQUAL "DEFAULT")
+      set(${var} ${ARGV3} PARENT_SCOPE)
+    endif()
+  else()
+    set(${var} $ENV{${env_var}} PARENT_SCOPE)
   endif()
-  set(${var} $ENV{${env_var}} PARENT_SCOPE)
 endfunction()
 
-
-set_from_env(CTEST_DASHBOARD_ROOT  "CTEST_SOURCE_DIRECTORY")
-set_from_env(CTEST_DASHBOARD_ROOT  "CTEST_DASHBOARD_ROOT")
-set_from_env(CTEST_CONFIGURATION_TYPE "CTEST_CONFIGURATION_TYPE")
-
-string(SUBSTRING $ENV{CIRCLE_SHA1} 0 7 commit_sha1)
-
-set(CTEST_BUILD_NAME "CircleCI-$ENV{CIRCLE_BRANCH}-${commit_sha1}")
-
-if( NOT "$ENV{CI_PULL_REQUEST}" STREQUAL "" )
-  set(CTEST_BUILD_NAME "${CTEST_BUILD_NAME}-pull")
-endif()
-
-
-set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
-
-if (DEFINED ENV{MAKEJ})
-  set( CTEST_BUILD_FLAGS -j$ENV{MAKEJ})
-endif()
-
+set(CTEST_SITE "CircleCI")
+set(CTEST_UPDATE_VERSION_ONLY 1)
 set( CTEST_TEST_ARGS ${CTEST_TEST_ARGS} PARALLEL_LEVEL 2 )
 
-set_from_env(CTEST_SOURCE_DIRECTORY "CTEST_SOURCE_DIRECTORY" )
+
+# Make environment variables to CMake variables for CTest
+set_from_env(CTEST_CMAKE_GENERATOR "CTEST_CMAKE_GENERATOR" DEFAULT "Unix Makefiles" )
 set_from_env(CTEST_BINARY_DIRECTORY "CTEST_BINARY_DIRECTORY")
-set_from_env(dashboard_model "DASHBOARD_MODEL")
+set_from_env(CTEST_DASHBOARD_ROOT  "CTEST_DASHBOARD_ROOT" REQUIRED)
+set_from_env(CTEST_SOURCE_DIRECTORY "CTEST_SOURCE_DIRECTORY" REQUIRED)
+set_from_env(CTEST_CONFIGURATION_TYPE "CTEST_CONFIGURATION_TYPE" DEFAULT "Release")
 
-if (DEFINED ENV{CTEST_CONFIGURATION_TYPE})
-  set(CTEST_CONFIGURATION_TYPE "$ENV{CTEST_CONFIGURATION_TYPE}")
-endif()
+# Legacy support for MAKEJ environment variable
+# Please set CTEST_BUILD_FLAGS directly
+set_from_env(makej "MAKEJ")
+set_from_env(CTEST_BUILD_FLAGS "CTEST_BUILD_FLAGS" DEFAULT ${MAKEJ})
 
+# Construct build name based on what is being built
+string(SUBSTRING $ENV{CIRCLE_SHA1} 0 7 commit_sha1)
+set(CTEST_BUILD_NAME "CircleCI-$ENV{CIRCLE_BRANCH}-${commit_sha1}")
 
-set(CTEST_UPDATE_VERSION_ONLY 1)
-
+set_from_env(dashboard_git_branch "CIRCLE_BRANCH")
+set_from_env(dashboard_model "DASHBOARD_MODEL" DEFAULT "Continuous" )
 set(dashboard_loop 0)
 
 list(APPEND CTEST_NOTES_FILES
