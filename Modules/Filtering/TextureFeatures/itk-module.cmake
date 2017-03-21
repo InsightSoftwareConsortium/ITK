5bce10bbb3621f2de9033fc70aa908d7a63615f9
Jean-Baptiste VIMORT <jb.vimort@kitware.com>
2017-03-21 14:29:47 -0400

    ENH: First commit
    ITK Module Template

---
+++ Modules/Filtering/TextureFeatures/itk-module.cmake
@@ -0,0 +1,23 @@
+# the top-level README is used for describing this module, just
+# re-used it for documentation here
+# itk_module() defines the module dependencies in ModuleTemplate
+# ModuleTemplate depends on ITKCommon
+# The testing module in ModuleTemplate depends on ITKTestKernel
+# and ITKMetaIO(besides ModuleTemplate and ITKCore)
+# By convention those modules outside of ITK are not prefixed with
+# ITK.
+
+# define the dependencies of the include module and the tests
+itk_module(
+  ITKTextureFeatures
+  DEPENDS
+    ITKCommon
+  COMPILE_DEPENDS
+    ITKImageSources
+  TEST_DEPENDS
+    ITKTestKernel
+    ITKMetaIO
+  DESCRIPTION "Module ingested from upstream."
+  EXCLUDE_FROM_DEFAULT
+  ENABLE_SHARED
+)
