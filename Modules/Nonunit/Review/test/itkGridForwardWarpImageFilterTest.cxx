0e9e1f01c1ac090999b7bbedabce16957df7d8c3
Jon Haitz Legarreta <jhlegarreta@vicomtech.org>
2017-04-05 17:30:29 +0200

    ENH: Improve coverage for itkGridForwardWarpImageFilter.
    Exercise basic object methods.
    
    Exercise all Set/Get methods using the TEST_SET_GET_VALUE macro.
    
    Make the test quantitative: write the output image and compare agains the
    baseline using the MD5 hash.
    
    Minor style changes (white spaces, etc.)
    
    Change-Id: I66ca0310528c7eee4e69b4acca90989e263b9db9

--- Modules/Nonunit/Review/test/itkGridForwardWarpImageFilterTest.cxx
+++ Modules/Nonunit/Review/test/itkGridForwardWarpImageFilterTest.cxx
@@ -16,11 +16,21 @@
  *
  *=========================================================================*/
 
+#include "itkImageFileWriter.h"
 #include "itkGridForwardWarpImageFilter.h"
+#include "itkNumericTraits.h"
+#include "itkTestingMacros.h"
 
 
-int itkGridForwardWarpImageFilterTest(int, char* [] )
+int itkGridForwardWarpImageFilterTest( int argc, char* argv[] )
 {
+
+  if( argc != 2 )
+    {
+    std::cerr << "Missing parameters." << std::endl;
+    std::cerr << "Usage: " << argv[0] << " outputImage" << std::endl;
+    return EXIT_FAILURE;
+    }
 
   // Define the dimension of the images
   const unsigned int ImageDimension = 3;
@@ -29,27 +39,26 @@ int itkGridForwardWarpImageFilterTest(int, char* [] )
   typedef unsigned char                           OutputPixelType;
 
   // Declare the types of the images
-  typedef itk::Image<DeformationPixelType, ImageDimension>  DisplacementFieldType;
-  typedef itk::Image<OutputPixelType, ImageDimension>       OutputImageType;
+  typedef itk::Image< DeformationPixelType, ImageDimension >  DisplacementFieldType;
+  typedef itk::Image< OutputPixelType, ImageDimension >       OutputImageType;
 
-  // Declare Iterator types apropriated for each image
-  typedef itk::ImageRegionIteratorWithIndex< DisplacementFieldType >  DeformationIteratorType;
-  typedef itk::ImageRegionIteratorWithIndex< OutputImageType >  OutputIteratorType;
+  // Declare iterator types apropriated for each image
+  typedef itk::ImageRegionIteratorWithIndex< DisplacementFieldType > DeformationIteratorType;
 
 
   // Declare the type of the index to access images
-  typedef itk::Index<ImageDimension>         IndexType;
+  typedef itk::Index< ImageDimension >         IndexType;
 
   // Declare the type of the size
-  typedef itk::Size<ImageDimension>          SizeType;
+  typedef itk::Size< ImageDimension >          SizeType;
 
-  // Declare the type of the Region
-  typedef itk::ImageRegion<ImageDimension>   RegionType;
+  // Declare the type of the region
+  typedef itk::ImageRegion< ImageDimension >   RegionType;
 
-  // Create two images
-  DisplacementFieldType ::Pointer inputDisplacementField  = DisplacementFieldType ::New();
+  // Create an input image
+  DisplacementFieldType ::Pointer inputDisplacementField = DisplacementFieldType ::New();
 
-  // Define their size, and start index
+  // Define its size, and start index
   SizeType size;
   size[0] = 2;
   size[1] = 2;
@@ -64,17 +73,17 @@ int itkGridForwardWarpImageFilterTest(int, char* [] )
   region.SetIndex( start );
   region.SetSize( size );
 
-  // Initialize Image A
+  // Initialize the input image
   inputDisplacementField->SetLargestPossibleRegion( region );
   inputDisplacementField->SetBufferedRegion( region );
   inputDisplacementField->SetRequestedRegion( region );
   inputDisplacementField->Allocate();
 
-  // Create one iterator for the Input Image (this is a light object)
+  // Create one iterator for the input image (this is a light object)
   DeformationIteratorType it( inputDisplacementField,
                               inputDisplacementField->GetBufferedRegion() );
 
-  // Initialize the content of Image A
+  // Initialize the content of the input image
   DeformationPixelType vectorValue;
   vectorValue.Fill( 5.0 ); // FIXME: replace with something more interesting...
 
@@ -82,45 +91,48 @@ int itkGridForwardWarpImageFilterTest(int, char* [] )
   while( !it.IsAtEnd() )
     {
     it.Set( vectorValue );
-    std::cout << it.Get() << std::endl;
     ++it;
     }
 
-  // Declare the type for the Log filter
-  typedef itk::GridForwardWarpImageFilter<
-    DisplacementFieldType, OutputImageType  >   FilterType;
+  // Declare the type for the GridForwardWarpImageFilter filter
+  typedef itk::GridForwardWarpImageFilter< DisplacementFieldType,
+    OutputImageType > FilterType;
 
 
-  // Create one Filter
+  // Create the filter instance
   FilterType::Pointer filter = FilterType::New();
 
+  EXERCISE_BASIC_OBJECT_METHODS( filter, GridForwardWarpImageFilter,
+    ImageToImageFilter );
 
-  // Connect the input images
+  FilterType::PixelType backgroundValue =
+    itk::NumericTraits< FilterType::PixelType >::ZeroValue();
+  filter->SetBackgroundValue( backgroundValue );
+  TEST_SET_GET_VALUE( backgroundValue, filter->GetBackgroundValue() );
+
+  FilterType::PixelType foregroundValue =
+    itk::NumericTraits< FilterType::PixelType >::OneValue();
+  filter->SetForegroundValue( foregroundValue );
+  TEST_SET_GET_VALUE( foregroundValue, filter->GetForegroundValue());
+
+
+  // Set the input image
   filter->SetInput( inputDisplacementField );
 
   // Execute the filter
-  filter->Update();
+  TRY_EXPECT_NO_EXCEPTION( filter->Update() );
 
-  // Get the Smart Pointer to the Filter Output
-  OutputImageType::Pointer outputImage = filter->GetOutput();
 
-  // Create an iterator for going through the image output
-  OutputIteratorType ot(outputImage, outputImage->GetRequestedRegion());
+  // Write the result image
+  typedef itk::ImageFileWriter< OutputImageType > WriterType;
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetInput( filter->GetOutput() );
+  writer->SetFileName( argv[1] );
 
-  //  Check the content of the result image
-  std::cout << "Verification of the output " << std::endl;
 
-  ot.GoToBegin();
-  it.GoToBegin();
-  while( !ot.IsAtEnd() )
-    {
-    DeformationPixelType input  = it.Get();
-    OutputPixelType output = ot.Get();
-    std::cout << input << " => ";
-    std::cout << output  << std::endl;
-    ++ot;
-    ++it;
-    }
+  TRY_EXPECT_NO_EXCEPTION( writer->Update() );
 
+
+  std::cout << "Test finished." << std::endl;
   return EXIT_SUCCESS;
 }
