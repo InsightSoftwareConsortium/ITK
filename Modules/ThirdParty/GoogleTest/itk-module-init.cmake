23235bae339f22fd7bd09be1e3e64c1a3ffcb402
Bradley Lowekamp <blowekamp@mail.nih.gov>
2017-06-15 09:06:33 -0400

    COMP: Use single line for string argument, by-pass multi-line issues
    This addresses the following CMake configuration error:
    CMake Error at Modules/ThirdParty/GoogleTest/itk-module-init.cmake:1
    (option):
      Syntax error in cmake code at
    
        /home/kitware/Dashboards/src/ITK/Modules/ThirdParty/GoogleTest/itk-module-init.cmake:1
    
      when parsing string
    
        Use an outside build or install \
    
      source of GoogleTest.  Set GTEST_ROOT to specify location.
    
      syntax error, unexpected cal_SYMBOL, expecting $end (91)
    
    Change-Id: I6d2d61d4ed2a3a12b166a4dd3a9531ddf8bc2066

--- Modules/ThirdParty/GoogleTest/itk-module-init.cmake
+++ Modules/ThirdParty/GoogleTest/itk-module-init.cmake
@@ -1,3 +1,2 @@
-option(ITK_USE_SYSTEM_GOOGLETEST "Use an outside build or install \
-source of GoogleTest. Set GTEST_ROOT to specify location." ${ITK_USE_SYSTEM_LIBRARIES})
+option(ITK_USE_SYSTEM_GOOGLETEST "Use an outside build or installed source of GoogleTest. Set GTEST_ROOT to specify location." ${ITK_USE_SYSTEM_LIBRARIES})
 mark_as_advanced(ITK_USE_SYSTEM_GOOGLETEST)
