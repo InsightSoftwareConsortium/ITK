26fa3e04bc348d809825e839155dd24f97ae24eb
Rene-Paul Debroize <rene-paul.debroize@childrens.harvard.edu>
2017-06-28 13:22:43 -0400

    ENH: Add remote module TBBImageToImageFilter
    Change-Id: I02ec6da33e1f36ea8a8dc1c65121cf290b1812ab

---
+++ Modules/Remote/TBBImageToImageFilter.remote.cmake
@@ -0,0 +1,8 @@
+itk_fetch_module(TBBImageToImageFilter
+  "TBBImageToImageFilter
+  =====================
+  This module contains a class inheriting from `itk::ImageToimageFilter` that aim to optimize thread management using the TBB library.
+  Insight Journal article: http://www.insight-journal.org/browse/publication/974"
+  GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKTBBImageToImageFilter.git
+  GIT_TAG a59908d51a7ee8c342b699b5f9b8acfdf7324f07
+  )
