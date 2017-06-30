57adecf21e8800bd74108c054932ce36d6cb9c60
Bradley Lowekamp <blowekamp@mail.nih.gov>
2017-06-30 15:56:06 -0400

    ENH: Remove unneeded const_cast

--- Modules/Filtering/TextureFeatures/include/itkCoocurrenceTextureFeaturesImageFilter.hxx
+++ Modules/Filtering/TextureFeatures/include/itkCoocurrenceTextureFeaturesImageFilter.hxx
@@ -73,7 +73,7 @@ template <typename TInputImage, typename TOutputImage>
 void
 CoocurrenceTextureFeaturesImageFilter<TInputImage, TOutputImage>::BeforeThreadedGenerateData()
 {
-  InputImageType * maskPointer = const_cast<TInputImage *>(this->GetMaskImage());
+  const InputImageType * maskPointer = this->GetMaskImage();
   this->m_DigitalisedInputImageg = InputImageType::New();
   this->m_DigitalisedInputImageg->SetRegions(this->GetInput()->GetRequestedRegion());
   this->m_DigitalisedInputImageg->CopyInformation(this->GetInput());
@@ -404,7 +404,6 @@ CoocurrenceTextureFeaturesImageFilter<TInputImage, TOutputImage>::ComputeMeansAn
   {
     for (unsigned int b = 0; b < m_NumberOfBinsPerAxis; b++)
     {
-      int   k = hist[a][b];
       float frequency = hist[a][b] / (float)totalNumberOfFreq;
       pixelMean += a * frequency;
       marginalSums[a] += frequency;
