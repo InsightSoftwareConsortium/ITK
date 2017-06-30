13d2b790ba8d458e869539333d862fc0d0921017
Bradley Lowekamp <blowekamp@mail.nih.gov>
2017-06-30 10:56:00 -0400

    COMP: Use itk::Math::abs over vnl_math_abs
    This addressed a compilation error on the dashboard:
    itkSliceImageFilter.hxx:207:97: error: 'vnl_math_abs' was not declared
    in this scope
    
    Change-Id: Id4d940b3b435f4a9ee5b271eaf8a13c45bbc4290

--- Modules/Filtering/ImageGrid/include/itkSliceImageFilter.hxx
+++ Modules/Filtering/ImageGrid/include/itkSliceImageFilter.hxx
@@ -30,6 +30,7 @@
 
 #include "itkSliceImageFilter.h"
 #include "itkImageRegionIteratorWithIndex.h"
+#include "itkMath.h"
 #include "itkContinuousIndex.h"
 #include "itkObjectFactory.h"
 #include "itkProgressReporter.h"
@@ -204,7 +205,7 @@ SliceImageFilter< TInputImage, TOutputImage >
     {
     if ( outputRequestedRegionSize[i] > 0 )
       {
-      inputRequestedRegionSize[i] = (outputRequestedRegionSize[i] - 1 ) * vnl_math_abs(m_Step[i]) + 1;
+      inputRequestedRegionSize[i] = (outputRequestedRegionSize[i] - 1 ) * itk::Math::abs(m_Step[i]) + 1;
       }
     }
 
@@ -265,7 +266,7 @@ SliceImageFilter< TInputImage, TOutputImage >
 
   for ( unsigned int i = 0; i < TOutputImage::ImageDimension; ++i )
     {
-    outputSpacing[i] = inputSpacing[i] * vnl_math_abs(m_Step[i]);
+    outputSpacing[i] = inputSpacing[i] * itk::Math::abs(m_Step[i]);
 
     // clamp start
     // Based on the sign of the step include 1 after the end.
