599558cb8c1e6fe2d23a77aaab19592813f0a916
Jon Haitz Legarreta Gorroño <jhlegarreta@vicomtech.org>
2017-07-05 22:38:58 +0200

    ENH: Bump lastest version of remote repository.
    Bump latest commit hash of the ITKTBBImageToImageFilter remote repo.
    
    Change-Id: I5e776a246f04b53a0b00485fd214bd26427f1a8c

--- Modules/Remote/TBBImageToImageFilter.remote.cmake
+++ Modules/Remote/TBBImageToImageFilter.remote.cmake
@@ -4,5 +4,5 @@ itk_fetch_module(TBBImageToImageFilter
   This module contains a class inheriting from `itk::ImageToimageFilter` that aim to optimize thread management using the TBB library.
   Insight Journal article: http://www.insight-journal.org/browse/publication/974"
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKTBBImageToImageFilter.git
-  GIT_TAG a59908d51a7ee8c342b699b5f9b8acfdf7324f07
+  GIT_TAG 8f84d0dc9d09a5ff47460646fd99905c3c2b759e
   )
