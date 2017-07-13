bf6e0711d185bf53d4c17cc1ec5f80401ea875d2
Francois Budin <francois.budin@kitware.com>
2017-07-13 01:25:51 -0400

    ENH: Replace ICONV with ICU to compile DCMTK
    DMCTK now supports more character libraries [1]: ICONV, ICU, ICONV implemented in
    libc. If available, libc will be used. If this is not available, ICU can be
    compiled instead of ICONV. This allows to compiled DCMTK on Windows as part
    of ITK.
    
    [1] https://blog.jriesmeier.com/2017/03/18th-dcmtk-snapshot-361/
    Change-Id: I34b106030a413cbfd997e8cf5941baad2369bd4e

--- Modules/ThirdParty/DCMTK/itk-module-init.cmake
+++ Modules/ThirdParty/DCMTK/itk-module-init.cmake
@@ -1,9 +1,26 @@
 ## Only present and build DCMTK options if ITKIODCMTK is requested
 
 option(ITK_USE_SYSTEM_DCMTK "Use an outside build of DCMTK." OFF)
-# iconv library seems to be present unpredictably, never
-# on linux, sometimes on OS X and probably never on Windows
-option(DCMTK_USE_LIBICONV "Use IConv Library in DCMTK" OFF)
+
+if(NOT WIN32)
+  set(lib_prefix lib)
+  if(BUILD_SHARED_LIBS)
+    set(lib_suffix "${CMAKE_SHARED_LIBRARY_SUFFIX}")
+    set(lib_prefix "${CMAKE_SHARED_LIBRARY_PREFIX}")
+  else()
+    set(lib_suffix "${CMAKE_STATIC_LIBRARY_SUFFIX}")
+    set(lib_prefix "${CMAKE_STATIC_LIBRARY_PREFIX}")
+  endif()
+else()
+  set(lib_prefix "")
+  if(BUILD_SHARED_LIBS)
+    set(lib_suffix "${CMAKE_IMPORT_LIBRARY_SUFFIX}")
+    set(lib_prefix "${CMAKE_IMPORT_LIBRARY_PREFIX}")
+  else()
+    set(lib_suffix "${CMAKE_STATIC_LIBRARY_SUFFIX}")
+    set(lib_prefix "${CMAKE_IMPORT_LIBRARY_PREFIX}")
+  endif()
+endif()
 
 # Once ITK requires CMake >= 2.8.8 we can use
 #  add_library(... IMPORTED GLOBAL)
@@ -28,4 +45,39 @@ else(ITK_USE_SYSTEM_DCMTK)
     # add it as an imported  library target
     add_library(${lib} ${_ITKDCMTK_LIB_LINKAGE} IMPORTED)
   endforeach()
+  # Copied and mofified from DCMTK/CMake/3rdparty.cmake
+  if(NOT DEFINED DCMTK_USE_ICU)
+    include(CheckCXXSourceCompiles)
+    check_cxx_source_compiles("#include <iconv.h>\nint main(){iconv_t cd = iconv_open(\"\",\"\");iconv(cd,0,0,0,0);iconv_close(cd);return 0;}" WITH_STDLIBC_ICONV)
+    if(WITH_STDLIBC_ICONV)
+      message(STATUS "Info: found builtin ICONV support inside the C standard library.")
+      set(ICU_ARGS
+        -DDCMTK_WITH_STDLIBC_ICONV:BOOL=ON
+        -DDCMTK_WITH_ICU:BOOL=OFF
+        "-DDCMTK_ENABLE_CHARSET_CONVERSION:STRING=stdlibc (iconv)"
+        )
+      set(_DCMTK_USE_ICU_default OFF)
+    else()
+      set(_DCMTK_USE_ICU_default ON)
+    endif()
+    option(DCMTK_USE_ICU "Downloads and compile ICU for DCMTK" ${_DCMTK_USE_ICU_default})
+  endif()
+  if(DCMTK_USE_ICU)
+    if(ITK_USE_SYSTEM_ICU)
+      find_package(ICU COMPONENTS uc data)
+      set(ITKDCMTK_ICU_LIBRARIES ${ICU_LIBRARIES})
+    else()
+      set(ITKDCMTK_PREREQS ${ITKDCMTK_BINARY_DIR}/DCMTK_Prereqs)
+      set(ITKDCMTK_ICU_LIBRARIES )
+      foreach(lib_name uc data)
+        set(lib ICU::${lib_name})
+        list(APPEND ITKDCMTK_ICU_LIBRARIES ${lib})
+        add_library(${lib} STATIC IMPORTED)
+        set(lib_path ${ITKDCMTK_PREREQS}/lib/${lib_prefix}icu${lib_name}${lib_suffix})
+        set_property(TARGET ${lib} PROPERTY
+          IMPORTED_LOCATION ${lib_path})
+        list(APPEND ICU_BYPRODUCTS "${lib_path}")
+      endforeach()
+    endif()
+  endif()
 endif(ITK_USE_SYSTEM_DCMTK)
