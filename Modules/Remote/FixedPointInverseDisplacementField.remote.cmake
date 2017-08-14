6e3081b45c43d2257ee604173070ed34e2efc5af
Bradley Lowekamp <blowekamp@mail.nih.gov>
2017-08-14 14:59:16 -0400

    ENH: Update FixedPointInverseDisplacementField remote module
    Bradley Lowekamp (7):
          BUG: Always request the full input image
          ENH: replace run-time check of dimension with concept check
          ENH: Prefer raw pointers in pipeline methods
          ENH: Prefer FillBuffer over iterator loop
          ENH: Remove unneeded negative input
          COMP: Fix warning about variable index being shadowed
          Merge pull request #3 from blowekamp/FixShadowWarning
    
    Francois Budin (2):
          Merge pull request #1 from
          blowekamp/PipelineFixesAndImprovements
          Merge pull request #2 from blowekamp/Improvements
    
    Change-Id: Ica14efa46f5ee2ef7d38ab9b22999f69f98b3eb6

--- Modules/Remote/FixedPointInverseDisplacementField.remote.cmake
+++ Modules/Remote/FixedPointInverseDisplacementField.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(FixedPointInverseDisplacementField
   "Computes the inverse of a displacement field."
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKFixedPointInverseDisplacementField.git
-  GIT_TAG 23698cf58d439e62c872a3e7426c364702494ceb
+  GIT_TAG 57eec80969458bb1ff9c0da79548f7965e15851f
   )
