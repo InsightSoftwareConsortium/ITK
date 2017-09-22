0265829c2ee2bd7825bdc20eeef7b6fef3502a16
Matt McCormick <matt.mccormick@kitware.com>
2017-09-22 14:53:24 -0400

    COMP: Update MorphologicalContourInterpolation to 2017-09-22
     Matt McCormick (10):
           COMP: Expand slice set assignment
           COMP: Use std::abs
           ENH: Add wrapping configuration
           ENH: Add TravisCI configuration
           ENH: Add Appveyor configuration
           ENH: Update CircleCI config to include Python packaging
           COMP: Specify std:: prefix for pow
           COMP: Replace std::abs with itk::Math::abs
           COMP: sqrt disambiguation
           DOC: Add IJ article link, Python package installation instructions
    
    Change-Id: Id88c68970ae4d922e0005be8304c317a88e7a66f

--- Modules/Remote/MorphologicalContourInterpolation.remote.cmake
+++ Modules/Remote/MorphologicalContourInterpolation.remote.cmake
@@ -14,5 +14,5 @@ IEEE Trans Biomed Eng. 2008 Aug;55(8):2022-38. doi: 10.1109/TBME.2008.921158.
 This work is supported by NIH grant R01 EB014346
 'Continued development and maintenance of the ITK-SNAP 3D image segmentation software'."
   GIT_REPOSITORY ${git_protocol}://github.com/KitwareMedical/ITKMorphologicalContourInterpolation.git
-  GIT_TAG 19f256a3de5e7f93865b33eb37fa6b0f9fc9adb5
+  GIT_TAG 3a036898fea21ad1e9f463de28328fa763b62a22
   )
