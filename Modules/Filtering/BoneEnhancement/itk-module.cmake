b45e75a649dbd2c66627680b08f61db4cdfd69b5
Bryce Besler <babesler@ucalgary.ca>
2017-11-12 21:07:45 -0700

    DOC: Setup project from ITKModuleTemplate

---
+++ Modules/Filtering/BoneEnhancement/itk-module.cmake
@@ -0,0 +1,24 @@
+# the top-level README is used for describing this module, just
+# re-used it for documentation here
+# itk_module() defines the module dependencies in BoneEnhancement
+# BoneEnhancement depends on ITKCommon
+# The testing module in BoneEnhancement depends on ITKTestKernel
+# and ITKMetaIO(besides BoneEnhancement and ITKCore)
+# By convention those modules outside of ITK are not prefixed with
+# ITK.
+
+# define the dependencies of the include module and the tests
+itk_module(
+  BoneEnhancement
+  DEPENDS
+    ITKCommon
+    ITKStatistics
+  COMPILE_DEPENDS
+    ITKImageSources
+  TEST_DEPENDS
+    ITKTestKernel
+    ITKMetaIO
+  DESCRIPTION "Module ingested from upstream."
+  EXCLUDE_FROM_DEFAULT
+  ENABLE_SHARED
+)
