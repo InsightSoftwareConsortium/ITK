9807540677af1ded5d6126bc71e75d40f93da104
Matt McCormick <matt.mccormick@kitware.com>
2017-11-14 22:15:57 -0500

    COMP: Address build errors in itkPolarTransformTest

--- Modules/Filtering/PolarTransform/test/itkPolarTransformTest.cxx
+++ Modules/Filtering/PolarTransform/test/itkPolarTransformTest.cxx
@@ -1,7 +1,20 @@
-#if defined(_MSC_VER)
-#  pragma warning(disable : 4786)
-#endif
-
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
 #include <iostream>
 
 #include "itkPolarToCartesianTransform.h"
@@ -13,8 +26,6 @@ main(int, char *[])
 {
   const unsigned int Dimension = 6;
   const double       epsilon = 1e-10;
-
-  int i;
 
   /* Create 3D polar transforms and print them. */
   typedef itk::PolarToCartesianTransform<double, Dimension> P2CTransformType;
@@ -32,7 +43,7 @@ main(int, char *[])
   c[1] = sqrt(3.0);
   p[0] = vnl_math::pi / 3.0;
   p[1] = 2;
-  for (i = 2; i < Dimension; i++)
+  for (unsigned int i = 2; i < Dimension; ++i)
   {
     c[i] = 3;
     p[i] = c[i];
@@ -40,39 +51,47 @@ main(int, char *[])
 
   /* Transform point from polar to cartesian space. */
   tmp = p2c->TransformPoint(p);
-  for (i = 0; i < Dimension; i++)
-    if (abs(tmp[i] - c[i]) > epsilon)
+  for (unsigned int i = 0; i < Dimension; ++i)
+  {
+    if (std::abs(tmp[i] - c[i]) > epsilon)
     {
       std::cout << "Invalid polar to cartesian computed." << std::endl;
       return EXIT_FAILURE;
     }
+  }
 
   /* Transform point from cartesian to polar space. */
   tmp = c2p->TransformPoint(c);
-  for (i = 0; i < Dimension; i++)
-    if (abs(tmp[i] - p[i]) > epsilon)
+  for (unsigned int i = 0; i < Dimension; ++i)
+  {
+    if (std::abs(tmp[i] - p[i]) > epsilon)
     {
       std::cout << "Invalid cartesian to polar computed." << std::endl;
       return EXIT_FAILURE;
     }
+  }
 
   /* Transform point from polar to cartesian space and back. */
   tmp = c2p->TransformPoint(p2c->TransformPoint(p));
-  for (i = 0; i < Dimension; i++)
-    if (abs(tmp[i] - p[i]) > epsilon)
+  for (unsigned int i = 0; i < Dimension; ++i)
+  {
+    if (std::abs(tmp[i] - p[i]) > epsilon)
     {
       std::cout << "Invalid polar to cartesian and back computed." << std::endl;
       return EXIT_FAILURE;
     }
+  }
 
   /* Transform point from cartesian to polar space and back. */
   tmp = p2c->TransformPoint(c2p->TransformPoint(c));
-  for (i = 0; i < Dimension; i++)
-    if (abs(tmp[i] - c[i]) > epsilon)
+  for (unsigned int i = 0; i < Dimension; ++i)
+  {
+    if (std::abs(tmp[i] - c[i]) > epsilon)
     {
       std::cout << "Invalid cartesian to polar and back computed." << std::endl;
       return EXIT_FAILURE;
     }
+  }
 
   return EXIT_SUCCESS;
 }
