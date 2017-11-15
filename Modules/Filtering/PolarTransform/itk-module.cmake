e4659b3940bcfca47b9a0a63b5ea448af23f89db
Matt McCormick <matt.mccormick@kitware.com>
2017-11-14 21:59:37 -0500

    BUG: Trim module dependencies

--- Modules/Filtering/PolarTransform/itk-module.cmake
+++ Modules/Filtering/PolarTransform/itk-module.cmake
@@ -13,14 +13,10 @@ file(READ "${MY_CURRENT_DIR}/README.md" DOCUMENTATION)
 # define the dependencies of the include module and the tests
 itk_module(
   PolarTransform
-  DEPENDS
-    ITKCommon
-    ITKStatistics
   COMPILE_DEPENDS
-    ITKImageSources
+    ITKTransform
   TEST_DEPENDS
     ITKTestKernel
-    ITKMetaIO
   DESCRIPTION "${DOCUMENTATION}"
   EXCLUDE_FROM_DEFAULT
   ENABLE_SHARED
