408e20688b42da1ca190ed23035ab334ab8d5935
Dženan Zukić <dzenan.zukic@kitware.com>
2017-11-20 12:46:20 -0500

    BUG: tests writing to the same file randomly fail during parallel invocation
    When ctest is invoked with -jN parameter for running tests in parallel,
    some tests would occasionally fail. The cause was different tests writing
    into same (temporary) files, occasionally interfering with each other.
    
    A typical error message:
    Exception detected while reading /home/dzenan/Dashboards/Tests/ITK-build/ExternalData/Modules/Remote/MorphologicalContourInterpolation/test/Baseline/BigZ_T_1.nrrd :  Could not create IO object for reading file /home/dzenan/Dashboards/Tests/ITK-build/Testing/Temporary/BigZ_T_1.nrrd
      Tried to create one of the following:
        MetaImageIO
        GDCMImageIO
        JPEGImageIO
        VTKImageIO
        PNGImageIO
        TIFFImageIO
        BMPImageIO
        NrrdImageIO
        GiplImageIO
        NiftiImageIO
      You probably failed to set a file suffix, or
        set the suffix to an unsupported type.
    
    Change-Id: I0a9ad5dcd21e6b58c2ab645e680956df669a2709

--- Modules/Remote/MorphologicalContourInterpolation.remote.cmake
+++ Modules/Remote/MorphologicalContourInterpolation.remote.cmake
@@ -14,5 +14,5 @@ IEEE Trans Biomed Eng. 2008 Aug;55(8):2022-38. doi: 10.1109/TBME.2008.921158.
 This work is supported by NIH grant R01 EB014346
 'Continued development and maintenance of the ITK-SNAP 3D image segmentation software'."
   GIT_REPOSITORY ${git_protocol}://github.com/KitwareMedical/ITKMorphologicalContourInterpolation.git
-  GIT_TAG 6d21fa9c6bd8d1a0a2a2cbb290727b2f64444800
+  GIT_TAG 608853faf597c38132eaff8586f1f9c5c31f70aa
   )
