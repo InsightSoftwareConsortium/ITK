e532c682930ff431d66fb6da0aab22374a760686
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2017-12-01 04:21:03 -0500

    ENH: Explicit set the precision when printing double
    This will make ITK independent of any internal change in MetaIO.
    
    Change-Id: I3a9dc4a03386bf1158236305989d383c73c104f7

--- Modules/IO/Meta/src/itkMetaImageIO.cxx
+++ Modules/IO/Meta/src/itkMetaImageIO.cxx
@@ -796,6 +796,7 @@ MetaImageIO
 
   m_MetaImage.InitializeEssential( numberOfDimensions, dSize, eSpacing, eType, nChannels,
                                    const_cast< void * >( buffer ) );
+  m_MetaImage.SetDoublePrecision(6);
   m_MetaImage.Position(eOrigin);
   m_MetaImage.BinaryData(binaryData);
 
