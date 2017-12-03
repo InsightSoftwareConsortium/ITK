InsightSoftwareConsortium/ITK

commit c95a8aa2b86b6f2f55cdc037430e81d040111bc5

BUG: Fix hooks clone when ITK is a submodule
To address the error:

  ./SetupHooks.sh: Zeile 39: cd:
  /cygdrive/g/super/itk/Utilities/DevelopmentSetupScripts/…/…/.git/hooks: Not
  a directory

when ITK is as Git submodule.

Utilities/DevelopmentSetupScripts/SetupHooks.sh
@@ -36,7 +36,7 @@ die() {
 }
 
 u=$(cd "$(echo "$0"|sed 's/[^/]*$//')"; pwd)
-cd "$u/../../.git/hooks"
+cd "$(git rev-parse --git-dir)"
 
 # We need to have a git repository to do a pull.
 if ! test -d ./.git; then

commit af623e81c93b913525bec5a00cf0e0417a47760e

BUG: Fix hook clone
This commit fixes a regression introduced in c95a8aa (BUG: Fix hooks clone
when ITK is a submodule) ensuring hooks are fetched in "hooks" sub-directory.

It addresses this error:

Setting up git hooks...
Initialized empty Git repository in /path/to/ITKv4/.git/.git/
Pulling the hooks...
remote: Counting objects: 202, done.
remote: Compressing objects: 100% (81/81), done.
remote: Total 202 (delta 122), reused 195 (delta 117)
Receiving objects: 100% (202/202), 39.95 KiB | 0 bytes/s, done.
Resolving deltas: 100% (122/122), done.
From http://public.kitware.com/ITK
 * branch            hooks      -> FETCH_HEAD
fatal: ambiguous argument 'FETCH_HEAD': both revision and filename
Use '--' to separate paths from revisions, like this:
'git <command> [<revision>...] -- [<file>...]'
failure during hook setup
-------------------------

Change-Id: Ida2e7878c614bed3c5faa41efdef2c4d0220fbc1

Utilities/DevelopmentSetupScripts/SetupHooks.sh
@@ -35,8 +35,8 @@ die() {
   exit 1
 }
 
-u=$(cd "$(echo "$0"|sed 's/[^/]*$//')"; pwd)
-cd "$(git rev-parse --git-dir)"
+u=$(git rev-parse --git-dir)
+cd "$u/hooks"
 
 # We need to have a git repository to do a pull.
 if ! test -d ./.git; then