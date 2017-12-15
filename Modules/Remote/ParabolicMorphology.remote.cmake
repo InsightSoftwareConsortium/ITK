b8d35cd95dfca282918e73bb0206a819b3eda6f1
Hans Johnson <hans-johnson@uiowa.edu>
2017-12-15 07:31:43 -0600

    COMP: Remove override warnings
    Several overridden functions were not marked with
    ITK_OVERRIDE.
    
    Change-Id: I7a51c42191522a2ef17d11154cbbefa72a1a18ca

--- Modules/Remote/ParabolicMorphology.remote.cmake
+++ Modules/Remote/ParabolicMorphology.remote.cmake
@@ -5,5 +5,5 @@ itk_fetch_module(ParabolicMorphology
   http://www.insight-journal.org/browse/publication/228
   https://hdl.handle.net/1926/1370"
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/parabolicMorphology.git
-  GIT_TAG 8615dfe9781b6b78ea4d172123f8b94416258b67
+  GIT_TAG 3c5fd312a642f7d26663e58d0c7132d50a14eff4
   )
