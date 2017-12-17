8478c95682dc2e144887819134b176efae32d5ad
Hans Johnson <hans-johnson@uiowa.edu>
2017-12-17 08:22:47 -0600

    COMP: Use nullptr instead of 0 or NULL
    The check converts the usage of null pointer constants (eg. NULL, 0) to
    use the new C++11 nullptr keyword.
    
    SRCDIR= #My local SRC
    BLDDIR= #My local BLD
    
    clang-tidy -p ${BLD}  $(find Modules/[A-J]*  -name "*.cxx" \|fgrep -v ThirdParty) -checks=-*,modernize-use-nullptr  -header-filter=.* -fix
    clang-tidy -p ${BLD}  $(find Modules/[K-Z]*  -name "*.cxx" \|fgrep -v ThirdParty) -checks=-*,modernize-use-nullptr  -header-filter=.* -fix

--- Modules/Filtering/GenericLabelInterpolator/include/itkLabelImageGenericInterpolateImageFunction.h
+++ Modules/Filtering/GenericLabelInterpolator/include/itkLabelImageGenericInterpolateImageFunction.h
@@ -83,7 +83,7 @@ public:
   OutputType
   EvaluateAtContinuousIndex(const ContinuousIndexType & cindex) const override
   {
-    return this->EvaluateAtContinuousIndex(cindex, NULL);
+    return this->EvaluateAtContinuousIndex(cindex, nullptr);
   }
 
   void
