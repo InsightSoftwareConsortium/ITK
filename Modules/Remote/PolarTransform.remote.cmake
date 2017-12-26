3efef54aa09e99450a1ec1e5de39f9303dbffabc
Matt McCormick <matt.mccormick@kitware.com>
2017-12-26 17:09:55 -0500

    ENH: Add PolarTransform remote module
    This is a module to transform from cartesian to polar coordinate systems and back.
    
    For more information, see the Insight Journal article:
    
      Bican J.
      Polar Transforms for ITK
      The Insight Journal. July-December. 2006.
      http://hdl.handle.net/1926/342
      http://insight-journal.org/browse/publication/130
    
    Change-Id: I4e259cddbd1d9257deeda844066f8f133299e461

---
+++ Modules/Remote/PolarTransform.remote.cmake
@@ -0,0 +1,15 @@
+# Contact: Matt McCormick <matt.mccormick@kitware.com>
+itk_fetch_module(PolarTransform
+"This is a module to transform from cartesian to polar coordinate systems and back.
+
+For more information, see the Insight Journal article:
+
+  Bican J.
+  Polar Transforms for ITK
+  The Insight Journal. July-December. 2006.
+  http://hdl.handle.net/1926/342
+  http://insight-journal.org/browse/publication/130
+  "
+  GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKPolarTransform.git
+  GIT_TAG f531f3d3f8bcf9c5aefe0a961c901caebcc86348
+  )
