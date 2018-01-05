b71e88a2332e84420233b3e61258a09faea8b6cc
Hans Johnson <hans-johnson@uiowa.edu>
2018-01-05 10:45:55 -0600

    COMP: Support OSX Python default SDK builds
    OSX SDK 10.6 is still the standard for python
    integration.
    
    Change-Id: I1bb8cdd9ccdb527a5148bdc6d55c5b18a223aaa2

--- CMake/itkApple.cmake
+++ CMake/itkApple.cmake
@@ -12,7 +12,8 @@ mark_as_advanced(
   CMAKE_OSX_DEPLOYMENT_TARGET
   CMAKE_OSX_SYSROOT)
 
+# OSX SDK 10.6 is the standard SDK for Python
 if (CMAKE_OSX_DEPLOYMENT_TARGET AND
-    CMAKE_OSX_DEPLOYMENT_TARGET VERSION_LESS "10.7")
-  message(FATAL_ERROR "Minimum OS X deployment target is 10.7, please update CMAKE_OSX_DEPLOYMENT_TARGET.")
+    CMAKE_OSX_DEPLOYMENT_TARGET VERSION_LESS "10.6")
+  message(FATAL_ERROR "Minimum OS X deployment target is 10.6, please update CMAKE_OSX_DEPLOYMENT_TARGET.")
 endif ()
