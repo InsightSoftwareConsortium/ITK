67f44f902487806a2a81ad2c3b0da287c0ae4dae
Dženan Zukić <dzenan.zukic@kitware.com>
2018-01-19 15:14:18 -0500

    STYLE: avoid redirect for repository address in DVMeshNoise
    Change-Id: Icfa4d22a6675e71ae91962643668e3911c3307f0

--- Modules/Remote/DVMeshNoise.remote.cmake
+++ Modules/Remote/DVMeshNoise.remote.cmake
@@ -5,5 +5,5 @@ itk_fetch_module(DVMeshNoise
   Vigneault, DM.  Perturbing Mesh Vertices with Additive Gaussian Noise.
   http://hdl.handle.net/10380/3567
   "
-  GIT_REPOSITORY ${git_protocol}://github.com/DVigneault/DVMeshNoise
+  GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/DVMeshNoise
   GIT_TAG e7a360ec0aad87035d0b231741f63833c0ffe6f7)
