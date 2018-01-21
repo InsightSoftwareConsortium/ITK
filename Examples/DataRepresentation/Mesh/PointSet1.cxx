52ede01d98310c102b678d3b1c5a727d726d1225
Jon Haitz Legarreta Gorroño <jhlegarreta@vicomtech.org>
2018-01-21 16:04:12 +0100

    BUG: Fix unnecessary explicit itk namespace mention in Doxygen link.
    Remove unnecessary/redundant `itk` namespace mention in Doxygen link in
    the LaTeX documentation used by the ITK SoftwareGuide.
    
    Change-Id: Ic23a853748e02c6d85c3fc47228f09ee14fd78ea

--- Examples/DataRepresentation/Mesh/PointSet1.cxx
+++ Examples/DataRepresentation/Mesh/PointSet1.cxx
@@ -18,9 +18,9 @@
 
 //  Software Guide : BeginLatex
 //
-//  The \doxygen{itk::PointSet} is a basic class intended to represent geometry
+//  The \doxygen{PointSet} is a basic class intended to represent geometry
 //  in the form of a set of points in $N$-dimensional space. It is the base
-//  class for the \doxygen{itk::Mesh} providing the methods necessary to
+//  class for the \doxygen{Mesh} providing the methods necessary to
 //  manipulate sets of points. Points can have values associated with
 //  them. The type of such values is defined by a template parameter of the
 //  \code{itk::PointSet} class (i.e., \code{TPixelType}). Two basic
