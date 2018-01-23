8aef40cecabdb5d5bb5f2428e2ed864d0ff75c8b
Bradley Lowekamp <blowekamp@mail.nih.gov>
2018-01-23 09:34:34 -0500

    COMP: Explicitly include the fixed width integer header before usage
    Change-Id: I34a1ea9fc5db66495bcdf0a70f940e2515fa9af9

--- Modules/Core/Common/include/itkIntTypes.h
+++ Modules/Core/Common/include/itkIntTypes.h
@@ -20,12 +20,11 @@
 
 #include "itkMacro.h"
 
+#include <cstdint>
 #include <climits>
 
 namespace itk
 {
-// Note: these types are technically optional in C99 stdint.h file. As
-// such a try complile for their existence may be needed.
 typedef::int8_t   int8_t;
 typedef::uint8_t  uint8_t;
 typedef::int16_t  int16_t;
@@ -35,7 +34,6 @@ typedef::uint32_t uint32_t;
 typedef::int64_t  int64_t;
 typedef::uint64_t uint64_t;
 
-// Note: these types are required for the C99 stdint.h file.
 typedef::int_least8_t   int_least8_t;
 typedef::uint_least8_t  uint_least8_t;
 typedef::int_least16_t  int_least16_t;
@@ -45,7 +43,6 @@ typedef::uint_least32_t uint_least32_t;
 typedef::int_least64_t  int_least64_t;
 typedef::uint_least64_t uint_least64_t;
 
-// Note: these types are required for the C99 stdint.h file.
 typedef::int_fast8_t   int_fast8_t;
 typedef::uint_fast8_t  uint_fast8_t;
 typedef::int_fast16_t  int_fast16_t;
