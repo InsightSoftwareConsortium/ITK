cf91a98cd3a703513e4a665e6b02871a7b093a6a
Hans Johnson <hans-johnson@uiowa.edu>
2018-02-13 08:25:02 -0600

    STYLE: Prefer constexpr for const numeric literals
    Use constexpr for constant numeric literals.

--- Modules/Filtering/ParabolicMorphology/test/itkParaDilateTest.cxx
+++ Modules/Filtering/ParabolicMorphology/test/itkParaDilateTest.cxx
@@ -32,7 +32,7 @@ int
 itkParaDilateTest(int argc, char * argv[])
 {
   itk::MultiThreader::SetGlobalMaximumNumberOfThreads(1);
-  const int dim = 2;
+  constexpr int dim = 2;
 
   using PType = unsigned char;
   using IType = itk::Image<PType, dim>;
