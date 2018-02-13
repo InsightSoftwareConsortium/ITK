InsightSoftwareConsortium/ITKVariationalRegistration

STYLE: Replace itkStaticConstMacro with static constexpr
Use static constexpr directly now that C++11 conformance
is required by all compilers.

:%s/itkStaticConstMacro *( *\([^,]*\),[ \_s]*\([^,]*\),\_s*\([^)]*\)) */static constexpr \2 \1 = \3/ge

include/itkContinuousBorderWarpImageFilter.h
@@ -91,12 +91,9 @@ public:
   typedef typename OutputImageType::SpacingType       SpacingType;
 
   /** Determine the image dimension. */
-  itkStaticConstMacro(ImageDimension, unsigned int,
-                      TOutputImage::ImageDimension );
-  itkStaticConstMacro(InputImageDimension, unsigned int,
-                      TInputImage::ImageDimension );
-  itkStaticConstMacro(DeformationFieldDimension, unsigned int,
-      TDisplacementField::ImageDimension );
+  static constexpr unsigned int ImageDimension = TOutputImage::ImageDimension ;
+  static constexpr unsigned int InputImageDimension = TInputImage::ImageDimension ;
+  static constexpr unsigned int DeformationFieldDimension = TDisplacementField::ImageDimension ;
 
   /** Displacement field typedef support. */
   typedef TDisplacementField                        DisplacementFieldType;

InsightSoftwareConsortium/ITK

Modules/Registration/VariationalRegistration/include/itkContinuousBorderWarpImageFilter.h
@@ -86,9 +86,9 @@ public:
   typedef typename OutputImageType::SpacingType       SpacingType;
 
   /** Determine the image dimension. */
-  itkStaticConstMacro(ImageDimension, unsigned int, TOutputImage::ImageDimension);
-  itkStaticConstMacro(InputImageDimension, unsigned int, TInputImage::ImageDimension);
-  itkStaticConstMacro(DeformationFieldDimension, unsigned int, TDisplacementField::ImageDimension);
+  static constexpr unsigned int ImageDimension = TOutputImage::ImageDimension;
+  static constexpr unsigned int InputImageDimension = TInputImage::ImageDimension;
+  static constexpr unsigned int DeformationFieldDimension = TDisplacementField::ImageDimension;
 
   /** Displacement field typedef support. */
   typedef TDisplacementField                        DisplacementFieldType;