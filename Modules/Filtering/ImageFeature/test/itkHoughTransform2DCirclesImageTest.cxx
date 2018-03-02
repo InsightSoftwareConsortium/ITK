InsightSoftwareConsortium/ITK

commit 1722d1ea69fe92c0cb66f56802fd3cd0d9c12206

ENH: Added GetCenterPoint and SetCenterPoint to EllipseSpatialObject
Wraps access to ellipseSpatialObject->GetObjectToWorldTransform()->m_Offset.

These new member functions allow simplifying the implementation, as well as
use cases of itk::HoughTransform2DCirclesImageFilter::GetCircles(). With this
commit, the implementation, test, and example of the HoughTransform2DCircles
filter are adapted to make use of them.

Note that this change silently breaks old ITK user code (ITK < 5.0) calling
HoughTransform2DCirclesImageFilter::GetCircles().

Triggered by comments from Tim Evain at ITK Discourse, starting at
https://discourse.itk.org/t/hough-transform-2d-circles-image-filter-getcircles-patch/350/46

Change-Id: I49a9f0504c0fc85aeefddd813fcabca309bd09e8

Modules/Core/SpatialObjects/include/itkEllipseSpatialObject.hxx
@@ -51,6 +51,27 @@ EllipseSpatialObject< TDimension >
     }
 }
 
+template< unsigned int TDimension >
+auto
+EllipseSpatialObject<TDimension>::GetCenterPoint() const
+-> PointType
+{
+  PointType originPoint;
+  originPoint.Fill(0);
+  // GetObjectToWorldTransform() never returns nullptr, no need to check.
+  return originPoint + this->GetObjectToWorldTransform()->GetOffset();
+}
+
+template< unsigned int TDimension >
+void
+EllipseSpatialObject<TDimension>::SetCenterPoint(const PointType& point)
+{
+  PointType originPoint;
+  originPoint.Fill(0);
+  // GetModifiableObjectToWorldTransform() never returns nullptr, no need to check.
+  this->GetModifiableObjectToWorldTransform()->SetOffset(point - originPoint);
+}
+
 /** Test whether a point is inside or outside the object
  *  For computational speed purposes, it is faster if the method does not
  *  check the name of the class and the current depth */

Modules/Filtering/ImageFeature/test/itkHoughTransform2DCirclesImageTest.cxx
@@ -198,21 +198,14 @@ namespace
       return false;
     }
 
-    const CircleType::TransformType* const transform1 = circle1->GetObjectToParentTransform();
-    const CircleType::TransformType* const transform2 = circle2->GetObjectToParentTransform();
-
-    if ( (transform1 == nullptr) || (transform2 == nullptr) )
-    {
-      std::cout << "A GetObjectToParentTransform() call appears to be incorrect!" << std::endl;
-      return false;
-    }
-
     bool success = true;
 
-    const itk::Vector<double, 2>& center1 = transform1->GetOffset();
-    const itk::Vector<double, 2>& center2 = transform2->GetOffset();
+    using PointType = CircleType::PointType;
 
-    if (center1 != center2)
+    const PointType& centerPoint1 = circle1->GetCenterPoint();
+    const PointType& centerPoint2 = circle2->GetCenterPoint();
+
+    if (centerPoint1 != centerPoint2)
     {
       // The choice of the radius image type should not affect the center estimation.
       std::cout << "center1 and center2 should be equal, even while the radius image types differ!"

Examples/Segmentation/HoughTransform2DCirclesImageFilter.cxx
@@ -215,7 +215,7 @@ int main( int argc, char *argv[] )
   while( itCircles != circles.end() )
     {
     std::cout << "Center: ";
-    std::cout << (*itCircles)->GetObjectToParentTransform()->GetOffset()
+    std::cout << (*itCircles)->GetCenterPoint()
               << std::endl;
     std::cout << "Radius: " << (*itCircles)->GetRadius()[0] << std::endl;
     // Software Guide : EndCodeSnippet
@@ -231,15 +231,14 @@ int main( int argc, char *argv[] )
          angle <= itk::Math::twopi;
          angle += itk::Math::pi/60.0 )
       {
-      using TransformType = HoughTransformFilterType::CircleType::TransformType;
-      using OffsetType = TransformType::OutputVectorType;
-      const OffsetType offset =
-        (*itCircles)->GetObjectToParentTransform()->GetOffset();
+      const HoughTransformFilterType::CircleType::PointType centerPoint =
+        (*itCircles)->GetCenterPoint();
+      using IndexValueType = ImageType::IndexType::IndexValueType;
       localIndex[0] =
-         itk::Math::Round<long int>(offset[0]
+         itk::Math::Round<IndexValueType>(centerPoint[0]
                     + (*itCircles)->GetRadius()[0]*std::cos(angle));
       localIndex[1] =
-         itk::Math::Round<long int>(offset[1]
+         itk::Math::Round<IndexValueType>(centerPoint[1]
                     + (*itCircles)->GetRadius()[0]*std::sin(angle));
       OutputImageType::RegionType outputRegion =
                                   localOutputImage->GetLargestPossibleRegion();

Modules/Core/SpatialObjects/include/itkEllipseSpatialObject.h
@@ -68,6 +68,12 @@ public:
   /** Get radii via an array of radius values */
   itkGetConstReferenceMacro(Radius, ArrayType);
 
+  /* Returns the center point (in world coordinates) of the ellipse */
+  PointType GetCenterPoint()  const;
+
+  /* Set the center point (in world coordinates) of the ellipse */
+  void SetCenterPoint(const PointType& point);
+
   /** Returns a degree of membership to the object.
    *  That's useful for fuzzy objects. */
   bool ValueAt(const PointType & point, double & value,
@@ -109,6 +115,7 @@ protected:
   /** Print the object informations in a stream. */
   void PrintSelf(std::ostream & os, Indent indent) const override;
 };
+
 } // end namespace itk
 
 #ifndef ITK_MANUAL_INSTANTIATION

Modules/Filtering/ImageFeature/include/itkHoughTransform2DCirclesImageFilter.hxx
@@ -248,10 +248,10 @@ HoughTransform2DCirclesImageFilter< TInputPixelType, TOutputPixelType, TRadiusPi
       Circle->SetId(static_cast<int>( circles ));
       Circle->SetRadius( m_RadiusImage->GetPixel( indexOfMaximum ) );
 
-      CircleType::VectorType center;
+      CircleType::PointType center;
       center[0] = indexOfMaximum[0];
       center[1] = indexOfMaximum[1];
-      Circle->GetObjectToParentTransform()->SetOffset(center);
+      Circle->SetCenterPoint(center);
       Circle->ComputeBoundingBox();
 
       m_CirclesList.push_back(Circle);

commit 210ba16c02641dfc14d7b1381aa19545fbdb0338

ENH: IsInside(point) made easier, especially for HoughTransform circles
Added ComputeObjectToParentTransform() call to EllipseSpatialObject::
SetCenterPoint(point). This will allow users to directly call
circle->IsInside(point) for a circle detected by HoughTransform, without
having to worry about calling ComputeObjectToParentTransform() or
ComputeObjectToWorldTransform() themselves. Added a test.

This commit indirectly also restores a backward compatibility issue introduced
by commit 1722d1ea69fe92c0cb66f56802fd3cd0d9c12206, which replaced
GetObjectToParentTransform()->m_Offset by GetObjectToWorldTransform()->m_Offset
as storage location for the center coordinates. This new commit ensures that
the center coordinates will be placed at both storage locations.

Change-Id: Iff7fc5ba60f3a9bd0ad4165c047930d283fc7c48

Modules/Core/SpatialObjects/include/itkEllipseSpatialObject.hxx
@@ -70,6 +70,7 @@ EllipseSpatialObject<TDimension>::SetCenterPoint(const PointType& point)
   originPoint.Fill(0);
   // GetModifiableObjectToWorldTransform() never returns nullptr, no need to check.
   this->GetModifiableObjectToWorldTransform()->SetOffset(point - originPoint);
+  this->ComputeObjectToParentTransform();
 }
 
 /** Test whether a point is inside or outside the object

Modules/Filtering/ImageFeature/test/itkHoughTransform2DCirclesImageTest.cxx
@@ -28,8 +28,8 @@
 static const unsigned int Dimension = 2;
 
 
-template< typename ImageType >
-void CreateCircle( typename ImageType::Pointer image, const unsigned int center[Dimension], double radius )
+template< typename ImageType, typename CenterCoordinateType >
+void CreateCircle( typename ImageType::Pointer image, const CenterCoordinateType center[Dimension], double radius )
 {
   typename ImageType::IndexType index = image->GetLargestPossibleRegion().GetIndex();
 
@@ -240,6 +240,53 @@ namespace
     }
 
     return success;
+  }
+
+
+  // Tests that the center of a circle that was created on the input image
+  // is inside the spatial object produced by GetCircles().
+  bool Test_Center_IsInside_SpatialObject_from_GetCircles()
+  {
+    using PixelType = unsigned;
+    using ImageType = itk::Image<PixelType>;
+    const auto image = ImageType::New();
+    const ImageType::SizeType imageSize = { { 16, 32 } };
+    image->SetRegions(imageSize);
+    image->Allocate(true);
+    const double center[] = { 6.0, 9.0 };
+    const double radius = 1.0;
+    CreateCircle<ImageType>(image, center, radius);
+
+    using FilterType = itk::HoughTransform2DCirclesImageFilter< PixelType, unsigned, double >;
+    const auto filter = FilterType::New();
+    filter->SetInput(image);
+    filter->Update();
+
+    const FilterType::CirclesListType& circles = filter->GetCircles();
+
+    if (circles.size() != 1)
+    {
+      std::cout << "ERROR: GetCircles() should have found exactly one circle!" << std::endl;
+      return false;
+    }
+
+    const FilterType::CirclePointer& circle = circles.front();
+
+    if (circle == nullptr)
+    {
+      std::cout << "ERROR: The circle found by GetCircles() should not be null!" << std::endl;
+      return false;
+    }
+
+    const bool isInside = circle->IsInside(center);
+
+    if (!isInside)
+    {
+      std::cout <<
+        "ERROR: The center of the actual circle should be inside the spacial object of the detected circle!"
+        << std::endl;
+    }
+    return isInside;
   }
 
 }
@@ -500,6 +547,7 @@ int itkHoughTransform2DCirclesImageTest( int, char* [] )
   success &= Test_GetCircles_should_return_empty_list_when_NumberOfCircles_is_set_to_zero();
   success &= Test_GetCircles_should_return_empty_list_when_input_image_is_uniform();
   success &= Test_RadiusImage_and_OutputImage_may_have_different_types();
+  success &= Test_Center_IsInside_SpatialObject_from_GetCircles();
 
   if (success)
   {