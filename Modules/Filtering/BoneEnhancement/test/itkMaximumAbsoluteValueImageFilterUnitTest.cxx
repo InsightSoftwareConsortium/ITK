f2357432fc7f4dafbfcd8c4c838a96291d93f535
Bryce Besler <babesler@ucalgary.ca>
2018-03-03 17:07:28 -0700

    ENH: Move itkMaximumAbsoluteValueImageFilterTest to GTest

--- Modules/Filtering/BoneEnhancement/test/itkMaximumAbsoluteValueImageFilterUnitTest.cxx
+++ Modules/Filtering/BoneEnhancement/test/itkMaximumAbsoluteValueImageFilterUnitTest.cxx
@@ -17,24 +17,20 @@
  *=========================================================================*/
 
 #include "itkMaximumAbsoluteValueImageFilter.h"
-#include "itkTestingMacros.h"
+#include "gtest/gtest.h"
 #include "itkImageRegionIterator.h"
 
-int
-itkMaximumAbsoluteValueImageFilterTest(int, char *[])
+TEST(itkMaximumAbsoluteValueImageFilterUnitTest, TakesAbsMaxOfSimpleImages)
 {
-  constexpr unsigned int Dimension = 2;
-  using PixelType = int;
-  using ImageType = itk::Image<PixelType, Dimension>;
-  using MaximumAbsoluteValueImageFilterType = itk::MaximumAbsoluteValueImageFilter<ImageType>;
-  MaximumAbsoluteValueImageFilterType::Pointer maxAbsFilter = MaximumAbsoluteValueImageFilterType::New();
-
-  EXERCISE_BASIC_OBJECT_METHODS(maxAbsFilter, MaximumAbsoluteValueImageFilter, BinaryFunctorImageFilter);
+  const unsigned int                                      Dimension = 2;
+  typedef int                                             PixelType;
+  typedef itk::Image<PixelType, Dimension>                ImageType;
+  typedef itk::MaximumAbsoluteValueImageFilter<ImageType> MaximumAbsoluteValueImageFilterType;
+  MaximumAbsoluteValueImageFilterType::Pointer            maxAbsFilter = MaximumAbsoluteValueImageFilterType::New();
 
   /** Create an image and run a basic test */
   ImageType::RegionType region;
   ImageType::IndexType  start;
-
   start[0] = 0;
   start[1] = 0;
 
@@ -53,7 +49,12 @@ itkMaximumAbsoluteValueImageFilterTest(int, char *[])
   image2->SetRegions(region);
   image2->Allocate();
 
+<<<<<<< HEAD:test/itkMaximumAbsoluteValueImageFilterTest.cxx
   using IteratorType = itk::ImageRegionIterator<ImageType>;
+=======
+  /* Iterate over images and set */
+  typedef itk::ImageRegionIterator<ImageType> IteratorType;
+>>>>>>> ENH: Move itkMaximumAbsoluteValueImageFilterTest to GTest:test/itkMaximumAbsoluteValueImageFilterUnitTest.cxx
   IteratorType it1(image1, region);
   IteratorType it2(image2, region);
   it1.GoToBegin();
@@ -75,9 +76,10 @@ itkMaximumAbsoluteValueImageFilterTest(int, char *[])
     ++it2;
   }
 
+  /* Apply filter */
   maxAbsFilter->SetInput1(image1);
   maxAbsFilter->SetInput2(image2);
-  maxAbsFilter->Update();
+  EXPECT_NO_THROW(maxAbsFilter->Update());
   ImageType::Pointer outputImage = maxAbsFilter->GetOutput();
 
   IteratorType ot(outputImage, region);
@@ -87,16 +89,12 @@ itkMaximumAbsoluteValueImageFilterTest(int, char *[])
   {
     if ((i % 2) == 0)
     {
-      TEST_EXPECT_EQUAL(ot.Get(), -2);
+      EXPECT_EQ(ot.Get(), -2);
     }
     else
     {
-      TEST_EXPECT_EQUAL(ot.Get(), 2);
+      EXPECT_EQ(ot.Get(), 2);
     }
     ++ot;
   }
-
-  /** TODO: Write an integration test */
-
-  return EXIT_SUCCESS;
 }
