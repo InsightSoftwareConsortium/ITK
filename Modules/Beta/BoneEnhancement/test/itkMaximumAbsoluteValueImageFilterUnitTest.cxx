31943e2e22829513405e4ee66b300cfffeb4f5ce
Bryce Besler <babesler@ucalgary.ca>
2018-03-03 17:07:28 -0700

    ENH: Move itkMaximumAbsoluteValueImageFilterTest to GTest

--- Modules/Beta/BoneEnhancement/test/itkMaximumAbsoluteValueImageFilterUnitTest.cxx
+++ Modules/Beta/BoneEnhancement/test/itkMaximumAbsoluteValueImageFilterUnitTest.cxx
@@ -17,23 +17,19 @@
  *=========================================================================*/
 
 #include "itkMaximumAbsoluteValueImageFilter.h"
-#include "itkTestingMacros.h"
+#include "gtest/gtest.h"
 #include "itkImageRegionIterator.h"
 
-int itkMaximumAbsoluteValueImageFilterTest( int, char * [] )
-{
-  const unsigned int                         Dimension = 2;
-  typedef int                                PixelType;
-  typedef itk::Image< PixelType, Dimension > ImageType;
+TEST(itkMaximumAbsoluteValueImageFilterUnitTest, TakesAbsMaxOfSimpleImages) {
+  const unsigned int                                      Dimension = 2;
+  typedef int                                             PixelType;
+  typedef itk::Image< PixelType, Dimension >              ImageType;
   typedef itk::MaximumAbsoluteValueImageFilter<ImageType> MaximumAbsoluteValueImageFilterType;
   MaximumAbsoluteValueImageFilterType::Pointer maxAbsFilter = MaximumAbsoluteValueImageFilterType::New();
-  
-  EXERCISE_BASIC_OBJECT_METHODS( maxAbsFilter, MaximumAbsoluteValueImageFilter, BinaryFunctorImageFilter );
 
   /** Create an image and run a basic test */
   ImageType::RegionType region;
   ImageType::IndexType start;
-
   start[0] = 0;
   start[1] = 0;
  
@@ -52,6 +48,7 @@ int itkMaximumAbsoluteValueImageFilterTest( int, char * [] )
   image2->SetRegions(region);
   image2->Allocate();
 
+  /* Iterate over images and set */
   typedef itk::ImageRegionIterator< ImageType > IteratorType;
   IteratorType  it1( image1, region);
   IteratorType  it2( image2, region);
@@ -71,9 +68,10 @@ int itkMaximumAbsoluteValueImageFilterTest( int, char * [] )
     ++it2;
   }
 
+  /* Apply filter */
   maxAbsFilter->SetInput1(image1);
   maxAbsFilter->SetInput2(image2);
-  maxAbsFilter->Update();
+  EXPECT_NO_THROW(maxAbsFilter->Update());
   ImageType::Pointer outputImage = maxAbsFilter->GetOutput();
 
   IteratorType  ot( outputImage, region);
@@ -82,14 +80,10 @@ int itkMaximumAbsoluteValueImageFilterTest( int, char * [] )
   while( !ot.IsAtEnd() )
   {
     if ((i % 2) == 0) {
-      TEST_EXPECT_EQUAL(ot.Get(), -2);
+      EXPECT_EQ(ot.Get(), -2);
     } else {
-      TEST_EXPECT_EQUAL(ot.Get(), 2);
+      EXPECT_EQ(ot.Get(), 2);
     }
     ++ot;
   }
-
-  /** TODO: Write an integration test */
-
-  return EXIT_SUCCESS;
 }
