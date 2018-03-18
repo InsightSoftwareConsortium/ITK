7d33664a2d6477c4125395f02d7fa9858486df10
Matt McCormick <matt.mccormick@kitware.com>
2018-03-18 15:37:11 -0400

    COMP: Address change in GetMultiThreader return type
    To address:
    
      /home/matt/src/ITKParabolicMorphology/include/itkParabolicErodeDilateImageFilter.hxx:184:18: error: cannot initialize a variable of type 'itk::MultiThreader *' with an rvalue of type 'MultiThreaderType *' (aka 'itk::MultiThreaderBase *')
      MultiThreader *multithreader = this->GetMultiThreader();
                     ^               ~~~~~~~~~~~~~~~~~~~~~~~~
    /home/matt/src/ITKParabolicMorphology/include/itkParabolicErodeImageFilter.h:88:3: note: in instantiation of member function 'itk::ParabolicErodeDilateImageFilter<itk::Image<float, 2>, false, itk::Image<float, 2> >::GenerateData' requested here
      ParabolicErodeImageFilter(){}
    
    ->GetMultiThreader() returns itk::MultiThreaderBase * type instead of itk::MultiThreader.

--- Modules/Filtering/ParabolicMorphology/include/itkParabolicOpenCloseImageFilter.hxx
+++ Modules/Filtering/ParabolicMorphology/include/itkParabolicOpenCloseImageFilter.hxx
@@ -194,7 +194,7 @@ ParabolicOpenCloseImageFilter<TInputImage, doOpen, TOutputImage>::GenerateData(v
   typename ImageSource<OutputImageType>::ThreadStruct str;
   str.Filter = this;
 
-  MultiThreader * multithreader = this->GetMultiThreader();
+  ProcessObject::MultiThreaderType * multithreader = this->GetMultiThreader();
   multithreader->SetNumberOfThreads(nbthreads);
   multithreader->SetSingleMethod(this->ThreaderCallback, &str);
 
