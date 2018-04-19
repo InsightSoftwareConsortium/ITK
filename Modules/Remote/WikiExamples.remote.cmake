cae32c995cfff73d2eccbb59dee269bea2814f92
Jon Haitz Legarreta <jhlegarreta@vicomtech.org>
2018-04-19 10:15:26 +0200

    BUG: Address use of CMake variables in CMakeLists.txt.
    Use the variables referenced for that purpose in the `if/else` block in
    order to pick the appropriate commit hash from git.
    
    Change-Id: Ieac69918eb51bc90bc4591c0e709d441d80ab53f

--- Modules/Remote/WikiExamples.remote.cmake
+++ Modules/Remote/WikiExamples.remote.cmake
@@ -4,15 +4,13 @@
 
 # If the environment var WikiExamplesTag exists, use it
 if (NOT DEFINED ENV{WikiExamplesTag})
-  set(GIT_TAG ee85e99ab3cdd0dd4bedbdd7893c48a09cab0244
-     )
+  set(GIT_TAG ee85e99ab3cdd0dd4bedbdd7893c48a09cab0244)
 else()
-  set(GIT_TAG ee85e99ab3cdd0dd4bedbdd7893c48a09cab0244
-     )
+  set(GIT_TAG $ENV{WikiExamplesTag})
 endif()
 
 itk_fetch_module(WikiExamples
   "A collection of examples that illustrate how to use ITK."
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKWikiExamples.git
-  GIT_TAG ee85e99ab3cdd0dd4bedbdd7893c48a09cab0244
+  GIT_TAG ${GIT_TAG}
   )
