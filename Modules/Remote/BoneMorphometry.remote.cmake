1e161bb9ec4c018408ec0524319d3325b6048c4a
Jon Haitz Legarreta Gorroño <jhlegarreta@vicomtech.org>
2018-05-13 20:52:30 +0200

    ENH: Add the ITK BoneMorphometry module as a remote.
    Add the ITK BoneMorphometry module as a remote:
    https://github.com/InsightSoftwareConsortium/ITKBoneMorphometry
    
    Change-Id: If530dc9b899d1a2901728d3fc3db6cb8a238687f

---
+++ Modules/Remote/BoneMorphometry.remote.cmake
@@ -0,0 +1,11 @@
+itk_fetch_module(BoneMorphometry
+  "An Insight Toolkit (ITK) based implementation for bone morphometry quantification from computed tomography (CT) images.
+  A more detailed description can be found in the Insight Journal (IJ) article:
+  Vimort J., McCormick M., Paniagua B.
+  Computing Bone Morphometric Feature Maps from 3-Dimensional Images
+  The Insight Journal. January-December. 2017.
+  http://hdl.handle.net/10380/3588
+  "
+  GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKBoneMorphometry.git
+  GIT_TAG 9d19bf205a124cbad90f0d42c07ef21abb62182d
+)
