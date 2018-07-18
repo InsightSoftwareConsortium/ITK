41ececa90886efc191e64de54bf7f35239d6f304
Matt McCormick <matt.mccormick@kitware.com>
2018-07-18 15:11:59 -0400

    BUG: Bump SplitComponents remote module to 2018-07-18
    Shortlog:
    
     Jon Haitz Legarreta Gorroño (1):
           ENH: Add CI configuration files.
    
     Matt McCormick (4):
           COMP: Fix Python wrapping
           BUG: Add missing CTestConfig.cmake
           DOC: Update the build instructions
           BUG: Add missing ITK_TEMPLATE_EXPORT
    
    Change-Id: Ie1f3625a2529b6d8b5fb7e4aee2e89977df2c1b8

--- Modules/Remote/SplitComponents.remote.cmake
+++ Modules/Remote/SplitComponents.remote.cmake
@@ -5,5 +5,5 @@ itk::SplitComponentsImageFilter.  This filter generates component images from an
 itk::Image of, for example, itk::Vector, itk::CovariantVector, or
 itk::SymmetricSecondRankTensor. https://hdl.handle.net/10380/3230"
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKSplitComponents.git
-  GIT_TAG c579808cb9dbd37da6b3122bb25e0af05dc3fc18
+  GIT_TAG f52bef3212f764a25c8e1398c170b599d3490f6d
   )
