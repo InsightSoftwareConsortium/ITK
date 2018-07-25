ffe11da9e29f7f208c30d97393bc65367e34ccfd
Dženan Zukić <dzenan.zukic@kitware.com>
2018-07-25 10:16:26 -0400

    COMP: SmoothingRecursiveYvvGaussianFilter compiles with new ITKv5 threading
    Change-Id: I4e6eb9291a252ea7a8bcdb239fac83a2d6e87368

--- Modules/Remote/SmoothingRecursiveYvvGaussianFilter.remote.cmake
+++ Modules/Remote/SmoothingRecursiveYvvGaussianFilter.remote.cmake
@@ -3,5 +3,5 @@ itk_fetch_module(SmoothingRecursiveYvvGaussianFilter
   "GPU and CPU Young & Van Vliet Recursive Gaussian Smoothing Filter: https://hdl.handle.net/10380/3425"
   #UPSTREAM_REPO GIT_REPOSITORY ${git_protocol}://github.com/Inria-Asclepios/SmoothingRecursiveYvvGaussianFilter
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/SmoothingRecursiveYvvGaussianFilter.git
-  GIT_TAG 62fbb0db2cc1dc23bf2c8331bc5104669d8026f7
+  GIT_TAG 7e5946aa49017541076d4a707527ccaf99b1a6ec
   )
