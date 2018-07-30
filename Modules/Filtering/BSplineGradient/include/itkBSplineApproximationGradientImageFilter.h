0907185aeb5b68f79cda04737e0325f0017a2e10
Matt McCormick <matt.mccormick@kitware.com>
2018-07-30 14:42:04 -0400

    ENH: Import itkBSplineApproximationGradientImageFilter

---
+++ Modules/Filtering/BSplineGradient/include/itkBSplineApproximationGradientImageFilter.h
@@ -0,0 +1,162 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef itkBSplineApproximationGradientImageFilter_h
+#define itkBSplineApproximationGradientImageFilter_h
+
+#include "itkImageToImageFilter.h"
+#include "itkCovariantVector.h"
+#include "itkMesh.h"
+#include "itkPointSet.h"
+
+#include "itkBSplineScatteredDataPointSetToGradientImageFilter.h"
+#include "itkImageToPointSetFilter.h"
+
+namespace itk
+{
+
+/** \class BSplineApproximationGradientImageFilter
+ *
+ * \brief Uses a B-spline approximation to an Image to calculate a gradient
+ * image.
+ *
+ * This is an ImageToImageFilter whose input image pixels should be of the type
+ * itk::Vector and whose outputs will be of the type itk::CovariantVector.
+ *
+ * \sa BSplineScatteredDataPointSetToGradientImageFilter
+ * \sa Vector
+ * \sa CovariantVector
+ *
+ * \ingroup BSplineGradient
+ */
+template <class TInputImage, class TOutputValueType>
+class BSplineApproximationGradientImageFilter
+  : public ImageToImageFilter<
+      TInputImage,
+      Image<CovariantVector<TOutputValueType, TInputImage::ImageDimension>, TInputImage::ImageDimension>>
+{
+public:
+  ITK_DISALLOW_COPY_AND_ASSIGN(BSplineApproximationGradientImageFilter);
+
+  itkStaticConstMacro(ImageDimension, unsigned int, TInputImage::ImageDimension);
+
+  typedef BSplineApproximationGradientImageFilter Self;
+
+  typedef TInputImage                                                              InputImageType;
+  typedef Image<CovariantVector<TOutputValueType, ImageDimension>, ImageDimension> OutputImageType;
+  typedef typename OutputImageType::Pointer                                        OutputImagePointer;
+
+  typedef ImageToImageFilter<InputImageType, OutputImageType> Superclass;
+  typedef SmartPointer<Self>                                  Pointer;
+  typedef SmartPointer<const Self>                            ConstPointer;
+
+  itkNewMacro(Self);
+
+  itkTypeMacro(BSplineApproximationGradientImageFilter, ImageToImageFilter);
+
+  typedef TOutputValueType                                                 OutputValueType;
+  typedef typename InputImageType::PixelType                               InputPixelType;
+  typedef PointSet<InputPixelType, itkGetStaticConstMacro(ImageDimension)> PointSetType;
+
+  itkStaticConstMacro(InputVectorDimension, unsigned int, InputPixelType::Dimension);
+
+  /** Internal filter type */
+  /** Internal filter type */
+  typedef BSplineScatteredDataPointSetToGradientImageFilter<PointSetType, OutputValueType> PointSetToGradientFilterType;
+  typedef typename PointSetToGradientFilterType::Pointer   PointSetToGradientFilterPointerType;
+  typedef typename PointSetToGradientFilterType::ArrayType ArrayType;
+  typedef Mesh<InputPixelType, ImageDimension>             MeshType;
+  typedef ImageToPointSetFilter<InputImageType, MeshType>  ImageToPointSetFilterType;
+  typedef typename ImageToPointSetFilterType::Pointer      ImageToPointSetFilterPointerType;
+
+  typedef FixedArray<double, itkGetStaticConstMacro(ImageDimension)> ControlPointSpacingRatioType;
+
+  /** Set/Get number of levels.  This is the number of levels used in the
+   * BSplineScatteredDataPointSetToImageFilter for calculating the BSpline grid.
+   * */
+  void
+  SetNumberOfLevels(const unsigned int levels)
+  {
+    this->m_NumberOfLevels.Fill(levels);
+    this->Modified();
+  }
+  itkSetMacro(NumberOfLevels, ArrayType);
+  itkGetConstReferenceMacro(NumberOfLevels, ArrayType);
+
+  /** Set/Get the order of the BSpline in each direction. */
+  void
+  SetSplineOrder(const unsigned int order)
+  {
+    this->m_SplineOrder.Fill(order);
+    this->Modified();
+  }
+  itkSetMacro(SplineOrder, ArrayType);
+  itkGetConstReferenceMacro(SplineOrder, ArrayType);
+
+  /** Set/Get the ratio of the approximating B-spline control point spacing to
+   * to the input image spacing in each direction.  Note that this will result
+   * in specifying the number of top level controls points in the
+   * approximating B-spline filter according to the following expression
+   *
+   * c_size = floor( i_size / ( r * 2^(l-1) ) )
+   *
+   * c_size = number of control points in the top level
+   * i_size = size of the image
+   * r      = specified ratio
+   * l      = number of levels
+   *
+   */
+  void
+  SetControlPointSpacingRatio(const double & ratio)
+  {
+    this->m_ControlPointSpacingRatio.Fill(ratio);
+    this->Modified();
+  }
+  itkSetMacro(ControlPointSpacingRatio, ControlPointSpacingRatioType);
+  itkGetConstReferenceMacro(ControlPointSpacingRatio, ControlPointSpacingRatioType);
+
+protected:
+  BSplineApproximationGradientImageFilter();
+  virtual ~BSplineApproximationGradientImageFilter() {}
+
+  /** Needs everything. */
+  void
+  GenerateInputRequestedRegion() override;
+
+  /** This filter produces the LargestPossibleRegion. */
+  void
+  EnlargeOutputRequestedRegion(DataObject * data) override;
+
+  void
+  GenerateData() override;
+
+private:
+  ImageToPointSetFilterPointerType    m_ImageToPointSetFilter;
+  PointSetToGradientFilterPointerType m_PointSetToGradientFilter;
+
+  ArrayType m_NumberOfLevels;
+  ArrayType m_SplineOrder;
+
+  ControlPointSpacingRatioType m_ControlPointSpacingRatio;
+};
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#  include "itkBSplineApproximationGradientImageFilter.hxx"
+#endif
+
+#endif
---
+++ Modules/Filtering/BSplineGradient/test/itkBSplineApproximationGradientImageFilterTest.cxx
@@ -0,0 +1,94 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#include "itkVectorMagnitudeImageFilter.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+
+#include "itkBSplineApproximationGradientImageFilter.h"
+#include "itkImageToImageOfVectorsFilter.h"
+
+int
+itkBSplineApproximationGradientImageFilterTest(int argc, char * argv[])
+{
+  if (argc < 3)
+  {
+    std::cerr << "Usage: " << argv[0];
+    std::cerr << " inputImage outputImage ";
+    std::cerr << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  const unsigned int                       Dimension = 2;
+  typedef float                            PixelType;
+  typedef itk::Image<PixelType, Dimension> InputImageType;
+  typedef InputImageType                   OutputImageType;
+
+  typedef itk::ImageFileReader<InputImageType> ReaderType;
+  ReaderType::Pointer                          reader = ReaderType::New();
+  reader->SetFileName(argv[1]);
+
+  typedef itk::ImageToImageOfVectorsFilter<InputImageType, 1> ImageToVectorImageFilterType;
+  ImageToVectorImageFilterType::Pointer                       imageToVI = ImageToVectorImageFilterType::New();
+  imageToVI->SetInput(0, reader->GetOutput());
+  typedef ImageToVectorImageFilterType::OutputImageType VectorImageType;
+  try
+  {
+    reader->UpdateOutputInformation();
+  }
+  catch (itk::ExceptionObject & ex)
+  {
+    std::cerr << "Exception caught!" << std::endl;
+    std::cerr << ex << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  typedef itk::BSplineApproximationGradientImageFilter<VectorImageType, float> GradientImageFilterType;
+  GradientImageFilterType::Pointer gradientImageFilter = GradientImageFilterType::New();
+  gradientImageFilter->SetInput(imageToVI->GetOutput());
+  gradientImageFilter->SetNumberOfLevels(1);
+  gradientImageFilter->SetControlPointSpacingRatio(2.0);
+
+  typedef itk::VectorMagnitudeImageFilter<GradientImageFilterType::OutputImageType, OutputImageType>
+                                       GradientMagnitudeFilterType;
+  GradientMagnitudeFilterType::Pointer gradientMagnitude = GradientMagnitudeFilterType::New();
+  gradientMagnitude->SetInput(gradientImageFilter->GetOutput());
+
+  typedef itk::ImageFileWriter<OutputImageType> WriterType;
+  WriterType::Pointer                           writer = WriterType::New();
+  writer->SetInput(gradientMagnitude->GetOutput());
+  writer->SetFileName(std::string(argv[2]) + "GradientMagnitudeOutput.mha");
+
+  typedef itk::ImageFileWriter<GradientImageFilterType::OutputImageType> FullWriterType;
+  FullWriterType::Pointer                                                fullWriter = FullWriterType::New();
+  fullWriter->SetInput(gradientImageFilter->GetOutput());
+  fullWriter->SetFileName(std::string(argv[2]) + "GradientOutput.mha");
+
+  try
+  {
+    writer->Update();
+    fullWriter->Update();
+  }
+  catch (itk::ExceptionObject & ex)
+  {
+    std::cerr << "Exception caught!" << std::endl;
+    std::cerr << ex << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
