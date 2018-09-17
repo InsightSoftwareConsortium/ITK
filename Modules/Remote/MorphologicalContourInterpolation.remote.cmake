2941aa411e6baa8485fb7f0ab10dd55d9e59e5b0
Dženan Zukić <dzenan.zukic@kitware.com>
2018-09-17 12:30:35 -0400

    ENH: update remote module MorphologicalContourInterpolation
    $ git shortlog --no-merges e132944a..210b1a9
    Dženan Zukić (2):
          ENH: switching to ITKv5 threading model
          STYLE: method-local variables do not follow member-name pattern
    
    Change-Id: Id176d70f88b62fd3b786e350125081d197d32e46

--- Modules/Remote/MorphologicalContourInterpolation.remote.cmake
+++ Modules/Remote/MorphologicalContourInterpolation.remote.cmake
@@ -14,5 +14,5 @@ IEEE Trans Biomed Eng. 2008 Aug;55(8):2022-38. doi: 10.1109/TBME.2008.921158.
 This work is supported by NIH grant R01 EB014346
 'Continued development and maintenance of the ITK-SNAP 3D image segmentation software'."
   GIT_REPOSITORY ${git_protocol}://github.com/KitwareMedical/ITKMorphologicalContourInterpolation.git
-  GIT_TAG e132944a3fad51465919464b57ed54e8194fe5ae
+  GIT_TAG 210b1a92a3a2ff4611c769e8a657eeae448428a6
   )
