InsightSoftwareConsortium/ITK

commit 3ca3103604256ec643c78eab3901d1f4bdf5b635

COMP: Move ITK_DISALLOW_COPY_AND_ASSIGN calls to public section.
Move `ITK_DISALLOW_COPY_AND_ASSIGN` calls to public section following
the discussion in
https://discourse.itk.org/t/noncopyable

If legacy (pre-macro) copy and assing methods existed, subsitute them
for the `ITK_DISALLOW_COPY_AND_ASSIGN` macro.

Modules/Registration/VariationalRegistration/include/itkVariationalRegistrationSSDFunction.h
@@ -50,10 +50,11 @@ namespace itk
  *  \author Jan Ehrhardt
  */
 template <class TFixedImage, class TMovingImage, class TDisplacementField>
-class VariationalRegistrationSSDFunction
-  : public VariationalRegistrationFunction<TFixedImage, TMovingImage, TDisplacementField>
+class : public VariationalRegistrationFunction<TFixedImage, TMovingImage, TDisplacementField>
 {
 public:
+  ITK_DISALLOW_COPY_AND_ASSIGN(VariationalRegistrationSSDFunction);
+
   /** Standard class type alias. */
   using Self = VariationalRegistrationSSDFunction;
   using Superclass = VariationalRegistrationFunction<TFixedImage, TMovingImage, TDisplacementField>;
@@ -180,10 +181,6 @@ protected:
   };
 
 private:
-  VariationalRegistrationSSDFunction(const Self &); // purposely not implemented
-  void
-  operator=(const Self &); // purposely not implemented
-
   /** Function to compute derivatives of the fixed image. */
   GradientCalculatorPointer m_FixedImageGradientCalculator;
 

Modules/Registration/VariationalRegistration/include/itkContinuousBorderWarpImageFilter.h
@@ -59,6 +59,8 @@ template <class TInputImage, class TOutputImage, class TDisplacementField>
 class ContinuousBorderWarpImageFilter : public WarpImageFilter<TInputImage, TOutputImage, TDisplacementField>
 {
 public:
+  ITK_DISALLOW_COPY_AND_ASSIGN(ContinuousBorderWarpImageFilter);
+
   /** Standard class type alias. */
   using Self = ContinuousBorderWarpImageFilter;
   using Superclass = WarpImageFilter<TInputImage, TOutputImage, TDisplacementField>;
@@ -110,11 +112,6 @@ protected:
    * ThreadedGenerateData(). */
   void
   ThreadedGenerateData(const OutputImageRegionType & outputRegionForThread, ThreadIdType threadId) override;
-
-private:
-  ContinuousBorderWarpImageFilter(const Self &); // purposely not implemented
-  void
-  operator=(const Self &); // purposely not implemented
 };
 
 } // end namespace itk

Modules/Registration/VariationalRegistration/include/itkVariationalDiffeomorphicRegistrationFilter.h
@@ -83,6 +83,8 @@ class VariationalDiffeomorphicRegistrationFilter
   : public VariationalRegistrationFilter<TFixedImage, TMovingImage, TDisplacementField>
 {
 public:
+  ITK_DISALLOW_COPY_AND_ASSIGN(VariationalDiffeomorphicRegistrationFilter);
+
   /** Standard class type alias */
   using Self = VariationalDiffeomorphicRegistrationFilter;
   using Superclass = VariationalRegistrationFilter<TFixedImage, TMovingImage, TDisplacementField>;
@@ -191,10 +193,6 @@ protected:
   }
 
 private:
-  VariationalDiffeomorphicRegistrationFilter(const Self &); // purposely not implemented
-  void
-  operator=(const Self &); // purposely not implemented
-
   /** The deformation field. */
   FieldExponentiatorPointer m_Exponentiator;
   DisplacementFieldPointer  m_DisplacementField;

Modules/Registration/VariationalRegistration/include/itkVariationalRegistrationCurvatureRegularizer.h
@@ -63,6 +63,8 @@ template <class TDisplacementField>
 class VariationalRegistrationCurvatureRegularizer : public VariationalRegistrationRegularizer<TDisplacementField>
 {
 public:
+  ITK_DISALLOW_COPY_AND_ASSIGN(VariationalRegistrationCurvatureRegularizer);
+
   /** Standard class type alias */
   using Self = VariationalRegistrationCurvatureRegularizer;
   using Superclass = VariationalRegistrationRegularizer<TDisplacementField>;
@@ -150,10 +152,6 @@ protected:
   CalculateImageIndex(OffsetValueType offset);
 
 private:
-  VariationalRegistrationCurvatureRegularizer(const Self &); // purposely not implemented
-  void
-  operator=(const Self &); // purposely not implemented
-
   /** Weight of the regularization term. */
   ValueType m_Alpha;
 

Modules/Registration/VariationalRegistration/include/itkVariationalRegistrationDemonsFunction.h
@@ -58,6 +58,8 @@ class VariationalRegistrationDemonsFunction
   : public VariationalRegistrationFunction<TFixedImage, TMovingImage, TDisplacementField>
 {
 public:
+  ITK_DISALLOW_COPY_AND_ASSIGN(VariationalRegistrationDemonsFunction);
+
   /** Standard class type alias. */
   using Self = VariationalRegistrationDemonsFunction;
   using Superclass = VariationalRegistrationFunction<TFixedImage, TMovingImage, TDisplacementField>;
@@ -174,10 +176,6 @@ protected:
   };
 
 private:
-  VariationalRegistrationDemonsFunction(const Self &); // purposely not implemented
-  void
-  operator=(const Self &); // purposely not implemented
-
   /** Function to compute derivatives of the fixed image. */
   GradientCalculatorPointer m_FixedImageGradientCalculator;
 

Modules/Registration/VariationalRegistration/include/itkVariationalRegistrationDiffusionRegularizer.h
@@ -52,6 +52,8 @@ template <class TDisplacementField>
 class VariationalRegistrationDiffusionRegularizer : public VariationalRegistrationRegularizer<TDisplacementField>
 {
 public:
+  ITK_DISALLOW_COPY_AND_ASSIGN(VariationalRegistrationDiffusionRegularizer);
+
   /** Standard class type alias */
   using Self = VariationalRegistrationDiffusionRegularizer;
   using Superclass = VariationalRegistrationRegularizer<TDisplacementField>;
@@ -162,10 +164,6 @@ protected:
   SplitBoundaryFaceRegion(int i, int num, int inDir, BufferImageRegionType & splitRegion);
 
 private:
-  VariationalRegistrationDiffusionRegularizer(const Self &); // purposely not implemented
-  void
-  operator=(const Self &); // purposely not implemented
-
   /** Weight of the regularization term. */
   ValueType m_Alpha;
 

Modules/Registration/VariationalRegistration/include/itkVariationalRegistrationElasticRegularizer.h
@@ -59,6 +59,8 @@ template <class TDisplacementField>
 class VariationalRegistrationElasticRegularizer : public VariationalRegistrationRegularizer<TDisplacementField>
 {
 public:
+  ITK_DISALLOW_COPY_AND_ASSIGN(VariationalRegistrationElasticRegularizer);
+
   /** Standard class type alias */
   using Self = VariationalRegistrationElasticRegularizer;
   using Superclass = VariationalRegistrationRegularizer<TDisplacementField>;
@@ -156,10 +158,6 @@ protected:
   CalculateComplexImageIndex(OffsetValueType offset);
 
 private:
-  VariationalRegistrationElasticRegularizer(const Self &); // purposely not implemented
-  void
-  operator=(const Self &); // purposely not implemented
-
   /** Weight of the regularization term. */
   ValueType m_Lambda;
 

Modules/Registration/VariationalRegistration/include/itkVariationalRegistrationFastNCCFunction.h
@@ -66,6 +66,8 @@ class VariationalRegistrationFastNCCFunction
   : public VariationalRegistrationNCCFunction<TFixedImage, TMovingImage, TDisplacementField>
 {
 public:
+  ITK_DISALLOW_COPY_AND_ASSIGN(VariationalRegistrationFastNCCFunction);
+
   /** Standard class type alias. */
   using Self = VariationalRegistrationFastNCCFunction;
   using Superclass = VariationalRegistrationNCCFunction<TFixedImage, TMovingImage, TDisplacementField>;
@@ -159,11 +161,6 @@ protected:
     double              smmLastValue;
     double              sfmLastValue;
   };
-
-private:
-  VariationalRegistrationFastNCCFunction(const Self &); // purposely not implemented
-  void
-  operator=(const Self &); // purposely not implemented
 };
 
 

commit ee2e64b40c8767adf2d63f78641123ddb29ef10a

BUG: Add missing classname.
Add missing classname.

Bug introduced by commit:
https://github.com/InsightSoftwareConsortium/ITKVariationalRegistration/commit/3ca3103604256ec643c78eab3901d1f4bdf5b635

Modules/Registration/VariationalRegistration/include/itkVariationalRegistrationSSDFunction.h
@@ -50,7 +50,8 @@ namespace itk
  *  \author Jan Ehrhardt
  */
 template <class TFixedImage, class TMovingImage, class TDisplacementField>
-class : public VariationalRegistrationFunction<TFixedImage, TMovingImage, TDisplacementField>
+class VariationalRegistrationSSDFunction
+  : public VariationalRegistrationFunction<TFixedImage, TMovingImage, TDisplacementField>
 {
 public:
   ITK_DISALLOW_COPY_AND_ASSIGN(VariationalRegistrationSSDFunction);