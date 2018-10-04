e09a1c56c5df25daa8c9e212cd2ea5e1148aa314
Matt McCormick <matt.mccormick@kitware.com>
2018-10-04 14:07:48 -0400

    ENH: Add azure_dashboard.cmake

---
+++ azure_dashboard.cmake
@@ -0,0 +1,63 @@
+
+# set_from_env
+# ------------
+#
+# Sets a CMake variable from an environment variable. If the
+# environment variable  is not  defined then the CMake variable is not
+# modified. If DEFAULT is specified then if the environment variable
+# is not defined the default value is used. Alternatively, if REQUIRED
+# is specified then a FATAL_ERROR is generated.
+#
+# set_from_env( <variable> <environment variable> [REQUIRED|DEFAULT value] )
+function(set_from_env var env_var)
+  if(NOT DEFINED ENV{${env_var}})
+    if (ARGV2 STREQUAL "REQUIRED")
+      message(FATAL_ERROR "Required environment variable \"${env_var}\" not defined.")
+    elseif (ARGV2 STREQUAL "DEFAULT")
+      set(${var} ${ARGV3} PARENT_SCOPE)
+    endif()
+  else()
+    set(${var} $ENV{${env_var}} PARENT_SCOPE)
+  endif()
+endfunction()
+
+set_from_env(CTEST_SITE "AGENT_MACHINENAME" REQUIRED)
+set(CTEST_SITE "Azure.${CTEST_SITE}")
+set(CTEST_UPDATE_VERSION_ONLY 1)
+
+set_from_env(PARALLEL_LEVEL "PARALLEL_LEVEL" DEFAULT 3)
+set(CTEST_TEST_ARGS ${CTEST_TEST_ARGS} PARALLEL_LEVEL  ${PARALLEL_LEVEL})
+
+
+set_from_env(workspace "AGENT_BUILDDIRECTORY" REQUIRED)
+file(TO_CMAKE_PATH "${workspace}" CTEST_DASHBOARD_ROOT)
+# Make environment variables to CMake variables for CTest
+set_from_env(CTEST_CMAKE_GENERATOR "CTEST_CMAKE_GENERATOR" DEFAULT "Ninja")
+set_from_env(CTEST_CONFIGURATION_TYPE "CTEST_CONFIGURATION_TYPE" DEFAULT "Release")
+
+if(NOT CTEST_BUILD_NAME)
+  if(DEFINED ENV{SYSTEM_PULLREQUEST_SOURCEBRANCH})
+    set(branch "-$ENV{SYSTEM_PULLREQUEST_SOURCEBRANCH}")
+  else()
+    set(branch "-master")
+  endif()
+  if(DEFINED ENV{SYSTEM_PULLREQUEST_PULLREQUESTNUMBER})
+    set(pr "-PR$ENV{SYSTEM_PULLREQUEST_PULLREQUESTNUMBER}")
+  else()
+    set(pr "")
+  endif()
+  set(CTEST_BUILD_NAME
+    "$ENV{AGENT_OS}-Build$ENV{BUILD_BUILDID}${pr}${branch}")
+endif()
+
+set_from_env(BUILD_SHARED_LIBS "BUILD_SHARED_LIBS" DEFAULT "OFF")
+set(dashboard_cache "
+    BUILD_EXAMPLES:BOOL=ON
+    BUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
+    BUILD_TESTING:BOOL=ON
+" )
+
+string(TIMESTAMP build_date "%Y-%m-%d")
+message("CDash Build Identifier: ${build_date} ${CTEST_BUILD_NAME}")
+message("CTEST_SITE = ${CTEST_SITE}")
+include("${CTEST_SCRIPT_DIRECTORY}/itk_common.cmake")
