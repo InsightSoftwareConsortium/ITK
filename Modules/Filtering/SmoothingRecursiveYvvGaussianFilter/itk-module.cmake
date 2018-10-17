d8553e12c2fba9fed7d378adccddeab62f616466
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2018-10-16 21:16:56 -0400

    ENH: Add CI.
    - Add `*.yml` configuration files for CI.
    - Make the `README` file be a reSTructured text format file to conform to
      the `ITKModuleTemplate` directions.
    - Update the `README.rst` file to reflect the CI status.
    - Remove the outdated installation instructions from the `README.rst`
      file.
    - Refer to the `LICENSE` file in the `README.rst` *License* section.
    - Modify the current `CTestConfig.cmake` file to make the build results
      be sent to the **Insight** project in **open.cdash.org**.
    - Add the `setup.py` file for Python packaging.
    - Modify the `CTestConfig.cmake` to take advantage of the module
      documentation in the `README.rst` file.

--- Modules/Filtering/SmoothingRecursiveYvvGaussianFilter/itk-module.cmake
+++ Modules/Filtering/SmoothingRecursiveYvvGaussianFilter/itk-module.cmake
@@ -1,7 +1,16 @@
-set(DOCUMENTATION "This module contains a collection of classes for performing
-recursive gaussian filtering (Young Van Vliet implementation).")
+# the top-level README is used for describing this module, just
+# re-used it for documentation here
+get_filename_component(MY_CURRENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+file(READ "${MY_CURRENT_DIR}/README.rst" DOCUMENTATION)
 
+# itk_module() defines the module dependencies in
+# SmoothingRecursiveYvvGaussianFilter
+# The testing module in SmoothingRecursiveYvvGaussianFilter depends on
+# ITKTestKernel
+# By convention those modules outside of ITK are not prefixed with
+# ITK.
 
+# define the dependencies of the include module and the tests
 set(ModuleName "SmoothingRecursiveYvvGaussianFilter")
 if(ITK_USE_GPU)
     itk_module(${ModuleName}
