f2027ed935805766533bf2c459e79c21bc441f91
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2018-10-17 08:38:59 -0400

    ENH: Add CI.
    - Add `*.yml` configuration files for CI.
    - Update the `README.rst` file to reflect the CI status.
    - Modify the current `CTestConfig.cmake` file to make the build
      results be sent to the **Insight** project in **open.cdash.org**.
    - Add the necessary `*.wrap` files.
    - Add the `setup.py` file for Python packaging.
    - Modify the `itk-module.cmake` to take advantage of the module
      documentation in the `README.rst` file.
    - Take advantage to improve the `README` file contents.

--- Modules/Filtering/MeshNoise/itk-module.cmake
+++ Modules/Filtering/MeshNoise/itk-module.cmake
@@ -1,8 +1,15 @@
-set(DOCUMENTATION "This module contains classes to perturb
-itk::Mesh and itk::QuadEdgeMesh objects with Gaussian noise.
-This module is introduced in the Insight Journal article
-http://hdl.handle.net/10380/3567")
 
+# the top-level README is used for describing this module, just
+# re-used it for documentation here
+get_filename_component(MY_CURRENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+file(READ "${MY_CURRENT_DIR}/README.rst" DOCUMENTATION)
+
+# itk_module() defines the module dependencies in DVMeshNoise
+# The testing module in DVMeshNoise depends on ITKTestKernel
+# By convention those modules outside of ITK are not prefixed with
+# ITK.
+
+# define the dependencies of the include module and the tests
 itk_module(DVMeshNoise
   DEPENDS
     ITKCommon
