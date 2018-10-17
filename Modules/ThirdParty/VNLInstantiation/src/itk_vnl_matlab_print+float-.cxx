2bfad396142f32d8fab6d7ab466cf4ac15b42b4e
Hans Johnson <hans.j.johnson@gmail.com>
2018-10-17 08:35:16 -0500

    ENH: Adding explicit instantiations for dimensions 1-10
    When explicit instantiation of dimensions is supported in ITK,
    all the dimensions between 1 and 10 should be supported.
    
    This is an intermediate improvement to allow the ANTs package
    to compile against the head fo ITKv5.  ANTs has need to support
    for 5D vnl_svd_fixed+float.5.5-.cxx
    
    A medium term review of the explicitly instantiated vnl
    indicates that further improvements can be made, but those
    changes are more intrusive than this simple addition of
    missing instantiations.
    
    Created new files for explicit instantiations needed
    by ITK for matlab printing.
    itk_vnl_matlab_print+float-.cxx
    itk_vnl_matlab_print+double-.cxx
    
    Change-Id: Id4b47ceaf26e50ac799c748ab3b6de3f97f387c1

--- Modules/ThirdParty/VNLInstantiation/src/itk_vnl_matlab_print+float-.cxx
+++ Modules/ThirdParty/VNLInstantiation/src/itk_vnl_matlab_print+float-.cxx
@@ -4,4 +4,9 @@
 using T=float;
 
 template VNL_EXPORT std::ostream &vnl_matlab_print(std::ostream &, vnl_matrix_fixed<T,1,1> const&, char const*, vnl_matlab_print_format);
+template VNL_EXPORT std::ostream &vnl_matlab_print(std::ostream &, vnl_matrix_fixed<T,5,5> const&, char const*, vnl_matlab_print_format);
+template VNL_EXPORT std::ostream &vnl_matlab_print(std::ostream &, vnl_matrix_fixed<T,6,6> const&, char const*, vnl_matlab_print_format);
+template VNL_EXPORT std::ostream &vnl_matlab_print(std::ostream &, vnl_matrix_fixed<T,7,7> const&, char const*, vnl_matlab_print_format);
+template VNL_EXPORT std::ostream &vnl_matlab_print(std::ostream &, vnl_matrix_fixed<T,8,8> const&, char const*, vnl_matlab_print_format);
+template VNL_EXPORT std::ostream &vnl_matlab_print(std::ostream &, vnl_matrix_fixed<T,9,9> const&, char const*, vnl_matlab_print_format);
 template VNL_EXPORT std::ostream &vnl_matlab_print(std::ostream &, vnl_matrix_fixed<T,10,10> const&, char const*, vnl_matlab_print_format);
