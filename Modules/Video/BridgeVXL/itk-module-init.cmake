0249113314b6fb33dc000905d1043cbed90dcedc
Hans Johnson <hans-johnson@uiowa.edu>
2018-11-03 14:24:34 -0500

    BUG: Do not override user selection settings
    The old code silently changed a user controlled setting.
    It is better to give an error and request that the user
    manually make the required changes.
    
    Change-Id: I1517f788e3925cf8b7b208b7b7da6213d4485e77

--- Modules/Video/BridgeVXL/itk-module-init.cmake
+++ Modules/Video/BridgeVXL/itk-module-init.cmake
@@ -1,7 +1,9 @@
-find_package ( VXL REQUIRED )
+if(NOT ITK_USE_SYSTEM_VXL)
+  message( FATAL_ERROR "System installed VXL is required for ITKVideoBridgeVXL.")
+endif()
+find_package(VXL 2.0.0 REQUIRED)
 
 if (VXL_FOUND)
-  set(ITK_USE_SYSTEM_VXL ON CACHE BOOL "System installed VXL is required for ITKVideoBridgeVXL." FORCE)
   include( ${VXL_CMAKE_DIR}/UseVXL.cmake )
   message("ITK_USE_SYSTEM_VXL is ON; Use an outside build of VXL. ")
 endif()
