f11cde58e510b22ad16d55ee4c1a45c1d867cbe5
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2018-11-10 15:29:20 -0500

    ENH: Add CI.
    Add CI for the module:
    - Add Circle, Travis and AppVeyor CI configuration `*.yml` files.
    - Add the `CTestConfig.cmake` file to submit builds to the **Inisght**
      project in the **open.cdash.org** dashboard.
    - Add the Python setup `setup.py` file
    - Add the Python wrapping `*.wrap` files.
    - Re-use the `README` file documentation in the `itk-module.cmake` file.

--- Modules/Filtering/SubdivisionQuadEdgeMeshFilter/itk-module.cmake
+++ Modules/Filtering/SubdivisionQuadEdgeMeshFilter/itk-module.cmake
@@ -1,10 +1,11 @@
-set(
-  DOCUMENTATION
-  "Triangle Mesh Subdivision
-http://www.insight-journal.org/browse/publication/831
-"
-)
+# the top-level README is used for describing this module, just
+# re-used it for documentation here
+# itk_module() defines the module dependencies in SubdivisionQuadEdgeMeshFilter
+# The testing module in SubdivisionQuadEdgeMeshFilter depends on ITKTestKernel
+# By convention those modules outside of ITK are not prefixed with
+# ITK
 
+# define the dependencies of the include module and the tests
 itk_module(
   SubdivisionQuadEdgeMeshFilter
   DEPENDS
