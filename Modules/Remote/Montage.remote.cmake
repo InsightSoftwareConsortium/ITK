44df2b28354bfa104876db90b90c9ff9d50438bd
Dženan Zukić <dzenan.zukic@kitware.com>
2018-11-20 09:38:27 -0500

    ENH: update Montage remote module
    The updates include
     * std::mutex refactoring
     * Butterworth band-pass filtering of input tiles
     * adjust to VXL2 changes

--- Modules/Remote/Montage.remote.cmake
+++ Modules/Remote/Montage.remote.cmake
@@ -2,5 +2,5 @@
 itk_fetch_module(Montage
 "Reconstruction of 3D volumetric dataset from a collection of 2D slices"
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKMontage.git
-  GIT_TAG adc26dfe60384a512bd4993bd9a1fab872e9597f
+  GIT_TAG a8154ee8544e6414b87167ffa95fc1186d3a2881
   )
