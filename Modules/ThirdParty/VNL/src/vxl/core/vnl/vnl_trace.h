381a2856b147fd92e19b4f0060fde6f05b23c1f8
Dženan Zukić <dzenan.zukic@kitware.com>
2018-11-28 12:20:22 -0500

    COMP: Remove VNL_EXPORT from header only class. Closes #191.
    C:\Dev\ITK-git\Modules\ThirdParty\VNL\src\vxl\core\vnl/vnl_trace.h(29): error C2491: 'vnl_trace': definition of dllimport function not allowed
    C:\Dev\ITK-git\Modules\ThirdParty\VNL\src\vxl\core\vnl/vnl_trace.h(40): error C2491: 'vnl_trace': definition of dllimport function not allowed

--- Modules/ThirdParty/VNL/src/vxl/core/vnl/vnl_trace.h
+++ Modules/ThirdParty/VNL/src/vxl/core/vnl/vnl_trace.h
@@ -18,7 +18,7 @@
 
 //: Calculate trace of a matrix
 // \relatesalso vnl_matrix
-template <class T> VNL_EXPORT
+template <class T>
 T vnl_trace(vnl_matrix<T> const& M)
 {
   T sum(0);
@@ -30,7 +30,7 @@ T vnl_trace(vnl_matrix<T> const& M)
 
 //: Calculate trace of a matrix
 // \relatesalso vnl_matrix_fixed
-template <class T, unsigned int N1, unsigned int N2> VNL_EXPORT
+template <class T, unsigned int N1, unsigned int N2>
 T vnl_trace(vnl_matrix_fixed<T,N1,N2> const& M)
 {
   T sum(0);
