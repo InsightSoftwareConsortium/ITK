4443fbb9be26e50db8c73f58cb298c59960b46e9
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2018-12-21 20:48:34 -0500

    DOC: Re-use `README.rst` info in `itk-module.cmake`.
    Re-use `README.rst` info in `itk-module.cmake`.
    
    Helps avoiding duplications/diverging versions of the documentation.

--- Modules/Filtering/SplitComponents/itk-module.cmake
+++ Modules/Filtering/SplitComponents/itk-module.cmake
@@ -1,11 +1,14 @@
-set(
-  DOCUMENTATION
-  "This module contains filter called
-itk::SplitComponentsImageFilter.  This filter generates component images from an
-itk::Image of, for example, itk::Vector, itk::CovariantVector, or
-itk::SymmetricSecondRankTensor."
-)
+# the top-level README is used for describing this module, just
+# re-used it for documentation here
+get_filename_component(MY_CURRENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+file(READ "${MY_CURRENT_DIR}/README.md" DOCUMENTATION)
 
+# itk_module() defines the module dependencies in SplitComponents
+# The testing module in SplitComponents depends on ITKTestKernel
+# By convention those modules outside of ITK are not prefixed with
+# ITK.
+
+# define the dependencies of the include module and the tests
 itk_module(
   SplitComponents
   DEPENDS
