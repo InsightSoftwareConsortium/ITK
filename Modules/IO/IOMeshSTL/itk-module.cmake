53453d60ca9b0d09d56f236571e401f6fb0ea939
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2018-12-21 20:44:41 -0500

    DOC: Re-use `README.rst` info in `itk-module.cmake`.
    Re-use `README.rst` info in `itk-module.cmake`.
    
    Helps avoiding duplications/diverging versions of the documentation.

--- Modules/IO/IOMeshSTL/itk-module.cmake
+++ Modules/IO/IOMeshSTL/itk-module.cmake
@@ -1,8 +1,14 @@
-set(
-  DOCUMENTATION
-  "This module contains classes for reading and writing
-QuadEdgeMeshes using the STL file format."
-)
+# the top-level README is used for describing this module, just
+# re-used it for documentation here
+get_filename_component(MY_CURRENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+file(READ "${MY_CURRENT_DIR}/README.md" DOCUMENTATION)
+
+# itk_module() defines the module dependencies in IOSTL
+# The testing module in IOSTL depends on ITKTestKernel
+# By convention those modules outside of ITK are not prefixed with
+# ITK.
+
+# define the dependencies of the include module and the tests
 itk_module(
   IOSTL
   ENABLE_SHARED
