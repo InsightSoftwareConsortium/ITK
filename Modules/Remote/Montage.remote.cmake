7ef0ffd2c326fae0ebc32f89159ab976806ee858
Dženan Zukić <dzenan.zukic@kitware.com>
2019-01-03 12:08:08 -0500

    ENH: updating Montage module
    git shortlog --no-merges 46cad95..d0994e6
    
    Dženan Zukic (2):
          BUG: updating test output path to allow testing as part of ITK
          BUG: removing extraneous comma in documentation
    
    Jon Haitz Legarreta Gorroño (1):
          DOC: Improve Python package information.

--- Modules/Remote/Montage.remote.cmake
+++ Modules/Remote/Montage.remote.cmake
@@ -2,5 +2,5 @@
 itk_fetch_module(Montage
 "Reconstruction of 3D volumetric dataset from a collection of 2D slices"
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKMontage.git
-  GIT_TAG 6e89d6d4cc30d7fa0682218fde407e9f4c04bb6c
+  GIT_TAG d0994e6f3495ba081569fa4270698771ea636c6c
   )
