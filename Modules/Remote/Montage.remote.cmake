4ebf9109d9c91b6566fab167fa5906c42760a10f
Dženan Zukić <dzenan.zukic@kitware.com>
2019-01-12 12:12:40 -0500

    ENH: updating Montage: fix double-conversion include directory

--- Modules/Remote/Montage.remote.cmake
+++ Modules/Remote/Montage.remote.cmake
@@ -2,5 +2,5 @@
 itk_fetch_module(Montage
 "Reconstruction of 3D volumetric dataset from a collection of 2D slices"
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKMontage.git
-  GIT_TAG d0994e6f3495ba081569fa4270698771ea636c6c
+  GIT_TAG d5f241c6fffc1a85714cc182f6d3620a283ce2d2
   )
