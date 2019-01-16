a8aec78df7eae087b912808f912b9a92035a4062
Matt McCormick <matt.mccormick@kitware.com>
2019-01-16 16:52:14 -0500

    ENH: Update IOSTL to 2019-01-16 master
     Jon Haitz Legarreta Gorroño (3):
           ENH: Improve the `README` file.
           ENH: Add CI.
           DOC: Re-use `README.rst` info in `itk-module.cmake`.
    
     Matt McCormick (1):
           ENH: Enable mesh module factory registration

--- Modules/Remote/IOSTL.remote.cmake
+++ Modules/Remote/IOSTL.remote.cmake
@@ -3,5 +3,5 @@ itk_fetch_module(IOSTL
   "This module contains classes for reading and writing QuadEdgeMeshes using
   the STL (STereoLithography)file format. https://hdl.handle.net/10380/3452"
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKSTLMeshIO.git
-  GIT_TAG f4723954fdfe294f84f69907e964b50e48b60218
+  GIT_TAG e681a38aba1d89561eec89584d80f62dd2affa23
   )
