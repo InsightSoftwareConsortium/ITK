6956711741ef8c52ae1c24bc86778e2040fe47af
adigherman <adig@jhu.edu>
2019-01-16 16:19:36 -0500

    adding ## to __VA_ARGS__

--- Modules/ThirdParty/HDF5/src/itkhdf5/src/H5win32defs.h
+++ Modules/ThirdParty/HDF5/src/itkhdf5/src/H5win32defs.h
@@ -54,7 +54,7 @@ typedef __int64             h5_stat_size_t;
  * Also note that the variadic macro is using a VC++ extension
  * where the comma is dropped if nothing is passed to the ellipsis.
  */
-#define HDopen(S,F,...)       _open(S, F | _O_BINARY, __VA_ARGS__)
+#define HDopen(S,F,...)       _open(S, F | _O_BINARY, ##__VA_ARGS__)
 #define HDread(F,M,Z)       _read(F,M,Z)
 #define HDrmdir(S)          _rmdir(S)
 #define HDsetvbuf(F,S,M,Z)  setvbuf(F,S,M,(Z>1?Z:2))
