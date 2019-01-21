4de6af343078c9755a85f523503fba27b1981973
Matt McCormick <matt.mccormick@kitware.com>
2019-01-21 16:02:24 -0500

    ENH: Update IOMeshSTL remote module and update name
    git shortlog --no-merges e681a38aba1d89561eec89584d80f62dd2affa23..
    
     Matt McCormick (6):
        ENH: Rename repository to be consistent with ITK modules
        ENH: Python package requires itk>4
        BUG: Set PointDimension to 3
        BUG: Fix wrapping module name
        ENH: Make module name consistent with conventions
        DOC: Fix AppVeyor badge and link after module name update

--- Modules/Remote/IOMeshSTL.remote.cmake
+++ Modules/Remote/IOMeshSTL.remote.cmake
@@ -1,7 +1,7 @@
 # Contact: Luis Ibanez <luis.ibanez@kitware.com>
-itk_fetch_module(IOSTL
+itk_fetch_module(IOMeshSTL
   "This module contains classes for reading and writing QuadEdgeMeshes using
   the STL (STereoLithography)file format. https://hdl.handle.net/10380/3452"
-  GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKSTLMeshIO.git
-  GIT_TAG e681a38aba1d89561eec89584d80f62dd2affa23
+  GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKIOMeshSTL.git
+  GIT_TAG e6f4aebc8ab3b3a2ec36b259ea4734d65df09a5f
   )
