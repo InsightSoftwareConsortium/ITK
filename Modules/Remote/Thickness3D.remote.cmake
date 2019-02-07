fd3e34788226c572fe7e8c6a83ceaf753b12175c
Thomas Janvier <thomas.p.janvier@gmail.com>
2019-02-07 16:48:50 +0100

    ENH: Add Thickness3D Remote module.

---
+++ Modules/Remote/Thickness3D.remote.cmake
@@ -0,0 +1,6 @@
+# Contact: Thomas Janvier <thomas.p.janvier@gmail.com>
+itk_fetch_module(Thickness3D
+  "Tools for 3D thickness measurement"
+  GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKThickness3D.git
+  GIT_TAG 7b2de28581369586595fdd5e3b9bec2606a2d516
+)
