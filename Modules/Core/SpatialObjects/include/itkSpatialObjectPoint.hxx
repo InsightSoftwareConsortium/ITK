9927f6c37b8c2eb718a57b2b89af6d78fa2f63c6
Stephen Aylward <stephen.aylward@kitware.com>
2019-02-08 16:25:19 -0500

    ENH: SpatialObjectPoint and Properties updated
    Points can now return their position in WorldSpace.
    
    SpatialObjectProperties now supports tag-value tag-string dictionaries.
    - Should be used to indicate if a tube is an artery, etc.

--- Modules/Core/SpatialObjects/include/itkSpatialObjectPoint.hxx
+++ Modules/Core/SpatialObjects/include/itkSpatialObjectPoint.hxx
@@ -26,30 +26,33 @@ namespace itk
 template< unsigned int TPointDimension >
 SpatialObjectPoint< TPointDimension >
 ::SpatialObjectPoint()
-:  m_X(0.0)
 {
+  m_Id = -1;
+
+  m_X.Fill( 0 );
+
   m_Color.SetRed(1.0); // red by default
   m_Color.SetGreen(0);
   m_Color.SetBlue(0);
   m_Color.SetAlpha(1);
+
+  m_SpatialObject = nullptr;
 }
 
-/** Return the color of the point */
 template< unsigned int TPointDimension >
-const typename SpatialObjectPoint< TPointDimension >::PixelType &
+PointType
 SpatialObjectPoint< TPointDimension >
-::GetColor() const
+::GetPosition( )
 {
-  return m_Color;
-}
+  if( m_SpatialObject == nullptr )
+    {
+    ExceptionObject e(__FILE__);
+    e.SetLocation( "SpatialObjectPoint:GetPosition" );
+    e.SetDescription( "The SpatialObject must be set prior to calling." );
+    throw e;
+    }
 
-/** Set the color of the point */
-template< unsigned int TPointDimension >
-void
-SpatialObjectPoint< TPointDimension >
-::SetColor(const PixelType & color)
-{
-  m_Color = color;
+  return m_SpatialObject->GetObjectToWorldTransform()->TransformPoint( m_X );
 }
 
 /** Set the color of the point */
@@ -62,123 +65,6 @@ SpatialObjectPoint< TPointDimension >
   m_Color.SetGreen(g);
   m_Color.SetBlue(b);
   m_Color.SetAlpha(a);
-}
-
-/** Set the red channel of the point */
-template< unsigned int TPointDimension >
-void
-SpatialObjectPoint< TPointDimension >
-::SetRed(float r)
-{
-  m_Color.SetRed(r);
-}
-
-/** Return the red channel of the point */
-template< unsigned int TPointDimension >
-float
-SpatialObjectPoint< TPointDimension >
-::GetRed() const
-{
-  return m_Color.GetRed();
-}
-
-/** Set the green channel of the point */
-template< unsigned int TPointDimension >
-void
-SpatialObjectPoint< TPointDimension >
-::SetGreen(float g)
-{
-  m_Color.SetGreen(g);
-}
-
-/** Return the green channel of the point */
-template< unsigned int TPointDimension >
-float
-SpatialObjectPoint< TPointDimension >
-::GetGreen() const
-{
-  return m_Color.GetGreen();
-}
-
-/** Set the blue channel of the point */
-template< unsigned int TPointDimension >
-void
-SpatialObjectPoint< TPointDimension >
-::SetBlue(float b)
-{
-  m_Color.SetBlue(b);
-}
-
-/** Return the blue channel of the point */
-template< unsigned int TPointDimension >
-float
-SpatialObjectPoint< TPointDimension >
-::GetBlue() const
-{
-  return m_Color.GetBlue();
-}
-
-/** Set the alpha value of the point */
-template< unsigned int TPointDimension >
-void
-SpatialObjectPoint< TPointDimension >
-::SetAlpha(float a)
-{
-  m_Color.SetAlpha(a);
-}
-
-/** Return the alpha value of the point */
-template< unsigned int TPointDimension >
-float
-SpatialObjectPoint< TPointDimension >
-::GetAlpha() const
-{
-  return m_Color.GetAlpha();
-}
-
-/** Set the Identification number of a point */
-template< unsigned int TPointDimension >
-void
-SpatialObjectPoint< TPointDimension >
-::SetID(const int newID)
-{
-  m_ID = newID;
-}
-
-/** Get the Identification number of a point */
-template< unsigned int TPointDimension >
-int
-SpatialObjectPoint< TPointDimension >
-::GetID() const
-{
-  return m_ID;
-}
-
-/** Return the position of a point */
-template< unsigned int TPointDimension >
-const typename SpatialObjectPoint< TPointDimension >::PointType &
-SpatialObjectPoint< TPointDimension >
-::GetPosition() const
-{
-  return m_X;
-}
-
-/** Set the position : n-D case */
-template< unsigned int TPointDimension >
-void
-SpatialObjectPoint< TPointDimension >
-::SetPosition(const PointType & newX)
-{
-  m_X = newX;
-}
-
-/** PrintSelfMethod */
-template< unsigned int TPointDimension >
-void
-SpatialObjectPoint< TPointDimension >
-::Print(std::ostream & os) const
-{
-  this->PrintSelf(os, 3);
 }
 
 /** PrintSelfMethod */
