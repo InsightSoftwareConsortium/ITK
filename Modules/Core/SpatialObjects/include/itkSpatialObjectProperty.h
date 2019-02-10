125b4cfa9b4c699a7b8683f179209a246cef6bcd
Stephen Aylward <stephen.aylward@kitware.com>
2019-02-09 21:21:59 -0500

    COMP: SpatialObjectProperties now a cxx file that compiles

--- Modules/Core/SpatialObjects/include/itkSpatialObjectProperty.h
+++ Modules/Core/SpatialObjects/include/itkSpatialObjectProperty.h
@@ -19,9 +19,12 @@
 #define itkSpatialObjectProperty_h
 
 #include <string>
+#include <map>
 
-#include "itkRGBAPixel.h"
 #include "itkLightObject.h"
+#include "itkRGBAPixel.h"
+#include "itkTimeStamp.h"
+#include "itkSpatialObjectFactory.h"
 #include "itkObjectFactory.h"
 
 namespace itk
@@ -31,16 +34,16 @@ namespace itk
  * it's templated over the representation to use for each color component.
  */
 
-template< typename TComponentType = double >
-class ITK_TEMPLATE_EXPORT SpatialObjectProperty:
-  public LightObject
+class SpatialObjectProperty
+  : public LightObject
 {
 public:
   //ITK_DISALLOW_COPY_AND_ASSIGN(SpatialObjectProperty);
 
-  using Self = SpatialObjectProperty< TComponentType >;
+  using Self = SpatialObjectProperty;
   using Superclass = LightObject;
-  using PixelType = RGBAPixel< TComponentType >;
+
+  using ColorType = RGBAPixel< double >;
 
   using Pointer = SmartPointer< Self >;
   using ConstPointer = SmartPointer< const Self >;
@@ -48,50 +51,47 @@ public:
   itkNewMacro(Self);
   itkTypeMacro(SpatialObjectProperty, LightObject);
 
-  const PixelType & GetColor() const;
+  itkSetMacro( Color, ColorType );
+  itkGetConstMacro( Color, ColorType );
 
-  void SetColor(const PixelType & color);
+  void SetColor(double r, double g, double b);
 
-  void SetColor(TComponentType r, TComponentType g, TComponentType b);
+  void SetRed(double r);
+  double GetRed() const;
 
-  void SetRed(TComponentType r);
+  void SetGreen(double g);
+  double GetGreen() const;
 
-  TComponentType GetRed() const;
+  void SetBlue(double b);
+  double GetBlue() const;
 
-  void SetGreen(TComponentType g);
-
-  TComponentType GetGreen() const;
-
-  void SetBlue(TComponentType b);
-
-  TComponentType GetBlue() const;
-
-  void SetAlpha(TComponentType a);
-
-  TComponentType GetAlpha() const;
+  void SetAlpha(double a);
+  double GetAlpha() const;
 
   SpatialObjectProperty();
   ~SpatialObjectProperty() override = default;
 
-  void SetName(const std::string & name);
-  const std::string & GetName() const;
+  itkSetMacro( Name, std::string );
+  itkGetConstMacro( Name, std::string );
 
   void SetTagScalarValue( const std::string & tag, double value );
   void SetTagStringValue( const std::string & tag, const std::string & value );
 
-  double      GetTagScalarValue( const std::string & tag );
+  double      GetTagScalarValue( const std::string & tag ) const;
   std::string SetTagStringValue( const std::string & tag );
 
-  std::map< std::string, double >      & GetTagScalarDictionary();
-  std::map< std::string, std::string > & GetTagStringDictionary();
+  std::map< std::string, double > &            GetTagScalarDictionary();
+  const std::map< std::string, double > &      GetTagScalarDictionary() const;
+  std::map< std::string, std::string > &       GetTagStringDictionary();
+  const std::map< std::string, std::string > & GetTagStringDictionary() const;
 
   void SetTagScalarDictionary( const std::map< std::string, double > & dict );
-  void SetTagStringDictionary( const std::map< std::string, std::string > & dict );
+  void SetTagStringDictionary( const std::map< std::string,
+    std::string > & dict );
 
-  Self & operator=(const Self & rhs );
+  SpatialObjectProperty & operator=(const SpatialObjectProperty * rhs );
 
-  ModifiedTimeType GetMTime()
-  { return m_MTime; }
+  itkGetConstMacro( MTime, ModifiedTimeType );
 
 protected:
 
@@ -101,7 +101,7 @@ protected:
 
 private:
 
-  PixelType        m_Color;
+  ColorType        m_Color;
   std::string      m_Name;
 
   std::map< std::string, double >      m_ScalarDictionary;
@@ -109,10 +109,7 @@ private:
 
   ModifiedTimeType m_MTime;
 };
-}
 
-#ifndef ITK_MANUAL_INSTANTIATION
-#include "itkSpatialObjectProperty.hxx"
-#endif
+}
 
 #endif // __SpatialObjectProperty_h
