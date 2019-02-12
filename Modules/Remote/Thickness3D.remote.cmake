36b574e48a24c2aefa16b6001d6babce7a553733
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2019-02-11 19:34:30 -0500

    ENH: Bump the latest version of the `ITKThickness3D` remote.
    Bump the latest version of the `ITKThickness3D` remote module.

--- Modules/Remote/Thickness3D.remote.cmake
+++ Modules/Remote/Thickness3D.remote.cmake
@@ -2,5 +2,5 @@
 itk_fetch_module(Thickness3D
   "Tools for 3D thickness measurement"
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKThickness3D.git
-  GIT_TAG 7b2de28581369586595fdd5e3b9bec2606a2d516
+  GIT_TAG ed537630b3cf5f9f38c53d26ff18e93bf9ea62f6
 )
