77389e70b1268c548ff96b7ed13b12c17ba8f36e
Matt McCormick <matt.mccormick@kitware.com>
2019-03-25 15:15:17 -0400

    ENH: Add PolyData Point and PointData

--- Modules/Beta/MeshToPolyData/test/itkPolyDataTest.cxx
+++ Modules/Beta/MeshToPolyData/test/itkPolyDataTest.cxx
@@ -15,17 +15,66 @@
  *  limitations under the License.
  *
  *=========================================================================*/
-
 #include "itkPolyData.h"
+
+#include "itkTestingMacros.h"
 
 int itkPolyDataTest( int, char *[] )
 {
-  using PixelType = float;
+  using PixelType = double;
 
   using PolyDataType = itk::PolyData< PixelType >;
   PolyDataType::Pointer polyData = PolyDataType::New();
+  constexpr unsigned int PointDimension = PolyDataType::PointDimension;
 
-  polyData->Print(std::cout);
+  polyData->Initialize();
+
+
+  PolyDataType::PointsContainer::Pointer pointsContainer = PolyDataType::PointsContainer::New();
+
+  PolyDataType::PointType point;
+  point[0] = 1.0;
+  point[1] = 3.0;
+  point[2] = 5.0;
+  pointsContainer->InsertElement( 0, point );
+  point[0] = 2.0;
+  point[1] = 4.0;
+  point[2] = 6.0;
+  pointsContainer->InsertElement( 1, point );
+  polyData->SetPoints( pointsContainer );
+
+  for( unsigned int dim = 0; dim < PointDimension; ++dim )
+    {
+    TEST_SET_GET_VALUE( point[dim], polyData->GetPoint( 1 )[dim] );
+    }
+
+  point[0] = 3.0;
+  point[1] = 5.0;
+  point[2] = 7.0;
+  polyData->SetPoint( 2, point );
+  for( unsigned int dim = 0; dim < PointDimension; ++dim )
+    {
+    TEST_SET_GET_VALUE( point[dim], polyData->GetPoint( 2 )[dim] );
+    }
+  polyData->SetPoint( 1, point );
+  for( unsigned int dim = 0; dim < PointDimension; ++dim )
+    {
+    TEST_SET_GET_VALUE( point[dim], polyData->GetPoint( 1 )[dim] );
+    }
+
+
+  PolyDataType::PointDataContainer::Pointer pointDataContainer = PolyDataType::PointDataContainer::New();
+  pointDataContainer->InsertElement( 0, 2.0 );
+  pointDataContainer->InsertElement( 1, 7.0 );
+  polyData->SetPointData( pointDataContainer );
+  double pointData;
+  polyData->GetPointData( 1, &pointData );
+  TEST_SET_GET_VALUE( 7.0, pointData );
+  polyData->SetPointData( 2, 9.9 );
+  polyData->GetPointData( 2, &pointData );
+  TEST_SET_GET_VALUE( 9.9, pointData );
+
+  EXERCISE_BASIC_OBJECT_METHODS( polyData, PolyData, DataObject );
 
   return EXIT_SUCCESS;
 }
