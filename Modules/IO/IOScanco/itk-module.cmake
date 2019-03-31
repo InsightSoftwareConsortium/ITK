26c36adee0cf1c8828dd6fc3924199ee8af16e3f
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2019-03-31 12:46:32 -0400

    DOC: Improve `README` file
    Improve `README` file:
    - Add Python install instructions.
    - Cross-reference the license.
    
    Take advantage of the commit to re-use the `README.rst` file documentation
    in the `itk-module.cmake` file.

--- Modules/IO/IOScanco/itk-module.cmake
+++ Modules/IO/IOScanco/itk-module.cmake
@@ -1,10 +1,11 @@
-set(
-  DOCUMENTATION
-  "This module contains IO classes for reading and writing
-Scanco Medical microCT images. These images are usually saved as .isq or .aim
-files."
-)
+# the top-level README is used for describing this module, just
+# re-used it for documentation here
+# itk_module() defines the module dependencies in IOScanco
+# The testing module in IOScanco depends on ITKTestKernel
+# By convention those modules outside of ITK are not prefixed with
+# ITK.
 
+# define the dependencies of the include module and the tests
 itk_module(
   IOScanco
   ENABLE_SHARED
@@ -12,5 +13,5 @@ itk_module(
     ITKIOImageBase
   TEST_DEPENDS
     ITKTestKernel
-  DESCRIPTION "${DOCUMENTATION}"
+  DESCRIPTION "Module ingested from upstream."
 )
