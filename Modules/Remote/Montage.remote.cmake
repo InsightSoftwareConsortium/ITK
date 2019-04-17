777dd3acbcf8cad17be1a71e6d184b8a75a45fab
maekclena <maggot_fff@hotmail.fr>
2019-04-17 11:29:24 +0200

    ENH: Update remote modules

--- Modules/Remote/Montage.remote.cmake
+++ Modules/Remote/Montage.remote.cmake
@@ -2,5 +2,5 @@
 itk_fetch_module(Montage
 "Reconstruction of 3D volumetric dataset from a collection of 2D slices"
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKMontage.git
-  GIT_TAG 5200bffd7a8e3a4f8b10e909c0919957281be923
+  GIT_TAG 9c043d6610765f07f4bb759ef605cfc8d2aff16c
   )
