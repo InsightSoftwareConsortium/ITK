4a05bcbd57d563ba451f4128f3582af20a39b91b
Bradley Lowekamp <blowekamp@mail.nih.gov>
2019-04-25 16:26:04 -0400

    DOC: Correct documented Canny Segmentation filter example
    The advection term is negative in the test, which is require for
    outward propagation of the level set.

--- Examples/Segmentation/CannySegmentationLevelSetImageFilter.cxx
+++ Examples/Segmentation/CannySegmentationLevelSetImageFilter.cxx
@@ -20,7 +20,7 @@
 //    INPUTS:  {BrainProtonDensitySlice.png}
 //    INPUTS:  {ThresholdSegmentationLevelSetImageFilterVentricle.png}
 //    OUTPUTS: {CannySegmentationLevelSetImageFilterVentricle1.png}
-//    ARGUMENTS:    7.0 0.1 10.0 127.5 15
+//    ARGUMENTS:    7.0 0.1 -10.0 127.5 15
 //  Software Guide : EndCommandLineArgs
 
 // Software Guide : BeginLatex
