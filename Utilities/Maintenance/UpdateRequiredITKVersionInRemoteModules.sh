9fd6ee5c87ad7e9f3f7fc24ab06a27d849de0378
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2019-04-30 13:02:03 -0400

    DOC: Improve remote modules' maintenance scripts documentation
    Improve remote modules' maintenance scripts documentation.

--- Utilities/Maintenance/UpdateRequiredITKVersionInRemoteModules.sh
+++ Utilities/Maintenance/UpdateRequiredITKVersionInRemoteModules.sh
@@ -19,22 +19,54 @@
 #==========================================================================*/
 
 
-program_name=$0
+# This script, given the location of the 'azure-pipelines.yml' and 'setup.py'
+# files of a remote module, updates the required ITK version to its latest in
+# them.
+#
+# Since the required ITK version update is a major change, the Python package
+# of the remote module is updated to a new major version.
 
-function usage {
-  echo "Usage: ${program_name} "
-  echo "  param1 Azure pipelines file path"
-  echo "  param2 Setup.py file path"
-  exit 1
+
+# Utility functions
+usage() {
+cat << EOF
+Usage: $0 <azure_pipelines_ci_filename> <python_setup_filename>
+
+Use this script to update the required ITK version to its latest in the
+'azure-pipelines.yml' and 'setup.py' files of a remote module.
+
+Since the required ITK version update is a major change, the
+Python package of the remote module is updated to a new major version.
+EOF
 }
 
-# Display usage
-if [ ${#@} != 2 ]; then
-  usage
-fi
+die() {
+  echo "$@" 1>&2; exit 1
+}
+
+# Parse arguments
+help=false
+folder_id=""
+while test $# -gt 0;
+do
+  opt="$1";
+  case "$opt" in
+    "-h"|"--help")
+      shift;
+      help=true
+      break;;
+    *)
+      break;;
+  esac
+done
 
 azure_pipelines_ci_filename=$1
 python_setup_filename=$2
+
+if test "${azure_pipelines_ci_filename}" = "" || "${python_setup_filename}" = "" || $help; then
+  usage
+  exit 1
+fi
 
 # Get the latest ITK git tag
 latest_git_tag=$(git ls-remote --tags --sort="v:refname" \
