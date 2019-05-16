e14b8f2ec1f98f087422b5555876471e80346e3d
Hans J. Johnson <hans-johnson@uiowa.edu>
2019-05-16 14:08:15 -0500

    STYLE: Remove custom environmental override for WikiExamples

--- Modules/Remote/WikiExamples.remote.cmake
+++ Modules/Remote/WikiExamples.remote.cmake
@@ -1,16 +1,8 @@
 #
 # ITK WikiExamples
 #
-
-# If the environment var WikiExamplesTag exists, use it
-if (NOT DEFINED ENV{WikiExamplesTag})
-  set(GIT_TAG 07f0e1622ca3eeed8b9c5b9665b066b785af86e8)
-else()
-  set(GIT_TAG $ENV{WikiExamplesTag})
-endif()
-
 itk_fetch_module(WikiExamples
   "A collection of examples that illustrate how to use ITK."
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKWikiExamples.git
-  GIT_TAG ${GIT_TAG}
+  GIT_TAG 07f0e1622ca3eeed8b9c5b9665b066b785af86e8
   )
