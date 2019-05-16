00439b74d8ec93438f73ee6e364983696ba01212
Dženan Zukić <dzenan.zukic@kitware.com>
2019-05-16 16:50:34 -0400

    ENH: updating remote modules

--- Modules/Remote/IOMeshSTL.remote.cmake
+++ Modules/Remote/IOMeshSTL.remote.cmake
@@ -3,5 +3,5 @@ itk_fetch_module(IOMeshSTL
   "This module contains classes for reading and writing QuadEdgeMeshes using
   the STL (STereoLithography)file format. https://hdl.handle.net/10380/3452"
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKIOMeshSTL.git
-  GIT_TAG 59c416c85e7fe5dd6f65e1737b3f89378ae5b787
+  GIT_TAG 532a442cd69395a8743688f161b0e9f3bc0cad21
   )
--- Modules/Remote/SphinxExamples.remote.cmake
+++ Modules/Remote/SphinxExamples.remote.cmake
@@ -2,5 +2,5 @@
 itk_fetch_module(SphinxExamples
   "This module builds the examples found at https://itk.org/ITKExamples/"
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKExamples.git
-  GIT_TAG 29e5ef35d28d85b9bd0d00960fcf9f3e7afb49b7
+  GIT_TAG 3966234dc803b269a8acad2dcdc8ef9ffbda4940
   )
