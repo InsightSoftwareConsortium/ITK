InsightSoftwareConsortium/ITK

commit 814419ae9f989c10c597a5e913166d3e8c949267

STYLE: Remove (deprecate) BoundingBox::GetCorners() + m_CornersContainer
Removed `GetCorners()` from `BoundingBox`, as it is superseded by the
new member function `ComputeCorners()`

Various issues of the old `GetCorners()` are solved by the new `ComputeCorners()`:

* GetCorners() is non-const
* GetCorners() has its own dynamically allocated data member,
  `BoundingBox::m_CornersContainer`, that should not be necessary
* GetCorners() does not make optimal use of the fact that the number of
  corners is known at compile-time
* GetCorners() returns a pointer, instead of the container itself,
  leaving it up to the user to check if the pointer is valid

Also removed `m_CornersContainer`, which was only being used by `GetCorners()`.

`GetCorners()` only remains available when `ITK_LEGACY_REMOVE=OFF`

Modules/Core/Common/include/itkBoundingBox.hxx
@@ -112,6 +112,7 @@ BoundingBox< TPointIdentifier, VPointDimension, TCoordRep, TPointsContainer >
   return result;
 }
 
+#if !defined(ITK_LEGACY_REMOVE)
 /** Compute and get the corners of the bounding box */
 template< typename TPointIdentifier, int VPointDimension,
           typename TCoordRep, typename TPointsContainer >
@@ -131,6 +132,7 @@ BoundingBox< TPointIdentifier, VPointDimension, TCoordRep, TPointsContainer >
 
   return m_CornersContainer.GetPointer();
 }
+#endif
 
 /** */
 template< typename TPointIdentifier, int VPointDimension,
@@ -139,7 +141,6 @@ BoundingBox< TPointIdentifier, VPointDimension, TCoordRep, TPointsContainer >
 ::BoundingBox():m_PointsContainer(nullptr)
 {
   m_Bounds.Fill(NumericTraits< CoordRepType >::ZeroValue());
-  m_CornersContainer = PointsContainer::New();
 }
 
 template< typename TPointIdentifier, int VPointDimension,

Modules/Core/Common/include/itkBoundingBox.h
@@ -127,7 +127,7 @@ public:
   std::array<PointType, NumberOfCorners> ComputeCorners() const;
 
   /** Compute and return the corners of the bounding box */
-  const PointsContainer * GetCorners();
+  itkLegacyMacro( const PointsContainer * GetCorners() );
 
   /** Method that actually computes bounding box. */
   bool ComputeBoundingBox() const;
@@ -191,7 +191,9 @@ protected:
 
 private:
   PointsContainerConstPointer m_PointsContainer;
-  PointsContainerPointer      m_CornersContainer;
+#if !defined(ITK_LEGACY_REMOVE)
+  PointsContainerPointer      m_CornersContainer{ PointsContainer::New() };
+#endif
   mutable BoundsArrayType     m_Bounds;
   mutable TimeStamp           m_BoundsMTime; // The last time the bounds
                                              // were computed.

commit 27133b9714de753b57613e02501530aa699ca4f0

COMP: follow up to 814419a (bounding box m_CornersContainer removal)
Enable compiling in ITK_LEGACY_REMOVE mode. The problem was
introduced by 814419ae9f989c10c597a5e913166d3e8c949267.

Modules/Core/Common/include/itkBoundingBox.hxx
@@ -390,6 +390,7 @@ BoundingBox< TPointIdentifier, VPointDimension, TCoordRep, TPointsContainer >
   // Connect the same points container into the clone
   clone->SetPoints(this->m_PointsContainer);
 
+#if !defined( ITK_LEGACY_REMOVE )
   // Copy the corners into the clone.
   clone->m_CornersContainer->clear();
 
@@ -404,6 +405,7 @@ BoundingBox< TPointIdentifier, VPointDimension, TCoordRep, TPointsContainer >
     dest.Value() = itr.Value();
     ++itr;
     }
+#endif
 
   // Copy the bounds into the clone
   for ( unsigned int i = 0; i < 2 * PointDimension; i++ )