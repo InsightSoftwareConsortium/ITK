f8c2621e62bf200231eb3e18cd6bfbcf0463f6ce
Niels Dekker <N.Dekker@lumc.nl>
2019-05-21 14:27:05 +0200

    STYLE: Rename ITK macro EXPECT_VECTOR_NEAR to ITK_EXPECT_VECTOR_NEAR
    Renamed the unit test macro `EXPECT_VECTOR_NEAR` to `ITK_EXPECT_VECTOR_NEAR`,
    to explicitly indicate that it is an ITK specific extension to GTest.

--- Modules/Core/TestKernel/test/itkGoogleTest.cxx
+++ Modules/Core/TestKernel/test/itkGoogleTest.cxx
@@ -36,28 +36,28 @@ TEST(GoogleTest,TypedefsAndConstructors_Dimension2) {
   pt1[1] = 2.2;
   const PointType pt2 = MakePoint(1.1, 2.2);
   EXPECT_TRUE(pt1 == pt2);
-  EXPECT_VECTOR_NEAR(pt1, pt2, 1e-10);
-  EXPECT_VECTOR_NEAR(pt1, MakePoint(1.1, 2.2), 1e-10);
+  ITK_EXPECT_VECTOR_NEAR(pt1, pt2, 1e-10);
+  ITK_EXPECT_VECTOR_NEAR(pt1, MakePoint(1.1, 2.2), 1e-10);
 
   VectorType vec1;
   vec1[0] = 1.1;
   vec1[1] = 2.2;
   const VectorType vec2 = MakeVector(1.1, 2.2);
   EXPECT_TRUE(vec1 == vec2);
-  EXPECT_VECTOR_NEAR(vec1, vec2, 1e-10);
-  EXPECT_VECTOR_NEAR(vec1, MakeVector(1.1, 2.2), 1e-10);
+  ITK_EXPECT_VECTOR_NEAR(vec1, vec2, 1e-10);
+  ITK_EXPECT_VECTOR_NEAR(vec1, MakeVector(1.1, 2.2), 1e-10);
 
   const IndexType idx1 = {{0,1}};
   const IndexType idx2 = MakeIndex(0,1);
   EXPECT_TRUE(idx1 == idx2);
-  EXPECT_VECTOR_NEAR(idx1, idx2, 1e-10);
-  EXPECT_VECTOR_NEAR(idx1, MakeIndex(0,1), 1e-10);
+  ITK_EXPECT_VECTOR_NEAR(idx1, idx2, 1e-10);
+  ITK_EXPECT_VECTOR_NEAR(idx1, MakeIndex(0,1), 1e-10);
 
   const SizeType sz1 = {{0u,1u}};
   const SizeType sz2 = MakeSize(0u,1u);
   EXPECT_TRUE(sz1 == sz2);
-  EXPECT_VECTOR_NEAR(sz1, sz2, 1e-10);
-  EXPECT_VECTOR_NEAR(sz1, MakeSize(0u,1u), 1e-10);
+  ITK_EXPECT_VECTOR_NEAR(sz1, sz2, 1e-10);
+  ITK_EXPECT_VECTOR_NEAR(sz1, MakeSize(0u,1u), 1e-10);
 }
 
 
@@ -72,8 +72,8 @@ TEST(GoogleTest,TypedefsAndConstructors_Dimension3) {
   pt1[2] = 3.3;
   const PointType pt2 = MakePoint(1.1, 2.2, 3.3);
   EXPECT_TRUE(pt1 == pt2);
-  EXPECT_VECTOR_NEAR(pt1, pt2, 1e-10);
-  EXPECT_VECTOR_NEAR(pt1,  MakePoint(1.1, 2.2, 3.3), 1e-10);
+  ITK_EXPECT_VECTOR_NEAR(pt1, pt2, 1e-10);
+  ITK_EXPECT_VECTOR_NEAR(pt1,  MakePoint(1.1, 2.2, 3.3), 1e-10);
 
   VectorType vec1;
   vec1[0] = 1.1;
@@ -81,16 +81,16 @@ TEST(GoogleTest,TypedefsAndConstructors_Dimension3) {
   vec1[2] = 3.3;
   const VectorType vec2 = MakeVector(1.1, 2.2, 3.3);
   EXPECT_TRUE(vec1 == vec2);
-  EXPECT_VECTOR_NEAR(vec1, vec2, 1e-10);
-  EXPECT_VECTOR_NEAR(vec1, MakeVector(1.1, 2.2, 3.3), 1e-10);
+  ITK_EXPECT_VECTOR_NEAR(vec1, vec2, 1e-10);
+  ITK_EXPECT_VECTOR_NEAR(vec1, MakeVector(1.1, 2.2, 3.3), 1e-10);
 
   const IndexType idx1 = {{0,1,2}};
   const IndexType idx2 = MakeIndex(0,1,2);
   EXPECT_TRUE(idx1 == idx2);
-  EXPECT_VECTOR_NEAR(idx1, idx2, 1e-10);
+  ITK_EXPECT_VECTOR_NEAR(idx1, idx2, 1e-10);
 
   const SizeType sz1 = {{0u,1u,2u}};
   const SizeType sz2 = MakeSize(0u,1u,2u);
   EXPECT_TRUE(sz1 == sz2);
-  EXPECT_VECTOR_NEAR(sz1, MakeSize(0u,1u,2u), 1e-10);
+  ITK_EXPECT_VECTOR_NEAR(sz1, MakeSize(0u,1u,2u), 1e-10);
 }
