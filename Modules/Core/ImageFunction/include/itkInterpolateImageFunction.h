3b644cc71241ca90c4bf067b69d62c20c2f6ddee
Bradley Lowekamp <blowekamp@mail.nih.gov>
2019-05-22 11:50:10 -0400

    BUG: Preserve Interpolate GetRadius behavior with ITKv4Compatibility
    Continue to use derived interpolator's GetRadius implementation when
    compatibility is enabled. When compatibility is enabled provide an
    default implementation of GetRadius which requires the whole image.

--- Modules/Core/ImageFunction/include/itkInterpolateImageFunction.h
+++ Modules/Core/ImageFunction/include/itkInterpolateImageFunction.h
@@ -132,7 +132,21 @@ public:
    * This defines the number of surrounding pixels required to interpolate at
    * a given point.
    */
+  #if defined(ITKV4_COMPATIBILITY)
+  virtual SizeType GetRadius() const
+  {
+    // if ITKv4 compatibility is enabled then set the radius to the
+    // largest by default.
+    const InputImageType* input = this->GetInputImage();
+    if ( !input )
+      {
+      itkExceptionMacro( "Input image required!" );
+      }
+    return input->GetLargestPossibleRegion().GetSize();
+  }
+  #else
   virtual SizeType GetRadius() const = 0;
+  #endif
 
 protected:
   InterpolateImageFunction()= default;
