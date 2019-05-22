23a771fe8eea9a6a0bd7878c6a2601ca84867ec4
Bradley Lowekamp <blowekamp@mail.nih.gov>
2019-05-22 14:24:13 -0400

    ENH: Add testing for CastImageFilter for more type conversions

--- Modules/Filtering/ImageFilterBase/test/itkCastImageFilterTest.cxx
+++ Modules/Filtering/ImageFilterBase/test/itkCastImageFilterTest.cxx
@@ -25,6 +25,8 @@
 #include "itkRandomImageSource.h"
 #include "itkVectorImage.h"
 #include "itkFloatingPointExceptions.h"
+#include "itkRGBPixel.h"
+#include "itkRGBAPixel.h"
 
 
 // Better name demanging for gcc
@@ -37,6 +39,23 @@
 #include <cxxabi.h>
 #include "itkMath.h"
 #endif
+
+
+
+// Compile time check
+template class itk::CastImageFilter<itk::Image<std::complex<float> >, itk::Image<std::complex<float> > >;
+template class itk::CastImageFilter<itk::Image<std::complex<double> >, itk::Image<std::complex<double> > >;
+template class itk::CastImageFilter<itk::Image<std::complex<float> >, itk::Image<std::complex<double> > >;
+
+
+template class itk::CastImageFilter<itk::Image<itk::RGBPixel<unsigned char> >, itk::Image<itk::RGBPixel<unsigned short> > >;
+template class itk::CastImageFilter<itk::Image<itk::RGBPixel<unsigned char> >, itk::Image<itk::Vector<float,3> > >;
+template class itk::CastImageFilter<itk::Image<itk::Vector<float,3> >, itk::Image<itk::RGBPixel<unsigned char> > >;
+template class itk::CastImageFilter<itk::Image<itk::RGBAPixel<unsigned char> >, itk::Image<itk::RGBAPixel<unsigned short> > >;
+template class itk::CastImageFilter<itk::Image<itk::RGBAPixel<unsigned char> >, itk::Image<itk::Vector<float,4> > >;
+template class itk::CastImageFilter<itk::Image<itk::Vector<float,4> >, itk::Image<itk::RGBAPixel<unsigned char> > >;
+
+template class itk::CastImageFilter<itk::VectorImage<short>, itk::VectorImage<double> >;
 
 template< typename T >
 std::string GetCastTypeName()
