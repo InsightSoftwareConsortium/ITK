76218623fd8499140508efb8df66f275ddec0097
Bryn Lloyd <lloyd@itis.swiss>
2019-06-04 14:01:53 +0200

    COMP: add FindTBB.cmake from VTK
    - this allows ITK to find TBB also on Windows (TBBConfig.cmake is not generated on Windows)
    - I tried to make it backwards compatible, i.e. use the current mechanism (find TBBConfig.cmake), and only if it fails use the FindTBB.cmake

--- Modules/ThirdParty/TBB/itk-module-init.cmake
+++ Modules/ThirdParty/TBB/itk-module-init.cmake
@@ -1,2 +1,11 @@
-find_package(TBB REQUIRED CONFIG) # must have TBBConfig.cmake, provided since version tbb2017_20170604oss
+find_package(TBB CONFIG) # must have TBBConfig.cmake, provided since version tbb2017_20170604oss
+
+if (NOT TBB_FOUND)
+  list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/CMake")
+  find_package(TBB)
+endif()
+if (NOT TBB_FOUND)
+  message(FATAL_ERROR "find_package(TBB) failed.")
+endif()
+
 get_target_property(TBB_INCLUDE_DIRS TBB::tbb INTERFACE_INCLUDE_DIRECTORIES)
