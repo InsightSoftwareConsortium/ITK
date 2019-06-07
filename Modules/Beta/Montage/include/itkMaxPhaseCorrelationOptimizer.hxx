fb68d33b1ac7146d000b9952982c96208d80f6a4
Dženan Zukić <dzenan.zukic@kitware.com>
2019-06-07 15:14:58 -0400

    BUG: cosine interpolation avoids NaNs for additional candidates

--- Modules/Beta/Montage/include/itkMaxPhaseCorrelationOptimizer.hxx
+++ Modules/Beta/Montage/include/itkMaxPhaseCorrelationOptimizer.hxx
@@ -347,7 +347,7 @@ MaxPhaseCorrelationOptimizer< TRegistrationMethod >
             break;
           case PeakInterpolationMethod::Cosine:
             ratio = ( y0 + y2 ) / ( 2 * y1 );
-            if ( m_MergePeaks ) // clip to -0.999... to 0.999... range
+            if ( m > 0 ) // clip to -0.999... to 0.999... range
               {
               ratio = std::min( ratio, 1.0 - std::numeric_limits< OffsetScalarType >::epsilon() );
               ratio = std::max( ratio, -1.0 + std::numeric_limits< OffsetScalarType >::epsilon() );
