584ce34e49b98b78a89468172ccd3ddcf8125af5
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2019-06-14 09:28:39 -0400

    BUG: Fix wrong variable names
    Fix wrong variable names in remote modules required ITK version update
    script.

--- Utilities/Maintenance/UpdateRequiredITKVersionInRemoteModules.sh
+++ Utilities/Maintenance/UpdateRequiredITKVersionInRemoteModules.sh
@@ -77,7 +77,7 @@ sed 's/.*\///; s/\^{}//')
 git_tag_label='ITKGitTag: '
 
 # Read the ITK git tag information
-curr_git_str=($(grep $git_tag_label $filename))
+curr_git_str=($(grep $git_tag_label $azure_pipelines_ci_filename))
 curr_git_tag=${curr_git_str[1]}
 
 # Sed the latest ITK git tag in the Azure pipelines config file
@@ -91,7 +91,7 @@ latest_git_tag=${latest_git_tag:1}
 git_install_req_tag_label='\install_requires=\['
 
 # Read the ITK install requirement git tag information
-git_install_req_tag_str=($(grep -A1 -P ${git_install_req_tag_label}$ $filename))
+git_install_req_tag_str=($(grep -A1 -P ${git_install_req_tag_label}$ $python_setup_filename))
 git_install_req_tag=${git_install_req_tag_str[1]}
 
 git_install_req_tag_arr=($(echo $git_install_req_tag | tr "=" " "))
@@ -103,7 +103,7 @@ sed -i "s/${curr_git_tag}/${latest_git_tag}/g" $python_setup_filename
 pckg_version_label='version'
 
 # Read the module Python package version tag information
-pckg_version_tag_str=($(grep -A1 -P ${pckg_version_label} $filename))
+pckg_version_tag_str=($(grep -A1 -P ${pckg_version_label} $python_setup_filename))
 
 # Strip the ending comma
 pckg_version_tag_str=${pckg_version_tag_str::-1}
@@ -122,4 +122,4 @@ new_pckg_major_version=$((pckg_major_version + 1))
 # Update to a new major version
 new_pckg_version="version='$new_pckg_major_version.0.0'"
 
-sed -i "s/${pckg_version_tag_str}/${new_pckg_version}/g" $filename
+sed -i "s/${pckg_version_tag_str}/${new_pckg_version}/g" $python_setup_filename
