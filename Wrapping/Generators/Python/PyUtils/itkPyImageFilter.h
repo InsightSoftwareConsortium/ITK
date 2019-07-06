dbfa3e882599bb087daf3adb5ec39e0a937768c8
Matt McCormick <matt.mccormick@kitware.com>
2019-07-06 17:12:08 -0400

    BUG: Remove Windows Python 2.7 hypot workaround
    This was a workaround for a bug with the compiler required to build
    Python 2.7 on Windows and Python 2.7. Since we do not support Python 2.7
    anymore, it is no longer required.

--- Wrapping/Generators/Python/PyUtils/itkPyImageFilter.h
+++ Wrapping/Generators/Python/PyUtils/itkPyImageFilter.h
@@ -24,9 +24,6 @@
 // The python header defines _POSIX_C_SOURCE without a preceding #undef
 #undef _POSIX_C_SOURCE
 #undef _XOPEN_SOURCE
-// For Python 2.7 hypot bug, see https://bugs.python.org/issue11566
-#include "PatchedPython27pyconfig.h"
-#include <Python.h>
 
 namespace itk
 {
