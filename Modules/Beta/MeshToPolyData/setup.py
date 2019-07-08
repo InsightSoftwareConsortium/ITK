7debee48206936541cfe90760f2a3d4f07e084de
Matt McCormick <matt.mccormick@kitware.com>
2019-07-07 22:10:42 -0400

    BUG: Python package dependency is only itk-core

--- Modules/Beta/MeshToPolyData/setup.py
+++ Modules/Beta/MeshToPolyData/setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
     name='itk-meshtopolydata',
-    version='0.2.0',
+    version='0.2.1',
     author='Insight Software Consortium',
     author_email='itk+community@discourse.itk.org',
     packages=['itk'],
@@ -44,6 +44,6 @@ setup(
     keywords='ITK InsightToolkit',
     url=r'https://itk.org/',
     install_requires=[
-        r'itk>=5.0.0.post1'
+        r'itk-core>=5.0.0.post1'
     ]
     )
