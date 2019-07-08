30f34488bcda67e82a5e590a01151b23df6558fd
Matt McCormick <matt.mccormick@kitware.com>
2019-07-07 23:19:15 -0400

    ENH: Bump version to 0.3.0

--- Modules/Beta/MeshToPolyData/setup.py
+++ Modules/Beta/MeshToPolyData/setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
     name='itk-meshtopolydata',
-    version='0.2.1',
+    version='0.3.0',
     author='Insight Software Consortium',
     author_email='itk+community@discourse.itk.org',
     packages=['itk'],
