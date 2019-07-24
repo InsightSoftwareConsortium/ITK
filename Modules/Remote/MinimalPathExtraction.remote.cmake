5de9da7eda86bb7141fe3a3eeb033377368ccdd9
Dženan Zukić <dzenan.zukic@kitware.com>
2019-07-24 09:35:03 -0400

    ENH: updating remote modules

--- Modules/Remote/MinimalPathExtraction.remote.cmake
+++ Modules/Remote/MinimalPathExtraction.remote.cmake
@@ -3,5 +3,5 @@ itk_fetch_module(MinimalPathExtraction
  "http://www.insight-journal.org/browse/publication/213
 "
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKMinimalPathExtraction.git
-  GIT_TAG d75aa9034bf6c92a4b80980ed835d8de5fcba2f0
+  GIT_TAG c00dbb883a44db59004210840d6c3f9deebaf92c
   )
--- Modules/Remote/SCIFIO.remote.cmake
+++ Modules/Remote/SCIFIO.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(SCIFIO
   "SCIFIO (Bioformats) ImageIO plugin for ITK"
   GIT_REPOSITORY ${git_protocol}://github.com/scifio/scifio-imageio.git
-  GIT_TAG f27bc5552c68ec11bb22e48e082a8e492d4489af
+  GIT_TAG 5081a00976cd67c782715a6df2bd4db1071eac3c
   )
