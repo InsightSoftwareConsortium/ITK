322a6070f3b7bad4f8234cb9fb1aa29714458772
Dženan Zukić <dzenan.zukic@kitware.com>
2019-08-21 11:18:57 -0400

    ENH: updating remote modules

--- Modules/Remote/Montage.remote.cmake
+++ Modules/Remote/Montage.remote.cmake
@@ -2,5 +2,5 @@
 itk_fetch_module(Montage
 "Reconstruction of 3D volumetric dataset from a collection of 2D slices"
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKMontage.git
-  GIT_TAG e6ba4d41922073abc5e6345aec69e015300ce440
+  GIT_TAG d48b400cea8d96f1db51829f0d3a6596e4da2c78
   )
