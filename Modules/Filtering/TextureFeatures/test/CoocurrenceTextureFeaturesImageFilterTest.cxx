InsightSoftwareConsortium/ITKTextureFeatures

STYLE: Add ITK prefix to testing macros

test/CoocurrenceTextureFeaturesImageFilterTest.cxx
@@ -81,7 +81,7 @@ int CoocurrenceTextureFeaturesImageFilterTest( int argc, char *argv[] )
     filter->SetNeighborhoodRadius( hood.GetRadius() );
     }
 
-  TRY_EXPECT_NO_EXCEPTION( filter->Update() );
+  ITK_TRY_EXPECT_NO_EXCEPTION( filter->Update() );
 
   // Create and set up a writer
   using WriterType = itk::ImageFileWriter< OutputImageType >;
@@ -89,7 +89,7 @@ int CoocurrenceTextureFeaturesImageFilterTest( int argc, char *argv[] )
   writer->SetFileName( argv[3] );
   writer->SetInput( filter->GetOutput() );
 
-  TRY_EXPECT_NO_EXCEPTION( writer->Update() );
+  ITK_TRY_EXPECT_NO_EXCEPTION( writer->Update() );
 
 
   std::cout << "Test finished." << std::endl;

test/CoocurrenceTextureFeaturesImageFilterTestWithoutMask.cxx
@@ -80,7 +80,7 @@ int CoocurrenceTextureFeaturesImageFilterTestWithoutMask( int argc, char *argv[]
     filter->SetNeighborhoodRadius( hood.GetRadius() );
     }
 
-  TRY_EXPECT_NO_EXCEPTION( filter->Update() );
+  ITK_TRY_EXPECT_NO_EXCEPTION( filter->Update() );
 
   // Create and set up a writer
   using WriterType = itk::ImageFileWriter< OutputImageType >;
@@ -88,7 +88,7 @@ int CoocurrenceTextureFeaturesImageFilterTestWithoutMask( int argc, char *argv[]
   writer->SetFileName( argv[2] );
   writer->SetInput( filter->GetOutput() );
 
-  TRY_EXPECT_NO_EXCEPTION( writer->Update() );
+  ITK_TRY_EXPECT_NO_EXCEPTION( writer->Update() );
 
 
   std::cout << "Test finished." << std::endl;

test/RunLengthTextureFeaturesImageFilterTestSeparateFeatures.cxx
@@ -96,7 +96,7 @@ int RunLengthTextureFeaturesImageFilterTestSeparateFeatures( int argc, char *arg
     filter->SetNeighborhoodRadius( hood.GetRadius() );
     }
 
-  TRY_EXPECT_NO_EXCEPTION( filter->Update() );
+  ITK_TRY_EXPECT_NO_EXCEPTION( filter->Update() );
 
 
   using FeatureImageType = itk::Image< OutputPixelComponentType, ImageDimension >;
@@ -119,7 +119,7 @@ int RunLengthTextureFeaturesImageFilterTestSeparateFeatures( int argc, char *arg
     writer->SetFileName( outputFilename + "_" + s + ".nrrd" );
     writer->SetInput( indexSelectionFilter->GetOutput() );
 
-    TRY_EXPECT_NO_EXCEPTION( writer->Update() );
+    ITK_TRY_EXPECT_NO_EXCEPTION( writer->Update() );
     }
 
 

test/RunLengthTextureFeaturesImageFilterTestVectorImageSeparateFeatures.cxx
@@ -96,7 +96,7 @@ int RunLengthTextureFeaturesImageFilterTestVectorImageSeparateFeatures( int argc
     filter->SetNeighborhoodRadius( hood.GetRadius() );
     }
 
-  TRY_EXPECT_NO_EXCEPTION( filter->Update() );
+  ITK_TRY_EXPECT_NO_EXCEPTION( filter->Update() );
 
 
   using FeatureImageType = itk::Image< OutputPixelComponentType, ImageDimension >;
@@ -119,7 +119,7 @@ int RunLengthTextureFeaturesImageFilterTestVectorImageSeparateFeatures( int argc
     writer->SetFileName( outputFilename + "_" + s + ".nrrd" );
     writer->SetInput( indexSelectionFilter->GetOutput() );
 
-    TRY_EXPECT_NO_EXCEPTION( writer->Update() );
+    ITK_TRY_EXPECT_NO_EXCEPTION( writer->Update() );
     }
 
 

InsightSoftwareConsortium/ITK

Modules/Filtering/TextureFeatures/test/CoocurrenceTextureFeaturesImageFilterTest.cxx
@@ -80,7 +80,7 @@ CoocurrenceTextureFeaturesImageFilterTest(int argc, char * argv[])
     filter->SetNeighborhoodRadius(hood.GetRadius());
   }
 
-  TRY_EXPECT_NO_EXCEPTION(filter->Update());
+  ITK_TRY_EXPECT_NO_EXCEPTION(filter->Update());
 
   // Create and set up a writer
   using WriterType = itk::ImageFileWriter<OutputImageType>;
@@ -88,7 +88,7 @@ CoocurrenceTextureFeaturesImageFilterTest(int argc, char * argv[])
   writer->SetFileName(argv[3]);
   writer->SetInput(filter->GetOutput());
 
-  TRY_EXPECT_NO_EXCEPTION(writer->Update());
+  ITK_TRY_EXPECT_NO_EXCEPTION(writer->Update());
 
 
   std::cout << "Test finished." << std::endl;

Modules/Filtering/TextureFeatures/test/CoocurrenceTextureFeaturesImageFilterTestWithoutMask.cxx
@@ -78,7 +78,7 @@ CoocurrenceTextureFeaturesImageFilterTestWithoutMask(int argc, char * argv[])
     filter->SetNeighborhoodRadius(hood.GetRadius());
   }
 
-  TRY_EXPECT_NO_EXCEPTION(filter->Update());
+  ITK_TRY_EXPECT_NO_EXCEPTION(filter->Update());
 
   // Create and set up a writer
   using WriterType = itk::ImageFileWriter<OutputImageType>;
@@ -86,7 +86,7 @@ CoocurrenceTextureFeaturesImageFilterTestWithoutMask(int argc, char * argv[])
   writer->SetFileName(argv[2]);
   writer->SetInput(filter->GetOutput());
 
-  TRY_EXPECT_NO_EXCEPTION(writer->Update());
+  ITK_TRY_EXPECT_NO_EXCEPTION(writer->Update());
 
 
   std::cout << "Test finished." << std::endl;

Modules/Filtering/TextureFeatures/test/RunLengthTextureFeaturesImageFilterTestSeparateFeatures.cxx
@@ -95,7 +95,7 @@ RunLengthTextureFeaturesImageFilterTestSeparateFeatures(int argc, char * argv[])
     filter->SetNeighborhoodRadius(hood.GetRadius());
   }
 
-  TRY_EXPECT_NO_EXCEPTION(filter->Update());
+  ITK_TRY_EXPECT_NO_EXCEPTION(filter->Update());
 
 
   using FeatureImageType = itk::Image<OutputPixelComponentType, ImageDimension>;
@@ -117,7 +117,7 @@ RunLengthTextureFeaturesImageFilterTestSeparateFeatures(int argc, char * argv[])
     writer->SetFileName(outputFilename + "_" + s + ".nrrd");
     writer->SetInput(indexSelectionFilter->GetOutput());
 
-    TRY_EXPECT_NO_EXCEPTION(writer->Update());
+    ITK_TRY_EXPECT_NO_EXCEPTION(writer->Update());
   }
 
 

Modules/Filtering/TextureFeatures/test/RunLengthTextureFeaturesImageFilterTestVectorImageSeparateFeatures.cxx
@@ -95,7 +95,7 @@ RunLengthTextureFeaturesImageFilterTestVectorImageSeparateFeatures(int argc, cha
     filter->SetNeighborhoodRadius(hood.GetRadius());
   }
 
-  TRY_EXPECT_NO_EXCEPTION(filter->Update());
+  ITK_TRY_EXPECT_NO_EXCEPTION(filter->Update());
 
 
   using FeatureImageType = itk::Image<OutputPixelComponentType, ImageDimension>;
@@ -117,7 +117,7 @@ RunLengthTextureFeaturesImageFilterTestVectorImageSeparateFeatures(int argc, cha
     writer->SetFileName(outputFilename + "_" + s + ".nrrd");
     writer->SetInput(indexSelectionFilter->GetOutput());
 
-    TRY_EXPECT_NO_EXCEPTION(writer->Update());
+    ITK_TRY_EXPECT_NO_EXCEPTION(writer->Update());
   }
 
 