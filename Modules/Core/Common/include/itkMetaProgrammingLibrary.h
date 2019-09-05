68a9840d6ee17a4b3bfaadfc8a7217c4c47d313f
maekclena <maekclena@users.noreply.github.com>
2019-09-06 01:11:34 +0200

    BUG: Re-implement is_static_castable to be compatible with gcc 8 (#1227)

--- Modules/Core/Common/include/itkMetaProgrammingLibrary.h
+++ Modules/Core/Common/include/itkMetaProgrammingLibrary.h
@@ -215,15 +215,10 @@ struct Not : NotC<TF::Value>
  *
  * Identifies if "static_cast<TToType>(TFromType)" can be done.
  */
-template <typename TFromType, typename TToType, typename TNoDeclType = TToType>
-struct is_static_castable : std::false_type
-{};
-/// \cond SPECIALIZATION_IMPLEMENTATION
 template <typename TFromType, typename TToType>
-struct is_static_castable<TFromType, TToType, decltype(static_cast<TToType>(std::declval<TFromType>()))>
-  : std::true_type
-{};
-/// \endcond
+using is_static_castable = std::integral_constant<bool,
+                                                  std::is_constructible<TToType, TFromType>::value ||
+                                                    std::is_convertible<TFromType, TToType>::value>;
 } // namespace mpl
 
 
