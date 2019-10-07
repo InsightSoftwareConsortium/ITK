b0991df3e864b1fe9c92e2cb8cb4474b2c1e3ee2
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2019-10-07 13:29:41 +0200

    COMP: Update Eigen3 to top of branch 3.3
    Top is commit:
    [79c4574](https://bitbucket.org/eigen/eigen/commits/79c45742ea21cbb368742d441c31ecc64c005ec4)
    
    Also rebase all Kitware changes on top of eigen to facilitate updates
    
    This update discovered bug in branch3.3 in eigen, reported and solved
    here:
    http://eigen.tuxfamily.org/bz/show_bug.cgi?id=1761
    The fix is included in this update.

--- Modules/ThirdParty/Eigen3/UpdateFromUpstream.sh
+++ Modules/ThirdParty/Eigen3/UpdateFromUpstream.sh
@@ -10,7 +10,7 @@ readonly subtree="Modules/ThirdParty/Eigen3/src/itkeigen"
 # readonly repo="https://github.com/eigenteam/eigen-git-mirror"
 # readonly repo="https://gitlab.kitware.com/third-party/eigen.git"
 readonly repo="https://gitlab.kitware.com/phcerdan/eigen.git"
-readonly tag="for/itk"
+readonly tag="for/itk-eigen-3.3"
 readonly paths="
 Eigen/Cholesky
 Eigen/CholmodSupport
