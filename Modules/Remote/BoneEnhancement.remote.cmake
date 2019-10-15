2dd90d2941dd0a5991e910a329f499e5a0f41cfa
Dženan Zukić <dzenan.zukic@kitware.com>
2019-10-15 16:31:35 -0400

    ENH: adding BoneEnhancement remote module

---
+++ Modules/Remote/BoneEnhancement.remote.cmake
@@ -0,0 +1,5 @@
+itk_fetch_module(BoneEnhancement
+  "Various filters for enhancing cortical bones in quantitative computed tomography."
+  GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKBoneEnhancement.git
+  GIT_TAG 00f62b474a74079d04397bad881b080743f41b9d
+)
