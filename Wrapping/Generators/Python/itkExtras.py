c9d8271d0c813c08dfb35310233d0c3ad69a094c
Matt McCormick <matt.mccormick@kitware.com>
2019-10-22 14:41:53 -0400

    BUG: Wrap new enum classes
    Following the transition to uint8_t based C++11 strongly typed enum's,
    these are now wrapped.
    
      https://github.com/InsightSoftwareConsortium/ITK/pull/1061
    
    See also:
    
      7.2.13 Strongly typed enumerations
      http://www.swig.org/Doc4.0/CPlusPlus11.html

--- Wrapping/Generators/Python/itkExtras.py
+++ Wrapping/Generators/Python/itkExtras.py
@@ -552,7 +552,7 @@ def imread(filename, pixel_type=None, fallback_only=False):
         increase_dimension=False
         kwargs={'FileName':filename}
     if pixel_type:
-        imageIO = itk.ImageIOFactory.CreateImageIO(io_filename, itk.ImageIOFactory.ReadMode)
+        imageIO = itk.ImageIOFactory.CreateImageIO(io_filename, itk.ImageIOFactory.FileModeType_ReadMode)
         if not imageIO:
             raise RuntimeError("No ImageIO is registered to handle the given file.")
         imageIO.SetFileName(io_filename)
@@ -611,7 +611,7 @@ def meshread(filename, pixel_type=None, fallback_only=False):
     increase_dimension=False
     kwargs={'FileName':filename}
     if pixel_type:
-        meshIO = itk.MeshIOFactory.CreateMeshIO(io_filename, itk.MeshIOFactory.ReadMode)
+        meshIO = itk.MeshIOFactory.CreateMeshIO(io_filename, itk.MeshIOFactory.FileModeType_ReadMode)
         if not meshIO:
             raise RuntimeError("No MeshIO is registered to handle the given file.")
         meshIO.SetFileName(io_filename)
