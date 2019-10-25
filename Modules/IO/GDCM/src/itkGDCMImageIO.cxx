InsightSoftwareConsortium/ITK

commit c074bc5aba60d5371cc6c484939f5c6e049ab03c

BUG: Always convert YBR_FULL to RGB
itk::Image do not carry color space information. So convert from YBR_FULL to
RGB, even if it introduce a small loss. Loss is visually imperceptible, should
only impact quantitative analysis.

Modules/IO/GDCM/src/itkGDCMImageIO.cxx
@@ -41,6 +41,7 @@
 #include "gdcmImageHelper.h"
 #include "gdcmFileExplicitFilter.h"
 #include "gdcmImageChangeTransferSyntax.h"
+#include "gdcmImageChangePhotometricInterpretation.h"
 #include "gdcmDataSetHelper.h"
 #include "gdcmStringFilter.h"
 #include "gdcmImageApplyLookupTable.h"
@@ -300,6 +301,18 @@ GDCMImageIO::Read(void * pointer)
     ialut.Apply();
     image = ialut.GetOutput();
     len *= 3;
+  }
+  else if (pi == gdcm::PhotometricInterpretation::YBR_FULL ||
+           pi == gdcm::PhotometricInterpretation::YBR_FULL_422)
+  {
+    // ITK does not carry color space associated with an image. It is pretty
+    // much assumed that color image is expressed in RGB.
+    gdcm::ImageChangePhotometricInterpretation icpi;
+    icpi.SetInput(image);
+    icpi.SetPhotometricInterpretation( gdcm::PhotometricInterpretation::RGB );
+    icpi.Change();
+    itkWarningMacro(<< "Converting from integer YBR to integer RGB is a lossy operation.");
+    image = icpi.GetOutput();
   }
 
   if (!image.GetBuffer((char *)pointer))

commit 6667dfa0164a6c3bc7007b079140ae4bc9deb5d1

BUG: Properly handle JPEGBaseline1 DICOM files
A bug was introduced in :

c074bc5aba BUG: Always convert YBR_FULL to RGB

The ImageChangePhotometricInterpretation filter is too simple and does not work
on compressed stream. Add an explicit decompression step to make sure
decompression from YBR_FULL_422 to YBR_FULL (JPEGBaseline1) happen in the
background.

Modules/IO/GDCM/src/itkGDCMImageIO.cxx
@@ -283,6 +283,17 @@ GDCMImageIO::Read(void * pointer)
 #endif
   SizeValueType len = image.GetBufferLength();
 
+  // Decompress the Pixel Data buffer when needed. This is required here in the
+  // pipeline to make sure to decompress JPEGBaseline1 into YBR_FULL.
+  if( image.GetTransferSyntax().IsEncapsulated() )
+  {
+    gdcm::ImageChangeTransferSyntax icts;
+    icts.SetInput(image);
+    icts.SetTransferSyntax( gdcm::TransferSyntax::ImplicitVRLittleEndian );
+    icts.Change();
+    image = icts.GetOutput();
+  }
+
   // I think ITK only allow RGB image by pixel (and not by plane)
   if (image.GetPlanarConfiguration() == 1)
   {
@@ -302,8 +313,7 @@ GDCMImageIO::Read(void * pointer)
     image = ialut.GetOutput();
     len *= 3;
   }
-  else if (pi == gdcm::PhotometricInterpretation::YBR_FULL ||
-           pi == gdcm::PhotometricInterpretation::YBR_FULL_422)
+  else if (pi == gdcm::PhotometricInterpretation::YBR_FULL)
   {
     // ITK does not carry color space associated with an image. It is pretty
     // much assumed that color image is expressed in RGB.