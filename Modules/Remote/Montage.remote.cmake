bc8c9bdff73be31ef072984ba18e75188c563fe4
Dženan Zukić <dzenan.zukic@kitware.com>
2019-10-29 14:36:30 -0400

    ENH: update remote modules Montage and Examples

--- Modules/Remote/Montage.remote.cmake
+++ Modules/Remote/Montage.remote.cmake
@@ -2,5 +2,5 @@
 itk_fetch_module(Montage
 "Reconstruction of 3D volumetric dataset from a collection of 2D slices"
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKMontage.git
-  GIT_TAG f7b6eaf99a1a7f49a874be25e91bb423c285a8f6
+  GIT_TAG d6544075a0838d6df2fb14b566522c3fec2c0876
   )
--- Modules/Remote/SphinxExamples.remote.cmake
+++ Modules/Remote/SphinxExamples.remote.cmake
@@ -2,5 +2,5 @@
 itk_fetch_module(SphinxExamples
   "This module builds the examples found at https://itk.org/ITKExamples/"
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKExamples.git
-  GIT_TAG 850df7885f570eb70d00dfe978922b24e910644f
+  GIT_TAG 74cf200c3cb8ae7fe218432e24eaffd5ad05b3a5
   )
