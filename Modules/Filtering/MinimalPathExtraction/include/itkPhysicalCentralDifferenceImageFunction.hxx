4e5ece1dc7e9ac942566f0875d16c650cef2cc21
Matt McCormick <matt.mccormick@kitware.com>
2019-10-30 11:58:44 -0400

    STYLE: Update license header

--- Modules/Filtering/MinimalPathExtraction/include/itkPhysicalCentralDifferenceImageFunction.hxx
+++ Modules/Filtering/MinimalPathExtraction/include/itkPhysicalCentralDifferenceImageFunction.hxx
@@ -1,19 +1,20 @@
 /*=========================================================================
-
-  Program:   Insight Segmentation & Registration Toolkit
-  Module:    $RCSfile: itkPhysicalCentralDifferenceImageFunction.hxx,v $
-  Language:  C++
-  Date:      $Date$
-  Version:   $Revision$
-
-  Copyright (c) Insight Software Consortium. All rights reserved.
-  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
-
-     This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-     PURPOSE.  See the above copyright notices for more information.
-
-=========================================================================*/
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
 #ifndef itkPhysicalCentralDifferenceImageFunction_hxx
 #define itkPhysicalCentralDifferenceImageFunction_hxx
 
@@ -23,9 +24,6 @@ namespace itk
 {
 
 
-/**
- * Constructor
- */
 template <class TInputImage, class TCoordRep>
 PhysicalCentralDifferenceImageFunction<TInputImage, TCoordRep>::PhysicalCentralDifferenceImageFunction()
 {
@@ -33,9 +31,6 @@ PhysicalCentralDifferenceImageFunction<TInputImage, TCoordRep>::PhysicalCentralD
 }
 
 
-/**
- *
- */
 template <class TInputImage, class TCoordRep>
 void
 PhysicalCentralDifferenceImageFunction<TInputImage, TCoordRep>::PrintSelf(std::ostream & os, Indent indent) const
@@ -44,9 +39,6 @@ PhysicalCentralDifferenceImageFunction<TInputImage, TCoordRep>::PrintSelf(std::o
 }
 
 
-/**
- *
- */
 template <class TInputImage, class TCoordRep>
 typename PhysicalCentralDifferenceImageFunction<TInputImage, TCoordRep>::OutputType
 PhysicalCentralDifferenceImageFunction<TInputImage, TCoordRep>::Evaluate(const PointType & point) const
@@ -72,7 +64,6 @@ PhysicalCentralDifferenceImageFunction<TInputImage, TCoordRep>::Evaluate(const P
 
   return (derivative);
 }
-
 
 } // end namespace itk
 
--- Modules/Filtering/MinimalPathExtraction/include/itkSingleImageCostFunction.hxx
+++ Modules/Filtering/MinimalPathExtraction/include/itkSingleImageCostFunction.hxx
@@ -1,19 +1,20 @@
 /*=========================================================================
-
-  Program:   Insight Segmentation & Registration Toolkit
-  Module:    $RCSfile: itkSingleImageCostFunction.hxx,v $
-  Language:  C++
-  Date:      $Date$
-  Version:   $Revision$
-
-  Copyright (c) Insight Software Consortium. All rights reserved.
-  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
-
-     This software is distributed WITHOUT ANY WARRANTY; without even
-     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-     PURPOSE.  See the above copyright notices for more information.
-
-=========================================================================*/
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
 #ifndef itkSingleImageCostFunction_hxx
 #define itkSingleImageCostFunction_hxx
 
@@ -23,7 +24,7 @@
 namespace itk
 {
 
-template <class TImage>
+template <typename TImage>
 SingleImageCostFunction<TImage>::SingleImageCostFunction()
 {
   m_Image = nullptr;                 // Provided by user
@@ -33,7 +34,7 @@ SingleImageCostFunction<TImage>::SingleImageCostFunction()
 }
 
 
-template <class TImage>
+template <typename TImage>
 void
 SingleImageCostFunction<TImage>::Initialize()
 {
@@ -75,7 +76,7 @@ SingleImageCostFunction<TImage>::Initialize()
 /*
  * Get the value by interpolating the underlying image.
  */
-template <class TImage>
+template <typename TImage>
 typename SingleImageCostFunction<TImage>::MeasureType
 SingleImageCostFunction<TImage>::GetValue(const ParametersType & parameters) const
 {
@@ -102,7 +103,7 @@ SingleImageCostFunction<TImage>::GetValue(const ParametersType & parameters) con
 /*
  * Get the derivative by applying the gradient image function.
  */
-template <class TImage>
+template <typename TImage>
 void
 SingleImageCostFunction<TImage>::GetDerivative(const ParametersType & parameters, DerivativeType & derivative) const
 {
@@ -146,7 +147,7 @@ SingleImageCostFunction<TImage>::GetDerivative(const ParametersType & parameters
 /*
  * PrintSelf
  */
-template <class TImage>
+template <typename TImage>
 void
 SingleImageCostFunction<TImage>::PrintSelf(std::ostream & os, Indent indent) const
 {
