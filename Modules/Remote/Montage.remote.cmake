cd8e827c87c6fd44bb37fd218fb3979a917e2a0f
Dženan Zukić <dzenan.zukic@kitware.com>
2019-11-12 17:36:45 -0500

    ENH: updating Cuberille and Montage remote modules

--- Modules/Remote/Montage.remote.cmake
+++ Modules/Remote/Montage.remote.cmake
@@ -2,5 +2,5 @@
 itk_fetch_module(Montage
 "Reconstruction of 3D volumetric dataset from a collection of 2D slices"
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKMontage.git
-  GIT_TAG d6544075a0838d6df2fb14b566522c3fec2c0876
+  GIT_TAG 62ed2415126ad824ba519e36a94544aa7afb9e37
   )
