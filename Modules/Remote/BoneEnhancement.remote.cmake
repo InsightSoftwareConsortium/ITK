c71fe7d6311a08cc867cf12d835d22e385475d2b
Dženan Zukić <dzenan.zukic@kitware.com>
2019-11-25 14:49:23 -0500

    ENH: update BoneEnhancement remote module
    Some of the filters are now streamable,
    greatly reducing main filter's memory consumption.

--- Modules/Remote/BoneEnhancement.remote.cmake
+++ Modules/Remote/BoneEnhancement.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(BoneEnhancement
   "Various filters for enhancing cortical bones in quantitative computed tomography."
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKBoneEnhancement.git
-  GIT_TAG 22bfcc50561c174fd5942e6ea817a54fff89b6f3
+  GIT_TAG 872ac188933b44ee84986e984ce73d53c2c4ef5f
 )
