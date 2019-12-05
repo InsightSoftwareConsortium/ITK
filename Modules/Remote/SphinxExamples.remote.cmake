18f3ad1c73a63875437110df5c4c3fe8b0e9f701
Dženan Zukić <dzenan.zukic@kitware.com>
2019-12-05 10:13:26 -0500

    ENH: update ITKExamples remote module version

--- Modules/Remote/SphinxExamples.remote.cmake
+++ Modules/Remote/SphinxExamples.remote.cmake
@@ -2,5 +2,5 @@
 itk_fetch_module(SphinxExamples
   "This module builds the examples found at https://itk.org/ITKExamples/"
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKExamples.git
-  GIT_TAG 29b93b310bff5d68eec3235cb3f067b631498de5
+  GIT_TAG a3799deceb572852281d7294bbb5f7a6dc0c0117
   )
