c5245f4aef2eb9777dea7df342a54f41ee185c7a
Matt McCormick <matt.mccormick@kitware.com>
2019-12-22 22:20:48 -0500

    ENH: Use a script to generate the release changelog

--- Utilities/Maintenance/AuthorsChangesSince.py
+++ Utilities/Maintenance/AuthorsChangesSince.py
@@ -76,20 +76,33 @@ def remote_tag(remote_spec):
     print(remote_spec)
     sys.exit(1)
 
+scratch_dir = Path('/tmp/AuthorsChangesSince')
+if not scratch_dir.exists():
+    os.makedirs(scratch_dir)
 
-# ITK Repository
-update_previous_authors(revision)
-update_recent_authors(revision + '..')
-update_authors_with_email(revision + '..')
-
+changelog_file = scratch_dir / 'Changelog.txt'
+with open(changelog_file, 'w') as fp:
+    fp.write('')
+def write_changelog(repo_name, git_revision):
+    log = subprocess.check_output('git shortlog --topo-order --no-merges {0}'.format(git_revision), shell=True).decode('utf-8')
+    with open(changelog_file, 'a') as fp:
+        fp.write('{0} Changes Since {1}\n'.format(repo_name, revision))
+        fp.write('---------------------------------------------\n\n')
+        fp.write('```\n')
+        fp.write(log)
+        fp.write('```\n')
+        fp.write('\n\n')
 
 revision_time = subprocess.check_output('git show -s --format="%ci" {0}^{{commit}}'.format(revision), shell=True)
 revision_time = revision_time.decode('utf-8').strip()
 print('Revision time: ' + revision_time + '\n')
 itk_dir = Path(os.path.dirname(os.path.abspath(__file__))) / '..' / '..'
-scratch_dir = Path('/tmp/authors-since')
-if not scratch_dir.exists():
-    os.makedirs(scratch_dir)
+
+# ITK Repository
+update_previous_authors(revision)
+update_recent_authors(revision + '..')
+update_authors_with_email(revision + '..')
+write_changelog('ITK', revision + '..')
 
 # ITKExamples Repository
 os.chdir(scratch_dir)
@@ -100,6 +113,7 @@ os.chdir(examples_dir)
 update_previous_authors('--until="{0}"'.format(revision_time))
 update_recent_authors('--since="{0}"'.format(revision_time))
 update_authors_with_email('--since="{0}"'.format(revision_time))
+write_changelog('ITK Examples', '--since="{0}"'.format(revision_time))
 
 # ITKSoftwareGuide Repository
 os.chdir(scratch_dir)
@@ -110,22 +124,33 @@ os.chdir(examples_dir)
 update_previous_authors('--until="{0}"'.format(revision_time))
 update_recent_authors('--since="{0}"'.format(revision_time))
 update_authors_with_email('--since="{0}"'.format(revision_time))
+write_changelog('ITK Software Guide', '--since="{0}"'.format(revision_time))
 
 # Remote modules
 os.chdir(itk_dir)
 changed_remotes = subprocess.check_output('git diff-index --diff-filter=AM --name-only {0} -- Modules/Remote/'.format(revision), shell=True).decode('utf-8').strip()
+with open(changelog_file, 'a') as fp:
+    fp.write('Remote Module Changes Since {0}\n'.format(revision))
+    fp.write('---------------------------------------------\n\n')
 print('Remote module:')
 for remote in changed_remotes.split():
-    print(remote)
+    module_name = remote.split('/')[-1].split('.')[0]
+    if module_name == 'SphinxExamples':
+        continue
+    print(module_name)
     os.chdir(itk_dir)
 
     # The remote file could have been added or its name changed. Use the oldest
     # commit since revision with the current name.
     old_commit = subprocess.check_output('git rev-list {0}.. -- {1}'.format(revision, remote),
             shell=True).decode('utf-8').split()[-1]
+    try:
+        remote_spec = subprocess.check_output('git show {0}^:{1}'.format(old_commit,
+            remote), shell=True).decode('utf-8')
+    except subprocess.CalledProcessError:
+        remote_spec = subprocess.check_output('git show {0}:{1}'.format(old_commit,
+            remote), shell=True).decode('utf-8')
 
-    remote_spec = subprocess.check_output('git show {0}:{1}'.format(old_commit,
-        remote), shell=True).decode('utf-8')
     remote_old_tag = remote_tag(remote_spec)
 
     remote_spec = subprocess.check_output('git show HEAD:{1}'.format(revision,
@@ -143,6 +168,14 @@ for remote in changed_remotes.split():
     update_recent_authors('{0}..{1}'.format(remote_old_tag, remote_new_tag))
     update_authors_with_email('{0}..{1}'.format(remote_old_tag, remote_new_tag))
 
+    log = subprocess.check_output('git shortlog --topo-order --no-merges {0}..{1}'.format(remote_old_tag, remote_new_tag), shell=True).decode('utf-8')
+    with open(changelog_file, 'a') as fp:
+        fp.write('*{0}*:\n'.format(module_name))
+        fp.write('```\n')
+        fp.write(log)
+        fp.write('```\n')
+        fp.write('\n')
+
 os.chdir(scratch_dir)
 
 recent_authors = remove_maintainers(recent_authors)
@@ -154,10 +187,10 @@ new_authors = recent_authors.difference(previous_authors)
 print('\n\nPrevious authors: ' + str(len(previous_authors)))
 print('Recent authors: ' + str(len(recent_authors)))
 print('\nNew authors: ' + str(len(new_authors)))
-with open(scratch_dir / 'new_authors.txt', 'w') as fp:
+with open(scratch_dir / 'NewAuthors.txt', 'w') as fp:
     for author in new_authors:
         sys.stdout.write(author + ', ')
-        fp.write(author + ',')
+        fp.write(author + ', ')
 
 recent_authors_with_email = []
 for author in authors_with_email:
@@ -165,25 +198,25 @@ for author in authors_with_email:
         if author.find(an) != -1:
             recent_authors_with_email.append(author)
 print('\n\nRecent with emails:')
-with open(scratch_dir / 'recent_with_emails.txt', 'w') as fp:
+with open(scratch_dir / 'RecentWithEmails.txt', 'w') as fp:
     for author in recent_authors_with_email:
         sys.stdout.write(author + ', ')
         fp.write(author + ', ')
 
 print('\n\nWith emails oneline:')
-with open(scratch_dir / 'with_emails_oneline.txt', 'w') as fp:
+with open(scratch_dir / 'WithEmailsOneline.txt', 'w') as fp:
     for author in authors_with_email:
         sys.stdout.write(author + '\n')
         fp.write(author + '\n')
 
 print('\n\nNames:')
-with open(scratch_dir / 'names.txt', 'w') as fp:
+with open(scratch_dir / 'Names.txt', 'w') as fp:
     for author in authors_with_email:
         sys.stdout.write(author.split('<')[0] + '\n')
         fp.write(author.split('<')[0] + '\n')
 
 print('\n\nEmails:')
-with open(scratch_dir / 'emails.txt', 'w') as fp:
+with open(scratch_dir / 'Emails.txt', 'w') as fp:
     for author in authors_with_email:
         sys.stdout.write(author.split('<')[1][:-1] + '\n')
         fp.write(author.split('<')[1][:-1] + '\n')
