81035432e3cc24976e02f7a5176acfb631c178ef
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2020-01-09 22:21:46 +0100

    ENH: latest version of remote module RTK

--- Modules/Remote/RTK.remote.cmake
+++ Modules/Remote/RTK.remote.cmake
@@ -2,5 +2,5 @@
 itk_fetch_module(RTK
     "Reconstruction Toolkit (RTK) http://www.openrtk.org/"
   GIT_REPOSITORY ${git_protocol}://github.com/SimonRit/RTK.git
-  GIT_TAG v2.1.0
+  GIT_TAG 3fc00efadef8324aba31f77390c3e562f9af5147
 )
