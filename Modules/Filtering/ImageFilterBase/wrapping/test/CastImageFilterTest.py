58661ec04d0cef370556a86e41531a960f64b66b
Matt McCormick <matt.mccormick@kitware.com>
2020-01-29 13:56:30 -0500

    ENH: WIP: Wrap CastImageFilter for VectorImage <-> Vector

--- Modules/Filtering/ImageFilterBase/wrapping/test/CastImageFilterTest.py
+++ Modules/Filtering/ImageFilterBase/wrapping/test/CastImageFilterTest.py
@@ -25,11 +25,11 @@ from sys import argv
 itk.auto_progress(2)
 
 dim = 2
-IType = itk.Image[itk.F, dim]
-OIType = itk.Image[itk.UC, dim]
+InputImageType = itk.Image[itk.F, dim]
+OutputImageType = itk.Image[itk.UC, dim]
 
-reader = itk.ImageFileReader[IType].New(FileName=argv[1])
-filter = itk.CastImageFilter[IType, OIType].New(reader)
-writer = itk.ImageFileWriter[OIType].New(filter, FileName=argv[2])
+reader = itk.ImageFileReader[InputImageType].New(FileName=argv[1])
+filt = itk.CastImageFilter[InputImageType, OutputImageType].New(reader)
+writer = itk.ImageFileWriter[OutputImageType].New(filt, FileName=argv[2])
 
 writer.Update()
