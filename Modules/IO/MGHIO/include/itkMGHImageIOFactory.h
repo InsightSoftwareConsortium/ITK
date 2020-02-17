0f0ed3473f611cde6725a9a5bc34752045442d42
Hans Johnson <hans-johnson@uiowa.edu>
2020-02-17 14:48:39 -0600

    STYLE: Remove redundant void argument lists
    Find and remove redundant void argument lists.

--- Modules/IO/MGHIO/include/itkMGHImageIOFactory.h
+++ Modules/IO/MGHIO/include/itkMGHImageIOFactory.h
@@ -43,10 +43,10 @@ public:
 
   /** Class methods used to interface with the registered factories **/
   const char *
-  GetITKSourceVersion(void) const override;
+  GetITKSourceVersion() const override;
 
   const char *
-  GetDescription(void) const override;
+  GetDescription() const override;
 
   /** Method for class instantiation **/
   itkFactorylessNewMacro(Self);
@@ -56,7 +56,7 @@ public:
 
   /** Register one factory of this type */
   static void
-  RegisterOneFactory(void)
+  RegisterOneFactory()
   {
     MGHImageIOFactory::Pointer MGHFactory = MGHImageIOFactory::New();
 
