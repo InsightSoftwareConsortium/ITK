9196803bdbd9c230b8138b1c31a95713337525f0
Hans Johnson <hans-johnson@uiowa.edu>
2020-02-16 22:07:45 -0600

    STYLE: Replace integer literals which are cast to bool.
    Finds and replaces integer literals which are cast to bool.
    
    SRCDIR= #My local SRC
    BLDDIR= #My local BLD
    
    cd
    run-clang-tidy.py -extra-arg=-D__clang__ -checks=-*,modernize-use-bool-literals  -header-filter=.* -fix

--- Modules/Filtering/ImageFrequency/test/itkFrequencyFFTLayoutImageRegionIteratorWithIndexTest.cxx
+++ Modules/Filtering/ImageFrequency/test/itkFrequencyFFTLayoutImageRegionIteratorWithIndexTest.cxx
@@ -55,7 +55,7 @@ public:
     m_PositiveHalfRegion.SetSize(size);
     m_PositiveHalfRegion.SetIndex(start);
 
-    m_ImageIsOdd = inputImageSize % 2 == 1 ? 1 : 0;
+    m_ImageIsOdd = inputImageSize % 2 == 1 ? true : false;
     // Setup the half, negative frequencies region.
     unsigned int isImageSizeOdd = m_ImageIsOdd ? 1 : 0;
     size.Fill(inputImageSize / 2);
