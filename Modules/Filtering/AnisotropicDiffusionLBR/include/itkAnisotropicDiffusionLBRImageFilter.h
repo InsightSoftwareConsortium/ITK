c4d7296199fa50e0fc43c291d997555713263c68
Mathew Seng <mathewseng@gmail.com>
2020-02-18 06:39:03 -0600

    BUG: Fixes header mismatch test fail

--- Modules/Filtering/AnisotropicDiffusionLBR/include/itkAnisotropicDiffusionLBRImageFilter.h
+++ Modules/Filtering/AnisotropicDiffusionLBR/include/itkAnisotropicDiffusionLBRImageFilter.h
@@ -1,6 +1,6 @@
 /*=========================================================================
  *
- *  Copyright Insight Software Consortium
+ *  Copyright NumFOCUS
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -15,6 +15,8 @@
  *  limitations under the License.
  *
  *=========================================================================*/
+
+
 //
 //  Created by Jean-Marie Mirebeau on 28/02/2014.
 //
