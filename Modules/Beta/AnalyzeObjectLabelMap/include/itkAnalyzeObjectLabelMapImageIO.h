59720e7c5bc434ba3549c460160b8a3a8375eeed
Hans Johnson <hans-johnson@uiowa.edu>
2020-02-19 10:56:02 -0600

    DOC: Update copyright assignment to NumFOCUS
    The mission of NumFOCUS is to promote open
    practices in research, data, and scientific
    computing.
    
    https://numfocus.org

--- Modules/Beta/AnalyzeObjectLabelMap/include/itkAnalyzeObjectLabelMapImageIO.h
+++ Modules/Beta/AnalyzeObjectLabelMap/include/itkAnalyzeObjectLabelMapImageIO.h
@@ -1,20 +1,20 @@
 /*=========================================================================
-
-Program:   Insight Segmentation & Registration Toolkit
-Module:    $RCSfile: itkAnalyzeObjectLabelMapImageIO.h,v $
-Language:  C++
-Date:      $Date: 2007/03/29 20:11:16 $
-Version:   $Revision: 1.5 $
-
-Copyright (c) Insight Software Consortium. All rights reserved.
-See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
-
-This software is distributed WITHOUT ANY WARRANTY; without even
-the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-PURPOSE.  See the above copyright notices for more information.
-
-=========================================================================*/
-
+ *
+ *  Copyright NumFOCUS
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
 #ifndef itkAnalyzeObjectLabelMapImageIO_h
 #define itkAnalyzeObjectLabelMapImageIO_h
 
