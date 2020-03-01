ec3be42aa27c5baafb4cbdb214171912c2e4dfd0
Hans Johnson <hans-johnson@uiowa.edu>
2020-03-01 15:50:17 -0600

    COMP: Match template style more closely.

--- Modules/Filtering/SmoothingRecursiveYvvGaussianFilter/itk-module.cmake
+++ Modules/Filtering/SmoothingRecursiveYvvGaussianFilter/itk-module.cmake
@@ -1,8 +1,5 @@
 # the top-level README is used for describing this module, just
 # re-used it for documentation here
-get_filename_component(MY_CURRENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-file(READ "${MY_CURRENT_DIR}/README.rst" DOCUMENTATION)
-
 # itk_module() defines the module dependencies in
 # SmoothingRecursiveYvvGaussianFilter
 # The testing module in SmoothingRecursiveYvvGaussianFilter depends on
@@ -13,34 +10,23 @@ file(READ "${MY_CURRENT_DIR}/README.rst" DOCUMENTATION)
 # define the dependencies of the include module and the tests
 set(ModuleName "SmoothingRecursiveYvvGaussianFilter")
 if(ITK_USE_GPU)
-    itk_module(${ModuleName}
-     DEPENDS
-        ITKCommon
-        ITKIOImageBase
-        ITKImageFilterBase
-        ITKSmoothing
-        ITKGPUSmoothing
-        ITKGPUCommon
-     TEST_DEPENDS
-        ITKTestKernel #to handle IO in src
-        ITKGPUCommon
-        ITKSmoothing
-     EXCLUDE_FROM_DEFAULT
-     DESCRIPTION
-         "${DOCUMENTATION}"
-    )
-else()
-    itk_module(${ModuleName}
-     DEPENDS
-        ITKCommon
-        ITKIOImageBase
-        ITKImageFilterBase
-        ITKSmoothing
-     TEST_DEPENDS
-        ITKTestKernel #to handle IO in src
-        ITKSmoothing
-     EXCLUDE_FROM_DEFAULT
-     DESCRIPTION
-         "${DOCUMENTATION}"
-    )
+  set(${ModuleName}_GPU_DEPENDANCIES "ITKGPUSmoothing")
+  set(${ModuleName}_GPU_COMMON "ITKGPUCommon")
 endif()
+itk_module(
+  ${ModuleName}
+  DEPENDS
+    ITKCommon
+    ITKIOImageBase
+    ITKImageFilterBase
+    ITKSmoothing
+    ${${ModuleName}_GPU_DEPENDANCIES}
+    ${${ModuleName}_GPU_COMMON}
+  TEST_DEPENDS
+    ITKTestKernel #to handle IO in src
+    ${${ModuleName}_GPU_COMMON}
+    ITKSmoothing
+  DESCRIPTION "Module ingested from upstream."
+  EXCLUDE_FROM_DEFAULT
+  # Header only library does not use ENABLE_SHARED
+)
