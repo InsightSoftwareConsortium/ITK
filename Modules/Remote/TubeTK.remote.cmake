0c24f8651858e9abb56e7bad1aa106156480230c
Stephen R. Aylward <stephen.aylward@kitware.com>
2020-03-03 23:48:20 -0500

    ENH: Add TubeTK as a remote module for ITK

---
+++ Modules/Remote/TubeTK.remote.cmake
@@ -0,0 +1,53 @@
+#-- # Grading Level Criteria Report
+#-- EVALUATION DATE: 2020-03-03
+#-- EVALUATORS: [<<NO EVALUATOR>>,<<NO EVALUATOR>>]
+#--
+#-- ## Compliance level 5 star (AKA ITK main modules, or remote modules that could become core modules)
+#--   - [ ] Widespread community dependance
+#--   - [ ] Above 90% code coverage
+#--   - [ ] CI dashboards and testing monitored rigorously
+#--   - [ ] Key API features are exposed in wrapping interface
+#--   - [ ] All requirements of Levels 4,3,2,1
+#--
+#-- ## Compliance Level 4 star (Very high-quality code, perhaps small community dependance)
+#--   - [ ] Meets all ITK code style standards
+#--   - [ ] No external requirements beyond those needed by ITK proper
+#--   - [ ] Builds and passes tests on all supported platforms within 1 month of each core tagged release
+#--            - [ ] Windows Shared Library Build with Visual Studio
+#--            - [ ] Mac with clang compiller
+#--            - [ ] Linux with gcc compiler
+#--   - [X] Active developer community dedicated to maintaining code-base
+#--   - [ ] 75% code coverage demonstrated for testing suite
+#--   - [X] Continuous integration testing performed
+#--   - [X] All requirements of Levels 3,2,1
+#--
+#-- ## Compliance Level 3 star (Quality beta code)
+#--   - [X] API | executable interface is considered mostly stable and feature complete
+#--   - [X] 10% C0-code coverage demonstrated for testing suite
+#--   - [X] Some tests exist and pass on at least some platform
+#--   - [X] All requirements of Levels 2,1
+#--
+#-- ## Compliance Level 2 star (Alpha code feature API development or niche community/exectution environment dependance )
+#--   - [X] Compiles for at least 1 niche set of execution envirionments, and perhaps others
+#--         (may depend on specific external tools like a java environment, or specific external libraries to work )
+#--   - [X] All requirements of Levels 1
+#--
+#-- ## Compliance Level 1 star (Pre-alpha features under development and code of unkown quality)
+#--   - [X] Code complies on at least 1 platform
+#--
+#-- ## Compliance Level 0 star ( Code/Feature of known poor-quality or deprecated status )
+#--   - [ ] Code reviewed and explicitly identified as not recommended for use
+#--
+#-- ### Please document here any justification for the criteria above
+#-- See Dashboard at https://open.cdash.org/index.php?project=TubeTK
+#--   Runs Azure builds on Linux, Mac, and Windows, including coverage
+#--     and memcheck
+#--
+# Contact: Stephen Aylward <stephen.aylward@kitware.com>
+itk_fetch_module(TubeTK
+ "http://www.tubetk.org
+"
+  MODULE_COMPLIANCE_LEVEL 3
+  GIT_REPOSITORY ${git_protocol}://github.com/KitwareMedical/ITKTubeTK.git
+  GIT_TAG a6d1ed624e453b32199f71b2e1d18fb63737dcfe
+  )
