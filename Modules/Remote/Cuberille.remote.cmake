37a9976c74bcbb23a0ca4188dc671f35db9c41b5
Dženan Zukić <dzenan.zukic@kitware.com>
2020-03-24 13:10:24 -0400

    DOC: update compliance worksheet for 6 remote modules

--- Modules/Remote/Cuberille.remote.cmake
+++ Modules/Remote/Cuberille.remote.cmake
@@ -1,30 +1,30 @@
 #-- # Grading Level Criteria Report
-#-- EVALUATION DATE: 2020-03-01
-#-- EVALUATORS: [<<NO EVALUATOR>>,<<NO EVALUATOR>>]
+#-- EVALUATION DATE: 2020-03-24
+#-- EVALUATORS: [Dženan Zukić]
 #--
 #-- ## Compliance level 5 star (AKA ITK main modules, or remote modules that could become core modules)
 #--   - [ ] Widespread community dependance
 #--   - [ ] Above 90% code coverage
 #--   - [ ] CI dashboards and testing monitored rigorously
-#--   - [ ] Key API features are exposed in wrapping interface
+#--   - [X] Key API features are exposed in wrapping interface
 #--   - [ ] All requirements of Levels 4,3,2,1
 #--
 #-- ## Compliance Level 4 star (Very high-quality code, perhaps small community dependance)
-#--   - [ ] Meets all ITK code style standards
-#--   - [ ] No external requirements beyond those needed by ITK proper
+#--   - [X] Meets all ITK code style standards
+#--   - [X] No external requirements beyond those needed by ITK proper
 #--   - [ ] Builds and passes tests on all supported platforms within 1 month of each core tagged release
 #--            - [ ] Windows Shared Library Build with Visual Studio
 #--            - [ ] Mac with clang compiller
 #--            - [ ] Linux with gcc compiler
 #--   - [ ] Active developer community dedicated to maintaining code-base
 #--   - [ ] 75% code coverage demonstrated for testing suite
-#--   - [ ] Continuous integration testing performed
-#--   - [ ] All requirements of Levels 3,2,1
+#--   - [X] Continuous integration testing performed
+#--   - [X] All requirements of Levels 3,2,1
 #--
 #-- ## Compliance Level 3 star (Quality beta code)
-#--   - [ ] API | executable interface is considered mostly stable and feature complete
-#--   - [ ] 10% C0-code coverage demonstrated for testing suite
-#--   - [ ] Some tests exist and pass on at least some platform
+#--   - [X] API | executable interface is considered mostly stable and feature complete
+#--   - [X] 10% C0-code coverage demonstrated for testing suite
+#--   - [X] Some tests exist and pass on at least some platform
 #--   - [X] All requirements of Levels 2,1
 #--
 #-- ## Compliance Level 2 star (Alpha code feature API development or niche community/exectution environment dependance )
@@ -59,7 +59,7 @@ A more detailed description can be found in the Insight Journal article:
   July 20, 2010.
 http://www.insight-journal.org/browse/publication/213
 "
-  MODULE_COMPLIANCE_LEVEL 2
+  MODULE_COMPLIANCE_LEVEL 3
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKCuberille.git
   GIT_TAG 105d1478ee82b674a9bfa07d78a7967f6e91830b
   )
