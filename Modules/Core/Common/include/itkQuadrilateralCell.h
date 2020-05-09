InsightSoftwareConsortium/ITK

commit eb8ce3c6195b13878721ffc5475e1f32e183843a

COMP: Fix compiling an app with Clang when ITK is built with GCC

Modules/Core/Common/include/itkQuadrilateralCell.h
@@ -138,7 +138,7 @@ public:
     }
   }
 
-#if defined(__GNUC__) && (__GNUC__ > 5)
+#if defined(__GNUC__) && (__GNUC__ > 5) || defined(__clang__)
   ~QuadrilateralCell() override = default;
 #else
   ~QuadrilateralCell() override{};

Modules/Core/Common/include/itkTriangleCell.h
@@ -150,7 +150,7 @@ public:
   TriangleCell()
     : m_PointIds(NumberOfPoints, NumericTraits<PointIdentifier>::max())
   {}
-#if defined(__GNUC__) && (__GNUC__ > 5)
+#if defined(__GNUC__) && (__GNUC__ > 5) || defined(__clang__)
   ~TriangleCell() override = default;
 #else
   ~TriangleCell() override{};

Modules/Core/Transform/include/itkTransform.h
@@ -552,7 +552,7 @@ protected:
 
   Transform();
   Transform(NumberOfParametersType NumberOfParameters);
-#if defined(__GNUC__) && __GNUC__ < 6
+#if defined(__GNUC__) && __GNUC__ < 6 && !defined(__clang__)
   ~Transform() override{};
 #else
   ~Transform() override = default;

Modules/Core/Transform/include/itkTransformBase.h
@@ -151,7 +151,7 @@ public:
   GetTransformCategory() const = 0;
 
 protected:
-#if defined(__GNUC__) && __GNUC__ < 6
+#if defined(__GNUC__) && __GNUC__ < 6 && !defined(__clang__)
   // A bug in some versions of the gcc 5.4.0 compiler
   // result in a linker error when = default is requested
   TransformBaseTemplate(){};

commit 1bb185a073aae01a83289c22552dabdd06e7d1d7

COMP: Fixed link error with old versions of AppleClang
eb8ce3c6195b13878721ffc5475e1f32e183843a caused a link error with AppleClang 7 and 8.

It in turn was a follow up to:
af537fc3a75835475649d7fe9850000c4b6e0d46 and 1ad4e98c92e97b5b0aa16137ce1dfb4796eab5c1

which were working around similar link errors with old versions of GCC.

Since using =default doesn't win us so much, just don't use it with any version of GCC/clang in these four cases.

Modules/Core/Common/include/itkQuadrilateralCell.h
@@ -138,10 +138,15 @@ public:
     }
   }
 
-#if defined(__GNUC__) && (__GNUC__ > 5) || defined(__clang__)
-  ~QuadrilateralCell() override = default;
-#else
+#if defined(__GNUC__)
+  // A bug in some versions of the GCC and Clang compilers
+  // result in an ICE or linker error when "= default" is requested.
+  // This was observed in at least gcc 4.8 and 5.4.0, and
+  // AppleClang 7.0.2 and 8.0.0. Probably others too.
+  // "= default" doesn't gain us much, so just don't use it here.
   ~QuadrilateralCell() override{};
+#else
+  ~QuadrilateralCell() override = default;
 #endif
 
 protected:

Modules/Core/Common/include/itkTriangleCell.h
@@ -150,10 +150,15 @@ public:
   TriangleCell()
     : m_PointIds(NumberOfPoints, NumericTraits<PointIdentifier>::max())
   {}
-#if defined(__GNUC__) && (__GNUC__ > 5) || defined(__clang__)
-  ~TriangleCell() override = default;
-#else
+#if defined(__GNUC__)
+  // A bug in some versions of the GCC and Clang compilers
+  // result in an ICE or linker error when "= default" is requested.
+  // This was observed in at least gcc 4.8 and 5.4.0, and
+  // AppleClang 7.0.2 and 8.0.0. Probably others too.
+  // "= default" doesn't gain us much, so just don't use it here.
   ~TriangleCell() override{};
+#else
+  ~TriangleCell() override = default;
 #endif
 
 protected:

Modules/Core/Transform/include/itkTransform.h
@@ -553,7 +553,12 @@ protected:
 
   Transform();
   Transform(NumberOfParametersType NumberOfParameters);
-#if defined(__GNUC__) && __GNUC__ < 6 && !defined(__clang__)
+#if defined(__GNUC__)
+  // A bug in some versions of the GCC and Clang compilers
+  // result in an ICE or linker error when "= default" is requested.
+  // This was observed in at least gcc 4.8 and 5.4.0, and
+  // AppleClang 7.0.2 and 8.0.0. Probably others too.
+  // "= default" doesn't gain us much, so just don't use it here.
   ~Transform() override{};
 #else
   ~Transform() override = default;

Modules/Core/Transform/include/itkTransformBase.h
@@ -166,9 +166,12 @@ public:
   GetTransformCategory() const = 0;
 
 protected:
-#if defined(__GNUC__) && __GNUC__ < 6 && !defined(__clang__)
-  // A bug in some versions of the gcc 5.4.0 compiler
-  // result in a linker error when = default is requested
+#if defined(__GNUC__)
+  // A bug in some versions of the GCC and Clang compilers
+  // result in an ICE or linker error when "= default" is requested.
+  // This was observed in at least gcc 4.8 and 5.4.0, and
+  // AppleClang 7.0.2 and 8.0.0. Probably others too.
+  // "= default" doesn't gain us much, so just don't use it here.
   TransformBaseTemplate(){};
   ~TransformBaseTemplate() override{};
 #else