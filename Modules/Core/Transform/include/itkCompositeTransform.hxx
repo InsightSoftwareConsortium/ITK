c22e46b126f78bc096a59b238b81d3973d1e693f
Bradley Lowekamp <blowekamp@mail.nih.gov>
2020-06-04 08:48:10 -0400

    STYLE: remove extraneous semi-colon after bracket

--- Modules/Core/Transform/include/itkCompositeTransform.hxx
+++ Modules/Core/Transform/include/itkCompositeTransform.hxx
@@ -720,7 +720,7 @@ CompositeTransform<TParametersValueType, NDimensions>::GetFixedParameters() cons
     std::copy_n(
       subFixedParameters.data_block(), subFixedParameters.Size(), &(this->m_FixedParameters.data_block())[offset]);
     offset += subFixedParameters.Size();
-  };
+  }
 
   return this->m_FixedParameters;
 }
