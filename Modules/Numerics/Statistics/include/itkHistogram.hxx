InsightSoftwareConsortium/ITK

commit fb95a5e613362e956788ab0cdb444cc8e7f6a211

ENH: automatic threshold calculators
This is the code of the insight journal contribution "Histogram-based
thresholding - some missing methods".

  http://www.insight-journal.org/browse/publication/811
  http://hdl.handle.net/10380/3279

The Otsu filters and calculators have also been modified to use the
architecture of the other calculators.
The histogram is no more computed by the calculator. This is delegated
to another filter, such as ImageToHistogramFilter. This change lets us
easily implementing the multithreading and the ability to mask the
image to compute the histogram, for example by using
MaskedImageToHistogramFilter or by using the histogram attached to a
LabelObject.

The old OtsuThresholdImageCalculator is available in the
ITKV3Compatibility module.

Patch 15 - Changed itkTypeMacro superclass field from ImageToImageFilter
to HistogramThresholdImageFilter in all subclasses

Removed some redundant include files

added methods for setting number of histogram bins and turning
AutoMinimumMaximum on/off. Still need methods for setting
minimum and maximum explicitly. Not sure of the appropriate
way of doing this with the decorator structures.

Added required number of inputs and outputs.

Patch 16 - Rebased after the removal of the Testing/Data submodule.

Patch 17 - Added my (Richard Beare) complete affiliation. Also changed
the strategy for derived classes that include calculators with different
parameters. This change is intended for discussion. I have removed the
virtual method - ConfigureCalculator - and included a pointer to the
calculator in the derived class. This eliminated dynamic_casts inside the
derived class as well as the local variables in the derived class. However
it leads to the need for explicit Set/Get methods, rather than ones
created using macros. Perhaps there are macros that can help that I am
unaware of? The example is the Intermodes filter.

Note - we also need a new test for intermodes filter using
SetUseInterMode(false)

Modules/Numerics/Statistics/include/itkHistogram.hxx
@@ -699,6 +699,21 @@ Histogram< TMeasurement, TFrequencyContainer >
 }
 
 template< class TMeasurement, class TFrequencyContainer >
+double
+Histogram< TMeasurement, TFrequencyContainer >
+::Mean(unsigned int dimension) const
+{
+  const unsigned int size = this->GetSize(dimension);
+  double             totalFrequency = double( this->GetTotalFrequency() );
+  double             sum = 0;
+  for( unsigned int i = 0; i<size; i++ )
+    {
+    sum += this->GetFrequency( i, dimension );
+    }
+  return sum / totalFrequency;
+}
+
+template< class TMeasurement, class TFrequencyContainer >
 void
 Histogram< TMeasurement, TFrequencyContainer >
 ::PrintSelf(std::ostream & os, Indent indent) const

Modules/Filtering/Thresholding/test/CMakeLists.txt
@@ -1,31 +1,37 @@
 itk_module_test()
 set(ITKThresholdingTests
-itkOtsuThresholdImageCalculatorTest.cxx
-itkOtsuMultipleThresholdsImageFilterTest.cxx
-itkOtsuThresholdImageFilterTest.cxx
-itkThresholdLabelerImageFilterTest.cxx
-itkBinaryThresholdImageFilterTest.cxx
-itkThresholdImageFilterTest.cxx
-itkOtsuMultipleThresholdsCalculatorTest.cxx
 itkBinaryThresholdImageFilterTest2.cxx
+itkBinaryThresholdImageFilterTest.cxx
 itkBinaryThresholdProjectionImageFilterTest.cxx
 itkBinaryThresholdSpatialFunctionTest.cxx
+itkHuangThresholdImageFilterTest.cxx
+itkIntermodesThresholdImageFilterTest.cxx
+itkIsoDataThresholdImageFilterTest.cxx
+itkKittlerIllingworthThresholdImageFilterTest.cxx
+itkLiThresholdImageFilterTest.cxx
+itkMaximumEntropyThresholdImageFilterTest.cxx
+itkMomentsThresholdImageFilterTest.cxx
+itkOtsuMultipleThresholdsCalculatorTest.cxx
+itkOtsuMultipleThresholdsImageFilterTest.cxx
+itkOtsuThresholdCalculatorTest.cxx
+itkOtsuThresholdImageFilterTest.cxx
+itkRenyiEntropyThresholdImageFilterTest.cxx
+itkShanbhagThresholdImageFilterTest.cxx
+itkThresholdImageFilterTest.cxx
+itkThresholdLabelerImageFilterTest.cxx
+itkTriangleThresholdImageFilterTest.cxx
+itkYenThresholdImageFilterTest.cxx
 )
 
 CreateTestDriver(ITKThresholding  "${ITKThresholding-Test_LIBRARIES}" "${ITKThresholdingTests}")
 
-itk_add_test(NAME itkOtsuThresholdImageCalculatorTest
-      COMMAND ITKThresholdingTestDriver itkOtsuThresholdImageCalculatorTest)
+itk_add_test(NAME itkOtsuThresholdCalculatorTest
+      COMMAND ITKThresholdingTestDriver itkOtsuThresholdCalculatorTest)
 itk_add_test(NAME itkOtsuMultipleThresholdsImageFilterTest
       COMMAND ITKThresholdingTestDriver
     --compare DATA{${ITK_DATA_ROOT}/Baseline/Algorithms/OtsuMultipleThresholdsImageFilterTest.png}
               ${ITK_TEST_OUTPUT_DIR}/OtsuMultipleThresholdsImageFilterTest.png
     itkOtsuMultipleThresholdsImageFilterTest DATA{${ITK_DATA_ROOT}/Input/peppers.png} ${ITK_TEST_OUTPUT_DIR}/OtsuMultipleThresholdsImageFilterTest.png 64 3 2)
-itk_add_test(NAME itkOtsuThresholdImageFilterTest
-      COMMAND ITKThresholdingTestDriver
-    --compare DATA{${ITK_DATA_ROOT}/Baseline/Algorithms/OtsuThresholdImageFilterTest.png}
-              ${ITK_TEST_OUTPUT_DIR}/OtsuThresholdImageFilterTest.png
-    itkOtsuThresholdImageFilterTest DATA{${ITK_DATA_ROOT}/Input/cthead1.png} ${ITK_TEST_OUTPUT_DIR}/OtsuThresholdImageFilterTest.png 64)
 itk_add_test(NAME itkThresholdLabelerImageFilterTest
       COMMAND ITKThresholdingTestDriver itkThresholdLabelerImageFilterTest)
 itk_add_test(NAME itkBinaryThresholdImageFilterTest
@@ -46,3 +52,63 @@ itk_add_test(NAME itkBinaryThresholdProjectionImageFilterTest
     itkBinaryThresholdProjectionImageFilterTest DATA{${ITK_DATA_ROOT}/Input/HeadMRVolume.mhd,HeadMRVolume.raw} ${ITK_TEST_OUTPUT_DIR}/HeadMRVolumeBinaryThresholdProjection.png 100 255 0)
 itk_add_test(NAME itkBinaryThresholdSpatialFunctionTest
       COMMAND ITKThresholdingTestDriver itkBinaryThresholdSpatialFunctionTest)
+itk_add_test(NAME itkHuangThresholdImageFilterTest
+      COMMAND ITKThresholdingTestDriver
+    --compare DATA{Baseline/itkHuangThresholdImageFilterTest.png}
+              ${ITK_TEST_OUTPUT_DIR}/itkHuangThresholdImageFilterTest.png
+    itkHuangThresholdImageFilterTest DATA{${ITK_DATA_ROOT}/Input/cthead1.png} ${ITK_TEST_OUTPUT_DIR}/itkHuangThresholdImageFilterTest.png)
+itk_add_test(NAME itkIntermodesThresholdImageFilterTest
+      COMMAND ITKThresholdingTestDriver
+    --compare DATA{Baseline/itkIntermodesThresholdImageFilterTest.png}
+              ${ITK_TEST_OUTPUT_DIR}/itkIntermodesThresholdImageFilterTest.png
+    itkIntermodesThresholdImageFilterTest DATA{${ITK_DATA_ROOT}/Input/cthead1.png} ${ITK_TEST_OUTPUT_DIR}/itkIntermodesThresholdImageFilterTest.png)
+itk_add_test(NAME itkIsoDataThresholdImageFilterTest
+      COMMAND ITKThresholdingTestDriver
+    --compare DATA{Baseline/itkIsoDataThresholdImageFilterTest.png}
+              ${ITK_TEST_OUTPUT_DIR}/itkIsoDataThresholdImageFilterTest.png
+    itkIsoDataThresholdImageFilterTest DATA{${ITK_DATA_ROOT}/Input/cthead1.png} ${ITK_TEST_OUTPUT_DIR}/itkIsoDataThresholdImageFilterTest.png)
+itk_add_test(NAME itkKittlerIllingworthThresholdImageFilterTest
+      COMMAND ITKThresholdingTestDriver
+    --compare DATA{Baseline/itkKittlerIllingworthThresholdImageFilterTest.png}
+              ${ITK_TEST_OUTPUT_DIR}/itkKittlerIllingworthThresholdImageFilterTest.png
+    itkKittlerIllingworthThresholdImageFilterTest DATA{${ITK_DATA_ROOT}/Input/cthead1.png} ${ITK_TEST_OUTPUT_DIR}/itkKittlerIllingworthThresholdImageFilterTest.png)
+itk_add_test(NAME itkLiThresholdImageFilterTest
+      COMMAND ITKThresholdingTestDriver
+    --compare DATA{Baseline/itkLiThresholdImageFilterTest.png}
+              ${ITK_TEST_OUTPUT_DIR}/itkLiThresholdImageFilterTest.png
+    itkLiThresholdImageFilterTest DATA{${ITK_DATA_ROOT}/Input/cthead1.png} ${ITK_TEST_OUTPUT_DIR}/itkLiThresholdImageFilterTest.png)
+itk_add_test(NAME itkMaximumEntropyThresholdImageFilterTest
+      COMMAND ITKThresholdingTestDriver
+    --compare DATA{Baseline/itkMaximumEntropyThresholdImageFilterTest.png}
+              ${ITK_TEST_OUTPUT_DIR}/itkMaximumEntropyThresholdImageFilterTest.png
+    itkMaximumEntropyThresholdImageFilterTest DATA{${ITK_DATA_ROOT}/Input/cthead1.png} ${ITK_TEST_OUTPUT_DIR}/itkMaximumEntropyThresholdImageFilterTest.png)
+itk_add_test(NAME itkMomentsThresholdImageFilterTest
+      COMMAND ITKThresholdingTestDriver
+    --compare DATA{Baseline/itkMomentsThresholdImageFilterTest.png}
+              ${ITK_TEST_OUTPUT_DIR}/itkMomentsThresholdImageFilterTest.png
+    itkMomentsThresholdImageFilterTest DATA{${ITK_DATA_ROOT}/Input/cthead1.png} ${ITK_TEST_OUTPUT_DIR}/itkMomentsThresholdImageFilterTest.png)
+itk_add_test(NAME itkOtsuThresholdImageFilterTest
+      COMMAND ITKThresholdingTestDriver
+    --compare DATA{Baseline/itkOtsuThresholdImageFilterTest.png}
+              ${ITK_TEST_OUTPUT_DIR}/itkOtsuThresholdImageFilterTest.png
+    itkOtsuThresholdImageFilterTest DATA{${ITK_DATA_ROOT}/Input/cthead1.png} ${ITK_TEST_OUTPUT_DIR}/itkOtsuThresholdImageFilterTest.png)
+itk_add_test(NAME itkRenyiEntropyThresholdImageFilterTest
+      COMMAND ITKThresholdingTestDriver
+    --compare DATA{Baseline/itkRenyiEntropyThresholdImageFilterTest.png}
+              ${ITK_TEST_OUTPUT_DIR}/itkRenyiEntropyThresholdImageFilterTest.png
+    itkRenyiEntropyThresholdImageFilterTest DATA{${ITK_DATA_ROOT}/Input/cthead1.png} ${ITK_TEST_OUTPUT_DIR}/itkRenyiEntropyThresholdImageFilterTest.png)
+itk_add_test(NAME itkShanbhagThresholdImageFilterTest
+      COMMAND ITKThresholdingTestDriver
+    --compare DATA{Baseline/itkShanbhagThresholdImageFilterTest.png}
+              ${ITK_TEST_OUTPUT_DIR}/itkShanbhagThresholdImageFilterTest.png
+    itkShanbhagThresholdImageFilterTest DATA{${ITK_DATA_ROOT}/Input/cthead1.png} ${ITK_TEST_OUTPUT_DIR}/itkShanbhagThresholdImageFilterTest.png)
+itk_add_test(NAME itkTriangleThresholdImageFilterTest
+      COMMAND ITKThresholdingTestDriver
+    --compare DATA{Baseline/itkTriangleThresholdImageFilterTest.png}
+              ${ITK_TEST_OUTPUT_DIR}/itkTriangleThresholdImageFilterTest.png
+    itkTriangleThresholdImageFilterTest DATA{${ITK_DATA_ROOT}/Input/cthead1.png} ${ITK_TEST_OUTPUT_DIR}/itkTriangleThresholdImageFilterTest.png)
+itk_add_test(NAME itkYenThresholdImageFilterTest
+      COMMAND ITKThresholdingTestDriver
+    --compare DATA{Baseline/itkYenThresholdImageFilterTest.png}
+              ${ITK_TEST_OUTPUT_DIR}/itkYenThresholdImageFilterTest.png
+    itkYenThresholdImageFilterTest DATA{${ITK_DATA_ROOT}/Input/cthead1.png} ${ITK_TEST_OUTPUT_DIR}/itkYenThresholdImageFilterTest.png)

Modules/Compatibility/V3Compatibility/test/CMakeLists.txt
@@ -7,6 +7,7 @@ set(ITKV3CompatibilityTests
   itkTransformToDeformationFieldSourceTest.cxx
   itkTransformToDeformationFieldSourceTest1.cxx
   itkDeformationFieldJacobianDeterminantFilterTest.cxx
+  itkOtsuThresholdImageCalculatorTest.cxx
 )
 
 CreateTestDriver(ITKV3Compatibility  "${ITKV3Compatibility-Test_LIBRARIES}" "${ITKV3CompatibilityTests}")
@@ -38,3 +39,5 @@ itk_add_test(NAME itkTransformToDeformationFieldSourceTest02
               BSpline ${ITK_TEST_OUTPUT_DIR}/itkTransformToDeformationFieldSourceTestField02.mha DATA{${ITK_DATA_ROOT}/Input/parametersBSpline.txt} ${ITK_TEST_OUTPUT_DIR}/itkTransformToDeformationFieldSourceTestImage02.mha)
 itk_add_test(NAME itkExponentialDeformationFieldImageFilterTest
       COMMAND ITKV3CompatibilityTestDriver itkExponentialDeformationFieldImageFilterTest)
+itk_add_test(NAME itkOtsuThresholdImageCalculatorTest
+      COMMAND ITKV3CompatibilityTestDriver itkOtsuThresholdImageCalculatorTest)

Modules/Filtering/Thresholding/wrapping/CMakeLists.txt
@@ -1,6 +1,8 @@
 WRAP_LIBRARY(ITKThresholding)
 
 set(WRAPPER_LIBRARY_GROUPS
+  itkHistogramThresholdCalculator
+  itkHistogramThresholdImageFilter
 )
 
 set(WRAPPER_LIBRARY_DEPENDS ITKImageIntensity ITKImageStatistics)

commit 71b598d71ca51efbf47b54ba1701fbfb057a9ecd

BUG: fix Statistics::Histogram::Mean calculation
This method was introduced by fb95a5e613362e956788ab0cdb444cc8e7f6a211,
but it never worked properly, always returning 1.

Modules/Numerics/Statistics/include/itkHistogram.hxx
@@ -645,11 +645,12 @@ double
 Histogram<TMeasurement, TFrequencyContainer>::Mean(unsigned int dimension) const
 {
   const unsigned int size = this->GetSize(dimension);
-  auto               totalFrequency = double(this->GetTotalFrequency());
+  double             totalFrequency = this->GetTotalFrequency();
   double             sum = 0;
   for (unsigned int i = 0; i < size; i++)
   {
-    sum += this->GetFrequency(i, dimension);
+    double frequency = this->GetFrequency(i, dimension);
+    sum += frequency * this->GetMeasurement(i, dimension);
   }
   return sum / totalFrequency;
 }

Modules/Filtering/Thresholding/test/CMakeLists.txt
@@ -121,7 +121,7 @@ itk_add_test(NAME itkIsoDataThresholdImageFilterTestNoAutoMinMax
       COMMAND ITKThresholdingTestDriver
     --compare-MD5 DATA{Baseline/itkIsoDataThresholdImageFilterTestNoAutoMinMax.png}
               9f844fd120ff49a7812e329a1b0216d8
-    itkIsoDataThresholdImageFilterTest DATA{${ITK_DATA_ROOT}/Input/cthead1.png} ${ITK_TEST_OUTPUT_DIR}/itkIsoDataThresholdImageFilterTestNoAutoMinMax.png 32 0 1)
+    itkIsoDataThresholdImageFilterTest DATA{${ITK_DATA_ROOT}/Input/cthead1.png} ${ITK_TEST_OUTPUT_DIR}/itkIsoDataThresholdImageFilterTestNoAutoMinMax.png 32 0 1023)
 
 itk_add_test(NAME itkIsoDataMaskedThresholdImageFilterTest
       COMMAND ITKThresholdingTestDriver