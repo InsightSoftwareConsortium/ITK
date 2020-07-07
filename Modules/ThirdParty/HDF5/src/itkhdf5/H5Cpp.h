20e54fe70eb7d21fd663b6963998d37bbbd20441
Hans Johnson <hans-johnson@uiowa.edu>
2020-07-07 10:31:07 -0500

    BUG: itkhdf5 installed paths were incorrect with recent hdf5 versions
    The aliasing for the build tree was incorrectly matched to
    install tree file name directory for H5Cpp.h.
    
    \#include "itkhdf5/itk_H5Cpp.h"
    
    Resulted in an error of missing headerfile "itkhdf5/cpp/H5cpp.h"
    when building against an installed version of ITK.

---
+++ Modules/ThirdParty/HDF5/src/itkhdf5/H5Cpp.h
@@ -0,0 +1,6 @@
+/* This allows itk_H5Cpp.h to include itkhdf5/H5Cpp.h in the ITK build tree.
+   Do NOT install this header. */
+#include "../c++/src/H5Cpp.h"
+/* NOTE:  The file 'c++/src/H5Cpp.h' is installed as '{INSTALL_PREFIX}/itkhdf5/H5Cpp.h'
+ *        so that this local alias is not needed in the install tree. */
+
