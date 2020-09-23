8f0b4936a9e24afd7df0d3c058f25a731fc42bf4
Niels Dekker <N.Dekker@lumc.nl>
2020-09-23 16:39:55 +0200

    STYLE: Allow conversion from nullptr to WeakPointer, not from zero
    Small WeakPointer improvements:
    
    - Defaulted default-constructor (allowing the compiler to consider it
    "constexpr" and "noexcept")
    - Removed the other user-defined special functions (Rule Of Zero)
    - Removed redundant user-defined `operator=(ObjectType*)`
    - Added converting constructor, WeakPointer(std::nullptr_t).
    
    No longer allowed implicit conversion from the integer zero.
    
    Included GoogleTest unit tests.

---
+++ Modules/Core/Common/test/itkWeakPointerGTest.cxx
@@ -0,0 +1,68 @@
+/*=========================================================================
+ *
+ *  Copyright NumFOCUS
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#include "itkGTest.h"
+
+#include "itkWeakPointer.h"
+#include "itkLightObject.h"
+
+#include <type_traits> // For is_default_constructible, is_copy_constructible, etc.
+
+namespace
+{
+using WeakPointerType = itk::WeakPointer<itk::LightObject>;
+
+template <typename T>
+constexpr bool
+AllSpecialMemberFunctionsNoThrow()
+{
+  return std::is_nothrow_default_constructible<T>::value && std::is_nothrow_copy_constructible<T>::value &&
+         std::is_nothrow_copy_assignable<T>::value && std::is_nothrow_move_constructible<T>::value &&
+         std::is_nothrow_move_assignable<T>::value && std::is_nothrow_destructible<T>::value;
+}
+
+} // namespace
+
+static_assert(AllSpecialMemberFunctionsNoThrow<WeakPointerType>(),
+              "All special member functions should be non-throwing.");
+
+
+TEST(WeakPointer, DefaultConstructedEqualsNullptr)
+{
+  ASSERT_EQ(WeakPointerType{}, nullptr);
+  ASSERT_TRUE(WeakPointerType{} == nullptr);
+  ASSERT_TRUE(nullptr == WeakPointerType{});
+}
+
+
+TEST(WeakPointer, ConvertedFromNullptrEqualsNullptr)
+{
+  ASSERT_EQ(WeakPointerType{ nullptr }, nullptr);
+  ASSERT_TRUE(WeakPointerType{ nullptr } == nullptr);
+  ASSERT_TRUE(nullptr == WeakPointerType{ nullptr });
+}
+
+
+TEST(WeakPointer, AssignedFromNullptrEqualsNullptr)
+{
+  WeakPointerType ptr;
+  ptr = nullptr;
+  ASSERT_EQ(ptr, nullptr);
+  ASSERT_TRUE(ptr == nullptr);
+  ASSERT_TRUE(nullptr == ptr);
+}
