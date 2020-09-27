14e49c9b81ade7acdff49a2690f867c000883b60
Nick Tustison <ntustison@gmail.com>
2020-09-27 08:34:47 -0700

    DOC:  Update description.

--- Modules/Remote/AdaptiveDenoising.remote.cmake
+++ Modules/Remote/AdaptiveDenoising.remote.cmake
@@ -1,5 +1,5 @@
 itk_fetch_module(AdaptiveDenoising
-  "A spatially adaptive denoising image filter."
+  "A spatially adaptive denoising image filter using non-local means."
   GIT_REPOSITORY http://github.com/ntustison/ITKAdaptiveDenoising.git
   GIT_TAG 914e6958a0773e18d6bb45cc5feda72f3180a8ba
   )
