223b708c40f02fbce51dbdf92a01f81e9f509ba8
Nick Tustison <ntustison@gmail.com>
2020-09-26 19:26:09 -0700

    ENH:  Add adaptive denoising module.

---
+++ Modules/Remote/AdaptiveDenoising.remote.cmake
@@ -0,0 +1,5 @@
+itk_fetch_module(AdaptiveDenoising
+  "A spatially adaptive denoising image filter."
+  GIT_REPOSITORY http://github.com/ntustison/ITKAdaptiveDenoising.git
+  GIT_TAG 914e6958a0773e18d6bb45cc5feda72f3180a8ba
+  )
