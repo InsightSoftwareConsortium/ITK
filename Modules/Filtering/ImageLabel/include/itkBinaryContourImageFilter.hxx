b706cf64c7e10f8985e6fdb18c77b607f1991f75
Bradley Lowekamp <blowekamp@mail.nih.gov>
2020-09-29 13:20:04 -0400

    STYLE: Improve function types used in SignedMaurerDistanceMap
    Prefer using constants, prefer using raw pointers in threaded methods,
    and avoid coping ImageRegion by using const ref.

--- Modules/Filtering/ImageLabel/include/itkBinaryContourImageFilter.hxx
+++ Modules/Filtering/ImageLabel/include/itkBinaryContourImageFilter.hxx
@@ -106,9 +106,9 @@ BinaryContourImageFilter<TInputImage, TOutputImage>::BeforeThreadedGenerateData(
   OutputImagePointer     output = this->GetOutput();
   InputImageConstPointer input = this->GetInput();
 
-  RegionType    reqRegion = output->GetRequestedRegion();
-  SizeValueType pixelcount = reqRegion.GetNumberOfPixels();
-  SizeValueType xsize = reqRegion.GetSize()[0];
+  const RegionType &  reqRegion = output->GetRequestedRegion();
+  const SizeValueType pixelcount = reqRegion.GetNumberOfPixels();
+  const SizeValueType xsize = reqRegion.GetSize()[0];
   const SizeValueType linecount = (xsize > 0 ? pixelcount / xsize : 0);
 
   m_ForegroundLineMap.clear();
@@ -123,8 +123,8 @@ void
 BinaryContourImageFilter<TInputImage, TOutputImage>::DynamicThreadedGenerateData(
   const RegionType & outputRegionForThread)
 {
-  OutputImagePointer     output = this->GetOutput();
-  InputImageConstPointer input = this->GetInput();
+  OutputImageType *      output = this->GetOutput();
+  const InputImageType * input = this->GetInput();
 
   using InputLineIteratorType = ImageScanlineConstIterator<InputImageType>;
   InputLineIteratorType inLineIt(input, outputRegionForThread);
