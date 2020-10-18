fade2565bb6aa595635740b74f5921e9abcbc385
Hans Johnson <hans-johnson@uiowa.edu>
2020-10-18 17:15:42 -0500

    COMP: Remove python2 dictionary compatibilties.

--- Wrapping/Generators/Python/itkExtras.py
+++ Wrapping/Generators/Python/itkExtras.py
@@ -1131,6 +1131,12 @@ class templated_class:
     def keys(self):
         return self.__templates__.keys()
 
+    def values(self):
+        return list(self.__templates__.values())
+
+    def items(self):
+        return list(self.__templates__.items())
+
     # everything after this comment is for dict interface
     # and is a copy/paste from DictMixin
     # only methods to edit dictionary are not there
@@ -1147,25 +1153,6 @@ class templated_class:
 
     def __contains__(self, key):
         return key in self
-
-    # third level takes advantage of second level definitions
-    def iteritems(self):
-        for k in self:
-            yield (k, self[k])
-
-    def iterkeys(self):
-        return self.__iter__()
-
-    # fourth level uses definitions from lower levels
-    def itervalues(self):
-        for _, v in self.iteritems():
-            yield v
-
-    def values(self):
-        return [v for _, v in self.iteritems()]
-
-    def items(self):
-        return list(self.iteritems())
 
     def get(self, key, default=None):
         try:
