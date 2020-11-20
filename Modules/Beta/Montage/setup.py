32adb17a9851439868405ba6e223bcc7af0808e2
Mathew Seng <mathewseng@gmail.com>
2020-11-20 08:55:03 -0600

    ENH: Update python package version for modules This will bump the package version in setup.py from 0.6.0 to 0.7.0

--- Modules/Beta/Montage/setup.py
+++ Modules/Beta/Montage/setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
     name='itk-montage',
-    version='0.6.0',
+    version='0.7.0',
     author='Dženan Zukić, Matt McCormick',
     author_email='itk+community@discourse.itk.org',
     packages=['itk'],
