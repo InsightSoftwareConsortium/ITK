fdccf50daa92cc3d84b28df0a43593d89a69997a
Niels Dekker <N.Dekker@lumc.nl>
2021-01-17 20:38:54 +0100

    STYLE: Reduce buf size, remove builder.Reset() calls from NumberToString
    Various style improvements of the implementation of the
    `NumberToString<TValue>` specializations for floating point numbers:
    
     - Did clean up the #include's.
     - Removed unnecessary builder.Reset() calls. (`StringBuilder::Reset()` only just sets the internal position to zero, but that is already taken care of by the constructor of `StringBuilder`.)
     - Added local helper function, `FloatingPointNumberToString(val)`, sharing code between `float` and `double` specialization.
     - Reduced size of local buffer (`buf`) from 256 to 32 chars.

--- Modules/Core/Common/src/itkNumberToString.cxx
+++ Modules/Core/Common/src/itkNumberToString.cxx
@@ -16,10 +16,50 @@
  *
  *=========================================================================*/
 #include "itkNumberToString.h"
-#include "itkNumericTraits.h"
-#include "double-conversion/double-conversion.h"
+#include "double-conversion/double-to-string.h"
 
 #include <sstream>
+
+namespace
+{
+
+bool
+ConvertToShortest(const double_conversion::DoubleToStringConverter & converter,
+                  const double                                       val,
+                  double_conversion::StringBuilder &                 builder)
+{
+  return converter.ToShortest(val, &builder);
+}
+
+bool
+ConvertToShortest(const double_conversion::DoubleToStringConverter & converter,
+                  const float                                        val,
+                  double_conversion::StringBuilder &                 builder)
+{
+  // Call the converter member function that is specific for single-precision `float`.
+  return converter.ToShortestSingle(val, &builder);
+}
+
+template <typename TValue>
+std::string
+FloatingPointNumberToString(const TValue val)
+{
+  // Declare a buffer, large enough for strings like:
+  // "-100000000000000000000" (-1e20, either float or double, 23 chars)
+  // "-1.7976931348623157e+308" (-DBL_MAX, 25 chars)
+  // "-0.0000033333333333333333" (-3e-005/9.0, 26 chars)
+  char buf[32];
+
+  double_conversion::StringBuilder builder(buf, sizeof(buf));
+
+  if (!ConvertToShortest(double_conversion::DoubleToStringConverter::EcmaScriptConverter(), val, builder))
+  {
+    itkGenericExceptionMacro(<< "Conversion failed for " << val);
+  }
+  return std::string(builder.Finalize());
+}
+
+} // namespace
 
 namespace itk
 {
@@ -28,33 +68,14 @@ template <>
 std::string
 NumberToString<double>::operator()(double val) const
 {
-  char                                               buf[256];
-  const double_conversion::DoubleToStringConverter & converter =
-    double_conversion::DoubleToStringConverter::EcmaScriptConverter();
-  double_conversion::StringBuilder builder(buf, sizeof(buf));
-  builder.Reset();
-  if (!converter.ToShortest(val, &builder))
-  {
-    itkGenericExceptionMacro(<< "Conversion failed for " << val);
-  }
-  return std::string(builder.Finalize());
+  return FloatingPointNumberToString(val);
 }
 
 template <>
 std::string
 NumberToString<float>::operator()(float val) const
 {
-  char                                               buf[256];
-  const double_conversion::DoubleToStringConverter & converter =
-    double_conversion::DoubleToStringConverter::EcmaScriptConverter();
-
-  double_conversion::StringBuilder builder(buf, sizeof(buf));
-  builder.Reset();
-  if (!converter.ToShortestSingle(val, &builder))
-  {
-    itkGenericExceptionMacro(<< "Conversion failed for " << val);
-  }
-  return std::string(builder.Finalize());
+  return FloatingPointNumberToString(val);
 }
 
 } // namespace itk
