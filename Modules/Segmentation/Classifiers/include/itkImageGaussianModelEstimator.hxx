bd11fd58a75d02068d7e9c4a526a126b603823cb
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2021-02-15 20:43:22 -0500

    STYLE: Provide default initialization to constructor in header file
    Provide default initialization to constructor in header file in line with
    the common practice in ITK.

--- Modules/Segmentation/Classifiers/include/itkImageGaussianModelEstimator.hxx
+++ Modules/Segmentation/Classifiers/include/itkImageGaussianModelEstimator.hxx
@@ -24,10 +24,6 @@
 
 namespace itk
 {
-template <typename TInputImage, typename TMembershipFunction, typename TTrainingImage>
-ImageGaussianModelEstimator<TInputImage, TMembershipFunction, TTrainingImage>::ImageGaussianModelEstimator()
-
-  = default;
 
 template <typename TInputImage, typename TMembershipFunction, typename TTrainingImage>
 ImageGaussianModelEstimator<TInputImage, TMembershipFunction, TTrainingImage>::~ImageGaussianModelEstimator()
