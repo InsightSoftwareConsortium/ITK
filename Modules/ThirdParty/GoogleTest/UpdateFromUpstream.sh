4699a88925bb03eec6e3e6b7a01cd8b0979521d4
Dženan Zukić <dzenan.zukic@kitware.com>
2021-02-24 17:17:06 -0500

    Revert "ENH: use master branch of GoogleTest until a stable version is released"
    This reverts commit 58d757c4afa7cf707e0063ae89c95f784de0d7f9.

--- Modules/ThirdParty/GoogleTest/UpdateFromUpstream.sh
+++ Modules/ThirdParty/GoogleTest/UpdateFromUpstream.sh
@@ -3,7 +3,7 @@
 thirdparty_module_name='GoogleTest'
 
 upstream_git_url='https://github.com/google/googletest.git'
-upstream_git_branch='master'
+upstream_git_branch='release-1.10.0'
 
 github_compare=true
 
