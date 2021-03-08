deb4f26249f7254964ac823d47dfb3592f77c9ea
al-sabr <e.bekrek@yandex.com>
2021-03-08 12:52:03 +0100

    Resolving compilation error of ITK within CTK.
    https://github.com/InsightSoftwareConsortium/ITK/issues/2362

--- Modules/ThirdParty/GDCM/src/gdcm/Source/Common/gdcmSystem.cxx
+++ Modules/ThirdParty/GDCM/src/gdcm/Source/Common/gdcmSystem.cxx
@@ -48,6 +48,19 @@
 #include <io.h>
 #include <direct.h>
 #define _unlink unlink
+
+#include <chrono>
+
+int gettimeofday(struct timeval* tp, struct timezone* tzp) {
+    namespace sc = std::chrono;
+    sc::system_clock::duration d = sc::system_clock::now().time_since_epoch();
+    sc::seconds s = sc::duration_cast<sc::seconds>(d);
+    tp->tv_sec = s.count();
+    tp->tv_usec = sc::duration_cast<sc::microseconds>(d - s).count();
+
+    return 0;
+}
+
 #else
 //#include <features.h> // we want GNU extensions
 #include <dlfcn.h>
