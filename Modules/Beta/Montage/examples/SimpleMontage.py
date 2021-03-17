f728e75a967a3e52ef54041c7b3df902b858ab9a
Dženan Zukić <dzenan.zukic@kitware.com>
2021-03-17 17:59:40 -0400

    ENH: review suggestions

--- Modules/Beta/Montage/examples/SimpleMontage.py
+++ Modules/Beta/Montage/examples/SimpleMontage.py
@@ -17,90 +17,77 @@
 # ==========================================================================*/
 import sys
 import os
-import itkConfig
-itkConfig.LazyLoading = False
 import itk
-
-itk.auto_progress(2)
+from pathlib import Path
 
 if len(sys.argv) < 4:
-  print(f"Usage: {sys.argv[0]} <directoryWithInputData> <outputDirectory> <outputFilename>")
+  print(f"Usage: {sys.argv[0]} <directoryWithInputTiles> <outputDirectory> <outputFilename>")
   sys.exit(1)
 
-inputPath = sys.argv[1]
-outputPath = sys.argv[2]
-outFile = sys.argv[3]
+input_path = Path(sys.argv[1])
+output_path = Path(sys.argv[2])
+out_file = Path(sys.argv[3])
+if not out_file.is_absolute():
+  out_file = (output_path / out_file).resolve()
 
-if not inputPath.endswith('/') and not inputPath.endswith('\\'):
-  inputPath += '/'
-
-if not outputPath.endswith('/') and not outputPath.endswith('\\'):
-  outputPath += '/'
-
-os.chdir(outputPath)
-outFile = os.path.abspath(outFile)
 
 dimension = 2
 
-stageTiles = itk.TileConfiguration[dimension]()
-stageTiles.Parse(inputPath + "TileConfiguration.txt")
+stage_tiles = itk.TileConfiguration[dimension]()
+stage_tiles.Parse(str(input_path / "TileConfiguration.txt"))
 
-ScalarPixelType = itk.UC
-OriginalPixelType = itk.RGBPixel[itk.UC]
-ScalarImageType = itk.Image[ScalarPixelType,dimension]
-OriginalImageType = itk.Image[OriginalPixelType,dimension]
+color_images = [] # for mosaic creation
+grayscale_images = [] # for registration
+for t in range(stage_tiles.LinearSize()):
+  origin = stage_tiles.GetTile(t).GetPosition()
+  filename = str(input_path / stage_tiles.GetTile(t).GetFileName())
+  image = itk.imread(filename)
+  spacing = image.GetSpacing()
 
-oImages = [] # RGB images, for mosaic creation
-sImages = [] # grayscale images, for registration
-for t in range(stageTiles.LinearSize()):
-  origin = stageTiles.GetTile(t).GetPosition()
-  filename = inputPath + stageTiles.GetTile(t).GetFileName()
-  image = itk.imread(filename, OriginalPixelType)
-  sp = image.GetSpacing()
   # tile configurations are in pixel (index) coordinates
   # so we convert them into physical ones
   for d in range(dimension):
-    origin[d] *= sp[d]
+    origin[d] *= spacing[d]
 
   image.SetOrigin(origin)
-  oImages.append(image)
+  color_images.append(image)
 
-  image = itk.imread(filename, ScalarPixelType)
+  image = itk.imread(filename, itk.F) # read as grayscale
   image.SetOrigin(origin)
-  sImages.append(image)
+  grayscale_images.append(image)
 
 # only float is wrapped as coordinate representation type in TileMontage
-montage = itk.TileMontage[ScalarImageType, itk.F].New()
-montage.SetMontageSize(stageTiles.GetAxisSizes())
-for t in range(stageTiles.LinearSize()):
-  montage.SetInputTile(t, sImages[t])
+montage = itk.TileMontage[type(grayscale_images[0]), itk.F].New()
+montage.SetMontageSize(stage_tiles.GetAxisSizes())
+for t in range(stage_tiles.LinearSize()):
+  montage.SetInputTile(t, grayscale_images[t])
 
 print("Computing tile registration transforms")
 montage.Update()
 
 print("Writing tile transforms")
-actualTiles = stageTiles # we will update it later
-for t in range(stageTiles.LinearSize()):
-  ind = stageTiles.LinearIndexToNDIndex(t)
-  regTr = montage.GetOutputTransform(ind)
-  tile = stageTiles.GetTile(t)
-  itk.transformwrite([regTr], outputPath + tile.GetFileName() + ".tfm")
+actual_tiles = stage_tiles # we will update it later
+for t in range(stage_tiles.LinearSize()):
+  index = stage_tiles.LinearIndexToNDIndex(t)
+  regTr = montage.GetOutputTransform(index)
+  tile = stage_tiles.GetTile(t)
+  itk.transformwrite([regTr], str(output_path / (tile.GetFileName() + ".tfm")))
 
   # calculate updated positions - transform physical into index shift
   pos = tile.GetPosition()
   for d in range(dimension):
-    pos[d] -= regTr.GetOffset()[d] / sp[d]
+    pos[d] -= regTr.GetOffset()[d] / spacing[d]
   tile.SetPosition(pos)
-  actualTiles.SetTile(t, tile)
-actualTiles.Write(outputPath + "TileConfiguration.registered.txt")
+  actual_tiles.SetTile(t, tile)
+actual_tiles.Write(str(output_path / "TileConfiguration.registered.txt"))
 
 print("Producing the mosaic")
-resampleF = itk.TileMergeImageFilter[OriginalImageType].New()
-resampleF.SetMontageSize(stageTiles.GetAxisSizes())
-for t in range(stageTiles.LinearSize()):
-  resampleF.SetInputTile(t, oImages[t])
-  ind = stageTiles.LinearIndexToNDIndex(t)
-  resampleF.SetTileTransform(ind, montage.GetOutputTransform(ind))
+resampleF = itk.TileMergeImageFilter[type(color_images[0])].New()
+resampleF.SetMontageSize(stage_tiles.GetAxisSizes())
+for t in range(stage_tiles.LinearSize()):
+  resampleF.SetInputTile(t, color_images[t])
+  index = stage_tiles.LinearIndexToNDIndex(t)
+  resampleF.SetTileTransform(index, montage.GetOutputTransform(index))
 resampleF.Update()
-itk.imwrite(resampleF.GetOutput(), outFile)
+itk.imwrite(resampleF.GetOutput(), str(out_file))
 print("Resampling complete")
