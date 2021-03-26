59ebc33c6e835bd8ed52e54e40ee15a7fa24377e
Niels Dekker <N.Dekker@lumc.nl>
2021-03-26 13:21:45 +0100

    ENH: Make itk::Matrix trivially copyable, following Rule of Zero
    Defaulted the default-constructor of `itk::Matrix`, and added a
    GoogleTest, to check that it does properly zero-initialize its elements.
    
    Removed its user-defined copy-constructor and copy-assignment, following
    the C++ "Rule of Zero". Tested that `itk::Matrix` has thereby become
    "trivially copyable" for built-in element types.
    
    Adjusted the constructors `Matrix(const vnl_matrix<T> &)` and
    `Matrix(const InternalMatrixType &)` to call the corresponding
    constructors of the internal matrix type, instead of the
    corresponding assignment operators.

---
+++ Modules/Core/Common/test/itkMatrixGTest.cxx
@@ -0,0 +1,54 @@
+/*=========================================================================
+ *
+ *  Copyright NumFOCUS
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+// First include the header file to be tested:
+#include "itkMatrix.h"
+#include <gtest/gtest.h>
+#include <type_traits> // For is_trivially_copyable.
+
+
+namespace
+{
+template <typename TMatrix>
+void
+Expect_Matrix_default_constructor_zero_initializes_all_elements()
+{
+  const TMatrix defaultConstructedMatrix;
+
+  for (unsigned row{}; row < TMatrix::RowDimensions; ++row)
+  {
+    for (unsigned column{}; column < TMatrix::ColumnDimensions; ++column)
+    {
+      EXPECT_EQ(defaultConstructedMatrix(row, column), 0);
+    }
+  }
+}
+} // namespace
+
+
+static_assert(std::is_trivially_copyable<itk::Matrix<float>>() && std::is_trivially_copyable<itk::Matrix<double>>() &&
+                std::is_trivially_copyable<itk::Matrix<double, 2, 2>>(),
+              "Matrix classes of built-in element types should be trivially copyable!");
+
+
+TEST(Matrix, DefaultConstructorZeroInitializesAllElements)
+{
+  Expect_Matrix_default_constructor_zero_initializes_all_elements<itk::Matrix<float>>();
+  Expect_Matrix_default_constructor_zero_initializes_all_elements<itk::Matrix<double>>();
+  Expect_Matrix_default_constructor_zero_initializes_all_elements<itk::Matrix<double, 2, 2>>();
+}
