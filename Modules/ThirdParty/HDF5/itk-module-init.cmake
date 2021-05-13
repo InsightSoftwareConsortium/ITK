f83a0ba9f45773967a3f5d9eeedb6c941e893592
Bradley Lowekamp <blowekamp@mail.nih.gov>
2021-05-13 19:51:39 +0000

    BUG: Propagate usage of HDF5 find_package NO_MODULE arg to install

--- Modules/ThirdParty/HDF5/itk-module-init.cmake
+++ Modules/ThirdParty/HDF5/itk-module-init.cmake
@@ -2,6 +2,7 @@ option(ITK_USE_SYSTEM_HDF5 "Use an outside build of HDF5." ${ITK_USE_SYSTEM_LIBR
 mark_as_advanced(ITK_USE_SYSTEM_HDF5)
 
 if(ITK_USE_SYSTEM_HDF5)
+  set(HDF5_NO_MODULE 1)
   if(ITK_BUILD_SHARED_LIBS)
     find_package(HDF5 QUIET NO_MODULE COMPONENTS CXX C shared)
   else()
@@ -10,5 +11,6 @@ if(ITK_USE_SYSTEM_HDF5)
 
   if(NOT HDF5_FOUND)
     find_package(HDF5 REQUIRED COMPONENTS CXX C)
+    set(HDF5_NO_MODULE 0)
   endif()
 endif()
