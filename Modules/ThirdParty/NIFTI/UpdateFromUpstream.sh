68221fdd30fb9ba89219a4bd5a47870b2d1cf7e6
Gabriel A. Devenyi <gdevenyi@gmail.com>
2021-07-02 16:13:18 -0400

    ENH: Prepare for update to NIFTILIB3.0.1

--- Modules/ThirdParty/NIFTI/UpdateFromUpstream.sh
+++ Modules/ThirdParty/NIFTI/UpdateFromUpstream.sh
@@ -8,17 +8,16 @@ readonly name="nifti"
 readonly ownership="NIFTI Upstream <nifti@github.com>"
 readonly subtree="Modules/ThirdParty/NIFTI/src/nifti"
 readonly repo="https://github.com/NIFTI-Imaging/nifti_clib.git"
-readonly tag="master"
+readonly tag="v3.0.1"
 readonly shortlog=false
 readonly paths="
 CMakeLists.txt
 CTestConfig.cmake
 LICENSE
-README
+README.md
 cmake
 znzlib
 niftilib
-nifti2
 "
 
 extract_source () {
