9ef180e254b6a50c46f26d08643f1083e57f2412
Matt McCormick <matt.mccormick@kitware.com>
2021-08-13 15:07:06 -0400

    BUG: Initialize C++ standard for remote modules
    We now default to C++14, which is not the default standard on most compilers. Addresses:
    
      Modules/Core/Common/include/itkMacro.h:192:6: error: "Apple LLVM < 5.1 (clang < 3.4) is not supported under ITKv5.3"
    
    When building remote modules externally, which is triggered by too low of a C++ standard.

---
+++ CMake/ITKInitializeCXXStandard.cmake
@@ -0,0 +1,10 @@
+##  Set the default target properties for ITK
+if(NOT CMAKE_CXX_STANDARD)
+  set(CMAKE_CXX_STANDARD 14) # Supported values are 14, 17, 20, and 23.
+endif()
+if(NOT CMAKE_CXX_STANDARD_REQUIRED)
+  set(CMAKE_CXX_STANDARD_REQUIRED ON)
+endif()
+if(NOT CMAKE_CXX_EXTENSIONS)
+  set(CMAKE_CXX_EXTENSIONS OFF)
+endif()
