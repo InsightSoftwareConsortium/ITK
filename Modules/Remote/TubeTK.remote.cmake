04a50e4cd652b3d945da530a6bdbcc4c6b61a808
Stephen R. Aylward <stephen.aylward@kitware.com>
2021-10-15 10:43:16 -0400

    ENH: Bump TubeTK to v1.0.0

--- Modules/Remote/TubeTK.remote.cmake
+++ Modules/Remote/TubeTK.remote.cmake
@@ -48,5 +48,5 @@ itk_fetch_module(TubeTK
   "http://www.tubetk.org"
   MODULE_COMPLIANCE_LEVEL 3
   GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKTubeTK.git
-  GIT_TAG 06b083a7158be433f55ccc05bbfcafe124f94fde
+  GIT_TAG cee053ac95743ed6d7a879465ac2d4ddd4beb04f
   )
