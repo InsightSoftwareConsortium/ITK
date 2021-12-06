InsightSoftwareConsortium/ITK

commit 61e2be7e6dd666ccf6a7db2f6175bef310381927

PERF: Improve MatrixOffset Jacobian computation performance
Use Array2D constructor from vnl_matrix to perform efficient copy of
the bulk matrix transform via std::copy.

Change-Id: Ib4d253de1a592693ed253a3e7e95ddbe21df76c4

Modules/Core/Transform/include/itkMatrixOffsetTransformBase.hxx
@@ -683,13 +683,7 @@ MatrixOffsetTransformBase<TParametersValueType, NInputDimensions, NOutputDimensi
                                        JacobianType & jac) const
 {
   jac.SetSize( MatrixType::RowDimensions, MatrixType::ColumnDimensions );
-  for( unsigned int i = 0; i < MatrixType::RowDimensions; i++ )
-    {
-    for( unsigned int j = 0; j < MatrixType::ColumnDimensions; j++ )
-      {
-      jac[i][j] = this->GetMatrix()[i][j];
-      }
-    }
+  jac = this->GetMatrix().GetVnlMatrix();
 }
 
 
@@ -701,13 +695,7 @@ MatrixOffsetTransformBase<TParametersValueType, NInputDimensions, NOutputDimensi
                                        JacobianType & jac) const
 {
   jac.SetSize( MatrixType::ColumnDimensions, MatrixType::RowDimensions );
-  for( unsigned int i = 0; i < MatrixType::ColumnDimensions; i++ )
-    {
-    for( unsigned int j = 0; j < MatrixType::RowDimensions; j++ )
-      {
-      jac[i][j] = this->GetInverseMatrix()[i][j];
-      }
-    }
+  jac = this->GetMatrix().GetVnlMatrix();
 }
 
 

commit 9111b93afa6a2dcb6a466fcfd42f7d11d2d302f6

BUG: ComputeInverseJacobianWithRespectToPosition didn't do inversion
itk::MatrixOffsetTransformBase::ComputeInverseJacobianWithRespectToPosition was returning directly the matrix (the same as ComputeJacobianWithRespectToPosition). It has been corrected to return the inverse matrix.

This correction has required also the correction of a few function return and parameters' Types.
Those corrected Types were inconsistent when instantiated with InputImage and OutputImage of different dimensions.

Apparently the bug was inadvertently introduced in
61e2be7e6dd666ccf6a7db2f6175bef310381927 (PERF: Improve MatrixOffset Jacobian computation performance)
and subsequently not fixed but got mixed wrong JacobianTypes in
212cae522d8451ea089c41f8a151279e1dd17042 (PERF: Change type of Jacobian w.r. position to vnl_matrix_fixed).

Modules/Core/Transform/include/itkMatrixOffsetTransformBase.h
@@ -197,6 +197,11 @@ public:
   using InverseTransformBaseType = typename Superclass::InverseTransformBaseType;
   using InverseTransformBasePointer = typename InverseTransformBaseType::Pointer;
 
+  using InverseTransformType = MatrixOffsetTransformBase<TParametersValueType, NOutputDimensions, NInputDimensions>;
+  /** InverseTransformType must be a friend to allow the generation of a transformation
+   * from its inverse (function GetInverse). */
+  friend class MatrixOffsetTransformBase<TParametersValueType, NOutputDimensions, NInputDimensions>;
+
   /** Set the transformation to an Identity
    *
    * This sets the matrix to identity and the Offset to null. */
@@ -471,7 +476,7 @@ public:
    *
    */
   bool
-  GetInverse(Self * inverse) const;
+  GetInverse(InverseTransformType * inverse) const;
 
   /** Return an inverse of this transform. */
   InverseTransformBasePointer

Modules/Core/Transform/include/itkMatrixOffsetTransformBase.hxx
@@ -417,7 +417,8 @@ MatrixOffsetTransformBase<TParametersValueType, NInputDimensions, NOutputDimensi
 
 template <typename TParametersValueType, unsigned int NInputDimensions, unsigned int NOutputDimensions>
 bool
-MatrixOffsetTransformBase<TParametersValueType, NInputDimensions, NOutputDimensions>::GetInverse(Self * inverse) const
+MatrixOffsetTransformBase<TParametersValueType, NInputDimensions, NOutputDimensions>::GetInverse(
+  InverseTransformType * inverse) const
 {
   if (!inverse)
   {
@@ -446,7 +447,7 @@ typename MatrixOffsetTransformBase<TParametersValueType, NInputDimensions, NOutp
   InverseTransformBasePointer
   MatrixOffsetTransformBase<TParametersValueType, NInputDimensions, NOutputDimensions>::GetInverseTransform() const
 {
-  Pointer inv = New();
+  auto inv = InverseTransformType::New();
 
   return GetInverse(inv) ? inv.GetPointer() : nullptr;
 }
@@ -604,7 +605,7 @@ void
 MatrixOffsetTransformBase<TParametersValueType, NInputDimensions, NOutputDimensions>::
   ComputeInverseJacobianWithRespectToPosition(const InputPointType &, InverseJacobianPositionType & jac) const
 {
-  jac = this->GetMatrix().GetVnlMatrix();
+  jac = this->GetInverseMatrix().GetVnlMatrix();
 }
 
 