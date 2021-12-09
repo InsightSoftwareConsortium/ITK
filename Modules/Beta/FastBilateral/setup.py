17071a0f191318aba35b8b67e38dce8b5c98c57c
Pablo Hernandez-Cerdan <pablo.hernandez.cerdan@outlook.com>
2021-12-09 21:09:41 +0100

    BUG: Fix setup.py

--- Modules/Beta/FastBilateral/setup.py
+++ Modules/Beta/FastBilateral/setup.py
@@ -17,9 +17,9 @@ setup(
     author_email='pablo.hernandez.cerdan@outlook.com',
     packages=['itk'],
     package_dir={'itk': 'itk'},
-    download_url=r'https://github.com/InsightSoftwareConsortium/FastBilateral',
-    description=r'This is a template that serves as a starting point for a new module.',
-    long_description='ITK is an open-source, cross-platform library that provides developers with an extensive suite of software tools for image analysis. Developed through extreme programming methodologies, ITK employs leading-edge algorithms for registering and segmenting multidimensional scientific images.',
+    download_url=r'https://github.com/InsightSoftwareConsortium/ITKFastBilateral',
+    description=r'Faster implementation of the itkBilateralImageFilter. From the insight journal:.',
+    long_description='Faster implementation of the itkBilateralImageFilter. From the insight journal submission: https://www.insight-journal.org/browse/publication/692.',
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
