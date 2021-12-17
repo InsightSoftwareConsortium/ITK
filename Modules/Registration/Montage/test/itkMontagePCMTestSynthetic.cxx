5e94388029e0b6d22e0b86f0660e7985205874de
Hans Johnson <hans-johnson@uiowa.edu>
2021-12-17 14:28:08 -0600

    STYLE: Match clang-format style

--- Modules/Registration/Montage/test/itkMontagePCMTestSynthetic.cxx
+++ Modules/Registration/Montage/test/itkMontagePCMTestSynthetic.cxx
@@ -271,7 +271,8 @@ PhaseCorrelationRegistration(int argc, char * argv[])
           // All other parameters must be 0
           for (unsigned int i = numberOfParameters; i < VDimension; i++)
           {
-            if ((itk::Math::abs(finalParameters[i]) > tolerance) || (itk::Math::abs(transformParameters[i]) > tolerance))
+            if ((itk::Math::abs(finalParameters[i]) > tolerance) ||
+                (itk::Math::abs(transformParameters[i]) > tolerance))
             {
               std::cout << "Tolerance exceeded at component " << i << std::endl;
               pass = false;
