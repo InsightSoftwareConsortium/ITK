1151f6e1fba9dd84b5772dd690fe04f15742dac0
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2021-12-27 13:29:40 -0500

    ENH: Add boolean macros to `itk::SpatialObjectWriter` boolean ivars
    Add boolean macros to `itk::SpatialObjectWriter` boolean ivars.
    
    Exercise the boolean macros and the boolean ivar Set/Get methods.
    
    Take advantage of the commit to simplify and improve the style and
    readability around such testing: use the `ITK_TRY_EXPECT_NO_EXCEPTION`
    macro to avoid boilerplate code when updating a filter that can trigger
    exceptions, and take out the rest of the statements of the `try/catch`
    block.

--- Modules/IO/SpatialObjects/include/itkSpatialObjectWriter.h
+++ Modules/IO/SpatialObjects/include/itkSpatialObjectWriter.h
@@ -79,6 +79,7 @@ public:
 
   itkSetMacro(BinaryPoints, bool);
   itkGetConstMacro(BinaryPoints, bool);
+  itkBooleanMacro(BinaryPoints);
 
   void
   SetTransformPrecision(unsigned int precision);
@@ -89,6 +90,7 @@ public:
   /** Set/Get if the images should be written in a different file */
   itkSetMacro(WriteImagesInSeparateFile, bool);
   itkGetConstMacro(WriteImagesInSeparateFile, bool);
+  itkBooleanMacro(WriteImagesInSeparateFile);
 
   /** Add a converter for a new MetaObject/SpatialObject type */
   void
