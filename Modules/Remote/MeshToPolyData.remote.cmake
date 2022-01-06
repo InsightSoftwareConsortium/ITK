8ecef7b8a0dcb1aa14af0ec5a787007a7be3a0e1
Matt McCormick <matt.mccormick@kitware.com>
2022-01-06 09:46:44 -0500

    ENH: Add MeshToPolyData remote module
    This module converts an ITK Mesh to a simple data structure compatible with vtkPolyData.

---
+++ Modules/Remote/MeshToPolyData.remote.cmake
@@ -0,0 +1,49 @@
+#-- # Grading Level Criteria Report
+#-- EVALUATION DATE: 2022-01-06
+#-- EVALUATORS: [Matt McCormick]
+#--
+#-- ## Compliance level 5 star (AKA ITK main modules, or remote modules that could become core modules)
+#--   - [ ] Widespread community dependance
+#--   - [ ] Above 90% code coverage
+#--   - [ ] CI dashboards and testing monitored rigorously
+#--   - [X] Key API features are exposed in wrapping interface
+#--   - [ ] All requirements of Levels 4,3,2,1
+#--
+#-- ## Compliance Level 4 star (Very high-quality code, perhaps small community dependance)
+#--   - [X] Meets all ITK code style standards
+#--   - [X] No external requirements beyond those needed by ITK proper
+#--   - [X] Builds and passes tests on all supported platforms within 1 month of each core tagged release
+#--            - [X] Windows Shared Library Build with Visual Studio
+#--            - [X] Mac with clang compiller
+#--            - [X] Linux with gcc compiler
+#--   - [X] Active developer community dedicated to maintaining code-base
+#--   - [ ] 75% code coverage demonstrated for testing suite
+#--   - [X] Continuous integration testing performed
+#--   - [X] All requirements of Levels 3,2,1
+#--
+#-- ## Compliance Level 3 star (Quality beta code)
+#--   - [X] API | executable interface is considered mostly stable and feature complete
+#--   - [X] 10% C0-code coverage demonstrated for testing suite
+#--   - [X] Some tests exist and pass on at least some platform
+#--   - [X] All requirements of Levels 2,1
+#--
+#-- ## Compliance Level 2 star (Alpha code feature API development or niche community/exectution environment dependance )
+#--   - [X] Compiles for at least 1 niche set of execution envirionments, and perhaps others
+#--         (may depend on specific external tools like a java environment, or specific external libraries to work )
+#--   - [X] All requirements of Levels 1
+#--
+#-- ## Compliance Level 1 star (Pre-alpha features under development and code of unkown quality)
+#--   - [X] Code complies on at least 1 platform
+#--
+#-- ## Compliance Level 0 star ( Code/Feature of known poor-quality or deprecated status )
+#--   - [ ] Code reviewed and explicitly identified as not recommended for use
+#--
+#-- ### Please document here any justification for the criteria above
+#       Code style enforced by clang-format on 2020-02-19, and clang-tidy modernizations completed
+
+itk_fetch_module(MeshToPolyData
+  "Convert an ITK Mesh to a simple data structure compatible with vtkPolyData."
+  MODULE_COMPLIANCE_LEVEL 3
+  GIT_REPOSITORY ${git_protocol}://github.com/InsightSoftwareConsortium/ITKMeshToPolyData.git
+  GIT_TAG 12bedef0f6443bf122f2d5eb856dab196a453fe1
+)
