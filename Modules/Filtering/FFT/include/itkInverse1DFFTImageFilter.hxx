3671be23b861edc7c671b0a481b26148ad20dbd1
Hans Johnson <hans-johnson@uiowa.edu>
2022-02-04 13:09:36 -0600

    STYLE: Prefer = default to explicitly trivial implementations
    This check replaces default bodies of special member functions with
    = default;. The explicitly defaulted function declarations enable more
    opportunities in optimization, because the compiler might treat
    explicitly defaulted functions as trivial.
    
    Additionally, the C++11 use of = default more clearly expreses the
    intent for the special member functions.

--- Modules/Filtering/FFT/include/itkInverse1DFFTImageFilter.hxx
+++ Modules/Filtering/FFT/include/itkInverse1DFFTImageFilter.hxx
@@ -27,7 +27,7 @@ namespace itk
 template <typename TInputImage, typename TOutputImage>
 Inverse1DFFTImageFilter<TInputImage, TOutputImage>::Inverse1DFFTImageFilter()
 
-{}
+  = default;
 
 
 template <typename TInputImage, typename TOutputImage>
