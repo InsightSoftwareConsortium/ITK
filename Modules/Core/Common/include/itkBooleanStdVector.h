19a9b72557c6e598803c9df9249568364a1746b9
Niels Dekker <N.Dekker@lumc.nl>
2022-02-08 18:21:01 +0100

    ENH: Add itk::BooleanStdVectorType as alternative to `std::vector<bool>`
    Eases working around problems caused by the fact that `std::vector<bool>`
    may have "a space-optimized representation of bits", according to the
    C++ Standard.

---
+++ Modules/Core/Common/include/itkBooleanStdVector.h
@@ -0,0 +1,41 @@
+/*=========================================================================
+ *
+ *  Copyright NumFOCUS
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef itkBooleanStdVector_h
+#define itkBooleanStdVector_h
+
+#include "itkIntTypes.h"
+#include <vector>
+
+namespace itk
+{
+
+/** The type alias `BooleanStdVectorType` provides an alternative to `std::vector<bool>`. `std::vector<bool>` is not
+ * thread safe due to the possibility of multiple bits being packed together in the same memory location.
+ * BooleanStdVectorType does not have such a space optimization. BooleanStdVectorType is semantically like
+ * `std::vector<bool>`, but unlike `std::vector<bool>`, it does "avoid data races when the contents of the contained
+ * object in different elements in the same container [...] are modified concurrently", according to the C++ Standard,
+ * section [container.requirements.dataraces], "Container data races".
+ *
+ * \note BooleanStdVectorType is only intended to be used for the storage of Boolean values (true and false), not for
+ * arbitrary integer values.
+ */
+using BooleanStdVectorType = std::vector<uint8_t>;
+
+} // namespace itk
+
+#endif
---
+++ Modules/Core/Common/test/itkBooleanStdVectorGTest.cxx
@@ -0,0 +1,43 @@
+/*=========================================================================
+ *
+ *  Copyright NumFOCUS
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+// First include the header file to be tested:
+#include "itkBooleanStdVector.h"
+
+#include <type_traits>
+
+namespace
+{
+using ValueType = itk::BooleanStdVectorType::value_type;
+
+static_assert(sizeof(ValueType) <= sizeof(bool),
+              "The value type of BooleanStdVectorType should not be larger than the built-in C++ bool type.");
+static_assert(
+  ValueType{ false } == false,
+  "The value type of BooleanStdVectorType should be allowed as a conditional expression, evaluating to false.");
+static_assert(
+  ValueType{ true } == true,
+  "The value type of BooleanStdVectorType should be allowed as a conditional expression, evaluating to true.");
+static_assert(bool{ ValueType{ false } } == false,
+              "The value type of BooleanStdVectorType should support a lossless round-trip from bool value false.");
+static_assert(bool{ ValueType{ true } } == true,
+              "The value type of BooleanStdVectorType should support a lossless round-trip from bool value true.");
+static_assert(std::is_same<decltype(*itk::BooleanStdVectorType{}.data()), ValueType &>::value,
+              "BooleanStdVectorType should have a valid data() member function (unlike std::vector<bool>).");
+
+} // namespace
