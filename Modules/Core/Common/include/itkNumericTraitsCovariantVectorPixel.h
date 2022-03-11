908a29aa3c729d819d0ac08681334b399e5ee1b2
Niels Dekker <N.Dekker@lumc.nl>
2022-03-11 18:46:47 +0100

    STYLE: Use MakeFilled in any fixed array `NumericTraits` specialization
    Uses MakeFilled in the `NumericTraits` specializations of "fixed array"
    classes: CovariantVector, DiffusionTensor3D, FixedArray, and
    SymmetricSecondRankTensor.
    
    Follow-up to:
    pull request https://github.com/InsightSoftwareConsortium/ITK/pull/3262
    commit 82001f3024cbc879fce5a68b5f69f6bb5d5c9ea7
    "STYLE: Use MakeFilled, () or {}, instead of `Vector(const ValueType &)`"

--- Modules/Core/Common/include/itkNumericTraitsCovariantVectorPixel.h
+++ Modules/Core/Common/include/itkNumericTraitsCovariantVectorPixel.h
@@ -86,43 +86,43 @@ public:
   static const Self
   max(const Self &)
   {
-    return Self(NumericTraits<T>::max());
+    return MakeFilled<Self>(NumericTraits<T>::max());
   }
 
   static const Self
   min(const Self &)
   {
-    return Self(NumericTraits<T>::min());
+    return MakeFilled<Self>(NumericTraits<T>::min());
   }
 
   static const Self
   max()
   {
-    return Self(NumericTraits<T>::max());
+    return MakeFilled<Self>(NumericTraits<T>::max());
   }
 
   static const Self
   min()
   {
-    return Self(NumericTraits<T>::min());
+    return MakeFilled<Self>(NumericTraits<T>::min());
   }
 
   static const Self
   NonpositiveMin()
   {
-    return Self(NumericTraits<T>::NonpositiveMin());
+    return MakeFilled<Self>(NumericTraits<T>::NonpositiveMin());
   }
 
   static const Self
   ZeroValue()
   {
-    return Self(NumericTraits<T>::ZeroValue());
+    return MakeFilled<Self>(NumericTraits<T>::ZeroValue());
   }
 
   static const Self
   OneValue()
   {
-    return Self(NumericTraits<T>::OneValue());
+    return MakeFilled<Self>(NumericTraits<T>::OneValue());
   }
 
   static const Self
