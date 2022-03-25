b8285fb20cd29438e802c5858f29cfd91b6c6272
Gabriel A. Devenyi <gdevenyi@gmail.com>
2022-03-25 19:05:52 -0400

    BUG: Fix isAffine insuffcient precision in caluclations
    Fixes #3333
    
    Co-authored-by: Vladimir S. FONOV <vladimir.fonov@gmail.com>

---
+++ Modules/IO/NIFTI/test/itkNiftiImageIOTest13.cxx
@@ -0,0 +1,52 @@
+/*=========================================================================
+ *
+ *  Copyright NumFOCUS
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#include "itkNiftiImageIOTest.h"
+
+// Test to read small voxel NIFTI file which was triggering numerical instability
+// in IsAffine loading code
+int
+itkNiftiImageIOTest13(int argc, char * argv[])
+{
+  if (argc != 2)
+  {
+    return EXIT_FAILURE;
+  }
+
+  constexpr unsigned int Dimension = 3;
+
+  using PixelType = float;
+  using ImageType = itk::Image<PixelType, Dimension>;
+
+  try
+  {
+    using ReaderType = itk::ImageFileReader<ImageType>;
+    ReaderType::Pointer reader = ReaderType::New();
+    reader->SetFileName(argv[1]);
+    reader->Update();
+    ImageType::Pointer image = reader->GetOutput();
+  }
+  catch (const itk::ExceptionObject & err)
+  {
+    std::cerr << "ExceptionObject caught !" << std::endl;
+    std::cerr << err << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
