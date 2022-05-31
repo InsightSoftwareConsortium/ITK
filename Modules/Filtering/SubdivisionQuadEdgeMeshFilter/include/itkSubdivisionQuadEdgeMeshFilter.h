6bedfe5d5d5520afa20ca682a6cdfc764cc2384f
Tom Birdsong <tom.birdsong@kitware.com>
2022-05-31 13:40:33 -0400

    ENH: Bump ITK and change http to https

--- Modules/Filtering/SubdivisionQuadEdgeMeshFilter/include/itkSubdivisionQuadEdgeMeshFilter.h
+++ Modules/Filtering/SubdivisionQuadEdgeMeshFilter/include/itkSubdivisionQuadEdgeMeshFilter.h
@@ -6,7 +6,7 @@
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *         https://www.apache.org/licenses/LICENSE-2.0.txt
  *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
@@ -32,7 +32,7 @@ namespace itk
  * Code imported from Insight Journal publication:
  *
  * Wanlin Zhu, Triangle Mesh Subdivision
- * http://hdl.handle.net/10380/3307
+ * https://hdl.handle.net/10380/3307
  *
  * \ingroup SubdivisionQuadEdgeMeshFilter
  */
