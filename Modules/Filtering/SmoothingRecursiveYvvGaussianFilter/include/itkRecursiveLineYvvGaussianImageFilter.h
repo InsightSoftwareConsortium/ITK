d88504395d15dc593be9124314bc3d4c692e4d1f
Tom Birdsong <tom.birdsong@kitware.com>
2022-05-31 13:29:51 -0400

    ENH: Bump ITK version and change http links to https

--- Modules/Filtering/SmoothingRecursiveYvvGaussianFilter/include/itkRecursiveLineYvvGaussianImageFilter.h
+++ Modules/Filtering/SmoothingRecursiveYvvGaussianFilter/include/itkRecursiveLineYvvGaussianImageFilter.h
@@ -6,7 +6,7 @@
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *         https://www.apache.org/licenses/LICENSE-2.0.txt
  *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
@@ -36,7 +36,7 @@ namespace itk
  *  performs better for your particular hardware configuration.
  *
  *  More information in the Insight Journal publication:
- *  http://hdl.handle.net/10380/3425
+ *  https://hdl.handle.net/10380/3425
  *
  * \ingroup SmoothingRecursiveYvvGaussianFilter
  */
