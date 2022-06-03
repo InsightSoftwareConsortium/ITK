365fbdd5f148f41d83baeea5f90deb6ce95d5399
Tom Birdsong <40648863+tbirdso@users.noreply.github.com>
2022-06-03 15:13:06 -0400

    Bump version in setup.py

--- Modules/Beta/MeshToPolyData/setup.py
+++ Modules/Beta/MeshToPolyData/setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
     name='itk-meshtopolydata',
-    version='0.8.1',
+    version='0.8.2',
     author='Insight Software Consortium',
     author_email='itk+community@discourse.itk.org',
     packages=['itk'],
@@ -44,7 +44,7 @@ setup(
     keywords='ITK InsightToolkit',
     url=r'https://itk.org/',
     install_requires=[
-        r'itk-core>=5.2.0.post2',
+        r'itk-core>=5.3rc04',
         r'numpy',
     ]
     )
