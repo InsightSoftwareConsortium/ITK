InsightSoftwareConsortium/ITK

commit c173dfd803acf943d9ab80683ac4133fea29cdf5

STYLE: Remove `std::` prefix from types that work without it
This is accomplished with
cd git/ITK
find * -type f |
  egrep '\.(c|cc|cxx|cpp|h|hh|hxx|hpp)$' |
  fgrep -v ThirdParty |
  fgrep -v itkIntTypes.h |
  xargs sed -i -r -e \
    's/std::((size|ptrdiff|max_align|u?int(_fast|_least|max|ptr)?[0-9]*)_t)/\1/g'

Modules/Compatibility/Deprecated/include/itkAtomicInt.h
@@ -203,13 +203,13 @@ public:
   }
 
   T *
-  operator+=(std::ptrdiff_t val)
+  operator+=(ptrdiff_t val)
   {
     return reinterpret_cast<T *>(m_Object += val * sizeof(T));
   }
 
   T *
-  operator-=(std::ptrdiff_t val)
+  operator-=(ptrdiff_t val)
   {
     return reinterpret_cast<T *>(m_Object -= val * sizeof(T));
   }

Modules/Core/Common/include/itkIndex.h
@@ -326,7 +326,7 @@ public:
   using iterator = value_type *;
   using const_iterator = const value_type *;
   using size_type = unsigned int;
-  using difference_type = std::ptrdiff_t;
+  using difference_type = ptrdiff_t;
   using reverse_iterator = std::reverse_iterator<iterator>;
   using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 

Modules/Core/Common/include/itkOffset.h
@@ -279,7 +279,7 @@ public:
   using iterator = value_type *;
   using const_iterator = const value_type *;
   using size_type = unsigned int;
-  using difference_type = std::ptrdiff_t;
+  using difference_type = ptrdiff_t;
   using reverse_iterator = std::reverse_iterator<iterator>;
   using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 

Modules/Core/Common/include/itkSize.h
@@ -238,7 +238,7 @@ public:
   using iterator = value_type *;
   using const_iterator = const value_type *;
   using size_type = unsigned int;
-  using difference_type = std::ptrdiff_t;
+  using difference_type = ptrdiff_t;
   using reverse_iterator = std::reverse_iterator<iterator>;
   using const_reverse_iterator = std::reverse_iterator<const_iterator>;
 

Examples/IO/DicomSeriesReadSeriesWrite.cxx
@@ -143,7 +143,7 @@ main(int argc, char * argv[])
     namesGenerator->GetInputFileNames();
   // Software Guide : EndCodeSnippet
 
-  std::size_t numberOfFileNames = filenames.size();
+  size_t numberOfFileNames = filenames.size();
   std::cout << numberOfFileNames << std::endl;
   for (unsigned int fni = 0; fni < numberOfFileNames; ++fni)
   {

Examples/IO/IOFactoryRegistration.cxx
@@ -26,7 +26,7 @@ main()
 {
   const std::list<itk::ObjectFactoryBase *> & factories =
     itk::ObjectFactoryBase::GetRegisteredFactories();
-  const std::size_t numFactories = factories.size();
+  const size_t numFactories = factories.size();
 
   std::cout << numFactories << " Image IO factories registered:" << std::endl;
 

Examples/IO/IOPlugin.cxx
@@ -58,7 +58,7 @@ main(int argc, char * argv[])
   // List all registered factories
   std::list<itk::ObjectFactoryBase *> factories =
     itk::ObjectFactoryBase::GetRegisteredFactories();
-  const std::size_t numFactories = factories.size();
+  const size_t numFactories = factories.size();
 
   std::cout << "----- Registered factories -----" << std::endl;
   std::cout << "Count: " << numFactories << std::endl;

Modules/Core/Common/include/itkConnectedImageNeighborhoodShape.h
@@ -57,7 +57,7 @@ namespace itk
  * neighborhood shape, including the center pixel, and asserts that the result
  * is as expected:
    \code
-   std::size_t maximumCityblockDistance = 2;
+   size_t maximumCityblockDistance = 2;
    bool includeCenterPixel = true;
    ConnectedImageNeighborhoodShape<3> shape{ maximumCityblockDistance, includeCenterPixel };
    std::vector<Offset<3>> offsets = GenerateImageNeighborhoodOffsets(shape);
@@ -110,8 +110,8 @@ public:
    * The parameter 'includeCenterPixel' specifies whether or not the center
    * pixel (offset zero) should be included with the offsets for this shape.
    */
-  constexpr explicit ConnectedImageNeighborhoodShape(const std::size_t maximumCityblockDistance,
-                                                     const bool        includeCenterPixel) noexcept
+  constexpr explicit ConnectedImageNeighborhoodShape(const size_t maximumCityblockDistance,
+                                                     const bool   includeCenterPixel) noexcept
     : m_MaximumCityblockDistance{ maximumCityblockDistance }
     , m_IncludeCenterPixel{ includeCenterPixel }
     , m_NumberOfOffsets{ CalculateNumberOfOffsets(maximumCityblockDistance, includeCenterPixel) }
@@ -119,7 +119,7 @@ public:
 
 
   /** Returns the number of offsets needed for this shape. */
-  constexpr std::size_t
+  constexpr size_t
   GetNumberOfOffsets() const noexcept
   {
     return m_NumberOfOffsets;
@@ -136,12 +136,12 @@ public:
       Offset<ImageDimension> offset;
       std::fill_n(offset.begin(), ImageDimension, -1);
 
-      std::size_t i = 0;
+      size_t i = 0;
 
       while (i < m_NumberOfOffsets)
       {
-        const std::size_t numberOfNonZeroOffsetValues =
-          ImageDimension - static_cast<std::size_t>(std::count(offset.begin(), offset.end(), 0));
+        const size_t numberOfNonZeroOffsetValues =
+          ImageDimension - static_cast<size_t>(std::count(offset.begin(), offset.end(), 0));
 
         if ((m_IncludeCenterPixel || (numberOfNonZeroOffsetValues > 0)) &&
             (numberOfNonZeroOffsetValues <= m_MaximumCityblockDistance))
@@ -170,20 +170,20 @@ public:
 private:
   // The maximum city-block distance (Manhattan distance) between the center
   // pixel and each connected neighbor pixel.
-  std::size_t m_MaximumCityblockDistance;
+  size_t m_MaximumCityblockDistance;
 
   // Specifies whether or not the center pixel (offset zero) should be included
   // with the offsets for this shape.
   bool m_IncludeCenterPixel;
 
   // The number of offsets needed to represent this shape.
-  std::size_t m_NumberOfOffsets;
+  size_t m_NumberOfOffsets;
 
 
   // Calculates a + b. Numeric overflow triggers a compilation error in
   // "constexpr context" and a debug assert failure at run-time.
-  static constexpr std::uintmax_t
-  CalculateSum(const std::uintmax_t a, const std::uintmax_t b) noexcept
+  static constexpr uintmax_t
+  CalculateSum(const uintmax_t a, const uintmax_t b) noexcept
   {
     return ((a + b) >= a) && ((a + b) >= b) ? (a + b) : (ITK_X_ASSERT(!"CalculateSum overflow!"), 0);
   }
@@ -191,11 +191,11 @@ private:
 
   // Calculates 2 ^ n. Numeric overflow triggers a compilation error in
   // "constexpr context" and a debug assert failure at run-time.
-  static constexpr std::uintmax_t
-  CalculatePowerOfTwo(const std::size_t n) noexcept
+  static constexpr uintmax_t
+  CalculatePowerOfTwo(const size_t n) noexcept
   {
-    return (n < std::numeric_limits<std::uintmax_t>::digits) ? (std::uintmax_t{ 1 } << n)
-                                                             : (ITK_X_ASSERT(!"CalculatePowerOfTwo overflow!"), 0);
+    return (n < std::numeric_limits<uintmax_t>::digits) ? (uintmax_t{ 1 } << n)
+                                                        : (ITK_X_ASSERT(!"CalculatePowerOfTwo overflow!"), 0);
   }
 
 
@@ -203,8 +203,8 @@ private:
   // Inspired by the 'binom' function from Walter, June 23, 2017:
   // https://stackoverflow.com/questions/44718971/calculate-binomial-coffeficient-very-reliable/44719165#44719165
   // Optimized for small values of 'k' (k <= n/2).
-  static constexpr std::uintmax_t
-  CalculateBinomialCoefficient(const std::uintmax_t n, const std::uintmax_t k) noexcept
+  static constexpr uintmax_t
+  CalculateBinomialCoefficient(const uintmax_t n, const uintmax_t k) noexcept
   {
     return (k > n) ? (ITK_X_ASSERT(!"Out of range!"), 0)
                    : (k == 0) ? 1 : Math::UnsignedProduct(n, CalculateBinomialCoefficient(n - 1, k - 1)) / k;
@@ -214,8 +214,8 @@ private:
   // Calculates the number of m-dimensional hypercubes on the boundary of an
   // n-cube, as described at https://en.wikipedia.org/wiki/Hypercube#Elements
   // (Which refers to H.S.M. Coxeter, Regular polytopes, 3rd ed., 1973, p.120.)
-  static constexpr std::size_t
-  CalculateNumberOfHypercubesOnBoundaryOfCube(const std::size_t m, const std::size_t n) noexcept
+  static constexpr size_t
+  CalculateNumberOfHypercubesOnBoundaryOfCube(const size_t m, const size_t n) noexcept
   {
     // Calculate 2^(n-m) * BinomialCoefficient(n, m)
     return Math::UnsignedProduct(CalculatePowerOfTwo(n - m),
@@ -229,8 +229,8 @@ private:
 
 
   // Iterates recursively from i = ImageDimension-1 down to m (inclusive).
-  static constexpr std::size_t
-  CalculateSumOfNumberOfHypercubesOnBoundaryOfCube(const std::size_t i, const std::size_t m) noexcept
+  static constexpr size_t
+  CalculateSumOfNumberOfHypercubesOnBoundaryOfCube(const size_t i, const size_t m) noexcept
   {
     return ITK_X_ASSERT(i >= m),
            CalculateSum(CalculateNumberOfHypercubesOnBoundaryOfCube(i, ImageDimension),
@@ -239,8 +239,8 @@ private:
 
 
   /** Calculates the number of neighbors connected to the center pixel. */
-  static constexpr std::size_t
-  CalculateNumberOfConnectedNeighbors(const std::size_t maximumCityblockDistance) noexcept
+  static constexpr size_t
+  CalculateNumberOfConnectedNeighbors(const size_t maximumCityblockDistance) noexcept
   {
     return (((maximumCityblockDistance == 0) || (ImageDimension == 0))
               ? 0
@@ -252,8 +252,8 @@ private:
 
 
   /** Calculates the number of offsets needed for this shape. */
-  static constexpr std::size_t
-  CalculateNumberOfOffsets(const std::size_t maximumCityblockDistance, const bool includeCenterPixel) noexcept
+  static constexpr size_t
+  CalculateNumberOfOffsets(const size_t maximumCityblockDistance, const bool includeCenterPixel) noexcept
   {
     return (includeCenterPixel ? 1 : 0) + CalculateNumberOfConnectedNeighbors(maximumCityblockDistance);
   }
@@ -261,7 +261,7 @@ private:
 
 
 /** Generates the offsets for a connected image neighborhood shape. */
-template <unsigned int VImageDimension, std::size_t VMaximumCityblockDistance, bool VIncludeCenterPixel>
+template <unsigned int VImageDimension, size_t VMaximumCityblockDistance, bool VIncludeCenterPixel>
 auto
 GenerateConnectedImageNeighborhoodShapeOffsets() noexcept
 {

commit 056325814a4e11c1109845ab5bae572d96da2eee

COMP: Fix older gcc build error including "cstddef" for "ptrdiff_t"
This commit fixes a regression likely introduced in c173dfd80 (STYLE: Remove
`std::` prefix from types that work without it) to support building ITK
wheels using manylinux_2_24 providing gcc 6.3.

It fixes build error like the following:

  In file included from /work/ITK-source/ITK/Modules/Core/Common/include/itkImageRegion.h:33:0,
                   from /work/ITK-source/ITK/Modules/Core/Common/include/itkMultiThreaderBase.h:35,
                   from /work/ITK-source/ITK/Modules/Core/Common/src/itkTotalProgressReporter.cxx:19:
  /work/ITK-source/ITK/Modules/Core/Common/include/itkSize.h:242:27: error: ‘ptrdiff_t’ does not name a type
     using difference_type = ptrdiff_t;
                             ^~~~~~~~~

Modules/Compatibility/Deprecated/include/itkAtomicInt.h
@@ -39,6 +39,7 @@
 #include "itkConceptChecking.h"
 
 #include <atomic>
+#include <cstddef> // For ptrdiff_t.
 
 
 namespace itk

Modules/Core/Common/include/itkIndex.h
@@ -20,6 +20,8 @@
 
 #include "itkMakeFilled.h"
 #include "itkOffset.h"
+
+#include <cstddef>     // For ptrdiff_t.
 #include <type_traits> // For is_integral.
 
 namespace itk

Modules/Core/Common/include/itkOffset.h
@@ -21,6 +21,8 @@
 #include "itkSize.h"
 #include "itkMath.h"
 
+#include <cstddef> // For ptrdiff_t.
+
 namespace itk
 {
 

Modules/Core/Common/include/itkSize.h
@@ -22,6 +22,7 @@
 #include "itkMacro.h"
 #include "itkMakeFilled.h"
 #include <algorithm>   // For copy_n.
+#include <cstddef>     // For ptrdiff_t.
 #include <type_traits> // For is_integral.
 #include <memory>
 

Modules/IO/ImageBase/include/itkImageSeriesReader.hxx
@@ -25,6 +25,7 @@
 #include "itkMath.h"
 #include "itkProgressReporter.h"
 #include "itkMetaDataObject.h"
+#include <cstddef> // For ptrdiff_t.
 #include <iomanip>
 
 namespace itk