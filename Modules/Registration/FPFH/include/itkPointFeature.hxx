3cc411509e09a489a639967723cae380606e0779
Pranjal Sahu <pranjalsahu5@gmail.com>
2022-07-22 12:00:01 -0400

    PERF: Optimize code with vector and distances

--- Modules/Registration/FPFH/include/itkPointFeature.hxx
+++ Modules/Registration/FPFH/include/itkPointFeature.hxx
@@ -88,20 +88,17 @@ PointFeature<TInputPointSet, TOutputPointSet>::ComputeSPFHFeature(TInputPointSet
   kdtree->SetPoints(input->GetPoints());
   kdtree->Initialize();
 
-  unsigned long int num_of_points = input->GetNumberOfPoints();
-  // auto feature = FeatureType::New();
-  // feature.resize(33 * num_of_points);
-  // feature->Reserve(33 * num_of_points);
-
+  unsigned long int   num_of_points = input->GetNumberOfPoints();
   std::vector<double> feature1(33 * num_of_points, 0);
-  // for(int i = 0; i < num_of_points; ++i)
+
   auto ProcessPoint = [&](int i) {
     auto point = input->GetPoint(i);
     auto normal = input_normals->GetPoint(i);
 
+    std::vector<double> distances;
+
     typename PointsLocatorType::NeighborsIdentifierType indices;
-    // kdtree->FindPointsWithinRadius(point, radius, indices);
-    kdtree->FindClosestNPoints(point, neighbors, indices);
+    kdtree->FindClosestNPoints(point, neighbors, indices, distances);
 
     if (indices.size() > 1)
     {
@@ -110,25 +107,25 @@ PointFeature<TInputPointSet, TOutputPointSet>::ComputeSPFHFeature(TInputPointSet
 
       for (size_t k = 0; k < indices.size(); k++)
       {
-        auto   point_diff = point - input->GetPoint(indices[k]);
-        double dist = point_diff.GetNorm();
-        dist = dist * dist;
+        auto dist = distances[k];
 
         // skip the point itself
         if (dist == 0.0)
           continue;
 
-        neighbor_vect.push_back(std::make_pair(dist, indices[k]));
+        if (dist < radius)
+        {
+          dist = dist * dist;
+          neighbor_vect.push_back(std::make_pair(dist, indices[k]));
+        }
       }
 
-      // std::sort(neighbor_vect.begin(), neighbor_vect.end());
       unsigned int neighbor_count = std::min(neighbors, (unsigned int)neighbor_vect.size());
 
       // only compute SPFH feature when a point has neighbors
       double hist_incr = 100.0 / (double)neighbor_count;
       for (size_t k = 0; k < neighbor_count; k++)
       {
-        // std::cout << "Point " << k << " is " <<  neighbor_vect[k].second << std::endl;
         auto point2 = input->GetPoint(neighbor_vect[k].second);
         auto normal2 = input_normals->GetPoint(neighbor_vect[k].second);
 
@@ -157,7 +154,6 @@ PointFeature<TInputPointSet, TOutputPointSet>::ComputeSPFHFeature(TInputPointSet
           h_index = 10;
         }
         unsigned int temp_index = h_index * num_of_points + i;
-        // feature->SetElement(temp_index, hist_incr + feature->GetElement(temp_index));
         feature1[temp_index] = hist_incr + feature1[temp_index];
 
         h_index = (int)(floor(11 * (pair_feature[1] + 1.0) * 0.5));
@@ -170,7 +166,6 @@ PointFeature<TInputPointSet, TOutputPointSet>::ComputeSPFHFeature(TInputPointSet
           h_index = 10;
         }
         temp_index = (h_index + 11) * num_of_points + i;
-        // feature->SetElement(temp_index, hist_incr + feature->GetElement(temp_index));
         feature1[temp_index] = hist_incr + feature1[temp_index];
 
         h_index = (int)(floor(11 * (pair_feature[2] + 1.0) * 0.5));
@@ -183,7 +178,6 @@ PointFeature<TInputPointSet, TOutputPointSet>::ComputeSPFHFeature(TInputPointSet
           h_index = 10;
         }
         temp_index = (h_index + 22) * num_of_points + i;
-        // feature->SetElement(temp_index, hist_incr + feature->GetElement(temp_index));
         feature1[temp_index] = hist_incr + feature1[temp_index];
       }
     }
@@ -192,6 +186,7 @@ PointFeature<TInputPointSet, TOutputPointSet>::ComputeSPFHFeature(TInputPointSet
   itk::MultiThreaderBase::Pointer mt = itk::MultiThreaderBase::New();
   mt->ParallelizeArray(0, num_of_points, ProcessPoint, nullptr);
 
+  // This is done to optimize the code by avoiding GetElement, SetElement overhead.
   auto feature = FeatureType::New();
   feature->CastToSTLContainer() = feature1;
   return feature;
@@ -204,10 +199,7 @@ PointFeature<TInputPointSet, TOutputPointSet>::ComputeFPFHFeature(TInputPointSet
                                                                   unsigned int     radius,
                                                                   unsigned int     neighbors)
 {
-  unsigned long int num_of_points = input->GetNumberOfPoints();
-  // this->m_FpfhFeature = FeatureType::New();
-  // this->m_FpfhFeature->Reserve(33 * num_of_points);
-
+  unsigned long int   num_of_points = input->GetNumberOfPoints();
   std::vector<double> fpfh2(33 * num_of_points, 0.0);
 
   PointsLocatorTypePointer kdtree = PointsLocatorType::New();
@@ -219,14 +211,14 @@ PointFeature<TInputPointSet, TOutputPointSet>::ComputeFPFHFeature(TInputPointSet
   std::cout << "After SPFH Feature calculation " << std::time(0) << std::endl;
 
   auto & spfh1 = spfh->CastToSTLContainer();
+
   // Method to perform processing in parallel
-  // for(int i = 0; i < num_of_points; ++i)
   auto ProcessPoint = [&](int i) {
     auto point = input->GetPoint(i);
 
     typename PointsLocatorType::NeighborsIdentifierType indices;
-    kdtree->FindClosestNPoints(point, neighbors, indices);
-    // kdtree->FindPointsWithinRadius(point, radius, indices);
+    std::vector<double>                                 distances;
+    kdtree->FindClosestNPoints(point, neighbors, indices, distances);
 
     if (indices.size() > 1)
     {
@@ -237,18 +229,18 @@ PointFeature<TInputPointSet, TOutputPointSet>::ComputeFPFHFeature(TInputPointSet
 
       for (size_t k = 0; k < indices.size(); k++)
       {
-        auto   point_diff = point - input->GetPoint(indices[k]);
-        double dist = point_diff.GetNorm();
-        dist = dist * dist;
+        double dist = distances[k];
 
         // skip the point itself
         if (dist == 0.0)
           continue;
 
-        neighbor_vect.push_back(std::make_pair(dist, indices[k]));
+        if (dist < radius)
+        {
+          dist = dist * dist;
+          neighbor_vect.push_back(std::make_pair(dist, indices[k]));
+        }
       }
-
-      // std::sort(neighbor_vect.begin(), neighbor_vect.end());
 
       // Take only first neighbors in sorted order
       unsigned int neighbor_count = std::min(neighbors, (unsigned int)neighbor_vect.size());
@@ -257,11 +249,8 @@ PointFeature<TInputPointSet, TOutputPointSet>::ComputeFPFHFeature(TInputPointSet
         for (int j = 0; j < 33; j++)
         {
           double val = spfh1[j * num_of_points + neighbor_vect[k].second] / neighbor_vect[k].first;
-          // double val = spfh->GetElement(j*num_of_points + neighbor_vect[k].second) / neighbor_vect[k].first;
           sum[j / 11] += val;
           fpfh2[j * num_of_points + i] = fpfh2[j * num_of_points + i] + val;
-          // this->m_FpfhFeature->SetElement(j*num_of_points + i, this->m_FpfhFeature->GetElement(j*num_of_points + i) +
-          // val);
         }
       }
 
@@ -275,15 +264,8 @@ PointFeature<TInputPointSet, TOutputPointSet>::ComputeFPFHFeature(TInputPointSet
 
       for (int j = 0; j < 33; j++)
       {
-        // this->m_FpfhFeature->SetElement(j*num_of_points + i, this->m_FpfhFeature->GetElement(j*num_of_points + i) *
-        // sum[j / 11]); this->m_FpfhFeature->SetElement(j*num_of_points + i,
-        // this->m_FpfhFeature->GetElement(j*num_of_points + i) + spfh->GetElement(j*num_of_points + i));
         fpfh2[j * num_of_points + i] = fpfh2[j * num_of_points + i] * sum[j / 11];
         fpfh2[j * num_of_points + i] = fpfh2[j * num_of_points + i] + spfh1[j * num_of_points + i];
-        // auto temp = this->m_FpfhFeature->GetElement(j*num_of_points + i);
-        // this->m_FpfhFeature->SetElement(j*num_of_points + i, temp * sum[j / 11]);
-        // this->m_FpfhFeature->SetElement(j*num_of_points + i, temp * sum[j / 11] + spfh->GetElement(j*num_of_points +
-        // i));
       }
     }
   };
@@ -291,6 +273,7 @@ PointFeature<TInputPointSet, TOutputPointSet>::ComputeFPFHFeature(TInputPointSet
   itk::MultiThreaderBase::Pointer mt = itk::MultiThreaderBase::New();
   mt->ParallelizeArray(0, num_of_points, ProcessPoint, nullptr);
 
+  // This is done to optimize the code by avoiding GetElement, SetElement overhead.
   this->m_FpfhFeature = FeatureType::New();
   this->m_FpfhFeature->CastToSTLContainer() = fpfh2;
   std::cout << "After SPFH Feature calculation " << std::time(0) << std::endl;
