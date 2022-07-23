fc0b64defa024557ee0b737251383c1ec5da2d88
Tom Birdsong <tom.birdsong@kitware.com>
2022-07-23 11:55:29 -0400

    ENH: Bump ITK version to v5.3rc04

--- Modules/Beta/MeshToPolyData/setup.py
+++ Modules/Beta/MeshToPolyData/setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
     name='itk-meshtopolydata',
-    version='0.8.2',
+    version='0.8.3',
     author='Insight Software Consortium',
     author_email='itk+community@discourse.itk.org',
     packages=['itk'],
@@ -44,7 +44,7 @@ setup(
     keywords='ITK InsightToolkit',
     url=r'https://itk.org/',
     install_requires=[
-        r'itk-core>=5.3rc04',
+        r'itk-core>=5.3rc04.post1',
         r'numpy',
     ]
     )
