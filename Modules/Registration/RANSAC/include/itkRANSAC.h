ceb0afe8d5a7d6630742567ac7ad3284e8f92679
Pranjal Sahu <pranjalsahu5@gmail.com>
2022-08-22 21:17:18 -0400

    DOC: Added license

--- Modules/Registration/RANSAC/include/itkRANSAC.h
+++ Modules/Registration/RANSAC/include/itkRANSAC.h
@@ -1,3 +1,21 @@
+/*=========================================================================
+ *
+ *  Copyright NumFOCUS
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         https://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
 #ifndef itkRANSAC_h
 #define itkRANSAC_h
 
@@ -47,7 +65,15 @@
 namespace itk
 {
 
-template <class T, class S>
+/** \class RANSAC
+ *
+ * \brief RANSAC for various usecases such as plane estimation, point set registration.
+ *
+ * \ingroup RANSAC
+ *
+ */
+
+template <typename T, typename S>
 class ITK_TEMPLATE_EXPORT RANSAC : public Object
 {
 public:
