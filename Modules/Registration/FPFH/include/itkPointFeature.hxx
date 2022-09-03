dc540f8c0cd0d286b2088c9f24f58fa4be0df6b2
Pranjal Sahu <pranjalsahu5@gmail.com>
2022-09-03 18:44:04 -0400

    ENH: Remove log messages

--- Modules/Registration/FPFH/include/itkPointFeature.hxx
+++ Modules/Registration/FPFH/include/itkPointFeature.hxx
@@ -205,9 +205,7 @@ PointFeature<TInputPointSet, TOutputPointSet>::ComputeFPFHFeature(TInputPointSet
   kdtree->SetPoints(input->GetPoints());
   kdtree->Initialize();
 
-  std::cout << "Before SPFH Feature calculation " << std::time(0) << std::endl;
   auto spfh = ComputeSPFHFeature(input, input_normals, radius, neighbors);
-  std::cout << "After SPFH Feature calculation " << std::time(0) << std::endl;
 
   auto & spfh1 = spfh->CastToSTLContainer();
 
@@ -275,7 +273,6 @@ PointFeature<TInputPointSet, TOutputPointSet>::ComputeFPFHFeature(TInputPointSet
   // This is done to optimize the code by avoiding GetElement, SetElement overhead.
   this->m_FpfhFeature = FeatureType::New();
   this->m_FpfhFeature->CastToSTLContainer() = fpfh2;
-  std::cout << "After SPFH Feature calculation " << std::time(0) << std::endl;
 }
 
 
