3f2b04020c9ecc0f70fc80119317bbd2bcb30f4c
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2022-09-23 21:11:25 -0400

    DOC: Fix module description
    Fix module description: remove hanging sentence part.

--- Modules/Filtering/HigherOrderAccurateGradient/itk-module.cmake
+++ Modules/Filtering/HigherOrderAccurateGradient/itk-module.cmake
@@ -3,7 +3,6 @@ set(
   DOCUMENTATION
   "This module contains a filter to compute higher order
 accurate numerical derivatives and gradients from an input scalar image.
-field from a displacement field image.
 Higher Order Accurate Derivative and Gradient Calculation in ITK
 https://www.insight-journal.org/browse/publication/775
 https://hdl.handle.net/10380/3231
