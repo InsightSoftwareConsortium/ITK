InsightSoftwareConsortium/ITK

commit 2b1b5312fadf1a156882d06dc2e7c6b2a2290044

STYLE: Remove space between class and member names (follow-up)
Removed spaces between class and member names, that were
accidentally introduced with pull request https://github.com/InsightSoftwareConsortium/ITK/pull/1191
commit 2074c2f9e5ed3f087d0e2059f8e0e8992fcad7ef "STYLE: Enforce ITK style
defined by .clang-format".

Using bash commands like:

  find . \( -iname *.cxx \) -exec perl -pi -w -e 's/([A-Z]\w+) \:\:(\w)/$1::$2/g;' {} \;

Follow-up to pull request https://github.com/InsightSoftwareConsortium/ITK/pull/2096
commit 734d6f84d728b343baae5ef66643a5568a5a30c7 "STYLE: Remove space between
class and member names in C++ source files"

Modules/IO/PhilipsREC/src/itkPhilipsRECImageIO.cxx
@@ -387,14 +387,11 @@ PhilipsRECImageIO::PhilipsRECImageIO()
   // Start out with file byte order == system byte order
   // this will be changed if we're reading a file to whatever
   // the file actually contains.
-  if (ByteSwapper<int>::SystemIsBigEndian())
+  iff(ByteSwapper<int>::SystemIsBigEndian())
   {
     this->m_MachineByteOrder = this->m_ByteOrder = IOByteOrderEnum::BigEndian;
   }
-  else
-  {
-    this->m_MachineByteOrder = this->m_ByteOrder = IOByteOrderEnum::LittleEndian;
-  }
+  else { this->m_MachineByteOrder = this->m_ByteOrder = IOByteOrderEnum::LittleEndian; }
   this->m_SliceIndex = new SliceIndexType();
 }
 

Modules/Core/Common/src/itkHexahedronCellTopology.cxx
@@ -22,14 +22,14 @@ namespace itk
 /**
  * The hexahedron's topology data: Edges.
  */
-const int HexahedronCellTopology ::m_Edges[12][2] = { { 0, 1 }, { 1, 2 }, { 3, 2 }, { 0, 3 }, { 4, 5 }, { 5, 6 },
-                                                      { 7, 6 }, { 4, 7 }, { 0, 4 }, { 1, 5 }, { 3, 7 }, { 2, 6 } };
+const int HexahedronCellTopology::m_Edges[12][2] = { { 0, 1 }, { 1, 2 }, { 3, 2 }, { 0, 3 }, { 4, 5 }, { 5, 6 },
+                                                     { 7, 6 }, { 4, 7 }, { 0, 4 }, { 1, 5 }, { 3, 7 }, { 2, 6 } };
 
 /**
  * The hexahedron's topology data: Faces.
  */
-const int HexahedronCellTopology ::m_Faces[6][4] = { { 0, 4, 7, 3 }, { 1, 2, 6, 5 }, { 0, 1, 5, 4 },
-                                                     { 3, 7, 6, 2 }, { 0, 3, 2, 1 }, { 4, 5, 6, 7 } };
+const int HexahedronCellTopology::m_Faces[6][4] = { { 0, 4, 7, 3 }, { 1, 2, 6, 5 }, { 0, 1, 5, 4 },
+                                                    { 3, 7, 6, 2 }, { 0, 3, 2, 1 }, { 4, 5, 6, 7 } };
 
 HexahedronCellTopology::HexahedronCellTopology() = default;
 

Modules/Core/Common/src/itkQuadraticTriangleCellTopology.cxx
@@ -22,7 +22,7 @@ namespace itk
 /**
  * The triangle's topology data: Edges
  */
-const int QuadraticTriangleCellTopology ::m_Edges[3][3] = { { 0, 4, 1 }, { 1, 5, 2 }, { 2, 3, 0 } };
+const int QuadraticTriangleCellTopology::m_Edges[3][3] = { { 0, 4, 1 }, { 1, 5, 2 }, { 2, 3, 0 } };
 
 QuadraticTriangleCellTopology::QuadraticTriangleCellTopology() = default;
 

Modules/Core/Common/src/itkQuadrilateralCellTopology.cxx
@@ -22,7 +22,7 @@ namespace itk
 /**
  * The quadrilateral's topology data: Edges.
  */
-const int QuadrilateralCellTopology ::m_Edges[4][2] = { { 0, 1 }, { 1, 2 }, { 2, 3 }, { 3, 0 } };
+const int QuadrilateralCellTopology::m_Edges[4][2] = { { 0, 1 }, { 1, 2 }, { 2, 3 }, { 3, 0 } };
 
 QuadrilateralCellTopology::QuadrilateralCellTopology() = default;
 

Modules/Core/Common/src/itkSmapsFileParser.cxx
@@ -438,7 +438,7 @@ SmapsData_2_6::GetStackUsage()
 
 /**              ---            VMMapData               ---              **/
 
-VMMapData_10_2 ::VMMapData_10_2() = default;
+VMMapData_10_2::VMMapData_10_2() = default;
 
 VMMapData_10_2::~VMMapData_10_2() = default;
 

Modules/Core/Common/src/itkTetrahedronCellTopology.cxx
@@ -22,12 +22,12 @@ namespace itk
 /**
  * The tetrahedron's topology data: Faces
  */
-const int TetrahedronCellTopology ::m_Faces[4][3] = { { 0, 1, 3 }, { 1, 2, 3 }, { 2, 0, 3 }, { 0, 2, 1 } };
+const int TetrahedronCellTopology::m_Faces[4][3] = { { 0, 1, 3 }, { 1, 2, 3 }, { 2, 0, 3 }, { 0, 2, 1 } };
 
 /**
  * The tetrahedron's topology data: Faces
  */
-const int TetrahedronCellTopology ::m_Edges[6][2] = { { 0, 1 }, { 1, 2 }, { 2, 0 }, { 0, 3 }, { 1, 3 }, { 2, 3 } };
+const int TetrahedronCellTopology::m_Edges[6][2] = { { 0, 1 }, { 1, 2 }, { 2, 0 }, { 0, 3 }, { 1, 3 }, { 2, 3 } };
 
 TetrahedronCellTopology::TetrahedronCellTopology() = default;
 

Modules/Core/Common/src/itkTriangleCellTopology.cxx
@@ -22,7 +22,7 @@ namespace itk
 /**
  * The triangle's topology data: Edges
  */
-const int TriangleCellTopology ::m_Edges[3][2] = { { 0, 1 }, { 1, 2 }, { 2, 0 } };
+const int TriangleCellTopology::m_Edges[3][2] = { { 0, 1 }, { 1, 2 }, { 2, 0 } };
 
 TriangleCellTopology::TriangleCellTopology() = default;
 

Modules/Core/Common/src/itkXMLFileOutputWindow.cxx
@@ -26,7 +26,7 @@ namespace itk
 /**
  * Prompting off by default
  */
-XMLFileOutputWindow ::XMLFileOutputWindow() = default;
+XMLFileOutputWindow::XMLFileOutputWindow() = default;
 
 XMLFileOutputWindow::~XMLFileOutputWindow() = default;
 

commit f8fda8ae1ad6d221eb4676319f13a7cc325efbc1

COMP: Fix typo (compile error) in `PhilipsRECImageIO` constructor
Fixed a compile error, which appeared when enabling `Module_ITKIOPhilipsREC`:

> itkPhilipsRECImageIO.cxx(390,3): error C3861: 'iff': identifier not found

This obvious typo was introduced with pull request https://github.com/InsightSoftwareConsortium/ITK/pull/3664
commit 2b1b5312fadf1a156882d06dc2e7c6b2a2290044 "STYLE: Remove space between
class and member names (follow-up)", and reported by Bradley Lowekamp.

Modules/IO/PhilipsREC/src/itkPhilipsRECImageIO.cxx
@@ -387,11 +387,14 @@ PhilipsRECImageIO::PhilipsRECImageIO()
   // Start out with file byte order == system byte order
   // this will be changed if we're reading a file to whatever
   // the file actually contains.
-  iff(ByteSwapper<int>::SystemIsBigEndian())
+  if (ByteSwapper<int>::SystemIsBigEndian())
   {
     this->m_MachineByteOrder = this->m_ByteOrder = IOByteOrderEnum::BigEndian;
   }
-  else { this->m_MachineByteOrder = this->m_ByteOrder = IOByteOrderEnum::LittleEndian; }
+  else
+  {
+    this->m_MachineByteOrder = this->m_ByteOrder = IOByteOrderEnum::LittleEndian;
+  }
   this->m_SliceIndex = new SliceIndexType();
 }
 