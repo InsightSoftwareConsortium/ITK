c33c4fd2380c26e8c26963735f2a7e19f783af56
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2022-10-13 08:53:55 +0200

    DOC: Link groups in Doxygen in module description
    Create links to referenced groups when generating the Doxygen
    documentation from the module description.
    
    Doxygen grouping documentation:
    https://www.doxygen.nl/manual/grouping.html
    
    Triple escape the commands.
    
    Related to/left behind in a0d0b4c, f8cd176, and bf6d071. Automatic group
    linking was present originally, but commands were mistakenly removed in
    1f4c6e9 following a wrong assumption/misinterpretation of the Doxygen
    autolinking vs. group linking.

--- Modules/Filtering/CurvatureFlow/itk-module.cmake
+++ Modules/Filtering/CurvatureFlow/itk-module.cmake
@@ -2,7 +2,8 @@ set(DOCUMENTATION "This module contains filters that implement variations of
 Curvature Flow. This is a technique that uses an iterative solution of partial
 differential equations to implement image denoising image filtering. These
 classes are typically used as edge-preserving smoothing filters. You may also
-find the ITKSmoothing and the ITKAnisotropicSmoothing useful as well.")
+find the \\\\ref ITKSmoothing and the \\\\ref ITKAnisotropicSmoothing useful
+as well.")
 
 itk_module(ITKCurvatureFlow
   COMPILE_DEPENDS
