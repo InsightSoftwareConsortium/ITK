5d1aa4aced99a920ead67787292c1cbbc08c5285
Hans Johnson <hans.j.johnson@gmail.com>
2022-12-23 10:19:37 -0600

    COMP: Threads package is required by TBB
    ```
    CMake Error at /Users/johnsonhj/src/BT-release/BRAINSTools-Release-5.7.0/lib/cmake/TBB/TBBTargets.cmake:61 (set_target_properties):
      The link interface of target TBB::tbb contains:
        Threads::Threads
      but the target was not found.
    ```
    
    In many builds "find_package(Threads)" is included from other third
    party libraries.  When requesting TBB, Threads should unconditionally
    be found and included.

--- Modules/ThirdParty/TBB/itk-module-init.cmake
+++ Modules/ThirdParty/TBB/itk-module-init.cmake
@@ -1,2 +1,3 @@
+find_package(Threads REQUIRED) # TBB depends on Threads::Threads
 find_package(TBB REQUIRED CONFIG) # must have TBBConfig.cmake, provided since version tbb2017_20170604oss
 get_target_property(TBB_INCLUDE_DIRS TBB::tbb INTERFACE_INCLUDE_DIRECTORIES)
