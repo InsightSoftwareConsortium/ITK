626c1b4d992e54d7506df7f2cafcda9d4b6eb146
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2023-01-13 18:31:03 -0500

    STYLE: Break line before calling `PrintSelf` in test
    Break line before calling `PrintSelf` in test in order to be able to
    clearly idenify the information printed by the `PrintSelf` method being
    called by the basic method exercising macro.

--- Modules/Core/Common/test/itkBSplineInterpolationWeightFunctionTest.cxx
+++ Modules/Core/Common/test/itkBSplineInterpolationWeightFunctionTest.cxx
@@ -50,7 +50,7 @@ itkBSplineInterpolationWeightFunctionTest(int, char *[])
     constexpr unsigned int SplineOrder = 2;
 
     std::cout << "Testing SpaceDimension= " << SpaceDimension;
-    std::cout << " and SplineOrder= " << SplineOrder << "  ";
+    std::cout << " and SplineOrder= " << SplineOrder << std::endl;
 
     using FunctionType = itk::BSplineInterpolationWeightFunction<CoordRepType, SpaceDimension, SplineOrder>;
     using ContinuousIndexType = FunctionType::ContinuousIndexType;
@@ -150,7 +150,7 @@ itkBSplineInterpolationWeightFunctionTest(int, char *[])
     constexpr unsigned int SplineOrder = 3;
 
     std::cout << "Testing SpaceDimension= " << SpaceDimension;
-    std::cout << " and SplineOrder= " << SplineOrder << "  ";
+    std::cout << " and SplineOrder= " << SplineOrder << std::endl;
 
     using FunctionType = itk::BSplineInterpolationWeightFunction<CoordRepType, SpaceDimension, SplineOrder>;
     using ContinuousIndexType = FunctionType::ContinuousIndexType;
@@ -238,7 +238,7 @@ itkBSplineInterpolationWeightFunctionTest(int, char *[])
     constexpr unsigned int SpaceDimension = 3;
     constexpr unsigned int SplineOrder = 3;
     std::cout << "Testing SpaceDimension= " << SpaceDimension;
-    std::cout << " and SplineOrder= " << SplineOrder << "  ";
+    std::cout << " and SplineOrder= " << SplineOrder << std::endl;
 
     using FunctionType = itk::BSplineInterpolationWeightFunction<CoordRepType, SpaceDimension, SplineOrder>;
     using ContinuousIndexType = FunctionType::ContinuousIndexType;
