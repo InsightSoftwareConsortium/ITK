InsightSoftwareConsortium/ITK

commit c47ed1c1b3e26051de655f7ad03e975fae93b73b

STYLE: Make `PrintSelf` implementation style consistent
Make `PrintSelf`implementation style consistent following the ITK SWG
coding style guideline and the available ITK macros:
- Always print the superclass first.
- Print only the class instance variables.
- Use the `os << indent << "{ivarName}: " << m_ivarName << std::endl`
  recipe: do not print the leading `m_` of the ivar; use a colon after
  printing its name; leave a whitespace between the colon and the
  printed value of the ivar.
- For boolean ivars print `On`/`Off` according to their values using the
  ternary operator.
- Use `itkPrintSelfObjectMacro` to print objects inheriting from
  `itk::LightObject` that can be null.
- For pointers that do not inherit from `itk::LightObject` check whether
  they are non-null before attempting to print them and print the
  `(empty)` string otherwise.
- Rely on the self printing ability of the ITK classes to avoid
  boilerplate code when printing the corresponding ivars in other
  classes' `PrintSelf` methods.
- Take advantage of the output stream `<<` insertion operator overload
  for `std::vector` types in the `itk::print_helper` namespace defined
  in `itkPrintHelper.h` to print such types consistently.
- Cast custom types to their numerical values using
  `itk::NumericTraits::PrinType` so that they are printed as expected.
- Remove statements printing the hard-coded class name and its own
  pointer, e.g.
  ```
  os << indent << "ArrowSpatialObject(" << this << ")" << std::endl;
  ```
  `PrintSelf` does this without requiring the removed line.
- Save typing `this->` to point to the instance of the current class.

Apply the same principles to the `<<` operator overloads.

Overload the ostream operator for the `FixedImageSamplePoint` class used
by the `itk::ImageToImageMetric` class.

Overload the ostream operator for the `ImageVoxel` class used by the
`itk::DeformableSimplexMesh3DGradientConstraintForceFilter` class.

Modules/Registration/GPUPDEDeformable/include/itkGPUPDEDeformableRegistrationFilter.hxx
@@ -83,6 +83,41 @@ GPUPDEDeformableRegistrationFilter<TFixedImage, TMovingImage, TDisplacementField
   Indent         indent) const
 {
   GPUSuperclass::PrintSelf(os, indent);
+
+  itkPrintSelfObjectMacro(TempField);
+
+  os << indent << "SmoothingKernelSizes: " << m_SmoothingKernelSizes << std::endl;
+
+  os << indent << "SmoothingKernels: ";
+  if (m_SmoothingKernels != nullptr)
+  {
+    os << static_cast<typename NumericTraits<DeformationScalarType>::PrintType>(*m_SmoothingKernels);
+  }
+  os << std::endl;
+
+  itkPrintSelfObjectMacro(GPUSmoothingKernels);
+
+  os << indent << "UpdateFieldSmoothingKernelSizes: " << m_UpdateFieldSmoothingKernelSizes << std::endl;
+
+  os << indent << "UpdateFieldSmoothingKernels: ";
+  if (m_UpdateFieldSmoothingKernels != nullptr)
+  {
+    os << static_cast<typename NumericTraits<DeformationScalarType>::PrintType>(*m_UpdateFieldSmoothingKernels);
+  }
+  os << std::endl;
+
+  itkPrintSelfObjectMacro(UpdateFieldGPUSmoothingKernels);
+
+  os << indent << "ImageSizes: ";
+  if (m_ImageSizes != nullptr)
+  {
+    os << *m_ImageSizes;
+  }
+  os << std::endl;
+
+  itkPrintSelfObjectMacro(m_GPUImageSizes);
+
+  os << indent << "SmoothDisplacementFieldGPUKernelHandle: " << m_SmoothDisplacementFieldGPUKernelHandle << std::endl;
 }
 
 template <typename TFixedImage, typename TMovingImage, typename TDisplacementField, typename TParentImageFilter>

commit b603cfc5d516f9b80feb4ba7413c9ebbbcae16af

COMP: Provide prefix-less member name to `itkPrintSelfObjectMacro`
Provide prefix-less member name as the argument to the
`itkPrintSelfObjectMacro` macro in
`itkGPUPDEDeformableRegistrationFilter.hxx`.

Introduced inadvertently in commit c47ed1c1b3e26051de655f7ad03e975fae93b73b.

Modules/Registration/GPUPDEDeformable/include/itkGPUPDEDeformableRegistrationFilter.hxx
@@ -141,7 +141,7 @@ GPUPDEDeformableRegistrationFilter<TFixedImage, TMovingImage, TDisplacementField
     os << "(null)" << std::endl;
   }
 
-  itkPrintSelfObjectMacro(m_GPUImageSizes);
+  itkPrintSelfObjectMacro(GPUImageSizes);
 
   os << indent << "SmoothDisplacementFieldGPUKernelHandle: " << m_SmoothDisplacementFieldGPUKernelHandle << std::endl;
 }