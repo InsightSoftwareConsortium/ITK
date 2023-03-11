33d65c85baaf0496d3821980742faf9e3a4e1225
Hans Johnson <hans-johnson@uiowa.edu>
2023-03-11 07:52:51 -0600

    COMP: Manually merged formatting changes for jpeg9f
    After running jpeg9f/configure command on the reference
    library, manually merged formatting changes.
    
    Primary change was to explicitly undefine
    variables for unused features of libjpeg.

--- Modules/ThirdParty/JPEG/src/itkjpeg/jconfig.h
+++ Modules/ThirdParty/JPEG/src/itkjpeg/jconfig.h
@@ -1,9 +1,12 @@
-/* jconfig.h --- source file edited by configure script */
+/* jconfig.h.  Generated from jconfig.cfg by configure.  */
+/* jconfig.cfg --- source file edited by configure script */
 /* see jconfig.txt for explanations */
 
 #define HAVE_PROTOTYPES
 #define HAVE_UNSIGNED_CHAR
 #define HAVE_UNSIGNED_SHORT
+/* #undef void */
+/* #undef const */
 #undef CHAR_IS_UNSIGNED
 #ifndef HAVE_STDDEF_H
 #define HAVE_STDDEF_H
@@ -11,6 +14,7 @@
 #ifndef HAVE_STDLIB_H
 #define HAVE_STDLIB_H
 #endif
+/*  #define HAVE_LOCALE_H  -- jpeg9f*/
 #undef NEED_BSD_STRINGS
 #undef NEED_SYS_TYPES_H
 #undef NEED_FAR_POINTERS
@@ -18,17 +22,24 @@
 /* Define this if you get warnings about undefined structures. */
 #undef INCOMPLETE_TYPES_BROKEN
 
-/* Define "boolean" as unsigned char, not int, on Windows systems. */
+/* Define "boolean" as unsigned char, not enum, on Windows systems. */
 #ifdef _WIN32
-#ifndef __RPCNDR_H__		/* don't conflict if rpcndr.h already read */
+#ifndef __RPCNDR_H__        /* don't conflict if rpcndr.h already read */
 typedef unsigned char boolean;
 #endif
-#define HAVE_BOOLEAN		/* prevent jmorecfg.h from redefining it */
+#ifndef FALSE            /* in case these macros already exist */
+#define FALSE    0        /* values of boolean */
+#endif
+#ifndef TRUE
+#define TRUE        1
+#endif
+#define HAVE_BOOLEAN        /* prevent jmorecfg.h from redefining it */
 #endif
 
 #ifdef JPEG_INTERNALS
 
 #undef RIGHT_SHIFT_IS_UNSIGNED
+/* #define INLINE __inline__ -- jpeg9f */
 #define INLINE
 /* These are for configuring the JPEG memory manager. */
 #undef DEFAULT_MAX_MEM
@@ -38,11 +49,11 @@ typedef unsigned char boolean;
 
 #ifdef JPEG_CJPEG_DJPEG
 
-#define BMP_SUPPORTED           /* BMP image file format */
-#define GIF_SUPPORTED           /* GIF image file format */
-#define PPM_SUPPORTED           /* PBMPLUS PPM/PGM image file format */
-#undef RLE_SUPPORTED            /* Utah RLE image file format */
-#define TARGA_SUPPORTED         /* Targa image file format */
+#define BMP_SUPPORTED        /* BMP image file format */
+#define GIF_SUPPORTED        /* GIF image file format */
+#define PPM_SUPPORTED        /* PBMPLUS PPM/PGM image file format */
+#undef RLE_SUPPORTED           /* Utah RLE image file format */
+#define TARGA_SUPPORTED        /* Targa image file format */
 
 #undef TWO_FILE_COMMANDLINE
 #undef NEED_SIGNAL_CATCHER
@@ -57,7 +68,3 @@ typedef unsigned char boolean;
 /* names mangling */
 
 #include "itk_jpeg_mangle.h"
-
-#if defined(__BORLANDC__)
-#pragma warn -8004 /* Disable "foo is assigned a value that is never used." */
-#endif
