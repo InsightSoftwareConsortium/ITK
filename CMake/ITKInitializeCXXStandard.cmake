6b6ddf529f7da2e6436b07832849087b0285e9b1
Niels Dekker <N.Dekker@lumc.nl>
2023-03-17 21:13:22 +0100

    ENH: Upgrade ITK from C++14 to C++17
    Follow-up to pull request https://github.com/InsightSoftwareConsortium/ITK/pull/2563
    commit daec0fdd703b1bca16aeb07bc4e0a01101da950a
    "ENH: Upgrade ITK from C++11 to C++14"

--- CMake/ITKInitializeCXXStandard.cmake
+++ CMake/ITKInitializeCXXStandard.cmake
@@ -1,6 +1,6 @@
 ##  Set the default target properties for ITK
 if(NOT CMAKE_CXX_STANDARD)
-  set(CMAKE_CXX_STANDARD 14) # Supported values are 14, 17, 20, and 23.
+  set(CMAKE_CXX_STANDARD 17) # Supported values are 17, 20, and 23.
 endif()
 if(NOT CMAKE_CXX_STANDARD_REQUIRED)
   set(CMAKE_CXX_STANDARD_REQUIRED ON)
--- Wrapping/macro_files/itk_auto_load_submodules.cmake
+++ Wrapping/macro_files/itk_auto_load_submodules.cmake
@@ -22,9 +22,9 @@ function(generate_castxml_commandline_flags)
     # Avoid missing omp.h include
     set(_castxml_cc_flags ${CMAKE_CXX_FLAGS})
     if(CMAKE_CXX_EXTENSIONS)
-        set(_castxml_cc_flags "${_castxml_cc_flags} ${CMAKE_CXX14_EXTENSION_COMPILE_OPTION}")
+        set(_castxml_cc_flags "${_castxml_cc_flags} ${CMAKE_CXX17_EXTENSION_COMPILE_OPTION}")
     else()
-        set(_castxml_cc_flags "${_castxml_cc_flags} ${CMAKE_CXX14_STANDARD_COMPILE_OPTION}")
+        set(_castxml_cc_flags "${_castxml_cc_flags} ${CMAKE_CXX17_STANDARD_COMPILE_OPTION}")
     endif()
 
     # Aggressive optimization flags cause cast_xml to give invalid error conditions
