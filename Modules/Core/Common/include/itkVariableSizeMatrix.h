212c0e1cc4cf16e98fc17ebe88ea4ba7c646be9c
Niels Dekker <N.Dekker@lumc.nl>
2023-03-25 19:22:24 +0100

    COMP: Declare `operator*` and `operator/` of VariableSizeMatrix const
    The original code erroneously disallowed evaluating the expressions `m * value`
    and `m / value` when `m` would be a `const` reference to a VariableSizeMatrix.

--- Modules/Core/Common/include/itkVariableSizeMatrix.h
+++ Modules/Core/Common/include/itkVariableSizeMatrix.h
@@ -101,7 +101,7 @@ public:
   }
 
   /** Matrix by scalar multiplication.  */
-  Self operator*(const T & value)
+  Self operator*(const T & value) const
   {
     Self result(*this);
 
@@ -118,7 +118,7 @@ public:
 
   /** Matrix by scalar division.  */
   Self
-  operator/(const T & value)
+  operator/(const T & value) const
   {
     Self result(*this);
 
