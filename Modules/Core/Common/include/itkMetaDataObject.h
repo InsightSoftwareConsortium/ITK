2dcd8aa560f001f721b785b0e7c0f370bd3a3adf
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2023-08-06 13:17:27 -0400

    DOC: Fix `MetaDataObject::EncapsulateMetaData` empty return doc warning
    Fix `itk::MetaDataObject::EncapsulateMetaData` empty return type
    documentation Doxygen warning.
    
    Fixes:
    ```
    Generating XML output for class
    itk::Statistics::WeightedMeanS/work/ITK/Modules/Core/Common/include/itkMetaDataObject.h:217:
    warning: documented empty return type of itk::EncapsulateMetaData
    ```
    
    raised for example in:
    https://github.com/InsightSoftwareConsortium/ITKDoxygen/actions/runs/5773432695/job/15649347384#step:4:16781

--- Modules/Core/Common/include/itkMetaDataObject.h
+++ Modules/Core/Common/include/itkMetaDataObject.h
@@ -216,8 +216,6 @@ private:
  * \param Dictionary TODO
  * \param key TODO
  * \param invalue the value of type T that is to be encapsulated.
- * \return A smartpointer to a MetaDataObject that is suitable for
- * insertion into a MetaDataDictionary.
  */
 template <typename T>
 inline void
