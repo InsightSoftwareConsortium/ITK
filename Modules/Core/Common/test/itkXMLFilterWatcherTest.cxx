4a9a469b2eeb800f7ed899aff5d8dad3e74e1a35
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2023-11-12 22:59:18 -0500

    ENH: Add test for `itk::XMLFilterWatcher`
    Add test for `itk::XMLFilterWatcher`: the watcher is applied to an image
    reading filter, and the progress updates written to the standard output
    by the filter are redirected to a text file through the CMake
    `--redirectOutput` option.

---
+++ Modules/Core/Common/test/itkXMLFilterWatcherTest.cxx
@@ -0,0 +1,52 @@
+/*=========================================================================
+ *
+ *  Copyright NumFOCUS
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         https://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkXMLFilterWatcher.h"
+#include "itkTestingMacros.h"
+
+
+int
+itkXMLFilterWatcherTest(int argc, char * argv[])
+{
+  if (argc != 2)
+  {
+    std::cerr << "Missing parameters." << std::endl;
+    std::cerr << "Usage: " << itkNameOfTestExecutableMacro(argv);
+    std::cerr << " inputFileName" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  constexpr unsigned int Dimension = 2;
+
+  using PixelType = float;
+  using ImageType = itk::Image<PixelType, Dimension>;
+
+  using FilterType = itk::ImageFileReader<ImageType>;
+
+  auto reader = FilterType::New();
+
+  reader->SetFileName(argv[1]);
+
+  itk::XMLFilterWatcher watcher(reader, "filter");
+
+  reader->Update();
+
+  return EXIT_SUCCESS;
+}
