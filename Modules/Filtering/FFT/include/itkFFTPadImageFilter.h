98dece1a9f49d2ed80dcfbcc320a5dbf38750b85
Bradley Lowekamp <blowekamp@mail.nih.gov>
2024-06-24 07:08:19 -0400

    DOC: Correct Documentation for PadImageFilter::SizeGreatestPrimeFactor

--- Modules/Filtering/FFT/include/itkFFTPadImageFilter.h
+++ Modules/Filtering/FFT/include/itkFFTPadImageFilter.h
@@ -82,10 +82,12 @@ public:
 
   /**
    * Set/Get the greatest prime factor allowed on the size of the padded image.
-   * The filter increase the size of the image to reach a size with the greatest
-   * prime factor smaller or equal to the specified value. The default value is
-   * 13, which is the greatest prime number for which the FFT are precomputed
-   * in FFTW, and thus gives very good performance.
+   *
+   * The filter increases the dimensional sizes of the image to reach a size with the greatest
+   * prime factor smaller or equal to the specified value. The default values is
+   * obtained from the ForwardFFTImageFilter, which is 13 for FFTW,
+   * and 5 for VNL.
+   *
    * A greatest prime factor of 2 produce a size which is a power of 2, and thus
    * is suitable for vnl base fft filters.
    * A greatest prime factor of 1 or less - typically 0 - disable the extra padding.
