edee609da1ffdd08ec7820132da2bef194d8569b
Sean McBride <sean@rogue-research.com>
2024-06-26 14:06:40 -0400

    COMP: removed hack for old compilers avoiding =defaullt

--- Modules/Core/Transform/include/itkTransform.h
+++ Modules/Core/Transform/include/itkTransform.h
@@ -575,16 +575,8 @@ protected:
   Transform() = default;
 
   Transform(NumberOfParametersType numberOfParameters);
-#if defined(__GNUC__)
-  // A bug in some versions of the GCC and Clang compilers
-  // result in an ICE or linker error when "= default" is requested.
-  // This was observed in at least gcc 4.8 and 5.4.0, and
-  // AppleClang 7.0.2 and 8.0.0. Probably others too.
-  // "= default" doesn't gain us much, so just don't use it here.
-  ~Transform() override{};
-#else
   ~Transform() override = default;
-#endif
+
   mutable ParametersType      m_Parameters{};
   mutable FixedParametersType m_FixedParameters{};
 
