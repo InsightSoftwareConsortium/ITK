0b03a81a6cc1d7ec0adfdc9d6045cca357115dfe
Niels Dekker <N.Dekker@lumc.nl>
2024-09-09 16:00:48 +0200

    STYLE: Replace `#ifdef`s in "itkByteSwapper.hxx" with `if constexpr`
    Using the private static member `m_SystemIsBigEndian` that was introduced with
    pull request https://github.com/InsightSoftwareConsortium/ITK/pull/4835
    commit a44863b27d39415f97ee5b14f9a1864d2d614270
    "ENH: Declare ByteSwapper SystemIsBigEndian/LittleEndian `constexpr`"
    
    Added `[[maybe_unused]]` attributes to prevent warnings like:
    > warning: parameter 'p' set but not used [-Wunused-but-set-parameter]

--- Modules/Core/Common/include/itkByteSwapper.hxx
+++ Modules/Core/Common/include/itkByteSwapper.hxx
@@ -39,202 +39,170 @@ namespace itk
 //------Big Endian methods----------------------------------------------
 
 // Use different swap methods based on type
-#ifdef CMAKE_WORDS_BIGENDIAN
+
 template <typename T>
 void
-ByteSwapper<T>::SwapFromSystemToBigEndian(T *)
-{}
-#else
-template <typename T>
-void
-ByteSwapper<T>::SwapFromSystemToBigEndian(T * p)
+ByteSwapper<T>::SwapFromSystemToBigEndian([[maybe_unused]] T * p)
 {
-  switch (sizeof(T))
+  if constexpr (!m_SystemIsBigEndian)
   {
-    case 1:
-      return;
-    case 2:
-      Self::Swap2(p);
-      return;
-    case 4:
-      Self::Swap4(p);
-      return;
-    case 8:
-      Self::Swap8(p);
-      return;
-    default:
-      itkGenericExceptionMacro("Cannot swap number of bytes requested");
+    switch (sizeof(T))
+    {
+      case 1:
+        return;
+      case 2:
+        Self::Swap2(p);
+        return;
+      case 4:
+        Self::Swap4(p);
+        return;
+      case 8:
+        Self::Swap8(p);
+        return;
+      default:
+        itkGenericExceptionMacro("Cannot swap number of bytes requested");
+    }
   }
 }
 
-#endif
-
-#ifdef CMAKE_WORDS_BIGENDIAN
 template <typename T>
 void
-ByteSwapper<T>::SwapRangeFromSystemToBigEndian(T *, BufferSizeType)
+ByteSwapper<T>::SwapRangeFromSystemToBigEndian([[maybe_unused]] T * p, [[maybe_unused]] BufferSizeType num)
 {
-  // nothing needs to be done here...
-}
-
-#else
-
-#  ifdef __INTEL_COMPILER
-#    pragma warning disable 280 // remark #280: selector expression is constant
-#  endif
-template <typename T>
-void
-ByteSwapper<T>::SwapRangeFromSystemToBigEndian(T * p, BufferSizeType num)
-{
-  switch (sizeof(T))
+  if constexpr (!m_SystemIsBigEndian)
   {
-    case 1:
-      return;
-    case 2:
-      Self::Swap2Range(p, num);
-      return;
-    case 4:
-      Self::Swap4Range(p, num);
-      return;
-    case 8:
-      Self::Swap8Range(p, num);
-      return;
-    default:
-      itkGenericExceptionMacro("Cannot swap number of bytes requested");
+    switch (sizeof(T))
+    {
+      case 1:
+        return;
+      case 2:
+        Self::Swap2Range(p, num);
+        return;
+      case 4:
+        Self::Swap4Range(p, num);
+        return;
+      case 8:
+        Self::Swap8Range(p, num);
+        return;
+      default:
+        itkGenericExceptionMacro("Cannot swap number of bytes requested");
+    }
   }
 }
 
-#endif
-
-#ifdef CMAKE_WORDS_BIGENDIAN
 template <typename T>
 void
 ByteSwapper<T>::SwapWriteRangeFromSystemToBigEndian(const T * p, int num, OStreamType * fp)
 {
-  num *= sizeof(T);
-  fp->write(reinterpret_cast<const char *>(p), num);
-}
-
-#else
-template <typename T>
-void
-ByteSwapper<T>::SwapWriteRangeFromSystemToBigEndian(const T * p, int num, OStreamType * fp)
-{
-  switch (sizeof(T))
+  if constexpr (m_SystemIsBigEndian)
   {
-    case 1:
-      return;
-    case 2:
-      Self::SwapWrite2Range(p, num, fp);
-      return;
-    case 4:
-      Self::SwapWrite4Range(p, num, fp);
-      return;
-    case 8:
-      Self::SwapWrite8Range(p, num, fp);
-      return;
-    default:
-      itkGenericExceptionMacro("Cannot swap number of bytes requested");
+    num *= sizeof(T);
+    fp->write(reinterpret_cast<const char *>(p), num);
+  }
+  else
+  {
+    switch (sizeof(T))
+    {
+      case 1:
+        return;
+      case 2:
+        Self::SwapWrite2Range(p, num, fp);
+        return;
+      case 4:
+        Self::SwapWrite4Range(p, num, fp);
+        return;
+      case 8:
+        Self::SwapWrite8Range(p, num, fp);
+        return;
+      default:
+        itkGenericExceptionMacro("Cannot swap number of bytes requested");
+    }
   }
 }
 
-#endif
 
 //------Little Endian methods----------------------------------------------
 
-#ifdef CMAKE_WORDS_BIGENDIAN
 template <typename T>
 void
-ByteSwapper<T>::SwapFromSystemToLittleEndian(T * p)
+ByteSwapper<T>::SwapFromSystemToLittleEndian([[maybe_unused]] T * p)
 {
-  switch (sizeof(T))
+  if constexpr (m_SystemIsBigEndian)
   {
-    case 1:
-      return;
-    case 2:
-      Self::Swap2(p);
-      return;
-    case 4:
-      Self::Swap4(p);
-      return;
-    case 8:
-      Self::Swap8(p);
-      return;
-    default:
-      itkGenericExceptionMacro("Cannot swap number of bytes requested");
+    switch (sizeof(T))
+    {
+      case 1:
+        return;
+      case 2:
+        Self::Swap2(p);
+        return;
+      case 4:
+        Self::Swap4(p);
+        return;
+      case 8:
+        Self::Swap8(p);
+        return;
+      default:
+        itkGenericExceptionMacro("Cannot swap number of bytes requested");
+    }
   }
 }
 
-#else
 template <typename T>
 void
-ByteSwapper<T>::SwapFromSystemToLittleEndian(T *)
-{}
-#endif
-
-#ifdef CMAKE_WORDS_BIGENDIAN
-template <typename T>
-void
-ByteSwapper<T>::SwapRangeFromSystemToLittleEndian(T * p, BufferSizeType num)
+ByteSwapper<T>::SwapRangeFromSystemToLittleEndian([[maybe_unused]] T * p, [[maybe_unused]] BufferSizeType num)
 {
-  switch (sizeof(T))
+  if constexpr (m_SystemIsBigEndian)
   {
-    case 1:
-      return;
-    case 2:
-      Self::Swap2Range(p, num);
-      return;
-    case 4:
-      Self::Swap4Range(p, num);
-      return;
-    case 8:
-      Self::Swap8Range(p, num);
-      return;
-    default:
-      itkGenericExceptionMacro("Cannot swap number of bytes requested");
+    switch (sizeof(T))
+    {
+      case 1:
+        return;
+      case 2:
+        Self::Swap2Range(p, num);
+        return;
+      case 4:
+        Self::Swap4Range(p, num);
+        return;
+      case 8:
+        Self::Swap8Range(p, num);
+        return;
+      default:
+        itkGenericExceptionMacro("Cannot swap number of bytes requested");
+    }
   }
 }
 
-#else
-template <typename T>
-void
-ByteSwapper<T>::SwapRangeFromSystemToLittleEndian(T *, BufferSizeType)
-{}
-#endif
-
-#ifdef CMAKE_WORDS_BIGENDIAN
 template <typename T>
 void
 ByteSwapper<T>::SwapWriteRangeFromSystemToLittleEndian(const T * p, int num, OStreamType * fp)
 {
-  switch (sizeof(T))
+  if constexpr (m_SystemIsBigEndian)
   {
-    case 1:
-      return;
-    case 2:
-      Self::SwapWrite2Range(p, num, fp);
-      return;
-    case 4:
-      Self::SwapWrite4Range(p, num, fp);
-      return;
-    case 8:
-      Self::SwapWrite8Range(p, num, fp);
-      return;
-    default:
-      itkGenericExceptionMacro("Cannot swap number of bytes requested");
+    switch (sizeof(T))
+    {
+      case 1:
+        return;
+      case 2:
+        Self::SwapWrite2Range(p, num, fp);
+        return;
+      case 4:
+        Self::SwapWrite4Range(p, num, fp);
+        return;
+      case 8:
+        Self::SwapWrite8Range(p, num, fp);
+        return;
+      default:
+        itkGenericExceptionMacro("Cannot swap number of bytes requested");
+    }
+  }
+  else
+  {
+    num *= sizeof(T);
+    fp->write(reinterpret_cast<const char *>(p), num);
   }
 }
 
-#else
-template <typename T>
-void
-ByteSwapper<T>::SwapWriteRangeFromSystemToLittleEndian(const T * p, int num, OStreamType * fp)
-{
-  num *= sizeof(T);
-  fp->write(reinterpret_cast<const char *>(p), num);
-}
-
-#endif
 
 // The following are the protected methods -------------------------
 //
