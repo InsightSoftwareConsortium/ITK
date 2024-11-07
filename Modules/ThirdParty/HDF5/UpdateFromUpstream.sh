4e209dbf1b4b57412b77659fcd10552b3ef36c93
Bradley Lowekamp <blowekamp@mail.nih.gov>
2024-11-07 14:51:09 -0500

    ENH: script to update to HDF5 1.12.3

--- Modules/ThirdParty/HDF5/UpdateFromUpstream.sh
+++ Modules/ThirdParty/HDF5/UpdateFromUpstream.sh
@@ -8,14 +8,13 @@ readonly name="HDF5"
 readonly ownership="HDF5 Maintainers <hdf5-maintainers@hdfgroup.org>"
 readonly subtree="Modules/ThirdParty/HDF5/src/itkhdf5"
 readonly repo="https://github.com/HDFGroup/hdf5.git"
-readonly tag="hdf5-1_12_1"
+readonly tag="hdf5-1_12_3"
 readonly shortlog=false
 readonly paths="
    ACKNOWLEDGMENTS
    CTestConfig.cmake
-   README.txt
+   README.md
    config/cmake
-   config/cmake_ext_mod
    config/clang-warnings
    config/gnu-warnings
    config/intel-warnings
@@ -40,7 +39,7 @@ extract_source () {
     sed -i.bak -e '1 s| /bin/sh|/bin/sh|' c++/src/h5c++.in config/cmake/libh5cc.in
     rm c++/src/h5c++.in.bak config/cmake/libh5cc.in.bak
     chmod a+x c++/src/h5c++.in config/cmake/libh5cc.in
-    chmod a-x README.txt
+    chmod a-x README.md
     chmod a-x config/cmake/CTestScript.cmake
     chmod a-x config/cmake/HDF5_Examples_options.cmake
     chmod a-x config/cmake/libhdf5.pc.in
@@ -62,6 +61,7 @@ extract_source () {
     find . -name Makefile.am -delete
     find . -name Makefile.in -delete
     find . -name "*.lnt" -delete
+    find . -name "*.icns" -delete
     echo "* -whitespace" > .gitattributes
     popd
 }
