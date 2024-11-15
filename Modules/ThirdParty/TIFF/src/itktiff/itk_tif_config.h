07f2df4219423b8d49e4782941f4694cdec40aea
Brad King <brad.king@kitware.com>
2024-11-15 11:25:30 -0500

    ENH: itktiff: Update source tree layout to match upstream

---
+++ Modules/ThirdParty/TIFF/src/itktiff/itk_tif_config.h
@@ -0,0 +1,3 @@
+/* This allows "tiff.h" to include "itk_tif_config.h" in the ITK build.
+   Do NOT install this header.  */
+#include "libtiff/itk_tif_config.h"
---
+++ Modules/ThirdParty/TIFF/src/itktiff/itk_tiff_mangle.h
@@ -0,0 +1,3 @@
+/* This allows "tiff.h" to include "itk_tiff_mangle.h" in the ITK build.
+   Do NOT install this header.  */
+#include "libtiff/itk_tiff_mangle.h"
