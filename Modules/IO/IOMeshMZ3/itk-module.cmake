24cb8fae9daf915fe9f23833f6baee1414f9226d
Matt McCormick <matt@mmmccormick.com>
2024-12-01 15:14:03 -0500

    ENH: Initial commit, ITKModuleTemplate output

---
+++ Modules/IO/IOMeshMZ3/itk-module.cmake
@@ -0,0 +1,27 @@
+# the top-level README is used for describing this module, just
+# re-used it for documentation here
+get_filename_component(MY_CURRENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+file(READ "${MY_CURRENT_DIR}/README.md" DOCUMENTATION)
+
+# itk_module() defines the module dependencies in IOMeshMZ3
+# IOMeshMZ3 depends on ITKCommon
+# The testing module in IOMeshMZ3 depends on ITKTestKernel
+# and ITKMetaIO(besides IOMeshMZ3 and ITKCore)
+# By convention those modules outside of ITK are not prefixed with
+# ITK.
+
+# define the dependencies of the include module and the tests
+itk_module(
+  IOMeshMZ3
+  DEPENDS
+    ITKCommon
+    ITKStatistics
+  COMPILE_DEPENDS
+    ITKImageSources
+  TEST_DEPENDS
+    ITKTestKernel
+    ITKMetaIO
+  DESCRIPTION "${DOCUMENTATION}"
+  EXCLUDE_FROM_DEFAULT
+  ENABLE_SHARED
+)
