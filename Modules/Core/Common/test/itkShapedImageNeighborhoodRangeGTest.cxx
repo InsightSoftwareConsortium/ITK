c0a756ae410fe4cb17149dcc9b4251b6166f5b4c
Hans Johnson <hans-johnson@uiowa.edu>
2024-12-01 13:47:35 -0600

    COMP: Fix unused-lambda-capture statement
    lambda capture 'n' is not required to be captured for this use [-Wunused-lambda-capture]

--- Modules/Core/Common/test/itkShapedImageNeighborhoodRangeGTest.cxx
+++ Modules/Core/Common/test/itkShapedImageNeighborhoodRangeGTest.cxx
@@ -770,12 +770,12 @@ TEST(ShapedImageNeighborhoodRange, IteratorsSupportRandomAccess)
 
   {
     // Expression to be tested: 'r += n'
-    const difference_type n = 3;
+    constexpr difference_type n = 3;
 
     static_assert(std::is_same_v<decltype(r += n), X &>, "Return type tested");
 
     r = initialIterator;
-    const auto expectedResult = [&r, n] {
+    const auto expectedResult = [&r](const difference_type n) {
       // Operational semantics, as specified by the C++11 Standard:
       difference_type m = n;
       if (m >= 0)
@@ -785,53 +785,53 @@ TEST(ShapedImageNeighborhoodRange, IteratorsSupportRandomAccess)
         while (m++)
           --r;
       return r;
-    }();
+    }(n);
     r = initialIterator;
     const auto actualResult = r += n;
     EXPECT_EQ(actualResult, expectedResult);
   }
   {
     // Expressions to be tested: 'a + n' and 'n + a'
-    difference_type const n = 3;
+    constexpr difference_type n = 3;
 
     static_assert(std::is_same_v<decltype(a + n), X>, "Return type tested");
     static_assert(std::is_same_v<decltype(n + a), X>, "Return type tested");
 
-    const auto expectedResult = [a, n] {
+    const auto expectedResult = [a](const difference_type n) {
       // Operational semantics, as specified by the C++11 Standard:
       X tmp = a;
       return tmp += n;
-    }();
+    }(n);
 
     EXPECT_EQ(a + n, expectedResult);
     EXPECT_TRUE(a + n == n + a);
   }
   {
     // Expression to be tested: 'r -= n'
-    difference_type const n = 3;
+    constexpr difference_type n = 3;
 
     static_assert(std::is_same_v<decltype(r -= n), X &>, "Return type tested");
 
     r = initialIterator;
-    const auto expectedResult = [&r, n] {
+    const auto expectedResult = [&r](const difference_type n) {
       // Operational semantics, as specified by the C++11 Standard:
       return r += -n;
-    }();
+    }(n);
     r = initialIterator;
     const auto actualResult = r -= n;
     EXPECT_EQ(actualResult, expectedResult);
   }
   {
     // Expression to be tested: 'a - n'
-    difference_type const n = -3;
+    constexpr difference_type n = -3;
 
     static_assert(std::is_same_v<decltype(a - n), X>, "Return type tested");
 
-    const auto expectedResult = [a, n] {
+    const auto expectedResult = [a](const difference_type n) {
       // Operational semantics, as specified by the C++11 Standard:
       X tmp = a;
       return tmp -= n;
-    }();
+    }(n);
 
     EXPECT_EQ(a - n, expectedResult);
   }
@@ -839,13 +839,13 @@ TEST(ShapedImageNeighborhoodRange, IteratorsSupportRandomAccess)
     // Expression to be tested: 'b - a'
     static_assert(std::is_same_v<decltype(b - a), difference_type>, "Return type tested");
 
-    difference_type const n = b - a;
+    const difference_type n = b - a;
     EXPECT_TRUE(a + n == b);
     EXPECT_TRUE(b == a + (b - a));
   }
   {
     // Expression to be tested: 'a[n]'
-    const difference_type n = 3;
+    constexpr difference_type n = 3;
     static_assert(std::is_convertible_v<decltype(a[n]), reference>, "Return type tested");
     EXPECT_EQ(a[n], *(a + n));
   }
