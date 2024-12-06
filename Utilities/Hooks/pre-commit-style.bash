8b99c00ebee885554426470066739df14ca714f0
Hans Johnson <hans-johnson@uiowa.edu>
2024-12-05 18:52:23 -0600

    STYLE: Remove redundant cmake based clang-format
    Remove the cmake based custom download and running of
    clang-format with shell scripting.
    
    Prefer the more robust and easier to maintain
    pre-commit clang-format instrumentation.

--- Utilities/Hooks/pre-commit-style.bash
+++ Utilities/Hooks/pre-commit-style.bash
@@ -14,7 +14,7 @@
 # limitations under the License.
 #=============================================================================
 
-# Run clangformat and KWStyle pre-commit hooks.
+# Run KWStyle pre-commit hooks.
 #
 # 'git config' is used to enable the hooks and set their configuration files.
 # The repository .gitattributes must also enable the hooks on the targeted
@@ -29,8 +29,6 @@ die() {
 }
 
 do_KWStyle=$(git config --bool hooks.KWStyle) || do_KWStyle=true
-
-do_clangformat=$(git config --bool hooks.clangformat) || do_clangformat=true
 
 #-----------------------------------------------------------------------------
 # Check if we want to run the style on a given file.  Uses git attributes.  If
@@ -146,168 +144,9 @@ run_KWStyle() {
   done
 }
 
-#-----------------------------------------------------------------------------
-# clangformat.
-check_for_clangformat() {
-  clangformat_required_version=19.1.4
-  system_tools="
-    clang-format-$clangformat_required_version
-    clang-format
-"
-  for tool in $system_tools; do
-    if type -p "$tool" >/dev/null; then
-      system_clang_format="$tool"
-      break
-    fi
-  done
-  clangformat_path=$(git config clangFormat.binary) ||
-  clangformat_path=$(type -p "$system_clang_format" >/dev/null) ||
-  die "clang-format executable was not found.
-
-A clang-format binary will be downloaded and configured when ITK
-is built with the (i) BUILD_TESTING=True and (ii) ITK_USE_CLANG_FORMAT=True CMake configuration options enabled.
-
-Alternatively, install clang-format version $clangformat_required_version or set the executable location with
-
-  git config clangFormat.binary /path/to/clang-format
-"
-  if ! "$clangformat_path" --version | grep "clang-format version $clangformat_required_version" >/dev/null 2>/dev/null; then
-    die "clang-format version $clangformat_required_version is required
-
-Set the path the clang-format $clangformat_required_version executable with
-
-  git config clangFormat.binary /path/to/clang-format
-
-or disable the clang-format pre-commit hook with
-
-  git config hooks.clangformat false
-"
-  fi
-}
-
-run_clang_format_check_attr()
-{
-    IN=$1
-    OUT=$2
-    ERR=$3
-    "${clangformat_path}" -style=file "$IN" > "$OUT" 2> "$ERR"
-    return $?
-}
-
-run_clangformat_on_file() {
-  MERGED="$1"
-  if run_style_on_file "$MERGED" "clangformat"; then
-    ext="$$$(expr "$MERGED" : '.*\(\.[^/]*\)$')"
-    BACKUP="./$MERGED.BACKUP.$ext"
-    LOCAL="./$MERGED.STAGED.$ext"
-    REMOTE="./$MERGED.CLANGFORMAT.$ext"
-    NEW_MERGED="./$MERGED.NEW.$ext"
-    ERROR_LOG="./$MERGED.$ext.log"
-    OLD_MERGED="$MERGED"
-
-    mv -- "$MERGED" "$BACKUP"
-    # We temporarily change MERGED because the file might already be open, and
-    # the text editor may complain.
-    MERGED="$NEW_MERGED"
-    cp -- "$BACKUP" "$LOCAL"
-
-    run_clang_format_check_attr "$LOCAL" "$REMOTE" "$ERROR_LOG"
-    clang_format_status=$?
-    if [  $clang_format_status -ne 0 ]; then
-      mv -- "$BACKUP" "$OLD_MERGED"
-
-      if $merge_keep_temporaries; then
-        rm -f -- "$LOCAL" "$REMOTE" "$BACKUP"
-      fi
-      die "error when running clang-format on $OLD_MERGED"
-    fi
-    cp -- "$REMOTE" "$MERGED"
-
-    if test $(git hash-object -- "$LOCAL") != $(git hash-object -- "$REMOTE"); then
-      if [ "$(uname)" == "Darwin" ]; then
-        run_merge_tool "$merge_tool" "false" </dev/tty
-      elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-        run_merge_tool "$merge_tool" "false" </dev/tty
-      else
-        run_merge_tool "$merge_tool" "false"
-      fi
-      if [ $? -ne 0 ]; then
-        mv -- "$BACKUP" "$OLD_MERGED"
-
-        if $merge_keep_temporaries; then
-          rm -f -- "$LOCAL" "$REMOTE" "$BACKUP" "$NEW_MERGED"
-        fi
-
-        die "clang-format merge of $OLD_MERGED failed
-        Error log: $ERROR_LOG"
-      fi
-    fi
-
-    mv -- "$NEW_MERGED" "$OLD_MERGED"
-    MERGED="$OLD_MERGED"
-
-    if $merge_keep_backup; then
-      mv -- "$BACKUP" "$MERGED.orig"
-    else
-      rm -- "$BACKUP"
-    fi
-
-    git add -- "$MERGED"
-    rm -f -- "$LOCAL" "$REMOTE" "$BACKUP" "$ERROR_LOG" "$NEW_MERGED"
-
-  fi # end if run clang-format on file
-
-  if $do_KWStyle &&
-    $have_KWStyle &&
-    run_style_on_file "$MERGED" KWStyle
-  then
-    run_KWStyle_on_file "$MERGED"
-  else
-    return 0
-  fi
-}
-
-run_clangformat() {
-  $do_KWStyle && check_for_KWStyle
-  if test $?; then
-    have_KWStyle=true
-  else
-    have_KWStyle=false
-  fi
-
-  merge_tool=$(get_merge_tool "$merge_tool") || die "Merge tool not configured.
-
-Set the merge tool with
-
-  git config merge.tool <toolname>
-
-For more information, see
-
-  git help mergetool"
-  merge_keep_backup="$(git config --bool mergetool.keepBackup || echo true)"
-  merge_keep_temporaries="$(git config --bool mergetool.keepTemporaries || echo false)"
-  git diff-index --cached --diff-filter=ACMR --name-only HEAD -- |
-  while read MERGED; do
-    run_clangformat_on_file "$MERGED" || return
-  done # end for changed files
-}
-
 # Do not run during merge commits for now.
 if test -f "$GIT_DIR/MERGE_HEAD"; then
   :
-elif $do_clangformat; then
-  # We use git-mergetool settings to review the clangformat changes.
-  TOOL_MODE=merge
-  . "$(git --exec-path)/git-mergetool--lib"
-  # Redefine check_unchanged because we do not need to check if the merge was
-  # successful.
-  check_unchanged() {
-    status=0
-  }
-  check_for_clangformat
-  run_clangformat || exit 1
-# do_clangformat will run KWStyle on the files incrementally so excessive
-# clangformat merges do not have to occur.
 elif $do_KWStyle; then
   if check_for_KWStyle; then
     run_KWStyle || exit 1
