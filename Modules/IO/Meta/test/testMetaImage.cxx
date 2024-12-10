81f50c90cfffdd602a343d11ef72b7a2295aeeb8
Hans Johnson <hans-johnson@uiowa.edu>
2024-12-09 20:09:20 -0600

    COMP: Fix windows function ambiguity error
    Modules\IO\Meta\test\testMetaImage.cxx(154): error C2666: 'MetaImage::SequenceID': 2 overloads have similar conversions
    Modules\ThirdParty\MetaIO\src\MetaIO\src\metaImage.h(182): note: could be 'void MetaImage::SequenceID(const float *)'
    Modules\ThirdParty\MetaIO\src\MetaIO\src\metaImage.h(180): note: or       'float MetaImage::SequenceID(int) const'
    Modules\IO\Meta\test\testMetaImage.cxx(154): note: while trying to match the argument list '(const int)'
    Modules\IO\Meta\test\testMetaImage.cxx(154): note: note: qualification adjustment (const/volatile) may be causing the ambiguity
    
    Modules\IO\Meta\test\testMetaImage.cxx(165): error C2666: 'MetaImage::ElementSize': 3 overloads have similar conversions
    Modules\ThirdParty\MetaIO\src\MetaIO\src\metaImage.h(201): note: could be 'void MetaImage::ElementSize(const float *)'
    Modules\ThirdParty\MetaIO\src\MetaIO\src\metaImage.h(199): note: or       'void MetaImage::ElementSize(const double *)'
    Modules\ThirdParty\MetaIO\src\MetaIO\src\metaImage.h(197): note: or       'double MetaImage::ElementSize(int) const'
    Modules\IO\Meta\test\testMetaImage.cxx(165): note: while trying to match the argument list '(const int)'
    Modules\IO\Meta\test\testMetaImage.cxx(165): note: note: qualification adjustment (const/volatile) may be causing the ambiguity

--- Modules/IO/Meta/test/testMetaImage.cxx
+++ Modules/IO/Meta/test/testMetaImage.cxx
@@ -140,7 +140,7 @@ testMetaImage(int, char *[])
 
   MetaImage tIm2("test.mha");
 
-  const int im2Zero = 0;
+  int im2Zero = 0;
   std::cout << "Header size = " << tIm2.HeaderSize() << std::endl;
   tIm2.HeaderSize(tIm2.HeaderSize());
   tIm2.Modality(MET_MOD_CT);
