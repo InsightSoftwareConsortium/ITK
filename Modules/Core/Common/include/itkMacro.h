a6e7085859d617d1d7803e69cf7c995fb5b058ea
Bradley Lowekamp <blowekamp@mail.nih.gov>
2024-12-12 07:59:08 -0500

    COMP: Remove circular dependency with ExceptionObject
    Reduce the number of files that need to be read by the pre-processor and
    remove the circular include dependancies.
    
    The itkExceptionObject.h file is allowed to be included *only* by
    itkMacro.h which always included the file.
    
    Moving the inclusion of itkExceptionObject.h to the end of
    itkMacro.h simplifies the dependency complexity.
    
    itk::ExceptionObject is different from other classes because it depends
    on macros at the top of itkMacro.h for it's definition, but also is
    needed for many macros defnined in itkMacro.h.
    
    Removes 10000's of warnings like:
    ITK/Modules/Core/Common/include/itkExceptionObject.h:22:12:
    warning: circular header file dependency detected while including 'itkMacro.h', please check the include path [misc-header-include-cycle]
       22 | #  include "itkMacro.h"
          |            ^
    
    Co-Authored by: Hans Johnson <hans-johnson@uiowa.edu>

--- Modules/Core/Common/include/itkMacro.h
+++ Modules/Core/Common/include/itkMacro.h
@@ -1340,39 +1340,6 @@ compilers.
   }                                                                                                                   \
   ITK_MACROEND_NOOP_STATEMENT
 
-
-#define itkExceptionObject_h
-#include "itkExceptionObject.h"
-#undef itkExceptionObject_h
-
-/** itkDynamicCastInDebugMode
- * Use static_cast in Release builds, and dynamic_cast in Debug
- *
- * Note: this must come after:
- *
- *   #include "itkExceptionObject.h"
- */
-template <typename TTarget, typename TSource>
-TTarget
-itkDynamicCastInDebugMode(TSource x)
-{
-#ifndef NDEBUG
-  if (x == nullptr)
-  {
-    return nullptr;
-  }
-  TTarget rval = dynamic_cast<TTarget>(x);
-  if (rval == nullptr)
-  {
-    itkGenericExceptionMacro("Failed dynamic cast to " << typeid(TTarget).name()
-                                                       << " object type = " << x->GetNameOfClass());
-  }
-  return rval;
-#else
-  return static_cast<TTarget>(x);
-#endif
-}
-
 // ITK_FUTURE_DEPRECATED is only for internal use, within the implementation of ITK. It allows triggering "deprecated"
 // warnings when legacy support is removed, which warn that a specific feature may be removed in the future.
 #if defined(ITK_LEGACY_REMOVE) && !defined(ITK_LEGACY_SILENT)
@@ -1471,4 +1438,9 @@ itkDynamicCastInDebugMode(TSource x)
 #  define ITK_ITERATOR_FINAL    /*purposefully empty for ITKv6, iterators are not virtual for performance reasons*/
 #endif
 
+#define allow_inclusion_of_itkExceptionObject_h
+// Must include itkExceptionObject.h at the end of the file
+// because it depends on the macros defined above
+#include "itkExceptionObject.h"
+#undef allow_inclusion_of_itkExceptionObject_h
 #endif // itkMacro_h
