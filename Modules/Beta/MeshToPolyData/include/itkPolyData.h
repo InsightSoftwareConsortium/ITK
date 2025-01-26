4e01d203369223497110cda888ac9f68b02d9956
Hans Johnson <hans-johnson@uiowa.edu>
2025-01-26 08:59:37 -0600

    STYLE: Add itkVirtualGetNameOfClassMacro + itkOverrideGetNameOfClassMacro
    Added two new macro's, intended to replace the old 'itkTypeMacro' and
    'itkTypeMacroNoParent'.
    
    The main aim is to be clearer about what those macro's do: add a virtual
    'GetNameOfClass()' member function and override it. Unlike 'itkTypeMacro',
    'itkOverrideGetNameOfClassMacro' does not have a 'superclass' parameter, as it
    was not used anyway.
    
    Note that originally 'itkTypeMacro' did not use its 'superclass' parameter
    either, looking at commit 699b66cb04d410e555656828e8892107add38ccb, Will
    Schroeder, June 27, 2001:
    https://github.com/InsightSoftwareConsortium/ITK/blob/699b66cb04d410e555656828e8892107add38ccb/Code/Common/itkMacro.h#L331-L337

--- Modules/Beta/MeshToPolyData/include/itkPolyData.h
+++ Modules/Beta/MeshToPolyData/include/itkPolyData.h
@@ -49,7 +49,7 @@ public:
   itkNewMacro(Self);
 
   /** Run-time type information (and related methods). */
-  itkTypeMacro(PolyData, DataObject);
+  itkOverrideGetNameOfClassMacro(PolyData);
 
   /** Type of PointData or CellData */
   using PixelType = TPixel;
