b45f9b0f0477d2bbd6a9fb4573aa2b81a5f75143
Hans Johnson <hans-johnson@uiowa.edu>
2025-01-26 12:10:17 -0600

    STYLE: Replace itkStaticConstMacro with static constexpr
    Use static constexpr directly now that C++11 conformance
    is required by all compilers.
    
    :%s/itkStaticConstMacro *( *\([^,]*\),[ \_s]*\([^,]*\),\_s*\([^)]*\)) */static constexpr \2 \1 = \3/ge
    
    'itkStaticConstMacro(name, type, value)' became unconditionally
    identical to 'static constexpr type name = value' with ITK commit
    aec95193ab00e1322039911e1032da00f3a103b6 "ENH: Update compiler macros (#810)",
    maekclena, 7 May 2019.
    
    'itkGetStaticConstMacro(name)' became unconditionally identical to
    '(Self::name)' with ITK commit 84e490b81e3f3c2b0edb89ae7b9de53bfc52f2b2
    "Removing some outdated compiler conditionals", Hans Johnson, 31 July
    2010.
    
    Most 'itkStaticConstMacro' calls were removed by ITK commit 5c14741e1e063a132ea7e7ee69c5bd0a4e49af74

--- Modules/Filtering/Thickness3D/include/itkBinaryThinningImageFilter3D.h
+++ Modules/Filtering/Thickness3D/include/itkBinaryThinningImageFilter3D.h
@@ -116,8 +116,8 @@ public:
   GetThinning();
 
   /** ImageDimension enumeration   */
-  itkStaticConstMacro(InputImageDimension, unsigned int, TInputImage::ImageDimension);
-  itkStaticConstMacro(OutputImageDimension, unsigned int, TOutputImage::ImageDimension);
+  static constexpr unsigned int InputImageDimension = TInputImage::ImageDimension;
+  static constexpr unsigned int OutputImageDimension = TOutputImage::ImageDimension;
 
 #ifdef ITK_USE_CONCEPT_CHECKING
   /** Begin concept checking */
