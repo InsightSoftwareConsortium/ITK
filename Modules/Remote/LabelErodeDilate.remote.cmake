657d0a783b3a115279449be9c188870fd53134a5
Bradley Lowekamp <blowekamp@mail.nih.gov>
2025-02-26 09:20:58 -0500

    BUG: Address valgrind defect in LabelErodeDilate
    Update remote module to v1.3.1 hash to address valgrind defects.

--- Modules/Remote/LabelErodeDilate.remote.cmake
+++ Modules/Remote/LabelErodeDilate.remote.cmake
@@ -53,5 +53,5 @@ itk_fetch_module(
   MODULE_COMPLIANCE_LEVEL 2
   #UPSTREAM_GIT_REPOSITORY
   GIT_REPOSITORY https://github.com/InsightSoftwareConsortium/ITKLabelErodeDilate.git
-  GIT_TAG 7e23b12be913ff2bd935d9ffc2d10b573a923501
+  GIT_TAG 08c2551096c06fe993b0d5a977a752fb21972896
   )
