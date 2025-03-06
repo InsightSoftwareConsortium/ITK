InsightSoftwareConsortium/ITK

commit 70fd79f43c089bec068ba6f6657baca12996842d

ENH:  Adding transform parameters adaptors.
This commit entails a set of adaptors to be used for
multiresolution image registration.  During the course
of image registration, the user might want to expand
the number of parameters describing a transform to
provide more flexibility during optimization.  Relevant
transforms include B-spline and displacement field
transforms.

Change-Id: Id0ed30320468cdf907620952399e0a0841ea327f

Modules/Filtering/DisplacementField/include/itkDisplacementFieldTransform.hxx
@@ -32,10 +32,14 @@ namespace itk
  * Constructor
  */
 template <class TScalar, unsigned int NDimensions>
-DisplacementFieldTransform<TScalar, NDimensions>::DisplacementFieldTransform() : Superclass( 0 )
+DisplacementFieldTransform<TScalar, NDimensions>::DisplacementFieldTransform()
+: Superclass( 0 )
 {
   this->m_DisplacementField = NULL;
   this->m_InverseDisplacementField = NULL;
+
+  this->m_FixedParameters.SetSize( NDimensions * ( NDimensions + 3 ) );
+  this->m_FixedParameters.Fill( 0.0 );
 
   // Setup and assign default interpolator
   typedef VectorLinearInterpolateImageFunction<
@@ -691,6 +695,10 @@ void DisplacementFieldTransform<TScalar, NDimensions>
   if( this->m_DisplacementField != field )
     {
     this->m_DisplacementField = field;
+    if( !this->m_InverseDisplacementField.IsNull() )
+      {
+      this->VerifyFixedParametersInformation();
+      }
     this->Modified();
     /* Store this separately for use in smoothing because we only want
      * to know when the displacement field object has changed, not just
@@ -703,10 +711,99 @@ void DisplacementFieldTransform<TScalar, NDimensions>
     // Assign to parameters object
     this->m_Parameters.SetParametersObject( this->m_DisplacementField );
     }
+  this->SetFixedParametersFromDisplacementField();
 }
 
 template <class TScalar, unsigned int NDimensions>
 void DisplacementFieldTransform<TScalar, NDimensions>
+::SetInverseDisplacementField( DisplacementFieldType* inverseField )
+{
+  itkDebugMacro( "setting InverseDisplacementField to " << inverseField );
+  if( this->m_InverseDisplacementField != inverseField )
+    {
+    this->m_InverseDisplacementField = inverseField;
+    if( !this->m_DisplacementField.IsNull() )
+      {
+      this->VerifyFixedParametersInformation();
+      }
+    this->Modified();
+    }
+}
+
+template <class TScalar, unsigned int NDimensions>
+void
+DisplacementFieldTransform<TScalar, NDimensions>
+::VerifyFixedParametersInformation()
+{
+  if( !this->m_DisplacementField.IsNull() && !this->m_InverseDisplacementField.IsNull() )
+    {
+    // check to see if the candidate inverse displacement field has the
+    // same fixed parameters as the displacement field.
+
+    SizeType inverseFieldSize = this->m_InverseDisplacementField->GetLargestPossibleRegion().GetSize();
+    PointType inverseFieldOrigin = this->m_InverseDisplacementField->GetOrigin();
+    SpacingType inverseFieldSpacing = this->m_InverseDisplacementField->GetSpacing();
+    DirectionType inverseFieldDirection = this->m_InverseDisplacementField->GetDirection();
+
+    SizeType fieldSize = this->m_DisplacementField->GetLargestPossibleRegion().GetSize();
+    PointType fieldOrigin = this->m_DisplacementField->GetOrigin();
+    SpacingType fieldSpacing = this->m_DisplacementField->GetSpacing();
+    DirectionType fieldDirection = this->m_DisplacementField->GetDirection();
+
+    // tolerance for origin and spacing depends on the size of pixel
+    // tolerance for directions a fraction of the unit cube.
+    const double coordinateTolerance = 1.0e-6 * fieldSpacing[0];
+    const double directionTolerance = 1.0e-6;
+
+    std::ostringstream sizeString;
+    std::ostringstream originString;
+    std::ostringstream spacingString;
+    std::ostringstream directionString;
+
+    bool unequalSizes = false;
+    bool unequalOrigins = false;
+    bool unequalSpacings = false;
+    bool unequalDirections = false;
+
+    if( inverseFieldSize != fieldSize )
+      {
+      unequalSizes = true;
+      sizeString << "InverseDisplacementField Size: " << inverseFieldSize
+                   << ", DisplacementField Size: " << fieldSize << std::endl;
+
+      }
+    if( !inverseFieldOrigin.GetVnlVector().is_equal( fieldOrigin.GetVnlVector(), coordinateTolerance ) )
+      {
+      unequalOrigins = true;
+      originString << "InverseDisplacementField Origin: " << inverseFieldOrigin
+                   << ", DisplacementField Origin: " << fieldOrigin << std::endl;
+
+      }
+    if( !inverseFieldSpacing.GetVnlVector().is_equal( fieldSpacing.GetVnlVector(), coordinateTolerance ) )
+      {
+      unequalSpacings = false;
+      originString << "InverseDisplacementField Spacing: " << inverseFieldSpacing
+                   << ", DisplacementField Spacing: " << fieldSpacing << std::endl;
+
+      }
+    if( !inverseFieldDirection.GetVnlMatrix().as_ref().is_equal( fieldDirection.GetVnlMatrix(), directionTolerance ) )
+      {
+      unequalDirections = true;
+      originString << "InverseDisplacementField Direction: " << inverseFieldDirection
+                   << ", DisplacementField Direction: " << fieldDirection << std::endl;
+
+      }
+    if( unequalSizes || unequalOrigins || unequalSpacings || unequalDirections )
+      {
+      itkExceptionMacro( "The inverse and displacement fields do not have the same fixed parameters: "
+                        << std::endl << sizeString.str() << originString.str() << spacingString.str() << directionString.str() );
+      }
+    }
+}
+
+template <class TScalar, unsigned int NDimensions>
+void
+DisplacementFieldTransform<TScalar, NDimensions>
 ::SetInterpolator( InterpolatorType* interpolator )
 {
   itkDebugMacro("setting Interpolator to " << interpolator);
@@ -723,7 +820,115 @@ void DisplacementFieldTransform<TScalar, NDimensions>
 
 template <class TScalar, unsigned int NDimensions>
 void
-DisplacementFieldTransform<TScalar, NDimensions>::PrintSelf( std::ostream& os, Indent indent ) const
+DisplacementFieldTransform<TScalar, NDimensions>
+::SetFixedParameters( const ParametersType & fixedParameters )
+{
+  if( fixedParameters.Size() != NDimensions * ( NDimensions + 3 ) )
+    {
+    itkExceptionMacro( "The fixed parameters are not the right size." );
+    }
+
+  SizeType size;
+  for( unsigned int d = 0; d < NDimensions; d++ )
+    {
+    size[d] = fixedParameters[d];
+    }
+
+  PointType origin;
+  for( unsigned int d = 0; d < NDimensions; d++ )
+    {
+    origin[d] = fixedParameters[d + NDimensions];
+    }
+
+  SpacingType spacing;
+  for( unsigned int d = 0; d < NDimensions; d++ )
+    {
+    spacing[d] = fixedParameters[d + 2 * NDimensions];
+    }
+
+  DirectionType direction;
+  for( unsigned int di = 0; di < NDimensions; di++ )
+    {
+    for( unsigned int dj = 0; dj < NDimensions; dj++ )
+      {
+      direction[di][dj] = fixedParameters[3 * NDimensions + ( di * NDimensions + dj )];
+      }
+    }
+
+  PixelType zeroDisplacement;
+  zeroDisplacement.Fill( 0.0 );
+
+  typename DisplacementFieldType::Pointer displacementField = DisplacementFieldType::New();
+  displacementField->SetSpacing( spacing );
+  displacementField->SetOrigin( origin );
+  displacementField->SetDirection( direction );
+  displacementField->SetRegions( size );
+  displacementField->Allocate();
+  displacementField->FillBuffer( zeroDisplacement );
+
+  this->SetDisplacementField( displacementField );
+
+  if( !this->m_InverseDisplacementField.IsNull() )
+    {
+    typename DisplacementFieldType::Pointer inverseDisplacementField = DisplacementFieldType::New();
+    inverseDisplacementField->SetSpacing( spacing );
+    inverseDisplacementField->SetOrigin( origin );
+    inverseDisplacementField->SetDirection( direction );
+    inverseDisplacementField->SetRegions( size );
+    inverseDisplacementField->Allocate();
+    inverseDisplacementField->FillBuffer( zeroDisplacement );
+
+    this->SetInverseDisplacementField( inverseDisplacementField );
+    }
+}
+
+template <class TScalar, unsigned int NDimensions>
+void
+DisplacementFieldTransform<TScalar, NDimensions>
+::SetFixedParametersFromDisplacementField() const
+  {
+  this->m_FixedParameters.SetSize( NDimensions * ( NDimensions + 3 ) );
+
+  const typename DisplacementFieldType::RegionType & fieldRegion =
+    this->m_DisplacementField->GetLargestPossibleRegion();
+
+  // Set the field size parameters
+  SizeType fieldSize = fieldRegion.GetSize();
+  for( unsigned int i = 0; i < NDimensions; i++ )
+    {
+    this->m_FixedParameters[i] = static_cast<ParametersValueType>( fieldSize[i] );
+    }
+
+  // Set the origin parameters
+  PointType fieldOrigin = this->m_DisplacementField->GetOrigin();
+  for( unsigned int i = 0; i < NDimensions; i++ )
+    {
+    this->m_FixedParameters[NDimensions + i] = fieldOrigin[i];
+    }
+
+  // Set the spacing parameters
+  SpacingType fieldSpacing = this->m_DisplacementField->GetSpacing();
+  for( unsigned int i = 0; i < NDimensions; i++ )
+    {
+    this->m_FixedParameters[2 * NDimensions + i] = static_cast<ParametersValueType>( fieldSpacing[i] );
+    }
+
+  // Set the direction parameters
+  DirectionType fieldDirection = this->m_DisplacementField->GetDirection();
+  for( unsigned int di = 0; di < NDimensions; di++ )
+    {
+    for( unsigned int dj = 0; dj < NDimensions; dj++ )
+      {
+      this->m_FixedParameters[3 * NDimensions + ( di * NDimensions + dj )] =
+        static_cast<ParametersValueType>( fieldDirection[di][dj] );
+      }
+    }
+}
+
+template <class TScalar, unsigned int NDimensions>
+void
+DisplacementFieldTransform<TScalar, NDimensions>
+::PrintSelf( std::ostream& os, Indent indent ) const
 {
   Superclass::PrintSelf( os, indent );
 

Modules/Core/Transform/include/itkBSplineTransform.h
@@ -37,15 +37,19 @@ namespace itk
  * The deformation \f$ D(\vec{x}) \f$ at any point \f$ \vec{x} \f$
  * is obtained by using a B-spline interpolation kernel.
  *
- * The deformation field grid is defined by a user specified GridRegion,
- * GridSpacing and GridOrigin. Each grid/control point has associated with it
+ * The deformation field grid is defined by a user specified transform
+ * domain (origin, physical dimensions, direction) and B-spline mesh size
+ * where the mesh size is the number of polynomial patches comprising the
+ * finite domain of support.  The relationship between the mesh size (
+ * number of polynomical pieces) and the number of control points in any
+ * given dimension is
+ *
+ * mesh size = number of control points - spline order
+ *
+ * Each grid/control point has associated with it
  * N deformation coefficients \f$ \vec{\delta}_j \f$, representing the N
  * directional components of the deformation. Deformation outside the grid
  * plus support region for the BSpline interpolation is assumed to be zero.
- *
- * Additionally, the user can specified an addition bulk transform \f$ B \f$
- * such that the transformed point is given by:
- * \f[ \vec{y} = B(\vec{x}) + D(\vec{x}) \f]
  *
  * The parameters for this transform is N x N-D grid of spline coefficients.
  * The user specifies the parameters as one flat array: each N-D grid
@@ -62,14 +66,14 @@ namespace itk
  * typedef BSplineTransform<double,2,3> TransformType;
  * TransformType::Pointer transform = TransformType::New();
  *
- * transform->SetGridRegion( region );
- * transform->SetGridSpacing( spacing );
- * transform->SetGridOrigin( origin );
+ * transform->SetTransformDomainOrigin( origin );
+ * transform->SetTransformDomainPhysicalDimensions( physicalDimensions );
+ * transform->SetTransformDomainDirection( direction );
+ * transform->SetTransformDomainMeshSize( meshSize );
  *
  * // NB: the region must be set first before setting the parameters
  *
- * TransformType::ParametersType parameters(
- *                                       transform->GetNumberOfParameters() );
+ * TransformType::ParametersType parameters( transform->GetNumberOfParameters() );
  *
  * // Fill the parameters with values
  *
@@ -80,7 +84,7 @@ namespace itk
  * \endverbatim
  *
  * An alternative way to set the B-spline coefficients is via array of
- * images. The grid region, spacing and origin information is taken
+ * images. The fixed parameters of the transform are taken
  * directly from the first image. It is assumed that the subsequent images
  * are the same buffered region. The following illustrates the API:
  * \verbatim
@@ -94,7 +98,7 @@ namespace itk
  *
  * \endverbatim
  *
- * Warning: use either the SetParameters() or SetCoefficientImage()
+ * Warning: use either the SetParameters() or SetCoefficientImages()
  * API. Mixing the two modes may results in unexpected results.
  *
  * The class is templated coordinate representation type (float or double),
@@ -141,35 +145,29 @@ public:
   typedef typename Superclass::NumberOfParametersType NumberOfParametersType;
 
   /** Standard vector type for this class. */
-  typedef Vector<TScalarType, itkGetStaticConstMacro( SpaceDimension )> InputVectorType;
-  typedef Vector<TScalarType, itkGetStaticConstMacro( SpaceDimension )> OutputVectorType;
+  typedef Vector<TScalarType, SpaceDimension> InputVectorType;
+  typedef Vector<TScalarType, SpaceDimension> OutputVectorType;
 
   /** Standard covariant vector type for this class. */
-  typedef CovariantVector<TScalarType,
-    itkGetStaticConstMacro( SpaceDimension )> InputCovariantVectorType;
-  typedef CovariantVector<TScalarType,
-    itkGetStaticConstMacro( SpaceDimension )> OutputCovariantVectorType;
+  typedef CovariantVector<TScalarType, SpaceDimension> InputCovariantVectorType;
+  typedef CovariantVector<TScalarType, SpaceDimension> OutputCovariantVectorType;
 
   /** Standard vnl_vector type for this class. */
-  typedef vnl_vector_fixed<TScalarType,
-    itkGetStaticConstMacro( SpaceDimension )> InputVnlVectorType;
-  typedef vnl_vector_fixed<TScalarType,
-    itkGetStaticConstMacro( SpaceDimension )> OutputVnlVectorType;
+  typedef vnl_vector_fixed<TScalarType, SpaceDimension> InputVnlVectorType;
+  typedef vnl_vector_fixed<TScalarType, SpaceDimension> OutputVnlVectorType;
 
   /** Standard coordinate point type for this class. */
-  typedef Point
-    <TScalarType, itkGetStaticConstMacro( SpaceDimension )> InputPointType;
-  typedef Point
-    <TScalarType, itkGetStaticConstMacro( SpaceDimension )> OutputPointType;
+  typedef Point<TScalarType, SpaceDimension> InputPointType;
+  typedef Point<TScalarType, SpaceDimension> OutputPointType;
 
   /** This method sets the parameters of the transform.
    * For a BSpline deformation transform, the parameters are the BSpline
    * coefficients on a sparse grid.
    *
    * The parameters are N number of N-D grid of coefficients. Each N-D grid
-   * is represented as a flat array of doubles
-   * (in the same configuration as an itk::Image).
-   * The N arrays are then concatenated to form one parameter array.
+   * is represented as a flat array of scalars (in the same configuration as
+   * an itk::Image). The N arrays are then concatenated to form one parameter
+   * array.
    *
    * For efficiency, this transform does not make a copy of the parameters.
    * It only keeps a pointer to the input parameters. It assumes that the memory
@@ -177,23 +175,29 @@ public:
    * to call copy the parameters.
    *
    * This method wraps each grid as itk::Image's using the user specified
-   * grid region, spacing and origin.
-   * NOTE: The grid region, spacing and origin must be set first.
+   * fixed parameters.
+   * NOTE: The transform domain must be set first.
    *
    */
   void SetParameters( const ParametersType & parameters );
 
   /** This method sets the fixed parameters of the transform.
-   * For a BSpline deformation transform, the parameters are the following:
-   *    Grid Size, Grid Origin, and Grid Spacing
+   * For a BSpline deformation transform, the fixed parameters are the
+   * following: grid size, grid origin, grid spacing, and grid direction.
+   * However, all of these are set via the much more intuitive
+   * SetTransformDomainXXX() functions
    *
    * The fixed parameters are the three times the size of the templated
-   * dimensions.
-   * This function has the effect of make the following calls:
-   *       transform->SetGridSpacing( spacing );
-   *       transform->SetGridOrigin( origin );
-   *       transform->SetGridDirection( direction );
-   *       transform->SetGridRegion( bsplineRegion );
+   * dimensions.  This function has the effect of make the following non-
+   * existing functional calls:
+   *   transform->SetGridSpacing( spacing );
+   *   transform->SetGridOrigin( origin );
+   *   transform->SetGridDirection( direction );
+   *   transform->SetGridRegion( bsplineRegion );
+   *
+   * With recent updates to this transform, however, all these parameters
+   * are set indirectly by setting the transform domain parameters unless
+   * the user sets them with SetFixedParameters().
    *
    * This function was added to allow the transform to work with the
    * itkTransformReader/Writer I/O filters.
@@ -214,9 +218,8 @@ public:
    * efficiency the SetParameters method does not.
    *
    * This method wraps each grid as itk::Image's using the user specified
-   * grid region, spacing and origin.
-   * NOTE: The grid region, spacing and origin must be set first.
-   *
+   * fixed parameters.
+   * NOTE: The fixed parameters must be set first.
    */
   void SetParametersByValue( const ParametersType & parameters );
 
@@ -237,17 +240,16 @@ public:
   virtual const ParametersType & GetFixedParameters() const;
 
   /** Parameters as SpaceDimension number of images. */
-  typedef typename ParametersType::ValueType                                   ParametersValueType;
-  typedef Image<ParametersValueType, itkGetStaticConstMacro( SpaceDimension )> ImageType;
-  typedef typename ImageType::Pointer                                          ImagePointer;
-  typedef FixedArray<ImagePointer, NDimensions>                                CoefficientImageArray;
-
+  typedef typename ParametersType::ValueType           ParametersValueType;
+  typedef Image<ParametersValueType, SpaceDimension>   ImageType;
+  typedef typename ImageType::Pointer                  ImagePointer;
+  typedef FixedArray<ImagePointer, NDimensions>        CoefficientImageArray;
 
   /** Set the array of coefficient images.
    *
    * This is an alternative API for setting the BSpline coefficients
-   * as an array of SpaceDimension images. The grid region spacing
-   * and origin is taken from the first image. It is assume that
+   * as an array of SpaceDimension images. The fixed parameters are
+   * taken from the first image. It is assumed that
    * the buffered region of all the subsequent images are the same
    * as the first image. Note that no error checking is done.
    *
@@ -263,8 +265,7 @@ public:
   }
 
   /** Typedefs for specifying the extent of the grid. */
-  typedef ImageRegion<itkGetStaticConstMacro( SpaceDimension )> RegionType;
-
+  typedef ImageRegion<SpaceDimension>       RegionType;
   typedef typename RegionType::IndexType    IndexType;
   typedef typename RegionType::SizeType     SizeType;
   typedef typename ImageType::SpacingType   SpacingType;
@@ -274,9 +275,7 @@ public:
   OutputPointType  TransformPoint( const InputPointType & point ) const;
 
   /** Interpolation weights function type. */
-  typedef BSplineInterpolationWeightFunction<ScalarType,
-    itkGetStaticConstMacro( SpaceDimension ),
-     itkGetStaticConstMacro( SplineOrder )> WeightsFunctionType;
+  typedef BSplineInterpolationWeightFunction<ScalarType, SpaceDimension, SplineOrder> WeightsFunctionType;
 
   typedef typename WeightsFunctionType::WeightsType         WeightsType;
   typedef typename WeightsFunctionType::ContinuousIndexType ContinuousIndexType;
@@ -293,7 +292,7 @@ public:
    * ( i * this->GetNumberOfParametersPerDimension() ) to the indices array.
    */
   virtual void TransformPoint( const InputPointType & inputPoint, OutputPointType & outputPoint,
-    WeightsType & weights, ParameterIndexArrayType & indices, bool & inside) const;
+    WeightsType & weights, ParameterIndexArrayType & indices, bool & inside ) const;
 
   /** Get number of weights. */
   unsigned long GetNumberOfWeights() const

commit 05f198997b5cb5817737616a312493b453a21bb3

BUG: Fix strings in DisplacementField VerifyFixedParametersInformation
When spacing or direction differences were encountered, the original code would
produce an incorrect exception message. The exception might occur when calling
`DisplacementFieldTransform::SetInverseDisplacementField`.

The bug appears introduced with commit 70fd79f43c089bec068ba6f6657baca12996842d,
"ENH:  Adding transform parameters adaptors.", 20 Sep 2011.

Modules/Filtering/DisplacementField/include/itkDisplacementFieldTransform.hxx
@@ -383,10 +383,10 @@ DisplacementFieldTransform<TParametersValueType, VDimension>::VerifyFixedParamet
     const double coordinateTolerance = m_CoordinateTolerance * fieldSpacing[0];
     const double directionTolerance = m_DirectionTolerance;
 
-    std::ostringstream       sizeString;
-    std::ostringstream       originString;
-    const std::ostringstream spacingString;
-    const std::ostringstream directionString;
+    std::ostringstream sizeString;
+    std::ostringstream originString;
+    std::ostringstream spacingString;
+    std::ostringstream directionString;
 
     bool unequalSizes = false;
     bool unequalOrigins = false;
@@ -408,14 +408,14 @@ DisplacementFieldTransform<TParametersValueType, VDimension>::VerifyFixedParamet
     if (!inverseFieldSpacing.GetVnlVector().is_equal(fieldSpacing.GetVnlVector(), coordinateTolerance))
     {
       unequalSpacings = false;
-      originString << "InverseDisplacementField Spacing: " << inverseFieldSpacing
-                   << ", DisplacementField Spacing: " << fieldSpacing << std::endl;
+      spacingString << "InverseDisplacementField Spacing: " << inverseFieldSpacing
+                    << ", DisplacementField Spacing: " << fieldSpacing << std::endl;
     }
     if (!inverseFieldDirection.GetVnlMatrix().is_equal(fieldDirection.GetVnlMatrix(), directionTolerance))
     {
       unequalDirections = true;
-      originString << "InverseDisplacementField Direction: " << inverseFieldDirection
-                   << ", DisplacementField Direction: " << fieldDirection << std::endl;
+      directionString << "InverseDisplacementField Direction: " << inverseFieldDirection
+                      << ", DisplacementField Direction: " << fieldDirection << std::endl;
     }
     if (unequalSizes || unequalOrigins || unequalSpacings || unequalDirections)
     {