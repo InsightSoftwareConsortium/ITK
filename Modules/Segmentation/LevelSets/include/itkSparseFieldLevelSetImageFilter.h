InsightSoftwareConsortium/ITK

commit 1a718ceb4c69e32dcacc70aa15525c432c695fb6

DOC: Convert from raw in-code citations to bibtex (2/N)
Second PR out of N, converting raw inline citations with various styles to
using a bibliography database and the doxygen cite command.

Bibtex file was formatted using bibtex-tidy
(https://github.com/FlamingTempura/bibtex-tidy). This tool can be used
as a pre-commit hook but currently does not have an option for just
performing compliance checking so not added to .pre-commit-config.yaml
file.

Modules/Segmentation/LevelSets/include/itkSparseFieldLevelSetImageFilter.h
@@ -231,12 +231,7 @@ private:
  *  FiniteDifferenceFunction to use for calculations.  This is set using the
  *  method SetDifferenceFunction in the parent class.
  *
- * \par REFERENCES
- * Whitaker, Ross. A Level-Set Approach to 3D Reconstruction from Range Data.
- * International Journal of Computer Vision.  V. 29 No. 3, 203-231. 1998.
- *
- * \par
- * Sethian, J.A. Level Set Methods. Cambridge University Press. 1996.
+ * For algorithmic details see \cite whitaker1998 and \par sethian1996.
  *
  * \ingroup ITKLevelSets
  */

Documentation/Doxygen/doxygen.bib
@@ -70,6 +70,17 @@
   year         = 1998,
   publisher    = {CRC Press}
 }
+@article{besag1986,
+  title        = {On the Statistical Analysis of Dirty Pictures},
+  author       = {Julian Besag},
+  year         = 1986,
+  journal      = {Journal of the Royal Statistical Society: Series B (Methodological)},
+  volume       = 48,
+  number       = 3,
+  pages        = {259--279},
+  doi          = {10.1111/j.2517-6161.1986.tb01412.x},
+  url          = {https://doi.org/10.1111/j.2517-6161.1986.tb01412.x}
+}
 @article{besl1992,
   title        = {A method for registration of {3-D} shapes},
   author       = {Besl, P.J. and McKay, Neil D.},
@@ -157,6 +168,17 @@
   doi          = {10.1109/TPAMI.1986.4767851},
   url          = {https://doi.org/10.1109/TPAMI.1986.4767851}
 }
+@article{caselles1997,
+  title        = {Geodesic Active Contours},
+  author       = {Caselles, Vicent and Kimmel, Ron and Sapiro, Guillermo},
+  year         = 1997,
+  journal      = {International Journal of Computer Vision},
+  volume       = 22,
+  number       = 1,
+  pages        = {61--79},
+  doi          = {10.1023/A:1007979827043},
+  url          = {https://doi.org/10.1023/A:1007979827043}
+}
 @book{castleman1995,
   title        = {Digital Signal Processing},
   author       = {Kenneth R. Castleman},
@@ -194,6 +216,17 @@
   doi          = {10.1109/42.585766},
   url          = {https://doi.org/10.1109/42.585766}
 }
+@article{delingette1999,
+  title        = {General Object Reconstruction Based on Simplex Meshes},
+  author       = {Delingette, Herv{\'e}},
+  year         = 1999,
+  journal      = {International Journal of Computer Vision},
+  volume       = 32,
+  number       = 2,
+  pages        = {111--146},
+  doi          = {10.1023/A:1008157432188},
+  url          = {https://doi.org/10.1023/A:1008157432188}
+}
 @article{deriche1990,
   title        = {Fast algorithms for low-level vision},
   author       = {Rachid Deriche},
@@ -226,6 +259,17 @@
   doi          = {10.1016/j.laa.2003.10.021},
   url          = {https://doi.org/10.1016/j.laa.2003.10.021}
 }
+@article{fortune1987,
+  title        = {A sweepline algorithm for {Voronoi} diagrams},
+  author       = {Fortune, Steven},
+  year         = 1987,
+  journal      = {Algorithmica},
+  volume       = 2,
+  number       = 1,
+  pages        = {153--174},
+  doi          = {10.1007/BF01840357},
+  url          = {https://doi.org/10.1007/BF01840357}
+}
 @inproceedings{frangi1998,
   title        = {Multiscale vessel enhancement filtering},
   author       = {Frangi, Alejandro F. and Niessen, Wiro J. and Vincken, Koen L. and Viergever, Max A.},
@@ -240,6 +284,14 @@
   author       = {Erich Gamma and Richard Helm and Ralph Johnson and John Vlissides},
   year         = 1994,
   publisher    = {Addison-Wesley}
+}
+@book{gersho1992,
+  title        = {Vector Quantization and Signal Compression},
+  author       = {Allen Gersho and Robert M. Gray},
+  year         = 1992,
+  publisher    = {Springer New York, NY},
+  doi          = {10.1007/978-1-4615-3626-0},
+  url          = {https://doi.org/10.1007/978-1-4615-3626-0}
 }
 @article{glasbey1993,
   title        = {An Analysis of Histogram-Based Thresholding Algorithms},
@@ -343,6 +395,17 @@
   doi          = {10.1016/0031-3203(94)E0043-K},
   url          = {https://doi.org/10.1016/0031-3203(94)E0043-K}
 }
+@article{imelinska2000,
+  title        = {Semi-automated color segmentation of anatomical tissue},
+  author       = {C. Imeli{\'n}ska and M.S. Downes and W. Yuan},
+  year         = 2000,
+  journal      = {Computerized Medical Imaging and Graphics},
+  volume       = 24,
+  number       = 3,
+  pages        = {173--180},
+  doi          = {10.1016/S0895-6111(00)00017-3},
+  url          = {https://doi.org/10.1016/S0895-6111(00)00017-3}
+}
 @article{jin2005,
   title        = {A comparison of algorithms for vertex normal computation},
   author       = {Jin, Shuangshuang and Lewis, Robert R. and West, David},
@@ -406,6 +469,17 @@
   doi          = {10.1109/42.640737},
   url          = {https://doi.org/10.1109/42.640737}
 }
+@article{koepfler1994,
+  title        = {A Multiscale Algorithm for Image Segmentation by Variational Method},
+  author       = {Koepfler, G. and Lopez, C. and Morel, J. M.},
+  year         = 1994,
+  journal      = {SIAM Journal on Numerical Analysis},
+  volume       = 31,
+  number       = 1,
+  pages        = {282--299},
+  doi          = {10.1137/0731015},
+  url          = {https://doi.org/10.1137/0731015}
+}
 @article{lee1997,
   title        = {Scattered data interpolation with multilevel B-splines},
   author       = {Lee, S. and Wolberg, G. and Shin, S.Y.},
@@ -416,6 +490,16 @@
   pages        = {228--244},
   doi          = {10.1109/2945.620490},
   url          = {https://doi.org/10.1109/2945.620490}
+}
+@inproceedings{leventon2000,
+  title        = {Statistical shape influence in geodesic active contours},
+  author       = {Leventon, M.E. and Grimson, W.E.L. and Faugeras, O.},
+  year         = 2000,
+  booktitle    = {IEEE Conference on Computer Vision and Pattern Recognition (CVPR)},
+  volume       = 1,
+  pages        = {316--323 vol.1},
+  doi          = {10.1109/CVPR.2000.855835},
+  url          = {https://doi.org/10.1109/CVPR.2000.855835}
 }
 @article{li1993,
   title        = {Minimum cross entropy thresholding},
@@ -455,6 +539,29 @@
   pages        = {163--169},
   doi          = {10.1145/37402.37422},
   url          = {https://doi.org/10.1145/37402.37422}
+}
+@article{lorigo2001,
+  title        = {{CURVES:} Curve evolution for vessel segmentation},
+  author       = {L.M. Lorigo and O.D. Faugeras and W.E.L. Grimson and R. Keriven and R. Kikinis and A. Nabavi and C.-F. Westin},
+  year         = 2001,
+  journal      = {Medical Image Analysis},
+  volume       = 5,
+  number       = 3,
+  pages        = {195--206},
+  doi          = {10.1016/S1361-8415(01)00040-8},
+  issn         = {1361-8415},
+  url          = {https://doi.org/10.1016/S1361-8415(01)00040-8}
+}
+@article{malladi1995,
+  title        = {Shape modeling with front propagation: a level set approach},
+  author       = {Malladi, Ravikanth and Sethian, James A. and Vemuri, Baba C.},
+  year         = 1995,
+  journal      = {IEEE Transactions on Pattern Analysis and Machine Intelligence},
+  volume       = 17,
+  number       = 2,
+  pages        = {158--175},
+  doi          = {10.1109/34.368173},
+  url          = {https://doi.org/10.1109/34.368173}
 }
 @article{martin1968,
   title        = {Householder's tridiagonalization of a symmetric matrix},
@@ -612,6 +719,17 @@
   doi          = {10.1007/978-3-642-59223-2},
   url          = {https://doi.org/10.1007/978-3-642-59223-2}
 }
+@article{pikaz1996,
+  title        = {Digital image thresholding, based on topological stable-state},
+  author       = {Arie Pikaz and Amir Averbuch},
+  year         = 1996,
+  journal      = {Pattern Recognition},
+  volume       = 29,
+  number       = 5,
+  pages        = {829--843},
+  doi          = {https://doi.org/10.1016/0031-3203(95)00126-3},
+  doi          = {10.1016/0031-3203(95)00126-3}
+}
 @article{pluta2009,
   title        = {Appearance and incomplete label matching for diffeomorphic template based hippocampus segmentation},
   author       = {John Pluta and Brian B. Avants and Simon Glynn and Suyash Awate and James C. Gee and John A. Detre},
@@ -656,6 +774,28 @@
   doi          = {10.1016/j.patrec.2004.07.002},
   url          = {https://doi.org/10.1016/j.patrec.2004.07.002}
 }
+@article{rohlfing2004,
+  title        = {Performance-based classifier combination in atlas-based image segmentation using expectation-maximization parameter estimation},
+  author       = {Rohlfing, T. and Russakoff, D.B. and Maurer, C.R.},
+  year         = 2004,
+  journal      = {IEEE Transactions on Medical Imaging},
+  volume       = 23,
+  number       = 8,
+  pages        = {983--994},
+  doi          = {10.1109/TMI.2004.830803},
+  url          = {https://doi.org/10.1109/TMI.2004.830803}
+}
+@article{rohlfing2005,
+  title        = {Multi-classifier framework for atlas-based image segmentation},
+  author       = {Torsten Rohlfing and Calvin R. Maurer},
+  year         = 2005,
+  journal      = {Pattern Recognition Letters},
+  volume       = 26,
+  number       = 13,
+  pages        = {2070--2079},
+  doi          = {10.1016/j.patrec.2005.03.017},
+  url          = {https://doi.org/10.1016/j.patrec.2005.03.017}
+}
 @article{sapiro1996,
   title        = {Anisotropic diffusion of multivalued images with applications to color filtering},
   author       = {Sapiro, G. and Ringach, D.L.},
@@ -666,6 +806,12 @@
   pages        = {1582--1586},
   doi          = {10.1109/83.541429},
   url          = {https://doi.org/10.1109/83.541429}
+}
+@book{sethian1996,
+  title        = {Level Set Methods Evolving Interfaces in Geometry, Fluid Mechanics, Computer Vision, and Materials Science},
+  author       = {James A. Sethian},
+  year         = 1996,
+  publisher    = {Cambridge University Press}
 }
 @inbook{sethian1999,
   title        = {Image Enhancement and Noise Removal},
@@ -770,6 +916,17 @@
   doi          = {10.1007/978-3-662-05088-0_4},
   url          = {https://doi.org/10.1007/978-3-662-05088-0_4}
 }
+@inbook{soille2004c,
+  title        = {Segmentation},
+  author       = {Soille, Pierre},
+  year         = 2004,
+  booktitle    = {Morphological Image Analysis: Principles and Applications},
+  publisher    = {Springer Berlin Heidelberg},
+  address      = {Berlin, Heidelberg},
+  pages        = {267--292},
+  doi          = {10.1007/978-3-662-05088-0_9},
+  url          = {https://doi.org/10.1007/978-3-662-05088-0_9}
+}
 @inproceedings{sprengel1996,
   title        = {Thin-plate spline approximation for image registration},
   author       = {Sprengel, R. and Rohr, K. and Stiehl, H.S.},
@@ -798,7 +955,7 @@
   publisher    = {Addison Wesley}
 }
 @techreport{styner1997,
-  title        = {Evaluation of 2D/3D bias correction with {1+1ES-optimization}},
+  title        = {Evaluation of {2D/3D} bias correction with {1+1ES-optimization}},
   author       = {Martin Styner and Guido Gerig},
   year         = 1997,
   number       = {TR-197},
@@ -970,6 +1127,14 @@
   doi          = {10.1109/79.799930},
   url          = {https://doi.org/10.1109/79.799930}
 }
+@article{urish2005,
+  title        = {Unsupervised Segmentation for Myofiber Counting in Immunofluorescent Microscopy Images},
+  author       = {Urish, Kenneth and August, Jonas and Huard, Johnny},
+  year         = 2005,
+  doi          = {10.54294/h1vbsl},
+  url          = {https://doi.org/10.54294/h1vbsl},
+  jounral      = {The Insight Journal}
+}
 @article{vemuri2003,
   title        = {Image registration via level-set motion: Applications to atlas-based segmentation},
   author       = {B.C. Vemuri and J. Ye and Y. Chen and C.M. Leonard},
@@ -1050,6 +1215,17 @@
   year         = 2002,
   booktitle    = {International Society for Magnetic Resonance in Medicine (ISMRM)},
   url          = {https://cds.ismrm.org/ismrm-2002/PDF4/1166.PDF}
+}
+@article{whitaker1998,
+  title        = {A Level-Set Approach to {3D} Reconstruction from Range Data},
+  author       = {Whitaker, Ross T.},
+  year         = 1998,
+  journal      = {International Journal of Computer Vision},
+  volume       = 29,
+  number       = 3,
+  pages        = {203--231},
+  doi          = {10.1023/A:1008036829907},
+  url          = {https://doi.org/10.1023/A:1008036829907}
 }
 @inproceedings{whitaker2000,
   title        = {Reducing Aliasing Artifacts in Iso-Surfaces of Binary Volumes},

Modules/Filtering/BiasCorrection/include/itkMRIBiasFieldCorrectionFilter.h
@@ -219,15 +219,7 @@ private:
  * The multiresolution pyramid implementation is based on
  * itkMultiResolutionPyramidImageFilter (without Gaussian smoothing)
  *
- * For more details. refer to the following articles.
- * "Parametric estimate of intensity inhomogeneities applied to MRI"
- * Martin Styner, Guido Gerig, Christian Brechbuehler, Gabor Szekely,
- * IEEE TRANSACTIONS ON MEDICAL IMAGING; 19(3), pp. 153-165, 2000,
- * (https://www.cs.unc.edu/~styner/docs/tmi00.pdf)
- *
- * "Evaluation of 2D/3D bias correction with 1+1ES-optimization"
- * Martin Styner, Prof. Dr. G. Gerig (IKT, BIWI, ETH Zuerich), TR-197
- * (https://www.cs.unc.edu/~styner/docs/StynerTR97.pdf)
+ * For more details refer to \cite styner2000 and \cite styner1997.
  * \ingroup ITKBiasCorrection
  */
 template <typename TInputImage, typename TOutputImage, typename TMaskImage>

Modules/Filtering/BiasCorrection/include/itkN4BiasFieldCorrectionImageFilter.h
@@ -35,7 +35,7 @@ namespace itk
  * \brief Implementation of the N4  bias field correction algorithm.
  *
  * The nonparametric nonuniform intensity normalization (N3) algorithm, as
- * introduced by Sled et al. in 1998 is a method for correcting nonuniformity
+ * introduced by Sled et al. in 1998 \cite sled1998 is a method for correcting nonuniformity
  * associated with MR images. The algorithm assumes a simple parametric model
  * (Gaussian) for the bias field and does not require tissue class segmentation.
  * In addition, there are only a couple of parameters to tune with the default

Modules/Filtering/Convolution/include/itkFFTNormalizedCorrelationImageFilter.h
@@ -84,11 +84,7 @@ namespace itk
  * type. You will get a compilation error if the pixel type of the
  * output image is not float or double.
  *
- * References:
- * 1) D. Padfield. "Masked object registration in the Fourier domain."
- * Transactions on Image Processing.
- * 2) D. Padfield. "Masked FFT registration". In Proc. Computer
- * Vision and Pattern Recognition, 2010.
+ * For algorithmic details see \cite padfield2012 and \cite padfield2010.
  *
  * \author: Dirk Padfield, GE Global Research, padfield\@research.ge.com
  * \ingroup ITKConvolution

Modules/Segmentation/Classifiers/include/itkImageKmeansModelEstimator.h
@@ -95,9 +95,7 @@ namespace itk
  * is a table containing all cluster centers.  The GLA produces results
  * that are equivalent to the K-means clustering algorithm.
  *
- * For more information about the algorithms, see A. Gersho and R. M. Gray,
- * {\em Vector Quantization and Signal Compression},
- * Kluwer Academic Publishers, Boston, MA, 1992.
+ * For more information about the algorithms, see \cite gersho1992.
  *
  * This object supports data handling of multiband images. The object
  * accepts the input image in vector format only, where each pixel is a

Modules/Segmentation/ConnectedComponents/include/itkThresholdMaximumConnectedComponentsImageFilter.h
@@ -56,13 +56,7 @@ namespace itk
  * Stable State Thresholding works well on images with a large number
  * of objects to be counted.
  *
- * \par References:
- * 1) Urish KL, August J, Huard J. "Unsupervised segmentation for myofiber
- * counting in immunofluorescent microscopy images". Insight Journal.
- * ISC/NA-MIC/MICCAI Workshop on Open-Source Software (2005)
- * https://doi.org/10.54294/h1vbsl
- * 2) Pikaz A, Averbuch, A. "Digital image thresholding based on topological
- * stable-state". Pattern Recognition, 29(5): 829-843, 1996.
+ * For algorithmic details see \cite urish2005 and \cite pikaz1996.
  *
  * \par
  * Questions: email Ken Urish at ken.urish(at)gmail.com

Modules/Segmentation/DeformableMesh/include/itkDeformableSimplexMesh3DFilter.h
@@ -61,7 +61,7 @@ namespace itk
  * produce more regular mesh. Higher values ( 0.3 < gamma < 0.2) will allow to move the vertices to
  * regions of higher curvature.
  *
- * This approach for segmentation follows that of Delingette et al. (1997).
+ * This approach for segmentation follows that of \cite delingette1999.
  *
  * This filter currently assumes that the spacing of the input image is 1.
  *

commit 30cc6f1960d39d38502c48a5b9eff7119d322a18

DOC: Use Doxygen citation command instead of misplaced `\par`
Use Doxygen citation command instead of a misplaced `\par` paragraph
command.

Introduced inadvertently in commit 1a718ce.

Modules/Segmentation/LevelSets/include/itkSparseFieldLevelSetImageFilter.h
@@ -231,7 +231,7 @@ private:
  *  FiniteDifferenceFunction to use for calculations.  This is set using the
  *  method SetDifferenceFunction in the parent class.
  *
- * For algorithmic details see \cite whitaker1998 and \par sethian1996.
+ * For algorithmic details see \cite whitaker1998 and \cite sethian1996.
  *
  * \ingroup ITKLevelSets
  */