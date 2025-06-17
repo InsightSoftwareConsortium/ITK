bc11be5a5bf67065f8129e67e15e6188075777dc
Hans Johnson <hans-johnson@uiowa.edu>
2025-06-17 17:00:01 -0500

    COMP: Refactor histogram mapping logic with lambda for clarity
    Replaced repetitive linear interpolation logic with a lambda function to
    improve readability, maintainability, and organization.
    
    Reverts incorrect refactorings from caa61aa3cdfb1382a7d13d6418a96de87c44b3da
    that introduced valgrind failure of un-initialized reads.

--- Modules/Filtering/ImageIntensity/include/itkHistogramMatchingImageFilter.hxx
+++ Modules/Filtering/ImageIntensity/include/itkHistogramMatchingImageFilter.hxx
@@ -337,18 +337,21 @@ HistogramMatchingImageFilter<TInputImage, TOutputImage, THistogramMeasurement>::
         break;
       }
     }
-    // Linear interpolate from point[j] and point[j+1].
-    double mappedValue = m_QuantileTable[1][j - 1] + (srcValue - m_QuantileTable[0][j - 1]) * m_Gradients[j - 1];
-    if (j == 0)
-    {
-      // Linear interpolate from min to point[0]
-      mappedValue = m_ReferenceMinValue + (srcValue - m_SourceMinValue) * m_LowerGradient;
-    }
-    else if (j == m_NumberOfMatchPoints + 2)
-    {
-      // Linear interpolate from point[m_NumberOfMatchPoints+1] to max
-      mappedValue = m_ReferenceMaxValue + (srcValue - m_SourceMaxValue) * m_UpperGradient;
-    }
+
+    const double mappedValue = [this, j, srcValue]() -> double {
+      if (j == 0)
+      {
+        // Linearly interpolate from min to point[0]
+        return m_ReferenceMinValue + (srcValue - m_SourceMinValue) * m_LowerGradient;
+      }
+      if (j == m_NumberOfMatchPoints + 2)
+      {
+        // Linearly interpolate from point[m_NumberOfMatchPoints+1] to max
+        return m_ReferenceMaxValue + (srcValue - m_SourceMaxValue) * m_UpperGradient;
+      }
+      // Linearly interpolate from point[j] and point[j+1].
+      return m_QuantileTable[1][j - 1] + (srcValue - m_QuantileTable[0][j - 1]) * m_Gradients[j - 1];
+    }();
     outIter.Set(static_cast<OutputPixelType>(mappedValue));
   }
 }
