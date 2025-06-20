54b147e42f7601d54bfce7615e34f2a7a1fdcd85
Hans Johnson <hans-johnson@uiowa.edu>
2025-06-20 10:28:29 -0500

    PERF: performance-faster-string-find use character
    Optimize calls to std::string::find() and friends when the
    search item passed is a single character string literal.
    The character literal overload is more efficient.

--- Modules/IO/PhilipsREC/src/itkPhilipsPAR.cxx
+++ Modules/IO/PhilipsREC/src/itkPhilipsPAR.cxx
@@ -496,7 +496,7 @@ PhilipsPAR::GetGeneralInfoString(std::string file, int lineNum)
     return outString;
   }
   currentLine = this->GetLineNumber(file, lineNum);
-  index = currentLine.find(":");
+  index = currentLine.find(':');
   if (index != std::string::npos)
   {
     const std::string tempString = ":";
@@ -554,10 +554,10 @@ PhilipsPAR::ReadPAR(std::string parFile, struct par_parameter * pPar)
       strncpy(pPar->protocol_name, this->GetGeneralInfoString(parFile, 14).c_str(), sizeof(pPar->protocol_name));
       strncpy(pPar->exam_date,
               this->GetGeneralInfoString(parFile, 15).c_str(),
-              this->GetGeneralInfoString(parFile, 15).find("/"));
+              this->GetGeneralInfoString(parFile, 15).find('/'));
       strncpy(
         pPar->exam_time,
-        this->GetGeneralInfoString(parFile, 15).substr(this->GetGeneralInfoString(parFile, 15).find("/") + 1).c_str(),
+        this->GetGeneralInfoString(parFile, 15).substr(this->GetGeneralInfoString(parFile, 15).find('/') + 1).c_str(),
         sizeof(pPar->exam_time));
       inString.str(this->GetGeneralInfoString(parFile, 16));
       inString >> pPar->scno;
@@ -1046,10 +1046,10 @@ PhilipsPAR::ReadPAR(std::string parFile, struct par_parameter * pPar)
       strncpy(pPar->protocol_name, this->GetGeneralInfoString(parFile, 14).c_str(), sizeof(pPar->protocol_name));
       strncpy(pPar->exam_date,
               this->GetGeneralInfoString(parFile, 15).c_str(),
-              this->GetGeneralInfoString(parFile, 15).find("/"));
+              this->GetGeneralInfoString(parFile, 15).find('/'));
       strncpy(
         pPar->exam_time,
-        this->GetGeneralInfoString(parFile, 15).substr(this->GetGeneralInfoString(parFile, 15).find("/") + 1).c_str(),
+        this->GetGeneralInfoString(parFile, 15).substr(this->GetGeneralInfoString(parFile, 15).find('/') + 1).c_str(),
         sizeof(pPar->exam_time));
       strncpy(pPar->series_type, this->GetGeneralInfoString(parFile, 16).c_str(), sizeof(pPar->series_type));
       inString.str(this->GetGeneralInfoString(parFile, 17));
