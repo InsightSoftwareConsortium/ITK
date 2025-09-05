InsightSoftwareConsortium/ITK

commit bcb6876c7d9ed3315ed8cd2d3cdf9f0b11f3f951

COMP: Enable back ITKVtkGlue module in Python wrapping

Modules/Bridge/VtkGlue/itk-module-init.cmake
@@ -60,6 +60,8 @@ set(
   _required_vtk_libraries
   ${_target_prefix}IOImage
   ${_target_prefix}ImagingSources
+  ${_target_prefix}kwiml
+  ${_target_prefix}vtksys
 )
 if(ITK_WRAP_PYTHON)
   list(

Modules/Bridge/VtkGlue/wrapping/CMakeLists.txt
@@ -1,45 +1,36 @@
 # Forward include directories to CastXML
-if(NOT VTK_RENDERING_BACKEND STREQUAL "None")
-  foreach(_vtk_lib IN LISTS ITKVtkGlue_VTK_LIBRARIES)
-    get_target_property(
-      _vtk_lib_include_dirs
-      ${_vtk_lib}
-      INTERFACE_INCLUDE_DIRECTORIES
-    )
-    if(_vtk_lib_include_dirs)
-      include_directories(${_vtk_lib_include_dirs})
-    endif()
-  endforeach()
-endif()
+foreach(_vtk_lib IN LISTS ITKVtkGlue_VTK_LIBRARIES)
+  get_target_property(
+    _vtk_lib_include_dirs
+    ${_vtk_lib}
+    INTERFACE_INCLUDE_DIRECTORIES
+  )
+  if(_vtk_lib_include_dirs)
+    include_directories(${_vtk_lib_include_dirs})
+  endif()
+endforeach()
 
-# Python provides native conversion between ITK and VTK
-if(ITK_WRAP_PYTHON)
+itk_wrap_module(ITKVtkGlue)
+if("${VTK_VERSION}" VERSION_LESS 7.0.0)
   message(
-    STATUS
-    "ITK_WRAP_PYTHON is turned on - skipping ITKVtkGlue, since native version is available https://docs.itk.org/en/latest/learn/python_quick_start.html#itk-and-vtk."
+    WARNING
+    "The ITKVtkGlue module can only built with Python 3 and VTK >= 7."
   )
 else()
-  itk_wrap_module(ITKVtkGlue)
-  if("${VTK_VERSION}" VERSION_LESS 7.0.0)
-    message(
-      WARNING
-      "The ITKVtkGlue module can only built with Python 3 and VTK >= 7.\n"
-      "Only the c++ version of the module will be built."
-    )
-  else()
-    list(
-      APPEND
-      WRAPPER_SWIG_LIBRARY_FILES
-      "${CMAKE_CURRENT_SOURCE_DIR}/VtkGlue.i"
-    )
-    set(
-      WRAPPER_LIBRARY_DEPENDS
-      ITKCommon
-      ITKVTK
-    )
-
-    set(WRAPPER_LIBRARY_LINK_LIBRARIES ${ITK_LIBRARIES})
-
-    itk_auto_load_and_end_wrap_submodules()
-  endif()
+  list(
+    APPEND
+    WRAPPER_SWIG_LIBRARY_FILES
+    "${CMAKE_CURRENT_SOURCE_DIR}/VtkGlue.i"
+  )
+  set(
+    WRAPPER_LIBRARY_DEPENDS
+    ITKCommon
+    ITKVTK
+  )
+  set(
+    WRAPPER_LIBRARY_LINK_LIBRARIES
+    ${ITK_LIBRARIES}
+    ${ITKVtkGlue_VTK_LIBRARIES}
+  )
+  itk_auto_load_and_end_wrap_submodules()
 endif()

commit 958cfc5e447a3c2018f91a31e07ab6eea0a20c62

COMP: Fix error made by bcb6876: module vtkvtksys not available
Commit bcb6876c7d9ed3315ed8cd2d3cdf9f0b11f3f951 introduced a configure error:

CMake Error at C:/Libs/VTK-8.2.0/CMake/vtkModuleAPI.cmake:140 (message):
  Requested modules not available:

    vtkvtksys
Call Stack (most recent call first):
  Modules/Bridge/VtkGlue/itk-module-init.cmake:93 (vtk_module_config)
  CMake/ITKModuleEnablement.cmake:436 (include)
  CMakeLists.txt:754 (include)

-- Configuring incomplete, errors occurred!

Modules/Bridge/VtkGlue/itk-module-init.cmake
@@ -45,8 +45,10 @@ if(NOT VTK_RENDERING_BACKEND)
   endif()
 endif()
 set(_target_prefix "vtk")
+set(_vtk_sys_name "sys")
 if(VTK_VERSION VERSION_GREATER_EQUAL 8.90.0)
   set(_target_prefix "VTK::")
+  set(_vtk_sys_name "vtksys")
 endif()
 set(_target_freetypeopengl)
 if(TARGET ${_target_prefix}RenderingFreeType${VTK_RENDERING_BACKEND})
@@ -61,7 +63,7 @@ set(
   ${_target_prefix}IOImage
   ${_target_prefix}ImagingSources
   ${_target_prefix}kwiml
-  ${_target_prefix}vtksys
+  ${_target_prefix}${_vtk_sys_name}
 )
 if(ITK_WRAP_PYTHON)
   list(