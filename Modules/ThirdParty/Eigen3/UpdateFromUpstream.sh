7a05eb5835e3fe4672a19d5a2eef763dcbb9d9e6
Hans Johnson <hans-johnson@uiowa.edu>
2025-09-24 09:46:37 -0500

    ENH: Update to 3.4.1 Eigen release
    Resolves: #5484
    Replaces: #5482
    
    Add documentation for the process of updating Eigen from the upstream
    repo into the local ITK copy with ITK specific build support.

--- Modules/ThirdParty/Eigen3/UpdateFromUpstream.sh
+++ Modules/ThirdParty/Eigen3/UpdateFromUpstream.sh
@@ -1,4 +1,28 @@
 #!/usr/bin/env bash
+#
+# Process for updating eigen
+#
+cat > /dev/null << EOF
+TMP_DIR=${HOME}/tmp
+mkdir -p ${TMP_DIR}
+cd ${TMP_DIR}
+rm -rf ${TMP_DIR}/eigen
+git clone git@gitlab.com:libeigen/eigen.git
+cd eigen
+git remote add isceigen git@github.com:InsightSoftwareConsortium/eigen.git
+git fetch isceigen
+previous_isceigen_branch="for/itk-20250707-master-6854da2e"
+eigen_upstream_newreferenceupstream="origin/3.4.1"
+
+eigen_master_short_hash=$(git rev-parse --short ${eigen_upstream_newreferenceupstream})
+today=$(date +"%Y%m%d")
+new_isceigen_branch="for/itk-${today}-master-${eigen_master_short_hash}"
+git checkout isceigen/${previous_isceigen_branch} -b ${new_isceigen_branch}
+git rebase ${eigen_upstream_newreferenceupstream}
+git push isceigen ${new_isceigen_branch}:${new_isceigen_branch}
+echo "Change 'tag' line in Modules/ThirdParty/Eigen3/UpdateFromUpstream.sh to contain ${new_isceigen_branch}"
+EOF
+
 
 set -e
 set -x
@@ -8,7 +32,7 @@ readonly name="Eigen3"
 readonly ownership="Eigen Upstream <kwrobot@kitware.com>"
 readonly subtree="Modules/ThirdParty/Eigen3/src/itkeigen"
 readonly repo="https://github.com/InsightSoftwareConsortium/eigen"
-readonly tag="for/itk-20250707-master-6854da2e"
+readonly tag="for/itk-20250924-master-b66188b5d"  # Corresponds to eigen 3.4.1 tag
 readonly paths="
 Eigen/Cholesky
 Eigen/CholmodSupport
