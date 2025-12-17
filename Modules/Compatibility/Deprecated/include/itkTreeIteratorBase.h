InsightSoftwareConsortium/ITK

commit d58bf56fe2320fe3c7a451c96e3d8cd5e7c68358

DOC: Clean up method Doxygen documentation
Clean up method Doxygen documentation:
- Consolidate documentation so that methods are documented in the header
  files. By doing this fix the documentation of methods which had the
  wrong documentation (e.g. copied from another existing, unrelated
  method).
- Remove the documentation of trivial methods: constructors,
  destructors, `PrintSelf` methods, etc.
- Remove non-descriptive documentation of instance variables whose
  documentation should dwell in their public Get/Set macros according to
  the ITK SWG coding style guideline.
- Remove documentation blocks consisting solely of the method name from
  the implementation files.
- Remove unnecessary method ending brace comment blocks.
- Remove empty method documentation blocks from implementation file.
- Fix typos and method names cross-referenced in the documentation.
- Take advantage of the commit to improve the documentation style
  (capitalize first letter in sentence, finish sentences with a period,
  split brief and detailed documentation sentences, etc.)

Note that many of the implementation file method documentation blocks
were not in a Doxygen-recognizable format either:
https://doxygen.nl/manual/docblocks.html

Modules/Compatibility/Deprecated/include/itkTreeIteratorBase.h
@@ -89,27 +89,30 @@ public:
   static constexpr NodeType LEAF = NodeType::LEAF;
 #endif
 
-  /** Add an element to the tree */
+  /** Add an element to the tree at the current node.
+   *
+   * Creates a new child node.
+   */
   virtual bool
   Add(ValueType element);
 
-  /** Add an element at a given position */
+  /** Add an element to the tree at a given position. */
   virtual bool
   Add(int position, ValueType element);
 
-  /** Add a subtree */
+  /** Add a subtree. */
   virtual bool
   Add(TTreeType & subTree);
 
-  /** Get a value */
+  /** Get the value of the current node. */
   virtual const ValueType &
   Get() const;
 
-  /** Get the subtree */
+  /** Get the subtree. */
   virtual TTreeType *
   GetSubTree() const;
 
-  /** Return true if the current node is a leaf */
+  /** Return true if the current node is a leaf. */
   virtual bool
   IsLeaf() const;
 
@@ -121,85 +124,88 @@ public:
   virtual NodeType
   GetType() const = 0;
 
-  /** Go to the specified child */
+  /** Go to the specified child. */
   virtual bool
   GoToChild(ChildIdentifier number = 0);
 
-  /** Go to the parent */
+  /** Go to the parent of the current node. */
   virtual bool
   GoToParent();
 
-  /** Set the current value of the node */
+  /** Set the specified value to the current node. */
   void
   Set(ValueType element);
 
-  /** Return true if the current node has a child */
+  /** Return true if the current node has a child. */
   virtual bool
   HasChild(int number = 0) const;
 
-  /** Return the current ChildPosition of an element */
+  /** Return the current child position of an element. */
   virtual int
   ChildPosition(ValueType element) const;
 
-  /** Remove a child */
+  /** Remove a child.
+   *
+   * Removes its child nodes as well.
+   * /
   virtual bool
   RemoveChild(int number);
 
-  /** Count the number of children */
+  /** Count the number of children. */
   virtual int
   CountChildren() const;
 
-  /** Return true if the current node has a parent */
+  /** Return true if the current node has a parent. */
   virtual bool
   HasParent() const;
 
-  /** Disconnect the tree */
+  /** Disconnect the tree. */
   virtual bool
   Disconnect();
 
-  /** Return a list of children */
+  /** Return a list of children. */
   virtual TreeIteratorBase<TTreeType> *
   Children();
 
-  /** Return a list of parents */
+  /** Return the list of parents. */
   virtual TreeIteratorBase<TTreeType> *
   Parents();
 
-  /** Return a list of child */
+  /** Get the child corresponding to the given a number at the current node. */
   virtual TreeIteratorBase<TTreeType> *
   GetChild(int number) const;
 
-  /** Count the number of nodes */
+  /** Count the number of nodes. */
   virtual int
   Count();
 
-  /** Remove the current node from the tree */
+  /** Remove the current node from the tree. */
   bool
   Remove();
 
-  /** Get the current node */
+  /** Get the current node. */
   virtual TreeNodeType *
   GetNode();
 
   virtual const TreeNodeType *
   GetNode() const;
 
-  /** Get the root */
+  /** Get the root. */
   TreeNodeType *
   GetRoot();
 
   const TreeNodeType *
   GetRoot() const;
 
-  /** Get the tree */
+  /** Get the tree. */
   TTreeType *
   GetTree() const;
 
-  /** Return the first parent found */
+  /** Return the first parent found. */
   const TreeNodeType *
   GetParent() const;
 
-  /** Move an iterator to the beginning of the tree */
+  /** Move an iterator to the beginning of the tree. */
   void
   GoToBegin()
   {

commit f2c34d66a2828c70e226c52fe7df277f9a046781

COMP: Fix doc-string of TreeIteratorBase::RemoveChild from Deprecated
Problem inadvertently introduced by d58bf56fe2320fe3c7a451c96e3d8cd5e7c68358.

The error message was:

C:\Misc\ITK-patches-rel\Modules\Compatibility\Deprecated\include\itkTreeIteratorBase.hxx(238,30): error C2039: 'RemoveChild': is not a member of 'itk::TreeIteratorBase<TTreeType>'
C:\Misc\ITK-patches-rel\Modules\Compatibility\Deprecated\include\itkTreeIteratorBase.hxx(238,30): error C2039: 'RemoveChild': is not a member of 'itk::TreeIteratorBase<TTreeType>'
  (compiling source file '../../../../../ITK-patches-rel/Modules/Compatibility/Deprecated/test/itkTreeContainerTest.cxx')
      C:\Misc\ITK-patches-rel\Modules\Compatibility\Deprecated\include\itkTreeIteratorBase.h(68,27):
      see declaration of 'itk::TreeIteratorBase<TTreeType>'
C:\Misc\ITK-patches-rel\Modules\Compatibility\Deprecated\include\itkTreeIteratorBase.hxx(239,1): error C2447: '{': missing function header (old-style formal list?)
  (compiling source file '../../../../../ITK-patches-rel/Modules/Compatibility/Deprecated/test/itkTreeContainerTest.cxx')
  (compiling source file '../../../../../ITK-patches-rel/Modules/Compatibility/Deprecated/test/itkTreeContainerTest2.cxx')
      C:\Misc\ITK-patches-rel\Modules\Compatibility\Deprecated\include\itkTreeIteratorBase.h(68,27):
      see declaration of 'itk::TreeIteratorBase<TTreeType>'
C:\Misc\ITK-patches-rel\Modules\Compatibility\Deprecated\include\itkTreeIteratorBase.hxx(239,1): error C2447: '{': missing function header (old-style formal list?)
  (compiling source file '../../../../../ITK-patches-rel/Modules/Compatibility/Deprecated/test/itkTreeContainerTest2.cxx')

Modules/Compatibility/Deprecated/include/itkTreeIteratorBase.h
@@ -147,7 +147,7 @@ public:
   /** Remove a child.
    *
    * Removes its child nodes as well.
-   * /
+   */
   virtual bool
   RemoveChild(int number);
 