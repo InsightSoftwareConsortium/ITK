dc66d817c77894ce3fea6dcd02249ec4893430f1
Bradley Lowekamp <blowekamp@mail.nih.gov>
2026-01-08 23:29:50 +0000

    ENH: Configure factory meta-modules with registration properties
    Add ITK_*_FACTORY_REGISTER_MANAGER compile definitions and factory
    registration header includes to factory meta-module interface targets.
    
    Use itk_generate_factory_registration in Examples to register
    factories.

--- CMake/ITKFactoryRegistration.cmake
+++ CMake/ITKFactoryRegistration.cmake
@@ -136,10 +136,11 @@
 # Caveats
 # -------
 #
-# Since the both include directory containing the registration manager headers
-# and the `ITK_IO_FACTORY_REGISTER_MANAGER` COMPILE_DEFINITIONS are set as
-# directory properties, including external project (themselves including ITK)
-# after including ITK can have unintended side effects.
+# The include directory containing the registration manager headers and the
+# `ITK_<FACTORY_NAME>_FACTORY_REGISTER_MANAGER` COMPILE_DEFINITIONS are set as
+# target interface properties on the factory meta-module targets (ITKImageIO,
+# ITKMeshIO, ITKTransformIO, ITKFFTImageFilterInit). This ensures proper propagation
+# to dependent targets through modern CMake usage requirements.
 #
 
 # _itk_configure_FactoryRegisterManager(<factory_type> <formats>)
@@ -148,6 +149,8 @@
 # `<CMAKE_CURRENT_BINARY_DIR>/ITKFactoryRegistration/`.
 #
 # Header is named using the template `itk<factory_type>FactoryRegisterManager.h`
+# The include directory and compile definitions are added to the corresponding
+# factory meta-module target's INTERFACE properties.
 #
 function(_itk_configure_FactoryRegisterManager factory_type formats)
   set(LIST_OF_FACTORIES_REGISTRATION "")
@@ -168,6 +171,23 @@ function(_itk_configure_FactoryRegisterManager factory_type formats)
     ${ITK_CMAKE_DIR}/itk${factory_type}FactoryRegisterManager.h.in
     "${CMAKE_CURRENT_BINARY_DIR}/ITKFactoryRegistration/itk${factory_type}FactoryRegisterManager.h"
     @ONLY
+  )
+
+  # These functions may be called in the ITK source ( or a fetch content sub-project ),
+  # so support is needed when the namespaced meta-module target is an alias to the real target.
+  set(_meta_module ITK::ITK${factory_type})
+  get_property(
+    aliased_target_name
+    TARGET ${_meta_module}
+    PROPERTY ALIASED_TARGET
+  )
+  if(aliased_target_name)
+    set(_meta_module ${aliased_target_name})
+  endif()
+  target_include_directories(
+    ${_meta_module}
+    INTERFACE
+      "$<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/ITKFactoryRegistration>"
   )
 endfunction()
 
