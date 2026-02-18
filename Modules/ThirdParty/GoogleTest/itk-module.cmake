8dd7198bb28d2b74f3bd73eface7f09c739dfc5d
Bradley Lowekamp <blowekamp@mail.nih.gov>
2026-02-18 17:41:39 +0000

    BUG: Do not specify GTest targets as LIBRARIES for ITKGoogleTest
    The ITKGoogleTest_LIBRARIES variable must not specify the GoogleTest
    targets. This variable is exported for installation, but the targets
    are not. This may results in users of an installed ITK which use
    ITK_LIBRARIES to attempt to link to non-existing GTest Targets.

--- Modules/ThirdParty/GoogleTest/itk-module.cmake
+++ Modules/ThirdParty/GoogleTest/itk-module.cmake
@@ -1,7 +1,7 @@
 set(
   DOCUMENTATION
   "This module contains the third party <a href=\"https://github.com/google/googletest\">googletest</a> library,
-Google's C++ test framework."
+Google's C++ test framework. This module provides the GTest::gtest and GTest::gtest_main targets in the build directory only, and are not installed."
 )
 
 itk_module(ITKGoogleTest DEPENDS DESCRIPTION "${DOCUMENTATION}")
