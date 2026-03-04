InsightSoftwareConsortium/ITK

commit cfd3cad35d4496b2482141c0f0cba899ffb073d5

STYLE: Remove local type aliases of IteratorWithIndex in `*.hxx` files
Made it easier to figure out when an iterator "with index" is being used.

Using Notepad++, Replace in Files, doing:

    Find what: ^[ ]+ using (\w+) = (Image\w+IteratorWithIndex<\w+>);\r\n(.*?[^\w])\1
    Replace with: $3$2
    (*) Regular expression
    [v] . matches newline

Modules/Filtering/FFT/include/itkFFTWComplexToComplex1DFFTImageFilter.hxx
@@ -142,11 +142,9 @@ FFTWComplexToComplex1DFFTImageFilter<TInputImage, TOutputImage>::ThreadedGenerat
   const typename OutputImageType::SizeType & outputSize = outputPtr->GetRequestedRegion().GetSize();
   const unsigned int                         lineSize = outputSize[this->m_Direction];
 
-  using InputIteratorType = ImageLinearConstIteratorWithIndex<InputImageType>;
-  using OutputIteratorType = ImageLinearIteratorWithIndex<OutputImageType>;
-  InputIteratorType inputIt(inputPtr, outputRegion);
+  ImageLinearConstIteratorWithIndex<InputImageType> inputIt(inputPtr, outputRegion);
   // the output region should be the same as the input region in the non-fft directions
-  OutputIteratorType outputIt(outputPtr, outputRegion);
+  ImageLinearIteratorWithIndex<OutputImageType> outputIt(outputPtr, outputRegion);
 
   inputIt.SetDirection(this->m_Direction);
   outputIt.SetDirection(this->m_Direction);

Modules/Filtering/FFT/include/itkFFTWInverse1DFFTImageFilter.hxx
@@ -134,10 +134,8 @@ FFTWInverse1DFFTImageFilter<TInputImage, TOutputImage>::ThreadedGenerateData(con
   const typename OutputImageType::SizeType & outputSize = outputPtr->GetRequestedRegion().GetSize();
   const unsigned int                         lineSize = outputSize[this->m_Direction];
 
-  using InputIteratorType = ImageLinearConstIteratorWithIndex<InputImageType>;
-  using OutputIteratorType = ImageLinearIteratorWithIndex<OutputImageType>;
-  InputIteratorType  inputIt(inputPtr, outputRegion);
-  OutputIteratorType outputIt(outputPtr, outputRegion);
+  ImageLinearConstIteratorWithIndex<InputImageType> inputIt(inputPtr, outputRegion);
+  ImageLinearIteratorWithIndex<OutputImageType>     outputIt(outputPtr, outputRegion);
 
   inputIt.SetDirection(this->m_Direction);
   outputIt.SetDirection(this->m_Direction);

Modules/Core/SpatialObjects/include/itkSpatialObjectToImageFilter.hxx
@@ -277,9 +277,8 @@ SpatialObjectToImageFilter<TInputSpatialObject, TOutputImage>::GenerateData()
   OutputImage->SetDirection(m_Direction);
   OutputImage->Allocate(); // allocate the image
 
-  using myIteratorType = ImageRegionIteratorWithIndex<OutputImageType>;
 
-  myIteratorType it(OutputImage, region);
+  ImageRegionIteratorWithIndex<OutputImageType> it(OutputImage, region);
 
   itk::Point<double, ObjectDimension>      objectPoint;
   itk::Point<double, OutputImageDimension> imagePoint;

Modules/Core/SpatialObjects/include/itkSpatialObjectToImageStatisticsCalculator.hxx
@@ -114,12 +114,11 @@ SpatialObjectToImageStatisticsCalculator<TInputImage, TInputSpatialObject, TSamp
 
     const typename MaskImageType::ConstPointer maskImage = maskSpatialObject->GetImage();
 
-    using MaskIteratorType = ImageRegionConstIteratorWithIndex<MaskImageType>;
-    MaskIteratorType it(maskImage, maskImage->GetLargestPossibleRegion());
-    IndexType        ind;
-    PointType        pnt;
-    PointType        tPnt;
-    VectorType       mv;
+    ImageRegionConstIteratorWithIndex<MaskImageType> it(maskImage, maskImage->GetLargestPossibleRegion());
+    IndexType                                        ind;
+    PointType                                        pnt;
+    PointType                                        tPnt;
+    VectorType                                       mv;
     while (!it.IsAtEnd())
     {
       if (it.Get() > 0) // if inside the mask
@@ -180,11 +179,10 @@ SpatialObjectToImageStatisticsCalculator<TInputImage, TInputSpatialObject, TSamp
 
     const RegionType region(indMin, size);
 
-    using IteratorType = ImageRegionConstIteratorWithIndex<ImageType>;
-    IteratorType it(m_Image, region);
-    IndexType    ind;
-    PointType    pnt;
-    VectorType   mv;
+    ImageRegionConstIteratorWithIndex<ImageType> it(m_Image, region);
+    IndexType                                    ind;
+    PointType                                    pnt;
+    VectorType                                   mv;
     while (!it.IsAtEnd())
     {
       ind = it.GetIndex();

Modules/Filtering/DiffusionTensorImage/include/itkDiffusionTensor3DReconstructionImageFilter.hxx
@@ -301,7 +301,6 @@ DiffusionTensor3DReconstructionImageFilter<TReferenceImagePixelType,
   else if (m_GradientImageTypeEnumeration ==
            DiffusionTensor3DReconstructionImageFilterEnums::GradientImageFormat::GradientIsInASingleImage)
   {
-    using GradientIteratorType = ImageRegionConstIteratorWithIndex<GradientImagesType>;
     using GradientVectorType = typename GradientImagesType::PixelType;
     typename GradientImagesType::Pointer gradientImagePointer = nullptr;
 
@@ -309,7 +308,7 @@ DiffusionTensor3DReconstructionImageFilter<TReferenceImagePixelType,
     // The enum will ensure that an inappropriate cast is not done
     gradientImagePointer = itkDynamicCastInDebugMode<GradientImagesType *>(this->ProcessObject::GetInput(0));
 
-    GradientIteratorType git(gradientImagePointer, outputRegionForThread);
+    ImageRegionConstIteratorWithIndex<GradientImagesType> git(gradientImagePointer, outputRegionForThread);
     git.GoToBegin();
 
     // Compute the indices of the baseline images and gradient images

Modules/Filtering/DisplacementField/include/itkInverseDisplacementFieldImageFilter.hxx
@@ -150,11 +150,10 @@ InverseDisplacementFieldImageFilter<TInputImage, TOutputImage>::PrepareKernelBas
 
   const InputImageType * sampledInput = resampler->GetOutput();
 
-  using IteratorType = ImageRegionConstIteratorWithIndex<InputImageType>;
 
   unsigned int landmarkId = 0;
 
-  IteratorType ot(sampledInput, subsampledRegion);
+  ImageRegionConstIteratorWithIndex<InputImageType> ot(sampledInput, subsampledRegion);
 
   OutputPixelType               value;
   Point<double, ImageDimension> sourcePoint;
@@ -213,7 +212,6 @@ InverseDisplacementFieldImageFilter<TInputImage, TOutputImage>::GenerateData()
   outputPtr->Allocate();
 
   // Create an iterator that will walk the output region for this thread.
-  using OutputIterator = ImageRegionIteratorWithIndex<TOutputImage>;
 
   const OutputImageRegionType region = outputPtr->GetRequestedRegion();
 
@@ -230,7 +228,7 @@ InverseDisplacementFieldImageFilter<TInputImage, TOutputImage>::GenerateData()
   ProgressReporter progress(this, 0, region.GetNumberOfPixels(), 10);
 
   // Walk the output region
-  for (OutputIterator outIt(outputPtr, region); !outIt.IsAtEnd(); ++outIt)
+  for (ImageRegionIteratorWithIndex<TOutputImage> outIt(outputPtr, region); !outIt.IsAtEnd(); ++outIt)
   {
     // Determine the index of the current output pixel
     outputIndex = outIt.GetIndex();

Modules/Filtering/DisplacementField/include/itkLandmarkDisplacementFieldSource.hxx
@@ -115,7 +115,6 @@ LandmarkDisplacementFieldSource<TOutputImage>::GenerateData()
   outputPtr->Allocate();
 
   // Create an iterator that will walk the output region for this thread.
-  using OutputIterator = ImageRegionIteratorWithIndex<TOutputImage>;
 
   const OutputImageRegionType region = outputPtr->GetRequestedRegion();
 
@@ -132,7 +131,7 @@ LandmarkDisplacementFieldSource<TOutputImage>::GenerateData()
   ProgressReporter progress(this, 0, region.GetNumberOfPixels(), 10);
 
   // Walk the output region
-  for (OutputIterator outIt(outputPtr, region); !outIt.IsAtEnd(); ++outIt)
+  for (ImageRegionIteratorWithIndex<TOutputImage> outIt(outputPtr, region); !outIt.IsAtEnd(); ++outIt)
   {
     // Determine the index of the current output pixel
     outputIndex = outIt.GetIndex();

Modules/Filtering/FFT/include/itkFFTWForward1DFFTImageFilter.hxx
@@ -130,10 +130,8 @@ FFTWForward1DFFTImageFilter<TInputImage, TOutputImage>::ThreadedGenerateData(con
   const InputImageType * inputPtr = this->GetInput();
   OutputImageType *      outputPtr = this->GetOutput();
 
-  using InputIteratorType = ImageLinearConstIteratorWithIndex<InputImageType>;
-  using OutputIteratorType = ImageLinearIteratorWithIndex<OutputImageType>;
-  InputIteratorType  inputIt(inputPtr, outputRegion);
-  OutputIteratorType outputIt(outputPtr, outputRegion);
+  ImageLinearConstIteratorWithIndex<InputImageType> inputIt(inputPtr, outputRegion);
+  ImageLinearIteratorWithIndex<OutputImageType>     outputIt(outputPtr, outputRegion);
 
   inputIt.SetDirection(this->GetDirection());
   outputIt.SetDirection(this->GetDirection());

commit 920c6818db15eaf60cf9bbf02eec74e235c02d04

COMP: Fix missing alias for iterator types
Fixes compile errors introduced by cfd3cad35d4496b2482141c0f0cba899ffb073d5, e.g.:
```
C:\src\itk\ITK-main\Modules\Filtering\FFT\include\itkFFTWComplexToComplex1DFFTImageFilter.hxx(191,11): error C2065: 'inputBufferIt' : undeclared identifier [C:\src\itk\2019-main-Shared-Debug-FFTWON\Modules\Filtering\FFT\src\ITKFFT.vcxproj
C:\src\itk\ITK-main\Modules\Filtering\FFT\include\itkFFTWComplexToComplex1DFFTImageFilter.hxx(189,80): error C2061: syntax error : identifier 'PixelType' [C:\src\itk\2019-main-Shared-Debug-FFTWON\Modules\Filtering\FFT\src\ITKFFT.vcxproj]
```

Modules/Filtering/FFT/include/itkFFTWComplexToComplex1DFFTImageFilter.hxx
@@ -154,8 +154,7 @@ FFTWComplexToComplex1DFFTImageFilter<TInputImage, TOutputImage>::ThreadedGenerat
   {
     // copy the input line into our buffer
     inputIt.GoToBeginOfLine();
-    typename InputIteratorType::PixelType * inputBufferIt =
-      reinterpret_cast<typename InputIteratorType::PixelType *>(m_InputBufferArray[threadID]);
+    auto * inputBufferIt = reinterpret_cast<typename InputImageType::PixelType *>(m_InputBufferArray[threadID]);
     while (!inputIt.IsAtEndOfLine())
     {
       *inputBufferIt = inputIt.Get();
@@ -169,8 +168,7 @@ FFTWComplexToComplex1DFFTImageFilter<TInputImage, TOutputImage>::ThreadedGenerat
     if (this->m_TransformDirection == Superclass::DIRECT)
     {
       // copy the output from the buffer into our line
-      typename OutputIteratorType::PixelType * outputBufferIt =
-        reinterpret_cast<typename OutputIteratorType::PixelType *>(m_OutputBufferArray[threadID]);
+      auto * outputBufferIt = reinterpret_cast<typename OutputImageType::PixelType *>(m_OutputBufferArray[threadID]);
       outputIt.GoToBeginOfLine();
       while (!outputIt.IsAtEndOfLine())
       {
@@ -182,11 +180,11 @@ FFTWComplexToComplex1DFFTImageFilter<TInputImage, TOutputImage>::ThreadedGenerat
     else // m_TransformDirection == INVERSE
     {
       // copy the output from the buffer into our line
-      inputBufferIt = reinterpret_cast<typename OutputIteratorType::PixelType *>(m_OutputBufferArray[threadID]);
+      inputBufferIt = reinterpret_cast<typename OutputImageType::PixelType *>(m_OutputBufferArray[threadID]);
       outputIt.GoToBeginOfLine();
       while (!outputIt.IsAtEndOfLine())
       {
-        outputIt.Set(*inputBufferIt / static_cast<typename OutputIteratorType::PixelType>(lineSize));
+        outputIt.Set(*inputBufferIt / static_cast<typename OutputImageType::PixelType>(lineSize));
         ++outputIt;
         ++inputBufferIt;
       }

Modules/Filtering/FFT/include/itkFFTWInverse1DFFTImageFilter.hxx
@@ -145,8 +145,7 @@ FFTWInverse1DFFTImageFilter<TInputImage, TOutputImage>::ThreadedGenerateData(con
   {
     // copy the input line into our buffer
     inputIt.GoToBeginOfLine();
-    typename InputIteratorType::PixelType * inputBufferIt =
-      reinterpret_cast<typename InputIteratorType::PixelType *>(m_InputBufferArray[threadID]);
+    auto * inputBufferIt = reinterpret_cast<typename InputImageType::PixelType *>(m_InputBufferArray[threadID]);
     while (!inputIt.IsAtEndOfLine())
     {
       *inputBufferIt = inputIt.Get();