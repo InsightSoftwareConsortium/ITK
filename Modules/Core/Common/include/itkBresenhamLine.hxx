838f6020a5ab5e585071b21411f1f282714ac6b5
Viacheslav Ustymenko <vustymenko@protonmail.com>
2026-04-09 01:05:55 +0300

    BUG: BresenhamLine - use double precision floats for ending index computation
    Co-authored-by: greptile-apps[bot] <165735046+greptile-apps[bot]@users.noreply.github.com>

--- Modules/Core/Common/include/itkBresenhamLine.hxx
+++ Modules/Core/Common/include/itkBresenhamLine.hxx
@@ -41,7 +41,7 @@ BresenhamLine<VDimension>::BuildLine(LType Direction, IdentifierType length) -> 
   // compute actual line length because the shorter distance
   // the larger deviation due to rounding to integers
   const IdentifierType mainDirectionLen = length - 1;
-  const float          euclideanLineLen = mainDirectionLen / itk::Math::Absolute(Direction[maxDistanceDimension]);
+  const double         euclideanLineLen = mainDirectionLen / itk::Math::Absolute(Direction[maxDistanceDimension]);
 
   // we are going to start at 0
   constexpr IndexType StartIndex{ 0 };
