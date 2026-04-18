e8c0e64dba6b63ea5bc2d0796d5ce911d6e8a3f9
Hans Johnson <hans-johnson@uiowa.edu>
2026-04-18 17:36:27 -0500

    STYLE: Remove non-ITK artifacts from ingested Cuberille
    Removes standalone-repo scaffolding that has no role in the ITK monorepo:
    
      .clang-format, .gitattributes  - ITK root versions govern
      .github/                        - foreign CI; ITK workflows cover this
      CTestConfig.cmake               - standalone CDash; ITK top-level applies
      pyproject.toml                  - standalone Python packaging
      requirements-dev.txt            - standalone Python dev deps
      LICENSE                         - redundant (Apache-2.0 matches ITK root)
      test/Docker/                    - standalone-repo Docker CI harness
    
    No edits to ingested source files; structural cleanup only. Retained:
    CMakeLists.txt, itk-module.cmake, include/, test/, wrapping/, examples/,
    README.rst.

--- Modules/Beta/Cuberille/test/Docker/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-script_dir="`cd $(dirname $0); pwd`"
-
-docker build -t insighttoolkit/cuberille-test $script_dir
--- Modules/Beta/Cuberille/test/Docker/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-script_dir="`cd $(dirname $0); pwd`"
-
-docker run \
-  --rm \
-  -v $script_dir/../..:/usr/src/ITKCuberille \
-    insighttoolkit/cuberille-test \
-      /usr/src/ITKCuberille/test/Docker/test.sh
