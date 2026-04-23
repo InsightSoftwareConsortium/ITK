b8b239f9b8f66e8b0bc3e46e4af4ba951f2071bc
Niels Dekker <N.Dekker@lumc.nl>
2026-04-23 20:10:21 +0200

    STYLE: Use ConstShapedNeighborhoodIterator in CornerOffsets
    Improved const-correctness by using ConstShapedNeighborhoodIterator, instead of
    non-const ShapedNeighborhoodIterator, internally inside the BoxUtilities
    function `CornerOffsets(const TImage *)`.
    
    Similar to pull request https://github.com/InsightSoftwareConsortium/ITK/pull/6099
    commit 6e021ff4c6389cbad197249fbc5212ed809c51be
    "STYLE: Use ConstShapedNeighborhoodIterator in ShapedFloodFilledImageFunc"

--- Modules/Filtering/Smoothing/include/itkBoxUtilities.h
+++ Modules/Filtering/Smoothing/include/itkBoxUtilities.h
@@ -164,7 +164,7 @@ template <typename TImage>
 std::vector<typename TImage::OffsetType>
 CornerOffsets(const TImage * im)
 {
-  using NIterator = ShapedNeighborhoodIterator<TImage>;
+  using NIterator = ConstShapedNeighborhoodIterator<TImage>;
   auto                                     unitradius = TImage::SizeType::Filled(1);
   const NIterator                          n1(unitradius, im, im->GetRequestedRegion());
   const unsigned int                       centerIndex = n1.GetCenterNeighborhoodIndex();
