77edee27b53734e004bb6943cef8e4746272b821
Hans J. Johnson <hans-johnson@uiowa.edu>
2026-04-29 18:07:28 -0500

    STYLE: Apply gersemi + trim trailing whitespace to LabelErodeDilate

--- Modules/Filtering/LabelErodeDilate/itk-module.cmake
+++ Modules/Filtering/LabelErodeDilate/itk-module.cmake
@@ -1,15 +1,13 @@
-# the top-level README is used for describing this module, just
-# re-used it for documentation here
-get_filename_component(MY_CURENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-file(READ "${MY_CURENT_DIR}/README.rst" DOCUMENTATION)
+set(
+  DOCUMENTATION
+  "Morphological erosion and dilation for label images. Label collisions are
+handled consistently and operations execute in approximately constant time
+with respect to structuring-element size. Only circular, spherical, and
+hyperspherical structuring elements are supported."
+)
 
-# itk_module() defines the module dependencies in LabelErodeDilate
-# The testing module in LabelErodeDilate depends on ITKTestKernel
-# By convention those modules outside of ITK are not prefixed with
-# ITK
-
-# define the dependencies of the include module and the tests
-itk_module(LabelErodeDilate
+itk_module(
+  LabelErodeDilate
   DEPENDS
     ITKIOImageBase
   TEST_DEPENDS
@@ -17,6 +15,5 @@ itk_module(LabelErodeDilate
     ITKTestKernel
     ITKSmoothing
   EXCLUDE_FROM_DEFAULT
-  DESCRIPTION
-  "${DOCUMENTATION}"
+  DESCRIPTION "${DOCUMENTATION}"
 )
