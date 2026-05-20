eedb27fbd106b58dabb93ff315a6ffab81896e3c
Hans J. Johnson <hans-johnson@uiowa.edu>
2026-05-20 09:24:30 -0500

    ENH: Convert example driver to ITK testing-macro form (ITK#6308)
    Replace per-step raw try/catch + return EXIT_FAILURE blocks with
    ITK_TRY_EXPECT_NO_EXCEPTION; replace if(!ExposeMetaData) early
    returns with ITK_TEST_EXPECT_TRUE_STATUS_VALUE accumulating into
    testStatus. Drop the commented-out DCMTKImageIO and
    DCMTKSeriesFileNames type aliases (GDCM is the only path; the
    MetaDataDictionary-population gap for DCMTKImageIO is a separate
    concern). Use 'auto' uniformly for SmartPointer locals.

--- Modules/IO/IOTransformDCMTK/test/ReadDicomTransformAndResampleExample.cxx
+++ Modules/IO/IOTransformDCMTK/test/ReadDicomTransformAndResampleExample.cxx
@@ -20,20 +20,18 @@
 #include "itkDCMTKTransformIOFactory.h"
 #include "itkTransformFileReader.h"
 #include "itkImageSeriesReader.h"
-#include "itkDCMTKImageIO.h"
-#include "itkDCMTKSeriesFileNames.h"
 #include "itkGDCMImageIO.h"
 #include "itkGDCMSeriesFileNames.h"
 #include "itkCompositeTransform.h"
 #include "itkImageFileWriter.h"
 #include "itkMetaDataObject.h"
 #include "itkResampleImageFilter.h"
+#include "itkTestingMacros.h"
 
 
 int
 ReadDicomTransformAndResampleExample(int argc, char * argv[])
 {
-  // Parse command line arguments
   if (argc < 6)
   {
     std::cerr << "Usage: " << argv[0] << " fixedSeriesDirectory movingSeriesDirectory"
@@ -46,183 +44,122 @@ ReadDicomTransformAndResampleExample(int argc, char * argv[])
   const char * fixedImageOutputFileName = argv[4];
   const char * resampledMovingOutputFileName = argv[5];
 
+  int testStatus = EXIT_SUCCESS;
 
-  // Basic types
   constexpr unsigned int Dimension = 3;
   using PixelType = short;
   using ImageType = itk::Image<PixelType, Dimension>;
+  using ScalarType = float;
 
-
-  // Read the fixed and moving image
   using ReaderType = itk::ImageSeriesReader<ImageType>;
-  ReaderType::Pointer fixedReader = ReaderType::New();
-
-  // DCMTKImageIO does not populate the MetaDataDictionary yet
-  // using ImageIOType = itk::DCMTKImageIO;
   using ImageIOType = itk::GDCMImageIO;
-  ImageIOType::Pointer fixedIO = ImageIOType::New();
+  using SeriesFileNamesType = itk::GDCMSeriesFileNames;
+  using FileNamesContainerType = SeriesFileNamesType::FileNamesContainerType;
+
+  auto fixedReader = ReaderType::New();
+  auto fixedIO = ImageIOType::New();
   fixedReader->SetImageIO(fixedIO);
 
-  // using SeriesFileNamesType = itk::DCMTKSeriesFileNames;
-  using SeriesFileNamesType = itk::GDCMSeriesFileNames;
-  SeriesFileNamesType::Pointer fixedSeriesFileNames = SeriesFileNamesType::New();
+  auto fixedSeriesFileNames = SeriesFileNamesType::New();
   fixedSeriesFileNames->SetInputDirectory(fixedSeriesDirectory);
-  using FileNamesContainerType = SeriesFileNamesType::FileNamesContainerType;
   const FileNamesContainerType & fixedFileNames = fixedSeriesFileNames->GetInputFileNames();
   std::cout << "There are " << fixedFileNames.size() << " fixed image slices." << std::endl;
-  std::cout << "First fixed images series UID: " << fixedSeriesFileNames->GetSeriesUIDs()[0] << "\n" << std::endl;
+  std::cout << "First fixed images series UID: " << fixedSeriesFileNames->GetSeriesUIDs()[0] << '\n' << std::endl;
   fixedReader->SetFileNames(fixedFileNames);
 
-  ReaderType::Pointer  movingReader = ReaderType::New();
-  ImageIOType::Pointer movingIO = ImageIOType::New();
+  auto movingReader = ReaderType::New();
+  auto movingIO = ImageIOType::New();
   movingReader->SetImageIO(movingIO);
 
-  SeriesFileNamesType::Pointer movingSeriesFileNames = SeriesFileNamesType::New();
+  auto movingSeriesFileNames = SeriesFileNamesType::New();
   movingSeriesFileNames->SetInputDirectory(movingSeriesDirectory);
   const FileNamesContainerType & movingFileNames = movingSeriesFileNames->GetInputFileNames();
   std::cout << "There are " << movingFileNames.size() << " moving image slices." << std::endl;
-  std::cout << "First moving images series UID: " << movingSeriesFileNames->GetSeriesUIDs()[0] << "\n" << std::endl;
+  std::cout << "First moving images series UID: " << movingSeriesFileNames->GetSeriesUIDs()[0] << '\n' << std::endl;
   movingReader->SetFileNames(movingFileNames);
 
-  try
-  {
-    fixedReader->Update();
-    movingReader->Update();
-  }
-  catch (itk::ExceptionObject & error)
-  {
-    std::cerr << "Error: " << error << std::endl;
-    return EXIT_FAILURE;
-  }
+  ITK_TRY_EXPECT_NO_EXCEPTION(fixedReader->Update());
+  ITK_TRY_EXPECT_NO_EXCEPTION(movingReader->Update());
 
-
-  // Create a DICOM transform reader
-  using ScalarType = float;
-
-  itk::DCMTKTransformIOFactory::Pointer dcmtkTransformIOFactory = itk::DCMTKTransformIOFactory::New();
+  auto dcmtkTransformIOFactory = itk::DCMTKTransformIOFactory::New();
   itk::ObjectFactoryBase::RegisterFactory(dcmtkTransformIOFactory);
 
   using TransformReaderType = itk::TransformFileReaderTemplate<ScalarType>;
-  TransformReaderType::Pointer transformReader = TransformReaderType::New();
+  auto transformReader = TransformReaderType::New();
   transformReader->SetFileName(transformFileName);
 
   using TransformIOType = itk::DCMTKTransformIO<ScalarType>;
-  TransformIOType::Pointer transformIO = TransformIOType::New();
+  auto transformIO = TransformIOType::New();
   transformReader->SetTransformIO(transformIO);
 
+  using TransformListType = TransformReaderType::TransformListType;
+  using ReadTransformType = itk::CompositeTransform<ScalarType, Dimension>;
 
-  // Read in the fixed image transform
+  // Fixed image transform
   const ReaderType::DictionaryType & fixedMetaDataDict = fixedIO->GetMetaDataDictionary();
   std::string                        fixedFrameOfReferenceUID;
-  if (!itk::ExposeMetaData<std::string>(fixedMetaDataDict, "0020|0052", fixedFrameOfReferenceUID))
-  {
-    std::cerr << "Could not find the fixed image frame of reference UID." << std::endl;
-    return EXIT_FAILURE;
-  }
+  ITK_TEST_EXPECT_TRUE_STATUS_VALUE(
+    itk::ExposeMetaData<std::string>(fixedMetaDataDict, "0020|0052", fixedFrameOfReferenceUID), testStatus);
   std::cout << "Fixed image frame of reference UID: " << fixedFrameOfReferenceUID << std::endl;
   transformIO->SetFrameOfReferenceUID(fixedFrameOfReferenceUID);
 
-  try
-  {
-    transformReader->Update();
-  }
-  catch (itk::ExceptionObject & error)
-  {
-    std::cerr << "Error: " << error << std::endl;
-    return EXIT_FAILURE;
-  }
-  using TransformListType = TransformReaderType::TransformListType;
-  const TransformListType * transformList = transformReader->GetTransformList();
-
-  using ReadTransformType = itk::CompositeTransform<ScalarType, Dimension>;
-  auto                       transformIt = transformList->begin();
-  ReadTransformType::Pointer fixedTransform = dynamic_cast<ReadTransformType *>((*transformIt).GetPointer());
+  ITK_TRY_EXPECT_NO_EXCEPTION(transformReader->Update());
+  const TransformListType *        transformList = transformReader->GetTransformList();
+  auto                             transformIt = transformList->begin();
+  const ReadTransformType::Pointer fixedTransform = dynamic_cast<ReadTransformType *>((*transformIt).GetPointer());
   if (fixedTransform.IsNull())
   {
-    std::cerr << "Did not get the expected transform out." << std::endl;
+    std::cerr << "Did not get the expected fixed transform out." << std::endl;
     return EXIT_FAILURE;
   }
   std::cout << "Fixed transform: " << fixedTransform << std::endl;
 
-
-  // Read in the moving image transform
+  // Moving image transform
   const ReaderType::DictionaryType & movingMetaDataDict = movingIO->GetMetaDataDictionary();
   std::string                        movingFrameOfReferenceUID;
-  if (!itk::ExposeMetaData<std::string>(movingMetaDataDict, "0020|0052", movingFrameOfReferenceUID))
-  {
-    std::cerr << "Could not find the moving image frame of reference UID." << std::endl;
-    return EXIT_FAILURE;
-  }
+  ITK_TEST_EXPECT_TRUE_STATUS_VALUE(
+    itk::ExposeMetaData<std::string>(movingMetaDataDict, "0020|0052", movingFrameOfReferenceUID), testStatus);
   std::cout << "Moving image frame of reference UID: " << movingFrameOfReferenceUID << std::endl;
   transformIO->SetFrameOfReferenceUID(movingFrameOfReferenceUID);
 
-  try
-  {
-    transformReader->Update();
-  }
-  catch (itk::ExceptionObject & error)
-  {
-    std::cerr << "Error: " << error << std::endl;
-    return EXIT_FAILURE;
-  }
-
+  ITK_TRY_EXPECT_NO_EXCEPTION(transformReader->Update());
   transformList = transformReader->GetTransformList();
   transformIt = transformList->begin();
-  ReadTransformType::Pointer movingTransform = dynamic_cast<ReadTransformType *>((*transformIt).GetPointer());
+  const ReadTransformType::Pointer movingTransform = dynamic_cast<ReadTransformType *>((*transformIt).GetPointer());
   if (movingTransform.IsNull())
   {
-    std::cerr << "Did not get the expected transform out." << std::endl;
+    std::cerr << "Did not get the expected moving transform out." << std::endl;
     return EXIT_FAILURE;
   }
   std::cout << "Moving transform: " << movingTransform << std::endl;
 
-
   // Compose the transform from the fixed to the moving image
-  ReadTransformType::Pointer movingTransformInverse = ReadTransformType::New();
+  auto movingTransformInverse = ReadTransformType::New();
   movingTransform->GetInverse(movingTransformInverse);
 
-  ReadTransformType::Pointer fixedToMovingTransform = ReadTransformType::New();
+  auto fixedToMovingTransform = ReadTransformType::New();
   fixedToMovingTransform->AddTransform(fixedTransform);
   fixedToMovingTransform->AddTransform(movingTransformInverse);
-  // Flatten out the two component CompositeTransforms.
   fixedToMovingTransform->FlattenTransformQueue();
 
   using ResamplerType = itk::ResampleImageFilter<ImageType, ImageType, ScalarType, ScalarType>;
-  ResamplerType::Pointer resampler = ResamplerType::New();
+  auto resampler = ResamplerType::New();
   resampler->SetInput(movingReader->GetOutput());
   resampler->SetUseReferenceImage(true);
   resampler->SetReferenceImage(fixedReader->GetOutput());
   resampler->SetTransform(fixedToMovingTransform);
   resampler->SetDefaultPixelValue(-1000);
 
-
-  // Write the fixed image and resampled moving image (should look similar)
   using WriterType = itk::ImageFileWriter<ImageType>;
-  WriterType::Pointer writer = WriterType::New();
+  auto writer = WriterType::New();
   writer->SetFileName(fixedImageOutputFileName);
   writer->SetInput(fixedReader->GetOutput());
-  try
-  {
-    writer->Update();
-  }
-  catch (itk::ExceptionObject & error)
-  {
-    std::cerr << "Error: " << error << std::endl;
-    return EXIT_FAILURE;
-  }
+  ITK_TRY_EXPECT_NO_EXCEPTION(writer->Update());
 
   writer->SetInput(resampler->GetOutput());
   writer->SetFileName(resampledMovingOutputFileName);
-  try
-  {
-    writer->Update();
-  }
-  catch (itk::ExceptionObject & error)
-  {
-    std::cerr << "Error: " << error << std::endl;
-    return EXIT_FAILURE;
-  }
+  ITK_TRY_EXPECT_NO_EXCEPTION(writer->Update());
 
-
-  return EXIT_SUCCESS;
+  std::cout << "Test finished." << std::endl;
+  return testStatus;
 }
