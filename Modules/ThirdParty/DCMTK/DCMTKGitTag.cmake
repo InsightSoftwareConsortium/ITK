88ec2e79f6b303e32afbd981af32765c74e6a9bc
Hans J. Johnson <hans-johnson@uiowa.edu>
2026-05-29 10:15:30 -0500

    COMP: Update vendored DCMTK to 3.7.0
    Bump the DCMTK ExternalProject pin from the 2024-03-11 3.6.x snapshot
    to the DCMTK-3.7.0 patch branch maintained on the
    InsightSoftwareConsortium/DCMTK fork
    (20260524_DCMTK-3.7.0_PATCHES_FOR_ITK-wasm-3).
    
    The ITK-specific patch stack (ITK PNG/codec sharing, WASI/Emscripten
    fixes, GSPS/CSPS and plastimatch support) is carried on top of the
    upstream DCMTK-3.7.0 release tag and recorded in the patch list.

--- Modules/ThirdParty/DCMTK/DCMTKGitTag.cmake
+++ Modules/ThirdParty/DCMTK/DCMTKGitTag.cmake
@@ -4,7 +4,7 @@ set(
   DCMTK_GIT_REPOSITORY
   "https://github.com/InsightSoftwareConsortium/DCMTK.git"
 )
-# 2024.03.11 patched
+# DCMTK-3.7.0 patched
 # Patch list:
 
 #Francois Budin (1):
@@ -16,10 +16,24 @@ set(
 #Jean-Christophe Fillion-Robin (1):
 #COMP: Set 3rd party package CMake variables only if needed
 
-#Matt McCormick (1):
-#Support CMAKE_CROSSCOMPILING_EMULATOR # pushed upstream in https://github.com/DCMTK/dcmtk/pull/94
+#Matt McCormick (7):
+#COMP: Avoid dcmjpeg use of setjmp with WASI
+#COMP: Enable dcmjpeg, dcmjpls to support plastimatch
+#COMP: Add missing oflog libraries for native unix
+#COMP: Update __cxa_allocate_exception for WASI-SDK 22
+#COMP: Do not try to make mmap calls with wasi
+#COMP: Add WASI exception shim
+#COMP: Do not use non-type template argument -1 in variadic template
 
-#Shreeraj Jadhav (1):
-#ENH: Replace HTML_HYPERLINK_PREFIX_FOR_CGI with std::string argument
+#Shreeraj Jadhav (9):
+#COMP: enable dcmfg for dcmqi seg object read-write
+#COMP: Fixes for wasi compiler errors after enabling dcmpstat
+#COMP: Enable dcmpstat to support GSPS/CSPS IODs
+#COMP: disable use of log4cplus::tistringstream
+#COMP: add fixes for Emscripten linker errors
+#COMP: Enable dcm2pdf, fix WIN32 build
+#COMP: disable unneeded tests and apps
+#COMP: fix WASI compiler errors
+#COMP: fix missing <csetjmp> header issue
 
-set(DCMTK_GIT_TAG "8197ee21a2e17be65b72508dca9a91d4d60725aa") # 20240311_DCMTK_PATCHES_FOR_ITK-1
+set(DCMTK_GIT_TAG "2f645cb91b603ccc674857ea05b9822a8a899675") # for/itk-dcmtk-3.7.0-ccfd10b
