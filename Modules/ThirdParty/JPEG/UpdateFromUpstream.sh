6dfc8e0ff9e13e86fe57461f15e38aa36d9aee4e
Bradley Lowekamp <blowekamp@mail.nih.gov>
2026-06-03 15:09:41 +0000

    ENH: Update libjpeg-turbo to 3.1.4
    Add move to restore previous location of source code and flat layout.
    
    https://github.com/libjpeg-turbo/libjpeg-turbo/releases/tag/3.1.4

--- Modules/ThirdParty/JPEG/UpdateFromUpstream.sh
+++ Modules/ThirdParty/JPEG/UpdateFromUpstream.sh
@@ -8,14 +8,14 @@ readonly name="jpeg-turbo"
 readonly ownership="libjpeg-turbo Upstream <kwrobot@kitware.com>"
 readonly subtree="Modules/ThirdParty/JPEG/src/itk$name"
 readonly repo="https://github.com/libjpeg-turbo/libjpeg-turbo.git"
-readonly tag="3.0.4"
+readonly tag="3.1.4"
 readonly exact_tree_match=false
 readonly paths="
-j*.c
-j*.h
-jconfig.h.in
-jconfigint.h.in
-jversion.h.in
+src/j*.c
+src/j*.h
+src/jconfig.h.in
+src/jconfigint.h.in
+src/jversion.h.in
 
 LICENSE.md
 README.ijg
@@ -25,6 +25,9 @@ README.md
 extract_source () {
     git_archive
     pushd "${extractdir}/${name}-reduced"
+    # libjpeg-turbo 3.1.0 and later use a src/ subdirectory relocate to top-level structure.
+    mv src/* .
+    rmdir src
     echo "* -whitespace" >> .gitattributes
     echo "README.md conflict-marker-size=8" >> .gitattributes
     popd
