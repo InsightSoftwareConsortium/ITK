dfa3d7e5bcbdc8b5962d7c80580eb044f86663d3
Hans J. Johnson <hans-johnson@uiowa.edu>
2026-06-06 18:16:38 -0500

    COMP: Mark overridden Get/Set with itkVirtual* in Segmentation
    Convert legacy itkSet*/itkGet* macro calls at base-class members that an
    in-tree derived class overrides to the explicit itkVirtual* variant added
    in #6253. Zero behavior change: the legacy and itkVirtual* macros both
    emit `virtual` today. Each marker pins a member whose virtuality a
    derived class relies on, pre-staging the non-virtual default flip tracked
    in #6232 (refs #2785).
    
    Co-authored-by: Niels Dekker <N.Dekker@lumc.nl>
    Co-authored-by: Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>

--- Modules/Segmentation/LabelVoting/include/itkVotingBinaryImageFilter.h
+++ Modules/Segmentation/LabelVoting/include/itkVotingBinaryImageFilter.h
@@ -92,14 +92,14 @@ public:
   /** Birth threshold. Pixels that are OFF will turn ON when the number of
    * neighbors ON is larger than the value defined in this threshold. */
   /** @ITKStartGrouping */
-  itkGetConstReferenceMacro(BirthThreshold, unsigned int);
-  itkSetMacro(BirthThreshold, unsigned int);
+  itkVirtualGetConstReferenceMacro(BirthThreshold, unsigned int);
+  itkVirtualSetMacro(BirthThreshold, unsigned int);
   /** @ITKEndGrouping */
   /** Survival threshold. Pixels that are ON will turn OFF when the number of
    * neighbors ON is smaller than the value defined in this survival threshold. */
   /** @ITKStartGrouping */
-  itkGetConstReferenceMacro(SurvivalThreshold, unsigned int);
-  itkSetMacro(SurvivalThreshold, unsigned int);
+  itkVirtualGetConstReferenceMacro(SurvivalThreshold, unsigned int);
+  itkVirtualSetMacro(SurvivalThreshold, unsigned int);
   /** @ITKEndGrouping */
   /** VotingBinaryImageFilter needs a larger input requested region than
    * the output requested region.  As such, VotingBinaryImageFilter needs
