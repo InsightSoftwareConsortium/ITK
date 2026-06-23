2521ba1aa03af036afa285df4cabf526c9a40b4d
Bradley Lowekamp <blowekamp@mail.nih.gov>
2026-06-23 18:02:44 +0000

    BUG: Drop bundled-lib module deps when using system ThirdParty libs
    itk-module.cmake for HDF5, PNG, TIFF, and proxTV unconditionally
    declared DEPENDS on bundled ITK modules even when a system library
    was selected. Wrap each itk_module() call in the corresponding
    ITK_USE_SYSTEM_* conditional so the bundled-lib deps are absent for
    system builds.
    
    PNG and TIFF also lacked itk-module-init.cmake files, so their
    ITK_USE_SYSTEM_* options were invisible at itk-module.cmake time.
    Move the option()/mark_as_advanced() calls there.

--- Modules/ThirdParty/PNG/itk-module.cmake
+++ Modules/ThirdParty/PNG/itk-module.cmake
@@ -5,4 +5,8 @@ href=\"http://www.libpng.org/pub/png/libpng.html/\">Portable Network Graphics
 (PNG)</a> image file format library."
 )
 
-itk_module(ITKPNG DEPENDS ITKZLIB DESCRIPTION "${DOCUMENTATION}")
+if(ITK_USE_SYSTEM_PNG)
+  itk_module(ITKPNG DESCRIPTION "${DOCUMENTATION}")
+else()
+  itk_module(ITKPNG DEPENDS ITKZLIB DESCRIPTION "${DOCUMENTATION}")
+endif()
---
+++ Modules/ThirdParty/TIFF/itk-module-init.cmake
@@ -0,0 +1,6 @@
+option(
+  ITK_USE_SYSTEM_TIFF
+  "Use system-installed tiff"
+  ${ITK_USE_SYSTEM_LIBRARIES}
+)
+mark_as_advanced(ITK_USE_SYSTEM_TIFF)
