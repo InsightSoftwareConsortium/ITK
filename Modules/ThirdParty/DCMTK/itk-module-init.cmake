6f557421facb24f92a8cf925789c3992d771cc1d
Bradley Lowekamp <blowekamp@mail.nih.gov>
2026-06-23 13:37:27 +0000

    STYLE: Simplify DCMTK lib_prefix; remove dead lib_suffix
    Move lib_prefix into CMakeLists.txt next to its only use and reduce
    it to a single line: ICU is always built statically on non-Windows so
    CMAKE_STATIC_LIBRARY_PREFIX is the only relevant value. Remove
    lib_suffix entirely; it was set but never read.

--- Modules/ThirdParty/DCMTK/itk-module-init.cmake
+++ Modules/ThirdParty/DCMTK/itk-module-init.cmake
@@ -1,25 +1,6 @@
 ## Only present and build DCMTK options if ITKIODCMTK is requested
 
 option(ITK_USE_SYSTEM_DCMTK "Use an outside build of DCMTK." OFF)
-if(NOT WIN32)
-  set(lib_prefix lib)
-  if(BUILD_SHARED_LIBS)
-    set(lib_suffix "${CMAKE_SHARED_LIBRARY_SUFFIX}")
-    set(lib_prefix "${CMAKE_SHARED_LIBRARY_PREFIX}")
-  else()
-    set(lib_suffix "${CMAKE_STATIC_LIBRARY_SUFFIX}")
-    set(lib_prefix "${CMAKE_STATIC_LIBRARY_PREFIX}")
-  endif()
-else()
-  set(lib_prefix "")
-  if(BUILD_SHARED_LIBS)
-    set(lib_suffix "${CMAKE_IMPORT_LIBRARY_SUFFIX}")
-    set(lib_prefix "${CMAKE_IMPORT_LIBRARY_PREFIX}")
-  else()
-    set(lib_suffix "${CMAKE_STATIC_LIBRARY_SUFFIX}")
-    set(lib_prefix "${CMAKE_IMPORT_LIBRARY_PREFIX}")
-  endif()
-endif()
 
 if(ITK_USE_SYSTEM_DCMTK)
   # Use local FindDCMTK.cmake.
